4d4f774ab085ba917127367c7f823dd056ec9453
Mike Wheaton <mike@mikewheaton.ca>
2016-02-16 15:30:35 -0800

    Documents the properties of Dropdown.

--- src/demo/pages/examples/Dropdown/DropdownExample.tsx
+++ src/demo/pages/examples/Dropdown/DropdownExample.tsx
@@ -3,6 +3,7 @@ import Dropdown from '../../../../components/Dropdown';
 import Link from '../../../../components/Link';
 import ExampleCard from '../../../components/ExampleCard';
 import PropertiesTable from '../../../components/PropertiesTable';
+import DropdownProperties from './DropdownProperties';
 
 export default class DropdownExample extends React.Component<any, any> {
   public render() {
@@ -11,7 +12,7 @@ export default class DropdownExample extends React.Component<any, any> {
         <h1 className='ms-font-xxl'>Dropdown</h1>
         <div><Link target='_blank' text='Dropdowns' url='http://dev.office.com/fabric/components/dropdown' /> provide a way for users to choose an option.</div>
 
-        <PropertiesTable properties={ [] } />
+        <PropertiesTable properties={ DropdownProperties } />
 
         <h2 className='ms-font-xl'>Examples</h2>
 
