026e17b5d11dd4314e9ad5f14227b2b43d53e512
srdeshpa <srdeshpa@microsoft.com>
2016-03-02 14:03:10 -0800

    srdeshpa/removing the test code from textfield example.

--- src/demo/pages/examples/TextField/TextFieldExample.tsx
+++ src/demo/pages/examples/TextField/TextFieldExample.tsx
@@ -17,7 +17,7 @@ export default class TextFieldExample extends React.Component<any, any> {
         <h2 className='ms-font-xl'>Examples</h2>
 
         <ExampleCard title='Default text field'>
-          <TextField label="Some Label" onTextChange={ this._onTextChange.bind(this) }/>
+          <TextField label="Some Label"/>
         </ExampleCard>
 
         <ExampleCard title='Placeholder'>
@@ -39,9 +39,4 @@ export default class TextFieldExample extends React.Component<any, any> {
       </div>
     );
   }
-
-  private _onTextChange(newVal: any) {
-    window.alert(newVal);
-  }
-
 }
