2342ea9411eaed7d0bf2365b3e8f00c543d72dfe
Junle Li <junlel@microsoft.com>
2016-03-19 03:31:33 +0800

    Extending class name in Button.
    - Leverage the `css` helper to construct the class name.
    - Remove unnecessary onClick prop.
    - Update the property table.
    - Mention button properties are acceptable after property table.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './Button.scss';
+import { css } from '../../utilities/css';
 
 export enum ButtonType {
   normal,
@@ -13,7 +14,6 @@ export interface IButtonProps extends React.HTMLProps<HTMLButtonElement> {
   buttonType?: ButtonType;
   icon?: string;
   description?: string;
-  onClick?: (ev?: any) => void;
 }
 
 export default class Button extends React.Component<IButtonProps, any> {
@@ -22,12 +22,14 @@ export default class Button extends React.Component<IButtonProps, any> {
   };
 
   public render() {
-    let { buttonType, children, icon, description, onClick } = this.props;
-    let rootClass = 'ms-Button'
-      + (buttonType === ButtonType.primary ? ' ms-Button--primary' : '')
-      + (buttonType === ButtonType.hero ? ' ms-Button--hero' : '')
-      + (buttonType === ButtonType.compound ? ' ms-Button--compound' : '')
-      + (buttonType === ButtonType.command ? ' ms-Button--command' : '');
+    let { buttonType, children, icon, description } = this.props;
+
+    const className = css(this.props.className, 'ms-Button', {
+      'ms-Button--primary': buttonType === ButtonType.primary,
+      'ms-Button--hero': buttonType === ButtonType.hero,
+      'ms-Button--compound': buttonType === ButtonType.compound,
+      'ms-Button--command': buttonType === ButtonType.command
+    });
 
     const iconSpan = icon && (buttonType === ButtonType.command || buttonType === ButtonType.hero)
       ? <span className='ms-Button-icon'><i className={`ms-Icon ms-Icon--${icon}`}></i></span>
@@ -39,7 +41,7 @@ export default class Button extends React.Component<IButtonProps, any> {
     }
 
     return (
-      <button {...this.props} className={ rootClass } onClick={ onClick }>
+      <button { ...this.props } className={ className }>
         { iconSpan }
         <span className='ms-Button-label'>{ children }</span>
         { descriptionSpan }
