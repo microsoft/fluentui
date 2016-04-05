d36eb581ab1fd0da2860480f107252a36819c762
Thomas O'Connor <v-thocon@microsoft.com>
2016-04-05 13:48:50 -0700

    Fix Dialog Props documentation

--- src/components/Dialog/Dialog.Props.ts
+++ src/components/Dialog/Dialog.Props.ts
@@ -4,11 +4,24 @@ import { DialogType } from './interfaces';
 import { ResponsiveMode } from '../../utilities/decorators/withResponsiveMode';
 
 export interface IDialogProps extends React.Props<Dialog> {
-    isOpen?: boolean;                                /** Whether the dialog is displayed. */
-    type?: DialogType;                               /** The type of Dialog to display. */
-    onDismiss?: (ev?: React.MouseEvent) => any;      /** A callback function for when the Dialog is dismissed from the close button or light dismiss. */
-    title?: string;                                  /** The title text to display at the top of the dialog. */
-    subText?: string;                                /** The subtext to display in the dialog. */
-    isBlocking?: boolean;                            /** Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay). */
-    responsiveMode?: ResponsiveMode;
+  /** Whether the dialog is displayed. */
+  isOpen?: boolean;
+
+  /** The type of Dialog to display. */
+  type?: DialogType;
+
+  /** A callback function for when the Dialog is dismissed from the close button or light dismiss. */
+  onDismiss?: (ev?: React.MouseEvent) => any;
+
+  /** The title text to display at the top of the dialog. */
+  title?: string;
+
+  /** The subtext to display in the dialog. */
+  subText?: string;
+
+  /** Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay). */
+  isBlocking?: boolean;
+
+  /** Provided automatically, used to adjust component based on the current viewport */
+  responsiveMode?: ResponsiveMode;
 }
