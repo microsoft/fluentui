de076b10e3e0c8937476681f9673cb52fe1d3469
Thomas O'Connor <v-thocon@microsoft.com>
2016-04-05 16:40:16 -0700

    Add default values to Dialog Props documentation

--- src/components/Dialog/Dialog.Props.ts
+++ src/components/Dialog/Dialog.Props.ts
@@ -4,24 +4,41 @@ import { DialogType } from './interfaces';
 import { ResponsiveMode } from '../../utilities/decorators/withResponsiveMode';
 
 export interface IDialogProps extends React.Props<Dialog> {
-  /** Whether the dialog is displayed. */
+  /**
+  * Whether the dialog is displayed.
+  * @default false
+  */
   isOpen?: boolean;
 
-  /** The type of Dialog to display. */
+  /**
+  * The type of Dialog to display.
+  * @default DialogType.normal
+  */
   type?: DialogType;
 
-  /** A callback function for when the Dialog is dismissed from the close button or light dismiss. */
+  /**
+  * A callback function for when the Dialog is dismissed from the close button or light dismiss.
+  */
   onDismiss?: (ev?: React.MouseEvent) => any;
 
-  /** The title text to display at the top of the dialog. */
+  /**
+  * The title text to display at the top of the dialog.
+  */
   title?: string;
 
-  /** The subtext to display in the dialog. */
+  /**
+  * The subtext to display in the dialog.
+  */
   subText?: string;
 
-  /** Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay). */
+  /**
+  * Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay).
+  * @default false
+  */
   isBlocking?: boolean;
 
-  /** Provided automatically, used to adjust component based on the current viewport */
+  /**
+  * Provided automatically, used to adjust component based on the current viewport.
+  */
   responsiveMode?: ResponsiveMode;
 }
