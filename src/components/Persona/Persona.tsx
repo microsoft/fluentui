6289a71b592ee225249d6b44b5fca7e8d49b22c0
Harry Hao <hahao@microsoft.com>
2016-04-05 18:29:14 -0700

    Adding initialsColor prop to Persona component so that user can control the background color of the initials when the initials are displaying.

--- src/components/Persona/Persona.tsx
+++ src/components/Persona/Persona.tsx
@@ -20,11 +20,30 @@ export enum PersonaPresence {
   blocked
 }
 
+export enum PersonaInitialsColor {
+  lightBlue,
+  blue,
+  darkBlue,
+  teal,
+  lightGreen,
+  green,
+  darkGreen,
+  lightPink,
+  pink,
+  magenta,
+  purple,
+  black,
+  orange,
+  red,
+  darkRed
+}
+
 export interface IPersonaProps {
   primaryText: string;
   size?: PersonaSize;
   imageUrl?: string;
   imageInitials?: string;
+  initialsColor?: PersonaInitialsColor;
   presence?: PersonaPresence;
   secondaryText?: string;
   tertiaryText?: string;
@@ -52,21 +71,40 @@ const PERSONA_PRESENCE = {
   [PersonaPresence.blocked]: 'ms-Persona--blocked'
 };
 
+const PERSONA_INITIALS_COLOR = {
+  [PersonaInitialsColor.lightBlue]: 'ms-Persona-initials--lightBlue',
+  [PersonaInitialsColor.blue]: 'ms-Persona-initials--blue',
+  [PersonaInitialsColor.darkBlue]: 'ms-Persona-initials--darkBlue',
+  [PersonaInitialsColor.teal]: 'ms-color-Persona--teal',
+  [PersonaInitialsColor.lightGreen]: 'ms-Persona-initials--lightGreen',
+  [PersonaInitialsColor.green]: 'ms-Persona-initials--green',
+  [PersonaInitialsColor.darkGreen]: 'ms-Persona-initials--darkGreen',
+  [PersonaInitialsColor.lightPink]: 'ms-Persona-initials--lightPink',
+  [PersonaInitialsColor.pink]: 'ms-Persona-initials--pink',
+  [PersonaInitialsColor.magenta]: 'ms-Persona-initials--magenta',
+  [PersonaInitialsColor.purple]: 'ms-Persona-initials--purple',
+  [PersonaInitialsColor.black]: 'ms-Persona-initials--black',
+  [PersonaInitialsColor.orange]: 'ms-Persona-initials--orange',
+  [PersonaInitialsColor.red]: 'ms-Persona-initials--red',
+  [PersonaInitialsColor.darkRed]: 'ms-Persona-initials--darkRed'
+};
+
 export class Persona extends React.Component<IPersonaProps, any> {
   public static defaultProps: IPersonaProps = {
     primaryText: '',
     size: PersonaSize.regular,
-    presence: PersonaPresence.offline
+    presence: PersonaPresence.offline,
+    initialsColor: PersonaInitialsColor.blue
   };
 
   public render() {
-    let { className, size, imageUrl, imageInitials, presence, primaryText, secondaryText, tertiaryText, optionalText } = this.props;
+    let { className, size, imageUrl, imageInitials, initialsColor, presence, primaryText, secondaryText, tertiaryText, optionalText } = this.props;
 
     return (
       <div className={ css('ms-Persona', className, PERSONA_SIZE[size], PERSONA_PRESENCE[presence]) }>
         { size !== PersonaSize.tiny ? (
         <div className='ms-Persona-imageArea'>
-          <div className='ms-Persona-initials ms-Persona-initials--blue'>{ imageInitials }</div>
+          <div className={ css('ms-Persona-initials', PERSONA_INITIALS_COLOR[initialsColor]) }>{ imageInitials }</div>
           <Image className='ms-Persona-image' src={ imageUrl } />
         </div>
         ) : (null) }
--- src/demo/pages/PersonaPage/PersonaProps.ts
+++ src/demo/pages/PersonaPage/PersonaProps.ts
@@ -3,6 +3,7 @@ const PersonaProps = [
   { name: 'size', type: 'PersonaSize enum', defaultValue: 'regular', description: 'Decides the size of the control. { tiny, extraSmall, small, regular, large, extraLarge }' },
   { name: 'imageUrl', type: 'string', defaultValue: '', description: 'Url to the image to use, should be a square aspect ratio and big enough to fit in the image area.' },
   { name: 'imageInitials', type: 'string', defaultValue: '', description: `The user's initials to display in the image area when there is no image.` },
+  { name: 'initialsColor', type: 'PersonaInitialsColor enum', defaultValue: 'blue', description: `The background color when the user's initials are displayed.`},
   { name: 'presence', type: 'PersonaPresence enum', defaultValue: 'offline', description: 'Presence of the person to display. { offline, online, away, dnd, blocked }' },
   { name: 'secondaryText', type: 'string', defaultValue: '', description: 'Secondary text to display, usually the role of the user. (e.g. Actor)' },
   { name: 'tertiaryText', type: 'string', defaultValue: '', description: 'Tertiary text to display, usually the status of the user. (e.g. In a meeting)' },
