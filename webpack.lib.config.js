7fe1887fa11fbf847900588b683b79657f8e6081
David Zearing <dzearing@microsoft.com>
2016-04-13 12:57:52 -0700

    Removing dependency.

--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -3,9 +3,7 @@
 /** Note: this require may need to be fixed to point to the build that exports the gulp-core-build-webpack instance. */
 let webpackTaskResources = require('web-library-build').webpack.resources;
 let webpack = webpackTaskResources.webpack;
-
 let path = require('path');
-let WebpackNotifierPlugin = require('webpack-notifier');
 
 // Create an array of configs, prepopulated with a debug (non-minified) build.
 let configs = [
