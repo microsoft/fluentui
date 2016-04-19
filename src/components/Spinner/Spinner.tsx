2474c7389fff892383aff3c2661b94cbe35367e9
Thomas O'Connor <v-thocon@microsoft.com>
2016-04-19 14:08:10 -0700

    Fix Spinner RTL issues, fix small spinner rendering as squares.

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './Spinner.scss';
+import { getRTL } from '../../utilities/rtl';
 
 export interface ISpinnerProps {
   type?: SpinnerType;
@@ -11,7 +12,6 @@ export enum SpinnerType {
   large
 }
 
-const CIRCLE_OFFSET_SIZE = 0.179;
 const CIRCLE_COUNT = 8;
 
 export default class Spinner extends React.Component<ISpinnerProps, any> {
@@ -21,25 +21,26 @@ export default class Spinner extends React.Component<ISpinnerProps, any> {
 
   public render() {
     let { type, label } = this.props;
+    let isRTL = getRTL();
 
     let parentSize: number = type === SpinnerType.large ? 28 : 20;
 
-    let offset = parentSize * CIRCLE_OFFSET_SIZE;
+    let offsetSize: number = type === SpinnerType.large ? 0.179 : 0.2;
+    let offset = parentSize * offsetSize;
     let step = (2 * Math.PI) / CIRCLE_COUNT;
     let angle = 0;
     let i = CIRCLE_COUNT;
     let radius = (parentSize - offset) * 0.5;
-    let clientWidth = 0;
-    let clientHeight = 0;
     let circleObjects = [];
 
     while (i--) {
-      let x = Math.round(parentSize * 0.5 + radius * Math.cos(angle) - clientWidth * 0.5) - offset * 0.5;
-      let y = Math.round(parentSize * 0.5 + radius * Math.sin(angle) - clientHeight * 0.5) - offset * 0.5;
-      let size = parentSize * CIRCLE_OFFSET_SIZE + 'px';
+      let x = Math.round(parentSize * 0.5 + radius * Math.cos(angle)) - offset * 0.5;
+      let y = Math.round(parentSize * 0.5 + radius * Math.sin(angle)) - offset * 0.5;
+      let size = offset + 'px';
+      let style = { left: !isRTL ? x : 'auto', right: !isRTL ? 'auto' : x, top: y, width: size, height: size }
 
       angle += step;
-      circleObjects.push(<div className='ms-Spinner-circle' key={ i } style={ { left: x, top: y, width: size, height: size } } /> );
+      circleObjects.push(<div className='ms-Spinner-circle' key={ i } style={ style } /> );
     }
 
     let spinnerClass = type === SpinnerType.large ? 'ms-Spinner ms-Spinner--large' : 'ms-Spinner';
