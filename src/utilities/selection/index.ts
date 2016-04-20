01399fd8cadad0ebbeb0232a9b2579222d5e21f2
David Zearing <dzearing@microsoft.com>
2016-04-20 16:44:23 -0700

    Adding index files.

---
+++ src/utilities/selection/index.ts
@@ -0,0 +1,6 @@
+export * from './interfaces';
+export * from './Selection';
+export * from './SelectionLayout';
+
+export * from './SelectionZone';
+export { default as SelectionZone } from './SelectionZone';
