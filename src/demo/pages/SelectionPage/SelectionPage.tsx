794020ffdf01af8de252e09cceae11d755f2d759
Lambert Wang <t-lawang@microsoft.com>
2016-06-01 10:24:11 -0700

    Added linebreaks to SelectionPage text

--- src/demo/pages/SelectionPage/SelectionPage.tsx
+++ src/demo/pages/SelectionPage/SelectionPage.tsx
@@ -12,10 +12,14 @@ export class SelectionPage extends React.Component<any, any> {
       <div className='ms-SelectionPage'>
         <h1 className='ms-font-xxl'>Selection</h1>
         <p>
-          Selection is a store that maintains the selection state of items in an efficient way. It exposes methods for accessing the selection state given an item index. If the items change, it can resolve the selection if items move in the array.
+          Selection is a store that maintains the selection state of items in an efficient way.
+          It exposes methods for accessing the selection state given an item index.
+          If the items change, it can resolve the selection if items move in the array.
         </p>
         <p>
-          SelectionZone is a React component that handles selection change events. It can help abstract range selection, unselecting/selecting items based on selection modes, and handling common keystrokes like ctrl-A for select all and escape to clear selection.
+          SelectionZone is a React component that handles selection change events.
+          It can help abstract range selection, unselecting/selecting items based on selection modes,
+          and handling common keystrokes like ctrl-A for select all and escape to clear selection.
         </p>
         <h2 className='ms-font-xl'>Examples</h2>
         <ExampleCard title='Basic Selection Example' code={ SelectionBasicExampleCode }>
