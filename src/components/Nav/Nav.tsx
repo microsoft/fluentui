978392c4063fea7015605adb8f2d0c90fd505b3c
Ina Teegan <inateeg@microsoft.com>
2016-06-28 19:47:10 +0000

    PR 22216: iconLink color
     - iconLink: remove ms-Nav-Icon class bs it has special color not apply here

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -53,7 +53,7 @@ export class Nav extends React.Component<INavProps, INavState> {
           title={ link.name }
         >
           { (link.iconClassName !== '' ?
-          <i className={'ms-Nav-Icon ms-Icon ms-Nav-IconLink ' + link.iconClassName}></i>
+          <i className={'ms-Icon ms-Nav-IconLink ' + link.iconClassName}></i>
           : '') }
          { this.props.onRenderLink(link)}
         </a> { this._renderLinks(link.links) }
