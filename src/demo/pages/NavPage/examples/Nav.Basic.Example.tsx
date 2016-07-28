42873cc69587b1a21dce52f300c5b62e5fdd02a3
David Zearing <dzearing@microsoft.com>
2016-07-28 15:34:24 -0700

    Applying an undo of the recent Nav changes until we sort out the correct fix (#27)
    * Applying an undo of the recent Nav changes until we sort out the correct fix.
    
    * Adding anton as a PR approver.

--- src/demo/pages/NavPage/examples/Nav.Basic.Example.tsx
+++ src/demo/pages/NavPage/examples/Nav.Basic.Example.tsx
@@ -2,18 +2,13 @@ import * as React from 'react';
 import {
   Nav
 } from '../../../../index';
-import './Nav.Basic.Example.scss';
 
 export class NavBasicExample extends React.Component<any, any> {
   public render() {
     return (
-      <div className='ms-NavExample-LeftPane'>
-        <Nav
-          groups={ [ { name: 'LINK GROUP', links: [{ name: 'A link', url: 'http://example.com', links: [{name: 'A Child Link is a very long name', url: 'http://msn.com'} ], isExpanded: true}, { name: 'B link', url: 'http://example.com', links: [{name: 'B Child Link is a very long name', url: 'http://msn.com'} ], isExpanded: true}, {name: 'C Link no chid', url: 'http://msn.com'} ]}]}
-          expandedStateText={ 'expanded' }
-          collapsedStateText={ 'collapsed' }
-          />
-       </div>
+      <Nav
+        groups={ [ { name: 'LINK GROUP', links: [ { name: 'A link', url: 'http://example.com',iconClassName:'' }, {name: 'Edit', url: 'http://msn.com', iconClassName: 'ms-Icon--pencil'} ]}]}
+        />
     );
   }
 
