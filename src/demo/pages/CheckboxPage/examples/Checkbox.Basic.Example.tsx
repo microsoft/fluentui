11210998ff5a36646e782e732f2f2b82966fab4c
David Zearing <dzearing@microsoft.com>
2016-08-10 18:43:33 -0700

    Updating some samples to be more robust.

--- src/demo/pages/CheckboxPage/examples/Checkbox.Basic.Example.tsx
+++ src/demo/pages/CheckboxPage/examples/Checkbox.Basic.Example.tsx
@@ -3,18 +3,45 @@ import {
   Checkbox
   } from '../../../../index';
 
+export interface CheckboxBasicExample {
+  isChecked: boolean;
+}
+
 export class CheckboxBasicExample extends React.Component<any, any> {
   constructor() {
     super();
+
+    this.state = {
+      isChecked: false
+    };
+
     this._onCheckboxChange = this._onCheckboxChange.bind(this);
   }
 
   public render() {
+    let { isChecked } = this.state;
+
     return (
       <div>
-        <Checkbox label='Unselected item' onChange={ this._onCheckboxChange } />
-        <Checkbox label='Selected item' onChange={ this._onCheckboxChange } checked={ true } />
-        <Checkbox label='Disabled item' disabled={ true } />
+        <Checkbox
+          label='Uncontrolled checkbox'
+          onChange={ this._onCheckboxChange } />
+
+        <Checkbox
+          label='Uncontrolled checkbox with defaultChecked true'
+          defaultChecked={ true }
+          onChange={ this._onCheckboxChange } />
+
+        <Checkbox
+          label='Disabled uncontrolled checkbox with defaultChecked true'
+          disabled={ true }
+          defaultChecked={ true }
+          onChange={ this._onCheckboxChange } />
+
+        <Checkbox
+          label='Controlled checkbox'
+          checked={ isChecked }
+          onChange={ (ev, checked) => this.setState({ isChecked: checked }) } />
       </div>
     );
   }
