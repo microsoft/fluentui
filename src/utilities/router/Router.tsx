3b4f6d7f1834e5132ccb2fed9b217cad165ddca0
John Miller <miljo@microsoft.com>
2016-08-25 11:18:10 -0700

    Added the ability to add an anchro link to the end of the URL.

--- src/utilities/router/Router.tsx
+++ src/utilities/router/Router.tsx
@@ -4,10 +4,16 @@ import { EventGroup } from '../eventGroup/EventGroup';
 export interface IRouterProps {
   replaceState?: boolean;
   children?: React.ReactElement<any>[];
+  routerDidMount?: Function;
 }
 
 export interface IRouterState {
   path: string;
+}
+
+export interface urlObject {
+  anchorLink?: string;
+  route: string;
 }
 
 export class Router extends React.Component<IRouterProps, IRouterState> {
@@ -23,6 +29,7 @@ export class Router extends React.Component<IRouterProps, IRouterState> {
   }
 
   public componentDidUpdate(prevProps: IRouterProps, prevState: IRouterState) {
+
     if (this.state.path !== prevState.path) {
       window.scrollTo(0, 0);
     }
@@ -36,8 +43,16 @@ export class Router extends React.Component<IRouterProps, IRouterState> {
     this._events.on(window, 'hashchange', () => {
       if (this.state.path !== location.hash) {
         this.setState({ path: location.hash });
+
+        if (this.props.routerDidMount) {
+          this.props.routerDidMount();
+        }
       }
     });
+
+    if (this.props.routerDidMount) {
+      this.props.routerDidMount();
+    }
   }
 
   public componentWillUnmount() {
@@ -46,22 +61,37 @@ export class Router extends React.Component<IRouterProps, IRouterState> {
 }
 
 function _getComponent(matchPath, children) {
+  let path = matchPath;
   if (children && children.$$typeof) {
     children = [ children ];
+  }
+
+  if (_hasAnchorLink(path)) {
+    path = _extractBasePath(path);
   }
 
   for (let i = 0; children && i < children.length; i++) {
     let currentChild = children[i];
 
-    if (_match(matchPath, currentChild)) {
+    if (_match(path, currentChild)) {
       let component = currentChild.props.component;
-      let childComponent = _getComponent(matchPath, currentChild.props.children);
+      let childComponent = _getComponent(path, currentChild.props.children);
 
       return React.createElement(component, null, childComponent);
     }
   }
 
   return null;
+}
+
+function _hasAnchorLink(path) {
+  return (path.match(/#/g) || []).length > 1;
+}
+
+function _extractBasePath(path) {
+  let split = path.split('#');
+  split.splice(split.length -1, 1);
+  return '#' + split.join('');
 }
 
 function _match(currentPath, child): boolean {
