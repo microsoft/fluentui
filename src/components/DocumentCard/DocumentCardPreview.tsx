ed74cb6245201e02c8c9674fd73c0e16c0933ccb
Mike Wheaton <mike@mikewheaton.ca>
2016-08-25 18:16:05 -0700

    Move single image to its own render function

--- src/components/DocumentCard/DocumentCardPreview.tsx
+++ src/components/DocumentCard/DocumentCardPreview.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { IDocumentCardPreviewProps } from './DocumentCard.Props';
+import { IDocumentCardPreviewProps, IDocumentCardPreviewImage } from './DocumentCard.Props';
 import { Image } from '../../Image';
 import { Async } from '../../utilities/Async/Async';
 import './DocumentCardPreview.scss';
@@ -8,38 +8,60 @@ export class DocumentCardPreview extends React.Component<IDocumentCardPreviewPro
 
   public render() {
     let { previewImages } = this.props;
-    let previewImage = previewImages[0];
+    let style, preview;
+
+    if (previewImages.length > 1) {
+      // Render a list of files
+      preview = this._renderPreviewList();
+    } else {
+      // Render a single preview
+      preview = this._renderPreviewImage(previewImages[0]);
+
+      // Override the border color if an accent color was provided
+      if (previewImages[0].accentColor) {
+        console.log(previewImages[0].accentColor);
+        style = {
+          borderBottomColor: previewImages[0].accentColor
+        };
+      }
+    }
+
+    return (
+      <div className='ms-DocumentCardPreview' style={ style }>
+        { preview }
+      </div>
+    );
+  }
+
+  private _renderPreviewImage(previewImage: IDocumentCardPreviewImage): React.ReactElement<React.HTMLProps<HTMLDivElement>> {
     let { accentColor, width, height, imageFit } = previewImage;
 
-    let style;
-    if (accentColor) {
-      style = {
-        borderBottomColor: accentColor
-      };
-    }
+    let image = (
+      <Image
+        width={ width }
+        height={ height }
+        imageFit={ imageFit }
+        src={ previewImage.previewImageSrc }
+        errorSrc={ previewImage.errorImageSrc }
+        role='presentation' alt=''/>
+    );
 
     let icon;
     if (previewImage.iconSrc) {
       icon = <Image className='ms-DocumentCardPreview-icon' src={ previewImage.iconSrc } role='presentation' alt=''/>;
     }
 
-    let multiple;
-    if (previewImages.length > 1) {
-      multiple = <p>I have multiple documents</p>;
-    }
-
     return (
-      <div className='ms-DocumentCardPreview' style={ style }>
-        <Image
-          width={ width }
-          height={ height }
-          imageFit={ imageFit }
-          src={ previewImage.previewImageSrc }
-          errorSrc={ previewImage.errorImageSrc }
-          role='presentation' alt=''/>
+      <div>
+        { image }
         { icon }
-        { multiple }
       </div>
     );
   }
+
+  private _renderPreviewList(): React.ReactElement<React.HTMLProps<HTMLDivElement>> {
+    console.log('rendering a list of files with icons');
+    return <p>List of files with icons</p>;
+  }
+
 }
