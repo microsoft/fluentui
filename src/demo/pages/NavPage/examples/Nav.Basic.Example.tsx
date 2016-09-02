2c8442689af36c9625f26c20b884fc86bb70fa7a
inateeg <inateeg@microsoft.com>
2016-09-02 16:47:25 -0700

    add NL

--- src/demo/pages/NavPage/examples/Nav.Basic.Example.tsx
+++ src/demo/pages/NavPage/examples/Nav.Basic.Example.tsx
@@ -36,7 +36,6 @@ export class NavBasicExample extends React.Component<any, any> {
                   { name: 'Documents', url: 'http://example.com', isExpanded: true },
                   { name: 'Pages', url: 'http://msn.com' },
                   { name: 'Notebook', url: 'http://msn.com' },
-<<<<<<< HEAD
                   { name: 'Long Name Test for elipse', url: 'http://msn.com' },
                   { name: 'Edit Link', url: 'http://example.com', iconClassName: 'ms-Icon--Edit' },
                   {
@@ -45,9 +44,6 @@ export class NavBasicExample extends React.Component<any, any> {
                     onClick: this._onClickHandler,
                     iconClassName: 'ms-Icon--Edit'
                   }
-=======
-                  { name: 'Long Name Test for elipse', url: 'http://msn.com' }
->>>>>>> add long name to test case
                 ]
               }
             ]
