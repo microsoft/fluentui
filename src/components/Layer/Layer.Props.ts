74028f6b9a88805cf7f7fb5fc52f87464d334094
David Zearing <dzearing@microsoft.com>
2016-09-13 19:21:35 -0700

    lint

--- src/components/Layer/Layer.Props.ts
+++ src/components/Layer/Layer.Props.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Layer } from './Layer';
 
 export interface ILayerProps extends React.HTMLProps<HTMLDivElement> {
   /** Callback for when the layer is mounted. */
