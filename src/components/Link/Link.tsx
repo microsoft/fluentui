ef40e1043080d479dab7b53cd64dd5ee5c455805
Qingbo Liu <qingliu@microsoft.com>
2016-09-16 06:45:12 -0700

    recover preventDefault for popup window.

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -51,6 +51,7 @@ export class Link extends React.Component<ILinkProps, any> {
 
     if (popupWindowProps) {
       this._popupWindow(popupWindowProps);
+      ev.preventDefault();
     }
 
     if (onClick) {
