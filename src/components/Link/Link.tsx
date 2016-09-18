5980b32c2fe0983dec89b18508ed0f0c86462611
Qingbo Liu <qingliu@microsoft.com>
2016-09-17 19:35:23 -0700

    remove popupWindow at one time for link.

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -6,7 +6,7 @@ import {
   css,
   getNativeProps
 } from '../../Utilities';
-import { ILinkProps, IPopupWindowProps, PopupWindowPosition } from './Link.Props';
+import { ILinkProps } from './Link.Props';
 import './Link.scss';
 
 interface IMyScreen extends Screen {
@@ -47,58 +47,10 @@ export class Link extends React.Component<ILinkProps, any> {
 
   @autobind
   private _onClick(ev: React.MouseEvent) {
-    let { popupWindowProps, onClick } = this.props;
-
-    if (popupWindowProps) {
-      this._popupWindow(popupWindowProps);
-    }
+    let { onClick } = this.props;
 
     if (onClick) {
       onClick(ev);
-    }
-  }
-
-  private _popupWindow(popupWindowProps: IPopupWindowProps) {
-    const dualScreenLeft: number = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-    const dualScreenTop: number = window.screenTop !== undefined ? window.screenTop : screen.top;
-
-    const width: number = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ?
-      document.documentElement.clientWidth : screen.width;
-    const height: number = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ?
-      document.documentElement.clientHeight : screen.height;
-
-    let left: number = 0;
-    let top: number = 0;
-    switch (popupWindowProps.positionWindowPosition) {
-      case PopupWindowPosition.center:
-        left = ((width / 2) - (popupWindowProps.width / 2)) + dualScreenLeft;
-        top = ((height / 2) - (popupWindowProps.height / 2)) + dualScreenTop;
-        break;
-      case PopupWindowPosition.leftBottom:
-        left = dualScreenLeft;
-        top = (height - popupWindowProps.height) + dualScreenTop;
-        break;
-      case PopupWindowPosition.leftTop:
-        left = dualScreenLeft;
-        top = dualScreenTop;
-        break;
-      case PopupWindowPosition.rightBottom:
-        left = (width - popupWindowProps.width) + dualScreenLeft;
-        top = (height - popupWindowProps.height) + dualScreenTop;
-        break;
-      case PopupWindowPosition.rightTop:
-        left = (width - popupWindowProps.width) + dualScreenLeft;
-        top = dualScreenTop;
-        break;
-      default:
-        break;
-    }
-
-    const newWindow = window.open(this.props.href, popupWindowProps.title, 'scrollbars=yes, width='
-      + popupWindowProps.width + ', height=' + popupWindowProps.height + ', top=' + top + ', left=' + left);
-    // Puts focus on the newWindow
-    if (window.focus && newWindow) {
-      newWindow.focus();
     }
   }
 }
--- src/demo/pages/LinkPage/examples/Link.Basic.Example.tsx
+++ src/demo/pages/LinkPage/examples/Link.Basic.Example.tsx
@@ -12,6 +12,10 @@ export class LinkBasicExample extends React.Component<any, any> {
         <Link>I am a link without an href. </Link>
         <span> Not to be outdone, </span>
         <Link disabled={ true } href='http://dev.office.com/fabric/components/link'>I am a disabled link, even with an href.</Link>
+        <Link href='http://dev.office.com/fabric/components/link' onClick={(ev)=>{
+          alert('click now');
+          ev.preventDefault();
+        }}>I am a link with own OnClick method.</Link>
       </div>
     );
   }
