a24fd74c5896946b23efb8e9cc799837d526347e
Yimin Wu <yimwu@microsoft.com>
2016-09-19 11:08:58 -0700

    Use private method instead of inline method in DocumentCard. This is to avoid create new function every time component is rerendered.

--- src/components/DocumentCard/DocumentCardLocation.tsx
+++ src/components/DocumentCard/DocumentCardLocation.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
 import { IDocumentCardLocationProps } from './DocumentCard.Props';
 import './DocumentCardLocation.scss';
+import { autobind } from '../../utilities/autobind';
 
 export class DocumentCardLocation extends React.Component<IDocumentCardLocationProps, any> {
   public render() {
-    let { location, locationHref, onClick, ariaLabel } = this.props;
-
-    // If no onClick Function was provided and we do have an locationHref, create a function from it.
-    if (!onClick && locationHref) {
-      onClick = (ev: Event) => {
-        window.location.href = locationHref;
-        ev.preventDefault();
-        ev.stopPropagation();
-      };
-    }
+    let { location, locationHref, ariaLabel } = this.props;
 
     return (
-      <a className='ms-DocumentCardLocation' href={ locationHref } onClick={ onClick } aria-label={ ariaLabel }>{ location }</a>
+      <a className='ms-DocumentCardLocation' href={ locationHref } onClick={ this._onClick } aria-label={ ariaLabel }>{ location }</a>
     );
+  }
+
+  @autobind
+  private _onClick(ev: React.MouseEvent): void {
+    let { locationHref, onClick } = this.props;
+
+    if (onClick) {
+      onClick(ev);
+    } else if (!onClick && locationHref) {
+      // If no onClick Function was provided and we do have an locationHref, redirect to the locationHref
+      window.location.href = locationHref;
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
   }
 }
