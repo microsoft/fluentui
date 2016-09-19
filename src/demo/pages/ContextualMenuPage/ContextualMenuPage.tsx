5cc1917a60e81071ffb7006f8f4a5a10a3f813af
Peter Jahn <pejahn@microsoft.com>
2016-09-18 17:40:35 -0700

    Add updated ContextualMenu documentation

--- src/demo/pages/ContextualMenuPage/ContextualMenuPage.tsx
+++ src/demo/pages/ContextualMenuPage/ContextualMenuPage.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import {
-  Link
-} from '../../../index';
-import {
   ExampleCard,
   PropertiesTableSet,
   ComponentPage
@@ -59,8 +56,35 @@ export class ContextualMenuPage extends React.Component<IComponentDemoPageProps,
         }
         overview={
           <div>
-            <Link target='_blank' href='http://dev.office.com/fabric/components/contextualMenu'>ContextualMenus</Link>
-            <span> provide a menu for use in context menus and dropdowns.</span>
+            <p>
+              ContextualMenus are lists of commands that are based on the context of selection, mouse hover or keyboard focus. They are one of the most effective and highly used command surfaces, and can be used in a variety of places.
+            </p>
+            <p>
+              There are variants that originate from a command bar, or from cursor or focus. Those that come from CommandBars use a beak that is horizontally centered on the button. Ones that come from right click and menu button do not have a beak, but appear to the right and below the cursor. ContextualMenus can have submenus from commands, show selection checks, and icons.
+            </p>
+            <p>
+              Organize commands in groups divided by rules. This helps users remember command locations, or find less used commands based on proximity to others. One should also group sets of mutually exclusive or multiple selectable options. Use icons sparingly, for high value commands, and don’t mix icons with selection checks, as it makes parsing commands difficult. Avoid submenus of submenus as they can be difficult to invoke or remember.
+            </p>
+          </div>
+        }
+        dos={
+          <div>
+            <ul>
+              <li>Use to display commands.</li>
+              <li>Divide groups of commands with rules.</li>
+              <li>Use selection checks without icons.</li>
+              <li>Provide submenus for sets of related commands that aren’t as critical as others.</li>
+            </ul>
+          </div>
+        }
+        donts={
+          <div>
+            <ul>
+              <li>Use them to display content.</li>
+              <li>Show commands as one large group.</li>
+              <li>Mix checks and icons.</li>
+              <li>Create submenus of submenus.</li>
+            </ul>
           </div>
         }
         route={ this._url }
