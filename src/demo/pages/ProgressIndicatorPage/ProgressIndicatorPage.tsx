c58bddcdf870142251624f64f63ec7dda8b82aac
Peter Jahn <pejahn@microsoft.com>
2016-09-18 17:43:58 -0700

    Add updated documentation for Nav, ProgressIndicator, SearchBox, Slider, Spinner and Toggle

--- src/demo/pages/ProgressIndicatorPage/ProgressIndicatorPage.tsx
+++ src/demo/pages/ProgressIndicatorPage/ProgressIndicatorPage.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import {
-  Link
-} from '../../../index';
-import {
   ExampleCard,
   PropertiesTableSet,
   ComponentPage
@@ -59,10 +56,10 @@ export class ProgressIndicatorPage extends React.Component<IComponentDemoPagePro
               </li>
             </ul>
             <p>
-            Status text is generally in units elapsed and total units. If the operation can be canceled, an “X” icon is used and should be placed in the upper right, aligned with the baseline of the operation name. When an error occurs, replace the status text with the error description using ms-fontColor-redDark.
+              Status text is generally in units elapsed and total units. If the operation can be canceled, an “X” icon is used and should be placed in the upper right, aligned with the baseline of the operation name. When an error occurs, replace the status text with the error description using ms-fontColor-redDark.
             </p>
             <p>
-            Real-world examples include copying files to a storage location, saving edits to a file, and more. Use units that are informative and relevant to give the best idea to users of how long the operation will take to complete. Avoid time units as they are rarely accurate enough to be trustworthy. Also, combine steps of a complex operation into one total bar to avoid “rewinding” the bar. Instead change the operation description to reflect the change if necessary. Bars moving backwards reduce confidence in the service.
+              Real-world examples include copying files to a storage location, saving edits to a file, and more. Use units that are informative and relevant to give the best idea to users of how long the operation will take to complete. Avoid time units as they are rarely accurate enough to be trustworthy. Also, combine steps of a complex operation into one total bar to avoid “rewinding” the bar. Instead change the operation description to reflect the change if necessary. Bars moving backwards reduce confidence in the service.
             </p>
 
           </div>
