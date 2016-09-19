571f0513d5777cd74691be85c17554ff8649d084
Peter Jahn <pejahn@microsoft.com>
2016-09-18 18:46:33 -0700

    Add updated component documentation for Checkbox, ChoiceGroup, DatePicker, Label, Layer, Link, Overlay, Panel, PeoplePicker, Persona, Pivot, and TextField

--- src/demo/pages/DatePickerPage/DatePickerPage.tsx
+++ src/demo/pages/DatePickerPage/DatePickerPage.tsx
@@ -48,7 +48,30 @@ export class DatePickerPage extends React.Component<IComponentDemoPageProps, any
         }
         overview={
           <div>
-            Date pickers provide a menu for use in context menus and dropdowns.
+            <p>
+              The DatePicker component enables a user to pick a date value.
+            </p>
+          </div>
+        }
+        dos={
+          <div>
+            <ul>
+              <li>Use the control as a single entity.</li>
+              <li>Set the default date to the current date unless a specific date is required for context (e.g. the date of the conference).</li>
+              <li>The control is designed to resize relative to available screen width. Allow it to render in either wide or narrow as appropriate.</li>
+              <li>When the control is engaged, the DatePicker renders as a flyout and has defined widths (300px -narrow and 440px – wide). Plan your UI implementation accordingly.</li>
+              <li>The control renders date in a specific format. If allowing for manual entry of date, provide helper text in the appropriate format.</li>
+            </ul>
+          </div>
+        }
+        donts={
+          <div>
+            <ul>
+              <li>Don't attempt to break apart year from month/day selectors. If granularity is required, use the Dropdown control or something similar.</li>
+              <li>Don't attempt to force resize the control in any way.</li>
+              <li>Don't force the control to render one mode vs. the other (year or month/day)</li>
+              <li>The flyout selector is a light dismiss control. Don't modify this behavior in any way.</li>
+            </ul>
           </div>
         }
         related={
