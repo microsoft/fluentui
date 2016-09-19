104e446bd7e7ed044f4645568fd017cbc8e588f2
Mike Wheaton <mike@mikewheaton.ca>
2016-09-19 15:32:58 -0700

    Use actual MDL2 icon for show code button. Add text to clarify what the button does

--- src/demo/components/ExampleCard/ExampleCard.tsx
+++ src/demo/components/ExampleCard/ExampleCard.tsx
@@ -42,9 +42,9 @@ export class ExampleCard extends React.Component<IExampleCardProps, IExampleCard
           <span className='ExampleCard-title ms-font-l'>{ title }</span>
           <span className='ExampleCard-toggleButtons ms-font-l'>
             { code ? (
-            <Button buttonType={ ButtonType.primary }
-              onClick={ this._onToggleCodeClick } className={ isCodeVisible ? 'is-active' : ''}>
-              { '</>' }
+            <Button buttonType={ ButtonType.icon } icon='Embed'
+              onClick={ this._onToggleCodeClick } className={ css('codeButton', { 'is-active': isCodeVisible }) }>
+              { this.state.isCodeVisible ? 'Hide code' : 'Show code' }
             </Button>
             ) : ( null ) }
           </span>
