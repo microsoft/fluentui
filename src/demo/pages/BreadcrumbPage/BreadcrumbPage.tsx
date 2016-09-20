4e37f6e8ffd6d677e904a6fb7cc15bab0157a4fe
Edward Liu <edward.liu@webtellect.com>
2016-09-19 20:40:54 -0700

    change from span to paragraph for breadcrumb page

--- src/demo/pages/BreadcrumbPage/BreadcrumbPage.tsx
+++ src/demo/pages/BreadcrumbPage/BreadcrumbPage.tsx
@@ -40,7 +40,8 @@ export class BreadcrumbPage extends React.Component<IComponentDemoPageProps, any
         }
         overview={
           <div>
-            <span>Breadcrumbs should be used as a navigational aid in your app or site. They indicate the current page’s location within a hierarchy and help the user understand where they are in relation to the rest of that hierarchy. They also afford one-click access to higher levels of that hierarchy. Breadcrumbs are typically placed, in horizontal form, under the masthead or navigation of an experience, above the primary content area.</span> View <a target='_blank' href='http://dev.office.com/fabric/components/breadcrumb'>Breadcrumbs</a> on our website.
+            <p>Breadcrumbs should be used as a navigational aid in your app or site. They indicate the current page’s location within a hierarchy and help the user understand where they are in relation to the rest of that hierarchy. They also afford one-click access to higher levels of that hierarchy.</p>
+            <p>Breadcrumbs are typically placed, in horizontal form, under the masthead or navigation of an experience, above the primary content area.</p>
           </div>
         }
         related={
