379309d87eccbd174db2ec6c395e6f920762d45d
Yimin Wu <yimwu@microsoft.com>
2016-09-22 08:49:51 -0700

    pass through href and onClick directly to the anchor element inside of DocumentCardLocation

--- src/components/DocumentCard/DocumentCardLocation.tsx
+++ src/components/DocumentCard/DocumentCardLocation.tsx
@@ -1,28 +1,13 @@
 import * as React from 'react';
 import { IDocumentCardLocationProps } from './DocumentCard.Props';
 import './DocumentCardLocation.scss';
-import { autobind } from '../../utilities/autobind';
 
 export class DocumentCardLocation extends React.Component<IDocumentCardLocationProps, any> {
   public render() {
-    let { location, locationHref, ariaLabel } = this.props;
+    let { location, locationHref, ariaLabel, onClick } = this.props;
 
     return (
-      <a className='ms-DocumentCardLocation' href={ locationHref } onClick={ this._onClick } aria-label={ ariaLabel }>{ location }</a>
+      <a className='ms-DocumentCardLocation' href={ locationHref } onClick={ onClick } aria-label={ ariaLabel }>{ location }</a>
     );
-  }
-
-  @autobind
-  private _onClick(ev: React.MouseEvent): void {
-    let { locationHref, onClick } = this.props;
-
-    if (onClick) {
-      onClick(ev);
-    } else if (!onClick && locationHref) {
-      // If no onClick Function was provided and we do have an locationHref, redirect to the locationHref
-      window.location.href = locationHref;
-      ev.preventDefault();
-      ev.stopPropagation();
-    }
   }
 }
