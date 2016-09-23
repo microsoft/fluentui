8ff555a128d5be70058af666c6c1e6bdcbd7cdde
David Zearing <dzearing@microsoft.com>
2016-09-22 17:25:08 -0700

    Fixing merge.

--- src/components/DocumentCard/DocumentCardLocation.tsx
+++ src/components/DocumentCard/DocumentCardLocation.tsx
@@ -1,31 +1,13 @@
 import * as React from 'react';
-import { autobind } from '../../Utilities';
 import { IDocumentCardLocationProps } from './DocumentCard.Props';
 import './DocumentCardLocation.scss';
 
 export class DocumentCardLocation extends React.Component<IDocumentCardLocationProps, any> {
   public render() {
-    let { location, locationHref, ariaLabel } = this.props;
+    let { location, locationHref, ariaLabel, onClick } = this.props;
 
     return (
-      <a
-        className='ms-DocumentCardLocation'
-        href={ locationHref }
-        onClick={ this._onClick }
-        aria-label={ ariaLabel }>
-        { location }
-      </a>
+      <a className='ms-DocumentCardLocation' href={ locationHref } onClick={ onClick } aria-label={ ariaLabel }>{ location }</a>
     );
-  }
-
-  @autobind
-  private _onClick(ev?: React.MouseEvent) {
-    let { locationHref, onClick } = this.props;
-
-    if (!onClick && locationHref) {
-      window.location.href = locationHref;
-      ev.preventDefault();
-      ev.stopPropagation();
-    }
   }
 }
