27ff4897ba9091bc5af510d64df417b6ff540bdf
REDMOND\miclo <miclo@microsoft.com>
2016-10-10 09:47:49 -0700

    Fix up examples

--- src/demo/pages/DatePickerPage/examples/DatePicker.Basic.Example.tsx
+++ src/demo/pages/DatePickerPage/examples/DatePicker.Basic.Example.tsx
@@ -7,56 +7,6 @@ import {
 } from '../../../../index';
 
 const DayPickerStrings = {
-  months: [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ],
-
-  shortMonths: [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-  ],
-
-  days: [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ],
-
-  shortDays: [
-    'S',
-    'M',
-    'T',
-    'W',
-    'T',
-    'F',
-    'S'
-  ],
-
   goToToday: 'Go to today'
 };
 
