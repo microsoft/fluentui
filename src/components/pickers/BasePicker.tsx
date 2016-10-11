f09a23b5be42c18db37677fcc1b49a8e76fb1e16
Jon Schectman <joschect@microsoft.com>
2016-10-11 13:39:34 -0700

    added a generic picker where the selected list appears below

--- src/components/pickers/BasePicker.tsx
+++ src/components/pickers/BasePicker.tsx
@@ -356,3 +356,45 @@ export class BasePicker<T, P extends IBasePickerProps<T>> extends BaseComponent<
     }
   }
 }
+
+export class BasePickerListBelow<T, P extends IBasePickerProps<T>> extends BasePicker<T, P> {
+  public render() {
+    let { displayValue } = this.state;
+
+    return (
+      <div>
+        <div ref={ this._resolveRef('_root') } className='ms-BasePicker' onKeyDown={ this._onKeyDown }>
+          <SelectionZone selection={ this._selection }
+            selectionMode={SelectionMode.multiple}>
+            <div className='ms-BasePicker-text'>
+              <input ref={ this._resolveRef('_input') }
+                onFocus={ this._onInputFocus }
+                onChange={ this._onInputChange }
+                value={ displayValue }
+                className='ms-BasePicker-input'
+                />
+            </div>
+          </SelectionZone>
+        </div>
+        { this._renderSuggestions() }
+        <FocusZone ref={ this._resolveRef('_focusZone') }>
+          { this._renderItems() }
+        </FocusZone>
+
+      </div>
+    );
+  }
+
+  protected _onBackspace(ev: React.KeyboardEvent) {
+    let { value } = this.state;
+    if (ev.target === this._input) {
+      if (value && this._input.selectionStart !== this._input.selectionEnd) {
+        this._updateValue(value.substring(0, this._input.selectionStart));
+        // Since this effectively deletes a letter from the string we need to preventDefault so that
+        // the backspace doesn't try to delete a letter that's already been deleted. If a letter is deleted
+        // it can trigger the onChange event again which can have unintended consequences.
+        ev.preventDefault();
+      }
+    }
+  }
+}
