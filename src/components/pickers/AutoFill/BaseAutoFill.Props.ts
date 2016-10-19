c1736dfd011d9efa97c24d0eff52c27aa4c2bd03
Jon Schectman <joschect@microsoft.com>
2016-10-18 17:08:41 -0700

    added better comments

--- src/components/pickers/AutoFill/BaseAutoFill.Props.ts
+++ src/components/pickers/AutoFill/BaseAutoFill.Props.ts
@@ -2,23 +2,37 @@ import * as React from 'react';
 import { BaseAutoFill } from './BaseAutoFill';
 
 export interface IBaseAutoFillProps extends React.HTMLProps<HTMLInputElement | BaseAutoFill> {
-
+  /**
+   * The suggested autofill value that will display.
+   */
   suggestedDisplayValue?: string;
   /**
- * The aria label of the button for the benefit of screen readers.
- */
-  ariaLabel?: string;
-
+   * A callback for when the current input value changes.
+   */
   onInputValueChange?: (newValue?: string) => void;
   /**
-   * Detailed description of the button for the benefit of screen readers.
-   *
-   * Besides the compound button, other button types will need more information provided to screen reader.
+   * Corresponds to aria-description for screen readers.
    */
   ariaDescription?: string;
 
+  /**
+   * Corresponds to aria-label for screen readers.
+   */
+  ariaLabel?: string;
+  /**
+   * Corresponds to aria-activedescendant for screen readers.
+   */
   ariaActiveDescendant?: string;
+  /**
+   * Corresponds to aria-owns for screen readers.
+   */
   ariaOwns?: string;
+  /**
+   * Corresponds to aria-expanded for screen readers.
+   */
   ariaExpanded?: string;
+  /**
+   * Corresponds to aria-haspopup for screen readers.
+   */
   ariaHasPopup?: string;
 }
