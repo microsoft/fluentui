098ffeb4c4c23b0d8f0b39fe882ecf133631beb7
Jon Schectman <joschect@microsoft.com>
2016-10-21 15:44:29 -0700

    small nits

--- src/components/CommandBar/CommandBar.tsx
+++ src/components/CommandBar/CommandBar.tsx
@@ -144,11 +144,11 @@ export class CommandBar extends React.Component<ICommandBarProps, ICommandBarSta
   }
 
   private _renderItemInCommandBar(item: IContextualMenuItem, index: number, expandedMenuItemKey: string, isFarItem?: boolean) {
-    const itemKey = item.key || index;
+    const itemKey = item.key || String(index);
     const className = css(item.onClick ? 'ms-CommandBarItem-link' : 'ms-CommandBarItem-text', !item.name && 'ms-CommandBarItem--noName');
     const classNameValue = css(className, { 'is-expanded': (expandedMenuItemKey === item.key) });
 
-    return <div className={ css('ms-CommandBarItem', item.className) } key={ itemKey } ref={ itemKey.toString() }>
+    return <div className={ css('ms-CommandBarItem', item.className) } key={ itemKey } ref={ itemKey }>
              {(() => {
                if (item.onClick || item.items) {
                  return <button
