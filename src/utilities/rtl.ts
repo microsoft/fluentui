510af304af59a325aa16ae552d13c0a200626add
David Zearing <dzearing@microsoft.com>
2016-10-21 17:06:04 -0700

    Updating get/setRTL to be SSR friendly.

--- src/utilities/rtl.ts
+++ src/utilities/rtl.ts
@@ -1,13 +1,21 @@
 import { KeyCodes } from './KeyCodes';
 
-let _isRTL: boolean;
+let _isRTL: boolean = false;
+let _isInBrowser = (typeof document !== 'undefined');
 
 /**
  * Gets the rtl state of the page (returns true if in rtl.)
  */
 export function getRTL(): boolean {
   if (_isRTL === undefined) {
-    _isRTL = document.documentElement.getAttribute('dir') === 'rtl';
+    if (_isInBrowser) {
+      _isRTL = document.documentElement.getAttribute('dir') === 'rtl';
+    } else {
+      throw new Error(
+        'getRTL was called in a server environment without setRTL being called first. ' +
+        'Call setRTL to set the correct direction first.'
+        );
+    }
   }
 
   return _isRTL;
@@ -17,7 +25,10 @@ export function getRTL(): boolean {
  * Sets the rtl state of the page (by adjusting the dir attribute of the html element.)
  */
 export function setRTL(isRTL: boolean) {
-  document.documentElement.setAttribute('dir', isRTL ? 'rtl' : 'ltr');
+  if (_isInBrowser) {
+    document.documentElement.setAttribute('dir', isRTL ? 'rtl' : 'ltr');
+  }
+
   _isRTL = isRTL;
 }
 
