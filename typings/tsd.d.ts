40ddfe0898863198da38c9377e8a646b7756e16f
David Zearing <dzearing@microsoft.com>
2016-10-24 10:43:12 -0700

    Moving chair to @types.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="chai/chai.d.ts" />
 /// <reference path="webpack/webpack.d.ts" />
 /// <reference path="node/node.d.ts" />
