65afa4d200c4bbe0f02a10e7b183b3a49caed8ac
David Zearing <dzearing@microsoft.com>
2016-10-24 10:38:51 -0700

    Moving mocha typings to @types

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="mocha/mocha.d.ts" />
 /// <reference path="assertion-error/assertion-error.d.ts" />
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="webpack/webpack.d.ts" />
