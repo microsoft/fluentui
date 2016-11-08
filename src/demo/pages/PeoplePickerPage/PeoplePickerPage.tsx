13c8df96498a4ba01fd2c8b9bbecbaa5287b3c19
Jon Schectman <joschect@microsoft.com>
2016-11-08 10:41:27 -0800

    added a period

--- src/demo/pages/PeoplePickerPage/PeoplePickerPage.tsx
+++ src/demo/pages/PeoplePickerPage/PeoplePickerPage.tsx
@@ -47,7 +47,7 @@ export class PeoplePickerPage extends React.Component<IComponentDemoPageProps, a
         route={this._url}
         isHeaderVisible={this.props.isHeaderVisible}
         bestPractices={
-          <div>The PeoplePicker is used to select one or more entities, such as people or groups. Entry points for PeoplePickers are typically specialized TextField-like input fields known as a "well", which are used to search for recipients from a list. When a recipient is selected from the list, it is added to the well as a specialized Persona that can be interacted with or removed. Clicking on a Persona from the well should invoke a PersonaCard or open a profile pane for that recipient</div>
+          <div>The PeoplePicker is used to select one or more entities, such as people or groups. Entry points for PeoplePickers are typically specialized TextField-like input fields known as a "well", which are used to search for recipients from a list. When a recipient is selected from the list, it is added to the well as a specialized Persona that can be interacted with or removed. Clicking on a Persona from the well should invoke a PersonaCard or open a profile pane for that recipient.</div>
         }
         dos={
           <div>
