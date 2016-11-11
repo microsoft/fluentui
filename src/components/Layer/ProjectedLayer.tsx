e8059ec5cd0e420d61ab700bcf61a6ccd5004d0c
David Zearing <dzearing@microsoft.com>
2016-11-10 18:47:12 -0800

    Dropdown: Fixing accessibility issues using FocusZone and a few tweaks (#529)
    * Set correct ARIA attributes on Dropdown and focus selected items
    
    * Updates
    
    * Fix build error
    
    * Remove unnecessary application role and selected text from aria-label
    
    * Fixing a really bad bug in projected layer where we were rendering the content before the virtual parent was set up. Now we set the parent first and then render content.
    
    * Updating css to remove zindex and fix truncation for long strings.

--- src/components/Layer/ProjectedLayer.tsx
+++ src/components/Layer/ProjectedLayer.tsx
@@ -20,30 +20,47 @@ export interface IProjectedLayerProps extends React.Props<ProjectedLayer> {
   defaultRemoteProps: ILayerProps;
 }
 
+export interface IProjectedLayerState {
+  isMounted: boolean;
+}
+
 /**
  * ProjectedLayer is an internal helper component that projects the contents rendered within a Layer. It is created
  * by the corresponding LayerHost that the originating Layer communicates with.
  */
-export class ProjectedLayer extends BaseComponent<IProjectedLayerProps, {}> {
+export class ProjectedLayer extends BaseComponent<IProjectedLayerProps, IProjectedLayerState> {
   private _rootElement: HTMLElement;
   private _remoteProps: ILayerProps;
 
   constructor(props?: IProjectedLayerProps) {
     super(props);
 
+    this.state = {
+      isMounted: false
+    };
+
     this._remoteProps = props.defaultRemoteProps;
   }
 
   public shouldComponentUpdate() {
-    return false;
+    return !this.state.isMounted;
   }
 
   public componentDidMount() {
     setVirtualParent(this._rootElement, this.props.parentElement);
+    this.setState({ isMounted: true });
   }
 
   public render() {
     let remoteProps = getNativeProps<React.HTMLProps<HTMLDivElement>>(this._remoteProps, divProperties);
+
+    // If this is the first render, let's avoid rendering children until we're certain that we've set
+    // the virtual parent. After that, we can safely render the children, which in turn can safely call
+    // dom utilities like elementContains, which respects the virtual parent.
+
+    if (!this.state.isMounted) {
+      delete remoteProps.children;
+    }
 
     return (
       <div
