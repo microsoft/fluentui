9ec2e7967ffcadc249d5094d75f51bbfc64d55ce
REDMOND\miclo <miclo@microsoft.com>
2016-11-14 15:06:22 -0800

    Add test for bug

---
+++ src/components/ContextualMenu/ContextualMenu.test.tsx
@@ -0,0 +1,35 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+/* tslint:enable:no-unused-variable */
+
+import * as ReactDOM from 'react-dom';
+import * as ReactTestUtils from 'react-addons-test-utils';
+
+let { expect } = chai;
+
+import { ContextualMenu } from './ContextualMenu';
+import { IContextualMenuItem } from './ContextualMenu.Props';
+
+describe('ContextualMenu', () => {
+
+  it('does not have a scrollbar due to an overflowing icon', () => {
+    const items: IContextualMenuItem[] = [
+      { name: 'TestText 1', key: 'TestKey1', canCheck: true, isChecked: true },
+      { name: 'TestText 2', key: 'TestKey2', canCheck: true, isChecked: true },
+      { name: 'TestText 3', key: 'TestKey3', canCheck: true, isChecked: true },
+      { name: 'TestText 4', key: 'TestKey4', canCheck: true, isChecked: true },
+    ];
+
+    let component = ReactTestUtils.renderIntoDocument<ContextualMenu>(
+      <ContextualMenu
+        items={ items }
+        />
+    );
+
+    let renderedDOM = ReactDOM.findDOMNode(component as React.ReactInstance);
+    let menuList = renderedDOM.querySelector('.ms-ContextualMenu-list');
+
+    expect(menuList.scrollHeight).to.be.lte(menuList.clientHeight, "ContextualMenu is showing a scrollbar due to checkmark");
+  });
+
+});
