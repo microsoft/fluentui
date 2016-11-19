ebfb3a9b10ec939615fb8a24ba046c48a4efd1e0
Jon Schectman <joschect@microsoft.com>
2016-11-18 17:50:56 -0800

    adding in callout tests

---
+++ packages/office-ui-fabric-react/src/components/Callout/Callout.test.tsx
@@ -0,0 +1,111 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+/* tslint:enable:no-unused-variable */
+
+import * as ReactTestUtils from 'react-addons-test-utils';
+import { Callout } from './Callout';
+import { DirectionalHint } from '../../common/DirectionalHint';
+
+let { expect } = chai;
+
+describe('Callout', () => {
+
+    it('Callout target id strings does not throw exception', () => {
+
+        let threwException: boolean = false;
+        let exception;
+        try {
+            ReactTestUtils.renderIntoDocument<HTMLDivElement>(
+                <div>
+                    <button id='target' style={ { top: '10px', left: '10px', height: '0', width: '0px' } }> target </button>
+                    <Callout
+                        target='target'
+                        directionalHint={ DirectionalHint.topLeftEdge }
+                        >
+                        <div>
+                            Content
+                        </div>
+                    </Callout>
+                </div>
+            );
+        } catch (e) {
+            exception = e;
+            threwException = true;
+        }
+
+        expect(threwException).to.be.false;
+    });
+
+    it('Callout target MouseEvents does not throw exception', () => {
+        let mouseEvent = document.createEvent('MouseEvent');
+        let eventTarget = document.createElement('div');
+        mouseEvent.initMouseEvent('click', false, false, window, 0, 0, 0, 0, 0, false, false, false, false, 1, eventTarget);
+        let threwException: boolean = false;
+        try {
+
+            ReactTestUtils.renderIntoDocument<HTMLDivElement>(
+                <div>
+                    <Callout
+                        target={ eventTarget }
+                        directionalHint={ DirectionalHint.topLeftEdge }
+                        >
+                        <div>
+                            Content
+                        </div>
+                    </Callout>
+                </div>
+            );
+        } catch (e) {
+            threwException = true;
+        }
+
+        expect(threwException).to.be.false;
+    });
+
+    it('Callout target HTMLElements does not throw exception', () => {
+        let eventTarget = document.createElement('div');
+        document.body.appendChild(eventTarget);
+        let threwException: boolean = false;
+        try {
+
+            ReactTestUtils.renderIntoDocument<HTMLDivElement>(
+                <div>
+                    <Callout
+                        target={ eventTarget }
+                        directionalHint={ DirectionalHint.topLeftEdge }
+                        >
+                        <div>
+                            Content
+                        </div>
+                    </Callout>
+                </div>
+            );
+        } catch (e) {
+            threwException = true;
+        }
+
+        expect(threwException).to.be.false;
+    });
+
+    it('Callout without target does not throw exception', () => {
+        let threwException: boolean = false;
+        try {
+
+            ReactTestUtils.renderIntoDocument<HTMLDivElement>(
+                <div>
+                    <Callout
+                        directionalHint={ DirectionalHint.topLeftEdge }
+                        >
+                        <div>
+                            Content
+                        </div>
+                    </Callout>
+                </div>
+            );
+        } catch (e) {
+            threwException = true;
+        }
+        expect(threwException).to.be.false;
+    });
+
+});
