ac3d044608affa0edb364a22dda92f1eb38176e0
Vatan Aggarwal <vatanaggarwal4@live.com>
2016-11-21 16:35:26 -0800

    Set default value of presence to none in the People Picker sguggestion and selcted item

--- packages/office-ui-fabric-react/src/components/pickers/PeoplePicker/PeoplePickerItems/SelectedItemDefault.tsx
+++ packages/office-ui-fabric-react/src/components/pickers/PeoplePicker/PeoplePickerItems/SelectedItemDefault.tsx
@@ -24,7 +24,7 @@ export const SelectedItemDefault: (props: IPeoplePickerItemProps) => JSX.Element
       <div className='ms-PickerItem-content'>
         <Persona
           { ...item }
-          presence = { item.presence != null ? item.presence : PersonaPresence.online }
+          presence = { item.presence != null ? item.presence : PersonaPresence.none }
           size={ PersonaSize.extraSmall }
           />
       </div>
