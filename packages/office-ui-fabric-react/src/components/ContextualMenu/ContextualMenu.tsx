42b0b2d7a9d7ef83e46d1529e5e4913dfd46cd10
REDMOND\miclo <miclo@microsoft.com>
2016-11-22 15:16:12 -0800

    Don't close submenu on left arrow if arrowDirection is not vertical

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -344,7 +344,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
 
     if (ev.which === KeyCodes.escape
       || ev.which === KeyCodes.tab
-      || (ev.which === submenuCloseKey && this.props.isSubMenu)) {
+      || (ev.which === submenuCloseKey && this.props.isSubMenu && this.props.arrowDirection == FocusZoneDirection.vertical)) {
       // When a user presses escape, we will try to refocus the previous focused element.
       this._isFocusingPreviousElement = true;
       ev.preventDefault();
