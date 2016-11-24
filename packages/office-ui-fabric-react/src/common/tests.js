8297a12c7cb82b94c99441157b3acca53ebd4098
David Zearing <dzearing@microsoft.com>
2016-11-23 16:09:56 -0800

    TextField: No longer logs console warning when using defaultValue. (#606)
    * Fixing TextField.
    
    * Adding another test.
    
    * Tests: Adding Button tests, fixing spacing in ChoiceGroup tests.
    
    * Fixing test.
    
    * lint fix.
    
    * Adding type=button to details row check.
    
    * Fixing MarqueeSelection to only cancel click events after a successful marquee drag.

--- packages/office-ui-fabric-react/src/common/tests.js
+++ packages/office-ui-fabric-react/src/common/tests.js
@@ -1,4 +1,13 @@
-/** This is a test entry point to help karma-webpack find all tests in the project. Do not modify. */
+'use strict';
+
+/**
+ * This is a test entry point to help karma-webpack find all tests in the project.
+ **/
+
+// Before loading modules, treat errors and warnings as test failures.
+console.error = console.warn = function (warning) {
+  throw new Error(warning);
+}
 
 var context = require.context('..', true, /.+\.test\.js?$/);
 
