b7ef929f5d21f3a23a5a994ad025a7bd12f23606
Jon Schectman <joschect@microsoft.com>
2016-11-28 17:13:19 -0800

    adding start of test

---
+++ packages/office-ui-fabric-react/src/components/pickers/BasePicker.test.tsx
@@ -0,0 +1,12 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+/* tslint:enable:no-unused-variable */
+
+let { expect } = chai;
+
+import { BasePicker } from './BasePicker';
+
+describe('BasePicker', () => {
+
+});
