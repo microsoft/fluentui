4dfc945a19cc8476b512f01ad50a79c6a0e07315
zuzhan <zuzhan@microsoft.com>
2016-12-19 20:18:26 -0800

    Add support for customization of icon with <img> label.

--- packages/office-ui-fabric-react/src/components/Icon/Icon.Props.ts
+++ packages/office-ui-fabric-react/src/components/Icon/Icon.Props.ts
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { IconName } from './IconName';
+import { IconType } from './IconType';
 export interface IIconProps extends React.HTMLProps<HTMLElement> {
   iconName: IconName;
+  iconType?: IconType;
+  imageCss?: string;
 }
--- packages/office-ui-fabric-react/src/components/Icon/Icon.tsx
+++ packages/office-ui-fabric-react/src/components/Icon/Icon.tsx
@@ -1,8 +1,10 @@
 /* tslint:disable */
 import * as React from 'react';
+import './Icon.scss'
 /* tslint:enable */
 import { IIconProps } from './Icon.Props';
 import { IconName } from './IconName';
+import { IconType } from './IconType';
 import {
   css,
   getNativeProps,
@@ -11,7 +13,19 @@ import {
 
 export const Icon: (props: IIconProps) => JSX.Element = (props: IIconProps) => {
   let customIcon = props.iconName === IconName.None;
-  let className = css('ms-Icon', customIcon ? '' : ('ms-Icon--' + IconName[props.iconName]), props.className);
 
-  return <i { ...getNativeProps(props, htmlElementProperties) } className={ className } />;
+  if (props.iconType === IconType.IconSheet) {
+    let containerClassName = css('ms-Icon', 'ms-Icon-ImageContainer', props.className);
+    let imageClassName = css('ms-Icon', 'ms-Icon-Image', props.imageCss);
+
+    return (
+      <div className={ containerClassName }>
+        <img { ...getNativeProps(props, htmlElementProperties) } className={ imageClassName } />
+      </div>
+    );
+  } else {
+    let className = css('ms-Icon', customIcon ? '' : ('ms-Icon--' + IconName[props.iconName]), props.className);
+
+    return <i { ...getNativeProps(props, htmlElementProperties) } className={ className } />;
+  }
 };
