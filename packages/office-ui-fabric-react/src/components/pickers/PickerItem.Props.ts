b4d565f458b3206378a26f0f5d4bb0b2888bdd19
Jon Schectman <joschect@microsoft.com>
2016-12-20 16:46:58 -0800

    fixed typo

--- packages/office-ui-fabric-react/src/components/pickers/PickerItem.Props.ts
+++ packages/office-ui-fabric-react/src/components/pickers/PickerItem.Props.ts
@@ -5,7 +5,7 @@ export interface IPickerItemProps<T> extends React.Props<any> {
   selected?: boolean;
   onRemoveItem?: () => void;
   /**
-   * Internal Use only, gives a clalback to the renderer to call when an item has changed.
+   * Internal Use only, gives a callback to the renderer to call when an item has changed.
    * This allows the base picker to keep track of changes in the items.
    */
   onItemChange?: (item: T, index: number) => void;
