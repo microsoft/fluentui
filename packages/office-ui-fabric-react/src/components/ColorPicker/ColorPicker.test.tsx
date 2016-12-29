35d3639f6dda1a85beb3818a450654cd8cdcd608
Manish Dahamiwal <manishda@microsoft.com>
2016-12-29 15:12:23 +0530

    Adding colorpicker tests

---
+++ packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.test.tsx
@@ -0,0 +1,30 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+/* tslint:enable:no-unused-variable */
+
+import * as ReactDOM from 'react-dom';
+import * as ReactTestUtils from 'react-addons-test-utils';
+
+let { expect } = chai;
+
+import { ColorPicker } from './ColorPicker';
+
+describe('ColorPicker', () => {
+
+  it('Props are correctly parsed', () => {
+    let component = ReactTestUtils.renderIntoDocument(
+      <ColorPicker color='#FFFFFF'/>
+    ) as ColorPicker;
+
+    expect(component.state.color.hex).to.equal('ffffff');
+  });
+
+  it('Reacts to props changes', () => {
+    let component = ReactTestUtils.renderIntoDocument(
+      <ColorPicker color='#FFFFFF'/>
+    ) as ColorPicker;
+
+    component.componentWillReceiveProps({ color: '#AEAEAE' });
+    expect(component.state.color.hex).to.equal('aeaeae');
+  });
+});
