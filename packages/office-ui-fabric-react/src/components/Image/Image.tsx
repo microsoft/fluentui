012fa24ed205125198903e551f5b7df51b5f8028
Mike Wheaton <miwhea@microsoft.com>
2016-12-30 10:32:19 -0800

    Split attributes across multiple lines for readability

--- packages/office-ui-fabric-react/src/components/Image/Image.tsx
+++ packages/office-ui-fabric-react/src/components/Image/Image.tsx
@@ -82,7 +82,11 @@ export class Image extends BaseComponent<IImageProps, IImageState> {
 
     // If image dimensions aren't specified, the natural size of the image is used.
     return (
-      <div className={ css('ms-Image', className) } style={ { width: width, height: height } } ref={ this._resolveRef('_frameElement') }>
+      <div
+        className={ css('ms-Image', className) }
+        style={ { width: width, height: height } }
+        ref={ this._resolveRef('_frameElement') }
+        >
         <img
           { ...imageProps }
           className={
