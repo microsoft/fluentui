3662e84166647014f148a1f4d6c1ce90d62a159c
zuzhan <zuzhanzzm@gmail.com>
2017-01-04 09:15:42 -0800

    Icon: Add support for image sprites (#711)
    * Add support for customization of icon with <img> label.
    
    * In the process of adding Icon demo Page
    
    * Add demo page for the Icon (without text description including overview & best practices
    
    * Add overview/dos/donts of Icon
    
    * Add npmx change file
    
    * Use <Image> instead of <img> to implement image icon
    
    * Fix missing semicolon

--- packages/office-ui-fabric-react/src/components/Icon/Icon.tsx
+++ packages/office-ui-fabric-react/src/components/Icon/Icon.tsx
@@ -1,8 +1,11 @@
 /* tslint:disable */
 import * as React from 'react';
+import './Icon.scss'
 /* tslint:enable */
 import { IIconProps } from './Icon.Props';
 import { IconName } from './IconName';
+import { IconType } from './IconType';
+import { Image } from '../Image/Image';
 import {
   css,
   getNativeProps,
@@ -11,7 +14,18 @@ import {
 
 export const Icon: (props: IIconProps) => JSX.Element = (props: IIconProps) => {
   let customIcon = props.iconName === IconName.None;
-  let className = css('ms-Icon', customIcon ? '' : ('ms-Icon--' + IconName[props.iconName]), props.className);
 
-  return <i { ...getNativeProps(props, htmlElementProperties) } className={ className } />;
+  if (props.iconType === IconType.Image) {
+    let containerClassName = css('ms-Icon', 'ms-Icon-imageContainer', props.className);
+
+    return (
+      <div className={ containerClassName } >
+        <Image { ...props.imageProps as any } />
+      </div>
+    );
+  } else {
+    let className = css('ms-Icon', customIcon ? '' : ('ms-Icon--' + IconName[props.iconName]), props.className);
+
+    return <i { ...getNativeProps(props, htmlElementProperties) } className={ className } />;
+  }
 };
