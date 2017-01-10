37d48376e4864daa504299a9b9296f852c58adf5
Christopher Schleiden <cschleid@microsoft.com>
2017-01-09 20:29:02 -0800

    Rating: new star-rating component

---
+++ packages/office-ui-fabric-react/src/components/Rating/Rating.Props.ts
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+/**
+ * Rating component props.
+ */
+export interface IRatingProps extends React.HTMLProps<HTMLElement> {
+  /**
+   * Selected rating
+   */
+  rating?: number;
+
+  /**
+   * Minimum rating, defaults to 1, has to be >= 0
+   */
+  min?: number;
+
+  /**
+   * Maximum rating, defaults to 5, has to be <= min
+   */
+  max?: number;
+
+  /**
+   * Custom icon, defaults to FavoriteStar
+   */
+  icon?: string;
+
+  /**
+   * Size of rating, defaults to small
+   */
+  size?: RatingSize;
+
+  /**
+   * Callback when the rating has changed
+   */
+  onChanged?: (rating: number) => void;
+}
+
+export enum RatingSize {
+  Small,
+  Large
+}
