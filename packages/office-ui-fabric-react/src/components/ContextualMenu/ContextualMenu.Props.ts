0de482b932e5973bf70696ac18f78c62cb11f092
joschect <joschect@microsoft.com>
2017-01-11 13:01:57 -0800

    Positioning/Callout: Add the ability to specify that callout should not flip to reposition. (#771)
    * added the ability for callout and contextualmenu to fix the edge so that it is not flipped when positioning
    
    * fixed some minor problems added more tests
    
    * adding change file
    
    * accidentally deleted a comment
    
    * renamed isedgefixed to directionalHintFixed for clarity

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.Props.ts
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.Props.ts
@@ -135,6 +135,13 @@ export interface IContextualMenuProps extends React.Props<ContextualMenu> {
    */
   arrowDirection?: FocusZoneDirection;
 
+  /**
+   * If true the position will not change sides in an attempt to fit the ContextualMenu within bounds.
+   * It will still attempt to align it to whatever bounds are given.
+   * @default false
+   */
+  directionalHintFixed?: boolean;
+
 }
 
 export interface IContextualMenuItem {
