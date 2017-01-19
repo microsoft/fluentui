208ecd2f6a63678217c1f52d9c1e390e417eeca0
Jon Schectman <joschect@microsoft.com>
2017-01-18 18:49:53 -0800

    adding in text

--- packages/office-ui-fabric-react/src/components/pickers/BasePicker.test.tsx
+++ packages/office-ui-fabric-react/src/components/pickers/BasePicker.test.tsx
@@ -78,6 +78,33 @@ describe('Pickers', () => {
 
     });
 
+    it('correctly limits amount of items that can be selected', () => {
+      let root = document.createElement('div');
+      document.body.appendChild(root);
+      let picker: TypedBasePicker = ReactDOM.render(
+        <BasePickerWithType
+          onResolveSuggestions={ onResolveSuggestions }
+          onRenderItem={ (props: IPickerItemProps<{ key: string, name: string }>) => <div key={ props.item.name }>{ basicRenderer(props) }</div> }
+          onRenderSuggestionsItem={ basicSuggestionRenderer }
+          itemLimit={ 2 }
+          />,
+        root
+      ) as TypedBasePicker;
+      let input = document.querySelector('.ms-BasePicker-input') as HTMLInputElement;
+
+      clickSuggestionAtIndex(input, 'bl', 0, 2);
+
+      expect(picker.items.length).to.be.equal(1, 'There was not only 1 item selected');
+      expect(picker.items[0].name).to.be.equal('black', 'The selected item did not have the correct text');
+      expect(input.disabled).to.be.eq(false, 'Input was disabled even though item limit had not been reached');
+
+      clickSuggestionAtIndex(input, 'blu');
+
+      expect(picker.items.length).to.be.equal(2, 'There were not only 2 items selected');
+      expect(input.disabled).to.be.eq(true, 'Input was not disabled even though item limit reached');
+
+      ReactDOM.unmountComponentAtNode(root);
+    });
   });
 
   describe('TagPicker', () => {
@@ -92,23 +119,33 @@ describe('Pickers', () => {
         root
       ) as TagPicker;
       let input = document.querySelector('.ms-BasePicker-input') as HTMLInputElement;
-      input.focus();
-      input.value = 'bl';
 
-      ReactTestUtils.Simulate.change(input);
-
-      let suggestions = document.querySelector('.ms-Suggestions') as HTMLInputElement;
-
-      expect(suggestions).to.exist;
-      let suggestionOptions = document.querySelectorAll('.ms-Suggestions-item');
-
-      expect(suggestionOptions.length).to.be.equal(2, 'There were not 2 suggestions');
-      ReactTestUtils.Simulate.click(suggestionOptions[0]);
+      clickSuggestionAtIndex(input, 'bl', 0, 2);
 
       expect(picker.items.length).to.be.equal(1, 'There was not only 1 item selected');
       expect(picker.items[0].name).to.be.equal('black', 'The selected item did not have the correct text');
-      ReactDOM.unmountComponentAtNode(root);
 
+      ReactDOM.unmountComponentAtNode(root);
     });
   });
 });
+
+export function clickSuggestionAtIndex(input: HTMLInputElement, startingText: string, index: number = 0, expectedNumberOfSuggestions?: number) {
+  input.focus();
+  input.value = startingText;
+
+  ReactTestUtils.Simulate.change(input);
+
+  let suggestions = document.querySelector('.ms-Suggestions') as HTMLInputElement;
+
+  expect(suggestions).to.exist;
+  let suggestionOptions = document.querySelectorAll('.ms-Suggestions-item');
+
+  expect(suggestionOptions.length).to.be.gte(1, `There were no suggestions for the given text ${startingText}`);
+
+  if (expectedNumberOfSuggestions) {
+    expect(suggestionOptions.length).to.be.eq(expectedNumberOfSuggestions, `There were not ${expectedNumberOfSuggestions} suggestions`);
+  }
+
+  ReactTestUtils.Simulate.click(suggestionOptions[0]);
+}
