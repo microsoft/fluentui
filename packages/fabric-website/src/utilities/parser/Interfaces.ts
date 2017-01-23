131201d1e71483222568f46242078857c1ddf88d
John Miller <miljo@microsoft.com>
2017-01-23 15:58:45 -0800

    Fixing build

---
+++ packages/fabric-website/src/utilities/parser/Interfaces.ts
@@ -0,0 +1,30 @@
+
+export interface IProperty {
+  name: string;
+  propertyName: string;
+  propertyType: PropertyType;
+  property: IInterfaceProperty[] | IEnumProperty[];
+}
+
+export interface IInterfaceProperty {
+  name: string;
+  type: string;
+  defaultValue?: string;
+  description: string;
+}
+
+export interface IEnumProperty {
+  name: string;
+  description: string;
+}
+
+export interface IPropertiesTableProps {
+  title?: string;
+  properties: IInterfaceProperty[] | IEnumProperty[];
+  renderAsEnum?: boolean;
+  key?: string;
+}
+
+export enum PropertyType {
+  enum, interface
+}
