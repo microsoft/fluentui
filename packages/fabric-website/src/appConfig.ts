d97c9238edee398c5f3349f5f5c98526a29fdb6b
John Miller <miljo@microsoft.com>
2017-01-24 13:21:27 -0800

    Adding fabric website and changing paths to CDN

---
+++ packages/fabric-website/src/appConfig.ts
@@ -0,0 +1,1 @@
+export let baseURL = '/Modules/DevOffice.Fabric/';
---
+++ packages/fabric-website/src/pages/Components/TextFieldComponentPage.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { TextFieldPage } from 'office-ui-fabric-react/lib/demo/pages/TextFieldPage/TextFieldPage';
+import { PageHeader } from '../../components/PageHeader/PageHeader';
+import { ComponentPage } from '../../components/ComponentPage/ComponentPage';
+
+export class TextFieldComponentPage extends React.Component<any, any> {
+  public render() {
+    return (
+      <div ref='pageElement'>
+        <ComponentPage>
+          <PageHeader pageTitle='TextField' backgroundColor='#038387'
+          links={
+            [
+              {
+                'text': 'Overview',
+                'location': 'Overview'
+              },
+              {
+                'text': 'Variants',
+                'location': 'Variants'
+              },
+              {
+                'text': 'Implementation',
+                'location': 'Implementation'
+              }
+            ]
+          }/>
+          <TextFieldPage isHeaderVisible={ false } />
+        </ComponentPage>
+      </div>
+    );
+  }
+}
