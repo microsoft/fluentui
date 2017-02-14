aedd6d2cc24c0899f24f0bc86e715ae4bc382f95
David Zearing <dzearing@microsoft.com>
2017-02-14 12:24:31 -0800

    Dropdown: uncontrolled usage now correctly renders selected item. (#1003)
    * merge changes
    
    * Undoing unnecessaries.
    
    * Adding change file.
    
    * Update Dropdown.tsx

--- packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.test.tsx
+++ packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.test.tsx
@@ -2,26 +2,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 /* tslint:enable:no-unused-variable */
+import * as ReactTestUtils from 'react-addons-test-utils';
 
 let { expect } = chai;
 
 import { Dropdown } from './Dropdown';
 import { IDropdownOption } from './Dropdown.Props';
 
+const DEFAULT_OPTIONS: IDropdownOption[] = [
+  { key: '1', text: '1' },
+  { key: '2', text: '2' },
+  { key: '3', text: '3' }
+];
+
 describe('Dropdown', () => {
 
   it('Can flip between enabled and disabled.', () => {
-    const options: IDropdownOption[] = [
-      { key: '1', text: '1' },
-      { key: '2', text: '2' },
-      { key: '3', text: '3' }
-    ];
+
     let container = document.createElement('div');
     ReactDOM.render(
       <Dropdown
         label='testgroup'
-        options={ options }
-        />,
+        options={ DEFAULT_OPTIONS }
+      />,
       container);
     let dropdownRoot = container.querySelector('.ms-Dropdown') as HTMLElement;
 
@@ -32,12 +35,85 @@ describe('Dropdown', () => {
       <Dropdown
         disabled={ true }
         label='testgroup'
-        options={ options }
-        />,
+        options={ DEFAULT_OPTIONS }
+      />,
       container);
 
     expect(dropdownRoot.className).contains('is-disabled', `should be disabled`);
     expect(dropdownRoot.getAttribute('data-is-focusable')).equals('false', 'data-is-focusable');
+  });
+
+  it('Renders no selected item in default case', () => {
+    let container = document.createElement('div');
+
+    ReactDOM.render(
+      <Dropdown
+        label='testgroup'
+        options={ DEFAULT_OPTIONS }
+      />,
+      container);
+    let dropdownRoot = container.querySelector('.ms-Dropdown') as HTMLElement;
+    let titleElement = dropdownRoot.querySelector('.ms-Dropdown-title');
+
+    expect(titleElement.textContent).equals('');
+  });
+
+  it('Renders a selected item in uncontrolled case', () => {
+    let container = document.createElement('div');
+
+    ReactDOM.render(
+      <Dropdown
+        label='testgroup'
+        defaultSelectedKey='1'
+        options={ DEFAULT_OPTIONS }
+      />,
+      container);
+    let dropdownRoot = container.querySelector('.ms-Dropdown') as HTMLElement;
+    let titleElement = dropdownRoot.querySelector('.ms-Dropdown-title');
+
+    expect(titleElement.textContent).equals('1');
+  });
+
+  it('Renders a selected item in controlled case', () => {
+    let container = document.createElement('div');
+
+    ReactDOM.render(
+      <Dropdown
+        label='testgroup'
+        selectedKey='1'
+        options={ DEFAULT_OPTIONS }
+      />,
+      container);
+    let dropdownRoot = container.querySelector('.ms-Dropdown') as HTMLElement;
+    let titleElement = dropdownRoot.querySelector('.ms-Dropdown-title');
+
+    expect(titleElement.textContent).equals('1');
+  });
+
+  it('Can change items in uncontrolled case', () => {
+    let container = document.createElement('div');
+    let dropdownRoot;
+
+    document.body.appendChild(container);
+
+    try {
+      ReactDOM.render(
+        <Dropdown
+          label='testgroup'
+          defaultSelectedKey='1'
+          options={ DEFAULT_OPTIONS }
+        />,
+        container);
+      dropdownRoot = container.querySelector('.ms-Dropdown') as HTMLElement;
+
+      ReactTestUtils.Simulate.click(dropdownRoot);
+
+      let secondItemElement = document.querySelector('.ms-Dropdown-item[data-index="1"]');
+      ReactTestUtils.Simulate.click(secondItemElement);
+    }
+    finally {
+      expect(dropdownRoot.querySelector('.ms-Dropdown-title').textContent).equals('2');
+    }
   });
 
 });
