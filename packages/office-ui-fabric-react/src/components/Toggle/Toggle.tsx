d52453b25bf9079c2bacdf5b4432e491dd2f12c3
David Zearing <dzearing@microsoft.com>
2017-02-14 23:27:54 -0800

    Enable module css support. (#998)
    * Initial checkin of module css.
    
    * Adding :global wrappers for everything. This allows us to remove them incrementally.
    
    * Fixing focus border.
    
    * minor undos.
    
    * Fixing more nits found in module css global definitions.
    
    * Updating one more miss.
    
    * Update CommandButton.scss

--- packages/office-ui-fabric-react/src/components/Toggle/Toggle.tsx
+++ packages/office-ui-fabric-react/src/components/Toggle/Toggle.tsx
@@ -6,7 +6,7 @@ import {
 } from '../../Utilities';
 import { IToggleProps } from './Toggle.Props';
 import { Label } from '../../Label';
-import './Toggle.scss';
+import styles from './Toggle.scss';
 
 export interface IToggleState {
   isChecked: boolean;
@@ -55,34 +55,38 @@ export class Toggle extends React.Component<IToggleProps, IToggleState> {
 
     return (
       <div className={
-        css('ms-Toggle', className, {
+        css(styles.root, 'ms-Toggle', className, {
           'is-checked': isChecked,
           'is-enabled': !disabled,
-          'is-disabled': disabled
+          'is-disabled': disabled,
+          [styles.isChecked]: isChecked,
+          [styles.isEnabled]: !disabled,
+          [styles.isDisabled]: disabled,
+
         })
       }>
-        <div className='ms-Toggle-innerContainer'>
+        <div className={ css(styles.innerContainer, 'ms-Toggle-innerContainer') }>
           { label && (
             <Label className='ms-Toggle-label' htmlFor={ this._id }>{ label }</Label>
           ) }
-          <div className='ms-Toggle-slider'>
+          <div className={ css(styles.slider, 'ms-Toggle-slider') }>
             <button
               ref={ (c): HTMLButtonElement => this._toggleButton = c }
               type='button'
               id={ this._id }
               name={ this._id }
-              className='ms-Toggle-button'
+              className={ css(styles.button, 'ms-Toggle-button') }
               disabled={ disabled }
               role='checkbox'
               aria-checked={ isChecked }
               onClick={ this._onClick }
-              />
-            <div className='ms-Toggle-background'>
-              <div className='ms-Toggle-focus' />
-              <div className='ms-Toggle-thumb' />
+            />
+            <div className={ css(styles.background, 'ms-Toggle-background') }>
+              <div className={ css(styles.focus, 'ms-Toggle-focus') } />
+              <div className={ css(styles.thumb, 'ms-Toggle-thumb') } />
             </div>
             { stateText && (
-              <Label className='ms-Toggle-stateText'>{ stateText }</Label>
+              <Label className={ css(styles.stateText, 'ms-Toggle-stateText' )}>{ stateText }</Label>
             ) }
           </div>
 
