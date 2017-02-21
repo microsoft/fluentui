cf439a131cb753760f7983a45c394b40de2a5a9e
Tim Tong <timto@microsoft.com>
2017-02-21 14:32:11 -0800

    Add pagination to dropdown list

--- apps/fabric-examples/src/pages/DropdownPage/examples/Dropdown.Basic.Example.tsx
+++ apps/fabric-examples/src/pages/DropdownPage/examples/Dropdown.Basic.Example.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { Slider } from 'office-ui-fabric-react/lib/Slider';
 import './Dropdown.Basic.Example.scss';
 
 export class DropdownBasicExample extends React.Component<any, any> {
   constructor() {
     super();
     this.state = {
-      selectedItem: null
+      selectedItem: null,
+      pageSize: 10,
     };
   }
 
@@ -70,6 +72,50 @@ export class DropdownBasicExample extends React.Component<any, any> {
           }
         />
 
+        <Dropdown
+          label="Paginated dropdown sample:"
+          pageSize={ this.state.pageSize }
+          options={
+            [
+              { key: 'A', text: 'Option a' },
+              { key: 'B', text: 'Option b' },
+              { key: 'C', text: 'Option c' },
+              { key: 'D', text: 'Option d' },
+              { key: 'E', text: 'Option e' },
+              { key: 'F', text: 'Option f' },
+              { key: 'G', text: 'Option g' },
+              { key: 'H', text: 'Option h' },
+              { key: 'I', text: 'Option i' },
+              { key: 'J', text: 'Option j' },
+              { key: 'K', text: 'Option k' },
+              { key: 'L', text: 'Option l' },
+              { key: 'M', text: 'Option m' },
+              { key: 'N', text: 'Option n' },
+              { key: 'O', text: 'Option o' },
+              { key: 'P', text: 'Option p' },
+              { key: 'Q', text: 'Option q' },
+              { key: 'R', text: 'Option r' },
+              { key: 'S', text: 'Option s' },
+              { key: 'T', text: 'Option t' },
+              { key: 'U', text: 'Option u' },
+              { key: 'V', text: 'Option v' },
+              { key: 'W', text: 'Option w' },
+              { key: 'X', text: 'Option x' },
+              { key: 'Y', text: 'Option y' },
+              { key: 'Z', text: 'Option z' }
+            ]
+          }
+        />
+
+        <Slider
+          label='Set page size:'
+          min={ 1 }
+          max={ 26 }
+          step={ 1 }
+          defaultValue={ this.state.pageSize }
+          showValue={ true }
+          onChange={ (value) => this.setState({ pageSize: value }) }
+        />
       </div>
 
     );
