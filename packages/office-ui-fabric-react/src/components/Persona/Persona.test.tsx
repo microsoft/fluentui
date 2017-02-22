28a8a0176e61ae14c2a504df4376b74e4c0ab150
Qingbo Liu <qingliu@microsoft.com>
2017-02-21 17:52:38 -0800

    change persona.test.tsx to use module css.

--- packages/office-ui-fabric-react/src/components/Persona/Persona.test.tsx
+++ packages/office-ui-fabric-react/src/components/Persona/Persona.test.tsx
@@ -5,6 +5,7 @@ import { Persona } from './Persona';
 import { PersonaInitialsColor } from './Persona.Props';
 import { shallow } from 'enzyme';
 import * as chai from 'chai';
+import styles from './Persona.scss';
 
 const { expect } = chai;
 
@@ -16,38 +17,38 @@ describe('Persona', () => {
 
     it('renders with expected initialsColor if none was provided', () => {
       const wrapper = shallow(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.ms-Persona-initials--red');
+      let result = wrapper.find(styles.isRootInitialsRed);
       expect(result).to.have.length(1);
 
       const wrapper2 = shallow(<Persona primaryText='Annie Lindqvist' />);
-      result = wrapper2.find('.ms-Persona-initials--darkRed');
+      result = wrapper2.find(styles.isRootInitialsDarkRed);
       expect(result).to.have.length(1);
     });
 
     it('uses provided initialsColor if one was specified', () => {
       const wrapper = shallow(<Persona primaryText='Kat Larrson' initialsColor={ PersonaInitialsColor.lightBlue } />);
-      let result = wrapper.find('.ms-Persona-initials--lightBlue');
+      let result = wrapper.find(styles.isRootInitialsLightBlue);
       expect(result).to.have.length(1);
     });
 
     it('calculates an expected initials in LTR if one was not specified', () => {
       let wrapper = shallow(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.ms-Persona-initials');
+      let result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('KL');
 
       wrapper = shallow(<Persona primaryText='David Zearing-Goff' />);
-      result = wrapper.find('.ms-Persona-initials');
+      result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('DZ');
 
       wrapper = shallow(<Persona primaryText='4lex 4loo' />);
-      result = wrapper.find('.ms-Persona-initials');
+      result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('44');
 
       wrapper = shallow(<Persona primaryText='David (The man) Goff' />);
-      result = wrapper.find('.ms-Persona-initials');
+      result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('D');
     });
@@ -55,7 +56,7 @@ describe('Persona', () => {
     it('calculates an expected initials in RTL if one was not specified', () => {
       setRTL(true);
       const wrapper = shallow(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.ms-Persona-initials');
+      let result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('LK');
 
@@ -65,7 +66,7 @@ describe('Persona', () => {
     it('uses provided initial', () => {
       setRTL(true);
       const wrapper = shallow(<Persona primaryText='Kat Larrson' imageInitials='AT' />);
-      let result = wrapper.find('.ms-Persona-initials');
+      let result = wrapper.find(styles.initials);
       expect(result).to.have.length(1);
       expect(result.text()).to.equal('AT');
 
