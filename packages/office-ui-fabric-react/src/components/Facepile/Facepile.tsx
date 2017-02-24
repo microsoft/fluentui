b649f17b2da1367d2d004ba04b48096a0c4add93
Micah Godbolt <micahgodbolt@gmail.com>
2017-02-24 13:07:08 -0800

    Init fix for persona add button, need to fix persona first

--- packages/office-ui-fabric-react/src/components/Facepile/Facepile.tsx
+++ packages/office-ui-fabric-react/src/components/Facepile/Facepile.tsx
@@ -41,8 +41,8 @@ export class Facepile extends React.Component<IFacepileProps, {}> {
     }
 
     return (
-      <div className={css('ms-Facepile', styles.root)}>
-        <div className={css('ms-Facepile-members')}>
+      <div className={ css('ms-Facepile', styles.root) }>
+        <div className={ css('ms-Facepile-members') }>
           { showAddButton ? this._getAddNewElement() : null }
           <FocusZone direction={ FocusZoneDirection.horizontal }>
             {
@@ -56,7 +56,7 @@ export class Facepile extends React.Component<IFacepileProps, {}> {
             { overflowButtonProps ? this._getOverflowElement(numPersonasToShow) : null }
           </FocusZone>
         </div>
-        <div className={css('ms-Facepile-clear', styles.clear)}></div>
+        <div className={ css('ms-Facepile-clear', styles.clear) }></div>
       </div>
     );
   }
@@ -77,7 +77,7 @@ export class Facepile extends React.Component<IFacepileProps, {}> {
   private _getElementWithOnClickEvent(personaControl: JSX.Element, persona: IFacepilePersona, index: number): JSX.Element {
     return <button
       { ...getNativeProps(persona, buttonProperties) }
-      className={css('ms-Facepile-itemButton', styles.itemButton)}
+      className={ css('ms-Facepile-itemButton', styles.itemButton) }
       title={ persona.personaName }
       key={ (!!persona.imageUrl ? 'i' : '') + index }
       onClick={ this._onPersonaClick.bind(this, persona) }
@@ -91,7 +91,7 @@ export class Facepile extends React.Component<IFacepileProps, {}> {
   private _getElementWithoutOnClickEvent(personaControl: JSX.Element, persona: IFacepilePersona, index: number): JSX.Element {
     return <div
       { ...getNativeProps(persona, divProperties) }
-      className={css('ms-Facepile-itemButton', styles.itemButton)}
+      className={ css('ms-Facepile-itemButton', styles.itemButton) }
       title={ persona.personaName }
       key={ (!!persona.imageUrl ? 'i' : '') + index }
       onMouseMove={ this._onPersonaMouseMove.bind(this, persona) }
@@ -141,9 +141,17 @@ export class Facepile extends React.Component<IFacepileProps, {}> {
 
   private _getAddNewElement(): JSX.Element {
     let { personaSize } = this.props;
-    return <button { ...getNativeProps(this.props.addButtonProps, buttonProperties) }
-      className={ css('ms-Persona', PERSONA_SIZE[personaSize], 'ms-Facepile-addButton', 'ms-Facepile-itemButton', styles.addButton, styles.itemButton) }>
-      <i className='ms-Icon msIcon ms-Icon--AddFriend' aria-hidden='true'></i>
+    return <button
+      { ...getNativeProps(this.props.addButtonProps, buttonProperties) }
+      className={ css('ms-Facepile-itemButton', styles.itemButton) }
+    >
+      <Persona
+        className={ css('ms-Facepile-addButton', 'ms-Facepile-itemButton', styles.addButton) }
+        size={ personaSize }
+      >
+        <i className='ms-Icon msIcon ms-Icon--AddFriend' aria-hidden='true'></i>
+      </Persona>
+
     </button>;
   }
 
