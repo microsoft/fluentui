a71105c2298dc2170ecd740fc0609bdb90d61210
gitwick <saritwik@microsoft.com>
2017-03-09 12:54:20 -0800

    Breadcrumb: Add onRenderItem property (#1220)
    * Implement onRenderItem in Breadcrumb Control

--- apps/fabric-examples/src/pages/BreadcrumbPage/examples/Breadcrumb.Basic.Example.tsx
+++ apps/fabric-examples/src/pages/BreadcrumbPage/examples/Breadcrumb.Basic.Example.tsx
@@ -5,6 +5,9 @@ import {
   Breadcrumb, IBreadcrumbItem
 } from 'office-ui-fabric-react/lib/Breadcrumb';
 
+import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import { Link } from 'office-ui-fabric-react/lib/Link';
+
 export class BreadcrumbBasicExample extends React.Component<any, any> {
   constructor() {
     super();
@@ -34,6 +37,16 @@ export class BreadcrumbBasicExample extends React.Component<any, any> {
             { text: 'This is link 5', 'key': 'l5', href: '#/examples/breadcrumb', onClick: this._onBreadcrumbItemClicked },
           ] }
           maxDisplayedItems={ 3 } />
+        <Breadcrumb
+          items={ [
+            { text: 'Files', 'key': 'files', onClick: this._onBreadcrumbItemClicked },
+            { text: 'Folder 1', 'key': 'f1', onClick: this._onBreadcrumbItemClicked },
+            { text: 'Folder 2', 'key': 'f2', onClick: this._onBreadcrumbItemClicked },
+            { text: 'Folder 3', 'key': 'f3', onClick: this._onBreadcrumbItemClicked },
+            { text: 'Link', 'key': 'l', onClick: this._onBreadcrumbItemClicked }
+          ] }
+          onRenderItem={ this._onRenderBreadcrumbItem }
+          maxDisplayedItems={ 3 } />
       </div>
     );
   }
@@ -42,4 +55,27 @@ export class BreadcrumbBasicExample extends React.Component<any, any> {
     console.log(`Breadcrumb item with key "${item.key}" has been clicked.`);
   }
 
+  private _onRenderBreadcrumbItem(item: IBreadcrumbItem, defaultRender?: (item?: IBreadcrumbItem) => JSX.Element): JSX.Element {
+    let getIcon = (key: string) => {
+      if (key === 'files') {
+        return <Icon iconName={ 'OneDrive' } />;
+      } else if (key[0] === 'f') {
+        return <Icon iconName={ 'Folder' } />;
+      } else {
+        return <Icon iconName={ 'Link' } />;
+      }
+    };
+
+    return (
+      <span>
+        <Link
+          className='ms-Breadcrumb-itemLink'
+          href={ item.href }
+          onClick={ item.onClick }>
+          { getIcon(item.key) }
+        </Link>
+        { defaultRender(item) }
+      </span>
+    );
+  }
 }
