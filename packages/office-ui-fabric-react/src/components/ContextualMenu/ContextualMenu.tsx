e03f6f37cf3d1448ae15403935829308b5fb6afd
Eddie Liu <leddie24@gmail.com>
2017-03-10 15:56:36 -0800

    Remove nested role attribute for contextual menu

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -307,7 +307,6 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
             'ms-ContextualMenu-link',
             styles.link,
             (item.isDisabled || item.disabled) && 'is-disabled') }
-          role='menuitem'
           style={ item.style }
           onClick={ this._onAnchorClick.bind(this, item) }>
           { (hasIcons) ? (
@@ -342,7 +341,6 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
       onMouseLeave: this._onMouseLeave,
       onMouseDown: (ev: any) => this._onItemMouseDown(item, ev),
       disabled: item.isDisabled || item.disabled,
-      role: 'menuitem',
       href: item.href,
       title: item.title,
       'aria-label': ariaLabel,
