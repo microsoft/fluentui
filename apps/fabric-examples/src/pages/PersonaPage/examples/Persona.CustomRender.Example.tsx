421a06c9a973d49d112f75633d4b098f2e0d63c1
Ross Brodbeck <hross@users.noreply.github.com>
2017-03-13 12:34:20 -0400

    People Picker: Add render overrides (#1257)
    * Add custom rendering overrides to people picker props
    
    * Add to changelog

---
+++ apps/fabric-examples/src/pages/PersonaPage/examples/Persona.CustomRender.Example.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {
+  autobind,
+  css
+} from 'office-ui-fabric-react/lib/Utilities';
+import {
+  IPersonaProps,
+  Persona,
+  PersonaSize,
+  PersonaPresence
+} from 'office-ui-fabric-react/lib/Persona';
+import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import './PersonaExample.scss';
+
+const examplePersona = {
+  imageUrl: './images/persona-female.png',
+  imageInitials: 'AL',
+  primaryText: 'Annie Lindqvist',
+  secondaryText: 'Software Engineer',
+  tertiaryText: 'In a meeting',
+  optionalText: 'Available at 4:00pm'
+};
+
+export class PersonaCustomRenderExample extends React.Component<React.Props<PersonaCustomRenderExample>, any> {
+  constructor() {
+    super();
+  }
+
+  public render() {
+    return (
+      <div>
+        <Persona
+          { ...examplePersona }
+          size={ PersonaSize.large }
+          presence={ PersonaPresence.offline }
+          onRenderSecondaryText={ this._onRenderSecondaryText }
+        />
+      </div>
+    );
+  }
+
+  @autobind
+  private _onRenderSecondaryText(props: IPersonaProps): JSX.Element {
+    return (
+      <div>
+        <Icon iconName={ 'Suitcase' } className={ 'ms-JobIconExample' } />
+        { props.secondaryText }
+      </div>
+    );
+  }
+}
