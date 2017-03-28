abe9ecb8029cec769297a8d72808ac8284f5e4bc
Christopher Schleiden <cschleid@microsoft.com>
2017-03-27 19:56:34 -0700

    Website: Add Rating control

---
+++ apps/fabric-website/src/pages/Components/RatingComponentPage.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { RatingPage } from 'office-ui-fabric-react/lib/components/Rating/RatingPage';
+import { PageHeader } from '../../components/PageHeader/PageHeader';
+import { ComponentPage } from '../../components/ComponentPage/ComponentPage';
+
+export class RatingComponentPage extends React.Component<any, any> {
+  public render() {
+    return (
+      <div ref='pageElement'>
+        <ComponentPage>
+          <PageHeader pageTitle='Ratings' backgroundColor='#038387'
+            links={
+              [
+                {
+                  'text': 'Overview',
+                  'location': 'Overview'
+                },
+                {
+                  'text': 'Best Practices',
+                  'location': 'Best Practices'
+                },
+                {
+                  'text': 'Variants',
+                  'location': 'Variants'
+                },
+                {
+                  'text': 'Implementation',
+                  'location': 'Implementation'
+                }
+              ]
+            } />
+          <RatingPage isHeaderVisible={ false } />
+        </ComponentPage>
+      </div>
+    );
+  }
+}
