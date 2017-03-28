6d0c4d0c143ad8d67e3bdefbdd9a6448596a41c3
Aniket Handa <anihan@Anikets-MBP.guest.corp.microsoft.com>
2017-03-27 17:04:46 -0700

    Minor fixes

--- packages/office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Image.Example.tsx
+++ packages/office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Image.Example.tsx
@@ -31,7 +31,7 @@ export class ChoiceGroupImageExample extends React.Component<any, IChoiceGroupIm
               checked: this.state.imageKey === 'bar',
               imageSrc: './images/choicegroup-bar-unselected.png',
               selectedImageSrc: 'dist/choicegroup-bar-selected.png',
-              imageSize: { width: 50, height: 50 },
+              imageSize: { width: 32, height: 32 },
               text: 'Bar chart'
             },
             {
@@ -39,7 +39,7 @@ export class ChoiceGroupImageExample extends React.Component<any, IChoiceGroupIm
               checked: this.state.imageKey === 'pie',
               imageSrc: './images/choicegroup-pie-unselected.png',
               selectedImageSrc: 'dist/choicegroup-pie-selected.png',
-              imageSize: { width: 50, height: 50 },
+              imageSize: { width: 32, height: 32 },
               text: 'Pie chart'
             }
           ] }
