917ab3403a94442fdcfae1e72d8f775bbac452dd
David Zearing <dzearing@microsoft.com>
2017-04-01 11:15:13 -0700

    Updates to BaseComponent to support componentRef, adding initial Customizer implementation, adding warning helpers.

---
+++ packages/utilities/src/customizable.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+export function customizable<P>(componentName?: string) {
+  return function customizableFactory<P, S>(
+    ComposedComponent: (new (props: P, ...args: any[]) => React.Component<P, S>)
+  ): any {
+    return class ComponentWithInjectedProps extends React.Component<P, {}> {
+      public static contextTypes = {
+        injectedProps: React.PropTypes.object
+      };
+
+      public render() {
+        let defaultProps = ((this.context.injectedProps) ?
+          this.context.injectedProps[componentName] :
+          null) || {};
+
+        return (
+          <ComposedComponent { ...defaultProps } { ...this.props } />
+        );
+      }
+    };
+  };
+}
---
+++ packages/utilities/src/warn.ts
@@ -0,0 +1,63 @@
+let _warningCallback = _warn;
+
+export interface IStringMap {
+  [key: string]: string;
+}
+
+/**
+ * Warns when a deprecated props are being used.
+ *
+ * @export
+ * @param {string} componentName The name of the component being used.
+ * @param {Object} props The props passed into the component.
+ * @param {IStringMap} deprecationMap The map of deprecations, where key is the prop name and the value is
+ * either null or a replacement prop name.
+ */
+export function warnDeprecations(
+  componentName: string,
+  props: Object,
+  deprecationMap: IStringMap): void {
+
+  for (const propName in deprecationMap) {
+    if (props && propName in props) {
+      let deprecationMessage = `${componentName} property '${propName}' was used but has been deprecated.`;
+      const replacementPropName = deprecationMap[propName];
+
+      if (replacementPropName) {
+        deprecationMessage += ` Use '${replacementPropName}' instead.`;
+      }
+      _warningCallback(deprecationMessage);
+    }
+  }
+}
+
+export function warnMutuallyExclusive(
+  componentName: string,
+  props: Object,
+  exclusiveMap: IStringMap): void {
+
+  for (const propName in exclusiveMap) {
+    if (props && propName in props && exclusiveMap[propName] in props) {
+      _warningCallback(
+        `${componentName} property '${propName}' is mutually exclusive with '${exclusiveMap[propName]}'. Use one or the other.`
+      );
+    }
+  }
+}
+
+/**
+ * Configures the warning callback. Passing in undefined will reset it to use the default
+ * console.warn function.
+ *
+ * @export
+ * @param {(message) => void} warningCallback
+ */
+export function setWarningCallback(warningCallback: (message) => void): void {
+  _warningCallback = warningCallback === undefined ? _warn : warningCallback;
+}
+
+function _warn(message: string): void {
+  if (console && console.warn) {
+    console.warn(message);
+  }
+}
