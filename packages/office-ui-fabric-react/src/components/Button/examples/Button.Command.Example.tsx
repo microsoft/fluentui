b81fa02713805a041cf1bd0ca38727ffcc7ea735
David Zearing <dzearing@microsoft.com>
2017-04-01 10:26:04 -0700

    Reverting command button example

--- packages/office-ui-fabric-react/src/components/Button/examples/Button.Command.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Button/examples/Button.Command.Example.tsx
@@ -1,89 +1,31 @@
 import * as React from 'react';
 import {
-  CommandButton,
-  CompoundButton,
-  IconButton,
-  PrimaryButton,
-  DefaultButton,
-  IButton,
-  IButtonProps
+  CommandButton
 } from 'office-ui-fabric-react/lib/Button';
 import {
   Label
 } from 'office-ui-fabric-react/lib/Label';
-import { Customizer, autobind } from 'office-ui-fabric-react/lib/Utilities';
+import { IButtonProps } from '../Button.Props';
 
-const BUTTON_PROPS: IButtonProps[] = [
-  {
-    text: 'Text only button'
-  },
-  {
-    iconName: 'Mail',
-    text: 'Text/icon button'
-  },
-  {
-    iconName: 'Mail',
-    text: 'Text/icon/description button',
-    description: 'This is the description.'
-  },
-  {
-    iconName: 'Mail',
-    text: 'Text/icon/description/href button',
-    description: 'This is the description.',
-    href: 'http://www.bing.com',
-    target: '_blank',
-  },
-  {
-    iconName: 'Mail',
-    text: 'Text/icon/description/menu button',
-    description: 'This is the description.',
-    menuProps: {
-      items: [{
-        key: 'item A',
-        name: 'Item A',
-        iconProps: { iconName: 'Mail' }
-      }]
-    }
+export class ButtonCommandExample extends React.Component<IButtonProps, any> {
+  public constructor() {
+    super();
   }
-];
-
-const BUTTON_VARIANTS = [
-  { name: 'CommandButton', component: CommandButton },
-  { name: 'CompoundButton', component: CompoundButton },
-  { name: 'IconButton', component: IconButton },
-  { name: 'DefaultButton', component: DefaultButton },
-  { name: 'PrimaryButton', component: PrimaryButton }
-];
-
-export class ButtonCommandExample extends React.Component<IButtonProps, { any }> {
-  private _button: IButton;
 
   public render() {
     let { disabled } = this.props;
 
     return (
-      <Customizer settings={ { BaseButton: {} } } >
-        <div className='ms-BasicButtonsExample'>
-
-          <div>
-            <button>I'm a button</button>
-            <button className='ms-font-xxl'>I'm a button</button>
-            <button>I'm a button</button>
-            <button>I'm a button</button>
-          </div>
-
-          { BUTTON_VARIANTS.map(({ name, component: ButtonComponent }) => (
-            <div key={ name }>
-              <Label>{ name }</Label>
-              { BUTTON_PROPS.map((buttonProps, index) => (
-                <ButtonComponent key={ name + index } disabled={ disabled } { ...buttonProps } />
-              )) }
-            </div>
-          )) }
-
-        </div>
-      </Customizer>
+      <div className='ms-BasicButtonsExample'>
+        <Label>Command button</Label>
+        <CommandButton
+          data-automation-id='test'
+          icon='AddFriend'
+          disabled={ disabled }
+        >
+          Create account
+        </CommandButton>
+      </div>
     );
   }
-
 }
