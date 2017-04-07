c3e5d7880122c3479d2039e5538406eb14d8b558
gitjain <alreut@yahoo.com>
2017-04-07 15:36:39 -0700

    Update PrimaryButtonPage.visualtest.tsx

--- packages/office-ui-fabric-react/src/components/Button/PrimaryButtonPage.visualtest.tsx
+++ packages/office-ui-fabric-react/src/components/Button/PrimaryButtonPage.visualtest.tsx
@@ -1,6 +1,4 @@
 import { PrimaryButton } from './index';
-import { IconName } from '../../Icon';
-
 /* tslint:disable:no-unused-variable */
 import * as React from 'react';
 /* tslint:enable:no-unused-variable */
