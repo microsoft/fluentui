3e57650fbfad0c4efd1aa55c935ab6e8a2bd7462
Reena Jain (InConsulting Inc.) <v-reja@microsoft.com>
2017-04-10 16:23:49 -0700

    Regression Tests: ContextualMenu tests

--- packages/office-ui-fabric-react/src/VisualTestState.ts
+++ packages/office-ui-fabric-react/src/VisualTestState.ts
@@ -10,6 +10,7 @@ export const VisualTestState: IVisualTestState = {
     './Button/DefaultButtonPage.visualtest',
     './Button/PrimaryButtonPage.visualtest',
     './Checkbox/CheckboxPage.visualtest',
-    './ChoiceGroup/ChoiceGroupPage.visualtest'
+    './ChoiceGroup/ChoiceGroupPage.visualtest',
+    './ContextualMenu/ContextualMenuPage.visualtest'
   ]
 };
