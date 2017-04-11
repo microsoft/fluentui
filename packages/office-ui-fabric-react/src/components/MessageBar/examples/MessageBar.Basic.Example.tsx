76a993d50af8b82018010348c0d45cc04ded0541
Ben Truelove <betrue@microsoft.com>
2017-04-11 12:28:55 -0700

    Fixed link color to meet 4.5:1 contrast

--- packages/office-ui-fabric-react/src/components/MessageBar/examples/MessageBar.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/MessageBar/examples/MessageBar.Basic.Example.tsx
@@ -10,13 +10,13 @@ import './MessageBar.Basic.Example.scss';
 export const MessageBarBasicExample = () => (
   <div className='ms-BasicMessageBarsExample'>
     <Label>Info/Default MessageBar</Label>
-    <MessageBar>Info - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit.</MessageBar>
+    <MessageBar>Info - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit. <Link href='www.bing.com'>Visit our website</Link></MessageBar>
 
     <Label>Error MessageBar - only dismiss</Label>
     <MessageBar
       messageBarType={ MessageBarType.error }
       onDismiss={ () => { console.log('test'); } }>
-      Error - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit.</MessageBar>
+      Error - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit. <Link href='www.bing.com'>Visit our website</Link></MessageBar>
     <p>
       Add a close box when the user can safely dismiss the message. They’ll want to do this to reclaim space or if they feel too disrupted.
           </p>
@@ -31,7 +31,7 @@ export const MessageBarBasicExample = () => (
     <Label>SevereWarning MessageBar - multiline (default)</Label>
     <MessageBar
       messageBarType={ MessageBarType.severeWarning }
-      actions={ <div><Button>Yes</Button><Button>No</Button></div> }>SevereWarning - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit.</MessageBar>
+      actions={ <div><Button>Yes</Button><Button>No</Button></div> }>SevereWarning - lorem ipsum dolor sit amet, a elit sem interdum consectetur adipiscing elit. <Link href='www.bing.com'>Visit our website</Link></MessageBar>
 
     <Label>Success MessageBar - single line, long text with buttons</Label>
     <MessageBar
@@ -47,7 +47,7 @@ export const MessageBarBasicExample = () => (
       messageBarType={ MessageBarType.warning }
       ariaLabel='Aria help text here'
       actions={ <div><Button>Yes</Button><Button>No</Button></div> }>
-      Warning - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris.<Link href='www.bing.com'>Visit our website</Link>
+      Warning - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris. <Link href='www.bing.com'>Visit our website</Link>
     </MessageBar>
   </div>
 );
