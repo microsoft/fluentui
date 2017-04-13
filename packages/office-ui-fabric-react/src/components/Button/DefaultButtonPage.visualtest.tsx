03c919d6f250c6db534daccdfc6e4d051827e1ff
Reena Jain (InConsulting Inc.) <v-reja@microsoft.com>
2017-04-13 13:44:54 -0700

    update

--- packages/office-ui-fabric-react/src/components/Button/DefaultButtonPage.visualtest.tsx
+++ packages/office-ui-fabric-react/src/components/Button/DefaultButtonPage.visualtest.tsx
@@ -9,12 +9,12 @@ export default class DefaultButtonVPage extends React.Component<any, any> {
     return <div>
       <div><label> Default Button:   </label>
         <DefaultButton id='DefaultButton' icon='Add' text='Default Button' /></div>
-      <div><label> Default Button image:   </label>
-        <div><label> Default Button Disabled:   </label>
-          <DefaultButton id='DefaultButtonDisabled' disabled={ true } icon='Add' text='Default Button' /></div>
-        <div style={ { backgroundColor: 'white' } }>
-          <IconButton id={ 'IconButton' } icon={ iconName } />
-        </div>
-      </div>;
+
+      <div><label> Default Button Disabled:   </label>
+        <DefaultButton id='DefaultButtonDisabled' disabled={ true } icon='Add' text='Default Button' /></div>
+      <div style={ { backgroundColor: 'white' } }>
+        <IconButton id={ 'IconButton' } icon={ iconName } />
+      </div>
+    </div>;
   }
 }
