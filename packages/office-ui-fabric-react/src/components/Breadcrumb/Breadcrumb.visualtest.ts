85192df13dbd12ee3d5ea97d315d2c59d99634d1
REDMOND\v-reja <r33najain@gmail.com>
2017-04-17 14:23:46 -0700

    Regression Tests: breadcrumb tests updates

--- packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.visualtest.ts
+++ packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.visualtest.ts
@@ -7,11 +7,15 @@ casper.start(baseUrl + 'breadcrumb').
   then(function () {
     phantomcss.screenshot('.Breadcrumb', 'Breadcrumb_not_pressed');
   }).then(function () {
-    this.mouse.move('.Breadcrumb');
-    phantomcss.screenshot('.ms-Breadcrumb-listItem', 'Breadcrumb_hovered');
+    this.mouse.move('.ms-Breadcrumb-listItem');
+    phantomcss.screenshot('.Breadcrumb', 'Breadcrumb_hovered');
+
   }).then(function () {
     this.mouse.down('.ms-Breadcrumb-listItem');
     phantomcss.screenshot('.Breadcrumb', 'Breadcrumb_pressed');
+
   });
 casper.run(function () { casper.test.done(); });
 /* tslint:enable:no-function-expression */
+
+
