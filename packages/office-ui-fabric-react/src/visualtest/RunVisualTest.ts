7593f8f675e7e69a3f0c96d2f96aef5d8c9c35d0
REDMOND\v-reja <r33najain@gmail.com>
2017-04-19 15:08:44 -0700

    Regression Test: Helper Class updates

--- packages/office-ui-fabric-react/src/visualtest/RunVisualTest.ts
+++ packages/office-ui-fabric-react/src/visualtest/RunVisualTest.ts
@@ -5,20 +5,24 @@ declare var phantomcss: IPhantomCSS;
 
 import { Enum } from "typescript-string-enums";
 
+export const enum eventLayer {
+  SINGLE = 0,
+  DOUBLE = 1,
+};
+
 const fileExtn = Enum({
   DEFAULT: "_default",
   HOVERED: "_hovered",
   DOWN: "_pressed",
   CLICK: "_clicked"
-
 });
-
 
 export const enum screenEvent {
   DEFAULT = 0,
   HOVERED = 1,
   DOWN = 2,
-  CLICK = 3
+  CLICK = 3,
+  DOUBLECLICK = 4
 
 };
 
@@ -27,19 +31,29 @@ export const idType = Enum({
   ID: '#'
 });
 
+
 export class RunVisualTest {
+
+  private componentId: string;
+  private componentIdType;
+  private eventType: [eventLayer];
+  private eventList: [screenEvent];
+  private secondLayer: RunVisualTest;
+  private componentExtnid;
   private casps: Casper;
-  private id: string;
-  private events;
   private fileName: string;
   private phanta: IPhantomCSS;
 
-  constructor(casper, id, idExtn) {
+  constructor(casper, componentId, componentIdType, eventType, eventList, secondLayer) {
     this.casps = casper;
     this.phanta = phantomcss;
-    this.fileName = id.key;
-    this.events = id.value;
-    this.id = idExtn + id.key;
+    this.componentId = componentId;
+    this.componentIdType = componentIdType;
+    this.eventType = eventType;
+    this.eventList = eventList;
+    this.secondLayer = secondLayer;
+    this.fileName = componentId;
+    this.componentExtnid = componentIdType + componentId;
 
   }
 
@@ -50,66 +64,71 @@ export class RunVisualTest {
   public defaultScreenshot() {
     let self = this;
     this.casps.then(function () {
-      self.phanta.screenshot(self.id, self.fileName + fileExtn.DEFAULT);
+      self.phanta.screenshot(self.componentExtnid, self.fileName + fileExtn.DEFAULT);
     });
   }
 
   public mouseMoveScreenshot() {
     let self = this;
     this.casps.then(function () {
-      this.mouse.move(self.id);
-      self.phanta.screenshot(self.id, self.fileName + fileExtn.HOVERED);
+      this.mouse.move(self.componentExtnid);
+      self.phanta.screenshot(self.componentExtnid, self.fileName + fileExtn.HOVERED);
     });
   }
 
   public mouseDownScreenshot() {
     let self = this;
     this.casps.then(function () {
-      this.mouse.down(self.id);
-      self.phanta.screenshot(self.id, self.fileName + fileExtn.DOWN);
+      this.mouse.down(self.componentExtnid);
+      self.phanta.screenshot(self.componentExtnid, self.fileName + fileExtn.DOWN);
+    });
+  }
+
+  public mouseDoubleClickedScreenshot() {
+    let self = this;
+    this.casps.then(function () {
+      this.click(self.componentExtnid);
+      self.phanta.screenshot(self.componentExtnid, self.fileName + fileExtn.CLICK);
+      this.click(self.componentExtnid);
     });
   }
 
   public mouseClickedScreenshot() {
     let self = this;
     this.casps.then(function () {
-      this.click(self.id);
-      self.phanta.screenshot(self.id, self.fileName + fileExtn.CLICK);
-      this.click(self.id);
+      this.click(self.componentExtnid);
+      self.phanta.screenshot(self.componentExtnid, self.fileName + fileExtn.CLICK);
+
     });
   }
 
-  public listEventScreenshot() {
 
+
+  public listEventScreenshot() {
     let self = this;
-    self.events.map(function (event) {
-      switch (event) {
-        case screenEvent.CLICK:
-          self.casps.then(function () {
-            this.click(self.id);
-            self.phanta.screenshot(self.id, self.fileName + fileExtn.CLICK);
-            this.click(self.id);
-          });
-          break;
-        case screenEvent.DEFAULT:
-          self.casps.then(function () {
-            self.phanta.screenshot(self.id, self.fileName + fileExtn.DEFAULT);
-          });
-          break;
-        case screenEvent.DOWN:
-          self.casps.then(function () {
-            this.mouse.down(self.id);
-            self.phanta.screenshot(self.id, self.fileName + fileExtn.DOWN);
-          });
-          break;
-        case screenEvent.HOVERED:
-          self.casps.then(function () {
-            this.mouse.move(self.id);
-            self.phanta.screenshot(self.id, self.fileName + fileExtn.HOVERED);
-          });
-          break;
-      }
+    self.eventList.map(function (event) {
+      self.runEvent(event);
     });
 
+  }
+  public runEvent(event) {
+    let self = this;
+    switch (event) {
+      case screenEvent.DOUBLECLICK:
+        self.mouseDoubleClickedScreenshot();
+        break;
+      case screenEvent.CLICK:
+        self.mouseClickedScreenshot();
+        break;
+      case screenEvent.DEFAULT:
+        self.defaultScreenshot();
+        break;
+      case screenEvent.DOWN:
+        self.mouseDownScreenshot();
+        break;
+      case screenEvent.HOVERED:
+        self.mouseMoveScreenshot();
+        break;
+    }
   }
 }
--- packages/office-ui-fabric-react/src/visualtest/VisualTestHelper.ts
+++ packages/office-ui-fabric-react/src/visualtest/VisualTestHelper.ts
@@ -1,38 +1,51 @@
 import { Casper, IPhantomCSS } from './PhantomCssInterface';
 import { baseUrl } from '../common/VisualTest';
 import { RunVisualTest } from './RunVisualTest';
-import { idType, screenEvent } from './RunVisualTest';
+import { idType, screenEvent, eventLayer } from './RunVisualTest';
+
+
 
 declare var casper: Casper;
 
 
-var componentIds = [
-  {
-    key: "ContextualButton",
-    value: [screenEvent.DEFAULT, screenEvent.DOWN, screenEvent.HOVERED, screenEvent.CLICK]
-  },
-  {
-    key: "ContextualButtonDisabled",
-    value: [screenEvent.DEFAULT, screenEvent.DOWN, screenEvent.HOVERED, screenEvent.CLICK]
-  }
-];
+let componentIds = [];
 
+let eventList = [screenEvent.DEFAULT, screenEvent.DOWN, screenEvent.HOVERED, screenEvent.DOUBLECLICK];
+let component = new RunVisualTest(casper, 'ContextualButton', idType.ID, eventLayer.SINGLE, eventList, null);
+componentIds.push(component);
 
+eventList = [screenEvent.DEFAULT, screenEvent.DOWN, screenEvent.HOVERED, screenEvent.DOUBLECLICK];
+component = new RunVisualTest(casper, 'ContextualButtonDisabled', idType.ID, eventLayer.SINGLE, eventList, null);
+componentIds.push(component);
 
-var temp = [];
-
-componentIds.map(function (ids) {
-  temp.push(new RunVisualTest(casper, ids, idType.ID));
-});
+eventList = [screenEvent.DEFAULT];
+var childComponent = new RunVisualTest(casper, 'ms-ContextualMenu-list', idType.CLASSNAME, eventLayer.SINGLE, eventList, null);
+eventList = [screenEvent.CLICK];
+component = new RunVisualTest(casper, 'ContextualButton', idType.ID, eventLayer.DOUBLE, eventList, childComponent);
+componentIds.push(component);
 
 /* tslint:disable:no-function-expression */
 casper.
   start(baseUrl + 'contextualButton').
   then(function () {
-    temp.map(function (test) {
-      test.listEventScreenshot();
+    componentIds.map(function (test) {
+      switch (test.eventType) {
+        case eventLayer.SINGLE:
+          test.listEventScreenshot();
+          break;
+
+        case eventLayer.DOUBLE:
+          test.runEvent(test.eventList[0]);
+          var temp1: RunVisualTest;
+          casper.then(function () {
+            temp1 = test.secondLayer;
+            temp1.listEventScreenshot();
+          });
+
+          break;
+      }
     });
   });
-
 casper.run(function () { casper.test.done(); });
 /* tslint:enable:no-function-expression */
+
