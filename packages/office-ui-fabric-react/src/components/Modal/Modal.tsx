84d379c27e6196f081789c8543202fc1ecccb335
Micah Godbolt <micahgodbolt@gmail.com>
2017-04-20 15:10:20 -0700

    Update Modal.tsx

--- packages/office-ui-fabric-react/src/components/Modal/Modal.tsx
+++ packages/office-ui-fabric-react/src/components/Modal/Modal.tsx
@@ -63,7 +63,7 @@ export class Modal extends BaseComponent<IModalProps, IDialogState> {
 
     // Closing the dialog
     if (!newProps.isOpen && this.state.isOpen) {
-      this._onModalCloseTimer = this._async.setTimeout(this._onModalClose, 1000);
+      this._onModalCloseTimer = this._async.setTimeout(this._onModalClose, 300);
       this.setState({
         isVisible: false
       });
