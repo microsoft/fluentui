29d815a96a85b1019cf1cb6862eec4ce720f203a
David Zearing <dzearing@microsoft.com>
2017-05-04 11:54:52 -0700

    Re-enabling SSR tests, fixing SSR issues, removing deprecations (#1710)
    * Re-enabling SSR tests, fixing SSR issues.
    
    * Adding change file.
    
    * Removing debugger statements.
    
    * removing fsevents.

---
+++ apps/ssr-tests/webpack.config.js
@@ -0,0 +1,45 @@
+let path = require('path');
+let nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './test/test.js',
+
+  output: {
+    filename: 'dist/ssr-tests.js',
+  },
+
+  target: 'node',
+
+  externals: [
+    //  nodeExternals()
+    "vertx"
+  ],
+
+  node: {
+    fs: 'empty'
+  },
+
+  resolve: {
+    alias: {
+      'office-ui-fabric-react/src': path.join(__dirname, '../../packages/office-ui-fabric-react/src'),
+      'office-ui-fabric-react/lib': path.join(__dirname, '../../packages/office-ui-fabric-react/lib'),
+      'Props.ts.js': 'Props'
+    },
+    extensions: ['.js', '.tsx']
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    inline: true,
+    port: 4321
+  },
+
+  module: {
+    loaders: [
+    ]
+  },
+
+  plugins: [
+  ]
+}
