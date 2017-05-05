054f1a04e6a4df5d335536e8969474481ad6009a
Micah Godbolt <micahgodbolt@gmail.com>
2017-05-05 14:12:22 -0700

    Tooltip: Added custom content render function and exposed tooltip props to host

--- packages/office-ui-fabric-react/src/components/Tooltip/examples/Tooltip.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Tooltip/examples/Tooltip.Basic.Example.tsx
@@ -12,7 +12,7 @@ export class TooltipBasicExample extends BaseComponent<any, any> {
   public render() {
     return (
       <div>
-        <TooltipHost content='This is the tooltip' id='myID' calloutProps={ { gapSpace: 25 } }>
+        <TooltipHost content='This is the tooltip' id='myID' calloutProps={ { gapSpace: 0 } }>
           <DefaultButton aria-describedby='myID'>Hover Over Me</DefaultButton>
         </TooltipHost>
       </div>
