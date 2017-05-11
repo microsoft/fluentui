bbddcb76324ae849f4a813cb8645fc4e78d77b56
Christian Gonzalez <chrisgo@microsoft.com>
2017-05-10 17:08:39 -0700

    Fix bugs with overflow set to ensure that no overflow menu is rendered when the overflow items is empty or undefined

---
+++ packages/office-ui-fabric-react/src/components/OverflowSet/OverflowSet.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import { OverflowSet } from './OverflowSet';
+import * as sinon from 'sinon';
+
+describe('Overflow Set', () => {
+  it('does not render overflow when there are no overflow items', () => {
+    const onRenderItem = sinon.spy();
+    const onRenderOverflowButton = sinon.spy();
+    const wrapper = shallow(<OverflowSet onRenderItem={ onRenderItem } onRenderOverflowButton={ onRenderOverflowButton } />);
+
+    expect(onRenderOverflowButton.called).to.equal(false);
+  });
+
+  it('does not render overflow when overflow items is an empty array', () => {
+    const onRenderItem = sinon.spy();
+    const onRenderOverflowButton = sinon.spy();
+    const wrapper = shallow(<OverflowSet onRenderItem={ onRenderItem } onRenderOverflowButton={ onRenderOverflowButton } overflowItems={ [] } />);
+
+    expect(onRenderOverflowButton.called).to.equal(false);
+  })
+});
