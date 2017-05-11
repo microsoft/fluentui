17ff7182aa35520b28872ac7920b35d52142439d
Christian Gonzalez <chrisgo@microsoft.com>
2017-05-10 21:41:50 -0700

    Remove changes to overflowset.basic.example.tsx for reproing the bug

--- packages/office-ui-fabric-react/src/components/OverflowSet/examples/OverflowSet.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/OverflowSet/examples/OverflowSet.Basic.Example.tsx
@@ -1,96 +1,71 @@
 /* tslint:disable:no-unused-variable */
 import * as React from 'react';
 /* tslint:enable:no-unused-variable */
-import { BaseComponent, css, autobind } from 'office-ui-fabric-react/lib/Utilities';
+import { BaseComponent, css } from 'office-ui-fabric-react/lib/Utilities';
 import { IconButton } from 'office-ui-fabric-react/lib/Button';
 import { Link } from 'office-ui-fabric-react/lib/Link';
-import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox'
 import {
   OverflowSet
 } from 'office-ui-fabric-react/lib/OverflowSet';
 
 import * as stylesImport from './OverflowSet.Example.scss';
-
 const styles: any = stylesImport;
 
-export interface IOverflowSetBasicExampleState {
-  hasOverflowItems: boolean;
-}
-
-export class OverflowSetBasicExample extends BaseComponent<any, IOverflowSetBasicExampleState> {
-  public constructor() {
-    super();
-
-    this.state = {
-      hasOverflowItems: true
-    };
-  }
+export class OverflowSetBasicExample extends BaseComponent<any, any> {
 
   public render() {
-    let { hasOverflowItems } = this.state;
     return (
-      <div>
-        <OverflowSet
-          items={ [
-            {
-              key: 'item1',
-              name: 'Link 1',
-              ariaLabel: 'New. Use left and right arrow keys to navigate',
-              onClick: () => { return; },
-            },
-            {
-              key: 'item2',
-              name: 'Link 2',
-              onClick: () => { return; },
-            },
-            {
-              key: 'item3',
-              name: 'Link 3',
-              onClick: () => { return; }
-            }
-          ] }
-          overflowItems={ hasOverflowItems ? [
-            {
-              key: 'item4',
-              name: 'Overflow Link 1',
-              onClick: () => { return; }
-            },
-            {
-              key: 'item5',
-              name: 'Overflow Link 2',
-              onClick: () => { return; }
-            }
-          ] : undefined
+      <OverflowSet
+        items={ [
+          {
+            key: 'item1',
+            name: 'Link 1',
+            ariaLabel: 'New. Use left and right arrow keys to navigate',
+            onClick: () => { return; },
+          },
+          {
+            key: 'item2',
+            name: 'Link 2',
+            onClick: () => { return; },
+          },
+          {
+            key: 'item3',
+            name: 'Link 3',
+            onClick: () => { return; }
           }
+        ] }
+        overflowItems={ [
+          {
+            key: 'item4',
+            name: 'Overflow Link 1',
+            onClick: () => { return; }
+          },
+          {
+            key: 'item5',
+            name: 'Overflow Link 2',
+            onClick: () => { return; }
+          }
+        ]
+        }
           onRenderOverflowButton={ (overflowItems) => {
-            return (
-              <IconButton
-                className={ css(styles.overflowButton) }
-                iconProps={ { iconName: 'More' } }
-                menuIconProps={ null }
+          return (
+            <IconButton
+              className={ css(styles.overflowButton) }
+              iconProps={ { iconName: 'More' } }
+              menuIconProps={ null }
                 menuProps={ { items: overflowItems } }
-              />
-            );
-          } }
-          onRenderItem={ (item) => {
-            return (
-              <Link
-                className={ css(styles.overflowLinks) }
-                onClick={ item.onClick }
-              >{ item.name }</Link>
-            );
-          } }
-        />
-
-        <Checkbox label='Has Overflow Items' checked={ hasOverflowItems } onChange={ this._onHasOverflowItemsChange } />
-      </div>
+            />
+          );
+        } }
+        onRenderItem={ (item) => {
+          return (
+            <Link
+              className={ css(styles.overflowLinks) }
+              onClick={ item.onClick }
+            >{ item.name }</Link>
+          );
+        } }
+      />
     );
-  }
-
-  @autobind
-  private _onHasOverflowItemsChange(ev: React.FormEvent<HTMLElement>, isVisible: boolean) {
-    this.setState({
-      hasOverflowItems: isVisible
-    });
   }
 }
