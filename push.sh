4b560d3dd2472a99dce287fa550c4203358a17f1
Reena Jain (InConsulting Inc.) <v-reja@microsoft.com>
2017-05-10 17:47:05 -0700

    Regression test CI build update

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "alreut@yahoo.com"
+  git config --global user.name "gitjain"
 }
 
 commit_website_files() {
