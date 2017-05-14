e1f8cb3e015cd03bf3609bb7394e69568d4aeb5c
John Miller <miljo@microsoft.com>
2017-05-13 21:00:20 -0700

    Settup the karma tests and added one test to start

--- apps/fabric-website/gulpfile.js
+++ apps/fabric-website/gulpfile.js
@@ -52,5 +52,8 @@ build.task('webpack', build.webpack);
 build.task('tslint', build.tslint);
 build.task('ts', build.typescript);
 
+// Use Karma Tests - Disable during develoment if prefered
+build.karma.isEnabled = () => true;
+
 // initialize tasks.
 build.initialize(gulp);
