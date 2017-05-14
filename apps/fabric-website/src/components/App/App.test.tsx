07c5a283b696bd98c8d2f0007ad645fefe9cdf83
John Miller <miljo@microsoft.com>
2017-05-13 21:04:02 -0700

    Adding test files

---
+++ apps/fabric-website/src/components/App/App.test.tsx
@@ -0,0 +1,33 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+/* tslint:enable:no-unused-variable */
+
+import * as ReactDOM from 'react-dom';
+import * as ReactTestUtils from 'react-addons-test-utils';
+
+let { expect } = chai;
+
+import { App } from './App';
+
+describe('App', () => {
+  it('Has header.', () => {
+    let exception;
+    let threwException = false;
+    let app;
+    try {
+      app = ReactTestUtils.renderIntoDocument<App>(
+        <App />
+      );
+    } catch (e) {
+      exception = e;
+      threwException = true;
+    }
+    expect(threwException).to.be.false;
+
+    let renderedDOM = ReactDOM.findDOMNode(app as React.ReactInstance);
+    let headerElement = renderedDOM.querySelector('.od-Header');
+
+    expect(headerElement).to.not.be.undefined;
+
+  });
+});
