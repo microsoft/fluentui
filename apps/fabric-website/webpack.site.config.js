de46c2e7342a5c42b90e553d95642a0ad7766ae9
John Miller <miljo@microsoft.com>
2017-05-13 21:20:07 -0700

    Fixing the order of the file name change

--- apps/fabric-website/webpack.site.config.js
+++ apps/fabric-website/webpack.site.config.js
@@ -12,92 +12,92 @@ const version = require('./package.json').version;
 let publicPath = 'https://static2.sharepointonline.com/files/fabric/fabric-website/dist/';
 
 if (isDogfood) {
-  publicPath = 'https://static2df.sharepointonline.com/files/fabric/fabric-website/dist/';
+    publicPath = 'https://static2df.sharepointonline.com/files/fabric/fabric-website/dist/';
 } else if (!isProduction) {
-  publicPath = "/dist/";
+    publicPath = "/dist/";
 }
 
 // Create an array of configs, prepopulated with a debug (non-minified) build.
 let configs = [
-  createConfig(false, publicPath)
+    createConfig(false, publicPath)
 ];
 
 // Create a production config if applicable.
 if (isProduction) {
-  configs.push(createConfig(true, publicPath));
+    configs.push(createConfig(true, publicPath));
 }
 
 
 // Helper to create the config.
 function createConfig(isProduction, publicPath) {
-  let today = new Date();
-  let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  let minFileNamePart = isProduction ? '.min' : '';
-  let webpackConfig = {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let minFileNamePart = isProduction ? '.min' : '';
+    let webpackConfig = {
 
-    entry: {
-      'fabric-site': './lib/root.js'
-    },
+        entry: {
+            'fabric-site': './lib/root.js'
+        },
 
-    output: {
-      path: path.join(__dirname, '/dist'),
-      publicPath: publicPath,
-      filename: `${minFileNamePart}-[name].js`,
-      chunkFilename: `fabric-site-${version}${minFileNamePart}-[name].js`
-    },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            publicPath: publicPath,
+            filename: `[name]${minFileNamePart}.js`,
+            chunkFilename: `fabric-site-${version}-${minFileNamePart}[name].js`
+        },
 
-    devServer: {
-      stats: 'none'
-    },
+        devServer: {
+            stats: 'none'
+        },
 
-    externals: [
-      {
-        'react': 'React'
-      },
-      {
-        'react-dom': 'ReactDOM'
-      },
-    ],
+        externals: [
+            {
+                'react': 'React'
+            },
+            {
+                'react-dom': 'ReactDOM'
+            },
+        ],
 
-    resolve: {
-      alias: {
-        'office-ui-fabric-react/src': path.join(__dirname, 'node_modules/office-ui-fabric-react/src'),
-        'office-ui-fabric-react/lib': path.join(__dirname, 'node_modules/office-ui-fabric-react/lib')
-      },
-      extensions: ['', '.js']
-    },
+        resolve: {
+            alias: {
+                'office-ui-fabric-react/src': path.join(__dirname, 'node_modules/office-ui-fabric-react/src'),
+                'office-ui-fabric-react/lib': path.join(__dirname, 'node_modules/office-ui-fabric-react/lib')
+            },
+            extensions: ['', '.js']
+        },
 
-    module: {
-      noParse: [/autoit.js/],
+        module: {
+            noParse: [/autoit.js/],
 
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'source-map-loader',
-          enforce: 'pre'
-        }
-      ],
-    },
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'source-map-loader',
+                    enforce: 'pre'
+                }
+            ],
+        },
 
-    plugins: [
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'fabric-site.stats.html',
-        openAnalyzer: false
-      })
-    ]
-  };
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'fabric-site.stats.html',
+                openAnalyzer: false
+            })
+        ]
+    };
 
-  if (isProduction) {
-    webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }));
-  }
+    if (isProduction) {
+        webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }));
+    }
 
-  return webpackConfig;
+    return webpackConfig;
 }
 
 module.exports = configs;
