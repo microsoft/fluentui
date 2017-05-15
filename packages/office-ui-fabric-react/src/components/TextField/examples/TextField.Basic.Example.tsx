a02ea532622145204bcb7c6065066b7916cbaf7e
David Zearing <dzearing@microsoft.com>
2017-05-15 10:56:38 -0700

    Adding rush updates.

--- packages/office-ui-fabric-react/src/components/TextField/examples/TextField.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/TextField/examples/TextField.Basic.Example.tsx
@@ -15,7 +15,7 @@ export class TextFieldBasicExample extends React.Component<any, any> {
         <TextField label='TextField with a placeholder' placeholder='Now I am a Placeholder' ariaLabel='Please enter text here' />
         <TextField label='TextField with an icon' iconProps={ { iconName: 'calendar' } } />
         <TextField label='Textfield with an addon' addonString='https://' />
-        <TextField label='Multiline TextField' multiline rows={ 4 } iconClass='ms-Icon--Calendar ms-Icon' />
+        <TextField label='Multiline TextField' multiline rows={ 4 } iconProps={ { iconName: 'calendar' } } />
         <TextField label='Multiline TextField Unresizable' multiline resizable={ false } />
         <TextField label='Multiline TextField with auto adjust height' multiline autoAdjustHeight />
         <TextField label='Underlined TextField' underlined />
