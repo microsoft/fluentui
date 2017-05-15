a082ab7fd2c5b68012ff7a8f8db4e30099c4d3ab
Micah Godbolt <micahgodbolt@gmail.com>
2017-05-15 16:08:26 -0700

    Switched tests to enzyme mount

--- packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.test.tsx
+++ packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import { ResizeGroup } from './ResizeGroup';
 import * as sinon from 'sinon';
@@ -45,16 +45,18 @@ describe('ResizeGroup', () => {
 
     const div = document.createElement('div');
 
-    ReactDOM.render(<ResizeGroup
+    let wrapper = mount(<ResizeGroup
       data={ { a: 1 } }
       onReduceData={ onReduceData }
       onRenderData={ onRenderData }
-    />, div);
-    ReactDOM.render(<ResizeGroup
+    />);
+
+    wrapper = mount(<ResizeGroup
       data={ { a: 2 } }
       onReduceData={ onReduceData }
       onRenderData={ onRenderData }
-    />, div);
+    />);
+
 
     // onRenderData should get called to measure and to render when props are updated.
     expect(onRenderData.callCount).to.equal(4);
