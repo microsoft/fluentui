d777b97deb69f3b8e1a69ce4ed3fa028c6d40a44
Micah Godbolt <micahgodbolt@gmail.com>
2017-05-24 09:13:18 -0700

    Fixed searchbox on blur not working

--- packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.tsx
@@ -113,7 +113,7 @@ export class SearchBox extends BaseComponent<ISearchBoxProps, ISearchBoxState> {
       hasFocus: true
     });
 
-    this._events.on(getDocument().body, 'focus', this._handleDocumentFocus, true);
+    this._events.on(this._rootElement, 'blur', this._onBlur, true);
   }
 
   @autobind
@@ -140,6 +140,14 @@ export class SearchBox extends BaseComponent<ISearchBoxProps, ISearchBoxState> {
   }
 
   @autobind
+  private _onBlur(ev: React.ChangeEvent<HTMLInputElement>) {
+    this._events.off(this._rootElement, 'blur');
+    this.setState({
+      hasFocus: false
+    });
+  }
+
+  @autobind
   private _onInputChange(ev: React.ChangeEvent<HTMLInputElement>) {
     const value = this._inputElement.value;
     if (value === this._latestValue) {
@@ -149,15 +157,6 @@ export class SearchBox extends BaseComponent<ISearchBoxProps, ISearchBoxState> {
 
     this.setState({ value });
     this._callOnChange(value);
-  }
-
-  private _handleDocumentFocus(ev: FocusEvent) {
-    if (!elementContains(this._rootElement, ev.target as HTMLElement)) {
-      this._events.off(getDocument().body, 'focus');
-      this.setState({
-        hasFocus: false
-      });
-    }
   }
 
   private _callOnChange(newValue: string): void {
