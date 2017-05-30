b6634efe0d29aaeed26332375b05eba16d9d53a5
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-05-30 15:07:59 -0700

    ResizeGroup: Reduce render times when shrinking the contianer (#1881)
    * Don't rerender when everything already fits in a resize group.
    
    * Add comments
    
    * More accurate typing of _lastKnownRootWidth
    
    * update grammer, add new test
    
    * Rush change
    
    * When the window shrinks and the contents of the container don't fit, we should call onReduceData from the rendered data instead of starting from the beginning.
    
    * Remove early returns
    
    * Revert "Remove early returns"
    
    This reverts commit 277df88dcaa14d00b29d2d209d35dfea7846e38d.
    
    * Move to single return.
    
    * Remove all returns
    
    * Rush change

--- packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.tsx
+++ packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.tsx
@@ -92,19 +92,33 @@ export class ResizeGroup extends BaseComponent<IResizeGroupProps, IResizeGroupSt
   }
 
   private _onResize() {
-    // If we have some cached measurements, let's see if we can skip rendering
+    let shouldMeasure = true;
+    let nextMeasuredData = this.state.measuredData;
+
     if (this._root && this._lastKnownRootWidth && this._lastKnownMeasuredWidth) {
+      // If we have some cached measurements, let's see if we can skip rendering
       let containerWidth = this._root.getBoundingClientRect().width;
 
-      // If the container didn't grow and the component still fits, don't trigger a remeasure.
-      // If the container grew, we want to trigger a remeasure since we might be able to fit more content.
-      if (containerWidth <= this._lastKnownRootWidth && this._lastKnownMeasuredWidth <= containerWidth) {
-        this._lastKnownRootWidth = containerWidth;
-        return;
+      if (containerWidth <= this._lastKnownRootWidth) {
+        // If the container shrank as a result of this resize, we can do an optimized rerender.
+        if (this._lastKnownMeasuredWidth <= containerWidth) {
+          // If the contents still fit within the container, don't trigger a remeasure.
+          this._lastKnownRootWidth = containerWidth;
+          shouldMeasure = false;
+        } else {
+          // If the container shrank and the contents don't fit, we can trigger a measurement
+          // pass starting from the current value of rendered data.
+          nextMeasuredData = this.state.renderedData;
+        }
       }
     }
 
-    this.setState({ shouldMeasure: true });
+    if (shouldMeasure) {
+      this.setState({
+        shouldMeasure: true,
+        measuredData: nextMeasuredData
+      });
+    }
   }
 
   private _measureItems() {
