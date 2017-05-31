4b3bb8caef41cc4ea8b9567d4f8c5d312498314d
David Zearing <dzearing@microsoft.com>
2017-05-30 17:42:25 -0700

    Addressing a weird issue with merging processed styles.

--- packages/styling/src/examples/IconTile/IconTile.tsx
+++ packages/styling/src/examples/IconTile/IconTile.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
-import { IconCodes } from '@uifabric/styling';
+import { IconClassNames } from '@uifabric/styling';
 import { IIconTileStyles, getStyles } from './IconTile.styles';
 
 export interface IIconTileProps extends React.HTMLProps<HTMLDivElement> {
@@ -8,12 +8,12 @@ export interface IIconTileProps extends React.HTMLProps<HTMLDivElement> {
 }
 
 export function IconTile(props: IIconTileProps): JSX.Element {
-  const { iconName, ...divProps }: IIconTileProps = props;
+  const { iconName, children, ...divProps }: IIconTileProps = props;
   const styles: IIconTileStyles = getStyles();
 
   return (
     <div className={ styles.iconTile as string } { ...divProps }>
-      <i className={ styles.icon as string }>{ IconCodes[iconName] }</i>
+      <i className={ IconClassNames[iconName] + ' ' + styles.icon } />
       <div>{ iconName }</div>
     </div>
   );
