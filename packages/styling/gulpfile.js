1820479f14ee2ddd8b6c926f7420aac1415feaa2
David Zearing <dzearing@microsoft.com>
2017-06-05 22:33:26 -0700

    Adding tslib dependency to remove redundant code. (#1942)
    * Updating all packages to use tslib.
    
    * Updating dependencies.

--- packages/styling/gulpfile.js
+++ packages/styling/gulpfile.js
@@ -1,7 +1,12 @@
 'use strict';
 
 let build = require('@microsoft/sp-build-web');
+let path = require('path');
 let gulp = require('gulp');
+let buildConfig = build.getConfig();
+
+let isProduction = process.argv.indexOf('--production') >= 0;
+let isClean = process.argv.indexOf('clean') >= 0;
 
 // initialize tasks.
 build.initialize(gulp);
@@ -16,13 +21,25 @@ let rules = Object.assign(
   require('./node_modules/office-ui-fabric-react-tslint/tslint.json').rules,
   require('./tslint.json').rules
 );
-build.tslint.setConfig({ lintConfig: { rules } });
+build.tslint.setConfig({
+  lintConfig: { rules },
+  displayAsWarning: false
+});
 
 // Disable all unnecessary tasks.
 build.sass.isEnabled = () => false;
 build.karma.isEnabled = () => false;
 build.text.isEnabled = () => false;
 build.preCopy.isEnabled = () => false;
+
+let packageFolder = buildConfig.packageFolder || '';
+let distFolder = buildConfig.distFolder;
+
+if (isProduction || isClean) {
+  build.setConfig({
+    libAMDFolder: path.join(packageFolder, 'lib-amd')
+  });
+}
 
 // Until typings work.
 build.apiExtractor.isEnabled = () => false;
