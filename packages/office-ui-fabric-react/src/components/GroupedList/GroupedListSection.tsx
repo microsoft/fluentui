814bce74504bd60660283611d2b228efde582bc2
adsurang <adsurang@microsoft.com>
2017-06-12 15:32:08 +0530

    Changing the role of groupelist from grid to row

--- packages/office-ui-fabric-react/src/components/GroupedList/GroupedListSection.tsx
+++ packages/office-ui-fabric-react/src/components/GroupedList/GroupedListSection.tsx
@@ -191,6 +191,7 @@ export class GroupedListSection extends BaseComponent<IGroupedListSectionProps, 
       <div
         ref='root'
         className={ css('ms-GroupedList-group', styles.group, this._getDroppingClassName()) }
+        role="group"
       >
         { onRenderGroupHeader(groupHeaderProps, this._onRenderGroupHeader) }
         {
