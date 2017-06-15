9f7d858419ce61b53aebef7572062d51bddb77fa
Micah Godbolt <micahgodbolt@gmail.com>
2017-06-15 10:56:50 -0700

    Cleanup state, move aria-owns to button

--- packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.tsx
@@ -19,47 +19,30 @@ import { ResizeGroup } from '../../ResizeGroup';
 import * as stylesImport from './Breadcrumb.scss';
 const styles: any = stylesImport;
 
-export interface IBreadcrumbState {
-  isOverflowOpen: boolean;
-  overflowAnchor?: HTMLElement;
-}
-
 export interface IBreadCrumbData {
   props: IBreadcrumbProps;
-  overflowMenuId: string;
   renderedItems: IBreadcrumbItem[];
   renderedOverflowItems: IBreadcrumbItem[];
 }
 
 const OVERFLOW_KEY = 'overflow';
-const OVERFLOW_WIDTH = 44;
 
-export class Breadcrumb extends BaseComponent<IBreadcrumbProps, IBreadcrumbState> {
+export class Breadcrumb extends BaseComponent<IBreadcrumbProps, any> {
   public static defaultProps: IBreadcrumbProps = {
     items: [],
     maxDisplayedItems: 999
-  };
-
-  public refs: {
-    [key: string]: React.ReactInstance;
-    renderingArea: HTMLElement;
   };
 
   private _id: string;
 
   constructor(props: IBreadcrumbProps) {
     super(props);
-
-    this._id = getId('Breadcrumb');
-    this.state = this._getStateFromProps(props);
   }
 
   public render() {
     let { onReduceData = this._onReduceData } = this.props;
-    let overflowMenuId = this._id + '-overflow';
     let breadCrumbData: IBreadCrumbData = {
       props: this.props,
-      overflowMenuId: this._id,
       renderedItems: this.props.items.slice(0, this.props.maxDisplayedItems),
       renderedOverflowItems: this.props.items.slice(this.props.maxDisplayedItems + 1)
     };
@@ -89,12 +72,11 @@ export class Breadcrumb extends BaseComponent<IBreadcrumbProps, IBreadcrumbState
   @autobind
   private _onRenderBreadcrumb(data: IBreadCrumbData) {
     let { className, ariaLabel, items, onRenderItem = this._onRenderItem } = data.props;
-    let { renderedOverflowItems, renderedItems, overflowMenuId } = data;
-    let { isOverflowOpen, overflowAnchor } = this.state;
+    let { renderedOverflowItems, renderedItems } = data;
 
-    let contextualItems: IContextualMenuItem[] = renderedOverflowItems.map(
+    let contextualItems = renderedOverflowItems.map(
       (item, index) => ({
-        names: item.text,
+        name: item.text,
         key: item.key,
         onClick: item.onClick ? this._onBreadcrumbClicked.bind(this, item) : null,
         href: item.href
@@ -115,14 +97,11 @@ export class Breadcrumb extends BaseComponent<IBreadcrumbProps, IBreadcrumbState
                 <IconButton
                   className={ css('ms-Breadcrumb-overflowButton', styles.overflowButton) }
                   iconProps={ { iconName: 'more' } }
-                  onClick={ this._onOverflowClicked }
                   role='button'
                   aria-haspopup='true'
-                  aria-owns={ isOverflowOpen ? overflowMenuId : null }
                   menuIconProps={ { iconName: null } }
                   menuProps={ {
                     items: contextualItems,
-                    id: overflowMenuId,
                     directionalHint: DirectionalHint.bottomLeftEdge
                   } }
                 />
@@ -174,36 +153,9 @@ export class Breadcrumb extends BaseComponent<IBreadcrumbProps, IBreadcrumbState
   }
 
   @autobind
-  private _onOverflowClicked(ev: React.MouseEvent<HTMLButtonElement>) {
-    this.setState({
-      'isOverflowOpen': !this.state.isOverflowOpen,
-      'overflowAnchor': ev.currentTarget as HTMLElement
-    });
-  }
-
-  @autobind
-  private _onOverflowDismissed(ev: React.MouseEvent<HTMLElement>) {
-    this.setState({
-      'isOverflowOpen': false,
-      'overflowAnchor': null
-    });
-  }
-
-  @autobind
   private _onBreadcrumbClicked(item: IBreadcrumbItem, ev: React.MouseEvent<HTMLElement>) {
     if (item.onClick) {
       item.onClick(ev, item);
     }
-    this.setState({
-      'isOverflowOpen': false
-    });
   }
-
-  private _getStateFromProps(nextProps: IBreadcrumbProps) {
-    return {
-      isOverflowOpen: false,
-      overflowAnchor: null
-    };
-  }
-
 }
