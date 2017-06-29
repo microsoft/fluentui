8003ab2f04dbbdc0c0c481aad70aa11369c4dd0b
Christopher Schleiden <cschleiden@live.de>
2017-06-29 09:42:37 -0700

    Button: Revert font-weight change to semi-bold

--- packages/office-ui-fabric-react/src/components/Button/DefaultButton/DefaultButton.styles.ts
+++ packages/office-ui-fabric-react/src/components/Button/DefaultButton/DefaultButton.styles.ts
@@ -43,7 +43,7 @@ export const getStyles = memoizeFunction((
     },
 
     label: {
-      fontWeight: 'bold' // theme.fontWeights.semibold,
+      fontWeight: theme.fontWeights.semibold,
     }
 
   };
