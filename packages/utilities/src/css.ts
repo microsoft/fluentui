1b1a3c1c347ab1c784f20fa7551cabd05ef9a832
dzearing <dzearing@hotmail.com>
2017-07-07 19:10:38 -0700

    Fixing all @types to be locked to avoid generate failing to build. (Found that some newer typings can't be consumed until we bump typescript.) Fixed comments, to make api-extractor happier. Fixed an amd import.

--- packages/utilities/src/css.ts
+++ packages/utilities/src/css.ts
@@ -1,14 +1,33 @@
-
+/**
+ * Dictionary of booleans.
+ *
+ * @internal
+ */
 export interface IDictionary {
   [className: string]: boolean;
 }
 
+/**
+ * Serializable object.
+ *
+ * @internal
+ */
 export interface ISerializableObject {
   toString?: () => string;
 }
 
+/**
+ * css input type.
+ *
+ * @internal
+ */
 export type ICssInput = string | ISerializableObject | IDictionary | null | undefined | boolean;
 
+/**
+ * Concatination helper, which can merge class names together. Skips over falsey values.
+ *
+ * @public
+ */
 export function css(...args: ICssInput[]) {
   let classes = [];
 
