d25ecec940f116a16ef4d1818c3cf9e7610395cd
Micah Godbolt <micahgodbolt@gmail.com>
2017-07-20 09:15:32 -0700

    Add pull request to folder name

--- vrtDeploy.js
+++ vrtDeploy.js
@@ -1,15 +1,16 @@
-let fs = require('vinyl-fs');
-let ftp = require('vinyl-ftp');
-let minimist = require('minimist');
+const fs = require('vinyl-fs');
+const ftp = require('vinyl-ftp');
+const path = require('path');
+const minimist = require('minimist');
 
-let args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2));
 
-let conn = new ftp({
+const dest = path.join('/site/wwwroot/mgodbolt/vrt/', args.pull || './');
+const conn = new ftp({
   host: 'waws-prod-bay-049.ftp.azurewebsites.windows.net',
   user: args.user,
   password: args.password
 });
 
 fs.src(['./packages/office-ui-fabric-react/visualtests/baseline/*.png'], { buffer: false })
-  .pipe(conn.dest('/site/wwwroot/mgodbolt/vrt/'));
-
+  .pipe(conn.dest(dest));
