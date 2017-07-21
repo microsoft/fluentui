5c6449de45d4a29d26cd5cf4664bc5c192b655f5
Micah Godbolt <micahgodbolt@gmail.com>
2017-07-21 12:43:22 -0700

    Update vrtDeploy.sh

--- scripts/vrtDeploy.sh
+++ scripts/vrtDeploy.sh
@@ -4,5 +4,5 @@
 for f in packages/office-ui-fabric-react/visualtests/baseline/*; do
   b=$(basename $f)
   echo "Uploading $f as $b"
-	curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://waws-prod-bay-049.ftp.azurewebsites.windows.net/site/wwwroot/mgodbolt/vrt/$TRAVIS_PULL_REQUEST/$b
+  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://waws-prod-bay-049.ftp.azurewebsites.windows.net/site/wwwroot/mgodbolt/vrt/$TRAVIS_PULL_REQUEST/$b
 done
