97326da5525e3404dbcc09bdff542bfa0a71b520
Micah Godbolt <micahgodbolt@gmail.com>
2017-07-21 09:49:17 -0700

    Added script tag and encrypted un/pw

---
+++ scripts/vrtDeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for f in packages/office-ui-fabric-react/visualtests/baseline/*; do
+  b=$(basename $f)
+  echo "Uploading $f as $b"
+  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://waws-prod-bay-049.ftp.azurewebsites.windows.net/site/wwwroot/buildassets/$TRAVIS_PULL_REQUEST/$b
+done
