ebc389f5e99db3f6e86c9a6c750024fb15ee7a95
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-08-02 18:10:31 -0700

    More styling

--- packages/office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.FlexBox.Example.tsx
+++ packages/office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.FlexBox.Example.tsx
@@ -4,23 +4,47 @@ import { mergeStyles, IStyle } from 'office-ui-fabric-react/lib/Styling';
 
 export interface IFlexBoxResizeGroupExampleStyles {
   root: IStyle;
+  numberedBox: IStyle;
 }
 
 const styles: IFlexBoxResizeGroupExampleStyles = {
   root: mergeStyles({
     display: 'flex',
     justifyContent: 'space-between'
+  }),
+  numberedBox: mergeStyles({
+    display: 'inline-block',
+    textAlign: 'center',
+    fontSize: '20px',
+    lineHeight: '50px',
+    height: '50px',
+    width: '50px',
+    backgroundColor: 'green',
+    marginLeft: '10px',
+    marginRight: '10px'
   })
 };
+
+interface IBoxWithLabelProps {
+  label: string;
+}
+
+const BoxWithLabel: React.StatelessComponent<IBoxWithLabelProps> =
+  (props: IBoxWithLabelProps) => (<div className={ styles.numberedBox as string } >{ props.label }</div>);
 
 export class FlexBoxResizeGroupExample extends BaseComponent<{}, {}> {
 
   public render() {
     return (
       <div className={ styles.root as string }>
-        <span>Left</span>
+        <div>
+          <BoxWithLabel
+            label='1' />
+          <BoxWithLabel
+            label='2' />
+        </div>
         <span>Right </span>
-      </div>
+      </div >
     );
   }
 }
