77209c743d4ae9a1c6345f5cd15b17b864116b75
Micah Godbolt <micahgodbolt@gmail.com>
2017-08-07 13:17:06 -0700

    moved position back to styles

--- packages/office-ui-fabric-react/src/components/Callout/CalloutContent.tsx
+++ packages/office-ui-fabric-react/src/components/Callout/CalloutContent.tsx
@@ -150,9 +150,9 @@ export class CalloutContent extends BaseComponent<ICalloutProps, ICalloutState> 
               styles.root,
               className,
               directionalClassName,
-              positions ? positions.calloutPosition : OFF_SCREEN_STYLE,
               calloutWidth && { width: calloutWidth }
             ) as string }
+          style={ positions ? positions.calloutPosition : OFF_SCREEN_STYLE }
           tabIndex={ -1 } // Safari and Firefox on Mac OS requires this to back-stop click events so focus remains in the Callout.
           // See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
           ref={ this._resolveRef('_calloutElement') }
