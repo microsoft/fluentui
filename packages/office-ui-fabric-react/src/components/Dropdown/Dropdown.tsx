499d5f3bf756daa6f2efa47b1530f0674056d76b
Kris Brown <v-krbrow@microsoft.com>
2017-08-08 12:47:51 -0700

    Adding for property to droprop labels

--- packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.tsx
+++ packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.tsx
@@ -146,7 +146,7 @@ export class Dropdown extends BaseComponent<IDropdownInternalProps, IDropdownSta
     return (
       <div ref='root' className={ css('ms-Dropdown-container') }>
         { label && (
-          <Label className={ css('ms-Dropdown-label') } id={ id + '-label' } ref={ this._resolveRef('_dropdownLabel') } required={ required }>{ label }</Label>
+          <Label className={ css('ms-Dropdown-label') } id={ id + '-label' } htmlFor={ id } ref={ this._resolveRef('_dropdownLabel') } required={ required }>{ label }</Label>
         ) }
         <div
           data-is-focusable={ !disabled }
