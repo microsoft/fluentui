17a67941983e7c2c8712175be54f4820fcd5aaed
Micah Godbolt <micahgodbolt@gmail.com>
2017-08-14 14:40:53 -0700

    Adding styling to dependencies

---
+++ packages/office-ui-fabric-react-experiments/index.ts
@@ -0,0 +1,9 @@
+export * from './BaseButton';
+export { getStyles as getBaseButtonStyles } from './BaseButton.styles';
+export * from './Button.Props';
+export * from './Button';
+export * from './CommandButton/CommandButton';
+export * from './CompoundButton/CompoundButton';
+export * from './DefaultButton/DefaultButton';
+export * from './PrimaryButton/PrimaryButton';
+export * from './IconButton/IconButton';
