14b77d319f5e2f38bb920d05d3a9d7572fc0a78d
t-zhajin <t-zhajin@microsoft.com>
2017-08-17 09:46:38 +0800

    add vertical slider

--- packages/office-ui-fabric-react/src/components/Slider/Slider.tsx
+++ packages/office-ui-fabric-react/src/components/Slider/Slider.tsx
@@ -93,118 +93,57 @@ export class Slider extends BaseComponent<ISliderProps, ISliderState> implements
     const onMouseDownProp: {} = disabled ? {} : { onMouseDown: this._onMouseDownOrTouchStart };
     const onTouchStartProp: {} = disabled ? {} : { onTouchStart: this._onMouseDownOrTouchStart };
     const onKeyDownProp: {} = disabled ? {} : { onKeyDown: this._onKeyDown };
-    if (horizontal) {
-      return (
-        <div
-          className={ css('ms-Slider', styles.root, className, {
-            ['ms-Slider-enabled ' + styles.rootIsEnabled]: !disabled,
-            ['ms-Slider-disabled ' + styles.rootIsDisabled]: disabled,
-            ['ms-Slider-row ' + styles.row]: horizontal,
-            ['ms-Slider-column ' + styles.column]: !horizontal
-          }) }
-          ref='root'>
-          { label && (
-            <Label className={ styles.titleLabel } { ...ariaLabel ? {} : { 'htmlFor': this._id } }>
-              { label }
-            </Label>
-          ) }
-          <div className={ css('ms-Slider-container', styles.container) }>
-            <button
-              aria-valuenow={ value }
-              aria-valuemin={ min }
-              aria-valuemax={ max }
-              { ...onMouseDownProp }
-              { ...onTouchStartProp }
-              { ...onKeyDownProp }
-              { ...buttonProps }
-              className={ css('ms-Slider-slideBox', styles.slideBox, buttonProps!.className, {
-                'ms-Slider-showValue': !!showValue,
-                ['ms-Slider-showTransitions ' + styles.showTransitions]: (renderedValue === value)
-              }) }
-              id={ this._id }
-              disabled={ disabled }
-              type='button'
-              role='slider'
+    return (
+      <div
+        className={ css('ms-Slider', styles.root, className, {
+          ['ms-Slider-enabled ' + styles.rootIsEnabled]: !disabled,
+          ['ms-Slider-disabled ' + styles.rootIsDisabled]: disabled,
+          ['ms-Slider-row ' + styles.row]: horizontal,
+          ['ms-Slider-column ' + styles.column]: !horizontal
+        }) }
+        ref='root'>
+        { label && (
+          <Label className={ styles.titleLabel } { ...ariaLabel ? {} : { 'htmlFor': this._id } }>
+            { label }
+          </Label>
+        ) }
+        <div className={ css('ms-Slider-container', styles.container) }>
+          <button
+            aria-valuenow={ value }
+            aria-valuemin={ min }
+            aria-valuemax={ max }
+            { ...onMouseDownProp }
+            { ...onTouchStartProp }
+            { ...onKeyDownProp }
+            { ...buttonProps }
+            className={ css('ms-Slider-slideBox', styles.slideBox, buttonProps!.className, {
+              'ms-Slider-showValue': !!showValue,
+              ['ms-Slider-showTransitions ' + styles.showTransitions]: (renderedValue === value)
+            }) }
+            id={ this._id }
+            disabled={ disabled }
+            type='button'
+            role='slider'
+          >
+            <div
+              ref='sliderLine'
+              className={ css('ms-Slider-line', styles.line) }
             >
-              <div
-                ref='sliderLine'
-                className={ css('ms-Slider-line', styles.line) }
-              >
-                <span
-                  ref='thumb'
-                  className={ css('ms-Slider-thumb', styles.thumb) }
-                  { ...ariaLabel ? { 'aria-label': ariaLabel } : {} }
-                  style={ getRTL() ?
-                    { 'right': thumbOffsetPercent + '%' } :
-                    { 'left': thumbOffsetPercent + '%' } }
-                />
-                <span className={ css('ms-Slider-active', styles.activeSection) } style={ { 'width': thumbOffsetPercent + '%' } }></span>
-                <span className={ css('ms-Slider-inactive', styles.inactiveSection) } style={ { 'width': (100 - thumbOffsetPercent) + '%' } }></span>
-              </div>
-            </button>
-            { showValue && <Label className={ css('ms-Slider-value', styles.valueLabel) }>{ value }</Label> }
-          </div>
+              <span
+                ref='thumb'
+                className={ css('ms-Slider-thumb', styles.thumb) }
+                { ...ariaLabel ? { 'aria-label': ariaLabel } : {} }
+                style={ this._getThumbStyle(horizontal, thumbOffsetPercent) }
+              />
+              <span className={ css('ms-Slider-active', styles.activeSection) } style={ horizontal ? { 'width': thumbOffsetPercent + '%' } : { 'height': (thumbOffsetPercent) + '%' } }></span>
+              <span className={ css('ms-Slider-inactive', styles.inactiveSection) } style={ horizontal ? { 'width': (100 - thumbOffsetPercent) + '%' } : { 'height': (100 - thumbOffsetPercent) + '%' } }></span>
+            </div>
+          </button>
+          { showValue && <Label className={ css('ms-Slider-value', styles.valueLabel) }>{ value }</Label> }
         </div>
-      ) as React.ReactElement<{}>;
-    } else {
-      return (
-        <div
-          className={ css('ms-Slider', styles.root, className, {
-            ['ms-Slider-enabled ' + styles.rootIsEnabled]: !disabled,
-            ['ms-Slider-disabled ' + styles.rootIsDisabled]: disabled,
-          }) }
-          ref='root'>
-          { label && (
-            <Label className={ styles.titleLabel } { ...ariaLabel ? {} : { 'htmlFor': this._id } }>
-              { label }
-            </Label>
-          ) }
-          <div className={ css('ms-Slider-container', styles.container) }>
-            <button
-              aria-valuenow={ value }
-              aria-valuemin={ min }
-              aria-valuemax={ max }
-              { ...onMouseDownProp }
-              //{ ...onTouchStartProp }
-              { ...onKeyDownProp }
-              { ...buttonProps }
-              className={ css('ms-Slider-slideBox', styles.slideBox, buttonProps!.className, {
-                'ms-Slider-showValue': !!showValue,
-                ['ms-Slider-showTransitions ' + styles.showTransitions]: (renderedValue === value)
-              }) }
-              id={ this._id }
-              disabled={ disabled }
-              type='button'
-              role='slider'
-            >
-              <div
-                ref='sliderLine'
-                className={ css('ms-Slider-line', styles.line) }
-              >
-                <span
-                  ref='thumb'
-                  className={ css('ms-Slider-thumb', styles.thumb) }
-                  { ...ariaLabel ? { 'aria-label': ariaLabel } : {} }
-                  style={ getRTL() ?
-                    { 'bottom': thumbOffsetPercent + '%' } :
-                    { 'top': thumbOffsetPercent + '%' } }
-                /*style={ getRTL() ?
-                  { 'right': thumbOffsetPercent + '%' } :
-                  { 'left': thumbOffsetPercent + '%' } }*/
-                />
-                <span className={ css('ms-Slider-active', styles.activeSection) } style={ { 'height': (thumbOffsetPercent) + '%' } } > </span>
-                <span className={ css('ms-Slider-inactive', styles.inactiveSection) } style={ { 'height': (100 - thumbOffsetPercent) + '%' } }
-                ></span>
-              </div>
-            </button>
-            { showValue && <Label className={ css('ms-Slider-value', styles.valueLabel) }>{ value }</Label> }
-          </div>
-        </div>
-      ) as React.ReactElement<{}>;
-    }
-
+      </div>
+    ) as React.ReactElement<{}>;
   }
-
   public focus(): void {
     if (this.refs.thumb) {
       this.refs.thumb.focus();
@@ -213,6 +152,17 @@ export class Slider extends BaseComponent<ISliderProps, ISliderState> implements
 
   public get value(): number | undefined {
     return this.state.value;
+  }
+
+
+  private _getThumbStyle(horizontal: boolean | undefined, thumbOffsetPercent: number): any {
+    var thumbStyle: string;
+    if (horizontal) {
+      return getRTL() ? { 'right': thumbOffsetPercent + '%' } : { 'left': thumbOffsetPercent + '%' };
+    } else {
+      return getRTL() ? { 'bottom': thumbOffsetPercent + '%' } : { 'top': thumbOffsetPercent + '%' };
+    }
+
   }
 
   @autobind
@@ -233,31 +183,49 @@ export class Slider extends BaseComponent<ISliderProps, ISliderState> implements
     const { max, min, step } = this.props;
     const steps: number = (max! - min!) / step!;
     const sliderPositionRect: ClientRect = this.refs.sliderLine.getBoundingClientRect();
-    const sliderLength: number = sliderPositionRect.width;
+    const sliderLength: number = this.props.horizontal ? sliderPositionRect.width : sliderPositionRect.height;
     const stepLength: number = sliderLength / steps;
     let currentSteps: number | undefined;
-
-    if (event.type === 'mousedown' || event.type === 'mousemove') {
-      currentSteps = getRTL() ?
-        (sliderPositionRect.right - (event as MouseEvent).clientX) / stepLength :
-        ((event as MouseEvent).clientX - sliderPositionRect.left) / stepLength;
-    } else if (event.type === 'touchstart' || event.type === 'touchmove') {
-      currentSteps = getRTL() ?
-        (sliderPositionRect.right - (event as TouchEvent).touches[0].clientX) / stepLength :
-        ((event as TouchEvent).touches[0].clientX - sliderPositionRect.left) / stepLength;
+    if (this.props.horizontal) {
+      if (event.type === 'mousedown' || event.type === 'mousemove') {
+        currentSteps = getRTL() ?
+          (sliderPositionRect.right - (event as MouseEvent).clientX) / stepLength :
+          ((event as MouseEvent).clientX - sliderPositionRect.left) / stepLength;
+      } else if (event.type === 'touchstart' || event.type === 'touchmove') {
+        currentSteps = getRTL() ?
+          (sliderPositionRect.right - (event as TouchEvent).touches[0].clientX) / stepLength :
+          ((event as TouchEvent).touches[0].clientX - sliderPositionRect.left) / stepLength;
+      }
+    } else {
+      if (event.type === 'mousedown' || event.type === 'mousemove') {
+        currentSteps = getRTL() ?
+          ((event as MouseEvent).clientY - sliderPositionRect.top) / stepLength :
+          (sliderPositionRect.bottom - (event as MouseEvent).clientY) / stepLength;
+      } else if (event.type === 'touchstart' || event.type === 'touchmove') {
+        currentSteps = getRTL() ?
+          ((event as TouchEvent).touches[0].clientY - sliderPositionRect.top) / stepLength :
+          (sliderPositionRect.bottom - (event as TouchEvent).touches[0].clientY) / stepLength;
+      }
     }
+
 
     let currentValue: number | undefined;
     let renderedValue: number | undefined;
 
     // The value shouldn't be bigger than max or be smaller than min.
     if (currentSteps! > Math.floor(steps)) {
-      renderedValue = currentValue = max as number;
+      this.props.horizontal ? renderedValue = currentValue = max as number : renderedValue = currentValue = min as number;
     } else if (currentSteps! < 0) {
-      renderedValue = currentValue = min as number;
+      this.props.horizontal ? renderedValue = currentValue = min as number : renderedValue = currentValue = max as number;
     } else {
-      renderedValue = min! + step! * currentSteps!;
-      currentValue = min! + step! * Math.round(currentSteps!);
+      if (this.props.horizontal) {
+        renderedValue = min! + step! * currentSteps!;
+        currentValue = min! + step! * Math.round(currentSteps!);
+      } else {
+        renderedValue = max! - step! * currentSteps!;
+        currentValue = max! - step! * Math.round(currentSteps!);
+      }
+
     }
 
     this._updateValue(currentValue, renderedValue);
@@ -302,11 +270,11 @@ export class Slider extends BaseComponent<ISliderProps, ISliderState> implements
     switch (event.which) {
       case getRTLSafeKeyCode(KeyCodes.left):
       case KeyCodes.down:
-        diff = -(step as number);
+        diff = this.props.horizontal ? -(step as number) : +(step as number);
         break;
       case getRTLSafeKeyCode(KeyCodes.right):
       case KeyCodes.up:
-        diff = step;
+        diff = this.props.horizontal ? step : -(step as number);
         break;
 
       case KeyCodes.home:
--- packages/office-ui-fabric-react/src/components/Slider/examples/Slider.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Slider/examples/Slider.Basic.Example.tsx
@@ -13,37 +13,74 @@ export class SliderBasicExample extends React.Component<any, any> {
 
   public render() {
     return (
-      <div className='ms-SliderBasicExample'>
+      <div className='ms-basicSlider'>
+        <div className='ms-SliderBasicExample'>
 
-        <Slider
-          label='Basic example:'
-          min={ 1 }
-          max={ 3 }
-          step={ 1 }
-          defaultValue={ 2 }
-          showValue={ true }
-          horizontal={ false }
-          onChange={ (value) => console.log(value) }
-        />
+          <Slider
+            label='Basic example:'
+            min={ 1 }
+            max={ 3 }
+            step={ 1 }
+            defaultValue={ 2 }
+            showValue={ true }
+            horizontal={ true }
+            onChange={ (value) => console.log(value) }
+          />
 
-        <Slider
-          label='Disabled example:'
-          min={ 50 }
-          max={ 500 }
-          step={ 50 }
-          defaultValue={ 300 }
-          showValue={ true }
-          disabled={ true }
-        />
+          <Slider
+            label='Disabled example:'
+            min={ 50 }
+            max={ 500 }
+            step={ 50 }
+            defaultValue={ 300 }
+            showValue={ true }
+            disabled={ true }
+          />
 
-        <Slider
-          label='Controlled example:'
-          max={ 10 }
-          value={ this.state.value }
-          onChange={ value => this.setState({ value }) }
-          showValue={ true }
-        />
+          <Slider
+            label='Controlled example:'
+            max={ 10 }
+            value={ this.state.value }
+            onChange={ value => this.setState({ value }) }
+            showValue={ true }
+          />
 
+        </div>
+        <div className='ms-SliderVertical '>
+          <div className='ms-verticalBox'>
+            <Slider
+              label='Basic example:'
+              min={ 1 }
+              max={ 3 }
+              step={ 1 }
+              defaultValue={ 2 }
+              showValue={ true }
+              horizontal={ false }
+              onChange={ (value) => console.log(value) }
+            />
+          </div>
+          <div className='ms-verticalBox'>
+            <Slider
+              label='Disabled example:'
+              min={ 50 }
+              max={ 500 }
+              step={ 50 }
+              defaultValue={ 300 }
+              showValue={ true }
+              horizontal={ false }
+              disabled={ true }
+            />
+          </div>
+          <div className='ms-verticalBox'>
+            <Slider
+              label='Controlled example:'
+              max={ 10 }
+              value={ this.state.value }
+              horizontal={ false }
+              showValue={ true }
+            />
+          </div>
+        </div>
       </div>
     );
   }
