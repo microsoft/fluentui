7141e63018997876d0c4f9d8dc6e420eb5f52db9
David Zearing <dzearing@microsoft.com>
2017-08-18 15:08:28 -0700

    Adding a dedicated merge-styles package which will replace the glamor usage. (#2527)
    * Initial checkin.
    
    * Updates to add webpack and slightly reduce the bundle size of merge-styles to 3.4k total.
    
    * Adding change files.
    
    * Adding more tests.
    
    * Updating version number.
    
    * Updates: remove ALL dependencies bringing the total size down to 2.7k, fix tests, remove async and appendChild features from stylesheet.
    
    * Fixing for loops and readme and strict mode.
    
    * const updates.
    
    * pr feedback.
    
    * Assume pixels when providing number types.
    
    * ignore opacity.
    
    * Adding keyframes test.
    
    * Adding comments, removing obsolete or deprecated properties from rawstyle interface.
    
    * Removing import.
    
    * Updating to optimize the transforms to an O(n) approach.
    
    * Updating documentation.
    
    * Minor grammar fixes.

---
+++ packages/merge-styles/src/transforms/rtlifyRules.ts
@@ -0,0 +1,85 @@
+const _nameReplacements: { [key: string]: string } = {
+  'left': 'right',
+  'right': 'left'
+};
+
+const _valueReplacements: { [key: string]: string } = {
+  'w-resize': 'e-resize',
+  'sw-resize': 'se-resize',
+  'nw-resize': 'ne-resize'
+};
+
+const NO_FLIP = 'noflip';
+
+let _rtl = getRTL();
+
+export function setRTL(isRTL: boolean): void {
+  _rtl = isRTL;
+}
+export function getRTL(): boolean {
+  if (_rtl === undefined) {
+    _rtl = (
+      typeof document !== undefined &&
+      !!document.documentElement &&
+      document.documentElement.getAttribute('dir') === 'rtl'
+    );
+  }
+  return _rtl;
+}
+
+export function rtlifyRules(
+  rulePairs: (string | number)[],
+  index: number
+): void {
+  if (getRTL()) {
+    const name = rulePairs[index] as string;
+    const value = rulePairs[index + 1] as string;
+
+    if (value.indexOf(NO_FLIP) >= 0) {
+      return;
+    } else if (name.indexOf('left') >= 0) {
+      rulePairs[index] = name.replace('left', 'right');
+    } else if (name.indexOf('right') >= 0) {
+      rulePairs[index] = name.replace('right', 'left');
+    } else if (String(value).indexOf('left') >= 0) {
+      rulePairs[index + 1] = value.replace('left', 'right');
+    } else if (String(value).indexOf('right') >= 0) {
+      rulePairs[index + 1] = value.replace('right', 'left');
+    } else if (_nameReplacements[name]) {
+      rulePairs[index] = _nameReplacements[name];
+    } else if (_valueReplacements[value]) {
+      rulePairs[index + 1] = _valueReplacements[value];
+    } else {
+      switch (name) {
+        case 'margin':
+        case 'padding':
+          rulePairs[index + 1] = flipQuad(value);
+          break;
+        case 'box-shadow':
+          rulePairs[index + 1] = negateNum(value, 0);
+          break;
+      }
+    }
+  }
+}
+
+function negateNum(value: string, partIndex: number): string {
+  const parts = value.split(' ');
+  const numberVal = parseInt(parts[partIndex], 10);
+
+  parts[0] = parts[0].replace(String(numberVal), String(numberVal * -1));
+
+  return parts.join(' ');
+}
+
+function flipQuad(value: string): string {
+  if (typeof value === 'string') {
+    const parts = value.split(' ');
+
+    if (parts.length === 4) {
+      return `${parts[0]} ${parts[3]} ${parts[2]} ${parts[1]}`;
+    }
+  }
+
+  return value;
+}
