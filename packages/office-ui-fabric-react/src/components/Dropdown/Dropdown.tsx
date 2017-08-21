63ba3ea6753f99e7bbe86e27102d10033f41e94b
Kris Brown <v-krbrow@microsoft.com>
2017-08-21 09:53:14 -0700

    Simplifying aria-label prop of dropdown

--- packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.tsx
+++ packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.tsx
@@ -156,7 +156,7 @@ export class Dropdown extends BaseComponent<IDropdownInternalProps, IDropdownSta
           aria-expanded={ isOpen ? 'true' : 'false' }
           role='menu'
           aria-live={ disabled || isOpen ? 'off' : 'assertive' }
-          aria-label={ ariaLabel && !label ? ariaLabel : null }
+          aria-label={ ariaLabel }
           aria-describedby={ id + '-option' }
           aria-activedescendant={ isOpen && selectedIndex! >= 0 ? (this._id + '-list' + selectedIndex) : null }
           aria-disabled={ disabled }
