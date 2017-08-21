baed68a6f2e6e33626c749e41534bf97ce7c921b
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-08-21 15:07:07 -0700

    Fix build break

--- packages/office-ui-fabric-react/src/components/MarqueeSelection/examples/MarqueeSelection.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/MarqueeSelection/examples/MarqueeSelection.Basic.Example.tsx
@@ -7,10 +7,16 @@ import {
   createArray
 } from 'office-ui-fabric-react/lib/Utilities';
 import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
-import { MarqueeSelection, Selection } from 'office-ui-fabric-react/lib/MarqueeSelection';
+import { MarqueeSelection, Selection, IObjectWithKey } from 'office-ui-fabric-react/lib/MarqueeSelection';
 import './MarqueeSelection.Basic.Example.scss';
 
-const PHOTOS = createArray(250, () => {
+interface IPhoto extends IObjectWithKey {
+  url: string;
+  width: number;
+  height: number;
+}
+
+const PHOTOS: IPhoto[] = createArray(250, () => {
   const randomWidth = 50 + Math.floor(Math.random() * 150);
 
   return {
