ebdb3ed62e16b1f4bd701cd2c9c5e3f28026f46e
David Zearing <dzearing@microsoft.com>
2017-08-22 11:36:31 -0700

    More updates.

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,14 @@
 const isProduction = process.argv.indexOf('--production') > -1;
 
-[
-  'sass',
-  'tslint',
-  'typescript',
-  'karma',
-  'webpack'
-].forEach(task => require('./tasks/' + task)({ isProduction }));
+console.log(process.argv);
+if (process.argv.length >= 3 && process.argv[2].indexOf('--') === -1) {
+  require('./tasks/' + process.argv[2])({ isProduction });
+} else {
+  [
+    'sass',
+    'tslint',
+    'ts',
+    'karma',
+    'webpack'
+  ].forEach(task => require('./tasks/' + task)({ isProduction }));
+}
---
+++ scripts/tasks/ts.js
@@ -0,0 +1,12 @@
+module.exports = function (options) {
+  const path = require('path');
+  const execSync = require('../exec-sync');
+  const typescriptPath = 'tsc';
+
+  execSync(typescriptPath + ' -outDir lib -t es5 -m commonjs --pretty', 'typescript commonjs/es5');
+
+  if (options.isProduction) {
+    execSync(typescriptPath + ' -outDir lib-amd -t es5 -m amd --pretty', 'typescript amd/es5');
+    // execSync(typescriptPath + ' -outDir lib-es6 -t es6 -m es6 --pretty', 'typescript es6');
+  }
+};
