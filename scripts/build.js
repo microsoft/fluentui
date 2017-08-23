7a256c33a1984938ca724411efe58778591446ee
David Zearing <dzearing@microsoft.com>
2017-08-23 11:55:01 -0700

    Fixing the OS specific build problem, adding more logging.

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,9 @@
-const isProduction = process.argv.indexOf('--production') > -1;
 const chalk = require('chalk');
-const { logStartTask, logEndTask } = require('./logging');
+const fs = require('fs');
+const path = require('path');
+const { logStartTask, logEndTask, logEndBuild } = require('./logging');
+
+const isProduction = process.argv.indexOf('--production') > -1;
 
 let tasks = [
   'copy',
@@ -17,6 +20,7 @@ if (process.argv.length >= 3 && process.argv[2].indexOf('--') === -1) {
 
 let promise = Promise.resolve();
 let hasFailures = false;
+let buildStartTime = new Date().getTime();
 
 tasks.forEach(task => {
   promise = promise.then(() => runTask(task));
@@ -26,18 +30,29 @@ promise.then(() => {
   if (hasFailures) {
     process.exitCode = 1;
   }
+  logEndBuild(getPackageName(), !hasFailures, buildStartTime);
 });
 
 function runTask(task) {
-  let start = new Date().getTime();
+  let taskStartTime = new Date().getTime();
 
   return Promise.resolve()
     .then(() => !hasFailures && Promise.resolve()
       .then(() => logStartTask(task))
       .then(() => require('./tasks/' + task)({ isProduction }))
-      .then(() => logEndTask(task, new Date().getTime() - start))
+      .then(() => logEndTask(task, taskStartTime))
       .catch((e) => {
         hasFailures = true;
-        logEndTask(task, new Date().getTime() - start, e);
+        logEndTask(task, taskStartTime, e);
       }));
+}
+
+function getPackageName() {
+  let packagePath = path.resolve(process.cwd(), 'package.json');
+
+  if (fs.existsSync(packagePath)) {
+    return JSON.parse(fs.readFileSync(packagePath, 'utf8')).name;
+  }
+
+  return '';
 }
--- scripts/exec-sync.js
+++ scripts/exec-sync.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const execSync = require('child_process').execSync;
 const chalk = require('chalk');
+const { logStatus } = require('./logging');
 
 const SEPARATOR = process.platform === 'win32' ? ';' : ':',
   env = Object.assign({}, process.env);
@@ -10,6 +11,8 @@ env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + env.PATH;
 function myExecSync(cmd, displayName) {
   let returnValue = 0;
   let start = new Date().getTime();
+
+  logStatus(chalk.gray('Executing: ') + chalk.cyan(displayName || cmd));
 
   const output = execSync(cmd, {
     cwd: process.cwd(),
