3869dd6fa38967cc9dbe0787f7f20b6c530ba1ce
David Zearing <dzearing@microsoft.com>
2017-08-25 13:12:35 -0700

    Update ts.js

--- scripts/tasks/ts.js
+++ scripts/tasks/ts.js
@@ -2,8 +2,8 @@ module.exports = function (options) {
   const path = require('path');
   const execSync = require('../exec-sync');
   const typescriptPath = 'node ' + path.resolve(__dirname, '../node_modules/typescript/lib/tsc');
-
   const extraParams = '--pretty' + (options.isProduction ? ' --inlineSources --mapRoot ../src' : '');
+  
   execSync(typescriptPath + ' -outDir lib -t es5 -m commonjs ' + extraParams);
 
   if (options.isProduction) {
