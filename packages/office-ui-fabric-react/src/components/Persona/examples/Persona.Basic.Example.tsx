af60278ddfba26d18e22b20c6e20824be22cf8dd
REDMOND\emlynam <lynam.emily@gmail.com>
2017-08-25 11:22:48 -0700

    Added label to Persona Example and respaced to fit

--- packages/office-ui-fabric-react/src/components/Persona/examples/Persona.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Persona/examples/Persona.Basic.Example.tsx
@@ -37,47 +37,62 @@ export class PersonaBasicExample extends React.Component<React.Props<PersonaBasi
             onChange={ (ev, checked) => { this.setState({ renderPersonaDetails: checked }); } } />
         </div>
 
+        <Label>Tiny Persona (12px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.tiny }
           presence={ PersonaPresence.offline }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Extra Extra Small Persona (24px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.extraExtraSmall }
           presence={ PersonaPresence.none }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Size 28 Persona (28px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.size28 }
           presence={ PersonaPresence.none }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Extra Small Persona (32px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.extraSmall }
           presence={ PersonaPresence.online }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Small Persona (40px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.small }
           presence={ PersonaPresence.away }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Medium Persona (48px)</Label>
         <Persona
           { ...examplePersona }
           hidePersonaDetails={ !renderPersonaDetails }
           presence={ PersonaPresence.busy }
         />
+
+        <Label>Large Persona (72px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.large }
           presence={ PersonaPresence.dnd }
           hidePersonaDetails={ !renderPersonaDetails }
         />
+
+        <Label>Extra Large Persona (100px)</Label>
         <Persona
           { ...examplePersona }
           size={ PersonaSize.extraLarge }
