8f5a5f4c39b46ef3a8f4d6f279ddc6f5b4fe4619
David Zearing <dzearing@microsoft.com>
2017-08-28 13:23:10 -0700

    Fixing oufr webpack.

--- packages/office-ui-fabric-react/webpack.config.js
+++ packages/office-ui-fabric-react/webpack.config.js
@@ -2,10 +2,11 @@ let path = require('path');
 const resources = require('../../scripts/tasks/webpack-resources');
 
 const BUNDLE_NAME = 'office-ui-fabric-react';
+const IS_PRODUCTION = process.argv.indexOf('--production') > -1;
 
 module.exports = resources.createConfig(
   BUNDLE_NAME,
-  isProduction,
+  IS_PRODUCTION,
   {
     entry: {
       [BUNDLE_NAME]: './lib/index.js'
