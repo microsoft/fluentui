2e6b4e0752f5cbf1e08253b14da08e65f1d87a11
REDMOND\emlynam <lynam.emily@gmail.com>
2017-08-28 08:19:51 -0700

    Updated alt text examples to be more descriptive

--- packages/office-ui-fabric-react/src/components/Image/examples/Image.Center.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Image/examples/Image.Center.Example.tsx
@@ -20,10 +20,10 @@ export class ImageCenterExample extends React.Component<any, any> {
       <div>
         <p>Setting the imageFit property to "center" behaves the same as "none", while centering the image within the frame.</p>
         <Label>The image is larger than the frame, so all sides are cropped to center the image.</Label>
-        <Image  { ...imageProps as any } src='http://placehold.it/800x300' alt='larger image fit center example' />
+        <Image  { ...imageProps as any } src='http://placehold.it/800x300' alt='Example implementation of the property image fit using the center value on an image larger than the frame.' />
         <br />
         <Label>The image is smaller than the frame, so there is empty space within the frame. The image is centered in the available space.</Label>
-        <Image  { ...imageProps as any } src='http://placehold.it/100x100' alt='smaller image fit center example' />
+        <Image  { ...imageProps as any } src='http://placehold.it/100x100' alt='Example implementation of the property image fit using the center value on an image smaller than the frame.' />
       </div>
     );
   }
--- packages/office-ui-fabric-react/src/components/Image/examples/Image.None.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Image/examples/Image.None.Example.tsx
@@ -19,10 +19,10 @@ export class ImageNoneExample extends React.Component<any, any> {
       <div>
         <p>By setting the imageFit property to "none", the image will remain at its natural size, even if the frame is made larger or smaller by setting the width and height props.</p>
         <Label>The image is larger than the frame, so it is cropped to fit. The image is positioned at the upper left of the frame.</Label>
-        <Image  { ...imageProps as any } alt='larger image fit none example' />
+        <Image  { ...imageProps as any } alt='Example implementation of the property image fit using the none value on an image larger than the frame.' />
         <br />
         <Label>The image is smaller than the frame, so there is empty space within the frame. The image is positioned at the upper left of the frame.</Label>
-        <Image  { ...imageProps as any } src='http://placehold.it/100x100' alt='smaller image fit none example' />
+        <Image  { ...imageProps as any } src='http://placehold.it/100x100' alt='Example implementation of the property image fit using the none value on an image smaller than the frame.' />
       </div>
     );
   }
