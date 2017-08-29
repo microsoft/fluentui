86bd53b6e02539e20098c815ccd9c0c81f7cdc54
John Miller <rubixws@gmail.com>
2017-08-29 10:37:52 -0700

    Resolving Conflicts With Cherry Pick

--- apps/fabric-website/webpack.serve.config.js
+++ apps/fabric-website/webpack.serve.config.js
@@ -1,3 +1,9 @@
+/**
+ * TODO Remove this plugin when merging back into master as it greatly slows down
+ * build times. This is mainly so that we can serve up the entry point to our devx
+ * testing site
+ */
+let WriteFilePlugin = require('write-file-webpack-plugin');
 let path = require('path');
 let resources = require('../../scripts/tasks/webpack-resources');
 
@@ -7,7 +13,9 @@ module.exports = resources.createServeConfig({
   entry: './src/root.tsx',
 
   output: {
-    filename: 'fabric-website.js',
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/dist/',
+    filename: 'fabric-website.js'
   },
 
   externals: {
@@ -23,5 +31,4 @@ module.exports = resources.createServeConfig({
       'Example.tsx.js': 'Example'
     }
   }
-
 });
