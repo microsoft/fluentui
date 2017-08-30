fec3fc510ac160def7944608fbbc82c1f4899ab5
David Zearing <dzearing@microsoft.com>
2017-08-29 17:38:17 -0700

    Adding a beta publishing script.

---
+++ scripts/publish-beta.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const execSync = require('./exec-sync');
+
+const newVersion = process.argv[2];
+const newDep = process.argv[3] || newVersion;
+
+const packages = [
+  'utilities',
+  'merge-styles',
+  'icons',
+  'styling',
+  'office-ui-fabric-react'
+];
+
+for (const package of packages) {
+  let packagePath = path.resolve('../packages', package);
+
+  console.log(`Publishing ${chalk.magenta(package)} in ${packagePath}`);
+  execSync('npm publish --tag beta', undefined, packagePath);
+}
