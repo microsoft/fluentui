f4aef6565d74cc9a0a029eb9a7e1d82ec8dec168
John Miller <rubixws@gmail.com>
2017-08-30 10:32:06 -0700

    Resolving conflicts with Webpack conifg and package.json

--- apps/fabric-website/webpack.serve.config.js
+++ apps/fabric-website/webpack.serve.config.js
@@ -1,9 +1,4 @@
-/**
- * TODO Remove this plugin when merging back into master as it greatly slows down
- * build times. This is mainly so that we can serve up the entry point to our devx
- * testing site
- */
-let WriteFilePlugin = require('write-file-webpack-plugin');
+
 let path = require('path');
 let resources = require('../../scripts/tasks/webpack-resources');
 
@@ -12,11 +7,7 @@ const PACKAGE_NAME = require('./package.json').name;
 module.exports = resources.createServeConfig({
   entry: './src/root.tsx',
 
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/dist/',
-    filename: 'fabric-website.js'
-  },
+  output: outputConfig,
 
   externals: {
     'react': 'React',
