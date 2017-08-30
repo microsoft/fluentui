0affad9f899c864de35d748ab7a96f4d9fa1b95d
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-08-29 17:35:43 -0700

    update comments

--- packages/utilities/src/sessionStorage.ts
+++ packages/utilities/src/sessionStorage.ts
@@ -1,6 +1,6 @@
 /**
  * Fetches an item from session storage without throwing an exception
- * @param key The key of the item to fetch from local storage
+ * @param key The key of the item to fetch from session storage
  */
 export function getItem(key: string): string | null {
   let result = null;
@@ -14,8 +14,8 @@ export function getItem(key: string): string | null {
 
 /**
  * Inserts an item into session storage without throwing an exception
- * @param key The key of the item to add to local storage
- * @param data The data to put into local storage
+ * @param key The key of the item to add to session storage
+ * @param data The data to put into session storage
  */
 export function setItem(key: string, data: string) {
   try {
