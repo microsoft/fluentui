118a0de98c1c9caccfbbc66ced7ce7a3067a3c04
David Zearing <dzearing@microsoft.com>
2017-08-31 11:06:22 -0700

    Pressing f5 now is able to run tests in the current project and hit debugger; statements.

--- scripts/tasks/jest.js
+++ scripts/tasks/jest.js
@@ -2,13 +2,14 @@ module.exports = function (options) {
   const path = require('path');
   const fs = require('fs');
   const execSync = require('../exec-sync');
+  const findConfig = require('../find-config');
 
   const jestPath = path.resolve(__dirname, '../node_modules/jest/bin/jest');
-  const jestConfigPath = path.join(process.cwd(), 'jest.config.js');
+  const jestConfigPath = findConfig('jest.config.js');
 
   if (fs.existsSync(jestConfigPath)) {
     const command = `node ${jestPath} --config ${jestConfigPath} ${options.args || ''}`;
 
-    execSync(command);
+    execSync(command, undefined, path.dirname(jestConfigPath));
   }
 };
