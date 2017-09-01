57ea45fbfaf4f4ee89a4da54a0a4beab7b16051b
Thomas Michon <thomas@michon.com>
2017-09-01 16:19:30 -0400

    Add support for pre-computation of Tile and FolderCover size (experiments) (#2679)
    * Add TileSize props
    
    * Add fitContentToBounds function to math utilities
    
    * Add size pre-computation to Tile
    
    * Add size pre-computation to FolderCover
    
    * Update Tile to use ISelection interface
    
    * Update change output
    
    * Fix FolderCover index reference
    
    * Add const enums for layout values
    
    * Touch up examples for Tiles
    
    * More documentation for fitContentToBounds

--- packages/experiments/src/components/FolderCover/FolderCover.tsx
+++ packages/experiments/src/components/FolderCover/FolderCover.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { IFolderCoverProps, FolderCoverSize, FolderCoverType } from './FolderCover.Props';
 import { css } from 'office-ui-fabric-react/lib/Utilities';
+import { ISize } from '@uifabric/utilities';
 import * as FolderCoverStylesModule from './FolderCover.scss';
 
 // tslint:disable-next-line:no-any
@@ -11,7 +12,28 @@ export interface IFolderCoverState {
   // TODO Add animation support for drag/drop events.
 }
 
+const enum FolderCoverLayoutValues {
+  smallWidth = 72,
+  smallHeight = 52,
+  largeWidth = 112,
+  largeHeight = 80,
+  contentPadding = 4
+}
+
 const ASSET_CDN_BASE_URL = 'https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets';
+
+const SIZES: {
+  [P in FolderCoverSize]: ISize;
+} = {
+    small: {
+      width: FolderCoverLayoutValues.smallWidth - FolderCoverLayoutValues.contentPadding * 2,
+      height: FolderCoverLayoutValues.smallHeight - FolderCoverLayoutValues.contentPadding * 2
+    },
+    large: {
+      width: FolderCoverLayoutValues.largeWidth - FolderCoverLayoutValues.contentPadding * 2,
+      height: FolderCoverLayoutValues.largeHeight - FolderCoverLayoutValues.contentPadding * 2
+    }
+  };
 
 const ASSETS: {
   [P in FolderCoverSize]: {
@@ -84,4 +106,31 @@ export class FolderCover extends React.Component<IFolderCoverProps, IFolderCover
       </div>
     );
   }
+}
+
+export interface IFolderCoverLayout {
+  contentSize: ISize;
+}
+
+export function getFolderCoverLayout(element: JSX.Element): IFolderCoverLayout {
+  const folderCoverProps: IFolderCoverProps = element.props;
+
+  const {
+    folderCoverSize = 'large'
+  } = folderCoverProps;
+
+  return {
+    contentSize: SIZES[folderCoverSize]
+  };
+}
+
+export function renderFolderCoverWithLayout(element: JSX.Element, props: Partial<IFolderCoverProps>): JSX.Element {
+  const Tag = element.type;
+
+  return (
+    <Tag
+      { ...element.props }
+      { ...props }
+    />
+  );
 }
