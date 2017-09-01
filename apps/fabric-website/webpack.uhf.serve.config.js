2afe121074cd9f87b8d0ddb54b3b30edaf6cd650
John Miller <miljo@microsoft.com>
2017-09-01 03:38:19 -0700

    explicity set the output filename to fabric-websitev5 in the uhf webpack config

--- apps/fabric-website/webpack.uhf.serve.config.js
+++ apps/fabric-website/webpack.uhf.serve.config.js
@@ -1,5 +1,5 @@
 /**
- * NOTE: Use this config file is for when you want to build and serve the website
+ * NOTE: This config file is for when you want to build and serve the website
  * for the purpose of testing your local changes with the UHF.
  * Using this will considerably slow down the build and serve process
  * but will allow you to access the output file on your local machine.
@@ -25,6 +25,7 @@ serveConfig.devServer.host = HOST_NAME;
 serveConfig.devServer.disableHostCheck = true;
 serveConfig.output.path = path.join(__dirname, '/dist');
 serveConfig.output.publicPath = '/dist/';
+serveConfig.output.filename = entryPointFilename + '.js';
 serveConfig.output.chunkFilename = `${entryPointFilename}-${version}-[name]${minFileNamePart}.js`;
 
 module.exports = serveConfig;
