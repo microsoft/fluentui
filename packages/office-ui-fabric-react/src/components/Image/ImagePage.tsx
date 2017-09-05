30af1e97a79ea7605472b5365bc9577410003952
REDMOND\emlynam <lynam.emily@gmail.com>
2017-09-05 11:05:43 -0700

    Revised "do" text

--- packages/office-ui-fabric-react/src/components/Image/ImagePage.tsx
+++ packages/office-ui-fabric-react/src/components/Image/ImagePage.tsx
@@ -67,10 +67,11 @@ export class ImagePage extends React.Component<IComponentDemoPageProps, {}> {
           <div>
             <ul>
               <li>
-                Provide descriptive values for the alt text attribute for all images. If alt text isn't provided for images, the image information is inaccessible, for example, to people who cannot see and use a screen reader that reads aloud the information on a page, including the alt text for the visual image. Make the description brief, but accurate.
+                Provide descriptive values for the alt text attribute for all images.
+                If alt text isn't provided for images, the image information is inaccessible, for example, to people who cannot see and use a screen reader that reads aloud the information on a page.
                 <ul>
                   <li>Consider context. If the image represents a function, be sure to indicate that. If it is to be consumed with other objects on the page, consider that as well.</li>
-                  <li>Be brief. Use one sentence, two at the most.</li>
+                  <li>Make the description brief but accurate. Use one sentence, two at the most.</li>
                 </ul>
               </li>
             </ul>
