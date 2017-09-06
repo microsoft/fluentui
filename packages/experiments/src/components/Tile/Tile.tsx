86ccff93a3fb16aa901ced4ad61ff91a71bdd420
Thomas Michon <tmichon@microsoft.com>
2017-09-06 16:26:17 -0700

    Add support for Tile breakpoint sizes

--- packages/experiments/src/components/Tile/Tile.tsx
+++ packages/experiments/src/components/Tile/Tile.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { ITileProps } from './Tile.Props';
+import { ITileProps, TileSize } from './Tile.Props';
 import { Check } from 'office-ui-fabric-react/lib/Check';
 import { SELECTION_CHANGE } from 'office-ui-fabric-react/lib/Selection';
 import { css, BaseComponent, autobind, getId } from 'office-ui-fabric-react/lib/Utilities';
@@ -15,15 +15,42 @@ const SignalStyles: any = SignalStylesModule;
 
 const enum TileLayoutValues {
   nameplatePadding = 12,
-  nameplateNameHeight = 17,
+  largeNameplateNameHeight = 17,
+  smallNameplateNameHeight = 14,
   nameplateMargin = 4,
-  nameplateActivityHeight = 14,
+  largeNameplateActivityHeight = 14,
+  smallNameplateActivityHeight = 14,
   foregroundMargin = 16
 }
 
 export interface ITileState {
   isSelected?: boolean;
 }
+
+const SIZES: {
+  [P in TileSize]: {
+    nameplatePadding: number;
+    nameplateNameHeight: number;
+    nameplateMargin: number;
+    nameplateActivityHeight: number;
+    foregroundMargin: number;
+  };
+} = {
+    small: {
+      nameplatePadding: TileLayoutValues.nameplatePadding,
+      nameplateNameHeight: TileLayoutValues.smallNameplateNameHeight,
+      nameplateMargin: TileLayoutValues.nameplateMargin,
+      nameplateActivityHeight: TileLayoutValues.smallNameplateActivityHeight,
+      foregroundMargin: TileLayoutValues.foregroundMargin
+    },
+    large: {
+      nameplatePadding: TileLayoutValues.nameplatePadding,
+      nameplateNameHeight: TileLayoutValues.largeNameplateNameHeight,
+      nameplateMargin: TileLayoutValues.nameplateMargin,
+      nameplateActivityHeight: TileLayoutValues.largeNameplateActivityHeight,
+      foregroundMargin: TileLayoutValues.foregroundMargin
+    }
+  };
 
 /**
  * A tile provides a frame for a potentially-selectable item which displays its contents prominently.
@@ -120,6 +147,8 @@ export class Tile extends BaseComponent<ITileProps, ITileState> {
       itemActivity,
       componentRef,
       className,
+      tileSize = 'large',
+      contentSize,
       ...aProps
     } = this.props;
 
@@ -136,6 +165,8 @@ export class Tile extends BaseComponent<ITileProps, ITileState> {
         aria-labelledby={ this._nameId }
         aria-describedby={ this._activityId }
         className={ css('ms-Tile', className, TileStyles.tile, {
+          [`ms-Tile--isSmall ${TileStyles.isSmall}`]: tileSize === 'small',
+          [`ms-Tile--isLarge ${TileStyles.isLarge}`]: tileSize === 'large',
           [`ms-Tile--hasBackgroundFrame ${TileStyles.hasBackgroundFrame}`]: showBackgroundFrame,
           [`ms-Tile--isSelected ${TileStyles.selected} ${SignalStyles.selected}`]: isSelected,
           [`ms-Tile--isSelectable ${TileStyles.selectable}`]: isSelectable,
@@ -308,7 +339,8 @@ export function getTileLayout(tileElement: JSX.Element): ITileLayout {
   const tileProps: ITileProps = tileElement.props;
 
   const {
-    contentSize
+    contentSize,
+    tileSize = 'large'
   } = tileProps;
 
   if (!contentSize) {
@@ -317,22 +349,30 @@ export function getTileLayout(tileElement: JSX.Element): ITileLayout {
 
   const width = contentSize.width;
 
+  const {
+    nameplatePadding,
+    nameplateMargin,
+    nameplateActivityHeight,
+    nameplateNameHeight,
+    foregroundMargin
+  } = SIZES[tileSize];
+
   let nameplateHeight = 0;
 
   if (tileProps.itemName || tileProps.itemActivity) {
-    nameplateHeight += TileLayoutValues.nameplatePadding * 2; // 12px top/bottom padding.
+    nameplateHeight += nameplatePadding * 2; // 12px top/bottom padding.
     if (tileProps.itemName) {
-      nameplateHeight += TileLayoutValues.nameplateNameHeight;
+      nameplateHeight += nameplateNameHeight;
     }
     if (tileProps.itemActivity) {
-      nameplateHeight += TileLayoutValues.nameplateActivityHeight + TileLayoutValues.nameplateMargin;
+      nameplateHeight += nameplateActivityHeight + nameplateMargin;
     }
   }
 
   return {
     foregroundSize: {
-      width: width - TileLayoutValues.foregroundMargin * 2,
-      height: contentSize.height - TileLayoutValues.foregroundMargin - nameplateHeight
+      width: width - foregroundMargin * 2,
+      height: contentSize.height - foregroundMargin - nameplateHeight
     },
     backgroundSize: contentSize
   };
