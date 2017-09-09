a8421a37737bd0661825baef89346ce13ea6dbab
Erich Abellera <a.erich@gmail.com>
2017-09-08 17:14:23 -0700

    Update deploy github status to get latest commit ID

--- scripts/deploy-github-status.js
+++ scripts/deploy-github-status.js
@@ -2,8 +2,19 @@ let argv = require('yargs').argv;
 let GitHubApi = require('github');
 
 const REPO_DETAILS = {
-
+  owner: "erichdev",
+  repo: "office-ui-fabric-react",
 };
+
+let statusConfig = {
+  ...REPO_DETAILS,
+  state: "success",
+  target_url: "http://odsp-ext.azurewebsites.net/fabric-deploy-test/" + argv.prID,
+  description: "PR Deployed - click \"Details\" to view site",
+  context: "vsts/pr-deploy"
+};
+
+let pr = parsePRNumber();
 
 if (!argv.token) {
   throw new Error("No token specified. Use --token=<token> to provide a token.");
@@ -17,20 +28,46 @@ github.authenticate({
   token: argv.token
 });
 
-let statusConfig = {
-  owner: "erichdev",
-  repo: "office-ui-fabric-react",
-  state: "success",
-  sha: argv.commitID,
-  target_url: "http://odsp-ext.azurewebsites.net/fabric-deploy-test/" + argv.prID,
-  description: "PR Deployed - click \"Details\" to view site",
-  context: "vsts/pr-deploy"
-};
+getLatestCommitFromPR();
 
-github.repos.createStatus(statusConfig, (err, res) => {
-  if (err) {
-    throw new Error(`Failed to deploy pull request #${argv.prID}. \n ${err}`);
+function createStatus(sha) {
+  github.repos.createStatus({ ...statusConfig, sha }, (err, res) => {
+    if (err) {
+      throw new Error(`Failed to deploy pull request #${pr}. \n ${err}`);
+    }
+
+    console.log(`Successfully deployed pull request #${pr}`);
+  });
+}
+
+function getLatestCommitFromPR() {
+  github.pullRequests.get({
+    ...REPO_DETAILS,
+    number: pr
+  }, onGetLatestCommit);
+}
+
+function onGetLatestCommit(err, res) {
+  if (err)
+    throw new Error(`Failed to get latest commit. \n ${err}`);
+
+  // Extract the head sha from response. See https://developer.github.com/v3/pulls/#get-a-single-pull-request for full response details
+  if (res.head && res.head.sha)
+    createStatus(res.head.sha);
+}
+
+/*
+The PR ID is input in the format 'refs/pull/12/merge'. This function returns just the number.
+*/
+function parsePRNumber() {
+  let splitString;
+
+  if (argv.prID) {
+    splitString = argv.prID.split('/');
   }
 
-  console.log(`Successfully deployed pull request #${argv.prID}`);
-});
+  if (!argv.prID || !splitString[2])
+    throw new Error(`Failed to get PR number. \n ${err}`);
+
+  return splitString[2];
+}
