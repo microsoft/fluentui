af4c1af478b905d9475e403bcd550c627a380647
John Miller <miljo@microsoft.com>
2017-09-13 23:39:33 -0700

    Cleaning up the webpack.config file, updated the logic for choosing an entry point/filename for webpack and added a timestamp to the chunk names so they don't overwrite each other on the CDN.

--- apps/fabric-website/webpack.config.js
+++ apps/fabric-website/webpack.config.js
@@ -1,49 +1,55 @@
-const path = require('path');
-const resources = require('../../scripts/tasks/webpack-resources');
+module.exports = function (argv) {
+  const path = require('path');
+  const resources = require('../../scripts/tasks/webpack-resources');
+  const version = require('./package.json').version;
+  const isDogfoodArg = argv.indexOf('--dogfood') > -1;
+  const isProductionArg = argv.indexOf('--production') > -1;
+  const now = Date.now();
 
-const isProduction = process.argv.indexOf('--production') > -1;
-const isDogfood = process.argv.indexOf('--dogfood') > -1;
-const version = require('./package.json').version;
-let publicPath = 'https://static2.sharepointonline.com/files/fabric/fabric-website/dist/';
+  // Production defaults
+  let minFileNamePart = '';
+  let entryPointName = 'fabric-sitev5';
+  let publicPath = 'https://static2.sharepointonline.com/files/fabric/fabric-website/dist/';
 
-let minFileNamePart = isDogfood ? '.df' : '';
+  // Dogfood overrides
+  if (isDogfoodArg) {
+    publicPath = 'https://static2df.sharepointonline.com/files/fabric/fabric-website/dist/';
+    entryPointName = 'fabric-websitev5-df';
+  } else if (!isProductionArg) {
+    publicPath = "/dist/";
+  } else {
+    minFileNamePart = '.min';
+  }
 
-if (isDogfood) {
-  publicPath = 'https://static2df.sharepointonline.com/files/fabric/fabric-website/dist/';
-} else if (!isProduction) {
-  publicPath = "/dist/";
+  return resources.createConfig(
+    entryPointName,
+    isProductionArg,
+    {
+      entry: {
+        [entryPointName]: './lib/root.js'
+      },
+
+      output: {
+        publicPath: publicPath,
+        chunkFilename: `${entryPointName}-${version}-[name]-${now}${minFileNamePart}.js`
+      },
+
+      externals: [
+        {
+          'react': 'React'
+        },
+        {
+          'react-dom': 'ReactDOM'
+        },
+      ],
+
+      resolve: {
+        alias: {
+          'office-ui-fabric-react/src': path.join(__dirname, 'node_modules/office-ui-fabric-react/src'),
+          'office-ui-fabric-react/lib': path.join(__dirname, 'node_modules/office-ui-fabric-react/lib')
+        }
+      },
+    },
+    isProductionArg /* only production */
+  );
 }
-
-const PACKAGE_NAME = 'fabric-sitev5';
-
-module.exports = resources.createConfig(
-  PACKAGE_NAME,
-  isProduction,
-  {
-    entry: {
-      [PACKAGE_NAME]: './lib/root.js'
-    },
-
-    output: {
-      publicPath: publicPath,
-      chunkFilename: `${PACKAGE_NAME}-${version}-[name]${minFileNamePart}.min.js`
-    },
-
-    externals: [
-      {
-        'react': 'React'
-      },
-      {
-        'react-dom': 'ReactDOM'
-      },
-    ],
-
-    resolve: {
-      alias: {
-        'office-ui-fabric-react/src': path.join(__dirname, 'node_modules/office-ui-fabric-react/src'),
-        'office-ui-fabric-react/lib': path.join(__dirname, 'node_modules/office-ui-fabric-react/lib')
-      }
-    },
-  },
-  true /* only production */
-);
