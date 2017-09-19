8a6e521a97c77a5b7eb698151447cede06861d88
Philip Kuo <phkuo@microsoft.com>
2017-09-19 15:56:36 -0700

    fix spacing and comments

--- packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.Props.ts
+++ packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.Props.ts
@@ -1,21 +1,21 @@
 export interface IColorPickerProps {
-    /**
-     * Gets the component ref.
-     */
-    componentRef?: () => void;
+  /**
+   * Gets the component ref.
+   */
+  componentRef?: () => void;
 
-    /**
-     * The CSS-compatible string to describe the initial color
-     */
-    color: string;
+  /**
+   * The CSS-compatible string to describe the initial color
+   */
+  color: string;
 
-    /**
-     * Callback issued when the user changes the color
-     */
-    onColorChanged?: (color: string) => void;
+  /**
+   * Callback issued when the user changes the color
+   */
+  onColorChanged?: (color: string) => void;
 
-    /**
-     * The setting of whether hide alpha control slider.
-     */
-    alphaSliderHidden?: boolean;
+  /**
+   * The setting of whether to hide the alpha control slider.
+   */
+  alphaSliderHidden?: boolean;
 }
