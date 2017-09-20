d7ac9da1a4e43bd6503b82398777f8c128b89b9c
Thomas Michon <tmichon@microsoft.com>
2017-09-20 15:53:24 -0700

    Make alwaysShowCheck the default for Check

--- packages/experiments/src/components/Tile/Tile.tsx
+++ packages/experiments/src/components/Tile/Tile.tsx
@@ -340,7 +340,6 @@ export class Tile extends BaseComponent<ITileProps, ITileState> {
         aria-checked={ isSelected }
       >
         <Check
-          alwaysShowCheck={ false }
           checked={ isSelected }
         />
       </button>
