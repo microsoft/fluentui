94ca47adc6aefba7821eca2ea2fcd863e9357f80
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-09-22 18:06:33 -0500

    Utilities: Add utilities for making changes to arrays immutably (#2919)
    * Add flatten function
    
    * Port over addElementAtIndex and replaceElement
    
    * Fix tests
    
    * Refactor the duplicate code for copying arrays
    
    * Rush change
    
    * Fix tslint
    
    * Rename callback paremeter name
    
    * Update comment
    
    * REmove the copy and mutate funtion, the arrow function overhead worries me.

--- packages/utilities/src/array.ts
+++ packages/utilities/src/array.ts
@@ -79,3 +79,37 @@ export function toMatrix<T>(items: T[], columnCount: number): T[][] {
 export function removeIndex<T>(array: T[], index: number): T[] {
   return array.filter((_: T, i: number) => index !== i);
 }
+
+/**
+ * Given an array, this function returns a new array where the element at a given index has been replaced.
+ * @param array - The array to operate on
+ * @param newElement - The element that will be placed in the new array
+ * @param index - The index of the element that should be replaced
+ */
+export function replaceElement<T>(array: T[], newElement: T, index: number): T[] {
+  const copy = array.slice();
+  copy[index] = newElement;
+  return copy;
+}
+
+/**
+ * Given an array, this function returns a new array where an element has been inserted at the given index.
+ * @param array - The array to operate on
+ * @param index - The index where an element should be inserted
+ * @param itemToAdd - The element to insert
+ */
+export function addElementAtIndex<T>(array: T[], index: number, itemToAdd: T): T[] {
+  const copy = array.slice();
+  copy.splice(index, 0, itemToAdd);
+  return copy;
+}
+
+/**
+ * Given an array where each element is of type T or T[], flatten it into an array of T
+ * @param array - The array where each element can optionally also be an array
+ */
+export function flatten<T>(array: (T | T[])[]): T[] {
+  let result: T[] = [];
+  array.forEach((item: T | T[]): T[] => (result = result.concat(item)));
+  return result;
+}
