2bd351cc11260a602aad572cc2beb780ce48b6e1
Erich Abellera <a.erich@gmail.com>
2017-09-26 10:26:37 -0700

    Update script to generate json files for each component

--- scripts/generate-docs-json.js
+++ scripts/generate-docs-json.js
@@ -7,10 +7,12 @@ const encoding = { encoding: 'utf-8' };
 
 let docs = {};
 
+
 glob("**/*.Props.ts", globConfig, (err, files) => {
   if (files) {
     parseFiles(files);
-    writeJsonTofile();
+    // writeJsonTofile();
+    writeJsonFilesForComponents();
   }
   if (err) {
     console.error('ERROR: ', err);
@@ -27,11 +29,6 @@ function parseFiles(files) {
 
     props.forEach(prop => {
       // Add prop to `docs` only if it's not already there
-      // if (docs.filter(item => item.name === prop.name).length === 0) {
-      //   docs.push(prop);
-      // }
-
-      // Add prop to `docs` only if it's not already there
       if (!docs[prop.name]) {
         docs[prop.name] = prop;
       }
@@ -40,13 +37,100 @@ function parseFiles(files) {
 }
 
 /**
- * Save `docs` to json file.
+ * Save `docs` to json file. This is the master docs file that has all interfaces.
  */
-function writeJsonTofile() {
-  fs.writeFile('packages/example-app-base/docs.json', JSON.stringify(docs), err => {
-    if (!err)
-      console.log('Successfully saved docs.json.');
-    else
-      console.error(err);
+// function writeJsonTofile() {
+//   fs.writeFile('packages/example-app-base/docs.json', JSON.stringify(docs), err => {
+//     if (!err)
+//       console.log('Successfully saved docs.json.');
+//     else
+//       console.error(err);
+//   });
+// }
+
+
+/**
+ * Create docs.json file for each component
+ */
+function writeJsonFilesForComponents() {
+  // Find all documentation files ending in *Page.tsx
+  glob("packages/office-ui-fabric-react/**/*Page.tsx", globConfig, (err, files) => {
+    if (files) {
+      files.forEach((file) => {
+        // Extract the `sources` that are passed into PropertiesTableSet - these are a .Props.ts files
+        let regex = new RegExp('sources={\\s*\\[\\s*([\\s\\S]*)\\s*\\]\\s*}', 'g');
+        let result = regex.exec(fs.readFileSync(file, encoding));
+
+        if (result && result[1]) {
+          let sources = result[1].split(',');
+          sources.forEach(source => {
+            if (source) {
+              // Extract just the filename
+              let regex2 = new RegExp('\\S*!\\S*!(\\S*)\'', 'g');
+              let sourceFile = regex2.exec(source); // sourceFile, i.e. *.Props.ts
+
+              if (sourceFile && sourceFile[1]) {
+                let content = fs.readFileSync(`packages/${sourceFile[1]}`, encoding);
+
+                let interfaces = parser.parse(content);
+
+                let currentDoc = [];
+
+                if (interfaces) {
+                  interfaces.forEach(int => {
+                    if (int) {
+                      getProperties(int.name);
+                    }
+
+                  });
+                }
+
+                function getProperties(int) {
+                  if (int && docs[int]) {
+                    // console.log('int', int);
+
+                    if (!currentDoc[int]) {
+                      currentDoc.push(docs[int]);
+                      // console.log('docs int', docs[int]);
+
+                    }
+
+
+                    //console.log('currentDoc', currentDoc);
+                    if (docs[int].extends.length > 0) {
+                      // console.log('greater');
+                      docs[int].extends.forEach((prop) => getProperties(prop));
+                    }
+                  }
+                }
+
+                let lastSlashIndex = sourceFile[1].lastIndexOf('/');
+                let path = 'packages/' + sourceFile[1].substring(0, lastSlashIndex);
+
+
+                fs.writeFile(`${path}/docs.json`, JSON.stringify(currentDoc), err => {
+                  if (!err)
+                    console.log('Successfully saved file');
+                  else
+                    console.error(err);
+                });
+
+
+
+              }
+            }
+          });
+        }
+      });
+    }
+    if (err) {
+      console.error('ERROR: Couldn\'t parse *Page.tsx files: ', err);
+    }
+
+
+
   });
+
+
 }
+
