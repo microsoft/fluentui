3f87e07b777bcf6a0337994977e4943925ef6e61
Micah Godbolt <micahgodbolt@gmail.com>
2017-09-27 09:57:23 -0700

    updated tests to crop, easier to add new tests (#2956)
    * updated tests to crop, easier to add new tests
    
    * Updated tests, capturing only component area

---
+++ apps/vr-tests/src/utilities/FabricDecorator.tsx
@@ -0,0 +1,22 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+import * as React from 'react';
+import { Fabric } from 'office-ui-fabric-react';
+
+// Wrap all stories in a Fabric component for proper styling
+
+// tslint:disable:jsx-ban-props
+export const FabricDecorator = (story) => (
+  <Fabric style={ { display: 'flex' } }>
+    <div className='testWrapper' style={ { padding: '10px' } }>
+      { story() }
+    </div>
+  </Fabric>
+);
+
+export const FabricDecoratorTall = (story) => (
+  <Fabric style={ { display: 'flex' } }>
+    <div className='testWrapper' style={ { padding: '10px 10px 120px' } }>
+      { story() }
+    </div>
+  </Fabric>
+);
