cb0f427aeeacf0333e241624e2285623368bb48e
Erich Abellera <a.erich@gmail.com>
2017-09-28 18:32:51 -0700

    Use strings for Persona test styles

--- packages/office-ui-fabric-react/src/components/Persona/Persona.test.tsx
+++ packages/office-ui-fabric-react/src/components/Persona/Persona.test.tsx
@@ -4,10 +4,17 @@ import { setRTL } from '../../Utilities';
 import { Persona } from './Persona';
 import { PersonaInitialsColor } from './Persona.Props';
 import { mount, ReactWrapper } from 'enzyme';
-import * as stylesImport from './Persona.scss';
-const styles: any = stylesImport;
+// import * as stylesImport from './Persona.scss';
+// const styles: any = stylesImport;
 
 const testImage1x1 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQImWP4DwQACfsD/eNV8pwAAAAASUVORK5CYII=';
+const STYLES = {
+  darkRed: '.ms-Persona-initials--darkRed',
+  initials: '.ms-Persona-initials',
+  lightBlue: '.ms-Persona-initials--lightBlue',
+  red: '.ms-Persona-initials--red',
+
+};
 
 describe('Persona', () => {
   beforeEach(() => {
@@ -17,38 +24,41 @@ describe('Persona', () => {
   describe('initials and colors', () => {
     it('renders with expected initialsColor if none was provided', () => {
       const wrapper = mount(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.' + styles.initialsIsRed);
+      let result = wrapper.find(STYLES.red);
       expect(result).toHaveLength(1);
 
       const wrapper2 = mount(<Persona primaryText='Annie Lindqvist' />);
-      result = wrapper2.find('.' + styles.initialsIsDarkRed);
+      result = wrapper2.find(STYLES.darkRed);
       expect(result).toHaveLength(1);
     });
 
     it('uses provided initialsColor if one was specified', () => {
       const wrapper = mount(<Persona primaryText='Kat Larrson' initialsColor={ PersonaInitialsColor.lightBlue } />);
-      let result = wrapper.find('.' + styles.initialsIsLightBlue);
+      let result = wrapper.find(STYLES.lightBlue);
       expect(result).toHaveLength(1);
     });
 
-    it('calculates an expected initials in LTR if one was not specified', () => {
+    it.only('calculates an expected initials in LTR if one was not specified', () => {
       let wrapper = mount(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.' + styles.initials);
+      let result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('KL');
+      wrapper.unmount();
 
       wrapper = mount(<Persona primaryText='David Zearing-Goff' />);
-      result = wrapper.find('.' + styles.initials);
+      result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('DZ');
+      wrapper.unmount();
 
       wrapper = mount(<Persona primaryText='4lex 4loo' />);
-      result = wrapper.find('.' + styles.initials);
+      result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('44');
+      wrapper.unmount();
 
       wrapper = mount(<Persona primaryText='David (The man) Goff' />);
-      result = wrapper.find('.' + styles.initials);
+      result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('DG');
     });
@@ -56,7 +66,7 @@ describe('Persona', () => {
     it('calculates an expected initials in RTL if one was not specified', () => {
       setRTL(true);
       const wrapper = mount(<Persona primaryText='Kat Larrson' />);
-      let result = wrapper.find('.' + styles.initials);
+      let result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('LK');
 
@@ -66,7 +76,7 @@ describe('Persona', () => {
     it('uses provided initial', () => {
       setRTL(true);
       const wrapper = mount(<Persona primaryText='Kat Larrson' imageInitials='AT' />);
-      let result = wrapper.find('.' + styles.initials);
+      let result = wrapper.find(STYLES.initials);
       expect(result).toHaveLength(1);
       expect(result.text()).toEqual('AT');
 
