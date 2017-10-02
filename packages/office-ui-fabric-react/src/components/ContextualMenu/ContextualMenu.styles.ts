6b7f245fb2e5c2f966ce786cccaea413e7723b39
montselozanod <montse_967@hotmail.com>
2017-10-02 00:20:30 -0700

    Add styles file to ContextualMenu component

---
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.styles.ts
@@ -0,0 +1,171 @@
+import { memoizeFunction } from '../../Utilities';
+import { IContextualMenuStyles, IMenuItemStyle } from './ContextualMenu.Props';
+import {
+  ITheme,
+  //  IStyle,
+  mergeStyleSets,
+  //  IFontStyles,
+  FontWeights,
+  //  getFocusStyle
+} from '../../Styling';
+
+const ContextualMenuItemHeight = '32px';
+const ContextualMenuIconWidth = '14px';
+
+export const getMenuItemStyles = memoizeFunction((
+  theme: ITheme,
+  customMenuItemStyles?: Partial<IMenuItemStyle>
+): IMenuItemStyle => {
+  const { semanticColors, fonts } = theme;
+  const ContextualMenuIconColor = semanticColors.menuIcon;
+  const ContextualMenuTextColor = semanticColors.bodyText;
+  const ContextualMenuItemBackgroundHoverColor = semanticColors.menuItemBackgroundHovered;
+  const ContextualMenuItemBackgroundSelectedColor = semanticColors.menuItemBackgroundChecked;
+  const ContextualMenuTextSelectedColor = semanticColors.menuSelectedText;
+  const ContextualMenuTextDisabledColor = semanticColors.disabledText;
+
+  const menuItemStyles: IMenuItemStyle = {
+    item: [
+      fonts.medium,
+      {
+        color: ContextualMenuTextColor,
+        position: 'relative',
+        boxSizing: 'border-box',
+      }],
+    root: {
+      font: 'inherit',
+      color: 'inherit',
+      background: 'none',
+      border: 'none',
+      width: '100%',
+      height: ContextualMenuItemHeight,
+      lineHeight: ContextualMenuItemHeight,
+      display: 'block',
+      cursor: 'pointer',
+      padding: '0px 6px',
+      textAlign: 'left',
+    },
+    rootDisabled: {
+      color: ContextualMenuTextDisabledColor,
+      cursor: 'default',
+      pointerEvents: 'none',
+    },
+    rootHovered: {
+      background: ContextualMenuItemBackgroundHoverColor,
+    },
+    rootExpanded: {
+      background: ContextualMenuItemBackgroundSelectedColor,
+      color: ContextualMenuTextSelectedColor,
+      fontWeight: FontWeights.semibold,
+    },
+    linkContent: {
+      whiteSpace: 'nowrap',
+      height: 'inherit',
+      display: 'flex',
+      alignItems: 'center',
+      maxWidth: '100%'
+    },
+    anchorLink: {
+      padding: '0px 6px',
+      textRendering: 'auto',
+      color: 'inherit',
+      letterSpacing: 'normal',
+      wordSpacing: 'normal',
+      textTransform: 'none',
+      textIndent: '0px',
+      textShadow: 'none',
+      textDecoration: 'none',
+      boxSizing: 'border-box'
+    },
+    label: {
+      margin: '0 4px',
+      verticalAlign: 'middle',
+      display: 'inline-block',
+      flexGrow: '1',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap'
+    },
+    icon: {
+      display: 'inline-block',
+      minHeight: '1px',
+      maxHeight: ContextualMenuItemHeight,
+      width: ContextualMenuIconWidth,
+      margin: '0 4px',
+      verticalAlign: 'middle',
+      flexShrink: '0',
+    },
+    iconColor: {
+      color: ContextualMenuIconColor
+    },
+    subMenuIcon: {
+      height: ContextualMenuItemHeight,
+      lineHeight: ContextualMenuItemHeight,
+      textAlign: 'center',
+      fontSize: fonts.xSmall,
+      display: 'inline-block',
+      verticalAlign: 'middle',
+      flexShrink: '0'
+    }
+  };
+
+  return mergeStyleSets(menuItemStyles, customMenuItemStyles) as IMenuItemStyle;
+});
+
+export const getStyles = memoizeFunction((
+  theme: ITheme,
+  customStyles?: Partial<IContextualMenuStyles>,
+): IContextualMenuStyles => {
+  const { semanticColors, fonts } = theme;
+
+  const ContextualMenuBackground = semanticColors.bodyBackground;
+  const ContextualMenuHeaderColor = semanticColors.menuHeader;
+  const ContextualMenuDividerColor = semanticColors.bodyDivider;
+
+  const styles: IContextualMenuStyles = {
+    root: {
+      backgroundColor: ContextualMenuBackground,
+      minWidth: '180px',
+    },
+    container: {
+
+    },
+    list: {
+      listStyleType: 'none',
+      margin: '0',
+      padding: '0',
+      lineHeight: '0',
+    },
+    title: {
+      fontSize: '16px',
+      paddingRight: '14px',
+      paddingLeft: '14px',
+      paddingBottom: '5px',
+      paddingTop: '5px',
+      backgroundColor: theme.palette.neutralLight
+    },
+    header: [
+      // @include focus-border();
+      fonts.small,
+      {
+        fontWeight: FontWeights.semibold,
+        color: ContextualMenuHeaderColor,
+        background: 'none',
+        border: 'none',
+        height: ContextualMenuItemHeight,
+        lineHeight: ContextualMenuItemHeight,
+        cursor: 'default',
+        padding: '0px 6px',
+        userSelect: 'none',
+        textAlign: 'left',
+      }
+    ],
+    divider: {
+      display: 'block',
+      height: '1px',
+      backgroundColor: ContextualMenuDividerColor,
+      position: 'relative'
+    },
+  };
+  return mergeStyleSets(styles, customStyles) as IContextualMenuStyles;
+});
