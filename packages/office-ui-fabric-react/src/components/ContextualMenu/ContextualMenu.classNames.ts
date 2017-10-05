c62a4aabff2a4147bd75f70eb6c2716a646d4321
montselozanod <montse_967@hotmail.com>
2017-10-05 13:26:16 -0700

    Make classnames in ContextualMenu optional

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.classNames.ts
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.classNames.ts
@@ -3,21 +3,21 @@ import { ITheme, mergeStyles } from '../../Styling';
 import { IContextualMenuStyles, IMenuItemStyles } from './ContextualMenu.Props';
 import { getStyles as getContextualMenuStyles, getMenuItemStyles } from './ContextualMenu.styles';
 export interface IContextualMenuClassNames {
-  container: string;
-  root: string;
-  list: string;
-  divider: string;
-  header: string;
-  title: string;
+  container?: string;
+  root?: string;
+  list?: string;
+  divider?: string;
+  header?: string;
+  title?: string;
 }
 
 export interface IMenuItemClassNames {
-  item: string;
-  root: string;
-  linkContent: string;
+  item?: string;
+  root?: string;
+  linkContent?: string;
   icon?: string;
   subMenuIcon?: string;
-  label: string;
+  label?: string;
 }
 
 export const getContextualMenuClassNames = memoizeFunction((
