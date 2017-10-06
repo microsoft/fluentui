ebff4e48592c3ffb7927845e1bea04e540536279
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-10-05 17:02:20 -0700

    Use customizable to get the theme.

--- packages/office-ui-fabric-react/src/components/Label/Label.tsx
+++ packages/office-ui-fabric-react/src/components/Label/Label.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import { BaseComponent, divProperties, getNativeProps } from '../../Utilities';
+import { BaseComponent, divProperties, getNativeProps, customizable } from '../../Utilities';
 import { ILabelProps } from './Label.Props';
 import { getLabelClassNames } from './Label.classNames';
-import { getTheme } from '../../Styling';
 
-
-export class Label extends BaseComponent<ILabelProps, any> {
+@customizable('Label', ['theme'])
+export class Label extends BaseComponent<ILabelProps, {}> {
   public render() {
-    let { disabled, required, children, className } = this.props;
-    let theme = getTheme();
+    let { disabled, required, children, className, theme } = this.props;
     return (
       <label
         { ...getNativeProps(this.props, divProperties) }
-        className={ getLabelClassNames(theme, className, !!disabled, !!required).root }
+        className={ getLabelClassNames(theme!, className, !!disabled, !!required).root }
       >
         { children }
       </label>
