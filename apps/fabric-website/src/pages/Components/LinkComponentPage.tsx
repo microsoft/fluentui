1902b3b8f073d28c0a8f46e7194f8e0c3fc3d064
lynamemi <lynam.emily@gmail.com>
2017-10-06 16:04:17 -0700

    Website: Added /removed Best Practices links on component pages (#3057)
    * Added /removed Best Practices links on componenet pages
    
    * Added change file
    
    * Removed comma
    
    * Title case for best practices section

--- apps/fabric-website/src/pages/Components/LinkComponentPage.tsx
+++ apps/fabric-website/src/pages/Components/LinkComponentPage.tsx
@@ -17,6 +17,10 @@ export class LinkComponentPage extends React.Component<any, any> {
                   'location': 'Overview'
                 },
                 {
+                  'text': 'Best Practices',
+                  'location': 'Best Practices'
+                },
+                {
                   'text': 'Variants',
                   'location': 'Variants'
                 },
--- apps/fabric-website/src/pages/Components/ModalComponentPage.tsx
+++ apps/fabric-website/src/pages/Components/ModalComponentPage.tsx
@@ -17,6 +17,10 @@ export class ModalComponentPage extends React.Component<any, any> {
                   'location': 'Overview'
                 },
                 {
+                  'text': 'Best Practices',
+                  'location': 'Best Practices'
+                },
+                {
                   'text': 'Variants',
                   'location': 'Variants'
                 },
