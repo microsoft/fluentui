e151da5520b367108ec4bd30c50d79b493e98c74
Ian VanSchooten <ian.vanschooten@gmail.com>
2017-10-09 12:32:55 -0400

    Clarify ComboBox autoComplete docs

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.Props.ts
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.Props.ts
@@ -54,9 +54,11 @@ export interface IComboBoxProps extends ISelectableDroppableTextProps<IComboBox>
 
   /**
    * Whether the ComboBox auto completes. As the user is inputing text, it will be suggested potential matches from the list of options. If
-   * the combo box is expanded, this will also scroll to the suggested option, and give it a selected style. Defaults to false.
+   * the combo box is expanded, this will also scroll to the suggested option, and give it a selected style.
+   *
+   * @default "on"
    */
-  autoComplete?: string;
+  autoComplete?: 'on' | 'off';
 
   /**
    * Value to show in the input, does not have to map to a combobox option
