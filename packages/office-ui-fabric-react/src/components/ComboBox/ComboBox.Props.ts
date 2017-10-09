0f730811fb36cd55a853f9be0f69dcf78af1b577
kellygorr <kegorr@microsoft.com>
2017-10-09 09:44:22 -0700

    Updating file

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.Props.ts
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.Props.ts
@@ -94,6 +94,7 @@ export interface IComboBoxProps extends ISelectableDroppableTextProps<IComboBox>
    * @default false;
    */
   scrollSelectedToTop?: boolean;
+  
 }
 
 export interface IComboBoxStyles {
