87edd33957969167859dac08cdca4b6e8a9b6230
Kelly GORR <kegorr@microsoft.com>
2017-10-09 12:38:26 -0700

    Removing unnecessary code

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
@@ -949,10 +949,6 @@ export class ComboBox extends BaseComponent<IComboBoxProps, IComboBoxState> {
       return -1;
     }
 
-    if (typeof selectedKey === 'number') {
-      return selectedKey;
-    }
-
     return findIndex(options, (option => (option.selected || option.key === selectedKey)));
   }
 
