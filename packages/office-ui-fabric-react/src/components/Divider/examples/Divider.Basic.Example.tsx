2d99fe653d782007d95a3ad89072140af7b34f66
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-10-12 18:07:22 -0700

    More setup

--- packages/office-ui-fabric-react/src/components/Divider/examples/Divider.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Divider/examples/Divider.Basic.Example.tsx
@@ -1,8 +1,36 @@
 import * as React from 'react';
 import { Divider } from 'office-ui-fabric-react/lib/Divider';
+import { mergeStyleSets, getTheme, ITheme } from 'office-ui-fabric-react/lib/Styling';
+import { memoizeFunction } from 'office-ui-fabric-react/lib/Utilities';
+
+interface IBasicDividerExampleClassNames {
+  wrapper: string;
+  text: string;
+}
+
+const getClassNames = memoizeFunction((theme: ITheme): IBasicDividerExampleClassNames => {
+  const exampleHeight = 40;
+  return mergeStyleSets({
+    wrapper: {
+      height: 40,
+      backgroundColor: theme.palette.neutralLight,
+    },
+    text: {
+      display: 'inline-block',
+      padding: '0 10px'
+    }
+  });
+});
 
 export class DividerBasicExample extends React.Component<any, any> {
   public render() {
-    return (<Divider />);
+    const theme = getTheme();
+    const classNames = getClassNames(theme);
+    return (
+      <div className={ classNames.wrapper }>
+        <p className={ classNames.text }> Some text before the divider. </p>
+        <Divider />
+        <p className={ classNames.text }>Some text after the divider. </p>
+      </div>);
   }
 }
