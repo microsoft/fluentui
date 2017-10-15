20825b0b48145c2d6c0fd793ede5631aaf3c048f
David Zearing <dzearing@microsoft.com>
2017-10-15 13:30:38 -0700

    Fixing paths.

--- packages/experiments/jest.config.js
+++ packages/experiments/jest.config.js
@@ -6,7 +6,7 @@ const config = createConfig({
     path.resolve(__dirname, 'lib/common/tests.js')
   ],
   snapshotSerializers: [
-    path.resolve(__dirname, '../node_modules/@uifabric/jest-serializer-merge-styles')
+    path.resolve(__dirname, './node_modules/@uifabric/jest-serializer-merge-styles')
   ]
 });
 
