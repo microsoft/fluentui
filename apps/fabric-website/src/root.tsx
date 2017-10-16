3b3163c0889bba473c06a312bc617f2365f955f5
lynamemi <lynam.emily@gmail.com>
2017-10-16 09:27:29 -0700

    Updated website docs to most recent fabric core

--- apps/fabric-website/src/root.tsx
+++ apps/fabric-website/src/root.tsx
@@ -145,4 +145,4 @@ function addCSSToHeader(fileName: string) {
   headEl.appendChild(linkEl);
 }
 
-addCSSToHeader('https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/7.3.0/css/fabric.min.css');
+addCSSToHeader('https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/9.0.0/css/fabric.min.css');
