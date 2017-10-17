ca61e4d4d87c4c9a2562d8e82a5cf36aeca2c40d
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-10-17 13:27:36 -0700

    Remove empty space

--- packages/office-ui-fabric-react/src/components/Divider/VerticalDivider.Props.ts
+++ packages/office-ui-fabric-react/src/components/Divider/VerticalDivider.Props.ts
@@ -5,7 +5,6 @@ export interface IDivider {
 }
 
 export interface IVerticalDividerProps {
-
   /**
    * Optional callback to access the IDivider interface. Use this instead of ref for accessing
    * the public methods and properties of the component.
