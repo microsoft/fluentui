dd17707d4bbc820f0ff9247b5762a6dea3acd368
David Zearing <dzearing@microsoft.com>
2017-10-17 09:57:50 -0700

    Merge styles: fixed RTL registration case. (#3136)
    * Addressing issue when registering opacity: 0 in rtl using mergeStyles keyframes helper.
    
    * Adding change file.

--- packages/merge-styles/src/keyframes.test.ts
+++ packages/merge-styles/src/keyframes.test.ts
@@ -3,6 +3,7 @@ import {
   Stylesheet,
   InjectionMode
 } from './Stylesheet';
+import { setRTL } from './transforms/rtlifyRules';
 
 const _stylesheet: Stylesheet = Stylesheet.getInstance();
 
@@ -13,7 +14,27 @@ describe('keyframes', () => {
     _stylesheet.reset();
   });
 
+  afterEach(() => {
+    setRTL(false);
+  });
+
   it('can register from/to keyframes', () => {
+    keyframes({
+      from: {
+        opacity: 0,
+      },
+      to: {
+        opacity: 1
+      }
+    });
+
+    expect(_stylesheet.getRules()).toEqual(
+      '@keyframes css-0{from{opacity:0;}to{opacity:1;}}'
+    );
+  });
+
+  it('can register from/to keyframes in rtl', () => {
+    setRTL(true);
     keyframes({
       from: {
         opacity: 0,
