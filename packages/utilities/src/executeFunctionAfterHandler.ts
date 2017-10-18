90f215f84c4ec5ff4729dd06affe95f7c1461636
Christian Gonzalez <christianjordangonzalez@gmail.com>
2017-10-17 18:08:23 -0700

    No memoize for now

--- packages/utilities/src/executeFunctionAfterHandler.ts
+++ packages/utilities/src/executeFunctionAfterHandler.ts
@@ -5,10 +5,10 @@ import { memoizeFunction } from './memoize';
  * Given a function that is used as an event handler, execute the given function after the handler has been executed.
  * An example would be to execute a function that logs that an event happened after handling the event.
  */
-export const executeFunctionAfterHandler = memoizeFunction(<T extends (...args: any[]) => void>(
+export function executeFunctionAfterHandler<T extends (...args: any[]) => void>(
   handler: T | undefined,
   functionToExecute: () => void
-): T => {
+): T {
   // tslint:disable-next-line:no-function-expression
   return function wrappedFunction(...args: any[]): void {
     if (handler) {
@@ -16,4 +16,4 @@ export const executeFunctionAfterHandler = memoizeFunction(<T extends (...args: 
     }
     functionToExecute();
   } as T;
-});
+}
