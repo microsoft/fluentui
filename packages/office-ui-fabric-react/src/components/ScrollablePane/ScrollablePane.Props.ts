dcee15b2a8e47acc3b7bd5fd4f3125139c660a62
Bryan Watts <brwatt@microsoft.com>
2017-10-19 10:52:33 -0700

    Add a way to force a layout update for ScrollablePane (#3152)
    * Add a way to force a layout update for ScrollablePane
    
    * Add change file

--- packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePane.Props.ts
+++ packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePane.Props.ts
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { ScrollablePane } from './ScrollablePane';
 
+export interface IScrollablePane {
+  /** Triggers a layout update for the pane. */
+  forceLayoutUpdate(): void;
+}
+
 export interface IScrollablePaneProps extends React.HTMLAttributes<HTMLElement | ScrollablePane> {
   /**
-   * Gets ref to component interface.
+   * Optional callback to access the IScrollablePane interface. Use this instead of ref for accessing
+   * the public methods and properties of the component.
    */
-  componentRef?: (component: IScrollablePaneProps) => void;
+  componentRef?: (component: IScrollablePane) => void;
 
   /**
    * Class name to apply to the root in addition to ms-ScrollablePane.
