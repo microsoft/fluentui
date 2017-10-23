cb1ccd11641e62ff52a1e6f139f632d82c2b688a
erichdev <a.erich@gmail.com>
2017-10-23 15:29:51 -0700

    Nav: Add 'onRenderLink' back to rendering (#3199)
    * Add onRenderLink to JSX
    
    * Add change file
    
    * Update Nav snapshot
    
    * Implement IRenderFunction in onRenderLink
    
    * Fix demo
    
    * Fix css overflow stlyes
    
    * Update styles and snapshot

--- packages/office-ui-fabric-react/src/components/Nav/examples/Nav.FabricDemoApp.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Nav/examples/Nav.FabricDemoApp.Example.tsx
@@ -13,13 +13,15 @@ export class NavFabricDemoAppExample extends React.Component<any, any> {
     );
   }
 
-  private _onRenderLink(link: any): (JSX.Element | null)[] {
-    return ([
-      <span key={ 1 } className='Nav-linkText'>{ link.name }</span>,
-      (link.status !== undefined ?
-        <span key={ 2 } className={ 'Nav-linkFlair ' + 'is-state' + link.status } >{ ExampleStatus[link.status] }</span> :
-        null)
-    ]);
+  private _onRenderLink(link: any): (JSX.Element | null) {
+    return (
+      <span>
+        <span key={ 1 } className='Nav-linkText'>{ link.name }</span>
+        { link.status !== undefined ?
+          <span key={ 2 } className={ 'Nav-linkFlair ' + 'is-state' + link.status } >{ ExampleStatus[link.status] }</span> :
+          null }
+      </span>
+    );
   }
 
 }
