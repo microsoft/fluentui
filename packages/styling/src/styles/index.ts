9d49e3008e26d684a7c259f9c0de5df95f4de492
Micah Godbolt <micahgodbolt@gmail.com>
2017-10-26 09:38:10 -0700

    Panel navigation layout (#3231)
    * changed navigation layout to use flexbox, and pass overrides directly into button to avoid load order issues
    
    * Change files
    
    * Change to theme.palette
    
    * use correct prop for hover

--- packages/styling/src/styles/index.ts
+++ packages/styling/src/styles/index.ts
@@ -1,6 +1,6 @@
 export { AnimationStyles } from './AnimationStyles';
 export { DefaultPalette } from './DefaultPalette';
-export { DefaultFontStyles, FontSizes, FontWeights } from './DefaultFontStyles';
+export { DefaultFontStyles, FontSizes, FontWeights, IconFontSizes } from './DefaultFontStyles';
 export { getFocusStyle } from './getFocusStyle';
 export { hiddenContentStyle } from './hiddenContentStyle';
 export {
