641c7dc2e492c599cc722a8dd75b7d258f9d267d
John Miller <miljo@microsoft.com>
2017-10-31 02:31:05 -0700

    Adding layer to Coachmark

--- packages/experiments/src/components/Coachmark/Coachmark.tsx
+++ packages/experiments/src/components/Coachmark/Coachmark.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { BaseComponent } from '../../Utilities';
 import { ICoachmarkProps } from './Coachmark.Props';
+import { Layer } from 'office-ui-fabric-react/lib/Layer';
 
-export class Coachmark extends BaseComponent < ICoachmarkProps, {} > {
+export class Coachmark extends BaseComponent<ICoachmarkProps, {}> {
   public render() {
-    return(
-      <div>Hello World!</div>
+    return (
+      <Layer>
+        <div>Hello world</div>
+      </Layer>
     );
   }
 };
