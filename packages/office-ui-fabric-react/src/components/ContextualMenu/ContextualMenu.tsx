e8782f700b18b1efdc98b54d1aa7040796fa9358
Montse Lozano Dieck <malozano@microsoft.com>
2017-11-01 10:31:53 -0700

    Remove unused style imports and simplifiy getClassnames assignment

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -3,10 +3,6 @@ import { IContextualMenuProps, IContextualMenuItem, ContextualMenuItemType } fro
 import { DirectionalHint } from '../../common/DirectionalHint';
 import { FocusZone, FocusZoneDirection } from '../../FocusZone';
 import {
-  getStyles,
-  getMenuItemStyles
-} from './ContextualMenu.styles';
-import {
   IMenuItemClassNames,
   IContextualMenuClassNames,
   getContextualMenuClassNames,
@@ -214,7 +210,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
       onRenderSubMenu = this._onRenderSubMenu
     } = this.props;
 
-    let menuClassNames = this.props.getMenuClassNames ? this.props.getMenuClassNames : getContextualMenuClassNames;
+    let menuClassNames = this.props.getMenuClassNames || getContextualMenuClassNames;
     this._classNames = menuClassNames(theme!, className);
 
     let hasIcons = itemsHaveIcons(items);
@@ -320,7 +316,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
     // We only send a dividerClassName when the item to be rendered is a divider. For all other cases, the default divider style is used.
     let dividerClassName = item.itemType === ContextualMenuItemType.Divider ? item.className : undefined;
     let subMenuIconClassName = item.submenuIconProps ? item.submenuIconProps.className : '';
-    let getClassNames = item.getItemClassNames ? item.getItemClassNames : getItemClassNames;
+    let getClassNames = item.getItemClassNames || getItemClassNames;
     let itemClassNames = getClassNames(
       this.props.theme!,
       !!item.disabled,
@@ -581,7 +577,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
   }
 
   private _renderSplitDivider(item: IContextualMenuItem) {
-    let getDividerClassnames = item.getVerticalDividerClassNames ? item.getVerticalDividerClassNames : getVerticalDividerClassNames;
+    let getDividerClassnames = item.getVerticalDividerClassNames || getVerticalDividerClassNames;
     return <VerticalDivider getClassNames={ getDividerClassnames } />;
   }
 
