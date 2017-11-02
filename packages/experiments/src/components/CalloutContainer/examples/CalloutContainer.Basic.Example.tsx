eb7d6e3633361932e2f9a75a13790d429a1d958d
John Miller <miljo@microsoft.com>
2017-11-02 15:59:47 -0700

    Adding the callout basic example

--- packages/experiments/src/components/CalloutContainer/examples/CalloutContainer.Basic.Example.tsx
+++ packages/experiments/src/components/CalloutContainer/examples/CalloutContainer.Basic.Example.tsx
@@ -1,10 +1,56 @@
 import * as React from 'react';
 import { CalloutContainer } from '../CalloutContainer';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
-export class CalloutContainerBasicExample extends React.Component<{}, {}> {
+export interface ICalloutContainerBasicExampleState {
+  isContextualSurfaceVisible?: boolean;
+}
+
+export class CalloutContainerBasicExample extends React.Component<{}, ICalloutContainerBasicExampleState> {
+  private _menuButtonElement: HTMLElement | null;
+  public constructor() {
+    super();
+
+    this._onShowMenuClicked = this._onShowMenuClicked.bind(this);
+    this._onCalloutDismiss = this._onCalloutDismiss.bind(this);
+
+    this.state = {
+      isContextualSurfaceVisible: false
+    }
+  }
   public render(): JSX.Element {
+    let { isContextualSurfaceVisible } = this.state;
     return (
-      <CalloutContainer />
+      <div>
+        <div className='ms-CalloutContainerBasicExample-buttonArea' ref={ (menuButton) => this._menuButtonElement = menuButton }>
+          <DefaultButton
+            onClick={ this._onShowMenuClicked }
+            text={ isContextualSurfaceVisible ? 'Hide callout' : 'Show callout' }
+          />
+        </div>
+        { isContextualSurfaceVisible && (
+          <CalloutContainer
+            className='ms-CalloutContainer'
+            role={ 'alertdialog' }
+            target={ this._menuButtonElement }
+            onDismiss={ this._onCalloutDismiss }
+            setInitialFocus={ true }
+          >
+            Ayyy test content
+          </CalloutContainer>
+        ) }
+      </div>
     );
+  }
+  private _onShowMenuClicked() {
+    this.setState({
+      isContextualSurfaceVisible: !this.state.isContextualSurfaceVisible
+    });
+  }
+
+  private _onCalloutDismiss() {
+    this.setState({
+      isContextualSurfaceVisible: false
+    });
   }
 }
