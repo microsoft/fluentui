792a6a1d21697816e6f46b835310c79d14b6f334
John Miller <miljo@microsoft.com>
2017-11-01 20:37:02 -0700

    Added classNames to classNames file

--- packages/experiments/src/components/CalloutContainer/CalloutContainer.classNames.ts
+++ packages/experiments/src/components/CalloutContainer/CalloutContainer.classNames.ts
@@ -1,10 +1,11 @@
 import { memoizeFunction } from '../../Utilities';
-import { mergeStyleSets} from '../../Styling';
+import { mergeStyleSets } from '../../Styling';
+import { IStyle } from '../../Styling';
 
 export interface ICalloutContainerStyles {
-   /**
-   * Style for the root element in the default enabled/unchecked state.
-   */
+  /**
+  * Style for the root element in the default enabled/unchecked state.
+  */
   root?: IStyle;
 }
 
@@ -12,11 +13,52 @@ export interface ICalloutContainerNames {
 	/**
 	* Root html container for this component.
 	*/
-	root?: string;
+  root: string;
+  container: string;
+  main: string;
+  overFlowYHidden: string;
+  beak: string;
+  beakCurtain: string;
+}
+
+export function highContrastActive(styles: IStyle): any {
+  return {
+    '@media screen and (-ms-high-contrast: active)': styles
+  };
+}
+
+export function focusClear() {
+  return {
+    '&::-moz-focus-inner': {
+      border: 0
+    },
+    '&': {
+      outline: 'transparent'
+    }
+  }
 }
 
 export const getClassNames = memoizeFunction((): ICalloutContainerNames => {
-	return mergeStyleSets({
-		root: []
-	});
+  return mergeStyleSets({
+    root: {
+      position: 'absolute',
+      boxSizing: 'border-box',
+      border: '1px solid ${}',
+      selectors: Object.assign(highContrastActive({
+        border: '1px solid WindowText',
+      }), focusClear()),
+    },
+    container: {
+      position: 'relative'
+    },
+    main: {
+      backgroundColor: '#ffffff',
+      overflowX: 'hidden',
+      overflowY: 'hidden',
+      position: 'relative'
+    },
+    overFlowYHidden: {
+      overflowY: 'hidden'
+    }
+  });
 });
