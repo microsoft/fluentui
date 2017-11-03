5325d5da0541087da2cf07265ea8631122f0b749
chaofeng2014 <fengchao199011@hotmail.com>
2017-11-03 11:31:52 -0700

    add directionalHintFixed as a prop in hover card (#3294)
    * add directionalHintFixed as a prop in hover card
    
    * remove space
    
    * put default value in right place
    
    * Add comment in expanding card props
    
    * rebuild

--- packages/office-ui-fabric-react/src/components/HoverCard/ExpandingCard.tsx
+++ packages/office-ui-fabric-react/src/components/HoverCard/ExpandingCard.tsx
@@ -26,6 +26,7 @@ export class ExpandingCard extends BaseComponent<IExpandingCardProps, IExpanding
     compactCardHeight: 156,
     expandedCardHeight: 384,
     directionalHint: DirectionalHint.bottomLeftEdge,
+    directionalHintFixed: true,
     gapSpace: 0
   };
 
@@ -68,6 +69,7 @@ export class ExpandingCard extends BaseComponent<IExpandingCardProps, IExpanding
       theme,
       styles: customStyles,
       compactCardHeight,
+      directionalHintFixed,
       expandedCardHeight
     } = this.props;
     this._styles = getStyles(theme!, customStyles);
@@ -96,7 +98,7 @@ export class ExpandingCard extends BaseComponent<IExpandingCardProps, IExpanding
         target={ targetElement }
         isBeakVisible={ false }
         directionalHint={ this.props.directionalHint }
-        directionalHintFixed={ true }
+        directionalHintFixed={ directionalHintFixed }
         finalHeight={ compactCardHeight! + expandedCardHeight! }
         minPagePadding={ 24 }
         gapSpace={ this.props.gapSpace }
