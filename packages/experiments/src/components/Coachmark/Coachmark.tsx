538fc74254603bd5aa46425b300238fe9458ce1f
John Miller <miljo@microsoft.com>
2017-11-07 07:43:30 -0800

    Adding main containers for Coachmark

--- packages/experiments/src/components/Coachmark/Coachmark.tsx
+++ packages/experiments/src/components/Coachmark/Coachmark.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import { BaseComponent } from '../../Utilities';
 import { ICoachmarkProps } from './Coachmark.Props';
-import { Layer } from 'office-ui-fabric-react/lib/Layer';
+import { DynamicallyPositionedContainer } from '../DynamicallyPositionedContainer/DynamicallyPositionedContainer';
+
+export interface ICoachmarkState {
+  isInflated: boolean;
+  isExpanded: boolean;
+
+}
 
 export class Coachmark extends BaseComponent<ICoachmarkProps, {}> {
   public render() {
+    let {
+      children
+    } = this.props;
+
     return (
-      <Layer>
-        <div>Hello world</div>
-      </Layer>
+      <DynamicallyPositionedContainer>
+        <div
+          className={ 'ms-Coachmark' }
+          ref={ this._resolveRef('_hostElement') }
+        >
+          <div className='ms-Coachmark-beacon'></div>
+          <div className="ms-Coachmark-translateAnimationLayer">
+            <div className="ms-Coachmark-scaleAnimationLayer">
+              <div className="ms-Coachmark-rotateAnimationLayer">
+                <div className="ms-Coachmark-content">
+                  { children }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div >
+      </DynamicallyPositionedContainer >
     );
   }
 };
