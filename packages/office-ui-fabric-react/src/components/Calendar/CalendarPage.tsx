328b1af1d852bb04d7393b3e701b0cbd9ae2f3ae
Evan Levy <evlevy@microsoft.com>
2017-11-07 13:13:21 -0500

    Add Calendar with date boundary example (#3306)
    * Add Calendar with date boundary example
    
    * Update CalendarPage.tsx

--- packages/office-ui-fabric-react/src/components/Calendar/CalendarPage.tsx
+++ packages/office-ui-fabric-react/src/components/Calendar/CalendarPage.tsx
@@ -10,12 +10,14 @@ import { CalendarButtonExample } from './examples/Calendar.Button.Example';
 import { CalendarInlineExample } from './examples/Calendar.Inline.Example';
 import { ComponentStatus } from '../../demo/ComponentStatus/ComponentStatus';
 import { CalendarStatus } from './Calendar.checklist';
+import { addMonths, addYears } from '../../utilities/dateMath/DateMath';
 
 const CalendarButtonExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/Calendar/examples/Calendar.Button.Example.tsx') as string;
 const CalendarInlineExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/Calendar/examples/Calendar.Inline.Example.tsx') as string;
 
 export class CalendarPage extends React.Component<IComponentDemoPageProps, any> {
   public render() {
+    const today = new Date(Date.now());
     return (
       <ComponentPage
         title={ 'Calendar' }
@@ -92,6 +94,20 @@ export class CalendarPage extends React.Component<IComponentDemoPageProps, any> 
                 highlightCurrentMonth={ true }
                 isDayPickerVisible={ false }
               />
+            </ExampleCard>
+            <ExampleCard
+              title='Inline Calendar with date boundary (minDate, maxDate)'
+              code={ CalendarInlineExampleCode }
+            >
+              <CalendarInlineExample
+                dateRangeType={ DateRangeType.Day }
+                autoNavigateOnSelection={ true }
+                highlightCurrentMonth={ true }
+                showGoToToday={ false }
+                minDate={ addMonths(today, -1) }
+                maxDate={ addYears(today, 1) }
+              />
+
             </ExampleCard>
             <ExampleCard
               title='Calendar launched from a button'
