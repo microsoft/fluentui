329cff7ef857f4ee0893303e5c91b31f38daf8e8
John Miller <miljo@microsoft.com>
2017-11-07 07:47:15 -0800

    Updates to coachmark props.

--- packages/experiments/src/components/Coachmark/Coachmark.Props.ts
+++ packages/experiments/src/components/Coachmark/Coachmark.Props.ts
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Coachmark } from './Coachmark';
+import { ICoachmarkStyles, ICoachmarkStyleProps } from './Coachmark.Styles';
+import { IStyleFunction } from '../../Utilities';
 
 export interface ICoachmark {
 }
@@ -9,4 +11,14 @@ export interface ICoachmarkProps extends React.Props<Coachmark> {
   * All props for your component are to be defined here.
   */
   componentRef?: (component: ICoachmark) => void;
+
+  /**
+   * Called when the teaching bubble has been expanded.
+   */
+  onExpandComplete?: () => void;
+
+  /**
+   * Get styles method
+   */
+  getStyles?: IStyleFunction<ICoachmarkStyleProps, ICoachmarkStyles>;
 }
