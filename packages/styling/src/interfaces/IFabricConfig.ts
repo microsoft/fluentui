9b0c8b0a0e2bf5966bf902ad919354236daab4cf
David Zearing <dzearing@microsoft.com>
2017-11-08 17:23:04 -0800

    Adding alternative injection mode for rare conditions.

--- packages/styling/src/interfaces/IFabricConfig.ts
+++ packages/styling/src/interfaces/IFabricConfig.ts
@@ -1,6 +1,10 @@
+import {
+  IStyleSheetConfig
+} from '@uifabric/merge-styles/lib/index';
 /**
  * The interface of window.FabricConfig.
  */
 export interface IFabricConfig {
   fontBaseUrl?: string;
+  mergeStyles?: IStyleSheetConfig;
 }
