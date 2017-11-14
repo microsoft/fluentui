0dc9ba1046a9247f27999d5cadab4ae1bc2d5b91
John Miller <miljo@microsoft.com>
2017-11-14 14:07:47 -0800

    Adding proximity functionality

--- packages/experiments/src/components/Coachmark/Coachmark.tsx
+++ packages/experiments/src/components/Coachmark/Coachmark.tsx
@@ -7,15 +7,31 @@ import { getStyles } from './Coachmark.Styles';
 export interface ICoachmarkState {
   isCollapsed: boolean;
   isBeaconAnimating: boolean;
+  isMouseInProximity: boolean;
 }
 
 export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
+  private _entityElemenet: HTMLElement;
+  private _elementRect: ClientRect;
+
+  public static defaultProps = {
+    mouseProximityOffset: 100
+  };
+
   constructor(props: ICoachmarkProps) {
     super();
     this.state = {
       isCollapsed: true,
-      isBeaconAnimating: true
+      isBeaconAnimating: true,
+      isMouseInProximity: true
     };
+
+    // Remove the measurement from the initial paint
+    this._async.setTimeout(() => {
+      this._elementRect = this._entityElemenet.getBoundingClientRect();
+    }, 10);
+
+    this._isElementInProximity();
   }
 
   public render() {
@@ -40,7 +56,10 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
           <div className={ classNames.translateAnimationContainer }>
             <div className={ classNames.scaleAnimationLayer }>
               <div className={ classNames.rotateAnimationLayer }>
-                <div className={ classNames.entityHost }>
+                <div
+                  className={ classNames.entityHost }
+                  ref={ this._resolveRef('_') }
+                >
                   { children }
                 </div>
               </div>
@@ -49,5 +68,33 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
         </div >
       </DynamicallyPositionedContainer >
     );
+  }
+
+  private _isElementInProximity() {
+    let timeoutIds: number[] = [];
+
+    this._events.on(window, 'resize', () => {
+      timeoutIds.forEach((value) => {
+        clearInterval(value)
+      });
+
+      timeoutIds.push(this._async.setTimeout(() => {
+        this._elementRect = this._entityElemenet.getBoundingClientRect();
+      }, 100));
+    });
+
+    this._events.on(document, 'mousemove', (e: MouseEvent) => {
+      let mouseY = e.pageY;
+      let mouseX = e.pageX;
+
+      this.setState({
+        isMouseInProximity: this._isInsideElement(this._elementRect, mouseX, mouseY)
+      });
+    });
+  }
+
+  private _isInsideElement(elementRect: ClientRect, mouseX: number, mouseY: number): boolean {
+    return mouseX > (elementRect.left - this.props.mouseProximityOffset) && mouseX < ((elementRect.left + elementRect.width) + this.props.mouseProximityOffset) &&
+      mouseY > (elementRect.top - this.props.mouseProximityOffset) && mouseY < ((elementRect.top + elementRect.height) + this.props.mouseProximityOffset);
   }
 };
