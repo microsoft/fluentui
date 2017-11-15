2c7a3cba080f0753dd836879983a36012a52164c
David Zearing <dzearing@microsoft.com>
2017-11-14 17:05:01 -0800

    Styling updates.

--- packages/styling/src/interfaces/index.ts
+++ packages/styling/src/interfaces/index.ts
@@ -3,3 +3,5 @@ export { IFontStyles } from './IFontStyles';
 export { IPalette } from './IPalette';
 export { ISemanticColors } from './ISemanticColors';
 export { ITheme, IPartialTheme } from './ITheme';
+export { IClassNames } from './IClassNames';
+export { IStyleFunction } from './IStyleFunction';
