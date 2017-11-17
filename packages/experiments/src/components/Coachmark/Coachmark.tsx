1c3152c709a8b9cea62b10080597e4080b94e15b
John Miller <miljo@microsoft.com>
2017-11-17 00:26:24 -0800

    Cleaned up coachmark, removed dead properties, added several comments.

--- packages/experiments/src/components/Coachmark/Coachmark.tsx
+++ packages/experiments/src/components/Coachmark/Coachmark.tsx
@@ -1,49 +1,69 @@
+// Utilities
 import * as React from 'react';
-import { BaseComponent } from '../../Utilities';
-import { ICoachmarkProps } from './Coachmark.Props';
+import { autobind, BaseComponent } from 'office-ui-fabric-react/lib/Utilities';
+
+// Component Dependencies
 import { DynamicallyPositionedContainer } from '../DynamicallyPositionedContainer/DynamicallyPositionedContainer';
+
+// Coachmark
+import { ICoachmarkProps } from './Coachmark.Props';
 import { getStyles } from './Coachmark.Styles';
-import { autobind } from 'office-ui-fabric-react/lib/Utilities';
 
-export interface ICoachmarkState {
-  isCollapsed: boolean;
-  isBeaconAnimating: boolean;
-  isMeasuring: boolean;
-  entityInnerHostRect: IEntityRect;
-  isMouseInProximity: boolean;
-}
-
+/**
+ * An interface for the cached dimensions of entity inner host.
+ */
 export interface IEntityRect {
   width: number,
   height: number
 }
 
+export interface ICoachmarkState {
+  /**
+   * Is the Coachmark currently collapsed into
+   * a tear drop shape
+   */
+  isCollapsed: boolean;
+
+  /**
+   * Enables/Disables the beacon that radiates
+   * from the center of the coachmark.
+   */
+  isBeaconAnimating: boolean;
+
+  /**
+   * Is the teaching bubble currently retreiving the
+   * original dimensions of the hosted entity.
+   */
+  isMeasuring: boolean;
+
+  /**
+   * Cached width and height of _entityInnerHostElement
+   */
+  entityInnerHostRect: IEntityRect;
+
+  /**
+   * Is the mouse in proximity of the default target element
+   */
+  isMouseInProximity: boolean;
+}
+
 export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
 
-  private _elementRect: ClientRect;
-
+  /**
+   * The cached HTMLElement reference to the Entity Inner Host
+   * element.
+   */
   private _entityInnerHostElement: HTMLElement;
 
-  /**
-   * The height of _entityInnerHost before the
-   * teaching callout collapses.
-   */
-  private _originalEntityHostHeight: number;
-
-  /**
-   * The widht of _entityInnerHost before the
-   * teaching callout collapses
-   */
-  private _originalEntityHostWidth: number;
-
-
-  public static defaultProps = {
+  public static defaultProps: Partial<ICoachmarkProps> = {
     isCollapsed: true,
     mouseProximityOffset: 100
   };
 
   constructor(props: ICoachmarkProps) {
     super();
+
+    // Set defaults for state
     this.state = {
       isCollapsed: props.isCollapsed!,
       isBeaconAnimating: true,
@@ -57,10 +77,12 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
 
     // Remove the measurement from the initial paint
     this._async.setTimeout(() => {
-      this._elementRect = this._entityInnerHostElement.getBoundingClientRect();
+      this.setState({
+        entityInnerHostRect: this._entityInnerHostElement.getBoundingClientRect()
+      });
     }, 10);
 
-    this._isElementInProximity();
+    this._isElementInProximity(this._entityInnerHostElement);
   }
 
   public render() {
@@ -68,18 +90,18 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
       children
     } = this.props;
 
-    // Classnames
+    // Retrieve classnames
     const classNames = getStyles({
       isCollapsed: this.state.isCollapsed,
       isBeaconAnimating: this.state.isBeaconAnimating,
       isMeasuring: this.state.isMeasuring,
-      entityHostHeight: (this._originalEntityHostHeight) ? this._originalEntityHostHeight + 'px' : undefined,
-      entityHostWidth: (this._originalEntityHostWidth) ? this._originalEntityHostWidth + 'px' : undefined
+      entityHostHeight: (this.state.entityInnerHostRect.width) ? this.state.entityInnerHostRect.height + 'px' : undefined,
+      entityHostWidth: (this.state.entityInnerHostRect.width) ? this.state.entityInnerHostRect.height + 'px' : undefined
     });
 
     const entityHostStyles = {
-      height: this._originalEntityHostHeight,
-      width: this._originalEntityHostWidth
+      height: this.state.entityInnerHostRect.height,
+      width: this.state.entityInnerHostRect.width
     };
 
     return (
@@ -116,15 +138,12 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
     if (this.state.isMeasuring) {
       this._async.setTimeout(() => {
         if ((this.state.entityInnerHostRect.width + this.state.entityInnerHostRect.width) === 0) {
-          // Store the original dimensions
-          this._originalEntityHostHeight = this._entityInnerHostElement.offsetHeight;
-          this._originalEntityHostWidth = this._entityInnerHostElement.offsetWidth;
-
-
-          // Since measurements have been complete let's
-          // update the state.
           this.setState({
-            isMeasuring: false
+            isMeasuring: false,
+            entityInnerHostRect: {
+              width: this._entityInnerHostElement.offsetWidth,
+              height: this._entityInnerHostElement.offsetHeight
+            }
           });
         }
       }, 100);
@@ -145,31 +164,51 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
     }
   }
 
-  private _isElementInProximity() {
+  private _isElementInProximity(targetElement: HTMLElement, mouseProximityOffset: number = 0) {
+    /**
+     * An array of cached ids returned when setTimeout runs during
+     * the window resize event trigger.
+     */
     let timeoutIds: number[] = [];
 
+    /**
+     * The target element the mouse would be in
+     * proximity to
+     */
+    let targetElementRect: ClientRect;
+
+    // When the window resizes we want to async
+    // get the bounding client rectangle.
+    // Every time the event is triggered we wan't to
+    // setTimeout and then clear any previous instances
+    // of setTimeout.
     this._events.on(window, 'resize', () => {
       timeoutIds.forEach((value) => {
         clearInterval(value)
       });
 
       timeoutIds.push(this._async.setTimeout(() => {
-        this._elementRect = this._entityInnerHostElement.getBoundingClientRect();
+        targetElementRect = targetElement.getBoundingClientRect();
       }, 100));
     });
 
+    // Everytime the document's mouse move is triggered
+    // we want to check if inside of an element and
+    // set the state with the result.
     this._events.on(document, 'mousemove', (e: MouseEvent) => {
       let mouseY = e.pageY;
       let mouseX = e.pageX;
 
+      // We try to set state every time because we assume
+      //
       this.setState({
-        isMouseInProximity: this._isInsideElement(this._elementRect, mouseX, mouseY)
+        isMouseInProximity: this._isInsideElement(targetElementRect, mouseX, mouseY)
       });
     });
   }
 
-  private _isInsideElement(elementRect: ClientRect, mouseX: number, mouseY: number): boolean {
-    return mouseX > (elementRect.left - this.props.mouseProximityOffset) && mouseX < ((elementRect.left + elementRect.width) + this.props.mouseProximityOffset) &&
-      mouseY > (elementRect.top - this.props.mouseProximityOffset) && mouseY < ((elementRect.top + elementRect.height) + this.props.mouseProximityOffset);
+  private _isInsideElement(elementRect: ClientRect, mouseX: number, mouseY: number, mouseProximityOffset: number = 0): boolean {
+    return mouseX > (elementRect.left - mouseProximityOffset) && mouseX < ((elementRect.left + elementRect.width) + mouseProximityOffset) &&
+      mouseY > (elementRect.top - mouseProximityOffset) && mouseY < ((elementRect.top + elementRect.height) + mouseProximityOffset);
   }
 };
