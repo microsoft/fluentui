e1fa1e47750b952cac454933bdddafafb5f98c21
REDMOND\chiechan <chiechan@microsoft.com>
2017-11-16 19:09:51 -0800

    removed comment

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -640,7 +640,6 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
     let submenuCloseKey = getRTL() ? KeyCodes.right : KeyCodes.left;
 
     if (ev.which === KeyCodes.escape
-      // || ev.which === KeyCodes.tab
       || (ev.which === submenuCloseKey && this.props.isSubMenu && this.props.arrowDirection === FocusZoneDirection.vertical)) {
       // When a user presses escape, we will try to refocus the previous focused element.
       this._isFocusingPreviousElement = true;
