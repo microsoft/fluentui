2c399b1a3ddf10b8707e66ec53f3f1bbaf3d0a65
John Miller <rubixws@gmail.com>
2017-12-07 14:16:26 -0800

    Adding experimental components Coachmark and Positioning Container (#3445)
    * Adding Coachmark Files
    
    * Adding IStyle to coachmark
    
    * Removed i Style from props
    
    * Added nav entry to AppDefinition
    
    * Added Coachmark page and example
    
    * Adding layer to Coachmark
    
    * Adding Callout container bits
    
    * Removing sass file calloutContainer
    
    * Updating CalloutContainer with all the original positioning bits.
    
    * Added classNames to classNames file
    
    * Fixed import in CalloutContainer
    
    * Adding CalloutContainer (ContextualSurface) to the experiments demo site.
    
    * Removing beak styling
    
    * Adding the callout basic example
    
    * Updating target to be a css query selector.
    
    * Renaming CalloutContainer to ContextualSurface
    
    * Renaming folder to ContextualSurface
    
    * Renaming main export file
    
    * Adding main containers for Coachmark
    
    * Renaming ContextualSurface to DynamicallyPositionedContainer
    
    * Renaming classNames file
    
    * Updates to coachmark props.
    
    * Adding DynamicallyPostionedContainer files.
    
    * Updating name in appdefinition to DynamicallyPositionedContainer
    
    * Added @todo for calloutElementRect.
    
    * Adding states and setting
    
    * temporarily adding IStyleFunction
    
    * Adding style functions to Coachmark.Styles
    
    * Updating comment about maxHeight
    
    * Updated classes with mergeStyle classes.
    
    * Fixed the interface for mergeStyles.
    
    * Updated coachmark example with bits
    
    * Updating public css classnames for DynamicallyPositionedContainer
    
    * Adding target
    
    * Adding positioningTarget and positioningContainer props.
    
    * Updating example data
    
    * Added button to example
    
    * Updating example selection method.
    
    * Chaning class to entity host
    
    * Added entity inner host
    
    * Updated coachmark selection method.
    
    * Converted the element proximity binding from knockout.
    
    * More updates to Coachmark
    
    * Adding proximity functionality
    
    * Adding new mouseProximityOffset
    
    * Few more bug fixes
    
    * Making Proximity Offset optional
    
    * Cleaned up coachmark, removed dead properties, added several comments.
    
    * Making mouse proximity offset optional
    
    * Adding debugging in chrome for visual studio code.
    
    * Fixing a few rendering order issues with coachmark.
    
    * Fixing some styles for specific states.
    
    * Removing element proxmity
    
    * Adding new properties
    
    * Adding new references to HTML elements.
    
    * Fixed a couple items in Coachmark
    
    * Removing unused property
    
    * Rename Dynamically Positioned Container to PositioningContainer.
    
    * Adding new Beak component bare files.
    
    * Removing beak for now, adding to a different pull request.
    
    * Removing callout artifacts and general cleanup.
    
    * Updating the name of the props
    
    * Removing useless callback
    
    * Adding dos and don'ts for coachmark.
    
    * Adding beak control
    
    * Updating coachmark description
    
    * Renaming files
    
    * Renaming coachmark styles to styles
    
    * Fixing reference names
    
    * state and property name changes, more renaming of references.
    
    * Renaming beak styles file.
    
    * Adding styles to index export
    
    * Addressing a few PR nits
    
    * Remvoing references to callout
    
    * Removing useTargetPoint
    
    * Removing extra comma.
    
    * Removing beak
    
    * Adding the beak component back with the correct casing
    
    * Updating create component script to create the correct name for the mergeStyles file.
    
    * Adding the beak bits
    
    * Removing the beak for this pull request.
    
    * Removing beak top level import.
    
    * Updating styles to new method.
    
    * Fixing references to file-type-icons
    
    * Nits and fixing some build errors from other projects.

---
+++ packages/experiments/src/components/Coachmark/Coachmark.types.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Coachmark } from './Coachmark';
+import { ICoachmarkStyles, ICoachmarkStyleProps } from './Coachmark.styles';
+import { IPositioningContainerTypes } from '../PositioningContainer/PositioningContainer.types';
+import { IPoint, IStyleFunction } from '../../Utilities';
+
+export interface ICoachmark {
+}
+
+export interface ICoachmarkTypes extends React.Props<Coachmark> {
+  /**
+  * All props for your component are to be defined here.
+  */
+  componentRef?: (component: ICoachmark) => void;
+
+  /**
+   * Get styles method
+   */
+  getStyles?: IStyleFunction<ICoachmarkStyleProps, ICoachmarkStyles>;
+
+  /**
+   * The target that the TeachingBubble should try to position itself based on.
+   * It can be either an HTMLElement a querySelector string of a valid HTMLElement
+   * or a MouseEvent. If MouseEvent is given then the origin point of the event will be used.
+   */
+  target?: HTMLElement | string | MouseEvent | IPoint | null;
+
+  positioningContainerProps?: IPositioningContainerTypes;
+
+  /**
+   * Is the Coachmark expanded
+   * @default true
+   */
+  collapsed?: boolean;
+
+  /**
+   * The distance in pixels the mouse is located
+   * before opening up the coachmark.
+   * @default 100
+   */
+  mouseProximityOffset?: number;
+
+  /**
+   * Callback when the opening animation begins
+   */
+  onAnimationOpenStart?: () => void;
+
+  /**
+   * Callback when the opening animation completes
+   */
+  onAnimationOpenEnd?: () => void;
+}
