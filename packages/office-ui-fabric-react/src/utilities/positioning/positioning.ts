4c78d90653cceff5dbd5b26e7feecde455ce6a2d
Jon Schectman <joschect@microsoft.com>
2018-01-09 11:28:37 -0800

    Positioning/Callout: Fix positioning for HTMLImg elements when used as target (#3681)
    * fix a bug where the callout would position incorrectly if the html element being target was an img element
    
    * adding change file
    
    * adding in comment

--- packages/office-ui-fabric-react/src/utilities/positioning/positioning.ts
+++ packages/office-ui-fabric-react/src/utilities/positioning/positioning.ts
@@ -579,11 +579,12 @@ export module positioningFunctions {
       if ((target as MouseEvent).preventDefault) {
         let ev: MouseEvent = target as MouseEvent;
         targetRectangle = new Rectangle(ev.clientX, ev.clientX, ev.clientY, ev.clientY);
-      } else if ((target as IPoint).x !== undefined) {
+      } else if ((target as HTMLElement).getBoundingClientRect) {
+        targetRectangle = _getRectangleFromHTMLElement(target as HTMLElement);
+        // HTMLImgElements can have x and y values. The check for it being a point must go last.
+      } else {
         let point: IPoint = target as IPoint;
         targetRectangle = new Rectangle(point.x, point.x, point.y, point.y);
-      } else {
-        targetRectangle = _getRectangleFromHTMLElement(target as HTMLElement);
       }
 
       if (!_isRectangleWithinBounds(targetRectangle, bounds)) {
