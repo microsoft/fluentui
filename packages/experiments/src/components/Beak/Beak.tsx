707c57a7b712ed8bad3a8cd90c35d3a54110648f
John Miller <rubixws@gmail.com>
2018-01-18 17:55:57 -0800

    Removing junk and fixing getClassNames function.

--- packages/experiments/src/components/Beak/Beak.tsx
+++ packages/experiments/src/components/Beak/Beak.tsx
@@ -6,85 +6,17 @@ import {
   classNamesFunction
 } from '../../Utilities';
 import { IBeakProps } from './Beak.types';
-import {
-  ICalloutPositionedInfo
-} from 'office-ui-fabric-react/lib/utilities/positioning';
 import { getStyles, IBeakStyles, IBeakStylesProps } from './Beak.styles';
 
-const getClassNames = classNamesFunction<IBeakStylesProps, IBeakStyles>();
-
-export interface IBeakState {
-  positions?: ICalloutPositionedInfo;
-}
-
-const BEAK_ORIGIN_POSITION = { top: 0, left: 0 };
+export interface IBeakState { }
 
 export class Beak extends BaseComponent<IBeakProps, IBeakState> {
-
-  public static defaultProps = {
-    isBeakVisible: true,
-    beakWidth: 16
-  }
-
-  constructor() {
-    super();
-    this.state = {
-      positions: undefined,
-    }
-  }
-
-  public render(): JSX.Element {
-    let {
-      beakWidth,
-      backgroundColor,
-      isBeakVisible,
-      target
-    } = this.props;
-
-    const classNames = getClassNames(getStyles, {});
-
-    let { positions } = this.state;
-
-    const beakReactStyle = this._getBeakPosition(positions, beakWidth, backgroundColor);
-    let beakVisible = isBeakVisible && (!!target);
-
+  public render() {
+    const getClassNames = classNamesFunction<IBeakStylesProps, IBeakStyles>();
+    const classNames = getClassNames(getStyles);
     return (
-      <div>
-        { beakVisible &&
-          (
-            <div
-              className={ css('ms-Beak', classNames.root) }
-              style={ beakReactStyle }
-            />
-          )
-        }
-        { beakVisible &&
-          (<div className={ css('ms-Beak-curtain', classNames.curtain) } />)
-        }
+      <div className={ css("ms-Beak", classNames.root) }>
       </div>
     );
-  }
-
-  private _getBeakPosition(
-    positions?: ICalloutPositionedInfo,
-    beakWidth?: number,
-    backgroundColor?: string
-  ) {
-    let beakStyleWidth = beakWidth;
-
-    let beakReactStyle: React.CSSProperties = {
-      ...(positions && positions.beakPosition ? positions.beakPosition.elementPosition : null),
-    };
-
-    beakReactStyle.height = beakStyleWidth;
-    beakReactStyle.width = beakStyleWidth;
-    beakReactStyle.backgroundColor = backgroundColor;
-
-    if (!beakReactStyle.top && !beakReactStyle.bottom && !beakReactStyle.left && !beakReactStyle.right) {
-      beakReactStyle.left = BEAK_ORIGIN_POSITION.left;
-      beakReactStyle.top = BEAK_ORIGIN_POSITION.top;
-    }
-
-    return beakReactStyle;
   }
 };
