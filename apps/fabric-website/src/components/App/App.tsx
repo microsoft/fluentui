aece21dc36a35ce9df1c755f77e98eb1a983c243
lynamemi <lynam.emily@gmail.com>
2018-01-19 23:18:16 -0800

    Website: Left navigation scroll implementation (#3749)
    * Left nav scroll works, but not with uhf
    
    * Attached left nav and resizing on scroll
    
    * _handleNavPositioning for scroll and resize both
    
    * Wrapped Nav in FocusZone for arrow key nav instead of tab
    
    * Focus Zone for page header
    
    * Removed isNavOpen concept and other clean up
    
    * More isNavOpen cleanup
    
    * Revert "More isNavOpen cleanup"
    
    This reverts commit d8ac7dea5ab95f1c595bf4456e2d82db95af64ff.
    
    * Revert "Removed isNavOpen concept and other clean up"
    
    This reverts commit ed823f6290cbdf4ee5519e352754de0340eb2ebe.
    
    * Removed isNavOpen state, no longer needed since mobile nav is gone
    
    * Nav position fixed and float left, more comments, adjusted scroll thresh
    
    * AttachScroll refactor, some app content refactor
    
    * App content refactor for header
    
    * Removed height variable, using state
    
    * Fixes jittery scroll in edge, attached header in ie, and small refactors
    
    * Max-width to make IE happy
    
    * IE top:unset fix
    
    * Removed console.log
    
    * Added change file
    
    * Adding width back in
    
    * Fixed test env edge case
    
    * Removed unused variables

--- apps/fabric-website/src/components/App/App.tsx
+++ apps/fabric-website/src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.scss';
 import { AppState } from './AppState';
-import { css } from 'office-ui-fabric-react/lib/Utilities';
+import { autobind } from 'office-ui-fabric-react/lib/Utilities';
+import AttachedScrollUtility from '../../utilities/AttachedScrollUtility';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { Nav } from '../Nav/Nav';
 
@@ -9,37 +10,66 @@ export interface IAppProps extends React.Props<App> {
 }
 
 export interface IAppState {
-  isNavOpen: boolean;
+  isAttached: boolean;
+  navHeight: number;
 }
 
 export class App extends React.Component<IAppProps, any> {
+
+  private _attachedScrollThreshold: number;
+  private _appContent: HTMLDivElement;
+  private _appContentRect: ClientRect;
 
   constructor(props: IAppProps) {
     super(props);
 
     this.state = {
-      isNavOpen: false
+      isAttached: false
     };
   }
 
+  public componentDidMount() {
+    window.addEventListener('scroll', this._handleNavPositioning);
+    window.addEventListener('resize', this._handleNavPositioning);
+
+    this._attachedScrollThreshold = AttachedScrollUtility.calculateAttachedScrollThreshold();
+    this._handleNavPositioning();
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener('scroll', this._handleNavPositioning);
+    window.removeEventListener('resize', this._handleNavPositioning);
+  }
+
   public render() {
-    let { isNavOpen } = this.state;
+    let { navHeight } = this.state;
+    let appContentTop = this._appContentRect ? this._appContentRect.top : 100;
+    // Using appContentTop as a reference to match instead of 'unset' because it does not work in IE.
+    let navTop: string = this.state.isAttached ? '0' : appContentTop.toString();
+    let navStyle = {
+      top: navTop,
+      height: navHeight
+    };
 
     return (
       <Fabric
-        className={ css(
-          'App',
-          isNavOpen && 'is-navOpen'
-        ) }
+        className='App'
       >
         <div className='App-wrapper'>
-          <div className='App-nav'>
+          <div
+            className='App-nav'
+            style={ navStyle }
+          >
             <Nav
               pages={ AppState.pages }
-              onLinkClick={ this._onNavItemClicked.bind(this) }
             />
           </div>
-          <div className='App-content' data-is-scrollable='true'>
+          <div
+            className='App-content'
+            data-is-scrollable='true'
+            ref={ (el) => this._appContent = el }
+            data-app-content-div='true'
+          >
             { this.props.children }
           </div>
         </div>
@@ -47,9 +77,41 @@ export class App extends React.Component<IAppProps, any> {
     );
   }
 
-  private _onNavItemClicked(ev: MouseEvent) {
+  @autobind
+  private _handleNavPositioning() {
+    let { isAttached, navHeight } = this.state;
+    this._appContentRect = this._appContent.getBoundingClientRect();
+    const viewPortHeight = window.innerHeight;
+    isAttached = AttachedScrollUtility.shouldComponentAttach(isAttached, this._attachedScrollThreshold);
+    navHeight = this._calculateNavHeight(viewPortHeight, this._appContentRect, navHeight);
     this.setState({
-      isNavOpen: false
+      isAttached: isAttached,
+      navHeight: navHeight
     });
+  }
+
+  private _calculateNavHeight(viewPortHeight: number, appContentRect: ClientRect, height: number): number {
+    if (!appContentRect) {
+      return height;
+    }
+    if (appContentRect.top >= 0) {
+      // This case accounts for space taken up by the UHF header in the viewport.
+      height = viewPortHeight - appContentRect.top;
+      if (appContentRect.height < appContentRect.bottom && viewPortHeight > appContentRect.bottom) {
+        // This case might only exist in the UHF testing environment, when content isn't rendering properly and its height is weird.
+        height = appContentRect.height;
+      }
+    } else if (viewPortHeight < appContentRect.bottom && appContentRect.top < 0) {
+      // For pages with content that's longer than the viewport, the viewport is the height.
+      // Takes effect when you scroll past the header.
+      height = viewPortHeight;
+    } else if (appContentRect.bottom < 0) {
+      // In smaller screens when you scroll till the footer takes the whole page, collapse the nav.
+      height = 0;
+    } else {
+      // Once the footer is in view, match nav bottom to content bottom.
+      height = appContentRect.bottom;
+    }
+    return height;
   }
 }
