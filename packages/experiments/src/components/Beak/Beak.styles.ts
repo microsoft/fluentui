c6d1edd8fe627b291dc015d21aa2d4db3a463999
John Miller <miljo@microsoft.com>
2018-01-30 15:04:08 -0800

    Adding canvas

--- packages/experiments/src/components/Beak/Beak.styles.ts
+++ packages/experiments/src/components/Beak/Beak.styles.ts
@@ -7,6 +7,7 @@ export interface IBeakStyles {
    * Style for the root element in the default enabled/unchecked state.
    */
   root?: IStyle;
+  canvas?: IStyle;
 }
 
 export function getStyles(props: IBeakStylesProps): IBeakStyles {
@@ -17,6 +18,9 @@ export function getStyles(props: IBeakStylesProps): IBeakStyles {
       position: "relative",
       overflow: "hidden",
       boxShadow: "0 16px 10px -17px rgba(0, 0, 0, 0.5)"
+    },
+    canvas: {
+      transform: "rotate(90deg)"
     }
   }
 };
--- packages/experiments/src/components/Beak/Beak.tsx
+++ packages/experiments/src/components/Beak/Beak.tsx
@@ -10,11 +10,30 @@ import { getStyles, IBeakStyles, IBeakStylesProps } from './Beak.styles';
 export interface IBeakState { }
 
 export class Beak extends BaseComponent<IBeakProps, IBeakState> {
+  private _canvasRef: HTMLCanvasElement;
+
+  private _createTriangle() {
+    if (this._canvasRef.getContext) {
+      var ctx = this._canvasRef.getContext('2d')!;
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(50, 50);
+      ctx.lineTo(100, 0);
+      ctx.fill();
+    }
+  }
+
+  public componentDidMount() {
+    this._createTriangle();
+  }
+
   public render() {
     const getClassNames = classNamesFunction<IBeakStylesProps, IBeakStyles>();
     const classNames = getClassNames(getStyles);
     return (
-      <div className={ css("ms-Beak", classNames.root) }></div>
+      <div className={ css("ms-Beak", classNames.root) }>
+        <canvas className={ classNames.canvas } ref={ this._resolveRef('_canvasRef') }></canvas>
+      </div>
     );
   }
 };
