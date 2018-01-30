e6136d1726d59c4766b58af9dd0bd40a3936952b
jspurlin <jspurlin@microsoft.com>
2018-01-30 09:59:39 -0800

    ComboBox: Fix auto expand for allowFreeform (#3817)
    * Update the way the comboBox expands to not autoexpand for allowfreeform (but it should stay expanded if it is already open)
    
    * rush change

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
@@ -1518,7 +1518,7 @@ export class ComboBox extends BaseComponent<IComboBoxProps, IComboBoxState> {
   @autobind
   private _onBaseAutofillClick() {
     if (this.props.allowFreeform) {
-      this.focus(true);
+      this.focus(this.state.isOpen);
     } else {
       this._onComboBoxClick();
     }
