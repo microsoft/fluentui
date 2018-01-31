07546b0bcfdd8ddca30d4c74ccb7b66a54ec1b2a
Vedika Loiya <veloiya@microsoft.com>
2018-01-31 16:24:13 +0530

    Resolving PR comments

--- packages/office-ui-fabric-react/src/components/pickers/PickerItem.types.ts
+++ packages/office-ui-fabric-react/src/components/pickers/PickerItem.types.ts
@@ -12,5 +12,4 @@ export interface IPickerItemProps<T> extends React.AllHTMLAttributes<HTMLElement
   onItemChange?: (item: T, index: number) => void;
   key?: string | number;
   removeButtonAriaLabel?: string;
-  onClick?: (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => void;
 }
--- packages/office-ui-fabric-react/src/components/pickers/TagPicker/TagItem.tsx
+++ packages/office-ui-fabric-react/src/components/pickers/TagPicker/TagItem.tsx
@@ -1,7 +1,7 @@
 /* tslint:disable */
 import * as React from 'react';
 /* tslint:enable */
-import { css, IDictionary, KeyCodes } from '../../../Utilities';
+import { css, IDictionary } from '../../../Utilities';
 import { Icon } from '../../../Icon';
 import { IPickerItemProps } from '../PickerItem.types';
 import { ITag } from './TagPicker';
@@ -9,18 +9,6 @@ import * as stylesImport from './TagItem.scss';
 const styles: any = stylesImport;
 
 export const TagItem = (props: IPickerItemProps<ITag>) => {
-  const onClick = (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
-    if (props.onClick) {
-      props.onClick(event);
-    }
-  };
-
-  const onKeydown = (event: React.KeyboardEvent<HTMLElement>) => {
-    if (event.keyCode === KeyCodes.enter || event.keyCode === KeyCodes.space) {
-      onClick(event);
-    }
-  };
-
   return (
     <div
       className={ css('ms-TagItem',
@@ -30,11 +18,14 @@ export const TagItem = (props: IPickerItemProps<ITag>) => {
       key={ props.index }
       data-selection-index={ props.index }
       data-is-focusable={ !props.disabled && true }
-      onClick={ onClick }
-      onKeyDown={ onKeydown }
-      role={ 'button' }
     >
-      <span className={ css('ms-TagItem-text', styles.tagItemText) } aria-label={ props.children }>{ props.children }</span>
+      <button
+        className={ css(styles.tagItemButton) }
+        onClick={ props.onClick }
+        disabled={ props.onClick ? false : true }
+      >
+        <span className={ css('ms-TagItem-text', styles.tagItemText) } aria-label={ props.children }>{ props.children }</span>
+      </button>
       { !props.disabled &&
         <span className={ css('ms-TagItem-close', styles.tagItemClose) } onClick={ props.onRemoveItem }>
           <Icon iconName='Cancel' />
