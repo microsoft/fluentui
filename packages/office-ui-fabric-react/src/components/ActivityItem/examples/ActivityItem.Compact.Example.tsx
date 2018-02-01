d5e9296103ebc5160b3962d454bfc97779aa18ee
Brian Garland <brian@efelle.com>
2018-02-01 11:20:29 -0800

    ActivityItemExample mergeStyles Converstion (#3844)
    migrated ActivityItemExample to use mergestyles instead of scss

--- packages/office-ui-fabric-react/src/components/ActivityItem/examples/ActivityItem.Compact.Example.tsx
+++ packages/office-ui-fabric-react/src/components/ActivityItem/examples/ActivityItem.Compact.Example.tsx
@@ -1,73 +1,82 @@
 /* tslint:disable:no-unused-variable */
 import * as React from 'react';
 /* tslint:enable:no-unused-variable */
+import { css, classNamesFunction } from '../../../Utilities';
 import { ActivityItem } from '../ActivityItem';
+import {
+  getStyles,
+  IActivityItemExampleStyleProps,
+  IActivityItemExampleStyles
+} from './ActivityItem.Example.styles';
 import { TestImages } from '../../../common/TestImages';
 import { Icon } from '../../../Icon';
 
-const activityItemExamples = [
-  {
-    key: 1,
-    activityDescription: [
-      <span key={ 1 } className={ 'ms-activityItem-nameText' }>Tahlia	Whittle</span>,
-      <span key={ 2 }> edited this file</span>
-    ],
-    activityPersonas: [
-      {
-        imageUrl: TestImages.personaFemale
-      }
-    ],
-    isCompact: true
-  },
-  {
-    key: 2,
-    activityDescription: [
-      <span key={ 1 } className={ 'ms-activityItem-nameText' }>Patrick Loton</span>,
-      <span key={ 2 }> and </span>,
-      <span key={ 3 } className={ 'ms-activityItem-nameText' }> 6 others</span>
-    ],
-    activityPersonas: [
-      {
-        imageInitials: 'PT',
-        primaryText: 'Robert Larsson'
-      },
-      {
-        imageUrl: TestImages.personaMale
-      },
-      {
-        imageInitials: 'EC',
-        primaryText: 'Eduarda Costa'
-      }
-    ],
-    isCompact: true
-  },
-  {
-    key: 3,
-    activityDescription: [
-      <span key={ 1 } className={ 'ms-activityItem-nameText' }>Sabrina De Luca</span>,
-      <span key={ 2 }> added this file</span>
-    ],
-    activityIcon: React.createElement(Icon, { iconName: 'Add' }),
-    isCompact: true
-  },
-  {
-    key: 4,
-    activityDescription: [
-      <span key={ 1 } className={ 'ms-activityItem-nameText' }>Chuan Rojumanong</span>,
-      <span key={ 2 }> shared this file</span>
-    ],
-    activityIcon: React.createElement(Icon, { iconName: 'Share' }),
-    isCompact: true
-  }
-];
-
 export class ActivityItemCompactExample extends React.Component<React.Props<ActivityItemCompactExample>, {}> {
   public render() {
+    const getClassNames = classNamesFunction<IActivityItemExampleStyleProps, IActivityItemExampleStyles>();
+    const classNames = getClassNames(getStyles);
+
+    const activityItemExamples = [
+      {
+        key: 1,
+        activityDescription: [
+          <span key={ 1 } className={ css(classNames.nameText) }>Tahlia	Whittle</span>,
+          <span key={ 2 }> edited this file</span>
+        ],
+        activityPersonas: [
+          {
+            imageUrl: TestImages.personaFemale
+          }
+        ],
+        isCompact: true
+      },
+      {
+        key: 2,
+        activityDescription: [
+          <span key={ 1 } className={ css(classNames.nameText) }>Patrick Loton</span>,
+          <span key={ 2 }> and </span>,
+          <span key={ 3 } className={ css(classNames.nameText) }> 6 others</span>
+        ],
+        activityPersonas: [
+          {
+            imageInitials: 'PT',
+            primaryText: 'Robert Larsson'
+          },
+          {
+            imageUrl: TestImages.personaMale
+          },
+          {
+            imageInitials: 'EC',
+            primaryText: 'Eduarda Costa'
+          }
+        ],
+        isCompact: true
+      },
+      {
+        key: 3,
+        activityDescription: [
+          <span key={ 1 } className={ css(classNames.nameText) }>Sabrina De Luca</span>,
+          <span key={ 2 }> added this file</span>
+        ],
+        activityIcon: React.createElement(Icon, { iconName: 'Add' }),
+        isCompact: true
+      },
+      {
+        key: 4,
+        activityDescription: [
+          <span key={ 1 } className={ css(classNames.nameText) }>Chuan Rojumanong</span>,
+          <span key={ 2 }> shared this file</span>
+        ],
+        activityIcon: React.createElement(Icon, { iconName: 'Share' }),
+        isCompact: true
+      }
+    ];
+
     let activityExampleList: Array<JSX.Element> = [];
     activityItemExamples.forEach((item) => {
       let props = item;
       activityExampleList.push(
-        <ActivityItem {...props} key={ item.key } className={ 'ms-activityItem-exampleRoot' } />
+        <ActivityItem {...props} key={ item.key } className={ css(classNames.exampleRoot) } />
       );
     });
 
