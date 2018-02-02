e66eaff44252f34f59657d8fb570041157b4e570
lynamemi <lynam.emily@gmail.com>
2018-02-01 17:27:10 -0800

    Initial bounce test

--- packages/office-ui-fabric-react/src/components/Callout/CalloutContent.styles.ts
+++ packages/office-ui-fabric-react/src/components/Callout/CalloutContent.styles.ts
@@ -2,13 +2,14 @@ import {
   HighContrastSelector,
   AnimationStyles,
   IRawStyle,
-  focusClear
+  focusClear,
 } from '../../Styling';
 import {
   ICalloutPositionedInfo,
   RectangleEdge
 } from '../../utilities/positioning';
 import { ICalloutContentStyleProps, ICalloutContentStyles } from './Callout.types';
+import { keyframes } from '@uifabric/merge-styles/lib/index';
 
 const BEAK_ORIGIN_POSITION = { top: 0, left: 0 };
 
@@ -17,6 +18,58 @@ const ANIMATIONS: { [key: number]: IRawStyle; } = {
   [RectangleEdge.bottom]: AnimationStyles.slideDownIn20,
   [RectangleEdge.left]: AnimationStyles.slideLeftIn20,
   [RectangleEdge.right]: AnimationStyles.slideRightIn20,
+};
+
+// all of the first 2 non zero values are divided by 2
+const BOUNCE: string = keyframes({
+  '0%': { transform: 'matrix3d(.5, 0, 0, 0, 0, .5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '17.4%': { transform: 'matrix3d(.5, 0, 0, 0, 0, .5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '17.5%': { transform: 'matrix3d(.5, 0, 0, 0, 0, .5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '19.2%': { transform: 'matrix3d(.658, 0, 0, 0, 0, .704, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '19.85%': { transform: 'matrix3d(.725, 0, 0, 0, 0, .8, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '20.9%': { transform: 'matrix3d(.83, 0, 0, 0, 0, .947, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '22.2%': { transform: 'matrix3d(.942, 0, 0, 0, 0, 1.084, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '22.61%': { transform: 'matrix3d(.971, 0, 0, 0, 0, 1.113, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '24.31%': { transform: 'matrix3d(1.062, 0, 0, 0, 0, 1.161, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '24.56%': { transform: 'matrix3d(1.071, 0, 0, 0, 0, 1.17, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '26.26%': { transform: 'matrix3d(1.104, 0, 0, 0, 0, 1.1195, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '26.86%': { transform: 'matrix3d(1.106, 0, 0, 0, 0, 1.094, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '28.16%': { transform: 'matrix3d(1.098, 0, 0, 0, 0, 1.035, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '29.66%': { transform: 'matrix3d(1.08, 0, 0, 0, 0, .98, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '30.11%': { transform: 'matrix3d(1.067, 0, 0, 0, 0, .969, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '32.01%': { transform: 'matrix3d(1.032, 0, 0, 0, 0, .95, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '32.46%': { transform: 'matrix3d(1.024, 0, 0, 0, 0, 0.9495, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '35.27%': { transform: 'matrix3d(0.99, 0, 0, 0, 0, 0.981, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '35.87%': { transform: 'matrix3d(0.986, 0, 0, 0, 0, .99, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '38.02%': { transform: 'matrix3d(.981, 0, 0, 0, 0, 1.011, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '39.72%': { transform: 'matrix3d(0.983, 0, 0, 0, 0, 1.016, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '43.58%': { transform: 'matrix3d(.996, 0, 0, 0, 0, 1.003, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '47.43%': { transform: 'matrix3d(1.003, 0, 0, 0, 0, 0.995, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '49.13%': { transform: 'matrix3d(1.004, 0, 0, 0, 0, 0.996, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '49.9%': { transform: 'matrix3d(1.004, 0, 0, 0, 0, 0.997, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '50%': { transform: 'matrix3d(1.004, 0, 0, 0, 0, .998, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '52.15%': { transform: 'matrix3d(1.003, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '54.3%': { transform: 'matrix3d(1.001, 0, 0, 0, 0, 1.002, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '55.14%': { transform: 'matrix3d(1.001, 0, 0, 0, 0, 1.002, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '56.46%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1.002, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '58.61%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1.001, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '60.24%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '62.85%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '64.16%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '67.5%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '69.72%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '80.83%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '91.99%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' },
+  '100%': { transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' }
+});
+
+const bounceAnimation: IRawStyle = {
+  // animation: `${BOUNCE} 2000ms linear infinite both`,
+  animationName: `${BOUNCE}`,
+  animationDuration: '2000ms',
+  animationTimingFunction: 'linear',
+  // animationIterationCount: 'infinite',
+  animationFillMode: 'both'
 };
 
 function getBeakStylePosition(positions?: ICalloutPositionedInfo,
@@ -83,7 +136,8 @@ export const getStyles = (props: ICalloutContentStyleProps): ICalloutContentStyl
       },
       focusClear(),
       className,
-      positions && positions.targetEdge && ANIMATIONS[positions.targetEdge],
+      bounceAnimation,
+      // positions && positions.targetEdge && ANIMATIONS[positions.targetEdge],
       // Microsoft Edge will overwrite inline styles if there is an animation pertaining to that style.
       // To help ensure that edge will respect the offscreen style opacity
       // filter needs to be added as an additional way to set opacity.
