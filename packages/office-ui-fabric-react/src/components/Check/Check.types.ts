653e51696e8a8983146c6bb539ff144c4b7f682a
Jordan Janzen <v-jojanz@microsoft.com>
2018-02-02 13:47:12 -0800

    Add more props and types

--- packages/office-ui-fabric-react/src/components/Check/Check.types.ts
+++ packages/office-ui-fabric-react/src/components/Check/Check.types.ts
@@ -1,25 +1,46 @@
 import * as React from 'react';
-import { Check } from './Check';
+import { CheckBase } from './Check.base';
 import { IStyle, ITheme } from '@uifabric/Styling';
+import { IStyleFunction } from '@uifabric/utilities';
 
-export interface ICheckProps extends React.Props<Check> {
+export interface ICheckProps extends React.Props<CheckBase> {
   /**
    * Gets the component ref.
    */
-  componentRef?: () => void;
+  componentRef?: (component: ICheckProps) => void;
 
   /**
    * Whether or not this menu item is currently checked.
    * @defaultvalue false
    */
   checked?: boolean;
+
   /**
    * Deprecated at v0.65.1 and will be removed by v 1.0. Use 'checked' instead.
    * @deprecated
    */
   isChecked?: boolean;
 
+  /**
+  * Call to provide customized styling that will layer on top of the variant rules
+  */
+  getStyles?: IStyleFunction<ICheckStyleProps, ICheckStyles>;
+
+  /**
+   * Flag to always show the check icon. Not currently working.
+   */
   alwaysShowCheck?: boolean;
+
+  /**
+   * Theme provided by HOC.
+   */
+  theme?: ITheme;
+
+  /**
+   * Additional css class to apply to the Check
+   * @defaultvalue undefined
+   */
+  className?: string;
 }
 
 export interface ICheckStyleProps {
@@ -27,12 +48,20 @@ export interface ICheckStyleProps {
    * Accept theme prop. Defaults to getTheme() function.
    * @defaultvalue getTheme()
    */
-  theme?: ITheme;
+  theme: ITheme;
+
+  /**
+   * Accept custom classNames
+   */
+  className?: string;
+
   /**
    * Accept custom checkBox size in pixels.
    * @defaultvalue '18px'
    */
   checkBoxHeight?: string;
+
+  checked?: boolean;
 }
 
 export interface ICheckStyles {
