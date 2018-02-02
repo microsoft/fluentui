077aec67fe32480009d778bbc06f371203ea69ce
Jordan Janzen <v-jojanz@microsoft.com>
2018-02-02 13:46:53 -0800

    Convert Check to mergeStyle pattern

---
+++ packages/office-ui-fabric-react/src/components/Check/Check.base.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {
+  BaseComponent,
+  classNamesFunction,
+  css,
+  customizable,
+} from '@uifabric/utilities';
+import {
+  ICheckProps,
+  ICheckStyleProps,
+  ICheckStyles
+} from './Check.types';
+import { Icon } from '../../Icon';
+import { getStyles } from './Check.styles';
+
+// import * as stylesImport from './Check.scss';
+// const styles: any = stylesImport;
+const getClassNames = classNamesFunction<ICheckStyleProps, ICheckStyles>();
+
+@customizable('Check', ['theme'])
+export class CheckBase extends BaseComponent<ICheckProps, {}> {
+  public static defaultProps: ICheckProps = {
+    isChecked: false
+  };
+
+  public shouldComponentUpdate(newProps: ICheckProps) {
+    return this.props.isChecked !== newProps.isChecked || this.props.checked !== newProps.checked;
+  }
+
+  public render(): JSX.Element {
+    const {
+      checked,
+      className,
+      theme
+    } = this.props;
+
+    const classNames = getClassNames(getStyles!, { theme: theme!, className, checked });
+
+    return (
+      <div
+        className={ css(
+          'ms-Check',
+          className,
+          classNames.root,
+          checked && 'is-checked'
+        ) }
+      >
+        { Icon({
+          className: `
+            ms-Check-circle ${classNames.circle}
+          `,
+          iconName: 'CircleRing'
+        }) }
+        { Icon({
+          className: `
+            ms-Check-check ${classNames.check}
+          `,
+          iconName: 'StatusCircleCheckmark'
+        }) }
+      </div>
+    );
+  }
+}
