2da7c3163979f613352d98471a943fbc530fce19
John Miller <rubixws@gmail.com>
2018-02-08 05:32:24 -0800

    Refactoring Beak a bit.

--- packages/office-ui-fabric-react/src/components/Coachmark/Beak/Beak.tsx
+++ packages/office-ui-fabric-react/src/components/Coachmark/Beak/Beak.tsx
@@ -6,55 +6,58 @@ import {
 } from '../../../Utilities';
 import { IBeakProps } from './Beak.types';
 import { getStyles, IBeakStyles, IBeakStylesProps } from './Beak.styles';
+import { getComboBoxOptionClassNames } from 'src/components/ComboBox/ComboBox.classNames';
 
-export interface IBeakState { }
+export interface IBeakState {
+  left: string | null;
+  top: string | null;
+}
 
 export class Beak extends BaseComponent<IBeakProps, IBeakState> {
+  private _beakElement: HTMLElement;
+
   constructor(props: IBeakProps) {
     super(props);
   }
 
   public componentDidMount(): void {
-    // Determine where the target is
-    if (this.props.target && this.props.targetBeakContainer) {
-      let targetRect: ClientRect = this.props.target.getBoundingClientRect();
-      let containerRect: ClientRect = this.props.targetBeakContainer.getBoundingClientRect();
-
-      if (targetRect.right < containerRect.left) {
-        console.log("left aligned");
-        // Aligned left
-      } else if (targetRect.right > containerRect.left) {
-        // Aligned right
-        console.log("right aligned");
-      } else if (targetRect.top > containerRect.bottom) {
-        console.log("top aligned");
-        // Aligned top
-      } else if (targetRect.bottom < containerRect.top) {
-        console.log("bottom aligned");
-        // Aligned bottom
-      }
-    }
+    const beakComputedStyles = window.getComputedStyle(this._beakElement);
+    this.setState({
+      left: beakComputedStyles.left,
+      top: beakComputedStyles.top
+    });
   }
 
   public render(): JSX.Element {
-    const getClassNames = classNamesFunction<IBeakStylesProps, IBeakStyles>();
-    const classNames = getClassNames(getStyles);
     const {
-      beakHeight = 18,
-      beakWidth = 18
+      height = 18,
+      width = 18,
+      left,
+      top
     } = this.props;
 
-    const pointOne = beakWidth / 2 + "," + 0;
-    const pointTwo = beakWidth + "," + beakHeight;
-    const pointThree = 0 + "," + beakHeight;
+    const getClassNames = classNamesFunction<IBeakStylesProps, IBeakStyles>();
+    const classNames = getClassNames(getStyles, {
+      left: this.props.left,
+      top: this.props.top,
+      height: height + 'px',
+      width: width + 'px'
+    });
+
+    const pointOne = width / 2 + ',' + 0;
+    const pointTwo = width + ',' + height;
+    const pointThree = 0 + ',' + height;
 
     return (
-      <div className={ css('ms-Beak', classNames.root) }>
+      <div
+        className={ css('ms-Beak', classNames.root) }
+        ref={ this._resolveRef('_beakElement') }
+      >
         <svg
-          height={ beakHeight } width={ beakWidth }
+          height={ height } width={ width }
           className={ classNames.beak }
         >
-          <polygon points={ pointOne + " " + pointTwo + " " + pointThree } />
+          <polygon points={ pointOne + ' ' + pointTwo + ' ' + pointThree } />
         </svg>
       </div>
     );
