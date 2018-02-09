15da56bc94fcdc8b69fbde93a1bdbaeaffe66a18
Mike Wheaton <mike@mikewheaton.ca>
2018-02-09 11:05:13 -0800

    Define types for styled()

--- packages/office-ui-fabric-react/src/components/Link/Link.tsx
+++ packages/office-ui-fabric-react/src/components/Link/Link.tsx
@@ -1,9 +1,9 @@
 import { styled } from '../../Utilities';
 import { LinkBase } from './Link.base';
-import { ILinkProps } from './Link.types';
+import { ILinkProps, ILinkStyleProps, ILinkStyles } from './Link.types';
 import { getStyles } from './Link.styles';
 
-export const Link = styled(
+export const Link = styled<ILinkProps, ILinkStyleProps, ILinkStyles>(
   LinkBase,
   getStyles
 );
