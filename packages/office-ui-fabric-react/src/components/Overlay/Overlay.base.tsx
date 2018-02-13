1a437ddcbd166b6ca285a3043ebfb33256dcc607
Jordan Janzen <v-jojanz@microsoft.com>
2018-02-12 16:25:04 -0800

    Create base

---
+++ packages/office-ui-fabric-react/src/components/Overlay/Overlay.base.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+  BaseComponent,
+  css,
+  getNativeProps,
+  divProperties,
+  enableBodyScroll,
+  disableBodyScroll
+} from '../../Utilities';
+import { IOverlayProps } from './Overlay.types';
+
+import * as stylesImport from './Overlay.scss';
+const styles: any = stylesImport;
+
+export class Overlay extends BaseComponent<IOverlayProps, {}> {
+
+  public componentDidMount() {
+    disableBodyScroll();
+  }
+
+  public componentWillUnmount() {
+    enableBodyScroll();
+  }
+
+  public render() {
+    let { isDarkThemed, className } = this.props;
+    let divProps = getNativeProps(this.props, divProperties);
+
+    let modifiedClassName = css(
+      'ms-Overlay',
+      styles.root,
+      className,
+      {
+        ['ms-Overlay--dark ' + styles.rootIsDark]: isDarkThemed,
+      });
+
+    return (
+      <div { ...divProps } className={ modifiedClassName } />
+    );
+  }
+}
