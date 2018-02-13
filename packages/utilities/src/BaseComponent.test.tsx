4b5d842f4fb71e67979b6158f4fcec2083611776
David Zearing <dzearing@microsoft.com>
2018-02-12 19:24:52 -0800

    Updating test.

--- packages/utilities/src/BaseComponent.test.tsx
+++ packages/utilities/src/BaseComponent.test.tsx
@@ -5,59 +5,7 @@ import * as React from 'react';
 import * as ReactTestUtils from 'react-dom/test-utils';
 import { BaseComponent } from './BaseComponent';
 
-let _originalOnError = BaseComponent.onError;
-
-class TestComponent extends BaseComponent<{}, {}> {
-
-  public componentWillMount(): void {
-    this._createNullRef();
-  }
-
-  public componentDidMount(): void {
-    this._createNullRef();
-  }
-
-  public shouldComponentUpdate(nextProps?: {}, nextState?: {}): boolean {
-    this._createNullRef();
-
-    return true;
-  }
-
-  public componentWillUpdate(): void {
-    this._createNullRef();
-  }
-
-  public componentWillReceiveProps(): void {
-    this._createNullRef();
-  }
-
-  public render(): JSX.Element | null {
-    this._createNullRef();
-    return null;
-  }
-
-  public componentDidUpdate(): void {
-    this._createNullRef();
-  }
-
-  public componentWillUnmount(): void {
-    this._createNullRef();
-  }
-
-  private _createNullRef(): void {
-    // tslint:disable-next-line:no-any
-    let foo: any = null;
-
-    // Calling a null
-    foo();
-  }
-}
-
 describe('BaseComponent', () => {
-  afterEach(() => {
-    BaseComponent.onError = _originalOnError;
-  });
-
   it('can resolve refs', () => {
     class Foo extends BaseComponent<{}, {}> {
       public root: HTMLElement;
