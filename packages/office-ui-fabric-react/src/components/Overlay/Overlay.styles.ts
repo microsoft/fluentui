c194e123fde035b98997c1bdba625845d7033b18
Jordan Janzen <v-jojanz@microsoft.com>
2018-02-14 10:11:39 -0800

    Convert overlay sass to styles.ts

---
+++ packages/office-ui-fabric-react/src/components/Overlay/Overlay.styles.ts
@@ -0,0 +1,51 @@
+import { IOverlayStyleProps, IOverlayStyles } from './Overlay.types';
+import {
+  IStyle,
+  ITheme,
+  HighContrastSelector,
+} from '../../Styling';
+
+export const getStyles = (
+  props: IOverlayStyleProps
+): IOverlayStyles => {
+  const {
+    className,
+    theme,
+    isNone,
+    isDarkThemed,
+  } = props;
+
+  const { palette, semanticColors } = theme;
+
+  return ({
+    root: [
+      'ms-Overlay',
+      {
+        backgroundColor: palette.blackTranslucent40,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+
+        selectors: {
+          [HighContrastSelector]: {
+            border: '1px solid WindowText',
+          }
+        }
+      },
+
+      isNone && {
+        visibility: 'hidden',
+      },
+
+      isDarkThemed && {
+        backgroundColor: palette.blackTranslucent40,
+      },
+
+      className
+    ],
+
+    // Insert className styles
+  });
+};
