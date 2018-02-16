8304e76ee9a618e3638ca1902790deff54b4ef65
Mike Pateras <mipatera@microsoft.com>
2018-02-15 22:36:55 -0500

    Added comment and changed import paths.

--- packages/office-ui-fabric-react/src/utilities/groupedList/GroupedListUtility.test.tsx
+++ packages/office-ui-fabric-react/src/utilities/groupedList/GroupedListUtility.test.tsx
@@ -1,4 +1,4 @@
-import { IGroup } from '../../components/GroupedList';
+import { IGroup } from '../../components/GroupedList/GroupedList.types';
 import { GetGroupCount } from './GroupedListUtility';
 
 const G = (children: IGroup[] = []): IGroup => {
--- packages/office-ui-fabric-react/src/utilities/groupedList/GroupedListUtility.tsx
+++ packages/office-ui-fabric-react/src/utilities/groupedList/GroupedListUtility.tsx
@@ -1,5 +1,9 @@
-import { IGroup } from '../../components/GroupedList';
+import { IGroup } from '../../components/GroupedList/GroupedList.types';
 
+/**
+ * Takes an array of groups and returns a count of the groups and all descendant groups.
+ * @param groups - The array of groups to count.
+ */
 export const GetGroupCount = (groups: IGroup[] | undefined): number => {
   let total = 0;
 
