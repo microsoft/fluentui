e493f6606e8d30b06825c8c9ea7aac9ec6536958
REDMOND\chiechan <chiechan@microsoft.com>
2018-02-21 14:04:52 -0800

    added tabbable focus zone example

---
+++ packages/office-ui-fabric-react/src/components/FocusZone/examples/FocusZone.Tabbable.Example.tsx
@@ -0,0 +1,22 @@
+/* tslint:disable:no-unused-variable */
+import * as React from 'react';
+/* tslint:enable:no-unused-variable */
+
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { FocusZone, FocusZoneDirection } from 'office-ui-fabric-react/lib/FocusZone';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import './FocusZone.Tabbable.Example.scss';
+
+export const FocusZoneTabbableExample = () => (
+  <div className='ms-FocusZoneTabbableExample'>
+    <div className='ms-Row'>
+      <FocusZone direction={ FocusZoneDirection.horizontal } allowTabKey={ true } isCircularNavigation={ true }>
+        <span>Circular Tabbable FocusZone: </span>
+        <DefaultButton>Button 1</DefaultButton>
+        <DefaultButton>Button 2</DefaultButton>
+        <TextField type="text" value='FocusZone TextField' className='ms-FocusZoneTabbableExample-textField' />
+        <DefaultButton>Button 3</DefaultButton>
+      </FocusZone>
+    </div>
+  </div>
+);
