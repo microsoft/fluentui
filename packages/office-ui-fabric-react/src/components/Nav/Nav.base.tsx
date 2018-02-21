0a147b4826e2c3652fee32483a73cda7c359d2a6
David Zearing <dzearing@microsoft.com>
2018-02-21 15:04:10 -0800

    Update Nav.base.tsx

--- packages/office-ui-fabric-react/src/components/Nav/Nav.base.tsx
+++ packages/office-ui-fabric-react/src/components/Nav/Nav.base.tsx
@@ -185,8 +185,7 @@ export class NavBase extends BaseComponent<INavProps, INavState> implements INav
     const isLinkSelected: boolean = this._isLinkSelected(link);
     const isRtl: boolean = getRTL();
     const absolutePositionString = `${_indentationSize * nestingLevel + 1}px`;
-    const divProps: React.HTMLProps<HTMLDivElement> = { ...getNativeProps(link, divProperties) };
-    delete divProps.onClick;
+    const divProps: React.HTMLProps<HTMLDivElement> = { ...getNativeProps(link, divProperties, ['onClick']) };
 
     return (
       <div
