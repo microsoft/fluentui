d73484f95543366a5e774a5262adf5f8837f3951
Jordan Janzen <jordancjanzen@gmail.com>
2018-02-23 11:18:06 -0800

    Convert Check to mergeStyles (#3880)
    * Separate types to types.ts file.
    Start converting check to mergeStyles
    
    * Convert bulk scss to mergeStyles. Needs to be reviewed, this is rough.
    
    * Add comments for style types.
    
    * Convert Check to mergeStyle pattern
    
    * Add more props and types
    
    * Fix most style conversions.
    
    * Remove setUserSelect related lines. Rename rootIsChecked to just checked state.
    
    * Remove setUserSelect related lines. Rename rootIsChecked to just checked state.
    
    * Fix line-height
    
    * Export types in index.
    
    * npm run change output
    
    * Fix import for Travis
    
    * Move className logic to styles file.
    
    * nit, remove _ in _sharedCircleCheck
    
    * Combine checked conditionals into array.
    
    * Make ICheckStyles required, remove checkHost
    
    * Export base component
    
    * Remove unused import
    
    * Make icons jsx
    
    * Remove uneeded const

---
+++ packages/office-ui-fabric-react/src/components/Check/Check.styles.ts
@@ -0,0 +1,142 @@
+import { ICheckStyleProps, ICheckStyles } from './Check.types';
+import {
+  getTheme,
+  HighContrastSelector,
+  IStyle,
+  ITheme,
+} from '@uifabric/styling';
+import { memoizeFunction } from '@uifabric/utilities';
+
+export const getStyles = (
+  props: ICheckStyleProps
+): ICheckStyles => {
+  const {
+    checkBoxHeight = '18px',
+    checked,
+    className,
+    theme,
+  } = props;
+
+  const { palette, semanticColors } = theme;
+
+  const sharedCircleCheck: IStyle = {
+    fontSize: checkBoxHeight,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: checkBoxHeight,
+    height: checkBoxHeight,
+    textAlign: 'center',
+    verticalAlign: 'middle'
+  };
+
+  return ({
+    root: [
+      'ms-Check',
+
+      {
+        // lineHeight currently needs to be a string to output without 'px'
+        lineHeight: '1',
+        width: checkBoxHeight,
+        height: checkBoxHeight,
+        verticalAlign: 'top',
+        position: 'relative',
+        userSelect: 'none',
+
+        selectors: {
+          ':before': {
+            content: '""',
+            position: 'absolute',
+            top: '1px',
+            right: '1px',
+            bottom: '1px',
+            left: '1px',
+            borderRadius: '50%',
+            opacity: 1,
+            background: semanticColors.bodyBackground,
+          },
+
+          /**
+           * TODO: Come back to this once .checkHost has been
+           * converted to mergeStyles
+           */
+          '.checkHost:hover &, .checkHost:focus &, &:hover, &:focus': {
+            opacity: 1
+          }
+        }
+      },
+
+      checked && [
+        'is-checked',
+        {
+          selectors: {
+            ':before': {
+              background: palette.themePrimary,
+              opacity: 1,
+              selectors: {
+                [HighContrastSelector]: {
+                  background: 'Window'
+                }
+              }
+            }
+          }
+        },
+      ],
+      className
+    ],
+
+    circle: [
+      'ms-Check-circle',
+      sharedCircleCheck,
+
+      {
+        color: palette.neutralTertiaryAlt,
+
+        selectors: {
+          [HighContrastSelector]: {
+            color: 'WindowText'
+          }
+        }
+      },
+
+      checked && {
+        color: palette.white
+      }
+    ],
+
+    check: [
+      'ms-Check-check',
+      sharedCircleCheck,
+
+      {
+        opacity: 0,
+        color: palette.neutralTertiaryAlt,
+        fontSize: '16px',
+        left: '.5px',
+
+        selectors: {
+          ':hover': {
+            opacity: 1
+          },
+
+          [HighContrastSelector]: {
+            MsHighContrastAdjust: 'none'
+          }
+        }
+      },
+
+      checked && {
+        opacity: 1,
+        color: palette.white,
+        fontWeight: 900,
+
+        selectors: {
+          [HighContrastSelector]: {
+            border: 'none',
+            color: 'WindowText'
+          }
+        }
+      }
+    ]
+  });
+};
--- packages/office-ui-fabric-react/src/components/Check/Check.tsx
+++ packages/office-ui-fabric-react/src/components/Check/Check.tsx
@@ -1,63 +1,13 @@
-import * as React from 'react';
-import { BaseComponent, css } from '../../Utilities';
-import { Icon } from '../../Icon';
-import * as stylesImport from './Check.scss';
-const styles: any = stylesImport;
+import { styled } from '@uifabric/utilities';
+import {
+  ICheckProps,
+  ICheckStyleProps,
+  ICheckStyles
+} from './Check.types';
+import { CheckBase } from './Check.base';
+import { getStyles } from './Check.styles';
 
-export interface ICheckProps extends React.Props<Check> {
-  /**
-   * Gets the component ref.
-   */
-  componentRef?: () => void;
-
-  /**
-   * Whether or not this menu item is currently checked.
-   * @defaultvalue false
-   */
-  checked?: boolean;
-  /**
-   * Deprecated at v0.65.1 and will be removed by v 1.0. Use 'checked' instead.
-   * @deprecated
-   */
-  isChecked?: boolean;
-
-  alwaysShowCheck?: boolean;
-}
-
-export class Check extends BaseComponent<ICheckProps, {}> {
-  public static defaultProps = {
-    isChecked: false
-  };
-
-  public shouldComponentUpdate(newProps: ICheckProps) {
-    return this.props.isChecked !== newProps.isChecked || this.props.checked !== newProps.checked;
-  }
-
-  public render() {
-    let { isChecked } = this.props;
-    const { checked } = this.props;
-
-    isChecked = isChecked || checked;
-
-    return (
-      <div
-        className={ css(
-          'ms-Check',
-          styles.root,
-          {
-            [`is-checked ${styles.rootIsChecked}`]: isChecked
-          }
-        ) }
-      >
-        { <Icon
-          className={ 'ms-Check-circle ' + styles.circle }
-          iconName={ 'CircleRing' }
-        /> }
-        { <Icon
-          className={ 'ms-Check-check ' + styles.check }
-          iconName={ 'StatusCircleCheckmark' }
-        /> }
-      </div>
-    );
-  }
-}
+export const Check = styled<ICheckProps, ICheckStyleProps, ICheckStyles>(
+  CheckBase,
+  getStyles
+);
