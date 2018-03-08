c80336f0af0f9603dd4103339c3bfb3137c016ed
lynamemi <lynam.emily@gmail.com>
2018-03-08 11:33:00 -0800

    Adding high contrast to getFocusStyle

--- packages/styling/src/styles/getFocusStyle.ts
+++ packages/styling/src/styles/getFocusStyle.ts
@@ -1,5 +1,6 @@
 import { IRawStyle } from '@uifabric/merge-styles/lib/index';
 import { ITheme } from '../interfaces/index';
+import { HighContrastSelector } from './CommonStyles';
 
 /**
  * Generates a focus style which can be used to define an :after focus border.
@@ -35,7 +36,16 @@ export function getFocusStyle(
         right: inset + 1,
         border: '1px solid ' + theme.palette.white,
         outline: '1px solid ' + theme.palette.neutralSecondary,
-        zIndex: 1
+        zIndex: 1,
+        selectors: {
+          [HighContrastSelector]: {
+            left: inset - 1,
+            top: inset - 1,
+            bottom: inset - 1,
+            right: inset - 1,
+            border: 'none'
+          }
+        }
       }
 
     }
