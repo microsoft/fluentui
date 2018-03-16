e3592f786788cbbedee324caff307f86c8e2f390
Jordan Janzen <v-jojanz@microsoft.com>
2018-03-16 13:17:03 -0700

    Add Markdown typings

---
+++ packages/example-app-base/typings/markdown-to-jsx/index.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="react" />
+import * as React from 'react';
+
+export interface IMarkdownSettings {
+  overrides: {
+    [key: string]: {
+      component: string | React.ComponentClass<any> | React.StatelessComponent<any>;
+      props?: any;
+    }
+  };
+}
+
+export interface IMarkdownProps {
+  options?: IMarkdownSettings;
+  children?: string | React.ComponentClass<any> | React.StatelessComponent<any>;
+}
+
+export default class Markdown extends React.Component<IMarkdownProps> { }
