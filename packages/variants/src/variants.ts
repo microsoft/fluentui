e3a40d52e73af6e5a0062452e550f1b066c851c2
lynamemi <lynam.emily@gmail.com>
2018-03-16 10:48:06 -0700

    Color: Changing themePrimary / Communication Blue (#4297)
    * Switched 0078d7 to 0078d4
    
    * Updated snapshots
    
    * Added change files

--- packages/variants/src/variants.ts
+++ packages/variants/src/variants.ts
@@ -33,7 +33,7 @@ export function getSoftVariant(theme: IPartialTheme): ITheme {
     // themeDarker: '#004578',
     // themeDark: '#005a9e',
     // themeDarkAlt: '#106ebe',
-    // themePrimary: '#0078d7',
+    // themePrimary: '#0078d4',
     // themeSecondary: '#2b88d8',
     // themeTertiary: '#71afe5',
     // themeLight: '#c7e0f4',
