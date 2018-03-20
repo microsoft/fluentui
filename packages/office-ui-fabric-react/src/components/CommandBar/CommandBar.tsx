3bfe9498f8cfa0cccc1129d10bd49744c8a152f5
Brian Garland <brian@efelle.com>
2018-03-20 12:56:23 -0700

    Addressing Issue #4231 - CommandBar Items with href should also support OnClick behavior (#4314)
    Reordered the logic so an anchor tag can have an onclick function if desired

--- packages/office-ui-fabric-react/src/components/CommandBar/CommandBar.tsx
+++ packages/office-ui-fabric-react/src/components/CommandBar/CommandBar.tsx
@@ -202,15 +202,43 @@ export class CommandBar extends BaseComponent<ICommandBarProps, ICommandBarState
     const ariaLabel = item.ariaLabel || (item.iconOnly ? item.name : undefined);
 
     let command: React.ReactNode;
-    if (isLink) {
+    if (item.href) {
+      // Allow the disabled property on anchor elements for commandbar
+      command = (
+        <a
+          { ...getNativeProps(item, anchorProperties.concat(['disabled'])) }
+          id={ this._id + item.key }
+          className={ className }
+          href={ item.disabled ? undefined : item.href }
+          onClick={ item.onClick }
+          title={''}
+          aria-disabled={item.inactive}
+          data-command-key={ itemKey }
+          aria-haspopup={ hasSubmenu(item) }
+          role='menuitem'
+          aria-label={ ariaLabel }
+          aria-setsize={ setSize }
+          aria-posinset={ posInSet }
+        >
+          { (hasIcon) ? this._renderIcon(item) : (null) }
+          { isNameVisible && (
+            <span
+              className={ css('ms-CommandBarItem-commandText', styles.itemCommandText) }
+            >
+              { item.name }
+            </span>
+          ) }
+        </a>
+      );
+    } else if (isLink) {
       command = (
         <button
           { ...getNativeProps(item, buttonProperties) }
           id={ this._id + item.key }
           className={ className }
+          onClick={ this._onItemClick(item) }
           title={''}
           aria-disabled={item.inactive}
-          onClick={ this._onItemClick(item) }
           data-command-key={ itemKey }
           aria-haspopup={ hasSubmenu(item) }
           aria-expanded={ hasSubmenu(item) ? expandedMenuItemKey === item.key : undefined }
@@ -231,33 +259,6 @@ export class CommandBar extends BaseComponent<ICommandBarProps, ICommandBarState
             <Icon className={ css('ms-CommandBarItem-chevronDown', styles.itemChevronDown) } iconName='ChevronDown' />
           ) : (null) }
         </button>
-      );
-    } else if (item.href) {
-      // Allow the disabled property on anchor elements for commandbar
-      command = (
-        <a
-          { ...getNativeProps(item, anchorProperties.concat(['disabled'])) }
-          id={ this._id + item.key }
-          className={ className }
-          title={''}
-          aria-disabled={item.inactive}
-          href={ item.disabled ? undefined : item.href }
-          data-command-key={ itemKey }
-          aria-haspopup={ hasSubmenu(item) }
-          role='menuitem'
-          aria-label={ ariaLabel }
-          aria-setsize={ setSize }
-          aria-posinset={ posInSet }
-        >
-          { (hasIcon) ? this._renderIcon(item) : (null) }
-          { isNameVisible && (
-            <span
-              className={ css('ms-CommandBarItem-commandText', styles.itemCommandText) }
-            >
-              { item.name }
-            </span>
-          ) }
-        </a>
       );
     } else {
       // Allow the disabled property on div elements for commandbar
