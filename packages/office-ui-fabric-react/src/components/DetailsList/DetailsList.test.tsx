641db52b04ef4bad3d8f2cbc469f924b66fbaad9
Aditi Mandal <aditima@microsoft.com>
2018-03-22 11:31:51 -0700

    adding a test to cover the scenario

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsList.test.tsx
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsList.test.tsx
@@ -118,4 +118,41 @@ describe('DetailsList', () => {
     }, 0);
     jest.runOnlyPendingTimers();
   });
+
+  it('reset focused index after setKey updates', () => {
+    jest.useFakeTimers();
+
+    let component: any;
+    const detailsList = mount(
+      <DetailsList
+        items={ mockItems(5) }
+        setKey={ 'key1' }
+        initialFocusedIndex={ 0 }
+        // tslint:disable-next-line:jsx-no-lambda
+        componentRef={ ref => component = ref }
+        skipViewportMeasures={ true }
+        // tslint:disable-next-line:jsx-no-lambda
+        onShouldVirtualize={ () => false }
+      />);
+
+    expect(component).toBeDefined();
+    (component as IDetailsList).focusIndex(3);
+    setTimeout(() => {
+      expect(document.activeElement.textContent).toEqual('3');
+      expect(document.activeElement.className.split(' ')).toContain('ms-DetailsRow');
+    }, 0);
+    jest.runOnlyPendingTimers();
+
+    // update props to new setKey
+    const newProps = { items: mockItems(7), setKey: 'set2', initialFocusedIndex: 0 };
+    detailsList.setProps(newProps);
+    detailsList.update();
+
+    // verify that focus index is reset to 0
+    setTimeout(() => {
+      expect(document.activeElement.textContent).toEqual('0');
+      expect(document.activeElement.className.split(' ')).toContain('ms-DetailsRow');
+    }, 0);
+    jest.runOnlyPendingTimers();
+  });
 });
