b6525fe4b7ddb02278bf1e3e4372ab4d02983860
Jordan Janzen <v-jojanz@microsoft.com>
2018-03-27 15:03:32 -0700

    Update exports

--- packages/office-ui-fabric-react/src/components/Persona/index.ts
+++ packages/office-ui-fabric-react/src/components/Persona/index.ts
@@ -1,6 +1,4 @@
 export * from './Persona';
 export * from './Persona.base';
 export * from './Persona.types';
-export * from './PersonaConsts';
 export * from './PersonaCoin';
-export * from './PersonaCoin.base';
