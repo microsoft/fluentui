0b367f153172e35db70f2537f88a18176f42a784
REDMOND\jspurlin <jspurlin@microsoft.com>
2018-04-06 18:04:13 -0700

    OverflowSet: Add Foucs(firstChild) and focusElement() functions

--- packages/office-ui-fabric-react/src/components/OverflowSet/OverflowSet.tsx
+++ packages/office-ui-fabric-react/src/components/OverflowSet/OverflowSet.tsx
@@ -5,7 +5,8 @@ import {
   createRef,
   getNativeProps,
   divProperties,
-  focusFirstChild
+  focusFirstChild,
+  elementContains
 } from '../../Utilities';
 import { mergeStyles } from '../../Styling';
 import { IOverflowSet, IOverflowSetProps, IOverflowSetItemProps } from './OverflowSet.types';
@@ -75,18 +76,50 @@ export class OverflowSet extends BaseComponent<IOverflowSetProps, {}> implements
     );
   }
 
-  public focus() {
+  /**
+   * Sets focus to the first tabbable item in the zone.
+   * @param {boolean} forceIntoFirstElement If true, focus will be forced into the first element, even if focus is already in the focus zone.
+   * @returns True if focus could be set to an active element, false if no operation was taken.
+   */
+  public focus(forceIntoFirstElement?: boolean): boolean {
+    let focusSucceeded = false;
+
     if (this.props.doNotContainWithinFocusZone) {
       if (this._divContainer.value) {
-        focusFirstChild(this._divContainer.value);
+        if (forceIntoFirstElement) {
+          focusSucceeded = focusFirstChild(this._divContainer.value);
+        } else {
+          this._divContainer.value.focus();
+          focusSucceeded = document.activeElement === (this._divContainer.value as Element);
+        }
       }
-
-      return;
+    } else if (this._focusZone.value) {
+      focusSucceeded = this._focusZone.value.focus(forceIntoFirstElement);
     }
 
-    if (this._focusZone.value) {
-      this._focusZone.value.focus();
+    return focusSucceeded;
+  }
+
+  /**
+   * Sets focus to a specific child element within the zone. This can be used in conjunction with
+   * onBeforeFocus to created delayed focus scenarios (like animate the scroll position to the correct
+   * location and then focus.)
+   * @param {HTMLElement} element The child element within the zone to focus.
+   * @returns True if focus could be set to an active element, false if no operation was taken.
+   */
+  public focusElement(element: HTMLElement): boolean {
+    let focusSucceeded = false;
+
+    if (this.props.doNotContainWithinFocusZone) {
+      if (this._divContainer.value && element && elementContains(this._divContainer.value, element)) {
+        element.focus();
+        focusSucceeded = document.activeElement === element;
+      }
+    } else if (this._focusZone.value) {
+      focusSucceeded = this._focusZone.value.focusElement(element);
     }
+
+    return focusSucceeded;
   }
 
   private _onRenderItems = (items: IOverflowSetItemProps[]): JSX.Element[] => {
