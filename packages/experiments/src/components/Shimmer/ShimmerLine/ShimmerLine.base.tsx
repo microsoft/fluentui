25dc012908a459f140a9ba6fd1cc9206d56a4c6b
David Zearing <dzearing@microsoft.com>
2018-04-10 21:32:46 -0700

    Broken import.

--- packages/experiments/src/components/Shimmer/ShimmerLine/ShimmerLine.base.tsx
+++ packages/experiments/src/components/Shimmer/ShimmerLine/ShimmerLine.base.tsx
@@ -8,7 +8,7 @@ import {
   IShimmerLineStyleProps,
   IShimmerLineStyles
 } from './ShimmerLine.types';
-import { ShimmerElementVerticalAlign } from 'experiments/lib/Shimmer';
+import { ShimmerElementVerticalAlign } from '../Shimmer.types';
 
 const getClassNames = classNamesFunction<IShimmerLineStyleProps, IShimmerLineStyles>();
 
