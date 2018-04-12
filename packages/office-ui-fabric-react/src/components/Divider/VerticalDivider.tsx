c080f19adb9fa03028febf4d35c7830f6363f831
REDMOND\chiechan <chiechan@microsoft.com>
2018-04-12 14:21:15 -0700

    add space for vertical divider back

--- packages/office-ui-fabric-react/src/components/Divider/VerticalDivider.tsx
+++ packages/office-ui-fabric-react/src/components/Divider/VerticalDivider.tsx
@@ -6,6 +6,7 @@ import { getTheme } from '../../Styling';
 export const VerticalDivider = (props: IVerticalDividerProps) => {
   const theme = getTheme();
   const classNames = props.getClassNames ? props.getClassNames(theme) : getDividerClassNames(theme);
+
   return (
     <span className={ classNames.wrapper }>
       <span className={ classNames.divider } />
