c39f7a776f63c4f5b1b177638b4922ef29ea8458
Brian Garland <v-brgarl@microsoft.com>
2018-04-12 14:33:06 -0700

    use focusTrapZoneProps.isClickableOutsideFocusTrap if set

--- packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
@@ -175,7 +175,7 @@ export class Panel extends BaseComponent<IPanelProps, IPanelState> implements IP
                 ) }
               style={ customWidthStyles }
               elementToFocusOnDismiss={ elementToFocusOnDismiss }
-              isClickableOutsideFocusTrap={ true }
+              isClickableOutsideFocusTrap={ focusTrapZoneProps && !focusTrapZoneProps.isClickableOutsideFocusTrap ? false : true }
             >
               <div className={ css('ms-Panel-commands') } data-is-visible={ true } >
                 { onRenderNavigation(this.props, this._onRenderNavigation) }
