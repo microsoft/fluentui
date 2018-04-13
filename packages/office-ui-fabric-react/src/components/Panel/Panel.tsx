0e488caa96a9ca0d8b48567aa2b87cc04c415e76
Brian Garland <v-brgarl@microsoft.com>
2018-04-13 11:25:29 -0700

    Addressing Issue #4305 - Input multiline inside Panel doesn't compatible with the auto spell check by Grammarly extension (#4481)
    * Set Panel's FocusTrapZone allow click interactions while panel is open
    
    * use focusTrapZoneProps.isClickableOutsideFocusTrap if set

--- packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
@@ -175,9 +175,7 @@ export class Panel extends BaseComponent<IPanelProps, IPanelState> implements IP
                 ) }
               style={ customWidthStyles }
               elementToFocusOnDismiss={ elementToFocusOnDismiss }
-              isClickableOutsideFocusTrap={
-                isLightDismiss || isHiddenOnDismiss || (focusTrapZoneProps && focusTrapZoneProps.isClickableOutsideFocusTrap)
-              }
+              isClickableOutsideFocusTrap={ focusTrapZoneProps && !focusTrapZoneProps.isClickableOutsideFocusTrap ? false : true }
             >
               <div className={ css('ms-Panel-commands') } data-is-visible={ true } >
                 { onRenderNavigation(this.props, this._onRenderNavigation) }
