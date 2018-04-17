7d86387f18e6bbac4e383312757f5739cf7964a9
Eddie Liu <edwl@microsoft.com>
2018-04-17 13:08:01 -0700

    Fix merge conflict

--- packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePanePage.tsx
+++ packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePanePage.tsx
@@ -53,20 +53,9 @@ export class ScrollablePanePage extends React.Component<IComponentDemoPageProps,
           <div />
         }
         dos={
-<<<<<<< HEAD
-          <div>
-            <ul>
-              <li>Ensure that a parent component has a CSS height, or max-height attribute set (and any intermediary containers have an inherit, or explicit height/max-height set).</li>
-              <li>Use Sticky component on block level elements</li>
-              <li>Sticky component are ideally section headers and/or footers</li>
-              <li>Ensure that the total height of Sticky components do not exceed the height of the ScrollablePane</li>
-            </ul>
-          </div>
-=======
           <PageMarkdown>
             { require<string>('!raw-loader!office-ui-fabric-react/src/components/ScrollablePane/docs/ScrollablePaneDos.md') }
           </PageMarkdown>
->>>>>>> 4efff92981d7301ac16bc8a2cbd7624585f9c42b
         }
         donts={
           <PageMarkdown>
