5a06fcdd3923a34845f318592b51bfb175cb5ccc
Jordan Janzen <v-jojanz@microsoft.com>
2018-04-18 12:47:47 -0700

    Add `isEqual` function to: - compare values and types of arrays, objects, numbers, and strings

---
+++ packages/utilities/src/isEqual.ts
@@ -0,0 +1,73 @@
+// tslint:disable-next-line no-any
+export const isEqual = (itemA: any, itemB: any): boolean => {
+  if (itemA === itemB) {
+    return true;
+  }
+  // Get the value type
+  const type = Object.prototype.toString.call(itemA);
+  // If the two objects are not the same type, return false
+  if (type !== Object.prototype.toString.call(itemB)) {
+    return false;
+  }
+  // If items are not an object or array, return false
+  if (['[object Array]', '[object Object]'].indexOf(type) < 0) {
+    return false;
+  }
+  // Compare the length of the length of the two items
+  const valueLen = type === '[object Array]' ? itemA.length : Object.keys(itemA).length;
+  const otherLen = type === '[object Array]' ? itemB.length : Object.keys(itemB).length;
+  if (valueLen !== otherLen) {
+    return false;
+  }
+  // Compare two items
+  // tslint:disable-next-line no-any
+  const compare = (item1: any, item2: any) => {
+
+    // Get the object type
+    const itemType = Object.prototype.toString.call(item1);
+
+    // If an object or array, compare recursively
+    if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+      if (!isEqual(item1, item2)) {
+        return false;
+      }
+    } else {
+
+      // If the two items are not the same type, return false
+      if (itemType !== Object.prototype.toString.call(item2)) {
+        return false;
+      }
+
+      // Else if it's a function, convert to a string and compare
+      // Otherwise, just compare
+      if (itemType === '[object Function]') {
+        if (item1.toString() !== item2.toString()) {
+          return false;
+        }
+      } else {
+        if (item1 !== item2) {
+          return false;
+        }
+      }
+
+    }
+  };
+  // Compare properties
+  if (type === '[object Array]') {
+    for (let i = 0; i < valueLen; i++) {
+      if (compare(itemA[i], itemB[i]) === false) {
+        return false;
+      }
+    }
+  } else {
+    for (const key in itemA) {
+      if (itemA.hasOwnProperty(key)) {
+        if (compare(itemA[key], itemB[key]) === false) {
+          return false;
+        }
+      }
+    }
+  }
+  // If nothing failed, return true
+  return true;
+};
