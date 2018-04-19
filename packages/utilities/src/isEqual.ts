747196325dc3dfbd5385fb060b5bda92cb1ad455
Cliff Koh <cliff.koh@microsoft.com>
2018-04-19 12:23:44 -0700

    Fix "the NOT same" to "NOT the same"
    Fix minor English issue.

--- packages/utilities/src/isEqual.ts
+++ packages/utilities/src/isEqual.ts
@@ -1,7 +1,7 @@
 /**
  * Checks if the first and second items are the same, recursively. Use for checking arrays and objects.
- * @param itemA First item to compare to second item
- * @param itemB Second item to compare to first item
+ * @param itemA First item to compare to second item.
+ * @param itemB Second item to compare to first item.
  */
 export const isEqual = (itemA: any, itemB: any): boolean => { // tslint:disable-line no-any
   // First, a simple check for strings and numbers
@@ -72,9 +72,9 @@ export const isEqual = (itemA: any, itemB: any): boolean => { // tslint:disable-
 };
 
 /**
- * Checks if the first and second items are the NOT same, recursively. Use for checking arrays and objects.
- * @param itemA First item to compare to second item
- * @param itemB Second item to compare to first item
+ * Checks if the first and second items are NOT the same, recursively. Use for checking arrays and objects.
+ * @param itemA First item to compare to second item.
+ * @param itemB Second item to compare to first item.
  */
 export const isNotEqual = (itemA: any, itemB: any): boolean => { // tslint:disable-line no-any
   return !isEqual(itemA, itemB);
