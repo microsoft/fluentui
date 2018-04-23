1aa8e878362bffff504651ae1a94524f06566712
Jordan Janzen <v-jojanz@microsoft.com>
2018-04-23 16:15:06 -0700

    fix jsdoc

--- packages/office-ui-fabric-react/src/components/Pivot/Pivot.tsx
+++ packages/office-ui-fabric-react/src/components/Pivot/Pivot.tsx
@@ -8,10 +8,10 @@ import { PivotBase } from './Pivot.base';
 import { getStyles } from './Pivot.styles';
 
 /**
-* The Pivot control and related tabs pattern are used for navigating frequently accessed,
-* distinct content categories. Pivots allow for navigation between two or more content
-* views and relies on text headers to articulate the different sections of content.
-*/
+ * The Pivot control and related tabs pattern are used for navigating frequently accessed,
+ * distinct content categories. Pivots allow for navigation between two or more content
+ * views and relies on text headers to articulate the different sections of content.
+ */
 export const Pivot = styled<IPivotProps, IPivotStyleProps, IPivotStyles>(
   PivotBase,
   getStyles
