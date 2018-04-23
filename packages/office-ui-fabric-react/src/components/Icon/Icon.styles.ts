760029e90e5f4c57701260f8e419da03247b9aac
David Zearing <dzearing@microsoft.com>
2018-04-22 19:03:46 -0700

    more.

---
+++ packages/office-ui-fabric-react/src/components/Icon/Icon.styles.ts
@@ -0,0 +1,28 @@
+import {
+  mergeStyleSets
+} from '../../Styling';
+import {
+  memoizeFunction
+} from '../../Utilities';
+import { IIconStyleProps, IIconStyles } from './Icon.types';
+
+export const getStyles = (props: IIconStyleProps): IIconStyles => {
+  const { className, isPlaceholder, isImage } = props;
+
+  return {
+    root: [
+      isImage && 'ms-Icon-imageContainer',
+      isPlaceholder && 'ms-Icon-placeHolder',
+      {
+        display: 'inline-block',
+      },
+      isPlaceholder && {
+        width: '1em'
+      },
+      isImage && {
+        overflow: 'hidden'
+      },
+      className
+    ],
+  };
+};
