3a24397dba6e182ba4749c4b0a1cd2e214dd7869
Mark Polak <mark@thedutchies.com>
2018-04-26 14:38:44 +0200

    JsDoc

--- packages/utilities/src/Customizer.tsx
+++ packages/utilities/src/Customizer.tsx
@@ -1,20 +1,55 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { BaseComponent, IBaseProps } from './BaseComponent';
-import { ICustomizations } from './Customizations';
+import { ICustomizations, Settings, SettingsFunction } from './Customizations';
 
 export interface ICustomizerContext {
   customizations: ICustomizations;
 }
 
-// tslint:disable-next-line:no-any
-export type Settings = { [key: string]: any };
-export type SettingsFunction = (settings: Settings) => Settings;
-
-export type ICustomizerProps = Partial<{
+export type ICustomizerProps = IBaseProps & Partial<{
+  /**
+   * @description
+   * Settings are used as general settings for the React tree below.
+   * Components can subscribe to receive the settings by using `customizable`.
+   *
+   * @example
+   * Settings can be represented by a plain object that contains the key value pairs.
+   * ```
+   *  <Customizer settings={{ color: 'red' }} />
+   * ```
+   * or a function that receives the current settings and returns the new ones
+   * ```
+   *  <Customizer settings={(currentSettings) => ({ ...currentSettings, color: 'red' })} />
+   * ```
+   */
   settings: Settings | SettingsFunction;
+  /**
+   * @description
+   * Scoped settings are settings that are scoped to a specific scope. The
+   * scope is the name that is passed to the `customizable` function when the
+   * the component is customized.
+   *
+   * @example
+   * Scoped settings can be represented by a plain object that contains the key value pairs.
+   * ```
+   *  const myScopedSettings = {
+   *    Button: { color: 'red' };
+   *  };
+   *
+   *  <Customizer scopedSettings={myScopedSettings} />
+   * ```
+   * or a function that receives the current settings and returns the new ones
+   * ```
+   *  const myScopedSettings = {
+   *    Button: { color: 'red' };
+   *  };
+   *
+   *  <Customizer scopedSettings={(currentScopedSettings) => ({ ...currentScopedSettings, ...myScopedSettings })} />
+   * ```
+   */
   scopedSettings: Settings | SettingsFunction
-}> & IBaseProps;
+}>;
 
 /**
  * The Customizer component allows for default props to be mixed into components which
@@ -23,8 +58,9 @@ export type ICustomizerProps = Partial<{
  * 1. render svg icons instead of the icon font within all buttons
  * 2. inject a custom theme object into a component
  *
- * Props are provided via the settings prop, which should be a json map which contains 1 or more
- * name/value pairs representing injectable props.
+ * Props are provided via the settings prop which should be one of the following:
+ * - A json map which contains 1 or more name/value pairs representing injectable props.
+ * - A function that receives the current settings and returns the new ones that apply to the scope
  *
  * @public
  */
