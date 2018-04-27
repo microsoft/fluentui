72b765e24645995c522367ba36a34a287cbb69d1
Kevin Coughlin <keco@microsoft.com>
2018-04-27 10:38:15 -0700

    split an example out

--- apps/fabric-website/src/components/App/AppState.tsx
+++ apps/fabric-website/src/components/App/AppState.tsx
@@ -269,6 +269,7 @@ export const AppState: IAppState = {
               url: '#/components/detailslist/compact',
               name: 'compact',
               component: () => <LoadingComponent title='DetailsList - Compact' />,
+              getComponent: cb => require.ensure([], (require) => cb(require<any>('../../pages/Components/DetailsListComponentPage').DetailsListCompactComponentPage))
             },
             {
               title: 'Grouped',
--- apps/fabric-website/src/pages/Components/DetailsListComponentPage.tsx
+++ apps/fabric-website/src/pages/Components/DetailsListComponentPage.tsx
@@ -1,39 +1,65 @@
 import * as React from 'react';
 import { DetailsListPage } from '@uifabric/fabric-website-resources/lib/components/pages/DetailsListPage';
 import { PageHeader } from '../../components/PageHeader/PageHeader';
-import { ComponentPage } from '../../components/ComponentPage/ComponentPage';
 const pageStyles: any = require('../PageStyles.module.scss');
+import { ComponentPage as DummyComponentPage } from '../../components/ComponentPage/ComponentPage';
+
+// @TODO(keco): Refactor these to live under Fabric instead of website package??
+import {
+  ExampleCard,
+  ComponentPage
+} from '@uifabric/example-app-base';
+import { DetailsListCompactExample } from 'office-ui-fabric-react/src/components/DetailsList/examples/DetailsList.Compact.Example';
+const DetailsListCompactExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/DetailsList/examples/DetailsList.Compact.Example.tsx') as string;
 
 export class DetailsListComponentPage extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <div className={pageStyles.basePage}>
-        <ComponentPage>
-          <PageHeader
-            pageTitle="DetailsList"
-            backgroundColor="#038387"
-            links={[
-              {
-                text: 'Overview',
-                location: 'Overview'
-              },
-              {
-                text: 'Best Practices',
-                location: 'BestPractices'
-              },
-              {
-                text: 'Variants',
-                location: 'Variants'
-              },
-              {
-                text: 'Implementation',
-                location: 'Implementation'
-              }
-            ]}
-          />
+      <div ref='pageElement' className={pageStyles.basePage}>
+        <DummyComponentPage>
+          <PageHeader pageTitle='DetailsList' backgroundColor='#038387'
+            links={
+              [
+                {
+                  'text': 'Overview',
+                  'location': 'Overview'
+                },
+                {
+                  'text': 'Best Practices',
+                  'location': 'BestPractices'
+                },
+                {
+                  'text': 'Variants',
+                  'location': 'Variants'
+                },
+                {
+                  'text': 'Implementation',
+                  'location': 'Implementation'
+                }
+              ]
+            } />
           <DetailsListPage isHeaderVisible={false} />
-        </ComponentPage>
+        </DummyComponentPage>
       </div>
+    );
+  }
+}
+
+export class DetailsListCompactComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='DetailsList'
+        componentName='DetailsListExample'
+        overview={
+          <div />
+        }
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList'
+        exampleCards={
+          <ExampleCard title='Compact DetailsList with 500 items, filtering, marquee selection' isOptIn={true} /*code={ DetailsListCompactExampleCode }*/>
+            <DetailsListCompactExample />
+          </ExampleCard>
+        } />
     );
   }
 }
