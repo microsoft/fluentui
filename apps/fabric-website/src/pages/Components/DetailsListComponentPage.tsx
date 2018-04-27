1a17df67afc9f73b442759fa49f0ae90e63bf5fb
Kevin Coughlin <keco@microsoft.com>
2018-04-27 12:35:18 -0700

    DetailsList Custom Column example

--- apps/fabric-website/src/pages/Components/DetailsListComponentPage.tsx
+++ apps/fabric-website/src/pages/Components/DetailsListComponentPage.tsx
@@ -104,21 +104,153 @@ export class DetailsListGroupedComponentPage extends React.Component<any, any> {
 }
 
 export class DetailsListCustomColumnsComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Custom Item Columns'
+        componentName='DetailsListCustomColumnsExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Rendering custom item columns with sorting' isOptIn={ true } code={ DetailsListCustomColumnsExampleCode }>
+            <DetailsListCustomColumnsExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts') // @TODO(keco): Worth inferring somehow if child component?
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 export class DetailsListCustomRowsComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Grouped DetailsListExample'
+        componentName='DetailsListGroupedExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Simple Grouped DetailsList' isOptIn={ true } code={ DetailsListGroupedExampleCode }>
+            <DetailsListGroupedExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts')
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 export class DetailsListCustomGroupHeadersComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Grouped DetailsListExample'
+        componentName='DetailsListGroupedExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Simple Grouped DetailsList' isOptIn={ true } code={ DetailsListGroupedExampleCode }>
+            <DetailsListGroupedExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts')
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 export class DetailsListAdvancedComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Grouped DetailsListExample'
+        componentName='DetailsListGroupedExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Simple Grouped DetailsList' isOptIn={ true } code={ DetailsListGroupedExampleCode }>
+            <DetailsListGroupedExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts')
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 export class DetailsListDragDropComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Grouped DetailsListExample'
+        componentName='DetailsListGroupedExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Simple Grouped DetailsList' isOptIn={ true } code={ DetailsListGroupedExampleCode }>
+            <DetailsListGroupedExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts')
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 export class DetailsListNavigatingFocusComponentPage extends React.Component<any, any> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title='Grouped DetailsListExample'
+        componentName='DetailsListGroupedExample'
+        componentUrl='https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/DetailsList' // @TODO(keco): UPDATE
+        exampleCards={
+          <ExampleCard title='Simple Grouped DetailsList' isOptIn={ true } code={ DetailsListGroupedExampleCode }>
+            <DetailsListGroupedExample />
+          </ExampleCard>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={ [
+              require<string>('!raw-loader!office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts')
+            ] }
+          />
+        }
+        isHeaderVisible={ false }
+      />
+    );
+  }
 }
 
 {/* <ExampleCard id='simple' title='Simple DetailsList with 500 items, filtering, marquee selection' isOptIn={ true } code={ DetailsListBasicExampleCode }>
