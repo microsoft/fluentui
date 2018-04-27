5640fc4350e46e1039f55f784c35a0d298ad209e
Kevin Coughlin <keco@microsoft.com>
2018-04-27 11:41:31 -0700

    extract out examples from main DetailsList page

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsListPage.tsx
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsListPage.tsx
@@ -42,27 +42,6 @@ export class DetailsListPage extends React.Component<IComponentDemoPageProps, {}
             <ExampleCard id='default' title='Document DetailsList with 500 items, sorting, filtering, marquee selection, justified columns' isOptIn={ true } code={ DetailsListDocumentsExampleCode }>
               <DetailsListDocumentsExample />
             </ExampleCard>
-            <ExampleCard id='simple' title='Simple DetailsList with 500 items, filtering, marquee selection' isOptIn={ true } code={ DetailsListBasicExampleCode }>
-              <DetailsListBasicExample />
-            </ExampleCard>
-            <ExampleCard title='Rendering custom item columns with sorting' isOptIn={ true } code={ DetailsListCustomColumnsExampleCode }>
-              <DetailsListCustomColumnsExample />
-            </ExampleCard>
-            <ExampleCard title='Rendering custom item rows' isOptIn={ true } code={ DetailsListCustomRowsExampleCode }>
-              <DetailsListCustomRowsExample />
-            </ExampleCard>,
-            <ExampleCard title='Rendering custom group headers' isOptIn={ true } code={ DetailsListCustomGroupHeadersExampleCode }>
-              <DetailsListCustomGroupHeadersExample />
-            </ExampleCard>
-            <ExampleCard title='Advanced DetailsList of 5000 items, variable row heights' isOptIn={ true } code={ DetailsListAdvancedExampleCode }>
-              <DetailsListAdvancedExample />
-            </ExampleCard>
-            <ExampleCard title='Drag and Drop DetailsList with 10 items' isOptIn={ true } code={ DetailsListDragDropExampleCode }>
-              <DetailsListDragDropExample />
-            </ExampleCard>
-            <ExampleCard title='Navigating to new content preserving keyboard focus with initialFocusedIndex' isOptIn={ true } code={ DetailsListNavigatingFocusExampleCode }>
-              <DetailsListNavigatingFocusExample />
-            </ExampleCard>
           </div>
         }
         propertiesTables={
