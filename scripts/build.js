bef391fe5eacc38d678ca442a67b5300d5c42f7f
Mark Polak <mark@thedutchies.com>
2018-04-29 21:12:15 +0200

    Support build with lint

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,15 @@
 const runTasks = require('./run-task');
 
+const withFormattingAndLinting = process.argv.indexOf('--lint') > -1
+
 let tasks = [
   'copy',
   'sass',
+  withFormattingAndLinting ? 'prettier' : undefined,
+  withFormattingAndLinting ? 'tslint' : undefined,
   'ts',
   'jest',
   'webpack'
-];
+].filter(v => v);
 
 runTasks(tasks);
