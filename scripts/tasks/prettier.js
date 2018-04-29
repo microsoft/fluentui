48f80b59c3262cd7c28c1ea9a9b7a35525311764
Mark Polak <mark@thedutchies.com>
2018-04-29 17:50:37 +0200

    Add prettier run on staged for ts and tsx files

--- scripts/tasks/prettier.js
+++ scripts/tasks/prettier.js
@@ -9,19 +9,14 @@ module.exports = function (options) {
   const sourcePath = path.join(process.cwd(), 'src', '**', '*.{ts,tsx}');
   const prettierPath = 'node ' + path.resolve(__dirname, '../node_modules/prettier/bin-prettier.js');
 
-  const prettierConfigPath = `${projectPath}/prettier.config.js`;
+  const prettierConfigPath = path.join(
+    process.cwd(), '..', '..', 'packages', 'office-ui-fabric-react-tslint', 'prettier.config.js'
+  );
 
   try {
     fs.accessSync(prettierConfigPath, fs.constants.R_OK);
   } catch (err) {
-    console.error(`
-      Can not find prettier.config.js if you are using the prettier task in a new package add a prettier.config.js file
-      with the following content.
-
-      const prettierConfig = require('office-ui-fabric-react-tslint/prettier.config.js');
-
-      module.exports = Object.assign({}, prettierConfig);
-    `);
+    console.error('Can not find prettier.config.js');
 
     process.exit(1);
   }
