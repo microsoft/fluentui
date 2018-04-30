8de19959324dc82ffb22fb340618cf4edfb45e4e
KatherineThayerMicrosoft <kathayer@microsoft.com>
2018-04-30 13:59:21 -0700

    CommandBar: Add aria label and change to single tab stop (#4686)
    * Fix CommandBar accessibility
    
    * Use aria label instead of aria describedby
    
    * Fix prop name
    
    * Fix example code
    
    * Remove redundant constructor
    
    * Add change file

--- packages/experiments/src/components/CommandBar/examples/CommandBar.Basic.Example.tsx
+++ packages/experiments/src/components/CommandBar/examples/CommandBar.Basic.Example.tsx
@@ -18,6 +18,7 @@ export class CommandBarBasicExample extends React.Component<ICommandBarProps, {}
     return (
       <div>
         <CommandBar
+          ariaLabel='Use left and right arrow keys to navigate between commands'
           elipisisAriaLabel='More options'
           items={ items }
           overflowItems={ overflowItems }
