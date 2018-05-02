fde588513859178f06341b5f58eee21f78b6e963
Rebecca Ballantyne <rebeba@microsoft.com>
2018-05-02 13:57:54 -0700

    Fixing up comment

--- packages/office-ui-fabric-react/src/components/Button/BaseButton.tsx
+++ packages/office-ui-fabric-react/src/components/Button/BaseButton.tsx
@@ -612,9 +612,9 @@ export class BaseButton extends BaseComponent<IBaseButtonProps, IBaseButtonState
     if (!ev.defaultPrevented) {
       const menuProps = this.props.menuProps;
       if (menuProps) {
-        // When Edge + Narrator are used together, pressing "Enter" on a button fires this method and not
-        // _onMenuKeyDown. Checking ev.nativeEvent.detail differentiates between a real click event and a
-        // keypress event.
+        // When Edge + Narrator are used together (regardless of if the button is in a form or not), pressing
+        // "Enter" fires this method and not _onMenuKeyDown. Checking ev.nativeEvent.detail differentiates
+        // between a real click event and a keypress event.
         menuProps.shouldFocusOnContainer = (ev.nativeEvent.detail !== 0);
       }
       this.setState({ menuProps: menuProps });
