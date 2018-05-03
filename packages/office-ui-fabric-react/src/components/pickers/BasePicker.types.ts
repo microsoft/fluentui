2797c1d6a2050679e08da3097ff30c7391f7b006
Rebecca Ballantyne <rebeba@microsoft.com>
2018-05-03 15:39:24 -0700

    Fixes for suggestions being read twice

--- packages/office-ui-fabric-react/src/components/pickers/BasePicker.types.ts
+++ packages/office-ui-fabric-react/src/components/pickers/BasePicker.types.ts
@@ -135,6 +135,12 @@ export interface IBasePickerProps<T> extends React.Props<any> {
    * A callback to override the default behavior of adding the selected suggestion on dismiss.
    */
   onDismiss?: (ev?: any, selectedItem?: T) => void;
+  /**
+   * Adds an additional alert for the currently selected suggestion. This prop should be set to true for IE11 and below, as it
+   * enables proper screen reader behavior for each suggestion. It should not be set for modern browsers (Edge, Chrome).
+   * @default false
+   */
+  enableSelectedSuggestionAlert?: boolean;
 }
 
 export interface IBasePickerSuggestionsProps {
