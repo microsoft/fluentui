5770690c2580f881f029b6e5a4db4bc1eb746275
Kelsey Young <kelseyyoung4@gmail.com>
2018-05-07 13:19:19 -0700

    Add functions to ContextualMenuItem to open and close menus on command (#4741)
    * Add open and dismiss functions that can be referenced by a contextual menu item
    
    * Add tests for new item functions
    
    * Add rush change file
    
    * Remove binds in submenu function
    - Move private ContextualMenuItem functions back to local functions
    - Renames and refactoring
    
    * Move contextual menu button and anchor into their own component
    - Move all wrapper items into their own folder under ContextualMenu
    
    * Move the open/dismiss functions back to ContextualMenuItem
    - Rename the prop to componentRef on IContextualMenuItem
    - Add tests for split button and anchor
    - Pass a prop function which returns the element to use in openSubMenu to ContextualMenuItem
    
    * Make base class ContextualMenuItemWrapper
    - Move shared functions into protected members
    - Rename getContainerElement to getSubmenuTarget
    
    * Remove dismiss bind and pass through undefined at ContextualMenuItem

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.types.ts
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.types.ts
@@ -15,7 +15,7 @@ import { IWithResponsiveModeState } from '../../utilities/decorators/withRespons
 import { IContextualMenuClassNames, IMenuItemClassNames } from './ContextualMenu.classNames';
 export { DirectionalHint } from '../../common/DirectionalHint';
 import { IVerticalDividerClassNames } from '../Divider/VerticalDivider.types';
-import { IContextualMenuItemProps } from './ContextualMenuItem.types';
+import { IContextualMenuItemProps, IContextualMenuRenderItem } from './ContextualMenuItem.types';
 import { IKeytipProps } from '../../Keytip';
 
 export enum ContextualMenuItemType {
@@ -256,6 +256,11 @@ export interface IContextualMenuProps extends React.Props<ContextualMenu>, IWith
 
 export interface IContextualMenuItem {
   /**
+   * Optional callback to access the IContextualMenuRenderItem interface. This will get passed down to ContextualMenuItem.
+   */
+  componentRef?: (component: IContextualMenuRenderItem | null) => void;
+
+  /**
    * Unique id to identify the item
    */
   key: string;
@@ -471,7 +476,6 @@ export interface IContextualMenuItem {
    * Optional prop to make an item readonly which is disabled but visitable by keyboard, will apply aria-readonly and some styling. Not supported by all components
    */
   inactive?: boolean;
-
 }
 
 export interface IContextualMenuSection extends React.Props<ContextualMenu> {
