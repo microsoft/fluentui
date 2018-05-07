0a5e92f12bc28ef071da04b727b85b7b2826d3a5
Katherine Kjeer <kakje@microsoft.com>
2018-05-07 16:09:21 -0700

    Fix build errors

--- packages/office-ui-fabric-react/src/components/MarqueeSelection/MarqueeSelection.test.tsx
+++ packages/office-ui-fabric-react/src/components/MarqueeSelection/MarqueeSelection.test.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
 import { MarqueeSelection } from './MarqueeSelection';
+import { Selection } from 'office-ui-fabric-react/lib/MarqueeSelection';
 
 describe('MarqueeSelection', () => {
   it('renders MarqueeSelection correctly', () => {
-    const component = renderer.create(<MarqueeSelection />);
+    const component = renderer.create(<MarqueeSelection selection={ new Selection() } />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-}
+});
