4206b3d4fadf35f39eb1c2e6b853d98427ec79cb
David Zearing <dzearing@microsoft.com>
2018-05-07 19:00:17 -0700

    Fixing

--- scripts/tasks/ts.js
+++ scripts/tasks/ts.js
@@ -2,6 +2,8 @@ module.exports = function (options) {
   const path = require('path');
   const execSync = require('../exec-sync');
   const typescriptPath = 'node ' + path.resolve(__dirname, '../node_modules/typescript/lib/tsc');
+  const libPath = path.resolve(process.cwd(), 'lib');
+  const srcPath = path.resolve(process.cwd(), 'src');
   const extraParams = '--pretty' + (options.isProduction ? ` --inlineSources --sourceRoot ${path.relative(libPath, srcPath)}` : '');
 
   execSync(typescriptPath + ' -outDir lib-commonjs -t es5 -m commonjs ' + extraParams);
