fda1afecbcdc27407665ce8cf2f638bccff08e17
REDMOND\chiechan <chiechan@microsoft.com>
2018-05-07 19:09:26 -0700

    added change file; added comment

--- packages/office-ui-fabric-react/src/components/Button/BaseButton.tsx
+++ packages/office-ui-fabric-react/src/components/Button/BaseButton.tsx
@@ -639,6 +639,9 @@ export class BaseButton extends BaseComponent<IBaseButtonProps, IBaseButtonState
       ev.preventDefault();
       ev.stopPropagation();
     }
+
+    // Note: When enter is pressed, we will let the event continue to propagate
+    // to trigger the onClick event on the button
   }
 
   private _onTouchStart: () => void = () => {
