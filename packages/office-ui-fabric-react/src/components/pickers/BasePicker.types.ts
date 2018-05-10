11d1f3ca2ba652d5d9ca0d0dd55cc69602476279
Rebecca Ballantyne <rebeba@microsoft.com>
2018-05-10 11:18:10 -0700

    Updating comment to reflect aria-activedescendant

--- packages/office-ui-fabric-react/src/components/pickers/BasePicker.types.ts
+++ packages/office-ui-fabric-react/src/components/pickers/BasePicker.types.ts
@@ -137,7 +137,8 @@ export interface IBasePickerProps<T> extends React.Props<any> {
   onDismiss?: (ev?: any, selectedItem?: T) => void;
   /**
    * Adds an additional alert for the currently selected suggestion. This prop should be set to true for IE11 and below, as it
-   * enables proper screen reader behavior for each suggestion. It should not be set for modern browsers (Edge, Chrome).
+   * enables proper screen reader behavior for each suggestion (since aria-activedescendant does not work with IE11).
+   * It should not be set for modern browsers (Edge, Chrome).
    * @default false
    */
   enableSelectedSuggestionAlert?: boolean;
