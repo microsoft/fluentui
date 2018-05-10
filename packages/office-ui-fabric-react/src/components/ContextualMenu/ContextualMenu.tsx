ca86524120872c9d7624c7893f78d2f592272cde
Rebecca Ballantyne <rebeba@microsoft.com>
2018-05-10 13:55:11 -0700

    Fixes issue with sub menus not opening on right and left arrow key

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -807,14 +807,9 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
   }
 
   private _onItemKeyDown = (item: any, ev: React.KeyboardEvent<HTMLElement>): void => {
-    let openKey;
-    if (item.split) {
-      openKey = getRTL() ? KeyCodes.left : KeyCodes.right;
-    } else {
-      openKey = KeyCodes.enter;
-    }
+    const openKey = getRTL() ? KeyCodes.left : KeyCodes.right;
 
-    if (ev.which === openKey && !item.disabled) {
+    if ((ev.which === openKey || ev.which === KeyCodes.enter) && !item.disabled) {
       this.setState({
         expandedByMouseClick: false
       });
