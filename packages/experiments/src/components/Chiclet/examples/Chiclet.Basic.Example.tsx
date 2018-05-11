c91dfe3075e95eec511b3428f92bc9b1d620d503
REDMOND\naethell <naethell@microsoft.com>
2018-05-11 09:46:28 -0700

    example with activities

--- packages/experiments/src/components/Chiclet/examples/Chiclet.Basic.Example.tsx
+++ packages/experiments/src/components/Chiclet/examples/Chiclet.Basic.Example.tsx
@@ -14,7 +14,7 @@ export class ChicletBasicExample extends React.Component<any, any> {
 
   public render() {
     var footerButtonProps: IButtonProps[] = [{ iconProps: { iconName: 'More' } }, { iconProps: { iconName: 'Save' } }, { iconProps: { iconName: 'Share' } }];
-    var footer = <FooterComponent buttonProps={ footerButtonProps } />
+    var footer = <FooterComponent buttonProps={ footerButtonProps } activities="10 Comments  16 Shares  87 Views" />
     return (
       <BaseChiclet url="http://localhost:4322" size={ ChicletSize.Medium } footer={ footer }
       />
@@ -29,24 +29,30 @@ export class FooterComponent extends React.Component<any, any> {
   }
 
   public render() {
-    const { buttonProps } = this.props;
+    const { buttonProps, activities } = this.props;
 
-    return _renderFooter(buttonProps);
+    return _renderFooter(buttonProps, activities);
   }
 }
 
 export interface IFooterComponent extends React.Props<FooterComponent> {
   buttonProps: IButtonProps[];
+  activities: string;
 }
 
-function _renderFooter(buttonProps: IButtonProps[]): React.ReactElement<any> {
+function _renderFooter(buttonProps: IButtonProps[], activities: string): React.ReactElement<any> {
   return (<div className={ exampleStyles.footer }>
-    { buttonProps && buttonProps.map((buttonProp, index) => {
-      return (
-        <div className={ exampleStyles.action } key={ index }>
-          <IconButton { ...buttonProp } />
-        </div>
-      );
-    }) }
+    <div className={ exampleStyles.activities }>
+      { activities ? activities : (null) }
+    </div>
+    <div className={ exampleStyles.actions }>
+      { buttonProps && buttonProps.map((buttonProp, index) => {
+        return (
+          <div className={ exampleStyles.action } key={ index }>
+            <IconButton { ...buttonProp } />
+          </div>
+        );
+      }) }
+    </div>
   </div>);
 }
