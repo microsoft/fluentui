d01ce314a35af59f3516e4d793498c067f21fcc6
Kevin Coughlin <kevintcoughlin@gmail.com>
2018-05-16 11:21:03 -0700

    Invoke TextField's onRenderDescription if provided regardless of if description prop provided (#4706)
    * Always render description if custom renderer supplied
    
    * Add test for rendering custom description for TextField
    
    * Check custom description render function invoked w/o description text
    
    * + changefile
    
    * Rm need for local var for if onRenderDescription provided via props
    
    * + TSLint semi-colons
    
    * Rm unnecessary parens

--- packages/office-ui-fabric-react/src/components/TextField/TextField.tsx
+++ packages/office-ui-fabric-react/src/components/TextField/TextField.tsx
@@ -160,7 +160,10 @@ export class TextField extends BaseComponent<ITextFieldProps, ITextFieldState> i
     } = this.props;
     const { isFocused } = this.state;
     const errorMessage = this._errorMessage;
-    this._isDescriptionAvailable = Boolean(description || errorMessage);
+
+    // If a custom description render function is supplied then treat description as always available.
+    // Otherwise defer to the presence of description or error message text.
+    this._isDescriptionAvailable = Boolean(this.props.onRenderDescription || description || errorMessage);
 
     const textFieldClassName = css('ms-TextField', styles.root, className, {
       ['is-required ' + styles.rootIsRequiredLabel]: this.props.label && required,
