9afc622e632197b13bc55ba6ea85f13d12ea5604
Josh Chang <coralbue@gmail.com>
2018-05-16 12:42:50 -0700

    ContextualMenuSplitButton: Adding the correct attributes for screen readers to read the button correctly (#4880)
    * added fix for alt + down on menu for a menu button and not just split button
    
    * added change file; added comment
    
    * update contextual menu split button to be read correctly with screen readers
    
    * update snapshot
    
    * added change files"
    
    * removed extra code
    
    * removed wrong spacing
    
    * removed default case if aria-label is empty

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -558,6 +558,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
     hasCheckmarks?: boolean,
     hasIcons?: boolean): JSX.Element {
     const { contextualMenuItemAs } = this.props;
+    const { expandedMenuItemKey } = this.state;
 
     return (
       <ContextualMenuSplitButton
@@ -580,6 +581,7 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
         openSubMenu={ this._onItemSubMenuExpand }
         dismissSubMenu={ this._onSubMenuDismiss }
         dismissMenu={ this.dismiss }
+        expandedMenuItemKey={ expandedMenuItemKey }
         onTap={ this._onPointerAndTouchEvent }
       />
     );
