db80bf270ecf036c183e64b72b01c3ec070af17c
REDMOND\naethell <naethell@microsoft.com>
2018-05-18 11:06:43 -0700

    better description for snapshot test

--- packages/experiments/src/components/Chiclet/ChicletCard.test.tsx
+++ packages/experiments/src/components/Chiclet/ChicletCard.test.tsx
@@ -5,7 +5,7 @@ import { ChicletCard } from './ChicletCard';
 import { IChicletCardProps } from './ChicletCard.types';
 
 describe('Dialog', () => {
-  it('renders chiclet with title and url', () => {
+  it('renders chiclet with title, icon, onClick, and url', () => {
     const chicletCardProps: IChicletCardProps = { title: 'My Daily Notes', itemType: 'docx', onClick: () => alert('test') };
     const component = renderer.create(
       <ChicletCard { ...chicletCardProps } url='https://onedrive.com' />
