841a6dea1e135fdd97154ec3c7ee41d83373dc7c
natalieethell <naethell@microsoft.com>
2018-05-21 14:49:27 -0700

    Experimental Chiclet Component (#4678)
    * base chiclet page
    
    * moving Chiclet to experiments
    
    * other changes to old chiclet
    
    * basic chiclet moved to experiments
    
    * get rid of chiclet references in old directory
    
    * get rid of last Chiclet reference in wrong directory
    
    * added icons and started cleaning up
    
    * almost fixed hover border
    
    * can choose chiclet size to render and cleanup
    
    * three layers of the Chiclet
    
    * actions can now be passed in as strings
    
    * Chiclet -> BaseChiclet, ChicletPicker -> Chiclet
    
    * ChicletPicker to Chiclet
    
    * styles sheet working
    
    * styling moved to styles sheet
    
    * remove references to scss file
    
    * remove Chiclet.scss and XSmall
    
    * use ChicletType for type safety
    
    * imageAlt
    
    * added imageAlt to getOpenGraphValues
    
    * change onKeyDown to only handle enter, as space is handled by onClick
    
    * separated types file out and other small fixes
    
    * styling cleanup (IChicletCardStyles)
    
    * remove Chiclet from fabric site, only needs to be in experiments
    
    * get rid of unnecessary dependencies
    
    * do not need ChicletComponentPage on main site
    
    * more cleanup with onClick and ChicletSize
    
    * separate out opengraph functions
    
    * created IChicletAction
    
    * cleanup
    
    * styling fixes
    
    * base files added for BaseChiclet and ChicletCard
    
    * added Chiclet.base
    
    * extract actions from Chiclet and just show a footer
    
    * small fixes: added comments, changed enum, removed placeholders
    
    * change breadcrumb to more icon in example
    
    * fixed parser and other small fixes
    
    * ogType to openGraphType
    
    * example with activities
    
    * add Chiclet to index
    
    * small styling fixes
    
    * Breadcrumb description example
    
    * separate out examples
    
    * nicer icons and defaultProps for file preview image
    
    * added properties table
    
    * fixed preview image
    
    * change file
    
    * just pass in a Breadcrumb component and breadcrumb styling fixes
    
    * openGraphType to itemType, remove old comments, and other cleanup
    
    * rename Chiclet
    
    * remove unnecessary newline
    
    * actually fix merge conflicts
    
    * extractMetaTags to getOpenGraphProperties
    
    * add return type to getOpenGraphProperties
    
    * fixed errors from npm run build
    
    * removed unnecessary whitespace change
    
    * snapshot test
    
    * better description for snapshot test
    
    * small fix for preview image
    
    * change preview image link to sharepoint link
    
    * change file for Chiclet
    
    * fixed tslint errors and added state to Chiclet
    
    * small bug fix for image preview and change sample url
    
    * numerical pixel values represented as numbers rather than strings
    
    * removed unnecessary styling names
    
    * description and footer now JSX.Elements
    
    * removed unnecessary styling from Chiclet and updated snapshot

---
+++ packages/experiments/src/components/Chiclet/Chiclet.tsx
@@ -0,0 +1,13 @@
+import { styled } from '../../Utilities';
+import {
+  IChicletProps,
+  IChicletStyleProps,
+  IChicletStyles
+} from './Chiclet.types';
+import { getStyles } from './Chiclet.styles';
+import { ChicletBase } from './Chiclet.base';
+
+export const Chiclet = styled<IChicletProps, IChicletStyleProps, IChicletStyles>(
+  ChicletBase,
+  getStyles
+);
