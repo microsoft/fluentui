212a850c0c86dcac4853cff117a0f5cc662fe955
Sivaraman Krishnan <sikrishn@microsoft.com>
2018-05-24 23:47:53 -0700

    auto select based on the selected key prop

--- packages/experiments/src/components/Nav/Nav.tsx
+++ packages/experiments/src/components/Nav/Nav.tsx
@@ -58,6 +58,9 @@ class NavComponent extends NavBase {
     if (hasChildren) {
       // show child links
       link.isExpanded = !link.isExpanded;
+      // disable auto expand based on selected key prop, instead allow to toggle child links
+      link.disableAutoExpand = true;
+
       nextState.isLinkExpandStateChanged = true;
     }
     else if (link.onClick) {
@@ -141,6 +144,10 @@ class NavComponent extends NavBase {
     }
 
     const linkText = this.getLinkText(link, this.props.showMore);
+    const isChildLinkSelected = this.isChildLinkSelected(link);
+
+    // if allowed, auto expand if the child is selected
+    link.isExpanded = link.disableAutoExpand ? link.isExpanded : isChildLinkSelected;
 
     return (
       <li
