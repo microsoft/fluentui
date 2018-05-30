877402c1603de7f7f7705c7d6626c7b80c466a4f
David Zearing <dzearing@microsoft.com>
2018-05-29 19:53:28 -0700

    Update iconAliases.ts

--- packages/icons/src/iconAliases.ts
+++ packages/icons/src/iconAliases.ts
@@ -3,4 +3,4 @@ import { registerIconAlias } from '@uifabric/styling';
 export const registerIconAliases = () => {
   registerIconAlias('trash', 'delete');
   registerIconAlias('onedrive', 'onedrivelogo');
-}
+};
