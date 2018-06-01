c363c698a828cdee9c51302c969635e1a125ba7e
REDMOND\benw <benw@microsoft.com>
2018-06-01 16:47:33 -0700

    update jdoc comments; remove utilities.api.ts

--- packages/utilities/src/focus.ts
+++ packages/utilities/src/focus.ts
@@ -43,7 +43,9 @@ export function getLastFocusable(
 /**
  * Gets the first tabbable element.
  * The difference between focusable and tabbable is that tabbable elements are focusable elements that also have tabIndex != -1.
- *
+ * @param rootElement The parent element to search beneath.
+ * @param currentElement The descendant of rootElement to start the search at.  This element is the first one checked, and iteration continues forward.  Typical use passes rootElement.firstChild.
+ * @param includeElementsInFocusZones true if traversal should go into FocusZone descendants.
  * @public
  */
 export function getFirstTabbable(
@@ -58,7 +60,9 @@ export function getFirstTabbable(
 /**
  * Gets the last tabbable element.
  * The difference between focusable and tabbable is that tabbable elements are focusable elements that also have tabIndex != -1.
- *
+ * @param rootElement The parent element to search beneath.
+ * @param currentElement The descendant of rootElement to start the search at.  This element is the first one checked, and iteration continues in reverse.  Typical use passes rootElement.lastChild.
+ * @param includeElementsInFocusZones true if traversal should go into FocusZone descendants.
  * @public
  */
 export function getLastTabbable(
