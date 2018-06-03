0c8481b44fe9bddbeaf3bf2e31c524e1220a68d5
Mark Polak <mark@thedutchies.com>
2018-06-03 11:08:48 +0200

    Log git diff to check why package changed

--- scripts/check-for-modified-files.js
+++ scripts/check-for-modified-files.js
@@ -6,10 +6,12 @@ const gitStatusOutput = execSync('git status -s --untracked-files=no').toString(
 const hasChangedFiles = gitStatusOutput.split(EOL).filter(notEmpty).length > 0;
 
 if (hasChangedFiles) {
-  console.log("This build has files that are tracked by git that resulted in changed files.");
-  console.log("Check the following output and resolve the problem that caused these files to change");
-  console.log("Most likely you committed your files with --no-verify");
+  console.log('This build has files that are tracked by git that resulted in changed files.');
+  console.log('Check the following output and resolve the problem that caused these files to change');
+  console.log('Most likely you committed your files with --no-verify');
   console.log(gitStatusOutput);
+
+  console.log(execSync('git diff').toString('utf8'));
 
   process.exit(1);
 } else {
