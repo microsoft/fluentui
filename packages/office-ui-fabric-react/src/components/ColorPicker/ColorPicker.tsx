3d1751d4e94ce98213f1bc18fbe8ab9355215233
Cliff Koh <cliff.koh@microsoft.com>
2018-06-04 16:55:36 -0700

    test

--- packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.tsx
+++ packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.tsx
@@ -55,85 +55,85 @@ export class ColorPicker extends BaseComponent<IColorPickerProps, IColorPickerSt
     const { color } = this.state;
 
     return (
-      <div className={css('ms-ColorPicker', styles.root)}>
-        <div className={css('ms-ColorPicker-panel', styles.panel)}>
-          <ColorRectangle color={color} onSVChanged={this._onSVChanged} />
+      <div className={ css('ms-ColorPicker', styles.root) }>
+        <div className={ css('ms-ColorPicker-panel', styles.panel) }>
+          <ColorRectangle color={ color } onSVChanged={ this._onSVChanged } />
           <ColorSlider
-            className={css('is-hue', styles.colorSliderIsHue)}
-            minValue={0}
-            maxValue={MAX_COLOR_HUE}
-            value={color.h}
-            onChanged={this._onHChanged}
+            className={ css('is-hue', styles.colorSliderIsHue) }
+            minValue={ 0 }
+            maxValue={ MAX_COLOR_HUE }
+            value={ color.h }
+            onChanged={ this._onHChanged }
           />
-          {!this.props.alphaSliderHidden && (
+          { !this.props.alphaSliderHidden && (
             <ColorSlider
-              className={css('is-alpha', styles.colorSliderIsAlpha)}
-              overlayStyle={{ background: `linear-gradient(to right, transparent 0, ${color.str} 100%)` }}
-              minValue={0}
-              maxValue={100}
-              value={color.a}
-              onChanged={this._onAChanged}
+              className={ css('is-alpha', styles.colorSliderIsAlpha) }
+              overlayStyle={ { background: `linear-gradient(to right, transparent 0, ${color.str} 100%)` } }
+              minValue={ 0 }
+              maxValue={ 100 }
+              value={ color.a }
+              onChanged={ this._onAChanged }
             />
-          )}
-          <table className={css('ms-ColorPicker-table', styles.table)} cellPadding="0" cellSpacing="0">
+          ) }
+          <table className={ css('ms-ColorPicker-table', styles.table) } cellPadding="0" cellSpacing="0">
             <thead>
-              <tr className={FontClassNames.small}>
-                <td className={styles.tableHexCell}>{this.props.hexLabel}</td>
-                <td>{this.props.redLabel}</td>
-                <td>{this.props.greenLabel}</td>
-                <td>{this.props.blueLabel}</td>
-                {!this.props.alphaSliderHidden && <td>{this.props.alphaLabel}</td>}
+              <tr className={ FontClassNames.small }>
+                <td className={ styles.tableHexCell }>{ this.props.hexLabel }</td>
+                <td>{ this.props.redLabel }</td>
+                <td>{ this.props.greenLabel }</td>
+                <td>{ this.props.blueLabel }</td>
+                { !this.props.alphaSliderHidden && <td>{ this.props.alphaLabel }</td> }
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>
                   <TextField
-                    className={css('ms-ColorPicker-input', styles.input)}
-                    value={color.hex}
-                    ref={ref => (this.hexText = ref!)}
-                    onBlur={this._onHexChanged}
-                    spellCheck={false}
+                    className={ css('ms-ColorPicker-input', styles.input) }
+                    value={ color.hex }
+                    ref={ ref => (this.hexText = ref!) }
+                    onBlur={ this._onHexChanged }
+                    spellCheck={ false }
                   />
                 </td>
-                <td style={{ width: '18%' }}>
+                <td style={ { width: '18%' } }>
                   <TextField
-                    className={css('ms-ColorPicker-input', styles.input)}
-                    onBlur={this._onRGBAChanged}
-                    value={String(color.r)}
-                    ref={ref => (this.rText = ref!)}
-                    spellCheck={false}
+                    className={ css('ms-ColorPicker-input', styles.input) }
+                    onBlur={ this._onRGBAChanged }
+                    value={ String(color.r) }
+                    ref={ ref => (this.rText = ref!) }
+                    spellCheck={ false }
                   />
                 </td>
-                <td style={{ width: '18%' }}>
+                <td style={ { width: '18%' } }>
                   <TextField
-                    className={css('ms-ColorPicker-input', styles.input)}
-                    onBlur={this._onRGBAChanged}
-                    value={String(color.g)}
-                    ref={ref => (this.gText = ref!)}
-                    spellCheck={false}
+                    className={ css('ms-ColorPicker-input', styles.input) }
+                    onBlur={ this._onRGBAChanged }
+                    value={ String(color.g) }
+                    ref={ ref => (this.gText = ref!) }
+                    spellCheck={ false }
                   />
                 </td>
-                <td style={{ width: '18%' }}>
+                <td style={ { width: '18%' } } >
                   <TextField
-                    className={css('ms-ColorPicker-input', styles.input)}
-                    onBlur={this._onRGBAChanged}
-                    value={String(color.b)}
-                    ref={ref => (this.bText = ref!)}
-                    spellCheck={false}
+                    className={ css('ms-ColorPicker-input', styles.input) }
+                    onBlur={ this._onRGBAChanged }
+                    value={ String(color.b) }
+                    ref={ ref => (this.bText = ref!) }
+                    spellCheck={ false }
                   />
                 </td>
-                {!this.props.alphaSliderHidden && (
-                  <td style={{ width: '18%' }}>
+                { !this.props.alphaSliderHidden && (
+                  <td style={ { width: '18%' } }>
                     <TextField
-                      className={css('ms-ColorPicker-input', styles.input)}
-                      onBlur={this._onRGBAChanged}
-                      value={String(color.a)}
-                      ref={ref => (this.aText = ref!)}
-                      spellCheck={false}
+                      className={ css('ms-ColorPicker-input', styles.input) }
+                      onBlur={ this._onRGBAChanged }
+                      value={ String(color.a) }
+                      ref={ ref => (this.aText = ref!) }
+                      spellCheck={ false }
                     />
                   </td>
-                )}
+                ) }
               </tr>
             </tbody>
           </table>
