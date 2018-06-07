4098e7050bee70a6292209c3f3942678e0bebdfb
Ken Chau <ken@gizzar.com>
2018-06-06 20:13:36 -0700

    added some documentation for the loader

--- packages/webpack-utils/src/fabricAsyncLoader.ts
+++ packages/webpack-utils/src/fabricAsyncLoader.ts
@@ -3,10 +3,23 @@ import * as webpack from 'webpack';
 import * as path from 'path';
 import * as loaderUtils from 'loader-utils';
 
+/**
+ * Fabric async loader will automatically replace the Fabric components specified in the "include" option of the loader with below code
+ * The code below uses the `react-loadable` control to facilitate auto code-splitting at the Fabric component level
+ *
+ * @param content the content - passed through during the
+ */
 const fabricAsyncLoader: webpack.loader.Loader = (content: string) => {
   return content;
 };
 
+/**
+ * Pitch is first phase of the loading process, it gives access to the module name. This method actually injects the code.
+ * @see https://github.com/webpack/docs/wiki/loaders
+ *
+ * @param remainingRequest the remaining requested module
+ * @param previousRequest the remaining requested module
+ */
 fabricAsyncLoader.pitch = (remainingRequest: string, precedingRequest: string) => {
   const moduleRequest = `!!${remainingRequest}`;
   const normalizedRequest = loaderUtils.stringifyRequest(this, moduleRequest);
@@ -20,4 +33,5 @@ fabricAsyncLoader.pitch = (remainingRequest: string, precedingRequest: string) =
   ].join('');
 };
 
+// Exports with the old module export standard (pre-ESM) to make it compatible with Webpack
 export = fabricAsyncLoader;
