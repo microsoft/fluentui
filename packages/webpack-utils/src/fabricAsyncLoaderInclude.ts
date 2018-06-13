f4b279efffda966877607dfef697025a7fe8d5f0
Ken Chau <kchau@microsoft.com>
2018-06-12 18:46:22 -0700

    adding a few useful options for fabricAsyncLoader

--- packages/webpack-utils/src/fabricAsyncLoaderInclude.ts
+++ packages/webpack-utils/src/fabricAsyncLoaderInclude.ts
@@ -1,3 +1,3 @@
-export const fabricAsyncLoaderInclude = (input: string) => {
-  return input.indexOf('ContextualMenu');
+export = (input: string) => {
+  return input.match(/office-ui-fabric-react[\\/]lib[\\/]components[\\/]ContextualMenu[\\/]ContextualMenu.js/);
 };
