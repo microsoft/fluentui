2c1889c88ceb188aff13f3b2da3873e4e6f70dc1
Amy Hackett <ahackett@microsoft.com>
2018-06-12 17:05:13 -0700

    DetailsList: Add getStartItemIndexInView() to for the item index that was in view  (#5053)
    * fix List scrolling example page issue
    
    * add code to preserve last focused index
    
    * rush change file
    
    * add method to get itemindex in view
    
    * rush change file
    
    * remove previous change file
    
    * no need for onWillUnmount in props, update some example pages
    
    * change file
    
    * cleanup
    
    * update with prettier
    
    * run prettier tool
    
    * use IPage.startIndex
    
    * add mesaureItem parameter
    
    * add IPage.isSpacer
    
    * fix a bug
    
    * code cleanup
    
    * update snapshots
    
    * Update ahackett-listscroll531_2018-06-06-22-29.json

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsList.types.ts
@@ -34,6 +34,11 @@ export interface IDetailsList extends IList {
     measureItem?: (itemIndex: number) => number,
     scrollToMode?: ScrollToMode
   ) => void;
+
+  /**
+   * Get the start index of the page that is currently in view
+   */
+  getStartItemIndexInView: () => number;
 }
 
 export interface IDetailsListProps extends React.Props<DetailsList>, IWithViewportProps {
@@ -114,7 +119,7 @@ export interface IDetailsListProps extends React.Props<DetailsList>, IWithViewpo
   /** Callback for when a given row has been mounted. Useful for identifying when a row has been rendered on the page. */
   onRowDidMount?: (item?: any, index?: number) => void;
 
-  /** Callback for when a given row has been mounted. Useful for identifying when a row has been removed from the page. */
+  /** Callback for when a given row has been unmounted. Useful for identifying when a row has been removed from the page. */
   onRowWillUnmount?: (item?: any, index?: number) => void;
 
   /** Callback for when the user clicks on the column header. */
