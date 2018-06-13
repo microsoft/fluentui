d646ebb99716b9fb98f03706b83360b0e3b28668
Veerash Ayyagari <veeray@microsoft.com>
2018-06-12 21:15:21 -0700

    update overview and content

--- packages/experiments/src/components/Recommendation/RecommendationPage.tsx
+++ packages/experiments/src/components/Recommendation/RecommendationPage.tsx
@@ -33,19 +33,39 @@ export class RecommendationPage extends React.Component<IComponentDemoPageProps,
             </ExampleCard>
           </div>
         }
-        overview={<div />}
+        /* tslint:disable:max-line-length */
+        overview={
+          <div>
+            <p>
+              The recommendation control allows you to recommend an action to an admin with and without a data
+              visualization.
+            </p>
+          </div>
+        }
         bestPractices={<div />}
         dos={
           <div>
             <ul>
-              <li>Do's</li>
+              <li>Use for features where activation can occur in a few steps in the context of M365 dashboard.</li>
+              <li>Provide a concise benefit headline that speaks to the product value of the feature.</li>
+              <li>
+                Keep description text brief and consider how localization may affect the message. Translation to other
+                languages may add up to 33% more characters to the string length.
+              </li>
+              <li>
+                Include a single data visualization only when it makes comprehension of the feature more efficient.
+              </li>
+              <li>Include a single CTA (“view recommendation”).</li>
+              <li>Offer a corresponding report card (if applicable) on the dashboard after activation.</li>
             </ul>
           </div>
         }
         donts={
           <div>
             <ul>
-              <li>Dont's</li>
+              <li>Make the admin leave the dashboard to activate the feature.</li>
+              <li>Include multiple calls to action on the recommendation card.</li>
+              <li>Include multiple data visualizations.</li>
             </ul>
           </div>
         }
