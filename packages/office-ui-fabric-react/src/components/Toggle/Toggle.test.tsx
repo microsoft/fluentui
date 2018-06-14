5967cc14233e75ff7d0322276a0e4011e0d33f0a
David Zearing <dzearing@microsoft.com>
2018-06-13 19:43:37 -0700

    wat

--- packages/office-ui-fabric-react/src/components/Toggle/Toggle.test.tsx
+++ packages/office-ui-fabric-react/src/components/Toggle/Toggle.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
-import * as ReactDOM from 'react-dom';
 import * as renderer from 'react-test-renderer';
 import * as sinon from 'sinon';
 
