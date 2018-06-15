d02e0b3a839318c81ec472de96793e4810a95f52
Raghuraman Krishnamurthy <raghurk@microsoft.com>
2018-06-15 16:31:14 -0700

    Add GridList component to the existing Dashboard Card Component (#5230)
    * Dashboard Card Component for Admin Portal Dashboard.
    
    * adding change log file
    
    * Fixed Code review feedback
    
    * Add GridList component  into Dashboard Card
    
    * Add change log file
    
    * Resolve merge conflict
    
    * resolve merge conflicts
    
    * Change more Icon to Admin Icon in Card Example
    
    * Reverting the change made in my master by TSlint/Prettier and keeping it the same as in the office-fabric-ui master

--- packages/experiments/src/components/Card/CardPage.tsx
+++ packages/experiments/src/components/Card/CardPage.tsx
@@ -3,9 +3,11 @@ import { ExampleCard, ComponentPage, PropertiesTableSet } from '@uifabric/exampl
 import { SmallCardBasicExample } from './examples/Card.Small.Basic.Example';
 import { MediumTallCardBasicExample } from './examples/Card.MediumTall.Basic.Example';
 import { MediumWideCardBasicExample } from './examples/Card.MediumWide.Basic.Example';
+import { LargeCardBasicExample } from './examples/Card.Large.Basic.Example';
 const SmallCardExampleCode = require('!raw-loader!@uifabric/experiments/src/components/Card/examples/Card.Small.Basic.Example.tsx') as string;
 const MediumTallCardExampleCode = require('!raw-loader!@uifabric/experiments/src/components/Card/examples/Card.MediumTall.Basic.Example.tsx') as string;
 const MediumWideCardExampleCode = require('!raw-loader!@uifabric/experiments/src/components/Card/examples/Card.MediumWide.Basic.Example.tsx') as string;
+const LargeCardExampleCode = require('!raw-loader!@uifabric/experiments/src/components/Card/examples/Card.Large.Basic.Example.tsx') as string;
 
 export class CardPage extends React.Component<{}, {}> {
   public render(): JSX.Element {
@@ -15,24 +17,56 @@ export class CardPage extends React.Component<{}, {}> {
         componentName="DashboardCardExample"
         exampleCards={
           <div>
-            {
-              <ExampleCard title="Small Card" code={SmallCardExampleCode}>
-                <SmallCardBasicExample />
-              </ExampleCard>
-            }
-            {
-              <ExampleCard title="Medium Tall Card" code={MediumTallCardExampleCode}>
-                <MediumTallCardBasicExample />
-              </ExampleCard>
-            }
+            <ExampleCard title="Small Card" code={SmallCardExampleCode}>
+              <SmallCardBasicExample />
+            </ExampleCard>
+            <ExampleCard title="Medium Tall Card" code={MediumTallCardExampleCode}>
+              <MediumTallCardBasicExample />
+            </ExampleCard>
             <ExampleCard title="Medium Wide Card" code={MediumWideCardExampleCode}>
               <MediumWideCardBasicExample />
+            </ExampleCard>
+            <ExampleCard title="Large Card" code={LargeCardExampleCode}>
+              <LargeCardBasicExample />
             </ExampleCard>
           </div>
         }
         propertiesTables={
           <PropertiesTableSet
-            sources={[require<string>('!raw-loader!@uifabric/experiments/src/components/Card/Card.types.ts')]}
+            sources={[
+              require<string>('!raw-loader!@uifabric/experiments/src/components/Card/Card.types.ts'),
+              require<
+                string
+              >('!raw-loader!@uifabric/experiments/src/components/Card/ThumbnailList/ThumbnailList.types.ts'),
+              require<string>('!raw-loader!@uifabric/experiments/src/components/Card/GridList/GridList.types.ts'),
+              require<string>('!raw-loader!@uifabric/experiments/src/components/Card/Layout/Layout.types.ts'),
+              require<
+                string
+              >('!raw-loader!@uifabric/experiments/src/components/Card/CompoundButtonStack/CompoundButtonStack.types.ts'),
+              require<string>('!raw-loader!@uifabric/experiments/src/components/Card/BodyText/BodyText.types.ts'),
+              require<string>('!raw-loader!@uifabric/experiments/src/components/Card/ActionBar/ActionBar.types.ts')
+            ]}
+            renderOnly={[
+              'CardSize',
+              'CardContentType',
+              'Priority',
+              'ICardFrameContent',
+              'ICardProps',
+              'IThumbnailItemProps',
+              'IThumbnailListProps',
+              'GridColumnContentType',
+              'IGridCellItem',
+              'IGridColumn',
+              'IGridRow',
+              'IGridListProps',
+              'ICardContentDetails',
+              'ILayoutProps',
+              'ICompoundAction',
+              'ICompoundButtonStackProps',
+              'IBodyTextProps',
+              'IAction',
+              'IActionBarProps'
+            ]}
           />
         }
         overview={
