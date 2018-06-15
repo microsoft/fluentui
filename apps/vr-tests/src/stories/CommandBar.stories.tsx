dd61a7f8e9f6831d289a6368cc4f0153b9419cc7
kkjeer <kkjeer@wellesley.edu>
2018-06-15 15:34:49 -0700

    Revert CommandBar.stories to match master

--- apps/vr-tests/src/stories/CommandBar.stories.tsx
+++ apps/vr-tests/src/stories/CommandBar.stories.tsx
@@ -1,4 +1,5 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
@@ -28,8 +29,8 @@ const items: ICommandBarItemProps[] = [
             iconName: 'Calendar'
           }
         }
-      ]
-    }
+      ],
+    },
   },
   {
     key: 'upload',
@@ -90,28 +91,34 @@ storiesOf('CommandBar', module)
   .addDecorator(FabricDecoratorTall)
   .addDecorator(story => (
     <Screener
-      steps={new Screener.Steps()
+      steps={ new Screener.Steps()
         .snapshot('default', { cropTo: '.testWrapper' })
         .hover('.ms-CommandBarItem-link')
         .snapshot('hover', { cropTo: '.testWrapper' })
         .click('.ms-CommandBarItem-link')
         .hover('.ms-CommandBarItem-link')
         .snapshot('click', { cropTo: '.testWrapper' })
-        .end()}
+        .end()
+      }
     >
-      {story()}
+      { story() }
     </Screener>
   ))
-  .add('Root', () => <CommandBar items={items} farItems={farItems} />)
+  .add('Root', () => (
+    <CommandBar
+      items={ items }
+      farItems={ farItems }
+    />
+  ))
   .add('Text only', () => (
     <CommandBar
-      items={items.map(item => ({ ...item, iconProps: undefined }))}
-      farItems={farItems.map(item => ({ ...item, iconProps: undefined }))}
+      items={ items.map(item => ({ ...item, iconProps: undefined })) }
+      farItems={ farItems.map(item => ({ ...item, iconProps: undefined })) }
     />
   ))
   .add('Icons only', () => (
     <CommandBar
-      items={items.map(item => ({ ...item, text: '' }))}
-      farItems={farItems.map(item => ({ ...item, text: '' }))}
+      items={ items.map(item => ({ ...item, text: '' })) }
+      farItems={ farItems.map(item => ({ ...item, text: '' })) }
     />
   ));
