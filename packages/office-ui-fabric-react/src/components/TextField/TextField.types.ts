b3465a47474b8494e2026203ec36dd7d16928444
eveleens <eveleen@helloshopper.com>
2018-06-15 14:27:02 -0700

    TextField: Add 'readonly' prop (#5218)
    * add readonly prop to textField
    
    * Update evsung-textfieldreadonly_2018-06-14-21-24.json
    
    * Update TextField.types.ts

--- packages/office-ui-fabric-react/src/components/TextField/TextField.types.ts
+++ packages/office-ui-fabric-react/src/components/TextField/TextField.types.ts
@@ -148,6 +148,12 @@ export interface ITextFieldProps extends React.AllHTMLAttributes<HTMLInputElemen
   disabled?: boolean;
 
   /**
+   * If true, the textfield is readonly.
+   * @default false
+   */
+  readOnly?: boolean;
+
+  /**
    * If set, this will display an error message for the text field.
    */
   errorMessage?: string;
