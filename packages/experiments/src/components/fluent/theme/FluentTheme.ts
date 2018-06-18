e91edac6be216c941dbed268102c82313700308d
Mike Wheaton <mike@mikewheaton.ca>
2018-06-18 14:52:47 -0700

    Remove CommunicationTint20, rename two Shared cyans to grays, remove High Contrast palette, and add communication colors to theme (values are the same)

--- packages/experiments/src/components/fluent/theme/FluentTheme.ts
+++ packages/experiments/src/components/fluent/theme/FluentTheme.ts
@@ -1,5 +1,5 @@
 import { createTheme, ITheme } from 'office-ui-fabric-react';
-import { NeutralColors } from './FluentColors';
+import { NeutralColors, CommunicationColors } from './FluentColors';
 
 const FluentTheme: ITheme = createTheme({
   palette: {
@@ -15,7 +15,15 @@ const FluentTheme: ITheme = createTheme({
     neutralLight: NeutralColors.gray30,
     neutralLighter: NeutralColors.gray20,
     neutralLighterAlt: NeutralColors.gray10,
-    white: NeutralColors.white
+    white: NeutralColors.white,
+    themeDarker: CommunicationColors.communicationShade30,
+    themeDark: CommunicationColors.communicationShade20,
+    themeDarkAlt: CommunicationColors.communicationShade10,
+    themePrimary: CommunicationColors.communicationPrimary,
+    themeSecondary: CommunicationColors.communicationTint10,
+    themeLight: CommunicationColors.communicationTint20,
+    themeLighter: CommunicationColors.communicationTint30,
+    themeLighterAlt: CommunicationColors.communicationTint40
   }
 });
 
