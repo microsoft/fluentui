f0364d9070144b31f2450c2b959029066e887d44
Jason Gore <jason.gore@grabtaxi.com>
2018-06-29 15:51:13 -0700

    Convert TeachingBubble to JS styling.

--- packages/office-ui-fabric-react/src/components/TeachingBubble/TeachingBubble.test.tsx
+++ packages/office-ui-fabric-react/src/components/TeachingBubble/TeachingBubble.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import * as ReactDOM from 'react-dom';
 import * as ReactTestUtils from 'react-dom/test-utils';
 import * as renderer from 'react-test-renderer';
 import { TeachingBubble } from './TeachingBubble';
@@ -70,19 +69,8 @@ describe('TeachingBubble', () => {
     expect(treeContent).toMatchSnapshot();
   });
 
-  // <Layer> components will lead ReactDOM.findDOMNode(test_component) return null, so the test is based on the teaching bubble content.
-  it('renders a label', () => {
-    const component = ReactTestUtils.renderIntoDocument(<TeachingBubbleContent headline="Title" />);
-    const renderedDOM = ReactDOM.findDOMNode(component as React.ReactInstance) as Element;
-    const titleElement = renderedDOM.querySelector('.ms-TeachingBubble-headline');
-
-    expect(titleElement!.textContent).toEqual('Title');
-  });
-
   it('merges callout classNames', () => {
-    ReactTestUtils.renderIntoDocument<TeachingBubbleContent>(
-      <TeachingBubbleContent headline="Title" calloutProps={{ className: 'foo' }} />
-    );
+    ReactTestUtils.renderIntoDocument(<TeachingBubbleContent headline="Title" calloutProps={{ className: 'foo' }} />);
     setTimeout(() => {
       const callout = document.querySelector('.ms-Callout') as HTMLElement;
       expect(callout).toBeDefined();
