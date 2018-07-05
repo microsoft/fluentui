514f06a3067c334501ccf5ace82d53e4e91d5d79
REDMOND\naethell <naethell@microsoft.com>
2018-07-05 14:14:43 -0700

    make Text styles more readable

--- packages/experiments/src/components/Text/Text.tsx
+++ packages/experiments/src/components/Text/Text.tsx
@@ -55,31 +55,47 @@ const styles = (props: IStyleProps<ITextProps, ITextStyles>): ITextStyles => {
 
   const { palette, fonts, /* semanticColors, isInverted, */ typography } = theme;
 
+  const fontFamily = typography // if typography is defined, we'll use it to set the property
+    ? family && typography.families[family] // if family prop is passed in and exists in typography.families, we'll use it
+      ? typography.families[family]
+      : typography.families.default // otherwise use default
+    : style && fonts[style] // if style prop is passed in and exists in fonts, we'll use that for fontFamily
+      ? fonts[style].fontFamily
+      : fonts.medium.fontFamily;
+
+  const fontWeight = typography
+    ? weight && typography.weights[weight]
+      ? typography.weights[weight]
+      : typography.weights.default
+    : style && fonts[style]
+      ? fonts[style].fontWeight
+      : fonts.medium.fontWeight;
+
+  const fontSize = typography
+    ? size && typography.sizes[size]
+      ? typography.sizes[size]
+      : typography.sizes.medium
+    : style && fonts[style]
+      ? fonts[style].fontSize
+      : fonts.medium.fontSize;
+
+  // just use fonts to set these properties
+  const mozOsxFontSmoothing =
+    style && fonts[style] ? fonts[style].MozOsxFontSmoothing : fonts.medium.MozOsxFontSmoothing;
+
+  const webkitFontSmoothing =
+    style && fonts[style] ? fonts[style].WebkitFontSmoothing : fonts.medium.WebkitFontSmoothing;
+
   const themeType = {
-    fontFamily: typography
-      ? family && typography.families[family]
-        ? typography.families[family]
-        : typography.families.default
-      : style && fonts[style]
-        ? fonts[style].fontFamily
-        : fonts.medium.fontFamily,
-    fontWeight: typography
-      ? weight && typography.weights[weight]
-        ? typography.weights[weight]
-        : typography.weights.default
-      : style && fonts[style]
-        ? fonts[style].fontWeight
-        : fonts.medium.fontWeight,
-    fontSize: typography
-      ? size && typography.sizes[size]
-        ? typography.sizes[size]
-        : typography.sizes.medium
-      : style && fonts[style]
-        ? fonts[style].fontSize
-        : fonts.medium.fontSize,
-    mozOsxFontSmoothing: style && fonts[style] ? fonts[style].MozOsxFontSmoothing : fonts.medium.MozOsxFontSmoothing,
-    webkitFontSmoothing: style && fonts[style] ? fonts[style].WebkitFontSmoothing : fonts.medium.WebkitFontSmoothing,
-    color: color && palette[color] ? palette[color] : palette.neutralPrimary
+    fontFamily: fontFamily,
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+    mozOsxFontSmoothing: mozOsxFontSmoothing,
+    webkitFontSmoothing: webkitFontSmoothing,
+    color:
+      color && palette[color] // use palette to set the color
+        ? palette[color]
+        : palette.neutralPrimary
   };
 
   return {
