72c3caf3b691350bb35d4fee14e9b955efb3f5e4
Bryan Watts <brwatt@microsoft.com>
2018-07-06 13:47:59 -0700

    Make Styling directly reference utilities modules

--- packages/styling/src/styles/getFocusStyle.ts
+++ packages/styling/src/styles/getFocusStyle.ts
@@ -1,7 +1,7 @@
 import { IRawStyle } from '@uifabric/merge-styles';
 import { ITheme } from '../interfaces/index';
 import { HighContrastSelector } from './CommonStyles';
-import { IsFocusVisibleClassName } from '@uifabric/utilities';
+import { IsFocusVisibleClassName } from '@uifabric/utilities/lib/initializeFocusRects';
 import { ZIndexes } from './zIndexes';
 
 /**
