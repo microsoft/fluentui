1a404bbfba5bdf9ed099f3754a9af59bf5a6df19
Ken <kchau@microsoft.com>
2018-07-09 15:25:14 -0700

    Revert "External documentation website: Searchbox feature for left nav (#5491)"
    This reverts commit 3e21a8c054b344e5e3b90d465cfcbb4947afe18e.

--- apps/fabric-website/src/components/Nav/Nav.tsx
+++ apps/fabric-website/src/components/Nav/Nav.tsx
@@ -1,25 +1,14 @@
 import * as React from 'react';
 import { css } from 'office-ui-fabric-react/lib/Utilities';
 import { FocusZone } from 'office-ui-fabric-react/lib/FocusZone';
-import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 import { getPathMinusLastHash } from '../../utilities/pageroute';
 import * as stylesImport from './Nav.module.scss';
 const styles: any = stylesImport;
 import { INavProps, INavPage } from './Nav.types';
 
-export interface INavState {
-  searchQuery: string;
-}
+export interface INavState {}
 
 export class Nav extends React.Component<INavProps, INavState> {
-  constructor(props: INavProps) {
-    super(props);
-
-    this.state = {
-      searchQuery: ''
-    };
-  }
-
   public render(): JSX.Element {
     let { pages } = this.props;
 
@@ -39,80 +28,37 @@ export class Nav extends React.Component<INavProps, INavState> {
   }
 
   private _renderLink(page: INavPage, linkIndex: number): React.ReactElement<{}> {
+    const childLinks = page.pages ? this._renderLinkList(page.pages, true) : null;
     const ariaLabel = page.pages ? 'Hit enter to open sub menu, tab to access sub menu items.' : '';
     const title = page.title === 'Fabric' ? 'Home page' : page.title;
-    const childLinks = page.pages ? this._renderLinkList(page.pages, true) : null;
 
     return (
-      <span>
-        {this._getSearchBox(title)}
-        <li
-          className={css(
-            styles.link,
-            _isPageActive(page) ? styles.isActive : '',
-            _hasActiveChild(page) ? styles.hasActiveChild : '',
-            page.isHomePage ? styles.isHomePage : '',
-            page.className ? styles[page.className] : ''
-          )}
-          key={linkIndex}
-        >
-          {page.isUhfLink && location.hostname !== 'localhost' ? (
-            ''
-          ) : (
-            <a href={page.url} onClick={this.props.onLinkClick} title={title} aria-label={ariaLabel}>
-              {page.title}
-            </a>
-          )}
+      <li
+        className={css(
+          styles.link,
+          _isPageActive(page) ? styles.isActive : '',
+          _hasActiveChild(page) ? styles.hasActiveChild : '',
+          page.isHomePage ? styles.isHomePage : '',
+          page.className ? styles[page.className] : ''
+        )}
+        key={linkIndex}
+      >
+        {page.isUhfLink && location.hostname !== 'localhost' ? (
+          ''
+        ) : (
+          <a href={page.url} onClick={this.props.onLinkClick} title={title} aria-label={ariaLabel}>
+            {page.title}
+          </a>
+        )}
 
-          {childLinks}
-        </li>
-      </span>
+        {childLinks}
+      </li>
     );
   }
 
-  private _getSearchBox(val) {
-    if (val === 'Components') {
-      return (
-        <div style={{ display: 'flex' }}>
-          <SearchBox
-            placeholder="Search"
-            underlined={true}
-            styles={{
-              iconContainer: {
-                color: 'white'
-              },
-              icon: {
-                color: 'white'
-              },
-              clearButton: {
-                color: 'white'
-              },
-              field: {
-                backgroundColor: 'transparent',
-                color: 'white'
-              },
-              root: {
-                marginBottom: '5px',
-                width: '180px'
-              }
-            }}
-            onChange={this._onChangeQuery.bind(this)}
-          />
-        </div>
-      );
-    }
-  }
-
   private _renderLinkList(pages: INavPage[], isSubMenu: boolean): React.ReactElement<{}> {
-    let { searchQuery } = this.state;
-
     const links: React.ReactElement<{}>[] = pages
       .filter(page => !page.hasOwnProperty('isHiddenFromMainNav'))
-      .filter(
-        page =>
-          page.title.toLowerCase().indexOf(searchQuery.toLowerCase()) > -1 ||
-          page.url.toLowerCase().indexOf('#/components/')
-      )
       .map((page: INavPage, linkIndex: number) => this._renderLink(page, linkIndex));
 
     return (
@@ -120,12 +66,6 @@ export class Nav extends React.Component<INavProps, INavState> {
         {links}
       </ul>
     );
-  }
-
-  private _onChangeQuery(newValue): void {
-    this.setState({
-      searchQuery: newValue
-    });
   }
 }
 
