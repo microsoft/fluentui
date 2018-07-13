5457ababcf42b9de178b6eb077d9b82637d14948
Claudia Sun <sun515@purdue.edu>
2018-07-13 14:56:20 -0700

    Add files via upload

---
+++ scripts/tasks/size-audit.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.generateSizeData = function() {
+  const projectName = path.basename(process.cwd());
+  const sizeFilePath = path.join(process.cwd(), 'dist', `${projectName}.json`);
+  if (fs.existsSync(path.dirname(sizeFilePath))) {
+    const result = {
+      chunks: analyzeChunks()
+    };
+    const fileContents = JSON.stringify(result);
+    fs.writeFileSync(sizeFilePath, fileContents);
+  }
+};
+
+function analyzeChunks() {
+  const distPath = path.join(process.cwd(), 'dist');
+  const result = {};
+  for (const filePath of fs.readdirSync(distPath)) {
+    const fileName = path.basename(filePath);
+    const extension = path.extname(fileName);
+    if (extension.toLowerCase() === '.js') {
+      result[fileName] = getFileSize(path.join(distPath, fileName));
+    }
+    return result;
+  }
+}
+
+function getFileSize(filePath) {
+  try {
+    const stats = fs.statSync(filePath);
+    return stats.size;
+  } catch (e) {
+    console.log(`Unable to get size of file "${filePath}"`);
+    return -1;
+  }
+}
