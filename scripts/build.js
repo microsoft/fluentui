38ffa022daa32f7b1b1d9e86f5c8fb7f428f2917
Claudia Sun <sun515@purdue.edu>
2018-07-13 14:55:16 -0700

    Add condition to run generating artifacts

--- scripts/build.js
+++ scripts/build.js
@@ -56,7 +56,12 @@ executeTasks(firstTasks).then(() => {
     process.exitCode = 1;
   }
   logEndBuild(packageName, !hasFailures, buildStartTime);
-}).then(() => executeTask());
+}).then(() => {
+  if (process.env['APPVEYOR']) {
+    const { generateSizeData } = require('./tasks/size-audit');
+    generateSizeData();
+  }
+});
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //// Build helper functions
@@ -167,46 +172,4 @@ function getDisabledTasks(process, defaultDisabled = []) {
   }
 
   return defaultDisabled;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//// Artifact helper functions
-////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
-   * Gets the size of the "default" locale bundle file
-   */
-function executeTask() {
-  const projectName = path.basename(process.cwd());
-  const sizeFilePath = path.join(process.cwd(), 'dist', `${projectName}.json`);
-  if (fs.existsSync(path.dirname(sizeFilePath))) {
-    const result = {
-      chunks: analyzeChunks(),
-    };
-    const fileContents = JSON.stringify(result);
-    fs.writeFileSync(sizeFilePath, fileContents);
-  }
-}
-
-function analyzeChunks() {
-  const distPath = path.join(process.cwd(), 'dist');
-  const result = {};
-  for (const filePath of fs.readdirSync(distPath)) {
-    const fileName = path.basename(filePath);
-    const extension = path.extname(fileName);
-    if (extension.toLowerCase() === '.js') {
-      result[fileName] = getFileSize(path.join(distPath, fileName));
-    }
-    return result;
-  }
-}
-
-function getFileSize(filePath) {
-  try {
-    const stats = fs.statSync(filePath);
-    return stats.size;
-  }
-  catch (e) {
-    console.log(`Unable to get size of file "${filePath}"`);
-    return -1;
-  }
 }
