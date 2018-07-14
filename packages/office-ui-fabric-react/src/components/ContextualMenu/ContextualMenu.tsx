98bebeaf8d7f8368736b06a6c03aba5b3c181c8a
REDMOND\chiechan <chiechan@microsoft.com>
2018-07-13 17:45:58 -0700

    resolved npm run style problem

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.tsx
@@ -920,11 +920,11 @@ export class ContextualMenu extends BaseComponent<IContextualMenuProps, IContext
     } else {
       if (item.key !== this.state.expandedMenuItemKey) {
         let expandedByMouseClick = false; // keyboard event
-        if (ev.type === "click") {
+        if (ev.type === 'click') {
           // When Edge + Narrator are used together (regardless of if the button is in a form or not), pressing
           // "Enter" fires this method and not _onMenuKeyDown. Checking ev.nativeEvent.screenX and ev.
           // nativeEvent.screenY differentiates between a real click event and a keypress event.
-          let clickEv = ev as React.MouseEvent<any>;
+          const clickEv = ev as React.MouseEvent<any>;
           expandedByMouseClick = clickEv.nativeEvent.screenX !== 0 && clickEv.nativeEvent.screenY !== 0;
         }
         // This has a collapsed sub menu. Expand it.
