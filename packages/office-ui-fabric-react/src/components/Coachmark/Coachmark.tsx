e27e76d0fa29c7fe530666719f23d7aef296948e
Eddie Liu <leddie24@gmail.com>
2018-07-20 16:30:45 -0700

    Coachmark: Add key prop to array in render (#5506)
    * Add key prop to array in render
    
    * Add change file

--- packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.tsx
+++ packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.tsx
@@ -240,12 +240,12 @@ export class Coachmark extends BaseComponent<ICoachmarkProps, ICoachmarkState> {
                   >
                     {isCollapsed && [
                       ariaLabelledBy && (
-                        <p id={ariaLabelledBy} className={classNames.ariaContainer}>
+                        <p id={ariaLabelledBy} key={0} className={classNames.ariaContainer}>
                           {ariaLabelledByText}
                         </p>
                       ),
                       ariaDescribedBy && (
-                        <p id={ariaDescribedBy} className={classNames.ariaContainer}>
+                        <p id={ariaDescribedBy} key={1} className={classNames.ariaContainer}>
                           {ariaDescribedByText}
                         </p>
                       )
