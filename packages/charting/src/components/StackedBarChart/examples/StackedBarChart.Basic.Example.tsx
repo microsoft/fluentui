2c5084e79109ba2f43cc966c6d6ba9cd508f897e
Rajesh Goriga <gorigarajesh@gmail.com>
2018-07-22 00:52:02 +0530

    Adding Stacked Bar  chart to charting package (#5535)
    * Adding Stacked Bar chart
    
    * Adding change file
    
    * create data interface for data object
    
    * remove yAxisTickCount from types
    
    * replce IClassNames to IProcessedStyleSet

---
+++ packages/charting/src/components/StackedBarChart/examples/StackedBarChart.Basic.Example.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { customizable } from '../../../Utilities';
+import { StackedBarChart, IStackedBarChartProps } from '../../StackedBarChart';
+
+@customizable('StackedBarChartBasicExample', ['theme', 'styles'])
+export class StackedBarChartBasicExample extends React.Component<IStackedBarChartProps, {}> {
+  public render(): JSX.Element {
+    return <div>{this._basicExample()}</div>;
+  }
+
+  private _basicExample(): JSX.Element {
+    const points = [
+      { label: 'first', value: 40 },
+      { label: 'second', value: 23 },
+      { label: 'third', value: 35 },
+      { label: 'fourth', value: 87 }
+    ];
+
+    return <StackedBarChart data={points} chartTitle={'Stacked Bar chart'} />;
+  }
+}
