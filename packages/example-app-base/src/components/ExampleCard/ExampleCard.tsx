2e979a5ace4b5e77f8ba76f3529df6a4e373fa4a
Avital Baral <avitalbaral99@gmail.com>
2018-07-23 15:34:13 -0700

    Label 'Export to Codepen' button (#5542)
    * export to codepen button for label
    
    * label codepen example file
    
    * rush change files
    
    * changed codepen title
    
    * moved boilerplate codepen code into ExampleCard, added documentation
    
    * added some more props documentation in ExampleCard
    
    * abstracted codepen functionality into codepen component, simplified examplecard
    
    * small example card code cleanup
    
    * Update label_export_to_codepen_button_2018-07-12-17-51.json

--- packages/example-app-base/src/components/ExampleCard/ExampleCard.tsx
+++ packages/example-app-base/src/components/ExampleCard/ExampleCard.tsx
@@ -3,16 +3,27 @@ import { css } from 'office-ui-fabric-react/lib/Utilities';
 import './ExampleCard.scss';
 import { CommandButton } from 'office-ui-fabric-react/lib/Button';
 import { Highlight } from '../Highlight/Highlight';
+import { CodepenComponent } from '../CodepenComponent/CodepenComponent';
 
 export interface IExampleCardProps {
+  /* Example Title */
   title: string;
+  /* Experimental Component? */
   isOptIn?: boolean;
+  /* Example Code as a string */
   code?: string;
+  /* Children of the Example */
   children?: React.ReactNode;
+  /* Example is Right-Aligned ? */
   isRightAligned?: boolean;
+  /* Example dos */
   dos?: JSX.Element;
+  /* Example don'ts */
   donts?: JSX.Element;
+  /* Example is scrollable ? */
   isScrollable?: boolean;
+  /* JS string for Codepen portion of Example */
+  codepenJS?: string;
 }
 
 export interface IExampleCardState {
@@ -31,7 +42,8 @@ export class ExampleCard extends React.Component<IExampleCardProps, IExampleCard
   }
 
   public render(): JSX.Element {
-    const { title, code, children, isRightAligned = false, isScrollable = true } = this.props;
+    const { title, code, children, isRightAligned = false, isScrollable = true, codepenJS } = this.props;
+
     const { isCodeVisible } = this.state;
     let rootClass = 'ExampleCard' + (this.state.isCodeVisible ? ' is-codeVisible' : '');
 
@@ -40,7 +52,8 @@ export class ExampleCard extends React.Component<IExampleCardProps, IExampleCard
         <div className="ExampleCard-header">
           <span className="ExampleCard-title ms-font-l">{title}</span>
           <div className="ExampleCard-toggleButtons ms-font-l">
-            {code ? (
+            {codepenJS && <CodepenComponent jsContent={codepenJS} />}
+            {code && (
               <CommandButton
                 iconProps={{ iconName: 'Embed' }}
                 onClick={this._onToggleCodeClick}
@@ -48,7 +61,7 @@ export class ExampleCard extends React.Component<IExampleCardProps, IExampleCard
               >
                 {isCodeVisible ? 'Hide code' : 'Show code'}
               </CommandButton>
-            ) : null}
+            )}
           </div>
         </div>
 
