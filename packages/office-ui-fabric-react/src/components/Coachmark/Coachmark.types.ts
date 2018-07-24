6536c5a866cdec883628c9ccdf75b93033137bfa
Eddie Liu <leddie24@gmail.com>
2018-07-23 17:49:11 -0700

    Cherry pick Coachmark accessibility and positioning fixes into 5.0 (#5656)
    * Cherry pick Coachmark: Positioning fixes (#4995) e87289bebda6cf31f7ddbc38888daec68de168c9
    
    * Cherry pick TeachingBubble/Coachmark: Fix TeachingBubble content wrapping to next line unnecessarily (#5121) 72bab3c4bd2deb739b3c98c7be0bd8ae75c2d313
    
    * Cherrry pick Fix import paths to use relative paths in product code (#5338) e2076e13cf2f5dd47f8c7277f2d3e91de6bde90e
    
    * Cherry pick Coachmark accessibility #5155 7a36bb911c84c170bff0630191bf378a391e1cb3
    
    * Revert formatting back to 5.0 in Coachmark.tsx
    
    * Fix unnecessary ; and change " to '
    
    * Fix merge conflicts for Coachmark.Basic.Example.tsx
    
    * " to '
    
    * Fix imports for TeachingBubble.tsx
    
    * Update TeachinBubbleContent, update TeachingBubble snapshot
    
    * Update snapshot.  Fix TeachingBubble.types
    
    * Fix import for RefObject
    
    * Fix some inadvertent formatting from prettier.

--- packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.types.ts
+++ packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.types.ts
@@ -3,35 +3,58 @@ import { Coachmark } from './Coachmark';
 import { ICoachmarkStyles, ICoachmarkStyleProps } from './Coachmark.styles';
 import { IPositioningContainerTypes } from './PositioningContainer/PositioningContainer.types';
 import { IStyleFunction } from '../../Utilities';
+import { ITeachingBubble } from '../../TeachingBubble';
 
 export interface ICoachmark {
 }
 
 export interface ICoachmarkTypes extends React.Props<Coachmark> {
+  /**
+   * Optional callback to access the ICoachmark interface. Use this instead of ref for accessing
+   * the public methods and properties of the component.
+   */
   componentRef?: (component: ICoachmark | null) => void;
 
   /**
-   * Get styles method.
+   * Call to provide customized styling that will layer on top of the variant rules
    */
   getStyles?: IStyleFunction<ICoachmarkStyleProps, ICoachmarkStyles>;
 
   /**
-   * The target that the TeachingBubble should try to position itself based on.
+   * The target that the Coachmark should try to position itself based on.
    */
-  target: HTMLElement | null;
+  target: HTMLElement | string | null;
 
+  /**
+   * Props to pass to the PositioningContainer component.  Specific the `directionalHint` to indicate which edge the
+   * Coachmark/TeachingBubble should live.
+   * @default directionalHint: DirectionalHint.bottomAutoEdge
+   */
   positioningContainerProps?: IPositioningContainerTypes;
 
   /**
-   * The starting collapsed state for the Coachmark?
+   * Whether or not to force the Coachmark/TeachingBubble content to fit within the window bounds.
    * @default true
+   */
+  isPositionForced?: boolean;
+
+  /**
+   * The starting collapsed state for the Coachmark.  Use isCollapsed instead.
+   * @default true
+   * @deprecated
    */
   collapsed?: boolean;
 
   /**
+   * The starting collapsed state for the Coachmark.
+   * @default true
+   */
+  isCollapsed?: boolean;
+
+  /**
    * The distance in pixels the mouse is located
-   * before opening up the coachmark.
-   * @default 100
+   * before opening up the Coachmark.
+   * @default 10
    */
   mouseProximityOffset?: number;
 
@@ -46,48 +69,82 @@ export interface ICoachmarkTypes extends React.Props<Coachmark> {
   onAnimationOpenEnd?: () => void;
 
   /**
-   * The width of the beak component.
+   * The width of the Beak component.
+   * @deprecated
    */
   beakWidth?: number;
 
   /**
-   * The height of the beak component
+   * The height of the Beak component.
+   * @deprecated
    */
   beakHeight?: number;
 
   /**
-   * Delay before allowing mouse movements to open
-   * the Coachmark
+   * Delay before allowing mouse movements to open the Coachmark.
+   * @default 3600
    */
   delayBeforeMouseOpen?: number;
 
   /**
-   * Runs every time the mouse moves
+   * Callback to run when the mouse moves.
    */
   onMouseMove?: (e: MouseEvent) => void;
 
   /**
-   * The width of the coachmark
+   * The width of the Coachmark.
+   * @deprecated
    */
   width?: number;
 
   /**
-   * The height of the coachmark
+   * The height of the Coachmark.
+   * @deprecated
    */
   height?: number;
 
   /**
-   * Color
+   * Color of the Coachmark/TeachingBubble.
    */
   color?: string;
 
   /**
-   * Beacon color one
+   * Beacon color one.
    */
   beaconColorOne?: string;
 
   /**
-   * Beacon color two
+   * Beacon color two.
    */
   beaconColorTwo?: string;
+
+  /**
+   * Text to announce to screen reader / narrator when Coachmark is displayed
+   */
+  ariaAlertText?: string;
+
+  /**
+   * Ref for TeachingBubble
+   */
+  teachingBubbleRef?: ITeachingBubble;
+
+  /**
+   *  Defines the element id referencing the element containing label text for Coachmark.
+   */
+  ariaLabelledBy?: string;
+
+  /**
+   * Defines the element id referencing the element containing the description for the Coachmark.
+   */
+  ariaDescribedBy?: string;
+
+  /**
+   *  Defines the text content for the ariaLabelledBy element
+   */
+  ariaLabelledByText?: string;
+
+  /**
+   * Defines the text content for the ariaDescribedBy element
+   */
+  ariaDescribedByText?: string;
 }
