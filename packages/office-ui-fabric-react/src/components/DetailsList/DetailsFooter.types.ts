5ff25d3a2b3ca833a751d22d0f6933ee04ca9266
Harshada Trivedi <hatrived@microsoft.com>
2018-07-30 16:57:31 +0530

    removing comments

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsFooter.types.ts
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsFooter.types.ts
@@ -1,14 +1,6 @@
 import { IColumn } from './DetailsList.types';
 import { ISelection } from '../../utilities/selection/index';
-// export interface IDetailsFooter {
-//   focus: () => boolean;
-// }
-
 export interface IDetailsFooterProps {
-  // componentRef?: (component: IDetailsFooter | null) => void;
-  /**
-   * Custom classname
-   */
   columns?: IColumn[];
   groupNestingDepth?: number;
   selection?: ISelection;
