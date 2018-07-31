16503800478eeba40faa029292478dcac574625d
Kartik Nuli (Zen3 Infosolutions America Inc) <v-kanuli@microsoft.com>
2018-07-31 10:14:11 -0700

    changing imports for example page

--- packages/dashboard/src/components/MultiCountChart/examples/MultiCountChart.Example.tsx
+++ packages/dashboard/src/components/MultiCountChart/examples/MultiCountChart.Example.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { AnnotationType, MultiCountChart } from '../index';
+import { AnnotationType, MultiCountChart } from '@uifabric/dashboard';
 
 export class MultiCountChartExample extends React.Component<{}, {}> {
   public render(): JSX.Element {
