e602510ad7d8d86efb352bddbe5060af1e9c4243
David Zearing <dzearing@microsoft.com>
2018-07-31 19:32:52 -0700

    Check: now reflects theme changes. (#5750)
    * Fixing check.
    
    * Adding change file.

--- packages/office-ui-fabric-react/src/components/Check/Check.base.tsx
+++ packages/office-ui-fabric-react/src/components/Check/Check.base.tsx
@@ -14,7 +14,11 @@ export class CheckBase extends BaseComponent<ICheckProps, {}> {
   };
 
   public shouldComponentUpdate(newProps: ICheckProps): boolean {
-    return this.props.checked !== newProps.checked;
+    return (
+      this.props.checked !== newProps.checked ||
+      this.props.theme !== newProps.theme ||
+      this.props.className !== newProps.className
+    );
   }
 
   public render(): JSX.Element {
