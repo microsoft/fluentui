0d01322e871784bb86c68f52ecd2ea8a768cd93e
Jessica Clark <v-jescla@microsoft.com>
2018-08-01 15:58:38 -0700

    adds JSDoc comments on enums

--- packages/variants/src/variants.ts
+++ packages/variants/src/variants.ts
@@ -41,7 +41,7 @@ export function getVariant(theme: ITheme, variant: VariantThemeType): ITheme {
  * A variant where the background is a soft shade of the neutral color. Most other colors remain unchanged.
  *
  * @export
- * @param {IPartialTheme} theme the theme for which to build a variant for
+ * @param {IPartialTheme} theme the theme to build a variant for
  * @returns {ITheme} the variant theme
  */
 export function getNeutralVariant(theme: IPartialTheme): ITheme {
@@ -93,7 +93,7 @@ export function getNeutralVariant(theme: IPartialTheme): ITheme {
  * A variant where the background is a soft version of the primary color. Most other colors remain unchanged.
  *
  * @export
- * @param {IPartialTheme} theme the theme for which to build a variant for
+ * @param {IPartialTheme} theme the theme to build a variant for
  * @returns {ITheme} the variant theme
  */
 export function getSoftVariant(theme: IPartialTheme): ITheme {
@@ -157,7 +157,7 @@ export function getSoftVariant(theme: IPartialTheme): ITheme {
  * The primary color becomes shades of the background.
  *
  * @export
- * @param {IPartialTheme} theme the theme for which to build a variant for
+ * @param {IPartialTheme} theme the theme to build a variant for
  * @returns {ITheme} the variant theme
  */
 export function getStrongVariant(theme: IPartialTheme): ITheme {
