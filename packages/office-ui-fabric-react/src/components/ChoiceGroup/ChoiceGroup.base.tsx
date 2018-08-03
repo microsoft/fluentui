77cbc1034b40d51470a0aaeaf065c9e969c71f82
Mark Polak <mark@thedutchies.com>
2018-08-03 10:23:20 +0200

    Use find from utilities

--- packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.base.tsx
+++ packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.base.tsx
@@ -7,7 +7,7 @@ import {
   OnChangeCallback
 } from './ChoiceGroupOption/index';
 import { IChoiceGroupOption, IChoiceGroupProps, IChoiceGroupStyleProps, IChoiceGroupStyles } from './ChoiceGroup.types';
-import { BaseComponent, classNamesFunction, createRef, getId } from '../../Utilities';
+import { BaseComponent, classNamesFunction, createRef, getId, find } from '../../Utilities';
 
 const getClassNames = classNamesFunction<IChoiceGroupStyleProps, IChoiceGroupStyles>();
 
@@ -134,7 +134,7 @@ export class ChoiceGroupBase extends BaseComponent<IChoiceGroupProps, IChoiceGro
     this.changedVars[key]
       ? this.changedVars[key]
       : (this.changedVars[key] = (evt, option: IChoiceGroupOption) => {
-          const { onChanged, onChange, selectedKey, options } = this.props;
+          const { onChanged, onChange, selectedKey, options = [] } = this.props;
 
           // Only manage state in uncontrolled scenarios.
           if (selectedKey === undefined) {
@@ -143,7 +143,7 @@ export class ChoiceGroupBase extends BaseComponent<IChoiceGroupProps, IChoiceGro
             });
           }
 
-          const originalOption = options!.find((value: IChoiceGroupOption) => value.key === key);
+          const originalOption = find(options, (value: IChoiceGroupOption) => value.key === key);
 
           // TODO: onChanged deprecated, remove else if after 07/17/2017 when onChanged has been removed.
           if (onChange) {
@@ -158,7 +158,9 @@ export class ChoiceGroupBase extends BaseComponent<IChoiceGroupProps, IChoiceGro
       return props.selectedKey;
     }
 
-    const optionsChecked = props.options!.filter((option: IChoiceGroupOption) => {
+    const { options = [] } = props;
+
+    const optionsChecked = options.filter((option: IChoiceGroupOption) => {
       return option.checked;
     });
 
