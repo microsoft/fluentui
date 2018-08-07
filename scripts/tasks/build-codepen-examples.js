ed4976e30d5c11bf0e4e15dcddc6e3a4e4b0b8e1
Avital Baral <t-avba@microsoft.com>
2018-08-07 10:04:23 -0700

    code cleanup

--- scripts/tasks/build-codepen-examples.js
+++ scripts/tasks/build-codepen-examples.js
@@ -5,23 +5,23 @@ module.exports = function(options) {
   const files = glob.sync(path.resolve(process.cwd(), 'src/components/**/examples/*Example*.tsx'));
   const jscodeshift = require('jscodeshift');
   const fs = require('fs');
-  const async = require('async');
 
   // Return a promise.
   return processFiles();
   function processFiles() {
     const promises = [];
     if (files.length) {
-      async.eachLimit(files, 5, function(file, callback) {
+      files.forEach(file => {
         const fileSource = fs.readFileSync(file).toString();
+
         promises.push(
           new Promise((resolve, reject) => {
             // check if the @codepen tag is present
             if (fileSource.indexOf('@codepen') >= 0) {
+              console.log('FILESOURCE', fileSource);
               const exampleName = path.basename(file, '.tsx');
               // extract the name of the component (relies on component/examples/examplefile.tsx structure)
               const exampleComponentName = file.split('/').reverse()[2];
-              // parameters for transform file
               const fileInfo = { path: file, source: fileSource };
               const api = { jscodeshift: jscodeshift.withParser('babylon'), stats: {} };
               const transformResult = transformer(fileInfo, api);
