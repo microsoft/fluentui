2966d34694931cb5cc965e670bc000fcae07e4f3
Natalie Ethell <naethell@microsoft.com>
2018-08-07 11:11:50 -0700

    Panel: When isHiddenOnDismiss and closed, forceFocusInsideTrap set to false (#5805)
    * don't force focus in trap if isHiddenOnDismiss is set
    
    * change file
    
    * only set forceFocusInsideTrap to false if isHiddenOnDismiss and !isOpen
    
    * add extra comment to isHiddenOnDismiss prop
    
    * tests for screener

--- apps/vr-tests/src/stories/Panel.stories.tsx
+++ apps/vr-tests/src/stories/Panel.stories.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
 import { FabricDecorator } from '../utilities';
-import { Panel, PanelType } from 'office-ui-fabric-react';
+import { Panel, PanelType, SearchBox } from 'office-ui-fabric-react';
 
 Panel.defaultProps = {
   isOpen: true,
@@ -12,58 +12,35 @@ Panel.defaultProps = {
 
 storiesOf('Panel', module)
   .addDecorator(FabricDecorator)
+  .addDecorator(story => <Screener steps={new Screener.Steps().snapshot('default').end()}>{story()}</Screener>)
+  .add('Small left w/ close button', () => <Panel hasCloseButton type={PanelType.smallFixedNear} headerText="Small" />)
+  .add('Small fixed right w/ close button', () => (
+    <Panel hasCloseButton type={PanelType.smallFixedFar} headerText="Small fixed" />
+  ))
+  .add('Small fluid right', () => <Panel type={PanelType.smallFluid} headerText="Small fluid" />)
+  .add('Medium right', () => <Panel type={PanelType.medium} headerText="Medium" />)
+  .add('Large right', () => <Panel type={PanelType.large} headerText="Large" />)
+  .add('Large fixed right', () => <Panel type={PanelType.largeFixed} headerText="Large fixed" />)
+  .add('Extra large right', () => <Panel type={PanelType.extraLarge} headerText="Extra Large" />);
+
+storiesOf('Panel', module)
+  .addDecorator(FabricDecorator)
   .addDecorator(story => (
     <Screener
-      steps={ new Screener.Steps()
+      steps={new Screener.Steps()
         .snapshot('default')
-        .end()
-      }
+        .click('.ms-SearchBox-field')
+        .snapshot('click')
+        .end()}
     >
-      { story() }
+      {story()}
     </Screener>
   ))
-  .add('Small left w/ close button', () => (
-    <Panel
-      hasCloseButton
-      type={ PanelType.smallFixedNear }
-      headerText='Small'
-    />
-  ))
-  .add('Small fixed right w/ close button', () => (
-    <Panel
-      hasCloseButton
-      type={ PanelType.smallFixedFar }
-      headerText='Small fixed'
-    />
-  ))
-  .add('Small fluid right', () => (
-    <Panel
-      type={ PanelType.smallFluid }
-      headerText='Small fluid'
-    />
-  ))
-  .add('Medium right', () => (
-    <Panel
-      type={ PanelType.medium }
-      headerText='Medium'
-    />
-  ))
-  .add('Large right', () => (
-    <Panel
-      type={ PanelType.large }
-      headerText='Large'
-    />
-  ))
-  .add('Large fixed right', () => (
-    <Panel
-      type={ PanelType.largeFixed }
-      headerText='Large fixed'
-    />
-  ))
-  .add('Extra large right', () => (
-    <Panel
-      type={ PanelType.extraLarge }
-      headerText='Extra Large'
-    />
-  ))
-  ;
+  .add('SearchBox and Right Panel', () => (
+    <div>
+      <SearchBox placeholder="Search" />
+      <Panel isOpen={false} type={PanelType.medium} headerClassName="" headerText={'Header'} isHiddenOnDismiss>
+        {null}
+      </Panel>
+    </div>
+  ));
