f6eb5ee1318dad992c68e5c3c72c3a05fffe4d52
Avital Baral <t-avba@microsoft.com>
2018-08-08 16:07:51 -0700

    rate-limiting on transforms

--- scripts/tasks/build-codepen-examples.js
+++ scripts/tasks/build-codepen-examples.js
@@ -5,35 +5,48 @@ module.exports = function(options) {
   const files = glob.sync(path.resolve(process.cwd(), 'src/components/**/examples/*Example*.tsx'));
   const jscodeshift = require('jscodeshift');
   const fs = require('fs');
+  const async = require('async');
 
-  // Return a promise.
   return processFiles();
+
   function processFiles() {
     const promises = [];
     if (files.length) {
-      files.forEach(file => {
-        const fileSource = fs.readFileSync(file).toString();
-
-        promises.push(
-          new Promise((resolve, reject) => {
-            // check if the @codepen tag is present
-            if (fileSource.indexOf('@codepen') >= 0) {
-              const exampleName = path.basename(file, '.tsx');
-              // extract the name of the component (relies on component/examples/examplefile.tsx structure)
-              const exampleComponentName = file.split('/').reverse()[2];
-              const fileInfo = { path: file, source: fileSource };
-              const api = { jscodeshift: jscodeshift.withParser('babylon'), stats: {} };
-              const transformResult = transformer(fileInfo, api);
-              fs.writeFileSync(
-                'lib/components/' + exampleComponentName + '/' + exampleName + '.Codepen.txt',
-                transformResult
-              );
-              resolve();
+      promises.push(
+        new Promise((resolve, reject) => {
+          async.eachLimit(
+            files,
+            5,
+            function(file, callback) {
+              const fileSource = fs.readFileSync(file).toString();
+              if (fileSource.indexOf('@codepen') >= 0) {
+                const exampleName = path.basename(file, '.tsx');
+                // extract the name of the component (relies on component/examples/examplefile.tsx structure)
+                const exampleComponentName = file.split('/').reverse()[2];
+                const fileInfo = { path: file, source: fileSource };
+                const api = { jscodeshift: jscodeshift.withParser('babylon'), stats: {} };
+                const transformResult = transformer(fileInfo, api);
+                fs.writeFileSync(
+                  'lib/components/' + exampleComponentName + '/' + exampleName + '.Codepen.txt',
+                  transformResult
+                );
+                callback();
+              } else {
+                callback();
+              }
+            },
+            function(err) {
+              if (err) {
+                reject();
+              } else {
+                resolve();
+              }
             }
-          })
-        );
-      });
+          );
+        })
+      );
     }
+
     return Promise.all(promises);
   }
 };
