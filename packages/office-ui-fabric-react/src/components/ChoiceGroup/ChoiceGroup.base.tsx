631b8e40616edd10409dcebcb2f5211f1d5c1a81
Jason Gore <jagore@microsoft.com>
2018-08-08 09:47:50 -0700

    Remove unused import.

--- packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.base.tsx
+++ packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.base.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { Label } from '../../Label';
-import {
-  IChoiceGroupOptionProps,
-  ChoiceGroupOption,
-  OnFocusCallback,
-  OnChangeCallback
-} from './ChoiceGroupOption/index';
+import { ChoiceGroupOption, OnFocusCallback, OnChangeCallback } from './ChoiceGroupOption/index';
 import { IChoiceGroupOption, IChoiceGroupProps, IChoiceGroupStyleProps, IChoiceGroupStyles } from './ChoiceGroup.types';
 import { BaseComponent, classNamesFunction, createRef, getId, find } from '../../Utilities';
 
