1f9da81391bb45dd3ece215039fb6678d8765441
Swapnil Vaibhav <svaibhav@microsoft.com>
2018-08-08 16:37:52 +0530

    Adding test cases to handle cases where onRender callbacks can be used to wrap the default render functionality

--- packages/office-ui-fabric-react/src/components/Persona/PersonaCoin/PersonaCoin.test.tsx
+++ packages/office-ui-fabric-react/src/components/Persona/PersonaCoin/PersonaCoin.test.tsx
@@ -1,11 +1,31 @@
 /* tslint:disable-next-line:no-unused-variable */
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import { setRTL } from '../../../Utilities';
+import { setRTL, IRenderFunction } from '../../../Utilities';
 import { PersonaCoin } from './PersonaCoin';
+import { IPersonaSharedProps, IPersonaProps } from '../../../index';
 
 const testImage1x1 =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQImWP4DwQACfsD/eNV8pwAAAAASUVORK5CYII=';
+
+const coinProp = {
+  text: 'Swapnil Vaibhav',
+  secondaryText: 'Software Engineer',
+  tertiaryText: 'In a meeting',
+  optionalText: 'Available at 4:00pm',
+  size: 15
+};
+
+/**
+ * function to override the default onRender callbacks
+ */
+const wrapPersona = (
+  example: IPersonaSharedProps
+): ((coinProps: IPersonaProps, defaultRenderer: IRenderFunction<IPersonaProps>) => JSX.Element | null) => {
+  return (coinProps, defaultCoinRenderer): JSX.Element | null => {
+    return defaultCoinRenderer(coinProps);
+  };
+};
 
 describe('PersonaCoin', () => {
   beforeEach(() => {
@@ -47,6 +67,14 @@ describe('PersonaCoin', () => {
   it('does not render the initials when showInitialsUntilImageLoads is false', () => {
     const component = renderer.create(
       <PersonaCoin text="Kat Larrson" imageUrl={testImage1x1} showInitialsUntilImageLoads={false} />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with onRender callback', () => {
+    const component = renderer.create(
+      <PersonaCoin {...coinProp} onRenderCoin={wrapPersona(coinProp)} onRenderInitials={wrapPersona(coinProp)} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
