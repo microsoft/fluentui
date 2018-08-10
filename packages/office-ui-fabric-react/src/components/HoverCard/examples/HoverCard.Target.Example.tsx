d37761797f5b5d391f713b736f3e1aa07fcab555
Vitalie Braga <vitaliebraga@gmail.com>
2018-08-09 17:15:38 -0700

    HoverCard: Add more documentation and fix accessibility bug. (#5866)
    * Adds a conditional for 'data-is-focusable' for the root node.
    
    * Adds more documentation.
    
    * More documentation...
    
    * Cleanup code.
    
    * Rush change log.
    
    * Snapshot updates.

--- packages/office-ui-fabric-react/src/components/HoverCard/examples/HoverCard.Target.Example.tsx
+++ packages/office-ui-fabric-react/src/components/HoverCard/examples/HoverCard.Target.Example.tsx
@@ -79,7 +79,13 @@ export class HoverCardTargetExample extends BaseComponent<{}, IHoverCardExampleS
 
     return (
       <div>
-        <p> Hover over key of a row item to see the card </p>
+        <p>
+          Hover over the <i>key</i> cell of a row item to see the card or use the keyboard to navigate to it.
+        </p>
+        <p>
+          When using the keyboard to tab to it, open the card with the hotKey and it will automatically focus the first
+          focusable element in the card allowing further navigation inside the card.
+        </p>
         <DetailsList
           setKey="hoverSet"
           items={items!}
