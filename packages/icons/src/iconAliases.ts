25a03949d97b792d4d32e8cb6931bfc8b861e9c9
Thomas Michon <thomas@michon.com>
2018-08-21 15:40:12 -0700

    Add a way for consumers to use incremental build with Icon const enums (#5972)
    * Add '__Input' type exports alongside const enums
    
    * Update change output
    
    * Add unit tests for Icon enums and fix violation

--- packages/icons/src/iconAliases.ts
+++ packages/icons/src/iconAliases.ts
@@ -3,4 +3,6 @@ import { registerIconAlias } from '@uifabric/styling';
 export const registerIconAliases = () => {
   registerIconAlias('trash', 'delete');
   registerIconAlias('onedrive', 'onedrivelogo');
+  registerIconAlias('sixpointstar', '6pointstar');
+  registerIconAlias('twelvepointstar', '12pointstar');
 };
