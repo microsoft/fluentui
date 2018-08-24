55d59941b04c97f4e40cd4f8cb197289279185a0
Brian Garland <v-brgarl@microsoft.com>
2018-08-24 14:38:10 -0700

    Panel MergeStyles Conversion (#5823)
    Convert Panel to mergeStyles.

---
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.ts
@@ -0,0 +1,11 @@
+import { styled } from '../../Utilities';
+import { IPanelProps, IPanelStyleProps, IPanelStyles } from './Panel.types';
+import { PanelBase } from './Panel.base';
+import { getStyles } from './Panel.styles';
+
+/**
+ * Panel description
+ */
+export const Panel = styled<IPanelProps, IPanelStyleProps, IPanelStyles>(PanelBase, getStyles, undefined, {
+  scope: 'Panel'
+});
