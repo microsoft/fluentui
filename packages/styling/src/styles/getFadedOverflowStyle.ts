604fa0340646634edd2fd86790f71deaeddc236a
Vitalie Braga <v-vibr@microsoft.com>
2018-08-24 16:53:50 -0700

    Vertical overflow support added.

--- packages/styling/src/styles/getFadedOverflowStyle.ts
+++ packages/styling/src/styles/getFadedOverflowStyle.ts
@@ -1,6 +1,6 @@
 import { IRawStyle } from '@uifabric/merge-styles';
-import { getRTL } from '@uifabric/utilities';
 import { HighContrastSelector } from './CommonStyles';
+import { ITheme } from '../interfaces';
 
 export interface IRGB {
   r: number;
@@ -8,35 +8,47 @@ export interface IRGB {
   b: number;
 }
 
+const DEFAULT_WIDTH = 20;
+const DEFAULT_HEIGHT = '4em';
 const DEFAULT_COLOR = '#ffffff';
-const DEFAULT_WIDTH = '20px';
 
 /**
  * Generates a style which can be used to gracefully fade out an overflowing content by defining an :after style.
  * Requires the target to have position set to relative and textOverflow set to clip (no ellipsis).
  *
- * @param color - The background color to fade out to.
+ * @param theme - The theme object to use.
+ * @param color - The background color to fade out to. Defaults to theme.semanticColors.bodyBackground.
+ * @param direction - The direction of the overflow. Defaults to horizontal.
+ * @param width - The width of the fading overflow. Vertical direction defaults it to 100% vs 20px when horizontal.
+ * @param height - The Height of the fading overflow. Vertical direction defaults it to 4em vs 100% when horizontal.
  * @returns The style object.
  */
-export function getFadedOverflowStyle(color: string = DEFAULT_COLOR, width: string = DEFAULT_WIDTH): IRawStyle {
-  const isRTL = getRTL();
+export function getFadedOverflowStyle(
+  theme: ITheme,
+  color: string = theme.semanticColors ? theme.semanticColors.bodyBackground : DEFAULT_COLOR,
+  direction: 'horizontal' | 'vertical' = 'horizontal',
+  width: string | number = getDefaultValue('width', direction),
+  height: string | number = getDefaultValue('height', direction)
+): IRawStyle {
   const rgbColor: IRGB = hex2rgb(color);
-  const rgba = `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, 0)`; // opacity = 0
+  const rgba = `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, 0)`; // opacity = 0 for starting color of gradient.
+  const gradientDirection = direction === 'vertical' ? 'to bottom' : 'to right'; // mergeStyles take care of RTL direction.
 
   return {
-    whiteSpace: 'nowrap',
     overflow: 'hidden',
-    textOverflow: 'clip',
+    width: '100%',
+    display: 'inline-block',
+    position: 'relative',
     selectors: {
       '&:after': {
         content: '""',
         position: 'absolute',
         right: 0,
-        top: 0,
         bottom: 0,
         width: width,
+        height: height,
         pointerEvents: 'none',
-        backgroundImage: `linear-gradient(${isRTL ? 'to left' : 'to right'}, ${rgba} 0%, ${color} 100%)`,
+        backgroundImage: `linear-gradient(${gradientDirection}, ${rgba} 0%, ${color} 100%)`,
         selectors: {
           [HighContrastSelector]: {
             backgroundImage: 'none'
@@ -48,12 +60,28 @@ export function getFadedOverflowStyle(color: string = DEFAULT_COLOR, width: stri
 }
 
 /**
- * @param str - Color to be converted from hex to rgba.
+ * Helper function to convert a string hex color to an RGB object.
+ *
+ * @param color - Color to be converted from hex to rgba.
  */
-function hex2rgb(str: string): IRGB {
+function hex2rgb(color: string): IRGB {
   return {
-    r: parseInt(str.slice(1, 3), 16),
-    g: parseInt(str.slice(3, 5), 16),
-    b: parseInt(str.slice(5, 7), 16)
+    r: parseInt(color.slice(1, 3), 16),
+    g: parseInt(color.slice(3, 5), 16),
+    b: parseInt(color.slice(5, 7), 16)
   };
+}
+
+/**
+ * Helper function to get the default values for parameters of main function.
+ *
+ * @param style - Which style to get the default value for.
+ * @param direction - What direction to take into consideration.
+ */
+function getDefaultValue(style: 'width' | 'height', direction: string): number | string {
+  if (style === 'width') {
+    return direction === 'horizontal' ? DEFAULT_WIDTH : '100%';
+  } else {
+    return direction === 'vertical' ? DEFAULT_HEIGHT : '100%';
+  }
 }
