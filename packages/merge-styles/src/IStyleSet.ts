ba9d5e0c172f22820b802421069fb9867e1c7e09
Sebastian Kappen <nimelriansc2@gmail.com>
2018-08-27 08:21:30 +0200

    Merge-Styles: Remove TrimToFunction type stripping
    The TrimToFunction helper type now carries
    non-function types over, instead of stripping them
    to never.

--- packages/merge-styles/src/IStyleSet.ts
+++ packages/merge-styles/src/IStyleSet.ts
@@ -6,7 +6,7 @@ export type Diff<T extends keyof any, U extends keyof any> = ({ [P in T]: P } &
 
 export type Omit<U, K extends keyof U> = Pick<U, Diff<keyof U, K>>;
 
-export type TrimToFunction<T> = T extends (...args: any[]) => any ? T : never;
+export type TrimToFunction<T> = T extends (...args: any[]) => any ? T : () => T;
 
 /**
  * A style set is a dictionary of display areas to IStyle objects.
