f8dde792ac3eaa6a28f8bdb827b0acb8fba2bd3e
Ken <kchau@microsoft.com>
2018-08-28 19:21:52 -0700

    Codepen gen retooling to account for examples have multiple exports

--- scripts/tasks/build-codepen-examples.js
+++ scripts/tasks/build-codepen-examples.js
@@ -1,8 +1,10 @@
-module.exports = function() {
+module.exports = function(filter) {
   const path = require('path');
   const transformer = require('./codepen-examples-transform');
   const glob = require('glob');
-  const files = glob.sync(path.resolve(__dirname, '../../packages/*/src/components/**/examples/*Example*.tsx'));
+  const files = glob
+    .sync(path.resolve(__dirname, '../../packages/*/src/components/**/examples/*Example*.tsx'))
+    .filter(name => (filter ? name.indexOf(filter) > -1 : name));
   const jscodeshift = require('jscodeshift');
   const fs = require('fs');
   const async = require('async');
@@ -46,6 +48,10 @@ module.exports = function() {
 };
 
 // This is run as a CLI when we do 'npm start' - otherwise, it is simply a module to be run by build.js as a task
+//
+// You can also test out a transform by running ./scripts/build-codepen-examples.js Chedckbox
+// This will run the CLI tool in a one off basis on the examples that match a certain pattern
 if (require.main == module) {
-  module.exports();
+  const args = process.argv.slice(2);
+  module.exports(args ? args[0] : null);
 }
