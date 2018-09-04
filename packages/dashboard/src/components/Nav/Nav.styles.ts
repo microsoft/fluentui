323fae5e4ef979bcc4d24c79125d0db525768571
yuz <yuz@YUZHP440>
2018-09-04 10:52:59 -0700

    improve NAV per designer feedback

--- packages/dashboard/src/components/Nav/Nav.styles.ts
+++ packages/dashboard/src/components/Nav/Nav.styles.ts
@@ -22,6 +22,7 @@ const navFloatingWidth = 230;
 const navItemHeight = 48;
 const navChildItemHeight = 32;
 const navBackgroundColor = '#E5E5E5';
+const floatingNavBackgroundColor = 'rgba(255,255,255,1)';
 const navItemHoverColor = '#CCCCCC';
 const navGroupSeparatorItemHeight = 40;
 const navGroupSeparatorWithGroupNameHeight = 70;
@@ -29,6 +30,7 @@ const navItemWithChildBgColor = '#CCCCCC';
 const navItemSelectedColor = '#B7B7B7';
 const navItemIndentSize = 50;
 const navFloatingItemIndentSize = 20;
+const BackDropSelector = '@supports (backdrop-filter: blur(20px)) or (-webkit-backdrop-filter: blur(20px))';
 
 export const getStyles = (props: INavStyleProps): INavStyles => {
   const { isSelected, hasChildren, nestingLevel, isCollapsed, scrollTop, isChildLinkSelected, hasGroupName } = props;
@@ -115,12 +117,21 @@ export const getStyles = (props: INavStyleProps): INavStyles => {
     navFloatingRoot: [
       {
         display: 'none',
+        zIndex: 1901,
         position: 'absolute',
         marginLeft: navCollapsedWidth,
         marginTop: -navItemHeight - (!!scrollTop && scrollTop > 0 ? scrollTop : 0),
         width: navFloatingWidth,
         color: navTextColor,
+        boxShadow: '0px 1.2px 3.6px rgba(0, 0, 0, 0.18), 0px 6.4px 14.4px rgba(0, 0, 0, 0.22)',
+        backgroundColor: floatingNavBackgroundColor,
+        opacity: '0.6',
         selectors: {
+          [BackDropSelector]: {
+            webkitBackdropFilter: 'blur(20px) saturate(125%)',
+            backdropFilter: 'blur(20px) saturate(125%)',
+            backgroundColor: 'rgba(255,255,255,.6)'
+          },
           a: {
             width: '100%',
             backgroundColor: 'inherit'
@@ -132,7 +143,7 @@ export const getStyles = (props: INavStyleProps): INavStyles => {
     navFloatingItemRoot: {
       height: !!nestingLevel && nestingLevel > 0 ? navChildItemHeight : navItemHeight,
       cursor: 'pointer',
-      backgroundColor: !(nestingLevel && nestingLevel > 0) ? navItemHoverColor : navBackgroundColor,
+      backgroundColor: !(nestingLevel && nestingLevel > 0) ? navItemHoverColor : floatingNavBackgroundColor,
       paddingLeft: navFloatingItemIndentSize,
       selectors: {
         ':hover': {
