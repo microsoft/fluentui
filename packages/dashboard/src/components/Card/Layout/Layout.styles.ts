e7724d52044d2c71789d5ffc0977f847408f965e
Raghuraman Krishnamurthy <raghurk@microsoft.com>
2018-09-05 10:55:57 -0700

    Fix content area clipping in card because of overflow set to hidden (#6233)
    * Fix Content Area clipping
    
    * Create Change list

--- packages/dashboard/src/components/Card/Layout/Layout.styles.ts
+++ packages/dashboard/src/components/Card/Layout/Layout.styles.ts
@@ -27,8 +27,7 @@ export const getStyles = (props: ILayoutProps): ILayoutStyles => {
     contentArea1: {
       margin: isMediumTall ? '0 0px 13px 0' : '0 12px 16px 0',
       flex: isMediumTall ? '0 1 auto' : 1,
-      flexDirection: 'column',
-      overflow: 'hidden'
+      flexDirection: 'column'
     },
     dataVizLastUpdatedOn: {
       fontSize: '10px',
@@ -38,8 +37,7 @@ export const getStyles = (props: ILayoutProps): ILayoutStyles => {
     },
     contentArea2: {
       margin: isMediumTall ? '13px 0px 16px 0' : '0px 0px 16px 12px',
-      flex: isMediumTall ? '0 1 auto' : 1,
-      overflow: 'hidden'
+      flex: isMediumTall ? '0 1 auto' : 1
     },
     footer: {
       width: '100%',
