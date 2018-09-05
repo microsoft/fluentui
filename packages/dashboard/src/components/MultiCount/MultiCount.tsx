aa27d6b109acb3d727563ccc1b8a14d4dfd75bdf
kartik nuli <nulikarthik@gmail.com>
2018-09-04 17:43:41 -0700

    Introducing hover behaviour and onclick for multicount component (#6171)
    * Introducing hover behaviour and onclick for multicount component
    
    * change log file
    
    * removing id to div element
    
    * updating equality check types

--- packages/dashboard/src/components/MultiCount/MultiCount.tsx
+++ packages/dashboard/src/components/MultiCount/MultiCount.tsx
@@ -5,9 +5,18 @@ import { getStyles } from './MultiCountStyles';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { classNamesFunction } from 'office-ui-fabric-react/lib/Utilities';
 
-export class MultiCount extends React.Component<IMultiCountProps, {}> {
+import { HoverCard, IExpandingCardProps, ExpandingCardMode } from 'office-ui-fabric-react/lib/HoverCard';
+
+export interface IMultiCountState {
+  hoveredText: string;
+}
+
+export class MultiCount extends React.Component<IMultiCountProps, IMultiCountState> {
   constructor(props: IMultiCountProps) {
     super(props);
+    this.state = {
+      hoveredText: ''
+    };
   }
 
   public render(): JSX.Element {
@@ -46,7 +55,11 @@ export class MultiCount extends React.Component<IMultiCountProps, {}> {
         formattedData = Math.floor(data);
       }
       const changeIconIndicator =
-        AnnotationType[row.type] === 'neutral' ? '' : AnnotationType[row.type] === 'positive' ? 'FlickDown' : 'FlickUp';
+        AnnotationType[row.type] === AnnotationType.neutral
+          ? ''
+          : AnnotationType[row.type] === AnnotationType.positive
+            ? 'FlickDown'
+            : 'FlickUp';
       const getClassNames = classNamesFunction<IMultiCountProps, IMultiCountStyles>();
       const classNames = getClassNames(
         getStyles({
@@ -55,24 +68,91 @@ export class MultiCount extends React.Component<IMultiCountProps, {}> {
           annotationTextFontSize: annotationTextFontSize,
           annotationTextColor: annotationTextColor,
           bodyTextColor: bodyTextColor,
-          bodyTextFontSize: bodyTextFontSize
+          bodyTextFontSize: bodyTextFontSize,
+          hoveredText: this.state.hoveredText,
+          currentText: row.data + row.bodyText + row.annotaionText,
+          href: row.href
         })
       );
+      const expandingCardProps: IExpandingCardProps = {
+        onRenderCompactCard: this._onRenderCompactCard,
+        renderData: row,
+        mode: ExpandingCardMode.compact,
+        styles: {
+          compactCard: {
+            width: 'auto',
+            height: 'auto'
+          }
+        }
+      };
+      const hoverKey = row.data + row.bodyText + row.annotaionText;
       formattedRows.push(
-        <div key={index} className={classNames.root}>
-          <div className={classNames.bodyText}>
-            <span className={classNames.data}>{formattedData + units[indexForUnits]}</span>
-            <span>{row.bodyText}</span>
+        <HoverCard
+          key={index}
+          expandingCardProps={expandingCardProps}
+          instantOpenOnClick={true}
+          onCardHide={this._hoverStateUpdate.bind(this, hoverKey, false)}
+          onCardVisible={this._hoverStateUpdate.bind(this, hoverKey, true)}
+        >
+          <div key={index} className={classNames.root} onClick={this._redirectToUrl.bind(this, row.href)}>
+            <div className={classNames.bodyText}>
+              <span className={classNames.data}>{formattedData + units[indexForUnits]}</span>
+              <span>{row.bodyText}</span>
+            </div>
+            <div className={classNames.annotationText}>
+              <span className={classNames.icon}>
+                <Icon iconName={changeIconIndicator} />
+              </span>
+              <span className={classNames.annotationText}>{row.annotaionText}</span>
+            </div>
           </div>
-          <div className={classNames.annotationText}>
-            <span className={classNames.icon}>
-              <Icon iconName={changeIconIndicator} />
-            </span>
-            <span className={classNames.annotationText}>{row.annotaionText}</span>
-          </div>
-        </div>
+        </HoverCard>
       );
     });
     return formattedRows;
   }
+
+  private _hoverStateUpdate = (hoverKey: string, hoverState: boolean): void => {
+    if (hoverState) {
+      this.setState({
+        hoveredText: hoverKey
+      });
+    } else {
+      this.setState({
+        hoveredText: ''
+      });
+    }
+  };
+
+  private _redirectToUrl(href: string | undefined): void {
+    href ? (window.location.href = href) : '';
+  }
+
+  private _onRenderCompactCard = (data: IMultiCountRow): JSX.Element => {
+    const changeIconIndicator =
+      AnnotationType[data.type] === AnnotationType.neutral
+        ? ''
+        : AnnotationType[data.type] === AnnotationType.positive
+          ? 'FlickDown'
+          : 'FlickUp';
+    const getClassNames = classNamesFunction<IMultiCountProps, IMultiCountStyles>();
+    const classNames = getClassNames(
+      getStyles({
+        color: data.color,
+        iconName: changeIconIndicator
+      })
+    );
+    return (
+      <div className={classNames.hoverCardRoot}>
+        <div className={classNames.hoverCardText}>
+          <div className={classNames.hoverCardBodyText}>{data.bodyText}</div>
+          <div className={classNames.icon}>
+            <Icon iconName={changeIconIndicator} />
+          </div>
+          <div className={classNames.hoverCardAnnotationText}>{data.annotaionText}</div>
+        </div>
+        <div className={classNames.hoverCardData}>{data.data.toLocaleString()}</div>
+      </div>
+    );
+  };
 }
