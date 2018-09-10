7221e1d7d620965f30f8f28bd9a9a19ec63bc6bc
Vincent Bailly <vibailly@microsoft.com>
2018-09-10 09:59:30 +0200

    fix hover state

--- packages/office-ui-fabric-react/src/components/Button/BaseButton.classNames.ts
+++ packages/office-ui-fabric-react/src/components/Button/BaseButton.classNames.ts
@@ -65,15 +65,15 @@ export const getBaseButtonClassNames = memoizeFunction(
           !checked && {
             selectors: {
               ':hover': styles.rootHovered,
-              ':hover $label': styles.labelHovered,
-              ':hover $icon': styles.iconHovered,
-              ':hover $description': styles.descriptionHovered,
-              ':hover $menuIcon': styles.menuIconHovered,
+              [`:hover ${classNames.msButtonLabel}`]: styles.labelHovered,
+              [`:hover ${classNames.msButtonIcon}`]: styles.iconHovered,
+              [`:hover ${classNames.msButtonDescription}`]: styles.descriptionHovered,
+              [`:hover ${classNames.msButtonMenuIcon}`]: styles.menuIconHovered,
               ':focus': styles.rootFocused,
-              ':active': styles.rootPressed,
-              ':active $icon': styles.iconPressed,
-              ':active $description': styles.descriptionPressed,
-              ':active $menuIcon': styles.menuIconPressed
+              [`:active ${classNames.msButtonLabel}`]: styles.rootPressed,
+              [`:active ${classNames.msButtonIcon}`]: styles.iconPressed,
+              [`:active ${classNames.msButtonDescription}`]: styles.descriptionPressed,
+              [`:active ${classNames.msButtonMenuIcon}`]: styles.menuIconPressed
             }
           },
         disabled && checked && [styles.rootCheckedDisabled],
