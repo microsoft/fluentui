8375d775bbf2527651d4da9dab33d55fabdec92f
Katherine Kjeer <kakje@microsoft.com>
2018-09-10 17:13:05 -0700

    Clean up VerticalStack

--- packages/experiments/src/components/Stack/VerticalStack/VerticalStack.styles.ts
+++ packages/experiments/src/components/Stack/VerticalStack/VerticalStack.styles.ts
@@ -4,7 +4,7 @@ import { IVerticalStackProps, IVerticalStackStyles } from './VerticalStack.types
 export const styles = (props: IThemedProps<IVerticalStackProps>): IVerticalStackStyles => {
   const { className } = props;
   return {
-    root: [className]
+    root: ['ms-VerticalStack', className]
     // TODO: this cast may be hiding some potential issues with styling and name
     //        lookups and should be removed
   } as IVerticalStackStyles;
