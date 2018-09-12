fe084d5161e09ddc8abef8215317cd487bfcbc02
Vitalie Braga <v-vibr@microsoft.com>
2018-09-12 15:56:44 -0700

    Remove unnecessary style reference and file

--- packages/office-ui-fabric-react/src/components/HoverCard/HoverCard.doc.tsx
+++ packages/office-ui-fabric-react/src/components/HoverCard/HoverCard.doc.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import { HoverCardBasicExample } from './examples/HoverCard.Basic.Example';
 
 import { IDocPageProps } from '../../common/DocPage.types';
+import { HoverCardBasicExample } from './examples/HoverCard.Basic.Example';
 import { HoverCardTargetExample } from './examples/HoverCard.Target.Example';
 import { HoverCardStatus } from './HoverCard.checklist';
-
-import './HoverCardPage.global.scss';
 
 const HoverCardBasicExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/HoverCard/examples/HoverCard.Basic.Example.tsx') as string;
 const HoverCardTargetExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/HoverCard/examples/HoverCard.Target.Example.tsx') as string;
@@ -13,8 +11,7 @@ const HoverCardTargetExampleCode = require('!raw-loader!office-ui-fabric-react/s
 export const HoverCardPageProps: IDocPageProps = {
   title: 'HoverCard',
   componentName: 'HoverCard',
-  componentUrl:
-    'https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/HoverCard',
+  componentUrl: 'https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/HoverCard',
   componentStatus: HoverCardStatus,
   examples: [
     {
