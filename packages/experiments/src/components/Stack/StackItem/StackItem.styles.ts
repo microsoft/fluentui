713c091b9ce2ae6248c89cadc95b27cba3ba5ba3
Katherine Kjeer <kakje@microsoft.com>
2018-09-18 15:21:15 -0700

    Add fillHorizontal and fillVertical props to StackItem

--- packages/experiments/src/components/Stack/StackItem/StackItem.styles.ts
+++ packages/experiments/src/components/Stack/StackItem/StackItem.styles.ts
@@ -1,5 +1,10 @@
 import { IThemedProps } from '../../../Foundation';
+import { getGlobalClassNames } from '../../../Styling';
 import { IStackItemProps, IStackItemStyles } from './StackItem.types';
+
+const GlobalClassNames = {
+  root: 'ms-StackItem'
+};
 
 const alignMap: { [key: string]: string } = {
   start: 'flex-start',
@@ -7,11 +12,17 @@ const alignMap: { [key: string]: string } = {
 };
 
 export const styles = (props: IThemedProps<IStackItemProps>): IStackItemStyles => {
-  const { grow, shrink, preventShrink, align, className } = props;
+  const { grow, shrink, preventShrink, align, fillHorizontal, fillVertical, className, theme } = props;
+
+  const classNames = getGlobalClassNames(GlobalClassNames, theme);
 
   return {
     root: [
-      'ms-StackItem',
+      classNames.root,
+      {
+        width: fillHorizontal ? '100%' : 'auto',
+        height: fillVertical ? '100%' : 'auto'
+      },
       grow && { flexGrow: grow === true ? 1 : grow },
       (preventShrink || (!grow && !shrink)) && {
         flexShrink: 0
