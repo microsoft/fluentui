28062386ba70b2488e493c85cd45705ac0b16728
Katherine Kjeer <kakje@microsoft.com>
2018-09-19 10:03:59 -0700

    Fix Panel stories defaultProps so stories compile

--- apps/vr-tests/src/stories/Panel.stories.tsx
+++ apps/vr-tests/src/stories/Panel.stories.tsx
@@ -5,7 +5,7 @@ import { storiesOf } from '@storybook/react';
 import { FabricDecorator } from '../utilities';
 import { Panel, PanelType, SearchBox } from 'office-ui-fabric-react';
 
-Panel.defaultProps = {
+const defaultProps = {
   isOpen: true,
   children: 'Content goes here'
 };
@@ -13,16 +13,16 @@ Panel.defaultProps = {
 storiesOf('Panel', module)
   .addDecorator(FabricDecorator)
   .addDecorator(story => <Screener steps={new Screener.Steps().snapshot('default').end()}>{story()}</Screener>)
-  .add('Small left w/ close button', () => <Panel hasCloseButton type={PanelType.smallFixedNear} headerText="Small" />)
+  .add('Small left w/ close button', () => <Panel {...defaultProps} hasCloseButton type={PanelType.smallFixedNear} headerText="Small" />)
   .add('Small fixed right w/ close button', () => (
-    <Panel hasCloseButton type={PanelType.smallFixedFar} headerText="Small fixed" />
+    <Panel {...defaultProps} hasCloseButton type={PanelType.smallFixedFar} headerText="Small fixed" />
   ))
-  .add('Small fluid right', () => <Panel type={PanelType.smallFluid} headerText="Small fluid" />)
-  .add('Medium right', () => <Panel type={PanelType.medium} headerText="Medium" />)
-  .add('Large right', () => <Panel type={PanelType.large} headerText="Large" />)
-  .add('Large fixed right', () => <Panel type={PanelType.largeFixed} headerText="Large fixed" />)
-  .add('Extra large right', () => <Panel type={PanelType.extraLarge} headerText="Extra Large" />)
-  .add('Custom', () => <Panel type={PanelType.custom} headerText="Custom" customWidth="200vw" />);
+  .add('Small fluid right', () => <Panel {...defaultProps} type={PanelType.smallFluid} headerText="Small fluid" />)
+  .add('Medium right', () => <Panel {...defaultProps} type={PanelType.medium} headerText="Medium" />)
+  .add('Large right', () => <Panel {...defaultProps} type={PanelType.large} headerText="Large" />)
+  .add('Large fixed right', () => <Panel {...defaultProps} type={PanelType.largeFixed} headerText="Large fixed" />)
+  .add('Extra large right', () => <Panel {...defaultProps} type={PanelType.extraLarge} headerText="Extra Large" />)
+  .add('Custom', () => <Panel {...defaultProps} type={PanelType.custom} headerText="Custom" customWidth="200vw" />);
 
 storiesOf('Panel', module)
   .addDecorator(FabricDecorator)
@@ -40,7 +40,7 @@ storiesOf('Panel', module)
   .add('SearchBox and Right Panel', () => (
     <div>
       <SearchBox placeholder="Search" />
-      <Panel isOpen={false} type={PanelType.medium} headerClassName="" headerText={'Header'} isHiddenOnDismiss>
+      <Panel {...defaultProps} isOpen={false} type={PanelType.medium} headerClassName="" headerText={'Header'} isHiddenOnDismiss>
         {null}
       </Panel>
     </div>
