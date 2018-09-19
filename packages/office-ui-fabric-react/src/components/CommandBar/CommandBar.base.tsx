178f6e47182cf110f70278ce09304771eb450bb8
Micah Godbolt <mgodbolt@microsoft.com>
2018-09-19 16:48:41 -0700

    update snapshots, add role=menuitems to menubar buttons

--- packages/office-ui-fabric-react/src/components/CommandBar/CommandBar.base.tsx
+++ packages/office-ui-fabric-react/src/components/CommandBar/CommandBar.base.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 
 import { BaseComponent, css, nullRender } from '../../Utilities';
-import {
-  ICommandBar,
-  ICommandBarItemProps,
-  ICommandBarProps,
-  ICommandBarStyleProps,
-  ICommandBarStyles
-} from './CommandBar.types';
+import { ICommandBar, ICommandBarItemProps, ICommandBarProps, ICommandBarStyleProps, ICommandBarStyles } from './CommandBar.types';
 import { IOverflowSet, OverflowSet } from '../../OverflowSet';
 import { IResizeGroup, ResizeGroup } from '../../ResizeGroup';
 import { FocusZone, FocusZoneDirection } from '../../FocusZone';
@@ -141,6 +135,7 @@ export class CommandBarBase extends BaseComponent<ICommandBarProps, {}> implemen
     }
     const commandButtonProps: ICommandBarItemProps = {
       allowDisabledFocus: true,
+      role: 'menuitem',
       ...item,
       styles: { root: { height: '100%' }, label: { whiteSpace: 'nowrap' }, ...item.buttonStyles },
       className: css('ms-CommandBarItem-link', item.className),
