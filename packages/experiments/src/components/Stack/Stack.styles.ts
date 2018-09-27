9e45030357156e0469790bf08a35fd1a2f3e14dd
kkjeer <kkjeer@wellesley.edu>
2018-09-27 12:55:44 -0700

    Stack wrap styles (#6488)
    * Replace padding with inline-block, remove unnecessary calc() calls, add maxWidth: 100vw to inner wrapped HorizontalStacks
    
    * Add change file

--- packages/experiments/src/components/Stack/Stack.styles.ts
+++ packages/experiments/src/components/Stack/Stack.styles.ts
@@ -75,8 +75,8 @@ export const styles = (props: IThemedProps<IStackProps>): IStackStyles => {
           '> *': {
             margin: `${0.5 * vGap.value}${vGap.unit} ${0.5 * hGap.value}${hGap.unit}`,
 
-            // extra 2px to account for padding on wrapped Stacks
-            maxWidth: `calc(100% - ${hGap.value}${hGap.unit} - 2px)`,
+            // avoid unnecessary calc() calls if horizontal gap is 0
+            maxWidth: hGap.value === 0 ? '100%' : `calc(100% - ${hGap.value}${hGap.unit})`,
 
             ...childStyles
           },
