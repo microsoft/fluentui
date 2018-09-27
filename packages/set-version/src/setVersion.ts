87202550ca2c5511a0d2a79cc2bca588ea466139
Ken <kchau@microsoft.com>
2018-09-27 09:50:23 -0700

    version files now expose a call that can be called by apps that don't do index import

--- packages/set-version/src/setVersion.ts
+++ packages/set-version/src/setVersion.ts
@@ -1,5 +1,10 @@
+// A packages cache that makes sure that we don't inject the same packageName twice in the same bundle -
+// this cache is local to the module closure inside this bundle
+const packagesCache: { [name: string]: string } = {};
 export function setVersion(packageName: string, packageVersion: string): void {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && !packagesCache[packageName]) {
+    packagesCache[packageName] = packageVersion;
+
     // tslint:disable-next-line:no-any
     const packages = ((window as any).__packages__ = (window as any).__packages__ || {});
     const versions = (packages[packageName] = packages[packageName] || []);
--- scripts/generate-version-files.js
+++ scripts/generate-version-files.js
@@ -62,7 +62,8 @@ packageJsons.forEach(packageJsonPath => {
       `// ${packageJson.name}@${packageJson.version}
 // Do not modify this file, the file is generated as part of publish. The checked in version is a placeholder only.
 import { setVersion } from '@uifabric/set-version';
-setVersion('${packageJson.name}', '${packageJson.version}');`
+export const version = () => setVersion('${packageJson.name}', '${packageJson.version}');
+version();`
     );
   }
 });
