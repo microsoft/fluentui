0a04a373a6e12bea711df5cb71dbdd8c59f0b5d8
Jason Gore <jagore@microsoft.com>
2018-10-02 16:08:38 -0700

    Remove test code.

--- packages/experiments/src/components/CollapsibleSection/examples/CollapsibleSection.Basic.Example.tsx
+++ packages/experiments/src/components/CollapsibleSection/examples/CollapsibleSection.Basic.Example.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import { FocusZone } from 'office-ui-fabric-react/lib/FocusZone';
 import { CollapsibleSection } from '@uifabric/experiments/lib/CollapsibleSection';
-import { Test } from '@uifabric/experiments/lib/Test';
-import { TestStateless } from '@uifabric/experiments/lib/TestStateless';
 
 export class CollapsibleSectionBasicExample extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
       <div>
-        <Test defaultText="default text" />
-        <TestStateless text="stateless text" />
         <FocusZone>
           <CollapsibleSection
             key={1}
