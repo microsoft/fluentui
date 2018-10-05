df26624e48824d005e5afff015e405773381272b
Micah Godbolt <micahgodbolt@gmail.com>
2018-10-06 03:22:08 +0530

    Introduce Leftnav from Power Apps Fabric Repo to Experiments (#6552)
    * init sidebar
    
    * init sidebar
    
    * Fix issues, get Sidebar rendering in docs
    
    * Removed incorrect docs
    
    * Fixed footer styles
    
    * Changefile
    
    * Fixed formatting
    
    * removed unnecessary jest loading
    
    * change path for direction hint
    
    * commenting out tests for now
    
    * accordion cleanup
    
    * remove unused files
    
    * turning off tests for now
    
    * Fixed paths

---
+++ packages/experiments/src/components/BAFAccordion/Accordion.types.tsx
@@ -0,0 +1,36 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ */
+
+import { IButtonProps } from 'office-ui-fabric-react/lib/Button';
+import { IContextualMenuProps } from 'office-ui-fabric-react/lib/ContextualMenu';
+import { IRenderFunction } from 'office-ui-fabric-react/lib/Utilities';
+import { IComponentAs } from 'office-ui-fabric-react/lib/Utilities';
+
+export interface IAccordion {}
+
+export interface IAccordionProps extends IButtonProps {
+  /*
+     * Renders the content for the accordion
+     * @type { IRenderFunction<IContextualMenuProps> }
+     */
+  onRenderContent: IRenderFunction<IContextualMenuProps>;
+
+  /**
+   * Button to use for the accordion header
+   * @type { IComponentAs<IButtonProps> }
+   */
+  buttonAs?: IComponentAs<IButtonProps>;
+
+  /**
+   * Callback for when the accordion is opened
+   * @type {any}
+   */
+  onOpen?: () => void;
+
+  /**
+   * Callback for when the accordion is opened
+   * @type {any}
+   */
+  onClose?: () => void;
+}
