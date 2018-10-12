ad86c7baac8f23e4034dfe15c20df8dc42911b44
Ken <kchau@microsoft.com>
2018-10-11 18:42:11 -0700

    restores the shrinkwrap and now use the built-in rush capability of doing prelease names

--- scripts/prerelease.js
+++ scripts/prerelease.js
@@ -1,77 +1,31 @@
-/* This script will turn all packages in the repo into a prerelease with the existing version */
-const npmFetch = require('npm-registry-fetch');
-const fs = require('fs');
 const path = require('path');
-const semver = require('semver');
+const execSync = require('child_process').execSync;
+const rushCmd = path.resolve('../common/scripts/install-run-rush.js');
+const gitCmd = 'git';
 
-const rootFolder = path.join(__dirname, '../');
-const rushConfig = JSON.parse(fs.readFileSync(path.join(rootFolder, 'rush.json')).toString());
+function getPrereleaseName(originalCommitish) {
+  const now = new Date();
 
-const newVersions = {};
-const preleaseTag = 'beta';
+  let suffix = '';
 
-function semverSort(a, b) {
-  if (semver.gt(a, b)) {
-    return 1;
+  if (process.env.BUILD_BUILDID) {
+    suffix = process.env.BUILD_BUILDID;
+  } else {
+    suffix = `${now.getFullYear()}${now.getMonth().padStart(2, '0')}${now.getDate().padStart(2, '0')}`;
   }
 
-  if (semver.lt(a, b)) {
-    return -1;
-  }
-
-  return 0;
+  return `prerelease-${originalCommitish}-${suffix}`;
 }
 
-(async () => {
-  await rushConfig.projects.reduce(async (previousPromise, project) => {
-    await previousPromise;
+function getOriginalCommitish() {
+  let commitish = execSync(`${gitCmd} rev-parse --abbrev-ref HEAD`).trim();
+  if (commitish === 'HEAD') {
+    return execSync(`${gitCmd} rev-parse HEAD`).trim();
+  }
 
-    const projectFolder = path.join(rootFolder, project.projectFolder);
-    const packageJson = JSON.parse(fs.readFileSync(path.join(projectFolder, 'package.json')));
+  return commitish;
+}
 
-    let npmInfo = {};
-
-    try {
-      const response = await npmFetch(`/${packageJson.name}`);
-      npmInfo = await response.json();
-    } catch (e) {
-      console.log(`cannot fetch ${packageJson.name} from public npm registry`);
-    }
-
-    if (npmInfo.versions) {
-      const checkedInVersion = packageJson.version;
-      const prereleaseVersions = Object.keys(npmInfo.versions).filter(version => version.startsWith(checkedInVersion + '-' + preleaseTag));
-      const latestVersion =
-        prereleaseVersions.length > 0
-          ? prereleaseVersions.sort(semverSort)[prereleaseVersions.length - 1]
-          : checkedInVersion + '-' + preleaseTag + '.0';
-      const newVersion = semver.inc(latestVersion, 'prerelease', preleaseTag);
-
-      packageJson.version = newVersion;
-
-      newVersions[packageJson.name] = newVersion;
-    }
-  }, Promise.resolve());
-
-  rushConfig.projects.forEach(project => {
-    const projectFolder = path.join(rootFolder, project.projectFolder);
-    const packageJsonPath = path.join(projectFolder, 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-
-    if (newVersions[packageJson.name]) {
-      packageJson.version = newVersions[packageJson.name];
-    }
-
-    Object.keys(newVersions).forEach(dep => {
-      if (packageJson.dependencies && packageJson.dependencies[dep]) {
-        packageJson.dependencies[dep] = newVersions[dep];
-      }
-
-      if (packageJson.devDependencies && packageJson.devDependencies[dep]) {
-        packageJson.devDependencies[dep] = newVersions[dep];
-      }
-    });
-
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
-  });
-})();
+const originalCommitish = getOriginalCommitish();
+execSync(`${gitCmd} checkout -b prelease`);
+execSync(`${rushCmd} publish -a --prerelease-name prelease ${getPrereleaseName(originalCommitish)}`);
