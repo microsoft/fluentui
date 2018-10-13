3cad1f6789ca7352635307ceb57d17f5092d353c
Aftab Hassan <aftabh@uw.edu>
2018-10-13 08:06:11 -0700

    Update size-audit.js

--- scripts/tasks/size-audit.js
+++ scripts/tasks/size-audit.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 exports.generateSizeData = function() {
   const projectName = path.basename(process.cwd());
-  const sizeFilePath = path.join(process.cwd(), 'dist', `${projectName}-sizedataaaaa.json`);
+  const sizeFilePath = path.join(process.cwd(), 'dist', `${projectName}-sizedata.json`);
   if (fs.existsSync(path.dirname(sizeFilePath))) {
     const result = {
       chunks: analyzeChunks()
