ab63e321c97e68be649d42a760f8e9a05dfdb8a5
David Guerrero Del Rio <davguerr@microsoft.com>
2018-10-16 13:58:40 -0700

    undo auto formatting

--- packages/office-ui-fabric-react/src/components/List/List.tsx
+++ packages/office-ui-fabric-react/src/components/List/List.tsx
@@ -438,8 +438,8 @@ export class List extends BaseComponent<IListProps, IListState> implements IList
       ...(getPageStyle ? getPageStyle(page) : {}),
       ...(!page.items
         ? {
-          height: page.height
-        }
+            height: page.height
+          }
         : {})
     };
   }
@@ -825,12 +825,12 @@ export class List extends BaseComponent<IListProps, IListState> implements IList
     itemIndex: number,
     visibleRect: IRectangle
   ): {
-      // These return values are now no longer optional.
-      itemCount: number;
-      height: number;
-      data?: any;
-      key?: string;
-    } {
+    // These return values are now no longer optional.
+    itemCount: number;
+    height: number;
+    data?: any;
+    key?: string;
+  } {
     const { getPageSpecification } = this.props;
     if (getPageSpecification) {
       const pageData = getPageSpecification(itemIndex, visibleRect);
