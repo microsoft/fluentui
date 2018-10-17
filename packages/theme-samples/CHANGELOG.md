d68bb6afe698eba7fde0d76d6bc97a75f6047d6e
Jason Gore <jagore@microsoft.com>
2018-10-17 15:21:00 -0700

    Theme Samples: Create Package (#6732)
    * Add new theme-samples package for centralizing Fluent and other theme definitions for use by OUFR and experiments packages.
    
    * Change file.

---
+++ packages/theme-samples/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Change Log - @uifabric/theme-samples
+
+This log was last generated on Wed, 17 Oct 2018 20:55:55 GMT and should not be manually modified.
---
+++ packages/theme-samples/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const resources = require('../../scripts/tasks/webpack-resources');
+
+const BUNDLE_NAME = 'theme-samples';
+const IS_PRODUCTION = process.argv.indexOf('--production') > -1;
+
+module.exports = resources.createConfig(BUNDLE_NAME, IS_PRODUCTION, {
+  entry: {
+    [BUNDLE_NAME]: './lib/index.js'
+  },
+
+  output: {
+    libraryTarget: 'var',
+    library: 'FabricThemeSamples'
+  },
+
+  externals: [{ react: 'React' }, { 'react-dom': 'ReactDOM' }],
+
+  resolve: {
+    alias: {
+      '@uifabric/theme-samples/src': path.join(__dirname, 'src'),
+      '@uifabric/theme-samples/lib': path.join(__dirname, 'lib'),
+      '@uifabric/theme-samples': path.join(__dirname, 'lib')
+    }
+  }
+});
