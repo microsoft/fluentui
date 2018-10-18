e2c08ba166c0256abf7d685b74f63b267d68133a
REDMOND\naethell <naethell@microsoft.com>
2018-10-18 14:22:43 -0700

    move around demo page files

--- packages/experiments/src/components/Announced/AnnouncedBulkLongRunningPage.tsx
+++ packages/experiments/src/components/Announced/AnnouncedBulkLongRunningPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { DemoPage } from '../../../../../apps/fabric-website-resources/src/components/DemoPage';
-import { AnnouncedBulkLongRunningPageProps } from './Announced.doc';
+import { AnnouncedBulkLongRunningPageProps } from '@uifabric/experiments/lib/components/Announced/Announced.doc';
 
 export const AnnouncedBulkLongRunningPage = (props: { isHeaderVisible: boolean }) => (
   <DemoPage {...{ ...AnnouncedBulkLongRunningPageProps, ...props }} />
--- packages/experiments/src/components/Announced/AnnouncedSearchResultsPage.tsx
+++ packages/experiments/src/components/Announced/AnnouncedSearchResultsPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import { DemoPage } from '../../../../../apps/fabric-website-resources/src/components/DemoPage';
-import { AnnouncedPageProps } from './Announced.doc';
+import { AnnouncedPageProps } from '@uifabric/experiments/lib/components/Announced/Announced.doc';
 
 export const AnnouncedSearchResultsPage = (props: { isHeaderVisible: boolean }) => <DemoPage {...{ ...AnnouncedPageProps, ...props }} />;
