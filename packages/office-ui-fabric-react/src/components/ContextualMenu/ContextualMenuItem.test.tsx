024f0c449b28fa1c661852096fc90cd5263eb53e
Kevin Coughlin <keco@microsoft.com>
2018-10-18 11:12:33 -0700

    Add unit tests to verify API contract of getItemClassNames

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenuItem.test.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenuItem.test.tsx
@@ -5,8 +5,9 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import { ContextualMenuItemBase } from './ContextualMenuItem.base';
 import { IContextualMenuItemProps } from './ContextualMenuItem.types';
 import { IContextualMenuItem } from './ContextualMenu.types';
-import { IMenuItemClassNames } from './ContextualMenu.classNames';
+import { IMenuItemClassNames, getItemClassNames } from './ContextualMenu.classNames';
 import { hasSubmenu } from '../../utilities/contextualMenu/index';
+import { createTheme } from '../../Styling';
 
 describe('ContextMenuItemChildren', () => {
   describe('when a checkmark icon', () => {
@@ -58,9 +59,7 @@ describe('ContextMenuItemChildren', () => {
         menuItem = { key: '123', iconProps: { iconName: 'itemIcon' }, text: 'menuItem' };
         menuClassNames = getMenuItemClassNames();
 
-        wrapper = shallow(
-          <ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />
-        );
+        wrapper = shallow(<ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />);
       });
 
       it('renders the icon', () => {
@@ -77,9 +76,7 @@ describe('ContextMenuItemChildren', () => {
         menuItem = { key: '123', iconProps: {} };
         menuClassNames = getMenuItemClassNames();
 
-        wrapper = shallow(
-          <ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />
-        );
+        wrapper = shallow(<ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />);
       });
 
       it('renders the icon with iconName', () => {
@@ -98,14 +95,71 @@ describe('ContextMenuItemChildren', () => {
       menuItem = { key: '123', iconProps: {}, submenuIconProps: {} };
       menuClassNames = getMenuItemClassNames();
 
-      wrapper = shallow(
-        <ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />
-      );
+      wrapper = shallow(<ContextualMenuItemBase item={menuItem} classNames={menuClassNames} index={1} hasIcons={true} />);
     });
 
     it('renders the menu icon', () => {
       expect(wrapper).toMatchSnapshot();
     });
+  });
+});
+
+describe('getItemClassNames', () => {
+  // This test exists to validate that getItemClassNames signature has not changed
+  // to avoid breaking internal partners relying on this private API.
+  // See: https://github.com/OfficeDev/office-ui-fabric-react/pull/6738
+  it('accepts an argument list of style props values', () => {
+    const NoClassNamesTheme = createTheme({ disableGlobalClassNames: true });
+    const isDisabled = false;
+    const isExpanded = false;
+    const isChecked = false;
+    const isAnchorLink = false;
+    const isKnownIcon = true;
+    const dividerClassName = 'dividerFoo';
+    const itemClassName = 'foo';
+    const iconClassName = 'iconFoo';
+
+    const itemClassNames = getItemClassNames(
+      NoClassNamesTheme,
+      isDisabled,
+      isExpanded,
+      isChecked,
+      isAnchorLink,
+      isKnownIcon,
+      itemClassName,
+      dividerClassName,
+      iconClassName
+    );
+
+    expect(itemClassNames).toBeDefined();
+  });
+
+  it('applies custom classNames to style slots', () => {
+    const NoClassNamesTheme = createTheme({ disableGlobalClassNames: true });
+    const isDisabled = false;
+    const isExpanded = false;
+    const isChecked = false;
+    const isAnchorLink = false;
+    const isKnownIcon = true;
+    const dividerClassName = 'dividerFoo';
+    const itemClassName = 'foo';
+    const iconClassName = 'iconFoo';
+
+    const itemClassNames = getItemClassNames(
+      NoClassNamesTheme,
+      isDisabled,
+      isExpanded,
+      isChecked,
+      isAnchorLink,
+      isKnownIcon,
+      itemClassName,
+      dividerClassName,
+      iconClassName
+    );
+
+    expect(itemClassNames.item).toContain('foo');
+    expect(itemClassNames.divider).toContain('dividerFoo');
+    expect(itemClassNames.icon).toContain('iconFoo');
   });
 });
 
