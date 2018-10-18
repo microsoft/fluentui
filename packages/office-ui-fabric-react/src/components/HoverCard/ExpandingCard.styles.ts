e8e12b1b6d7918cb17fbc56992db85b891cb7bd3
Vitalie Braga <vitaliebraga@gmail.com>
2018-10-18 15:47:17 -0700

    HoverCard: Adds a new card type plus some refactor (#6411)
    * Bring HoverCard mergeStyle API to current standards
    
    * Update mersgeStyles API for Expanding Card
    
    * Update ExpandingCard documentation
    
    * Remove unnecessary style reference and file
    
    * Extract all common things to a base card class
    
    * Some more extraction and a little cleanup
    
    * Some alphabetizing of props and imports
    
    * Organization of files in folders and fix imports
    
    * Renaming BaseCard for easier semantics
    
    * Update documentation
    
    * Rename type parameter for less confusion on names
    
    * Reuse basic skeleton of ExpandingCard to setup a BasicCard
    
    * Remove unused import
    
    * Move an enum to a more proper location and fix typo
    
    * Setup new type of card
    Changes to Card class methods
    
    * Updates the documentation in Card class
    
    * Setup example for basic card
    
    * Fix pointer events
    
    * Fix directionalHintFixed prop for basic card
    
    * Documentation update
    
    * Set up some basic tests for HoverCard
    
    * More tests
    
    * Rush change log
    
    * Remove tests for now and get back later to them
    
    * Add Card class to excludedComponents list in Conformance test.
    
    * Add some required props to conformance test
    
    * Add some classname hooks for injection
    
    * More fixes for Conformance test
    
    * Another fix (((
    
    * Ups - typo
    
    * Fix import statement
    
    * A little more corrections on imports
    
    * Move ExpandingCard files back to root folder
    
    * Fix require path
    
    * Bring back the tests
    
    * Fix typo in tests
    
    * Rename generic type
    Remove unnecessary constructor
    
    * Renaming some classes for less name confusion
    Change the pattern from inheritance to composition
    
    * Remove an extra check to leave allow requestAnimationFrame to check for needsScroll
    
    * Update snapshots and fix Conformance test
    
    * Fix lint error about imports
    
    * Fix documentation and remove screenshot file
    
    * Update snapshots

--- packages/office-ui-fabric-react/src/components/HoverCard/ExpandingCard.styles.ts
+++ packages/office-ui-fabric-react/src/components/HoverCard/ExpandingCard.styles.ts
@@ -1,11 +1,23 @@
-import { IExpandingCardStyles } from './ExpandingCard.types';
-import { memoizeFunction } from '../../Utilities';
-import { concatStyleSets, ITheme, HighContrastSelector } from '../../Styling';
+import { getGlobalClassNames, HighContrastSelector } from '../../Styling';
+import { IExpandingCardStyles, IExpandingCardStyleProps } from './ExpandingCard.types';
 
-export const getStyles = memoizeFunction(
-  (theme: ITheme, customStyles?: IExpandingCardStyles): IExpandingCardStyles => {
-    const styles: IExpandingCardStyles = {
-      root: {
+const GlobalClassNames = {
+  root: 'ms-ExpandingCard-root',
+  compactCard: 'ms-ExpandingCard-compactCard',
+  expandedCard: 'ms-ExpandingCard-expandedCard',
+  expandedCardScroll: 'ms-ExpandingCard-expandedCardScrollRegion'
+};
+
+export function getStyles(props: IExpandingCardStyleProps): IExpandingCardStyles {
+  const { theme, needsScroll, expandedCardFirstFrameRendered, compactCardHeight, expandedCardHeight, className } = props;
+
+  const { palette } = theme;
+  const classNames = getGlobalClassNames(GlobalClassNames, theme);
+
+  return {
+    root: [
+      classNames.root,
+      {
         width: '340px',
         pointerEvents: 'none',
         selectors: {
@@ -20,37 +32,47 @@ export const getStyles = memoizeFunction(
           }
         }
       },
-      compactCard: {
+      className
+    ],
+    compactCard: [
+      classNames.compactCard,
+      {
         pointerEvents: 'auto',
-        position: 'relative'
-      },
-      expandedCard: [
-        {
-          height: '1px',
-          overflowY: 'hidden',
-          pointerEvents: 'auto',
-          transition: 'height 0.467s cubic-bezier(0.5, 0, 0, 1)',
-          selectors: {
-            ':before': {
-              content: '""',
-              position: 'relative',
-              display: 'block',
-              top: '0',
-              left: '24px',
-              width: '292px',
-              height: '1px',
-              backgroundColor: theme.palette.neutralLighter
-            }
+        position: 'relative',
+        height: compactCardHeight
+      }
+    ],
+    expandedCard: [
+      classNames.expandedCard,
+      {
+        height: '1px',
+        overflowY: 'hidden',
+        pointerEvents: 'auto',
+        transition: 'height 0.467s cubic-bezier(0.5, 0, 0, 1)',
+        selectors: {
+          ':before': {
+            content: '""',
+            position: 'relative',
+            display: 'block',
+            top: '0',
+            left: '24px',
+            width: '292px',
+            height: '1px',
+            backgroundColor: palette.neutralLighter
           }
         }
-      ],
-      expandedCardScroll: {
+      },
+      expandedCardFirstFrameRendered && {
+        height: expandedCardHeight
+      }
+    ],
+    expandedCardScroll: [
+      classNames.expandedCardScroll,
+      needsScroll && {
         height: '100%',
         boxSizing: 'border-box',
         overflowY: 'auto'
       }
-    };
-
-    return concatStyleSets(styles, customStyles)!;
-  }
-);
+    ]
+  };
+}
