5a5615184adf1f52ad165a6518a852af33896c30
Vitalie Braga <v-vibr@microsoft.com>
2018-10-19 13:33:37 -0700

    Fluentize TextField

--- packages/fluent-theme/src/fluent/styles/TextField.styles.ts
+++ packages/fluent-theme/src/fluent/styles/TextField.styles.ts
@@ -1,7 +1,43 @@
+import { ITextFieldStyleProps } from 'office-ui-fabric-react/lib/TextField';
 import { fluentBorderRadius } from './styleConstants';
+import { NeutralColors, SharedColors } from '../FluentColors';
 
-export const TextFieldStyles = {
-  fieldGroup: {
-    borderRadius: fluentBorderRadius
-  }
+export const TextFieldStyles = (props: ITextFieldStyleProps) => {
+  const { focused, disabled, hasErrorMessage, multiline } = props;
+  return {
+    fieldGroup: [
+      {
+        borderRadius: fluentBorderRadius
+      },
+      !focused &&
+        !disabled &&
+        !hasErrorMessage && {
+          borderColor: NeutralColors.gray80
+        },
+      hasErrorMessage && [
+        {
+          borderColor: SharedColors.red10,
+          selector: {
+            '&:hover': {
+              borderColor: SharedColors.red20
+            }
+          }
+        },
+        focused && {
+          borderColor: SharedColors.red20
+        }
+      ]
+    ],
+    field: {
+      color: NeutralColors.gray190,
+      padding: !multiline ? '0 8px' : '6px 8px',
+      selectors: {
+        '::placeholder': [disabled && { color: NeutralColors.gray90 }],
+        ':-ms-input-placeholder': [disabled && { color: NeutralColors.gray90 }]
+      }
+    },
+    errorMessage: {
+      color: SharedColors.red20
+    }
+  };
 };
