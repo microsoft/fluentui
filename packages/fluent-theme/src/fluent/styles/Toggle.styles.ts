fc5491c6690e76e85bb3c74e00b71256674fdf3a
Mike Wheaton <miwhea@microsoft.com>
2018-10-19 14:34:31 -0700

    Update Toggle styles to match latest designs

--- packages/fluent-theme/src/fluent/styles/Toggle.styles.ts
+++ packages/fluent-theme/src/fluent/styles/Toggle.styles.ts
@@ -1,14 +1,46 @@
-export const ToggleStyles = {
-  pill: {
-    width: '40px',
-    height: '20px',
-    borderRadius: '10px',
-    padding: '0 4px'
-  },
-  thumb: {
-    width: '12px',
-    height: '12px',
-    borderRadius: '12px',
-    borderColor: 'transparent'
-  }
+import { IToggleProps } from '../../../../office-ui-fabric-react/lib';
+import { CommunicationColors, NeutralColors } from '../FluentColors';
+
+export const ToggleStyles = (props: IToggleProps) => {
+  const { disabled, checked } = props;
+  return {
+    pill: [
+      {
+        width: '40px',
+        height: '20px',
+        borderRadius: '10px',
+        padding: '0 4px'
+      },
+      !disabled && [
+        checked && {
+          selectors: {
+            ':hover': [
+              {
+                backgroundColor: CommunicationColors.shade20
+              }
+            ]
+          }
+        },
+        !checked && {
+          selectors: {
+            ':hover .ms-Toggle-thumb': {
+              backgroundColor: NeutralColors.gray160
+            }
+          }
+        }
+      ]
+    ],
+    thumb: [
+      {
+        width: '12px',
+        height: '12px',
+        borderRadius: '12px',
+        borderColor: 'transparent'
+      },
+      !disabled &&
+        !checked && {
+          backgroundColor: NeutralColors.gray130
+        }
+    ]
+  };
 };
