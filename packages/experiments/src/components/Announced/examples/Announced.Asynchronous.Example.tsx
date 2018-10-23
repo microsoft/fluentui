73ac3dcfce74c88861c606b5cd8d7d91b0516bf0
REDMOND\naethell <naethell@microsoft.com>
2018-10-23 09:54:02 -0700

    fix onFocusPhotoCell

--- packages/experiments/src/components/Announced/examples/Announced.Asynchronous.Example.tsx
+++ packages/experiments/src/components/Announced/examples/Announced.Asynchronous.Example.tsx
@@ -12,12 +12,11 @@ const DELAY = 10;
 export interface IAnnouncedAsynchronousExampleState {
   photos: { url: string; width: number; height: number }[];
   total: number;
-  seconds: number;
   announced?: JSX.Element;
   percentComplete: number;
   loading: boolean;
   complete: boolean;
-  timeSinceLastFocusAnnounce: number;
+  timeSinceLastAnnounce: number;
 }
 
 export interface IAnnouncedAsynchronousExampleProps {}
@@ -26,19 +25,17 @@ export class AnnouncedAsynchronousExample extends React.Component<IAnnouncedAsyn
   private _root = createRef<HTMLElement>();
   private timer: number;
   private increaseTotal: number;
-  private delay: number;
 
   constructor(props: {}) {
     super(props);
     this.state = {
       photos: this._createPhotos(),
       total: 0,
-      seconds: 0,
       announced: undefined,
       percentComplete: 0,
       loading: false,
       complete: false,
-      timeSinceLastFocusAnnounce: DELAY
+      timeSinceLastAnnounce: 0
     };
 
     this._renderPhotos = this._renderPhotos.bind(this);
@@ -57,21 +54,24 @@ export class AnnouncedAsynchronousExample extends React.Component<IAnnouncedAsyn
 
     this.timer = setInterval(() => {
       if (this.state.loading && !this.state.complete) {
-        this.setState({ seconds: this.state.seconds + 1, timeSinceLastFocusAnnounce: this.state.timeSinceLastFocusAnnounce + 1 });
+        this.setState({ timeSinceLastAnnounce: this.state.timeSinceLastAnnounce + 1 });
 
-        if (this.state.seconds % this.delay === 0 && this.state.timeSinceLastFocusAnnounce >= DELAY) {
+        if (this.state.timeSinceLastAnnounce === DELAY || this.state.percentComplete === 1) {
           this.setState({
             announced: (
-              <Announced message={`${this.state.total}/${this.state.photos.length} photos loaded`} id={'announced-' + this.state.total} />
-            )
+              <Announced
+                message={`${this.state.total} of ${this.state.photos.length} photos loaded`}
+                id={'announced-' + this.state.total}
+              />
+            ),
+            timeSinceLastAnnounce: 0
           });
-        } else {
-          this.setState({ announced: undefined });
         }
+        // else {
+        //   this.setState({ announced: undefined });
+        // }
       }
     }, 1000);
-
-    this.delay = DELAY;
   }
 
   public render(): JSX.Element {
@@ -83,7 +83,12 @@ export class AnnouncedAsynchronousExample extends React.Component<IAnnouncedAsyn
           Turn on Narrator and check the toggle to start loading photos. Announced should announce the number of photos loaded every 10
           seconds, as that is the delay chosen for this example.
         </p>
-        <p>When focusing on a photo that hasn't loaded yet, the Announced component should announce "Photo loading".</p>
+        <p>
+          An exception is if a user focuses on a photo that hasn't loaded yet. When focusing on a photo that hasn't loaded yet, the
+          Announced component should announce "Photo loading" and another status message should not be read for another 10 seconds. Once
+          this status message triggered by the focus event is sent to the screen reader, the 10 second timer starts again so that the status
+          messages are not read over one another.
+        </p>
         <Toggle label="Check to start loading photos" onText="Start/Resume" offText="Pause" onChange={this._onToggleChange} />
         <ProgressIndicator label={percentComplete < 1 ? 'Loading photos' : 'Finished loading photos'} percentComplete={percentComplete} />
         <FocusZone elementType="ul" className="ms-AnnouncedExamples-photoList">
@@ -137,7 +142,20 @@ export class AnnouncedAsynchronousExample extends React.Component<IAnnouncedAsyn
       document.activeElement.children[0] &&
       document.activeElement.children[0].children.length === 0
     ) {
-      this.setState({ timeSinceLastFocusAnnounce: 0, announced: <Announced message={`Photo loading`} /> });
+      this.setState({
+        timeSinceLastAnnounce: 0,
+        announced: (
+          <Announced
+            message={`Photo loading`}
+            id={
+              'announced-photo-' +
+              (document.activeElement.getAttribute('aria-posinset')
+                ? document.activeElement.getAttribute('aria-posinset')
+                : this.state.total)
+            }
+          />
+        )
+      });
     }
   }
 
