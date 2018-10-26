5fe87c9767e005dafd2ba267386db885458d35f8
Mike Wheaton <miwhea@microsoft.com>
2018-10-26 09:29:53 -0700

    PrimaryButton styles

--- packages/fluent-theme/src/fluent/styles/PrimaryButton.styles.ts
+++ packages/fluent-theme/src/fluent/styles/PrimaryButton.styles.ts
@@ -1,7 +1,17 @@
 import { fluentBorderRadius } from './styleConstants';
+import { CommunicationColors, NeutralColors } from '../FluentColors';
 
 export const PrimaryButtonStyles = {
   root: {
-    borderRadius: fluentBorderRadius
+    borderRadius: fluentBorderRadius,
+    border: 'none',
+    backgroundColor: CommunicationColors.primary,
+    color: NeutralColors.white
+  },
+  rootHovered: {
+    backgroundColor: CommunicationColors.shade10
+  },
+  rootPressed: {
+    backgroundColor: CommunicationColors.shade20
   }
 };
