b64f0a0c1d7841d3eaf8ed49e0827e12e782e398
Peter Jahn <pejahn@microsoft.com>
2018-11-02 11:37:42 -0700

    Add isLocal check to location helper

--- apps/fabric-website/src/utilities/location.ts
+++ apps/fabric-website/src/utilities/location.ts
@@ -4,3 +4,8 @@
  */
 export const hasUHF: boolean =
   window.location.hostname === 'developer.microsoft.com' || window.location.hostname === 'developer.microsoft-tst.com';
+
+/**
+ * Determines if the site is running locally.
+ */
+export const isLocal: boolean = window.location.hostname === 'localhost' || window.location.hostname.indexOf('ngrok.io') > -1;
