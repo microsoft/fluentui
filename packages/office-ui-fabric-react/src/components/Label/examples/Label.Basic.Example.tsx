139755ea529ebb7006860e4d7853a54f91348a96
Ken <kchau@microsoft.com>
2018-11-06 15:59:10 -0800

    making label use htmlFor instead

--- packages/office-ui-fabric-react/src/components/Label/examples/Label.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Label/examples/Label.Basic.Example.tsx
@@ -1,14 +1,15 @@
 // @codepen
 import * as React from 'react';
 
-import { Label, TextField } from 'office-ui-fabric-react';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
 export const LabelBasicExample = () => (
   <div>
     <Label>I'm a Label</Label>
     <Label disabled={true}>I'm a disabled Label</Label>
     <Label required={true}>I'm a required Label</Label>
-    <Label for="anInput">A Label for An Input</Label>
+    <Label htmlFor="anInput">A Label for An Input</Label>
     <TextField id="anInput" />
   </div>
 );
