b657ea362f51ff8cf80238580f47e6aabfe7a428
John Lorenz <lorejoh12@gmail.com>
2018-11-05 18:23:37 -0800

    force committing more files that prettier is trying to update that I don't want updated

--- apps/fabric-website-resources/src/components/pages/CalendarPage.tsx
+++ apps/fabric-website-resources/src/components/pages/CalendarPage.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import { CalendarPageProps } from 'office-ui-fabric-react/lib/components/Calendar/Calendar.doc';
 import { DemoPage } from '../DemoPage';
 
-export const CalendarPage = (props: { isHeaderVisible: boolean }) => <DemoPage {...{ ...CalendarPageProps, ...props }} />;
+export const CalendarPage = (props: { isHeaderVisible: boolean }) => (
+  <DemoPage {...{ ...CalendarPageProps, ...props }} />
+);
