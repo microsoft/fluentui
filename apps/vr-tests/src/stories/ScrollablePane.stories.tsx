837ad79349ea4a085f0e4afe649213a0dbff6ec8
Eddie Liu <edwl@microsoft.com>
2018-11-15 10:16:39 -0800

    Fix ScrollablePane screener test to not use ranodmly generated text.

--- apps/vr-tests/src/stories/ScrollablePane.stories.tsx
+++ apps/vr-tests/src/stories/ScrollablePane.stories.tsx
@@ -4,9 +4,12 @@ import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
 import { FabricDecorator } from '../utilities';
 import { ScrollablePane, StickyPositionType, Sticky } from 'office-ui-fabric-react';
-import { lorem } from 'office-ui-fabric-react/lib/utilities/exampleData';
 
 const colors = ['#eaeaea', '#dadada', '#d0d0d0', '#c8c8c8', '#a6a6a6', '#c7e0f4'];
+
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh enim, tincidunt pellentesque orci id, efficitur dapibus nulla. In sed arcu eget neque rhoncus molestie sit amet in diam. Pellentesque suscipit sem quis arcu luctus, quis tincidunt magna auctor. Mauris dictum ligula ac nulla mattis, ac tincidunt nibh volutpat. Aliquam ante quam, efficitur sit amet euismod in, finibus at ipsum. Praesent blandit nisi in augue eleifend accumsan. Etiam imperdiet sit amet elit iaculis semper. Aliquam et erat egestas nunc ultricies fringilla. Nulla non gravida mauris. Sed euismod aliquam odio a tempus. Curabitur vitae lacus ex. Vivamus iaculis, libero in scelerisque ultricies, enim neque blandit augue, ut ultrices nulla enim ac ligula. In vitae orci vitae diam vestibulum rhoncus.
+
+Pellentesque ante leo, pellentesque nec gravida ac, rutrum eget justo. Sed metus enim, pretium non odio a, varius dapibus mi. Vivamus pharetra accumsan malesuada. Phasellus congue urna ex, efficitur sollicitudin neque vulputate aliquet. Integer eget turpis ultricies, pulvinar libero quis, dictum elit. Fusce elementum porttitor pulvinar. Aenean tempus posuere leo quis cursus. Aliquam nec nisi id nisi rutrum tincidunt. Aliquam erat volutpat. Nullam tempor orci diam, eget pellentesque justo porttitor blandit. Phasellus vulputate augue turpis, ut iaculis nisi malesuada at. Duis euismod.`;
 
 const contentAreas: JSX.Element[] = [];
 for (let i = 0; i < 5; i++) {
@@ -34,7 +37,7 @@ function createContentArea(index: number) {
             color: '#201f1e'
           }}>Sticky Component #{index + 1}</div>
       </Sticky>
-      <div className="textContent">{lorem(200)}</div>
+      <div className="textContent">{LOREM_IPSUM}</div>
     </div>
   );
 }
