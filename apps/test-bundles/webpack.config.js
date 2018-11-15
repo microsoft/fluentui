d2f1e9fffbd47d116a17de968f112d2d2d9637d9
David Zearing <dzearing@microsoft.com>
2018-11-15 12:46:45 -0800

    Updates.

--- apps/test-bundles/webpack.config.js
+++ apps/test-bundles/webpack.config.js
@@ -4,21 +4,46 @@ const resources = require('../../scripts/tasks/webpack-resources');
 
 // Files which should not be considered top-level entries.
 const TopLevelEntryFileExclusions = ['index.js', 'version.js', 'index.bundle.js'];
+const StatsFileName = 'test-bundles';
 
 module.exports = resources.createConfig(
-  'test-bundles',
+  StatsFileName,
   true,
   {
     entry: _buildEntries('office-ui-fabric-react'),
     externals: {
       react: 'React',
       'react-dom': 'ReactDOM'
-    }
+    },
+    plugins: [
+      {
+        apply: compiler => {
+          compiler.hooks.afterEmit.tap('AfterEmitPlugin', compilation => _copyStats('office-ui-fabric-react'));
+        }
+      }
+    ]
   },
   true,
   true
 );
 
+/**
+ * Copy the stats file to dist folder the package the bundles were generated from.
+ */
+function _copyStats(packageName) {
+  try {
+    fs.copyFileSync(
+      path.join(__dirname, 'dist/test-bundles.stats.html'),
+      path.resolve(__dirname, `../../packages/${packageName}/dist/${StatsFileName}.stats.html`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+/**
+ * Build webpack entries based on top level imports available in a package.
+ */
 function _buildEntries(packageName) {
   const entries = {};
   let packagePath = '';
@@ -37,6 +62,8 @@ function _buildEntries(packageName) {
 
     if (isJavascriptFile && isAllowedFile) {
       const entryName = itemName.replace(/.js$/, '');
+
+      // Replace commonjs paths with lib paths.
       const entryPath = path.join(packagePath, itemName).replace('/lib-commonjs/', '/lib/');
 
       entries[`${packageName}-${entryName}`] = entryPath;
