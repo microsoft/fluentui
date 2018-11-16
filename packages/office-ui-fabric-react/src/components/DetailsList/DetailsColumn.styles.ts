f2c9ee3a1675254382bb87061a25cde9ba3fda3f
Kevin Coughlin <keco@microsoft.com>
2018-11-15 17:38:18 -0800

    Fix lint error due to unused import in DetailsColumn.styles

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsColumn.styles.ts
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsColumn.styles.ts
@@ -1,5 +1,5 @@
 import { IDetailsColumnStyleProps, IDetailsColumnStyles } from './DetailsColumn.types';
-import { getFocusStyle, getGlobalClassNames, hiddenContentStyle, keyframes, IStyle } from '../../Styling';
+import { getFocusStyle, getGlobalClassNames, hiddenContentStyle, IStyle } from '../../Styling';
 import { DEFAULT_CELL_STYLE_PROPS } from './DetailsRow.styles';
 import { getCellStyles } from './DetailsHeader.styles';
 
