8c5b0bb45fa40006eb740ba2e61192e617b3b714
Micah Godbolt <mgodbolt@microsoft.com>
2018-11-21 14:22:06 -0800

    change stuff

--- packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
+++ packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
@@ -6689,6 +6689,7 @@ interface IDatePickerProps extends IBaseProps<IDatePicker>, React.HTMLAttributes
   allowTextInput?: boolean;
   ariaLabel?: string;
   borderless?: boolean;
+  calendarAs?: IComponentAs<ICalendarProps>;
   calendarProps?: ICalendarProps;
   className?: string;
   componentRef?: IRefObject<IDatePicker>;
