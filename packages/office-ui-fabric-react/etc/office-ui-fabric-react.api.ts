851e198c65525763062105eba8359b8fb948b8df
Kevin Coughlin <keco@microsoft.com>
2018-11-27 13:40:04 -0800

    Add updated API file.

--- packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
+++ packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
@@ -10656,7 +10656,14 @@ interface ITeachingBubbleStyles {
   // (undocumented)
   secondaryButton: IStyle;
   // (undocumented)
+  subComponentStyles?: ITeachingBubbleSubComponentStyles;
+  // (undocumented)
   subText: IStyle;
+}
+
+// @public (undocumented)
+interface ITeachingBubbleSubComponentStyles {
+  callout: IStyleFunctionOrObject<ICalloutContentStyleProps, ICalloutContentStyles>;
 }
 
 // @public (undocumented)
