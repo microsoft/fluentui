9b61cb962038f6d085e53058c7b806ceefea6a58
Kenneth Chau <kenotron@users.noreply.github.com>
2018-12-03 22:21:12 -0800

    DevExp: Part 2 of 2 - the codebase no longer uses const enums, enabling consumers to be able to use isolatedModules (#7119)
    * Completely eliminate const enums in our codebase
    
    * change back the IconNames
    
    * change file

--- packages/experiments/src/components/Tile/Tile.tsx
+++ packages/experiments/src/components/Tile/Tile.tsx
@@ -13,15 +13,17 @@ const SignalStyles: any = SignalStylesModule;
 const CheckStyles: any = CheckStylesModule;
 // tslint:enable:no-any
 
-export const enum TileLayoutValues {
-  nameplatePadding = 12,
-  largeNameplateNameHeight = 15,
-  smallNameplateNameHeight = 12,
-  nameplateMargin = 0,
-  largeNameplateActivityHeight = 20,
-  smallNameplateActivityHeight = 20,
-  foregroundMargin = 16
-}
+export const TileLayoutValues = {
+  nameplatePadding: 12 as 12,
+  largeNameplateNameHeight: 15 as 15,
+  smallNameplateNameHeight: 12 as 12,
+  nameplateMargin: 0 as 0,
+  largeNameplateActivityHeight: 20 as 20,
+  smallNameplateActivityHeight: 20 as 20,
+  foregroundMargin: 16 as 16
+};
+
+export type TileLayoutValues = typeof TileLayoutValues[keyof typeof TileLayoutValues];
 
 export interface ITileState {
   isSelected?: boolean;
