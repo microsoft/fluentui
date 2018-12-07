dff9ad9efac939e480950467ab01b265abbc6f9c
Natalie Ethell <natalie.ethell@microsoft.com>
2018-12-06 16:08:37 -0800

    last bit of Text cleanup

--- packages/experiments/src/components/Text/Text.styles.ts
+++ packages/experiments/src/components/Text/Text.styles.ts
@@ -3,20 +3,19 @@ import { ITextComponent } from './Text.types';
 export const TextStyles: ITextComponent['styles'] = props => {
   const { as, className, inline, theme, wrap, variant } = props;
   const { fonts } = theme;
-  const variantObject = fonts[variant!] || fonts.medium; // TODO: add default to fonts
+  const variantObject = variant && fonts[variant] ? fonts[variant] : fonts.medium;
 
   return {
     root: [
       theme.fonts.medium,
       {
         display: inline ? 'inline' : as === 'td' ? 'table-cell' : 'block',
-        fontFamily: variantObject.fontFamily || 'inherit',
-        fontSize: variantObject.fontSize || 'inherit',
-        // tslint:disable-next-line:no-any
-        fontWeight: (variantObject.fontWeight as any) || 'inherit',
-        color: variantObject.color || 'inherit',
-        mozOsxFontSmoothing: variantObject.MozOsxFontSmoothing,
-        webkitFontSmoothing: variantObject.WebkitFontSmoothing
+        fontFamily: (variantObject && variantObject.fontFamily) || 'inherit',
+        fontSize: (variantObject && variantObject.fontSize) || 'inherit',
+        fontWeight: (variantObject && variantObject.fontWeight) || 'inherit',
+        color: (variantObject && variantObject.color) || 'inherit',
+        mozOsxFontSmoothing: variantObject && variantObject.MozOsxFontSmoothing,
+        webkitFontSmoothing: variantObject && variantObject.WebkitFontSmoothing
       },
       !wrap && {
         whiteSpace: 'nowrap',
