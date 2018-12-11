7650240bb8f4a907029301b5418753308d927e5e
Rick Kessler <rkessler@microsoft.com>
2018-12-11 14:43:31 -0800

    color notes.

--- packages/ibiza-themes/src/ibiza/IbizaColors.ts
+++ packages/ibiza-themes/src/ibiza/IbizaColors.ts
@@ -1,5 +1,7 @@
 /* tslint:disable:no-any */
 export namespace LightColors {
+  export const errorText = '#e00b1c';
+  export const errorBorder = '#e00b1c';
   export const themeShade30 = '#003379';
   export const themeShade20 = '#0045a5';
   export const themeShade10 = '#0051c3';
@@ -12,6 +14,8 @@ export namespace LightColors {
 }
 
 export namespace DarkColors {
+  export const errorText = '#f63747';
+  export const errorBorder = '#e00b1c';
   export const themeShade50 = '#03060a';
   export const themeShade40 = '#0b1829';
   export const themeShade30 = '#152c4d';
@@ -45,6 +49,7 @@ export namespace NeutralColors {
   export const gray130 = '#605e5c';
 }
 
+// referenced colors from fluent
 export namespace CommunicationColors {
   export const shade30 = '#004578';
   export const shade20 = '#005a9e';
@@ -56,39 +61,8 @@ export namespace CommunicationColors {
   export const tint40 = '#eff6fc';
 }
 
+// referenced colors from fluent
 export namespace SharedColors {
-  export const pinkRed10 = '#750b1c';
   export const red20 = '#a4262c';
   export const red10 = '#d13438';
-  export const redOrange20 = '#603d30';
-  export const redOrange10 = '#da3b01';
-  export const orange30 = '#8e562e';
-  export const orange20 = '#ca5010';
-  export const orange10 = '#ffaa44';
-  export const yellow10 = '#fce100';
-  export const orangeYellow20 = '#986f0b';
-  export const orangeYellow10 = '#c19c00';
-  export const yellowGreen10 = '#8cbd18';
-  export const green20 = '#0b6a0b';
-  export const green10 = '#498205';
-  export const greenCyan10 = '#00ad56';
-  export const cyan40 = '#005e50';
-  export const cyan30 = '#005b70';
-  export const cyan20 = '#038387';
-  export const cyan10 = '#00b7c3';
-  export const cyanBlue20 = '#004e8c';
-  export const cyanBlue10 = '#0078d4';
-  export const blue10 = '#4f6bed';
-  export const blueMagenta40 = '#373277';
-  export const blueMagenta30 = '#5c2e91';
-  export const blueMagenta20 = '#8764b8';
-  export const blueMagenta10 = '#8378de';
-  export const magenta20 = '#881798';
-  export const magenta10 = '#c239b3';
-  export const magentaPink20 = '#9b0062';
-  export const magentaPink10 = '#e3008c';
-  export const gray40 = '#393939';
-  export const gray30 = '#7a7574';
-  export const gray20 = '#69797e';
-  export const gray10 = '#a0aeb2';
 }
