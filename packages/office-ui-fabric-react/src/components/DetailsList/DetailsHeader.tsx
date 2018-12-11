2fd20433d9d8a9c59063e701a45c1a7023fba0c5
Arko Gupta <arkgupta@microsoft.com>
2018-12-11 14:15:21 +0530

    changed display property to none when drop hint is hidden

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsHeader.tsx
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsHeader.tsx
@@ -417,14 +417,14 @@ export class DetailsHeader extends BaseComponent<IDetailsHeaderProps, IDetailsHe
 
   private _resetDropHints(): void {
     if (this._currentDropHintIndex >= 0) {
-      this._updateDropHintElement(this._dropHintDetails[this._currentDropHintIndex].dropHintElementRef, 'hidden');
+      this._updateDropHintElement(this._dropHintDetails[this._currentDropHintIndex].dropHintElementRef, 'none');
       this._currentDropHintIndex = Number.MIN_SAFE_INTEGER;
     }
   }
 
-  private _updateDropHintElement(element: HTMLElement, property: string) {
-    (element.childNodes[1] as HTMLElement).style.visibility = property;
-    (element.childNodes[0] as HTMLElement).style.visibility = property;
+  private _updateDropHintElement(element: HTMLElement, displayProperty: string) {
+    (element.childNodes[1] as HTMLElement).style.display = displayProperty;
+    (element.childNodes[0] as HTMLElement).style.display = displayProperty;
   }
 
   private _getDropHintPositions(): void {
@@ -544,7 +544,7 @@ export class DetailsHeader extends BaseComponent<IDetailsHeaderProps, IDetailsHe
       }
     } else if (currentDropHintIndex !== indexToUpdate && indexToUpdate >= 0) {
       this._resetDropHints();
-      this._updateDropHintElement(this._dropHintDetails[indexToUpdate].dropHintElementRef, 'visible');
+      this._updateDropHintElement(this._dropHintDetails[indexToUpdate].dropHintElementRef, 'inline-block');
       this._currentDropHintIndex = indexToUpdate;
     }
   }
