845edf2df203b7efaddf054011df700e1f964388
Kartik Nuli (Zen3 Infosolutions America Inc) <v-kanuli@microsoft.com>
2018-12-12 15:15:47 -0800

    Adding aria-hidden and alt text properties for thumbnail image

--- packages/dashboard/src/components/Card/examples/Card.MediumWide.Basic.Example.tsx
+++ packages/dashboard/src/components/Card/examples/Card.MediumWide.Basic.Example.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, CardContentType, CardSize, IAction, ICardProps, IThumbnailItemProps, Priority } from '@uifabric/dashboard';
+import { Card, CardContentType, CardSize, IAction, ICardProps, IThumbnailItemProps, Priority } from '../../../index';
 export class MediumWideCardBasicExample extends React.Component<{}, {}> {
   constructor(props: ICardProps) {
     super(props);
@@ -28,7 +28,9 @@ export class MediumWideCardBasicExample extends React.Component<{}, {}> {
         description: 'This is the first thumbnail item',
         handleThumbnailItemClick: () => {
           alert('First Item clicked');
-        }
+        },
+        altImageText: 'First item',
+        imageAriaHidden: true
       },
       {
         imageSource: '../../../public/images/download.jpg',
@@ -36,7 +38,8 @@ export class MediumWideCardBasicExample extends React.Component<{}, {}> {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
         handleThumbnailItemClick: () => {
           alert('Second Item clicked');
-        }
+        },
+        altImageText: 'Second item'
       }
     ];
 
