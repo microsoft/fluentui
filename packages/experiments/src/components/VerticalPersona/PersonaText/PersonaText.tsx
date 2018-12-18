7a2a3076cf5cc6be51f6df5912dfe00cda8d2557
Mark Polak <mark@thedutchies.com>
2018-12-18 21:26:02 +0100

    Add Slots

--- packages/experiments/src/components/VerticalPersona/PersonaText/PersonaText.tsx
+++ packages/experiments/src/components/VerticalPersona/PersonaText/PersonaText.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { Text } from '@uifabric/experiments/lib/Text';
 
-export const PersonaText = function personaText<T extends { children?: React.ReactChild; className?: string }>(
-  props: T
-): React.ReactElement<T> | null {
+export const PersonaText = <T extends { children?: React.ReactChild; className?: string }>(props: T): React.ReactElement<T> | null => {
   return props.children ? (
     <Text wrap className={props.className}>
       {props.children}
