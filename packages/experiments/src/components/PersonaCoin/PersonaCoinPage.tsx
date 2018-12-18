e781b82c33ced811e3b90e6b4250e358d5578376
Mark Polak <mark@thedutchies.com>
2018-12-18 15:51:20 +0100

    Make example pages functional components

--- packages/experiments/src/components/PersonaCoin/PersonaCoinPage.tsx
+++ packages/experiments/src/components/PersonaCoin/PersonaCoinPage.tsx
@@ -7,51 +7,47 @@ import { PersonaCoinSizeAndColorExample } from './examples/PersonaCoinSizeAndCol
 const PersonaCoinExampleCode = require('!raw-loader!@uifabric/experiments/src/components/PersonaCoin/examples/PersonaCoin.Example.tsx') as string;
 const PersonaCoinSizeAndColorExampleCode = require('!raw-loader!@uifabric/experiments/src/components/PersonaCoin/examples/PersonaCoinSizeAndColor.Example.tsx') as string;
 
-export class PersonaCoinPage extends React.Component<IComponentDemoPageProps, {}> {
-  public render(): JSX.Element {
-    return (
-      <ComponentPage
-        title=" PersonaCoin"
-        componentName=" PersonaCoin"
-        exampleCards={
-          <div>
-            <ExampleCard title="PersonaCoin basic usage" code={PersonaCoinExampleCode}>
-              <PersonaCoinExample />
-            </ExampleCard>
+export const PersonaCoinPage = (props: IComponentDemoPageProps) => {
+  return (
+    <ComponentPage
+      title=" PersonaCoin"
+      componentName=" PersonaCoin"
+      exampleCards={
+        <div>
+          <ExampleCard title="PersonaCoin basic usage" code={PersonaCoinExampleCode}>
+            <PersonaCoinExample />
+          </ExampleCard>
 
-            <ExampleCard title="PersonaCoin sizes and colors" code={PersonaCoinSizeAndColorExampleCode}>
-              <PersonaCoinSizeAndColorExample />
-            </ExampleCard>
-          </div>
-        }
-        propertiesTables={
-          <PropertiesTableSet
-            sources={[require<string>('!raw-loader!@uifabric/experiments/src/components/PersonaCoin/PersonaCoin.types.ts')]}
-          />
-        }
-        // overview={<div />}
-        // bestPractices={<div />}
-        dos={
-          <div>
-            <ul>
-              <li>Provide as few props as needed to render the coin</li>
-              <li>
-                Use the <code>text</code> prop instead of the <code>initials</code> prop
-              </li>
-            </ul>
-          </div>
-        }
-        donts={
-          <div>
-            <ul>
-              <li>
-                Change the colors of the initials or the coin unless you have a well grounded reason. (For consistency across products)
-              </li>
-            </ul>
-          </div>
-        }
-        isHeaderVisible={this.props.isHeaderVisible}
-      />
-    );
-  }
-}
+          <ExampleCard title="PersonaCoin sizes and colors" code={PersonaCoinSizeAndColorExampleCode}>
+            <PersonaCoinSizeAndColorExample />
+          </ExampleCard>
+        </div>
+      }
+      propertiesTables={
+        <PropertiesTableSet
+          sources={[require<string>('!raw-loader!@uifabric/experiments/src/components/PersonaCoin/PersonaCoin.types.ts')]}
+        />
+      }
+      // overview={<div />}
+      // bestPractices={<div />}
+      dos={
+        <div>
+          <ul>
+            <li>Provide as few props as needed to render the coin</li>
+            <li>
+              Use the <code>text</code> prop instead of the <code>initials</code> prop
+            </li>
+          </ul>
+        </div>
+      }
+      donts={
+        <div>
+          <ul>
+            <li>Change the colors of the initials or the coin unless you have a well grounded reason. (For consistency across products)</li>
+          </ul>
+        </div>
+      }
+      isHeaderVisible={props.isHeaderVisible}
+    />
+  );
+};
--- packages/experiments/src/components/VerticalPersona/VerticalPersonaPage.tsx
+++ packages/experiments/src/components/VerticalPersona/VerticalPersonaPage.tsx
@@ -5,40 +5,38 @@ import { VerticalPersonaExample } from './examples/VerticalPersona.Example';
 
 const VerticalPersonaExampleCode = require('!raw-loader!@uifabric/experiments/src/components/VerticalPersona/examples/VerticalPersona.Example.tsx') as string;
 
-export class VerticalPersonaPage extends React.Component<IComponentDemoPageProps, {}> {
-  public render(): JSX.Element {
-    return (
-      <ComponentPage
-        title=" VerticalPersona"
-        componentName=" VerticalPersona"
-        exampleCards={
-          <div>
-            <ExampleCard title="VerticalPersona basic usage" code={VerticalPersonaExampleCode}>
-              <VerticalPersonaExample />
-            </ExampleCard>
-          </div>
-        }
-        propertiesTables={
-          <PropertiesTableSet
-            sources={[require<string>('!raw-loader!@uifabric/experiments/src/components/VerticalPersona/VerticalPersona.types.ts')]}
-          />
-        }
-        // overview={<div />}
-        // bestPractices={<div />}
-        dos={
-          <div>
-            <ul>
-              <li>Provide as few props as needed to render the coin</li>
-            </ul>
-          </div>
-        }
-        donts={
-          <div>
-            <ul />
-          </div>
-        }
-        isHeaderVisible={this.props.isHeaderVisible}
-      />
-    );
-  }
-}
+export const VerticalPersonaPage = (props: IComponentDemoPageProps) => {
+  return (
+    <ComponentPage
+      title=" VerticalPersona"
+      componentName=" VerticalPersona"
+      exampleCards={
+        <div>
+          <ExampleCard title="VerticalPersona basic usage" code={VerticalPersonaExampleCode}>
+            <VerticalPersonaExample />
+          </ExampleCard>
+        </div>
+      }
+      propertiesTables={
+        <PropertiesTableSet
+          sources={[require<string>('!raw-loader!@uifabric/experiments/src/components/VerticalPersona/VerticalPersona.types.ts')]}
+        />
+      }
+      // overview={<div />}
+      // bestPractices={<div />}
+      dos={
+        <div>
+          <ul>
+            <li>Provide as few props as needed to render the coin</li>
+          </ul>
+        </div>
+      }
+      donts={
+        <div>
+          <ul />
+        </div>
+      }
+      isHeaderVisible={props.isHeaderVisible}
+    />
+  );
+};
