850951d2bc98e585b9ba1d68df84253b5d3f3eaf
Vitalie Braga <v-vibr@microsoft.com>
2018-12-18 20:13:46 -0800

    Fix import in test file

--- packages/office-ui-fabric-react/src/components/pickers/Suggestions/Suggestions.test.tsx
+++ packages/office-ui-fabric-react/src/components/pickers/Suggestions/Suggestions.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
 import { Suggestions } from './Suggestions';
-import { ISuggestionModel } from './SuggestionsController';
+import { ISuggestionModel } from './Suggestions.types';
 
 const suggestions = [
   'black',
