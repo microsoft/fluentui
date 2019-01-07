253af13aae07bbcc932998b2062d801aa756d851
Natalie Ethell <natalie.ethell@microsoft.com>
2019-01-07 15:18:18 -0800

    remove test code

--- apps/fabric-website-resources/src/components/pages/CheckboxPage.tsx
+++ apps/fabric-website-resources/src/components/pages/CheckboxPage.tsx
@@ -2,19 +2,5 @@ import * as React from 'react';
 
 import { DemoPage } from '../DemoPage';
 import { CheckboxPageProps } from 'office-ui-fabric-react/lib/components/Checkbox/Checkbox.doc';
-import { loadTheme, IPartialTheme } from 'office-ui-fabric-react/lib/Styling';
-
-const myTheme: IPartialTheme = {
-  fonts: {
-    medium: {
-      fontSize: '30px'
-    }
-  },
-  defaultFontStyle: {
-    fontFamily: 'Segoe UI'
-  }
-};
-
-loadTheme(myTheme, false);
 
 export const CheckboxPage = (props: { isHeaderVisible: boolean }) => <DemoPage {...{ ...CheckboxPageProps, ...props }} />;
