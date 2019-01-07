f59568b4639c25220d2ace743edf92d5e6211027
Ryan Kelly <rykelly@microsoft.com>
2019-01-07 10:23:42 -0800

    Commenting out azure-themes references in customizations for now

--- apps/fabric-website-resources/src/customizations/customizations.ts
+++ apps/fabric-website-resources/src/customizations/customizations.ts
@@ -1,15 +1,15 @@
 import { IExampleCardCustomizations, IAppCustomizations } from '@uifabric/example-app-base';
 import { FluentCustomizations } from '@uifabric/fluent-theme';
 import { DefaultCustomizations, TeamsCustomizations, WordCustomizations } from '@uifabric/theme-samples';
-import { AzureCustomizationsLight, AzureCustomizationsDark } from '@uifabric/azure-themes';
+// import { AzureCustomizationsLight, AzureCustomizationsDark } from '@uifabric/azure-themes';
 
 const exampleCardCustomizations: IExampleCardCustomizations[] = [
   { title: 'Default', customizations: DefaultCustomizations },
   { title: 'Fluent', customizations: FluentCustomizations },
   { title: 'Word', customizations: WordCustomizations },
-  { title: 'Teams', customizations: TeamsCustomizations },
-  { title: 'AzureLight', customizations: AzureCustomizationsLight },
-  { title: 'AzureDark', customizations: AzureCustomizationsDark }
+  { title: 'Teams', customizations: TeamsCustomizations }
+  // { title: 'AzureLight', customizations: AzureCustomizationsLight },
+  // { title: 'AzureDark', customizations: AzureCustomizationsDark }
 ];
 
 export const AppCustomizations: IAppCustomizations = {
