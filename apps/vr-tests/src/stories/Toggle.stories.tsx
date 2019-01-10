f08270a4c0d89efd294138b02bf217c6360f522a
Vitalie Braga <v-vibr@microsoft.com>
2019-01-10 12:50:17 -0800

    Fix stories

--- apps/vr-tests/src/stories/Toggle.stories.tsx
+++ apps/vr-tests/src/stories/Toggle.stories.tsx
@@ -19,6 +19,6 @@ storiesOf('Toggle', module)
   .addStory('Checked', () => <Toggle {...baseProps} defaultChecked={true} />, { rtl: true })
   .addStory('Unchecked', () => <Toggle {...baseProps} defaultChecked={false} />, { rtl: true })
   .addStory('Disabled checked', () => <Toggle {...baseProps} defaultChecked={true} disabled={true} />)
-  .addStory('Disabled unchecked', () => <Toggle {...baseProps} defaultChecked={false} disabled={true} />);
-  .addStory('With inline label', () => <Toggle {...baseProps} defaultChecked={true} disabled={false} inlineLabel={true} />);
+  .addStory('Disabled unchecked', () => <Toggle {...baseProps} defaultChecked={false} disabled={true} />)
+  .addStory('With inline label', () => <Toggle {...baseProps} defaultChecked={true} disabled={false} inlineLabel={true} />)
   .addStory('With inline label disabled', () => <Toggle {...baseProps} defaultChecked={true} disabled={true} inlineLabel={true} />);
