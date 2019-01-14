b8ef9ce7b04bb850e8430c4d4fe67fccd7bf1948
Natalie Ethell <natalie.ethell@microsoft.com>
2019-01-14 11:40:23 -0800

    add script and newly generated scss files

---
+++ packages/office-ui-fabric-react/scripts/loadTheme.js
@@ -0,0 +1,43 @@
+const { getTheme } = require('@uifabric/styling');
+const fs = require('fs');
+const path = require('path');
+
+const defaultTheme = getTheme(true);
+const fonts = defaultTheme.fonts;
+
+const lines = [];
+for (const fontName in fonts) {
+  const font = fonts[fontName];
+  for (const propName in font) {
+    const name = 'ms-font-' + fontName + '-' + propName;
+    lines.push(`$${name}: "[theme:${name}, default: ${font[propName]}]";`);
+  }
+}
+
+const srcRoot = './src/common';
+const fontsOutputFilename = '_themeVariables.scss';
+fs.writeFileSync(path.join(srcRoot, fontsOutputFilename), lines.join('\n'));
+
+// load palette
+
+const palette = defaultTheme.palette;
+const colorLines = [];
+for (const color in palette) {
+  const name = 'ms-font-' + color;
+  colorLines.push(`$${name}: "[theme:${color}, default: ${palette[color]}]";`);
+}
+
+const paletteOutputFilename = '_themePalette.scss';
+fs.writeFileSync(path.join(srcRoot, paletteOutputFilename), colorLines.join('\n'));
+
+// load semantic colors
+
+const semanticColors = defaultTheme.semanticColors;
+const semanticLines = [];
+for (const color in semanticColors) {
+  const name = color + 'Color';
+  semanticLines.push(`$${name}: '[theme:${color}, default: ${semanticColors[color]}]';`);
+}
+
+const semanticOutputFilename = '_semanticColors.scss';
+fs.writeFileSync(path.join(srcRoot, semanticOutputFilename), semanticLines.join('\n'));
