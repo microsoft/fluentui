ad66a786355b332c854d9e6c6f2afe43068f6236
Evan Levy <evlevy@microsoft.com>
2019-01-15 14:53:34 -0500

    Date-time package year picking (#7615)
    * Port year-picking to date-time calendar
    
    * Remove extraneous tabIndex=0 on buttons
    
    * Remove ms-DatePicker-* classNames
    
    * CalendarPicker interface for common styles
    
    * Consitent header state between pickers

--- packages/date-time/src/components/Calendar/CalendarYear/CalendarYear.styles.ts
+++ packages/date-time/src/components/Calendar/CalendarYear/CalendarYear.styles.ts
@@ -1,12 +1,12 @@
-import { ICalendarMonthStyleProps, ICalendarMonthStyles } from './CalendarMonth.types';
+import { ICalendarYearStyleProps, ICalendarYearStyles } from './CalendarYear.types';
 import { getStyles as getPickerStyles } from '../CalendarPicker/CalendarPicker.styles';
 
-export const getStyles = (props: ICalendarMonthStyleProps): ICalendarMonthStyles => {
+export const getStyles = (props: ICalendarYearStyleProps): ICalendarYearStyles => {
   /*  Return styles from the base class.
    *  If this component has extra styles not in the base, apply them here i.e.:
    *  const myStyle: IStyle = {
    *    display: "block"
-   *  };     *
+   *  };
    *  return {...getPickerStyles(props), myStyle};
    */
 
