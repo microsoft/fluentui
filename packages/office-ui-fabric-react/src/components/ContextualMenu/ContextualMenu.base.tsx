ffdb139e6c298a52cabef2371f820bfac163ed4e
Dan Marshall <danmar@microsoft.com>
2019-01-17 14:41:58 -0800

    key with fallbacks

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
@@ -531,7 +531,7 @@ export class ContextualMenuBase extends BaseComponent<IContextualMenuProps, ICon
 
     if (sectionProps.items && sectionProps.items.length > 0) {
       return (
-        <li role="presentation" key={sectionItem.key}>
+        <li role="presentation" key={sectionProps.key || sectionItem.key || `section-${index}`}>
           <div role="group">
             <ul className={this._classNames.list}>
               {sectionProps.topDivider && this._renderSeparator(index, menuClassNames, true, true)}
