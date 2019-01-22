610264c337e3d98f04467bb340e825d529dbc7e2
Elizabeth Craig <elcraig@microsoft.com>
2019-01-22 13:55:23 -0800

    build updates

--- scripts/tasks/build-codepen-examples.js
+++ scripts/tasks/build-codepen-examples.js
@@ -4,7 +4,7 @@ module.exports = function() {
   const path = require('path');
   const transformer = require('../codepen/codepen-examples-transform');
   const glob = require('glob');
-  const fse = require('fs-extra');
+  const fs = require('fs-extra');
   const async = require('async');
 
   const files = glob.sync(path.resolve(__dirname, '../../packages/*/src/components/**/examples/*Example*.tsx'));
@@ -14,12 +14,12 @@ module.exports = function() {
     let fileSource = '';
     let fd;
     try {
-      fd = fse.openSync(file, 'r');
-      fse.readSync(fd, buffer, 0, len, 0);
+      fd = fs.openSync(file, 'r');
+      fs.readSync(fd, buffer, 0, len, 0);
       fileSource = buffer.toString();
     } finally {
       if (fd) {
-        fse.closeSync(fd);
+        fs.closeSync(fd);
       }
     }
     return fileSource;
@@ -30,31 +30,27 @@ module.exports = function() {
       files,
       5,
       function(file, callback) {
-        if (readFileStart(file, 50).indexOf('@codepen') >= 0) {
-          let fileSource = fse.readFileSync(file).toString();
+        if (readFileStart(file, 150).includes('@codepen')) {
+          let fileSource = fs.readFileSync(file).toString();
           const exampleName = path.basename(file, '.tsx');
 
           // extract the name of the component (relies on component/examples/examplefile.tsx structure)
           const transformResult = transformer(fileSource);
           const dirPath = path.dirname(path.dirname(file)).replace(/((\b)src(\b))(?!.*\1)/, '$2lib/codepen$3');
 
-          if (!fse.existsSync(dirPath)) {
-            fse.mkdirpSync(dirPath);
+          if (!fs.existsSync(dirPath)) {
+            fs.mkdirpSync(dirPath);
           }
 
           const outPath = path.join(dirPath, exampleName + '.Codepen.txt');
           console.log('Writing: ' + outPath);
-          fse.writeFileSync(outPath, transformResult);
+          fs.writeFileSync(outPath, transformResult);
         }
 
         callback();
       },
       function(err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
+        err ? reject(err) : resolve();
       }
     );
   });
