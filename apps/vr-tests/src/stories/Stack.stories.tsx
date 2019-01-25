54892fd3a80e0651b606bd7c63b7b85c29a18dc3
REDMOND\humorimo <Humberto.Morimoto@microsoft.com>
2019-01-25 14:48:54 -0800

    Updating vr-tests.

--- apps/vr-tests/src/stories/Stack.stories.tsx
+++ apps/vr-tests/src/stories/Stack.stories.tsx
@@ -190,7 +190,7 @@ storiesOf('Stack', module)
     </Stack>
   ))
   .addStory('Vertical Stack - Shrinking items', () => (
-    <Stack {...defaultProps} gap={10} shrinkItems className={styles.fixedHeight}>
+    <Stack {...defaultProps} gap={10} className={styles.fixedHeight}>
       <Stack.Item className={styles.verticalShrinkItem}>1</Stack.Item>
       <Stack.Item preventShrink className={styles.verticalShrinkItem}>
         2 (does not shrink)
@@ -347,7 +347,7 @@ storiesOf('Stack', module)
     </Stack>
   ))
   .addStory('Horizontal Stack - Shrinking items', () => (
-    <Stack horizontal {...defaultProps} gap={10} shrinkItems>
+    <Stack horizontal {...defaultProps} gap={10}>
       <Stack.Item className={styles.horizontalShrinkItem}>1</Stack.Item>
       <Stack.Item preventShrink className={styles.horizontalShrinkItem}>
         2 (does not shrink)
