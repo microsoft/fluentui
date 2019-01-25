4835ddcba03c8204aae13f87c197e667b5dcaa3e
Vitalie Braga <vitaliebraga@gmail.com>
2019-01-25 08:07:18 -0800

    Coachmark: fixing how the theme is passed to styles plus some minor cleanup (#7797)
    * proper merge-styles pattern in Coachmark
    
    * Remove unused import
    
    * Update API file
    
    * Rush change file

--- packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
+++ packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
@@ -2574,6 +2574,7 @@ interface ICoachmarkProps extends React.ClassAttributes<CoachmarkBase> {
   beakHeight?: number;
   // @deprecated
   beakWidth?: number;
+  className?: string;
   // @deprecated
   collapsed?: boolean;
   color?: string;
@@ -2596,6 +2597,7 @@ interface ICoachmarkProps extends React.ClassAttributes<CoachmarkBase> {
   target: HTMLElement | string | null;
   // @deprecated
   teachingBubbleRef?: ITeachingBubble;
+  theme?: ITheme;
   // @deprecated
   width?: number;
 }
@@ -2622,6 +2624,7 @@ interface ICoachmarkState {
 interface ICoachmarkStyleProps {
   beaconColorOne?: string;
   beaconColorTwo?: string;
+  className?: string;
   // @deprecated
   collapsed?: boolean;
   // (undocumented)
@@ -2634,6 +2637,7 @@ interface ICoachmarkStyleProps {
   isCollapsed: boolean;
   isMeasured: boolean;
   isMeasuring: boolean;
+  theme?: ITheme;
   transformOrigin?: string;
   width?: string;
 }
