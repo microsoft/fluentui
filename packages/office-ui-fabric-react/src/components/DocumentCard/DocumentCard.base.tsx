microsoft/fluentui

commit 5b8befe55b8acb54c4ba88f0a651ac5318356426

DocumentCard: Convert to MergeStyles (#7584)
* Convert DocumentCardStatus to CSS-in-JS

* Remove unused import

* Convert DocumentCardTitle to MergeStyles

* Convert DocumentCardPreview to MergeStyles

* Correct extensions

* Convert DocumentCardLogo to MergeStyles

* Convert DocumentCardLocation to MergeStyles

* Convert DocumentCardActivity to MergeStyles

* Convert DocumentCardActions to MergeStyles

* Convert DocumentCard (root component) to MergeStyles

* Remove duplicate prop in interface

* Style subcomponents for the compact variant

* Add DocumentCardDetails component

* Fix typos

* Change let to const

* Update snapshots and fix errors found after running tests

* Update API

* Remove an invalid story (design does not have room for views in a compact card)

* Use full width decorator for the compact story (otherwise flex collapses it)

* Rename component files from TSX to TS

* Fix scopes

* Remove template strings

* Include an interface for each component and use it in componentRef

* Rename constants

* Fix a bug in a template string

* API update

* Add change file

* Update based on PR feedback

packages/office-ui-fabric-react/src/components/DocumentCard/DocumentCard.base.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { IDocumentCard, IDocumentCardProps, DocumentCardType } from './DocumentCard.types';
-import { BaseComponent, KeyCodes, css } from '../../Utilities';
-import * as stylesImport from './DocumentCard.scss';
-const styles: any = stylesImport;
+import { IProcessedStyleSet } from '../../Styling';
+import { BaseComponent, classNamesFunction, KeyCodes } from '../../Utilities';
+import { DocumentCardType, IDocumentCard, IDocumentCardProps, IDocumentCardStyleProps, IDocumentCardStyles } from './DocumentCard.types';
 
-export class DocumentCard extends BaseComponent<IDocumentCardProps, any> implements IDocumentCard {
+const getClassNames = classNamesFunction<IDocumentCardStyleProps, IDocumentCardStyles>();
+
+export class DocumentCardBase extends BaseComponent<IDocumentCardProps, any> implements IDocumentCard {
   public static defaultProps: IDocumentCardProps = {
     type: DocumentCardType.normal
   };
 
   private _rootElement = React.createRef<HTMLDivElement>();
+  private _classNames: IProcessedStyleSet<IDocumentCardStyles>;
 
   constructor(props: IDocumentCardProps) {
     super(props);
@@ -20,8 +22,15 @@ export class DocumentCard extends BaseComponent<IDocumentCardProps, any> impleme
   }
 
   public render(): JSX.Element {
-    const { onClick, onClickHref, children, className, type, accentColor } = this.props;
+    const { onClick, onClickHref, children, type, accentColor, styles, theme, className } = this.props;
     const actionable = onClick || onClickHref ? true : false;
+
+    this._classNames = getClassNames(styles!, {
+      theme: theme!,
+      className,
+      actionable,
+      compact: type === DocumentCardType.compact ? true : false
+    });
 
     // Override the border color if an accent color was provided (compact card only)
     let style;
@@ -32,7 +41,7 @@ export class DocumentCard extends BaseComponent<IDocumentCardProps, any> impleme
     }
 
     // if this element is actionable it should have an aria role
-    const role = this.props.role || (actionable ? (onClick ? 'button' : 'link') : undefined);
+    const role = this.props.role || actionable ? (onClick ? 'button' : 'link') : undefined;
     const tabIndex = actionable ? 0 : undefined;
 
     return (
@@ -41,15 +50,7 @@ export class DocumentCard extends BaseComponent<IDocumentCardProps, any> impleme
         tabIndex={tabIndex}
         data-is-focusable={actionable}
         role={role}
-        className={css(
-          'ms-DocumentCard',
-          styles.root,
-          {
-            ['ms-DocumentCard--actionable ' + styles.rootIsActionable]: actionable,
-            ['ms-DocumentCard--compact ' + styles.rootIsCompact]: type === DocumentCardType.compact ? true : false
-          },
-          className
-        )}
+        className={this._classNames.root}
         onKeyDown={actionable ? this._onKeyDown : undefined}
         onClick={actionable ? this._onClick : undefined}
         style={style}

apps/vr-tests/src/stories/DocumentCard.stories.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
-import { FabricDecorator } from '../utilities';
+import { FabricDecorator, FabricDecoratorFullWidth } from '../utilities';
 import {
   DocumentCard,
   DocumentCardPreview,
@@ -10,7 +10,7 @@ import {
   DocumentCardActivity,
   DocumentCardType,
   ImageFit,
-  DocumentCardActions,
+  DocumentCardDetails,
   Fabric
 } from 'office-ui-fabric-react';
 
@@ -118,35 +118,19 @@ storiesOf('DocumentCard', module)
         {DocActivity}
       </DocumentCard>
     </Fabric>
-  ))
+  ));
+
+storiesOf('DocumentCard', module)
+  .addDecorator(FabricDecoratorFullWidth)
+  .addDecorator(story => <Screener steps={new Screener.Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>{story()}</Screener>)
   .addStory('Compact', () => (
     <Fabric>
       <DocumentCard type={DocumentCardType.compact} onClickHref="http://bing.com">
         <DocumentCardPreview {...previewPropsCompact} />
-        <DocumentCardTitle title="4 files were uploaded" shouldTruncate={true} />
-        {DocActivity}
-      </DocumentCard>
-    </Fabric>
-  ))
-  .addStory(
-    'With Views',
-    () => (
-      <Fabric>
-        <DocumentCard type={DocumentCardType.compact} onClickHref="http://bing.com">
-          <DocumentCardPreview {...previewPropsCompact} />
+        <DocumentCardDetails>
           <DocumentCardTitle title="4 files were uploaded" shouldTruncate={true} />
           {DocActivity}
-          <DocumentCardActions
-            actions={[
-              {
-                iconProps: { iconName: 'Share' },
-                ariaLabel: 'share action'
-              }
-            ]}
-            views={432}
-          />
-        </DocumentCard>
-      </Fabric>
-    ),
-    { rtl: true }
-  );
+        </DocumentCardDetails>
+      </DocumentCard>
+    </Fabric>
+  ));

packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
@@ -972,67 +972,6 @@ enum DialogType {
 export function disableBodyScroll(): void;
 
 // @public (undocumented)
-class DocumentCard extends BaseComponent<IDocumentCardProps, any>, implements IDocumentCard {
-  constructor(props: IDocumentCardProps);
-  // (undocumented)
-  static defaultProps: IDocumentCardProps;
-  // (undocumented)
-  focus(): void;
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardActions extends BaseComponent<IDocumentCardActionsProps, any> {
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardActivity extends BaseComponent<IDocumentCardActivityProps, any> {
-  // (undocumented)
-  render(): JSX.Element | null;
-}
-
-// @public (undocumented)
-class DocumentCardLocation extends BaseComponent<IDocumentCardLocationProps, any> {
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardLogo extends BaseComponent<IDocumentCardLogoProps, any> {
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardPreview extends BaseComponent<IDocumentCardPreviewProps, any> {
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardStatus extends BaseComponent<IDocumentCardStatusProps, any> {
-  constructor(props: IDocumentCardStatusProps);
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
-class DocumentCardTitle extends BaseComponent<IDocumentCardTitleProps, IDocumentCardTitleState> {
-  constructor(props: IDocumentCardTitleProps);
-  // (undocumented)
-  componentDidMount(): void;
-  // (undocumented)
-  componentDidUpdate(): void;
-  // (undocumented)
-  componentWillReceiveProps(newProps: IDocumentCardTitleProps): void;
-  // (undocumented)
-  render(): JSX.Element;
-}
-
-// @public (undocumented)
 enum DocumentCardType {
   compact = 1,
   normal = 0
@@ -7311,10 +7250,40 @@ interface IDocumentCard {
 }
 
 // @public (undocumented)
-interface IDocumentCardActionsProps extends React.ClassAttributes<DocumentCardActions> {
+interface IDocumentCardActions {
+}
+
+// WARNING: The type "DocumentCardActionsBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardActionsProps extends React.ClassAttributes<DocumentCardActionsBase> {
   actions: IButtonProps[];
-  componentRef?: IRefObject<{}>;
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardActions>;
+  styles?: IStyleFunctionOrObject<IDocumentCardActionsStyleProps, IDocumentCardActionsStyles>;
+  theme?: ITheme;
   views?: Number;
+}
+
+// @public (undocumented)
+interface IDocumentCardActionsStyleProps {
+  className?: string;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardActionsStyles {
+  // (undocumented)
+  action: IStyle;
+  // (undocumented)
+  root: IStyle;
+  // (undocumented)
+  views: IStyle;
+  // (undocumented)
+  viewsIcon: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardActivity {
 }
 
 // @public (undocumented)
@@ -7326,27 +7295,123 @@ interface IDocumentCardActivityPerson {
   profileImageSrc: string;
 }
 
+// WARNING: The type "DocumentCardActivityBase" needs to be exported by the package (e.g. added to index.ts)
 // @public (undocumented)
-interface IDocumentCardActivityProps extends React.ClassAttributes<DocumentCardActivity> {
+interface IDocumentCardActivityProps extends React.ClassAttributes<DocumentCardActivityBase> {
   activity: string;
-  componentRef?: IRefObject<{}>;
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardActivity>;
   people: IDocumentCardActivityPerson[];
+  styles?: IStyleFunctionOrObject<IDocumentCardActivityStyleProps, IDocumentCardActivityStyles>;
+  theme?: ITheme;
 }
 
 // @public (undocumented)
-interface IDocumentCardLocationProps extends React.ClassAttributes<DocumentCardLocation> {
+interface IDocumentCardActivityStyleProps {
+  className?: string;
+  multiplePeople?: boolean;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardActivityStyles {
+  // (undocumented)
+  activity: IStyle;
+  // (undocumented)
+  avatar: IStyle;
+  // (undocumented)
+  avatars: IStyle;
+  // (undocumented)
+  details: IStyle;
+  // (undocumented)
+  name: IStyle;
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardDetails {
+}
+
+// WARNING: The type "DocumentCardDetailsBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardDetailsProps extends React.Props<DocumentCardDetailsBase> {
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardDetails>;
+  styles?: IStyleFunctionOrObject<IDocumentCardDetailsStyleProps, IDocumentCardDetailsStyles>;
+  theme?: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardDetailsStyleProps {
+  className?: string;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardDetailsStyles {
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardLocation {
+}
+
+// WARNING: The type "DocumentCardLocationBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardLocationProps extends React.ClassAttributes<DocumentCardLocationBase> {
   ariaLabel?: string;
-  componentRef?: IRefObject<{}>;
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardLocation>;
   location: string;
   locationHref?: string;
   onClick?: (ev?: React.MouseEvent<HTMLElement>) => void;
+  styles?: IStyleFunctionOrObject<IDocumentCardLocationStyleProps, IDocumentCardLocationStyles>;
+  theme?: ITheme;
 }
 
 // @public (undocumented)
-interface IDocumentCardLogoProps extends React.ClassAttributes<DocumentCardLogo> {
-  componentRef?: IRefObject<{}>;
+interface IDocumentCardLocationStyleProps {
+  className?: string;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardLocationStyles {
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardLogo {
+}
+
+// WARNING: The type "DocumentCardLogoBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardLogoProps extends React.ClassAttributes<DocumentCardLogoBase> {
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardLogo>;
   logoIcon: string;
   logoName?: string;
+  styles?: IStyleFunctionOrObject<IDocumentCardLogoStyleProps, IDocumentCardLogoStyles>;
+  theme?: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardLogoStyleProps {
+  className?: string;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardLogoStyles {
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardPreview {
 }
 
 // @public (undocumented)
@@ -7371,44 +7436,120 @@ interface IDocumentCardPreviewImage {
 
 // @public (undocumented)
 interface IDocumentCardPreviewProps extends IBaseProps<{}> {
-  componentRef?: IRefObject<{}>;
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardPreview>;
   getOverflowDocumentCountText?: (overflowCount: number) => string;
   previewImages: IDocumentCardPreviewImage[];
+  styles?: IStyleFunctionOrObject<IDocumentCardPreviewStyleProps, IDocumentCardPreviewStyles>;
+  theme?: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardPreviewStyleProps {
+  className?: string;
+  isFileList?: boolean;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardPreviewStyles {
+  // (undocumented)
+  fileList: IStyle;
+  // (undocumented)
+  fileListIcon: IStyle;
+  // (undocumented)
+  fileListOverflowText: IStyle;
+  // (undocumented)
+  previewFileTypeIcon: IStyle;
+  // (undocumented)
+  previewIcon: IStyle;
+  // (undocumented)
+  root: IStyle;
 }
 
 // @public (undocumented)
 interface IDocumentCardProps extends IBaseProps<IDocumentCard> {
   // @deprecated
   accentColor?: string;
+  children?: React.ReactNode;
   className?: string;
   componentRef?: IRefObject<IDocumentCard>;
   onClick?: (ev?: React.SyntheticEvent<HTMLElement>) => void;
   onClickHref?: string;
   role?: string;
+  styles?: IStyleFunctionOrObject<IDocumentCardStyleProps, IDocumentCardStyles>;
+  theme?: ITheme;
   type?: DocumentCardType;
 }
 
 // @public (undocumented)
-interface IDocumentCardStatusProps extends React.ClassAttributes<DocumentCardStatus> {
-  componentRef?: IRefObject<{}>;
+interface IDocumentCardStatus {
+}
+
+// WARNING: The type "DocumentCardStatusBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardStatusProps extends React.Props<DocumentCardStatusBase> {
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardStatus>;
   status: string;
   statusIcon?: string;
+  styles?: IStyleFunctionOrObject<IDocumentCardStatusStyleProps, IDocumentCardStatusStyles>;
+  theme?: ITheme;
 }
 
 // @public (undocumented)
-interface IDocumentCardTitleProps extends React.ClassAttributes<DocumentCardTitle> {
-  componentRef?: IRefObject<{}>;
+interface IDocumentCardStatusStyleProps {
+  className?: string;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardStatusStyles {
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardStyleProps {
+  actionable?: boolean;
+  className?: string;
+  compact?: boolean;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardStyles {
+  // (undocumented)
+  root: IStyle;
+}
+
+// @public (undocumented)
+interface IDocumentCardTitle {
+}
+
+// WARNING: The type "DocumentCardTitleBase" needs to be exported by the package (e.g. added to index.ts)
+// @public (undocumented)
+interface IDocumentCardTitleProps extends React.ClassAttributes<DocumentCardTitleBase> {
+  className?: string;
+  componentRef?: IRefObject<IDocumentCardTitle>;
   shouldTruncate?: boolean;
   showAsSecondaryTitle?: boolean;
+  styles?: IStyleFunctionOrObject<IDocumentCardTitleStyleProps, IDocumentCardTitleStyles>;
+  theme?: ITheme;
   title: string;
 }
 
 // @public (undocumented)
-interface IDocumentCardTitleState {
+interface IDocumentCardTitleStyleProps {
+  className?: string;
+  showAsSecondaryTitle?: boolean;
+  theme: ITheme;
+}
+
+// @public (undocumented)
+interface IDocumentCardTitleStyles {
   // (undocumented)
-  truncatedTitleFirstPiece?: string;
-  // (undocumented)
-  truncatedTitleSecondPiece?: string;
+  root: IStyle;
 }
 
 // @public (undocumented)
@@ -12568,6 +12709,15 @@ module ZIndexes {
 // WARNING: Unsupported export: DialogContent
 // WARNING: Unsupported export: DialogFooter
 // WARNING: Unsupported export: VerticalDivider
+// WARNING: Unsupported export: DocumentCard
+// WARNING: Unsupported export: DocumentCardActions
+// WARNING: Unsupported export: DocumentCardActivity
+// WARNING: Unsupported export: DocumentCardDetails
+// WARNING: Unsupported export: DocumentCardLocation
+// WARNING: Unsupported export: DocumentCardPreview
+// WARNING: Unsupported export: DocumentCardTitle
+// WARNING: Unsupported export: DocumentCardLogo
+// WARNING: Unsupported export: DocumentCardStatus
 // WARNING: Unsupported export: Dropdown
 // WARNING: Unsupported export: IDropdownStyleProps
 // WARNING: Unsupported export: people

packages/office-ui-fabric-react/src/components/DocumentCard/DocumentCard.scss
@@ -1,321 +1,0 @@
-@import '../../common/common';
-
-.root {
-  -webkit-font-smoothing: antialiased;
-  background-color: $ms-color-white;
-  border: 1px solid $ms-color-neutralLight;
-  box-sizing: border-box;
-  max-width: 320px;
-  min-width: 206px;
-  user-select: none;
-  position: relative;
-}
-
-.rootIsActionable:hover {
-  cursor: pointer;
-  border-color: $ms-color-neutralTertiaryAlt;
-
-  // Place a heavier border within the document card
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    border: 1px solid $ms-color-neutralTertiaryAlt;
-    pointer-events: none;
-  }
-}
-
-// When a DocumentCardTitle follows a DocumentCardLocation, we want to reduce the padding
-.location + .title {
-  padding-top: 4px;
-}
-
-// Modifier: Compact layout
-.rootIsCompact {
-  display: flex;
-  max-width: 480px;
-  height: 109px;
-  :global(.ms-DocumentCard-details) {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    justify-content: space-between;
-    overflow: hidden;
-  }
-
-  .preview {
-    border-bottom: none; // Remove the usual border from the preview
-    max-height: 106px;
-    max-width: 144px;
-
-    .icon {
-      :global(.ms-Image-image) {
-        max-height: 32px;
-        max-width: 32px;
-      }
-    }
-  }
-
-  .preview {
-    @include border-right(1px, solid, $ms-color-neutralLight);
-  }
-
-  .previewIconContainer {
-    height: 100%;
-  }
-
-  .title {
-    padding: 12px 16px 8px 16px;
-    @include ms-font-m-plus;
-    line-height: 16px;
-  }
-
-  .activity {
-    padding-bottom: 12px;
-  }
-}
-
-/** Actions **/
-$ms-DocumentCardActions-actionSize: 34px;
-$ms-DocumentCardActions-horizontalPadding: 12px;
-$ms-DocumentCardActions-verticalPadding: 4px;
-
-.actions {
-  height: $ms-DocumentCardActions-actionSize;
-  padding: $ms-DocumentCardActions-verticalPadding $ms-DocumentCardActions-horizontalPadding;
-  position: relative;
-}
-
-.action {
-  @include float(left);
-  @include ms-margin-right(4px);
-  color: $ms-color-neutralSecondary;
-  cursor: pointer;
-
-  :global(.ms-Button) {
-    font-size: 16px;
-    height: $ms-DocumentCardActions-actionSize;
-    width: $ms-DocumentCardActions-actionSize;
-  }
-
-  :global(.ms-Button:hover) {
-    :global(.ms-Button-icon) {
-      color: $buttonTextColor;
-      cursor: pointer;
-    }
-  }
-}
-
-.views {
-  @include text-align(right);
-  line-height: $ms-DocumentCardActions-actionSize;
-}
-
-.viewsIcon {
-  @include ms-margin-right(8px);
-  font-size: $ms-icon-size-m;
-  vertical-align: top;
-}
-
-/** Activity **/
-$ms-DocumentCardActivity-horizontalPadding: 16px;
-$ms-DocumentCardActivity-imageSize: 32px;
-$ms-DocumentCardActivity-verticalPadding: 8px;
-$ms-DocumentCardActivity-personaTextGutter: 8px;
-
-.activity {
-  padding: $ms-DocumentCardActivity-verticalPadding $ms-DocumentCardActivity-horizontalPadding;
-  position: relative;
-}
-
-.activityIsMultiplePeople {
-  .avatar:nth-of-type(2) {
-    @include margin-left(-16px);
-  }
-
-  .activityDetails {
-    @include ms-left(
-      $ms-DocumentCardActivity-horizontalPadding + ($ms-DocumentCardActivity-imageSize * 1.5) +
-        $ms-DocumentCardActivity-personaTextGutter
-    );
-  }
-}
-
-.avatars {
-  @include margin-left(-2px); // Avatars sit outside of the usual padding for visual balance
-  height: 32px;
-}
-
-.avatar {
-  display: inline-block;
-  vertical-align: top;
-  position: relative;
-  text-align: center;
-  width: $ms-DocumentCardActivity-imageSize;
-  height: $ms-DocumentCardActivity-imageSize;
-
-  &:after {
-    content: '';
-    position: absolute;
-    left: -1px;
-    top: -1px;
-    right: -1px;
-    bottom: -1px;
-    border: 2px solid $ms-color-white; // Match the background of the card
-    border-radius: 50%;
-  }
-}
-
-.activityDetails {
-  @include ms-left(
-    $ms-DocumentCardActivity-horizontalPadding + $ms-DocumentCardActivity-imageSize +
-      $ms-DocumentCardActivity-personaTextGutter
-  );
-  height: $ms-DocumentCardActivity-imageSize;
-  position: absolute;
-  top: $ms-DocumentCardActivity-verticalPadding;
-  width: calc(
-    100% - #{$ms-DocumentCardActivity-horizontalPadding + $ms-DocumentCardActivity-imageSize +
-      $ms-DocumentCardActivity-personaTextGutter + $ms-DocumentCardActivity-horizontalPadding}
-  );
-}
-
-.name,
-.activityActivity {
-  display: block;
-  @include ms-font-s;
-  color: $ms-color-neutralSecondary;
-  line-height: 15px;
-  height: 15px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-
-.name {
-  color: $ms-color-neutralPrimary;
-  @include ms-fontWeight-semibold;
-}
-
-/** Location **/
-.location {
-  @include ms-font-s;
-  color: $ms-color-neutralPrimary;
-  display: block;
-  padding: 8px 16px;
-  position: relative;
-  text-decoration: none;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-
-  &:hover {
-    color: $ms-color-themePrimary;
-    cursor: pointer;
-  }
-}
-
-/** Preview **/
-.preview {
-  border-bottom: 1px solid $ms-color-neutralLight;
-  position: relative;
-  background-color: $ms-color-neutralLighterAlt;
-  overflow: hidden;
-
-  &.previewIsFileList {
-    background-color: $ms-color-white;
-  }
-}
-
-.previewIconContainer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.icon {
-  @include ms-left(10px);
-  bottom: 10px;
-  position: absolute;
-}
-
-.fileList {
-  @include padding(16px, 16px, 0, 16px);
-  list-style-type: none;
-  margin: 0;
-
-  li {
-    height: 16px;
-    line-height: 16px; // Vertically center the text
-    margin-bottom: 8px;
-    overflow: hidden;
-    @include ms-padding-left(24px); // Make room for icon
-    position: relative;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  a {
-    font-size: $ms-font-size-s;
-    text-decoration: none;
-    color: $ms-color-neutralDark;
-
-    &:hover {
-      color: $ms-color-themePrimary;
-    }
-  }
-}
-
-.fileListIcon {
-  @include ms-left(0);
-  position: absolute;
-  top: 0;
-}
-
-.fileListMore {
-  @include padding(0px, 16px, 8px, 16px);
-  display: block;
-  font-size: $ms-font-size-s;
-}
-
-/** Title **/
-$DocumentCard-title-lineHeight: 21px;
-.title {
-  padding: 8px 16px;
-  display: block;
-  @include ms-font-l;
-  color: $ms-color-neutralPrimary;
-  height: 38px; // Two lines of text, making sure the third line is hidden
-  line-height: $DocumentCard-title-lineHeight;
-  overflow: hidden;
-  word-wrap: break-word;
-}
-
-.secondaryTitle {
-  padding: 8px 16px;
-  display: block;
-  @include ms-font-m;
-  color: $ms-color-neutralSecondary;
-  line-height: 18px;
-  overflow: hidden;
-  word-wrap: break-word;
-  height: 45px;
-}
-
-.logo {
-  font-size: 32px;
-  color: $ms-color-themePrimary;
-  display: block;
-  @include ms-padding(16px, 16px, 0, 16px);
-}
-
-.status {
-  @include ms-margin(8px, 16px, 8px, 16px);
-  @include ms-font-m;
-  color: $ms-color-neutralPrimary;
-  background-color: $ms-color-neutralLighter;
-  height: 32px;
-}

packages/office-ui-fabric-react/src/components/DocumentCard/DocumentCard.styles.ts
@@ -1,0 +1,90 @@
+import { getGlobalClassNames, FontSizes } from '../../Styling';
+import { IDocumentCardStyleProps, IDocumentCardStyles } from './DocumentCard.types';
+import { GlobalClassNames as previewClassNames } from './DocumentCardPreview.styles';
+import { GlobalClassNames as activityClassNames } from './DocumentCardActivity.styles';
+import { GlobalClassNames as titleClassNames } from './DocumentCardTitle.styles';
+import { GlobalClassNames as locationClassNames } from './DocumentCardLocation.styles';
+
+const GlobalClassNames = {
+  root: 'ms-DocumentCard',
+  rootActionable: 'ms-DocumentCard--actionable',
+  rootCompact: 'ms-DocumentCard--compact'
+};
+
+export const getStyles = (props: IDocumentCardStyleProps): IDocumentCardStyles => {
+  const { className, theme, actionable, compact } = props;
+  const { palette } = theme;
+
+  const classNames = getGlobalClassNames(GlobalClassNames, theme);
+
+  return {
+    root: [
+      classNames.root,
+      {
+        WebkitFontSmoothing: 'antialiased',
+        backgroundColor: palette.white,
+        border: `1px solid ${palette.neutralLight}`,
+        boxSizing: 'border-box',
+        maxWidth: '320px',
+        minWidth: '206px',
+        userSelect: 'none',
+        position: 'relative',
+        selectors: {
+          [`.${locationClassNames.root} + .${titleClassNames.root}`]: {
+            paddingTop: '4px'
+          }
+        }
+      },
+      actionable && [
+        classNames.rootActionable,
+        {
+          selectors: {
+            ':hover': {
+              cursor: 'pointer',
+              borderColor: palette.neutralTertiaryAlt
+            },
+            ':hover:after': {
+              content: '" "',
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+              border: `1px solid ${palette.neutralTertiaryAlt}`,
+              pointerEvents: 'none'
+            }
+          }
+        }
+      ],
+      compact && [
+        classNames.rootCompact,
+        {
+          display: 'flex',
+          maxWidth: '480px',
+          height: '109px',
+          selectors: {
+            [`.${previewClassNames.root}`]: {
+              borderRight: `1px solid ${palette.neutralLight}`,
+              borderBottom: 0, // Remove the usual border from the preview
+              maxHeight: '106px',
+              maxWidth: '144px'
+            },
+            [`.${previewClassNames.previewFileTypeIcon}`]: {
+              maxHeight: '32px',
+              maxWidth: '32px'
+            },
+            [`.${activityClassNames.root}`]: {
+              paddingBottom: '12px'
+            },
+            [`.${titleClassNames.root}`]: {
+              paddingBottom: '12px 16px 8px 16px',
+              fontSize: FontSizes.mediumPlus,
+              lineHeight: '16px'
+            }
+          }
+        }
+      ],
+      className
+    ]
+  };
+};

packages/office-ui-fabric-react/src/components/DocumentCard/DocumentCard.ts
@@ -1,0 +1,11 @@
+import { styled } from '../../Utilities';
+import { DocumentCardBase } from './DocumentCard.base';
+import { getStyles } from './DocumentCard.styles';
+import { IDocumentCardProps, IDocumentCardStyleProps, IDocumentCardStyles } from './DocumentCard.types';
+
+export const DocumentCard = styled<IDocumentCardProps, IDocumentCardStyleProps, IDocumentCardStyles>(
+  DocumentCardBase,
+  getStyles,
+  undefined,
+  { scope: 'DocumentCard' }
+);

commit 454f0cda8a6c13984ec41ab2b1908b5248cd2b9b

Fix the regression in DocumentCard role (#7796)
* Fix the regression in DocumentCard role by commit 5b8befe. It should always use props.role if it is defined.

* change file

packages/office-ui-fabric-react/src/components/DocumentCard/DocumentCard.base.tsx
@@ -42,7 +42,7 @@ export class DocumentCardBase extends BaseComponent<IDocumentCardProps, any> imp
     }
 
     // if this element is actionable it should have an aria role
-    const role = this.props.role || actionable ? (onClick ? 'button' : 'link') : undefined;
+    const role = this.props.role || (actionable ? (onClick ? 'button' : 'link') : undefined);
     const tabIndex = actionable ? 0 : undefined;
 
     return (