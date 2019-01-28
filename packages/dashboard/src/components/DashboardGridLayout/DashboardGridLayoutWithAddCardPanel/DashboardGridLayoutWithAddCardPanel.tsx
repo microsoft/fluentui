0923a1d1b538ba0a1a365ecd0a17265a17a51be4
Rajesh Goriga <gorigarajesh@gmail.com>
2019-01-28 22:04:37 +0530

    Add cardId for dashboardGridLayout onLayoutChange (#7765)
    * Add cardId for dashboardGridLayout onLayoutChange
    
    * Add change file
    
    * using Object.is()  for comaring two objects

--- packages/dashboard/src/components/DashboardGridLayout/DashboardGridLayoutWithAddCardPanel/DashboardGridLayoutWithAddCardPanel.tsx
+++ packages/dashboard/src/components/DashboardGridLayout/DashboardGridLayoutWithAddCardPanel/DashboardGridLayoutWithAddCardPanel.tsx
@@ -189,6 +189,11 @@ export class DashboardGridLayoutWithAddCardPanel extends BaseComponent<
           layout: newLayout,
           cardNodes
         });
+        if (Object.is(newLayout, this.state.layout)) {
+          if (this.props.onLayoutChange) {
+            this.props.onLayoutChange(newLayout, cardSelected[0].id);
+          }
+        }
       }
     } else {
       const newLayout: DashboardGridBreakpointLayouts = { lg: [] };
@@ -217,8 +222,8 @@ export class DashboardGridLayoutWithAddCardPanel extends BaseComponent<
         newLayout.lg!.push(itemLayout);
       });
 
-      // for object comparision using JSON stringify
-      if (JSON.stringify(newLayout) !== JSON.stringify(this.state.layout)) {
+      // for object comparision using Object.id method
+      if (Object.is(newLayout, this.state.layout)) {
         if (this.props.onLayoutChange) {
           this.props.onLayoutChange(newLayout);
         }
@@ -260,6 +265,9 @@ export class DashboardGridLayoutWithAddCardPanel extends BaseComponent<
         layout,
         cardNodes
       });
+      if (this.props.onLayoutChange) {
+        this.props.onLayoutChange(layout, cardSelected[0].id);
+      }
       // scroll to the card that was added to the layout
       this._async.setTimeout(() => {
         if (document.getElementById(cardId + 'dglCard')) {
