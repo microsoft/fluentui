de7e6d8a55b45b0f89fe611cbd1254bf9acc4971
REDMOND\naethell <naethell@microsoft.com>
2019-01-29 12:08:33 -0800

    update api

--- packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
+++ packages/office-ui-fabric-react/etc/office-ui-fabric-react.api.ts
@@ -12572,10 +12572,6 @@ class TeachingBubbleBase extends BaseComponent<ITeachingBubbleProps, ITeachingBu
 class TeachingBubbleContentBase extends BaseComponent<ITeachingBubbleProps, ITeachingBubbleState> {
   constructor(props: ITeachingBubbleProps);
   // (undocumented)
-  componentDidMount(): void;
-  // (undocumented)
-  componentWillUnmount(): void;
-  // (undocumented)
   static defaultProps: {
     hasCondensedHeadline: boolean;
     imageProps: {
