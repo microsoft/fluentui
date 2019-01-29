54495955c73f540514f863acadc1b1bb052954dd
Jason Gore <jagore@microsoft.com>
2019-01-29 15:54:46 -0800

    Consolidate async example.

--- packages/experiments/src/slots/examples/Slots.Async.Example.tsx
+++ packages/experiments/src/slots/examples/Slots.Async.Example.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import { CollapsibleSection } from '@uifabric/experiments';
+import {
+  CollapsibleSection,
+  ICollapsibleSectionProps,
+  ICollapsibleSectionTitleProps,
+  ICollapsibleSectionTitleStylesReturnType
+} from '@uifabric/experiments';
 import { Label, Spinner } from 'office-ui-fabric-react';
 
 // Mock async data container component
@@ -26,7 +31,42 @@ class AsyncData extends React.Component<IAsyncDataProps, { loading: boolean }> {
   }
 }
 
-// tslint:disable:jsx-no-lambda
+const titleTextStyles: ICollapsibleSectionTitleProps['styles'] = (props, theme): ICollapsibleSectionTitleStylesReturnType => ({
+  text: [
+    theme.fonts.large,
+    {
+      fontWeight: 800
+    }
+  ]
+});
+
+const titleTextRender: ICollapsibleSectionTitleProps['text'] = render =>
+  render(
+    (ComponentType, props) => (
+      <AsyncData
+        data="done"
+        // tslint:disable-next-line:jsx-no-lambda
+        render={data => (data ? <ComponentType {...props} /> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />)}
+      />
+    ),
+    'Title loaded'
+  );
+
+const bodyRender: ICollapsibleSectionProps['body'] = render =>
+  render((ComponentType, props) => (
+    <AsyncData
+      data="done"
+      // tslint:disable-next-line:jsx-no-lambda
+      render={data => (
+        <div style={{ border: '1px solid black' }}>
+          <ComponentType {...props}>
+            {data ? <Label>{props.children}</Label> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />}
+          </ComponentType>
+        </div>
+      )}
+    />
+  ));
+
 export class SlotsAsyncExample extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
@@ -35,39 +75,10 @@ export class SlotsAsyncExample extends React.Component<{}, {}> {
           key={1}
           defaultCollapsed={true}
           title={{
-            styles: (props, theme) => ({
-              text: [
-                theme.fonts.large,
-                {
-                  fontWeight: 800
-                }
-              ]
-            }),
-            text: render =>
-              render(
-                (ComponentType, props) => (
-                  <AsyncData
-                    data="done"
-                    render={data => (data ? <ComponentType {...props} /> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />)}
-                  />
-                ),
-                'Title loaded'
-              )
+            styles: titleTextStyles,
+            text: titleTextRender
           }}
-          body={render =>
-            render((ComponentType, props) => (
-              <AsyncData
-                data="done"
-                render={data => (
-                  <div style={{ border: '1px solid black' }}>
-                    <ComponentType {...props}>
-                      {data ? <Label>{props.children}</Label> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />}
-                    </ComponentType>
-                  </div>
-                )}
-              />
-            ))
-          }
+          body={bodyRender}
         >
           Data loaded
         </CollapsibleSection>
@@ -75,43 +86,13 @@ export class SlotsAsyncExample extends React.Component<{}, {}> {
           key={1}
           defaultCollapsed={true}
           title={{
-            styles: (props, theme) => ({
-              text: [
-                theme.fonts.large,
-                {
-                  fontWeight: 800
-                }
-              ]
-            }),
-            text: render =>
-              render(
-                (ComponentType, props) => (
-                  <AsyncData
-                    data="done"
-                    render={data => (data ? <ComponentType {...props} /> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />)}
-                  />
-                ),
-                { children: 'Title loaded' }
-              )
+            styles: titleTextStyles,
+            text: titleTextRender
           }}
-          body={render =>
-            render(
-              (ComponentType, props) => (
-                <AsyncData
-                  data="done"
-                  render={data => (
-                    <div style={{ border: '1px solid black' }}>
-                      <ComponentType {...props}>
-                        {data ? <Label>{props.children}</Label> : <Spinner styles={{ root: { alignItems: 'flex-start' } }} />}
-                      </ComponentType>
-                    </div>
-                  )}
-                />
-              ),
-              { children: 'Data loaded' }
-            )
-          }
-        />
+          body={bodyRender}
+        >
+          Data loaded
+        </CollapsibleSection>
       </div>
     );
   }
