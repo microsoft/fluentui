e05b2aeb83a37ef049360b74a13f9053d55457c6
REDMOND\naethell <naethell@microsoft.com>
2019-01-30 10:35:08 -0800

    mix in native props

--- packages/office-ui-fabric-react/src/components/MessageBar/MessageBar.base.tsx
+++ packages/office-ui-fabric-react/src/components/MessageBar/MessageBar.base.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BaseComponent, DelayedRender, getId, classNamesFunction } from '../../Utilities';
+import { BaseComponent, DelayedRender, getId, classNamesFunction, getNativeProps, divProperties } from '../../Utilities';
 import { IconButton } from '../../Button';
 import { Icon } from '../../Icon';
 import { IMessageBarProps, IMessageBarStyleProps, IMessageBarStyles, MessageBarType } from './MessageBar.types';
@@ -106,8 +106,10 @@ export class MessageBarBase extends BaseComponent<IMessageBarProps, IMessageBarS
   }
 
   private _renderMultiLine(): React.ReactElement<React.HTMLAttributes<HTMLAreaElement>> {
+    const nativeProps = getNativeProps(this.props, divProperties);
+
     return (
-      <div className={this._classNames.root} aria-live={this._getAnnouncementPriority()}>
+      <div className={this._classNames.root} aria-live={this._getAnnouncementPriority()} {...nativeProps}>
         <div className={this._classNames.content}>
           {this._getIconSpan()}
           {this._renderInnerText()}
@@ -119,8 +121,10 @@ export class MessageBarBase extends BaseComponent<IMessageBarProps, IMessageBarS
   }
 
   private _renderSingleLine(): React.ReactElement<React.HTMLAttributes<HTMLAreaElement>> {
+    const nativeProps = getNativeProps(this.props, divProperties);
+
     return (
-      <div className={this._classNames.root}>
+      <div className={this._classNames.root} {...nativeProps}>
         <div className={this._classNames.content}>
           {this._getIconSpan()}
           {this._renderInnerText()}
@@ -133,9 +137,11 @@ export class MessageBarBase extends BaseComponent<IMessageBarProps, IMessageBarS
   }
 
   private _renderInnerText(): JSX.Element {
+    const nativeProps = getNativeProps(this.props, divProperties);
+
     return (
       <div className={this._classNames.text} id={this.state.labelId}>
-        <span className={this._classNames.innerText} role="status" aria-live={this._getAnnouncementPriority()}>
+        <span className={this._classNames.innerText} role="status" aria-live={this._getAnnouncementPriority()} {...nativeProps}>
           <DelayedRender>
             <span>{this.props.children}</span>
           </DelayedRender>
