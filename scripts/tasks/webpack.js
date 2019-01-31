6739a66e187d9194deca873d42290381c7afdcbc
David Zearing <dzearing@microsoft.com>
2019-01-31 14:44:31 -0800

    Update webpack.js

--- scripts/tasks/webpack.js
+++ scripts/tasks/webpack.js
@@ -13,9 +13,9 @@ exports.webpackDevServer = async function() {
   const port = await fp(4322, 4400);
 
   if (fs.existsSync(configPath)) {
-    ``;
     const webpackDevServerPath = path.resolve(__dirname, '../node_modules/webpack-dev-server/bin/webpack-dev-server.js');
     const execSync = require('../exec-sync');
+
     execSync(`node ${webpackDevServerPath} --config ${configPath} --port ${port} --open`);
   }
 };
