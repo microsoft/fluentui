b333fd16e2f74a91d4783ff748f3920d6552ab5d
REDMOND\aneeshak <aneeshak@microsoft.com>
2019-01-31 15:56:34 -0800

    changed outline to WindowText in HC mode, moved outline:none to inside focus styling

--- packages/office-ui-fabric-react/src/components/Link/Link.styles.ts
+++ packages/office-ui-fabric-react/src/components/Link/Link.styles.ts
@@ -18,7 +18,6 @@ export const getStyles = (props: ILinkStyleProps): ILinkStyles => {
       theme.fonts.medium,
       {
         color: semanticColors.link,
-        outline: 'none',
         fontSize: 'inherit',
         fontWeight: 'inherit',
         selectors: {
@@ -27,14 +26,15 @@ export const getStyles = (props: ILinkStyleProps): ILinkStyles => {
             // https://github.com/OfficeDev/office-ui-fabric-react/issues/4883#issuecomment-406743543
             // A box-shadow allows the focus rect to wrap links that span multiple lines
             // and helps the focus rect avoid getting clipped.
-            boxShadow: `0 0 0 1px ${theme.palette.neutralSecondary} inset`
+            boxShadow: `0 0 0 1px ${theme.palette.neutralSecondary} inset`,
+            outline: 'none'
           },
-          '@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none)': {
+          '@media screen and (-ms-high-contrast: active)': {
             // For IE high contrast mode
             borderBottom: 'none',
             selectors: {
               [`.${IsFocusVisibleClassName} &:focus`]: {
-                outline: `1px solid ${theme.palette.white}`
+                outline: `1px solid WindowText`
               }
             }
           }
