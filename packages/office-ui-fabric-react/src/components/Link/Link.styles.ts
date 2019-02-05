9fe86f257e64468b17003742846daaaeade99b0c
REDMOND\aneeshak <aneeshak@microsoft.com>
2019-02-04 18:21:14 -0800

    moved focus selector for HC back into the main HC media query because separating the HC case and HC-focus case caused the HC outline to not be visible

--- packages/office-ui-fabric-react/src/components/Link/Link.styles.ts
+++ packages/office-ui-fabric-react/src/components/Link/Link.styles.ts
@@ -31,11 +31,10 @@ export const getStyles = (props: ILinkStyleProps): ILinkStyles => {
           },
           '@media screen and (-ms-high-contrast: active)': {
             // For IE high contrast mode
-            borderBottom: 'none'
-          },
-          '@media screen and (-ms-high-contrast: active){.ms-Fabric--isFocusVisible &:focus': {
-            // In high contrast mode, when in focus, override outline: none.
-            outline: '1px solid WindowText'
+            borderBottom: 'none',
+            selectors: {
+              '&:focus': { outline: '1px solid WindowText' }
+            }
           }
         }
       },
