2f2dab9bb85c2e84f000e76d2343b3f28aaa33e2
Max Huang-Hobbs <mhuan13@gmail.com>
2019-02-07 16:26:09 -0800

    make change non-breaking

--- packages/office-ui-fabric-react/src/components/pickers/Suggestions/Suggestions.types.ts
+++ packages/office-ui-fabric-react/src/components/pickers/Suggestions/Suggestions.types.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { IRefObject, IRenderFunction, KeyCodes, IStyleFunctionOrObject } from '../../../Utilities';
+import { IPersonaProps } from '../../Persona/Persona.types';
 import { IStyle, ITheme } from '../../../Styling';
 import { ISpinnerStyleProps } from '../../Spinner/Spinner.types';
 
@@ -152,8 +153,10 @@ export interface ISuggestionsProps<T> extends React.Props<any> {
 
   /**
    * Function to fire when one of the optional remove buttons on a suggestion is clicked.
+   *
+   * TODO (adjective-object) clean up
    */
-  onSuggestionRemove?: (ev?: React.MouseEvent<HTMLElement>, item?: T, index?: number) => void;
+  onSuggestionRemove?: (ev?: React.MouseEvent<HTMLElement>, item?: T | IPersonaProps, index?: number) => void;
 
   /**
    * Indicates if the text in resultsFooter or resultsFooterFull should be shown at the end of the suggestion list.
