0f66fa64f655cd5c3dcb9f43c074502420388d2a
Humberto Makoto Morimoto Burgos <makotom@microsoft.com>
2019-02-11 15:45:25 -0800

    Removing Stack that was wrapping basic example given that there is only one.

--- packages/experiments/src/components/Card/examples/Card.Basic.Example.tsx
+++ packages/experiments/src/components/Card/examples/Card.Basic.Example.tsx
@@ -21,37 +21,35 @@ export class CardBasicExample extends React.Component<{}, {}> {
     });
 
     return (
-      <Stack gap={5}>
-        <Card>
-          <Persona text="Kevin Jameson" secondaryText="Feb 2, 2019" coin={{ imageUrl: PersonaTestImages.personMale }} />
-          <Card.Item preventPadding>
-            <Image
-              src="https://placehold.it/250x120"
-              alt="Example implementation of the property image fit using the center value on an image larger than the frame."
-              width="100%"
-              height="120px"
-            />
-          </Card.Item>
-          <Text variant="large" className={styles.textLarge}>
-            Contoso
-          </Text>
-          <Text wrap variant="large">
-            Contoso Denver expansion design marketing hero guidelines
-          </Text>
-          <Text className={styles.textBold}>Is this recommendation helpful?</Text>
-          <Card.Item preventPadding>
-            <Stack
-              horizontal
-              horizontalAlign="space-between"
-              padding="10px 10px -2px 10px"
-              styles={{ root: { borderTop: '1px solid lightgray' } }}
-            >
-              <Icon iconName="RedEye" styles={{ root: { color: 'mediumturquoise', fontSize: '20px' } }} />
-              <Icon iconName="MoreVertical" styles={{ root: { color: 'mediumturquoise', fontSize: '20px' } }} />
-            </Stack>
-          </Card.Item>
-        </Card>
-      </Stack>
+      <Card>
+        <Persona text="Kevin Jameson" secondaryText="Feb 2, 2019" coin={{ imageUrl: PersonaTestImages.personMale }} />
+        <Card.Item preventPadding>
+          <Image
+            src="https://placehold.it/250x120"
+            alt="Example implementation of the property image fit using the center value on an image larger than the frame."
+            width="100%"
+            height="120px"
+          />
+        </Card.Item>
+        <Text variant="large" className={styles.textLarge}>
+          Contoso
+        </Text>
+        <Text wrap variant="large">
+          Contoso Denver expansion design marketing hero guidelines
+        </Text>
+        <Text className={styles.textBold}>Is this recommendation helpful?</Text>
+        <Card.Item preventPadding>
+          <Stack
+            horizontal
+            horizontalAlign="space-between"
+            padding="10px 10px -2px 10px"
+            styles={{ root: { borderTop: '1px solid lightgray' } }}
+          >
+            <Icon iconName="RedEye" styles={{ root: { color: 'mediumturquoise', fontSize: '20px' } }} />
+            <Icon iconName="MoreVertical" styles={{ root: { color: 'mediumturquoise', fontSize: '20px' } }} />
+          </Stack>
+        </Card.Item>
+      </Card>
     );
   }
 }
