7390c7532247277249b3867c07239a41aa4a3859
Kenneth Chau <kenotron@users.noreply.github.com>
2019-02-11 14:05:06 -0800

    Tooltip: update some tooltip examples to better show how to do aria tags with tooltip (#7929)
    * update some tooltip examples to better show how to do aria tags with tooltip
    
    * change file
    
    * update example per feedback
    
    * fix changefile:

--- packages/office-ui-fabric-react/src/components/Tooltip/examples/Tooltip.Interactive.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Tooltip/examples/Tooltip.Interactive.Example.tsx
@@ -7,8 +7,8 @@ export class TooltipInteractiveExample extends BaseComponent<any, any> {
   public render() {
     return (
       <div>
-        <TooltipHost content="This is the tooltip" id="myID" calloutProps={{ gapSpace: 0 }} closeDelay={500}>
-          <DefaultButton aria-describedby="myID">Interact with my tooltip</DefaultButton>
+        <TooltipHost content="This is the tooltip" id="myID-interactive" calloutProps={{ gapSpace: 0 }} closeDelay={500}>
+          <DefaultButton aria-labelledby="myID-interactive">Interact with my tooltip</DefaultButton>
         </TooltipHost>
       </div>
     );
