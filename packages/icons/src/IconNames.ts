6a8837737669cee8b2ba6c77bace5d0e4ac2bdf3
Jason Gore <jagore@microsoft.com>
2019-02-14 13:58:40 -0800

    Deprecate iconNames due to const enum usage.

--- packages/icons/src/IconNames.ts
+++ packages/icons/src/IconNames.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated Const enum use is deprecated. See github issue #7110: https://github.com/OfficeDev/office-ui-fabric-react/issues/7110
+ */
 export const enum IconNames {
   GlobalNavButton = 'GlobalNavButton',
   InternetSharing = 'InternetSharing',
