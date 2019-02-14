958e0600b5f63c49c19bf48f086d217fb276228b
Vitalie Braga <v-vibr@microsoft.com>
2019-02-13 23:31:19 -0800

    Remove some overrides for now for a future redesign

--- packages/fluent-theme/src/fluent/styles/Panel.styles.ts
+++ packages/fluent-theme/src/fluent/styles/Panel.styles.ts
@@ -6,21 +6,7 @@ import { FontWeights } from 'office-ui-fabric-react/lib/Styling';
 export const PanelStyles = (props: IPanelStyleProps): Partial<IPanelStyles> => {
   return {
     main: {
-      selectors: {
-        // increasing the specificity to override all the media queries in the default styles
-        '.ms-Panel &': {
-          boxShadow: Depths.depth64,
-          border: 'none'
-        }
-      }
-    },
-    header: {
-      selectors: {
-        // increasing the specificity to override all the media queries in the default styles
-        '.ms-Panel-contentInner &': {
-          marginTop: 0
-        }
-      }
+      boxShadow: Depths.depth64
     },
     headerText: {
       fontSize: FontSizes.size20,
@@ -30,16 +16,6 @@ export const PanelStyles = (props: IPanelStyleProps): Partial<IPanelStyles> => {
     footerInner: {
       paddingBottom: 16,
       paddingTop: 16
-    },
-    closeButton: {
-      width: 32,
-      height: 32,
-      margin: '10px 5px 0 0', // 5px right taking into account the 5px padding coming from the wrapper.
-      selectors: {
-        '.ms-Button-icon': {
-          fontSize: FontSizes.size12
-        }
-      }
     }
   };
 };
