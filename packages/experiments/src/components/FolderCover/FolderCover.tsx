a8e6072b340f2f5dcb06d1b530f63291f19c33da
Thomas Michon <thomas@michon.com>
2019-02-20 12:04:56 -0800

    Add Stories for Tile component (#8021)
    * Add Stories for Tile component
    
    * Add support for render-prop pattern in Tile and FolderCover
    
    * Add Fabric root around Tile and FolderCover tests
    
    * Update change output

--- packages/experiments/src/components/FolderCover/FolderCover.tsx
+++ packages/experiments/src/components/FolderCover/FolderCover.tsx
@@ -69,7 +69,6 @@ export class FolderCover extends React.Component<IFolderCoverProps, IFolderCover
       folderCoverSize: size = 'large',
       folderCoverType: type = 'default',
       hideContent = false,
-      ref,
       metadata,
       signal,
       children,
@@ -98,11 +97,7 @@ export class FolderCover extends React.Component<IFolderCoverProps, IFolderCover
         })}
       >
         <Icon aria-hidden={true} className={css('ms-FolderCover-back', FolderCoverStyles.back)} iconName={assets.back} />
-        {children ? (
-          <span className={css('ms-FolderCover-content', FolderCoverStyles.content)}>
-            <span className={css('ms-FolderCover-frame', FolderCoverStyles.frame)}>{children}</span>
-          </span>
-        ) : null}
+        {this._renderChildren({ children })}
         <Icon aria-hidden={true} className={css('ms-FolderCover-front', FolderCoverStyles.front)} iconName={assets.front} />
         {isFluent ? (
           <React.Fragment>
@@ -118,6 +113,16 @@ export class FolderCover extends React.Component<IFolderCoverProps, IFolderCover
       </div>
     );
   }
+
+  private _renderChildren({ children }: Pick<IFolderCoverProps, 'children'>): JSX.Element | null {
+    const finalChildren = typeof children === 'function' ? children(getFolderCoverLayoutFromProps(this.props)) : children;
+
+    return finalChildren ? (
+      <span className={css('ms-FolderCover-content', FolderCoverStyles.content)}>
+        <span className={css('ms-FolderCover-frame', FolderCoverStyles.frame)}>{finalChildren}</span>
+      </span>
+    ) : null;
+  }
 }
 
 export interface IFolderCoverLayout {
@@ -127,10 +132,13 @@ export interface IFolderCoverLayout {
 export function getFolderCoverLayout(element: JSX.Element): IFolderCoverLayout {
   const folderCoverProps: IFolderCoverProps = element.props;
 
-  const { folderCoverSize = 'large' } = folderCoverProps;
+  return getFolderCoverLayoutFromProps(folderCoverProps);
+}
+
+function getFolderCoverLayoutFromProps(folderCoverProps: IFolderCoverProps): IFolderCoverLayout {
+  const { folderCoverSize = 'large', isFluent } = folderCoverProps;
 
   const contentSize = { ...SIZES[folderCoverSize] };
-  const { isFluent } = element.props;
 
   if (isFluent) {
     contentSize.height -= 8;
