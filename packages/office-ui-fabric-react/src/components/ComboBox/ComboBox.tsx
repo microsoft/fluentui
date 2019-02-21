9b3a9b7b4d43d8586e7ae208bc2a392d3c15f705
Dalimil <dalimilhajek@gmail.com>
2019-02-21 09:26:37 -0800

    Add missing space

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
@@ -385,7 +385,7 @@ export class ComboBox extends BaseComponent<IComboBoxProps, IComboBoxState> {
                 readOnly={disabled || !allowFreeform}
                 aria-labelledby={label && id + '-label'}
                 aria-label={ariaLabel && !label ? ariaLabel : undefined}
-                aria-describedby={mergeAriaAttributeValues(ariaDescribedBy, keytipAttributes['aria-describedby'])}
+                aria-describedby={mergeAriaAttributeValues(ariaDescribedBy, ' ', keytipAttributes['aria-describedby'])}
                 aria-activedescendant={this._getAriaActiveDescentValue()}
                 aria-disabled={disabled}
                 aria-owns={isOpen ? id + '-list' : undefined}
