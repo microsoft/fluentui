d35f143990f5424ff185670b5a761b526e9594c6
Vitalie Braga <v-vibr@microsoft.com>
2019-02-21 16:33:13 -0800

    Remove comment

--- packages/fluent-theme/src/fluent/styles/CommandBarButton.styles.ts
+++ packages/fluent-theme/src/fluent/styles/CommandBarButton.styles.ts
@@ -1,9 +1,6 @@
 import { getFocusStyle } from 'office-ui-fabric-react/lib/Styling';
 import { IButtonStyles, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 
-// TODO: "any" is used here to get around "is using xxx but cannot be named" TS error. Should be able to remove
-//        this 'any' once we upgrade to TS3.1+
-// tslint:disable-next-line:no-any
 export const CommandBarButtonStyles = (props: IButtonProps): Partial<IButtonStyles> => {
   const { theme } = props;
   if (!theme) {
