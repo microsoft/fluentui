41ea2607cdd4127b770e5dd284972e7544292944
Jordan Janzen <jordancjanzen@gmail.com>
2019-02-22 11:25:00 -0800

    styling: Refactor getFocusStyle to use `options` object instead of many arguments.

--- packages/styling/src/styles/getFocusStyle.ts
+++ packages/styling/src/styles/getFocusStyle.ts
@@ -1,31 +1,23 @@
 import { IRawStyle } from '@uifabric/merge-styles';
-import { ITheme } from '../interfaces/index';
+import { IGetFocusStyleOptions, ITheme } from '../interfaces/index';
 import { HighContrastSelector } from './CommonStyles';
 import { IsFocusVisibleClassName } from '@uifabric/utilities';
 import { ZIndexes } from './zIndexes';
 
 /**
  * Generates a focus style which can be used to define an :after focus border.
- *
  * @param theme - The theme object to use.
- * @param inset - The number of pixels to inset the border.
- * @param position - The positioning applied to the container. Must
- * be 'relative' or 'absolute' so that the focus border can live around it.
- * @param highContrastStyle - Style for high contrast mode.
- * @param borderColor - Color of the border.
- * @param outlineColor - Color of the outline.
- * @param isFocusedOnly - If the styles should apply on focus or not.
- * @returns The style object.
+ * @param options {IGetFocusStyleOptions} - Options to customize the focus border.
  */
-export function getFocusStyle(
-  theme: ITheme,
-  inset: number = 0,
-  position: 'relative' | 'absolute' = 'relative',
-  highContrastStyle: IRawStyle | undefined = undefined,
-  borderColor: string = theme.palette.white,
-  outlineColor: string = theme.palette.neutralSecondary,
-  isFocusedOnly: boolean = true
-): IRawStyle {
+export function getFocusStyle(theme: ITheme, options?: IGetFocusStyleOptions): IRawStyle {
+  // Explicity define each option since `options` is optional.
+  const inset: IGetFocusStyleOptions['inset'] = options!.inset || 0;
+  const position: IGetFocusStyleOptions['position'] = options!.position || 'relative';
+  const highContrastStyle: IGetFocusStyleOptions['highContrastStyle'] = options!.highContrastStyle;
+  const borderColor: IGetFocusStyleOptions['borderColor'] = options!.borderColor || theme.palette.white;
+  const outlineColor: IGetFocusStyleOptions['outlineColor'] = options!.outlineColor || theme.palette.neutralSecondary;
+  const isFocusedOnly: IGetFocusStyleOptions['isFocusedOnly'] = options!.isFocusedOnly || true;
+
   return {
     outline: 'transparent',
     position,
