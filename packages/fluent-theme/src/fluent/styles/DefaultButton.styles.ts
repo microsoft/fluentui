3b36ee629df7a41e97a4d0d7f24fb108e7eb90cd
Vitalie Braga <v-vibr@microsoft.com>
2019-02-21 16:32:41 -0800

    Replacing some colors

--- packages/fluent-theme/src/fluent/styles/DefaultButton.styles.ts
+++ packages/fluent-theme/src/fluent/styles/DefaultButton.styles.ts
@@ -1,66 +1,73 @@
 import { fluentBorderRadius } from './styleConstants';
 import { getFocusStyle } from 'office-ui-fabric-react/lib/Styling';
-import FluentTheme from '../FluentTheme';
-import { NeutralColors, CommunicationColors } from '../FluentColors';
-import { IButtonStyles } from 'office-ui-fabric-react/lib/Button';
+import { NeutralColors } from '../FluentColors';
+import { IButtonStyles, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 
 // TODO: "any" is used here to get around "is using xxx but cannot be named" TS error. Should be able to remove
 //        this 'any' once we upgrade to TS3.1+
 // tslint:disable-next-line:no-any
-export const DefaultButtonStyles: Partial<IButtonStyles> = {
-  root: {
-    borderRadius: fluentBorderRadius,
-    backgroundColor: NeutralColors.white,
-    border: `1px solid ${NeutralColors.gray110}`,
-    ...getFocusStyle(FluentTheme, 1)
-  },
-  rootHovered: {
-    backgroundColor: NeutralColors.gray20,
-    selectors: {
-      '.ms-Button--primary': {
-        backgroundColor: CommunicationColors.shade10
-      }
-    }
-  },
-  rootPressed: {
-    backgroundColor: NeutralColors.gray30
-  },
-  rootChecked: {
-    backgroundColor: NeutralColors.gray30
-  },
-  rootDisabled: {
-    backgroundColor: NeutralColors.gray20,
-    borderColor: NeutralColors.gray20
-  },
-  splitButtonMenuButton: {
-    background: 'transparent',
-    borderTopRightRadius: fluentBorderRadius,
-    borderBottomRightRadius: fluentBorderRadius,
-    border: `1px solid ${NeutralColors.gray110}`,
-    borderLeft: 'none'
-  },
-  splitButtonContainer: {
-    selectors: {
-      '.ms-Button--default': {
-        borderRight: 'none',
-        borderTopRightRadius: '0',
-        borderBottomRightRadius: '0'
-      },
-      '.ms-Button--primary': {
-        border: 'none',
-        backgroundColor: CommunicationColors.primary
-      },
-      '.ms-Button--primary + .ms-Button': {
-        backgroundColor: CommunicationColors.primary,
-        border: 'none'
-      },
-      '.ms-Button.is-disabled': {
-        backgroundColor: NeutralColors.gray20
-      },
-      '.ms-Button.is-disabled + .ms-Button': {
-        backgroundColor: NeutralColors.gray20,
-        border: 'none'
-      }
-    }
+export const DefaultButtonStyles = (props: IButtonProps): Partial<IButtonStyles> => {
+  const { theme } = props;
+  if (!theme) {
+    throw new Error('Theme is undefined or null.');
   }
+  const { palette } = theme;
+
+  return {
+    root: {
+      borderRadius: fluentBorderRadius,
+      backgroundColor: palette.white,
+      border: `1px solid ${NeutralColors.gray110}`,
+      ...getFocusStyle(theme, 1)
+    },
+    rootHovered: {
+      backgroundColor: palette.neutralLighter,
+      selectors: {
+        '.ms-Button--primary': {
+          backgroundColor: palette.themeDarkAlt
+        }
+      }
+    },
+    rootPressed: {
+      backgroundColor: palette.neutralLight
+    },
+    rootChecked: {
+      backgroundColor: palette.neutralLight
+    },
+    rootDisabled: {
+      backgroundColor: palette.neutralLighter,
+      borderColor: palette.neutralLighter
+    },
+    splitButtonMenuButton: {
+      background: 'transparent',
+      borderTopRightRadius: fluentBorderRadius,
+      borderBottomRightRadius: fluentBorderRadius,
+      border: `1px solid ${NeutralColors.gray110}`,
+      borderLeft: 'none'
+    },
+    splitButtonContainer: {
+      selectors: {
+        '.ms-Button--default': {
+          borderRight: 'none',
+          borderTopRightRadius: '0',
+          borderBottomRightRadius: '0'
+        },
+        '.ms-Button--primary': {
+          border: 'none',
+          backgroundColor: palette.themePrimary
+        },
+        '.ms-Button--primary + .ms-Button': {
+          backgroundColor: palette.themePrimary,
+          border: 'none'
+        },
+        '.ms-Button.is-disabled': {
+          backgroundColor: palette.neutralLighter
+        },
+        '.ms-Button.is-disabled + .ms-Button': {
+          backgroundColor: palette.neutralLighter,
+          border: 'none'
+        }
+      }
+    }
+  };
 };
