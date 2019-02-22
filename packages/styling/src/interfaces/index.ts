dceecd0d3b05964b2e0a3ed3e00e706e1906b911
Jordan Janzen <jordancjanzen@gmail.com>
2019-02-22 11:22:22 -0800

    styling: Define IGetFocusStyleOptions interface.

--- packages/styling/src/interfaces/index.ts
+++ packages/styling/src/interfaces/index.ts
@@ -1,8 +1,9 @@
 export { IAnimationStyles, IAnimationVariables } from './IAnimationStyles';
+export { IEffects } from './IEffects';
+export { IGetFocusStyleOptions } from './IGetFocusStyle';
 export { IFontStyles } from './IFontStyles';
 export { IPalette } from './IPalette';
 export { ISemanticColors } from './ISemanticColors';
 export { ISemanticTextColors } from './ISemanticTextColors';
 export { ISpacing } from './ISpacing';
 export { ITheme, IPartialTheme, IScheme, ISchemeNames } from './ITheme';
-export { IEffects } from './IEffects';
