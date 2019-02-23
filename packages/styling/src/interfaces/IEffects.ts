5962a888d34226308e1e3f034ca936bfcfb9300b
Vitalie Braga <v-vibr@microsoft.com>
2019-02-22 18:25:20 -0800

    Remove comment in IEffects

--- packages/styling/src/interfaces/IEffects.ts
+++ packages/styling/src/interfaces/IEffects.ts
@@ -7,25 +7,25 @@ import { IRawStyle } from '@uifabric/merge-styles';
 export interface IEffects {
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
-   * Usually a shadow. This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * This is lower than elevations with a higher value, and higher than elevations with a lower value.
    * Used for: cards, grid items
    */
   elevation4: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
-   * Usually a shadow. This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * This is lower than elevations with a higher value, and higher than elevations with a lower value.
    * Used for: menus, command surfaces
    */
   elevation8: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
-   * Usually a shadow. This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * This is lower than elevations with a higher value, and higher than elevations with a lower value.
    * Used for: search result dropdowns, hover cards, tooltips, help bubbles
    */
   elevation16: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
-   * Usually a shadow. This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * This is lower than elevations with a higher value, and higher than elevations with a lower value.
    * Used for: Panels, Dialogs
    */
   elevation64: string;
