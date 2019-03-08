5a545156dd482eca6546fa019083df004c7e4b06
Aneesha Kommineni <aneeshak@Aneeshas-MacBook-Pro.local>
2019-03-08 11:04:17 -0800

    Setting up index & package.json

---
+++ apps/theming-designer/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+// import ThemingDesigner from './components/ThemingDesigner';
+
+function start(): void {
+  ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+}
+
+// Start the application.
+start();
