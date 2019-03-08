6746b4f4f179f7ac4e38a6bf3e9054c6012a2362
Aneesha Kommineni <aneeshak@Aneeshas-MacBook-Pro.local>
2019-03-08 11:17:56 -0800

    removing prior change to use of deprecated color and setting icon and unselectedIcon to FavoriteStarFill and FavoriteStar respectively in Rating.base.tsx

--- packages/office-ui-fabric-react/src/components/Rating/Rating.styles.ts
+++ packages/office-ui-fabric-react/src/components/Rating/Rating.styles.ts
@@ -38,7 +38,7 @@ export function getStyles(props: IRatingStyleProps): IRatingStyles {
   const ratingVerticalPadding = 8;
   const ratingHorizontalPadding = 2;
 
-  const ratingStarUncheckedColor = palette.neutralSecondaryAlt;
+  const ratingStarUncheckedColor = palette.neutralTertiary;
   const ratingStarUncheckedHoverColor = palette.themePrimary;
   const ratingStarUncheckedHoverSelectedColor = palette.themeDark;
   const ratingStarCheckedColor = semanticColors.bodyTextChecked;
