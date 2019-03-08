4954f3b4b48fbcfaf1b78a5fc9a182f6b7bf5aa9
Aneesha Kommineni <aneeshak@Aneeshas-MacBook-Pro.local>
2019-03-08 11:05:06 -0800

    trying index file again

--- apps/theming-designer/src/index.tsx
+++ apps/theming-designer/src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 // import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 // import ThemingDesigner from './components/ThemingDesigner';
 
 function start(): void {
-  ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+  ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
 }
 
 // Start the application.
