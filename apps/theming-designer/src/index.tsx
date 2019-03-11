e39a8fbf9c9dc169c649fba5c170b5df9f4f6839
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-03-11 11:40:25 -0700

    pre-copy.json missing which prevented dist from getting index.html

--- apps/theming-designer/src/index.tsx
+++ apps/theming-designer/src/index.tsx
@@ -3,8 +3,13 @@ import * as ReactDOM from 'react-dom';
 
 // import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 // import ThemingDesigner from './components/ThemingDesigner';
+let _rootDiv: HTMLElement;
 
 function start(): void {
+  if (!_rootDiv) {
+    _rootDiv = document.createElement('div');
+    document.body.appendChild(_rootDiv);
+  }
   ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
 }
 
