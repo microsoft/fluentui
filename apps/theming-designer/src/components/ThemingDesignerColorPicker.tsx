9820942bd8d428bba172fb6eea31be9deb3e2e7a
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-03-13 16:37:46 -0700

    Made Primary, Text, and Background color boxes using an inline block div instead of SwatchColorPicker and they update properly but need to add mixins to scss so initial colors can be different

--- apps/theming-designer/src/components/ThemingDesignerColorPicker.tsx
+++ apps/theming-designer/src/components/ThemingDesignerColorPicker.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as stylesImport from './app.scss';
 import { Stack } from 'office-ui-fabric-react/lib/Stack';
 import { Dropdown } from '../../../../packages/office-ui-fabric-react/lib/Dropdown';
 import { Text } from '../../../../packages/office-ui-fabric-react/lib/Text';
@@ -8,15 +9,16 @@ import { IColorCellProps, ColorPickerGridCell, SwatchColorPicker } from '../../.
 
 const DEFAULT_OPTIONS: IColorCellProps[] = [{ id: 'primary', label: 'green', color: '#00ff00' }];
 //<ColorPickerGridCell selected={false} item={DEFAULT_OPTIONS} color={'#ffa500'} label={'orange'} circle={true} />
+const styles: any = stylesImport;
 
 export class ThemingDesignerColorPicker extends React.Component {
-  constructor(props: any) {
-    super(props);
+  // constructor(props: any) {
+  //   super(props);
 
-    this.state = {
-      color: undefined
-    };
-  }
+  //   this.state = {
+  //     color: undefined
+  //   };
+  // }
   public render(): JSX.Element {
     return (
       <>
@@ -31,34 +33,54 @@ export class ThemingDesignerColorPicker extends React.Component {
             //componentRef={this._basicDropdown}
           />
           <span />
-          <Stack horizontal disableShrink className="colorpicker">
+          {/* <div className={styles.colorpicker}>
+            <input className={styles.textinput} type="text" value="Orange" />
+            <div className={styles.colorinput} background-color="#FF850A;" />
+          </div> */}
+          <Stack horizontal disableShrink gap={5}>
             <Text>Primary color</Text>
-            <SwatchColorPicker
-              colorCells={[{ id: 'a', label: 'green', color: '#00ff00' }]}
-              columnCount={1}
-              cellMargin={2}
-              cellShape={'square'}
-              //disabled={true}
-              //setSize={1}
-              //focusOnHover={false}
-            />
-            <TextField value={'#00ff00'} onChange={this._updateColor} />
+            <div className={styles.colorpicker}>
+              <TextField className={styles.textinput} id="text-box" value={'#0078D4'} onChange={this._updatePrimaryColor} />
+              <div className={styles.colorinput} id="color-primary-box" />
+            </div>
           </Stack>
-          <Stack horizontal disableShrink className="colorpicker">
+          <Stack horizontal disableShrink gap={5}>
             <Text>Text color</Text>
-            <SwatchColorPicker columnCount={1} cellShape={'square'} colorCells={[{ id: 'a', label: 'blue', color: '#0078d4' }]} />
-            <TextField value={'#0078d4'} onChange={this._updateColor} />
+            <div className={styles.colorpicker}>
+              <TextField className={styles.textinput} id="text-box" value={'#323130'} onChange={this._updateTextColor} />
+              <div className={styles.colorinput} id="color-text-box" background-color="#323130;" />
+            </div>
           </Stack>
-          <Stack horizontal disableShrink className="colorpicker">
-            <Text>Background color color</Text>
-            <SwatchColorPicker columnCount={1} cellShape={'square'} colorCells={[{ id: 'a', label: 'white', color: '#FFFFFF' }]} />
-            <TextField value={'#FFFFFF'} onChange={this._updateColor} />
+          <Stack horizontal disableShrink gap={5}>
+            <Text>Background color</Text>
+            <div className={styles.colorpicker}>
+              <TextField className={styles.textinput} id="text-box" value={'#FFFFFF'} onChange={this._updateBackgroundColor} />
+              <div className={styles.colorinput} id="color-background-box" background-color="#FFFFFF;" />
+            </div>
           </Stack>
         </Stack>
       </>
     );
   }
-  private _updateColor = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-    this.setState({ color: newValue });
+  private _updatePrimaryColor = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    const colorBox = document.getElementById('color-primary-box');
+    if (colorBox) {
+      let currentcolor = colorBox.style.backgroundColor;
+      colorBox.style.backgroundColor = newValue ? newValue : currentcolor;
+    }
+  };
+  private _updateTextColor = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    const colorBox = document.getElementById('color-text-box');
+    if (colorBox) {
+      let currentcolor = colorBox.style.backgroundColor;
+      colorBox.style.backgroundColor = newValue ? newValue : currentcolor;
+    }
+  };
+  private _updateBackgroundColor = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+    const colorBox = document.getElementById('color-background-box');
+    if (colorBox) {
+      let currentcolor = colorBox.style.backgroundColor;
+      colorBox.style.backgroundColor = newValue ? newValue : currentcolor;
+    }
   };
 }
