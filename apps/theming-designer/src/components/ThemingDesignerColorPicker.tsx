1a40d7f7de27d99f3f67dda0fdbbcb3350695107
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-03-14 11:07:04 -0700

    Separated default colors for the three different pickers

--- apps/theming-designer/src/components/ThemingDesignerColorPicker.tsx
+++ apps/theming-designer/src/components/ThemingDesignerColorPicker.tsx
@@ -12,13 +12,15 @@ const DEFAULT_OPTIONS: IColorCellProps[] = [{ id: 'primary', label: 'green', col
 const styles: any = stylesImport;
 
 export class ThemingDesignerColorPicker extends React.Component {
-  // constructor(props: any) {
-  //   super(props);
+  constructor(props: any) {
+    super(props);
 
-  //   this.state = {
-  //     color: undefined
-  //   };
-  // }
+    this.state = {
+      primaryColor: undefined,
+      textColor: undefined,
+      backgroundColor: undefined
+    };
+  }
   public render(): JSX.Element {
     return (
       <>
@@ -30,32 +32,27 @@ export class ThemingDesignerColorPicker extends React.Component {
             label="Theme dropdown"
             ariaLabel="Theme dropdown"
             options={[{ key: 'light', text: 'Light theme' }, { key: 'dark', text: 'Dark theme' }]}
-            //componentRef={this._basicDropdown}
           />
           <span />
-          {/* <div className={styles.colorpicker}>
-            <input className={styles.textinput} type="text" value="Orange" />
-            <div className={styles.colorinput} background-color="#FF850A;" />
-          </div> */}
           <Stack horizontal disableShrink gap={5}>
             <Text>Primary color</Text>
             <div className={styles.colorpicker}>
-              <TextField className={styles.textinput} id="text-box" value={'#0078D4'} onChange={this._updatePrimaryColor} />
-              <div className={styles.colorinput} id="color-primary-box" />
+              <TextField className={styles.textinput} id="text-box" value={'#00ff00'} onChange={this._updatePrimaryColor} />
+              <div className={styles.colorinputprimary} id="color-primary-box" />
             </div>
           </Stack>
           <Stack horizontal disableShrink gap={5}>
             <Text>Text color</Text>
             <div className={styles.colorpicker}>
               <TextField className={styles.textinput} id="text-box" value={'#323130'} onChange={this._updateTextColor} />
-              <div className={styles.colorinput} id="color-text-box" background-color="#323130;" />
+              <div className={styles.colorinputtext} id="color-text-box" background-color="#323130;" />
             </div>
           </Stack>
           <Stack horizontal disableShrink gap={5}>
             <Text>Background color</Text>
             <div className={styles.colorpicker}>
               <TextField className={styles.textinput} id="text-box" value={'#FFFFFF'} onChange={this._updateBackgroundColor} />
-              <div className={styles.colorinput} id="color-background-box" background-color="#FFFFFF;" />
+              <div className={styles.colorinputbackground} id="color-background-box" background-color="#FFFFFF;" />
             </div>
           </Stack>
         </Stack>
@@ -69,6 +66,13 @@ export class ThemingDesignerColorPicker extends React.Component {
       colorBox.style.backgroundColor = newValue ? newValue : currentcolor;
     }
   };
+  // private _updatePrimaryColor() {
+  //   const colorSquare = document.getElementById('color-primary-box');
+  //   const colorText = document.getElementById('text-box');
+  //   this.setState((prevState, props) => {
+  //     return { primaryColor: colorText.value }
+  //   })
+  // }
   private _updateTextColor = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
     const colorBox = document.getElementById('color-text-box');
     if (colorBox) {
