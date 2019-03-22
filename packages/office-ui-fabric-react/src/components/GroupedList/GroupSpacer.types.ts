b489fc15020a8f370b2aaa6faf5a49d401586f33
Kevin Coughlin <keco@microsoft.com>
2019-03-21 20:40:47 -0700

    Add deprecations to GroupSpacer style props

--- packages/office-ui-fabric-react/src/components/GroupedList/GroupSpacer.types.ts
+++ packages/office-ui-fabric-react/src/components/GroupedList/GroupSpacer.types.ts
@@ -4,11 +4,15 @@ import { IStyleFunctionOrObject } from '../../Utilities';
 export interface IGroupSpacerProps {
   /**
    * Theme from Higher Order Component
+   *
+   * @deprecated unused, to be removed in 7.0
    */
   theme?: ITheme;
 
   /**
    * Style function to be passed in to override the themed or default styles
+   *
+   * @deprecated unused, to be removed in 7.0
    */
   styles?: IStyleFunctionOrObject<IGroupSpacerStyleProps, IGroupSpacerStyles>;
 
@@ -19,10 +23,16 @@ export interface IGroupSpacerProps {
   indentWidth?: number;
 }
 
+/**
+ * @deprecated unused, to be removed in 7.0. Use {@link IGroupSpacerProps.indentWidth}
+ */
 export type IGroupSpacerStyleProps = Required<Pick<IGroupSpacerProps, 'theme'>> & {
   width?: number;
 };
 
+/**
+ * @deprecated unused, to be removed in 7.0.
+ */
 export interface IGroupSpacerStyles {
   root: IStyle;
 }
