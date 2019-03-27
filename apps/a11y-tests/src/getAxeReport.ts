9e436feb6f64b0643b9f9e5a6514ae40c2d06f06
Brian Zhao <rezha@microsoft.com>
2019-03-27 12:43:37 -0700

    Use SARIF for jest snapshots

--- apps/a11y-tests/src/getAxeReport.ts
+++ apps/a11y-tests/src/getAxeReport.ts
@@ -1,13 +1,13 @@
 import { AxePuppeteer } from 'axe-puppeteer';
 import * as puppeteer from 'puppeteer';
 
-const OUFR_EXAMPLES_ROOT = 'https://fabricweb.z5.web.core.windows.net/oufr/6.87.0/';
+const TEST_URL_ROOT = 'http://localhost:4322/';
 
 export async function getAxeReport(subUrl: string) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setBypassCSP(true);
-  await page.goto(`${OUFR_EXAMPLES_ROOT}${subUrl}`);
+  await page.goto(`${TEST_URL_ROOT}${subUrl}`);
 
   const report = await new AxePuppeteer(page).include(['.ExampleCard-example']).analyze();
 
--- apps/a11y-tests/src/testComponent.ts
+++ apps/a11y-tests/src/testComponent.ts
@@ -4,14 +4,7 @@ import { axeToSarif } from './sarif-converter';
 export async function testComponent(component: { name: string; url: string }) {
   it(`runs Axe on ${component.name}`, async () => {
     const report = await getAxeReport(component.url);
-    expect(report.violations).toMatchSnapshot('violations');
-  }, 20000);
-}
-
-export async function testComponentSarif(component: { name: string; url: string }) {
-  it(`runs Axe on ${component.name}`, async () => {
-    const report = await getAxeReport(component.url);
     const sarifReport = axeToSarif(report);
-    expect(sarifReport.runs[0].results).toMatchSnapshot('sarif results');
+    expect(sarifReport.runs[0].results).toMatchSnapshot();
   }, 20000);
 }
