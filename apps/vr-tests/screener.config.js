735e543f2ec5d53ca96b52cf750f60384b031b48
Ken <kchau@microsoft.com>
2019-03-28 15:14:12 -0700

    refactored the baseBranch calculation to be cleaner

--- apps/vr-tests/screener.config.js
+++ apps/vr-tests/screener.config.js
@@ -24,13 +24,16 @@ export function getCurrentHash() {
   return '';
 }
 
+const baseBranch = process.env.SYSTEM_PULLREQUEST_TARGETBRANCH
+  ? process.env.SYSTEM_PULLREQUEST_TARGETBRANCH.replace(/^refs\/heads\//, '')
+  : 'master';
+
 module.exports = {
   projectRepo: 'OfficeDev/office-ui-fabric-react',
   storybookConfigDir: '.storybook',
   apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
-  baseBranch:
-    process.env['SYSTEM_PULLREQUEST_TARGETBRANCH'].replace(/^refs\/heads\//, '') || 'master',
+  baseBranch,
   failureExitCode: 0,
   alwaysAcceptBaseBranch: true,
   commit: getCurrentHash()
