ac98848becc24e9c98a126e0c8097302e20f7d21
Brian Zhao <rezha@microsoft.com>
2019-03-28 13:47:09 -0700

    Dehydrate snapshots to contain only errors

--- apps/a11y-tests/src/tests/testComponent.ts
+++ apps/a11y-tests/src/tests/testComponent.ts
@@ -1,8 +1,16 @@
 import { getSarifReport } from '../getSarifReport';
+import { ISarifLog } from 'src/sarif-converter/sarif/isarflog';
+import { Result } from 'src/sarif-converter/sarif/sarifv2';
+
+// Extract interesting info to reduce snapshot size
+function dehydrateSarifReport(report: ISarifLog): Result[] {
+  const results = report.runs[0]!.results!;
+  return results.filter(item => item.level === 'error');
+}
 
 export async function testComponent(component: { name: string; url: string }) {
   it(`runs Axe on ${component.name}`, async () => {
     const report = await getSarifReport(component.url);
-    expect(report.runs[0].results).toMatchSnapshot();
+    expect(dehydrateSarifReport(report)).toMatchSnapshot();
   }, 60000);
 }
