086e5d0a452fcb9dc21ff44d80634bdb2ff0b5d5
Elizabeth Craig <ecraig12345@gmail.com>
2019-03-30 01:16:20 -0700

    More TextField example updates (#8540)

--- packages/office-ui-fabric-react/src/components/TextField/examples/TextField.PrefixAndSuffix.Example.tsx
+++ packages/office-ui-fabric-react/src/components/TextField/examples/TextField.PrefixAndSuffix.Example.tsx
@@ -1,36 +1,43 @@
 import * as React from 'react';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
-import { IStackTokens, Stack } from 'office-ui-fabric-react/lib/Stack';
+import { Stack, IStackProps } from 'office-ui-fabric-react/lib/Stack';
 
 export const TextFieldPrefixAndSuffixExample: React.StatelessComponent = () => {
-  const stackTokens: IStackTokens = { childrenGap: 15 };
+  const columnProps: Partial<IStackProps> = {
+    tokens: { childrenGap: 15 },
+    styles: { root: { width: 300 } }
+  };
 
   return (
-    <Stack maxWidth={300} tokens={stackTokens}>
-      <TextField // prettier-ignore
-        label="With prefix"
-        prefix="https://"
-        ariaLabel="Example text field with https:// prefix"
-      />
-      <TextField // prettier-ignore
-        label="Disabled with prefix"
-        prefix="https://"
-        disabled
-        ariaLabel="Example text field with https:// prefix"
-      />
+    <Stack horizontal tokens={{ childrenGap: 50 }} styles={{ root: { width: 650 } }}>
+      <Stack {...columnProps}>
+        <TextField // prettier-ignore
+          label="With prefix"
+          prefix="https://"
+          ariaLabel="Example text field with https:// prefix"
+        />
+        <TextField // prettier-ignore
+          label="Disabled with prefix"
+          prefix="https://"
+          disabled
+          ariaLabel="Example text field with https:// prefix"
+        />
+      </Stack>
 
-      <TextField // prettier-ignore
-        label="With suffix"
-        suffix=".com"
-        ariaLabel="Example text field with .com suffix"
-      />
+      <Stack {...columnProps}>
+        <TextField // prettier-ignore
+          label="With suffix"
+          suffix=".com"
+          ariaLabel="Example text field with .com suffix"
+        />
 
-      <TextField
-        label="With prefix and suffix"
-        prefix="https://"
-        suffix=".com"
-        ariaLabel="Example text field with https:// prefix and .com suffix"
-      />
+        <TextField
+          label="With prefix and suffix"
+          prefix="https://"
+          suffix=".com"
+          ariaLabel="Example text field with https:// prefix and .com suffix"
+        />
+      </Stack>
     </Stack>
   );
 };
