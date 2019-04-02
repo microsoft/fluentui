50e3817023d5989acc57dc6c101cd81be029ffaa
Vitalie Braga <v-vibr@microsoft.com>
2019-04-02 15:49:53 -0700

    Remove fluent decorator on the stories

--- apps/vr-tests/src/stories/ComboBox.stories.tsx
+++ apps/vr-tests/src/stories/ComboBox.stories.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import Screener from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
-import { FabricDecoratorTallFixedWidth } from '../utilities/FluentDecorator';
+import { FabricDecoratorTallFixedWidth } from '../utilities';
 import { ComboBox, SelectableOptionMenuItemType, ISelectableOption } from 'office-ui-fabric-react';
 
 const testOptions = [
