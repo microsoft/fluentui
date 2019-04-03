a74f7586310faad5ec76f315f430d0baaf66281b
Brian Zhao <rezha@microsoft.com>
2019-04-03 14:16:48 -0700

    Remove placeholder from axe-sarif-converter

--- apps/a11y-tests/src/axe-sarif-converter/placeholder.test.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { sum } from './placeholder';
-
-describe('placholder', () => {
-  it('adds two numbers', () => {
-    expect(sum(1, 2)).toEqual(3);
-  });
-});
--- apps/a11y-tests/src/axe-sarif-converter/placeholder.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-export function sum(a: number, b: number): number {
-  return a + b;
-}
