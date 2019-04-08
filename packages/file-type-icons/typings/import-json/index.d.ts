aa78c6af313188823cef8ada7534a45410f0fdfa
Kosta Tachtevrenidis <kosta.tachtevrenidis@microsoft.com>
2019-04-08 10:30:45 -0700

    not needed anymore

--- packages/file-type-icons/typings/import-json/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.json' {
-  // Disabling ts-lint because this is supposed to be able to load any json file
-  // tslint:disable-next-line
-  const value: any;
-  export default value;
-}
