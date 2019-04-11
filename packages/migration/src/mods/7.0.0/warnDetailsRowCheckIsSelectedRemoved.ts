aac7553a339e07dc75465764925293116d77bca0
Kevin Coughlin <keco@microsoft.com>
2019-04-11 10:27:56 -0700

    Add doc comment to DetailsRowCheck.isSelected mod

--- packages/migration/src/mods/7.0.0/warnDetailsRowCheckIsSelectedRemoved.ts
+++ packages/migration/src/mods/7.0.0/warnDetailsRowCheckIsSelectedRemoved.ts
@@ -2,6 +2,18 @@ import { migration } from '../../migration';
 import { mod } from 'riceburn';
 import { renameJsxProp } from 'riceburn/lib/mods/renameJsxProp';
 
+/**
+ * A code mod that transforms the following usage of
+ * the deprecated prop {@link DetailsRowCheck.isSelected}:
+ *
+ * `<DetailsRowCheck canSelect={true} isSelected={true} />`
+ *
+ * to {@link DetailsRowCheck.selected}:
+ *
+ * `<DetailsRowCheck canSelect={true} selected={true} />;`
+ *
+ * to assist with Fabric 7.0 migration.
+ */
 export default migration('Deprecated DetailsRowCheck.isSelected has been removed. Use DetailsRowCheck.selected instead.', () => {
   mod('**/*.ts?(x)').asTypescript((node, modder) => {
     return renameJsxProp('DetailsRowCheck', 'isSelected', 'selected')(node, modder);
