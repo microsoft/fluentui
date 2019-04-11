42356bb113544474bb16e5e0a9701c0f30e7324c
Meher Michail (ABIDI) <mabidi@microsoft.com>
2019-04-10 21:56:55 -0700

    Updated component

--- packages/experiments/src/components/Microfeedback/Microfeedback.test.tsx
+++ packages/experiments/src/components/Microfeedback/Microfeedback.test.tsx
@@ -8,9 +8,4 @@ describe('Microfeedback', () => {
     const tree = renderer.create(<Microfeedback />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-
-  it('renders text prop correctly', () => {
-    const tree = renderer.create(<Microfeedback text="textProp" />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 });
