7ab1d90394574f6723920c229641a30f49b0d1bb
Makoto Morimoto <Humberto.Morimoto@microsoft.com>
2019-04-11 12:38:50 -0700

    Button: Creating CompoundButton and BaseButton variants (#8679)
    * Button: Creating CompoundButton variant.
    
    * Adding change file.
    
    * Button: Creating BaseButton variant.
    
    * BaseButton styling improvements.
    
    * CompoundButton styling improvements.
    
    * Update snapshots.

--- packages/experiments/src/components/Button/index.ts
+++ packages/experiments/src/components/Button/index.ts
@@ -2,5 +2,7 @@ export * from './Button';
 export * from './Button.types';
 export * from './ButtonVariants.types';
 export * from './ActionButton';
+export * from './BaseButton';
+export * from './CompoundButton';
 export * from './DefaultButton';
 export * from './PrimaryButton';
