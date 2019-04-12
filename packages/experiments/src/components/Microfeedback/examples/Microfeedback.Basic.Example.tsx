00281bc7420840ae43671af3f5bbfb1c5b4c71f4
Meher Michail (ABIDI) <mabidi@microsoft.com>
2019-04-11 22:20:50 -0700

    Move followup questions to example.

--- packages/experiments/src/components/Microfeedback/examples/Microfeedback.Basic.Example.tsx
+++ packages/experiments/src/components/Microfeedback/examples/Microfeedback.Basic.Example.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import { Microfeedback } from '../Microfeedback';
+import { MicrofeedbackQuestion } from '../Microfeedback.types';
 
 export class MicrofeedbackBasicExample extends React.Component<{}, {}> {
   public render(): JSX.Element {
+    const followUpOnThumbsDown = new MicrofeedbackQuestion();
+    followUpOnThumbsDown.options = ['Translation is incorrect', 'Context is incorrect', 'Language can be better'];
+    followUpOnThumbsDown.question = 'Please help us improve';
+    const followUpOnThumbsUp = new MicrofeedbackQuestion();
+    followUpOnThumbsUp.options = ['Translation is great', 'Context is great'];
+    followUpOnThumbsUp.question = 'Please help us improve';
+
     return (
       <div>
-        <Microfeedback />
+        <Microfeedback
+          ThumbsDownQuestion={followUpOnThumbsDown}
+          ThumbsUpQuestion={followUpOnThumbsUp}
+          thumbsUpTitle="Like"
+          thumbsDownTitle="Dislike"
+        />
       </div>
     );
   }
