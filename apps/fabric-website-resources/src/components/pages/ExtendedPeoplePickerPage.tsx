4371622aef821226abd9c454bf8bfd6f1a23c711
Natalie Ethell <natalie.ethell@microsoft.com>
2019-04-16 15:31:35 -0700

    fix ExtendedPeoplePicker page

--- apps/fabric-website-resources/src/components/pages/ExtendedPeoplePickerPage.tsx
+++ apps/fabric-website-resources/src/components/pages/ExtendedPeoplePickerPage.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
 import { DemoPage } from '../DemoPage';
-
-// import { ExtendedPeoplePickerPageProps } from 'office-ui-fabric-react/lib/components/ExtendedPicker/PeoplePicker/ExtendedPeoplePicker.doc';
-import { DropdownPageProps } from 'office-ui-fabric-react/lib/components/Dropdown/Dropdown.doc';
+import { ExtendedPeoplePickerPageProps } from 'office-ui-fabric-react/lib/components/ExtendedPicker/PeoplePicker/ExtendedPeoplePicker.doc';
 
 export const ExtendedPeoplePickerPage = (props: { isHeaderVisible: boolean }) => (
   <DemoPage
     jsonDocs={require('@uifabric/api-docs/lib/pages/office-ui-fabric-react/ExtendedPeoplePicker.page.json')}
-    {...{ ...DropdownPageProps, ...props }}
+    {...{ ...ExtendedPeoplePickerPageProps, ...props }}
   />
 );
