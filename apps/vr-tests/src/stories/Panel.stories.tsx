68bf892681e7cec48499ea62312aad3dee5bbfd1
Elizabeth Craig <ecraig12345@gmail.com>
2019-04-16 12:56:19 -0700

    Run prettier on all relevant files (#8644)
    * Run prettier on all relevant files on commit and on demand
    * Check in result of running prettier almost everywhere
    * Exclude appropriate files

--- apps/vr-tests/src/stories/Panel.stories.tsx
+++ apps/vr-tests/src/stories/Panel.stories.tsx
@@ -62,10 +62,20 @@ storiesOf('Panel', module)
     <Panel {...defaultProps} type={PanelType.custom} headerText="Custom" customWidth="200vw" />
   ))
   .addStory('Custom anchored left', () => (
-    <Panel {...defaultProps} type={PanelType.customNear} headerText="Custom left" customWidth="320px" />
+    <Panel
+      {...defaultProps}
+      type={PanelType.customNear}
+      headerText="Custom left"
+      customWidth="320px"
+    />
   ))
   .addStory('With no navigation', () => (
-    <Panel {...defaultProps} type={PanelType.smallFixedFar} headerText="No navigation" hasCloseButton={false} />
+    <Panel
+      {...defaultProps}
+      type={PanelType.smallFixedFar}
+      headerText="No navigation"
+      hasCloseButton={false}
+    />
   ));
 
 storiesOf('Panel', module)
--- packages/azure-themes/README.md
+++ packages/azure-themes/README.md
@@ -1,17 +1,9 @@
 # [@uifabric/azure-themes](http://dev.microsoft.com/fabric)
 
-##### AzureThemes components for Office UI Fabric React
+##### Azure theme for Office UI Fabric React
 
-These are not production ready components and __should never be used in product__. This AzureThemes space is useful for testing new components whose APIs might change before final release.
-
-To import AzureThemes components:
+To import the Azure theme:
 
 ```js
 import { AzureCustomizations } from '@uifabric/azure-themes';
-```
-
-Once the AzureThemes component graduates to a production release, the component will be available at:
-
-```js
-import { AzureCustomizations } from 'office-ui-fabric-react';
 ```
