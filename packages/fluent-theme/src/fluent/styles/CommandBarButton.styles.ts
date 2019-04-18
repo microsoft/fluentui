a20341591b58ced297e951a05d5d19866dc26266
Eddie Liu <edwl@microsoft.com>
2019-04-18 14:59:52 -0700

    Update commandBar and CommandBarButton to Fluent Styles

--- packages/fluent-theme/src/fluent/styles/CommandBarButton.styles.ts
+++ packages/fluent-theme/src/fluent/styles/CommandBarButton.styles.ts
@@ -6,10 +6,84 @@ export const CommandBarButtonStyles = (props: IButtonProps): Partial<IButtonStyl
   if (!theme) {
     throw new Error('Theme is undefined or null.');
   }
+  const { palette, semanticColors } = theme;
 
   return {
-    root: {
-      ...getFocusStyle(theme, 2)
+    root: [
+      { ...getFocusStyle(theme, 2) },
+      {
+        backgroundColor: palette.white
+      }
+    ],
+
+    rootHovered: {
+      backgroundColor: palette.neutralLighter,
+      selectors: {
+        '.ms-Button-icon': {
+          color: palette.themeDarkAlt
+        }
+      }
+    },
+
+    rootPressed: {
+      backgroundColor: palette.neutralLight,
+      color: palette.neutralDark,
+      selectors: {
+        '.ms-Button-icon': {
+          color: palette.themeDark
+        }
+      }
+    },
+
+    rootChecked: {
+      backgroundColor: palette.neutralLight,
+      color: palette.neutralDark,
+      selectors: {
+        '.ms-Button-icon': {
+          color: palette.themeDark
+        }
+      }
+    },
+
+    rootExpanded: {
+      color: palette.neutralDark,
+      selectors: {
+        '.ms-Button-icon': {
+          color: palette.themeDark
+        }
+      }
+    },
+
+    rootDisabled: {
+      backgroundColor: palette.white,
+      selectors: {
+        '.ms-Button-icon': {
+          color: semanticColors.disabledBodySubtext
+        }
+      }
+    },
+
+    splitButtonMenuButton: {
+      backgroundColor: palette.white,
+      color: palette.neutralSecondary,
+      selectors: {
+        ':hover': {
+          backgroundColor: palette.neutralLighter,
+          selectors: {
+            '.ms-Button-icon': {
+              color: palette.neutralPrimary
+            }
+          }
+        }
+      }
+    },
+
+    splitButtonMenuButtonDisabled: {
+      backgroundColor: palette.white
+    },
+
+    icon: {
+      color: palette.themePrimary
     }
   };
 };
