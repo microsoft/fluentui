4a1075dafc369da971209eda6270de07cd0d39d8
REDMOND\humorimo <Humberto.Morimoto@microsoft.com>
2019-04-23 11:33:20 -0700

    Adding typing to styles and tokens in Card examples and cleaning them up.

--- packages/react-cards/src/components/Card/examples/Card.Compact.Example.tsx
+++ packages/react-cards/src/components/Card/examples/Card.Compact.Example.tsx
@@ -1,86 +1,77 @@
 // @codepen
 import * as React from 'react';
-import { Icon, Image, Stack, Text } from 'office-ui-fabric-react';
-import { FontWeights } from 'office-ui-fabric-react/lib/Styling';
-import { Card } from '@uifabric/react-cards';
+import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
+import { FontWeights } from '@uifabric/styling';
+import { Icon, IIconStyles, Image, Stack, IStackTokens, Text, ITextStyles } from 'office-ui-fabric-react';
 
 const alertClicked = (): void => {
   alert('Clicked');
 };
 export class CardCompactExample extends React.Component<{}, {}> {
   public render(): JSX.Element {
-    const styles = {
-      siteText: {
-        root: {
-          color: '#025F52',
-          fontSize: 12,
-          fontWeight: FontWeights.semibold
-        }
-      },
-      descriptionText: {
-        root: {
-          color: '#333333',
-          fontSize: 14,
-          fontWeight: FontWeights.regular
-        }
-      },
-      helpfulText: {
-        root: {
-          color: '#333333',
-          fontSize: 12,
-          fontWeight: FontWeights.regular
-        }
-      },
-      icon: {
-        root: {
-          color: '#0078D4',
-          fontSize: 16,
-          fontWeight: FontWeights.regular
-        }
-      },
-      footerCardSection: {
-        root: {
-          borderLeft: '1px solid #F3F2F1'
-        }
+    const siteTextStyles: ITextStyles = {
+      root: {
+        color: '#025F52',
+        fontWeight: FontWeights.semibold
+      }
+    };
+    const descriptionTextStyles: ITextStyles = {
+      root: {
+        color: '#333333',
+        fontWeight: FontWeights.regular
+      }
+    };
+    const helpfulTextStyles: ITextStyles = {
+      root: {
+        color: '#333333',
+        fontWeight: FontWeights.regular
+      }
+    };
+    const iconStyles: IIconStyles = {
+      root: {
+        color: '#0078D4',
+        fontSize: 16,
+        fontWeight: FontWeights.regular
+      }
+    };
+    const footerCardSectionStyles: ICardSectionStyles = {
+      root: {
+        borderLeft: '1px solid #F3F2F1'
       }
     };
 
-    const tokens = {
-      sectionStack: {
-        childrenGap: 20
-      },
-      card: {
-        childrenMargin: 12
-      },
-      footerCardSection: {
-        padding: '0px 0px 0px 12px'
-      }
-    };
+    const sectionStackTokens: IStackTokens = { childrenGap: 20 };
+    const cardTokens: ICardTokens = { childrenMargin: 12 };
+    const footerCardSectionTokens: ICardSectionTokens = { padding: '0px 0px 0px 12px' };
 
     return (
-      <Stack tokens={tokens.sectionStack}>
-        <Card compact tokens={tokens.card}>
+      <Stack tokens={sectionStackTokens}>
+        <Card compact tokens={cardTokens}>
           <Card.Item>
             <Text>Basic compact card</Text>
           </Card.Item>
         </Card>
 
-        <Card compact onClick={alertClicked} tokens={tokens.card}>
+        <Card compact onClick={alertClicked} tokens={cardTokens}>
           <Card.Item fill>
             <Image src="https://placehold.it/180x135" alt="Placeholder image." />
           </Card.Item>
           <Card.Section>
-            <Text styles={styles.siteText}>Contoso</Text>
-            <Text styles={styles.descriptionText}>Contoso Denver expansion design marketing hero guidelines</Text>
-            <Text styles={styles.helpfulText}>Is this recommendation helpful?</Text>
+            <Text variant="small" styles={siteTextStyles}>
+              Contoso
+            </Text>
+            <Text styles={descriptionTextStyles}>Contoso Denver expansion design marketing hero guidelines</Text>
+            <Text variant="small" styles={helpfulTextStyles}>
+              Is this recommendation helpful?
+            </Text>
           </Card.Section>
-          <Card.Section styles={styles.footerCardSection} tokens={tokens.footerCardSection}>
-            <Icon iconName="RedEye" styles={styles.icon} />
-            <Icon iconName="SingleBookmark" styles={styles.icon} />
+          <Card.Section styles={footerCardSectionStyles} tokens={footerCardSectionTokens}>
+            <Icon iconName="RedEye" styles={iconStyles} />
+            <Icon iconName="SingleBookmark" styles={iconStyles} />
             <Stack.Item grow={1}>
               <span />
             </Stack.Item>
-            <Icon iconName="MoreVertical" styles={styles.icon} />
+            <Icon iconName="MoreVertical" styles={iconStyles} />
           </Card.Section>
         </Card>
       </Stack>
