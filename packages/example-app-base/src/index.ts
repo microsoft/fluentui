f8b051beee68be20218de5f83a0ab37bb3892eb7
Elizabeth Craig <elcraig@microsoft.com>
2019-04-24 22:03:24 -0700

    Fix example-app-base utility exports

--- packages/example-app-base/src/index.ts
+++ packages/example-app-base/src/index.ts
@@ -9,14 +9,7 @@ export * from './components/Highlight/Highlight';
 export * from './components/MarkdownTable/index';
 export * from './components/PropertiesTable/index';
 export * from './components/templates/index';
-export * from './utilities/beep';
-export * from './utilities/createApp';
-export * from './utilities/customizations';
-export * from './utilities/debugging';
-export * from './utilities/examplesOf';
+export * from './utilities/index';
 export * from './utilities/parser/index';
-
-// do NOT re-export wildcard in two levels: webpack does not understand this and will have errors finding the modules
-export * from 'office-ui-fabric-react/lib/utilities/exampleData';
 
 import './version';
---
+++ packages/example-app-base/src/utilities/index.ts
@@ -0,0 +1,6 @@
+export * from './beep';
+export * from './createApp';
+export * from './customizations';
+export * from './data';
+export * from './debugging';
+export * from './examplesOf';
