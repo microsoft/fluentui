77372701f6b72c2bf54431b55344ea0e42615a94
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-04-26 14:17:18 -0700

    getting some more of the plan for SemanticSlots' data structuring figured out

--- apps/theming-designer/src/components/SemanticSlots.tsx
+++ apps/theming-designer/src/components/SemanticSlots.tsx
@@ -5,6 +5,7 @@ import { ITheme } from 'office-ui-fabric-react/lib/Styling';
 import { Stack } from 'office-ui-fabric-react/lib/Stack';
 import { mergeStyles } from '@uifabric/merge-styles';
 import { SemanticSlotsDetailsList } from './SemanticSlotsDetailsList';
+import { getVariant } from '@uifabric/variants';
 
 export interface ISemanticSlotsProps {
   theme?: ITheme;
@@ -39,7 +40,87 @@ export class SemanticSlots extends React.Component<ISemanticSlotsProps, ISemanti
   constructor(props: any) {
     super(props);
     this.state = {
-      slotNames: [],
+      slotNames: [
+        // DEFAULTS - 13
+        'bodyBackground', // white
+        'bodyStandoutBackground', // neutralLighterAlt
+        'bodyFrameBackground', // white
+        'bodyFrameDivider', // neturalLight
+        'bodyText', // neutralPrimary
+        'bodyTextChecked', // black
+        'bodySubtext', // neutralSecondary
+        'bodyDivider', // neutralLight
+        'disabledBodyText', // neutralTertiary
+        'focusBorder', // neutralSecondary
+        'variantBorder', // neutralLight
+        'variantBorderHovered', // neutralTertiary
+        'defaultStateBackground', // neutralLighterAlt
+        // LINKS - 4
+        'actionLink', // neutralPrimary
+        'actionLinkHovered', // neutralDark
+        'link', // themePrimary
+        'linkHovered', // themeDarker
+        // BUTTONS - 25
+        'buttonBackground', // neutralLighter
+        'buttonBackgroundChecked', // neutralTertiaryAlt
+        'buttonBackgroundHovered', // neutralLight
+        'buttonBackgroundCheckedHovered', // neutralLight
+        'buttonBackgroundPressed', // neutralLight
+        'buttonBackgroundDisabled', // neutralLighter
+        'buttonBorder', // 'transparent'
+        'buttonText', // neutralPrimary
+        'buttonTextHovered', // neutralDark
+        'buttonTextChecked', // neutralDark
+        'buttonTextCheckedHovered', // black
+        'buttonTextPressed', // neutralDark
+        'buttonTextDisabled', // neutralTertiary
+        'buttonBorderDisabled', // 'transparent'
+
+        'primaryButtonBackground', // themePrimary
+        'primaryButtonBackgroundHovered', // themeDarkAlt
+        'primaryButtonBackgroundPressed', // themeDark
+        'primaryButtonBackgroundDisabled', // neutralLighter
+        'primaryButtonBorder', // 'transparent'
+        'primaryButtonText', // white
+        'primaryButtonTextHovered', // white
+        'primaryButtonTextPressed', // white
+        'primaryButtonTextDisabled', // neutralQuaternary
+
+        'accentButtonBackground', // accent
+        'accentButtonText', // white
+        // INPUTS - 14
+        'inputBorder', // neutralTertiary
+        'inputBorderHovered', // neutralPrimary
+        'inputBackground', // white
+        'inputBackgroundChecked', // themePrimary
+        'inputBackgroundCheckedHovered', // themeDarkAlt
+        'inputForegroundChecked', // white
+        'inputFocusBorderAlt', // themePrimary
+        'smallInputBorder', // neutralSecondary
+        'inputText', // neutralPrimary
+        'inputTextHovered', // neutralDark
+        'inputPlaceholderText', // neutralSecondary
+        'disabledBackground', // neutralLighter
+        'disabledText', // neutralTertiary
+        'disabledSubText', // neutralQuaternary
+        // LISTS - 7
+        'listBackground', // white
+        'listText', // neutralPrimary
+        'listItemBackgroundHovered', // neutralLighter
+        'listItemBackgroundChecked', // neutralLight
+        'listItemBackgroundCheckedHovered', // neutralQuaternaryAlt
+        'listHeaderBackgroundHovered', // neutralLighter
+        'listHeaderBackgroundPressed', // neutralLight
+        // MENUS - 8
+        'menuBackground', // white
+        'menuDivider', // neutralTertiaryAlt
+        'menuIcon', // themePrimary
+        'menuHeader', // themePrimary
+        'menuItemBackgroundHovered', // neutralLighter
+        'menuItemBackgroundPressed', // neutralLight
+        'menuItemText', // neutralPrimary
+        'menuItemTextHovered' // neutralDark
+      ],
       noneSlots: [],
       neutralSlots: [],
       softSlots: [],
@@ -68,93 +149,17 @@ export class SemanticSlots extends React.Component<ISemanticSlotsProps, ISemanti
     );
   };
 
-  // fill slotNames
-  slotNames = [
-    // DEFAULTS - 13
-    'bodyBackground', // white
-    'bodyStandoutBackground', // neutralLighterAlt
-    'bodyFrameBackground', // white
-    'bodyFrameDivider', // neturalLight
-    'bodyText', // neutralPrimary
-    'bodyTextChecked', // black
-    'bodySubtext', // neutralSecondary
-    'bodyDivider', // neutralLight
-    'disabledBodyText', // neutralTertiary
-    'focusBorder', // neutralSecondary
-    'variantBorder', // neutralLight
-    'variantBorderHovered', // neutralTertiary
-    'defaultStateBackground', // neutralLighterAlt
-    // LINKS - 4
-    'actionLink', // neutralPrimary
-    'actionLinkHovered', // neutralDark
-    'link', // themePrimary
-    'linkHovered', // themeDarker
-    // BUTTONS - 25
-    'buttonBackground', // neutralLighter
-    'buttonBackgroundChecked', // neutralTertiaryAlt
-    'buttonBackgroundHovered', // neutralLight
-    'buttonBackgroundCheckedHovered', // neutralLight
-    'buttonBackgroundPressed', // neutralLight
-    'buttonBackgroundDisabled', // neutralLighter
-    'buttonBorder', // 'transparent'
-    'buttonText', // neutralPrimary
-    'buttonTextHovered', // neutralDark
-    'buttonTextChecked', // neutralDark
-    'buttonTextCheckedHovered', // black
-    'buttonTextPressed', // neutralDark
-    'buttonTextDisabled', // neutralTertiary
-    'buttonBorderDisabled', // 'transparent'
-
-    'primaryButtonBackground', // themePrimary
-    'primaryButtonBackgroundHovered', // themeDarkAlt
-    'primaryButtonBackgroundPressed', // themeDark
-    'primaryButtonBackgroundDisabled', // neutralLighter
-    'primaryButtonBorder', // 'transparent'
-    'primaryButtonText', // white
-    'primaryButtonTextHovered', // white
-    'primaryButtonTextPressed', // white
-    'primaryButtonTextDisabled', // neutralQuaternary
-
-    'accentButtonBackground', // accent
-    'accentButtonText', // white
-    // INPUTS - 14
-    'inputBorder', // neutralTertiary
-    'inputBorderHovered', // neutralPrimary
-    'inputBackground', // white
-    'inputBackgroundChecked', // themePrimary
-    'inputBackgroundCheckedHovered', // themeDarkAlt
-    'inputForegroundChecked', // white
-    'inputFocusBorderAlt', // themePrimary
-    'smallInputBorder', // neutralSecondary
-    'inputText', // neutralPrimary
-    'inputTextHovered', // neutralDark
-    'inputPlaceholderText', // neutralSecondary
-    'disabledBackground', // neutralLighter
-    'disabledText', // neutralTertiary
-    'disabledSubText', // neutralQuaternary
-    // LISTS - 7
-    'listBackground', // white
-    'listText', // neutralPrimary
-    'listItemBackgroundHovered', // neutralLighter
-    'listItemBackgroundChecked', // neutralLight
-    'listItemBackgroundCheckedHovered', // neutralQuaternaryAlt
-    'listHeaderBackgroundHovered', // neutralLighter
-    'listHeaderBackgroundPressed', // neutralLight
-    // MENUS - 8
-    'menuBackground', // white
-    'menuDivider', // neutralTertiaryAlt
-    'menuIcon', // themePrimary
-    'menuHeader', // themePrimary
-    'menuItemBackgroundHovered', // neutralLighter
-    'menuItemBackgroundPressed', // neutralLight
-    'menuItemText', // neutralPrimary
-    'menuItemTextHovered' // neutralDark
-  ];
-
   // fill noneSlots
-  // semanticSlotWidget(SemanticColorSlots.bodyBackground);
+  private _fillNoneVariantSlots() {
+    // call getVariant to get the default
+  }
 
   // fill neutralSlots
+  private _fillNeutralVariantSlots() {
+    // call getVariant with VariantThemeType.Neutral & access the returned theme object's semanticColors to get the list
+    getVariant();
+    // call setState for neturalSlots and fill the list: each value is the JSX for that slot's colorbox and label
+  }
 
   // fill softSlots
 
