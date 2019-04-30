634914a899a098a97441d33c58b6cbe3896c210c
Aneesha Kommineni <aneeshak@microsoft.com>
2019-04-30 15:59:54 -0700

    Theme Designer app: Colors left nav, Samples card, A11y Checker card, Fabric Palette card, Export Panel (#8738)
    * Finished theme designer cleanup.
    
    * removing license files
    
    * removing color styles change that got added while resolving merge conflicts during squashing commit history
    
    * change file
    
    * adding Theme Designer tile to the PR deploy site
    
    * changes addressing some comments: a lot around usage of bang/undefined for themerules
    
    * changes responding to most of the comments, about 10 left: mostly around imports, removing bang operator usage (checking for undefined before instead of using bang), removing comments previously missed, etc
    
    * fixing imports - removing relative paths
    
    * removing serve static dependency
    
    * Finished responding to all comments. Also added ability to hide SemanticSlotsCard (hidden for now). Made a couple other minor changes.
    
    * removing card change console log statement
    
    * change file
    
    * fixed a bunch of the styling: all the Stacks resize/grow properly, header spans full width of page, etc
    
    * updating api
    
    * [Theme Designer] Layout/style rough-in
    
    * [Theme Designer] Initial theme should match initial state.
    
    * changing ensureSlots back to insureSlots to avoid breaking sharepoint & removing extra semantic slots enums (this list will be fully updated in next theming designer PR that has Semantic Slots Card fully implemented
    
    * fixing start off colors to match design spec & fixing problem where samples card's background color doesn't change with color picker input

---
+++ apps/theming-designer/src/components/ThemingDesigner.tsx
@@ -0,0 +1,186 @@
+import * as React from 'react';
+import { AccessibilityChecker } from './AccessibilityChecker';
+import { BaseComponent } from 'office-ui-fabric-react/lib/Utilities';
+import { BaseSlots, IThemeRules, ThemeGenerator, themeRulesStandardCreator } from 'office-ui-fabric-react/lib/ThemeGenerator';
+import { createTheme, ITheme } from 'office-ui-fabric-react/lib/Styling';
+import { FabricPalette } from './FabricPalette';
+import { getColorFromString, IColor } from 'office-ui-fabric-react/lib/Color';
+import { Header } from './Header';
+import { IconButton } from 'office-ui-fabric-react/lib/Button';
+import { isDark } from 'office-ui-fabric-react/lib/utilities/color/shades';
+import { mergeStyles } from '@uifabric/merge-styles';
+import { Samples } from './Samples';
+import { SemanticSlots } from './SemanticSlots';
+import { Stack } from 'office-ui-fabric-react/lib/Stack';
+import { ThemeDesignerColorPicker } from './ThemeDesignerColorPicker';
+import { ThemeProvider } from 'office-ui-fabric-react/lib/Foundation';
+
+export interface IThemingDesignerState {
+  primaryColor: IColor;
+  textColor: IColor;
+  backgroundColor: IColor;
+  theme?: ITheme;
+  themeRules?: IThemeRules;
+}
+
+const outerMostStack = mergeStyles({
+  width: '100%'
+});
+
+const sidebarStyles = mergeStyles({
+  borderRight: '1px solid #ddd',
+  minHeight: '100%',
+  paddingRight: '1rem',
+  position: 'fixed',
+  top: '60px',
+  left: '10px'
+});
+
+let colorChangeTimeout: number;
+
+let hideSemanticSlots: boolean;
+let semanticSlotsCard: JSX.Element;
+
+export class ThemingDesigner extends BaseComponent<{}, IThemingDesignerState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = this._buildInitialState();
+
+    this._onPrimaryColorPickerChange = this._onPrimaryColorPickerChange.bind(this);
+    this._onTextColorPickerChange = this._onTextColorPickerChange.bind(this);
+    this._onBkgColorPickerChange = this._onBkgColorPickerChange.bind(this);
+
+    hideSemanticSlots = true;
+    if (!hideSemanticSlots) {
+      semanticSlotsCard = <SemanticSlots />;
+    } else {
+      <div />;
+    }
+  }
+
+  public render() {
+    return (
+      <Stack gap={10} className={outerMostStack}>
+        <Header themeRules={this.state.themeRules} />
+        <Stack horizontal gap={10}>
+          <Stack gap={20} className={sidebarStyles}>
+            <h1>
+              <IconButton
+                disabled={false}
+                checked={false}
+                iconProps={{ iconName: 'Color', styles: { root: { fontSize: '20px' } } }}
+                title="Colors"
+                ariaLabel="Colors"
+              />
+              Color
+            </h1>
+            {/* the three base slots, prominently displayed at the top of the page */}
+            <ThemeDesignerColorPicker
+              color={this.state.primaryColor}
+              onColorChange={this._onPrimaryColorPickerChange}
+              label={'Primary color'}
+            />
+            <ThemeDesignerColorPicker color={this.state.textColor} onColorChange={this._onTextColorPickerChange} label={'Text color'} />
+            <ThemeDesignerColorPicker
+              color={this.state.backgroundColor}
+              onColorChange={this._onBkgColorPickerChange}
+              label={'Background color'}
+            />
+          </Stack>
+          <Stack.Item grow={1}>
+            <Stack horizontalAlign={'center'}>
+              <ThemeProvider theme={this.state.theme}>
+                <Samples backgroundColor={this.state.backgroundColor.str} />
+              </ThemeProvider>
+              <AccessibilityChecker theme={this.state.theme} themeRules={this.state.themeRules} />
+              <FabricPalette themeRules={this.state.themeRules} />
+              {semanticSlotsCard}
+            </Stack>
+          </Stack.Item>
+        </Stack>
+      </Stack>
+    );
+  }
+
+  private _onPrimaryColorPickerChange(newColor: IColor | undefined) {
+    this._onColorChange(this.state.primaryColor, BaseSlots.primaryColor, newColor);
+  }
+
+  private _onTextColorPickerChange(newColor: IColor | undefined) {
+    this._onColorChange(this.state.textColor, BaseSlots.foregroundColor, newColor);
+  }
+
+  private _onBkgColorPickerChange(newColor: IColor | undefined) {
+    this._onColorChange(this.state.backgroundColor, BaseSlots.backgroundColor, newColor);
+  }
+
+  private _makeNewTheme = (): void => {
+    if (this.state.themeRules) {
+      const themeAsJson: { [key: string]: string } = ThemeGenerator.getThemeAsJson(this.state.themeRules);
+
+      const finalTheme = createTheme({
+        ...{ palette: themeAsJson }
+      });
+      this.setState({ theme: finalTheme });
+    }
+  };
+
+  private _onColorChange = (colorToChange: IColor, baseSlot: BaseSlots, newColor: IColor | undefined) => {
+    if (colorChangeTimeout) {
+      clearTimeout(colorChangeTimeout);
+    }
+    if (newColor) {
+      if (colorToChange === this.state.primaryColor) {
+        this.setState({ primaryColor: newColor });
+      } else if (colorToChange === this.state.textColor) {
+        this.setState({ textColor: newColor });
+      } else if (colorToChange === this.state.backgroundColor) {
+        this.setState({ backgroundColor: newColor });
+      } else {
+        return;
+      }
+      colorChangeTimeout = this._async.setTimeout(() => {
+        const themeRules = this.state.themeRules;
+        if (themeRules) {
+          const currentIsDark = isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!);
+          ThemeGenerator.setSlot(themeRules[BaseSlots[baseSlot]], newColor, currentIsDark, true, true);
+          if (currentIsDark !== isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!)) {
+            // isInverted got swapped, so need to refresh slots with new shading rules
+            ThemeGenerator.insureSlots(themeRules, currentIsDark);
+          }
+        }
+        this.setState({ themeRules: themeRules }, this._makeNewTheme);
+      }, 20);
+      // 20ms is low enough that you can slowly drag to change color and see that theme,
+      // but high enough that quick changes don't get bogged down by a million changes inbetween
+    }
+  };
+
+  private _buildInitialState = (): IThemingDesignerState => {
+    const themeRules = themeRulesStandardCreator();
+    const colors = {
+      primaryColor: getColorFromString('#0078d4')!,
+      textColor: getColorFromString('#323130')!,
+      backgroundColor: getColorFromString('#ffffff')!
+    };
+    ThemeGenerator.insureSlots(themeRules, isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!));
+    ThemeGenerator.setSlot(themeRules[BaseSlots[BaseSlots.primaryColor]], colors.primaryColor);
+    ThemeGenerator.setSlot(themeRules[BaseSlots[BaseSlots.foregroundColor]], colors.textColor);
+    ThemeGenerator.setSlot(themeRules[BaseSlots[BaseSlots.backgroundColor]], colors.backgroundColor);
+
+    const themeAsJson: { [key: string]: string } = ThemeGenerator.getThemeAsJson(themeRules);
+
+    const finalTheme = createTheme({
+      ...{ palette: themeAsJson }
+    });
+
+    const state = {
+      ...colors,
+      theme: finalTheme,
+      themeRules: themeRules
+    };
+
+    return state;
+  };
+}
