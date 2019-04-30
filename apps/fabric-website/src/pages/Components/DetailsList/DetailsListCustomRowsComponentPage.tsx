3db39f4fcfa2503c488bf0686de0193e32fdb9fa
Elizabeth Craig <elcraig@microsoft.com>
2019-04-30 11:27:05 -0700

    Delete unneeded component pages and disable appInsights for now

--- apps/fabric-website/src/pages/Components/DetailsList/DetailsListCustomRowsComponentPage.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { DetailsListCustomRowsPage } from '@uifabric/fabric-website-resources/lib/components/pages/DetailsList/DetailsListCustomRowsPage';
-
-export class DetailsListCustomRowsComponentPage extends React.Component<any, any> {
-  public render(): JSX.Element {
-    return <DetailsListCustomRowsPage isHeaderVisible={false} />;
-  }
-}
