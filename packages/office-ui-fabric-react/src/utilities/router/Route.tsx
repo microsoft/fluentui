a629ff049f033c196a805093581be64951a66ad5
Elizabeth Craig <elcraig@microsoft.com>
2019-04-30 17:18:08 -0700

    Router typing and doc improvements

--- packages/office-ui-fabric-react/src/utilities/router/Route.tsx
+++ packages/office-ui-fabric-react/src/utilities/router/Route.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 
-export interface IRouteProps {
+/** A component function which doesn't explicitly declare itself as React.StatelessComponent/SFC/FunctionComponent */
+export type ComponentLike = (props?: {}) => React.ReactElement<{}> | null;
+// ^^ This is needed so we don't break consumers who may not explicitly declare their
+// functional components as one of React's functional component types.
+
+export interface IRouteProps extends React.ClassAttributes<Route> {
   path?: string;
-  component?: React.Component<any, any>;
-  getComponent?: (cb: any) => void;
+
+  children?: React.ReactNode;
+
+  /**
+   * The component to render for this route's content (or a loading placeholder if `getComponent` is provided).
+   */
+  component?: React.ComponentType | ComponentLike;
+
+  /**
+   * Function that loads the component asynchronously.
+   * Notify when loading has finished using `cb`.
+   * If `component` is provided, it will be rendered while waiting for the callback.
+   */
+  getComponent?: (cb: (component: React.ComponentType | ComponentLike) => void) => void;
 }
 
-export class Route extends React.Component<any, any> {}
+export class Route extends React.PureComponent<IRouteProps> {}
