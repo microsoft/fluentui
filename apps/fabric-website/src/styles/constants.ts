cdcb6da608b025a66906ccd681a1458240d9ccca
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-01 16:52:38 -0700

    Add app width constants

--- apps/fabric-website/src/styles/constants.ts
+++ apps/fabric-website/src/styles/constants.ts
@@ -26,6 +26,28 @@ export const contentPaddingLg = 32;
 export const contentPaddingXl = 40;
 export const contentPaddingBottom = 100;
 
+// Component variables
+export const contentWidth = 1024; // Target width for content area to show large breakpoint width for examples;
+export const contentPadding = appPaddingSm;
+
+export const PageHeaderFullHeight = 136;
+
+export const navWidthSm = 252;
+export const navWidthLg = 302;
+export const navPadding = appPaddingSm;
+
+// We want the text to appear the same distance from the body and the nav. It include 8 padding already.
+export const sideRailPaddingLeft = appPaddingMd - 8;
+export const sideRailWidth = 182 + sideRailPaddingLeft;
+
+export const CodeBlockToggleButtonHeight = 28;
+
+// Left padding + Nav + Body and Content Padding + Content + SideRail + Right padding.
+export const appMaximumWidthSm =
+  appPaddingLeftLg + navWidthSm + appPaddingMd + contentPadding * 2 + contentWidth + sideRailWidth + appPaddingRightLg;
+export const appMaximumWidthLg =
+  appPaddingLeftLg + navWidthLg + appPaddingMd + contentPadding * 2 + contentWidth + sideRailWidth + appPaddingRightLg;
+
 export const queryUhfMobileMin = `@media screen and (min-width: ${ScreenWidthMinUhfMobile}px)`;
 export const queryXLargeMin = `@media screen and (min-width: ${ScreenWidthMinXLarge}px)`;
 
