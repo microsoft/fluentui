10914cee6719aac15268728098ca3879309b7a6d
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-01 13:41:18 -0700

    Register file type icons

--- apps/fabric-website/src/root.tsx
+++ apps/fabric-website/src/root.tsx
@@ -1,4 +1,5 @@
 import { registerIcons } from 'office-ui-fabric-react';
+import { initializeFileTypeIcons } from '@uifabric/file-type-icons';
 import { createSite } from './utilities/createSite';
 import * as platformPickerStyles from '@uifabric/example-app-base/lib/components/PlatformPicker/PlatformPicker.module.scss';
 import { SiteDefinition } from './SiteDefinition/index';
@@ -6,6 +7,8 @@ import { HomePage } from './pages/HomePage/HomePage';
 import { AndroidLogo, AppleLogo, WebLogo } from './utilities/index';
 
 // TODO: handle redirects
+
+initializeFileTypeIcons('https://static2.sharepointonline.com/files/fabric/assets/item-types-fluent/');
 
 registerIcons({
   icons: {
