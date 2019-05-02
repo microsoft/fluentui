aac609a84a00fbe8664bbfff3f0af80c3eb62554
Elizabeth Craig <elcraig@microsoft.com>
2019-05-02 09:25:53 -0700

    Fix route types

--- packages/office-ui-fabric-react/src/utilities/router/Route.tsx
+++ packages/office-ui-fabric-react/src/utilities/router/Route.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 /** A component function which doesn't explicitly declare itself as React.StatelessComponent/SFC/FunctionComponent */
-export type ComponentLike = (props?: {}) => React.ReactElement<{}> | null;
+export type ComponentLike<P = {}> = React.ComponentClass<P> | ((props?: P) => React.ReactElement<P> | null);
 // ^^ This is needed so we don't break consumers who may not explicitly declare their
 // functional components as one of React's functional component types.
 
@@ -13,14 +13,14 @@ export interface IRouteProps extends React.ClassAttributes<Route> {
   /**
    * The component to render for this route's content (or a loading placeholder if `getComponent` is provided).
    */
-  component?: React.ComponentType | ComponentLike;
+  component?: ComponentLike;
 
   /**
    * Function that loads the component asynchronously.
    * Notify when loading has finished using `cb`.
    * If `component` is provided, it will be rendered while waiting for the callback.
    */
-  getComponent?: (cb: (component: React.ComponentType | ComponentLike) => void) => void;
+  getComponent?: (cb: (component: ComponentLike) => void) => void;
 }
 
 export class Route extends React.PureComponent<IRouteProps> {}
