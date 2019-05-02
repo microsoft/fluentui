c10e2981d691a1b2067546667d3cca134195095d
Kenneth Chau <kenotron@users.noreply.github.com>
2019-05-01 19:42:27 -0700

    Site deploy: this is the one for for master branch (#8916)
    * master web site deploy
    
    * fixing up shrinkwrap to accept newer just-task
    
    * fixing per PR feedback

--- scripts/just-task.js
+++ scripts/just-task.js
@@ -1,6 +1,8 @@
 // @ts-check
 
-const { task, series, parallel, condition, option, argv, logger } = require('just-task');
+const just = require('just-task');
+const { task, series, parallel, condition, option, argv, logger, addResolvePath } = just;
+
 const path = require('path');
 const fs = require('fs');
 
@@ -21,65 +23,73 @@ const perfTest = require('./tasks/perf-test');
 
 let packageJson;
 
-option('production');
+function preset() {
+  // this add s a resolve path for the build tooling deps like TS from the scripts folder
+  addResolvePath(__dirname);
 
-// Adds an alias for 'npm-install-mode' for backwards compatibility
-option('min', { alias: 'npm-install-mode' });
+  option('production');
 
-option('webpackConfig', { alias: 'w' });
+  // Adds an alias for 'npm-install-mode' for backwards compatibility
+  option('min', { alias: 'npm-install-mode' });
 
-// Build only commonjs (not other TS variants) but still run other tasks
-option('commonjs');
+  option('webpackConfig', { alias: 'w' });
 
-registerTask('clean', clean);
-registerTask('copy', copy);
-registerTask('jest', jest);
-registerTask('jest-watch', jestWatch);
-registerTask('sass', sass);
-registerTask('ts:commonjs', ts.commonjs);
-registerTask('ts:esm', ts.esm);
-registerTask('ts:amd', ts.amd);
-registerTask('tslint', tslint);
-registerTask('ts:commonjs-only', ts.commonjsOnly);
-registerTask('webpack', webpack);
-registerTask('webpack-dev-server', webpackDevServer);
-registerTask('verify-api-extractor', verifyApiExtractor);
-registerTask('update-api-extractor', updateApiExtractor);
-registerTask('lint-imports', lintImports);
-registerTask('prettier', prettier);
-registerTask('bundle-size-collect', bundleSizeCollect);
-registerTask('check-for-modified-files', checkForModifiedFiles);
-registerTask('generate-version-files', generateVersionFiles);
-registerTask('perf-test', perfTest);
+  // Build only commonjs (not other TS variants) but still run other tasks
+  option('commonjs');
 
-task('ts', parallel('ts:commonjs', 'ts:esm', condition('ts:amd', () => argv().production && !argv().min)));
+  registerTask('clean', clean);
+  registerTask('copy', copy);
+  registerTask('jest', jest);
+  registerTask('jest-watch', jestWatch);
+  registerTask('sass', sass);
+  registerTask('ts:commonjs', ts.commonjs);
+  registerTask('ts:esm', ts.esm);
+  registerTask('ts:amd', ts.amd);
+  registerTask('tslint', tslint);
+  registerTask('ts:commonjs-only', ts.commonjsOnly);
+  registerTask('webpack', webpack);
+  registerTask('webpack-dev-server', webpackDevServer);
+  registerTask('verify-api-extractor', verifyApiExtractor);
+  registerTask('update-api-extractor', updateApiExtractor);
+  registerTask('lint-imports', lintImports);
+  registerTask('prettier', prettier);
+  registerTask('bundle-size-collect', bundleSizeCollect);
+  registerTask('check-for-modified-files', checkForModifiedFiles);
+  registerTask('generate-version-files', generateVersionFiles);
+  registerTask('perf-test', perfTest);
 
-task(
-  'build',
-  series(
-    'clean',
-    'copy',
-    'sass',
-    parallel(
-      condition('tslint', () => !argv().min),
-      condition('jest', () => !argv().min),
-      series(
-        argv().commonjs ? 'ts:commonjs-only' : 'ts',
-        condition('lint-imports', () => !argv().min),
-        // verify-api-extractor must always be run now because the api-docs package depends on its output
-        parallel(condition('webpack', () => !argv().min), 'verify-api-extractor')
+  task('ts', parallel('ts:commonjs', 'ts:esm', condition('ts:amd', () => argv().production && !argv().min)));
+
+  task(
+    'build',
+    series(
+      'clean',
+      'copy',
+      'sass',
+      parallel(
+        condition('tslint', () => !argv().min),
+        condition('jest', () => !argv().min),
+        series(
+          argv().commonjs ? 'ts:commonjs-only' : 'ts',
+          condition('lint-imports', () => !argv().min),
+          // verify-api-extractor must always be run now because the api-docs package depends on its output
+          parallel(condition('webpack', () => !argv().min), 'verify-api-extractor')
+        )
       )
     )
-  )
-);
+  );
 
-// Special case build for the serializer, which needs to absolutely run typescript and jest serially.
-task('build-jest-serializer-merge-styles', series('ts', 'jest'));
+  // Special case build for the serializer, which needs to absolutely run typescript and jest serially.
+  task('build-jest-serializer-merge-styles', series('ts', 'jest'));
 
-task('build-commonjs-only', series('clean', 'ts:commonjs-only'));
-task('code-style', series('prettier', 'tslint'));
-task('update-api', series('clean', 'copy', 'sass', 'ts', 'update-api-extractor'));
-task('dev', series('clean', 'copy', 'sass', 'webpack-dev-server'));
+  task('build-commonjs-only', series('clean', 'ts:commonjs-only'));
+  task('code-style', series('prettier', 'tslint'));
+  task('update-api', series('clean', 'copy', 'sass', 'ts', 'update-api-extractor'));
+  task('dev', series('clean', 'copy', 'sass', 'webpack-dev-server'));
+}
+
+module.exports = preset;
+preset.just = just;
 
 // Utility functions
 
