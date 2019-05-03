f89f00753292edcd7a98705745b992bce27ca42b
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-05-03 11:00:02 -0700

    isInverted flag needs to be in initial set state

--- apps/theming-designer/src/components/Header.tsx
+++ apps/theming-designer/src/components/Header.tsx
@@ -61,9 +61,9 @@ export class Header extends React.Component<IHeaderProps, IHeaderState> {
               disabled={false}
               checked={false}
               iconProps={{ iconName: 'WindowsLogo', styles: { root: { fontSize: '20px' } } }}
-              ariaLabel="Microsoft UI Fabric | Theming Designer"
+              ariaLabel="Microsoft UI Fabric | Theme Designer"
             />
-            <Label styles={{ root: { fontWeight: 'bold' } }}>Microsoft UI Fabric | Theming Designer</Label>
+            <Label styles={{ root: { fontWeight: 'bold' } }}>Microsoft UI Fabric | Theme Designer</Label>
           </Stack>
           <Stack horizontal styles={{ root: { position: 'absolute', right: '20px' } }}>
             <IconButton
