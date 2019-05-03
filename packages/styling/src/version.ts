b715c8833ef467e6be5d22a7bea8961e6ca5122e
Elizabeth Craig <elcraig@microsoft.com>
2019-05-03 15:35:09 -0700

    Fix versions after unfinished release

--- packages/styling/src/version.ts
+++ packages/styling/src/version.ts
@@ -1,4 +1,4 @@
-// @uifabric/styling@6.47.1
+// @uifabric/styling@6.47.2
   // Do not modify this file, the file is generated as part of publish. The checked in version is a placeholder only.
   import { setVersion } from '@uifabric/set-version';
-  setVersion('@uifabric/styling', '6.47.1');
+  setVersion('@uifabric/styling', '6.47.2');
