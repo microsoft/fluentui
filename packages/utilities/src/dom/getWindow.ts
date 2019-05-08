ab2d5c1d06444a1bf93a39883399da2c58115de8
David Zearing <dzearing@microsoft.com>
2019-05-08 10:29:21 -0700

    Adjusting a few more places where I spotted "typeof window" references.

--- packages/utilities/src/dom/getWindow.ts
+++ packages/utilities/src/dom/getWindow.ts
@@ -1,4 +1,16 @@
 import { _isSSR } from './setSSR';
+
+let _window: Window | undefined = undefined;
+
+// Note: Accessing "window" in IE11 is somewhat expensive, and calling "typeof window"
+// hits a memory leak, whereas aliasing it and calling "typedef _window" does not.
+// Caching the window value at the file scope lets us minimize the impact.
+try {
+  _window = window;
+} catch (e) {
+  /* no-op */
+}
+
 /**
  * Helper to get the window object. Note that in popup scenarios the window object
  * may not be the window use ex
@@ -6,11 +18,11 @@ import { _isSSR } from './setSSR';
  * @public
  */
 export function getWindow(rootElement?: Element | null): Window | undefined {
-  if (_isSSR || typeof window === 'undefined') {
+  if (_isSSR || typeof _window === 'undefined') {
     return undefined;
   } else {
     return rootElement && rootElement.ownerDocument && rootElement.ownerDocument.defaultView
       ? rootElement.ownerDocument.defaultView
-      : window;
+      : _window;
   }
 }
