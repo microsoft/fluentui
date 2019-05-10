c6598634cbf30bbcd9d07b6134e3514c2f7a0013
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-10 16:36:01 -0700

    Replace use of withPlatform with PlatformContext.Consumer

--- apps/fabric-website/src/pages/Styles/StylesAreaPage.tsx
+++ apps/fabric-website/src/pages/Styles/StylesAreaPage.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { Page, IPageProps, withPlatform } from '@uifabric/example-app-base/lib/index2';
+import { Page, IPageProps, PlatformContext } from '@uifabric/example-app-base/lib/index2';
 import { getSubTitle } from '../../utilities/index';
 import { Platforms } from '../../interfaces/Platforms';
 
 export interface IStylesPageProps extends IPageProps<Platforms> {}
 
-export const StylesPageBase: React.StatelessComponent<IStylesPageProps> = props => {
+export const StylesAreaPageBase: React.StatelessComponent<IStylesPageProps> = props => {
   const { platform } = props;
   return <Page {...props} platform={platform} subTitle={getSubTitle(platform)} />;
 };
 
-export const StylesAreaPage: React.StatelessComponent<IStylesPageProps> = withPlatform<Platforms>(StylesPageBase);
+export const StylesAreaPage: React.StatelessComponent<IStylesPageProps> = (props: IStylesPageProps) => (
+  <PlatformContext.Consumer>{(platform: Platforms) => <StylesAreaPageBase platform={platform} {...props} />}</PlatformContext.Consumer>
+);
