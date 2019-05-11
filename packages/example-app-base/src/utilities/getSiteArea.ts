10f16e9adae7016d3b2321fabf7869231b1376d5
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-10 19:57:27 -0700

    Remove anchor link in getSiteArea

--- packages/example-app-base/src/utilities/getSiteArea.ts
+++ packages/example-app-base/src/utilities/getSiteArea.ts
@@ -1,4 +1,5 @@
 import { INavPage } from '../components/Nav/index';
+import { removeAnchorLink } from './removeAnchorLink';
 
 /**
  * Retrieves the current top level page name defined in the pages array from the window URL or the passed hash.
@@ -7,6 +8,7 @@ import { INavPage } from '../components/Nav/index';
  */
 
 export function getSiteArea(pages?: INavPage[], hash: string = location.hash): string {
+  hash = removeAnchorLink(hash);
   const topLevel = hash.indexOf('#/') > -1 ? hash.split('#/')[1].split('/')[0] : '';
   let area = topLevel;
   if (pages) {
