83c55e9da85dfe2d8ce1dd310a46b598101baaf2
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-05-13 12:44:17 -0700

    Add doc blocks for IDetailsColumnProps

--- packages/office-ui-fabric-react/src/components/DetailsList/DetailsColumn.types.ts
+++ packages/office-ui-fabric-react/src/components/DetailsList/DetailsColumn.types.ts
@@ -10,21 +10,65 @@ import { ITheme, IStyle } from '../../Styling';
  * {@docCategory DetailsList}
  */
 export interface IDetailsColumnProps extends React.ClassAttributes<DetailsColumnBase> {
+  /**
+   * The theme object to respect during render.
+   */
   theme?: ITheme;
+  /**
+   * The component styles to respect during render.
+   */
   styles?: IStyleFunctionOrObject<IDetailsColumnStyleProps, IDetailsColumnStyles>;
+  /**
+   * A reference to the component instance.
+   */
   componentRef?: () => void;
+  /**
+   * The column definition for the component instance.
+   */
   column: IColumn;
+  /**
+   * The column index for the component instance.
+   */
   columnIndex: number;
+  /**
+   * Parent ID used for accessibility label(s).
+   */
   parentId?: string;
+  /**
+   * Render function for providing a column header tooltip.
+   */
   onRenderColumnHeaderTooltip?: IRenderFunction<ITooltipHostProps>;
+  /**
+   * Callback fired when click event occurs.
+   */
   onColumnClick?: (ev: React.MouseEvent<HTMLElement>, column: IColumn) => void;
+  /**
+   * Callback fired on contextual menu event to provide contextual menu UI.
+   */
   onColumnContextMenu?: (column: IColumn, ev: React.MouseEvent<HTMLElement>) => void;
+  /**
+   * The drag and drop helper for the component instance.
+   */
   dragDropHelper?: IDragDropHelper | null;
+  /**
+   * Whether or not the column can be re-ordered via drag and drop.
+   */
   isDraggable?: boolean;
-  // @deprecated, use `updateDragInfo`
+  /**
+   * @deprecated, use `updateDragInfo`
+   */
   setDraggedItemIndex?: (itemIndex: number) => void;
+  /**
+   * Callback on drag and drop event.
+   */
   updateDragInfo?: (props: { itemIndex: number }, event?: MouseEvent) => void;
+  /**
+   * Whether or not the column has been dropped via drag and drop.
+   */
   isDropped?: boolean;
+  /**
+   * Custom styles for cell rendering.
+   */
   cellStyleProps?: ICellStyleProps;
 }
 
