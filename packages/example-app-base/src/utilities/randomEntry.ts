c039c0ef28803bf0222651cfc1ecf207eae47617
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-14 13:35:16 -0700

    Update comment

--- packages/example-app-base/src/utilities/randomEntry.ts
+++ packages/example-app-base/src/utilities/randomEntry.ts
@@ -1,5 +1,7 @@
-/** Returns a single random entry from an array. */
-// tslint:disable-next-line no-any
+/**
+ * Returns a single random entry from an array.
+ * @param arr Array you want a random entry from.
+ */
 export function randomEntry<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
