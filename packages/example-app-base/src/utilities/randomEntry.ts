cc15941550eb384cc42df7dda647ce124cfc5d88
Jordan Janzen <jordancjanzen@gmail.com>
2019-05-14 12:09:52 -0700

    Update packages/example-app-base/src/utilities/randomEntry.ts
    Co-Authored-By: Elizabeth Craig <ecraig12345@gmail.com>

--- packages/example-app-base/src/utilities/randomEntry.ts
+++ packages/example-app-base/src/utilities/randomEntry.ts
@@ -1,5 +1,5 @@
 /** Returns a single random entry from an array. */
 // tslint:disable-next-line no-any
-export function randomEntry(arr: any[]) {
+export function randomEntry<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
