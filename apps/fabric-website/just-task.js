7e651ea0068c2580700f46a4de3d0569789200d5
Ken <kchau@microsoft.com>
2019-05-14 12:09:42 -0700

    adding a yml file and accompanying tasks for release on Linux

--- apps/fabric-website/just-task.js
+++ apps/fabric-website/just-task.js
@@ -1,9 +1,10 @@
 const preset = require('@uifabric/build/just-task');
 const { task, option } = preset.just;
-const createFlightConfigTaskFactory = require('./scripts/createFlightConfig');
+const { createInternalFlightConfigTask, createPublicFlightConfigTask } = require('./scripts/createFlightConfig');
 
 module.exports = function() {
   preset();
   option('baseCDNUrl', { default: './dist' });
-  task('create-flight-config', createFlightConfigTaskFactory());
+  task('create-internal-flight-config', createInternalFlightConfigTask());
+  task('create-public-flight-config', createPublicFlightConfigTask());
 };
