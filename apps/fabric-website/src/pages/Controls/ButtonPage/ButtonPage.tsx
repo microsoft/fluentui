4382ed8bf9bc3793fe0511fdfae36a51b02f2ebb
lynamemi <lynam.emily@gmail.com>
2019-05-15 10:35:52 -0700

    Website: Adding Button to mobile docs (#9094)
    * Add button pages for ios and android
    
    * Add change file
    
    * Clean up props, urls, and remove Markdown tag
    
    * Change alt text and overview content

--- apps/fabric-website/src/pages/Controls/ButtonPage/ButtonPage.tsx
+++ apps/fabric-website/src/pages/Controls/ButtonPage/ButtonPage.tsx
@@ -3,7 +3,11 @@ import { Checkbox } from 'office-ui-fabric-react';
 import { ControlsAreaPage, IControlsPageProps } from '../ControlsAreaPage';
 import { ButtonPageProps } from './ButtonPage.doc';
 import * as exampleStylesImport from 'office-ui-fabric-react/lib/common/_exampleStyles.scss';
+import { Platforms } from 'src/interfaces/Platforms';
+import { IPageSectionProps, Markdown } from '../../../../../../packages/example-app-base/lib/index2';
+
 const exampleStyles: any = exampleStylesImport;
+const baseUrl = 'https://github.com/OfficeDev/office-ui-fabric-react/tree/master/apps/fabric-website/src/pages/Controls/ButtonPage/';
 
 export class ButtonPage extends React.Component<
   IControlsPageProps,
@@ -43,7 +47,37 @@ export class ButtonPage extends React.Component<
   public render() {
     const { areButtonsDisabled, areButtonsChecked } = this.state;
     const buttonPageProps = ButtonPageProps(areButtonsDisabled, areButtonsChecked);
-    return <ControlsAreaPage {...this.props} {...buttonPageProps[this.props.platform]} exampleKnobs={this.renderKnobs()} />;
+    return (
+      <ControlsAreaPage
+        {...this.props}
+        {...buttonPageProps[this.props.platform]}
+        exampleKnobs={this.renderKnobs()}
+        otherSections={this._otherSections(this.props.platform)}
+        title="Button"
+      />
+    );
+  }
+
+  private _otherSections(platform: Platforms): IPageSectionProps<Platforms>[] {
+    switch (platform) {
+      case 'ios':
+        return [
+          {
+            sectionName: 'Implementation',
+            editUrl: baseUrl + 'docs/ios/ButtonImplementation.md',
+            content: require('!raw-loader!@uifabric/fabric-website/src/pages/Controls/ButtonPage/docs/ios/ButtonImplementation.md') as string
+          }
+        ];
+
+      case 'android':
+        return [
+          {
+            sectionName: 'Implementation',
+            editUrl: baseUrl + 'docs/android/ButtonImplementation.md',
+            content: require('!raw-loader!@uifabric/fabric-website/src/pages/Controls/ButtonPage/docs/android/ButtonImplementation.md') as string
+          }
+        ];
+    }
   }
 
   private _onDisabledChanged = (ev: React.MouseEvent<HTMLElement>, disabled: boolean): void => {
