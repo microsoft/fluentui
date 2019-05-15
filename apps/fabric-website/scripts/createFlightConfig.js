129414e2b721578a4ff1ed3b211f812629af6167
Ken <kchau@microsoft.com>
2019-05-14 17:13:15 -0700

    adds a preconfigured basecdnurl and fix the oufrversion detection

--- apps/fabric-website/scripts/createFlightConfig.js
+++ apps/fabric-website/scripts/createFlightConfig.js
@@ -48,7 +48,7 @@ module.exports.createPublicFlightConfigTask = function() {
 
     let configData = {
       version: process.env.BUILD_BUILDNUMBER || '0',
-      baseCDNUrl: path.join(argv().baseCDNUrl || 'https://fabricweb.azureedge.net/fabric-website/', process.env.BUILD_BUILDNUMBER) + '/',
+      baseCDNUrl: path.join(argv().baseCDNUrl, process.env.BUILD_BUILDNUMBER) + '/',
       buildName: process.env.BUILD_DEFINITIONNAME || 'localbuild',
       createdDate: today
     };
