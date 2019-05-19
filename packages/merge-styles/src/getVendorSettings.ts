eba8e37a85720c8b74f09e9c397447061ca9d8a1
Dany Khalife <dakhalif@microsoft.com>
2019-05-19 12:03:22 -0700

    Fix undefined userAgent and add consistenct checks

--- packages/merge-styles/src/getVendorSettings.ts
+++ packages/merge-styles/src/getVendorSettings.ts
@@ -10,8 +10,6 @@ let _vendorSettings: IVendorSettings | undefined;
 export function getVendorSettings(): IVendorSettings {
   if (!_vendorSettings) {
     const doc = typeof document !== 'undefined' ? document : undefined;
-    const nav = typeof navigator !== 'undefined' ? navigator : undefined;
-    const userAgent = nav ? nav.userAgent.toLowerCase() : undefined;
 
     if (!doc) {
       _vendorSettings = {
@@ -21,11 +19,14 @@ export function getVendorSettings(): IVendorSettings {
         isMs: true
       };
     } else {
+        const nav = typeof navigator !== 'undefined' ? navigator : undefined;
+        const userAgent = nav && nav.userAgent ? nav.userAgent.toLowerCase() : undefined;
+
       _vendorSettings = {
-        isWebkit: !!(doc && 'WebkitAppearance' in doc.documentElement.style),
+        isWebkit: !!('WebkitAppearance' in doc.documentElement.style),
         isMoz: !!(userAgent && userAgent.indexOf('firefox') > -1),
         isOpera: !!(userAgent && userAgent.indexOf('opera') > -1),
-        isMs: !!(nav && (/rv:11.0/i.test(nav.userAgent) || /Edge\/\d./i.test(navigator.userAgent)))
+        isMs: !!(userAgent && (/rv:11.0/i.test(userAgent) || /Edge\/\d./i.test(userAgent)))
       };
     }
   }
