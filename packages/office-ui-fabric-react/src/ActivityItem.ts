f5b1740a9daf1c5c16291c677aec184596b2c4de
Aftab Hassan <aftabh@uw.edu>
2019-06-03 09:45:52 -0700

    Update ActivityItem.ts

--- packages/office-ui-fabric-react/src/ActivityItem.ts
+++ packages/office-ui-fabric-react/src/ActivityItem.ts
@@ -1,6 +1,1 @@
-// Adding a few comments
-// to increase bundle size
-// This is done to test a change in the build definition
-// aimed at improving size auditor website performance
-
 export * from './components/ActivityItem/index';
