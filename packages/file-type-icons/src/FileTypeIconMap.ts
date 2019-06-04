a0608b576f996ce322b524d2fb11541df9f7e390
Kosta Tachtevrenidis <tachtevrenidis@hotmail.com>
2019-06-04 23:36:45 +0300

    add extensions to icon groups to support LPC and Outlook mobile (#9320)
    * fixes to support LPC and other partner's missing mappings of filetype icons
    
    * missing sysfile entry
    
    * rush change
    
    * new extensions for icon groups
    
    * better mapping for cvs, layer

--- packages/file-type-icons/src/FileTypeIconMap.ts
+++ packages/file-type-icons/src/FileTypeIconMap.ts
@@ -290,6 +290,7 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
       'glb',
       'gltf',
       'holo',
+      'layer',
       'layout',
       'max',
       'mtl',
@@ -380,7 +381,7 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
   },
   sponews: {},
   spreadsheet: {
-    extensions: ['ods', 'gsheet', 'numbers']
+    extensions: ['odc', 'ods', 'gsheet', 'numbers']
   },
   rtf: {
     extensions: ['epub', 'gdoc', 'odt', 'rtf', 'wri', 'pages']
@@ -406,6 +407,7 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
       'kb',
       'ldt',
       'lrprev',
+      'pkpass',
       'ppa',
       'ppam',
       'pdb',
@@ -415,6 +417,7 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
       'vsl',
       'xla',
       'xlam',
+      'xlb',
       'xll'
     ]
   },
@@ -426,12 +429,14 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
   vector: {
     extensions: [
       'ai',
+      'cvs',
       'dgn',
       'gdraw',
       'pd',
       'emf',
       'eps',
       'fig',
+      'ind',
       'indd',
       'indt',
       'indb',
@@ -485,16 +490,16 @@ export const FileTypeIconMap: { [key: string]: { extensions?: string[] } } = {
   },
   video360: {},
   vsdx: {
-    extensions: ['vsd', 'vsdm', 'vsdx', 'vdw']
+    extensions: ['vdx', 'vsd', 'vsdm', 'vsdx', 'vsw', 'vdw']
   },
   vssx: {
     extensions: ['vss', 'vssm', 'vssx']
   },
   vstx: {
-    extensions: ['vst', 'vstm', 'vstx']
+    extensions: ['vst', 'vstm', 'vstx', 'vsx']
   },
   xlsx: {
-    extensions: ['xls', 'xlsb', 'xlsm', 'xlsx']
+    extensions: ['xlc', 'xls', 'xlsb', 'xlsm', 'xlsx']
   },
   xltx: {
     extensions: ['xlt', 'xltm', 'xltx']
