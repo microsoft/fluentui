4ea1db345fb3b925006ce28b8672152fb20f1f99
GitHubMaxwell <maxwell.rediker@gmail.com>
2019-06-07 14:40:26 -0700

    SearchBox fluent update

--- packages/fluent-theme/src/fluent/styles/SearchBox.styles.ts
+++ packages/fluent-theme/src/fluent/styles/SearchBox.styles.ts
@@ -1,14 +1,44 @@
 import { ISearchBoxStyleProps, ISearchBoxStyles } from 'office-ui-fabric-react/lib/SearchBox';
 
 export const SearchBoxStyles = (props: ISearchBoxStyleProps): Partial<ISearchBoxStyles> => {
-  const { theme } = props;
+  const { theme, underlined } = props;
   const { effects, palette } = theme;
 
   return {
     root: [
       {
         borderRadius: effects.roundedCorner2,
-        borderColor: palette.neutralSecondaryAlt
+        borderColor: palette.neutralTertiary
+      },
+      underlined && [
+        'is-underlined',
+        {
+          borderRadius: '0px'
+        }
+      ]
+    ],
+    iconContainer: [
+      'ms-SearchBox-iconContainer',
+      {
+        color: palette.themeDarkAlt
+      }
+    ],
+    clearButton: [
+      'ms-SearchBox-clearButton',
+      {
+        selectors: {
+          '.ms-Button:hover': {
+            backgroundColor: 'transparent'
+          },
+          '.ms-Button-icon': {
+            color: palette.neutralSecondary,
+            selectors: {
+              ':hover': {
+                color: palette.black
+              }
+            }
+          }
+        }
       }
     ]
   };
