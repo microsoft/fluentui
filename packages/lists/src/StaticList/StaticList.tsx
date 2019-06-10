a3574a8d72cfe76d6c2a43a0b53b805e3f9e51f1
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-10 11:22:28 -0700

    Add StaticList with static content

---
+++ packages/lists/src/StaticList/StaticList.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface IList<T> {
+  items: T[];
+}
+
+class StaticList<T> extends React.PureComponent<IList<T>> {
+  public render = () => {
+    const { items = [] } = this.props;
+
+    return (
+      <ul>
+        {items.map((_value: T, index: number) => {
+          return <li key={index}>{`Item #${index}`}</li>;
+        })}
+      </ul>
+    );
+  };
+}
+
+export { StaticList };
