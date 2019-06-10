9a05f105a586a36cb9d882914a76cf030dffd68e
Makoto Morimoto <Humberto.Morimoto@microsoft.com>
2019-06-10 16:35:13 -0700

    Card: Extending HTMLElement attributes in ICardProps (#9402)
    * Card: Extending HTMLElement attributes in ICardProps.
    
    * Adding change file.

--- packages/react-cards/src/components/Card/Card.types.ts
+++ packages/react-cards/src/components/Card/Card.types.ts
@@ -19,7 +19,11 @@ export interface ICardSlots {
   root?: IStackSlot;
 }
 
-export interface ICardProps extends ICardSlots, IStyleableComponentProps<ICardProps, ICardTokens, ICardStyles>, IBaseProps<ICard> {
+export interface ICardProps
+  extends ICardSlots,
+    IStyleableComponentProps<ICardProps, ICardTokens, ICardStyles>,
+    IBaseProps<ICard>,
+    React.AllHTMLAttributes<HTMLElement> {
   /**
    * Defines whether to render a regular or a compact Card.
    * @defaultvalue false
