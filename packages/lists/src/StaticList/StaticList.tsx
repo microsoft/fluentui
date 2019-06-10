c97fac25681f211d8a9238dcc7cfc05f4065dc03
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-10 16:07:09 -0700

    Provide StaticList default row renderer

--- packages/lists/src/StaticList/StaticList.tsx
+++ packages/lists/src/StaticList/StaticList.tsx
@@ -6,9 +6,13 @@ interface IStaticListProps<T> {
   children: (item: T, index: number) => React.ReactNode;
 }
 
+function defaultRowRenderer<T>(item: T, index: number): JSX.Element {
+  return <li key={index}>{item}</li>;
+}
+
 class StaticList<T> extends React.PureComponent<IStaticListProps<T>> {
   public render = () => {
-    const { items = [], as: RootTag = 'ul', children } = this.props;
+    const { items = [], as: RootTag = 'ul', children = defaultRowRenderer } = this.props;
 
     return <RootTag>{items.map((item: T, index: number) => children(item, index))}</RootTag>;
   };
