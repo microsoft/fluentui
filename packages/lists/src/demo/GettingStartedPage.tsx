6529c6455256f1f7a050c32353e949052f4c55c4
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-13 11:04:13 -0700

    Remove StaticList examples from root page

--- packages/lists/src/demo/GettingStartedPage.tsx
+++ packages/lists/src/demo/GettingStartedPage.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { Markdown } from '@uifabric/example-app-base';
 import { mergeStyles } from 'office-ui-fabric-react/lib/Styling';
-import {
-  StaticListExample,
-  StaticOrderedListExample,
-  StaticListTableExample
-  // StaticListAppendItemsExample
-} from '../StaticList/StaticList.Example';
 
 const rootClass = mergeStyles({
   marginTop: -20,
@@ -17,10 +11,6 @@ export const GettingStartedPage: React.FunctionComponent = () => {
   return (
     <div className={rootClass}>
       <Markdown>{require<string>('!raw-loader!../../README.md')}</Markdown>
-      <StaticListExample />
-      <StaticOrderedListExample />
-      <StaticListTableExample />
-      {/* <StaticListAppendItemsExample /> */}
     </div>
   );
 };
