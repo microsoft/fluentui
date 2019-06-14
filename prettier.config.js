737231c7566b9cc688f2993135f2d6408a96116d
Elizabeth Craig <elcraig@microsoft.com>
2019-06-14 15:24:04 -0700

    Don't run prettier in v5

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// Most files in the 5.0 branch aren't prettier-formatted, and starting to introduce prettier
+// formatting intermittently (due to format-on-save in VS Code with the prettier extension) would
+// be unnecessary churn. The following setting prevents this unintentional reformatting by telling
+// prettier to only run on files starting with a comment /** @prettier */.
+module.exports = {
+  requirePragma: true
+};
