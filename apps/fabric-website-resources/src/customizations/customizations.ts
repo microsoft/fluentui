bde78df84337fe33f2b75855a7ee6f176e573bc1
VitalieBraga <vibraga@microsoft.com>
2019-06-18 23:42:03 -0700

    Pipe mdl2-theme to fabric-website-resources

--- apps/fabric-website-resources/src/customizations/customizations.ts
+++ apps/fabric-website-resources/src/customizations/customizations.ts
@@ -1,5 +1,6 @@
 import { IExampleCardCustomizations, IAppCustomizations } from '@uifabric/example-app-base';
 import { FluentCustomizations } from '@uifabric/fluent-theme';
+import { Mdl2Customizations } from '@uifabric/mdl2-theme';
 import { AzureCustomizationsLight, AzureCustomizationsDark } from '@uifabric/azure-themes';
 import { DefaultCustomizations, TeamsCustomizations, WordCustomizations } from '@uifabric/theme-samples';
 
@@ -9,7 +10,8 @@ const exampleCardCustomizations: IExampleCardCustomizations[] = [
   { title: 'Word', customizations: WordCustomizations },
   { title: 'Teams', customizations: TeamsCustomizations },
   { title: 'Azure', customizations: AzureCustomizationsLight },
-  { title: 'Azure Dark', customizations: AzureCustomizationsDark }
+  { title: 'Azure Dark', customizations: AzureCustomizationsDark },
+  { title: 'Mdl2', customizations: Mdl2Customizations }
 ];
 
 export const AppCustomizations: IAppCustomizations = {
--- packages/mdl2-theme/src/mdl2/index.ts
+++ packages/mdl2-theme/src/mdl2/index.ts
@@ -1,6 +1,5 @@
 export * from './Mdl2Colors';
 export * from './Mdl2Depths';
-export * from './Mdl2Motion';
 export * from './Mdl2Styles';
 export * from './Mdl2Theme';
 export * from './Mdl2Type';
