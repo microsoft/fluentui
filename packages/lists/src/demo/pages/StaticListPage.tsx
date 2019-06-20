d7f18fefb7a657cb2043a4e5bbd5f768e1d76d1c
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-20 15:24:57 -0700

    Add dos and donts to StaticList

--- packages/lists/src/demo/pages/StaticListPage.tsx
+++ packages/lists/src/demo/pages/StaticListPage.tsx
@@ -21,5 +21,22 @@ export const StaticListPage = () => (
       </>
     }
     overview={<Markdown>{require<string>('!raw-loader!./docs/StaticListOverview.md')}</Markdown>}
+    bestPractices={<div />}
+    dos={
+      <div>
+        <ul>
+          <li>Render a reasonable number of rows that do not degrade user-experience.</li>
+          <li>Render content that is paginated either by prepending or appending rows.</li>
+        </ul>
+      </div>
+    }
+    donts={
+      <div>
+        <ul>
+          <li>Render large data sets with complex row DOM as it will likely result in a degraded user-experience.</li>
+          <li>Rely on default index as key behavior if sorting or filtering row content.</li>
+        </ul>
+      </div>
+    }
   />
 );
