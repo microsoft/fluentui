3c25d938266da825982c690fdaef87b645e49247
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-06-20 10:16:25 -0700

    Wrap StaticList examples in Profiler components

--- packages/lists/src/StaticList/StaticList.Example.tsx
+++ packages/lists/src/StaticList/StaticList.Example.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { StaticList } from './StaticList';
 
+/* tslint:disable-next-line:no-any */
+const Profiler = (React as any).unstable_Profiler;
+
 const ITEMS = new Array(25).fill(0);
 let APPEND_EXAMPLE_ITEMS = [...ITEMS];
 
@@ -8,15 +11,21 @@ function appendItems(): void {
   APPEND_EXAMPLE_ITEMS = [...APPEND_EXAMPLE_ITEMS, ...new Array(10).fill(0)];
 }
 
+function logProfilerRender(id: string, phase: string, actualTime: number, baseTime: number, startTime: number, commitTime: number): void {
+  console.table({ id, phase, actualTime, baseTime, startTime, commitTime });
+}
+
 const StaticListExample = () => {
   return (
     <>
       <h1>Unordered-list (default)</h1>
-      <StaticList items={ITEMS}>
-        {(_item: number, index: number) => {
-          return <li key={index}>{`Item #${index}`}</li>;
-        }}
-      </StaticList>
+      <Profiler id={'StaticList-Basic'} onRender={logProfilerRender}>
+        <StaticList items={ITEMS}>
+          {(_item: number, index: number) => {
+            return <li key={index}>{`Item #${index}`}</li>;
+          }}
+        </StaticList>
+      </Profiler>
     </>
   );
 };
@@ -25,9 +34,11 @@ const StaticOrderedListExample = () => {
   return (
     <>
       <h1>Ordered-list</h1>
-      <StaticList as={'ol'} items={ITEMS}>
-        {(_item: number, index: number) => <li key={index}>{`Item #${index}`}</li>}
-      </StaticList>
+      <Profiler id={'StaticList-Ordered'} onRender={LOG_PROFILER_RENDER}>
+        <StaticList as={'ol'} items={ITEMS}>
+          {(_item: number, index: number) => <li key={index}>{`Item #${index}`}</li>}
+        </StaticList>
+      </Profiler>
     </>
   );
 };
@@ -36,28 +47,30 @@ const StaticListTableExample = () => {
   return (
     <>
       <h1>Table</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>#</th>
-          </tr>
-        </thead>
-        <StaticList as="tbody" items={ITEMS}>
-          {(_item: number, index: number) => (
-            <tr key={index}>
-              <td>{'Item'}</td>
-              <td>{index}</td>
+      <Profiler id={'StaticList-Table'} onRender={LOG_PROFILER_RENDER}>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>#</th>
             </tr>
-          )}
-        </StaticList>
-        <tfoot>
-          <tr>
-            <td>{'Total count'}</td>
-            <td>{ITEMS.length}</td>
-          </tr>
-        </tfoot>
-      </table>
+          </thead>
+          <StaticList as="tbody" items={ITEMS}>
+            {(_item: number, index: number) => (
+              <tr key={index}>
+                <td>{'Item'}</td>
+                <td>{index}</td>
+              </tr>
+            )}
+          </StaticList>
+          <tfoot>
+            <tr>
+              <td>{'Total count'}</td>
+              <td>{ITEMS.length}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </Profiler>
     </>
   );
 };
