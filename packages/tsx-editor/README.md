d0b2f7aba3444c720ea99163a519cff566766ff9
Nishi Kaza <nishikaza@berkeley.edu>
2019-06-19 17:15:26 -0700

    initial commit of editor

---
+++ packages/tsx-editor/README.md
@@ -0,0 +1,5 @@
+# @uifabric/tsx-editor
+
+Live TSX editor for website
+
+These are not production-ready components and **should never be used in product**. This space is useful for testing new components whose APIs might change before final release.
---
+++ packages/tsx-editor/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const resources = require('../../scripts/webpack/webpack-resources');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const BUNDLE_NAME = 'tsx-editor';
+const IS_PRODUCTION = process.argv.indexOf('--production') > -1;
+
+module.exports = resources.createConfig(BUNDLE_NAME, IS_PRODUCTION, {
+  entry: {
+    [BUNDLE_NAME]: './lib/index.js'
+  },
+
+  output: {
+    libraryTarget: 'var',
+    library: 'FabricTsxEditor'
+  },
+
+  externals: [{ react: 'React' }, { 'react-dom': 'ReactDOM' }],
+
+  plugins: [
+    new MonacoWebpackPlugin({
+      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      languages: ['typescript']
+    }),
+    new BundleAnalyzerPlugin()
+  ],
+
+  resolve: {
+    alias: {
+      '@uifabric/tsx-editor/src': path.join(__dirname, 'src'),
+      '@uifabric/tsx-editor/lib': path.join(__dirname, 'lib'),
+      '@uifabric/tsx-editor': path.join(__dirname, 'lib')
+    }
+  }
+});
