7b01337c877f8a7e28c4bf301eca59748be0ab93
Sebastian Ottl <sebastian.oettl@microsoft.com>
2019-06-23 21:22:43 -0700

    Update interfaces of Viewport, make scroll container work

--- packages/lists/src/components/Viewport/Viewport.tsx
+++ packages/lists/src/components/Viewport/Viewport.tsx
@@ -7,29 +7,59 @@ export interface IViewportProps {
   children: (viewportState: IViewportState) => JSX.Element[] | JSX.Element;
 }
 
-export interface IViewportState {
-  isScrolling: boolean;
-  scrollOffset: number;
+export enum ScrollDirection {
+  none,
+  backward,
+  forward
 }
 
-const DONE_SCROLLING_TIMEOUT_IN_MILLISECONDS = 500;
-const NO_PENDING_DONE_SCROLLING_TIMEOUT = -1;
+export enum ScrollType {
+  none,
+  user,
+  programmatic
+}
+
+export interface IViewportState {
+  isScrolling: boolean;
+  scrollDistance: number;
+  scrollDirection: ScrollDirection;
+  scrollType: ScrollType;
+}
+
+const SCROLLING_TIMEOUT_IN_MILLISECONDS = 500;
+const NO_PENDING_SCROLLING_TIMEOUT = -1;
 
 export const Viewport = (props: IViewportProps) => {
   const { height, width, children } = props;
 
   const elRef = useRef<HTMLDivElement>(null);
-  const [elStyle] = useState({
-    height,
-    width,
-    willChange: 'scroll-position', // paints the scroll container on its own layer
-    overflow: 'auto'
+  const doneScrollingTimeoutId = useRef(-1);
+
+  const [viewportState, setViewportState] = useState<IViewportState>({
+    isScrolling: false,
+    scrollDistance: 0,
+    scrollDirection: ScrollDirection.none,
+    scrollType: ScrollType.none
   });
 
-  const [viewportState, setViewportState] = useState({
-    isScrolling: false,
-    scrollOffset: 0
-  });
+  const scheduleDoneScrolling = () => {
+    if (doneScrollingTimeoutId.current !== NO_PENDING_SCROLLING_TIMEOUT) {
+      window.clearTimeout(doneScrollingTimeoutId.current);
+    }
+
+    doneScrollingTimeoutId.current = window.setTimeout(() => {
+      setViewportState((prevViewportState: IViewportState) => {
+        return {
+          isScrolling: false,
+          scrollDistance: prevViewportState.scrollDistance,
+          scrollDirection: ScrollDirection.none,
+          scrollType: ScrollType.none
+        };
+      });
+
+      doneScrollingTimeoutId.current = NO_PENDING_SCROLLING_TIMEOUT;
+    }, SCROLLING_TIMEOUT_IN_MILLISECONDS);
+  };
 
   useEffect(() => {
     if (elRef.current) {
@@ -38,7 +68,9 @@ export const Viewport = (props: IViewportProps) => {
 
         setViewportState({
           isScrolling: true,
-          scrollOffset: scrollTop
+          scrollDistance: scrollTop,
+          scrollDirection: ScrollDirection.none,
+          scrollType: ScrollType.none
         });
 
         scheduleDoneScrolling();
@@ -54,26 +86,9 @@ export const Viewport = (props: IViewportProps) => {
     }
   }, []);
 
-  const doneScrollingTimeoutId = useRef(-1);
-  const scheduleDoneScrolling = () => {
-    if (doneScrollingTimeoutId.current !== NO_PENDING_DONE_SCROLLING_TIMEOUT) {
-      window.clearTimeout(doneScrollingTimeoutId.current);
-    }
-
-    doneScrollingTimeoutId.current = window.setTimeout(() => {
-      setViewportState((prevViewportState: IViewportState) => {
-        return {
-          isScrolling: false,
-          scrollOffset: prevViewportState.scrollOffset
-        };
-      });
-
-      doneScrollingTimeoutId.current = NO_PENDING_DONE_SCROLLING_TIMEOUT;
-    }, DONE_SCROLLING_TIMEOUT_IN_MILLISECONDS);
-  };
   useEffect(() => {
     return () => {
-      if (doneScrollingTimeoutId.current !== NO_PENDING_DONE_SCROLLING_TIMEOUT) {
+      if (doneScrollingTimeoutId.current !== NO_PENDING_SCROLLING_TIMEOUT) {
         window.clearTimeout(doneScrollingTimeoutId.current);
       }
     };
@@ -82,10 +97,28 @@ export const Viewport = (props: IViewportProps) => {
   return (
     <div
       ref={elRef}
-      data-is-scrollable={true} // some Fabric components need this to detect their parent scroll container more efficiently
-      style={elStyle} // tslint:disable-line:jsx-ban-props
+      // tslint:disable-next-line:jsx-ban-props
+      style={{
+        position: 'relative',
+        height,
+        width
+      }}
     >
-      {children(viewportState)}
+      <div
+        data-is-scrollable={true} // some Fabric components need this to detect their parent scroll container more efficiently
+        // tslint:disable-next-line:jsx-ban-props
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          willChange: 'scroll-position', // paints the scroll container on its own layer
+          overflow: 'auto'
+        }}
+      >
+        {children(viewportState)}
+      </div>
     </div>
   );
 };
