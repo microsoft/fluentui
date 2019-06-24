22fdbebe32178490e5edeeb2a019acaa389be413
Elizabeth Craig <elcraig@microsoft.com>
2019-06-24 15:47:34 -0700

    Fabric 7 website updates (#9461)

--- apps/fabric-website-resources/src/customizations/customizations.ts
+++ apps/fabric-website-resources/src/customizations/customizations.ts
@@ -1,11 +1,10 @@
 import { IExampleCardCustomizations, IAppCustomizations } from '@uifabric/example-app-base';
 import { FluentCustomizations } from '@uifabric/fluent-theme';
 import { AzureCustomizationsLight, AzureCustomizationsDark } from '@uifabric/azure-themes';
-import { DefaultCustomizations, TeamsCustomizations, WordCustomizations } from '@uifabric/theme-samples';
+import { TeamsCustomizations, WordCustomizations } from '@uifabric/theme-samples';
 
 const exampleCardCustomizations: IExampleCardCustomizations[] = [
-  { title: 'Default', customizations: DefaultCustomizations },
-  { title: 'Fluent', customizations: FluentCustomizations },
+  { title: 'Default', customizations: FluentCustomizations },
   { title: 'Word', customizations: WordCustomizations },
   { title: 'Teams', customizations: TeamsCustomizations },
   { title: 'Azure', customizations: AzureCustomizationsLight },
