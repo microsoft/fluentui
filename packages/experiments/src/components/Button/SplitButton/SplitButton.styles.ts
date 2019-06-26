5ba5b30f72eef0b8d6c14938fa21d5a50926a147
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2019-06-26 17:50:15 -0500

    Button: Removing Stack usage to improve perf (#9516)
    * Button: Removing Stack usage from slots and view to improve perf.
    
    * Adding change files for packages that were changed because of prettier.
    
    * Deleting perf log files.
    
    * Undoing prettier changes.
    
    * Removing unused change files.
    
    * Removing BaseButton changes from Actionable PR.

--- packages/experiments/src/components/Button/SplitButton/SplitButton.styles.ts
+++ packages/experiments/src/components/Button/SplitButton/SplitButton.styles.ts
@@ -74,6 +74,8 @@ export const SplitButtonStyles: ISplitButtonComponent['styles'] = (props, theme,
         borderRadius: tokens.borderRadius,
         boxSizing: 'border-box',
         display: 'inline-flex',
+        flexDirection: 'row',
+        justifyContent: 'stretch',
         zIndex: 1,
 
         selectors: {
@@ -146,6 +148,8 @@ export const SplitButtonStyles: ISplitButtonComponent['styles'] = (props, theme,
       borderRightWidth: 0,
       borderTopWidth: props.primaryActionDisabled ? 0 : tokens.borderWidth,
       boxSizing: 'border-box',
+      display: 'inline-flex',
+      flexDirection: 'column',
       height: 'auto',
       width: 'auto',
 
