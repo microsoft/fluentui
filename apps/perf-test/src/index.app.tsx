21b071595f2ebfa4531895ded6ce4c85f6e528aa
Jason Gore <jagore@microsoft.com>
2019-06-26 15:50:41 -0700

    Perf test cleanup, argument support, and new scenarios. (#9590)

--- apps/perf-test/src/index.app.tsx
+++ apps/perf-test/src/index.app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from './app/App';
 
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 
