aced6459de2a015ae646bf3fb6b08e357a38fb52
Aftab Hassan <afhassan@microsoft.com>
2019-06-26 12:50:15 -0700

    modify raiseClick to work for IE as well

--- packages/utilities/src/dom/raiseClick.ts
+++ packages/utilities/src/dom/raiseClick.ts
@@ -1,7 +1,19 @@
 /** Raises a click event. */
 export function raiseClick(target: Element): void {
-  const event = new Event('MouseEvents');
-
+  const event = createNewEvent('MouseEvents');
   event.initEvent('click', true, true);
   target.dispatchEvent(event);
+}
+
+function createNewEvent(eventName: string): Event {
+  let event;
+  // Chrome, Opera, Firefox
+  if (typeof Event === 'function') {
+    event = new Event(eventName);
+  } else {
+    // IE
+    event = document.createEvent('Event');
+    event.initEvent(eventName, true, true);
+  }
+  return event;
 }
