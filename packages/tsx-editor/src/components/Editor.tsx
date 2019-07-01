84a7497d70175ee332577561d3baf0942f0a1488
Nishi Kaza <nishikaza@berkeley.edu>
2019-07-01 16:53:48 -0700

    rewriting editor with hooks

--- packages/tsx-editor/src/components/Editor.tsx
+++ packages/tsx-editor/src/components/Editor.tsx
@@ -1,57 +1,26 @@
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import React from 'react';
+import * as React from 'react';
 import { IEditorProps } from './Editor.types';
 
-export class Editor extends React.Component<IEditorProps> {
-  private editor: monaco.editor.IStandaloneCodeEditor | undefined;
-  private editorRef = React.createRef<HTMLDivElement>();
+export const Editor = (props: IEditorProps) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const { width, height, onChange, code, language } = props;
+  const style = { width, height };
 
-  public componentDidMount() {
-    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-      allowNonTsExtensions: true,
-      target: monaco.languages.typescript.ScriptTarget.ES5,
-      jsx: monaco.languages.typescript.JsxEmit.React,
-      alwaysStrict: true,
-      jsxFactory: 'React.createElement',
-      experimentalDecorators: true,
-      preserveConstEnums: true,
-      outDir: 'lib',
-      module: monaco.languages.typescript.ModuleKind.CommonJS,
-      lib: ['es5', 'dom']
+  React.useEffect(() => {
+    const editor = monaco.editor.create(ref.current!, {
+      value: code,
+      language
     });
-    this._createEditor();
-  }
 
-  public componentWillUnmount() {
-    this._closeEditor();
-  }
+    editor.onDidChangeModelContent(() => {
+      onChange(editor.getValue());
+    });
 
-  public render() {
-    const { width, height } = this.props;
-
-    const style = {
-      width: width,
-      height: height
+    return () => {
+      editor.dispose();
     };
+  }, []);
 
-    return <div style={style} ref={this.editorRef} />;
-  }
-
-  private _createEditor() {
-    this.editor = monaco.editor.create(this.editorRef.current!, {
-      model: monaco.editor.createModel(this.props.code, 'typescript', monaco.Uri.parse('file:///main.tsx')),
-      value: this.props.code,
-      language: this.props.language
-    });
-
-    this.editor.onDidChangeModelContent(event => {
-      this.props.onChange(this.editor!.getValue());
-    });
-  }
-
-  private _closeEditor() {
-    if (this.editor) {
-      this.editor.dispose();
-    }
-  }
-}
+  return <div ref={ref} style={style} />;
+};
