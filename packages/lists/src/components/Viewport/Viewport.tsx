60cf63ffab28add812916d89660a1301cde8b7ce
Sebastian Ottl <sebastian.oettl@microsoft.com>
2019-07-02 15:36:36 -0700

    Remove React.memo from Viewport component

--- packages/lists/src/components/Viewport/Viewport.tsx
+++ packages/lists/src/components/Viewport/Viewport.tsx
@@ -29,82 +29,83 @@ function getScrollDirection(scrollDistance: number, prevScrollDistance: number):
 /**
  * Viewport represents a scrollable container that maintains information about its current scroll state.
  * The component takes a function component as its child component, using the current scroll state as its only argument.
+ *
+ * Note: The caller may wish to wrap this component with React.memo, in case they make sure that the 'children' function
+ * doesn't mutate on every render.
  */
-export const Viewport = React.memo(
-  (props: IViewportProps): JSX.Element => {
-    const { height, width, children } = props;
+export const Viewport = (props: IViewportProps): JSX.Element => {
+  const { height, width, children } = props;
 
-    const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
 
-    const [viewportState, setViewportState] = useState<IViewportState>({
-      isScrolling: false,
-      scrollDistance: SCROLL_DISTANCE_ORIGIN,
-      scrollDirection: NO_SCROLL_DIRECTION
+  const [viewportState, setViewportState] = useState<IViewportState>({
+    isScrolling: false,
+    scrollDistance: SCROLL_DISTANCE_ORIGIN,
+    scrollDirection: NO_SCROLL_DIRECTION
+  });
+  const prevViewportState = usePreviousValue(viewportState);
+
+  const [scheduleStoppedScrollingTimeout, clearStoppedScrollingTimeout] = useDebouncedTimeout(() => {
+    setViewportState((currentViewportState: IViewportState) => {
+      return {
+        isScrolling: false,
+        scrollDistance: currentViewportState.scrollDistance,
+        scrollDirection: NO_SCROLL_DIRECTION
+      };
     });
-    const prevViewportState = usePreviousValue(viewportState);
+  }, STOPPED_SCROLLING_TIMEOUT_IN_MILLISECONDS);
 
-    const [scheduleStoppedScrollingTimeout, clearStoppedScrollingTimeout] = useDebouncedTimeout(() => {
-      setViewportState((currentViewportState: IViewportState) => {
-        return {
-          isScrolling: false,
-          scrollDistance: currentViewportState.scrollDistance,
-          scrollDirection: NO_SCROLL_DIRECTION
-        };
-      });
-    }, STOPPED_SCROLLING_TIMEOUT_IN_MILLISECONDS);
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      const onScroll = (event: Event) => {
+        // tslint:disable-next-line:no-any
+        const { scrollLeft: scrollX, scrollTop: scrollY } = ((event as any) as React.UIEvent<HTMLDivElement>).currentTarget;
 
-    useEffect(() => {
-      if (scrollContainerRef.current) {
-        const onScroll = (event: Event) => {
-          // tslint:disable-next-line:no-any
-          const { scrollLeft: scrollX, scrollTop: scrollY } = ((event as any) as React.UIEvent<HTMLDivElement>).currentTarget;
+        const scrollDirectionX = getScrollDirection(
+          scrollX,
+          (prevViewportState && prevViewportState.scrollDistance[Axis.X]) || SCROLL_DISTANCE_ORIGIN[Axis.X]
+        );
+        const scrollDirectionY = getScrollDirection(
+          scrollY,
+          (prevViewportState && prevViewportState.scrollDistance[Axis.Y]) || SCROLL_DISTANCE_ORIGIN[Axis.Y]
+        );
 
-          const scrollDirectionX = getScrollDirection(
-            scrollX,
-            (prevViewportState && prevViewportState.scrollDistance[Axis.X]) || SCROLL_DISTANCE_ORIGIN[Axis.X]
-          );
-          const scrollDirectionY = getScrollDirection(
-            scrollY,
-            (prevViewportState && prevViewportState.scrollDistance[Axis.Y]) || SCROLL_DISTANCE_ORIGIN[Axis.Y]
-          );
+        setViewportState({
+          isScrolling: true,
+          scrollDistance: [scrollX, scrollY],
+          scrollDirection: [scrollDirectionX, scrollDirectionY]
+        });
 
-          setViewportState({
-            isScrolling: true,
-            scrollDistance: [scrollX, scrollY],
-            scrollDirection: [scrollDirectionX, scrollDirectionY]
-          });
+        scheduleStoppedScrollingTimeout();
+      };
 
-          scheduleStoppedScrollingTimeout();
-        };
+      scrollContainerRef.current.addEventListener<'scroll'>('scroll', onScroll);
 
-        scrollContainerRef.current.addEventListener<'scroll'>('scroll', onScroll);
+      return () => {
+        if (scrollContainerRef.current) {
+          scrollContainerRef.current.removeEventListener('scroll', onScroll);
+        }
 
-        return () => {
-          if (scrollContainerRef.current) {
-            scrollContainerRef.current.removeEventListener('scroll', onScroll);
-          }
+        clearStoppedScrollingTimeout();
+      };
+    }
+  }, []);
 
-          clearStoppedScrollingTimeout();
-        };
-      }
-    }, []);
+  const style: React.CSSProperties = {
+    position: 'relative',
+    height,
+    width,
+    willChange: 'scroll-position', // paints the scroll container on its own layer
+    overflow: 'auto'
+  };
 
-    const style: React.CSSProperties = {
-      position: 'relative',
-      height,
-      width,
-      willChange: 'scroll-position', // paints the scroll container on its own layer
-      overflow: 'auto'
-    };
-
-    return (
-      <div
-        ref={scrollContainerRef}
-        data-is-scrollable={true} // some Fabric components need this to detect their parent scroll container more efficiently
-        style={style} // tslint:disable-line:jsx-ban-props
-      >
-        {children(viewportState)}
-      </div>
-    );
-  }
-);
+  return (
+    <div
+      ref={scrollContainerRef}
+      data-is-scrollable={true} // some Fabric components need this to detect their parent scroll container more efficiently
+      style={style} // tslint:disable-line:jsx-ban-props
+    >
+      {children(viewportState)}
+    </div>
+  );
+};
