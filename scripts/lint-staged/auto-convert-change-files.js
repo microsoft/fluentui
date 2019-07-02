ed498fe06126ac43466ebac7bb0faaaa4dd56a48
Ken <kchau@microsoft.com>
2019-07-02 08:27:38 -0700

    use warn to display at the end

--- scripts/lint-staged/auto-convert-change-files.js
+++ scripts/lint-staged/auto-convert-change-files.js
@@ -8,14 +8,13 @@ const files = process.argv.slice(2);
 const legacyChangeFile = files.find(file => file.includes('common/changes'));
 
 if (legacyChangeFile && fs.existsSync(legacyChangeFile)) {
-  console.warn(chalk.yellow('Legacy change file detected, will auto convert these to the new format.'));
-
+  console.warn(chalk.red('Legacy change file detected, auto converted these to the new format.'));
   const convertChangeFiles = require('../convert-change-files');
   convertChangeFiles();
   spawnSync('git', ['add', 'change', 'common/changes'], { stdio: 'inherit' });
 
-  console.log(chalk.green('Changes are completed for you! Please run git commit again!'));
-  console.log(chalk.cyan('Please use "npm run change" to generate change files instead of "rush change"'));
+  console.warn(chalk.green('Changes are completed for you! Please run git commit again!'));
+  console.warn(chalk.cyan('Please use "npm run change" to generate change files instead of "rush change"'));
 
   process.exit(1);
 }
