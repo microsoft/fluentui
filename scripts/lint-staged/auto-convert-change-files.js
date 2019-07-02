13ff5382e55b581f23338181c718e9beead75904
Ken <kchau@microsoft.com>
2019-07-02 08:19:00 -0700

    verify that the files do exist as well

--- scripts/lint-staged/auto-convert-change-files.js
+++ scripts/lint-staged/auto-convert-change-files.js
@@ -1,13 +1,15 @@
 // @ts-check
+const { spawnSync } = require('child_process');
+const fs = require('fs');
 
 const files = process.argv.slice(2);
 
 const legacyChangeFile = files.find(file => file.includes('common/changes'));
 
-if (legacyChangeFile) {
+if (legacyChangeFile && fs.existsSync(legacyChangeFile)) {
   console.warn('Legacy change file detected, will auto convert these to the new format.');
   console.warn('Please use "npm run change" to generate change files instead of "rush change"');
+  const convertChangeFiles = require('../convert-change-files');
+  convertChangeFiles();
+  spawnSync('git', ['add', 'change', 'common/changes'], { stdio: 'inherit' });
 }
-
-const convertChangeFiles = require('../convert-change-files');
-convertChangeFiles();
