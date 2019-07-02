c75dd4cf7eab3d46d19fb5a37cb384736e80d4fd
Nishi Kaza <nishikaza@berkeley.edu>
2019-07-02 11:47:12 -0700

    Update packages/tsx-editor/src/components/Editor.tsx
    Co-Authored-By: Elizabeth Craig <ecraig12345@gmail.com>

--- packages/tsx-editor/src/components/Editor.tsx
+++ packages/tsx-editor/src/components/Editor.tsx
@@ -2,7 +2,7 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import * as React from 'react';
 import { IEditorProps } from './Editor.types';
 
-export const Editor = (props: IEditorProps) => {
+export const Editor: React.FunctionComponent<IEditorProps> = (props: IEditorProps) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const { width, height, onChange, code, language } = props;
   const style = { width, height };
