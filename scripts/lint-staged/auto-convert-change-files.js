c41e03ff078b5ea00f6219b921062b3532ffc1d7
Ken <kchau@microsoft.com>
2019-07-02 08:23:51 -0700

    refining the process of auto fix

--- scripts/lint-staged/auto-convert-change-files.js
+++ scripts/lint-staged/auto-convert-change-files.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { spawnSync } = require('child_process');
+const chalk = require('chalk').default;
 const fs = require('fs');
 
 const files = process.argv.slice(2);
@@ -7,9 +8,14 @@ const files = process.argv.slice(2);
 const legacyChangeFile = files.find(file => file.includes('common/changes'));
 
 if (legacyChangeFile && fs.existsSync(legacyChangeFile)) {
-  console.warn('Legacy change file detected, will auto convert these to the new format.');
-  console.warn('Please use "npm run change" to generate change files instead of "rush change"');
+  console.warn(chalk.yellow('Legacy change file detected, will auto convert these to the new format.'));
+
   const convertChangeFiles = require('../convert-change-files');
   convertChangeFiles();
   spawnSync('git', ['add', 'change', 'common/changes'], { stdio: 'inherit' });
+
+  console.log(chalk.green('Changes are completed for you! Please run git commit again!'));
+  console.log(chalk.cyan('Please use "npm run change" to generate change files instead of "rush change"'));
+
+  process.exit(1);
 }
