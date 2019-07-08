97653db607947919c9965683e618ca0d64da1a12
Ivan Dijan <ivdijan@microsoft.com>
2019-07-08 13:58:31 +0200

    Update PersonaInitialsColor.test.ts

--- packages/office-ui-fabric-react/src/components/Persona/PersonaInitialsColor.test.ts
+++ packages/office-ui-fabric-react/src/components/Persona/PersonaInitialsColor.test.ts
@@ -4,10 +4,10 @@ import { PersonaInitialsColor } from './Persona.types';
 describe('PersonaInitialsColor tests', () => {
   it('renders gets the correct colors if none was provided', () => {
     const colorCode = initialsColorPropToColorCode({ text: 'Kat Larrson' });
-    expect(colorCode).toEqual('#8764B8');
+    expect(colorCode).toEqual('#5E4B8B');
 
     const colorCode2 = initialsColorPropToColorCode({ text: 'Annie Lindqvist' });
-    expect(colorCode2).toEqual('#498205');
+    expect(colorCode2).toEqual('#00A300');
   });
 
   it('uses provided enum initialsColor if one was specified', () => {
