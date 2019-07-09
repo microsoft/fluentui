6d46d1d7b048e77dbea0c694136cfa0a7765ea61
Ken <kchau@microsoft.com>
2019-07-09 11:07:05 -0700

    WIP: fixing up node_modules references

--- scripts/debug-test.js
+++ scripts/debug-test.js
@@ -2,12 +2,13 @@ const path = require('path');
 const findConfig = require('./find-config');
 
 const configPath = findConfig('jest.config.js');
-const rushPath = findConfig('rush.json');
+const rootPath = findConfig('lerna.json');
 
-if (!configPath || !rushPath) {
+if (!configPath || !rootPath) {
   console.log(
-    'Unable to find jest.config.js relative to currently opened file. Run debug-test from an open source file in a jest enabled project.');
+    'Unable to find jest.config.js relative to currently opened file. Run debug-test from an open source file in a jest enabled project.'
+  );
 } else {
   process.chdir(path.dirname(configPath));
-  require(path.resolve(path.dirname(rushPath), 'scripts/node_modules/jest-cli/bin/jest'));
+  require(path.resolve(path.dirname(rootPath), 'node_modules/jest-cli/bin/jest'));
 }
