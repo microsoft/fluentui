417877eabb2335e93ddcc6ae5b1681b4426307a3
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-07-09 20:10:21 +0200

    use existing helper to get window context

--- packages/utilities/src/dom/getRootDocument.ts
+++ packages/utilities/src/dom/getRootDocument.ts
@@ -1,12 +1,14 @@
+import { getWindow } from './getWindow';
+
 /**
  * Helper to get the top-most document object.
  *
  * @public
  */
 export function getRootDocument(): Document | undefined {
-  let current = window;
-  while (current.parent !== current) {
-    current = current.parent;
+  let current = getWindow();
+  while (current!.parent !== current) {
+    current = current!.parent;
   }
 
   return current.document;
