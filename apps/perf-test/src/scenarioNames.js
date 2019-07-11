6a7b30e932892a16a7fc6bec71f0f3331cf6658d
Kevin Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2019-07-11 12:38:31 -0700

    Add DetailsHeader scenarios to perf-test

--- apps/perf-test/src/scenarioNames.js
+++ apps/perf-test/src/scenarioNames.js
@@ -2,6 +2,8 @@ const scenarioNames = {
   BaseButtonNew: 'BaseButton (experiments)',
   DefaultButtonNew: 'DefaultButton (experiments)',
   DetailsRowNoStyles: 'DetailsRow without styles',
+  DetailsHeaderFixed: 'DetailsHeader (fixed layout)',
+  DetailsHeaderJustified: 'DetailsHeader (justified layout)',
   DocumentCardTitle: 'DocumentCardTitle with truncation',
   MenuButtonNew: 'MenuButton (experiments)',
   PrimaryButtonNew: 'PrimaryButton (experiments)',
---
+++ apps/perf-test/src/scenarios/DetailsHeaderJustified.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { DetailsHeader, DetailsListLayoutMode, Selection } from 'office-ui-fabric-react';
+
+// tslint:disable-next-line:typedef
+const Items = Array.from({ length: 10 }, (n, i) => ({
+  key: `Item ${i}`,
+  name: `Item ${i}`,
+  modified: new Date().toString(),
+  shared: 'Private',
+  size: `${Math.round(Math.random() * 1000) / 10}KB`
+}));
+
+const Columns = [
+  { key: 'column1', name: 'Name', fieldName: 'name', minWidth: 100, maxWidth: 200, isResizable: true },
+  { key: 'column2', name: 'Modified', fieldName: 'modified', minWidth: 100, maxWidth: 200, isResizable: true },
+  { key: 'column2', name: 'Shared', fieldName: 'shared', minWidth: 100, maxWidth: 200, isResizable: true },
+  { key: 'column2', name: 'Size', fieldName: 'size', minWidth: 100, maxWidth: 200, isResizable: true }
+];
+
+const selection = new Selection();
+selection.setItems(Items);
+
+const scenario = <DetailsHeader selection={selection} columns={Columns} layoutMode={DetailsListLayoutMode.justified} />;
+
+export default scenario;
