bac5956cdaf0a7e9e32b23684d91da61129b3ebe
Ken <kchau@microsoft.com>
2019-07-12 10:43:25 -0700

    adds the ts-jest config that informs ts-jest where the root of the package is

--- packages/codepen-loader/jest.config.js
+++ packages/codepen-loader/jest.config.js
@@ -3,5 +3,12 @@ const path = require('path');
 module.exports = {
   preset: 'ts-jest',
   testRegex: '/__tests__/.*\\.test\\.ts$',
-  reporters: [path.resolve(__dirname, '../../scripts/jest/jest-reporter.js')]
+  reporters: [path.resolve(__dirname, '../../scripts/jest/jest-reporter.js')],
+  globals: {
+    'ts-jest': {
+      tsConfig: path.resolve(process.cwd(), 'tsconfig.json'),
+      packageJson: path.resolve(process.cwd(), 'package.json'),
+      diagnostics: false
+    }
+  }
 };
