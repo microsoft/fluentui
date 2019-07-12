edd898d91b1b77f1249535753a52a1823752ae84
Ken <kchau@microsoft.com>
2019-07-12 09:14:36 -0700

    had to resort ot using the custom reporter because it needs to silence the ts-jest not finding tsconfig issue

--- packages/codepen-loader/jest.config.js
+++ packages/codepen-loader/jest.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
   preset: 'ts-jest',
-  testRegex: '/__tests__/.*\\.test\\.ts$'
+  testRegex: '/__tests__/.*\\.test\\.ts$',
+  reporters: [path.resolve(__dirname, '../../scripts/jest/jest-reporter.js')]
 };
