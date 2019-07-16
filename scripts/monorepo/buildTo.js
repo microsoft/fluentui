aab66a49fb629a5b71e4edd6d37f482c9e7248e1
Ken <kchau@microsoft.com>
2019-07-16 15:42:40 -0700

    make the build to script stream (build in parallel)

--- scripts/monorepo/buildTo.js
+++ scripts/monorepo/buildTo.js
@@ -4,4 +4,6 @@ const lernaBin = require.resolve('lerna/cli.js');
 const argv = process.argv.slice(2);
 const [project, ...rest] = argv;
 
-spawnSync(lernaBin, ['run', 'build', '--scope', project, '--include-filtered-dependencies', '--', ...rest], { stdio: 'inherit' });
+spawnSync(lernaBin, ['run', 'build', '--scope', project, '--include-filtered-dependencies', '--stream', '--', ...rest], {
+  stdio: 'inherit'
+});
