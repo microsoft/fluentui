5e089eeef2368947e557f1d4e6c9e11c77dfd4b8
Ken <kchau@microsoft.com>
2019-07-16 20:02:54 -0700

    adding support for substring searches

--- scripts/monorepo/buildTo.js
+++ scripts/monorepo/buildTo.js
@@ -1,9 +1,42 @@
 const { spawnSync } = require('child_process');
 const lernaBin = require.resolve('lerna/cli.js');
+const getAllPackageInfo = require('./getAllPackageInfo');
 
 const argv = process.argv.slice(2);
+
+if (argv.length < 1) {
+  console.log(`Usage:
+
+  yarn buildto [packagename] [--min]
+
+This command builds all packages up to and including "packagename". The package name can actually be a substring.
+If multiple packages matched the pattern, they will all be built (along with their dependencies).`);
+
+  process.exit(0);
+}
+
 const [project, ...rest] = argv;
 
-spawnSync(lernaBin, ['run', 'build', '--scope', project, '--include-filtered-dependencies', '--stream', '--', ...rest], {
+const allPackages = getAllPackageInfo();
+let foundProjects = [project];
+
+if (!allPackages[project]) {
+  foundProjects = Object.keys(allPackages).filter(name => name.includes(project));
+  if (foundProjects.length === 0) {
+    console.log(`There is not project matching: ${project} in this repo`);
+  } else if (foundProjects.length > 1) {
+    console.log(`More than one project matched: "${foundProjects.join('", "')}"`);
+  } else {
+    console.log(`Found a match project named "${foundProjects[0]}"`);
+  }
+}
+
+const scopes = [];
+foundProjects.forEach(projectName => {
+  scopes.push('--scope');
+  scopes.push(projectName);
+});
+
+spawnSync(lernaBin, ['run', 'build', ...scopes, '--include-filtered-dependencies', '--stream', '--', ...rest], {
   stdio: 'inherit'
 });
