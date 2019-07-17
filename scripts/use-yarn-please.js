46997f6d3a1ab82875fbdf42715fef3d6d51abc8
Ken <kchau@microsoft.com>
2019-07-16 22:35:33 -0700

    addressing feedback

--- scripts/use-yarn-please.js
+++ scripts/use-yarn-please.js
@@ -3,35 +3,34 @@ const { spawnSync } = require('child_process');
 const npmPath = process.env.npm_execpath;
 
 const Strings = {
-  useYarnInstead: `Looks like you are trying to run "npm install", this repository has migrated to use Yarn as its package manager for installations.
+  useYarnInstead: `Looks like you are trying to run "npm install". This repository has migrated to use Yarn as its package manager.
   `,
   yarnDetected: `Great! Looks like you already have yarn installed!
   `,
-  installYarn: `You current do not have an installation of Yarn in your PATH. Be sure to install the latest stable version of Yarn here:
+  installYarn: `You currently do not have an installation of Yarn in your PATH. Please install the latest stable version of Yarn using one of the following links:
 
-Download an installer here: https://yarnpkg.com/en/docs/install
+  https://yarnpkg.com/en/docs/install
 
-Windows users can run the installer here:
-
-  https://yarnpkg.com/latest.msi
+Windows users can run the installer here: https://yarnpkg.com/latest.msi
 
 Mac users can run a one-liner for the install:
 
   curl -o- -L https://yarnpkg.com/install.sh | bash
+
 `,
   gettingStartedWithYarn: `To install UI Fabric monorepo dependencies and establish links between projects, simply run:
 
   yarn
 
-You can then build the packages
+You can then build the packages:
 
   yarn build
 
-Or start developing innerloop against the UI Fabric demo with this command
+Or start a development inner loop against the UI Fabric demo:
 
   yarn start
 
-To read more about all the commands that this mono-repo supports, go to this link:
+To learn more about all the commands that this monorepo supports, see the wiki:
 
   https://github.com/OfficeDev/office-ui-fabric-react/wiki/Build-Commands
 
