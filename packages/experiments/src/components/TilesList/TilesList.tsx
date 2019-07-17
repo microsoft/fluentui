656f5927f642ba7ab44302c4e2d93ddc37eb79e7
Kevin Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2019-07-17 11:22:02 -0700

    Initialize focus rects in top-level components where BaseComponent inheritance has been removed

--- packages/experiments/src/components/TilesList/TilesList.tsx
+++ packages/experiments/src/components/TilesList/TilesList.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ITilesListProps, ITilesGridItem, ITilesGridSegment, TilesGridMode, ITileSize } from './TilesList.types';
 import { List, IPageProps, ScrollToMode } from 'office-ui-fabric-react/lib/List';
 import { FocusZone, FocusZoneDirection } from 'office-ui-fabric-react/lib/FocusZone';
-import { css, IRenderFunction, IRectangle } from 'office-ui-fabric-react/lib/Utilities';
+import { css, IRenderFunction, IRectangle, initializeFocusRects } from 'office-ui-fabric-react/lib/Utilities';
 import * as TilesListStylesModule from './TilesList.scss';
 import { Shimmer } from 'office-ui-fabric-react/lib/Shimmer';
 
@@ -90,6 +90,8 @@ export class TilesList<TItem> extends React.Component<ITilesListProps<TItem>, IT
     this.state = {
       cells: this._getCells(props.items)
     };
+
+    initializeFocusRects();
   }
 
   public componentWillReceiveProps(nextProps: ITilesListProps<TItem>): void {
