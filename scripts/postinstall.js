8925dc0b35122e72974aa807ff219b1bd9049bbf
Ken <kchau@microsoft.com>
2019-07-17 22:12:36 -0700

    get rid of a git hook that doesn't work

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,9 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 
+// git v2.9.0 supports a custom hooks directory. This means we just need to checkin the hooks scripts
+spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
+
 if (!process.env.TF_BUILD && !process.env.UIFABRIC_NO_POSTINSTALL) {
   spawnSync(process.execPath, [path.join(__dirname, 'monorepo/buildTo.js'), '@uifabric/fabric-website-resources', '--min'], {
     stdio: 'inherit'
