aa9969f339cfa050c83802dd245ef12d3f3b5faa
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-07-23 16:46:08 -0700

    reverting change in path to buildEntries in webpack config

--- apps/test-bundles/webpack.config.js
+++ apps/test-bundles/webpack.config.js
@@ -5,7 +5,7 @@ const resources = require('../../scripts/webpack/webpack-resources');
 // Files which should not be considered top-level entries.
 const TopLevelEntryFileExclusions = ['index.js', 'version.js', 'index.bundle.js'];
 
-const Entries = _buildEntries('office-ui-fabric-react/lib/Coachmark');
+const Entries = _buildEntries('office-ui-fabric-react');
 
 let experimentalButtonPath = path.join(path.dirname(require.resolve('@uifabric/experiments')).replace('lib-commonjs', 'lib'), 'Button.js');
 Entries['experiments-Button'] = experimentalButtonPath;
