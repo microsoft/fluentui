f188640a51e9195a6c79940735d3c7f05a4b9739
Harshada Trivedi <hatrived@microsoft.com>
2019-07-23 15:45:04 +0530

    updating screener

--- apps/vr-tests/src/stories/ScrollablePane.Sticky.DetailsList.stories.tsx
+++ apps/vr-tests/src/stories/ScrollablePane.Sticky.DetailsList.stories.tsx
@@ -19,7 +19,8 @@ import { TooltipHost, ITooltipHostProps } from 'office-ui-fabric-react/lib/Toolt
 import {
   ScrollablePane,
   ScrollbarVisibility,
-  StickyContainerBehaviorType
+  StickyContainerBehaviorType,
+  IScrollablePaneProps
 } from 'office-ui-fabric-react/lib/ScrollablePane';
 import { Sticky, StickyPositionType } from 'office-ui-fabric-react/lib/Sticky';
 import { MarqueeSelection } from 'office-ui-fabric-react/lib/MarqueeSelection';
@@ -48,7 +49,10 @@ const _footerItem: IScrollablePaneDetailsListExampleItem = {
   key: 'footer',
   test1: 'Footer 1',
   test2: 'Footer 2',
-  test3: 'Footer 3'
+  test3: 'Footer 3',
+  test4: 'Footer 4',
+  test5: 'Footer 5',
+  test6: 'Footer 6'
 };
 
 export interface IScrollablePaneDetailsListExampleItem {
@@ -56,44 +60,55 @@ export interface IScrollablePaneDetailsListExampleItem {
   test1: string;
   test2: string;
   test3: string;
+  test4: string;
+  test5: string;
+  test6: string;
 }
 
 export interface IScrollablePaneDetailsListExampleState {
   items: IScrollablePaneDetailsListExampleItem[];
 }
-
+export interface IScrollablePaneDetailsListExampleProps {
+  scrollablePaneProps: IScrollablePaneProps;
+  itemCount: number;
+  numberOfColumns: number;
+}
 export class ScrollablePaneDetailsListExample extends React.Component<
-  {},
+  IScrollablePaneDetailsListExampleProps,
   IScrollablePaneDetailsListExampleState
 > {
   private _selection: Selection;
   private _allItems: IScrollablePaneDetailsListExampleItem[];
   private _columns: IColumn[];
 
-  constructor(props: {}) {
+  constructor(props: IScrollablePaneDetailsListExampleProps) {
     super(props);
 
     this._selection = new Selection();
 
     this._allItems = [];
+    const { numberOfColumns, itemCount } = this.props;
     let rowData = '';
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < itemCount; i++) {
       rowData = 'row ' + (i + 1).toString() + ', column ';
       this._allItems.push({
         key: i,
         test1: rowData + '1',
         test2: rowData + '2',
-        test3: rowData + '3'
+        test3: rowData + '3',
+        test4: rowData + '4',
+        test5: rowData + '5',
+        test6: rowData + '6'
       });
     }
 
     this._columns = [];
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i <= numberOfColumns; i++) {
       this._columns.push({
         key: 'column' + i,
         name: 'Test ' + i,
         fieldName: 'test' + i,
-        minWidth: 100,
+        minWidth: 150,
         maxWidth: 200,
         isResizable: true
       });
@@ -120,10 +135,7 @@ export class ScrollablePaneDetailsListExample extends React.Component<
           <div className={classNames.wrapper}>
             <ScrollablePane
               style={{ maxWidth: '900px', border: '1px solid #edebe9' }}
-              scrollbarVisibility={ScrollbarVisibility.always}
-              stickyHeaderContainerBehavior={StickyContainerBehaviorType.StickyOnScroll}
-              stickyFooterContainerBehavior={StickyContainerBehaviorType.StickyAlways}
-              optimizeForPerformace={true}
+              {...this.props.scrollablePaneProps}
             >
               <Sticky
                 stickyPosition={StickyPositionType.Header}
@@ -232,6 +244,16 @@ function hasText(item: IScrollablePaneDetailsListExampleItem, text: string): boo
 }
 const getElement = "document.getElementsByClassName('ms-ScrollablePane--contentContainer')[0]";
 const cropTo = { cropTo: '.testWrapper' };
+const scrollablePaneProps: IScrollablePaneProps = {
+  scrollbarVisibility: ScrollbarVisibility.always,
+  stickyFooterContainerBehavior: StickyContainerBehaviorType.StickyAlways,
+  stickyHeaderContainerBehavior: StickyContainerBehaviorType.StickyOnScroll,
+  optimizeForPerformace: true
+};
+const scrollablePaneProps1: IScrollablePaneProps = {
+  ...scrollablePaneProps,
+  stickyHeaderContainerBehavior: StickyContainerBehaviorType.StickyAlways
+};
 
 storiesOf('ScrollablePane-Sticky Details List', module)
   .addDecorator(FabricDecorator)
@@ -241,7 +263,20 @@ storiesOf('ScrollablePane-Sticky Details List', module)
         .snapshot('default', cropTo)
         .executeScript(`${getElement}.scrollTop=2`)
         .snapshot('scroll down by a small amount so that the first row is still visible', cropTo)
+        .executeScript(`${getElement}.scrollLeft=500`)
+        .snapshot(
+          'scroll horizontally to see if header and footer are aligned for non-zero scroll',
+          cropTo
+        )
+        .executeScript(`${getElement}.scrollLeft=0`)
+        .snapshot('scroll horizontally to see if header and footer are aligned', cropTo)
         .executeScript(`${getElement}.scrollTop=999999`)
+        .executeScript(`${getElement}.scrollLeft=500`)
+        .snapshot(
+          'scroll horizontally to see if header and footer are aligned for non-zero scroll (1)',
+          cropTo
+        )
+        .executeScript(`${getElement}.scrollLeft=0`)
         .snapshot('scroll down to the bottom', cropTo)
         .executeScript(`${getElement}.scrollTop=0`)
         .snapshot('scroll up to the top', cropTo)
@@ -253,5 +288,23 @@ storiesOf('ScrollablePane-Sticky Details List', module)
     </Screener>
   ))
   .addStory('ScrollablePane Details List with sticky header & footer (1)', () => (
-    <ScrollablePaneDetailsListExample />
+    <ScrollablePaneDetailsListExample
+      itemCount={200}
+      numberOfColumns={3}
+      scrollablePaneProps={scrollablePaneProps}
+    />
+  ))
+  .addStory('ScrollablePane Details List with sticky header & footer (2)', () => (
+    <ScrollablePaneDetailsListExample
+      itemCount={200}
+      numberOfColumns={6}
+      scrollablePaneProps={scrollablePaneProps}
+    />
+  ))
+  .addStory('ScrollablePane Details List with sticky header & footer (2)', () => (
+    <ScrollablePaneDetailsListExample
+      itemCount={200}
+      numberOfColumns={6}
+      scrollablePaneProps={scrollablePaneProps1}
+    />
   ));
