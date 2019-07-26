b4fe8654167a47c81925f8b49bf6a455831ff57e
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2019-07-26 09:48:10 -0700

    Panel: Change click listener to mousedown listener so that Panels aren't dismissed on mouseup (cherrypick of #5948) (#9840)
    * Panel: Change click listener to mousedown listener so that Panels aren't dismissed on mouseup (cherrypick of #5948).
    
    * Adding change file.

--- packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.tsx
@@ -62,7 +62,7 @@ export class Panel extends BaseComponent<IPanelProps, IPanelState> implements IP
     this._events.on(window, 'resize', this._updateFooterPosition);
 
     if (this._shouldListenForOuterClick(this.props)) {
-      this._events.on(document.body, 'click', this._dismissOnOuterClick, true);
+      this._events.on(document.body, 'mousedown', this._dismissOnOuterClick, true);
     }
 
     if (this.props.isOpen) {
@@ -75,9 +75,9 @@ export class Panel extends BaseComponent<IPanelProps, IPanelState> implements IP
     const previousShouldListenOnOuterClick = this._shouldListenForOuterClick(previousProps);
 
     if (shouldListenOnOuterClick && !previousShouldListenOnOuterClick) {
-      this._events.on(document.body, 'click', this._dismissOnOuterClick, true);
+      this._events.on(document.body, 'mousedown', this._dismissOnOuterClick, true);
     } else if (!shouldListenOnOuterClick && previousShouldListenOnOuterClick) {
-      this._events.off(document.body, 'click', this._dismissOnOuterClick, true);
+      this._events.off(document.body, 'mousedown', this._dismissOnOuterClick, true);
     }
   }
 
