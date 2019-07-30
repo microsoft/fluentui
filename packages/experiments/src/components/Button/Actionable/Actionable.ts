b83bcc1af7a91f2ceb7966aea79a495b6330e166
REDMOND\humorimo <Humberto.Morimoto@microsoft.com>
2019-07-29 17:22:04 -0700

    Button: Creating separate Actionable view to improve perf of Actionable, small perf improvements and perf scenario updates.

--- packages/experiments/src/components/Button/Actionable/Actionable.ts
+++ packages/experiments/src/components/Button/Actionable/Actionable.ts
@@ -2,9 +2,9 @@ import { createComponent } from '../../../Foundation';
 import { ActionableStyles as styles, ActionableTokens as tokens } from './Actionable.styles';
 import { IActionableProps } from './Actionable.types';
 import { useButtonState as state } from '../Button.state';
-import { ButtonView } from '../Button.view';
+import { ActionableView } from './Actionable.view';
 
-export const Actionable: React.StatelessComponent<IActionableProps> = createComponent(ButtonView, {
+export const Actionable: React.StatelessComponent<IActionableProps> = createComponent(ActionableView, {
   displayName: 'Actionable',
   state,
   styles,
