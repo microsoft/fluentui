9d92020ab7a98a2af8e2f3446d54fbd2137ac3f9
Brian Zhao <rezha@microsoft.com>
2019-08-01 17:21:10 -0700

    Move Date class mocking earlier

--- apps/a11y-tests/config/tests.js
+++ apps/a11y-tests/config/tests.js
@@ -8,3 +8,26 @@ initializeIcons();
 
 // Create `dist/reports` folder to save SARIF reports
 mkdirp.sync(path.resolve(__dirname, '../dist/reports'));
+
+// Mock Date class
+const RealDate = Date;
+const constantDate = new Date(Date.UTC(2017, 0, 6, 4, 41, 20));
+
+global.Date = class {
+  static now() {
+    return new RealDate(constantDate);
+  }
+
+  constructor() {
+    return new RealDate(constantDate);
+  }
+};
+
+// Ensure test output is consistent across machine locale and time zone config.
+const mockToLocaleString = () => {
+  return constantDate.toUTCString();
+};
+
+global.Date.prototype.toLocaleString = mockToLocaleString;
+global.Date.prototype.toLocaleTimeString = mockToLocaleString;
+global.Date.prototype.toLocaleDateString = mockToLocaleString;
