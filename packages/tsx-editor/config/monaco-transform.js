965890e42fae77aa0024fdd810e6c669e3c5566e
Elizabeth Craig <elcraig@microsoft.com>
2019-08-02 10:03:11 -0700

    delete duplicate file

--- packages/tsx-editor/config/monaco-transform.js
+++
@@ -1,6 +0,0 @@
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-  babelrc: false,
-  plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-modules-commonjs']
-});
