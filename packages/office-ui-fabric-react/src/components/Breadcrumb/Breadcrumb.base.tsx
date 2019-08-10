67751de82592bd67fe8d91ba145576f012ae09a7
Caio Monteiro <camontei@microsoft.com>
2019-08-09 17:22:57 -0700

    undoing forced change

--- packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.base.tsx
+++ packages/office-ui-fabric-react/src/components/Breadcrumb/Breadcrumb.base.tsx
@@ -156,7 +156,7 @@ export class BreadcrumbBase extends BaseComponent<IBreadcrumbProps, any> {
     if (item.onClick || item.href) {
       return (
         <Link
-          as={'as'}
+          as={'a'}
           className={this._classNames.itemLink}
           href={item.href}
           aria-current={item.isCurrentItem ? 'page' : undefined}
