bb90fe44a41b582c62a05bf80a556dd4dadbfc50
Elizabeth Craig <elcraig@microsoft.com>
2019-08-12 17:24:18 -0700

    Link example scss removal

--- packages/office-ui-fabric-react/src/components/Link/examples/Link.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Link/examples/Link.Basic.Example.tsx
@@ -1,28 +1,50 @@
 import * as React from 'react';
 import { Link } from 'office-ui-fabric-react/lib/Link';
-import * as styles from './Link.Example.scss';
+import { mergeStyles, getTheme } from 'office-ui-fabric-react/lib/Styling';
 
-export class LinkBasicExample extends React.Component<any, any> {
-  public render(): JSX.Element {
-    return (
-      <div className={styles.linkExample}>
-        <span>When a link has an href, </span>
-        <Link href="http://dev.office.com/fabric/components/link">it renders as an anchor tag.</Link>
-        <span> Without an href, </span>
-        <Link>the link is rendered as a button</Link>.<span> You can also use the disabled attribute to create a </span>
+// The Fabric site host (UHF) overrides link styles, so we need to re-assign them.
+// For use of Links elsewhere, this shouldn't be necessary.
+const theme = getTheme();
+const rootClass = mergeStyles({
+  selectors: {
+    '.ms-Link': {
+      color: theme.palette.themePrimary,
+      margin: 0,
+      padding: 0,
+      overflow: 'inherit',
+      textOverflow: 'inherit',
+      selectors: {
+        ':active, :hover, :active:hover': {
+          color: theme.palette.themeDarker
+        },
+        ':focus': {
+          color: theme.palette.themePrimary
+        }
+      }
+    },
+    '.ms-Link.is-disabled': {
+      color: theme.palette.neutralTertiary,
+      pointerEvents: 'none',
+      cursor: 'default'
+    }
+  }
+});
+
+export const LinkBasicExample: React.FunctionComponent = () => {
+  return (
+    <div className={rootClass}>
+      <p>
+        When a link has an href, <Link href="http://dev.office.com/fabric/components/link">it renders as an anchor tag.</Link> Without an
+        href, <Link>the link is rendered as a button</Link>. You can also use the disabled attribute to create a{' '}
         <Link disabled={true} href="http://dev.office.com/fabric/components/link">
           disabled link.
-        </Link>
-        <span>
-          {' '}
-          Example of a Link with <Link href="http://dev.office.com/fabric/components/link">mixed link content that wraps.</Link>
-        </span>
-        <br />
-        <br />
+        </Link>{' '}
+      </p>
+      <p>
         It's not recommended to use Links with imgs because Links are meant to render textual inline content. Buttons are inline-block or in
         the case of imgs, block. However, it is possible to create a linked image button by making a button with an unstyled variant and
         adding the img content and href source to that.
-      </div>
-    );
-  }
-}
+      </p>
+    </div>
+  );
+};
