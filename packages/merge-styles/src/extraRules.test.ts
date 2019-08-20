726180a1667f3dea459ac2fb7d7598d9870e414a
Aneesha Kommineni <aneeshak@Aneeshas-MacBook-Pro.local>
2019-08-20 10:18:22 -0700

    more tests passing

--- packages/merge-styles/src/extraRules.test.ts
+++ packages/merge-styles/src/extraRules.test.ts
@@ -5,4 +5,22 @@ describe('extractRules', () => {
     const rules = extractRules([]);
     expect(rules).toEqual({ __order: ['&'], '&': {} });
   });
+
+  it('handles an empty argument list with a specified currentSelector', () => {
+    const rules = extractRules([], { __order: [] }, '#');
+    expect(rules).toEqual({ __order: ['#'], '#': {} });
+  });
+
+  it('handles a background style', () => {
+    const rules = extractRules([{ background: '#fff' }]);
+    expect(rules).toEqual({ __order: ['&'], '&': { background: '#fff' } });
+  });
+
+  it('handles a background style', () => {
+    const rules = extractRules([{ background: '#fff', margin: '1' }]);
+    expect(rules).toEqual({
+      __order: ['&'],
+      '&': { background: '#fff', marginBottom: '1', marginLeft: '1', marginRight: '1', marginTop: '1' }
+    });
+  });
 });
