541b82ce04d7bbfed89d38e78b49dda6585cc096
Elizabeth Craig <elcraig@microsoft.com>
2019-08-23 18:22:13 -0700

    Remove example data exports from example-app-base

--- packages/example-app-base/src/utilities/data.ts
+++
@@ -1,4 +0,0 @@
-// Re-export these individually since webpack (or a plugin) incorrectly marks code as unused
-// if there's export * two levels deep.
-import { IExampleItem, createListItems, createGroups, lorem, isGroupable } from 'office-ui-fabric-react/lib/utilities/exampleData';
-export { IExampleItem, createListItems, createGroups, lorem, isGroupable };
--- packages/example-app-base/src/utilities/index.ts
+++ packages/example-app-base/src/utilities/index.ts
@@ -1,7 +1,6 @@
 export * from './createApp';
 export * from './createDemoApp';
 export * from './customizations';
-export * from './data';
 export * from './examplesOf';
 export * from './extractAnchorLink';
 export * from './router/index';
