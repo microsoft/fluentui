902caded70c4332a0c8f8a8a359af4952d40573f
Elizabeth Craig <elcraig@microsoft.com>
2019-08-26 15:25:33 -0700

    ColorRectangle styling fixes

--- packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.base.tsx
+++ packages/office-ui-fabric-react/src/components/ColorPicker/ColorPicker.base.tsx
@@ -157,11 +157,10 @@ export class ColorPickerBase extends React.Component<IColorPickerProps, IColorPi
     if (editingColor && editingColor.component === component) {
       return editingColor.value;
     }
-    if (color[component] === null || color[component] === undefined) {
-      return '';
-    } else {
+    if (typeof color[component] === 'number' && !isNaN(color[component] as number)) {
       return String(color[component]);
     }
+    return '';
   }
 
   private _onSVChanged = (ev: React.MouseEvent<HTMLElement>, color: IColor): void => {
