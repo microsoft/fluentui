e20811c0341a309ae1716b1459a6ca6414a5dbb2
lynamemi <emlynam@microsoft.com>
2019-08-30 17:36:08 -0700

    Website: Fix PopupMenu title (#10330)
    * Make title shared
    
    * Change files

--- apps/fabric-website/src/pages/Controls/PopupMenuPage/PopupMenuPage.doc.ts
+++ apps/fabric-website/src/pages/Controls/PopupMenuPage/PopupMenuPage.doc.ts
@@ -5,7 +5,6 @@ const componentUrl = 'https://github.com/OfficeDev/office-ui-fabric-react/tree/m
 
 export const PopupMenuPageProps: TFabricPlatformPageProps = {
   ios: {
-    title: 'Popup Menu',
     overview: require('!raw-loader!@uifabric/fabric-website/src/pages/Controls/PopupMenuPage/docs/ios/PopupMenuOverview.md') as string,
     related,
     componentUrl
