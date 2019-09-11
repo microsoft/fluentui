3ae33c9bbf415746246e3e2c9776157cc8473aba
Micah Godbolt <mgodbolt@microsoft.com>
2019-09-11 15:02:52 -0700

    Update SearchBox.doc.tsx

--- packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.doc.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.doc.tsx
@@ -48,7 +48,6 @@ export const SearchBoxPageProps: IDocPageProps = {
       view: <SearchBoxSmallExample />
     }
   ],
-
   overview: require<string>('!raw-loader!office-ui-fabric-react/src/components/SearchBox/docs/SearchBoxOverview.md'),
   bestPractices: require<string>('!raw-loader!office-ui-fabric-react/src/components/SearchBox/docs/SearchBoxBestPractices.md'),
   dos: require<string>('!raw-loader!office-ui-fabric-react/src/components/SearchBox/docs/SearchBoxDos.md'),
