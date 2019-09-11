a6aa1f94bfd1160c34bf748d660ca80c7c2a5ad1
Lijing29 <50400285+Lijing29@users.noreply.github.com>
2019-09-11 13:47:08 +0800

    Cherry pick #9110 and #10344 to Show Focus Indicator in Checkbox (#10403)
    * [Checkbox] Fix outline on focus. (#9110)
    
    * Checkbox: Tab focus rectangle fix (#10344)
    
    * Checkbox: Added initializeFocusRects to fix focus issue.
    
    * Change files
    
    * Change files
    
    * remove the old change file
    
    * add the new change file
    
    * Change files
    
    * remove the old change file
    
    * remove extra change file

--- packages/office-ui-fabric-react/src/components/Checkbox/Checkbox.base.tsx
+++ packages/office-ui-fabric-react/src/components/Checkbox/Checkbox.base.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { getId, classNamesFunction, mergeAriaAttributeValues, initializeComponentRef, warnMutuallyExclusive } from '../../Utilities';
+import {
+  getId,
+  classNamesFunction,
+  mergeAriaAttributeValues,
+  initializeComponentRef,
+  warnMutuallyExclusive,
+  initializeFocusRects
+} from '../../Utilities';
 import { Icon } from '../../Icon';
 import { ICheckbox, ICheckboxProps, ICheckboxStyleProps, ICheckboxStyles } from './Checkbox.types';
 import { KeytipData } from '../../KeytipData';
@@ -40,6 +47,8 @@ export class CheckboxBase extends React.Component<ICheckboxProps, ICheckboxState
     this.state = {
       isChecked: !!(props.checked !== undefined ? props.checked : props.defaultChecked)
     };
+
+    initializeFocusRects();
   }
 
   public componentWillReceiveProps(newProps: ICheckboxProps): void {
