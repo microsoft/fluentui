585c6e7f078190e220a706c7ce53f8fc1a10aa83
David Zearing <dzearing@microsoft.com>
2019-09-11 17:33:56 -0700

    Update kebabRules.ts

--- packages/merge-styles/src/transforms/kebabRules.ts
+++ packages/merge-styles/src/transforms/kebabRules.ts
@@ -1,16 +1,4 @@
-function makeKebabMap(): { [key: string]: string } {
-  const kebabMap: { [key: string]: string } = {};
-  const elementStyles = (typeof document !== 'undefined' && document.createElement('div').style) || {};
-  let key: string;
-  for (key in elementStyles) {
-    if (key) {
-      kebabMap[key] = key.replace(/([A-Z])/g, '-$1').toLowerCase();
-    }
-  }
-  return kebabMap;
-}
-
-const rules: { [key: string]: string } = makeKebabMap();
+const rules: { [key: string]: string } = {};
 
 export function kebabRules(rulePairs: (string | number)[], index: number): void {
   const rule: string = rulePairs[index] as string;
