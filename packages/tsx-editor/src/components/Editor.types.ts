71982e61f81d41d7f249f1ff7c523b3b9886fac3
Elizabeth Craig <elcraig@microsoft.com>
2019-09-19 08:16:24 -0700

    Refactor editor components into layers

--- packages/tsx-editor/src/components/Editor.types.ts
+++ packages/tsx-editor/src/components/Editor.types.ts
@@ -1,24 +1,50 @@
 import * as React from 'react';
-import * as monaco from '@uifabric/monaco-editor/esm/vs/editor/editor.api';
-
-export type ITextModel = monaco.editor.ITextModel;
+import { IMonacoEditorOptions, IMonacoTextModel } from '../interfaces/monaco';
 
 export interface IEditorProps {
-  /** Code to edit */
-  code: string;
-
-  /** Editor height */
+  /**
+   * Editor height. (Changing this prop should not re-create the editor, but it might not affect
+   * the layout of the existing editor.)
+   */
   height: number | string;
 
-  /** Editor width */
+  /**
+   * Editor width. (Changing this prop should not re-create the editor, but it might not affect
+   * the layout of the existing editor.)
+   */
   width: number | string;
 
-  /**
-   * Used to get notifications of changes to the editor text.
-   * This function's identity MUST stay constant to prevent the editor model from being re-created.
-   */
-  onChange?: (model: ITextModel) => void;
+  /** Class for the div containing the editor. (Can be changed without re-creating the editor.) */
+  className?: string;
 
-  /** Used to access the editor model. Cleared when editor component is disposed to avoid memory leaks. */
-  modelRef?: React.MutableRefObject<ITextModel | undefined>;
+  /**
+   * Initial code to edit. WARNING: Changing this will re-create the editor. (For this reason, the
+   * editor should NOT be used as a controlled component.)
+   */
+  code?: string;
+
+  /** Editor code language. WARNING: Changing this will re-create the editor. */
+  language?: string;
+
+  /** Name for the fake file. WARNING: Changing this will re-create the editor. */
+  filename?: string;
+
+  /** Options for creating the editor. WARNING: Changing this will re-create the editor. */
+  editorOptions?: IMonacoEditorOptions;
+
+  /** Callback to notify when the text changes. (Can be changed without re-creating the editor.) */
+  onChange?: (text: string) => void;
+
+  /**
+   * Debounce `onChange` calls by this many milliseconds, or 0 to disable.
+   * (Can be changed without re-creating the editor.)
+   * @defaultvalue 500
+   */
+  debounceTime?: number;
+
+  /**
+   * Used to access the editor model. Cleared when editor component is disposed to avoid memory leaks.
+   * WARNING: Changing this will re-create the editor.
+   */
+  modelRef?: React.MutableRefObject<IMonacoTextModel | undefined>;
 }
---
+++ packages/tsx-editor/src/utilities/isEditorSupported.ts
@@ -0,0 +1,23 @@
+import { getWindow, isIE11 } from 'office-ui-fabric-react/lib/Utilities';
+import { getMonacoConfig } from '@uifabric/monaco-editor/lib/configureEnvironment';
+import { tryParseExample } from '../transpiler/exampleParser';
+import { getSetting } from '../utilities/settings';
+import { IPackageGroup } from '../interfaces/packageGroup';
+
+export function isEditorSupported(code: string, supportedPackages: IPackageGroup[]): boolean {
+  const win = getWindow();
+  return (
+    // Not server-side rendering
+    !!win &&
+    // Required environment config available
+    !!getMonacoConfig() &&
+    // Opt-in query param or session storage is set
+    getSetting('useEditor') === '1' &&
+    // Not IE 11
+    !isIE11() &&
+    // Service worker available
+    !!win.navigator.serviceWorker &&
+    // No immediate issues detected in example (or exceptions thrown from parsing)
+    typeof tryParseExample(code!, supportedPackages) !== 'string'
+  );
+}
