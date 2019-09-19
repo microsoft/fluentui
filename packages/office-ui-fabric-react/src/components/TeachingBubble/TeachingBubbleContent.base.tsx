ece698da8eba6c359c2682ec2a7b1693f816b698
JD Huntington <jdh@microsoft.com>
2019-09-19 13:57:54 -0700

    TeachingBubble: Remove aria-level

--- packages/office-ui-fabric-react/src/components/TeachingBubble/TeachingBubbleContent.base.tsx
+++ packages/office-ui-fabric-react/src/components/TeachingBubble/TeachingBubbleContent.base.tsx
@@ -92,7 +92,7 @@ export class TeachingBubbleContentBase extends BaseComponent<ITeachingBubbleProp
 
       headerContent = (
         <div className={classNames.header}>
-          <HeaderWrapperAs aria-level="2" role="heading" className={classNames.headline} id={ariaLabelledBy}>
+          <HeaderWrapperAs role="heading" className={classNames.headline} id={ariaLabelledBy}>
             {headline}
           </HeaderWrapperAs>
         </div>
