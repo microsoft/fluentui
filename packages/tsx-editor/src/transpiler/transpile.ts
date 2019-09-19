efecaafb38bdcb37e70ebd055d55ab881b450616
Elizabeth Craig <elcraig@microsoft.com>
2019-09-19 00:09:31 -0700

    Transpile and eval inside a single method

--- packages/tsx-editor/src/transpiler/transpile.ts
+++ packages/tsx-editor/src/transpiler/transpile.ts
@@ -2,24 +2,30 @@ import * as monaco from '@uifabric/monaco-editor';
 import { TypeScriptWorker, EmitOutput } from '@uifabric/monaco-editor/monaco-typescript.d';
 import { transformExample } from './exampleTransform';
 import { _getErrorMessages } from './transpileHelpers';
+import { IMonacoTextModel, IBasicPackageGroup } from '../interfaces/index';
 
-export interface ITranspiledOutput {
+interface ITranspiledOutput {
   outputString?: string;
   error?: string | string[];
 }
 
+/**
+ * Transpile the model's current code from TS to JS.
+ */
 // This is intentionally not an async function, because debugging within transpiled async functions
 // is next to impossible.
-export function transpile(model: monaco.editor.ITextModel): Promise<ITranspiledOutput> {
+export function transpile(model: IMonacoTextModel): Promise<ITranspiledOutput> {
   const transpiledOutput: ITranspiledOutput = { error: undefined, outputString: undefined };
   const filename = model.uri.toString();
   return monaco.languages.typescript
     .getTypeScriptWorker()
-    .then(makeWorker => makeWorker(model.uri))
-    .then((worker: TypeScriptWorker) => {
+    .then((getWorker: (uri: monaco.Uri) => Promise<TypeScriptWorker>) => getWorker(model.uri))
+    .then(worker => {
       return worker.getEmitOutput(filename).then((output: EmitOutput) => {
         if (!output.emitSkipped) {
           transpiledOutput.outputString = output.outputFiles[0].text;
+          console.log('TRANSPILED:');
+          console.log(transpiledOutput.outputString);
           return transpiledOutput;
         }
         // There was an error, so get diagnostics
@@ -39,31 +45,51 @@ export function transpile(model: monaco.editor.ITextModel): Promise<ITranspiledO
       });
     })
     .catch(ex => {
+      // Log the error to the console so people can see the full stack/etc if they want
+      console.error(ex);
       transpiledOutput.error = ex.message;
       return transpiledOutput;
     });
 }
 
 /**
- * Tranforms the code since the given code might have unsupported imports and then evals the code.
+ * Transpiles the code, does an additional transform to prepare for rendering, and evals the code.
  *
- * @param code - Code to transform and eval
- * @param divId - `id` of the `div` element where the example will be rendered into after transforming
- * @returns Returns undefined if the transform was successful, or the error message if it was unsuccessful.
+ * @param model - Current editor text model
+ * @param id - `id` of the `div` element the example will be rendered into after transforming
+ * @param supportedPackages - Supported packages for imports, grouped by global name
+ * (React is implicitly supported)
+ * @returns Returns undefined if the transform and eval were successful, or the error message if unsuccessful.
  */
-export function evalCode(code: string, divId: string): string | undefined {
-  try {
-    // tslint:disable:no-eval
-    const transfromedExample = transformExample(code, divId);
-    if (transfromedExample.output !== undefined) {
-      eval(transfromedExample.output);
-    } else {
-      return transfromedExample.error;
-    }
-  } catch (ex) {
-    // Log the error to the console so people can see the full stack/etc if they want
-    console.error(ex);
-    return ex.message;
-  }
-  return undefined;
+export function transpileAndEval(
+  model: IMonacoTextModel,
+  id: string,
+  supportedPackages: IBasicPackageGroup[]
+): Promise<string | undefined> {
+  const exampleTs = model.getValue();
+  return transpile(model)
+    .then(output => {
+      if (output.error) {
+        return output.error;
+      }
+
+      // tslint:disable:no-eval
+      const transformedExample = transformExample({
+        tsCode: exampleTs,
+        jsCode: output.outputString,
+        supportedPackages,
+        id
+      });
+      if (transformedExample.output !== undefined) {
+        // Run the example inside a closure to avoid conflicts with pre-existing globals
+        eval(`{ ${transformedExample.output} }`);
+      } else {
+        return transformedExample.error || 'Unknown error transforming example';
+      }
+    })
+    .catch(err => {
+      // Log the error to the console so people can see the full stack/etc if they want
+      console.error(err);
+      return err.message || err;
+    });
 }
