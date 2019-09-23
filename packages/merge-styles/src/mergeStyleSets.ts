923fa14816041b1f6d5f18250aaa3ae066eeea26
JD Huntington <jdh@microsoft.com>
2019-09-23 09:47:51 -0700

    mergeCss[Sets] accepts options instead of relying on global rtl settings.

--- packages/merge-styles/src/mergeStyleSets.ts
+++ packages/merge-styles/src/mergeStyleSets.ts
@@ -1,8 +1,10 @@
-import { extractStyleParts } from './extractStyleParts';
 import { concatStyleSets } from './concatStyleSets';
+import { extractStyleParts } from './extractStyleParts';
 import { IStyle } from './IStyle';
-import { styleToRegistration, applyRegistration } from './styleToClassName';
-import { IStyleSet, IProcessedStyleSet, IConcatenatedStyleSet } from './IStyleSet';
+import { IStyleOptions } from './IStyleOptions';
+import { IConcatenatedStyleSet, IProcessedStyleSet, IStyleSet } from './IStyleSet';
+import { getStyleOptions } from './StyleOptionsState';
+import { applyRegistration, styleToRegistration } from './styleToClassName';
 
 /**
  * Takes in one or more style set objects, each consisting of a set of areas,
@@ -92,13 +94,113 @@ export function mergeStyleSets(...styleSets: Array<IStyleSet<any> | undefined | 
  * @param styleSets - One or more style sets to be merged.
  */
 export function mergeStyleSets(...styleSets: Array<IStyleSet<any> | undefined | false | null>): IProcessedStyleSet<any> {
+  return mergeCssSets(styleSets as any, getStyleOptions());
+}
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures we maintain the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets<TStyleSet extends IStyleSet<TStyleSet>>(
+  styleSets: [TStyleSet | false | null | undefined],
+  options?: IStyleOptions
+): IProcessedStyleSet<TStyleSet>;
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures we maintain the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets<TStyleSet1 extends IStyleSet<TStyleSet1>, TStyleSet2 extends IStyleSet<TStyleSet2>>(
+  styleSets: [TStyleSet1 | false | null | undefined, TStyleSet2 | false | null | undefined],
+  options?: IStyleOptions
+): IProcessedStyleSet<TStyleSet1 & TStyleSet2>;
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures we maintain the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets<
+  TStyleSet1 extends IStyleSet<TStyleSet1>,
+  TStyleSet2 extends IStyleSet<TStyleSet2>,
+  TStyleSet3 extends IStyleSet<TStyleSet3>
+>(
+  styleSets: [TStyleSet1 | false | null | undefined, TStyleSet2 | false | null | undefined, TStyleSet3 | false | null | undefined],
+  options?: IStyleOptions
+): IProcessedStyleSet<TStyleSet1 & TStyleSet2 & TStyleSet3>;
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures we maintain the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets<
+  TStyleSet1 extends IStyleSet<TStyleSet1>,
+  TStyleSet2 extends IStyleSet<TStyleSet2>,
+  TStyleSet3 extends IStyleSet<TStyleSet3>,
+  TStyleSet4 extends IStyleSet<TStyleSet4>
+>(
+  styleSets: [
+    TStyleSet1 | false | null | undefined,
+    TStyleSet2 | false | null | undefined,
+    TStyleSet3 | false | null | undefined,
+    TStyleSet4 | false | null | undefined
+  ],
+  options?: IStyleOptions
+): IProcessedStyleSet<TStyleSet1 & TStyleSet2 & TStyleSet3 & TStyleSet4>;
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures we maintain the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets<TStyleSet extends IStyleSet<TStyleSet>>(
+  styleSet: [TStyleSet | false | null | undefined],
+  options?: IStyleOptions
+): IProcessedStyleSet<TStyleSet>;
+
+/**
+ * Takes in one or more style set objects, each1consisting of a set of areas,
+ * each which will produce a class name. Using this is analogous to calling
+ * `mergeCss` for each property in the object, but ensures the
+ * set ordering when multiple style sets are merged.
+ *
+ * @param styleSets - One or more style sets to be merged.
+ * @param options - (optional) Options to use when creating rules.
+ */
+export function mergeCssSets(
+  styleSets: Array<IStyleSet<any> | undefined | false | null>,
+  options?: IStyleOptions
+): IProcessedStyleSet<any> {
   // tslint:disable-next-line:no-any
   const classNameSet: IProcessedStyleSet<any> = { subComponentStyles: {} };
 
   const styleSet = styleSets[0];
 
   if (!styleSet && styleSets.length <= 1) {
-    return { subComponentStyles: {} };
+    return { subComponentStyles: {} } as any;
   }
 
   const concatenatedStyleSet = concatStyleSets(...styleSets);
@@ -115,12 +217,12 @@ export function mergeStyleSets(...styleSets: Array<IStyleSet<any> | undefined | 
       const styles: IStyle = (concatenatedStyleSet as any)[styleSetArea];
 
       const { classes, objects } = extractStyleParts(styles);
-      const registration = styleToRegistration({ displayName: styleSetArea }, objects);
+      const registration = styleToRegistration(options || {}, { displayName: styleSetArea }, objects);
 
       registrations.push(registration);
 
       if (registration) {
-        (classNameSet as any)[styleSetArea] = classes.concat([registration.className]).join(' ');
+        classNameSet[styleSetArea] = classes.concat([registration.className]).join(' ');
       }
     }
   }
@@ -131,5 +233,5 @@ export function mergeStyleSets(...styleSets: Array<IStyleSet<any> | undefined | 
     }
   }
 
-  return classNameSet;
+  return classNameSet as any;
 }
--- packages/merge-styles/src/mergeStyles.ts
+++ packages/merge-styles/src/mergeStyles.ts
@@ -1,17 +1,33 @@
-import { IStyle, IStyleBaseArray } from './IStyle';
-import { styleToClassName } from './styleToClassName';
 import { extractStyleParts } from './extractStyleParts';
+import { IStyle, IStyleBaseArray } from './IStyle';
+import { IStyleOptions } from './IStyleOptions';
+import { getStyleOptions } from './StyleOptionsState';
+import { styleToClassName } from './styleToClassName';
 
 /**
- * Concatination helper, which can merge class names together. Skips over falsey values.
+ * Concatenation helper, which can merge class names together. Skips over falsey values.
  *
  * @public
  */
 export function mergeStyles(...args: (IStyle | IStyleBaseArray | false | null | undefined)[]): string {
-  const { classes, objects } = extractStyleParts(args);
+  return mergeCss(args, getStyleOptions());
+}
+
+/**
+ * Concatenation helper, which can merge class names together. Skips over falsey values.
+ * Accepts a set of options that will be used when calculating styles.
+ *
+ * @public
+ */
+export function mergeCss(
+  args: (IStyle | IStyleBaseArray | false | null | undefined) | (IStyle | IStyleBaseArray | false | null | undefined)[],
+  options?: IStyleOptions
+): string {
+  const styleArgs = args instanceof Array ? args : [args];
+  const { classes, objects } = extractStyleParts(styleArgs);
 
   if (objects.length) {
-    classes.push(styleToClassName(objects));
+    classes.push(styleToClassName(options || {}, objects));
   }
 
   return classes.join(' ');
