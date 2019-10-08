58de08945884437b40670543b59c1c7bb4f56596
Elizabeth Craig <elcraig@microsoft.com>
2019-10-07 22:31:06 -0700

    Fix breadcrumb issues

--- packages/office-ui-fabric-react/src/components/Breadcrumb/examples/Breadcrumb.Static.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Breadcrumb/examples/Breadcrumb.Static.Example.tsx
@@ -1,34 +1,35 @@
 import * as React from 'react';
 import { Breadcrumb, IBreadcrumbItem } from 'office-ui-fabric-react/lib/Breadcrumb';
 
-export class BreadcrumbStaticExample extends React.Component {
-  public render(): JSX.Element {
-    return (
-      <div style={{ display: 'inline-block' }}>
-        <Breadcrumb
-          items={[
-            { text: 'Files', key: 'Files', onClick: this._onBreadcrumbItemClicked },
-            { text: 'This is folder 1', key: 'f1', onClick: this._onBreadcrumbItemClicked },
-            { text: 'This is folder 2', key: 'f2', onClick: this._onBreadcrumbItemClicked },
-            { text: 'This is folder 3', key: 'f3', onClick: this._onBreadcrumbItemClicked },
-            { text: 'This is folder 4', key: 'f4', onClick: this._onBreadcrumbItemClicked },
-            { text: 'This is folder 5', key: 'f5', onClick: this._onBreadcrumbItemClicked, isCurrentItem: true }
-          ]}
-          // Returning undefined to OnReduceData tells the breadcrumb not to shrink
-          onReduceData={this._returnUndefined}
-          maxDisplayedItems={3}
-          ariaLabel={'Breadcrumb with static width'}
-          overflowAriaLabel={'More items'}
-        />
-      </div>
-    );
-  }
+export const BreadcrumbStaticExample: React.FunctionComponent = () => {
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <Breadcrumb
+        items={[
+          { text: 'Not a link', key: 'Files' },
+          { text: 'Link', key: 'f1', onClick: _onBreadcrumbItemClicked },
+          { text: 'Link', key: 'f2', onClick: _onBreadcrumbItemClicked },
+          { text: 'Not a link', key: 'f3' },
+          { text: 'Link', key: 'f4', onClick: _onBreadcrumbItemClicked },
+          {
+            text: 'Link',
+            key: 'f5',
+            onClick: _onBreadcrumbItemClicked,
+            isCurrentItem: true
+          }
+        ]}
+        // Returning undefined to OnReduceData tells the breadcrumb not to shrink
+        onReduceData={_returnUndefined}
+        maxDisplayedItems={3}
+        ariaLabel={'Breadcrumb with static width'}
+        overflowAriaLabel={'More items'}
+      />
+    </div>
+  );
+};
 
-  private _onBreadcrumbItemClicked = (ev: React.MouseEvent<HTMLElement>, item: IBreadcrumbItem): void => {
-    console.log(`Breadcrumb item with key "${item.key}" has been clicked.`);
-  };
-
-  private _returnUndefined(): undefined {
-    return undefined;
-  }
+function _onBreadcrumbItemClicked(ev: React.MouseEvent<HTMLElement>, item: IBreadcrumbItem): void {
+  console.log(`Breadcrumb item with key "${item.key}" has been clicked.`);
 }
+
+const _returnUndefined = () => undefined;
