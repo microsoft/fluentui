232b4b752a70c2581966fcb6e8e1780b924bf5ef
Aneesha Kommineni <aneeshak@Aneeshas-MBP.guest.corp.microsoft.com>
2019-10-15 15:47:16 -0700

    fix failing tests

--- packages/office-ui-fabric-react/src/components/Fabric/Fabric.test.tsx
+++ packages/office-ui-fabric-react/src/components/Fabric/Fabric.test.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import { Fabric } from './Fabric';
+import { renderIntoDocument } from '../../common/testUtilities';
 
 describe('Fabric', () => {
-  it('renders correctly', () => {
+  it('renders a Fabric component correctly', () => {
     const component = renderer.create(<Fabric>test</Fabric>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders a Fabric component with applyTheme correctly', () => {
+    const renderedDOM: HTMLElement = renderIntoDocument(<Fabric applyTheme>test</Fabric>);
+    expect(renderedDOM.getElementsByClassName('ms-Fabric root-themed')).toBeTruthy();
+  });
+
+  it('renders a Fabric component with applyThemeToBody correctly', () => {
+    const renderedDOM: HTMLElement = renderIntoDocument(<Fabric applyThemeToBody>test</Fabric>);
+    expect(renderedDOM.getElementsByClassName('ms-Fabric-body-themed')).toBeTruthy();
   });
 });
