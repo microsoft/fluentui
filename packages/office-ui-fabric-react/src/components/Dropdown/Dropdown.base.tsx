c31b256c602c05d4e5d21cbdaa9536c208f00e42
REDMOND\humorimo <Humberto.Morimoto@microsoft.com>
2019-10-24 12:22:34 -0700

    Dropdown: Moving announcement of selected options from aria-describedby to aria-labelledby.

--- packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.base.tsx
+++ packages/office-ui-fabric-react/src/components/Dropdown/Dropdown.base.tsx
@@ -267,9 +267,8 @@ export class DropdownBase extends React.Component<IDropdownInternalProps, IDropd
               aria-haspopup="listbox"
               aria-expanded={isOpen ? 'true' : 'false'}
               aria-label={ariaLabel}
-              aria-labelledby={label && !ariaLabel ? this._labelId : undefined}
+              aria-labelledby={label && !ariaLabel ? mergeAriaAttributeValues(this._labelId, this._optionId) : undefined}
               aria-describedby={mergeAriaAttributeValues(
-                this._optionId,
                 keytipAttributes['aria-describedby'],
                 hasErrorMessage ? this._id + '-errorMessage' : undefined
               )}
