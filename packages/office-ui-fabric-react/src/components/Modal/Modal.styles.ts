fc1af8c6d03b1fe49855ba82bb532ca2030d5200
maxwellred <v-mare@microsoft.com>
2019-10-30 14:25:34 -0700

    Removed unused screenwidth import

--- packages/office-ui-fabric-react/src/components/Modal/Modal.styles.ts
+++ packages/office-ui-fabric-react/src/components/Modal/Modal.styles.ts
@@ -1,6 +1,5 @@
 import { IModalStyleProps, IModalStyles } from './Modal.types';
 import { AnimationVariables, getGlobalClassNames, ZIndexes } from '../../Styling';
-import { ScreenWidthMinLarge } from 'office-ui-fabric-react/lib/Styling';
 
 export const animationDuration = AnimationVariables.durationValue2;
 
