b1f75eb716dbd91bfc39a81b1125ef1b5b14e5be
Sanket Panda <pandasa123@gmail.com>
2019-10-31 23:47:06 -0400

    Added codepen support

--- packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.doc.tsx
+++ packages/office-ui-fabric-react/src/components/ResizeGroup/ResizeGroup.doc.tsx
@@ -5,11 +5,14 @@ import { IDocPageProps } from '../../common/DocPage.types';
 import { FlexBoxResizeGroupExample } from './examples/ResizeGroup.FlexBox.Example';
 import { ResizeGroupVerticalOverflowSetExample } from './examples/ResizeGroup.VerticalOverflowSet.Example';
 
-const ResizeGroupBasicExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.OverflowSet.Example.tsx') as string;
+const ResizeGroupOverflowSetExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.OverflowSet.Example.tsx') as string;
+const ResizeGroupOverflowSetExampleCodepen = require('!@uifabric/codepen-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.OverflowSet.Example.tsx') as string;
 
-const ResizeGroupVerticalExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.VerticalOverflowSet.Example.tsx') as string;
+const ResizeGroupVerticalOverflowSetExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.VerticalOverflowSet.Example.tsx') as string;
+const ResizeGroupVerticalOverflowSetExampleCodepen = require('!@uifabric/codepen-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.VerticalOverflowSet.Example.tsx') as string;
 
 const ResizeGroupFlexBoxExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.FlexBox.Example.tsx') as string;
+const ResizeGroupFlexBoxExampleCodepen = require('!@uifabric/codepen-loader!office-ui-fabric-react/src/components/ResizeGroup/examples/ResizeGroup.FlexBox.Example.tsx') as string;
 
 export const ResizeGroupPageProps: IDocPageProps = {
   title: 'ResizeGroup',
@@ -19,18 +22,21 @@ export const ResizeGroupPageProps: IDocPageProps = {
   examples: [
     {
       title: 'Use ResizeGroup to move commands into an overflow menu',
-      code: ResizeGroupBasicExampleCode,
+      code: ResizeGroupOverflowSetExampleCode,
+      codepenJS: ResizeGroupOverflowSetExampleCodepen,
       view: <ResizeGroupOverflowSetExample />
     },
     {
       title: 'Use a vertical ResizeGroup to move commands into an overflow menu',
-      code: ResizeGroupVerticalExampleCode,
+      code: ResizeGroupVerticalOverflowSetExampleCode,
+      codepenJS: ResizeGroupVerticalOverflowSetExampleCodepen,
       view: <ResizeGroupVerticalOverflowSetExample />,
       isScrollable: false
     },
     {
       title: 'Use ResizeGroup to prevent two groups of items from overlapping',
       code: ResizeGroupFlexBoxExampleCode,
+      codepenJS: ResizeGroupFlexBoxExampleCodepen,
       view: <FlexBoxResizeGroupExample />
     }
   ],
