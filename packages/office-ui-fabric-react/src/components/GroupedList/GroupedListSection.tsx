bc30c5cdf4c22c90877d43dc5f11cf3a7efcd105
Thomas Michon <tmichon@microsoft.com>
2019-11-04 12:59:34 -0800

    Propagate 'version' (force-update) through GroupedList to List (#11064)
    * Propagate version through List and GroupedList
    
    * Change files

--- packages/office-ui-fabric-react/src/components/GroupedList/GroupedListSection.tsx
+++ packages/office-ui-fabric-react/src/components/GroupedList/GroupedListSection.tsx
@@ -182,13 +182,16 @@ export class GroupedListSection extends BaseComponent<IGroupedListSectionProps, 
       onShouldVirtualize,
       groupedListClassNames,
       groups,
-      compact
+      compact,
+      listProps = {}
     } = this.props;
     const { isSelected } = this.state;
     const renderCount = group && getGroupItemLimit ? getGroupItemLimit(group) : Infinity;
     const isShowAllVisible =
       group && !group.children && !group.isCollapsed && !group.isShowingAll && (group.count > renderCount || group.hasMoreData);
     const hasNestedGroups = group && group.children && group.children.length > 0;
+
+    const { version } = listProps;
 
     const dividerProps: IGroupDividerProps = {
       group,
@@ -224,6 +227,7 @@ export class GroupedListSection extends BaseComponent<IGroupedListSectionProps, 
             onRenderCell={this._renderSubGroup}
             getItemCountForPage={this._returnOne}
             onShouldVirtualize={onShouldVirtualize}
+            version={version}
             id={this._id}
           />
         ) : (
