92a06553c224251cbd4b173a5cda5aebab2e77c4
Sanket Panda <pandasa123@gmail.com>
2019-11-06 15:14:20 -0500

    Removed SCSS in favour of mergeStyleSets in FullSize Example

--- packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.FullSize.Example.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.FullSize.Example.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
-import './SearchBox.Examples.scss';
+import { mergeStyleSets } from '@uifabric/styling';
+
+const styles = mergeStyleSets({
+  searchBox: {
+    margin: '0 0 10px 0'
+  }
+});
 
 // tslint:disable:jsx-no-lambda
 
 export class SearchBoxFullSizeExample extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <div className="ms-SearchBoxExample">
+      <div className={styles.searchBox}>
         <SearchBox
           placeholder="Search"
           onSearch={newValue => console.log('value is ' + newValue)}
