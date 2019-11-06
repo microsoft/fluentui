f5865cc6a3d09206272e8be84c5379cb806a71c6
Sanket Panda <pandasa123@gmail.com>
2019-11-06 15:14:08 -0500

    Removed SCSS in favour of mergeStyleSets in CustomIcon Example

--- packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.CustomIcon.Example.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.CustomIcon.Example.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
-import './SearchBox.Examples.scss';
+import { mergeStyleSets } from '@uifabric/styling';
+
+const styles = mergeStyleSets({
+  searchBox: {
+    margin: '0 0 10px 0'
+  }
+});
 
 // tslint:disable:jsx-no-lambda
 export class SearchBoxCustomIconExample extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <div className="ms-SearchBoxExample">
+      <div className={styles.searchBox}>
         <SearchBox
           placeholder="Filter"
           onFocus={() => console.log('onFocus called')}
