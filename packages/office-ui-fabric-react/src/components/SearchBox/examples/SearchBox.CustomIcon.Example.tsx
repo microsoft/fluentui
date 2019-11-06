ade9358f9bb92a91ac06d10e49f94fb6ce784509
Sanket Panda <pandasa123@gmail.com>
2019-11-06 17:48:47 -0500

    Removed mergeStyleSets from CustomIcon since single SearchBox

--- packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.CustomIcon.Example.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.CustomIcon.Example.tsx
@@ -1,25 +1,16 @@
 import * as React from 'react';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
-import { mergeStyleSets } from '@uifabric/styling';
-
-const styles = mergeStyleSets({
-  searchBox: {
-    margin: '0 0 10px 0'
-  }
-});
 
 // tslint:disable:jsx-no-lambda
 export class SearchBoxCustomIconExample extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <div className={styles.searchBox}>
-        <SearchBox
-          placeholder="Filter"
-          onFocus={() => console.log('onFocus called')}
-          onBlur={() => console.log('onBlur called')}
-          iconProps={{ iconName: 'Filter' }}
-        />
-      </div>
+      <SearchBox
+        placeholder="Filter"
+        onFocus={() => console.log('onFocus called')}
+        onBlur={() => console.log('onBlur called')}
+        iconProps={{ iconName: 'Filter' }}
+      />
     );
   }
 }
