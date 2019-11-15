fc378d1cf157c6022db9e2d2b9a4422e3872a824
yamatatsu <the.kinnikuman@gmail.com>
2019-11-15 20:17:53 +0900

    Fix "the the" to "the"

--- packages/office-ui-fabric-react/src/components/Keytip/KeytipContent.base.tsx
+++ packages/office-ui-fabric-react/src/components/Keytip/KeytipContent.base.tsx
@@ -3,7 +3,7 @@ import { classNamesFunction } from '../../Utilities';
 import { IKeytipProps, IKeytipStyleProps, IKeytipStyles } from './Keytip.types';
 
 /**
- * A component corresponding the the content rendered inside the callout of the keytip component.
+ * A component corresponding the content rendered inside the callout of the keytip component.
  * {@docCategory Keytips}
  */
 export class KeytipContentBase extends React.Component<IKeytipProps, {}> {
