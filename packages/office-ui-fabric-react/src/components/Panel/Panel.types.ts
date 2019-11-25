8a9cbb2793ffdea31c326c7666c059d914b83c47
John Chou <kuzhou@microsoft.com>
2019-11-25 14:29:04 +0800

    Propose a new panel type

--- packages/office-ui-fabric-react/src/components/Panel/Panel.types.ts
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.types.ts
@@ -335,7 +335,18 @@ export enum PanelType {
    * - When screen width reaches the `customWidth` value it will behave like a fluid width Panel
    * taking up 100% of the viewport width
    */
-  customNear = 8
+  customNear = 8,
+
+  /**
+   * Renders the Panel under father container in `small` size, anchored to the far side (right in LTR mode).
+   * Recommended for use on small screen breakpoints.
+   * - Small (320-479): full screen width, 16px left/right padding
+   * - Medium (480-639): full screen width, 16px left/right padding
+   * - Large (640-1023): full screen width, 32px left/right padding
+   * - XLarge (1024-1365): full screen width, 32px left/right padding
+   * - XXLarge (1366-up): full screen width, 40px left/right padding
+   */
+  smallEmbedded = 9
 }
 
 /**
