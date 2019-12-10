6002b3a18330c270c00245c590f059299a728fbf
Xu Gao <xgao@microsoft.com>
2019-12-10 14:11:36 -0800

    can't believe it works

--- packages/office-ui-fabric-react/src/components/Button/examples/Button.Themed.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Button/examples/Button.Themed.Example.tsx
@@ -1,27 +1,54 @@
 import * as React from 'react';
 import { mergeCss } from '@uifabric/merge-styles';
 import { Provider } from './Provider';
-import { FluentTheme } from './fluent/FluentTheme';
+import { FluentTheme, PlannerFluentTheme } from './fluent/FluentTheme';
 import { FluentButton } from './fluent/FluentButton';
 
 const oddRedBorder = mergeCss({ border: '10px solid red' });
+const example = mergeCss({ margin: 20 });
 
+const Icon: React.FunctionComponent<any> = props => <span {...props}>@</span>;
 export const ButtonThemedExample: React.FunctionComponent<{}> = props => {
   const onClick = React.useCallback(() => console.log('clicked button'), []);
-
-  return (
-    <Provider theme={FluentTheme}>
-      <div>
-        <div>
+  const variants = () => {
+    return (
+      <>
+        <div className={example}>
+          <FluentButton tiny>tiny</FluentButton>
+        </div>
+        <div className={example}>
+          <FluentButton large>large</FluentButton>
+        </div>
+        <div className={example}>
+          <FluentButton shadowed>shadowed</FluentButton>
+        </div>
+        <div className={example}>
+          <FluentButton bigIcon={true} slots={{ icon: Icon, primaryText: () => <span>BigIcon</span> }} />
+        </div>
+        <div className={example}>
+          <FluentButton id="sdasdas" shadowed tiny>
+            shadowed & tiny
+          </FluentButton>
+        </div>
+        <div className={example}>
           <FluentButton onClick={onClick}>A standard fluent button</FluentButton>
         </div>
 
-        <div>
+        <div className={example}>
           <FluentButton onClick={onClick} className={oddRedBorder}>
             Fluent Button with an odd red border
           </FluentButton>
         </div>
-      </div>
-    </Provider>
+      </>
+    );
+  };
+  return (
+    <div>
+      <h1>Fluent Theme</h1>
+      <Provider theme={FluentTheme}>{variants()}</Provider>
+
+      <h1>Planner Fluent Theme</h1>
+      <Provider theme={PlannerFluentTheme}>{variants()}</Provider>
+    </div>
   );
 };
--- packages/office-ui-fabric-react/src/components/Button/examples/fluent/FluentTheme.ts
+++ packages/office-ui-fabric-react/src/components/Button/examples/fluent/FluentTheme.ts
@@ -10,3 +10,50 @@ export const FluentTheme: IFluentThemeShape = {
     fontFace: 'Futura'
   }
 };
+
+export const PlannerFluentTheme: IFluentThemeShape = {
+  colors: {
+    brand: new ColorRamp(['#00f9ff', '#008e91', '#003233']),
+    neutral: new ColorRamp(['#dedede', '#7c7c7c', '#292929'])
+  },
+  typography: {
+    ramp: [8, 10, 12, 16, 24, 36, 48, 128],
+    fontFace: 'Futura'
+  },
+  components: {
+    FluentButton: {
+      styles: ({ typography, colors }: any) => ({
+        root: {
+          fontFamily: typography.fontFace,
+          fontSize: typography.ramp[5],
+          backgroundColor: colors.brand.strongest(),
+          color: colors.neutral.weakest()
+        }
+      }),
+      variants: {
+        tiny: {
+          root: { fontSize: '20%' }
+        },
+        large: {
+          root: { fontSize: '400%' }
+        },
+        shadowed: {
+          root: { boxShadow: '10px 5px 5px purple' }
+        },
+        bigIcon: {
+          icon: { fontSize: '300%' }
+        }
+      }
+    }
+  }
+};
+
+/**
+ *
+  const rootClassName = mergeCss({
+    fontFamily: theme.typography.fontFace,
+    fontSize: theme.typography.ramp[5],
+    backgroundColor: theme.colors.brand.strongest(),
+    color: theme.colors.neutral.weakest()
+  });
+ */
