d98dc62d47081b1418acb082c3f5e4e2484c9c9e
Xu Gao <xgao@microsoft.com>
2019-12-16 20:57:03 -0800

    clean up

--- packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.styles.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/SearchBox.styles.tsx
@@ -96,12 +96,7 @@ export function getStyles(props: ISearchBoxStyleProps): ISearchBoxStyles {
       hasFocus && [
         'is-active',
         {
-          position: 'relative',
-          selectors: {
-            [HighContrastSelector]: {
-              borderColor: 'Highlight'
-            }
-          }
+          position: 'relative'
         },
         getFocusBorder(semanticColors.inputFocusBorderAlt)
       ],
