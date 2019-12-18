c98780c3071c6804444fb1b019a9d6d213268967
REDMOND\humorimo <Humberto.Morimoto@microsoft.com>
2019-12-18 14:03:27 -0800

    ScrollablePane: Fixing tabbing navigation with Sticky headers.

--- packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePane.base.tsx
+++ packages/office-ui-fabric-react/src/components/ScrollablePane/ScrollablePane.base.tsx
@@ -174,10 +174,10 @@ export class ScrollablePaneBase extends BaseComponent<IScrollablePaneProps, IScr
 
     return (
       <div {...getNativeProps(this.props, divProperties)} ref={this._root} className={classNames.root}>
+        <div ref={this._stickyAboveRef} className={classNames.stickyAbove} style={this._getStickyContainerStyle(stickyTopHeight, true)} />
         <div ref={this._contentContainer} className={classNames.contentContainer} data-is-scrollable={true}>
           <ScrollablePaneContext.Provider value={this._getScrollablePaneContext()}>{this.props.children}</ScrollablePaneContext.Provider>
         </div>
-        <div ref={this._stickyAboveRef} className={classNames.stickyAbove} style={this._getStickyContainerStyle(stickyTopHeight, true)} />
         <div className={classNames.stickyBelow} style={this._getStickyContainerStyle(stickyBottomHeight, false)}>
           <div ref={this._stickyBelowRef} className={classNames.stickyBelowItems} />
         </div>
