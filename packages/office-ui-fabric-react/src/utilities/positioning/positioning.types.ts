155383e065997acdbf405bd4ef494a1d11eaadbf
Lerch Leopold <u11q12p@post.at>
2019-12-20 10:53:42 +0100

    Removed circular import

--- packages/office-ui-fabric-react/src/utilities/positioning/positioning.types.ts
+++ packages/office-ui-fabric-react/src/utilities/positioning/positioning.types.ts
@@ -1,6 +1,5 @@
 
 import { DirectionalHint } from '../../common/DirectionalHint';
-import { IPoint } from './positioning.types';
 import { IRectangle } from '../../Utilities';
 
 export enum RectangleEdge {
