df9d42f327766a33456b5599eccc4df977a8c06e
Elizabeth Craig <elcraig@microsoft.com>
2019-12-20 17:46:43 -0800

    ChoiceGroup: update docs and examples

--- packages/office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Basic.Example.tsx
@@ -1,35 +1,15 @@
 import * as React from 'react';
 import { ChoiceGroup, IChoiceGroupOption } from 'office-ui-fabric-react/lib/ChoiceGroup';
 
+const options: IChoiceGroupOption[] = [
+  { key: 'A', text: 'Option A' },
+  { key: 'B', text: 'Option B' },
+  { key: 'C', text: 'Option C', disabled: true },
+  { key: 'D', text: 'Option D' }
+];
+
 export const ChoiceGroupBasicExample: React.FunctionComponent = () => {
-  return (
-    <ChoiceGroup
-      className="defaultChoiceGroup"
-      defaultSelectedKey="B"
-      options={[
-        {
-          key: 'A',
-          text: 'Option A'
-        },
-        {
-          key: 'B',
-          text: 'Option B'
-        },
-        {
-          key: 'C',
-          text: 'Option C',
-          disabled: true
-        },
-        {
-          key: 'D',
-          text: 'Option D'
-        }
-      ]}
-      onChange={_onChange}
-      label="Pick one"
-      required={true}
-    />
-  );
+  return <ChoiceGroup defaultSelectedKey="B" options={options} onChange={_onChange} label="Pick one" required={true} />;
 };
 
 function _onChange(ev: React.FormEvent<HTMLInputElement>, option: IChoiceGroupOption): void {
