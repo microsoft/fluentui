992537473a22e8d7f7ef217e17ab4da31ceef3db
Aneesha Kommineni <aneeshak@microsoft.com>
2019-12-27 13:38:30 -0800

    Layer: add touch events to filtering to stop those that are bubbling up

--- packages/office-ui-fabric-react/src/components/Layer/Layer.base.tsx
+++ packages/office-ui-fabric-react/src/components/Layer/Layer.base.tsx
@@ -163,7 +163,13 @@ export class LayerBase extends React.Component<ILayerProps, ILayerBaseState> {
 const _onFilterEvent = (ev: React.SyntheticEvent<HTMLElement>): void => {
   // We should just be able to check ev.bubble here and only stop events that are bubbling up. However, even though mouseenter and
   //    mouseleave do NOT bubble up, they are showing up as bubbling. Therefore we stop events based on event name rather than ev.bubble.
-  if (ev.eventPhase === Event.BUBBLING_PHASE && ev.type !== 'mouseenter' && ev.type !== 'mouseleave') {
+  if (
+    ev.eventPhase === Event.BUBBLING_PHASE &&
+    ev.type !== 'mouseenter' &&
+    ev.type !== 'mouseleave' &&
+    ev.type !== 'touchstart' &&
+    ev.type !== 'touchend'
+  ) {
     ev.stopPropagation();
   }
 };
@@ -193,6 +199,10 @@ function _getFilteredEvents() {
       'onMouseOver',
       'onMouseOut',
       'onMouseUp',
+      'onTouchMove',
+      'onTouchStart',
+      'onTouchCancel',
+      'onTouchEnd',
       'onKeyDown',
       'onKeyPress',
       'onKeyUp',
