4aa7e7d4b0b9f975dec9395cde8c937023e716ba
Elizabeth Craig <elcraig@microsoft.com>
2020-01-02 17:29:25 -0600

    ChoiceGroup: update docs and examples (#11536)
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Related to #11479
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    Update ChoiceGroupOption `labelId` docs to remove potential for confusion around `aria-owns`. Nothing in ChoiceGroup automatically applies `aria-owns`, and it's more potentially-misleading than helpful to be that specific in our docs about potential uses for an ID.
    
    Also update examples: best practices, add a controlled example, and change ordering (to discourage people from unnecessarily doing more complex things like using custom labels). And remove @srideshpande from codeowners moving forward due to team changes.

--- packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.doc.tsx
+++ packages/office-ui-fabric-react/src/components/ChoiceGroup/ChoiceGroup.doc.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { ChoiceGroupBasicExample } from './examples/ChoiceGroup.Basic.Example';
-import { ChoiceGroupLabelExample } from './examples/ChoiceGroup.Label.Example';
-
 import { IDocPageProps } from '../../common/DocPage.types';
+
+import { ChoiceGroupBasicExample } from './examples/ChoiceGroup.Basic.Example';
+import { ChoiceGroupControlledExample } from './examples/ChoiceGroup.Controlled.Example';
+import { ChoiceGroupLabelExample } from './examples/ChoiceGroup.Label.Example';
 import { ChoiceGroupCustomExample } from './examples/ChoiceGroup.Custom.Example';
 import { ChoiceGroupImageExample } from './examples/ChoiceGroup.Image.Example';
 import { ChoiceGroupIconExample } from './examples/ChoiceGroup.Icon.Example';
 
 const ChoiceGroupBasicExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Basic.Example.tsx') as string;
+const ChoiceGroupControlledExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Controlled.Example.tsx') as string;
 const ChoiceGroupLabelExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Label.Example.tsx') as string;
 const ChoiceGroupCustomExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Custom.Example.tsx') as string;
 const ChoiceGroupImageExampleCode = require('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/examples/ChoiceGroup.Image.Example.tsx') as string;
@@ -20,22 +22,17 @@ export const ChoiceGroupPageProps: IDocPageProps = {
     'https://github.com/OfficeDev/office-ui-fabric-react/tree/master/packages/office-ui-fabric-react/src/components/ChoiceGroup',
   examples: [
     {
-      title: 'Default ChoiceGroup',
+      title: 'Basic ChoiceGroup',
       code: ChoiceGroupBasicExampleCode,
       view: <ChoiceGroupBasicExample />
     },
     {
-      title: 'ChoiceGroup with a custom label',
-      code: ChoiceGroupLabelExampleCode,
-      view: <ChoiceGroupLabelExample />
+      title: 'Controlled ChoiceGroup',
+      code: ChoiceGroupControlledExampleCode,
+      view: <ChoiceGroupControlledExample />
     },
     {
-      title: 'ChoiceGroup with dropdown',
-      code: ChoiceGroupCustomExampleCode,
-      view: <ChoiceGroupCustomExample />
-    },
-    {
-      title: 'ChoiceGroups with images',
+      title: 'ChoiceGroup with images',
       code: ChoiceGroupImageExampleCode,
       view: <ChoiceGroupImageExample />
     },
@@ -43,6 +40,16 @@ export const ChoiceGroupPageProps: IDocPageProps = {
       title: 'ChoiceGroup with icons',
       code: ChoiceGroupIconExampleCode,
       view: <ChoiceGroupIconExample />
+    },
+    {
+      title: 'ChoiceGroup with a custom label',
+      code: ChoiceGroupLabelExampleCode,
+      view: <ChoiceGroupLabelExample />
+    },
+    {
+      title: 'ChoiceGroup with custom option rendering',
+      code: ChoiceGroupCustomExampleCode,
+      view: <ChoiceGroupCustomExample />
     }
   ],
   overview: require<string>('!raw-loader!office-ui-fabric-react/src/components/ChoiceGroup/docs/ChoiceGroupOverview.md'),
