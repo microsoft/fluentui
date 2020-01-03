8d4eacf3ae16f23391505b711a4f42c33764acb1
Micah Godbolt <mgodbolt@microsoft.com>
2020-01-03 11:14:15 -0800

    snaps

--- packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.Small.Example.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.Small.Example.tsx
@@ -1,44 +1,24 @@
 import * as React from 'react';
-import { SearchBox, DefaultButton } from 'office-ui-fabric-react';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
 // tslint:disable:jsx-no-lambda
 export class SearchBoxSmallExample extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <>
-        <SearchBox
-          styles={{ root: { width: 200 } }}
-          placeholder="Search"
-          // onEscape={ev => {
-          //   console.log('Custom onEscape Called');
-          // }}
-          // onClear={ev => {
-          //   console.log('Custom onClear Called');
-          // }}
-          // onChange={(_, newValue) => console.log('SearchBox onChange fired: ' + newValue)}
-          // onSearch={newValue => console.log('SearchBox onSearch fired: ' + newValue)}
-          // onFocus={() => console.log('onFocus called')}
-          // onBlur={() => console.log('onBlur called')}
-        />
-        <DefaultButton
-          menuProps={{
-            items: [
-              {
-                key: 'emailMessage',
-                text: 'Email message',
-                iconProps: { iconName: 'Mail' }
-              },
-              {
-                key: 'calendarEvent',
-                text: 'Calendar event',
-                iconProps: { iconName: 'Calendar' }
-              }
-            ]
-          }}
-        >
-          Click Me
-        </DefaultButton>
-      </>
+      <SearchBox
+        styles={{ root: { width: 200 } }}
+        placeholder="Search"
+        onEscape={ev => {
+          console.log('Custom onEscape Called');
+        }}
+        onClear={ev => {
+          console.log('Custom onClear Called');
+        }}
+        onChange={(_, newValue) => console.log('SearchBox onChange fired: ' + newValue)}
+        onSearch={newValue => console.log('SearchBox onSearch fired: ' + newValue)}
+        onFocus={() => console.log('onFocus called')}
+        onBlur={() => console.log('onBlur called')}
+      />
     );
   }
 }
