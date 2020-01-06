97ecee9dcf10904e65e6d7bcfe274d39ee95e24d
JD Huntington <jdh@microsoft.com>
2020-01-06 10:02:38 -0800

    [Migration] Update test fixtures to handle windows path issue.

--- packages/migration/just.config.js
+++ packages/migration/just.config.js
@@ -2,5 +2,4 @@ const { preset, just } = require('@uifabric/build');
 const { task, series } = just;
 
 preset();
-// TODO remove, fix tests on windows environments
-task('build', series('clean', 'copy', 'ts:commonjs-only')).cached();
+task('build', 'build:node-lib').cached();
