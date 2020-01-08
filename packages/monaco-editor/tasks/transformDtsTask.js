5ffc796091634fabb175c7efabe04c5670f07fa2
Elizabeth Craig <elcraig@microsoft.com>
2020-01-07 19:33:55 -0800

    fix parentheses order

--- packages/monaco-editor/tasks/transformDtsTask.js
+++ packages/monaco-editor/tasks/transformDtsTask.js
@@ -13,7 +13,7 @@ exports.transformDtsTask = function() {
     // Remove ambient get accessors, added in TS > 3.5 and incompatible with earlier versions.
     content = content.replace(/^    get (\w+)\(\)/gm, '    readonly $1');
 
-    const dtsDirname = path.dirname(dtsFile.replace(/\\/g, '/'));
+    const dtsDirname = path.dirname(dtsFile).replace(/\\/g, '/');
     if (dtsDirname.endsWith('/language/typescript') && content.includes(' monaco.')) {
       // Add imports for ambient monaco references in typescript language contribution
       content = "import * as monaco from '../../editor/editor.api';" + os.EOL + content;
