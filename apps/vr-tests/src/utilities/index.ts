6179acfc996eb84c3a87e278f92d82f1b30fd2b4
Ken Chau <kchau@microsoft.com>
2020-01-24 10:09:40 -0800

    Fixed where we declare the typings

--- apps/vr-tests/src/utilities/index.ts
+++ apps/vr-tests/src/utilities/index.ts
@@ -8,5 +8,13 @@ export interface IStoryConfig {
   rtl?: boolean;
 }
 
+declare module '@storybook/addons/dist/types' {
+  // tslint:disable-next-line: interface-name
+  interface StoryApi<StoryFnReturnType = unknown> {
+    /** adds a story, but via VR Tests' addon which auto adds variants like RTL */
+    addStory: this['add'];
+  }
+}
+
 export * from './FabricDecorator';
 export * from './DevOnlyStoryHeader';
