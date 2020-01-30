a7f4005a4306eeb8da8023c3b6a5486f304abfd0
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2020-01-29 16:34:01 -0800

    ContextualMenu: Transforming examples to Function Components and cleaning them up (#11736)
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Fixes #9535
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    This PR transforms all `ContextualMenu` example to be Function Components and cleans them up.
    
    #### Focus areas to test
    
    (optional)

--- packages/office-ui-fabric-react/src/components/ContextualMenu/examples/ContextualMenu.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/examples/ContextualMenu.Basic.Example.tsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import { ContextualMenu, ContextualMenuItemType, IContextualMenuItem } from 'office-ui-fabric-react/lib/ContextualMenu';
 import { useConstCallback } from '@uifabric/react-hooks';
 
-export interface IContextualMenuBasicExampleState {
-  showContextualMenu?: boolean;
-}
-
 export const ContextualMenuBasicExample: React.FunctionComponent = () => {
   const linkRef = React.useRef(null);
   const [showContextualMenu, setShowContextualMenu] = React.useState(false);
