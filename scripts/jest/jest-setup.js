8bba241259b5dda0a43c0a0e467a4e49a348075f
Elizabeth Craig <elcraig@microsoft.com>
2020-01-31 12:45:21 -0800

    revert componentexamples changes

--- scripts/jest/jest-setup.js
+++ scripts/jest/jest-setup.js
@@ -1,23 +1,13 @@
-// @ts-check
-
 // Fail on warnings.
-const consoleWarn = console.warn;
 const consoleError = console.error;
 
-console.error = customError.bind(null, 'error');
-console.warn = customError.bind(null, 'warn');
+console.error = console.warn = message => {
+  consoleError(message);
 
-function customError(type, ...args) {
-  if (type === 'warn') {
-    consoleWarn(...args);
-  } else {
-    consoleError(...args);
-  }
-
-  if (args.length === 1 && typeof args[0] === 'object' && args[0].stack) {
+  if (typeof message === 'object' && message.stack) {
     // If the "message" was an exception, re-throw it to get the full stack trace
-    throw args[0];
+    throw message;
   } else {
-    throw new Error(`[console ${type}] ${args.join(' ')}`);
+    throw new Error('Caught: ' + message);
   }
-}
+};
