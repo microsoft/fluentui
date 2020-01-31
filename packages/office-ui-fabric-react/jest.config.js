348e81e40760d8db283c18f4c8d4da4642cc97c3
Elizabeth Craig <elcraig@microsoft.com>
2020-01-31 12:34:55 -0800

    Prevent component examples test from eating errors

--- packages/office-ui-fabric-react/jest.config.js
+++ packages/office-ui-fabric-react/jest.config.js
@@ -5,6 +5,7 @@ const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
 
   moduleNameMapper: {
+    '@fluentui/react-focus/lib/(.*)$': '@fluentui/react-focus/lib-commonjs/$1',
     '@uifabric/react-hooks/lib/(.*)$': '@uifabric/react-hooks/lib-commonjs/$1',
     '@uifabric/utilities/lib/(.*)$': '@uifabric/utilities/lib-commonjs/$1',
     // These mappings allow Jest to run snapshot tests against Example files.
