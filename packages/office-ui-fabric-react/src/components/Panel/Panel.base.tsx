0aad966372e333e42ac517da1191f68c7679cc4c
Marrissa Hellesen <mahelles@microsoft.com>
2020-02-04 10:55:39 -0800

    ev.defaultPrevented is not a function, remove errant ()

--- packages/office-ui-fabric-react/src/components/Panel/Panel.base.tsx
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.base.tsx
@@ -395,7 +395,7 @@ export class PanelBase extends BaseComponent<IPanelProps, IPanelState> implement
 
   private _dismissOnOuterClick(ev: any): void {
     const panel = this._panel.current;
-    if (this.isActive && panel && !ev.defaultPrevented()) {
+    if (this.isActive && panel && !ev.defaultPrevented) {
       if (!elementContains(panel, ev.target)) {
         if (this.props.onOuterClick) {
           this.props.onOuterClick();
