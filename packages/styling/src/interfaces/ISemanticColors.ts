4faa49feeb708afb0aeaa5994eb652d9f2081804
Aneesha Kommineni <aneeshak@microsoft.com>
2020-02-07 11:44:29 -0800

    breaking out sentences

--- packages/styling/src/interfaces/ISemanticColors.ts
+++ packages/styling/src/interfaces/ISemanticColors.ts
@@ -53,7 +53,7 @@ import { ISemanticTextColors } from './ISemanticTextColors';
  * and have a large and complex interaction surface.
  * This category covers all kinds of lists, whether they're typical one-item-per-row lists (like DetailsList) or ones with a tiled layout.
  *
- * Note: We are aware that the usage of semantic slots vs. palette slots is not consistent across the Fabric controls code and that both
+ * Note: We are aware that the usage of semantic slots vs. palette slots is not consistent across the Fabric controls code. Both
  * offerings need updates such as adding/removing slots. We've been actively making some of these changes. And we're working on a a better
  * solution that satisfies a lot of different factors including avoiding bloat of the already sizable offering. We'll update this page when
  * that's ready. Thank you for your patience.
