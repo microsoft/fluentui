b9442af096f87f66a21a354a5389db7953fcf378
Katie Arriaga Maldonado <catalina@microsoft.com>
2020-02-14 15:40:46 -0800

    Setting touchmove event on capture phase for DraggableZone component

--- packages/office-ui-fabric-react/src/utilities/DraggableZone/DraggableZone.tsx
+++ packages/office-ui-fabric-react/src/utilities/DraggableZone/DraggableZone.tsx
@@ -153,7 +153,7 @@ export class DraggableZone extends React.Component<IDraggableZoneProps, IDraggab
     // hook up the appropriate mouse/touch events to the body to ensure
     // smooth dragging
     this._events = [
-      on(document.body, this._currentEventType.move, this._onDrag),
+      on(event.currentTarget, this._currentEventType.move, this._onDrag, true /* use capture phase */),
       on(document.body, this._currentEventType.stop, this._onDragStop)
     ];
   };
