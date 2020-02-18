9835bad8dfecc6717a3f805fcdd5bc5abe56dece
Jason Gore <jagore@microsoft.com>
2020-02-18 14:27:08 +0100

    Enable perf pipeline and basic filter for scenarios.

--- packages/fluentui/docs/src/config.ts
+++ packages/fluentui/docs/src/config.ts
@@ -1,5 +1,7 @@
 const config = {
-  getStatsUri: 'https://fluent-ui-react-stats.azurewebsites.net/api/GetPerfStats',
-}
+  // TODO: revert (temporarily using old URI for staging)
+  // getStatsUri: 'https://fluent-ui-react-stats.azurewebsites.net/api/GetPerfStats',
+  getStatsUri: 'https://stardust.azurewebsites.net/api/GetPerfStats'
+};
 
-export default config
+export default config;
