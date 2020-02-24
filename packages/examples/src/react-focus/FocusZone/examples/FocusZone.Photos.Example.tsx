6d8402e4feb3a18723a2ecfa7990ed14386f8c6f
Elizabeth Craig <elcraig@microsoft.com>
2020-02-24 15:26:16 -0800

    unused import

--- packages/examples/src/react-focus/FocusZone/examples/FocusZone.Photos.Example.tsx
+++ packages/examples/src/react-focus/FocusZone/examples/FocusZone.Photos.Example.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FocusZone, FocusZoneDirection } from '@fluentui/react-focus';
+import { FocusZone } from '@fluentui/react-focus';
 import { Image } from 'office-ui-fabric-react/lib/Image';
 import { getId } from 'office-ui-fabric-react/lib/Utilities';
 import { useConst } from '@uifabric/react-hooks';
