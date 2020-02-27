94e3cee7359ec1cc39ec3b10c0f67cdb4244533f
Ken Chau <kchau@microsoft.com>
2020-02-27 11:51:45 -0800

    making sure that the resolve.alias do NOT skip over the fluentui packages

--- scripts/webpack/getResolveAlias.js
+++ scripts/webpack/getResolveAlias.js
@@ -4,26 +4,48 @@ const findRepoDeps = require('../monorepo/findRepoDeps');
 const findGitRoot = require('../monorepo/findGitRoot');
 const { readConfig } = require('../read-config');
 
+function getOutputPath(packageJson) {
+  for (const mainField of ['module', 'main']) {
+    const main = packageJson[mainField];
+    if (main && main.includes('dist/es')) {
+      return 'dist/es';
+    } else {
+      return 'lib';
+    }
+  }
+
+  return 'lib';
+}
+
 function getResolveAlias() {
   const gitRoot = findGitRoot();
   const deps = findRepoDeps();
+
   const alias = {};
-  const excludedPackages = ['@uifabric/api-docs'];
+  const excludedPackages = ['@uifabric/api-docs', '@uifabric/build'];
 
   let cwd = process.cwd();
   const packageJson = readConfig(path.join(cwd, 'package.json'));
 
   deps.forEach(depInfo => {
-    if (!depInfo.packageJson.private && !excludedPackages.includes(depInfo.packageJson.name)) {
+    if (!excludedPackages.includes(depInfo.packageJson.name)) {
       alias[`${depInfo.packageJson.name}$`] = path.join(gitRoot, depInfo.packagePath, 'src');
       alias[`${depInfo.packageJson.name}/src`] = path.join(gitRoot, depInfo.packagePath, 'src');
-      alias[`${depInfo.packageJson.name}/lib`] = path.join(gitRoot, depInfo.packagePath, 'src');
+
+      const outputPath = getOutputPath(depInfo.packageJson);
+
+      alias[`${depInfo.packageJson.name}/${outputPath}`] = path.join(gitRoot, depInfo.packagePath, 'src');
     }
   });
 
   alias[`${packageJson.name}$`] = path.join(cwd, 'src');
   alias[`${packageJson.name}/src`] = path.join(cwd, 'src');
-  alias[`${packageJson.name}/lib`] = path.join(cwd, 'src');
+
+  const outputPath = getOutputPath(packageJson);
+
+  alias[`${packageJson.name}/${outputPath}`] = path.join(cwd, 'src');
+
+  console.log(alias);
 
   return alias;
 }
