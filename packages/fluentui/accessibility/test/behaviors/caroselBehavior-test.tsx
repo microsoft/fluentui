79031c7d6d87bec1e2486f1fd6590460a10a74e0
Milan Turon <mituron@microsoft.com>
2020-02-27 09:26:27 +0100

    adding changing tests

--- packages/fluentui/accessibility/test/behaviors/caroselBehavior-test.tsx
+++ packages/fluentui/accessibility/test/behaviors/caroselBehavior-test.tsx
@@ -28,31 +28,25 @@ describe('carouselBehavior.ts', () => {
       expect(expectedResult.attributes.root['aria-label']).toEqual(label);
     });
 
-    test('do NOT set "aria-roledescription" when carousel has navigation', () => {
+    test('do NOT set aria atributes and role when carousel has navigation', () => {
       const expectedResult = carouselBehavior({
         ariaLiveOn: false,
         navigation: true,
-        ariaRoleDescription: roleDescription
-      });
-      expect(expectedResult.attributes.root['aria-roledescription']).toBeUndefined();
-    });
-
-    test('do NOT set "aria-label" when carousel has navigation', () => {
-      const expectedResult = carouselBehavior({
-        ariaLiveOn: false,
-        navigation: true,
+        ariaRoleDescription: roleDescription,
         ariaLabel: label
       });
+      expect(expectedResult.attributes.root['aria-roledescription']).toBeUndefined();
       expect(expectedResult.attributes.root['aria-label']).toBeUndefined();
-    });
-
-    test(`do NOT set "role=region" when carousel has navigation`, () => {
-      const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: true });
       expect(expectedResult.attributes.root.role).toBeUndefined();
     });
   });
 
   describe('itemsContainer', () => {
+    test(`sets "role=region" when carousel has navigation`, () => {
+      const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: true });
+      expect(expectedResult.attributes.root.role).toEqual('region');
+    });
+
     test('sets "aria-roledescription" when carousel has navigation', () => {
       const expectedResult = carouselBehavior({
         ariaLiveOn: false,
@@ -71,27 +65,25 @@ describe('carouselBehavior.ts', () => {
       expect(expectedResult.attributes.itemsContainer['aria-label']).toEqual(label);
     });
 
-    test('do NOT set "aria-roledescription" when carousel has NO navigation', () => {
+    test('do NOT set aria attributes when carousel has NO navigation', () => {
       const expectedResult = carouselBehavior({
         ariaLiveOn: false,
         navigation: false,
-        ariaRoleDescription: roleDescription
-      });
-      expect(expectedResult.attributes.itemsContainer['aria-roledescription']).toBeUndefined();
-    });
-
-    test('do NOT set "aria-label" when carousel has NO navigation', () => {
-      const expectedResult = carouselBehavior({
-        ariaLiveOn: false,
-        navigation: false,
+        ariaRoleDescription: roleDescription,
         ariaLabel: label
       });
+      expect(expectedResult.attributes.itemsContainer['aria-roledescription']).toBeUndefined();
       expect(expectedResult.attributes.itemsContainer['aria-label']).toBeUndefined();
     });
 
-    test(`do NOT set "role=group" when carousel has NO navigation`, () => {
+    test(`sets "role=none" when carousel has NO navigation`, () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: false });
-      expect(expectedResult.attributes.itemsContainer.role).toBeUndefined();
+      expect(expectedResult.attributes.itemsContainer.role).toEqual('none');
+    });
+
+    test(`sets "tabindex=-1" when carousel has NO navigation`, () => {
+      const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: false });
+      expect(expectedResult.attributes.itemsContainer.tabIndex).toEqual('-1');
     });
   });
 });
