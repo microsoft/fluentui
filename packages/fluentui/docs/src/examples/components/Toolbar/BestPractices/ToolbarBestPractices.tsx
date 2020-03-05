78da5719eda0a829e589af5c0791674514e48d26
Silviu Avram <silviu.avram91@gmail.com>
2020-03-05 11:14:03 +0100

    add message in do list

--- packages/fluentui/docs/src/examples/components/Toolbar/BestPractices/ToolbarBestPractices.tsx
+++ packages/fluentui/docs/src/examples/components/Toolbar/BestPractices/ToolbarBestPractices.tsx
@@ -7,7 +7,8 @@ const doList = [
   <Text>
     Label each toolbar when the application contains more than one toolbar (using `aria-label` or `aria-labelledby` props). Refer to{' '}
     {link('toolbar(role)', 'https://www.w3.org/WAI/PF/aria/roles#toolbar')} for details.
-  </Text>
+  </Text>,
+  'Use `active` prop on a ToolbarMenuItem if you want to have an active icon indicator displayed next to it.'
 ];
 
 const ToolbarBestPractices: React.FunctionComponent<{}> = () => {
