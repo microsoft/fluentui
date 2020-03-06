48db247b55f3e6f2809009c6a338c52b6b3e39bd
David Zearing <dzearing@microsoft.com>
2020-03-05 21:34:25 -0800

    withViewport: cleaning up resize observer.

--- packages/office-ui-fabric-react/src/utilities/decorators/withViewport.tsx
+++ packages/office-ui-fabric-react/src/utilities/decorators/withViewport.tsx
@@ -116,6 +116,8 @@ export function withViewport<TProps extends { viewport?: IViewport }, TState>(
       if (this._viewportResizeObserver) {
         this._viewportResizeObserver.disconnect();
       }
+
+      this._unregisterResizeObserver();
     }
 
     public render(): JSX.Element {
@@ -124,7 +126,7 @@ export function withViewport<TProps extends { viewport?: IViewport }, TState>(
 
       return (
         <div className="ms-Viewport" ref={this._root} style={{ minWidth: 1, minHeight: 1 }}>
-          <ComposedComponent ref={this._updateComposedComponentRef} viewport={newViewport} {...(this.props as any)} />
+          <ComposedComponent ref={this._updateComposedComponentRef} viewport={newViewport} {...this.props as any} />
         </div>
       );
     }
