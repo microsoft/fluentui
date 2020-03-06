31e6dc834a76c6afb4c555b155b67247e290dbcb
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-03-05 16:07:38 -0800

    Fixing '_rtl not defined' error thrown when building @fluentui/docs.

--- packages/merge-styles/src/StyleOptionsState.ts
+++ packages/merge-styles/src/StyleOptionsState.ts
@@ -19,7 +19,10 @@ export function getRTL(): boolean {
   return _rtl;
 }
 
-let _rtl = getRTL();
+// This has been split into 2 lines because it was working in Fabric due to the code being transpiled to es5, so this was converted to var.
+// However, this approach work when importing into Fluent because it uses babel, which transpiles the code to be es6-like.
+let _rtl: boolean;
+_rtl = getRTL();
 
 export function getStyleOptions(): IStyleOptions {
   return {
