8c5d156297235aeb1c92e896fdc8844b21155f53
Silviu Avram <silviu.avram91@gmail.com>
2020-03-10 12:18:31 +0100

    add disabled Input example

---
+++ packages/fluentui/docs/src/examples/components/Input/State/InputExampleDisabled.shorthand.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Input } from '@fluentui/react';
+
+const InputExampleDisabled = () => <Input fluid disabled placeholder="You shall not type ..." />;
+
+export default InputExampleDisabled;
