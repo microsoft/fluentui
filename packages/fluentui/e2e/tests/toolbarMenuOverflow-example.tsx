2cf831cededb7af26072cb76207b2ae303f48d40
Milan Turon <mituron@microsoft.com>
2020-03-11 16:33:44 +0100

    adressing changes after review

--- packages/fluentui/e2e/tests/toolbarMenuOverflow-example.tsx
+++ packages/fluentui/e2e/tests/toolbarMenuOverflow-example.tsx
@@ -24,8 +24,8 @@ const ToolbarExampleOverflow = () => {
     content: `${icons[i % icons.length]} #${i}`,
     icon: icons[i % icons.length],
     title: `${icons[i % icons.length]} #${i}`,
-    onClick: i + 1 == itemsCount ? () => buttonAfterToolbarRef.current.focus() : undefined,
-    // first hald of items are unwrapped, rest are wrapped, expect of the last item
+    onClick: i + 1 === itemsCount ? () => buttonAfterToolbarRef.current.focus() : undefined,
+    // first half of items are unwrapped, rest are wrapped, expect of the last item
     // don't add submenu on last item, on last item onClick with moving focus is tested
     ...(i >= itemsCount / 2 && i + 1 < itemsCount && { menu: [] })
   }));
