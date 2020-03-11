778d7ea52ddf1cd08bab91376a4a645f9176042c
Andrew Martin <andmarti@microsoft.com>
2020-03-11 14:25:52 -0700

    fixing merge

--- packages/fluentui/react/src/themes/teams-high-contrast/componentStyles.ts
+++ packages/fluentui/react/src/themes/teams-high-contrast/componentStyles.ts
@@ -1,3 +1,1 @@
 export { default as MenuItem } from './components/Menu/menuItemStyles';
-export { default as Alert } from './components/Alert/alertStyles';
-export { default as Dialog } from './components/Dialog/dialogStyles';
