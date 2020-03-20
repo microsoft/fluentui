7010c00eda461bcecbfa0cd30c75e863f42455ad
Marija Najdova <mnajdova@gmail.com>
2020-03-20 12:35:32 +0100

    Update packages/fluentui/react-bindings/src/icons/createSvgIcon.tsx
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/react-bindings/src/icons/createSvgIcon.tsx
+++ packages/fluentui/react-bindings/src/icons/createSvgIcon.tsx
@@ -26,7 +26,7 @@ export const svgIconHandledProps: (keyof SvgIconProps)[] = [
   'xSpacing',
 ];
 
-const createSvgIcon = <TProps,>({ svg, displayName, handledProps = [] }: SvgIconCreateFnParams<TProps>) => {
+const createSvgIcon = <TProps>({ svg, displayName, handledProps = [] }: SvgIconCreateFnParams<TProps>) => {
   const Component: React.FC<React.HTMLAttributes<HTMLSpanElement> & TProps & SvgIconProps> & {
     handledProps: (keyof (TProps & SvgIconProps))[];
   } = props => {
