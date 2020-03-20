200179a4dacb7248c62c0205b4005a2155e191a8
Roman Sudarikov <pompomon@users.noreply.github.com>
2020-03-20 15:27:14 +0100

    feat(Card): Adding base layout and API for card component (#12349)
    * Adding prototype for card component
    
    * Remove unused code
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Remove unused code
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Remove unnecessary code
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Properly map props to styles
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Remove unnecessary code
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Remove unnecessary code
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * PR comments fixes, Card isConformant test
    
    * nit
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Refactor examples
    
    * Fix comment in cardBehavior
    
    Co-Authored-By: Marija Najdova <mnajdova@gmail.com>
    
    * Fix PR comments
    
    * Rename top-controls classname
    
    * Refactoring card styles
    
    * Simplify usage examples
    
    * Remove empty a11y behaviors
    
    * Fixes after react-northstar merge
    
    * Add missing exports for Card slots
    
    * Adding changelog
    
    * Fix accessibility behavior test
    
    * Update packages/fluentui/react-northstar/src/components/Card/Card.tsx
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/fluentui/react-northstar/src/components/Card/CardBody.tsx
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/fluentui/react-northstar/src/components/Card/CardBody.tsx
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Fix comments and add style variables
    
    * Fix test, refactor border styles
    
    * Move borderWidth to vars, add isConformat tests
    
    * Remove unused imports
    
    * Fix screener tests
    
    * Update Card to use `useUnhandledProps`
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    Co-authored-by: Marija Najdova <mnajdova@gmail.com>

---
+++ packages/fluentui/react-northstar/src/themes/teams/components/Card/cardFooterStyles.ts
@@ -0,0 +1,16 @@
+import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
+import { CardVariables } from './cardVariables';
+import { CardFooterStylesProps } from '../../../../components/Card/CardFooter';
+
+const cardFooterStyles: ComponentSlotStylesPrepared<CardFooterStylesProps, CardVariables> = {
+  root: ({ variables: v, props: p }): ICSSInJSStyle => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: v.footerMargin,
+      ...(p.fitted && { margin: v.fittedFooterMargin }),
+    };
+  },
+};
+
+export default cardFooterStyles;
