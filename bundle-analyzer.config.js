7f63727436cfdf341b37c7bc24d9edab1a0dd323
Elizabeth Craig <ecraig12345@gmail.com>
2020-03-24 15:04:09 -0700

    Create bundle-analyzer.config.js

---
+++ bundle-analyzer.config.js
@@ -0,0 +1,23 @@
+// test: a glob expression to match assets name
+// maxSize: the maximum size of the asset
+// compression: the compression used to compare the size (by default "none")
+module.exports = {
+  files: [
+    {
+      test: '*',
+      maxSize: '250 kB',
+    },
+  ],
+}
+/*
+    {
+      "test": "*.js",
+      "maxSize": "250 kB",
+      "compression": "none"
+    },
+    {
+      "test": "*.js",
+      "maxSize": "10 kB",
+      "compression": "none"
+    }
+  */
