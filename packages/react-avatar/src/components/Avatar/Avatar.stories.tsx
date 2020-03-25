e7b672707f80d6da0b41532785749e4334a57bf2
David Zearing <dzearing@microsoft.com>
2020-03-25 08:38:13 -0700

    Adding Avatar.

--- packages/react-avatar/src/components/Avatar/Avatar.stories.tsx
+++ packages/react-avatar/src/components/Avatar/Avatar.stories.tsx
@@ -1,4 +1,11 @@
 import * as React from 'react';
 import { Avatar } from './Avatar';
+import { Provider, themes } from '@fluentui/react-northstar';
 
-export const BasicAvatar = () => <Avatar />;
+export const BasicAvatar = () => (
+  <Provider theme={themes.teams}>
+    <Avatar size="small" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
+    <Avatar size="large" name="Jane Doe" status="error" />
+    <Avatar size="larger" name="Lorem Ipsum" status="warning" />
+  </Provider>
+);
