fb7e40b8baf07f6c12a287ad34136e388e4f184e
Oleksandr Fediashov <olfedias@microsoft.com>
2020-03-27 11:49:59 +0100

    RFC: drop `render` callback

--- packages/fluentui/docs/src/prototypes/hexagonalAvatar/CustomAvatar.tsx
+++ packages/fluentui/docs/src/prototypes/hexagonalAvatar/CustomAvatar.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { Avatar, AvatarProps } from '@fluentui/react-northstar';
 
-const CustomAvatar = (props: AvatarProps & { hexagonal?: boolean }) => {
+const CustomAvatar = (props: AvatarProps & { image?: string; hexagonal?: boolean }) => {
   const { hexagonal, ...rest } = props;
 
   if (hexagonal) {
     return (
       <Avatar
         {...rest}
-        image={render =>
-          render(rest.image, (Component, props) => {
+        image={{
+          src: rest.image,
+          children: (Component, props) => {
             const { src, ...restImageProps } = props;
             return (
               <svg
@@ -26,13 +27,12 @@ const CustomAvatar = (props: AvatarProps & { hexagonal?: boolean }) => {
                   clipPath="url(#bot-hexagon-clip-path)"
                   width="34px"
                   height="34px"
-                  xlinkHref={src}
                   {...restImageProps}
                 />
               </svg>
             );
-          })
-        }
+          },
+        }}
       />
     );
   }
