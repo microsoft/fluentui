cd712e606b5d62014ae968b49f1d9a454843f5eb
Marija Najdova <mnajdova@gmail.com>
2020-03-27 12:35:20 +0100

    -added icons

---
+++ packages/fluentui/react-icons-northstar/src/components/index.ts
@@ -0,0 +1,391 @@
+export { default as accept } from './accept';
+export { default as add } from './add';
+export { default as addParticipant } from './addParticipant';
+export { default as arrowUp } from './arrowUp';
+export { default as arrowDown } from './arrowDown';
+export { default as arrowLeft } from './arrowLeft';
+export { default as arrowRight } from './arrowRight';
+export { default as audioOff } from './audioOff';
+export { default as ban } from './ban';
+export { default as bell } from './bell';
+export { default as bellMute } from './bellMute';
+export { default as bold } from './bold';
+export { default as bookmark } from './bookmark';
+export { default as broadcast } from './broadcast';
+export { default as broadcastViewFullscreen } from './broadcastViewFullscreen';
+export { default as broadcastViewLeft } from './broadcastViewLeft';
+export { default as bullets } from './bullets';
+export { default as calendar } from './calendar';
+export { default as call } from './call';
+export { default as callBlocked } from './callBlocked';
+export { default as callParking } from './callParking';
+export { default as callControlCloseTray } from './callControlCloseTray';
+export { default as callControlRelease } from './callControlRelease';
+export { default as callControlRequest } from './callControlRequest';
+export { default as callControlPresentNew } from './callControlPresentNew';
+export { default as callControlStopPresentingNew } from './callControlStopPresentingNew';
+export { default as callControlShare } from './callControlShare';
+export { default as callDialpad } from './callDialpad';
+export { default as callEnd } from './callEnd';
+export { default as callPstn } from './callPstn';
+export { default as callRecording } from './callRecording';
+export { default as callVideo } from './callVideo';
+export { default as callVideoOff } from './callVideoOff';
+export { default as canvasAddPage } from './canvasAddPage';
+export { default as callMissedLine } from './callMissedLine';
+export { default as screencast } from './screencast';
+export { default as chat } from './chat';
+export { default as chevronDown } from './chevronDown';
+export { default as chevronStart } from './chevronStart';
+export { default as chevronEnd } from './chevronEnd';
+export { default as chevronDownMedium } from './chevronDownMedium';
+export { default as chevronRightMedium } from './chevronEndMedium';
+export { default as clipboardCopiedTo } from './clipboardCopiedTo';
+export { default as close } from './close';
+export { default as closedCaptions } from './closedCaptions';
+export { default as codeSnippet } from './codeSnippet';
+export { default as collapse } from './collapse';
+export { default as companion } from './companion';
+export { default as download } from './download';
+export { default as edit } from './edit';
+export { default as email } from './email';
+export { default as emoji } from './emoji';
+export { default as error } from './error';
+export { default as exclamationCircle } from './exclamationCircle';
+export { default as exclamationTriangle } from './exclamationTriangle';
+export { default as expand } from './expand';
+export { default as eye } from './eye';
+export { default as eyeFriendlier } from './eyeFriendlier';
+export { default as eyeSlash } from './eyeSlash';
+export { default as filesAftereffects } from './filesAftereffects';
+export { default as filesCode } from './filesCode';
+export { default as filesEmpty } from './filesEmpty';
+export { default as filesExcel } from './filesExcel';
+export { default as filesExcelBrand } from './filesExcelBrand';
+export { default as filesFlash } from './filesFlash';
+export { default as filesGif } from './filesGif';
+export { default as filesIllustrator } from './filesIllustrator';
+export { default as filesImage } from './filesImage';
+export { default as filesIndesign } from './filesIndesign';
+export { default as filesOneNote } from './filesOneNote';
+export { default as filesOneNoteBrand } from './filesOneNoteBrand';
+export { default as filesPdf } from './filesPdf';
+export { default as filesPhotoshop } from './filesPhotoshop';
+export { default as filesPowerPoint } from './filesPowerPoint';
+export { default as filesPowerPointBrand } from './filesPowerPointBrand';
+export { default as filesSketch } from './filesSketch';
+export { default as filesSound } from './filesSound';
+export { default as filesTxt } from './filesTxt';
+export { default as filesUpload } from './filesUpload';
+export { default as filesVideo } from './filesVideo';
+export { default as filesVisio } from './filesVisio';
+export { default as filesVisioBrand } from './filesVisioBrand';
+export { default as filesWord } from './filesWord';
+export { default as filesWordBrand } from './filesWordBrand';
+export { default as filesZip } from './filesZip';
+export { default as flag } from './flag';
+export { default as fontColor } from './fontColor';
+export { default as fontSize } from './fontSize';
+export { default as format } from './format';
+export { default as gallery } from './gallery';
+export { default as giphy } from './giphy';
+export { default as hand } from './hand';
+export { default as highlight } from './highlight';
+export { default as horizontalRule } from './horizontalRule';
+export { default as iconCircle } from './iconCircle';
+export { default as imageUnavailable } from './image-unavailable';
+export { default as indent } from './indent';
+export { default as info } from './info';
+export { default as italic } from './italic';
+export { default as leave } from './leave';
+export { default as like } from './like';
+export { default as link } from './link';
+export { default as lightning } from './lightning';
+export { default as lock } from './lock';
+export { default as markAsUnread } from './markAsUnread';
+export { default as meetingNew } from './meetingNew';
+export { default as mention } from './mention';
+export { default as menu } from './menu';
+export { default as mergeCalls } from './mergeCalls';
+export { default as messageSeen } from './messageSeen';
+export { default as mic } from './mic';
+export { default as micOff } from './micOff';
+export { default as microsoftStream } from './microsoftStream';
+export { default as more } from './more';
+export { default as noPresenter } from './noPresenter';
+export { default as notes } from './notes';
+export { default as notificationsMuted } from './notificationsMuted';
+export { default as numberList } from './numberList';
+export { default as oneDrive } from './oneDrive';
+export { default as outdent } from './outdent';
+export { default as openOutside } from './openOutside';
+export { default as panorama } from './panorama';
+export { default as pause } from './pause';
+export { default as pauseThick } from './pauseThick';
+export { default as paperclip } from './paperclip';
+export { default as participantRemove } from './participantRemove';
+export { default as phoneClock } from './phoneClock';
+export { default as phoneArrow } from './phoneArrow';
+export { default as play } from './play';
+export { default as plugs } from './plugs';
+export { default as poll } from './poll';
+export { default as presenceAvailable } from './presenceAvailable';
+export { default as presenceStroke } from './presenceStroke';
+export { default as presenter } from './presenter';
+export { default as qna } from './qna';
+export { default as questionCircle } from './questionCircle';
+export { default as quote } from './quote';
+export { default as readAloud } from './read-aloud';
+export { default as raiseHand } from './raiseHand';
+export { default as raiseHandColored } from './raiseHandColored';
+export { default as raiseHandDisabled } from './raiseHandDisabled';
+export { default as redbang } from './redbang';
+export { default as redo } from './redo';
+export { default as removeFormat } from './removeFormat';
+export { default as reply } from './reply';
+export { default as retry } from './retry';
+export { default as robot } from './robot';
+export { default as search } from './search';
+export { default as send } from './send';
+export { default as settings } from './settings';
+export { default as settingsAudio } from './settingsAudio';
+export { default as shareAlt } from './shareAlt';
+export { default as shareLocation } from './shareLocation';
+export { default as shareTo } from './shareTo';
+export { default as skypeLogo } from './skypeLogo';
+export { default as snooze } from './snooze';
+export { default as speakerMute } from './speakerMute';
+export { default as spotlight } from './spotlight';
+export { default as star } from './star';
+export { default as sticker } from './sticker';
+export { default as strike } from './strike';
+export { default as survey } from './survey';
+export { default as survey20px } from './survey20px';
+export { default as sync } from './sync';
+export { default as tabs } from './tabs';
+export { default as table } from './table';
+export { default as tableAdd } from './tableAdd';
+export { default as tableDelete } from './tableDelete';
+export { default as tag } from './tag';
+export { default as teamCreate } from './teamCreate';
+export { default as teams } from './teams';
+export { default as toDoList } from './toDoList';
+export { default as thumbtack } from './thumbtack';
+export { default as thumbtackSlash } from './thumbtackSlash';
+export { default as translation } from './translation';
+export { default as trashCan } from './trashCan';
+export { default as triangleDown } from './triangleDown';
+export { default as triangleRight } from './triangleEnd';
+export { default as triangleUp } from './triangleUp';
+export { default as underline } from './underline';
+export { default as undo } from './undo';
+export { default as urgent } from './urgent';
+export { default as userBlur } from './userBlur';
+export { default as userFriends } from './userFriends';
+export { default as userPhone } from './userPhone';
+export { default as videoCameraEmphasis } from './videoCameraEmphasis';
+export { default as volume } from './volume';
+export { default as volumeDown } from './volumeDown';
+export { default as volumeUp } from './volumeUp';
+export { default as yammer } from './yammer';
+export { default as windowMaximize } from './windowMaximize';
+export { default as windowMinimize } from './windowMinimize';
+export { default as windowRestore } from './windowRestore';
+
+// export default {
+//   'icon-circle': iconCircle,
+//   accept,
+//   add,
+//   'arrow-up': arrowUp,
+//   'arrow-down': arrowDown,
+//   'arrow-left': arrowLeft,
+//   'arrow-right': arrowRight,
+//   'audio-off': audioOff,
+//   ban,
+//   bell,
+//   'bell-slash': bellMute,
+//   'bell-snooze': snooze,
+//   bold,
+//   bookmark,
+//   broadcast,
+//   'broadcast-view-fullscreen': broadcastViewFullscreen,
+//   'broadcast-view-left': broadcastViewLeft,
+//   bullets,
+//   calendar,
+//   call,
+//   'call-blocked': callBlocked,
+//   'call-dialpad': callDialpad,
+//   'call-end': callEnd,
+//   'call-parking': callParking,
+//   'call-video': callVideo,
+//   'call-video-off': callVideoOff,
+//   'call-control-release': callControlRelease,
+//   'call-control-request': callControlRequest,
+//   'call-control-close-tray': callControlCloseTray,
+//   'call-control-present-new': callControlPresentNew,
+//   'call-control-stop-presenting-new': callControlStopPresentingNew,
+//   'call-control-share': callControlShare,
+//   'call-missed-line': callMissedLine,
+//   'call-pstn': callPstn,
+//   'call-recording': callRecording,
+//   screencast,
+//   'canvas-add-page': canvasAddPage,
+//   chat,
+//   'chevron-down': chevronDown,
+//   'icon-chevron-start': chevronStart,
+//   'icon-chevron-end': chevronEnd,
+//   'chevron-down-medium': chevronDownMedium,
+//   'chevron-right-medium': chevronRightMedium,
+//   'clipboard-copied-to': clipboardCopiedTo,
+//   close,
+//   'closed-captions': closedCaptions,
+//   'code-snippet': codeSnippet,
+//   collapse,
+//   companion,
+//   'files-aftereffects': filesAftereffects,
+//   'files-code': filesCode,
+//   'files-empty': filesEmpty,
+//   'files-flash': filesFlash,
+//   'files-gif': filesGif,
+//   'files-illustrator': filesIllustrator,
+//   'files-image': filesImage,
+//   'files-indesign': filesIndesign,
+//   'files-pdf': filesPdf,
+//   'files-photoshop': filesPhotoshop,
+//   'files-sketch': filesSketch,
+//   'files-sound': filesSound,
+//   'files-txt': filesTxt,
+//   'files-upload': filesUpload,
+//   'files-video': filesVideo,
+//   'files-zip': filesZip,
+//   flag,
+//   download,
+//   edit,
+//   email,
+//   emoji,
+//   error,
+//   'exclamation-circle': exclamationCircle,
+//   'exclamation-triangle': exclamationTriangle,
+//   expand,
+//   eye,
+//   'eye-friendlier': eyeFriendlier,
+//   'eye-slash': eyeSlash,
+//   format,
+//   'font-color': fontColor,
+//   'font-size': fontSize,
+//   gallery,
+//   giphy,
+//   highlight,
+//   'horizontal-rule': horizontalRule,
+//   'image-unavailable': imageUnavailable,
+//   indent,
+//   info,
+//   italic,
+//   leave,
+//   like,
+//   lightning,
+//   link,
+//   lock,
+//   'mark-as-unread': markAsUnread,
+//   'meeting-new': meetingNew,
+//   mention,
+//   menu,
+//   'merge-calls': mergeCalls,
+//   'message-seen': messageSeen,
+//   mic,
+//   'no-presenter': noPresenter,
+//   notes,
+//   'speaker-mute': speakerMute,
+//   hand,
+//   'mic-off': micOff,
+//   more,
+//   'user-phone': userPhone,
+//   'notifications-muted': notificationsMuted,
+//   'number-list': numberList,
+//   'open-outside': openOutside,
+//   outdent,
+//   paperclip,
+//   'participant-add': addParticipant,
+//   'participant-remove': participantRemove,
+//   panorama,
+//   pause,
+//   'pause-thick': pauseThick,
+//   'phone-clock': phoneClock,
+//   'phone-arrow': phoneArrow,
+//   play,
+//   plugs,
+//   poll,
+//   'presence-available': presenceAvailable,
+//   'presence-stroke': presenceStroke,
+//   presenter,
+//   qna,
+//   'question-circle': questionCircle,
+//   quote,
+//   'raise-hand': raiseHand,
+//   'raise-hand-colored': raiseHandColored,
+//   'raise-hand-disabled': raiseHandDisabled,
+//   'read-aloud': readAloud,
+//   redbang,
+//   redo,
+//   'remove-format': removeFormat,
+//   reply,
+//   retry,
+//   robot,
+//   search,
+//   send,
+//   settings,
+//   'settings-audio': settingsAudio,
+//   'share-alt': shareAlt,
+//   'share-location': shareLocation,
+//   'share-to': shareTo,
+//   spotlight,
+//   star,
+//   sticker,
+//   strike,
+//   survey,
+//   'survey-20px': survey20px,
+//   sync,
+//   tabs,
+//   table,
+//   'table-add': tableAdd,
+//   'table-delete': tableDelete,
+//   tag,
+//   teams,
+//   thumbtack,
+//   'thumbtack-slash': thumbtackSlash,
+//   'to-do-list': toDoList,
+//   translation,
+//   'trash-can': trashCan,
+//   'triangle-down': triangleDown,
+//   'triangle-right': triangleRight,
+//   'triangle-up': triangleUp,
+//   'team-create': teamCreate,
+//   underline,
+//   undo,
+//   urgent,
+//   'user-blur': userBlur,
+//   'user-friends': userFriends,
+//   'video-camera-emphasis': videoCameraEmphasis,
+//   volume,
+//   'volume-down': volumeDown,
+//   'volume-up': volumeUp,
+//   'window-maximize': windowMaximize,
+//   'window-minimize': windowMinimize,
+//   'window-restore': windowRestore,
+//
+//   // Office icons
+//   excel: filesExcel,
+//   'excel-color': filesExcelBrand,
+//   onedrive: oneDrive,
+//   onenote: filesOneNote,
+//   'onenote-color': filesOneNoteBrand,
+//   powerpoint: filesPowerPoint,
+//   'powerpoint-color': filesPowerPointBrand,
+//   'skype-logo': skypeLogo,
+//   'microsoft-stream': microsoftStream,
+//   word: filesWord,
+//   'word-color': filesWordBrand,
+//   yammer,
+//   visio: filesVisio,
+//   'visio-color': filesVisioBrand,
+// };
