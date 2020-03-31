8aaf7bac6699ce08b5a39108ffca712805ff4f32
Marija Najdova <mnajdova@gmail.com>
2020-03-31 08:03:05 +0200

    -perf experiments

--- packages/fluentui/docs/src/examples/components/Icon/Performance/IconMinimal.perf.tsx
+++ packages/fluentui/docs/src/examples/components/Icon/Performance/IconMinimal.perf.tsx
@@ -1,7 +1,7 @@
-import { Icon } from '@fluentui/react-northstar';
+import { UserFriends } from '@fluentui/react-icons-northstar';
 import * as React from 'react';
 
-const IconMinimalPerf = () => <Icon name="user-friends" />;
+const IconMinimalPerf = () => <UserFriends />;
 
 IconMinimalPerf.iterations = 5000;
 IconMinimalPerf.filename = 'IconMinimal.perf.tsx';
