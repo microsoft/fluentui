d2bcdda80d7dbe3d67b86c9a8ed9dc36b92bcae4
pompomon <pompomon@gmail.com>
2020-04-01 09:52:36 +0200

    Remove duplicate test

--- packages/fluentui/accessibility/test/behaviors/gridRowBehavior-test.tsx
+++ packages/fluentui/accessibility/test/behaviors/gridRowBehavior-test.tsx
@@ -9,17 +9,6 @@ describe('gridRowBehavior.ts', () => {
     expect(expectedResult.childBehaviors.cell.name).toEqual('gridHeaderCellBehavior');
   });
 
-  test('return aria-selected correctly depending on prop.selected', () => {
-    const props = {
-      selected: true,
-    };
-    const expectedResultSelected = gridRowBehavior(props);
-    expect(expectedResultSelected.attributes.root['aria-selected']).toBeTruthy();
-    props.selected = false;
-    const expectedResultNotSelected = gridRowBehavior(props);
-    expect(expectedResultNotSelected.attributes.root['aria-selected']).toBeFalsy();
-  });
-
   test('use gridRowNestedBehavior if grid row has NOT header prop defined', () => {
     const props = {};
     const expectedResult = gridRowBehavior(props);
