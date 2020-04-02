dc750cf04a51eecd5e96b20b86dfdfc9096e3fa2
caleb <czearing@outlook.com>
2020-04-02 14:15:54 -0700

    Examples update

--- packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.Underlined.Example.tsx
+++ packages/office-ui-fabric-react/src/components/SearchBox/examples/SearchBox.Underlined.Example.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
 // tslint:disable:jsx-no-lambda
-export class SearchBoxUnderlinedExample extends React.Component<any, any> {
-  public render(): JSX.Element {
-    return (
-      <SearchBox
-        placeholder="Search"
-        onFocus={() => console.log('onFocus called')}
-        onBlur={() => console.log('onBlur called')}
-        underlined={true}
-      />
-    );
-  }
-}
+
+export const SearchBoxUnderlinedExample = () => {
+  return (
+    <SearchBox
+      placeholder="Search"
+      onFocus={() => console.log('onFocus called')}
+      onBlur={() => console.log('onBlur called')}
+      underlined={true}
+    />
+  );
+};
