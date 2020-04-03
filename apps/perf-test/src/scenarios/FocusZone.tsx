8ce72d8a1f3e1d5fafb29129a81d6d85cd1af758
Xu Gao <xgao@microsoft.com>
2020-04-03 13:44:18 -0700

    improve focuszone scenario

--- apps/perf-test/src/scenarios/FocusZone.tsx
+++ apps/perf-test/src/scenarios/FocusZone.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { FocusZone } from 'office-ui-fabric-react';
 
-const FocusZoneExample: React.FunctionComponent = () => {
+const FocusZoneContent: React.FunctionComponent = () => {
   const items: React.ReactNode[] = [];
 
   for (let i = 0; i < 20; i++) {
     items.push(<li key={i} data-is-focusable={true} />);
   }
 
-  return <FocusZone as="ul">{items}</FocusZone>;
+  return <>{items}</>;
 };
 
-const scenario = <FocusZoneExample />;
+const scenario = (
+  <FocusZone as="ul">
+    <FocusZoneContent />
+  </FocusZone>
+);
 
 export default scenario;
