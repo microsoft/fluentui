5d4439a484d9a2b23c99698e22a43351f156c67f
Silviu Avram <silviu.avram91@gmail.com>
2020-04-06 19:39:42 +0300

    add unit tests

--- packages/fluentui/react-northstar/test/specs/components/Tooltip/Tooltip-test.tsx
+++ packages/fluentui/react-northstar/test/specs/components/Tooltip/Tooltip-test.tsx
@@ -11,6 +11,26 @@ describe('Tooltip', () => {
     requiredProps: { open: true },
   });
 
+  describe('aria-labelledby', () => {
+    test('is not added on trigger if aria-label is passed', () => {
+      const ariaLabelTestValue = 'test-aria-label';
+      const wrapper = mountWithProvider(<Tooltip defaultOpen trigger={<Button />} aria-label={ariaLabelTestValue} />);
+      const trigger = findIntrinsicElement(wrapper, `.${Tooltip.slotClassNames.trigger}`);
+
+      expect(trigger.getDOMNode()).toHaveAttribute('aria-label', ariaLabelTestValue);
+      expect(trigger.getDOMNode()).not.toHaveAttribute('aria-labelledby');
+    });
+
+    test('is not added on trigger if aria-label is passed to trigger shorthand', () => {
+      const ariaLabelTestValue = 'test-aria-label';
+      const wrapper = mountWithProvider(<Tooltip defaultOpen trigger={<Button aria-label={ariaLabelTestValue} />} />);
+      const trigger = findIntrinsicElement(wrapper, `.${Tooltip.slotClassNames.trigger}`);
+
+      expect(trigger.getDOMNode()).toHaveAttribute('aria-label', ariaLabelTestValue);
+      expect(trigger.getDOMNode()).not.toHaveAttribute('aria-labelledby');
+    });
+  });
+
   describe('content', () => {
     it('uses "id" if "content" with "id" is passed', () => {
       const contentId = 'element-id';
@@ -65,7 +85,7 @@ describe('Tooltip', () => {
       const wrapper = mountWithProvider(<Tooltip trigger={<button />} content="Foo" />);
       expect(wrapper.find('Popper').prop('enabled')).toBe(false);
 
-      wrapper.setProps({ open: true } as any);
+      wrapper.setProps({ open: true });
       expect(wrapper.find('Popper').prop('enabled')).toBe(true);
     });
   });
