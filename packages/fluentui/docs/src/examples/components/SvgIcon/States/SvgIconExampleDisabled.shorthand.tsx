d38f46a652c81669c6e64c52e95543c7e8132b4c
Marija Najdova <mnajdova@gmail.com>
2020-04-06 14:59:31 +0200

    -moved example changes

---
+++ packages/fluentui/docs/src/examples/components/SvgIcon/States/SvgIconExampleDisabled.shorthand.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { CallVideoIcon } from '@fluentui/react-icons-northstar';
+
+const IconExampleDisabled = () => (
+  <div>
+    <CallVideoIcon disabled />
+  </div>
+);
+
+export default IconExampleDisabled;
---
+++ packages/fluentui/docs/src/examples/components/SvgIcon/Usage/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import ComponentExample from '../../../../components/ComponentDoc/ComponentExample';
+import ExampleSection from '../../../../components/ComponentDoc/ExampleSection';
+
+const Usage = () => (
+  <ExampleSection title="Usage">
+    <ComponentExample
+      title="Theme Icons"
+      description="You can access all icon names available in the current theme."
+      examplePath="components/SvgIcon/Usage/SvgIconSetExample"
+    />
+  </ExampleSection>
+);
+
+export default Usage;
