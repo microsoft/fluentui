1b171e1f45e4bef2fc740127f8d7374906e60eef
Silviu Avram <silviu.avram91@gmail.com>
2020-04-07 17:28:05 +0300

    remove confirm button from dialog

--- packages/fluentui/docs/src/examples/components/Tooltip/Usage/TooltipExampleDialogContent.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Tooltip/Usage/TooltipExampleDialogContent.shorthand.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 const DialogExampleContent: React.FC = () => (
   <Dialog
     cancelButton="Cancel"
-    confirmButton="Confirm"
     content={<Tooltip trigger={<Button content="Click me!" />} content="Hello from tooltip!" />}
     header="I have a tooltip!"
     trigger={<Button content="Open a dialog" />}
