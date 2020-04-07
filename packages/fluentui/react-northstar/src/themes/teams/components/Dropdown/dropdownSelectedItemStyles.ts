d69245fa4466a18bc5877db0498d6f86d345df00
Marija Najdova <mnajdova@gmail.com>
2020-04-07 12:25:55 +0200

    -added getBorderFocusStyles

--- packages/fluentui/react-northstar/src/themes/teams/components/Dropdown/dropdownSelectedItemStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Dropdown/dropdownSelectedItemStyles.ts
@@ -5,44 +5,41 @@ import {
 import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
 import { DropdownVariables } from './dropdownVariables';
 import getIconFillOrOutlineStyles from '../../getIconFillOrOutlineStyles';
+import getBorderFocusStyles from '../../getBorderFocusStyles';
 import { pxToRem } from '../../../../utils';
 
 const dropdownSelectedItemStyles: ComponentSlotStylesPrepared<DropdownSelectedItemProps, DropdownVariables> = {
-  root: ({ props: p, variables: v, theme: { siteVariables } }): ICSSInJSStyle => ({
-    cursor: 'pointer',
-    margin: '.25rem 0 0 .4rem',
-    color: v.selectedItemColor,
-    position: 'relative',
-    border: v.selectedItemBorder,
-    height: pxToRem(24),
-    overflow: 'visible',
-    outline: 0,
-    fontWeight: siteVariables.fontWeightSemibold,
-    ...(v.selectedItemBackgroundColor && {
-      backgroundColor: v.selectedItemBackgroundColor,
-    }),
-    ':focus': {
-      color: v.selectedItemColorFocus,
-    },
-    ':hover': {
-      color: v.selectedItemColorHover,
-      backgroundColor: v.selectedItemBackgroundColorHover,
-      [`& .${DropDownSelectedItem.slotClassNames.icon}`]: {
-        color: v.selectedItemIconColorHover,
+  root: ({ props: p, variables: v, theme: { siteVariables } }): ICSSInJSStyle => {
+    const borderFocusStyles = getBorderFocusStyles({ variables: siteVariables });
+
+    return {
+      cursor: 'pointer',
+      margin: '.25rem 0 0 .4rem',
+      color: v.selectedItemColor,
+      position: 'relative',
+      border: v.selectedItemBorder,
+      height: pxToRem(24),
+      overflow: 'visible',
+      outline: 0,
+      fontWeight: siteVariables.fontWeightSemibold,
+      ...(v.selectedItemBackgroundColor && {
+        backgroundColor: v.selectedItemBackgroundColor,
+      }),
+      ':focus': {
+        color: v.selectedItemColorFocus,
       },
-    },
-    ':focus-visible': {
-      '::before': {
-        content: '""',
-        position: 'absolute',
-        left: '-2px',
-        top: '-2px',
-        bottom: '-2px',
-        right: '-2px',
-        border: `${pxToRem(1)} solid ${siteVariables.focusOuterBorderColor}`,
+      ':hover': {
+        color: v.selectedItemColorHover,
+        backgroundColor: v.selectedItemBackgroundColorHover,
+        [`& .${DropDownSelectedItem.slotClassNames.icon}`]: {
+          color: v.selectedItemIconColorHover,
+        },
       },
-    },
-  }),
+      ':focus-visible': {
+        ':after': borderFocusStyles[':focus-visible'][':after'],
+      },
+    };
+  },
   icon: ({ variables: v }) => ({
     color: v.selectedItemIconColor,
     ...getIconFillOrOutlineStyles({ outline: true }),
