e6cd5fb3db63c54a0f422bef555da91cccd19c0e
Michael Majgaard <michaelmajgaard@outlook.com>
2020-04-08 14:33:58 +0200

    fixed max line length in DatePicker story

--- packages/date-time/src/components/DatePicker/examples/DatePicker.Controlled.Example.tsx
+++ packages/date-time/src/components/DatePicker/examples/DatePicker.Controlled.Example.tsx
@@ -5,7 +5,8 @@ import './DatePicker.Examples.scss';
 const min = new Date('2020-04-01');
 const max = new Date('2020-04-03');
 
-const description = `DatePicker controlled using useState with min/max validation. ${min.toLocaleDateString()} - ${max.toLocaleDateString()}.`;
+const description = `DatePicker controlled using useState with min/max validation.
+${min.toLocaleDateString()} - ${max.toLocaleDateString()}.`;
 
 const ControlledDatePicker = () => {
   const [date, setDate] = React.useState<Date | undefined>(new Date('2020-04-03'));
