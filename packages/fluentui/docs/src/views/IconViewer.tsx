0220fb112b987ce7301025a510ff42ec33f4a497
Marija Najdova <mnajdova@gmail.com>
2020-04-10 14:27:00 +0200

    -fixed on the navigation buttons

--- packages/fluentui/docs/src/views/IconViewer.tsx
+++ packages/fluentui/docs/src/views/IconViewer.tsx
@@ -3,6 +3,7 @@ import { SvgIconProps, Input, Flex, Card, Box } from '@fluentui/react-northstar'
 import * as exports from '@fluentui/react-icons-northstar';
 import * as _ from 'lodash';
 import { CopyToClipboard } from '../../../docs-components/src';
+import GuidesNavigationFooter from '../components/GuidesNavigationFooter';
 
 const iconFlexStyles = {
   '> *:nth-child(5n)': {
@@ -31,55 +32,63 @@ const IconViewer = () => {
   const filteredIcons = icons.filter(IconComponent => regexQuery.test(IconComponent.displayName));
 
   return (
-    <Flex column gap="gap.medium" padding="padding.medium">
-      <Input
-        inverted
-        fluid
-        clearable
-        placeholder={`Search ${icons.length} icons`}
-        iconPosition="end"
-        role="search"
-        onChange={handleQueryChange}
-        value={query}
-      />
-      <Flex wrap gap="gap.medium" styles={iconFlexStyles}>
-        {filteredIcons.map(Icon => (
-          <Flex.Item align="center" key={Icon.displayName}>
-            <Card
-              centered
-              variables={{ borderColor: '#f2f2f2', width: 'calc(20% - 12px)' }}
-              styles={{ marginTop: '10px', background: 'white' }}
-            >
-              <Card.Header>
-                <Flex gap="gap.small" column hAlign="center">
-                  <Icon size="largest" />
-                  <br />
-                  <CopyToClipboard value={`<${Icon.displayName} />`} timeout={3000}>
-                    {(active, onClick) => (
-                      <Box
-                        as="code"
-                        onClick={onClick}
-                        styles={{
-                          fontSize: '10px !improtant',
-                          opacity: active ? 1 : 0.6,
-                          color: active ? 'green' : 'inherit',
-                          cursor: 'pointer',
-                          ...(!active && {
-                            ':hover': {
-                              opacity: 0.75,
-                            },
-                          }),
-                        }}
-                      >
-                        {active ? 'Copied! Happy coding :)' : `<${Icon.displayName} />`}
-                      </Box>
-                    )}
-                  </CopyToClipboard>
-                </Flex>
-              </Card.Header>
-            </Card>
-          </Flex.Item>
-        ))}
+    <Flex column gap="gap.medium">
+      <Flex column gap="gap.medium" padding="padding.medium">
+        <Input
+          inverted
+          fluid
+          clearable
+          placeholder={`Search ${icons.length} icons`}
+          iconPosition="end"
+          role="search"
+          onChange={handleQueryChange}
+          value={query}
+        />
+        <Flex wrap gap="gap.medium" styles={iconFlexStyles}>
+          {filteredIcons.map(Icon => (
+            <Flex.Item align="center" key={Icon.displayName}>
+              <Card
+                centered
+                variables={{ borderColor: '#f2f2f2', width: 'calc(20% - 12px)' }}
+                styles={{ marginTop: '10px', background: 'white' }}
+              >
+                <Card.Header>
+                  <Flex gap="gap.small" column hAlign="center">
+                    <Icon size="largest" />
+                    <br />
+                    <CopyToClipboard value={`<${Icon.displayName} />`} timeout={3000}>
+                      {(active, onClick) => (
+                        <Box
+                          as="code"
+                          onClick={onClick}
+                          styles={{
+                            fontSize: '10px !improtant',
+                            opacity: active ? 1 : 0.6,
+                            color: active ? 'green' : 'inherit',
+                            cursor: 'pointer',
+                            ...(!active && {
+                              ':hover': {
+                                opacity: 0.75,
+                              },
+                            }),
+                          }}
+                        >
+                          {active ? 'Copied! Happy coding :)' : `<${Icon.displayName} />`}
+                        </Box>
+                      )}
+                    </CopyToClipboard>
+                  </Flex>
+                </Card.Header>
+              </Card>
+            </Flex.Item>
+          ))}
+        </Flex>
+      </Flex>
+      <Flex padding="padding.medium">
+        <GuidesNavigationFooter
+          previous={{ name: 'Shorthand Props', url: 'shorthand-props' }}
+          next={{ name: 'Component Architecture', url: 'component-architecture' }}
+        />
       </Flex>
     </Flex>
   );
