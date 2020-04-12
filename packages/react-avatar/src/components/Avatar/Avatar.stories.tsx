cb3bf98bbc1bc081e3a584c8a4608257e1cc52f4
David Zearing <dzearing@microsoft.com>
2020-04-12 13:45:03 -0700

    Adding PersonaCoin example.

--- packages/react-avatar/src/components/Avatar/Avatar.stories.tsx
+++ packages/react-avatar/src/components/Avatar/Avatar.stories.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
 import { Avatar } from './Avatar';
-import { Status } from '../Status/Status';
-import {
-  Provider,
-  themes,
-  Header,
-  Text,
-  Avatar as FUIAvatar,
-  Status as FUIStatus,
-  Flex,
-} from '@fluentui/react-northstar';
+import { PersonaCoin, PersonaSize, PersonaPresence } from 'office-ui-fabric-react/lib/Persona';
+import { Provider, themes, Header, Avatar as FUIAvatar, Flex } from '@fluentui/react-northstar';
+
+const imageUrl = 'http://www.fillmurray.com/200/200';
 
 export const AvatarFela = () => (
   <Provider theme={themes.teams}>
@@ -32,25 +26,126 @@ export const AvatarFela = () => (
         <FUIAvatar square size="largest" name="Lorem Ipsum" status="warning" />
       </Flex>
       <Flex gap="gap.small">
-        <FUIAvatar size="smallest" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar size="smaller" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar size="small" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar size="large" name="Jane Doe" status="error" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar size="larger" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar size="largest" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square size="smallest" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square size="smaller" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square size="small" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square size="large" name="Jane Doe" status="error" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar square size="larger" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <FUIAvatar
-          square
-          size="largest"
-          name="Lorem Ipsum"
-          status="warning"
-          image="http://www.fillmurray.com/200/200"
+        <FUIAvatar size="smallest" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar size="smaller" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar size="small" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar size="large" name="Jane Doe" status="error" image={imageUrl} />
+        <FUIAvatar size="larger" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <FUIAvatar size="largest" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <FUIAvatar square size="smallest" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar square size="smaller" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar square size="small" name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar square name="John Doe" status="success" image={imageUrl} />
+        <FUIAvatar square size="large" name="Jane Doe" status="error" image={imageUrl} />
+        <FUIAvatar square size="larger" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <FUIAvatar square size="largest" name="Lorem Ipsum" status="warning" image={imageUrl} />
+      </Flex>
+    </Flex>
+  </Provider>
+);
+
+const squareStyles = {
+  initials: { borderRadius: 3 },
+  image: { borderRadius: 3 },
+};
+
+export const AvatarMergeStyles = () => (
+  <Provider theme={themes.teams}>
+    <Header>PersonaCoin (css)</Header>
+    <Flex column gap="gap.small">
+      <Flex gap="gap.small">
+        <PersonaCoin size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin size={PersonaSize.size32} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.busy} />
+        <PersonaCoin size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.away} />
+        <PersonaCoin size={PersonaSize.size48} text="John Doe" presence={PersonaPresence.away} />
+        <PersonaCoin
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          styles={squareStyles}
+          size={PersonaSize.size32}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin styles={squareStyles} size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.busy} />
+        <PersonaCoin styles={squareStyles} size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.away} />
+        <PersonaCoin styles={squareStyles} size={PersonaSize.size48} text="John Doe" presence={PersonaPresence.away} />
+      </Flex>
+      <Flex gap="gap.small">
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size24} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size32} text="John Doe" presence={PersonaPresence.online} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.busy} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size40} text="John Doe" presence={PersonaPresence.away} />
+        <PersonaCoin imageUrl={imageUrl} size={PersonaSize.size48} text="John Doe" presence={PersonaPresence.away} />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size24}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size32}
+          text="John Doe"
+          presence={PersonaPresence.online}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size40}
+          text="John Doe"
+          presence={PersonaPresence.busy}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size40}
+          text="John Doe"
+          presence={PersonaPresence.away}
+        />
+        <PersonaCoin
+          imageUrl={imageUrl}
+          styles={squareStyles}
+          size={PersonaSize.size48}
+          text="John Doe"
+          presence={PersonaPresence.away}
         />
       </Flex>
     </Flex>
@@ -78,20 +173,20 @@ export const AvatarCss = () => (
         <Avatar square size="largest" name="Lorem Ipsum" status="warning" />
       </Flex>
       <Flex gap="gap.small">
-        <Avatar size="smallest" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar size="smaller" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar size="small" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar size="large" name="Jane Doe" status="error" image="http://www.fillmurray.com/200/200" />
-        <Avatar size="larger" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <Avatar size="largest" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="smallest" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="smaller" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="small" name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar square name="John Doe" status="success" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="large" name="Jane Doe" status="error" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="larger" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
-        <Avatar square size="largest" name="Lorem Ipsum" status="warning" image="http://www.fillmurray.com/200/200" />
+        <Avatar size="smallest" name="John Doe" status="success" image={imageUrl} />
+        <Avatar size="smaller" name="John Doe" status="success" image={imageUrl} />
+        <Avatar size="small" name="John Doe" status="success" image={imageUrl} />
+        <Avatar name="John Doe" status="success" image={imageUrl} />
+        <Avatar size="large" name="Jane Doe" status="error" image={imageUrl} />
+        <Avatar size="larger" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <Avatar size="largest" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <Avatar square size="smallest" name="John Doe" status="success" image={imageUrl} />
+        <Avatar square size="smaller" name="John Doe" status="success" image={imageUrl} />
+        <Avatar square size="small" name="John Doe" status="success" image={imageUrl} />
+        <Avatar square name="John Doe" status="success" image={imageUrl} />
+        <Avatar square size="large" name="Jane Doe" status="error" image={imageUrl} />
+        <Avatar square size="larger" name="Lorem Ipsum" status="warning" image={imageUrl} />
+        <Avatar square size="largest" name="Lorem Ipsum" status="warning" image={imageUrl} />
       </Flex>
     </Flex>
   </Provider>
