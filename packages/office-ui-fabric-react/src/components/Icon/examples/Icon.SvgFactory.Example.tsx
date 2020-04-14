89ea0146d018c35b3002395ffce0d7436322785b
Marija Najdova <mnajdova@gmail.com>
2020-04-14 09:34:12 +0200

    -reverted changes in svg factory -reverted removed icons

--- packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as ReactIcons from '@fluentui/react-icons';
+import { OneDriveIcon, YammerIcon, BorderBlindsIcon } from '@fluentui/react-icons';
 import { mergeStyles } from 'office-ui-fabric-react/lib/Styling';
-import Button from '@uifabric/experiments/src';
 
 const rootClass = mergeStyles({
+  display: 'flex',
   selectors: {
     '> *': {
       height: 50,
@@ -13,63 +13,12 @@ const rootClass = mergeStyles({
   },
 });
 
-const cellClass = mergeStyles({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  margin: '80px',
-  float: 'left',
-});
-
-const codeClass = mergeStyles({
-  background: '#f2f2f2',
-  borderRadius: '4px',
-  padding: '4px',
-});
-
-const navigationTextClasses = mergeStyles({
-  width: 100,
-  margin: '0 5px',
-});
-
-const icons = Object.keys(ReactIcons).reduce((acc: React.FC[], exportName) => {
-  if (!!ReactIcons[exportName].displayName) {
-    acc.push(ReactIcons[exportName]);
-  }
-
-  return acc;
-}, []);
-
-const numOfIcons = icons.length;
-const numOfPages = parseInt((numOfIcons / 100).toString(), 10) + 1;
-
 export const IconSvgFactoryExample: React.FunctionComponent = () => {
-  const [page, setPage] = React.useState(1);
-  const nextPage = () => setPage(page + 1);
-  const prevPage = () => setPage(page - 1);
-
   return (
-    <div>
-      <div>
-        <Button onClick={prevPage} disabled={page === 1}>
-          Prev
-        </Button>
-        <span className={navigationTextClasses}>
-          Page {page} of {numOfPages}
-        </span>
-        <Button onClick={nextPage} disabled={page === numOfPages}>
-          Next
-        </Button>
-      </div>
-      <div className={rootClass}>
-        {icons.slice((page - 1) * 100, (page - 1) * 100 + 100).map(Icon => (
-          <div key={Icon.displayName} className={cellClass}>
-            <Icon />
-            <br />
-            <code className={codeClass}>{Icon.displayName}</code>
-          </div>
-        ))}
-      </div>
+    <div className={rootClass}>
+      <OneDriveIcon />
+      <YammerIcon />
+      <BorderBlindsIcon color3="pink" />
     </div>
   );
 };
