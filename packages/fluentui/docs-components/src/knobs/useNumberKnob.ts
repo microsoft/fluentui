fd74f24d7f90a7f8602e7836e36100dad2ac156e
Charles Assuncao <chassunc@microsoft.com>
2020-04-14 10:09:12 +0200

    chore(Knob): change interface props

--- packages/fluentui/docs-components/src/knobs/useNumberKnob.ts
+++ packages/fluentui/docs-components/src/knobs/useNumberKnob.ts
@@ -2,15 +2,15 @@ import { UseKnobOptions } from './types';
 import useKnob from './useKnob';
 
 type UseNumberKnobSpecificOptions = {
-  min?: number;
-  max?: number;
-  step?: number;
+  min?: string;
+  max?: string;
+  step?: string;
 };
 
 type UseNumberKnobOptions<T> = UseKnobOptions<T> & UseNumberKnobSpecificOptions;
 
 const useStringKnob = (options: UseNumberKnobOptions<number>) => {
-  const { initialValue = 0, min = 0, max = undefined, step = 1, ...rest } = options;
+  const { initialValue = parse, min = 0, max = undefined, step = 1, ...rest } = options;
 
   return useKnob<number, Required<UseNumberKnobSpecificOptions>>({
     initialValue,
