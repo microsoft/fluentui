3e285c019f9b1a550d386a7638e4b124575e332c
Charles Assuncao <chassunc@microsoft.com>
2020-04-14 09:30:49 +0200

    Fix(Dialog) Review changes

--- packages/fluentui/e2e/tests/dialogPreventScroll-test.ts
+++ packages/fluentui/e2e/tests/dialogPreventScroll-test.ts
@@ -6,8 +6,7 @@ const innerClose = `#${selectors.innerClose}`;
 const innerTrigger = `#${selectors.innerTrigger}`;
 
 const hiddenOverflow = 'overflow: hidden';
-const unsetOverflow = 'overflow: unset;';
-const dialogsCounterAttribute = 'fluent-dialogs-count';
+const unsetOverflow = 'overflow: unset';
 
 describe('Dialog scroll', () => {
   beforeEach(async () => {
@@ -17,42 +16,33 @@ describe('Dialog scroll', () => {
   it('should prevent scroll on the body when dialog is open', async () => {
     await e2e.clickOn(outerTrigger);
     const bodyStyles = await e2e.getAttributeValue('body', 'style');
-    const bodyCounter = await e2e.getAttributeValue('body', dialogsCounterAttribute);
     expect(bodyStyles).toContain(hiddenOverflow);
-    expect(bodyCounter).toBe('1');
   });
 
   it('should increment the dialogs counter in the body attribute', async () => {
     await e2e.clickOn(outerTrigger);
     await e2e.clickOn(innerTrigger);
     const bodyStyles = await e2e.getAttributeValue('body', 'style');
-    const bodyCounter = await e2e.getAttributeValue('body', dialogsCounterAttribute);
     expect(bodyStyles).toContain(hiddenOverflow);
-    expect(bodyCounter).toBe('2');
   });
 
   it('should decrement the dialogs counter in the body attribute', async () => {
     await e2e.clickOn(outerTrigger);
     await e2e.clickOn(innerTrigger);
     const bodyStyles = await e2e.getAttributeValue('body', 'style');
-    const bodyCounter = await e2e.getAttributeValue('body', dialogsCounterAttribute);
+
     expect(bodyStyles).toContain(hiddenOverflow);
-    expect(bodyCounter).toBe('2');
     await e2e.clickOn(innerClose);
-    const bodyCounterAfterCloseInner = await e2e.getAttributeValue('body', dialogsCounterAttribute);
-    expect(bodyCounterAfterCloseInner).toBe('1');
   });
 
   it('should reset overflow and reset the dialogs counter', async () => {
     await e2e.clickOn(outerTrigger);
     await e2e.clickOn(innerTrigger);
     const bodyStyles = await e2e.getAttributeValue('body', 'style');
-    const bodyCounter = await e2e.getAttributeValue('body', dialogsCounterAttribute);
     expect(bodyStyles).toContain(hiddenOverflow);
-    expect(bodyCounter).toBe('2');
     await e2e.clickOn(innerClose);
     await e2e.clickOn(outerClose);
     const bodyStylesAfterCloseInner = await e2e.getAttributeValue('body', 'style');
-    expect(bodyStylesAfterCloseInner).toBe(unsetOverflow);
+    expect(bodyStylesAfterCloseInner).toContain(unsetOverflow);
   });
 });
