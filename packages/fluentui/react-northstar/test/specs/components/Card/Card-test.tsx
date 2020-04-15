dbddd420ec0f80f3342706cd7670ac6100244fe5
pompomon <pompomon@gmail.com>
2020-04-15 17:07:24 +0200

    Add OnClick test

--- packages/fluentui/react-northstar/test/specs/components/Card/Card-test.tsx
+++ packages/fluentui/react-northstar/test/specs/components/Card/Card-test.tsx
@@ -1,10 +1,27 @@
-import { isConformant, handlesAccessibility } from 'test/specs/commonTests';
+import * as React from 'react';
 import Card from 'src/components/Card/Card';
+import { handlesAccessibility, isConformant } from 'test/specs/commonTests';
+import { mountWithProviderAndGetComponent } from 'test/utils';
 
 describe('Card', () => {
   isConformant(Card, { constructorName: 'Card' });
 
   describe('accessiblity', () => {
     handlesAccessibility(Card, { defaultRootRole: 'group' });
+  });
+
+  describe('onClick', () => {
+    test('is called with (e, props) on a click', () => {
+      const onClick = jest.fn();
+      const card = mountWithProviderAndGetComponent(Card, <Card onClick={onClick} />);
+
+      card.simulate('click');
+
+      expect(onClick).toHaveBeenCalledTimes(1);
+      expect(onClick).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'click' }),
+        expect.objectContaining({ onClick }),
+      );
+    });
   });
 });
