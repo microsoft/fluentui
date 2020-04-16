26ff20479eeb62c9bafab92f1532a70c87f4bfe8
Marija Najdova <mnajdova@gmail.com>
2020-04-16 07:30:30 +0200

    Update packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
    Co-Authored-By: David Zearing <dzearing@microsoft.com>

--- packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
@@ -33,7 +33,7 @@ const classes = mergeStyleSets({
 
 const icons = Object.keys(ReactIcons).reduce((acc: React.FC[], exportName) => {
   if (!!(ReactIcons as any)[exportName].displayName) {
-    acc.push((ReactIcons as any)[exportName] as React.FC);
+    acc.push((ReactIcons as any)[exportName] as React.FunctionComponent);
   }
 
   return acc;
