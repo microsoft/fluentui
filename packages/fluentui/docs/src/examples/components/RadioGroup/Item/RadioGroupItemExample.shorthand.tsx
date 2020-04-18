7200f2c3d97dd87299e4ce770d7ab369992bb939
Charles Assuncao <chassunc@microsoft.com>
2020-04-18 02:35:41 +0200

    chore(RadioGroup): remove checkedChanged from examples since it is not a prop from RadioGroupItem

--- packages/fluentui/docs/src/examples/components/RadioGroup/Item/RadioGroupItemExample.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/RadioGroup/Item/RadioGroupItemExample.shorthand.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import { RadioGroup } from '@fluentui/react-northstar';
 
-const handleChange = () => {
-  alert('The radio checked value was changed!');
-};
-
 const RadioGroupItemExample = () => (
   <RadioGroup
     items={[
-      { key: '1', label: 'Make your choice', value: '1', checkedChanged: handleChange },
-      { key: '2', label: 'Another option', value: '2', checkedChanged: handleChange },
+      { key: '1', label: 'Make your choice', value: '1' },
+      { key: '2', label: 'Another option', value: '2' },
     ]}
   />
 );
