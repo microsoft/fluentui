09725addb1542c27219b87c1c43d19a386f9ebca
Charles Assuncao <chassunc@microsoft.com>
2020-04-21 23:13:35 +0200

    chore(Video): Fix Video Specification Behavior

--- packages/fluentui/accessibility/src/behaviors/Video/videoBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Video/videoBehavior.ts
@@ -1,13 +1,8 @@
 import { Accessibility } from '../../types';
 
 /**
- * @description
- * Implements ARIA Radio Group design pattern.
  * @specification
- * Adds role='application'. This allows screen readers to handle the component as
- * a structure containing one or more focusable elements requiring user input, such as
- * keyboard or gesture events, that do not follow a standard interaction pattern supported
- * by a widget role.
+ * Adds role 'application' to 'root' slot
  */
 const videoBehavior: Accessibility = () => ({
   attributes: {
