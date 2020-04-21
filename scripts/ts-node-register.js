96f01407581e87eb3b6951af690cf147567a8da9
Elizabeth Craig <elcraig@microsoft.com>
2020-04-21 01:10:03 -0700

    use type checks

--- scripts/ts-node-register.js
+++ scripts/ts-node-register.js
@@ -1,6 +1,3 @@
-// Register ts-node so that it uses the scripts directory's tsconfig and only transpiles
+// Register ts-node so that it uses the scripts directory's tsconfig
 const tsNode = require('ts-node');
-tsNode.register({
-  dir: __dirname,
-  transpileOnly: true,
-});
+tsNode.register({ dir: __dirname });
