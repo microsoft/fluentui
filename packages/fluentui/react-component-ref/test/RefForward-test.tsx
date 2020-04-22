22c62424ac4f3079852c2dd90d55e5dfb5b30fd5
Marija Najdova <mnajdova@gmail.com>
2020-04-22 14:08:00 +0200

    -added more tests

--- packages/fluentui/react-component-ref/test/RefForward-test.tsx
+++ packages/fluentui/react-component-ref/test/RefForward-test.tsx
@@ -16,6 +16,26 @@ describe('RefForward', () => {
       expect(innerRef.current).toBeInstanceOf(Element);
     });
 
+    it('passes an updated node', () => {
+      const innerRef = jest.fn();
+      const forwardedRef = jest.fn();
+      const updatedForwardedRef = jest.fn();
+
+      const wrapper = mount(
+        <RefForward innerRef={innerRef}>{(<ForwardedRef ref={forwardedRef} key="first" />) as any}</RefForward>,
+      );
+
+      expect(innerRef).toHaveBeenCalledWith(expect.objectContaining({ tagName: 'BUTTON' }));
+      expect(forwardedRef).toHaveBeenCalledWith(expect.objectContaining({ tagName: 'BUTTON' }));
+
+      jest.resetAllMocks();
+      wrapper.setProps({ children: <ForwardedRef ref={updatedForwardedRef} key="second" /> });
+
+      expect(innerRef).toHaveBeenCalledWith(expect.objectContaining({ tagName: 'BUTTON' }));
+      expect(updatedForwardedRef).toHaveBeenCalledWith(expect.objectContaining({ tagName: 'BUTTON' }));
+      expect(forwardedRef).not.toHaveBeenCalled();
+    });
+
     it('handles updates of props', () => {
       const initialRef = jest.fn();
       const updatedRef = jest.fn();
