68b1739fc067dac924678fb243ee493e6d2bc8fa
Silviu Avram <silviu.avram91@gmail.com>
2020-04-23 17:15:10 +0300

    add unit test specification

--- packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemCheckboxBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemCheckboxBehavior.ts
@@ -3,10 +3,11 @@ import { Accessibility } from '../../types';
 
 /**
  * @specification
- *  Adds attribute 'aria-checked=true' based on the property 'active'.
- *  Adds attribute 'aria-disabled=true' based on the property 'disabled'.
- *  Adds role='menuitemcheckbox'.
+ * Adds attribute 'aria-checked=true' based on the property 'active'.
+ * Adds attribute 'aria-disabled=true' based on the property 'disabled'.
+ * Adds role='menuitemcheckbox'.
  * Adds role 'presentation' to 'wrapper' slot.
+ * Triggers 'performClick' action with 'Enter' or 'Spacebar' on 'root'.
  */
 const toolbarMenuItemCheckboxBehavior: Accessibility<ToolbarMenuItemCheckboxBehaviorProps> = props => ({
   attributes: {
