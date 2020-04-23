77b17b93ead149d714d770d70e725dfd166c5ff5
Xu Gao <xgao@microsoft.com>
2020-04-23 11:31:52 -0700

    add perf-test for rerender

--- apps/perf-test/src/index.scenarios.tsx
+++ apps/perf-test/src/index.scenarios.tsx
@@ -17,11 +17,36 @@ const defaultIterations = 10;
 const queryParams = qs.parse(window.location.search.substring(1));
 const iterations = queryParams.iterations ? parseInt(queryParams.iterations as string, 10) : defaultIterations;
 const scenario = queryParams.scenario ? (queryParams.scenario as string) : defaultScenarioName;
+const checkRerender: boolean = !!queryParams.rerender;
 
-// TODO: This seems to increase React (unstable_runWithPriority) render consumption from 4% to 72%!
-// const ScenarioContent = Array.from({ length: iterations }, () => scenarios[scenario]);
+const PerfTestScenario = scenarios[scenario];
 
-// TODO: Using React Fragments increases React (unstable_runWithPriority) render consumption from 4% to 26%.
-// It'd be interesting to root cause why at some point.
-// ReactDOM.render(<>{Array.from({ length: iterations }, () => (scenarios[scenario]))}</>, div);
-ReactDOM.render(<div>{Array.from({ length: iterations }, () => scenarios[scenario])}</div>, div);
+if (checkRerender) {
+  const PerfTestScenarioWrapper: React.FunctionComponent = () => {
+    const [renderCount, setRenderCounter] = React.useState(0);
+    React.useEffect(() => {
+      if (renderCount < iterations) {
+        setRenderCounter(renderCount + 1);
+      }
+    }, [renderCount]);
+
+    return <PerfTestScenario />;
+  };
+
+  ReactDOM.render(<PerfTestScenarioWrapper />, div);
+} else {
+  // TODO: This seems to increase React (unstable_runWithPriority) render consumption from 4% to 72%!
+  // const ScenarioContent = Array.from({ length: iterations }, () => scenarios[scenario]);
+
+  // TODO: Using React Fragments increases React (unstable_runWithPriority) render consumption from 4% to 26%.
+  // It'd be interesting to root cause why at some point.
+  // ReactDOM.render(<>{Array.from({ length: iterations }, () => (scenarios[scenario]))}</>, div);
+  ReactDOM.render(
+    <div>
+      {Array.from({ length: iterations }, () => (
+        <PerfTestScenario />
+      ))}
+    </div>,
+    div,
+  );
+}
