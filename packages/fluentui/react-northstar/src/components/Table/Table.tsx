9dca77e7f24182a219d5e30e5a0562c73cb78c45
Charles Assuncao <charles.assuncao@microsoft.com>
2020-04-24 10:54:13 +0200

    chore(Table): fix PropType

--- packages/fluentui/react-northstar/src/components/Table/Table.tsx
+++ packages/fluentui/react-northstar/src/components/Table/Table.tsx
@@ -161,7 +161,11 @@ Table.create = createShorthandFactory({
 });
 
 Table.propTypes = {
-  ...commonPropTypes.createCommon({ content: 'shorthand' }),
+  ...commonPropTypes.createCommon({ content: false }),
+  content: customPropTypes.every([
+    customPropTypes.disallow(['children']),
+    PropTypes.oneOfType([PropTypes.arrayOf(customPropTypes.nodeContent), customPropTypes.nodeContent]),
+  ]),
   header: customPropTypes.itemShorthand,
   rows: customPropTypes.collectionShorthand,
   compact: PropTypes.bool,
