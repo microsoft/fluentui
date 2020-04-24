bc1e9099bd6b2a9a4b7fb2dabb032472cb0eb79b
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2020-04-24 08:09:48 +0300

    fix(ToolbarMenuItem): execute click on Enter and Spaceabar for checked version (#12837)
    * add performClick to behavior
    
    * add unit test specification
    
    * fixing screen reader exprience to narrate checked state
    
    Co-authored-by: Milan Turon <mituron@microsoft.com>

--- packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemCheckboxBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemCheckboxBehavior.ts
@@ -1,11 +1,13 @@
+import * as keyboardKey from 'keyboard-key';
 import { Accessibility } from '../../types';
 
 /**
  * @specification
- *  Adds attribute 'aria-checked=true' based on the property 'active'.
- *  Adds attribute 'aria-disabled=true' based on the property 'disabled'.
- *  Adds role='menuitemcheckbox'.
+ * Adds attribute 'aria-checked=true' based on the property 'active'.
+ * Adds attribute 'aria-disabled=true' based on the property 'disabled'.
+ * Adds role='menuitemcheckbox'.
  * Adds role 'presentation' to 'wrapper' slot.
+ * Triggers 'performClick' action with 'Enter' or 'Spacebar' on 'root'.
  */
 const toolbarMenuItemCheckboxBehavior: Accessibility<ToolbarMenuItemCheckboxBehaviorProps> = props => ({
   attributes: {
@@ -16,6 +18,13 @@ const toolbarMenuItemCheckboxBehavior: Accessibility<ToolbarMenuItemCheckboxBeha
       'aria-checked': props.active,
       'aria-disabled': props.disabled,
       role: 'menuitemcheckbox',
+    },
+  },
+  keyActions: {
+    root: {
+      performClick: {
+        keyCombinations: [{ keyCode: keyboardKey.Enter }, { keyCode: keyboardKey.Spacebar }],
+      },
     },
   },
 });
