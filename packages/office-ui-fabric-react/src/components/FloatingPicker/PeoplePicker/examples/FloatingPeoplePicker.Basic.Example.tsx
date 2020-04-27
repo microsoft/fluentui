bb99c72df294b1bde65c25c751bab25410ec535b
Caleb Zearing <czearing@outlook.com>
2020-04-27 15:47:15 -0700

    Updating examples for ProgressIndicator, FloatingPeoplePicker and ResizeGroupOverflowSetExample to use FC

--- packages/office-ui-fabric-react/src/components/FloatingPicker/PeoplePicker/examples/FloatingPeoplePicker.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/FloatingPicker/PeoplePicker/examples/FloatingPeoplePicker.Basic.Example.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import { IPersonaProps } from 'office-ui-fabric-react/lib/Persona';
 import {
   IBaseFloatingPicker,
@@ -10,128 +9,95 @@ import {
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 import { people } from '@uifabric/example-data';
 
-export interface IPeoplePickerExampleState {
-  peopleList: IPersonaProps[];
-  searchValue: string;
-}
-
-export class FloatingPeoplePickerTypesExample extends React.Component<{}, IPeoplePickerExampleState> {
-  private _picker = React.createRef<IBaseFloatingPicker>();
-  private _inputElement: HTMLInputElement;
-
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      peopleList: people,
-      searchValue: '',
-    };
+let inputElement: HTMLInputElement;
+const suggestionsStore = new SuggestionsStore<IPersonaProps>();
+const searchBoxWrapperStyling = { width: 208 };
+const getTextFromItem = (persona: IPersonaProps): string => {
+  return persona.text || '';
+};
+const listContainsPersona = (persona: IPersonaProps, personas?: IPersonaProps[]): boolean => {
+  return !!personas && personas.some((item: IPersonaProps) => item.text === persona.text);
+};
+const validateInput = (input: string): boolean => {
+  return input.indexOf('@') !== -1;
+};
+const startsWith = (text: string, filterText: string): boolean => {
+  return text.toLowerCase().indexOf(filterText.toLowerCase()) === 0;
+};
+const setInputElementRef = (ref: HTMLDivElement | null) => {
+  const inputElements = ref && ref.getElementsByClassName('ms-SearchBox-field');
+  if (inputElements && inputElements.length > 0) {
+    inputElement = inputElements[0] as HTMLInputElement;
   }
+};
 
-  public render(): JSX.Element {
-    const suggestionProps: IBaseFloatingPickerSuggestionProps = {
-      footerItemsProps: [
-        {
-          renderItem: () => {
-            const picker = this._picker.current;
-            return <div>Showing {picker ? picker.suggestions.length : 0} results</div>;
-          },
-          shouldShow: () => {
-            const picker = this._picker.current;
-            return !!picker && picker.suggestions.length > 0;
-          },
-        },
-      ],
-    };
+export const FloatingPeoplePickerTypesExample: React.FunctionComponent = () => {
+  const [peopleList, setPeopleList] = React.useState(people);
+  const [searchValue, setSearchValue] = React.useState('');
+  const picker = React.useRef<IBaseFloatingPicker>(null);
 
-    return (
-      <div>
-        <div style={{ width: 208 }} ref={this._setInputElementRef}>
-          <SearchBox
-            placeholder="Search for person"
-            onChange={this._onSearchChange}
-            value={this.state.searchValue}
-            onFocus={this._onFocus}
-          />
-        </div>
-        <FloatingPeoplePicker
-          suggestionsStore={new SuggestionsStore<IPersonaProps>()}
-          onResolveSuggestions={this._onFilterChanged}
-          getTextFromItem={this._getTextFromItem}
-          pickerSuggestionsProps={suggestionProps}
-          key="normal"
-          onRemoveSuggestion={this._onRemoveSuggestion}
-          onValidateInput={this._validateInput}
-          componentRef={this._picker}
-          onChange={this._onPickerChange}
-          inputElement={this._inputElement}
-          resolveDelay={300}
-        />
-      </div>
-    );
-  }
-
-  private _onFocus = (): void => {
-    if (this._picker.current) {
-      this._picker.current.showPicker();
+  const onFocus = (): void => {
+    if (picker.current) {
+      picker.current.showPicker();
     }
   };
-
-  private _setInputElementRef = (ref: HTMLDivElement | null) => {
-    const inputElements = ref && ref.getElementsByClassName('ms-SearchBox-field');
-    if (inputElements && inputElements.length > 0) {
-      this._inputElement = inputElements[0] as HTMLInputElement;
+  const onSearchChange = (ev: React.ChangeEvent<HTMLInputElement>, newValue: string): void => {
+    if (newValue !== searchValue && picker.current) {
+      setSearchValue(newValue);
+      picker.current.onQueryStringChanged(newValue);
     }
   };
-
-  private _onSearchChange = (ev: React.ChangeEvent<HTMLInputElement>, newValue: string): void => {
-    if (newValue !== this.state.searchValue && this._picker.current) {
-      this.setState({ searchValue: newValue });
-      this._picker.current.onQueryStringChanged(newValue);
+  const onPickerChange = (selectedSuggestion: IPersonaProps): void => {
+    setSearchValue(selectedSuggestion.text || '');
+    if (picker.current) {
+      picker.current.hidePicker();
     }
   };
-
-  private _onPickerChange = (selectedSuggestion: IPersonaProps): void => {
-    this.setState({ searchValue: selectedSuggestion.text || '' });
-    if (this._picker.current) {
-      this._picker.current.hidePicker();
-    }
-  };
-
-  private _onRemoveSuggestion = (item: IPersonaProps): void => {
-    const { peopleList } = this.state;
+  const onRemoveSuggestion = (item: any): void => {
     const itemIndex = peopleList.indexOf(item);
-
     if (itemIndex >= 0) {
-      this.setState({
-        peopleList: peopleList.slice(0, itemIndex).concat(peopleList.slice(itemIndex + 1)),
-      });
+      setPeopleList(peopleList.slice(0, itemIndex).concat(peopleList.slice(itemIndex + 1)));
     }
   };
-
-  private _onFilterChanged = (filterText: string, currentPersonas?: IPersonaProps[]): IPersonaProps[] => {
+  const suggestionProps: IBaseFloatingPickerSuggestionProps = {
+    footerItemsProps: [
+      {
+        renderItem: () => {
+          return <>Showing {picker.current ? picker.current.suggestions.length : 0} results</>;
+        },
+        shouldShow: () => {
+          return !!picker.current && picker.current.suggestions.length > 0;
+        },
+      },
+    ],
+  };
+  const onFilterChanged = (filterText: string, currentPersonas?: IPersonaProps[]): IPersonaProps[] => {
     if (filterText) {
       // Filter by items starting with the current filter text, then remove duplicates
-      return this.state.peopleList
-        .filter((item: IPersonaProps) => _startsWith(item.text || '', filterText))
-        .filter((persona: IPersonaProps) => !this._listContainsPersona(persona, currentPersonas));
+      return peopleList
+        .filter((item: IPersonaProps) => startsWith(item.text || '', filterText))
+        .filter((persona: IPersonaProps) => !listContainsPersona(persona, currentPersonas));
     }
     return [];
   };
-
-  private _getTextFromItem = (persona: IPersonaProps): string => {
-    return persona.text || '';
-  };
-
-  private _listContainsPersona(persona: IPersonaProps, personas?: IPersonaProps[]): boolean {
-    return !!personas && personas.some((item: IPersonaProps) => item.text === persona.text);
-  }
-
-  private _validateInput = (input: string): boolean => {
-    return input.indexOf('@') !== -1;
-  };
-}
-
-function _startsWith(text: string, filterText: string): boolean {
-  return text.toLowerCase().indexOf(filterText.toLowerCase()) === 0;
-}
+  return (
+    <>
+      <div style={searchBoxWrapperStyling} ref={setInputElementRef}>
+        <SearchBox placeholder="Search for person" onChange={onSearchChange} value={searchValue} onFocus={onFocus} />
+      </div>
+      <FloatingPeoplePicker
+        suggestionsStore={suggestionsStore}
+        onResolveSuggestions={onFilterChanged}
+        getTextFromItem={getTextFromItem}
+        pickerSuggestionsProps={suggestionProps}
+        key="normal"
+        onRemoveSuggestion={onRemoveSuggestion}
+        onValidateInput={validateInput}
+        componentRef={picker}
+        onChange={onPickerChange}
+        inputElement={inputElement}
+        resolveDelay={300}
+      />
+    </>
+  );
+};
