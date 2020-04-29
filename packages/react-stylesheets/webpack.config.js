62dcd85da6074c9d12c0979f33665c659737ca76
David Zearing <dzearing@microsoft.com>
2020-04-28 17:17:34 -0700

    yoink.

--- packages/react-stylesheets/webpack.config.js
+++ packages/react-stylesheets/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = resources.createConfig(BUNDLE_NAME, IS_PRODUCTION, {
 
   output: {
     libraryTarget: 'var',
-    library: 'FluentUIReactFocus',
+    library: 'FluentUIReactStylesheets',
   },
 
   externals: {
