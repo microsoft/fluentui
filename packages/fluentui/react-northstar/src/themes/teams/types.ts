1a46feb0183d6613f1df36dda74bcafa59fb8819
Charles Assuncao <charles.assuncao@microsoft.com>
2020-04-29 16:23:19 +0200

    chore(CarouselNavigationItem): Convert to RFC

--- packages/fluentui/react-northstar/src/themes/teams/types.ts
+++ packages/fluentui/react-northstar/src/themes/teams/types.ts
@@ -11,6 +11,7 @@ import {
 import { AccordionContentStylesProps } from '../../components/Accordion/AccordionContent';
 import { AccordionProps } from '../../components/Accordion/Accordion';
 import { AccordionTitleStylesProps } from '../../components/Accordion/AccordionTitle';
+import { CarouselNavigationItemStylesProps } from '../../components/Carousel/CarouselNavigationItem';
 import { AlertStylesProps } from '../../components/Alert/Alert';
 import { AnimationProps } from '../../components/Animation/Animation';
 import { AttachmentProps } from '../../components/Attachment/Attachment';
@@ -165,6 +166,7 @@ export type TeamsThemeStylesProps = {
   CardHeader: CardHeaderStylesProps;
   CardBody: CardBodyStylesProps;
   CardFooter: CardFooterStylesProps;
+  CarouselNavigationItem: CarouselNavigationItemStylesProps;
 };
 
 export type TeamsContextualColors = {
