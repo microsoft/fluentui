0b04761b8ffa7c4f1aa0624a405102b7f6840c21
E.J. Layne <ejlayne@microsoft.com>
2020-04-30 00:51:05 -0700

    added button documentation for windows

--- apps/fabric-website/src/pages/Controls/ButtonPage/ButtonPage.tsx
+++ apps/fabric-website/src/pages/Controls/ButtonPage/ButtonPage.tsx
@@ -4,6 +4,7 @@ import { ControlsAreaPage, IControlsPageProps } from '../ControlsAreaPage';
 import { ButtonPageProps } from './ButtonPage.doc';
 import { Platforms } from '../../../interfaces/Platforms';
 import { IPageSectionProps } from '@uifabric/example-app-base/lib/index2';
+import { ImplementationSection } from '@uifabric/example-app-base/lib/index2';
 
 const toggleStyles: Partial<IToggleStyles> = {
   root: { margin: '10px 0' },
@@ -94,7 +95,174 @@ export class ButtonPage extends React.Component<
           {
             sectionName: 'Implementation',
             editUrl: baseUrl + 'docs/windows/ButtonImplementation.md',
-            content: require('!raw-loader!@uifabric/fabric-website/src/pages/Controls/ButtonPage/docs/windows/ButtonImplementation.md') as string,
+            content: (
+              <ImplementationSection
+                jsonDocs={{
+                  tables: [
+                    {
+                      kind: 'interface',
+                      name: 'IButtonProps',
+                      description: '\n',
+                      members: [
+                        {
+                          name: 'componentRef',
+                          typeTokens: [
+                            {
+                              text: 'React.RefObject<IFocusable>',
+                            },
+                          ],
+                          kind: 'property',
+                          description:
+                            'A RefObject to access the IButton interface. Use this to access the public methods and properties of the component.\n',
+                        },
+                        {
+                          name: 'content',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'Text to show on the Button\n',
+                        },
+                        {
+                          name: 'icon',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'Source URL or name of the icon to show on the Button\n',
+                        },
+                        {
+                          name: 'onClick',
+                          typeTokens: [
+                            {
+                              text: 'void',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'A callback to call on button click event\n',
+                        },
+                      ],
+                    },
+                    {
+                      kind: 'interface',
+                      name: 'IButtonTokens',
+                      description: '\n',
+                      extendsTokens: [
+                        {
+                          text: 'ITextTokens, ',
+                        },
+                        {
+                          text: 'IForegroundColorTokens, ',
+                        },
+                        {
+                          text: 'IBackgroundColorTokens, ',
+                        },
+                        {
+                          text: 'IBorderTokens',
+                        },
+                      ],
+                      members: [
+                        {
+                          name: 'contentPadding',
+                          typeTokens: [
+                            {
+                              text: 'number | string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The amount of padding between the border and the contents\n',
+                        },
+                        {
+                          name: 'contentPaddingFocused',
+                          typeTokens: [
+                            {
+                              text: 'number | string',
+                            },
+                          ],
+                          kind: 'property',
+                          description:
+                            'The amount of padding between the border and the contents when the Button has focus\n',
+                        },
+                        {
+                          name: 'iconColor',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The icon color\n',
+                        },
+                        {
+                          name: 'iconColorHovered',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The icon color when hovering over the Button.\n',
+                        },
+                        {
+                          name: 'iconColorPressed',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The icon color when the Button is being pressed\n',
+                        },
+                        {
+                          name: 'iconSize',
+                          typeTokens: [
+                            {
+                              text: 'number | string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The size of the icon\n',
+                        },
+                        {
+                          name: 'iconWeight',
+                          typeTokens: [
+                            {
+                              text: 'number',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'The weight of the lines used when drawing the icon\n',
+                        },
+                        {
+                          name: 'content',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'Text to show on the Button\n',
+                        },
+                        {
+                          name: 'icon',
+                          typeTokens: [
+                            {
+                              text: 'string',
+                            },
+                          ],
+                          kind: 'property',
+                          description: 'Source URL or name of the icon to show on the Button\n',
+                        },
+                      ],
+                    },
+                  ],
+                }}
+              />
+            ),
           },
         ];
     }
