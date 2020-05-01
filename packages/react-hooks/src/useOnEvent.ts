647fd12b748a0dbab5641aed3471c4f7b2a4923a
Michael Loughry <miclo@microsoft.com>
2020-05-01 16:02:50 +0000

    Make event argument generic, and minimize repeated attach/detach

--- packages/react-hooks/src/useOnEvent.ts
+++ packages/react-hooks/src/useOnEvent.ts
@@ -2,18 +2,22 @@ import { on } from '@uifabric/utilities';
 import * as React from 'react';
 
 /**
- * Hook to attach an event handler on mount and handle cleanup
+ * Hook to attach an event handler on mount and handle cleanup.
  * @param element- Element (or ref to an element) to attach the event handler to
  * @param eventName- The event to attach a handler for
  * @param callback- The handler for the event
  * @param useCapture- Whether or not to attach the handler for the capture phase
  */
-export function useOnEvent<TElement extends Element>(
+export function useOnEvent<TElement extends Element, TEvent extends Event>(
   element: React.RefObject<TElement | undefined | null> | TElement | Window | undefined | null,
   eventName: string,
-  callback: (ev: Event) => void,
+  callback: (ev: TEvent) => void,
   useCapture?: boolean,
 ) {
+  // Use a ref for the callback to prevent repeatedly attaching/unattaching callbacks that are unstable across renders
+  const callbackRef = React.useRef(callback);
+  callbackRef.current = callback;
+
   React.useEffect(() => {
     if (element && 'current' in element) {
       element = element.current;
@@ -22,7 +26,7 @@ export function useOnEvent<TElement extends Element>(
       return;
     }
 
-    const dispose = on(element, eventName, callback, useCapture);
+    const dispose = on(element, eventName, (ev: TEvent) => callbackRef.current(ev), useCapture);
     return dispose;
-  }, [element, eventName, callback, useCapture]);
+  }, [element, eventName, useCapture]);
 }
