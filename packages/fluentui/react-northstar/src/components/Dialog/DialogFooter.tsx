d98d281c4aa8e1ff3a4a04b0efd25a866c369617
Charles Assuncao <charles.assuncao@microsoft.com>
2020-05-02 20:45:36 +0200

    chore(DialogFooter): Fix handledprops

--- packages/fluentui/react-northstar/src/components/Dialog/DialogFooter.tsx
+++ packages/fluentui/react-northstar/src/components/Dialog/DialogFooter.tsx
@@ -11,7 +11,7 @@ import {
 } from '../../utils';
 
 import { WithAsProp, withSafeTypeForAs, FluentComponentStaticProps, ProviderContextPrepared } from '../../types';
-import { useTelemetry, getElementType, useUnhandledProps, useStyles } from '@fluentui/react-bindings';
+import { useTelemetry, getElementType, useUnhandledProps, useAccessibility, useStyles } from '@fluentui/react-bindings';
 import { Accessibility } from '@fluentui/accessibility';
 
 export interface DialogFooterProps extends UIComponentProps, ChildrenComponentProps, ContentComponentProps {
@@ -29,10 +29,13 @@ export const DialogFooter: React.FC<WithAsProp<DialogFooterProps>> &
   const context: ProviderContextPrepared = React.useContext(ThemeContext);
   const { setStart, setEnd } = useTelemetry(DialogFooter.displayName, context.telemetry);
   setStart();
-  const { children, content, className, design, styles, variables } = props;
+  const { children, content, className, design, styles, variables, accessibility } = props;
   const ElementType = getElementType(props);
   const unhandledProps = useUnhandledProps(DialogFooter.handledProps, props);
-
+  const getA11yProps = useAccessibility<never>(accessibility, {
+    debugName: DialogFooter.displayName,
+    rtl: context.rtl,
+  });
   const { classes } = useStyles<DialogFooterStylesProps>(DialogFooter.displayName, {
     className: dialogFooterClassName,
     mapPropsToInlineStyles: () => ({
@@ -44,7 +47,7 @@ export const DialogFooter: React.FC<WithAsProp<DialogFooterProps>> &
     rtl: context.rtl,
   });
   const element = (
-    <ElementType className={classes.root} {...unhandledProps}>
+    <ElementType {...getA11yProps('root', { className: classes.root, ...unhandledProps })}>
       {childrenExist(children) ? children : content}
     </ElementType>
   );
@@ -54,11 +57,11 @@ export const DialogFooter: React.FC<WithAsProp<DialogFooterProps>> &
 
 DialogFooter.displayName = 'DialogFooter';
 
-DialogFooter.handledProps = Object.keys(DialogFooter.propTypes) as any;
-
 DialogFooter.propTypes = {
   ...commonPropTypes.createCommon(),
 };
+
+DialogFooter.handledProps = Object.keys(DialogFooter.propTypes) as any;
 
 DialogFooter.create = createShorthandFactory({ Component: DialogFooter, mappedProp: 'content' });
 
