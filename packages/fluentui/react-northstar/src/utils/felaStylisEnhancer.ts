79f483b5ffcdb540501f5d347bd9b3a598b83d88
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2020-05-04 13:07:53 +0200

    Update felaStylisEnhancer.ts

--- packages/fluentui/react-northstar/src/utils/felaStylisEnhancer.ts
+++ packages/fluentui/react-northstar/src/utils/felaStylisEnhancer.ts
@@ -1,7 +1,12 @@
 import { RULE_TYPE } from 'fela-utils';
-import * as Stylis from 'stylis';
+// @ts-ignore
+import _Stylis from 'stylis';
 
 import { Renderer, RendererChange } from './types';
+
+// `stylis@3` is a CJS library, there are known issues with them:
+// https://github.com/rollup/rollup/issues/1267#issuecomment-446681320
+const Stylis = (_Stylis as any).default || _Stylis
 
 // We use Stylis only for vendor prefixing, all other capabilities are disabled
 const stylis = new Stylis({
