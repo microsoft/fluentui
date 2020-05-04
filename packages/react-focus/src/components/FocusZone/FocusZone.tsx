6c055de8e22610b9f96d29e0667f019d669b7df1
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2020-05-04 09:18:07 -0700

    feat(FocusZone): Adding wrapping and Page Up/Down functionality (#12448)
    #### Pull request checklist
    
    - [ ] Addresses an existing issue: Fixes #0000
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    This PR brings two sets of functionality to the v0's version of `FocusZone` from the v7's version:
    - It brings the functionality for focus to wrap around once the user hits an edge.
    - It brings the functionality of `Page Up` and `Page Down` keyboard presses moving focus inside of the `FocusZone`.
    
    This PR also adds a `pagingSupportDisabled` prop to both versions to provide a way to opt out of this behavior if needed.
    
    There are also some other very minor code edits on v7's version to improve maintainability.
    
    #### Focus areas to test
    
    (optional)

--- packages/react-focus/src/components/FocusZone/FocusZone.tsx
+++ packages/react-focus/src/components/FocusZone/FocusZone.tsx
@@ -539,7 +539,7 @@ export class FocusZone extends React.Component<IFocusZoneProps> implements IFocu
     }
 
     // tslint:disable-next-line:deprecation
-    const { direction, disabled, isInnerZoneKeystroke, shouldEnterInnerZone } = this.props;
+    const { direction, disabled, isInnerZoneKeystroke, pagingSupportDisabled, shouldEnterInnerZone } = this.props;
 
     if (disabled) {
       return;
@@ -632,12 +632,12 @@ export class FocusZone extends React.Component<IFocusZoneProps> implements IFocu
           }
           return;
         case KeyCodes.pageDown:
-          if (this._moveFocusPaging(true)) {
+          if (this._moveFocusPaging(true) && !pagingSupportDisabled) {
             break;
           }
           return;
         case KeyCodes.pageUp:
-          if (this._moveFocusPaging(false)) {
+          if (this._moveFocusPaging(false) && !pagingSupportDisabled) {
             break;
           }
           return;
@@ -972,10 +972,8 @@ export class FocusZone extends React.Component<IFocusZoneProps> implements IFocu
             this.props.direction !== FocusZoneDirection.vertical
           ) {
             distance = activeRect.right - targetRect.right;
-          } else {
-            if (!shouldWrap) {
-              distance = LARGE_NEGATIVE_DISTANCE_FROM_CENTER;
-            }
+          } else if (!shouldWrap) {
+            distance = LARGE_NEGATIVE_DISTANCE_FROM_CENTER;
           }
 
           return distance;
@@ -1055,18 +1053,15 @@ export class FocusZone extends React.Component<IFocusZoneProps> implements IFocu
         return 0;
       }
       return Math.abs(targetRect.left + targetRect.width / 2 - leftAlignment);
-    } else {
-      if (!this._shouldWrapFocus(this._activeElement as HTMLElement, NO_VERTICAL_WRAP)) {
-        return LARGE_NEGATIVE_DISTANCE_FROM_CENTER;
-      }
-      return LARGE_DISTANCE_FROM_CENTER;
     }
+
+    if (!this._shouldWrapFocus(this._activeElement as HTMLElement, NO_VERTICAL_WRAP)) {
+      return LARGE_NEGATIVE_DISTANCE_FROM_CENTER;
+    }
+    return LARGE_DISTANCE_FROM_CENTER;
   };
 
   private _moveFocusPaging(isForward: boolean, useDefaultWrap: boolean = true): boolean {
-    if (useDefaultWrap === void 0) {
-      useDefaultWrap = true;
-    }
     let element = this._activeElement;
     if (!element || !this._root.current) {
       return false;
@@ -1114,11 +1109,9 @@ export class FocusZone extends React.Component<IFocusZoneProps> implements IFocu
             targetBottom = targetRectBottom;
             candidateDistance = elementDistance;
             candidateElement = element;
-          } else {
-            if (candidateDistance === -1 || elementDistance <= candidateDistance) {
-              candidateDistance = elementDistance;
-              candidateElement = element;
-            }
+          } else if (candidateDistance === -1 || elementDistance <= candidateDistance) {
+            candidateDistance = elementDistance;
+            candidateElement = element;
           }
         }
       }
