ecfcd72d09e8c354498d0f1193a0e39f39243d40
E.J. Layne <ejlayne@microsoft.com>
2020-05-06 00:28:28 -0700

    added persona content for cross platform page

--- apps/fabric-website/src/pages/Controls/PersonaPage/PersonaPage.tsx
+++ apps/fabric-website/src/pages/Controls/PersonaPage/PersonaPage.tsx
@@ -64,6 +64,67 @@ function _otherSections(platform: Platforms): IPageSectionProps<Platforms>[] {
                 tables: [
                   {
                     kind: 'interface' as ApiKind,
+                    name: 'IPersonaProps',
+                    description: '\n',
+                    extendsTokens: [
+                      {
+                        text: 'IPersonaCoinProps, ',
+                      },
+                      {
+                        text: 'IPersonaState, ',
+                      },
+                    ],
+                    members: [
+                      {
+                        name: 'text',
+                        typeTokens: [
+                          {
+                            text: 'string',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Primary text to display, usually the name of the person.\n',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'secondaryText',
+                        typeTokens: [
+                          {
+                            text: 'string',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Secondary text to display, usually the role of the user.\n',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'tertiaryText',
+                        typeTokens: [
+                          {
+                            text: 'string',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description:
+                          'Tertiary text to display, usually the status of the user. The tertiary text will only be shown when using size72 or size100.\n',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'optionalText',
+                        typeTokens: [
+                          {
+                            text: 'string',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description:
+                          'Optional text to display, usually a custom message set. The optional text will only be shown when using size100.\n',
+                        deprecated: false,
+                      },
+                    ],
+                  },
+                  {
+                    kind: 'interface' as ApiKind,
                     name: 'IPersonaCoinProps',
                     description: '\n',
                     extendsTokens: [],
@@ -125,6 +186,28 @@ function _otherSections(platform: Platforms): IPageSectionProps<Platforms>[] {
                         description: 'Presence of the person to display - will not display presence if undefined. \n',
                         deprecated: false,
                       },
+                      {
+                        name: 'size',
+                        typeTokens: [
+                          {
+                            text: 'PersonaSize',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Decides the size of the PersonaCoin.\n',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'coinColor',
+                        typeTokens: [
+                          {
+                            text: 'PersonaCoinColor',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: "The background color when the user's initials are displayed.\n",
+                        deprecated: false,
+                      },
                     ],
                   },
                   {
@@ -132,7 +215,420 @@ function _otherSections(platform: Platforms): IPageSectionProps<Platforms>[] {
                     name: 'PersonaPresence',
                     description: '\n',
                     extendsTokens: [],
-                    members: [],
+                    members: [
+                      {
+                        name: 'away',
+                        typeTokens: [
+                          {
+                            text: 'away',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'blocked',
+                        typeTokens: [
+                          {
+                            text: 'blocked',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'busy',
+                        typeTokens: [
+                          {
+                            text: 'busy',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'dnd',
+                        typeTokens: [
+                          {
+                            text: 'dnd',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'none',
+                        typeTokens: [
+                          {
+                            text: 'none',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'offline',
+                        typeTokens: [
+                          {
+                            text: 'offline',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'online',
+                        typeTokens: [
+                          {
+                            text: 'online',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                    ],
+                  },
+                  {
+                    kind: 'enum' as ApiKind,
+                    name: 'PersonaSize',
+                    description: '\n',
+                    extendsTokens: [],
+                    members: [
+                      {
+                        name: 'size8',
+                        typeTokens: [
+                          {
+                            text: 'size8',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'No PersonaCoin is rendered.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size24',
+                        typeTokens: [
+                          {
+                            text: 'size24',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 24px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size32',
+                        typeTokens: [
+                          {
+                            text: 'size32',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 32px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size40',
+                        typeTokens: [
+                          {
+                            text: 'size40',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 40px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size48',
+                        typeTokens: [
+                          {
+                            text: 'size48',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 48px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size56',
+                        typeTokens: [
+                          {
+                            text: 'size56',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 56px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size72',
+                        typeTokens: [
+                          {
+                            text: 'size72',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 72px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size100',
+                        typeTokens: [
+                          {
+                            text: 'size100',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 100px PersonaCoin.',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'size120',
+                        typeTokens: [
+                          {
+                            text: 'size120',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: 'Renders a 120px PersonaCoin.',
+                        deprecated: false,
+                      },
+                    ],
+                  },
+                  {
+                    kind: 'enum' as ApiKind,
+                    name: 'PersonaCoinColor',
+                    description: '\n',
+                    extendsTokens: [],
+                    members: [
+                      {
+                        name: 'lightBlue',
+                        typeTokens: [
+                          {
+                            text: 'lightBlue',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'blue',
+                        typeTokens: [
+                          {
+                            text: 'blue',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'darkBlue',
+                        typeTokens: [
+                          {
+                            text: 'darkBlue',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'teal',
+                        typeTokens: [
+                          {
+                            text: 'teal',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'green',
+                        typeTokens: [
+                          {
+                            text: 'green',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'darkGreen',
+                        typeTokens: [
+                          {
+                            text: 'darkGreen',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'lightPink',
+                        typeTokens: [
+                          {
+                            text: 'lightPink',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'pink',
+                        typeTokens: [
+                          {
+                            text: 'pink',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'magenta',
+                        typeTokens: [
+                          {
+                            text: 'magenta',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'purple',
+                        typeTokens: [
+                          {
+                            text: 'purple',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'orange',
+                        typeTokens: [
+                          {
+                            text: 'orange',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'darkRed',
+                        typeTokens: [
+                          {
+                            text: 'darkRed',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'violet',
+                        typeTokens: [
+                          {
+                            text: 'violet',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'lightRed',
+                        typeTokens: [
+                          {
+                            text: 'lightRed',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'gold',
+                        typeTokens: [
+                          {
+                            text: 'gold',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'burgundy',
+                        typeTokens: [
+                          {
+                            text: 'burgundy',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'warmGray',
+                        typeTokens: [
+                          {
+                            text: 'warmGray',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'coolGray',
+                        typeTokens: [
+                          {
+                            text: 'coolGray',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'cyan',
+                        typeTokens: [
+                          {
+                            text: 'cyan',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                      {
+                        name: 'rust',
+                        typeTokens: [
+                          {
+                            text: 'rust',
+                          },
+                        ],
+                        kind: 'property' as 'property',
+                        description: '',
+                        deprecated: false,
+                      },
+                    ],
                   },
                 ],
               }}
