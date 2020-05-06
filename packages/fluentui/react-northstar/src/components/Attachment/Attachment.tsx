642736d57f382ff9cf15bd5bb0bbb43ce43771bc
Marija Najdova <mnajdova@gmail.com>
2020-05-06 16:20:08 +0200

    -moved merge shorthand variables out of render fn

--- packages/fluentui/react-northstar/src/components/Attachment/Attachment.tsx
+++ packages/fluentui/react-northstar/src/components/Attachment/Attachment.tsx
@@ -70,6 +70,13 @@ export interface AttachmentProps extends UIComponentProps, ChildrenComponentProp
 export type AttachmentStylesProps = Required<Pick<AttachmentProps, 'actionable' | 'disabled'>>;
 export const attachmentClassName = 'ui-attachment';
 
+// mergeComponentVariables is always creating a function even if the arguments are undefined
+// we have this temporary fix in place to avoid creating empty function because it is breaking caching
+// we should either fix mergeComponentVariables, or handle this in a more generic way
+const mergeShorthandVariables = (variables, shorthandVariables) => {
+  return (variables || shorthandVariables) && mergeComponentVariables(variables, shorthandVariables);
+};
+
 /**
  * An Attachment represents a file or media attachment, which may contain some metadata or actions.
  */
@@ -138,10 +145,6 @@ const Attachment = compose<'div', AttachmentProps, AttachmentStylesProps, {}, {}
       }
 
       _.invoke(props, 'onClick', e, props);
-    };
-
-    const mergeShorthandVariables = (variables, shorthandVariables) => {
-      return (variables || shorthandVariables) && mergeComponentVariables(variables, shorthandVariables);
     };
 
     const element = (
