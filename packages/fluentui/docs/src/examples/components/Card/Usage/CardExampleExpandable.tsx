e23b3e55421d59e5a638d233ae2faca10b8ce1cc
pompomon <pompomon@gmail.com>
2020-05-06 16:23:22 +0200

    PR comments fixes

--- packages/fluentui/docs/src/examples/components/Card/Usage/CardExampleExpandable.tsx
+++ packages/fluentui/docs/src/examples/components/Card/Usage/CardExampleExpandable.tsx
@@ -13,6 +13,7 @@ import {
   Text,
   ThemeInput,
 } from '@fluentui/react-northstar';
+import { useBooleanKnob } from '@fluentui/docs-components';
 import * as React from 'react';
 
 // Compose custom styled Box
@@ -22,7 +23,7 @@ type ExpandableBoxProps = {
 
 type ExpandableBoxStylesProps = ExpandableBoxProps;
 
-const ExpandableBox = compose<'button', ExpandableBoxProps, ExpandableBoxStylesProps, BoxProps, {}>(Box, {
+const ExpandableBox = compose<'div', ExpandableBoxProps, ExpandableBoxStylesProps, BoxProps, {}>(Box, {
   className: 'ui-expandable-box',
   displayName: 'ExpandableBox',
   mapPropsToStylesProps: props => ({ expand: props.expand }),
@@ -74,52 +75,54 @@ const customTheme: ThemeInput<ComponentStylesProps> = {
 };
 
 const CardExampleExpandable = () => {
-  const [expandCard, setExpandCard] = React.useState(false);
-  const expandCardHandler = () => {
-    setExpandCard(true);
-  };
-
-  const shrinkCardHandler = () => {
-    setExpandCard(false);
-  };
+  const [expandCard, setExpandCard] = useBooleanKnob({
+    name: 'expandCard',
+    initialValue: false,
+  });
 
   return (
-    <>
-      <Provider theme={customTheme}>
-        <Card
-          aria-roledescription="user card"
-          aria-expanded={expandCard}
-          accessibility={cardFocusableBehavior}
-          onFocus={expandCardHandler}
-          onMouseEnter={expandCardHandler}
-          onMouseLeave={shrinkCardHandler}
-          onBlur={shrinkCardHandler}
-        >
-          <Card.Header>
-            <Flex gap="gap.small">
-              <Avatar
-                image="public/images/avatar/small/matt.jpg"
-                label="Copy bandwidth"
-                name="Evie yundt"
-                status="unknown"
-              />
-              <Flex column>
-                <Text content="Using compose and custom styles" weight="bold" />
-                <Text content="Secondary line" size="small" />
-              </Flex>
+    <Provider theme={customTheme}>
+      <Card
+        aria-roledescription="user card"
+        aria-expanded={expandCard}
+        accessibility={cardFocusableBehavior}
+        onFocus={() => {
+          setExpandCard(true);
+        }}
+        onMouseEnter={() => {
+          setExpandCard(true);
+        }}
+        onMouseLeave={() => {
+          setExpandCard(false);
+        }}
+        onBlur={() => {
+          setExpandCard(false);
+        }}
+      >
+        <Card.Header>
+          <Flex gap="gap.small">
+            <Avatar
+              image="public/images/avatar/small/matt.jpg"
+              label="Copy bandwidth"
+              name="Evie yundt"
+              status="unknown"
+            />
+            <Flex column>
+              <Text content="Using compose and custom styles" weight="bold" />
+              <Text content="Secondary line" size="small" />
             </Flex>
-          </Card.Header>
-          <Card.Body>
-            <Flex column gap="gap.small">
-              <Image src="public/images/wireframe/square-image.png" />
-              <ExpandableBox expand={expandCard}>
-                <Text content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
-              </ExpandableBox>
-            </Flex>
-          </Card.Body>
-        </Card>
-      </Provider>
-    </>
+          </Flex>
+        </Card.Header>
+        <Card.Body>
+          <Flex column gap="gap.small">
+            <Image src="public/images/wireframe/square-image.png" />
+            <ExpandableBox expand={expandCard}>
+              <Text content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
+            </ExpandableBox>
+          </Flex>
+        </Card.Body>
+      </Card>
+    </Provider>
   );
 };
 
