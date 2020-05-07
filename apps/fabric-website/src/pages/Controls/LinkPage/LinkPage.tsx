4391890fd1803bf8a833168946acb2ee97d192e4
Micah Godbolt <mgodbolt@microsoft.com>
2020-05-06 20:56:11 -0700

    fixed some linting errors

--- apps/fabric-website/src/pages/Controls/LinkPage/LinkPage.tsx
+++ apps/fabric-website/src/pages/Controls/LinkPage/LinkPage.tsx
@@ -6,6 +6,8 @@ import { Platforms } from '../../../interfaces/Platforms';
 import { ImplementationSection } from '@uifabric/example-app-base/lib/index2';
 import { ApiKind } from 'office-ui-fabric-react/lib/common/DocPage.types';
 
+// tslint:disable:max-line-length
+
 const baseUrl = 'https://github.com/microsoft/fluentui/tree/master/apps/fabric-website/src/pages/Controls/LinkPage';
 
 export const LinkPage: React.FunctionComponent<IControlsPageProps> = props => {
--- apps/fabric-website/src/pages/Controls/SeparatorPage/SeparatorPage.tsx
+++ apps/fabric-website/src/pages/Controls/SeparatorPage/SeparatorPage.tsx
@@ -6,6 +6,8 @@ import { IPageSectionProps } from '@uifabric/example-app-base/lib/index2';
 import { ImplementationSection } from '@uifabric/example-app-base/lib/index2';
 import { ApiKind } from 'office-ui-fabric-react/lib/common/DocPage.types';
 
+// tslint:disable:max-line-length
+
 const baseUrl =
   'https://github.com/microsoft/fluentui/tree/master/apps/fabric-website/src/pages/Controls/SeparatorPage/';
 
