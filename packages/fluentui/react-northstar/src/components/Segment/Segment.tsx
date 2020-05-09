6b2c4fcc9731c0728cf9a226d5f6e716fe3f3ed8
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-09 14:03:30 +0200

    chore(Segment): optional attribute

--- packages/fluentui/react-northstar/src/components/Segment/Segment.tsx
+++ packages/fluentui/react-northstar/src/components/Segment/Segment.tsx
@@ -40,7 +40,7 @@ export interface SegmentProps
   /** A segment can have its colors inverted for contrast. */
   inverted?: boolean;
 
-  rtlAttributes: typeof rtlTextContainer.getAttributes;
+  rtlAttributes?: typeof rtlTextContainer.getAttributes;
 }
 
 export type SegmentStylesProps = Required<Pick<SegmentProps, 'color' | 'inverted' | 'disabled'>>;
