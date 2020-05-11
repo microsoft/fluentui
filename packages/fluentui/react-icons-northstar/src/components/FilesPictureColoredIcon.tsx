7a32f33f85ae2ab66724d62f2febcc82fbf5b023
Tanel Vari <tavari@microsoft.com>
2020-05-11 12:49:11 -0700

    PR feedback - not using 'fluent' in file names

--- packages/fluentui/react-icons-northstar/src/components/FilesPictureColoredIcon.tsx
+++ packages/fluentui/react-icons-northstar/src/components/FilesPictureColoredIcon.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import createSvgIcon from '../utils/createSvgIcon';
 
-const FluentFilePictureIcon = createSvgIcon({
+const FilesPictureColoredIcon = createSvgIcon({
   svg: ({ classes }) => (
     <svg role="presentation" focusable="false" viewBox="0 0 32 32" className={classes.svg}>
       <path
@@ -22,7 +22,7 @@ const FluentFilePictureIcon = createSvgIcon({
       />
     </svg>
   ),
-  displayName: 'FluentFilePictureIcon',
+  displayName: 'FilesPictureColoredIcon',
 });
 
-export default FluentFilePictureIcon;
+export default FilesPictureColoredIcon;
