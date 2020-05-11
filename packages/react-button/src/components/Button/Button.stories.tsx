cb257e2b79a1b6dfef39e37a70867e2422283da3
David Zearing <dzearing@microsoft.com>
2020-05-10 23:06:06 -0700

    Cleaning up styles.

--- packages/react-button/src/components/Button/Button.stories.tsx
+++ packages/react-button/src/components/Button/Button.stories.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Button } from './Button';
 import { ButtonProps } from './Button.types';
+import * as classes from './Button.stories.scss';
 
 const ButtonVariants = (props: ButtonProps) => (
-  <div>
+  <div className={classes.hStack}>
     <Button {...props} content="Hello, world" icon="X" />
     <Button {...props} primary content="Hello, world" icon="X" />
     <Button {...props} disabled content="Hello, world" icon="X" />
@@ -14,38 +15,33 @@ const ButtonVariants = (props: ButtonProps) => (
 export const ButtonCss = () => (
   <>
     <h3>A button can appear round using the `circular` prop.</h3>
-    <div>
-      <ButtonVariants circular />
-    </div>
+    <ButtonVariants circular />
 
     <h3>A button can fill the width of its container using the `fluid` prop.</h3>
-    <div>
-      <ButtonVariants fluid />
+    <div className={classes.vStack}>
+      <Button fluid content="Hello, world" icon="X" />
+      <Button fluid primary content="Hello, world" icon="X" />
+      <Button fluid disabled content="Hello, world" icon="X" />
+      <Button fluid primary disabled content="Hello, world" icon="X" />
     </div>
 
     <h3>A button can contain only an icon using the `iconOnly` prop.</h3>
-    <div>
-      <ButtonVariants iconOnly />
-    </div>
+    <ButtonVariants iconOnly />
 
     <h3>An icon button can format its Icon to appear before or after its content.</h3>
-    <div>
+    <div className={classes.vStack}>
       <ButtonVariants iconPosition="before" />
       <ButtonVariants iconPosition="after" />
     </div>
 
     <h3>A button can inherit its background and have a subtle appearance using the `inverted` prop.</h3>
-    <div>
-      <ButtonVariants inverted />
-    </div>
+    <ButtonVariants inverted />
 
     <h3>A button can show a loading indicator using the `loading` prop.</h3>
-    <div>
-      <ButtonVariants loading />
-    </div>
+    <ButtonVariants loading />
 
     <h3>A button can be sized.</h3>
-    <div>
+    <div className={classes.vStack}>
       <ButtonVariants size="smallest" />
       <ButtonVariants size="smaller" />
       <ButtonVariants size="small" />
@@ -59,16 +55,5 @@ export const ButtonCss = () => (
       <ButtonVariants text />
     </div>
     */}
-  </>
-);
-
-export const ButtonCss2 = () => (
-  <>
-    <Button icon="X" content="Hello, world" />
-    <Button primary content="Hello, world" />
-    <Button circular icon="X" />
-    <Button disabled content="Hello, world" />
-    <Button disabled primary content="Hello, world" />
-    <Button disabled circular icon="X" />
   </>
 );
