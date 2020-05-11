0694a8ed7d5c0214d398745daf11be08c0f93c4f
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-11 19:15:43 +0200

    chore(Components): remove selector not used

--- packages/fluentui/e2e/tests/dialogInDialogWithDropdown-example.tsx
+++ packages/fluentui/e2e/tests/dialogInDialogWithDropdown-example.tsx
@@ -5,12 +5,10 @@ export const selectors = {
   outerClose: 'outer-close',
   outerHeader: 'outer-header',
   outerTrigger: 'outer-trigger',
-  outerOverlay: 'outer-overlay',
 
   innerClose: 'inner-close',
   innerHeader: 'inner-header',
   innerTrigger: 'inner-trigger',
-  innerOverlay: 'inner-overlay',
 
   dropdown: 'dropdown-id',
 };
@@ -47,13 +45,11 @@ const DialogInPopupExample = () => (
       <Dialog
         cancelButton={{ content: 'Close', id: selectors.innerClose }}
         header={{ content: 'An inner', id: selectors.innerHeader }}
-        overlay={{ id: selectors.innerOverlay }}
         content={<DropdownExampleSearch />}
         trigger={<Button id={selectors.innerTrigger} content="Open a dialog" />}
       />
     }
     header={{ content: 'An outer', id: selectors.outerHeader }}
-    overlay={{ id: selectors.outerOverlay }}
     trigger={<Button id={selectors.outerTrigger} content="Open a dialog" />}
   />
 );
