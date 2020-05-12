904482d4471ee8e6dc6684a84b8ac3ecc12f75c6
David Zearing <dzearing@microsoft.com>
2020-05-11 19:54:06 -0700

    Removing console.log

--- packages/react-button/src/components/Button/Button.tsx
+++ packages/react-button/src/components/Button/Button.tsx
@@ -3,8 +3,6 @@ import { compose } from '@fluentui/react-compose';
 import * as classes from './Button.scss';
 import { ButtonProps } from './Button.types';
 
-console.log(classes);
-
 export const Button = compose<'button', {}, {}, ButtonProps, ButtonProps>(ButtonBase, {
   classes,
   slots: {
