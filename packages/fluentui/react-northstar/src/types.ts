3384732e232c8086d5af691eff8e563ed0ec278a
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-13 14:58:52 +0200

    fix(Types): change default generic

--- packages/fluentui/react-northstar/src/types.ts
+++ packages/fluentui/react-northstar/src/types.ts
@@ -64,9 +64,7 @@ type ReactNode = React.ReactChild | React.ReactNodeArray | React.ReactPortal | b
 
 export type ShorthandValue<P extends Props> = ReactNode | ObjectShorthandValue<P>;
 
-export type ShorthandCollection<P, K = Record<string, Props>> = ShorthandValue<
-  P | (P & { kind?: keyof K } & Partial<K[keyof K]>)
->[];
+export type ShorthandCollection<P, K = {}> = ShorthandValue<P | (P & { kind?: keyof K } & Partial<K[keyof K]>)>[];
 
 export type ObjectShorthandValue<P extends Props> = Props<P> & {
   children?: P['children'] | ShorthandRenderFunction<P>;
