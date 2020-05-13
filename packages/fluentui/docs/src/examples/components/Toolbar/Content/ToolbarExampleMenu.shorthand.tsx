eb7249a749eb58c51163a0186786716b4cd5d3ac
Silviu Avram <silviu.avram91@gmail.com>
2020-05-13 10:59:04 +0300

    augment ToolbarExampleMenu with SplitButton

--- packages/fluentui/docs/src/examples/components/Toolbar/Content/ToolbarExampleMenu.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Toolbar/Content/ToolbarExampleMenu.shorthand.tsx
@@ -1,8 +1,49 @@
 import { createCallbackLogFormatter } from '@fluentui/code-sandbox';
 import { useLogKnob } from '@fluentui/docs-components';
-import { Toolbar } from '@fluentui/react-northstar';
+import { Toolbar, SplitButton } from '@fluentui/react-northstar';
 import * as React from 'react';
 import { MoreIcon, PauseIcon, PlayIcon, BoldIcon, UnderlineIcon } from '@fluentui/react-icons-northstar';
+
+const ToolbarSplitButton = () => (
+  <>
+    <SplitButton
+      menu={[
+        {
+          key: 'group',
+          content: 'New group message',
+        },
+        {
+          key: 'channel',
+          content: 'New channel message',
+        },
+      ]}
+      button={{
+        content: 'New conversation',
+        'aria-roledescription': 'splitbutton',
+        'aria-describedby': 'instruction-message',
+      }}
+      toggleButton={{
+        'aria-label': 'more options',
+        // do not close toolbar menu on click.
+        onClick: e => e.stopPropagation(),
+      }}
+      styles={{ display: 'flex' }}
+      // do not close toolbar menu on click.
+      onMainButtonClick={e => e.stopPropagation()}
+    />
+    <span
+      aria-hidden="true"
+      id="instruction-message"
+      style={{
+        opacity: 0,
+        height: '1px',
+        width: '1px',
+      }}
+    >
+      to open menu, press Alt + Arrow Down
+    </span>
+  </>
+);
 
 const ToolbarExampleMenuShorthand = () => {
   const [menuOpen, setMenuOpen] = React.useState(false);
@@ -38,6 +79,7 @@ const ToolbarExampleMenuShorthand = () => {
           menu: {
             items: [
               { key: 'play', content: 'Play', icon: <PlayIcon /> },
+              { key: 'split', disabled: true, as: 'div', content: <ToolbarSplitButton /> },
               { key: 'pause', content: 'Pause', icon: <PauseIcon /> },
               { key: 'divider', kind: 'divider' },
               'Without icon',
