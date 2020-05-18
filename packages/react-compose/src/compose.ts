81d2bdbc98c0737eb0c6b969415486b2e595b85c
Oleksandr Fediashov <olfedias@microsoft.com>
2020-05-18 14:57:14 +0200

    fix assignment

--- packages/react-compose/src/compose.ts
+++ packages/react-compose/src/compose.ts
@@ -15,7 +15,7 @@ function compose<ElementType extends React.ElementType, InputProps, InputStylesP
 
   const Component = (React.forwardRef<ElementType, InputProps & ParentProps & { as?: React.ElementType }>(
     (props, ref) => {
-      return composeOptions.render(props, ref as React.Ref<HTMLDivElement>, composeOptions);
+      return composeOptions.render(props, (ref as unknown) as React.Ref<HTMLDivElement>, composeOptions);
     },
   ) as unknown) as ComponentWithAs<ElementType, InputProps & ParentProps>;
 
