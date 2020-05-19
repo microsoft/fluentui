38c4b51fe703087f21e3e502f64ab1e16308d23b
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-19 12:37:19 +0200

    feat(Menu): Pointing example remove items array

--- packages/fluentui/docs/src/examples/components/Menu/Types/MenuExamplePointing.tsx
+++ packages/fluentui/docs/src/examples/components/Menu/Types/MenuExamplePointing.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { Menu } from '@fluentui/react-northstar';
 
-const items = [
-  { key: 'editorials', content: 'Editorials' },
-  { key: 'review', content: 'Reviews' },
-  { key: 'events', content: 'Upcoming Events' },
-];
-
 const MenuExamplePointing = () => (
   <div>
     <Menu defaultActiveIndex={2} pointing primary>
@@ -21,7 +15,17 @@ const MenuExamplePointing = () => (
       </Menu.Item>
     </Menu>
     <br />
-    <Menu defaultActiveIndex={0} items={items} pointing="start" primary />
+    <Menu defaultActiveIndex={0} pointing="start" primary>
+      <Menu.Item index={0}>
+        <Menu.ItemContent>Editorials</Menu.ItemContent>
+      </Menu.Item>
+      <Menu.Item index={1}>
+        <Menu.ItemContent>Reviews</Menu.ItemContent>
+      </Menu.Item>
+      <Menu.Item index={2}>
+        <Menu.ItemContent>Upcoming Events</Menu.ItemContent>
+      </Menu.Item>
+    </Menu>
   </div>
 );
 export default MenuExamplePointing;
