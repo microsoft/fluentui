8e418f3824f1b58949bbeb1412312844be5b88dc
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-21 14:45:20 +0200

    Revert "feat(ChatMessages): use compose"
    This reverts commit 36f0764cb7cb1b5a1162ab117be82fe4880cbf1b.

--- packages/fluentui/react-northstar/src/components/Chat/ChatMessage.tsx
+++ packages/fluentui/react-northstar/src/components/Chat/ChatMessage.tsx
@@ -5,15 +5,7 @@ import {
   menuAsToolbarBehavior,
   ChatMessageBehaviorProps,
 } from '@fluentui/accessibility';
-import {
-  getElementType,
-  useUnhandledProps,
-  useAccessibility,
-  useStyles,
-  useTelemetry,
-  compose,
-  ComponentWithAs,
-} from '@fluentui/react-bindings';
+import { getElementType, useUnhandledProps, useAccessibility, useStyles, useTelemetry } from '@fluentui/react-bindings';
 import { useContextSelector } from '@fluentui/react-context-selector';
 import { Ref } from '@fluentui/react-component-ref';
 import * as customPropTypes from '@fluentui/react-proptypes';
@@ -39,14 +31,14 @@ import {
   ContentComponentProps,
   commonPropTypes,
   rtlTextContainer,
-  createShorthand,
-  ShorthandFactory,
 } from '../../utils';
 import {
+  WithAsProp,
   ShorthandValue,
   ComponentEventHandler,
   withSafeTypeForAs,
   ShorthandCollection,
+  FluentComponentStaticProps,
   ProviderContextPrepared,
 } from '../../types';
 import Box, { BoxProps } from '../Box/Box';
@@ -150,7 +142,248 @@ export const chatMessageSlotClassNames: ChatMessageSlotClassNames = {
   reactionGroup: `${chatMessageClassName}__reactions`,
 };
 
-const propTypes = {
+const ChatMessage: React.FC<WithAsProp<ChatMessageProps>> & FluentComponentStaticProps<ChatMessageProps> = props => {
+  const context: ProviderContextPrepared = React.useContext(ThemeContext);
+  const { setStart, setEnd } = useTelemetry(ChatMessage.displayName, context.telemetry);
+  setStart();
+
+  const parentAttached = useContextSelector(ChatItemContext, v => v.attached);
+  const {
+    accessibility,
+    actionMenu,
+    attached = parentAttached,
+    author,
+    badge,
+    badgePosition,
+    children,
+    className,
+    content,
+    design,
+    mine,
+    positionActionMenu,
+    reactionGroup,
+    reactionGroupPosition,
+    timestamp,
+    styles,
+    variables,
+    header,
+    unstable_overflow: overflow,
+  } = props;
+
+  const [focused, setFocused] = React.useState<boolean>(false);
+  const [messageNode, setMessageNode] = React.useState<HTMLElement | null>(null);
+
+  const updateActionsMenuPosition = React.useRef<(() => void) | null>(null);
+
+  const getA11Props = useAccessibility(accessibility, {
+    actionHandlers: {
+      // prevents default FocusZone behavior, e.g., in ChatMessageBehavior, it prevents FocusZone from using arrow keys
+      // as navigation (only Tab key should work)
+      preventDefault: event => {
+        // preventDefault only if event coming from inside the message
+        if (event.currentTarget !== event.target) {
+          event.preventDefault();
+        }
+      },
+
+      focus: event => {
+        if (messageNode) {
+          messageNode.focus();
+          event.stopPropagation();
+        }
+      },
+    },
+  });
+  const { classes, styles: resolvedStyles } = useStyles<ChatMessageStylesProps>(ChatMessage.displayName, {
+    className: chatMessageClassName,
+    mapPropsToStyles: () => ({
+      attached,
+      badgePosition,
+      focused,
+      mine,
+      hasBadge: !!badge,
+      hasReactionGroup: !!reactionGroup,
+    }),
+    mapPropsToInlineStyles: () => ({
+      className,
+      design,
+      styles,
+      variables,
+    }),
+    rtl: context.rtl,
+  });
+
+  const handleFocus = (e: React.SyntheticEvent) => {
+    _.invoke(updateActionsMenuPosition, 'current');
+
+    setFocused(true);
+    _.invoke(props, 'onFocus', e, props);
+  };
+
+  const handleBlur = (e: React.SyntheticEvent) => {
+    // `focused` controls is focused the whole `ChatMessage` or any of its children. When we're navigating
+    // with keyboard the focused element will be changed and there is no way to use `:focus` selector
+    const shouldPreserveFocusState = _.invoke(e, 'currentTarget.contains', (e as any).relatedTarget);
+
+    setFocused(shouldPreserveFocusState);
+    _.invoke(props, 'onBlur', e, props);
+  };
+
+  const handleMouseEnter = (e: React.SyntheticEvent) => {
+    _.invoke(updateActionsMenuPosition, 'current');
+    _.invoke(props, 'onMouseEnter', e, props);
+  };
+
+  const renderActionMenu = () => {
+    const actionMenuElement = Menu.create(actionMenu, {
+      defaultProps: () => ({
+        [IS_FOCUSABLE_ATTRIBUTE]: true,
+        accessibility: menuAsToolbarBehavior,
+        className: chatMessageSlotClassNames.actionMenu,
+        styles: resolvedStyles.actionMenu,
+      }),
+    });
+
+    if (!actionMenuElement) {
+      return actionMenuElement;
+    }
+
+    const modifiers: PopperModifiers | undefined = positionActionMenu && [
+      // https://popper.js.org/docs/v2/modifiers/flip/
+      // Forces to flip only in "top-*" positions
+      { name: 'flip', options: { fallbackPlacements: ['top'] } },
+      overflow && {
+        name: 'preventOverflow',
+        options: { boundary: getScrollParent(messageNode) },
+      },
+    ];
+
+    return (
+      <Popper
+        enabled={positionActionMenu}
+        align="end"
+        modifiers={modifiers}
+        position="above"
+        positionFixed={overflow}
+        targetRef={messageNode}
+        {...getPopperPropsFromShorthand(actionMenu)}
+      >
+        {({ scheduleUpdate }) => {
+          updateActionsMenuPosition.current = scheduleUpdate;
+
+          return actionMenuElement;
+        }}
+      </Popper>
+    );
+  };
+
+  const childrenPropExists = childrenExist(children);
+  const rootClasses = childrenPropExists ? cx(classes.root, classes.content) : classes.root;
+
+  const ElementType = getElementType(props);
+  const unhandledProps = useUnhandledProps(ChatMessage.handledProps, props);
+
+  const badgeElement = Label.create(badge, {
+    defaultProps: () => ({
+      className: chatMessageSlotClassNames.badge,
+      styles: resolvedStyles.badge,
+    }),
+  });
+
+  const reactionGroupElement = Reaction.Group.create(reactionGroup, {
+    defaultProps: () => ({
+      className: chatMessageSlotClassNames.reactionGroup,
+      styles: resolvedStyles.reactionGroup,
+    }),
+  });
+
+  const actionMenuElement = renderActionMenu();
+
+  const authorElement = Text.create(author, {
+    defaultProps: () => ({
+      size: 'small',
+      styles: resolvedStyles.author,
+      className: chatMessageSlotClassNames.author,
+    }),
+  });
+
+  const timestampElement = Text.create(timestamp, {
+    defaultProps: () => ({
+      size: 'small',
+      styles: resolvedStyles.timestamp,
+      timestamp: true,
+      className: chatMessageSlotClassNames.timestamp,
+    }),
+  });
+
+  const messageContent = Box.create(content, {
+    defaultProps: () => ({
+      className: chatMessageSlotClassNames.content,
+      styles: resolvedStyles.content,
+    }),
+  });
+
+  const headerElement = Box.create(header, {
+    defaultProps: () => ({
+      className: chatMessageSlotClassNames.header,
+      styles: resolvedStyles.header,
+    }),
+    overrideProps: () => ({
+      children: (
+        <>
+          {authorElement}
+          {timestampElement}
+          {reactionGroupPosition === 'start' && reactionGroupElement}
+        </>
+      ),
+    }),
+  });
+
+  const element = (
+    <Ref innerRef={setMessageNode}>
+      {getA11Props.unstable_wrapWithFocusZone(
+        <ElementType
+          {...getA11Props('root', {
+            className: rootClasses,
+            onBlur: handleBlur,
+            onFocus: handleFocus,
+            onMouseEnter: handleMouseEnter,
+            ...rtlTextContainer.getAttributes({ forElements: [children] }),
+            ...unhandledProps,
+          })}
+        >
+          {childrenPropExists ? (
+            children
+          ) : (
+            <>
+              {actionMenuElement}
+              {badgePosition === 'start' && badgeElement}
+              {headerElement}
+              {messageContent}
+              {reactionGroupPosition === 'end' && reactionGroupElement}
+              {badgePosition === 'end' && badgeElement}
+            </>
+          )}
+        </ElementType>,
+      )}
+    </Ref>
+  );
+  setEnd();
+
+  return element;
+};
+
+ChatMessage.displayName = 'ChatMessage';
+
+ChatMessage.defaultProps = {
+  accessibility: chatMessageBehavior,
+  badgePosition: 'end',
+  header: {},
+  positionActionMenu: true,
+  reactionGroupPosition: 'start',
+};
+
+ChatMessage.propTypes = {
   ...commonPropTypes.createCommon({ content: 'shorthand' }),
   actionMenu: PropTypes.oneOfType([customPropTypes.itemShorthand, customPropTypes.collectionShorthand]),
   attached: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf<'top' | 'bottom'>(['top', 'bottom'])]),
@@ -169,266 +402,7 @@ const propTypes = {
   unstable_overflow: PropTypes.bool,
 };
 
-const ChatMessage = compose<'div', ChatMessageProps, ChatMessageStylesProps, {}, {}>(
-  (props, ref, composeOptions) => {
-    const context: ProviderContextPrepared = React.useContext(ThemeContext);
-    const { setStart, setEnd } = useTelemetry(ChatMessage.displayName, context.telemetry);
-    setStart();
-
-    const parentAttached = useContextSelector(ChatItemContext, v => v.attached);
-
-    const {
-      accessibility,
-      actionMenu,
-      attached = parentAttached,
-      author,
-      badge,
-      badgePosition,
-      children,
-      className,
-      content,
-      design,
-      mine,
-      positionActionMenu,
-      reactionGroup,
-      reactionGroupPosition,
-      timestamp,
-      styles,
-      variables,
-      header,
-      unstable_overflow: overflow,
-    } = props;
-
-    const [focused, setFocused] = React.useState<boolean>(false);
-    const [messageNode, setMessageNode] = React.useState<HTMLElement | null>(null);
-
-    const updateActionsMenuPosition = React.useRef<(() => void) | null>(null);
-
-    const getA11Props = useAccessibility(accessibility, {
-      actionHandlers: {
-        // prevents default FocusZone behavior, e.g., in ChatMessageBehavior, it prevents FocusZone from using arrow keys
-        // as navigation (only Tab key should work)
-        preventDefault: event => {
-          // preventDefault only if event coming from inside the message
-          if (event.currentTarget !== event.target) {
-            event.preventDefault();
-          }
-        },
-
-        focus: event => {
-          if (messageNode) {
-            messageNode.focus();
-            event.stopPropagation();
-          }
-        },
-      },
-    });
-
-    const { classes, styles: resolvedStyles } = useStyles<ChatMessageStylesProps>(ChatMessage.displayName, {
-      className: chatMessageClassName,
-      mapPropsToStyles: () => ({
-        attached,
-        badgePosition,
-        focused,
-        mine,
-        hasBadge: !!badge,
-        hasReactionGroup: !!reactionGroup,
-      }),
-      mapPropsToInlineStyles: () => ({
-        className,
-        design,
-        styles,
-        variables,
-      }),
-      rtl: context.rtl,
-    });
-
-    const handleFocus = (e: React.SyntheticEvent) => {
-      _.invoke(updateActionsMenuPosition, 'current');
-
-      setFocused(true);
-      _.invoke(props, 'onFocus', e, props);
-    };
-
-    const handleBlur = (e: React.SyntheticEvent) => {
-      // `focused` controls is focused the whole `ChatMessage` or any of its children. When we're navigating
-      // with keyboard the focused element will be changed and there is no way to use `:focus` selector
-      const shouldPreserveFocusState = _.invoke(e, 'currentTarget.contains', (e as any).relatedTarget);
-
-      setFocused(shouldPreserveFocusState);
-      _.invoke(props, 'onBlur', e, props);
-    };
-
-    const handleMouseEnter = (e: React.SyntheticEvent) => {
-      _.invoke(updateActionsMenuPosition, 'current');
-      _.invoke(props, 'onMouseEnter', e, props);
-    };
-
-    const renderActionMenu = () => {
-      const actionMenuElement = Menu.create(actionMenu, {
-        defaultProps: () => ({
-          [IS_FOCUSABLE_ATTRIBUTE]: true,
-          accessibility: menuAsToolbarBehavior,
-          className: chatMessageSlotClassNames.actionMenu,
-          styles: resolvedStyles.actionMenu,
-        }),
-      });
-
-      if (!actionMenuElement) {
-        return actionMenuElement;
-      }
-
-      const modifiers: PopperModifiers | undefined = positionActionMenu && [
-        // https://popper.js.org/docs/v2/modifiers/flip/
-        // Forces to flip only in "top-*" positions
-        { name: 'flip', options: { fallbackPlacements: ['top'] } },
-        overflow && {
-          name: 'preventOverflow',
-          options: { boundary: getScrollParent(messageNode) },
-        },
-      ];
-
-      return (
-        <Popper
-          enabled={positionActionMenu}
-          align="end"
-          modifiers={modifiers}
-          position="above"
-          positionFixed={overflow}
-          targetRef={messageNode}
-          {...getPopperPropsFromShorthand(actionMenu)}
-        >
-          {({ scheduleUpdate }) => {
-            updateActionsMenuPosition.current = scheduleUpdate;
-
-            return actionMenuElement;
-          }}
-        </Popper>
-      );
-    };
-
-    const childrenPropExists = childrenExist(children);
-    const rootClasses = childrenPropExists ? cx(classes.root, classes.content) : classes.root;
-
-    const ElementType = getElementType(props);
-    const unhandledProps = useUnhandledProps(composeOptions.handledProps, props);
-
-    const badgeElement = createShorthand(composeOptions.slots.label, badge, {
-      defaultProps: () => ({
-        className: chatMessageSlotClassNames.badge,
-        styles: resolvedStyles.badge,
-      }),
-    });
-
-    const reactionGroupElement = Reaction.Group.create(reactionGroup, {
-      defaultProps: () => ({
-        className: chatMessageSlotClassNames.reactionGroup,
-        styles: resolvedStyles.reactionGroup,
-      }),
-    });
-
-    const actionMenuElement = renderActionMenu();
-
-    const authorElement = createShorthand(composeOptions.slots.author, author, {
-      defaultProps: () => ({
-        size: 'small',
-        styles: resolvedStyles.author,
-        className: chatMessageSlotClassNames.author,
-      }),
-    });
-
-    const timestampElement = createShorthand(composeOptions.slots.timestamp, timestamp, {
-      defaultProps: () => ({
-        size: 'small',
-        styles: resolvedStyles.timestamp,
-        timestamp: true,
-        className: chatMessageSlotClassNames.timestamp,
-      }),
-    });
-
-    const messageContent = createShorthand(composeOptions.slots.content, content, {
-      defaultProps: () => ({
-        className: chatMessageSlotClassNames.content,
-        styles: resolvedStyles.content,
-      }),
-    });
-
-    const headerElement = createShorthand(composeOptions.slots.header, header, {
-      defaultProps: () => ({
-        className: chatMessageSlotClassNames.header,
-        styles: resolvedStyles.header,
-      }),
-      overrideProps: () => ({
-        children: (
-          <>
-            {authorElement}
-            {timestampElement}
-            {reactionGroupPosition === 'start' && reactionGroupElement}
-          </>
-        ),
-      }),
-    });
-
-    const element = (
-      <Ref innerRef={setMessageNode}>
-        {getA11Props.unstable_wrapWithFocusZone(
-          <ElementType
-            {...getA11Props('root', {
-              className: rootClasses,
-              onBlur: handleBlur,
-              onFocus: handleFocus,
-              onMouseEnter: handleMouseEnter,
-              ...rtlTextContainer.getAttributes({ forElements: [children] }),
-              ...unhandledProps,
-            })}
-          >
-            {childrenPropExists ? (
-              children
-            ) : (
-              <>
-                {actionMenuElement}
-                {badgePosition === 'start' && badgeElement}
-                {headerElement}
-                {messageContent}
-                {reactionGroupPosition === 'end' && reactionGroupElement}
-                {badgePosition === 'end' && badgeElement}
-              </>
-            )}
-          </ElementType>,
-        )}
-      </Ref>
-    );
-    setEnd();
-
-    return element;
-  },
-  {
-    className: chatMessageClassName,
-    displayName: 'ChatMessage',
-    slots: {
-      header: Box,
-      label: Label,
-      author: Text,
-      timestamp: Text,
-      content: Box,
-    },
-    handledProps: Object.keys(propTypes) as any,
-  },
-) as ComponentWithAs<'div', ChatMessageProps> & {
-  create: ShorthandFactory<ChatMessageProps>;
-};
-
-ChatMessage.displayName = 'ChatMessage';
-
-ChatMessage.defaultProps = {
-  accessibility: chatMessageBehavior,
-  badgePosition: 'end',
-  header: {},
-  positionActionMenu: true,
-  reactionGroupPosition: 'start',
-};
-
-ChatMessage.propTypes = propTypes as typeof ChatMessage.propTypes;
+ChatMessage.handledProps = Object.keys(ChatMessage.propTypes) as any;
 
 ChatMessage.create = createShorthandFactory({ Component: ChatMessage, mappedProp: 'content' });
 
