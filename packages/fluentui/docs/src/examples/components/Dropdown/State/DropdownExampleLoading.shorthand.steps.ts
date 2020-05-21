0a4f7e1e05056946855709b956434078a5d5f8f5
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-21 19:02:52 +0200

    fix(dropdown): Screenshot

--- packages/fluentui/docs/src/examples/components/Dropdown/State/DropdownExampleLoading.shorthand.steps.ts
+++ packages/fluentui/docs/src/examples/components/Dropdown/State/DropdownExampleLoading.shorthand.steps.ts
@@ -2,11 +2,29 @@ import { dropdownSlotClassNames } from '@fluentui/react-northstar';
 
 const selectors = {
   triggerButton: `.${dropdownSlotClassNames.toggleIndicator}`,
+  itemsList: `.${dropdownSlotClassNames.itemsList}`,
 };
 
 const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
-  steps: [builder => builder.click(selectors.triggerButton).snapshot('List with loading state')],
+  steps: [
+    (builder, keys) =>
+      builder
+        .click(selectors.triggerButton)
+        .snapshot('List with loading state')
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .keys(selectors.itemsList, keys.downArrow)
+        .snapshot('Showing loading in the bottom'),
+  ],
 };
 
 export default config;
