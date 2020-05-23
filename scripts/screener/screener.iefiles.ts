8ebe95ab6fb4f93569f1bb412a8a0ec5c9a27e1a
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-23 08:59:15 +0200

    chore(Screener): change regex

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -1,3 +1,3 @@
-const defaultExamples = ['Alert'].map(example => `/^${example}Example.tsx$/`);
+const defaultExamples = ['Alert'].map(example => new RegExp(`^${example}Example.tsx$`));
 
 export default [...defaultExamples];
