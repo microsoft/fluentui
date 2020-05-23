0a7125e4e28cdbc54851cfa0761de5868a81eb2f
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-23 10:26:03 +0200

    chore(Loader): Fix loader id usage

--- packages/fluentui/react-northstar/src/components/Loader/Loader.tsx
+++ packages/fluentui/react-northstar/src/components/Loader/Loader.tsx
@@ -155,7 +155,7 @@ const Loader: React.FC<WithAsProp<LoaderProps>> &
           getA11yProps('label', {
             className: loaderSlotClassNames.label,
             styles: resolvedStyles.label,
-            id: labelId,
+            id: labelId.current,
           }),
       })}
     </ElementType>
