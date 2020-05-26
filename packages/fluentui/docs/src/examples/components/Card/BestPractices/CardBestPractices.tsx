8c241dd3dd46bc58aa8a9f4278257144f7347edf
pompomon <pompomon@gmail.com>
2020-05-26 13:54:16 +0200

    Consistent naming

--- packages/fluentui/docs/src/examples/components/Card/BestPractices/CardBestPractices.tsx
+++ packages/fluentui/docs/src/examples/components/Card/BestPractices/CardBestPractices.tsx
@@ -6,12 +6,12 @@ const doList = [
   'Limit use of `aria-roledescription` property to clarifying the purpose of card, or to providing a more specific description of a widget.',
   'Use `cardsContainerBehavior` for cards container, so it would be navigable using arrows.',
   'Use `cardChildrenFocusableBehavior` for focusable cards with interactive children.',
-  'Use `cardFocusableBehavior` for focusable cards without inteactive children.',
-  'Use `cardSelectableBehavior` for selectable cards without inteactive children.',
+  'Use `cardFocusableBehavior` for focusable cards without interactive children.',
+  'Use `cardSelectableBehavior` for selectable cards without interactive children.',
 ];
 
 const dontList = [
-  'Use `onClick` property together with clickable children.',
+  'Use `onClick` property together with interactive children.',
   'Create selectable cards with a single text child that has `id`. It might cause incorrect narration in NVDA with virtual cursor navigation.',
 ];
 
