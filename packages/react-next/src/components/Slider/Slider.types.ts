0237a69b9a723f3bf25353b4960cf1d07fce37d3
Jon Schectman <joschect@microsoft.com>
2020-05-26 15:30:38 -0700

    Slider hook part 1. (#13260)
    * root props working
    
    * adding in hook file
    
    * labelprops working
    
    * sliderbox props working
    
    * container props working
    
    * minor switch to make origin from zero easier
    
    * cleanup
    
    * update api
    
    * Change files
    
    * fix aria-label and minor refactor
    
    * update api
    
    * Update change/@fluentui-react-next-2020-05-20-19-12-25-slider-hook.json
    
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>
    
    * fix aria-label decomp
    
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-next/src/components/Slider/Slider.types.ts
+++ packages/react-next/src/components/Slider/Slider.types.ts
@@ -10,6 +10,7 @@ export interface ISlider {
 
   focus: () => void;
 }
+export type ISliderState = any;
 
 /**
  * {@docCategory Slider}
@@ -84,9 +85,14 @@ export interface ISliderProps extends React.ClassAttributes<HTMLElement> {
 
   /**
    * A description of the Slider for the benefit of screen readers.
+   * @deprecated use aria-label instead.
    */
   ariaLabel?: string;
 
+  /**
+   * A description of the Slider for the benefit of screen readers.
+   */
+  'aria-label'?: string;
   /**
    * A text description of the Slider number value for the benefit of screen readers.
    * This should be used when the Slider number value is not accurately represented by a number.
