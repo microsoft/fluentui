6708d1aa889c847b30e3fc9cbb52a6804529ed8b
pompomon <pompomon@gmail.com>
2020-05-26 13:14:29 +0200

    PR review comments

--- packages/fluentui/docs/src/examples/components/Card/BestPractices/CardBestPractices.tsx
+++ packages/fluentui/docs/src/examples/components/Card/BestPractices/CardBestPractices.tsx
@@ -4,16 +4,15 @@ import ComponentBestPractices from '../../../../components/ComponentBestPractice
 
 const doList = [
   'Limit use of `aria-roledescription` property to clarifying the purpose of card, or to providing a more specific description of a widget.',
-  'Use `cardBehavior` for disabled cards, so it would not be focusable.',
   'Use `cardsContainerBehavior` for cards container, so it would be navigable using arrows.',
-  'Use `cardChildrenFocusableBehavior` for focusable cards with inteactive children.',
+  'Use `cardChildrenFocusableBehavior` for focusable cards with interactive children.',
   'Use `cardFocusableBehavior` for focusable cards without inteactive children.',
   'Use `cardSelectableBehavior` for selectable cards without inteactive children.',
 ];
 
 const dontList = [
   'Use `onClick` property together with clickable children.',
-  'Create selectable cards with a single text child that has `id`, that might cause incorrect narration in JAWS with virtual cursor navigation',
+  'Create selectable cards with a single text child that has `id`. It might cause incorrect narration in NVDA with virtual cursor navigation.',
 ];
 
 const CardBestPractices = () => {
