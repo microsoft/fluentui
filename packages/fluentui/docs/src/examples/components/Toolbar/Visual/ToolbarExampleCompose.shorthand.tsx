c2cd365b28a4a975d75e73c4620644adcc6d3659
Marija Najdova <mnajdova@gmail.com>
2020-05-27 11:08:51 +0200

    -expanded example -added missing variables

--- packages/fluentui/docs/src/examples/components/Toolbar/Visual/ToolbarExampleCompose.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Toolbar/Visual/ToolbarExampleCompose.shorthand.tsx
@@ -1,15 +1,108 @@
-import { Toolbar, ToolbarMenuDivider, Provider, compose, ThemeInput } from '@fluentui/react-northstar';
+import {
+  Toolbar,
+  ToolbarMenuDivider,
+  ToolbarDivider,
+  ToolbarItemWrapper,
+  ToolbarItem,
+  ToolbarItemIcon,
+  ToolbarRadioGroup,
+  ToolbarMenuRadioGroupWrapper,
+  ToolbarMenuRadioGroup,
+  Provider,
+  compose,
+  ThemeInput,
+} from '@fluentui/react-northstar';
 import * as React from 'react';
-import { MoreIcon, PauseIcon, PlayIcon, BoldIcon, UnderlineIcon } from '@fluentui/react-icons-northstar';
+import {
+  MoreIcon,
+  PauseIcon,
+  PlayIcon,
+  BoldIcon,
+  UnderlineIcon,
+  BookmarkIcon,
+  ChatIcon,
+  BulletsIcon,
+  ToDoListIcon,
+} from '@fluentui/react-icons-northstar';
 
 const ToolbarMenuDividerBlue = compose(ToolbarMenuDivider, {
   displayName: 'ToolbarMenuDividerBlue',
+});
+
+const ToolbarDividerGreen = compose(ToolbarDivider, {
+  displayName: 'ToolbarDividerGreen',
+});
+
+const ToolbarItemWrapperBlue = compose(ToolbarItemWrapper, {
+  displayName: 'ToolbarItemWrapperBlue',
+});
+
+const ToolbarItemIconPurple = compose(ToolbarItemIcon, {
+  displayName: 'ToolbarItemIconPurple',
+});
+
+const ToolbarItemGrey = compose(ToolbarItem, {
+  displayName: 'ToolbarItemGrey',
+  slots: {
+    wrapper: ToolbarItemWrapperBlue,
+    icon: ToolbarItemIconPurple,
+  },
+});
+
+const ToolbarRadioGroupRed = compose(ToolbarRadioGroup, {
+  displayName: 'ToolbarRadioGroupRed',
+});
+
+const ToolbarMenuRadioGroupWrapperOrange = compose(ToolbarMenuRadioGroupWrapper, {
+  displayName: 'ToolbarMenuRadioGroupWrapperOrange',
+});
+
+const ToolbarMenuRadioGroupViolet = compose(ToolbarMenuRadioGroup, {
+  displayName: 'ToolbarMenuRadioGroupViolet',
+  slots: {
+    wrapper: ToolbarMenuRadioGroupWrapperOrange,
+  },
 });
 
 const themeOverrides: ThemeInput = {
   componentVariables: {
     ToolbarMenuDividerBlue: {
       menuDividerBorder: 'lightblue',
+    },
+    ToolbarDividerGreen: {
+      dividerBorder: 'lightgreen',
+    },
+  },
+  componentStyles: {
+    ToolbarItemWrapperBlue: {
+      root: {
+        border: '1px dashed lightblue',
+      },
+    },
+    ToolbarItemIconPurple: {
+      root: {
+        color: 'purple',
+      },
+    },
+    ToolbarItemGrey: {
+      root: {
+        background: 'lightgrey',
+      },
+    },
+    ToolbarRadioGroupRed: {
+      root: {
+        border: '1px dashed darkred',
+      },
+    },
+    ToolbarMenuRadioGroupWrapperOrange: {
+      root: {
+        border: '1px dashed orange',
+      },
+    },
+    ToolbarMenuRadioGroupViolet: {
+      root: {
+        border: '1px solid violet',
+      },
     },
   },
 };
@@ -33,6 +126,22 @@ const ToolbarExampleMenuShorthand = () => {
             title: 'Underline',
           },
           {
+            key: 'divider',
+            kind: 'divider',
+            // TODO: replace this with slots after compose in enabled in Toolbar
+            children: (C, p) => <ToolbarDividerGreen {...p} />,
+          },
+          {
+            key: 'group',
+            kind: 'group',
+            items: [
+              { key: 'bookmark', icon: <BookmarkIcon /> },
+              { key: 'chat', icon: <ChatIcon /> },
+            ],
+            // TODO: replace this with slots after compose in enabled in Toolbar
+            children: (C, p) => <ToolbarRadioGroupRed {...p} />,
+          },
+          {
             icon: <MoreIcon />,
             key: 'more',
             active: true,
@@ -48,9 +157,20 @@ const ToolbarExampleMenuShorthand = () => {
                   children: (C, p) => <ToolbarMenuDividerBlue {...p} />,
                 },
                 'Without icon',
+                {
+                  key: 'group',
+                  kind: 'group',
+                  items: [
+                    { key: 'bullets', icon: <BulletsIcon /> },
+                    { key: 'to-do-list', icon: <ToDoListIcon /> },
+                  ],
+                  // TODO: replace this with slots after compose in enabled in ToolbarMenu
+                  children: (C, p) => <ToolbarMenuRadioGroupViolet {...p} />,
+                },
               ],
             },
             menuOpen: true,
+            children: (C, p) => <ToolbarItemGrey {...p} />,
           },
         ]}
       />
