c5f516aeadb04f38c27a4d3b4c10a4400f6e7cfc
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-05-27 21:25:15 +0200

    fix(UseKnob): remove eslint disable

--- packages/fluentui/docs-components/src/knobs/useKnob.ts
+++ packages/fluentui/docs-components/src/knobs/useKnob.ts
@@ -3,30 +3,39 @@ import * as React from 'react';
 import { KnobContext } from './KnobContexts';
 import { KnobDefinition, UseKnobOptions } from './types';
 
+function useActualOptions<P>(props: P) {
+  const actualProps = React.useRef<P>(props);
+
+  React.useEffect(() => {
+    actualProps.current = props;
+  });
+
+  return actualProps;
+}
+
 const useKnob = <T, O = unknown>(
   options: UseKnobOptions<T> & { type: KnobDefinition['type'] } & O,
 ): [T, (newValue: T) => void] => {
-  const { initialValue, name } = options;
-  const knobContext = React.useContext(KnobContext);
+  const stableOptions = useActualOptions(options);
+  const { initialValue, name } = stableOptions.current;
+  const contextRef = React.useRef(React.useContext(KnobContext));
+  const knobContext = contextRef.current;
 
   const value: T = knobContext.knobs[name] === undefined ? initialValue : knobContext.knobs[name].value;
   const setValue = (newValue: T) => {
     knobContext.setKnobValue(name, newValue);
   };
 
-  React.useEffect(
-    () => {
-      knobContext.registerKnob({
-        ...options,
-        value: initialValue,
-      });
+  React.useEffect(() => {
+    contextRef.current.registerKnob({
+      ...stableOptions.current,
+      value: stableOptions.current.initialValue,
+    });
 
-      return () => knobContext.unregisterKnob(name);
-    },
-    // Fix dependencies if there will be any issues
-    // eslint-disable-next-line
-    [name],
-  );
+    const name = stableOptions.current.name;
+    const context = contextRef.current;
+    return () => context.unregisterKnob(name);
+  }, [contextRef, stableOptions]);
 
   return [value, setValue];
 };
