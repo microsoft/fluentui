8b0f5cdaa0329952ec0b18cd59eac03855842a4e
Elizabeth Craig <elcraig@microsoft.com>
2020-05-27 17:38:31 -0700

    Only use tsbuildinfo for fluentui packages

--- scripts/tasks/ts.js
+++ scripts/tasks/ts.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { tscTask, argv } = require('just-scripts');
 const libPath = path.resolve(process.cwd(), 'lib');
 const srcPath = path.resolve(process.cwd(), 'src');
+// Temporary hack: only use tsbuildinfo file for things under packages/fluentui
+const isV0 = /[\\/]packages[\\/]fluentui[\\/]/.test(process.cwd());
 
 function getExtraTscParams(args) {
   return {
@@ -20,16 +22,26 @@ module.exports.ts = {
       ...extraOptions,
       outDir: 'lib-commonjs',
       module: 'commonjs',
-      tsBuildInfoFile: '.commonjs.tsbuildinfo',
+      ...(isV0 && { tsBuildInfoFile: '.commonjs.tsbuildinfo' }),
     });
   },
   esm: () => {
     const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib', module: 'esnext', tsBuildInfoFile: '.tsbuildinfo' });
+    return tscTask({
+      ...extraOptions,
+      outDir: 'lib',
+      module: 'esnext',
+      ...(isV0 && { tsBuildInfoFile: '.tsbuildinfo' }),
+    });
   },
   amd: () => {
     const extraOptions = getExtraTscParams(argv());
-    return tscTask({ ...extraOptions, outDir: 'lib-amd', module: 'amd', tsBuildInfoFile: '.amd.tsbuildinfo' });
+    return tscTask({
+      ...extraOptions,
+      outDir: 'lib-amd',
+      module: 'amd',
+      ...(isV0 && { tsBuildInfoFile: '.amd.tsbuildinfo' }),
+    });
   },
   commonjsOnly: () => {
     const extraOptions = getExtraTscParams(argv());
