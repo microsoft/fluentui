a132355fa61ca96922418bd5f03dd87fdbdf6a95
Elizabeth Craig <ecraig12345@gmail.com>
2020-05-29 14:06:44 -0700

    Update packages/fluentui/state/src/createManager.ts
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/state/src/createManager.ts
+++ packages/fluentui/state/src/createManager.ts
@@ -6,7 +6,7 @@ const createManager = <State, Actions extends AnyActions>(
   const { actions, debug, middleware = [], sideEffects = [], state } = config;
   const _state: State = { ...state } as State;
 
-  const getState = (): State => ({ ...state } as State);
+  const getState = (): State => ({ ..._state });
   const setState = (partial: Partial<State>): State => Object.assign(_state, partial);
 
   const manager: Manager<State, Actions> = {
