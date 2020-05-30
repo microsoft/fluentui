46ac02b810e871c3e15a332fe4f9d53a8c8627e5
David Zearing <dzearing@microsoft.com>
2020-05-29 18:55:10 -0700

    Update packages/react-compose/src/mergeProps.ts
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-compose/src/mergeProps.ts
+++ packages/react-compose/src/mergeProps.ts
@@ -33,7 +33,7 @@ export function mergeProps<TProps, TState = TProps>(
   // Resolve classes.
   resolveClasses(result, options.classes);
 
-  // Resolve inline styles.
+  // TODO: Resolve inline styles.
 
   return result;
 }
