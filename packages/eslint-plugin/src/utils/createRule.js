c8ca9cd06399f03cae116291f5c050f73ceeb2a3
Elizabeth Craig <elcraig@microsoft.com>
2020-05-30 07:36:44 -0700

    update rule URL to point to readme

--- packages/eslint-plugin/src/utils/createRule.js
+++ packages/eslint-plugin/src/utils/createRule.js
@@ -1,5 +1,5 @@
 const { ESLintUtils } = require('@typescript-eslint/experimental-utils');
 
 module.exports = ESLintUtils.RuleCreator(
-  name => `https://github.com/microsoft/fluentui/blob/master/packages/eslint-plugin/src/rules/${name}.js`,
+  name => `https://github.com/microsoft/fluentui/blob/master/packages/eslint-plugin/README.md#${name}`,
 );
