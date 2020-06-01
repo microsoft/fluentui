566a163b2978e51c47782d4f5909b9cf1e848447
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-01 14:22:01 +0200

    feat(Screener): fix path

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -83,7 +83,7 @@ const glob = require('glob');
 const config = require('../config').default;
 const files = glob.sync(config.paths.docs('**/*.steps.ts'));
 console.log(files);
-const ieSteps = files.filter(file => require(file).steps.browsers.includes('ie11'));
+const ieSteps = files.filter(file => require(file).steps?.browsers.includes('ie11'));
 const ieRegexes = ieSteps.map(file => new RegExp(file.replace(/\.steps\.ts$/, '.tsx')));
 
 export default ieRegexes;
