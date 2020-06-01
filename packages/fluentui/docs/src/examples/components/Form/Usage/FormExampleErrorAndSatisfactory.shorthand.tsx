4076f7712c202aff8a7a23e9e4c1d5c17c31f04f
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-01 21:21:03 +0200

    feat(FormField): add error examples

---
+++ packages/fluentui/docs/src/examples/components/Form/Usage/FormExampleErrorAndSatisfactory.shorthand.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Form, Button } from '@fluentui/react-northstar';
+import { PresenceAvailableIcon } from '@fluentui/react-icons-northstar';
+
+const fields = [
+  {
+    label: 'First name',
+    name: 'firstName',
+    id: 'first-name-shorthand',
+    key: 'first-name',
+    errorMessage: 'ERROR',
+    required: true,
+  },
+  {
+    label: 'Last name',
+    name: 'lastName',
+    id: 'last-name-shorthand',
+    key: 'last-name',
+    satisfactoryIndicator: <PresenceAvailableIcon />,
+    required: true,
+  },
+  { control: { as: Button, content: 'Submit' }, key: 'submit' },
+];
+
+const FormExampleErrorAndSatisfactory = () => (
+  <Form
+    onSubmit={() => {
+      alert('Form submitted');
+    }}
+    fields={fields}
+  />
+);
+
+export default FormExampleErrorAndSatisfactory;
--- packages/fluentui/docs/src/examples/components/Form/Usage/index.tsx
+++ packages/fluentui/docs/src/examples/components/Form/Usage/index.tsx
@@ -5,6 +5,11 @@ import ExampleSection from '../../../../components/ComponentDoc/ExampleSection';
 const Usage = () => (
   <ExampleSection title="Usage">
     <ComponentExample
+      title="Error Message and Satisfactory Indicator"
+      description="A form can have fields with error message or satisfactory indicator."
+      examplePath="components/Form/Usage/FormExampleErrorAndSatisfactory"
+    />
+    <ComponentExample
       title="Checkbox"
       description="A form can have a Checkbox as a field."
       examplePath="components/Form/Usage/FormExampleCheckbox"
