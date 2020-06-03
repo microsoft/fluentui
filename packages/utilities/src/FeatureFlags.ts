9cd352a86a6f751631eb3fa5646037c94fdc2cfe
Philip Kuo <phkuo@microsoft.com>
2020-06-02 21:17:14 -0700

    documentation

--- packages/utilities/src/FeatureFlags.ts
+++ packages/utilities/src/FeatureFlags.ts
@@ -1,5 +1,11 @@
 import * as React from 'react';
 
+/**
+ * FeatureFlags allows us to ship features off by default, unless the consuming app turns on that feature. This is
+ * intended to let a partner test a given feature first before releasing it to the rest of FluentUI consumers.
+ * The flags are provided via React Context.
+ */
+
 type FeatureFlags = {
   ModalAriaHidden?: boolean;
   TestFeature?: boolean; // used for testing only, replace with a real feature in .test when a second feature is added
