970a405ec304d682607e6373cb5bb3eee5ae5640
Xu Gao <xgao@microsoft.com>
2020-06-04 12:33:03 -0700

    use resolver

--- packages/react-next/src/components/Checkbox/useCheckboxClasses.tsx
+++ packages/react-next/src/components/Checkbox/useCheckboxClasses.tsx
@@ -1,11 +1,10 @@
-import { ICheckboxState, ICheckboxSlots } from './Checkbox.types';
-import { useGlobalClassNames } from '../../Styling/useGlobalClassNames';
 import * as classes from './Checkbox.scss';
-import { ClassFunction } from '@fluentui/react-compose';
-import { css, memoizeFunction } from '../../Utilities';
-import { GlobalClassNames as GlobalClassNamesType } from '../../Styling';
+import { createClassResolver } from '@fluentui/react-compose';
+import { useGlobalClassNames } from '../../Styling/useGlobalClassNames';
+import { css } from '../../Utilities';
+import { ICheckboxState, ICheckboxClasses } from './Checkbox.types';
 
-const GlobalClassNames: GlobalClassNamesType<Omit<ICheckboxSlots, 'input'>> = {
+const GlobalClassNames: Omit<ICheckboxClasses, 'input'> = {
   root: 'ms-Checkbox',
   container: 'ms-Checkbox-label',
   checkbox: 'ms-Checkbox-checkbox',
@@ -13,54 +12,18 @@ const GlobalClassNames: GlobalClassNamesType<Omit<ICheckboxSlots, 'input'>> = {
   text: 'ms-Checkbox-text',
 };
 
-const getStaticStylesMemoized = memoizeFunction(
-  (
-    // tslint:disable-next-line:no-any
-    globalClassNames: any,
-    className: string | undefined,
-    disabled: boolean | undefined,
-    reversed: boolean | undefined,
-    checked: boolean | undefined,
-    indeterminate: boolean | undefined,
-    isUsingCustomLabelRender: boolean | undefined,
-  ) => {
-    const propControlledClasses = [
-      disabled && classes.disabled,
-      reversed && classes.reversed,
-      checked && classes.checked,
-      indeterminate && classes.indeterminate,
-      isUsingCustomLabelRender && classes.isUsingCustomLabelRender,
-    ];
+const resolveClasses = createClassResolver(classes);
 
-    const rootStaticClasses = [
-      reversed && 'reversed',
-      checked && 'is-checked',
-      !disabled && 'is-enabled',
-      disabled && 'is-disabled',
-    ];
+export const useCheckboxClasses = (state: ICheckboxState) => {
+  const { reversed, checked, disabled } = state;
 
-    return {
-      root: css(className, classes.root, globalClassNames.root, ...rootStaticClasses, ...propControlledClasses),
-      container: css(classes.container, globalClassNames.container, ...propControlledClasses),
-      checkbox: css(classes.checkbox, globalClassNames.checkbox, ...propControlledClasses),
-      checkmark: css(classes.checkmark, globalClassNames.checkmark, ...propControlledClasses),
-      text: css(classes.text, globalClassNames.text, ...propControlledClasses),
-      input: css(classes.input, ...propControlledClasses),
-    };
-  },
-);
-
-export const useCheckboxClasses: ClassFunction = (state: ICheckboxState) => {
   const globalClassNames = useGlobalClassNames(GlobalClassNames);
-  const { className, disabled, isUsingCustomLabelRender, reversed, checked, indeterminate } = state;
+  const resolvedClasses = resolveClasses(state, globalClassNames);
+  resolvedClasses.root += ` ${css(
+    reversed && 'reversed',
+    checked && 'is-checked',
+    disabled ? 'is-disabled' : 'is-enabled',
+  )}`;
 
-  return getStaticStylesMemoized(
-    globalClassNames,
-    className,
-    disabled,
-    reversed,
-    checked,
-    indeterminate,
-    isUsingCustomLabelRender,
-  );
+  return resolvedClasses;
 };
