7e32fb9c3972c7cda2698b7a3c22dfa33cff8771
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-05 10:26:39 +0200

    feat(screener): exclude files

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -3,6 +3,8 @@ const config = require('../config').default;
 const files = glob.sync(config.paths.docs('**/*.steps.ts'));
 console.log(files);
 const ieSteps = files.filter(file => require(file).steps?.browsers?.includes('ie11'));
+const excludeIeFiles = files.filter(file => !require(file).steps?.browsers?.includes('ie11'));
 const ieRegexes = ieSteps.map(file => new RegExp(file.replace(/\.steps\.ts$/, '.tsx')));
+export const ieRegexesExcludes = excludeIeFiles.map(file => new RegExp(file.replace(/\.steps\.ts$/, '.tsx')));
 
 export default ieRegexes;
