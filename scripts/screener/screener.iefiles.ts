017528491afb432cc7f32ced873cb0c40a08ae7d
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-06 03:56:12 +0200

    feat(screener): regex

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -2,6 +2,5 @@ const glob = require('glob');
 const config = require('../config').default;
 const files = glob.sync(config.paths.docs('**/*.steps.ts'));
 const ieSteps = files.filter(file => require(file).default.browsers?.includes('ie11'));
-const ieRegexes = ieSteps.map(file => new RegExp(file.replace(/\.steps\.ts/, '.tsx')));
-
+const ieRegexes = ieSteps.map(file => new RegExp(file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')));
 export default ieRegexes;
