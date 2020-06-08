4e058b89af037da8b421ecc16b628238de011107
tomi-msft <66456876+tomi-msft@users.noreply.github.com>
2020-06-08 14:38:36 -0700

    Link: fix incorrect href in Link component's example page (#13511)
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Fixes #13445 
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    Update incorrect href in Link's example page to go to the right page
    
    #### Focus areas to test
    
    (optional)

--- packages/office-ui-fabric-react/src/components/Link/examples/Link.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Link/examples/Link.Basic.Example.tsx
@@ -6,9 +6,12 @@ export const LinkBasicExample: React.FunctionComponent = () => {
     <div>
       <p>
         When a link has an href,{' '}
-        <Link href="http://dev.office.com/fabric/components/link">it renders as an anchor tag.</Link> Without an href,{' '}
-        <Link>the link is rendered as a button</Link>. You can also use the disabled attribute to create a{' '}
-        <Link disabled={true} href="http://dev.office.com/fabric/components/link">
+        <Link href="https://developer.microsoft.com/en-us/fluentui#/controls/web/link">
+          it renders as an anchor tag.
+        </Link>{' '}
+        Without an href, <Link>the link is rendered as a button</Link>. You can also use the disabled attribute to
+        create a{' '}
+        <Link disabled={true} href="https://developer.microsoft.com/en-us/fluentui#/controls/web/link">
           disabled link.
         </Link>
       </p>
