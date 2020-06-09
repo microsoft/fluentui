287cdab03fe9f55d18f76bdce3bd6bb1809ba73d
Xu Gao <xgao@microsoft.com>
2020-06-09 14:32:42 -0700

    add comment

--- packages/styling/src/index.ts
+++ packages/styling/src/index.ts
@@ -4,4 +4,7 @@ export * from './utilities/index';
 export * from './interfaces/index';
 export * from './MergeStyles';
 import './version';
+
+// This file contains logic to initialize default theme.
+// Unfortunately we need to ensure this file is always imported when this package is referenced.
 import './styles/theme';
