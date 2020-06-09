3cc0a5b11a95016fe213afe34c1777edf4b65481
nebhatna <nebhatna@microsoft.com>
2020-06-08 22:27:01 -0700

    fixed the unit test

--- packages/experiments/src/components/UnifiedPicker/UnifiedPicker.test.tsx
+++ packages/experiments/src/components/UnifiedPicker/UnifiedPicker.test.tsx
@@ -5,34 +5,13 @@ import { ISelectedItemProps, ISelectedItemsListProps } from '../SelectedItemsLis
 import { IBaseFloatingSuggestionsProps, BaseFloatingSuggestions } from '../FloatingSuggestionsComposite';
 import { create } from 'react-test-renderer';
 import { SelectedItemsList } from '../SelectedItemsList';
+import { IFloatingSuggestionItem, IFloatingSuggestionItemProps } from '../../FloatingSuggestionsComposite';
 
 export interface ISimple {
   key: string;
   name: string;
 }
 type InputElementWrapper = ReactWrapper<React.InputHTMLAttributes<any>, any>;
-
-function onResolveSuggestions(text: string): ISimple[] {
-  return [
-    'black',
-    'blue',
-    'brown',
-    'cyan',
-    'green',
-    'magenta',
-    'mauve',
-    'orange',
-    'pink',
-    'purple',
-    'red',
-    'rose',
-    'violet',
-    'white',
-    'yellow',
-  ]
-    .filter((tag: string) => tag.toLowerCase().indexOf(text.toLowerCase()) === 0)
-    .map((item: string) => ({ key: item, name: item }));
-}
 
 const basicSuggestionRenderer = (props: ISimple) => {
   return <div key={props.key}> {props.name} </div>;
@@ -51,9 +30,9 @@ const basicRenderSelectedItemsList = (props: ISelectedItemsListProps<ISimple>) =
 };
 
 let floatingPickerProps = ({
-  onResolveSuggestions: onResolveSuggestions,
+  onRenderSuggestion: basicSuggestionRenderer,
   onRenderSuggestionsItem: basicSuggestionRenderer,
-  isSuggestionVisible: true,
+  isSuggestionsVisible: false,
   suggestions: [],
   targetElement: null,
 } as unknown) as IBaseFloatingSuggestionsProps<ISimple>;
@@ -79,13 +58,16 @@ describe('UnifiedPicker', () => {
 
   it('renders BaseExtendedPicker correctly with selected and suggested items', () => {
     floatingPickerProps = ({
-      onResolveSuggestions: onResolveSuggestions,
+      onRenderSuggestion: basicSuggestionRenderer,
       onRenderSuggestionsItem: basicSuggestionRenderer,
-      isSuggestionVisible: true,
+      isSuggestionsVisible: false,
       suggestions: [
         {
-          name: 'yellow',
-          key: 'yellow',
+          id: '1',
+          displayText: 'Suggestion 1',
+          item: { name: 'yellow', key: 'yellow' },
+          isSelected: false,
+          showRemoveButton: true,
         },
       ],
       targetElement: null,
@@ -115,34 +97,44 @@ describe('UnifiedPicker', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
   it('force resolves to the first suggestion', () => {
     floatingPickerProps = ({
-      onResolveSuggestions: onResolveSuggestions,
-      onRenderSuggestionsItem: basicSuggestionRenderer,
-      isSuggestionVisible: true,
-      suggestions: [
-        {
-          name: 'yellow',
-          key: 'yellow',
-        },
-      ],
+      onRenderSuggestion: basicSuggestionRenderer,
       targetElement: null,
     } as unknown) as IBaseFloatingSuggestionsProps<ISimple>;
 
     selectedItemsListProps = {
       onRenderItem: basicItemRenderer,
-      selectedItems: [
-        {
-          name: 'red',
-          key: 'red',
-        },
-        {
-          name: 'green',
-          key: 'green',
-        },
-      ],
     };
-    const _onInputChange = jest.fn();
+
+    function _startsWith(text: string, filterText: string): boolean {
+      return text.toLowerCase().indexOf(filterText.toLowerCase()) === 0;
+    }
+    let suggestionList;
+    const _onInputChange = (filterText: string): void => {
+      const allColors = [
+        'black',
+        'blue',
+        'brown',
+        'cyan',
+        'green',
+        'magenta',
+        'mauve',
+        'orange',
+        'pink',
+        'purple',
+        'red',
+        'rose',
+        'violet',
+        'white',
+        'yellow',
+      ];
+      const colorSuggestions = allColors.filter((item: string) => _startsWith(item || '', filterText));
+      suggestionList = colorSuggestions.map(item => {
+        return ({ item: item, isSelected: false } as unknown) as IFloatingSuggestionItem<ISimple>;
+      });
+    };
     const wrapper = mount(
       <UnifiedPicker
         floatingSuggestionProps={floatingPickerProps}
@@ -156,7 +148,16 @@ describe('UnifiedPicker', () => {
     expect(wrapper.find('.ms-FloatingSuggestions')).toHaveLength(1);
     const inputElement: InputElementWrapper = wrapper.find('input');
     inputElement.simulate('input', { target: { value: 'bl' } });
+    floatingPickerProps = ({
+      onRenderSuggestion: basicSuggestionRenderer,
+      targetElement: null,
+      suggestions: suggestionList,
+    } as unknown) as IBaseFloatingSuggestionsProps<ISimple>;
+
+    // Due to https://github.com/enzymejs/enzyme/issues/2042, enzyme does not re-render when
+    // we call update. Will use the array to validate the right results are returned, till this
+    // is fixed by enzyme.
     wrapper.update();
-    expect(_onInputChange).toBeCalledTimes(1);
+    expect(suggestionList).toHaveLength(2);
   });
 });
