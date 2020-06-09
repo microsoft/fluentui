fcd949a4b8e2287a04ef1d07ecd08e30b9c3f163
Elizabeth Craig <elcraig@microsoft.com>
2020-06-09 10:07:01 -0700

    fix task options

--- scripts/tasks/eslint.js
+++ scripts/tasks/eslint.js
@@ -1,17 +1,14 @@
 // @ts-check
 
 const { eslintTask } = require('just-scripts');
-const fs = require('fs');
 const path = require('path');
 
-let configPath = path.join(process.cwd(), '.eslintrc.json');
-if (!fs.existsSync(configPath)) {
-  configPath = path.join(process.cwd(), '.eslintrc.js');
-}
-
 exports.eslint = eslintTask({
-  configPath,
   // TODO: also lint config files?
   files: [path.join(process.cwd(), 'src')],
+  extensions: '.ts,.tsx,.js,.jsx',
   fix: process.argv.includes('--fix'),
+  // If requested, display a table with 10 slowest rules after running.
+  // (to display more, edit the `slice` call in the `display` function of node_modules/eslint/lib/linter/timing.js)
+  timing: process.argv.includes('--timing'),
 });
