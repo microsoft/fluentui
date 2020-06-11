f70184e373260900208611fa2e297729d99bfe5e
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-11 20:28:07 +0200

    feat(FormExample): remove import coment

--- packages/fluentui/docs/src/examples/components/Form/Usage/FormExampleErrorAndSuccessful.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Form/Usage/FormExampleErrorAndSuccessful.shorthand.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Form, Button } from '@fluentui/react-northstar';
-// import { PresenceAvailableIcon } from '@fluentui/react-icons-northstar';
 
 const fields = [
   {
