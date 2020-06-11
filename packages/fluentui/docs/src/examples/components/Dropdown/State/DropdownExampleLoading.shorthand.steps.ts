f582e3a790531bf88506751ea3e6245b0e1c98db
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-11 10:27:14 +0200

    feat(Screener): limit dropdown examples

--- packages/fluentui/docs/src/examples/components/Dropdown/State/DropdownExampleLoading.shorthand.steps.ts
+++ packages/fluentui/docs/src/examples/components/Dropdown/State/DropdownExampleLoading.shorthand.steps.ts
@@ -9,9 +9,9 @@ const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: [
     builder => builder.click(selectors.triggerButton).snapshot('List with loading state'),
-    (builder, keys) => builder.keys(selectors.input, keys.upArrow).snapshot('showing loading in the bottom'),      
+    (builder, keys) => builder.keys(selectors.input, keys.upArrow).snapshot('showing loading in the bottom'),
   ],
-  browsers: ['ie11'],
+  // browsers: ['ie11'],
 };
 
 export default config;
