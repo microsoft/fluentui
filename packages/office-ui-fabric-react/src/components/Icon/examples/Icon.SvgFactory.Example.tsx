084561446b2b0012e34595d5e65c7a68083112bf
Xu Gao <xugao0131@hotmail.com>
2020-06-11 17:52:12 -0700

    react-icons: remove dead code and merge-styles (#13517)
    * react-icons: remove dead code and merge-styles
    
    * Change files
    
    * update test
    
    * Change files
    
    * fix icon css
    
    * Change files
    
    * remove change files
    
    * css fixes
    
    * update package.json
    
    * remove unneeded dep

--- packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
+++ packages/office-ui-fabric-react/src/components/Icon/examples/Icon.SvgFactory.Example.tsx
@@ -4,21 +4,17 @@ import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
 
 const classes = mergeStyleSets({
-  root: {
-    selectors: {
-      '> *': {
-        height: 50,
-        width: 50,
-        marginRight: 25,
-      },
-    },
-  },
   cell: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     margin: '80px',
     float: 'left',
+    height: '50px',
+    width: '50px',
+  },
+  icon: {
+    fontSize: '50px',
   },
   code: {
     background: '#f2f2f2',
@@ -32,7 +28,7 @@ const classes = mergeStyleSets({
 });
 
 const icons = Object.keys(ReactIcons).reduce((acc: React.FC[], exportName) => {
-  if (!!(ReactIcons as any)[exportName].displayName) {
+  if (!!(ReactIcons as any)[exportName]?.displayName) {
     acc.push((ReactIcons as any)[exportName] as React.FunctionComponent);
   }
 
@@ -60,14 +56,16 @@ export const IconSvgFactoryExample: React.FunctionComponent = () => {
           Next
         </PrimaryButton>
       </div>
-      <div className={classes.root}>
-        {icons.slice((page - 1) * 100, (page - 1) * 100 + 100).map(Icon => (
-          <div key={Icon.displayName} className={classes.cell}>
-            <Icon />
-            <br />
-            <code className={classes.code}>{Icon.displayName}</code>
-          </div>
-        ))}
+      <div>
+        {icons
+          .slice((page - 1) * 100, (page - 1) * 100 + 100)
+          .map((Icon: React.FunctionComponent<ReactIcons.ISvgIconProps>) => (
+            <div key={Icon.displayName} className={classes.cell}>
+              <Icon className={classes.icon} />
+              <br />
+              <code className={classes.code}>{Icon.displayName}</code>
+            </div>
+          ))}
       </div>
     </div>
   );
