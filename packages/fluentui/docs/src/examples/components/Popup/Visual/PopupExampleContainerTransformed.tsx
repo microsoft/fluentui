35fe45644fcce5e7397b9ce4293ad0a094a372d5
Charles Assunção <junioassuncaocharles@gmail.com>
2020-06-15 20:18:31 +0200

    Update packages/fluentui/docs/src/examples/components/Popup/Visual/PopupExampleContainerTransformed.tsx
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/docs/src/examples/components/Popup/Visual/PopupExampleContainerTransformed.tsx
+++ packages/fluentui/docs/src/examples/components/Popup/Visual/PopupExampleContainerTransformed.tsx
@@ -3,8 +3,17 @@ import { Button, Popup } from '@fluentui/react-northstar';
 import { MoreIcon } from '@fluentui/react-icons-northstar';
 
 const PopupExampleContainerTransformed = () => (
-  <div style={{ transform: 'translate3d(0,0,0)', margin: 200, border: '2px solid red' }}>
-    <Popup content="This popup is rendered next to the trigger." open positionFixed>
+  <div
+    style={{
+      border: '2px solid red',
+      margin: 200,
+      height: 400,
+      width: 400,
+      overflow: 'scroll',
+      transform: 'translate3d(0,0,0)',
+    }}
+  >
+    <Popup content="This popup is rendered next to the trigger." inline open positionFixed>
       <Button icon={<MoreIcon />} title="Show popup" />
     </Popup>
   </div>
