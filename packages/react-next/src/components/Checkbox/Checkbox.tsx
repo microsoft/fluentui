a26d24d3176f298433b5648a6dd8b03d89307eba
Xu Gao <xgao@microsoft.com>
2020-06-15 12:58:14 -0700

    merge

--- packages/react-next/src/components/Checkbox/Checkbox.tsx
+++ packages/react-next/src/components/Checkbox/Checkbox.tsx
@@ -1,14 +1,14 @@
-import { CheckboxBase } from './Checkbox.base';
 import { compose, ComposeOptions } from '@fluentui/react-compose';
+import { CheckMarkIcon } from '@fluentui/react-icons';
+import { CheckboxBase } from './Checkbox.base';
 import { useCheckboxClasses } from './useCheckboxClasses';
 import { ICheckboxProps } from './Checkbox.types';
-import { Icon } from '../../Icon';
 
 const composeOptions: ComposeOptions = {
   displayName: 'Checkbox',
   classes: useCheckboxClasses,
   slots: {
-    checkmark: Icon,
+    checkmark: CheckMarkIcon,
   },
   state: (parentState: ICheckboxProps) => {
     return {
