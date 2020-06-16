dab7e327dc4b0677b5136fad81cef3ee280214af
Esteban Munoz <esteban.230@hotmail.com>
2020-06-16 17:18:32 -0600

    React conformance - First groups of rules (#13553)
    #### Pull request checklist
    
    - [ ] Addresses an existing issue: Fixes #0000
    - [ ] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    Adding some of the rules. These are the tests were added: 
    
    1a- Component file exports a valid React element type (pre-runtime test: react-docgen-typescript can find an element)
    3- Has a docblock, 5 to 25 words
    (use react-docgen-typescript)
    
    1b- Component file exports a valid React element type (runtime test: require() file path, look for an export with expected name, attempt to render it)
    2- Constructor/component is a named function (or has displayName)
    16- Has static displayName matching constructor name
    4- Constructor/component name matches filename
    (similar to v0 but use react-docgen-typescript to get component info)

---
+++ packages/react-conformance/src/defaultTests.tsx
@@ -0,0 +1,69 @@
+import { TestObject, TestingOptions } from './types';
+import { ComponentDoc } from 'react-docgen-typescript';
+import { mount } from 'enzyme';
+import * as React from 'react';
+import * as _ from 'lodash';
+import * as path from 'path';
+
+export const defaultTests: TestObject = {
+  /** Component has a docblock with 5 to 25 words */
+  'has-docblock': (componentInfo: ComponentDoc, testInfo: TestingOptions) => {
+    const maxWords = 25;
+    const minWords = 5;
+
+    // No need to check if the description is undefined, ComponentDoc.description is a "string" not "string | undefined"
+    it(`has a docblock with ${minWords} to ${maxWords} words`, () => {
+      expect(_.words(componentInfo.description)).toBeGreaterThanOrEqual(minWords);
+      expect(_.words(componentInfo.description)).toBeLessThanOrEqual(maxWords);
+    });
+  },
+
+  /** Component file exports a valid React element type  */
+  'exports-component': (componentInfo: ComponentDoc, testInfo: TestingOptions) => {
+    it(`exports component from file under correct name`, () => {
+      const componentFile = require(testInfo.componentPath);
+      if (testInfo.useDefaultExport) {
+        expect(componentFile.default).toBe(testInfo.Component);
+      } else {
+        expect(componentFile[testInfo.displayName]).toBe(testInfo.Component);
+      }
+    });
+  },
+
+  /** Component file exports a valid React element and can render it */
+  'component-renders': (componentInfo: ComponentDoc, testInfo: TestingOptions) => {
+    it(`renders`, () => {
+      const { requiredProps, Component } = testInfo;
+      const mountedComponent = mount(<Component {...requiredProps} />);
+      expect(mountedComponent.exists()).toBeTruthy();
+    });
+  },
+
+  /**
+   * if functional component: component has a displayName
+   * else: component's constructor is a named function and matches displayName
+   */
+  'component-has-displayname': (componentInfo: ComponentDoc, testInfo: TestingOptions) => {
+    const { Component } = testInfo;
+    if (Component.prototype.constructor) {
+      it(`constructor is a named function and matches displayName`, () => {
+        const constructorName = Component.prototype.constructor.name;
+        expect(constructorName).toEqual(testInfo.displayName);
+      });
+    } else {
+      it(`has a displayName`, () => {
+        expect(Component.displayName).toEqual(testInfo.displayName);
+      });
+    }
+  },
+
+  /** Constructor/component name matches filename */
+  'name-matches-filename': (componentInfo: ComponentDoc, testInfo: TestingOptions) => {
+    it(`Component/constructor name matches filename`, () => {
+      const { componentPath, displayName } = testInfo;
+      const fileName = path.basename(componentPath, path.extname(componentPath));
+
+      expect(displayName).toEqual(fileName);
+    });
+  },
+};
