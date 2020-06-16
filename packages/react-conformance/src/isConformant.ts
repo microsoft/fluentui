ea062de8709ca6819c2ed186e3feaf0a56680c15
Elizabeth Craig <ecraig12345@gmail.com>
2020-06-16 15:23:51 -0700

    Apply suggestions from code review

--- packages/react-conformance/src/isConformant.ts
+++ packages/react-conformance/src/isConformant.ts
@@ -23,14 +23,11 @@ export function isConformant(testInfo: TestingOptions, extraTests?: TestObject) 
     }
   } else {
     if (components.length === 0) {
-      throw Error('No exported components in path: ' + componentPath);
+      throw new Error('No exported components in path: ' + componentPath);
     } else {
-      throw Error(
-        `No component with name '${displayName}' was found, these are the exported components: {${components
-          .map(component => {
-            return component.displayName;
-          })
-          .toString()}} in path ${componentPath}`,
+      throw new Error(
+        `No component with name '${displayName}' was found at ${componentPath}. ` +
+          `These are the exported component names: ${components.map(component => component.displayName).join(', ')}`,
       );
     }
   }
