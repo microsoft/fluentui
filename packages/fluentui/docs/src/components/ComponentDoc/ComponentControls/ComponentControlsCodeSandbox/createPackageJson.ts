f051443647506e154d94a7a735f5a1766d111afa
Oleksandr Fediashov <olfedias@microsoft.com>
2020-06-17 10:03:51 +0200

    docs: fix required version of CSB package, improve dependency generation

--- packages/fluentui/docs/src/components/ComponentDoc/ComponentControls/ComponentControlsCodeSandbox/createPackageJson.ts
+++ packages/fluentui/docs/src/components/ComponentDoc/ComponentControls/ComponentControlsCodeSandbox/createPackageJson.ts
@@ -5,21 +5,32 @@ import { ComponentSourceManagerLanguage } from '../../ComponentSourceManager';
 
 const name = 'fluent-ui-example';
 const description = 'An exported example from Fluent UI React, https://aka.ms/fluent-ui/';
-const dependencies = {
-  ..._.mapValues(imports, pkg => pkg.version),
-  // required to enable all features due old templates in https://github.com/codesandbox/codesandbox-importers
-  // https://github.com/microsoft/fluent-ui-react/issues/1519
-  'react-scripts': 'latest',
-};
 
-const createPackageJson = (mainFilename: string, language: ComponentSourceManagerLanguage) => ({
+function createDependencies(code: string) {
+  // Will include only required packages intentionally like "react" or required by a current example
+  const filteredPackages = _.pickBy(
+    imports,
+    (declaration, name) => declaration.required || new RegExp(`from ['|"]${name}['|"]`).exec(code),
+  );
+
+  console.log(filteredPackages);
+
+  return {
+    ..._.mapValues(filteredPackages, pkg => pkg.version),
+    // required to enable all features due old templates in https://github.com/codesandbox/codesandbox-importers
+    // https://github.com/microsoft/fluent-ui-react/issues/1519
+    'react-scripts': 'latest',
+  };
+}
+
+const createPackageJson = (mainFilename: string, code: string, language: ComponentSourceManagerLanguage) => ({
   content: JSON.stringify(
     {
       name,
       version: '1.0.0',
       description,
       main: mainFilename,
-      dependencies,
+      dependencies: createDependencies(code),
     },
     null,
     2,
