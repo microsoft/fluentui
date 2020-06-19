d014fb5e1930636ded12f1c650ba09c7ad517d7f
David Zearing <dzearing@microsoft.com>
2020-06-19 10:14:14 -0700

    useMergedRefs update to be immutable.

--- packages/react-hooks/src/useMergedRefs.ts
+++ packages/react-hooks/src/useMergedRefs.ts
@@ -1,19 +1,27 @@
-import * as React from 'react';
+import { useState, useCallback, Ref, MutableRefObject } from 'react';
 
 /**
  * React hook to merge multiple React refs (either MutableRefObjects or ref callbacks) into a single ref callback that
  * updates all provided refs
  * @param refs- Refs to collectively update with one ref value.
  */
-export function useMergedRefs<T>(...refs: React.Ref<T>[]): (instance: T) => void {
-  return React.useCallback((value: T) => {
-    refs.forEach(ref => {
+export function useMergedRefs<T>(...refObjects: React.Ref<T>[]): (instance: T) => void {
+  const [state] = useState<{ refs: (Ref<T> | undefined)[] }>({ refs: [] });
+
+  // Cache refs in immutatable state object.
+  state.refs = refObjects;
+
+  // Return cached callback which refers to mutable refs array within the immutable state.
+  return useCallback((value: T) => {
+    const { refs } = state;
+
+    for (const ref of refs) {
       if (typeof ref === 'function') {
         ref(value);
       } else if (ref) {
         // work around the immutability of the React.Ref type
-        ((ref as unknown) as React.MutableRefObject<T>).current = value;
+        ((ref as unknown) as MutableRefObject<T>).current = value;
       }
-    });
-  }, refs);
+    }
+  }, []);
 }
