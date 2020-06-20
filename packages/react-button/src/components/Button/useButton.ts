6f9b50d786a07252453c0cafd19a2a3da3f8e09a
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-06-19 18:13:38 -0700

    Button: Mixing style coming from slotProps.

--- packages/react-button/src/components/Button/useButton.ts
+++ packages/react-button/src/components/Button/useButton.ts
@@ -1,13 +1,18 @@
-import { tokensToStyleObject } from '@fluentui/react-theme-provider';
+import { ComposePreparedOptions } from '@fluentui/react-compose';
+import { getStyleFromPropsAndOptions } from '@fluentui/react-theme-provider';
 import { ButtonProps, ButtonState } from './Button.types';
 
 /**
  * The useButton hook processes the Button component props and returns state.
  * @param props - Button props to derive state from.
  */
-export const useButton = (props: ButtonProps): ButtonState => {
+export const useButton = (
+  props: ButtonProps,
+  ref: React.Ref<HTMLElement>,
+  options: ComposePreparedOptions,
+): ButtonState => {
   return {
     ...props,
-    style: { ...props.style, ...tokensToStyleObject(props.tokens, '--button') },
+    style: getStyleFromPropsAndOptions(props, options),
   };
 };
