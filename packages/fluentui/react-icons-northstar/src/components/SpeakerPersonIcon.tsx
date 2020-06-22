45779a1d5dc5a7efbe5301f5a2297fb04b1269d7
Marija Najdova <mnajdova@gmail.com>
2020-06-22 15:23:39 +0200

    cleanup

--- packages/fluentui/react-icons-northstar/src/components/SpeakerPersonIcon.tsx
+++
@@ -1,8 +0,0 @@
-import createSvgIcon from '../utils/createSvgIcon';
-
-const SpeakerPersonIcon = createSvgIcon({
-  svg: ({ classes, icons }) => (icons['speaker-person'] ? icons['speaker-person'].icon({ classes }) : null),
-  displayName: 'SpeakerPersonIcon',
-});
-
-export default SpeakerPersonIcon;
