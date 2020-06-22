c737d59c23f1f15ef16392717a7a7e498bf4f457
Tomi Olubeko <ololubek@microsoft.com>
2020-06-22 14:14:37 -0700

    Fix variable declaration so options for ChoiceGroup and DropDown can render

--- apps/codesandbox-react-template/src/index.tsx
+++ apps/codesandbox-react-template/src/index.tsx
@@ -11,23 +11,23 @@ import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
 
 let _rootDiv: HTMLElement;
-let choicegroupOptions: [
-  { key: 'A'; text: 'Option A' },
-  { key: 'B'; text: 'Option B' },
-  { key: 'C'; text: 'Option C'; disabled: true },
-  { key: 'D'; text: 'Option D' },
+let choicegroupOptions = [
+  { key: 'A', text: 'Option A' },
+  { key: 'B', text: 'Option B' },
+  { key: 'C', text: 'Option C', disabled: true },
+  { key: 'D', text: 'Option D' },
 ];
-let dropdownOptions: [
-  { key: 'fruitsHeader'; text: 'Fruits'; itemType: DropdownMenuItemType.Header },
-  { key: 'apple'; text: 'Apple' },
-  { key: 'banana'; text: 'Banana' },
-  { key: 'orange'; text: 'Orange'; disabled: true },
-  { key: 'grape'; text: 'Grape' },
-  { key: 'divider_1'; text: '-'; itemType: DropdownMenuItemType.Divider },
-  { key: 'vegetablesHeader'; text: 'Vegetables'; itemType: DropdownMenuItemType.Header },
-  { key: 'broccoli'; text: 'Broccoli' },
-  { key: 'carrot'; text: 'Carrot' },
-  { key: 'lettuce'; text: 'Lettuce' },
+let dropdownOptions = [
+  { key: 'fruitsHeader', text: 'Fruits', itemType: DropdownMenuItemType.Header },
+  { key: 'apple', text: 'Apple' },
+  { key: 'banana', text: 'Banana' },
+  { key: 'orange', text: 'Orange', disabled: true },
+  { key: 'grape', text: 'Grape' },
+  { key: 'divider_1', text: '-', itemType: DropdownMenuItemType.Divider },
+  { key: 'vegetablesHeader', text: 'Vegetables', itemType: DropdownMenuItemType.Header },
+  { key: 'broccoli', text: 'Broccoli' },
+  { key: 'carrot', text: 'Carrot' },
+  { key: 'lettuce', text: 'Lettuce' },
 ];
 let dropdownStyles = {
   dropdown: { width: 300 },
@@ -43,13 +43,7 @@ function start(): void {
     <Fabric>
       <Stack gap={8}>
         <Checkbox label="Unchecked checkbox (uncontrolled)" />
-        <ChoiceGroup defaultSelectedKey="B" options={choicegroupOptions} />
-        <Dropdown
-          placeholder="Select an option"
-          label="Basic uncontrolled example"
-          options={dropdownOptions}
-          styles={dropdownStyles}
-        />
+        <ChoiceGroup defaultSelectedKey="B" options={choicegroupOptions} label="Pick One" required={true} />
         <Slider
           label="Snapping slider example"
           min={0}
@@ -61,6 +55,12 @@ function start(): void {
         />
         <TextField label="Standard" />
         <Toggle label="Enabled and checked" defaultChecked={true} onText="On" offText="Off" />
+        <Dropdown
+          placeholder="Select an option"
+          label="Basic uncontrolled example"
+          options={dropdownOptions}
+          styles={dropdownStyles}
+        />
       </Stack>
     </Fabric>,
     _rootDiv,
