e50f8018c00e6368dd81f3ab4d9cb31f06d0f690
Elizabeth Craig <elcraig@microsoft.com>
2020-06-22 22:37:24 -0700

    fix tsx-editor tests

--- packages/tsx-editor/jest.config.js
+++ packages/tsx-editor/jest.config.js
@@ -1,9 +1,11 @@
-let { createConfig, resolveMergeStylesSerializer } = require('@uifabric/build/jest/jest-resources');
-let path = require('path');
+const { createConfig } = require('@uifabric/build/jest/jest-resources');
 
 const config = createConfig({
-  setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
-  snapshotSerializers: [resolveMergeStylesSerializer()],
+  moduleNameMapper: {
+    'react-syntax-highlighter/dist/esm/(.*)$': 'react-syntax-highlighter/dist/cjs/$1',
+    // have to manually add this one because monaco-editor intentionally doesn't have a `main`
+    '@uifabric/monaco-editor/lib/(.*)$': '@uifabric/monaco-editor/lib-commonjs/$1',
+  },
 });
 
 module.exports = config;
