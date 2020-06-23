5a839bfb04260c4a4c011f28c1dc9581b2bda89d
dzearing <dzearing@hotmail.com>
2020-06-22 18:48:41 -0700

    Adding missing boxshadow and width.

--- packages/react-button/src/components/Button/Button.types.tsx
+++ packages/react-button/src/components/Button/Button.types.tsx
@@ -99,6 +99,8 @@ export type ButtonTokens = ColorPlateSet & {
   iconSize: string;
   borderRadius: string;
   borderWidth: string;
+  boxShadow: string;
+  width: string;
 
   size: {
     smallest: string;
