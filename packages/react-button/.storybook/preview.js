feb62746b89cc945489b921c1ad73580837466ce
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-06-24 16:51:50 -0700

    Adding withThemeProvider decorator.

--- packages/react-button/.storybook/preview.js
+++ packages/react-button/.storybook/preview.js
@@ -1,13 +1,17 @@
 import { initializeIcons } from '@uifabric/icons';
 import generateStoriesFromExamples from '@uifabric/build/storybook/generateStoriesFromExamples';
 import { configure, addParameters, addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
 import { withA11y } from '@storybook/addon-a11y';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 import { withPerformance } from 'storybook-addon-performance';
+import { withThemeProvider } from './decorators/withThemeProvider';
 
 addDecorator(withPerformance);
 addDecorator(withInfo());
 addDecorator(withA11y());
+addDecorator(withKnobs({ escapeHTML: false }));
+addDecorator(withThemeProvider);
 addParameters({
   a11y: {
     manual: true,
@@ -24,6 +28,16 @@ function loadStories() {
   req.keys().forEach(key => {
     generateStoriesFromExamples({ key, req, stories });
   });
+
+  // Wrap examples with ThemeProvider
+  for (let [key, story] of stories) {
+    Object.keys(story).forEach(exampleName => {
+      const example = story[exampleName];
+      if (typeof example === 'function') {
+        story[exampleName] = () => example();
+      }
+    });
+  }
 
   // convert stories Set to array
   return [...stories.values()];
--- packages/react-button/src/components/Button/FluentButton.stories.tsx
+++ packages/react-button/src/components/Button/FluentButton.stories.tsx
@@ -1,48 +1,8 @@
 import * as React from 'react';
+import { Stack, Text } from 'office-ui-fabric-react';
 import { Button } from './Button';
-import { PartialTheme, ThemeProvider } from '@fluentui/react-theme-provider';
-import { Stack, Text, Toggle } from 'office-ui-fabric-react';
+import { ButtonProps } from './Button.types';
 import * as classes from './Button.stories.scss';
-
-const FluentTheme: PartialTheme = {
-  tokens: {
-    palette: {
-      accent: '#0078D4',
-    },
-
-    body: {
-      background: 'white',
-      contentColor: 'black',
-    },
-
-    accent: {
-      background: 'var(--palette-accent)',
-      disabled: {
-        background: '#FAFAFA',
-      },
-      hovered: {
-        background: '#0072C9',
-      },
-      pressed: {
-        background: '#0078D4',
-      },
-    },
-
-    button: {
-      background: '#F5F5F5',
-      disabled: {
-        background: '#FAFAFA',
-      },
-      hovered: {
-        background: '#F2F2F2',
-      },
-      pressed: {
-        background: '#F7F7F7',
-      },
-    },
-  },
-  stylesheets: [],
-};
 
 const ButtonVariants = (props: ButtonProps) => (
   <div className={classes.hStack}>
@@ -54,52 +14,46 @@ const ButtonVariants = (props: ButtonProps) => (
 );
 
 export const FluentButton = () => {
-  const [enabled, setEnabled] = React.useState(true);
-  const onChange = () => {
-    setEnabled(!enabled);
-  };
   return (
-    <ThemeProvider theme={FluentTheme}>
-      <Stack gap={20}>
-        <Text variant="xLarge">A button comes in default and `primary` flavors.</Text>
-        <ButtonVariants />
+    <Stack gap={20}>
+      <Text variant="xLarge">A button comes in default and `primary` flavors.</Text>
+      <ButtonVariants />
 
-        <Text variant="xLarge">A button can appear round using the `circular` prop.</Text>
-        <ButtonVariants circular />
+      <Text variant="xLarge">A button can appear round using the `circular` prop.</Text>
+      <ButtonVariants circular />
 
-        <Text variant="xLarge">A button can fill the width of its container using the `fluid` prop.</Text>
-        <div className={classes.vStack}>
-          <Button fluid content="Hello, world" icon="X" />
-          <Button fluid primary content="Hello, world" icon="X" />
-          <Button fluid disabled content="Hello, world" icon="X" />
-          <Button fluid primary disabled content="Hello, world" icon="X" />
-        </div>
+      <Text variant="xLarge">A button can fill the width of its container using the `fluid` prop.</Text>
+      <div className={classes.vStack}>
+        <Button fluid content="Hello, world" icon="X" />
+        <Button fluid primary content="Hello, world" icon="X" />
+        <Button fluid disabled content="Hello, world" icon="X" />
+        <Button fluid primary disabled content="Hello, world" icon="X" />
+      </div>
 
-        <Text variant="xLarge">A button can contain only an icon using the `iconOnly` prop.</Text>
-        <ButtonVariants iconOnly />
+      <Text variant="xLarge">A button can contain only an icon using the `iconOnly` prop.</Text>
+      <ButtonVariants iconOnly />
 
-        <Text variant="xLarge">A button can be both `circular` and `iconOnly`.</Text>
-        <ButtonVariants circular iconOnly />
+      <Text variant="xLarge">A button can be both `circular` and `iconOnly`.</Text>
+      <ButtonVariants circular iconOnly />
 
-        <Text variant="xLarge">An icon button can format its Icon to appear before or after its content.</Text>
-        <div className={classes.vStack}>
-          <ButtonVariants iconPosition="before" />
-          <ButtonVariants iconPosition="after" />
-        </div>
+      <Text variant="xLarge">An icon button can format its Icon to appear before or after its content.</Text>
+      <div className={classes.vStack}>
+        <ButtonVariants iconPosition="before" />
+        <ButtonVariants iconPosition="after" />
+      </div>
 
-        <Text variant="xLarge">A button can show a loading indicator using the `loading` prop.</Text>
-        <ButtonVariants loading />
+      <Text variant="xLarge">A button can show a loading indicator using the `loading` prop.</Text>
+      <ButtonVariants loading />
 
-        <Text variant="xLarge">A button can be sized.</Text>
-        <div className={classes.vStack}>
-          <ButtonVariants size="smallest" />
-          <ButtonVariants size="smaller" />
-          <ButtonVariants size="small" />
-          <ButtonVariants size="large" />
-          <ButtonVariants size="larger" />
-          <ButtonVariants size="largest" />
-        </div>
-      </Stack>
-    </ThemeProvider>
+      <Text variant="xLarge">A button can be sized.</Text>
+      <div className={classes.vStack}>
+        <ButtonVariants size="smallest" />
+        <ButtonVariants size="smaller" />
+        <ButtonVariants size="small" />
+        <ButtonVariants size="large" />
+        <ButtonVariants size="larger" />
+        <ButtonVariants size="largest" />
+      </div>
+    </Stack>
   );
 };
