38e495f6efa952c634a8ae516235fd1682c0f54b
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-06-24 10:16:31 -0700

    Updating user stories.

--- packages/react-button/src/components/Button/FluentButton.stories.tsx
+++ packages/react-button/src/components/Button/FluentButton.stories.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import { Button } from './Button';
 import { PartialTheme, ThemeProvider } from '@fluentui/react-theme-provider';
-import { Stack, Toggle } from 'office-ui-fabric-react';
-
-// tslint:disable: jsx-ban-props
+import { Stack, Text, Toggle } from 'office-ui-fabric-react';
+import * as classes from './Button.stories.scss';
 
 const FluentTheme: PartialTheme = {
   tokens: {
@@ -45,6 +44,15 @@ const FluentTheme: PartialTheme = {
   stylesheets: [],
 };
 
+const ButtonVariants = (props: ButtonProps) => (
+  <div className={classes.hStack}>
+    <Button {...props} content="Hello, world" icon="X" />
+    <Button {...props} primary content="Hello, world" icon="X" />
+    <Button {...props} disabled content="Hello, world" icon="X" />
+    <Button {...props} primary disabled content="Hello, world" icon="X" />
+  </div>
+);
+
 export const FluentButton = () => {
   const [enabled, setEnabled] = React.useState(true);
   const onChange = () => {
@@ -52,10 +60,45 @@ export const FluentButton = () => {
   };
   return (
     <ThemeProvider theme={FluentTheme}>
-      <Toggle defaultChecked={enabled} onText="Buttons enabled" offText="Buttons disabled" onChange={onChange} />
-      <Stack horizontal gap={20}>
-        <Button disabled={!enabled} content="I am a button" />
-        <Button disabled={!enabled} primary content="I am a primary button" />
+      <Stack gap={20}>
+        <Text variant="xLarge">A button comes in default and `primary` flavors.</Text>
+        <ButtonVariants />
+
+        <Text variant="xLarge">A button can appear round using the `circular` prop.</Text>
+        <ButtonVariants circular />
+
+        <Text variant="xLarge">A button can fill the width of its container using the `fluid` prop.</Text>
+        <div className={classes.vStack}>
+          <Button fluid content="Hello, world" icon="X" />
+          <Button fluid primary content="Hello, world" icon="X" />
+          <Button fluid disabled content="Hello, world" icon="X" />
+          <Button fluid primary disabled content="Hello, world" icon="X" />
+        </div>
+
+        <Text variant="xLarge">A button can contain only an icon using the `iconOnly` prop.</Text>
+        <ButtonVariants iconOnly />
+
+        <Text variant="xLarge">A button can be both `circular` and `iconOnly`.</Text>
+        <ButtonVariants circular iconOnly />
+
+        <Text variant="xLarge">An icon button can format its Icon to appear before or after its content.</Text>
+        <div className={classes.vStack}>
+          <ButtonVariants iconPosition="before" />
+          <ButtonVariants iconPosition="after" />
+        </div>
+
+        <Text variant="xLarge">A button can show a loading indicator using the `loading` prop.</Text>
+        <ButtonVariants loading />
+
+        <Text variant="xLarge">A button can be sized.</Text>
+        <div className={classes.vStack}>
+          <ButtonVariants size="smallest" />
+          <ButtonVariants size="smaller" />
+          <ButtonVariants size="small" />
+          <ButtonVariants size="large" />
+          <ButtonVariants size="larger" />
+          <ButtonVariants size="largest" />
+        </div>
       </Stack>
     </ThemeProvider>
   );
