08c10e1157359ce8a16565460f2a28bdf783d66e
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-24 10:20:22 +0200

    feat(Screener): test without mutiple regex

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -3,7 +3,9 @@ const config = require('../config').default;
 const files = glob.sync(config.paths.docs('**/*.steps.ts'));
 const ieSteps = files.filter(file => require(file).default.browsers?.includes('ie11'));
 const ieExcludeSteps = files.filter(file => !require(file).default.browsers?.includes('ie11'));
-const ieRegexes = ieSteps.map(file => `(${file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')})`).join('|');
+const ieRegexes = new RegExp(
+  ieSteps.map(file => `(${file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')})`).join('|'),
+);
 export const ieExcludeRegexes = ieExcludeSteps.map(
   file => new RegExp(file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')),
 );
