8d91073d24db91038afd494ff3f51f46eb027862
Ken Chau <kchau@microsoft.com>
2020-06-23 17:17:19 -0700

    replacing lerna with lage in root level package.json scripts

---
+++ lage.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  pipeline: {
+    build: ['^build'],
+    bundle: ['build'],
+    lint: [],
+    clean: [],
+    test: ['build'],
+    'code-style': [],
+    'update-snapshots': ['^update-snapshots'],
+  },
+
+  cacheOptions: {
+    outputGlob: [
+      'dist/**/*',
+      'lib/**/*',
+      'lib-commonjs/**/*',
+      'lib-amd/**/*',
+      '**/*.source.json',
+      '**/*.info.json',
+      '**/*.scss.ts',
+      '**/dist.stats.json',
+      'dist-storybook/**/*',
+      '**/*.tar.gz',
+      '!bower_components',
+      '!node_modules',
+      'lib-es2015/**/*',
+      'coverage/**/*',
+    ],
+  },
+};
