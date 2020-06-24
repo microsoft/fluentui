000779d114032e532884dd90e71f136f1169224e
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-24 12:32:14 +0200

    fix(Divider): add screener and example

---
+++ packages/fluentui/docs/src/examples/components/Divider/Visuals/DividerExampleRtlAndLtr.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Divider, Provider, Card, CardHeader, CardBody, Text } from '@fluentui/react-northstar';
+
+const DividerExampleFittedShorthand = () => (
+  <>
+    <Provider>
+      <Card fluid>
+        <CardHeader>
+          <Text content="LTR context" weight="bold" />
+        </CardHeader>
+        <CardBody>
+          <Text content="LTR text" />
+          <Divider content="Test text" color="green" />
+          <Text content="RTL text" />
+          <Divider content="نص تجربة" color="red" />
+        </CardBody>
+      </Card>
+    </Provider>
+    <Provider rtl>
+      <Card fluid>
+        <CardHeader>
+          <Text content="RTL context" weight="bold" />
+        </CardHeader>
+        <CardBody>
+          <Text content="RTL text" />
+          <Divider content="نص تجربة" color="green" />
+          <Text content="LTR text" />
+          <Divider content="Test text" color="red" />
+        </CardBody>
+      </Card>
+    </Provider>
+  </>
+);
+
+export default DividerExampleFittedShorthand;
