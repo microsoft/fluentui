091a848eeee49f845003db4d3a09b8d29e849ded
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-06-24 10:52:31 +0200

    feat(Screener): 50 tests

--- scripts/screener/screener.iefiles.ts
+++ scripts/screener/screener.iefiles.ts
@@ -3,10 +3,8 @@ const config = require('../config').default;
 const files = glob.sync(config.paths.docs('**/*.steps.ts'));
 const ieSteps = files.filter(file => require(file).default.browsers?.includes('ie11'));
 const ieExcludeSteps = files.filter(file => !require(file).default.browsers?.includes('ie11'));
-const ieRegexes = new RegExp(
-  ieSteps.map(file => `(${file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')})`).join('|'),
-);
+const ieRegexes = ieSteps.map(file => new RegExp(file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')));
 export const ieExcludeRegexes = ieExcludeSteps.map(
   file => new RegExp(file.replace(/\.steps\.ts/, '.tsx').replace(/.+\//, '')),
 );
-export default [ieRegexes];
+export default ieRegexes.splice(0, 50);
