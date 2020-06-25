fd26473ce7eceb0e1e8cdeea8a1bb2632cb5c6f7
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-06-24 17:00:25 -0700

    Enabling add-on and removing unused button story.

--- packages/react-button/.storybook/decorators/withThemeProvider.js
+++ packages/react-button/.storybook/decorators/withThemeProvider.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@fluentui/react-theme-provider';
 import { useTheme } from '../knobs/theme';
 
 export const withThemeProvider = storyFn => {
-  const theme = useTheme();
+  const { theme } = useTheme();
 
   return <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>;
 };
