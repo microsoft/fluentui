754fd7b6fbc8c2d7f17d4c8551f9433975e52853
David Zearing <dzearing@microsoft.com>
2020-06-24 18:06:28 -0700

    Adding test.

--- packages/merge-styles/src/transforms/kebabRules.test.ts
+++ packages/merge-styles/src/transforms/kebabRules.test.ts
@@ -24,4 +24,12 @@ describe('kebabRules', () => {
 
     expect(rules).toEqual(['--fooBar', 'test']);
   });
+
+  it('can leave webkit prefix intact', () => {
+    const rules = ['-webkit-font-smoothing', 'antialiased'];
+
+    kebabRules(rules, 0);
+
+    expect(rules).toEqual(['-webkit-font-smoothing', 'antialiased']);
+  });
 });
