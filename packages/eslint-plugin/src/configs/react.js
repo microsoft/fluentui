fbc031fe14104f5b62a206e350c0e451e77a7bc9
Elizabeth Craig <elcraig@microsoft.com>
2020-06-25 18:35:38 -0700

    Go back to naming convention helper function since override wasn't working

--- packages/eslint-plugin/src/configs/react.js
+++ packages/eslint-plugin/src/configs/react.js
@@ -1,12 +1,6 @@
 // @ts-check
 const configHelpers = require('../utils/configHelpers');
 
-// Regular expression parts for the naming convention rule
-const camelOrPascalCase = '[a-zA-Z][a-zA-Z\\d]*'; // must start with letter
-const upperCase = '[A-Z][A-Z\\d]*(_[A-Z\\d]*)*'; // must start with letter, no consecutive underscores
-const camelOrPascalOrUpperCase = `(${camelOrPascalCase}|${upperCase})`;
-const builtins = '^(any|Number|number|String|string|Boolean|boolean|Undefined|undefined)$';
-
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: [
@@ -51,44 +45,11 @@ const config = {
     '**/*.scss.ts',
   ],
   rules: {
+    '@fluentui/no-tslint-comments': 'error',
+
     // tslint: function-name, variable-name
-    '@typescript-eslint/naming-convention': [
-      'error',
-      { selector: 'function', format: ['camelCase'], leadingUnderscore: 'allow' },
-      { selector: 'method', modifiers: ['private'], format: ['camelCase'], leadingUnderscore: 'require' },
-      { selector: 'method', modifiers: ['protected'], format: ['camelCase'], leadingUnderscore: 'allow' },
-      // This will also pick up default-visibility methods and methods on plain objects,
-      // which is not really what we want, but there's not a good way around it.
-      { selector: 'method', modifiers: ['public'], format: ['camelCase'], leadingUnderscore: 'forbid' },
-      { selector: 'typeLike', format: ['PascalCase'], leadingUnderscore: 'forbid' },
-      // No leading I for interfaces
-      { selector: 'interface', format: ['PascalCase'], custom: { regex: '^I[A-Z]', match: false } },
-      {
-        selector: 'default',
-        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-        leadingUnderscore: 'allow',
-        filter: {
-          // Allow leading and optional trailing __
-          // (the rest of the regex just enforces the same casing constraint listed above)
-          regex: `^__${camelOrPascalOrUpperCase}(__)?$`,
-          match: false,
-        },
-        custom: {
-          // Ban names overlapping with built-in types.
-          regex: builtins,
-          match: false,
-        },
-        // An alternative way to set up this rule is set `format: null` and pass a single custom
-        // regex which matches absolutely everything. However, this leads to unhelpful error messages:
-        //   "Variable name `whatever` must match the RegExp: /someAbsurdlyLongUnreadableRegex/"
-        // For reference in case we ever want this anyway:
-        // format: null,
-        // custom: {
-        //   regex: `(?!${builtins})^(_?${camelOrPascalOrUpperCase}|__${camelOrPascalOrUpperCase}(__)?)$`,
-        //   match: true
-        // }
-      },
-    ],
+    ...configHelpers.getNamingConventionRule(false /* prefixWithI */),
+
     '@typescript-eslint/no-empty-function': 'error',
     '@typescript-eslint/no-explicit-any': 'error', // tslint: no-any
     '@typescript-eslint/prefer-namespace-keyword': 'error',
--- packages/eslint-plugin/src/utils/configHelpers.d.ts
+++ packages/eslint-plugin/src/utils/configHelpers.d.ts
@@ -1,4 +1,6 @@
-declare const configHelpers: {
+import { Linter } from 'eslint';
+
+export type ConfigHelpers = {
   /** File extensions to lint (with leading .) */
   extensions: string[];
 
@@ -12,6 +14,14 @@ declare const configHelpers: {
   devDependenciesFiles: string[];
 
   /**
+   * Returns a rule configuration for [`@typescript-eslint/naming-convention`](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md).
+   * This provides the ability to override *only* the interface rule without having to repeat or
+   * lose the rest of the (very complicated) config.
+   * @param prefixWithI - Whether to prefix interfaces with I
+   */
+  getNamingConventionRule: (prefixWithI: boolean) => Linter.RulesRecord;
+
+  /**
    * Rules requiring type information should be defined in the `overrides` section since they must
    * only run on TS files included in a tsconfig.json (generally those files under `src`), and they
    * require some extra configuration. They should be disabled entirely when running lint-staged
@@ -23,5 +33,7 @@ declare const configHelpers: {
    */
   getTypeInfoRuleOverrides: (rules: Linter.RulesRecord, tsconfigPath?: string) => Linter.ConfigOverride[];
 };
+
+declare const configHelpers: ConfigHelpers;
 
 export = configHelpers;
