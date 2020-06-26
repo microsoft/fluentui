9ccc6969399cd54b68dcafcb81501bc7f7d5eb16
Marija Najdova <mnajdova@gmail.com>
2020-06-26 11:21:42 +0200

    fixes

--- packages/fluentui/react-northstar/src/utils/assertions/assertValidIconValue.ts
+++ packages/fluentui/react-northstar/src/utils/assertions/assertValidIconValue.ts
@@ -1,4 +1,5 @@
 import { ShorthandValue } from '../../types';
+import * as _ from 'lodash';
 import inconsistentIconNames from './inconsistentIconNames';
 import { BoxProps } from '../../components/Box/Box';
 
@@ -6,6 +7,8 @@ const assertValidIconValue = (iconShorthand: ShorthandValue<BoxProps>, propName 
   if (process.env.NODE_ENV !== 'production') {
     if (typeof iconShorthand === 'string' || (typeof iconShorthand === 'object' && !!(iconShorthand as any).name)) {
       const name = typeof iconShorthand === 'string' ? iconShorthand : (iconShorthand as any).name;
+      const camelCaseName = _.camelCase(name);
+      const iconComponentName = camelCaseName[0].toUpperCase() + camelCaseName.slice(1);
       const wrongUsage =
         typeof iconShorthand === 'string'
           ? `${propName}="${iconShorthand}"`
@@ -13,10 +16,10 @@ const assertValidIconValue = (iconShorthand: ShorthandValue<BoxProps>, propName 
       const correctUsage = inconsistentIconNames[name]
         ? `<${inconsistentIconNames[name]} />`
         : name.length > 0
-        ? `<${name[0].toUpperCase() + name.slice(1)}Icon />`
+        ? `<${iconComponentName}Icon />`
         : 'some icon component';
-      console.warn(
-        `FluentUI - you are using the icon shorthand in a wrong way. Please replace ${wrongUsage} with ${correctUsage} and move all properties except the name on the new component.`,
+      console.error(
+        `Fluent UI - you are using the icon shorthand in a wrong way. Please replace ${wrongUsage} with ${correctUsage} and move all properties except the name on the new component.`,
       );
     }
   }
