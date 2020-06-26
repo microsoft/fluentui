29ae7a6b24e465f5ade8cf46ee1543f525dacbd5
Jon Schectman <joschect@microsoft.com>
2020-06-26 13:01:49 -0700

    Update ContextualMenu.base.tsx

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
@@ -454,7 +454,7 @@ export class ContextualMenuBase extends React.Component<IContextualMenuProps, IC
       // In some cases the object might exist but not be a real element.
       // This is primarily for IE 11 and should be removed once IE 11 is no longer in use.
       if (this._previousActiveElement.focus) {
-        this._previousActiveElement && this._previousActiveElement.focus();
+        this._previousActiveElement.focus();
       }
     }
   };
