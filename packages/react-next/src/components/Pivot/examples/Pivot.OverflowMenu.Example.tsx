e8fb57f3a7845b601d9a8bb8a88b3460c0168aad
Ben Howell <behowell@microsoft.com>
2020-06-29 15:33:18 -0400

    Update overflow menu example

--- packages/react-next/src/components/Pivot/examples/Pivot.OverflowMenu.Example.tsx
+++ packages/react-next/src/components/Pivot/examples/Pivot.OverflowMenu.Example.tsx
@@ -22,32 +22,29 @@ export const PivotOverflowMenuExample: React.FunctionComponent = () => {
           linkFormat={tabs ? 'tabs' : 'links'}
           headerOverflow={overflow ? 'menu' : 'none'}
         >
-          <PivotItem headerText="1. This Pivot">
+          <PivotItem headerText="My Files">
             <Label>Pivot #1</Label>
           </PivotItem>
-          <PivotItem headerText="2. Has many tabs">
+          <PivotItem itemCount={23} itemIcon="Recent">
             <Label>Pivot #2</Label>
           </PivotItem>
-          <PivotItem headerText="3. To demonstrate">
+          <PivotItem headerText="Placeholder" itemIcon="Globe">
             <Label>Pivot #3</Label>
           </PivotItem>
-          <PivotItem headerText="4. What happens when">
+          <PivotItem headerText="Shared with me" itemIcon="Ringer" itemCount={1}>
             <Label>Pivot #4</Label>
           </PivotItem>
-          <PivotItem headerText="5. Not all of the tabs can fit">
+          <PivotItem headerText="Custom Renderer" itemIcon="Airplane" onRenderItemLink={_customRenderer}>
             <Label>Pivot #5</Label>
           </PivotItem>
-          <PivotItem headerText="6. Onscreen">
+          <PivotItem headerText="This tab has a relatively long title">
             <Label>Pivot #6</Label>
           </PivotItem>
-          <PivotItem headerText="7. At the same time">
+          <PivotItem headerText="Short">
             <Label>Pivot #7</Label>
           </PivotItem>
-          <PivotItem headerText="8. This tab has a relatively long title">
+          <PivotItem headerText="The Last Tab">
             <Label>Pivot #8</Label>
-          </PivotItem>
-          <PivotItem headerText="9. Custom Renderer" onRenderItemLink={_customRenderer}>
-            <Label>Pivot #9</Label>
           </PivotItem>
         </Pivot>
       </Fabric>
@@ -57,9 +54,9 @@ export const PivotOverflowMenuExample: React.FunctionComponent = () => {
 
 function _customRenderer(link: IPivotItemProps, defaultRenderer: (link: IPivotItemProps) => JSX.Element): JSX.Element {
   return (
-    <>
-      {defaultRenderer(link)}
-      <Icon iconName="Airplane" style={{ color: 'red' }} />
-    </>
+    <span style={{ flex: '0 1 100%' }}>
+      {defaultRenderer({ ...link, itemIcon: undefined })}
+      <Icon iconName={link.itemIcon} style={{ color: 'red' }} />
+    </span>
   );
 }
