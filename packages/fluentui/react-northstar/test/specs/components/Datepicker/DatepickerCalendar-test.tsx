dd56043d8b06c23a0d7a8e04c7d25f31b8e9c0ed
Karel Rymes <karymes@microsoft.com>
2020-06-30 09:35:53 +0200

    Adding basic conformant tests

---
+++ packages/fluentui/react-northstar/test/specs/components/Datepicker/DatepickerCalendar-test.tsx
@@ -0,0 +1,6 @@
+import { isConformant } from 'test/specs/commonTests';
+import DatepickerCalendar from 'src/components/Datepicker/DatepickerCalendar';
+
+describe('Datepicker', () => {
+  isConformant(DatepickerCalendar, { constructorName: 'DatepickerCalendar' });
+});
