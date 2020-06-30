b2552ccd7c9b8f14eb8309f91634b5fb7a47464d
Trip Master <t-dama@microsoft.com>
2020-06-30 19:52:27 -0400

    Combine labelId and StateTextId in labelledById and update tests

--- packages/office-ui-fabric-react/src/components/Toggle/Toggle.base.tsx
+++ packages/office-ui-fabric-react/src/components/Toggle/Toggle.base.tsx
@@ -101,14 +101,15 @@ export class ToggleBase extends React.Component<IToggleProps, IToggleState> impl
     // The following properties take priority for what Narrator should read:
     // 1. ariaLabel
     // 2. onAriaLabel (if checked) or offAriaLabel (if not checked)
-    // 3. label
-    // 4. onText (if checked) or offText (if not checked)
+    // 3. label AND stateText, if existent
+
     let labelledById: string | undefined = undefined;
     if (!ariaLabel && !badAriaLabel) {
       if (label) {
         labelledById = labelId;
-      } else if (stateText) {
-        labelledById = stateTextId;
+      }
+      if (stateText) {
+        labelledById = labelledById ? (labelledById += ' ' + stateTextId) : stateTextId;
       }
     }
 
@@ -159,6 +160,8 @@ export class ToggleBase extends React.Component<IToggleProps, IToggleState> impl
         <div className={classNames.container}>
           {pillContent}
           {stateText && (
+            // This second "htmlFor" property is needed to allow the
+            // toggle's stateText to also trigger a state change when clicked.
             <Label htmlFor={this._id} className={classNames.text} id={stateTextId}>
               {stateText}
             </Label>
