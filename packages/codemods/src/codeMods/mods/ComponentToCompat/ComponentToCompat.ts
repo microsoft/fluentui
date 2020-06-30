074fd920937fd3b4c22120140440db89d9d10a57
Jon S <joschect@microsoft.com>
2020-06-30 16:04:24 -0700

    add rest of the compat things

--- packages/codemods/src/codeMods/mods/ComponentToCompat/ComponentToCompat.ts
+++ packages/codemods/src/codeMods/mods/ComponentToCompat/ComponentToCompat.ts
@@ -4,13 +4,25 @@ import { SourceFile } from 'ts-morph';
 
 // Not sure if this the best way to get all the things exported from button. It's dependent on version
 // And other things. Ideally we'd be able to get it from within ts-morph.
-import * as ButtonImports from 'office-ui-fabric-react/lib/Button';
+import * as Button from 'office-ui-fabric-react/lib/Button';
+import * as Pivot from 'office-ui-fabric-react/lib/Pivot';
+import * as Checkbox from 'office-ui-fabric-react/lib/Checkbox';
+import * as Link from 'office-ui-fabric-react/lib/Link';
+import * as Slider from 'office-ui-fabric-react/lib/Slider';
+import * as Toggle from 'office-ui-fabric-react/lib/Toggle';
 
 //const defaultSearchRegex = /^office\-ui\-fabric\-react/;
 const fabricindex = 'office-ui-fabric-react';
 const completePath = 'office-ui-fabric-react/lib/';
 const newPathStart = '@fluentui/react/lib/compat/';
-const exportMapping: rawCompat[] = [{ componentName: 'Button', namedExports: ButtonImports }];
+const exportMapping: rawCompat[] = [
+  { componentName: 'Button', namedExports: Button },
+  { componentName: 'Pivot', namedExports: Pivot },
+  { componentName: 'Checkbox', namedExports: Checkbox },
+  { componentName: 'Link', namedExports: Link },
+  { componentName: 'Slider', namedExports: Slider },
+  { componentName: 'Toggle', namedExports: Toggle },
+];
 function getPath(root: string, componentName: string) {
   return `${root}${componentName}`;
 }
