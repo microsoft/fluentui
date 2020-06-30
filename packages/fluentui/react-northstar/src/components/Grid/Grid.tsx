fcd3aade0b24743d7b6139beca2ad1928100bac6
Oleksandr Fediashov <olfedias@microsoft.com>
2020-06-30 15:42:44 +0200

    chore(Grid): convert to be function component

--- packages/fluentui/react-northstar/src/components/Grid/Grid.tsx
+++ packages/fluentui/react-northstar/src/components/Grid/Grid.tsx
@@ -1,25 +1,27 @@
-import { Accessibility } from '@fluentui/accessibility';
+import { Accessibility, GridBehaviorProps } from '@fluentui/accessibility';
+import { getElementType, useAccessibility, useStyles, useTelemetry, useUnhandledProps } from '@fluentui/react-bindings';
 import * as customPropTypes from '@fluentui/react-proptypes';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
+// @ts-ignore
+import { ThemeContext } from 'react-fela';
+
 import {
-  UIComponent,
   childrenExist,
-  RenderResultConfig,
   UIComponentProps,
   ChildrenComponentProps,
   commonPropTypes,
   ContentComponentProps,
   rtlTextContainer,
 } from '../../utils';
-import { WithAsProp, withSafeTypeForAs } from '../../types';
+import { FluentComponentStaticProps, ProviderContextPrepared, WithAsProp, withSafeTypeForAs } from '../../types';
 
 export interface GridProps extends UIComponentProps, ChildrenComponentProps, ContentComponentProps {
   /**
    * Accessibility behavior if overridden by the user.
    * @available gridBehavior, gridHorizontalBehavior
    * */
-  accessibility?: Accessibility;
+  accessibility?: Accessibility<GridBehaviorProps>;
 
   /** The columns of the grid with a space-separated list of values. The values represent the track size, and the space between them represents the grid line. */
   columns?: number;
@@ -30,42 +32,67 @@ export interface GridProps extends UIComponentProps, ChildrenComponentProps, Con
 
 export const gridClassName = 'ui-grid';
 
-class Grid extends UIComponent<WithAsProp<GridProps>> {
-  static displayName = 'Grid';
+export type GridStylesProps = Pick<GridProps, 'columns' | 'rows'>;
 
-  static deprecated_className = gridClassName;
+const Grid: React.FC<WithAsProp<GridProps>> & FluentComponentStaticProps<GridProps> = props => {
+  const context: ProviderContextPrepared = React.useContext(ThemeContext);
+  const { setStart, setEnd } = useTelemetry(Grid.displayName, context.telemetry);
+  setStart();
 
-  static propTypes = {
-    ...commonPropTypes.createCommon({
-      content: false,
+  const { accessibility, children, className, columns, content, design, rows, styles, variables } = props;
+
+  const getA11yProps = useAccessibility(accessibility, {
+    debugName: Grid.displayName,
+
+    rtl: context.rtl,
+  });
+  const { classes } = useStyles<GridStylesProps>(Grid.displayName, {
+    className: gridClassName,
+    mapPropsToStyles: () => ({ columns, rows }),
+    mapPropsToInlineStyles: () => ({
+      className,
+      design,
+      styles,
+      variables,
     }),
-    columns: PropTypes.number,
-    content: customPropTypes.every([
-      customPropTypes.disallow(['children']),
-      PropTypes.oneOfType([PropTypes.arrayOf(customPropTypes.nodeContent), customPropTypes.nodeContent]),
-    ]),
-    rows: PropTypes.number,
-  };
+    rtl: context.rtl,
+  });
 
-  static defaultProps: WithAsProp<GridProps> = {
-    as: 'div',
-  };
+  const ElementType = getElementType(props);
+  const unhandledProps = useUnhandledProps(Grid.handledProps, props);
 
-  renderComponent({ accessibility, ElementType, classes, unhandledProps }: RenderResultConfig<any>): React.ReactNode {
-    const { children, content } = this.props;
-
-    return (
+  const element = (
+    <>
       <ElementType
-        className={classes.root}
-        {...rtlTextContainer.getAttributes({ forElements: [children, content] })}
-        {...accessibility.attributes.root}
-        {...unhandledProps}
+        {...getA11yProps('root', {
+          className: classes.root,
+          ...rtlTextContainer.getAttributes({ forElements: [children, content] }),
+          ...unhandledProps,
+        })}
       >
         {childrenExist(children) ? children : content}
       </ElementType>
-    );
-  }
-}
+    </>
+  );
+  setEnd();
+
+  return element;
+};
+
+Grid.displayName = 'Grid';
+
+Grid.propTypes = {
+  ...commonPropTypes.createCommon({
+    content: false,
+  }),
+  columns: PropTypes.number,
+  content: customPropTypes.every([
+    customPropTypes.disallow(['children']),
+    PropTypes.oneOfType([PropTypes.arrayOf(customPropTypes.nodeContent), customPropTypes.nodeContent]),
+  ]),
+  rows: PropTypes.number,
+};
+Grid.handledProps = Object.keys(Grid.propTypes) as any;
 
 /**
  * A Grid is a layout component that harmonizes negative space, by controlling both the row and column alignment.
