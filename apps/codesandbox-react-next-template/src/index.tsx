590edad2e6570ba4251df809b0c1d281e90a2cbc
Tomi Olubeko <ololubek@microsoft.com>
2020-07-01 12:04:00 -0700

    add <ThemeProvider> and <Stack> components to fix template rendering

--- apps/codesandbox-react-next-template/src/index.tsx
+++ apps/codesandbox-react-next-template/src/index.tsx
@@ -1,32 +1,46 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { DefaultCustomizations } from '@uifabric/theme-samples';
 import {
-  Fabric,
   Checkbox,
   Dropdown,
   DropdownMenuItemType,
   SearchBox,
   Slider,
   SpinButton,
+  Stack,
   TextField,
+  ThemeProvider,
   Toggle,
 } from '@fluentui/react-next';
 
 let _rootDiv: HTMLElement;
 let dropdownOptions = [
-  { key: 'fruitsHeader', text: 'Fruits', itemType: DropdownMenuItemType.Header },
+  {
+    key: 'fruitsHeader',
+    text: 'Fruits',
+    itemType: DropdownMenuItemType.Header,
+  },
   { key: 'apple', text: 'Apple' },
   { key: 'banana', text: 'Banana' },
   { key: 'orange', text: 'Orange', disabled: true },
   { key: 'grape', text: 'Grape' },
   { key: 'divider_1', text: '-', itemType: DropdownMenuItemType.Divider },
-  { key: 'vegetablesHeader', text: 'Vegetables', itemType: DropdownMenuItemType.Header },
+  {
+    key: 'vegetablesHeader',
+    text: 'Vegetables',
+    itemType: DropdownMenuItemType.Header,
+  },
   { key: 'broccoli', text: 'Broccoli' },
   { key: 'carrot', text: 'Carrot' },
   { key: 'lettuce', text: 'Lettuce' },
 ];
 let dropdownStyles = {
   dropdown: { width: 300 },
+};
+
+const theme = {
+  tokens: DefaultCustomizations.settings.theme,
 };
 
 function start(): void {
@@ -36,35 +50,37 @@ function start(): void {
   }
 
   ReactDOM.render(
-    <Fabric>
-      <Checkbox label="Unchecked checkbox (uncontrolled)" />
-      <Slider
-        label="Snapping slider example"
-        min={0}
-        max={50}
-        step={10}
-        defaultValue={20}
-        showValue={true}
-        snapToStep={true}
-      />
-      <SearchBox placeholder="Search" />
-      <SpinButton
-        defaultValue="0"
-        min={0}
-        max={100}
-        step={1}
-        incrementButtonAriaLabel="Increase value by 1"
-        decrementButtonAriaLabel="Decrease value by 1"
-      />
-      <TextField label="Standard" />
-      <Toggle label="Enabled and checked" defaultChecked={true} onText="On" offText="Off" />
-      <Dropdown
-        placeholder="Select an option"
-        label="Basic uncontrolled example"
-        options={dropdownOptions}
-        styles={dropdownStyles}
-      />
-    </Fabric>,
+    <ThemeProvider theme={theme}>
+      <Stack gap={8}>
+        <Checkbox label="Unchecked checkbox (uncontrolled)" />
+        <Slider
+          label="Snapping slider example"
+          min={0}
+          max={50}
+          step={10}
+          defaultValue={20}
+          showValue={true}
+          snapToStep={true}
+        />
+        <SearchBox placeholder="Search" />
+        <SpinButton
+          defaultValue="0"
+          min={0}
+          max={100}
+          step={1}
+          incrementButtonAriaLabel="Increase value by 1"
+          decrementButtonAriaLabel="Decrease value by 1"
+        />
+        <TextField label="Standard" />
+        <Toggle label="Enabled and checked" defaultChecked={true} onText="On" offText="Off" />
+        <Dropdown
+          placeholder="Select an option"
+          label="Basic uncontrolled example"
+          options={dropdownOptions}
+          styles={dropdownStyles}
+        />
+      </Stack>
+    </ThemeProvider>,
     _rootDiv,
   );
 }
