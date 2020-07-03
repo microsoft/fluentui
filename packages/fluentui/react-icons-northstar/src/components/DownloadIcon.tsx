472f361b5533b4eb723e78dfb1ed0b24721a64e5
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-03 15:53:02 +0200

    chore: remove default exports [P3] (#13916)

--- packages/fluentui/react-icons-northstar/src/components/DownloadIcon.tsx
+++ packages/fluentui/react-icons-northstar/src/components/DownloadIcon.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import cx from 'classnames';
-import createSvgIcon from '../utils/createSvgIcon';
+import { createSvgIcon } from '../utils/createSvgIcon';
 import { iconClassNames } from '../utils/iconClassNames';
 
-const DownloadIcon = createSvgIcon({
+export const DownloadIcon = createSvgIcon({
   svg: ({ classes }) => (
     <svg role="presentation" focusable="false" viewBox="8 8 16 16" className={classes.svg}>
       <path
@@ -18,5 +18,3 @@ const DownloadIcon = createSvgIcon({
   ),
   displayName: 'DownloadIcon',
 });
-
-export default DownloadIcon;
