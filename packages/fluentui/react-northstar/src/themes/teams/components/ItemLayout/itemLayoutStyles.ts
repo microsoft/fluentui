56e86ee78678e3fe9bbedcc4cde9070a6425f915
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-03 12:25:56 +0200

    Update itemLayoutStyles.ts

--- packages/fluentui/react-northstar/src/themes/teams/components/ItemLayout/itemLayoutStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/ItemLayout/itemLayoutStyles.ts
@@ -1,6 +1,6 @@
 import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
 
-import { ItemLayoutStylesProps } from 'src/components/ItemLayout/ItemLayout';
+import { ItemLayoutStylesProps } from '../../../../components/ItemLayout/ItemLayout';
 import { ItemLayoutVariables } from './itemLayoutVariables';
 
 export const itemLayoutStyles: ComponentSlotStylesPrepared<ItemLayoutStylesProps, ItemLayoutVariables> = {
