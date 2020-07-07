86c15529e324143165c827afdde2fb2a50ef4c7a
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-07-07 10:23:00 +0200

    fix(CommonPropInterfaces): as ElementType

--- packages/fluentui/react-northstar/src/utils/commonPropInterfaces.ts
+++ packages/fluentui/react-northstar/src/utils/commonPropInterfaces.ts
@@ -15,7 +15,7 @@ export interface UIComponentProps<P = any, V = any> extends StyledComponentProps
   /** Additional CSS class name(s) to apply.  */
   className?: string;
   design?: ComponentDesignProp;
-  as?: React.ReactNode;
+  as?: React.ElementType<any>;
 }
 
 export type SizeValue = 'smallest' | 'smaller' | 'small' | 'medium' | 'large' | 'larger' | 'largest';
