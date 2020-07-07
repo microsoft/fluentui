a98023f8e1e7ffeb0742f38398462faef4ef7ca5
Karel Rymes <karymes@microsoft.com>
2020-07-07 18:17:40 +0200

    Fix build

--- packages/fluentui/docs/src/examples/components/Datepicker/Types/index.tsx
+++ packages/fluentui/docs/src/examples/components/Datepicker/Types/index.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import ComponentExample from '../../../../components/ComponentDoc/ComponentExample';
 import ExampleSection from '../../../../components/ComponentDoc/ExampleSection';
 
-const Usage = () => (
-  <ExampleSection title="Usage">
+const Types = () => (
+  <ExampleSection title="Types">
     <ComponentExample
-      title="Simple"
-      description="Simple datepicker"
-      examplePath="components/Datepicker/Usage/DatepickerExampleSimple"
+      title="Default"
+      description="Default Datepicker"
+      examplePath="components/Datepicker/Usage/DatepickerExample"
     />
   </ExampleSection>
 );
 
-export default Usage;
+export default Types;
