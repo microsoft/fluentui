51a6f05f3d404e8bde8fd2d88f64b0003ae5ef73
jennbost <45047773+jennbost@users.noreply.github.com>
2020-07-07 16:49:06 -0700

    Removed Do's and Don'ts file requirement
    Calling the Best practices file only

--- packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.doc.tsx
+++ packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.doc.tsx
@@ -21,8 +21,6 @@ export const CoachmarkPageProps: IDocPageProps = {
   bestPractices: require<
     string
   >('!raw-loader!office-ui-fabric-react/src/components/Coachmark/docs/CoachmarkBestPractices.md'),
-  dos: require<string>('!raw-loader!office-ui-fabric-react/src/components/Coachmark/docs/CoachmarkDos.md'),
-  donts: require<string>('!raw-loader!office-ui-fabric-react/src/components/Coachmark/docs/CoachmarkDonts.md'),
   isHeaderVisible: true,
   isFeedbackVisible: true,
 };
