231ab12b516532bf2948527b571ff1a5895be13d
Jon S <joschect@microsoft.com>
2020-07-08 11:54:21 -0700

    remove ignore lint

--- packages/codemods/src/codeMods/tests/mock/persona/mInterface.tsx
+++ packages/codemods/src/codeMods/tests/mock/persona/mInterface.tsx
@@ -1,6 +1,5 @@
 import { IPersonaProps } from 'office-ui-fabric-react/lib/Persona';
 
-// tslint:disable-next-line: interface-name
 export interface ExtendedProps extends IPersonaProps {
   internalProp?: boolean;
 }
