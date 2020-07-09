6196dbc8466e5e56658a347d71c0fb7b6b6a0872
vyhnalekl <t-lukasv@microsoft.com>
2020-07-09 13:25:04 +0200

    Roster Prototype

---
+++ packages/fluentui/docs/src/prototypes/Roaster/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import participants from './participantData';
+import presenters from './presentersData';
+
+import themeOverrides from './styles';
+import { List, Provider, Tree, Flex, Button, Input, Text, Header, teamsDarkTheme } from '@fluentui/react-northstar';
+import { TriangleDownIcon, TriangleEndIcon, CloseIcon, LinkIcon } from '@fluentui/react-icons-northstar';
+import { PrototypeSection, ComponentPrototype } from '../Prototypes';
+
+const ParticipantsList = () => <List navigable items={participants} />;
+
+const PresentersList = () => <List navigable items={presenters} />;
+
+const titleRenderer = (Component, { content, expanded, open, hasSubtree, ...restProps }) => (
+  <Component expanded={expanded} hasSubtree={hasSubtree} {...restProps}>
+    {expanded ? <TriangleDownIcon /> : hasSubtree ? <TriangleEndIcon /> : ''}
+    <Text size="small" content={content}></Text>
+  </Component>
+);
+
+const treeItems = [
+  {
+    id: 'participants',
+    title: 'Currently in this meeting',
+    items: [<ParticipantsList />],
+  },
+  {
+    id: 'invite',
+    title: 'Invite others from conversation (25)',
+    items: [<PresentersList />],
+  },
+];
+
+const RosterPrototype: React.FC = () => {
+  return (
+    <PrototypeSection title="Roster">
+      <ComponentPrototype>
+        <Provider theme={teamsDarkTheme}>
+          <Provider theme={themeOverrides}>
+            <Flex column variables={{ isContainer: true }}>
+              <Flex space="between" padding="padding.medium">
+                <Header as="h3" content="Participants"></Header>
+                <Button variables={{ vAlignCenter: true }} icon={<CloseIcon />} title="Search" iconOnly text />
+              </Flex>
+              <Flex padding="padding.medium" hAlign="center">
+                <Input fluid placeholder="Search..." />
+                <Button icon={<LinkIcon />} title="Search" iconOnly text />
+              </Flex>
+              <Tree
+                items={treeItems}
+                renderItemTitle={titleRenderer}
+                defaultActiveItemIds={['participants', 'invite']}
+              ></Tree>
+            </Flex>
+          </Provider>
+        </Provider>
+      </ComponentPrototype>
+    </PrototypeSection>
+  );
+};
+
+export default RoasterPrototype;
---
+++ packages/fluentui/docs/src/prototypes/Roaster/participant.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import { Avatar, Flex, Text, Animation, MenuButton, FlexItem } from '@fluentui/react-northstar';
+import { MoreIcon, MicOffIcon, ParticipantRemoveIcon, MicIcon } from '@fluentui/react-icons-northstar';
+
+const Participant: React.FC<{
+  image?: string;
+  name: string;
+  status: object;
+  role: string;
+  isTalking?: boolean;
+  isMuted?: boolean;
+}> = ({ image, name, status, role, isTalking, isMuted }) => {
+  const [muted, setMuted] = React.useState(isMuted);
+  const [talking, setTalking] = React.useState(isTalking);
+  return (
+    <Flex fill gap="gap.small" space="between">
+      {talking ? (
+        <Animation name="talking">
+          <Avatar image={image} name={name} status={status} />
+        </Animation>
+      ) : (
+        <Avatar image={image} name={name} status={status} />
+      )}
+
+      <Flex column>
+        {talking ? <Text weight="bold" content={name} /> : <Text content={name} />}
+        <Text disabled content={role} size="smaller" />
+      </Flex>
+
+      {muted ? (
+        <FlexItem push>
+          <MicOffIcon />
+        </FlexItem>
+      ) : null}
+      {muted ? (
+        <MenuButton
+          position="above"
+          align="end"
+          trigger={<MoreIcon />}
+          menu={[
+            {
+              icon: (
+                <MicIcon
+                  onClick={() => {
+                    setMuted(false);
+                    setTalking(isTalking);
+                  }}
+                />
+              ),
+              content: (
+                <Text
+                  onClick={() => {
+                    setMuted(false);
+                    setTalking(isTalking);
+                  }}
+                  content="Unmute"
+                />
+              ),
+            },
+            {
+              icon: <ParticipantRemoveIcon />,
+              content: 'Remove',
+            },
+          ]}
+        />
+      ) : (
+        <FlexItem push>
+          <MenuButton
+            position="above"
+            align="end"
+            trigger={<MoreIcon />}
+            menu={[
+              {
+                icon: (
+                  <MicOffIcon
+                    onClick={() => {
+                      setTalking(false);
+                      setMuted(true);
+                    }}
+                  />
+                ),
+                content: (
+                  <Text
+                    onClick={() => {
+                      setTalking(false);
+                      setMuted(true);
+                    }}
+                    content="Mute"
+                  />
+                ),
+              },
+              { icon: <ParticipantRemoveIcon />, content: 'Remove' },
+            ]}
+          />
+        </FlexItem>
+      )}
+    </Flex>
+  );
+};
+
+export default Participant;
