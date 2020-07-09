490b5fb3a5597b6988d253714a2b9769d5f81201
Hemantha Ponnuru <hemanp@microsoft.com>
2020-07-08 21:29:41 -0700

    Introduce FolderCover for linked folders
    Fixed the example to render the folder covers example

--- packages/experiments/src/components/FolderCover/initializeFolderCovers.tsx
+++ packages/experiments/src/components/FolderCover/initializeFolderCovers.tsx
@@ -16,6 +16,8 @@ export function initializeFolderCovers(baseUrl: string = ASSET_CDN_BASE_URL, opt
       icons: {
         folderCoverLargeDefaultFront: <img src={`${baseUrl}/lg-fg.svg`} />,
         folderCoverLargeDefaultBack: <img src={`${baseUrl}/lg-bg.svg`} />,
+        folderCoverLargeLinkedFront: <img src={`${baseUrl}/lg-fg-linked.svg`} />,
+        folderCoverLargeLinkedBack: <img src={`${baseUrl}/lg-bg.svg`} />,
         folderCoverLargeMediaFront: <img src={`${baseUrl}/lg-fg-media.svg`} />,
         folderCoverLargeMediaBack: <img src={`${baseUrl}/lg-bg.svg`} />,
       },
@@ -34,6 +36,8 @@ export function initializeFolderCovers(baseUrl: string = ASSET_CDN_BASE_URL, opt
       icons: {
         folderCoverSmallDefaultFront: <img src={`${baseUrl}/sm-fg.svg`} />,
         folderCoverSmallDefaultBack: <img src={`${baseUrl}/sm-bg.svg`} />,
+        folderCoverSmallLinkedFront: <img src={`${baseUrl}/sm-fg-linked.svg`} />,
+        folderCoverSmallLinkedBack: <img src={`${baseUrl}/sm-bg.svg`} />,
         folderCoverSmallMediaFront: <img src={`${baseUrl}/sm-fg-media.svg`} />,
         folderCoverSmallMediaBack: <img src={`${baseUrl}/sm-bg.svg`} />,
       },
