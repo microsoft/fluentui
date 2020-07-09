0bae11fc8b91cdbf1cf9d0d78dfe5c0e77766367
Philip Kuo <phkuo@microsoft.com>
2020-07-08 18:14:40 -0700

    Theming: new semantic card slots (#13906)
    * new slots
    
    * fix
    
    * Change files
    
    * fix

--- packages/styling/src/interfaces/IEffects.ts
+++ packages/styling/src/interfaces/IEffects.ts
@@ -1,29 +1,33 @@
 /**
- * Experimental interface for decorative styling in a theme.
+ * Decorative styles in a theme
  * {@docCategory IEffects}
  */
 export interface IEffects {
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
    * This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * The shadow's color should not change with the theme, because colored shadows look unnatural.
    * Used for: cards, grid items
    */
   elevation4: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
    * This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * The shadow's color should not change with the theme, because colored shadows look unnatural.
    * Used for: menus, command surfaces
    */
   elevation8: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
    * This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * The shadow's color should not change with the theme, because colored shadows look unnatural.
    * Used for: search result dropdowns, hover cards, tooltips, help bubbles
    */
   elevation16: string;
   /**
    * Used to provide a visual affordance that this element is elevated above the surface it rests on.
    * This is lower than elevations with a higher value, and higher than elevations with a lower value.
+   * The shadow's color should not change with the theme, because colored shadows look unnatural.
    * Used for: Panels, Dialogs
    */
   elevation64: string;
