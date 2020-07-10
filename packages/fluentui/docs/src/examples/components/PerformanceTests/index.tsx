3e85f61548cf35cd4a3d7aeef2cab5e6afd1a961
vyhnalekl <t-lukasv@microsoft.com>
2020-07-10 17:38:15 +0200

    Added page for performance test results

---
+++ packages/fluentui/docs/src/examples/components/PerformanceTests/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import ComponentPerfExample from '../../../components/ComponentDoc/ComponentPerfExample';
+import ExampleSection from '../../../components/ComponentDoc/ExampleSection';
+
+const PerformanceTests = () => (
+  <ExampleSection title="Roster Performance">
+    <ComponentPerfExample title="Roster" examplePath="components/PerformanceTests/Performance/roster.perf" />
+  </ExampleSection>
+);
+
+export default PerformanceTests;
