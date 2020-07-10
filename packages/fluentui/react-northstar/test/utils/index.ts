8bd74f67f13b14ced6d4b012d2afebe3c30a07f5
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-10 12:55:57 +0200

    Update index.ts

--- packages/fluentui/react-northstar/test/utils/index.ts
+++ packages/fluentui/react-northstar/test/utils/index.ts
@@ -1,8 +1,8 @@
 export * from './assertNodeContains';
-export { consoleUtil } from './consoleUtil';
-export { domEvent } from './domEvent';
+export * as consoleUtil from './consoleUtil';
+export * as domEvent from './domEvent';
 export { findIntrinsicElement } from './findIntrinsicElement';
 export { getDisplayName } from './getDisplayName';
-export { syntheticEvent } from './syntheticEvent';
+export * as syntheticEvent from './syntheticEvent';
 export { nextFrame } from './nextFrame';
 export { EmptyThemeProvider, mountWithProvider, mountWithProviderAndGetComponent } from './withProvider';
