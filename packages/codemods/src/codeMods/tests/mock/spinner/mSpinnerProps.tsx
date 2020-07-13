631f94a95f1e24aff445499dc2f958b1e54a759a
Trip Master <t-dama@microsoft.com>
2020-07-13 15:35:39 -0400

    fix commenting

--- packages/codemods/src/codeMods/tests/mock/spinner/mSpinnerProps.tsx
+++ packages/codemods/src/codeMods/tests/mock/spinner/mSpinnerProps.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import { Spinner, SpinnerType } from 'office-ui-fabric-react/lib/Spinner';
 
-enum foo {
-  ugh = 5,
-}
-
 export const RenderSpinner = (props: any) => {
   return (
     <div>
