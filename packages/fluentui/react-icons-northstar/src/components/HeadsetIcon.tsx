e6059c89d209c315bfa01140f9b7e84e2c3c3f24
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-14 15:06:40 +0200

    fix imports

--- packages/fluentui/react-icons-northstar/src/components/HeadsetIcon.tsx
+++ packages/fluentui/react-icons-northstar/src/components/HeadsetIcon.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import createSvgIcon from '../utils/createSvgIcon';
+import { createSvgIcon } from '../utils/createSvgIcon';
 
-const HeadsetIcon = createSvgIcon({
+export const HeadsetIcon = createSvgIcon({
   svg: ({ classes }) => (
     <svg role="presentation" focusable="false" viewBox="8 8 16 16" className={classes.svg}>
       <g>
@@ -11,5 +11,3 @@ const HeadsetIcon = createSvgIcon({
   ),
   displayName: 'HeadsetIcon',
 });
-
-export default HeadsetIcon;
