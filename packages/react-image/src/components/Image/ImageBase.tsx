373c3e9b506b7e1a1b3af8b2b641e4114d36dcef
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-16 15:02:59 +0200

    feat: add @fluentui/react-image

---
+++ packages/react-image/src/components/Image/ImageBase.tsx
@@ -0,0 +1,33 @@
+import { compose, mergeProps } from '@fluentui/react-compose';
+import { useMergedRefs } from '@uifabric/react-hooks';
+import * as React from 'react';
+
+import { ImageProps, ImageSlots, ImageSlotProps, ImageState } from './Image.types';
+import { useImage } from './useImage';
+
+export const ImageBase = compose<'img', ImageProps, ImageProps, {}, {}>(
+  (props, ref, options) => {
+    const { state } = options;
+    const { slots, slotProps } = mergeProps<ImageProps, ImageState, ImageSlots, ImageSlotProps>(state, options);
+
+    const { buttonRef } = state;
+
+    return <slots.root ref={useMergedRefs(ref, buttonRef)} {...slotProps.root} />;
+  },
+  {
+    displayName: 'ImageBase',
+    handledProps: [
+      'bordered',
+      'circular',
+      // TODO: https://github.com/microsoft/fluentui/issues/13679
+      'imageRef',
+      'rounded',
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ] as any,
+    state: useImage,
+  },
+);
+
+ImageBase.defaultProps = {
+  as: 'img',
+};
