dd5ca9bf6a8d6a4d6ce3a1a35906f6ade3280d16
Trip Master <t-dama@microsoft.com>
2020-07-21 17:56:09 -0400

    simplify transforms

--- packages/codemods/src/codeMods/utilities/transforms.ts
+++ packages/codemods/src/codeMods/utilities/transforms.ts
@@ -10,69 +10,79 @@ import { renamePropInSpread } from './helpers/propHelpers';
    because the user can simply overwrite data instead of
    mapping it. If both NEWVALUE and MAP are provided
    (which should not happen), MAP will be used. If neither
-   are provided, nothing will happen. */
+   is provided, nothing will happen. */
 export function boolTransform(newValue?: boolean, map?: ValueMap<string>): PropTransform {
   return (
     element: JsxExpression | JsxOpeningElement | JsxSelfClosingElement,
     toRename: string,
     replacementName: string,
   ) => {
-    switch (element.getKind()) {
-      case SyntaxKind.JsxExpression:
-        {
-          const toChange = Maybe(element.getFirstChildByKind(SyntaxKind.TrueKeyword));
-          if (toChange.something) {
-            const oldText = toChange.value.getText();
-            toChange.value.replaceWithText(
-              map ? map[oldText] : newValue !== undefined ? newValue.toString() : toRename,
-            );
-          }
-        }
-        break;
-      case SyntaxKind.JsxOpeningElement:
-      case SyntaxKind.JsxSelfClosingElement: {
-        renamePropInSpread(
-          element as JsxOpeningElement | JsxSelfClosingElement,
-          toRename,
-          replacementName,
-          map,
-          newValue?.toString(),
-        );
-        break;
+    if (elementNotInSpread(element)) {
+      const toChange = getValueToChange(element as JsxExpression);
+      if (toChange) {
+        const oldText = toChange.getText();
+        toChange.replaceWithText(map ? map[oldText] : newValue !== undefined ? newValue.toString() : toRename);
       }
+    } else {
+      renamePropInSpread(
+        element as JsxOpeningElement | JsxSelfClosingElement,
+        toRename,
+        replacementName,
+        map,
+        newValue?.toString(),
+      );
     }
   };
 }
 
-/* Transform function used if the value to change is a STRING. Follows
-   the same contraints as the above function. */
+/* Transform function used if the value to change is a STRING.
+   Follows the same contraints as the above function. */
 export function stringTransform(newValue?: string, map?: ValueMap<string>): PropTransform {
   return (
     element: JsxExpression | JsxOpeningElement | JsxSelfClosingElement,
     toRename: string,
     replacementName: string,
   ) => {
-    switch (element.getKind()) {
-      case SyntaxKind.JsxExpression:
-        {
-          const toChange = Maybe(element.getFirstChildByKind(SyntaxKind.StringLiteral));
-          if (toChange.just) {
-            const oldText = toChange.value.getText();
-            toChange.value.replaceWithText(map ? `'${map[oldText]}'` : newValue ? newValue : toRename);
-          }
-        }
-        break;
-      case SyntaxKind.JsxOpeningElement:
-      case SyntaxKind.JsxSelfClosingElement: {
-        renamePropInSpread(
-          element as JsxOpeningElement | JsxSelfClosingElement,
-          toRename,
-          replacementName,
-          map,
-          newValue,
-        );
-        break;
+    if (elementNotInSpread(element)) {
+      const toChange = getValueToChange(element as JsxExpression);
+      if (toChange) {
+        const oldText = toChange.getText();
+        toChange.replaceWithText(map ? `'${map[oldText]}'` : newValue ? newValue : toRename);
       }
+    } else {
+      renamePropInSpread(
+        element as JsxOpeningElement | JsxSelfClosingElement,
+        toRename,
+        replacementName,
+        map,
+        newValue,
+      );
+    }
+  };
+}
+
+/* Transform function used if the value to change is a NUMBER.
+   Follows the same contraints as the above function. */
+export function numberTransform(newValue?: number, map?: ValueMap<string>): PropTransform {
+  return (
+    element: JsxExpression | JsxOpeningElement | JsxSelfClosingElement,
+    toRename: string,
+    replacementName: string,
+  ) => {
+    if (elementNotInSpread(element)) {
+      const toChange = getValueToChange(element as JsxExpression);
+      if (toChange) {
+        const oldText = toChange.getText();
+        toChange.replaceWithText(map ? map[oldText] : newValue !== undefined ? newValue.toString() : toRename);
+      }
+    } else {
+      renamePropInSpread(
+        element as JsxOpeningElement | JsxSelfClosingElement,
+        toRename,
+        replacementName,
+        map,
+        newValue?.toString(),
+      );
     }
   };
 }
@@ -89,21 +99,29 @@ export function enumTransform(map: ValueMap<string>): PropTransform {
     if (!map) {
       return;
     }
-    switch (element.getKind()) {
-      case SyntaxKind.JsxExpression:
-        {
-          const toChange = Maybe(element.getFirstChildByKind(SyntaxKind.PropertyAccessExpression));
-          if (toChange.something) {
-            const oldText = toChange.value.getText();
-            toChange.value.replaceWithText(map[oldText]);
-          }
-        }
-        break;
-      case SyntaxKind.JsxOpeningElement:
-      case SyntaxKind.JsxSelfClosingElement: {
-        renamePropInSpread(element as JsxOpeningElement | JsxSelfClosingElement, toRename, replacementName, map);
-        break;
+    if (elementNotInSpread(element)) {
+      const toChange = getValueToChange(element as JsxExpression);
+      if (toChange) {
+        const oldText = toChange.getText();
+        toChange.replaceWithText(map[oldText]);
       }
+    } else {
+      renamePropInSpread(element as JsxOpeningElement | JsxSelfClosingElement, toRename, replacementName, map);
     }
   };
+}
+
+/* Returns whether the supplied ELEMENT contains a prop within a
+   spread attribute. This is done by detecting whether the supplied
+   ELEMENT is a JsxExpression, meaning that we have close access to
+   the prop, or if it's a Jsx opening element, meaning we can't access it. */
+function elementNotInSpread(element: JsxExpression | JsxOpeningElement | JsxSelfClosingElement): boolean {
+  return element.getKind() === SyntaxKind.JsxExpression;
+}
+
+/* Returns the piece of the AST that represents the value that the
+   developer can overwrite. */
+function getValueToChange(element: JsxExpression) {
+  const toChange = Maybe(element.getChildAtIndex(1)); // Child between {} operators.
+  return toChange.just ? toChange.value : undefined;
 }
