1e59edb88fab2c14241df164eddf11503563aa81
Trip Master <t-dama@microsoft.com>
2020-07-21 18:30:02 -0400

    merge fixes

--- packages/codemods/src/codeMods/utilities/transforms.ts
+++ packages/codemods/src/codeMods/utilities/transforms.ts
@@ -3,7 +3,34 @@ import { JsxExpression, SyntaxKind, JsxOpeningElement, JsxSelfClosingElement } f
 import { Maybe } from '../../maybe';
 import { renamePropInSpread } from './helpers/propHelpers';
 
-// Used in the renameProp utility.
+/*
+Steps to writing a transform:
+-----------------------------
+1. The expected parameters are either a value map (data in string form)
+   or a single value that will replace all instances of the old value.
+2. The return type is of kind PropTransform, whose info can be found in
+   ../types
+3. The function must return a function that takes in the following parameters:
+    * element: JsxExpression | JsxOpeningElement | JsxSelfClosingElement,
+    * toRename: string,
+    * replacementName: string,
+   These params will be collected and used in the renameProp function.
+4. There are typically two separate general cases to handle with props:
+   those in the open, and those embedded in an object via the spread
+   attribute. The elementNotInSpread() function uses the ELEMENT parameter
+   from the closure to indicate whether you are handling a spread case or
+   a non-spread case. If you believe your codebase only uses the props in a
+   single way, feel free to only support one of the ways.
+5. To handle a spread case, simply call the renamePropInSpread() function.
+   Currently, there is no extra support for modifying that function.
+6. To handle a non-spread case, get the AST node you want to change with the
+   getValueToChange() function, and use the replaceWithText() helper to change
+   the node's text to whatever data you want, as long as it's in string form.
+7. If your change requires the old value of the prop (like in a mapping), you
+   can retrieve that with the getText() method on the node.
+
+Happy modding!
+*/
 
 /* Transform function that returns the new boolean value
    specified by a user. Less complex than other transforms
