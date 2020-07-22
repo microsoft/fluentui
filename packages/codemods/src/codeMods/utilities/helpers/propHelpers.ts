2787eeebb7768365e73c06d94f643986b61c1b0f
Trip Master <t-dama@microsoft.com>
2020-07-22 19:09:37 -0400

    improve abstraction / documentation

--- packages/codemods/src/codeMods/utilities/helpers/propHelpers.ts
+++ packages/codemods/src/codeMods/utilities/helpers/propHelpers.ts
@@ -7,7 +7,7 @@ import {
   VariableDeclarationKind,
   CodeBlockWriter,
   Identifier,
-  JsxAttributeLike,
+  DefinitionInfo,
 } from 'ts-morph';
 import { ValueMap } from 'src/codeMods/types';
 import { Maybe } from '../../../helpers/maybe';
@@ -20,113 +20,129 @@ export function renamePropInSpread(
   changeValueMap?: ValueMap<string>,
   replacementValue?: string,
 ) {
+  /* Step 1: Figure out which attribute contains the spread prop. */
   const allAttributes = element.getAttributes();
   allAttributes.forEach(attribute => {
     if (attribute.getKind() === SyntaxKind.JsxSpreadAttribute) {
-      /* Get the name of the object that houses the prop. */
       const spreadProp = attribute.getFirstChildByKind(SyntaxKind.Identifier);
+      /* Verify this attribute contains the name of our desired prop. */
       if (spreadContains(toRename, spreadProp)) {
-        /* If we've found our prop inside the object,
-          continue attempt to rename it. */
-        const propKind = spreadProp!.getDefinitions();
-        if (propKind.length === 1) {
-          const newSpreadName = '__mig' + propKind[0].getName()[0].toUpperCase() + propKind[0].getName().substring(1);
+        /* Step 2: Verify that the spread prop is legitimately defined as a variable.  */
+        const propDefinitions = spreadProp!.getDefinitions();
+        if (verifyVariableDefinition(propDefinitions)) {
+          /* Step 3: Create names for your new potential objects. */
+          const propSpreadName = propDefinitions[0].getName();
+          const newSpreadName = '__mig' + propSpreadName[0].toUpperCase() + propSpreadName.substring(1);
           const newMapName = '__migEnumMap';
-          if (verifyVariableDefinition(propKind[0].getKind())) {
-            const propContainingObject = propKind[0];
-            let newJSXFlag = false;
-            const elementType = element.getKind();
-            /* Need these data because ELEMENT is invalid if we call propsArrowFunction */
-            let blockContainer = getBlockContainer(element);
-            if (blockContainer === undefined) {
-              const containerMaybe = propsArrowFunction(element);
-              if (containerMaybe.something) {
-                blockContainer = containerMaybe.value;
-                newJSXFlag = true;
-              } else {
-                // eslint-disable-next-line no-throw-literal
-                throw 'attempt to create a new block around prop failed.';
-              }
-            }
-            const parentContainer = blockContainer!.getParentIfKind(SyntaxKind.Block);
-            if (parentContainer === undefined) {
+          /* Metadata in case we need to reacquire the current element (AST modification). */
+          let newJSXFlag = false;
+          const elementType = element.getKind();
+
+          /* Step 4: Get the container node who is the direct child of the closest SyntaxKind.Block.
+             If we need to insert auxiliary variables, we'll insert them before this node. */
+          let blockContainer = getBlockContainer(element);
+          if (blockContainer === undefined) {
+            /* In the case there was NO code block, the following function will create one for you.
+               If successful, initialize the newJSXFlag because you'll need to reaquire the spread element. */
+            const containerMaybe = createBlockContainer(element);
+            if (containerMaybe.something) {
+              blockContainer = containerMaybe.value;
+              newJSXFlag = true;
+            } else {
               // eslint-disable-next-line no-throw-literal
-              throw 'unable to get parent container from block';
+              throw 'attempt to create a new block around prop failed.';
             }
-            //parentContainer.insertStatements;
-            const insertIndex = blockContainer!.getChildIndex();
-            if (insertIndex === undefined) {
-              // eslint-disable-next-line no-throw-literal
-              throw 'unable to find child index';
-            }
-            if (!parentContainer.getVariableStatement(newSpreadName)) {
-              parentContainer.insertVariableStatement(insertIndex, {
-                declarationKind: VariableDeclarationKind.Const,
-                declarations: [
-                  {
-                    name: `{${toRename}, ...${newSpreadName}}`,
-                    initializer: propContainingObject.getName(),
-                  },
-                ],
-              });
-            }
-            if (changeValueMap && !parentContainer.getVariableStatement(newMapName)) {
-              parentContainer.insertVariableStatement(insertIndex, {
-                declarationKind: VariableDeclarationKind.Const,
-                declarations: [
-                  {
-                    name: newMapName,
-                    initializer: JSON.stringify(changeValueMap),
-                  },
-                ],
-              });
-            }
-            let attrToRename = attribute;
-            /* attribute is an iterator variable in the forEach function. */
-            if (newJSXFlag) {
-              const newSpreadProp = Maybe(blockContainer!.getFirstDescendantByKind(SyntaxKind.JsxSpreadAttribute));
-              const newJSXElem = Maybe(
-                blockContainer!.getFirstDescendantByKind(
-                  elementType as SyntaxKind.JsxOpeningElement | SyntaxKind.JsxSelfClosingElement,
-                ),
-              );
-              if (newSpreadProp.something && newJSXElem.something) {
-                attrToRename = newSpreadProp.value;
-                element = newJSXElem.value;
-              }
-            }
-            /* Cannot use spreadProp because the node has been moved in the AST. */
-            addNewAttributeToComponent(
-              attrToRename,
-              element,
-              replacementName,
-              toRename,
-              changeValueMap,
-              replacementValue,
+          }
+
+          /* Step 5: Get the parent of BLOCKCONTAINER so that we can insert our own variable statements. */
+          const parentContainer = blockContainer!.getParentIfKind(SyntaxKind.Block);
+          if (parentContainer === undefined) {
+            // eslint-disable-next-line no-throw-literal
+            throw 'unable to get parent container from block';
+          }
+
+          /* Step 6: Get the index of BLOCKCONTAINER within PARENTCONTAINER that we'll use to insert our variables. */
+          const insertIndex = blockContainer!.getChildIndex();
+          if (insertIndex === undefined) {
+            // eslint-disable-next-line no-throw-literal
+            throw 'unable to find child index';
+          }
+
+          /* Step 7: Insert new variable statement deconstructing the spread prop, isolating the prop to be renamed. */
+          if (!parentContainer.getVariableStatement(newSpreadName)) {
+            parentContainer.insertVariableStatement(
+              insertIndex,
+              createDeconstructedProp(newSpreadName, toRename, propSpreadName),
             );
           }
+
+          /* Step 8: Declare other auxiliary objects if necessary (i.e. value mapping case). */
+          if (changeValueMap && !parentContainer.getVariableStatement(newMapName)) {
+            parentContainer.insertVariableStatement(
+              insertIndex,
+              createAuxiliaryVariable(VariableDeclarationKind.Const, newMapName, JSON.stringify(changeValueMap)),
+            );
+          }
+
+          /* Step 9: Handle any last auxiliary cases (i.e. component rendered with no body). */
+          let attrToRename = attribute;
+          /* attribute is an iterator variable in the forEach function. */
+          if (newJSXFlag) {
+            const newSpreadProp = Maybe(blockContainer!.getFirstDescendantByKind(SyntaxKind.JsxSpreadAttribute));
+            const newJSXElem = Maybe(
+              blockContainer!.getFirstDescendantByKind(
+                elementType as SyntaxKind.JsxOpeningElement | SyntaxKind.JsxSelfClosingElement,
+              ),
+            );
+            if (newSpreadProp.something && newJSXElem.something) {
+              attrToRename = newSpreadProp.value;
+              element = newJSXElem.value;
+            }
+          }
+
+          /* Step 10: Replace the props in the component with your new ones! */
+          attrToRename.replaceWithText(`{...${newSpreadName}}`); // Replace old spread name.
+          element.addAttribute({
+            name: replacementName,
+            initializer: changeValueMap
+              ? `{${newMapName}[${toRename}]}`
+              : replacementValue
+              ? `{${replacementValue}}`
+              : `{${toRename}}`,
+          }); // Add the updated prop name and set its value.
         }
       }
     }
   });
 }
-function addNewAttributeToComponent(
-  attrToRename: JsxAttributeLike,
-  element: JsxOpeningElement | JsxSelfClosingElement,
-  replacementName: string,
-  toRename: string,
-  changeValueMap?: ValueMap<string>,
-  replacementValue?: string,
-) {
-  attrToRename.replaceWithText(`{...${newSpreadName}}`);
-  element.addAttribute({
-    name: replacementName,
-    initializer: changeValueMap
-      ? `{${newMapName}[${toRename}]}`
-      : replacementValue
-      ? `{${replacementValue}}`
-      : `{${toRename}}`,
-  });
+
+/* Creates and returns a variable statement to the user's specification. KIND determines how
+   the variable is declared (i.e. const), and the name and value (which must be in string form)
+   will be inserted following. */
+function createAuxiliaryVariable(kind: VariableDeclarationKind, varName: string, varValue: string) {
+  return {
+    declarationKind: VariableDeclarationKind.Const,
+    declarations: [
+      {
+        name: varName,
+        initializer: varValue,
+      },
+    ],
+  };
+}
+
+/* Creates and returns a variable statement deconstructing the spread prop into
+   both a new spread prop and the desired prop to rename. */
+function createDeconstructedProp(newSpreadPropName: string, toRename: string, oldSpreadPropName: string) {
+  return {
+    declarationKind: VariableDeclarationKind.Const,
+    declarations: [
+      {
+        name: `{${toRename}, ...${newSpreadPropName}}`,
+        initializer: oldSpreadPropName,
+      },
+    ],
+  };
 }
 
 /* Helper function that returns TRUE if the supplied spread object
@@ -142,8 +158,13 @@ function spreadContains(oldPropName: string, spreadProp?: Identifier): boolean {
 }
 
 /* Verifies that the spread object supplied is a variable
-  defined by CONST or LET, or that it exists as a parameter or variable. */
-function verifyVariableDefinition(kind: ts.ScriptElementKind): boolean {
+  defined by CONST or LET, or that it exists as a parameter or variable.
+  Although an array is passed in, there should be only a single entry. */
+function verifyVariableDefinition(propDefinitions: DefinitionInfo[]): boolean {
+  if (propDefinitions.length !== 1) {
+    return false;
+  }
+  const kind = propDefinitions[0].getKind();
   return (
     kind === ts.ScriptElementKind.constElement ||
     kind === ts.ScriptElementKind.letElement ||
@@ -156,7 +177,7 @@ function verifyVariableDefinition(kind: ts.ScriptElementKind): boolean {
    meaning that the prop is in a function without a block for code,
    indicating a big-arrow function with no body. We will insert that
    block and then continue. */
-function propsArrowFunction(element: JsxOpeningElement | JsxSelfClosingElement) {
+function createBlockContainer(element: JsxOpeningElement | JsxSelfClosingElement) {
   return Maybe(
     element.getFirstAncestor(ans => {
       return ans.getKind() === SyntaxKind.ArrowFunction;
