3e18908badcc0be35ec26092570f4f31b3982267
Karel Rymes <karymes@microsoft.com>
2020-07-22 10:29:29 +0200

    Trying to remove whitespace

--- packages/date-time-utilities/src/dateGrid/isContiguous.ts
+++ packages/date-time-utilities/src/dateGrid/isContiguous.ts
@@ -2,7 +2,6 @@ import { DayOfWeek } from '../dateValues/dateValues';
 
 /**
  * Returns whether provided week days are contiguous.
- * The end of week is wrapping.
  * @param days - list of days in a week
  * @param isSingleWeek - decides whether the contiguous logic applies across week boundaries or not
  * @param firstDayOfWeek - decides which day of week is the first one in the order.
