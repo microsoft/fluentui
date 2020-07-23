cf36658040c2cb72fcf46c48251e9afe7df87fa3
Trip Master <t-dama@microsoft.com>
2020-07-23 13:32:20 -0400

    new button codemod

---
+++ packages/codemods/src/codeMods/mods/oldToNewButton/oldToNewButton.mod.ts
@@ -0,0 +1,23 @@
+import { SourceFile } from 'ts-morph';
+import { CodeMod } from 'src/codeMods/types';
+import { renameImport, renameProp, findJsxTag } from 'src/codeMods/utilities';
+
+//const buttonPath = 'office-ui-fabric-react/lib/Button';
+
+const oldToNewButton: CodeMod = {
+  run: (file: SourceFile) => {
+    try {
+      renameImport(file, 'toggled', 'checked');
+      const tags = findJsxTag(file, 'Button');
+      renameProp(tags, 'toggled', 'checked');
+    } catch (e) {
+      return { success: false };
+    }
+    return { success: true };
+  },
+  version: '100000',
+  name: 'oldToNewButton',
+  enabled: true,
+};
+
+export default oldToNewButton;
