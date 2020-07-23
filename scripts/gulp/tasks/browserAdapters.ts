809b30417fa6b9aed239d1d2a1007f3bf3d28a33
Oleksandr Fediashov <olfedias@microsoft.com>
2020-07-23 14:42:43 +0200

    add perf electron

---
+++ scripts/gulp/tasks/browserAdapters.ts
@@ -0,0 +1,76 @@
+import { spawn, spawnSync } from 'child_process';
+import electron from 'electron';
+import puppeteer from 'puppeteer';
+import CDP from 'chrome-remote-interface';
+
+import { safeLaunchOptions } from '../../puppeteer/puppeteer.config';
+
+const DEFAULT_ELECTRON_PATH = (electron as unknown) as string;
+
+export type Page = {
+  executeJavaScript: <R>(code: string) => Promise<R>;
+  close: () => Promise<void>;
+};
+
+export type Browser = {
+  openPage: (url: string) => Promise<Page>;
+
+  close: () => Promise<void>;
+};
+
+export async function createChrome(): Promise<Browser> {
+  const browser = await puppeteer.launch(safeLaunchOptions());
+
+  return {
+    openPage: async url => {
+      const page = await browser.newPage();
+
+      await page.goto(url);
+
+      return {
+        executeJavaScript: async code => {
+          return page.evaluate(code);
+        },
+        close: page.close,
+      };
+    },
+    close: browser.close,
+  };
+}
+
+export async function createElectron(electronPath: string = DEFAULT_ELECTRON_PATH): Promise<Browser> {
+  const electronVersion = spawnSync(electronPath, ['-v'], {
+    encoding: 'utf8',
+  }).stdout.trim();
+
+  console.log(`Electron version: ${electronVersion}`);
+
+  return {
+    openPage: async url => {
+      const { pid } = spawn(electronPath, ['--remote-debugging-port=9222']);
+
+      const cdp = await CDP();
+
+      await cdp.Network.enable();
+      await cdp.Page.enable();
+      await cdp.Runtime.enable();
+
+      await cdp.Page.navigate({ url });
+      await cdp.Page.loadEventFired();
+
+      return {
+        executeJavaScript: async code => {
+          const { result } = await cdp.Runtime.evaluate({ expression: code, awaitPromise: true, returnByValue: true });
+
+          return result.value;
+        },
+        close: async () => {
+          process.kill(pid);
+          await cdp.close();
+        },
+      };
+    },
+
+    close: async () => {},
+  };
+}
