7e171071ef535f9a705f782c645e4032862d96ee
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-07-29 17:41:32 +0200

    Grouped components in left-side menu into a Tree (#14213)
    * Grouped components in left-side menu into a Tree
    
    * Changed config representation, added expanding to component tree
    
    * Removed some changes
    
    * Added child components  into the groups
    
    * Removed unused code
    
    * Addressed comments
    
    * Code refactoring

--- packages/fluentui/react-builder/src/components/List.tsx
+++ packages/fluentui/react-builder/src/components/List.tsx
@@ -1,10 +1,10 @@
 import * as _ from 'lodash';
 import * as React from 'react';
-import { Box, Menu, Input, tabListBehavior } from '@fluentui/react-northstar';
-import { SearchIcon } from '@fluentui/react-icons-northstar';
+import { Box, Menu, Input, Tree, tabListBehavior } from '@fluentui/react-northstar';
+import { SearchIcon, TriangleDownIcon, TriangleEndIcon } from '@fluentui/react-icons-northstar';
 import { ComponentInfo } from '../componentInfo/types';
 import { componentInfoContext } from '../componentInfo/componentInfoContext';
-import { EXCLUDED_COMPONENTS } from '../config';
+import { EXCLUDED_COMPONENTS, COMPONENT_GROUP } from '../config';
 
 export type ListDisplayModes = 'Display Name' | 'Rendered';
 
@@ -30,11 +30,70 @@ export const List: React.FunctionComponent<ListProps> = ({ onDragStart, style })
     setFilter(value);
   }, []);
 
-  const [supportedComponents, unsupportedComponents] = _.partition(
-    _.values(componentInfoContext.byDisplayName),
-    ({ displayName }) => {
-      return !EXCLUDED_COMPONENTS.some(name => name === displayName);
-    },
+  const [supportedComponents, unsupportedComponents] = React.useMemo(
+    () =>
+      _.partition(_.values(componentInfoContext.byDisplayName), ({ displayName }) => {
+        return !EXCLUDED_COMPONENTS.some(name => name === displayName);
+      }),
+    [],
+  );
+
+  const titleComponent = (Component, { content, expanded, ...rest }) => {
+    return (
+      <div {...rest}>
+        {expanded ? <TriangleDownIcon /> : <TriangleEndIcon />}
+        {content}
+      </div>
+    );
+  };
+
+  const treeobj = React.useMemo(
+    () =>
+      Object.keys(COMPONENT_GROUP).reduce((acc, key) => {
+        return {
+          ...acc,
+          [key]: {
+            id: key,
+            title: {
+              children: titleComponent,
+              content: key,
+            },
+            items: supportedComponents
+              .filter(info => COMPONENT_GROUP[key].includes(info.isChild ? info.parentDisplayName : info.displayName))
+              .map(info => ({
+                id: info.displayName,
+                title: (
+                  <Box
+                    key={info.displayName}
+                    onMouseDown={handleMouseDown(info)}
+                    styles={{
+                      padding: '0.25em 0.75em',
+                      cursor: 'pointer',
+                      ':hover': {
+                        background: '#ddd',
+                        borderLeft: '2px solid #000',
+                      },
+                      borderLeft: '2px solid transparent',
+                      marginLeft: '2px',
+                    }}
+                  >
+                    {displayMode === 'Rendered' && (
+                      <div style={{ position: 'relative' }}>
+                        <div style={{ fontWeight: 'bold', opacity: 0.5 }}>{info.displayName}</div>
+                        <div style={{ padding: '0.5rem', pointerEvents: 'none' }}>
+                          {/* FIXME {React.createElement(resolveComponent(info.displayName), resolveDraggingProps(info.displayName))} */}
+                        </div>
+                      </div>
+                    )}
+
+                    {displayMode === 'Display Name' && info.displayName}
+                  </Box>
+                ),
+              })),
+          },
+        };
+      }, {}),
+    [displayMode, handleMouseDown, supportedComponents],
   );
 
   return (
@@ -77,35 +136,7 @@ export const List: React.FunctionComponent<ListProps> = ({ onDragStart, style })
         onChange={handleFilterChange}
         value={filter}
       />
-      {supportedComponents
-        .filter(info => info.displayName.match(filterRegexp))
-        .map(info => (
-          <Box
-            key={info.displayName}
-            onMouseDown={handleMouseDown(info)}
-            styles={{
-              padding: '0.25em 0.75em',
-              cursor: 'pointer',
-              ':hover': {
-                background: '#ddd',
-                borderLeft: '2px solid #000',
-              },
-              borderLeft: '2px solid transparent',
-              marginLeft: '2px',
-            }}
-          >
-            {displayMode === 'Rendered' && (
-              <div style={{ position: 'relative' }}>
-                <div style={{ fontWeight: 'bold', opacity: 0.5 }}>{info.displayName}</div>
-                <div style={{ padding: '0.5rem', pointerEvents: 'none' }}>
-                  {/* FIXME {React.createElement(resolveComponent(info.displayName), resolveDraggingProps(info.displayName))} */}
-                </div>
-              </div>
-            )}
-
-            {displayMode === 'Display Name' && info.displayName}
-          </Box>
-        ))}
+      <Tree items={Object.values(treeobj)} />
       {unsupportedComponents
         .filter(info => info.displayName.match(filterRegexp))
         .map(info => (
