d12198279424504102496e36a30376ae26b7335e
Caleb Zearing <czearing@outlook.com>
2020-07-29 12:54:45 -0700

    Adding perf and vr tests for TeachingBubble.

---
+++ apps/perf-test/src/scenarios/TeachingBubble.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { ThemeProvider } from 'office-ui-fabric-react';
+
+const Scenario = () => <ThemeProvider />;
+
+export default Scenario;
---
+++ apps/perf-test/src/scenarios/TeachingBubbleNext.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { TeachingBubble } from '@fluentui/react-next';
+
+const Scenario = () => <TeachingBubble />;
+
+export default Scenario;
