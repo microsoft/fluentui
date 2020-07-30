656c79c70ea9b758614ba142b2421681e9160f40
Rafael L. Toscano <rltoscano@gmail.com>
2020-07-30 15:22:35 +0200

    Feature/show element a11y role name (#14255)
    * When a component is selected show its accessible role and name
    
    * check if Accessibility Object Model (AOM) is enabled
    * if enabled, get role and name using it
    * if not, display warning
    
    * Move ReaderText to top and adjust layout
    
    * Refactoring of ComputeMessage
    
    * Fix small issues and add new fields to type ComputedAccessibleNode
    
    * Update packages/fluentui/react-builder/src/componentInfo/types.ts
    
    Co-authored-by: Charles Assunção <junioassuncaocharles@gmail.com>
    
    * Update packages/fluentui/react-builder/src/components/ReaderText.tsx
    
    Co-authored-by: Charles Assunção <junioassuncaocharles@gmail.com>
    
    Co-authored-by: Charles Assunção <junioassuncaocharles@gmail.com>

--- packages/fluentui/react-builder/src/components/ReaderText.tsx
+++ packages/fluentui/react-builder/src/components/ReaderText.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Alert, Ref } from '@fluentui/react-northstar';
+import { ExclamationTriangleIcon } from '@fluentui/react-icons-northstar';
 import { computeMessage } from '../narration/computeMessage';
 
 export type ReaderTextProps = {
@@ -7,29 +8,52 @@ export type ReaderTextProps = {
   node?: HTMLElement;
 };
 
+const AOMDisabledMessage = 'Accessible Object Model (AOM) feature is not enabled.';
+
 export const ReaderText: React.FunctionComponent<ReaderTextProps> = ({ selector, node }) => {
   const ref = React.useRef<HTMLElement>();
   const [text, setText] = React.useState('');
+  const [AOMWarning, setShowAOMWarning] = React.useState(false);
 
   React.useEffect(() => {
     if (ref && ref.current) {
       const element = node || ref.current.ownerDocument.querySelector(selector);
+      const aomEnabled = window.getComputedAccessibleNode;
       const narration = computeMessage(element as HTMLElement);
+
       if (typeof narration === 'string') {
         setText(narration);
       } else {
         narration.then(n => setText(n));
       }
-    }
-  }, [setText, ref, selector, node]);
 
-  if (!selector && !node) {
-    return null;
-  }
+      setShowAOMWarning(!aomEnabled);
+    }
+  // eslint-disable-next-line
+  }, [setShowAOMWarning, setText, ref.current, selector, node]);
 
   return (
-    <Ref innerRef={ref}>
-      <Alert warning content={text} />
-    </Ref>
+    (selector || node) && (
+      <Ref innerRef={ref}>
+        <>
+          <Alert warning content={text} />
+          {AOMWarning && (
+            <Alert
+              tabIndex={0}
+              info
+              icon={<ExclamationTriangleIcon />}
+              content={AOMDisabledMessage}
+              actions={[
+                {
+                  content: 'More Info',
+                  primary: true,
+                  onClick: () => window.open('https://wicg.github.io/aom/caniuse.html', '_blank'),
+                },
+              ]}
+            />
+          )}
+        </>
+      </Ref>
+    )
   );
 };
