d5fd2d384f2f110512a919d88bb9b939e190c571
David Zearing <dzearing@microsoft.com>
2020-07-31 18:46:30 -0700

    Updates

--- packages/react-compose/src/next/getSlots.tsx
+++ packages/react-compose/src/next/getSlots.tsx
@@ -4,11 +4,34 @@ import { GenericDictionary } from './types';
 import { nullRender } from './nullRender';
 
 /**
+ * Tiny helper to do the minimal amount of work in duplicating an object but omitting some
+ * props. This ends up faster than using object ...rest or reduce to filter.
+ *
+ * https://jsperf.com/omit-vs-rest-vs-reduce/1
+ */
+const _omit = <TObj,>(obj: TObj, exclusions: GenericDictionary) => {
+  const result = {};
+
+  for (const key of Object.keys(obj)) {
+    if (!exclusions[key]) {
+      (result as GenericDictionary)[key] = (obj as GenericDictionary)[key];
+    }
+  }
+
+  return result as TObj;
+};
+
+/**
  * Given the state and an array of slot names, will break out `slots` and `slotProps`
- * collections. Ensures that each slot is a renderable component, and each slotProp is
- * a list of acceptable native properties for the given slot. Native properties are only
- * filtered if the slot is rendered "as" a string type, and will use `getNativeElementProps`
- * to pass along only valid props. If the slot is a non-string value, no filtering is done.
+ * collections.
+ *
+ * The root is always derived from the `as` prop.
+ *
+ * Slots will render as null if they are rendered as primitives with undefined children.
+ *
+ * The slotProps will always omit the `as` prop within them, and for slots that are string
+ * primitives, the props will be filtered according the the slot type. For example, if the
+ * slot is rendered `as: 'a'`, the props will be filtered for acceptable anchor props.
  *
  * @param state - State including slot definitions
  * @param slotNames - Name of which props are slots
@@ -19,27 +42,26 @@ export const getSlots = (state: GenericDictionary, slotNames?: string[] | undefi
     root: state.as || nullRender,
   };
   const slotProps: GenericDictionary = {
-    root: getNativeElementProps(state.as, state),
+    root: typeof state.as === 'string' ? getNativeElementProps(state.as, state) : _omit(state, { as: 1 }),
   };
 
   if (slotNames) {
     for (const name of slotNames!) {
       const slotDefinition = state[name];
-      const { as: slotAs, children, ...rest } = slotDefinition;
+      const { as: slotAs, children } = slotDefinition;
+      const isSlotPrimitive = typeof slotAs === 'string';
 
-      const slot = (slots[name] = slotDefinition.children || typeof slotAs !== 'string' ? slotAs : nullRender);
+      slots[name] = slotDefinition.children !== undefined || !isSlotPrimitive ? slotAs : nullRender;
 
       if (slots[name] !== nullRender) {
-        slotProps[name] =
-          typeof slot === 'string'
-            ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              getNativeElementProps(slot as any, slotDefinition)
-            : { ...rest, children };
+        slotProps[name] = isSlotPrimitive
+          ? getNativeElementProps(slotAs, slotDefinition)
+          : _omit(slotDefinition, { as: 1 });
+      }
 
-        if (children === 'function') {
-          slotProps[name].children = children(slots[name], rest);
-          slots[name] = React.Fragment;
-        }
+      if (children === 'function') {
+        slotProps[name].children = children(slots[name], slotDefinition);
+        slots[name] = React.Fragment;
       }
     }
   }
--- packages/utilities/etc/utilities.api.md
+++ packages/utilities/etc/utilities.api.md
@@ -330,7 +330,7 @@ export function getLastFocusable(rootElement: HTMLElement, currentElement: HTMLE
 export function getLastTabbable(rootElement: HTMLElement, currentElement: HTMLElement, includeElementsInFocusZones?: boolean, checkNode?: boolean): HTMLElement | null;
 
 // @public
-export function getNativeElementProps<TAttributes extends React.HTMLAttributes<any>>(tagName: keyof React.ReactHTML, props: {}, excludedPropNames?: string[]): TAttributes;
+export function getNativeElementProps<TAttributes extends React.HTMLAttributes<any>>(tagName: string, props: {}, excludedPropNames?: string[]): TAttributes;
 
 // @public
 export function getNativeProps<T extends Record<string, any>>(props: Record<string, any>, allowedPropNames: string[] | Record<string, number>, excludedPropNames?: string[]): T;
