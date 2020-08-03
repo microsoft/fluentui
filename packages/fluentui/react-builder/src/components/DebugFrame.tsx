048c01996c3b3c8ead32cd8312a64d1d77a084fc
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-08-03 18:01:14 +0200

    For component at the top DebugFrame menu is at the bottom

--- packages/fluentui/react-builder/src/components/DebugFrame.tsx
+++ packages/fluentui/react-builder/src/components/DebugFrame.tsx
@@ -23,16 +23,17 @@ export const DebugFrame: React.FunctionComponent<DebugFrameProps> = ({
 }) => {
   const frameRef = React.useRef<HTMLPreElement>();
   const animationFrameId = React.useRef<number>();
+  const [isTopElement, setIsTopElement] = React.useState(false);
 
   const setFramePosition = React.useCallback((frameEl, controlEl) => {
     const rect = controlEl.getBoundingClientRect();
+    rect.top < 20 ? setIsTopElement(true) : setIsTopElement(false);
+
     frameEl.style.top = `${rect.top}px`;
     frameEl.style.left = `${rect.left}px`;
     frameEl.style.width = `${rect.width}px`;
     frameEl.style.height = `${rect.height}px`;
     frameEl.style.display = 'block';
-
-    animationFrameId.current = requestAnimationFrame(() => setFramePosition(frameEl, controlEl));
   }, []);
 
   const hideFrame = frameEl => {
@@ -71,7 +72,7 @@ export const DebugFrame: React.FunctionComponent<DebugFrameProps> = ({
     }
 
     const el = target.querySelectorAll(selector);
-
+    console.log(animationFrameId);
     animationFrameId.current =
       el.length === 1
         ? requestAnimationFrame(() => setFramePosition(frameRef.current, el[0]))
@@ -79,6 +80,22 @@ export const DebugFrame: React.FunctionComponent<DebugFrameProps> = ({
 
     return () => cancelAnimationFrame(animationFrameId.current);
   }, [target, selector, setFramePosition]);
+
+  const styles = {
+    position: 'absolute',
+    padding: '2px 4px',
+    margin: '-1px 0 0 -1px',
+    left: 0,
+    whiteSpace: 'nowrap',
+    background: '#ffc65c',
+    border: '1px solid #ffc65c',
+    pointerEvents: 'initial',
+
+    display: 'flex',
+    alignItems: 'flex-end',
+    zIndex: 99999998,
+  };
+  isTopElement ? (styles['top'] = '100%') : (styles['bottom'] = '100%');
 
   return (
     <pre
@@ -96,24 +113,7 @@ export const DebugFrame: React.FunctionComponent<DebugFrameProps> = ({
         userSelect: 'none',
       }}
     >
-      <div
-        style={{
-          position: 'absolute',
-          bottom: '100%',
-          padding: '2px 4px',
-          margin: '-1px 0 0 -1px',
-          left: 0,
-
-          whiteSpace: 'nowrap',
-          background: '#ffc65c',
-          border: '1px solid #ffc65c',
-          pointerEvents: 'initial',
-
-          display: 'flex',
-          alignItems: 'flex-end',
-          zIndex: 99999998,
-        }}
-      >
+      <div style={styles}>
         <span style={{ fontWeight: 'bold' }}>{componentName}</span>
         <LevelUpDebugButton onClick={handleGoToParent} />
         <MoveDebugButton onClick={handleMove} />
