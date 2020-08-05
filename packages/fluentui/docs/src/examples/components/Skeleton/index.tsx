c913948885a7254ec666d16c422adb19c99ec4ed
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-08-05 09:25:07 +0200

    feat(skeleton): add performance

--- packages/fluentui/docs/src/examples/components/Skeleton/index.tsx
+++ packages/fluentui/docs/src/examples/components/Skeleton/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Usage from './Usage';
 import Variations from './Variations';
+import Performance from './Performance';
 
 const SkeletonExamples = () => (
   <>
     <Usage />
     <Variations />
+    <Performance />
   </>
 );
 
