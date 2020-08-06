f5bdf53e0ed67e15561509295d4e4db321d6506c
Oleksandr Fediashov <olfedias@microsoft.com>
2020-08-06 12:51:57 +0200

    remove empty strings

--- packages/fluentui/docs/src/examples/components/Button/Performance/ButtonDesign.perf.tsx
+++ packages/fluentui/docs/src/examples/components/Button/Performance/ButtonDesign.perf.tsx
@@ -6,229 +6,229 @@ const ButtonDesignPerf = () => (
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
     {/* ---- */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
     {/* ---- */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
   </Provider>
 );
--- packages/fluentui/docs/src/examples/components/Button/Performance/ButtonDesignMiss.perf.tsx
+++ packages/fluentui/docs/src/examples/components/Button/Performance/ButtonDesignMiss.perf.tsx
@@ -6,229 +6,229 @@ const ButtonDesignMissPerf = () => (
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid blue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid yellow', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid magenta', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid olive', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid salmon', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid orange', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #fff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #000', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #ccc', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid black', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid white', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid pink', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid aliceblue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #cccfff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
     {/* ---- */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid blue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid yellow', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid magenta', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid olive', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid salmon', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid orange', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #fff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #000', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #ccc', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid black', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid white', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid pink', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid aliceblue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #cccfff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
     {/* ---- */}
     <Button
       content="Click here"
       design={{ border: '1px solid red', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 1 */}
     <Button
       content="Click here"
       design={{ border: '1px solid blue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 2 */}
     <Button
       content="Click here"
       design={{ border: '1px solid yellow', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 3 */}
     <Button
       content="Click here"
       design={{ border: '1px solid magenta', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 4 */}
     <Button
       content="Click here"
       design={{ border: '1px solid olive', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 5 */}
     <Button
       content="Click here"
       design={{ border: '1px solid salmon', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 6 */}
     <Button
       content="Click here"
       design={{ border: '1px solid orange', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 7 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #fff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 8 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #000', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 9 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #ccc', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 10 */}
     <Button
       content="Click here"
       design={{ border: '1px solid black', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 11 */}
     <Button
       content="Click here"
       design={{ border: '1px solid white', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 12 */}
     <Button
       content="Click here"
       design={{ border: '1px solid pink', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 13 */}
     <Button
       content="Click here"
       design={{ border: '1px solid aliceblue', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 14 */}
     <Button
       content="Click here"
       design={{ border: '1px solid #cccfff', display: 'flex', padding: '10px', margin: '10px', position: 'initial' }}
-    />{' '}
+    />
     {/* 15 */}
   </Provider>
 );
