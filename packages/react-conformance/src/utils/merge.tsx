c1c3fdfa14821ace1b1e69a532d6aae4ddd04de1
Esteban Munoz <esteban.230@hotmail.com>
2020-08-07 13:52:46 -0600

    adding requested changes

--- packages/react-conformance/src/utils/merge.tsx
+++ packages/react-conformance/src/utils/merge.tsx
@@ -1,26 +1,28 @@
 import * as _ from 'lodash';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const isObject = (o: any) => o !== null && typeof o === 'object' && !_.isArray(o);
+const isObject = (o: any) => o !== null && typeof o === 'object' && !Array.isArray(o);
 
 /**
  * Merge function with specific type
  * @param objs - objects to be merged
  */
 export function merge<T = Object>(...objs: Object[]): T {
-  const [merged, ...rest] = objs;
+  const merged = {};
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const customMergeFunction = (dest: any, src: any): any => {
-    if (isObject(dest) && isObject(dest)) {
-      return _.mergeWith(dest, src, customMergeFunction);
-    } else if (Array.isArray(src) && Array.isArray(dest)) {
+    if (_.isArray(dest)) {
       return _.uniq(dest.concat(src));
-    } else if (!Array.isArray(src) && !isObject(src) && Array.isArray(dest)) {
-      return _.uniq(dest.concat(src));
+    } else if (isObject(dest) && isObject(src)) {
+      const temp = {};
+      _.mergeWith(temp, dest, src, customMergeFunction);
+      return temp;
     }
     return dest;
   };
 
-  return (_.mergeWith(merged, ...rest, customMergeFunction) as unknown) as T;
+  _.mergeWith(merged, ...objs, customMergeFunction);
+
+  return merged as T;
 }
