3060a27afa38fbc60fc0b4a495bcc0147f63d40e
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-08-10 23:57:28 +0200

    feat(Skeleton): fix import

--- packages/fluentui/react-northstar/src/themes/teams-high-contrast/components/Skeleton/skeletonVariables.ts
+++ packages/fluentui/react-northstar/src/themes/teams-high-contrast/components/Skeleton/skeletonVariables.ts
@@ -1,4 +1,4 @@
-import { SkeletonVariables } from '../../../teams/components/Skeleton/SkeletonVariables';
+import { SkeletonVariables } from '../../../teams/components/Skeleton/skeletonVariables';
 
 export const skeletonVariables = (siteVars: any): Partial<SkeletonVariables> => ({
   lineBackground: siteVars.colorScheme.brand.background,
