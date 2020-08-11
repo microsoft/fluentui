39d8a279a7f09b82acb7ad7e6b6b797546c1cdd3
Michael Loughry <miclo@microsoft.com>
2020-08-11 19:35:50 +0000

    Revert changes to common

--- packages/react-next/src/common/sharedIsConformant.ts
+++ packages/react-next/src/common/sharedIsConformant.ts
@@ -3,7 +3,7 @@ import { isConformant, IsConformantOptions } from '@fluentui/react-conformance';
 
 export function sharedIsConformant(testInfo: IsConformantOptions) {
   const defaultOptions = {
-    disabledTests: ['has-docblock'],
+    disabledTests: ['has-docblock', 'kebab-aria-attributes'],
   };
 
   isConformant(defaultOptions, testInfo);
