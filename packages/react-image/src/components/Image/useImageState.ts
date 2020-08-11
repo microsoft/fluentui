77f039ba4aa98577859902f0f5aaf20e844ce1f1
Marija Najdova <mnajdova@gmail.com>
2020-08-11 13:48:28 +0200

    -added a11y

--- packages/react-image/src/components/Image/useImageState.ts
+++ packages/react-image/src/components/Image/useImageState.ts
@@ -1,11 +1,10 @@
-import { ComposePreparedOptions } from '@fluentui/react-compose';
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore Typings require esModuleInterop
 import objectFitImages from 'object-fit-images';
 /* eslint-enable @typescript-eslint/ban-ts-comment */
 import * as React from 'react';
 
-import { ImageProps, ImageState } from './Image.types';
+import { ImageState } from './Image.types';
 
 const isFitSupported = (function() {
   const testImg = new Image();
@@ -16,7 +15,7 @@ const isFitSupported = (function() {
 /**
  * The useImage hook processes the Image component props and returns state.
  */
-export const useImageState = (state: ImageProps & ImageState) => {
+export const useImageState = (state: ImageState) => {
   const imageRef = React.useRef<HTMLElement>(null);
 
   React.useEffect(() => {
@@ -25,8 +24,6 @@ export const useImageState = (state: ImageProps & ImageState) => {
     }
   }, []);
 
-  state = {
-    ...state,
-    imageRef,
-  };
+  state['aria-hidden'] = state.alt || state['aria-label'] ? undefined : 'true';
+  state.imageRef = imageRef;
 };
