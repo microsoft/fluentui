306edd737757ba27ae55332825d200fddcfa5bd7
Xu Gao <xugao0131@hotmail.com>
2020-08-11 10:53:31 -0700

    ThemeProvider: support V7 theme (#14398)
    * support v7 theme to pass to themeprovider
    
    * add vr test for theming
    
    * fixes
    
    * Change files & cleanups
    
    * remove createTheme
    
    * fix scss components
    
    * merge
    
    * resolve comment

--- packages/react-theme-provider/src/mergeThemes.ts
+++ packages/react-theme-provider/src/mergeThemes.ts
@@ -1,16 +1,15 @@
 import { PartialTheme, Theme } from './types';
 import { merge } from '@uifabric/utilities';
-import { createDefaultTheme } from './createDefaultTheme';
 
 /**
- * Merges all themes on top of a blank initial theme and ensures the theme is fully qualified.
+ * Merges multiple themes.
  */
-export const mergeThemes = (...themes: (undefined | PartialTheme | Theme)[]): Theme => {
-  const partialTheme = merge<PartialTheme | Theme>(createDefaultTheme(), ...themes);
+export function mergeThemes<TResult = PartialTheme>(...themes: (undefined | PartialTheme | Theme)[]): TResult {
+  const partialTheme = merge<PartialTheme | Theme>({}, ...themes);
 
   // Correctly merge stylesheets array
   partialTheme.stylesheets = [];
   themes.forEach(theme => theme && theme.stylesheets && partialTheme.stylesheets?.push(...theme.stylesheets));
 
-  return partialTheme as Theme;
-};
+  return partialTheme as TResult;
+}
