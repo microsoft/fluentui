f5b3cce79e9a4d0ede618e06eb5c66ec1f3d2c3d
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-08-10 17:22:55 -0700

    Fixing lint error.

--- packages/react-button/src/components/SplitButton/SplitButton.types.ts
+++ packages/react-button/src/components/SplitButton/SplitButton.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ShorthandProps } from '@fluentui/react-compose/lib/next';
+import { ShorthandProps } from '@fluentui/react-compose/lib/next/index';
 import { ButtonProps } from '../Button/Button.types';
 import { MenuButtonProps, MenuButtonState, MenuButtonTokens } from '../MenuButton/MenuButton.types';
 
