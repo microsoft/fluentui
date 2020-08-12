2d47091448c6f55cf88b4eb077d9d05e3da145b8
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-08-12 20:38:22 +0200

    fix(Sidebar): behavior

--- packages/fluentui/accessibility/src/behaviors/Tree/treeTitleBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Tree/treeTitleBehavior.ts
@@ -1,4 +1,4 @@
-import { SpacebarKey, EnterKey } from '@fluentui/keyboard-key';
+import { SpacebarKey } from '@fluentui/keyboard-key';
 
 import { IS_FOCUSABLE_ATTRIBUTE } from '../../attributes';
 import { Accessibility, AriaRole } from '../../types';
@@ -33,7 +33,7 @@ export const treeTitleBehavior: Accessibility<TreeTitleBehaviorProps> = props =>
     keyActions: {
       root: {
         performClick: {
-          keyCombinations: [{ keyCode: SpacebarKey }, { keyCode: EnterKey }],
+          keyCombinations: [{ keyCode: SpacebarKey }],
         },
       },
     },
