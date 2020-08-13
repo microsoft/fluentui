9823f5e5104dedccc68fa5bbd41afb7ed8b286aa
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-08-13 15:53:07 +0200

    Code refactoring

--- packages/fluentui/react-builder/src/components/Designer.tsx
+++ packages/fluentui/react-builder/src/components/Designer.tsx
@@ -467,8 +467,7 @@ export const Designer: React.FunctionComponent = () => {
     e => {
       let command = '';
       command += e.altKey ? 'Alt+' : '';
-      command += e.ctrlKey ? 'Ctrl+' : '';
-      command += e.metaKey ? 'Ctrl+' : ''; // Command key on Mac OS
+      command += e.ctrlKey | e.metaKey ? 'Ctrl+' : '';
       command += e.shiftKey ? 'Shift+' : '';
       command += e.key;
       hotkeys.hasOwnProperty(command) && hotkeys[command]();
