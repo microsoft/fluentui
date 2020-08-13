9d048af796f08d64a858c559379e272df4c6424e
Steve Adelson <sadelson@microsoft.com>
2020-08-13 16:00:24 -0700

    eslint wants imports to be absolute paths

--- packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/DoubleUnifiedPeoplePicker.Example.tsx
+++ packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/DoubleUnifiedPeoplePicker.Example.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { UnifiedPeoplePickerExample } from './UnifiedPeoplePicker.Example';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { UnifiedPeoplePickerExample } from '@uifabric/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/UnifiedPeoplePicker.Example';
 
 export const DoubleUnifiedPeoplePickerExample = (): JSX.Element => {
   return (
