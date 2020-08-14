6133123b023e1e6365dedfe8c6f893d981346032
Steve Adelson <sadelson@microsoft.com>
2020-08-13 18:06:08 -0700

    Trying to work around build eslint check issue

--- packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/DoubleUnifiedPeoplePicker.Example.tsx
+++ packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/DoubleUnifiedPeoplePicker.Example.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { UnifiedPeoplePickerExample } from '@uifabric/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/examples/UnifiedPeoplePicker.Example';
+import { UnifiedPeoplePickerExample } from '@uifabric/experiments/lib/UnifiedPeoplePicker';
 
 export const DoubleUnifiedPeoplePickerExample = (): JSX.Element => {
   return (
