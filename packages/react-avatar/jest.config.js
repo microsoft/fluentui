d82f19afe105152deb454ddf7597b83423a7646a
dzearing <dzearing@hotmail.com>
2020-08-13 19:22:20 -0700

    Ensuring tests work.

--- packages/react-avatar/jest.config.js
+++ packages/react-avatar/jest.config.js
@@ -1,16 +1,8 @@
-let { createConfig, resolveMergeStylesSerializer } = require('@uifabric/build/jest/jest-resources');
-let path = require('path');
+const { createConfig } = require('@uifabric/build/jest/jest-resources');
+const path = require('path');
 
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
-
-  moduleNameMapper: {
-    'office-ui-fabric-react/lib/(.*)$': 'office-ui-fabric-react/lib-commonjs/$1',
-    '@fluentui/react/lib/(.*)$': '@fluentui/react/lib-commonjs/$1',
-    '@fluentui/react-northstar/lib/(.*)$': '@fluentui/react-northstar/lib-commonjs/$1',
-  },
-
-  snapshotSerializers: [resolveMergeStylesSerializer()],
 });
 
 module.exports = config;
