7bc276e60fb13b37e247dc889063cb6bb82037a0
Karel Rymes <karymes@microsoft.com>
2020-08-14 16:25:31 +0200

    Reverting header cell

--- packages/fluentui/react-northstar/src/components/Datepicker/DatepickerCalendarHeaderCell.tsx
+++ packages/fluentui/react-northstar/src/components/Datepicker/DatepickerCalendarHeaderCell.tsx
@@ -1,23 +1,8 @@
-import { Accessibility } from '@fluentui/accessibility';
-import {
-  useAccessibility,
-  useStyles,
-  useFluentContext,
-  useTelemetry,
-  compose,
-  getElementType,
-  useUnhandledProps,
-} from '@fluentui/react-bindings';
-import * as React from 'react';
-import { commonPropTypes, createShorthand, ContentComponentProps, UIComponentProps } from '../../utils';
-import { Text, TextProps } from '../Text/Text';
+import { compose } from '@fluentui/react-bindings';
+import { commonPropTypes } from '../../utils';
+import { BoxProps, Box } from '../Box/Box';
 
-export interface DatepickerCalendarHeaderCellProps extends UIComponentProps, ContentComponentProps<TextProps> {
-  /**
-   * Accessibility behavior if overridden by the user.
-   */
-  accessibility?: Accessibility<never>;
-}
+export type DatepickerCalendarHeaderCellProps = {};
 
 export type DatepickerCalendarHeaderCellStylesProps = {};
 
@@ -30,66 +15,19 @@ export const DatepickerCalendarHeaderCell = compose<
   'span',
   DatepickerCalendarHeaderCellProps,
   DatepickerCalendarHeaderCellStylesProps,
-  {},
+  BoxProps,
   {}
->(
-  (props, ref, composeOptions) => {
-    const context = useFluentContext();
-    const { setStart, setEnd } = useTelemetry(composeOptions.displayName, context.telemetry);
-    const { className, design, styles, variables, content } = props;
-    setStart();
-
-    const unhandledProps = useUnhandledProps(composeOptions.handledProps, props);
-    const ElementType = getElementType(props);
-    const getA11yProps = useAccessibility(props.accessibility, {
-      debugName: composeOptions.displayName,
-      mapPropsToBehavior: () => ({}),
-      rtl: context.rtl,
-    });
-
-    const { classes } = useStyles<DatepickerCalendarHeaderCellStylesProps>(DatepickerCalendarHeaderCell.displayName, {
-      className: composeOptions.className,
-      mapPropsToStyles: () => ({}),
-      mapPropsToInlineStyles: () => ({
-        className,
-        design,
-        styles,
-        variables,
-      }),
-      rtl: context.rtl,
-      composeOptions,
-      unstable_props: props,
-    });
-
-    const element = (
-      <ElementType
-        {...getA11yProps('root', {
-          className: classes.root,
-          ref,
-          ...unhandledProps,
-        })}
-      >
-        {createShorthand(Text, content, {
-          defaultProps: () => getA11yProps('label', {}),
-        })}
-      </ElementType>
-    );
-
-    setEnd();
-    return element;
+>(Box, {
+  className: datepickerCalendarHeaderCellClassName,
+  displayName: 'DatepickerCalendarHeaderCell',
+  overrideStyles: true,
+  shorthandConfig: {
+    mappedProp: 'content',
   },
-  {
-    className: datepickerCalendarHeaderCellClassName,
-    displayName: 'DatepickerCalendarHeaderCell',
-
-    handledProps: ['accessibility', 'as', 'className', 'content', 'design', 'styles', 'variables'],
-  },
-);
-
-DatepickerCalendarHeaderCell.propTypes = {
-  ...commonPropTypes.createCommon({ children: false }),
-};
+});
 
 DatepickerCalendarHeaderCell.defaultProps = {
   as: 'span',
 };
+
+DatepickerCalendarHeaderCell.propTypes = commonPropTypes.createCommon();
