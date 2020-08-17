1cef811b7cd4a70e294a66676ed00681da70f99a
dzearing <dzearing@hotmail.com>
2020-08-17 15:13:04 -0700

    initial tweaks.

--- packages/react-theme-provider/src/ThemeProvider.types.ts
+++ packages/react-theme-provider/src/ThemeProvider.types.ts
@@ -8,4 +8,22 @@ export interface ThemeProviderProps extends React.HTMLAttributes<HTMLDivElement>
    * Defines the theme provided by the user.
    */
   theme?: PartialTheme | Theme;
+
+  /**
+   * Apply appropriate styling to the element, to body, or to none. This includes,
+   * but is not limited to, default fonts and colors. Defaults to the ThemeProvider
+   * element (`element`).
+   */
+  // applyTo?: 'none' | 'element' | 'body';
+
+  /**
+   * Targets styling to be injected into a particular window.
+   */
+  // targetWindow?: Window | null;
+
+  /**
+   * Allows the style rendering to be overridden to use your css in js mechanism
+   * of choice.
+   */
+  // styleRenderer?: (styleSet: StyleSet) => ClassNameSet;
 }
