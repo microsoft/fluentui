110e6d7820e2b751510b846aeefd12a916741d32
pompomon <pompomon@gmail.com>
2020-08-18 18:35:44 +0200

    Add cell customization example/test

--- packages/fluentui/docs/src/examples/components/Datepicker/index.tsx
+++ packages/fluentui/docs/src/examples/components/Datepicker/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 
 import Types from './Types';
 import States from './States';
+import Slots from './Slots';
 import Usage from './Usage';
 
 const DatepickerExamples = () => (
   <>
     <Types />
     <States />
+    <Slots />
     <Usage />
   </>
 );
