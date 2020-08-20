386b168a9d5286647920ede15041bbc9bcae62c8
Karel Rymes <karymes@microsoft.com>
2020-08-20 14:19:41 +0200

    Fixing tests

--- packages/fluentui/react-northstar/src/components/Datepicker/DatepickerCalendarHeader.tsx
+++ packages/fluentui/react-northstar/src/components/Datepicker/DatepickerCalendarHeader.tsx
@@ -171,6 +171,7 @@ DatepickerCalendarHeader.propTypes = {
   isOutOfBoundsErrorMessage: PropTypes.string,
   goToToday: PropTypes.string,
   openCalendarTitle: PropTypes.string,
+  inputPlaceholder: PropTypes.string,
   prevMonthAriaLabel: PropTypes.string,
   nextMonthAriaLabel: PropTypes.string,
   prevYearAriaLabel: PropTypes.string,
