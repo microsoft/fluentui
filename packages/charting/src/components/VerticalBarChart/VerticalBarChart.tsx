a95407790172cb25bad455e6d4cffb2bf12bc629
Elizabeth Craig <elcraig@microsoft.com>
2020-08-19 17:18:17 -0700

    Remove references to global React namespace and add lint rule against it (#14613)

--- packages/charting/src/components/VerticalBarChart/VerticalBarChart.tsx
+++ packages/charting/src/components/VerticalBarChart/VerticalBarChart.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { styled } from '../../Utilities';
 import { IVerticalBarChartProps, IVerticalBarChartStyleProps, IVerticalBarChartStyles } from './VerticalBarChart.types';
 import { VerticalBarChartBase } from './VerticalBarChart.base';
--- packages/experiments/src/components/FloatingSuggestions/Suggestions/SuggestionsItem.types.ts
+++ packages/experiments/src/components/FloatingSuggestions/Suggestions/SuggestionsItem.types.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { IStyle, ITheme } from '../../../Styling';
 import { IRefObject, IStyleFunctionOrObject } from '../../../Utilities';
 import { ISuggestionModel } from 'office-ui-fabric-react/lib/Pickers';
