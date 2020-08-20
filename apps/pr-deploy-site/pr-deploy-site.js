087f1b3674a4bb91f42ad73079e1e70773adea51
Oleksandr Fediashov <olfedias@microsoft.com>
2020-08-20 13:24:13 +0200

    add image to deployment

--- apps/pr-deploy-site/pr-deploy-site.js
+++ apps/pr-deploy-site/pr-deploy-site.js
@@ -27,6 +27,12 @@ const siteInfo = [
     title: 'Button',
   },
   {
+    package: '@fluentui/react-image',
+    link: './react-button/dist-storybook/index.html',
+    icon: 'FileImage',
+    title: 'Image',
+  },
+  {
     package: '@fluentui/react-tabs',
     link: './react-tabs/dist-storybook/index.html',
     icon: 'BrowserTab',
