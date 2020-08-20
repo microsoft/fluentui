6922c1c702860c903de06eb709fc0ac2f6cc2938
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-08-20 14:37:35 +0200

    fix(MenuDivider): add tests

---
+++ packages/fluentui/react-northstar/test/specs/components/Menu/MenuItemDivider-test.tsx
@@ -0,0 +1,8 @@
+import { isConformant } from 'test/specs/commonTests';
+import { MenuDivider } from 'src/components/Menu/MenuDivider';
+
+describe('MenuDivider', () => {
+  isConformant(MenuDivider, {
+    constructorName: 'MenuDivider',
+  });
+});
