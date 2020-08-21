7ae3876c2a81af4af22691d0792fa890ecb46a8b
Karel Rymes <karymes@microsoft.com>
2020-08-21 17:26:37 +0200

    Fixing test

--- packages/fluentui/docs/src/examples/components/Datepicker/Slots/DatepickerExampleClearable.shorthand.steps.tsx
+++ packages/fluentui/docs/src/examples/components/Datepicker/Slots/DatepickerExampleClearable.shorthand.steps.tsx
@@ -1,14 +1,14 @@
-import { buttonClassName, datepickerCalendarCellClassName } from '@fluentui/react-northstar';
+import { inputClassName, buttonClassName, datepickerCalendarCellClassName } from '@fluentui/react-northstar';
 
 const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: [
     builder =>
       builder
-        .click(`.${buttonClassName}`)
+        .click(`.${inputClassName}`)
         .click(`.${datepickerCalendarCellClassName}:nth-child(15)`)
         .snapshot('Shows selected date in input with clear possibility.')
-        .click(`.${buttonClassName}__icon`)
+        .click(`.${inputClassName}__icon`)
         .snapshot('Shows cleared input.')
         .click(`.${buttonClassName}`)
         .snapshot('Shows cleared calendar.'),
