8c75828d741dc845ef0fa5dd09a61da84831f47d
Caleb Zearing <czearing@outlook.com>
2020-08-21 14:41:22 -0700

    Removing a code comment from Rating.test.tsx

--- packages/react-next/src/components/Rating/Rating.test.tsx
+++ packages/react-next/src/components/Rating/Rating.test.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-
-// import * as ReactDOM from 'react-dom';
 import * as renderer from 'react-test-renderer';
 import { mount, ReactWrapper } from 'enzyme';
 import { Rating } from './Rating';
