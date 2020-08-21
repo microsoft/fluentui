21b027b98d4e7d28fd505784f6e31076a5f7afab
Trip Master <46080180+tmaster628@users.noreply.github.com>
2020-08-20 23:07:08 -0400

    CodeMods: Add Result to prop utility functions (#14642)
    * add result to prophelper functions
    
    * Change files
    
    Co-authored-by: Trip Master <t-dama@microsoft.com>

--- packages/codemods/src/codeMods/utilities/props.ts
+++ packages/codemods/src/codeMods/utilities/props.ts
@@ -1,7 +1,8 @@
 import { renamePropInSpread } from './helpers/propHelpers';
 import { JsxOpeningElement, JsxSelfClosingElement, SyntaxKind } from 'ts-morph';
 import { Maybe } from '../../helpers/maybe';
-import { PropTransform } from '../types';
+import { PropTransform, NoOp } from '../types';
+import { Result, Ok, Err } from '../../../src/helpers/result';
 
 export function renameProp(
   instances: (JsxOpeningElement | JsxSelfClosingElement)[],
@@ -9,31 +10,43 @@ export function renameProp(
   replacementName: string,
   replacementValue?: string,
   transform?: PropTransform,
-) {
+): Result<string, NoOp> {
   instances.forEach(val => {
     /* For each instance, first see if desired prop exists in the open. */
     const foundProp = Maybe(val.getAttribute(toRename));
-
     if (foundProp.something) {
       /* If found, do a simple name-replacementName. */
       foundProp.value.set({ name: replacementName });
       if (replacementValue) {
-        foundProp.value.getFirstChildByKind(SyntaxKind.JsxExpression)?.replaceWithText(`{${replacementValue}}`);
+        const childElem = Maybe(foundProp.value.getFirstChildByKind(SyntaxKind.JsxExpression));
+        if (childElem.something) {
+          childElem.value.replaceWithText(`{${replacementValue}}`);
+        } else {
+          return Err({ reason: 'Could not access prop from JSX tag' });
+        }
       } else {
         const enumInJsx = Maybe(foundProp.value.getFirstChildByKind(SyntaxKind.JsxExpression));
         if (enumInJsx.something) {
           if (transform) {
-            transform(enumInJsx.value, toRename, replacementName);
+            const res: Result<string, NoOp> = transform(enumInJsx.value, toRename, replacementName);
+            if (!res.ok) {
+              return Err({ reason: res.value.reason });
+            }
           }
         }
       }
     } else {
       /* If the prop is not found, check to see if the prop is in a spread attribute. */
+      let res: Result<string, NoOp>;
       if (transform) {
-        transform(val, toRename, replacementName);
+        res = transform(val, toRename, replacementName);
       } else {
-        renamePropInSpread(val, toRename, replacementName, undefined, replacementValue);
+        res = renamePropInSpread(val, toRename, replacementName, undefined, replacementValue);
+      }
+      if (!res.ok) {
+        return Err({ reason: res.value.reason });
       }
     }
   });
+  return Ok('Successfully renamed the given props.');
 }
