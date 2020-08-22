79026629eced7075c1718fbbd98fbb8f17d93e21
Caleb Zearing <czearing@outlook.com>
2020-08-21 19:46:47 -0700

    Casing fix

--- packages/office-ui-fabric-react/src/utilities/ButtonGrid/ButtonGrid.tsx
+++ packages/office-ui-fabric-react/src/utilities/ButtonGrid/ButtonGrid.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { styled } from '../../Utilities';
 import { ButtonGridBase } from './ButtonGrid.base';
 import { IButtonGridProps, IButtonGridStyleProps, IButtonGridStyles } from './ButtonGrid.types';
-import { getStyles } from './buttonGrid.styles';
+import { getStyles } from './ButtonGrid.styles';
 
 export const ButtonGrid: React.FunctionComponent<IButtonGridProps> = styled<
   IButtonGridProps,
