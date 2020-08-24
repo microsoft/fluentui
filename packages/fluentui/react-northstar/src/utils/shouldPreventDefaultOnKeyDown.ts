0e3942a69fde31a7e1d986f1e78238e1f822a72f
Oleksandr Fediashov <olfedias@microsoft.com>
2020-08-24 16:27:39 +0200

    add UTs

--- packages/fluentui/react-northstar/src/utils/shouldPreventDefaultOnKeyDown.ts
+++ packages/fluentui/react-northstar/src/utils/shouldPreventDefaultOnKeyDown.ts
@@ -4,12 +4,13 @@ import * as React from 'react';
 /**
  * Checks if `preventDefault()` should be called for a passed keyboard event.
  */
-export function shouldPreventDefaultOnKeyDown(e: React.KeyboardEvent) {
+export function shouldPreventDefaultOnKeyDown(e: KeyboardEvent | React.KeyboardEvent) {
   const code = getCode(e);
-  const target = e.target as HTMLElement;
+  const target: HTMLElement | undefined = e.target as HTMLElement;
 
   const matchesByKey = code === SpacebarKey || code === EnterKey;
-  const ignoredByTag = target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable === true;
+  const ignoredByTag =
+    target?.tagName === 'INPUT' || target?.tagName === 'TEXTAREA' || target?.isContentEditable === true;
 
   return matchesByKey && !ignoredByTag;
 }
