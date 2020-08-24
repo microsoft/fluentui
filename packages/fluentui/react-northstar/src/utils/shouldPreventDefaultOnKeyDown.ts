0366b1a7ea7be008f561f9bb054b79cc80e03c54
Oleksandr Fediashov <olfedias@microsoft.com>
2020-08-24 15:30:31 +0200

    fix(RadioGroupItem): do not always `preventDefault()`

---
+++ packages/fluentui/react-northstar/src/utils/shouldPreventDefaultOnKeyDown.ts
@@ -0,0 +1,12 @@
+import { getCode, SpacebarKey, EnterKey } from '@fluentui/keyboard-key';
+import * as React from 'react';
+
+export function shouldPreventDefaultOnKeyDown(e: React.KeyboardEvent) {
+  const code = getCode(e);
+  const target = e.target as HTMLElement;
+
+  const matchesByKey = code === SpacebarKey || code === EnterKey;
+  const ignoredByTag = target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable === true;
+
+  return matchesByKey && !ignoredByTag;
+}
