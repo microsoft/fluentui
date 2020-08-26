d4da27c66cb05e994b7b7f926054ba30bf8656fa
Oleksandr Fediashov <olfedias@microsoft.com>
2020-08-26 22:26:37 +0200

    fix(Toolbar): make Popup visible on disabled elements (#14756)
    (cherry picked from commit 2b10a774e2596fde30ee83461dea114207760065)

--- packages/fluentui/react-bindings/src/index.ts
+++ packages/fluentui/react-bindings/src/index.ts
@@ -17,6 +17,7 @@ export { useDispatchEffect as unstable_useDispatchEffect } from './hooks/useDisp
 export { useIsomorphicLayoutEffect } from './hooks/useIsomorphicLayoutEffect';
 export { useStateManager } from './hooks/useStateManager';
 export * from './hooks/useStyles';
+export { useTriggerElement } from './hooks/useTriggerElement';
 export { useUnhandledProps } from './hooks/useUnhandledProps';
 
 export { RendererContext } from './renderer/RendererContext';
@@ -29,6 +30,7 @@ export * from './styles/types';
 export { getTelemetry as deprecated_getTelemetry, useTelemetry } from './telemetry/useTelemetry';
 export * from './telemetry/types';
 
+export { childrenExist } from './utils/childrenExist';
 export { getElementType } from './utils/getElementType';
 export { getUnhandledProps } from './utils/getUnhandledProps';
 export { mergeVariablesOverrides } from './utils/mergeVariablesOverrides';
