e469f78d827efad37be8a787f505a06bca3372ff
Karel Rymes <karymes@microsoft.com>
2020-08-27 16:45:28 +0200

    PR trigger

--- packages/fluentui/docs/src/examples/components/Input/Variations/InputExampleClearable.shorthand.steps.ts
+++ packages/fluentui/docs/src/examples/components/Input/Variations/InputExampleClearable.shorthand.steps.ts
@@ -1,7 +1,7 @@
 import { inputClassName } from '@fluentui/react-northstar';
 
 const config: ScreenerTestsConfig = {
-  steps: [builder => builder.setValue(`.${inputClassName}__input`, 'Some text...').snapshot('Can be clearable')],
+  steps: [builder => builder.setValue(`.${inputClassName} input`, 'Some text...').snapshot('Can be clearable')],
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
 };
 
