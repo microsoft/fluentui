fdffd91e4fc2d293f4649069ae629d7f5a741773
Tomi Olubeko <ololubek@microsoft.com>
2020-08-26 19:26:34 -0700

    Add CommandBarButton to react-button package

---
+++ packages/react-button/src/components/CommandBarButton/CommandBarButton.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { useButton } from '../Button/useButton';
+import { ButtonProps } from '../Button/Button.types';
+import { useFocusRects } from '@uifabric/utilities';
+import { makeClasses } from '@fluentui/react-compose/lib/next/index';
+import { useInlineTokens } from '@fluentui/react-theme-provider';
+import * as commandBarButtonClasses from './CommandBarButton.scss';
+import { useButtonClasses } from '../Button/Button';
+
+// Create a hook to resolve classnames.
+export const useCommandBarButtonClasses = makeClasses(commandBarButtonClasses);
+
+/**
+ * Define a styled Button, using the `useButton` hook.
+ */
+export const CommandBarButton = React.forwardRef<HTMLElement, ButtonProps>((props, ref) => {
+  const { render, state } = useButton(props, ref);
+
+  // Apply styling.
+  useButtonClasses(state);
+  useCommandBarButtonClasses(state);
+  useFocusRects(state.ref);
+  useInlineTokens(state, '--button');
+
+  // Render component.
+  return render(state);
+});
+
+CommandBarButton.displayName = 'CommandBarButton';
