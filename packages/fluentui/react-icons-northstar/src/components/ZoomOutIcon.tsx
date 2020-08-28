6421efa6a4cb9d44b272f7c11eba26abfb91a1a2
TanelVari <tanel.vari@gmail.com>
2020-08-28 06:37:54 -0700

    Update ZoomOutIcon.tsx

--- packages/fluentui/react-icons-northstar/src/components/ZoomOutIcon.tsx
+++ packages/fluentui/react-icons-northstar/src/components/ZoomOutIcon.tsx
@@ -1,10 +1,22 @@
+// TODO: This is a Fluent icon. It has the same size, only artboard is 16x16 instead of original 20x20. This icon needs to be updated when moving to full set Fluent icons.
 import * as React from 'react';
+import cx from 'classnames';
 import { createSvgIcon } from '../utils/createSvgIcon';
+import { iconClassNames } from '../utils/iconClassNames';
 
 export const ZoomOutIcon = createSvgIcon({
   svg: ({ classes }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" className={classes.svg}>
-      <path d="M2048 1978q0 28-17 49t-47 21q-26 0-45-19l-785-784q-96 79-210 121t-240 42q-97 0-187-25t-168-71-142-110-111-143-71-168T0 704q0-97 25-187t71-168 110-142T349 96t168-71T704 0q97 0 187 25t168 71 142 110 111 143 71 168 25 187q0 125-42 239t-121 211l57 57q42 42 102 100t130 127 142 141 139 140 119 123 83 91 31 45zM704 1280q119 0 224-45t183-124 123-183 46-224q0-119-45-224t-124-183-183-123-224-46q-119 0-224 45T297 297 174 480t-46 224q0 119 45 224t124 183 183 123 224 46zM384 640h640v128H384V640z" />
+    <svg role="presentation" focusable="false" viewBox="0 0 16 16" className={classes.svg}>
+      <g>
+        <path
+          className={cx(iconClassNames.outline, classes.outlinePart)}
+          d="M6.5,1a5.5,5.5,0,0,1,4.381,8.825l-.154.2,4.127,4.126a.5.5,0,0,1-.562.809l-.076-.044-.07-.057L10.02,10.727A5.5,5.5,0,1,1,6.5,1Zm0,1A4.5,4.5,0,1,0,11,6.5,4.5,4.5,0,0,0,6.5,2ZM9,6A.5.5,0,0,1,9,7H4A.5.5,0,0,1,4,6Z"
+        />
+        <path
+          className={cx(iconClassNames.filled, classes.filledPart)}
+          d="M6.5,1a5.5,5.5,0,0,1,4.383,8.823h0L15.03,13.97a.75.75,0,0,1-.976,1.133h0l-.084-.073L9.823,10.883A5.5,5.5,0,1,1,6.5,1ZM9,5.75H4a.75.75,0,0,0,0,1.5H9a.75.75,0,0,0,0-1.5Z"
+        />
+      </g>
     </svg>
   ),
   displayName: 'ZoomOutIcon',
