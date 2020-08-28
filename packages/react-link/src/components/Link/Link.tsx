7fc03a6655f5488ac33ab32c38109d041c842f6c
Xu Gao <xugao0131@hotmail.com>
2020-08-28 16:57:59 -0700

    react-link: remove usage of compose and scss, move latest code to next folder (#14791)
    * remove compose and scss from Link
    
    * fix next
    
    * update screener tests
    
    * Change files
    
    * update snapshots
    
    * remove useLink export

--- packages/react-link/src/components/Link/Link.tsx
+++ packages/react-link/src/components/Link/Link.tsx
@@ -1,37 +1,12 @@
 import * as React from 'react';
-import { getGlobalClassNames, ITheme } from '@uifabric/styling';
-import { css, memoizeFunction, styled } from '@uifabric/utilities';
-import { LinkBase } from './LinkBase';
+import { styled } from '@uifabric/utilities';
+import { LinkBase } from './Link.base';
 import { ILinkProps, ILinkStyleProps, ILinkStyles } from './Link.types';
-import * as classes from './Link.scss';
-
-const GlobalClassNames = {
-  root: 'ms-Link',
-};
-
-const getStaticStylesMemoized = memoizeFunction(
-  (theme: ITheme, className?: string, isButton?: boolean, isDisabled?: boolean) => {
-    const globalClassNames = getGlobalClassNames(GlobalClassNames, theme);
-
-    const propControlledClasses = [isButton && classes.button, isDisabled && classes.disabled];
-
-    const rootStaticClasses = [isDisabled && 'is-disabled'];
-
-    return {
-      root: css(className, classes.root, globalClassNames.root, ...rootStaticClasses, ...propControlledClasses),
-    };
-  },
-);
-
-const getStaticStyles = (props: ILinkStyleProps): Required<ILinkStyles> => {
-  const { className, isButton, isDisabled, theme } = props;
-
-  return getStaticStylesMemoized(theme!, className, isButton, isDisabled);
-};
+import { getStyles } from './Link.styles';
 
 export const Link: React.FunctionComponent<ILinkProps> = styled<ILinkProps, ILinkStyleProps, ILinkStyles>(
   LinkBase,
-  getStaticStyles,
+  getStyles,
   undefined,
   {
     scope: 'Link',
