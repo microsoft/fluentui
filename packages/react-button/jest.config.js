d6b65c2237b85240b99085daf55add49e292aa69
dzearing <dzearing@hotmail.com>
2020-09-01 16:17:42 -0700

    Add merge-styles serializer support to jest snapshots.

--- packages/react-button/jest.config.js
+++ packages/react-button/jest.config.js
@@ -1,8 +1,10 @@
-const { createConfig } = require('@uifabric/build/jest/jest-resources');
-const path = require('path');
+let { createConfig, resolveMergeStylesSerializer } = require('@uifabric/build/jest/jest-resources');
+let path = require('path');
 
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
+
+  snapshotSerializers: [resolveMergeStylesSerializer()],
 });
 
 module.exports = config;
