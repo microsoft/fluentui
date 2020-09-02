8fc9fb4974a6fb96cfca7b39a2b3d234054f70e2
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-02 16:26:42 +0200

    feat(TextArea): add example

---
+++ packages/fluentui/docs/src/examples/components/TextArea/Variations/TextAreaExampleAutoHeight.shorthand.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { TextArea } from '@fluentui/react-northstar';
+
+const TextAreaExampleAutoHeight = () => <TextArea autoAdjustHeight placeholder="Type here..." />;
+
+export default TextAreaExampleAutoHeight;
