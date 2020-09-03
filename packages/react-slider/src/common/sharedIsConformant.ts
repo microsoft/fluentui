3ec6e2fa2d096b921a53ebdf3965c728945f301d
Caleb Zearing <czearing@outlook.com>
2020-09-02 17:03:30 -0700

    Renaming slider sharedIsConformant file to isConformant.

--- packages/react-slider/src/common/sharedIsConformant.ts
+++
@@ -1,9 +0,0 @@
-import { isConformant, IsConformantOptions } from '@fluentui/react-conformance';
-
-export function sharedIsConformant(testInfo: IsConformantOptions) {
-  const defaultOptions = {
-    disabledTests: ['has-docblock', 'kebab-aria-attributes'],
-  };
-
-  isConformant(defaultOptions, testInfo);
-}
