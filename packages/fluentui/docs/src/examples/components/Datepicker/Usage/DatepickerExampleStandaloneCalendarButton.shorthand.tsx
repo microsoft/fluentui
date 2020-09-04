1e1f8fa9b435ad4e4e98fc0210667ebeffb485dd
Karel Rymes <karymes@microsoft.com>
2020-09-04 09:02:44 +0200

    Fixing dependency problem

--- packages/fluentui/docs/src/examples/components/Datepicker/Usage/DatepickerExampleStandaloneCalendarButton.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Datepicker/Usage/DatepickerExampleStandaloneCalendarButton.shorthand.tsx
@@ -1,12 +1,11 @@
 import { Datepicker, DatepickerType, Button } from '@fluentui/react-northstar';
-import { formatMonthDayYear, DEFAULT_CALENDAR_STRINGS } from '@fluentui/date-time-utilities';
 import * as React from 'react';
 
 const DatepickerExampleStandaloneCalendarButton = () => {
   return (
     <Datepicker
       onDateChange={(e, v) => {
-        alert(`You picked '${formatMonthDayYear(v.value, DEFAULT_CALENDAR_STRINGS)}'.`);
+        alert(`You picked '${v.value}'.`);
       }}
       today={new Date(2020, 7, 30, 0, 0, 0, 0)}
       type={DatepickerType.ButtonOnly}
