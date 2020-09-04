352d6fb07366886d28cc2f1e0db7319cf55f14c5
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-04 08:34:41 +0200

    feat(TextArea): add comment

--- packages/fluentui/docs/src/prototypes/TextAreaAutoSize/TextAreaAutoResize.tsx
+++ packages/fluentui/docs/src/prototypes/TextAreaAutoSize/TextAreaAutoResize.tsx
@@ -3,11 +3,9 @@ import { TextArea } from '@fluentui/react-northstar';
 import TextareaAutosize from 'react-textarea-autosize';
 
 const TextAreaAutoResize = props => {
-  return (
-    <div>
-      <TextArea as={TextareaAutosize} />
-    </div>
-  );
+  // We are using 3rd part lib here since they have great support for several edge cases
+  // to know it better please, check https://github.com/Andarist/react-textarea-autosize
+  return <TextArea as={TextareaAutosize} />;
 };
 
 export default TextAreaAutoResize;
