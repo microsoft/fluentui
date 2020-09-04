b3826eab7cecbc987fdacc6b853d1c5d62b92646
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-04 08:38:26 +0200

    feat(TextArea): add comment

--- packages/fluentui/docs/src/prototypes/TextAreaAutoSize/index.tsx
+++ packages/fluentui/docs/src/prototypes/TextAreaAutoSize/index.tsx
@@ -4,7 +4,19 @@ import TextAreaAutoResize from './TextAreaAutoResize';
 
 export default () => (
   <PrototypeSection title="Text Area">
-    <ComponentPrototype title="TextArea Autosize" description="Using text area with auto size">
+    <ComponentPrototype
+      title="TextArea Autosize"
+      description={
+        <div>
+          We are using{' '}
+          <code>
+            <a href="https://github.com/Andarist/react-textarea-autosize">react-textarea-autosize</a>
+          </code>{' '}
+          to allow auto size, they handle several edge cases with great support. Please check{' '}
+          <a href="https://github.com/Andarist/react-textarea-autosize">their docs</a> for more info
+        </div>
+      }
+    >
       <TextAreaAutoResize />
     </ComponentPrototype>
   </PrototypeSection>
