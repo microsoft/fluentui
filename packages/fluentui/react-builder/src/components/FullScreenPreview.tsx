02b35612a5c328e9e6219b693db6a85125f8e0e3
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-09-04 14:48:00 +0200

    Added theme overrides to Popout

--- packages/fluentui/react-builder/src/components/FullScreenPreview.tsx
+++ packages/fluentui/react-builder/src/components/FullScreenPreview.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { Provider, teamsTheme } from '@fluentui/react-northstar';
-import { readTreeFromStore, readTreeFromURL } from '../utils/treeStore';
+import { readTreeFromStore, readTreeFromURL, readThemeFromStore, readThemeFromURL } from '../utils/treeStore';
 import { renderJSONTreeToJSXElement } from '../config';
 
 export const FullScreenPreview: React.FunctionComponent = () => {
   const jsonTree = readTreeFromURL(window.location.href) || readTreeFromStore();
+  const themeOverrides = readThemeFromURL(window.location.href) || readThemeFromStore() || {};
   return (
     <Provider theme={teamsTheme} target={document}>
-      {renderJSONTreeToJSXElement(jsonTree)}
+      <Provider theme={themeOverrides}>{renderJSONTreeToJSXElement(jsonTree)}</Provider>
     </Provider>
   );
 };
