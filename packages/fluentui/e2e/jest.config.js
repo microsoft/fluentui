be35ea933668c30ec6a1797772105284375611ae
Oleksandr Fediashov <olfedias@microsoft.com>
2020-09-07 14:22:58 +0200

    get CI env

--- packages/fluentui/e2e/jest.config.js
+++ packages/fluentui/e2e/jest.config.js
@@ -1,7 +1,10 @@
 const commonConfig = require('@uifabric/build/jest');
-
+console.log(process.env);
+throw new Error(11);
 module.exports = {
   ...commonConfig,
+  globalSetup: 'jest-environment-puppeteer/setup',
+  globalTeardown: 'jest-environment-puppeteer/teardown',
   moduleNameMapper: require('lerna-alias').jest({
     directory: require('@uifabric/build/monorepo/findGitRoot')(),
   }),
