cafcebefdff93c78b263bf709267cddd6740b382
Amber <yuanboxue@outlook.com>
2020-09-08 12:15:09 +0200

    fix: `<div></div>` to `<></>`
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/docs/src/examples/components/Breadcrumb/index.tsx
+++ packages/fluentui/docs/src/examples/components/Breadcrumb/index.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import Usage from './Usage';
 
 const BreadcrumbExamples = () => (
-  <div>
+  <>
     <Usage />
-  </div>
+  </>
 );
 
 export default BreadcrumbExamples;
