f14feed288458a9d314191f460e9105a6c290436
Oleksandr Fediashov <olfedias@microsoft.com>
2020-09-08 18:11:55 +0200

    fix slowMo val

--- packages/fluentui/e2e/setup.test.ts
+++ packages/fluentui/e2e/setup.test.ts
@@ -12,7 +12,7 @@ let consoleErrors: string[] = [];
 const launchOptions: puppeteer.LaunchOptions = safeLaunchOptions({
   headless: true,
   dumpio: false,
-  slowMo: 10,
+  slowMo: 0,
 });
 
 beforeAll(async () => {
