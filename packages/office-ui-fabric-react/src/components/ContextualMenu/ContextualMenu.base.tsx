f63fb2854ed0adc87e01817d3ab4e20cb3c13c25
Jodom Konuko <jokonuko@microsoft.com>
2020-09-08 13:37:02 -0700

    remove document focus check

--- packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
+++ packages/office-ui-fabric-react/src/components/ContextualMenu/ContextualMenu.base.tsx
@@ -453,7 +453,7 @@ export class ContextualMenuBase extends React.Component<IContextualMenuProps, IC
     if (this.props.onRestoreFocus) {
       this.props.onRestoreFocus(options);
     } else {
-      if (options && options.containsFocus && options.documentContainsFocus && this._previousActiveElement) {
+      if (options && options.containsFocus && this._previousActiveElement) {
         // Make sure that the focus method actually exists
         // In some cases the object might exist but not be a real element.
         // This is primarily for IE 11 and should be removed once IE 11 is no longer in use.
