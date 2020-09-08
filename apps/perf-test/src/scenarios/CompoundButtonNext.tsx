17c6c91fc25eebd24b852191c03fb390168f7503
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-09-08 14:24:21 -0700

    Adding perf test.

---
+++ apps/perf-test/src/scenarios/CompoundButtonNext.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { CompoundButton } from '@fluentui/react-button';
+
+const Scenario = () => (
+  <CompoundButton icon="O" secondaryContent="This is some secondary text">
+    Hello, world
+  </CompoundButton>
+);
+
+export default Scenario;
