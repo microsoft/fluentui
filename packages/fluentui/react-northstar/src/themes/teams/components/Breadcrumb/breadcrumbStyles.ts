df456dfb704075f39b7dc073cbd2920d6008ebae
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-10 14:47:21 +0200

    fix(BreadcrumbDivider): use children

---
+++ packages/fluentui/react-northstar/src/themes/teams/components/Breadcrumb/breadcrumbStyles.ts
@@ -0,0 +1,8 @@
+import { BreadcrumbStylesProps } from '../../../../components/Breadcrumb/Breadcrumb';
+import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
+
+export const breadcrumbStyles: ComponentSlotStylesPrepared<BreadcrumbStylesProps, {}> = {
+  root: (): ICSSInJSStyle => {
+    return {};
+  },
+};
