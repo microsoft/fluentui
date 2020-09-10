7bebb3b2e78e3372caba519a47d3cd1ac910f898
Elisabeth von Spakovsky <elvonspa@microsoft.com>
2020-09-10 13:56:00 -0700

    allow onRenderSuggestion to be used

--- packages/experiments/src/components/FloatingSuggestionsComposite/FloatingPeopleSuggestions/FloatingSuggestionsPage.tsx
+++ packages/experiments/src/components/FloatingSuggestionsComposite/FloatingPeopleSuggestions/FloatingSuggestionsPage.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { ExampleCard, IComponentDemoPageProps, ComponentPage, PropertiesTableSet } from '@uifabric/example-app-base';
 import { FloatingPeopleSuggestionsExample } from './examples/FloatingPeopleSuggestions.Example';
-
 const FloatingPeoplePickerSuggestionsExampleCode = require('!raw-loader!./examples/FloatingPeopleSuggestions.Example') as string;
+import { FloatingPeopleSuggestionsCustomRenderExample } from './examples/FloatingPeopleSuggestions.CustomRender.Example';
+const FloatingPeoplePickerSuggestionsCustomRenderExampleCode = require('!raw-loader!./examples/FloatingPeopleSuggestions.CustomRender.Example') as string;
 
 export class FloatingSuggestionPage extends React.Component<IComponentDemoPageProps, {}> {
   public render(): JSX.Element {
