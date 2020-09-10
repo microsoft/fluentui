a2d668ee738873b89da49913a5e5a50bcdc137db
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-09-10 16:38:29 +0200

    Changed package vesion

--- packages/fluentui/react-builder/src/utils/StyleProvider.tsx
+++ packages/fluentui/react-builder/src/utils/StyleProvider.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Stylesheet } from '@uifabric/merge-styles';
+import { Stylesheet } from '@uifabric/merge-styles/lib/Stylesheet';
 import { JSONTreeElement } from '../components/types';
 
 /**
