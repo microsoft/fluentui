5bc299f486b399954d5b3c622258bcbf21789bc1
Charles Assunção <junioassuncaocharles@gmail.com>
2020-09-10 15:11:54 +0200

    Delete breadcrumbStyles.ts

--- packages/fluentui/react-northstar/src/themes/teams/components/Breadcrumb/breadcrumbStyles.ts
+++
@@ -1,8 +0,0 @@
-import { BreadcrumbStylesProps } from '../../../../components/Breadcrumb/Breadcrumb';
-import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
-
-export const breadcrumbStyles: ComponentSlotStylesPrepared<BreadcrumbStylesProps, {}> = {
-  root: (): ICSSInJSStyle => {
-    return {};
-  },
-};
