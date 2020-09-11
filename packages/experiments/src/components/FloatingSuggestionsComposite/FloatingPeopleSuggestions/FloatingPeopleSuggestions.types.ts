708946f8a09e5a24278f130f061cdb804295215b
elisabethcvs <64664025+elisabethcvs@users.noreply.github.com>
2020-09-11 10:14:10 -0700

    allow consumers of FloatingPeopleSuggestions to use the onRenderSuggestion property (#14983)
    * allow onRenderSuggestion to be used
    
    * Change files
    
    * fix example
    
    * _onInputChange should use the state variable

--- packages/experiments/src/components/FloatingSuggestionsComposite/FloatingPeopleSuggestions/FloatingPeopleSuggestions.types.ts
+++ packages/experiments/src/components/FloatingSuggestionsComposite/FloatingPeopleSuggestions/FloatingPeopleSuggestions.types.ts
@@ -1,5 +1,4 @@
 import { IBaseFloatingSuggestionsProps } from '../FloatingSuggestions.types';
-import { Omit } from '@uifabric/utilities';
 import { IPersonaProps } from 'office-ui-fabric-react/lib/Persona';
 
-export type IFloatingPeopleSuggestionsProps = Omit<IBaseFloatingSuggestionsProps<IPersonaProps>, 'onRenderSuggestion'>;
+export type IFloatingPeopleSuggestionsProps = IBaseFloatingSuggestionsProps<IPersonaProps>;
