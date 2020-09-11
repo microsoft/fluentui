9786be651793a86a6ab087c537601252ce4baefb
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-11 11:10:07 +0200

    fix(MenuItem): revert wrapper call

--- packages/fluentui/react-northstar/src/components/Menu/MenuItem.tsx
+++ packages/fluentui/react-northstar/src/components/Menu/MenuItem.tsx
@@ -482,6 +482,7 @@ export const MenuItem = compose<'a', MenuItemProps, MenuItemStylesProps, {}, {}>
       },
       onClick: (e: React.MouseEvent) => {
         handleClick(e);
+        _.invoke(predefinedProps, 'onClick', e, props);
       },
       ...(on === 'hover' && {
         onMouseEnter: e => {
