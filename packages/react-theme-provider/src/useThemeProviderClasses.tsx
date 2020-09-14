be452f1163c32cf6e59ff316554aec3621284ec4
dzearing <dzearing@hotmail.com>
2020-09-14 13:33:23 -0700

    more updates

--- packages/react-theme-provider/src/useThemeProviderClasses.tsx
+++ packages/react-theme-provider/src/useThemeProviderClasses.tsx
@@ -9,13 +9,11 @@ export const useThemeProviderClasses = makeClasses(theme => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       tokensToStyleObject(tokens) as any,
       {
-        background: 'var(--body-background)',
         color: 'var(--body-contentColor)',
         fontFamily: 'var(--body-fontFamily)',
         fontWeight: 'var(--body-fontWeight)',
         fontSize: 'var(--body-fontSize)',
         lineHeight: 'var(--body-fontLineHeight)',
-        boxSizing: 'border-box',
       },
     ],
   };
