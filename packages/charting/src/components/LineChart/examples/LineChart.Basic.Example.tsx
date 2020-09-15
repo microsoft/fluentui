fa2ccc2959601e21314bb920fa478bab07b24bef
Mercedes Miller <memille@microsoft.com>
2020-09-15 15:29:47 -0700

    Updated examples

--- packages/charting/src/components/LineChart/examples/LineChart.Basic.Example.tsx
+++ packages/charting/src/components/LineChart/examples/LineChart.Basic.Example.tsx
@@ -138,34 +138,6 @@ export class LineChartBasicExample extends React.Component<{}, ILineChartBasicSt
             height={this.state.height}
             width={this.state.width}
             margins={margins}
-            legendProps={{ canSelectMultipleLegends: true, allowFocusOnLegends: true }}
-            colorFillBars={[
-              {
-                legend: 'Time range 1',
-                color: 'blue',
-                data: [
-                  {
-                    startX: new Date('2020-03-03T10:00:00.000Z'),
-                    endX: new Date('2020-03-05T00:00:00.000Z'),
-                  },
-                ],
-              },
-              {
-                legend: 'Time range 2',
-                color: 'red',
-                data: [
-                  {
-                    startX: new Date('2020-03-07T00:00:00.000Z'),
-                    endX: new Date('2020-03-08T00:00:00.000Z'),
-                  },
-                  {
-                    startX: new Date('2020-03-04T10:00:00.000Z'),
-                    endX: new Date('2020-03-06T00:00:00.000Z'),
-                  },
-                ],
-                applyPattern: true,
-              },
-            ]}
           />
         </div>
       </>
