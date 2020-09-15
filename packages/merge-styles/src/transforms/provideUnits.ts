f8312493cc5fc19ee559bf998c609ced1643355e
dzearing <dzearing@hotmail.com>
2020-09-14 18:07:00 -0700

    oops

--- packages/merge-styles/src/transforms/provideUnits.ts
+++ packages/merge-styles/src/transforms/provideUnits.ts
@@ -16,8 +16,8 @@ export function provideUnits(rulePairs: (string | number)[], index: number): voi
   const value = rulePairs[index + 1];
 
   if (typeof value === 'number') {
-    const isNonPixelProp = NON_PIXEL_NUMBER_PROPS.indexOf(name as string) === -1;
-    const isVariableOrPrefixed = name.indexOf('--') === -1;
+    const isNonPixelProp = NON_PIXEL_NUMBER_PROPS.indexOf(name as string) > -1;
+    const isVariableOrPrefixed = name.indexOf('--') > -1;
     const unit = isNonPixelProp || isVariableOrPrefixed ? '' : 'px';
 
     rulePairs[index + 1] = `${value}${unit}`;
