dd36477fa9642925f9fabbcc5fc749ba921050fd
Xu Gao <xgao@microsoft.com>
2020-09-15 14:54:43 -0700

    remove un-used prop

--- packages/react-theme-provider/src/ThemeProvider.types.ts
+++ packages/react-theme-provider/src/ThemeProvider.types.ts
@@ -17,12 +17,6 @@ export interface ThemeProviderProps extends ComponentProps, React.HTMLAttributes
   theme?: PartialTheme | Theme;
 
   /**
-   * Defines the target window to render into. Defaults to the global window. Providing `null`
-   * will opt out of style rendering, which is used for SSR.
-   */
-  targetWindow?: Window | null;
-
-  /**
    * Optional interface for registering dynamic styles. Defaults to using `merge-styles`. Use this
    * to opt into a particular rendering implementation, such as `emotion`, `styled-components`, or `jss`.
    * Note: performance will differ between all renders. Please measure your scenarios before using an alternative
