006119f081a05d4d4dcb65014aef043874992f87
Oleksandr Fediashov <olfedias@microsoft.com>
2020-09-16 14:33:54 +0200

    chore: refactor Webpack contexts in Fluent docs to improve treeshaking (#14843)
    * chore: refactor Webpack contexts in Fluent docs to improve treeshaking
    
    * one more chunk
    
    * fix HMR for examples
    
    * fix lint issue

--- packages/fluentui/docs/src/components/ExternalExampleLayout.tsx
+++ packages/fluentui/docs/src/components/ExternalExampleLayout.tsx
@@ -4,13 +4,11 @@ import * as React from 'react';
 import { match } from 'react-router-dom';
 import { KnobProvider } from '@fluentui/docs-components';
 
-import { ExampleSource } from '../types';
-import { exampleSourcesContext, exampleKebabNameToSourceFilename, parseExamplePath } from '../utils';
+import { examplesContext } from '../contexts/examplesContext';
 import PageNotFound from '../views/PageNotFound';
-import { SourceRender } from './ComponentDoc/SourceRender';
-import { babelConfig, importResolver } from './Playground/renderConfig';
+import { exampleKebabNameToFilename, parseExamplePath } from '../utils';
 
-const examplePaths = exampleSourcesContext.keys();
+const examplePaths = examplesContext.keys();
 
 type ExternalExampleLayoutProps = {
   match: match<{
@@ -28,7 +26,6 @@ const themes = {
 const ExternalExampleLayout: React.FC<ExternalExampleLayoutProps> = props => {
   const { exampleName, rtl } = props.match.params;
 
-  const [error, setError] = React.useState<Error | null>(null);
   const [renderId, setRenderId] = React.useState<number>(0);
   const [themeName, setThemeName] = React.useState<string>();
 
@@ -37,26 +34,18 @@ const ExternalExampleLayout: React.FC<ExternalExampleLayoutProps> = props => {
     window.switchTheme = setThemeName;
   }, []);
 
-  const exampleFilename = exampleKebabNameToSourceFilename(exampleName);
+  const exampleFilename = exampleKebabNameToFilename(exampleName);
   const examplePath = _.find(examplePaths, path => exampleFilename === parseExamplePath(path).exampleName);
 
   if (!examplePath) return <PageNotFound />;
 
-  const exampleSource: ExampleSource = exampleSourcesContext(examplePath);
+  const Example: React.ElementType = examplesContext(examplePath).default;
   const theme = (themeName && themes[themeName]) || {};
 
   return (
     <Provider key={renderId} theme={theme} rtl={rtl === 'true'}>
       <KnobProvider>
-        <SourceRender
-          babelConfig={babelConfig}
-          onRender={setError}
-          source={exampleSource.js}
-          resolver={importResolver}
-          hot
-        />
-        {/* This block allows to see issues with examples as visual regressions. */}
-        {error && <div style={{ fontSize: '5rem', color: 'red' }}>{error.toString()}</div>}
+        <Example />
       </KnobProvider>
     </Provider>
   );
