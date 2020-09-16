e7c1f17465f4bde37b807349925f68a6121a188f
Karel Rymes <karymes@microsoft.com>
2020-09-16 16:43:14 +0200

    Skipping accessibility test

--- packages/fluentui/accessibility/src/behaviors/Datepicker/datepickerCalendarGridRowBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Datepicker/datepickerCalendarGridRowBehavior.ts
@@ -3,6 +3,7 @@ import { Accessibility } from '../../types';
  * @description
  * Behavior for a datepicker calendar grid row.
  * @specification
+ *
  */
 export const datepickerCalendarGridRowBehavior: Accessibility<DatepickerCalendarGridRowBehaviorProps> = props => ({});
 
