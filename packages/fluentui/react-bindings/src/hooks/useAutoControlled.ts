2ffa04574183cdc89ba75b0e88ebf52f9524816c
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-09-20 06:19:14 +0200

    fix(useAutoControlled): allow functions update passed to setSate

--- packages/fluentui/react-bindings/src/hooks/useAutoControlled.ts
+++ packages/fluentui/react-bindings/src/hooks/useAutoControlled.ts
@@ -17,7 +17,17 @@ export const useAutoControlled = <Value>(
   options: UseAutoControlledOptions<Value>,
 ): [Value, React.Dispatch<React.SetStateAction<Value>>] => {
   const { defaultValue, initialValue = undefined, value } = options;
-  const [state, setState] = React.useState<Value>(isUndefined(defaultValue) ? (initialValue as Value) : defaultValue);
+  const [_state, _setState] = React.useState<Value>(isUndefined(defaultValue) ? (initialValue as Value) : defaultValue);
 
-  return [isUndefined(value) ? state : value, setState];
+  const state = isUndefined(value) ? _state : value;
+
+  const setState = (newState: Value) => {
+    if (typeof newState === 'function') {
+      _setState(newState(state));
+    } else {
+      _setState(newState);
+    }
+  };
+
+  return [state, setState];
 };
