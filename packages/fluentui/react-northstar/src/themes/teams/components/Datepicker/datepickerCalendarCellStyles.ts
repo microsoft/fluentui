61f9217d9b48811124bbbd0d075d4a992186acaa
Karel Rymes <karymes@microsoft.com>
2020-09-21 09:14:47 +0200

    Updating styles

--- packages/fluentui/react-northstar/src/themes/teams/components/Datepicker/datepickerCalendarCellStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Datepicker/datepickerCalendarCellStyles.ts
@@ -3,37 +3,41 @@ import { DatepickerVariables } from './datepickerVariables';
 import { DatepickerCalendarCellStylesProps } from '../../../../components/Datepicker/DatepickerCalendarCell';
 import { getBorderFocusStyles } from '../../getBorderFocusStyles';
 
+const cellStyles = (p: DatepickerCalendarCellStylesProps, v: DatepickerVariables) => ({
+  ...(p.quiet && {
+    color: v.calendarCellQuietColor,
+  }),
+
+  ...(p.selected && {
+    color: v.calendarCellSelectedColor,
+    backgroundColor: v.calendarCellSelectedBackgroundColor,
+  }),
+
+  ...(!p.disabled && {
+    ':hover': {
+      backgroundColor: v.calendarCellHoverBackgroundColor,
+      color: v.calendarCellHoverColor,
+    },
+  }),
+
+  ...(p.disabled && {
+    color: v.calendarCellDisabledColor,
+    cursor: 'default',
+    backgroundColor: v.calendarCellDisabledBackgroundColor,
+  }),
+});
+
 export const datepickerCalendarCellStyles: ComponentSlotStylesPrepared<
   DatepickerCalendarCellStylesProps,
   DatepickerVariables
 > = {
-  tableCell: ({ props: p, variables: v, theme }): ICSSInJSStyle => {
+  tableCell: ({ props: p, variables: v }): ICSSInJSStyle => {
     return {
       height: v.calendarCellHeight,
       width: v.calendarCellWidth,
       padding: v.calendarCellPadding,
 
-      ...(p.quiet && {
-        color: v.calendarCellQuietColor,
-      }),
-
-      ...(p.selected && {
-        color: v.calendarCellSelectedColor,
-        backgroundColor: v.calendarCellSelectedBackgroundColor,
-      }),
-
-      ...(!p.disabled && {
-        ':hover': {
-          backgroundColor: v.calendarCellHoverBackgroundColor,
-          color: v.calendarCellHoverColor,
-        },
-      }),
-
-      ...(p.disabled && {
-        color: v.calendarCellDisabledColor,
-        cursor: 'default',
-        backgroundColor: v.calendarCellDisabledBackgroundColor,
-      }),
+      ...cellStyles(p, v),
     };
   },
 
@@ -68,34 +72,14 @@ export const datepickerCalendarCellStyles: ComponentSlotStylesPrepared<
       backgroundColor: v.calendarCellBackgroundColor,
       color: v.calendarCellColor,
 
+      ...cellStyles(p, v),
+
       // Today's date only stays here because today's date is rendered circular.
       // The other properties would be rendered rectangular around it.
       ...(p.today && {
         backgroundColor: v.calendarCellTodayBackgroundColor,
         color: v.calendarCellTodayColor,
         borderRadius: v.calendarCellTodayBorderRadius,
-      }),
-
-      ...(p.quiet && {
-        color: v.calendarCellQuietColor,
-      }),
-
-      ...(p.selected && {
-        color: v.calendarCellSelectedColor,
-        backgroundColor: v.calendarCellSelectedBackgroundColor,
-      }),
-
-      ...(!p.disabled && {
-        ':hover': {
-          backgroundColor: v.calendarCellHoverBackgroundColor,
-          color: v.calendarCellHoverColor,
-        },
-      }),
-
-      ...(p.disabled && {
-        color: v.calendarCellDisabledColor,
-        cursor: 'default',
-        backgroundColor: v.calendarCellDisabledBackgroundColor,
       }),
     };
   },
