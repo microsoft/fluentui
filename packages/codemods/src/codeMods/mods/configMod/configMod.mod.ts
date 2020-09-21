c7a797e74572c5eba16e0d33969103d5a66d0fc8
Jon S <joschect@microsoft.com>
2020-09-21 13:41:32 -0700

    fix the config mod to correctly run

--- packages/codemods/src/codeMods/mods/configMod/configMod.mod.ts
+++ packages/codemods/src/codeMods/mods/configMod/configMod.mod.ts
@@ -11,13 +11,16 @@ import {
 } from '../../types';
 import { findJsxTag, renameProp, getImportsByPath, repathImport, renameImport } from '../../utilities/index';
 import { Ok, Err, Result } from '../../../helpers/result';
+import { MaybeDictionary } from '../../../helpers/maybe';
 
 import jsonObj from '../upgrades.json';
 
 /* Creates and returns an array of CodeMod objects from a JSON file. Optionally takes in
    an array of functions from user to turn into codemods as well. */
 export function createCodeModsFromJson(userMods?: CodeMod[]): CodeMod[] | undefined {
-  const funcs = getCodeModsFromJson();
+  const funcs = getCodeModsFromJson()
+    .filter(Result.isOk)
+    .map(v => v.value);
   /* If the user wants to supply any codemods (as a void function that takes in a sourcefile),
      add those offerings right now! */
   if (userMods) {
@@ -29,24 +32,26 @@ export function createCodeModsFromJson(userMods?: CodeMod[]): CodeMod[] | undefi
 /* Helper function that parses a json object for details about individual
    codemods and formats each into a function. These functions are stored in
    an array that is returned to the user. */
-export function getCodeModsFromJson(): CodeMod[] {
-  const mods = [];
-  const modDetails = jsonObj.upgrades;
-  for (let i = 0; i < modDetails.length; i++) {
-    /* Try and get the codemod function associated with the mod type. */
-    const func = codeModMap[modDetails[i].type](modDetails[i]);
-    if (func) {
-      const options: ModOptions = {
-        name: modDetails[i].name,
-        version: modDetails[i].version ? modDetails[i].version! : '100000',
-      };
-      mods.push(createCodeMod(options, func));
-    } else {
-      // eslint-disable-next-line no-throw-literal
-      throw 'Error: attempted to access a codeMod mapping from an unsupported type.';
-    }
-  }
-  return mods;
+export function getCodeModsFromJson(): Result<CodeMod, Error>[] {
+  return jsonObj.upgrades.map(modDetail =>
+    codeModMap[modDetail.type]
+      .then(v => v(modDetail))
+      .then<Result<CodeMod, Error>>(v => {
+        const options: ModOptions = {
+          name: modDetail.name,
+          version: modDetail.version ? modDetail.version! : '100000',
+        };
+        return Ok<CodeMod, Error>(createCodeMod(options, v));
+      })
+      .orElse(
+        Err<CodeMod, Error>(
+          new Error(
+            `ModConfiguration error: ${modDetail.name} is an invalid mod mapping.
+            Check to make sure there is a valid mapping in the upgrades.json file `,
+          ),
+        ),
+      ),
+  );
 }
 
 /* Helper function that creates a codeMod given a name and a list of functions that compose the mod. */
@@ -69,9 +74,9 @@ export function createCodeMod(options: ModOptions, mod: (file: SourceFile) => Re
 /* Dictionary that maps codemod names to functions that execute said mod.
    Used by getCodeModUtilitiesFromJson to easily get the desired function
    from the json object. */
-const codeModMap: CodeModMapType = {
-  renameProp: function(mod: RenamePropModType) {
-    return function(file: SourceFile) {
+const _codeModMap: CodeModMapType = {
+  renameProp: (mod: RenamePropModType) => {
+    return (file: SourceFile) => {
       const tags = findJsxTag(file, mod.options.from.importName);
       const res = renameProp(tags, mod.options.from.toRename, mod.options.to.replacementName);
       if (res.ok) {
@@ -83,8 +88,8 @@ const codeModMap: CodeModMapType = {
       }
     };
   },
-  repathImport: function(mod: RepathImportModType) {
-    return function(file: SourceFile) {
+  repathImport: (mod: RepathImportModType) => {
+    return (file: SourceFile) => {
       /* If the json indicates our search string is a regex, convert it. */
       const searchString = mod.options.from.isRegex
         ? new RegExp(
@@ -103,23 +108,30 @@ const codeModMap: CodeModMapType = {
       }
     };
   },
-  renameImport: function(mod: RenameImportType) {
-    return function(file: SourceFile) {
+  renameImport: (mod: RenameImportType) => {
+    return (file: SourceFile) => {
       return renameImport(file, mod.options.from.originalImport, mod.options.to.renamedImport);
     };
   },
 };
 
+const codeModMap = MaybeDictionary(_codeModMap);
+
+let __configs: CodeMod[] | undefined = undefined;
 /* ConfigMod is also an exportable code mod. All it does is wrap all of
    the codemods from the json file into a single code mod, so that devs
    can very easily run mods from json with a (npx fluent... -n "configMod"). */
 const configMod: CodeMod = {
   run: (file: SourceFile) => {
-    const mods = getCodeModsFromJson();
-    if (mods === undefined || mods.length === 0) {
-      return Err({ reason: `failed to get any mods from json. Perhaps the file is missing or malformed?` });
+    if (!__configs) {
+      __configs = getCodeModsFromJson()
+        .filter(Result.isOk)
+        .map(v => v.value);
+      if (__configs === undefined || __configs.length === 0) {
+        return Err({ reason: `failed to get any mods from json. Perhaps the file is missing or malformed?` });
+      }
     }
-    mods.forEach(mod => {
+    __configs.forEach(mod => {
       const res = mod.run(file);
       if (!res.ok) {
         return Err({ reason: `code mod ${mod.name} failed to run on ${file.getBaseName()}` });
