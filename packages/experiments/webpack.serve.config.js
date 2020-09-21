bfbb5110ee98fbaa215316b200c5b8ae9b373434
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-09-21 14:31:21 -0700

    Fixing bundling issue.

--- packages/experiments/webpack.serve.config.js
+++ packages/experiments/webpack.serve.config.js
@@ -13,16 +13,19 @@ module.exports = resources.createServeConfig({
 
   resolve: {
     alias: {
+      '@fluentui/examples$': path.join(__dirname, '../../packages/examples/src'),
+      '@fluentui/examples/lib': path.join(__dirname, '../../packages/examples/src'),
+      '@fluentui/examples/src': path.join(__dirname, '../../packages/examples/src'),
       '@uifabric/example-app-base$': path.join(__dirname, '../../packages/example-app-base/src'),
       '@uifabric/experiments/src': path.join(__dirname, 'src'),
       '@uifabric/experiments/lib': path.join(__dirname, 'src'),
       '@uifabric/experiments': path.join(__dirname, 'src'),
       '@uifabric/fluent-theme$': path.join(__dirname, '../../packages/fluent-theme/src'),
       '@uifabric/foundation$': path.join(__dirname, '../../packages/foundation/src'),
+      '@uifabric/theme-samples$': path.join(__dirname, '../../packages/theme-samples/src'),
       'office-ui-fabric-react$': path.resolve(__dirname, '../../packages/office-ui-fabric-react/src'),
       'office-ui-fabric-react/lib': path.resolve(__dirname, '../../packages/office-ui-fabric-react/src'),
       'office-ui-fabric-react/src': path.resolve(__dirname, '../../packages/office-ui-fabric-react/src'),
-      '@uifabric/theme-samples$': path.join(__dirname, '../../packages/theme-samples/src'),
       'Props.ts.js': 'Props',
       'Example.tsx.js': 'Example',
     },
