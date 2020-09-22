849e708a82f08fb7cabe852424693488e743e3b0
Elizabeth Craig <elcraig@microsoft.com>
2020-09-21 21:31:45 -0700

    fix paths

--- apps/fabric-website-resources/src/components/pages/ColorsPage.tsx
+++ apps/fabric-website-resources/src/components/pages/ColorsPage.tsx
@@ -142,7 +142,7 @@ export class ColorsPage extends React.Component<{}, IColorsPageState> {
          * We use that API to identify the most prominent background and foreground colors, and the accent color,
          * and generate a theme based off of those.
          * Since this API requires a personal subscription key, you'll have to enlist and insert your subscription
-         * key in _makeThemeFromImg() @ https://raw.githubusercontent.com/cliffkoh/office-ui-fabric-react/9c95e9b92f8caa1fe5ffb9da769ce0921a5272ed/@fluentui/examples/src/office-ui-fabric-react/ThemeGenerator/ThemeGeneratorPage.tsx
+         * key in _makeThemeFromImg() @ https://raw.githubusercontent.com/cliffkoh/office-ui-fabric-react/9c95e9b92f8caa1fe5ffb9da769ce0921a5272ed/packages/office-ui-fabric-react/src/components/ThemeGenerator/ThemeGeneratorPage.tsx
          * Then, just uncomment this section. */}
         {/*}
         <div style={ { display: 'flex' } }>
