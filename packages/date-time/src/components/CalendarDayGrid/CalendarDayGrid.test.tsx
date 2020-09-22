a89efd72e645e6e20869d6f33717f11554a8711a
Caleb Zearing <czearing@outlook.com>
2020-09-21 21:50:03 -0700

    Implementing v7 conformance tests (#14940)
    * Renaming v7 sharedIsConformant file to isConformant and updating eslint rule.
    
    * Adding conformance tests to v7 components.
    
    * Adding conformance tests to react-focus and date-time packages.
    
    * Adding conformance tests to v7 components.
    
    * Adding conformance tests to v7 components.
    
    * Adding conformanceTests to selectionZone.test.tsx
    
    * Reverting conformance tests for Slider and Shimmer.
    
    * Change files
    
    * Adding display names and requiredProps to components and conformant tests.
    
    * Quick fix to GroupedList.test.tsx
    
    * Resolving merge conflicts
    
    * Minor change to allow diffing to notice api line sequence change.
    
    * Reverting minor change
    
    * Updating isConformant version in react-focus and date-time
    
    * PR feedback
    
    * Change files
    
    * Updating FocusZone conformance tests
    
    * Change files
    
    * Updating snapshots
    
    * Adding asPropHandlesRef type to defaultTests
    
    Co-authored-by: Elizabeth Craig <elcraig@microsoft.com>

---
+++ packages/date-time/src/components/CalendarDayGrid/CalendarDayGrid.test.tsx
@@ -0,0 +1,29 @@
+import { CalendarDayGrid } from './CalendarDayGrid';
+import { defaultDayPickerStrings, DateRangeType, DayOfWeek, FirstWeekOfYear, ICalendarStrings } from '../Calendar';
+import { isConformant } from '../../common/isConformant';
+
+describe('CalendarDayGrid', () => {
+  const timeFormatter = {
+    formatMonthDayYear: (date: Date, strings?: ICalendarStrings) => 'm/d/y',
+    formatMonthYear: (date: Date, strings?: ICalendarStrings) => 'm/y',
+    formatDay: (date: Date) => 'd',
+    formatYear: (date: Date) => 'y',
+  };
+
+  console.log(new Date('2020-09-18T21:06:52.856Z'));
+  isConformant({
+    Component: CalendarDayGrid,
+    displayName: 'CalendarDayGrid',
+    requiredProps: {
+      strings: defaultDayPickerStrings,
+      selectedDate: new Date('2020-09-18T21:06:52.856Z'),
+      navigatedDate: new Date('2020-09-18T21:06:52.856Z'),
+      navigatedMonthDate: new Date('2020-09-18T21:06:52.856Z'),
+      dateRangeType: DateRangeType.Day,
+      firstDayOfWeek: DayOfWeek.Sunday,
+      firstWeekOfYear: FirstWeekOfYear.FirstFullWeek,
+      dateTimeFormatter: timeFormatter,
+    },
+    disabledTests: ['exported-top-level', 'has-top-level-file'],
+  });
+});
