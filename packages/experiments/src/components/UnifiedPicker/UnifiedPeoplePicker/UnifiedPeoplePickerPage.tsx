b26950a2faab4ee8d9bbf506d32394032cfe355d
elisabethcvs <64664025+elisabethcvs@users.noreply.github.com>
2020-09-24 09:13:58 -0700

    Add prop to SelectedItemList as callback for when an item is edited (#15160)
    * add callback prop
    
    * fix dependency
    
    * add example
    
    * fix snapshots
    
    * Change files
    
    * address PR comments
    
    * Revert "fix snapshots"
    
    This reverts commit 23a70bd8647c6ba2577086368fd4494779574a15.

--- packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/UnifiedPeoplePickerPage.tsx
+++ packages/experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/UnifiedPeoplePickerPage.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { ExampleCard, IComponentDemoPageProps, ComponentPage, PropertiesTableSet } from '@uifabric/example-app-base';
 import { UnifiedPeoplePickerExample } from './examples/UnifiedPeoplePicker.Example';
+import { UnifiedPeoplePickerWithEditExample } from './examples/UnifiedPeoplePicker.WithEdit.Example';
 import { DoubleUnifiedPeoplePickerExample } from './examples/DoubleUnifiedPeoplePicker.Example';
 
 const UnifiedPeoplePickerExampleCode = require('!raw-loader!./examples/UnifiedPeoplePicker.Example') as string;
+const UnifiedPeoplePickerWithEditExampleCode = require('!raw-loader!./examples/UnifiedPeoplePicker.WithEdit.Example') as string;
 const DoubleUnifiedPeoplePickerExampleCode = require('!raw-loader!./examples/DoubleUnifiedPeoplePicker.Example') as string;
 
 export class UnifiedPeoplePickerPage extends React.Component<IComponentDemoPageProps, {}> {
@@ -16,6 +18,10 @@ export class UnifiedPeoplePickerPage extends React.Component<IComponentDemoPageP
           <div>
             <ExampleCard title="Single Well" isOptIn={true} code={UnifiedPeoplePickerExampleCode}>
               <UnifiedPeoplePickerExample />
+            </ExampleCard>
+
+            <ExampleCard title="Single Well" isOptIn={true} code={UnifiedPeoplePickerWithEditExampleCode}>
+              <UnifiedPeoplePickerWithEditExample />
             </ExampleCard>
 
             <ExampleCard title="Double Well" isOptIn={true} code={DoubleUnifiedPeoplePickerExampleCode}>
