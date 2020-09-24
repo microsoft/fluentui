31c8c3233476ba8fb185b0031c63a88ab2af367a
Charles Assunção <junioassuncaocharles@gmail.com>
2020-09-24 17:09:33 +0200

    fix(TreeTitle): adding padding (#15206)
    * fix(TreeTitle): adding padding
    
    * fix(TreeTitle): adding changelog
    
    * fix(TreeTitle): move level control
    
    * fix(TreeTitle): use correct value for tree level padding

--- packages/fluentui/react-northstar/src/themes/teams/components/Tree/treeItemStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Tree/treeItemStyles.ts
@@ -1,5 +1,4 @@
 import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
-import { pxToRem } from '../../../../utils';
 import { getBorderFocusStyles } from '../../getBorderFocusStyles';
 import { TreeItemStylesProps } from '../../../../components/Tree/TreeItem';
 import { treeTitleClassName } from '../../../../components/Tree/TreeTitle';
@@ -7,7 +6,6 @@ import { treeTitleClassName } from '../../../../components/Tree/TreeTitle';
 export const treeItemStyles: ComponentSlotStylesPrepared<TreeItemStylesProps> = {
   root: ({ theme: { siteVariables }, props: p }): ICSSInJSStyle => ({
     listStyleType: 'none',
-    padding: `0 0 0 ${pxToRem(1 + (p.level - 1) * 10)}`,
     ...getBorderFocusStyles({ variables: siteVariables })[':focus'],
     ':focus-visible': {
       outline: 0,
