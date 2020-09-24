7a252be4beda4cd9d1c850d625853eb4d375757e
Milan Turon <mituron@microsoft.com>
2020-09-24 16:31:10 +0200

    removing not used code

--- packages/a11y-testing/src/index.ts
+++ packages/a11y-testing/src/index.ts
@@ -3,6 +3,3 @@ export * from './validators';
 export * from './facades';
 export * from './rules';
 export * from './definitions';
-// export * from './definitions/Button/buttonBehaviorDefinition';
-// export * from './definitions/Button/buttonGroupBehaviorDefinition';
-// export * from './definitions/Button/toggleButtonBehaviorDefinition';
