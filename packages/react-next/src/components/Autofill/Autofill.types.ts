db083dc145d324983ea6635f2d6743234738d9d7
Caleb Zearing <czearing@outlook.com>
2020-09-24 16:48:11 -0700

    Update Autofill.types.ts

--- packages/react-next/src/components/Autofill/Autofill.types.ts
+++ packages/react-next/src/components/Autofill/Autofill.types.ts
@@ -96,7 +96,7 @@ export interface IAutofillProps extends React.InputHTMLAttributes<HTMLInputEleme
 
   /**
    * Handler for checking if the full value of the input should
-   * be seleced in componentDidUpdate
+   * be selected in componentDidUpdate
    *
    * @returns - should the full value of the input be selected?
    */
