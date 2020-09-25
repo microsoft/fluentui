6a6738ec3198c664b00f9f466cdde0e0c724ff56
vyhnalekl <40213147+vyhnalekl@users.noreply.github.com>
2020-09-25 10:02:05 +0200

    Addressed comments

--- packages/fluentui/docs/src/routes.tsx
+++ packages/fluentui/docs/src/routes.tsx
@@ -85,7 +85,6 @@ const Routes = () => (
     <BrowserRouter basename={__BASENAME__ === '/' ? __BASENAME__ : __BASENAME__.slice(0, -1)}>
       <Switch>
         <Route exact path="/maximize/:exampleName/:rtl?" component={ExternalExampleLayout} />
-        <Route exact path="/maximize-compiled/:exampleName/:rtl?" component={PerfExampleLayout} />
         <Route exact path="/builder" component={Builder} />
         <Route exact path="/builder/maximize" component={FullScreenPreview} />
 
