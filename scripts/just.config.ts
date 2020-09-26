66e2ac6bba78a9c3560a9f95db3a2cb7a1b512ce
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-09-25 18:36:39 -0700

    Fixing broken storybook deployed sites.

--- scripts/just.config.ts
+++ scripts/just.config.ts
@@ -53,7 +53,7 @@ function checkForStorybookExistence() {
   // contains a folder with the current package's name.
   return (
     !!resolveCwd('./.storybook/main.js') ||
-    (!!resolveCwd('../examples/.storybook/main.js') &&
+    (!!resolveCwd('../react-examples/.storybook/main.js') &&
       fs.existsSync(path.join(findGitRoot(), `packages/react-examples/src/${packageName}`)))
   );
 }
