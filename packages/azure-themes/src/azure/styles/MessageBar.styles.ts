3ae8c2b55ef790fb2da9999c4639e32e2e4e5708
q-xg <67673432+q-xg@users.noreply.github.com>
2020-09-28 13:08:01 +0800

    Fixed Info MessageBar's dismiss button background color

--- packages/azure-themes/src/azure/styles/MessageBar.styles.ts
+++ packages/azure-themes/src/azure/styles/MessageBar.styles.ts
@@ -39,7 +39,7 @@ const IconButtonStyles = (props: IMessageBarStyleProps): IStyle => {
     (messageBarType === MessageBarType.warning || messageBarType === MessageBarType.blocked) &&
       generateBaseStyle(semanticColors.statusWarningBackground, semanticColors.statusWarningText),
 
-    !messageBarType && generateBaseStyle(semanticColors.bodyBackground, semanticColors.bodyText),
+    typeof messageBarType !== 'number' && generateBaseStyle(semanticColors.bodyBackground, semanticColors.bodyText),
   ];
 };
 
