4b24079f3d617496e48ba4cc41ca9c419e2fa779
Elizabeth Craig <elcraig@microsoft.com>
2020-09-27 21:59:08 -0700

    Delete northstar-only config files

--- scripts/gulp/plugins/util/parseBuffer.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from '@babel/parser';
-
-const parseBuffer = buffer =>
-  parse(buffer.toString(), {
-    plugins: ['classProperties', 'jsx'],
-    sourceType: 'module',
-  });
-
-export default parseBuffer;
