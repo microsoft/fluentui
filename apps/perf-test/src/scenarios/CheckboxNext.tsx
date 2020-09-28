c088337b17c8486ceebccb7a2c84ebcba05e776b
Elizabeth Craig <elcraig@microsoft.com>
2020-09-27 22:31:24 -0700

    Remove Next screener and perf tests since baseline aren't needed for those

--- apps/perf-test/src/scenarios/CheckboxNext.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Checkbox } from '@fluentui/react-next';
-
-const styles = { root: { background: 'red' } };
-const Scenario = () => <Checkbox label="I am a checkbox" styles={styles} />;
-
-export default Scenario;
