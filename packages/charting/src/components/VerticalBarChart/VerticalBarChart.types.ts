10bac37bcfaf7df87d0e8cee95d617c4aab92dab
Elizabeth Craig <elcraig@microsoft.com>
2020-09-29 13:25:21 -0700

    OUFR rename: update imports in charting

--- packages/charting/src/components/VerticalBarChart/VerticalBarChart.types.ts
+++ packages/charting/src/components/VerticalBarChart/VerticalBarChart.types.ts
@@ -1,7 +1,7 @@
-import { ITheme, IStyle } from 'office-ui-fabric-react/lib/Styling';
-import { IStyleFunctionOrObject } from 'office-ui-fabric-react/lib/Utilities';
+import { ITheme, IStyle } from '@fluentui/react/lib/Styling';
+import { IStyleFunctionOrObject } from '@fluentui/react/lib/Utilities';
 import { IDataPoint } from '../../types/IDataPoint';
-import { IOverflowSetProps } from 'office-ui-fabric-react/lib/OverflowSet';
+import { IOverflowSetProps } from '@fluentui/react/lib/OverflowSet';
 import { IFocusZoneProps } from '@fluentui/react-focus';
 import { ILegendsProps } from '../Legends/index';
 
