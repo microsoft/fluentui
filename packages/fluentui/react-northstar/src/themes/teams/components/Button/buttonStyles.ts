8b0d3767110a8808bb3c4d24b1eaf7fcb9ed77a4
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-09-29 17:40:09 +0200

    fix(button): extra width when loading=false

--- packages/fluentui/react-northstar/src/themes/teams/components/Button/buttonStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Button/buttonStyles.ts
@@ -23,7 +23,7 @@ export const buttonStyles: ComponentSlotStylesPrepared<ButtonStylesProps, Button
 
     return {
       height: v.height,
-      minWidth: _.isNil(p.loading) ? v.minWidth : v.loadingMinWidth,
+      minWidth: _.isNil(p.loading) || !p.loading ? v.minWidth : v.loadingMinWidth,
       maxWidth: v.maxWidth,
       color: v.color,
       backgroundColor: v.backgroundColor,
