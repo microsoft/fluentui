cf4923ddd8f196d33021d18c30e1915c54057568
Elizabeth Craig <elcraig@microsoft.com>
2020-09-30 23:38:39 -0700

    OUFR rename: update imports and references in other packages

--- packages/styling/src/styles/getPlaceholderStyles.ts
+++ packages/styling/src/styles/getPlaceholderStyles.ts
@@ -1,7 +1,7 @@
 import { IStyle } from '@uifabric/merge-styles';
 
 /**
- * Generates placeholder style for each of the browsers supported by office-ui-fabric-react.
+ * Generates placeholder style for each of the browsers supported by `@fluentui/react`.
  * @param styles - The style to use.
  * @returns The placeholder style object for each browser depending on the placeholder directive it uses.
  */
