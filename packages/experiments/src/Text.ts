3e078a95fe87e8dd7e2e2d390917fb8291aba779
Elizabeth Craig <elcraig@microsoft.com>
2020-09-30 23:52:55 -0700

    OUFR rename: fix other issues

--- packages/experiments/src/Text.ts
+++ packages/experiments/src/Text.ts
@@ -1,4 +1,4 @@
 /**
- * @deprecated Import from @fluentui/react package instead.
+ * @deprecated Import from `@fluentui/react` package instead.
  */
 export { Text } from '@fluentui/react/lib/Text';
