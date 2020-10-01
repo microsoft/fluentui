a0ae5c1150dc5c8d623cf529d9db983395aa4ed9
Elizabeth Craig <elcraig@microsoft.com>
2020-09-30 20:21:18 -0700

    Update references to renamed Pivot style prop

--- packages/azure-themes/src/azure/styles/Pivot.styles.ts
+++ packages/azure-themes/src/azure/styles/Pivot.styles.ts
@@ -3,9 +3,11 @@ import * as StyleConstants from '../Constants';
 import { IExtendedSemanticColors } from '../IExtendedSemanticColors';
 
 export const PivotStyles = (props: IPivotStyleProps): Partial<IPivotStyles> => {
-  const { theme, rootIsTabs, rootIsLarge } = props;
+  const { theme, linkFormat, linkSize } = props;
   const { semanticColors } = theme;
   const extendedSemanticColors = semanticColors as IExtendedSemanticColors;
+  const rootIsTabs = linkFormat === 'tabs';
+  const rootIsLarge = linkSize === 'large';
 
   return {
     root: [
