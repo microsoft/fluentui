22ff6aef6b6dd7393dfcb5f1486021724f9106a5
Caleb Zearing <czearing@outlook.com>
2020-10-01 16:27:16 -0700

    Reverting react-next changes.

--- packages/react-next/src/components/Persona/PersonaPresence/PersonaPresence.test.tsx
+++ packages/react-next/src/components/Persona/PersonaPresence/PersonaPresence.test.tsx
@@ -98,6 +98,6 @@ describe('PersonaPresence', () => {
   isConformant({
     Component: PersonaPresence,
     displayName: 'PersonaPresence',
-    isInternal: true,
+    disabledTests: ['exported-top-level', 'has-top-level-file'],
   });
 });
