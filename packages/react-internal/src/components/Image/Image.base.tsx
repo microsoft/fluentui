04c1954f8a1b97c784680730417b67399ee7a180
tomi-msft <66456876+tomi-msft@users.noreply.github.com>
2020-10-06 19:42:24 -0700

    Image: move Image component to react and delete from react-next (#15367)

--- packages/react-internal/src/components/Image/Image.base.tsx
+++ packages/react-internal/src/components/Image/Image.base.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { classNamesFunction, getNativeProps, imgProperties } from '../../Utilities';
 import { IImageProps, IImageStyleProps, IImageStyles, ImageCoverStyle, ImageFit, ImageLoadState } from './Image.types';
+import { useMergedRefs } from '@uifabric/react-hooks';
 
 const getClassNames = classNamesFunction<IImageStyleProps, IImageStyles>();
 
@@ -8,49 +9,78 @@ export interface IImageState {
   loadState?: ImageLoadState;
 }
 
+const SVG_REGEX = /\.svg$/i;
 const KEY_PREFIX = 'fabricImage';
 
-export class ImageBase extends React.Component<IImageProps, IImageState> {
-  public static defaultProps = {
-    shouldFadeIn: true,
-  };
+function useLoadState(
+  props: IImageProps,
+  imageElement: React.RefObject<HTMLImageElement>,
+): readonly [
+  ImageLoadState,
+  /* onImageLoad */ (ev: React.SyntheticEvent<HTMLImageElement>) => void,
+  /* onImageError */ (ev: React.SyntheticEvent<HTMLImageElement>) => void,
+] {
+  const { onLoadingStateChange, onLoad, onError, src } = props;
 
-  private static _svgRegex = /\.svg$/i;
+  const [loadState, setLoadState] = React.useState<ImageLoadState>(ImageLoadState.notLoaded);
 
-  // Make an initial assumption about the image layout until we can
-  // check the rendered element. The value here only takes effect when
-  // shouldStartVisible is true.
-  private _coverStyle: ImageCoverStyle = ImageCoverStyle.portrait;
-  private _imageElement = React.createRef<HTMLImageElement>();
-  private _frameElement = React.createRef<HTMLDivElement>();
+  React.useLayoutEffect(() => {
+    // If the src property changes, reset the load state
+    // (does nothing if the load state is already notLoaded)
+    setLoadState(ImageLoadState.notLoaded);
+  }, [src]);
 
-  constructor(props: IImageProps) {
-    super(props);
+  // eslint-disable-next-line react-hooks/exhaustive-deps -- intended to run every render
+  React.useEffect(() => {
+    if (loadState === ImageLoadState.notLoaded) {
+      // testing if naturalWidth and naturalHeight are greater than zero is better than checking
+      // .complete, because .complete will also be set to true if the image breaks. However,
+      // for some browsers, SVG images do not have a naturalWidth or naturalHeight, so fall back
+      // to checking .complete for these images.
+      const isLoaded: boolean = imageElement.current
+        ? (src && imageElement.current.naturalWidth > 0 && imageElement.current.naturalHeight > 0) ||
+          (imageElement.current.complete && SVG_REGEX.test(src!))
+        : false;
 
-    this.state = {
-      loadState: ImageLoadState.notLoaded,
-    };
-  }
-
-  public UNSAFE_componentWillReceiveProps(nextProps: IImageProps): void {
-    if (nextProps.src !== this.props.src) {
-      this.setState({
-        loadState: ImageLoadState.notLoaded,
-      });
-    } else if (this.state.loadState === ImageLoadState.loaded) {
-      this._computeCoverStyle(nextProps);
+      if (isLoaded) {
+        setLoadState(ImageLoadState.loaded);
+      }
     }
-  }
+  });
 
-  public componentDidUpdate(prevProps: IImageProps, prevState: IImageState) {
-    this._checkImageLoaded();
-    if (this.props.onLoadingStateChange && prevState.loadState !== this.state.loadState) {
-      this.props.onLoadingStateChange(this.state.loadState!);
-    }
-  }
+  React.useEffect(() => {
+    onLoadingStateChange?.(loadState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- should only run when loadState changes
+  }, [loadState]);
 
-  public render(): JSX.Element {
-    const imageProps = getNativeProps<React.ImgHTMLAttributes<HTMLImageElement>>(this.props, imgProperties, [
+  const onImageLoaded = React.useCallback(
+    (ev: React.SyntheticEvent<HTMLImageElement>) => {
+      onLoad?.(ev);
+      if (src) {
+        setLoadState(ImageLoadState.loaded);
+      }
+    },
+    [src, onLoad],
+  );
+
+  const onImageError = React.useCallback(
+    (ev: React.SyntheticEvent<HTMLImageElement>) => {
+      onError?.(ev);
+      setLoadState(ImageLoadState.error);
+    },
+    [onError],
+  );
+
+  return [loadState, onImageLoaded, onImageError] as const;
+}
+
+export const ImageBase: React.FunctionComponent<IImageProps> = React.forwardRef<HTMLImageElement, IImageProps>(
+  (props, forwardedRef) => {
+    const frameElement = React.useRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
+    const imageElement = React.useRef<HTMLImageElement>() as React.RefObject<HTMLImageElement>;
+    const [loadState, onImageLoaded, onImageError] = useLoadState(props, imageElement);
+
+    const imageProps = getNativeProps<React.ImgHTMLAttributes<HTMLImageElement>>(props, imgProperties, [
       'width',
       'height',
     ]);
@@ -59,7 +89,7 @@ export class ImageBase extends React.Component<IImageProps, IImageState> {
       alt,
       width,
       height,
-      shouldFadeIn,
+      shouldFadeIn = true,
       shouldStartVisible,
       className,
       imageFit,
@@ -67,9 +97,8 @@ export class ImageBase extends React.Component<IImageProps, IImageState> {
       maximizeFrame,
       styles,
       theme,
-    } = this.props;
-    const { loadState } = this.state;
-    const coverStyle = this.props.coverStyle !== undefined ? this.props.coverStyle : this._coverStyle;
+    } = props;
+    const coverStyle = useCoverStyle(props, loadState, imageElement, frameElement);
     const classNames = getClassNames(styles!, {
       theme: theme!,
       className,
@@ -79,8 +108,7 @@ export class ImageBase extends React.Component<IImageProps, IImageState> {
       shouldFadeIn,
       shouldStartVisible,
       isLoaded:
-        loadState === ImageLoadState.loaded ||
-        (loadState === ImageLoadState.notLoaded && this.props.shouldStartVisible),
+        loadState === ImageLoadState.loaded || (loadState === ImageLoadState.notLoaded && props.shouldStartVisible),
       isLandscape: coverStyle === ImageCoverStyle.landscape,
       isCenter: imageFit === ImageFit.center,
       isCenterContain: imageFit === ImageFit.centerContain,
@@ -94,105 +122,86 @@ export class ImageBase extends React.Component<IImageProps, IImageState> {
 
     // If image dimensions aren't specified, the natural size of the image is used.
     return (
-      <div className={classNames.root} style={{ width: width, height: height }} ref={this._frameElement}>
+      <div className={classNames.root} style={{ width: width, height: height }} ref={frameElement}>
         <img
           {...imageProps}
-          onLoad={this._onImageLoaded}
-          onError={this._onImageError}
-          key={KEY_PREFIX + this.props.src || ''}
+          onLoad={onImageLoaded}
+          onError={onImageError}
+          key={KEY_PREFIX + props.src || ''}
           className={classNames.image}
-          ref={this._imageElement}
+          ref={useMergedRefs(imageElement, forwardedRef)}
           src={src}
           alt={alt}
           role={role}
         />
       </div>
     );
+  },
+);
+ImageBase.displayName = 'ImageBase';
+
+function useCoverStyle(
+  props: IImageProps,
+  loadState: ImageLoadState,
+  imageElement: React.RefObject<HTMLImageElement>,
+  frameElement: React.RefObject<HTMLDivElement>,
+) {
+  const previousLoadState = React.useRef(loadState);
+  const coverStyle = React.useRef<ImageCoverStyle | undefined>();
+
+  if (
+    coverStyle === undefined ||
+    (previousLoadState.current === ImageLoadState.notLoaded && loadState === ImageLoadState.loaded)
+  ) {
+    coverStyle.current = computeCoverStyle(props, loadState, imageElement, frameElement);
   }
 
-  private _onImageLoaded = (ev: React.SyntheticEvent<HTMLImageElement>): void => {
-    const { src, onLoad } = this.props;
-    if (onLoad) {
-      onLoad(ev);
-    }
+  previousLoadState.current = loadState;
 
-    this._computeCoverStyle(this.props);
+  return coverStyle.current!;
+}
 
-    if (src) {
-      this.setState({
-        loadState: ImageLoadState.loaded,
-      });
-    }
-  };
+function computeCoverStyle(
+  props: IImageProps,
+  loadState: ImageLoadState,
+  imageElement: React.RefObject<HTMLImageElement>,
+  frameElement: React.RefObject<HTMLDivElement>,
+): ImageCoverStyle {
+  const { imageFit, width, height } = props;
 
-  private _checkImageLoaded(): void {
-    const { src } = this.props;
-    const { loadState } = this.state;
-
-    if (loadState === ImageLoadState.notLoaded) {
-      // testing if naturalWidth and naturalHeight are greater than zero is better than checking
-      // .complete, because .complete will also be set to true if the image breaks. However,
-      // for some browsers, SVG images do not have a naturalWidth or naturalHeight, so fall back
-      // to checking .complete for these images.
-      const isLoaded: boolean = this._imageElement.current
-        ? (src && this._imageElement.current.naturalWidth > 0 && this._imageElement.current.naturalHeight > 0) ||
-          (this._imageElement.current.complete && ImageBase._svgRegex.test(src!))
-        : false;
-
-      if (isLoaded) {
-        this._computeCoverStyle(this.props);
-        this.setState({
-          loadState: ImageLoadState.loaded,
-        });
-      }
-    }
-  }
-
-  private _computeCoverStyle(props: IImageProps): void {
-    const { imageFit, width, height } = props;
-
-    // Do not compute cover style if it was already specified in props
+  // Do not compute cover style if it was already specified in props
+  if (props.coverStyle !== undefined) {
+    return props.coverStyle;
+  } else if (
+    loadState === ImageLoadState.loaded &&
+    (imageFit === ImageFit.cover ||
+      imageFit === ImageFit.contain ||
+      imageFit === ImageFit.centerContain ||
+      imageFit === ImageFit.centerCover) &&
+    imageElement.current &&
+    frameElement.current
+  ) {
+    // Determine the desired ratio using the width and height props.
+    // If those props aren't available, measure measure the frame.
+    let desiredRatio;
     if (
-      (imageFit === ImageFit.cover ||
-        imageFit === ImageFit.contain ||
-        imageFit === ImageFit.centerContain ||
-        imageFit === ImageFit.centerCover) &&
-      this.props.coverStyle === undefined &&
-      this._imageElement.current &&
-      this._frameElement.current
+      typeof width === 'number' &&
+      typeof height === 'number' &&
+      imageFit !== ImageFit.centerContain &&
+      imageFit !== ImageFit.centerCover
     ) {
-      // Determine the desired ratio using the width and height props.
-      // If those props aren't available, measure measure the frame.
-      let desiredRatio;
-      if (
-        typeof width === 'number' &&
-        typeof height === 'number' &&
-        imageFit !== ImageFit.centerContain &&
-        imageFit !== ImageFit.centerCover
-      ) {
-        desiredRatio = width / height;
-      } else {
-        desiredRatio = this._frameElement.current.clientWidth / this._frameElement.current.clientHeight;
-      }
+      desiredRatio = width / height;
+    } else {
+      desiredRatio = frameElement.current.clientWidth / frameElement.current.clientHeight;
+    }
 
-      // Examine the source image to determine its original ratio.
-      const naturalRatio = this._imageElement.current.naturalWidth / this._imageElement.current.naturalHeight;
+    // Examine the source image to determine its original ratio.
+    const naturalRatio = imageElement.current.naturalWidth / imageElement.current.naturalHeight;
 
-      // Should we crop from the top or the sides?
-      if (naturalRatio > desiredRatio) {
-        this._coverStyle = ImageCoverStyle.landscape;
-      } else {
-        this._coverStyle = ImageCoverStyle.portrait;
-      }
+    // Should we crop from the top or the sides?
+    if (naturalRatio > desiredRatio) {
+      return ImageCoverStyle.landscape;
     }
   }
-
-  private _onImageError = (ev: React.SyntheticEvent<HTMLImageElement>): void => {
-    if (this.props.onError) {
-      this.props.onError(ev);
-    }
-    this.setState({
-      loadState: ImageLoadState.error,
-    });
-  };
+  return ImageCoverStyle.portrait;
 }
