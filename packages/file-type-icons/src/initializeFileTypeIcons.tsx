2c2123034292be2552555457c105ddb8b9efc01e
Arko Gupta <arkgupta@microsoft.com>
2020-10-07 21:24:27 +0530

    fileicons as html strings

--- packages/file-type-icons/src/initializeFileTypeIcons.tsx
+++ packages/file-type-icons/src/initializeFileTypeIcons.tsx
@@ -8,10 +8,18 @@ const SVG_SUFFIX = '_svg';
 const DEFAULT_BASE_URL = 'https://spoprod-a.akamaihd.net/files/fabric-cdn-prod_20200921.001/assets/item-types/';
 const ICON_SIZES: number[] = [16, 20, 24, 32, 40, 48, 64, 96];
 
+const _fileTypeIconsAsString: { [iconName: string]: string } = {};
+
 export function initializeFileTypeIcons(baseUrl: string = DEFAULT_BASE_URL, options?: Partial<IIconOptions>): void {
   ICON_SIZES.forEach((size: number) => {
     _initializeIcons(baseUrl, size, options);
   });
+}
+
+export function getFileTypeIconsAsHTMLStrings(iconName: string): string | undefined {
+  if (_fileTypeIconsAsString.hasOwnProperty(iconName)) {
+    return _fileTypeIconsAsString[iconName];
+  }
 }
 
 function _initializeIcons(baseUrl: string, size: number, options?: Partial<IIconOptions>): void {
@@ -20,8 +28,18 @@ function _initializeIcons(baseUrl: string, size: number, options?: Partial<IIcon
 
   iconTypes.forEach((type: string) => {
     const baseUrlSizeType = baseUrl + size + '/' + type;
-    fileTypeIcons[type + size + PNG_SUFFIX] = <img src={baseUrlSizeType + '.png'} alt="" />;
-    fileTypeIcons[type + size + SVG_SUFFIX] = <img src={baseUrlSizeType + '.svg'} alt="" />;
+    let src: string;
+    let iconName: string;
+
+    src = `{${baseUrlSizeType} + '.png'}`;
+    iconName = type + size + PNG_SUFFIX;
+    fileTypeIcons[iconName] = <img src={src} alt="" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" alt="" />`;
+
+    src = `{${baseUrlSizeType} + '.svg'}`;
+    iconName = type + size + SVG_SUFFIX;
+    fileTypeIcons[iconName] = <img src={src} alt="" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" alt="" />`;
 
     // For high resolution screens, register additional versions
     // Apply height=100% and width=100% to force image to fit into containing element
@@ -29,22 +47,30 @@ function _initializeIcons(baseUrl: string, size: number, options?: Partial<IIcon
     // SVGs scale well, so you can generally use the default image.
     // 1.5x is a special case where both SVGs and PNGs need a different image.
 
-    fileTypeIcons[type + size + '_1.5x' + PNG_SUFFIX] = (
-      <img src={baseUrl + size + '_1.5x/' + type + '.png'} height="100%" width="100%" />
-    );
-    fileTypeIcons[type + size + '_1.5x' + SVG_SUFFIX] = (
-      <img src={baseUrl + size + '_1.5x/' + type + '.svg'} height="100%" width="100%" />
-    );
+    iconName = type + size + '_1.5x' + PNG_SUFFIX;
+    src = `{${baseUrl} + ${size} + _1.5x/ + ${type} + .png}`;
+    fileTypeIcons[iconName] = <img src={src} height="100%" width="100%" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" height="100%" width="100%" />`;
 
-    fileTypeIcons[type + size + '_2x' + PNG_SUFFIX] = (
-      <img src={baseUrl + size + '_2x/' + type + '.png'} height="100%" width="100%" />
-    );
-    fileTypeIcons[type + size + '_3x' + PNG_SUFFIX] = (
-      <img src={baseUrl + size + '_3x/' + type + '.png'} height="100%" width="100%" />
-    );
-    fileTypeIcons[type + size + '_4x' + PNG_SUFFIX] = (
-      <img src={baseUrl + size + '_4x/' + type + '.png'} height="100%" width="100%" />
-    );
+    iconName = type + size + '_1.5x' + SVG_SUFFIX;
+    src = `{${baseUrl} + ${size} + _1.5x/ + ${type} + .svg}`;
+    fileTypeIcons[iconName] = <img src={src} height="100%" width="100%" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" height="100%" width="100%" />`;
+
+    iconName = type + size + '_2x' + PNG_SUFFIX;
+    src = `{${baseUrl} + ${size} + _2x/ + ${type} + .png}`;
+    fileTypeIcons[iconName] = <img src={src} height="100%" width="100%" />;
+    _fileTypeIconsAsString[iconName] = `<img src={src} height="100%" width="100%" />`;
+
+    iconName = type + size + '_3x' + PNG_SUFFIX;
+    src = `{${baseUrl} + ${size} + _3x/ + ${type} + .png}`;
+    fileTypeIcons[iconName] = <img src={src} height="100%" width="100%" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" height="100%" width="100%" />`;
+
+    iconName = type + size + '_4x' + PNG_SUFFIX;
+    src = `{${baseUrl} + ${size} + _4x/ + ${type} + .png}`;
+    fileTypeIcons[iconName] = <img src={src} height="100%" width="100%" />;
+    _fileTypeIconsAsString[iconName] = `<img src="${src}" height="100%" width="100%" />`;
   });
 
   registerIcons(
