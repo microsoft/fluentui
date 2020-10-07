4dbb92f6c59e79fcc22c6d7a418f918af992ecf3
Caleb Zearing <czearing@outlook.com>
2020-10-06 22:11:00 -0700

    Moving SwatchColorPicker and ButtonGrid to @fluentui/react-internal package (#15373)

--- packages/react-internal/src/utilities/ButtonGrid/ButtonGrid.base.tsx
+++ packages/react-internal/src/utilities/ButtonGrid/ButtonGrid.base.tsx
@@ -1,102 +1,78 @@
 import * as React from 'react';
-import {
-  getId,
-  toMatrix,
-  classNamesFunction,
-  getNativeProps,
-  htmlElementProperties,
-  initializeComponentRef,
-} from '../../Utilities';
+import { toMatrix, classNamesFunction, getNativeProps, htmlElementProperties } from '../../Utilities';
 import { FocusZone } from '../../FocusZone';
-import { IButtonGrid, IButtonGridProps, IButtonGridStyleProps, IButtonGridStyles } from './ButtonGrid.types';
+import { IButtonGridProps, IButtonGridStyleProps, IButtonGridStyles } from './ButtonGrid.types';
+import { useId } from '@uifabric/react-hooks';
 
 const getClassNames = classNamesFunction<IButtonGridStyleProps, IButtonGridStyles>();
 
-export class ButtonGridBase extends React.Component<IButtonGridProps, {}> implements IButtonGrid {
-  private _id: string;
+export const ButtonGridBase: React.FunctionComponent<IButtonGridProps> = React.forwardRef<
+  HTMLElement,
+  IButtonGridProps
+>((props, forwardedRef) => {
+  const id = useId(undefined, props.id);
 
-  constructor(props: IButtonGridProps) {
-    super(props);
+  const {
+    items,
+    columnCount,
+    onRenderItem,
+    // eslint-disable-next-line deprecation/deprecation
+    ariaPosInSet = props.positionInSet,
+    // eslint-disable-next-line deprecation/deprecation
+    ariaSetSize = props.setSize,
+    styles,
+    doNotContainWithinFocusZone,
+  } = props;
 
-    initializeComponentRef(this);
-    this._id = props.id || getId();
-  }
+  const htmlProps = getNativeProps<React.HTMLAttributes<HTMLTableElement>>(
+    props,
+    htmlElementProperties,
+    // avoid applying onBlur on the table if it's being used in the FocusZone
+    doNotContainWithinFocusZone ? [] : ['onBlur'],
+  );
 
-  public render(): JSX.Element {
-    const props = this.props;
-    const {
-      items,
-      columnCount,
-      onRenderItem,
+  const classNames = getClassNames(styles!, { theme: props.theme! });
 
-      /* eslint-disable deprecation/deprecation */
-      ariaPosInSet = props.positionInSet,
-      ariaSetSize = props.setSize,
-      /* eslint-enable deprecation/deprecation */
+  // Array to store the cells in the correct row index
+  const rowsOfItems: any[][] = toMatrix(items, columnCount);
 
-      styles,
-      doNotContainWithinFocusZone,
-    } = props;
+  const content = (
+    <table
+      aria-posinset={ariaPosInSet}
+      aria-setsize={ariaSetSize}
+      id={id}
+      role="grid"
+      {...htmlProps}
+      className={classNames.root}
+    >
+      <tbody>
+        {rowsOfItems.map((rows, rowIndex) => {
+          return (
+            <tr role={'row'} key={rowIndex}>
+              {rows.map((cell, cellIndex: number) => {
+                return (
+                  <td role="presentation" key={cellIndex + '-cell'} className={classNames.tableCell}>
+                    {onRenderItem(cell, cellIndex)}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 
-    const htmlProps = getNativeProps<React.HTMLAttributes<HTMLTableElement>>(
-      this.props,
-      htmlElementProperties,
-      // avoid applying onBlur on the table if it's being used in the FocusZone
-      doNotContainWithinFocusZone ? [] : ['onBlur'],
-    );
-
-    const classNames = getClassNames(styles!, { theme: this.props.theme! });
-
-    // Array to store the cells in the correct row index
-    const rowsOfItems: any[][] = toMatrix(items, columnCount);
-
-    const content = (
-      <table
-        aria-posinset={ariaPosInSet}
-        aria-setsize={ariaSetSize}
-        id={this._id}
-        role="grid"
-        {...htmlProps}
-        className={classNames.root}
-      >
-        <tbody>
-          {rowsOfItems.map((rows: any[], rowIndex: number) => {
-            return (
-              <tr role={'row'} key={this._id + '-' + rowIndex + '-row'}>
-                {rows.map((cell: any, cellIndex: number) => {
-                  return (
-                    <td
-                      role={'presentation'}
-                      key={this._id + '-' + cellIndex + '-cell'}
-                      className={classNames.tableCell}
-                    >
-                      {onRenderItem(cell, cellIndex)}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-
-    // Create the table/grid
-    return doNotContainWithinFocusZone ? (
-      content
-    ) : (
-      <FocusZone
-        isCircularNavigation={this.props.shouldFocusCircularNavigate}
-        className={classNames.focusedContainer}
-        onBlur={this.props.onBlur}
-      >
-        {content}
-      </FocusZone>
-    );
-  }
-}
-
-/**
- * @deprecated - use ButtonGridBase instead
- */
-export const GridBase = ButtonGridBase;
+  return doNotContainWithinFocusZone ? (
+    content
+  ) : (
+    <FocusZone
+      elementRef={forwardedRef}
+      isCircularNavigation={props.shouldFocusCircularNavigate}
+      className={classNames.focusedContainer}
+      onBlur={props.onBlur}
+    >
+      {content}
+    </FocusZone>
+  );
+});
