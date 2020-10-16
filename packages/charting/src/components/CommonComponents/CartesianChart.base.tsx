1cfa4ee4683bf7a6a58074a311f2cf8fe0abdb63
v-sivsar <v-sivsar@microsoft.com>
2020-10-16 19:45:01 +0530

    fix build errors

--- packages/charting/src/components/CommonComponents/CartesianChart.base.tsx
+++ packages/charting/src/components/CommonComponents/CartesianChart.base.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { IProcessedStyleSet, IStyle, mergeStyles } from 'office-ui-fabric-react/lib/Styling';
+import { IProcessedStyleSet, mergeStyles } from 'office-ui-fabric-react/lib/Styling';
 import { classNamesFunction, getId, getRTL } from 'office-ui-fabric-react/lib/Utilities';
 import { Callout } from 'office-ui-fabric-react/lib/Callout';
 import {
