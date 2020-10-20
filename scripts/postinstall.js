16d39ed1ea3b968de1944c700a9c919792623673
LavaAdventures <69821200+LavaAdventures@users.noreply.github.com>
2020-10-20 12:13:08 -0400

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,9 +4,11 @@ const chalk = require('chalk');
 // git v2.9.0 supports a custom hooks directory. This means we just need to check in the hooks scripts.
 spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
 
+// logs All dependencies are installed! This repo no longer requires a build to start the inner loop.
 console.log(
   chalk.green('\nAll dependencies are installed! This repo no longer requires a build to start the inner loop.'),
 );
+// logs For inner loop development and does more stuff
 console.log(`For inner loop development, run:
   ${chalk.yellow('yarn start')}
 `);
