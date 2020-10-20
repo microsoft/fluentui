930ec44b1f463e0db6cd6d557ef9f8cc4b89b6db
Caleb Zearing <czearing@outlook.com>
2020-10-20 13:35:26 -0700

    Adding a forwardRef to Check.base and removing disabledTests.

--- packages/react-internal/src/components/Check/Check.test.tsx
+++ packages/react-internal/src/components/Check/Check.test.tsx
@@ -5,8 +5,5 @@ describe('Check', () => {
   isConformant({
     Component: Check,
     displayName: 'Check',
-    // Problem: Is FunctionComponent but missing `forwardRef`
-    // Solution: add forwardRef and ref typing
-    disabledTests: ['component-has-root-ref', 'component-handles-ref'],
   });
 });
