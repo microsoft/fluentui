7e4b32b6772306af4922dec41b0ee29061b08fc2
KHMakoto <humbertomakotomorimoto@gmail.com>
2020-10-22 12:12:15 -0700

    Fixing CompoundButton error in examples.

--- packages/react-examples/src/azure-themes/stories/Themes/Themes.stories.tsx
+++ packages/react-examples/src/azure-themes/stories/Themes/Themes.stories.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react';
-import {
-  Customizer,
-  TextField,
-  Stack,
-  Fabric,
-  CompoundButton,
-  Checkbox,
-  SearchBox,
-  Link,
-  Label,
-  Text,
-} from '@fluentui/react';
-import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/compat/Button';
+import { Customizer, TextField, Stack, Fabric, Checkbox, SearchBox, Link, Label, Text } from '@fluentui/react';
+import { DefaultButton, CompoundButton, PrimaryButton } from '@fluentui/react/lib/compat/Button';
 import {
   AzureCustomizationsLight,
   AzureCustomizationsDark,
