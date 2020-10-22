8466903ed0a3e5a7bd341fadceae22837c2d1902
Xu Gao <xgao@microsoft.com>
2020-10-21 19:44:05 -0700

    remove @fluentui/keyboard-key package

--- apps/test-bundles/webpack.config.js
+++ apps/test-bundles/webpack.config.js
@@ -1,7 +1,6 @@
-const { createWebpackConfig, buildEntries, buildEntry } = require('./webpackUtils');
+const { createWebpackConfig, buildEntries } = require('./webpackUtils');
 
 // Create entries for all top level imports.
 const entries = buildEntries('office-ui-fabric-react');
-entries['keyboard-key'] = buildEntry('@fluentui/keyboard-key');
 
 module.exports = createWebpackConfig(entries);
