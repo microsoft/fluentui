11fe877aeb6f7e2f65570edd84b3d6bd16a23025
ling1726 <lingfangao@hotmail.com>
2020-10-23 18:22:54 +0200

    Docversions manifest 47 (#15675)
    * Support doc versions manifest file for version dropdown
    
    * update yarn lock

--- packages/fluentui/docs/src/components/Sidebar/VersionDropdown.tsx
+++ packages/fluentui/docs/src/components/Sidebar/VersionDropdown.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
 import { Dropdown, DropdownProps } from '@fluentui/react-northstar';
+import { gt, lt } from 'semver';
 
 const pkg = require('@fluentui/react-northstar/package.json');
 
 export function VersionDropdown(props: { width: number }) {
   const currentVersion = pkg.version;
+  const [versions, setVersions] = React.useState<string[]>([]);
+  React.useEffect(() => {
+    fetch('/manifest.json')
+      .then(res => res.json())
+      .then(manifest => {
+        const availableVersions = Object.keys(manifest).reduce((versions, version) => {
+          if (manifest[version]) {
+            versions.push(version);
+          }
 
-  if (
-    !window.sessionStorage.fluentuiDocsiteVersions ||
-    window.sessionStorage.fluentuiDocsiteVersions.indexOf(currentVersion) === -1
-  ) {
+          return versions;
+        }, []);
+
+        availableVersions.sort((a, b) => {
+          if (gt(a, b)) return -1;
+          if (lt(a, b)) return 1;
+          return 0;
+        });
+
+        setVersions(availableVersions);
+      });
+  });
+
+  if (!versions.length) {
     return null;
   }
-
-  const items = [...JSON.parse(window.sessionStorage.fluentuiDocsiteVersions)];
 
   // We make assumptions about routing
   // https://<domain>/<version> should be the basename for each docsite in a multi version scenario
@@ -30,7 +48,7 @@ export function VersionDropdown(props: { width: number }) {
     <Dropdown
       variables={{ width: `${props.width}px` }}
       placeholder="Select package version"
-      items={items}
+      items={versions}
       onChange={onChange}
       value={currentVersion}
     />
