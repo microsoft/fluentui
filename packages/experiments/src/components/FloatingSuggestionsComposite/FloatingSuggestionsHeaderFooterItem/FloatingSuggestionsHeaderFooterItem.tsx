d8c2263dd72e87fd07cde229548bfc911f62ca9d
Elisabeth von Spakovsky <elvonspa@microsoft.com>
2020-10-23 12:20:10 -0700

    fix relative path

--- packages/experiments/src/components/FloatingSuggestionsComposite/FloatingSuggestionsHeaderFooterItem/FloatingSuggestionsHeaderFooterItem.tsx
+++ packages/experiments/src/components/FloatingSuggestionsComposite/FloatingSuggestionsHeaderFooterItem/FloatingSuggestionsHeaderFooterItem.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { classNamesFunction, css } from '../../../Utilities';
+import { classNamesFunction, css } from 'office-ui-fabric-react/lib/Utilities';
 import {
   IFloatingSuggestionsHeaderFooterItemProps,
   IFloatingSuggestionHeaderFooterItemStylesProps,
