29a920f48b854224d6f03a1b9c26964e555dd6d8
Faizal Hasanwala <fahasanw@microsoft.com>
2020-10-23 15:36:29 +0530

    Use type from state iff canRevealPassword is true

--- packages/office-ui-fabric-react/src/components/TextField/TextField.base.tsx
+++ packages/office-ui-fabric-react/src/components/TextField/TextField.base.tsx
@@ -360,7 +360,7 @@ export class TextFieldBase extends React.Component<ITextFieldProps, ITextFieldSt
       this._hasWarnedNullValue = true;
       warn(
         `Warning: 'value' prop on '${COMPONENT_NAME}' should not be null. Consider using an ` +
-          'empty string to clear the component or undefined to indicate an uncontrolled component.',
+        'empty string to clear the component or undefined to indicate an uncontrolled component.',
       );
     }
   }
@@ -484,12 +484,13 @@ export class TextFieldBase extends React.Component<ITextFieldProps, ITextFieldSt
       'defaultValue',
     ]);
     const ariaLabelledBy = this.props['aria-labelledby'] || (this.props.label ? this._labelId : undefined);
+    const inputType = this.props.canRevealPassword ? this.state.type : (inputProps.type ?? 'text');
     return (
       <input
         id={this._id}
         aria-labelledby={ariaLabelledBy}
         {...inputProps}
-        type={this.state.type}
+        type={inputType}
         ref={this._textElement as React.RefObject<HTMLInputElement>}
         value={this.value || ''}
         onInput={this._onInputChange}
