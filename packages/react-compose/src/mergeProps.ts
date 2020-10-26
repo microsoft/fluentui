bdd9d54d630f50a60b61f4fdfbbb2fbb05507475
Xu Gao <xugao0131@hotmail.com>
2020-10-26 10:46:35 -0700

    Add @fluentui/react-compose@v0 to 7.0 branch (#15696)
    * add react-compose package to 7.0 branch
    
    * Change files
    
    * fix bundle

---
+++ packages/react-compose/src/mergeProps.ts
@@ -0,0 +1,36 @@
+import { ComposePreparedOptions, MergePropsResult, GenericDictionary } from './types';
+import { resolveClasses } from './resolveClasses';
+import { resolveSlotProps } from './resolveSlotProps';
+
+/**
+ * Merge props takes in state and compose options, and resolves slots and slotProps.
+ * It's expected that the component will call mergeProps(state, options) from within
+ * render; after resolving state and before rendering slots and slotProps.
+ */
+export function mergeProps<
+  TProps,
+  TState = TProps,
+  TSlots = GenericDictionary,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TSlotProps = { [key in keyof TSlots]: any }
+>(state: TState, options: ComposePreparedOptions<TProps, TState>): MergePropsResult<TState, TSlots, TSlotProps> {
+  const result: MergePropsResult<TState> = {
+    state: state,
+    slots: {
+      ...options.slots,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      root: (state as any).as || options.slots.root || 'div',
+    },
+    slotProps: {},
+  };
+
+  // Resolve classes.
+  resolveClasses(result, options.classes);
+
+  // Resolve slotProps/slots from state.
+  resolveSlotProps(result, options);
+
+  // TODO: Resolve inline styles.
+
+  return result as MergePropsResult<TState, TSlots, TSlotProps>;
+}
