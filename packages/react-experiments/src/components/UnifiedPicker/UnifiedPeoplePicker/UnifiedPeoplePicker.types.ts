f056fa832c659e36daaa7fe5243a2b7b70e8ce1a
Elizabeth Craig <elcraig@microsoft.com>
2020-10-30 15:50:40 -0700

    Remove TS < 3.7 compat code

--- packages/react-experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/UnifiedPeoplePicker.types.ts
+++ packages/react-experiments/src/components/UnifiedPicker/UnifiedPeoplePicker/UnifiedPeoplePicker.types.ts
@@ -1,5 +1,4 @@
 import { IUnifiedPickerProps } from '../UnifiedPicker.types';
-import { Omit } from '@fluentui/utilities';
 import { IPersonaProps } from '@fluentui/react/lib/Persona';
 
 export type IUnifiedPeoplePickerProps = Omit<
