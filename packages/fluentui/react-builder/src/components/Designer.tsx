29c7c40be2acaeb86b02c40afa303abac3b23108
Juraj Kapsiar <jukapsia@microsoft.com>
2020-11-04 15:07:47 +0100

    axe

--- packages/fluentui/react-builder/src/components/Designer.tsx
+++ packages/fluentui/react-builder/src/components/Designer.tsx
@@ -349,37 +349,39 @@ export const Designer: React.FunctionComponent = () => {
   const [headerMessage, setHeaderMessage] = React.useState('');
 
   const runAxeOnElement = React.useCallback(selectedElementUuid => {
-    const iframe = document.getElementsByTagName('iframe')[0];
-    const selectedComponentAxeErrors = [];
-    axeCore.run(
-      iframe,
-      {
-        rules: {
-          // excluding rules which are related to the whole page not to components
-          'page-has-heading-one': { enabled: false },
-          region: { enabled: false },
-          'landmark-one-main': { enabled: false },
+    setTimeout(() => {
+      const iframe = document.getElementsByTagName('iframe')[0];
+      const selectedComponentAxeErrors = [];
+      axeCore.run(
+        iframe,
+        {
+          rules: {
+            // excluding rules which are related to the whole page not to components
+            'page-has-heading-one': { enabled: false },
+            region: { enabled: false },
+            'landmark-one-main': { enabled: false },
+          },
         },
-      },
-      (err, result) => {
-        if (err) {
-          console.error('Axe failed', err);
-        } else {
-          result.violations.forEach(violation => {
-            violation.nodes.forEach(node => {
-              if (node.html.includes(`data-builder-id="${selectedElementUuid}"`)) {
-                selectedComponentAxeErrors.push(
-                  node.failureSummary
-                    .replace('Fix all of the following:', '-')
-                    .replace('Fix any of the following:', '-'),
-                );
-              }
+        (err, result) => {
+          if (err) {
+            console.error('Axe failed', err);
+          } else {
+            result.violations.forEach(violation => {
+              violation.nodes.forEach(node => {
+                if (node.html.includes(`data-builder-id="${selectedElementUuid}"`)) {
+                  selectedComponentAxeErrors.push(
+                    node.failureSummary
+                      .replace('Fix all of the following:', '-')
+                      .replace('Fix any of the following:', '-'),
+                  );
+                }
+              });
             });
-          });
-        }
-        setAxeErrors(selectedComponentAxeErrors);
-      },
-    );
+          }
+          setAxeErrors(selectedComponentAxeErrors);
+        },
+      );
+    }, 1000);
   }, []);
 
   React.useEffect(() => {
@@ -621,7 +623,7 @@ export const Designer: React.FunctionComponent = () => {
     selectedJSONTreeElement;
 
   const codeSandboxData = getCodeSandboxInfo(jsonTree, renderElementToJSX(renderJSONTreeToJSXElement(jsonTree)));
-
+  console.log(axeErrors);
   return (
     <div
       style={{
