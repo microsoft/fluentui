c7d842b9e4398bc32c4b0bfb384b6bbc2f7d02e5
Arko Gupta <arkgupta@microsoft.com>
2020-11-06 01:16:21 +0530

    addressing review comments

--- packages/file-type-icons/src/getFileTypeIconAsHTMLString.ts
+++ packages/file-type-icons/src/getFileTypeIconAsHTMLString.ts
@@ -6,6 +6,13 @@ import {
   IFileTypeIconOptions,
 } from './getFileTypeIconProps';
 
+/**
+ * Given the `fileTypeIconOptions`, this function returns the DOM element for the `FileTypeIcon`
+ * as an HTML string. Similar to `getFileTypeIconProps`, this also accepts the same type of object
+ * but rather than returning the `iconName`, this returns the entire DOM element as a string.
+ * @param options
+ * @param baseUrl - optionally provide a custom CDN base url to fetch icons from
+ */
 export function getFileTypeIconAsHTMLString(
   options: IFileTypeIconOptions,
   baseUrl: string = DEFAULT_BASE_URL,
