8c3cc60f7af46a136228842dd0f95b0bc9112bae
Elisabeth von Spakovsky <elvonspa@microsoft.com>
2020-11-06 15:43:03 -0800

    lint

--- packages/react-experiments/src/components/UnifiedPicker/hooks/useSelectedItems.ts
+++ packages/react-experiments/src/components/UnifiedPicker/hooks/useSelectedItems.ts
@@ -22,11 +22,15 @@ export const useSelectedItems = <T extends {}>(
 ): IUseSelectedItemsResponse<T> => {
   const [items, setSelectedItems] = React.useState(selectedItems || []);
 
-  React.useEffect(() => {
-    if (selectedItems !== undefined) {
-      selection.setItems(selectedItems);
-    }
-  }, []);
+  React.useEffect(
+    () => {
+      if (selectedItems !== undefined) {
+        selection.setItems(selectedItems);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- we want to do this once
+    [],
+  );
 
   React.useEffect(() => {
     setSelectedItems(selectedItems ? selectedItems : []);
