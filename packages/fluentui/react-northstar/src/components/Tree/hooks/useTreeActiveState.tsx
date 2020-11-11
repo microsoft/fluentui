cb8c24436a56ac7683abdd49316f4ef1ecdf4173
Amber <yuanboxue@outlook.com>
2020-11-11 15:17:18 +0100

    Update packages/fluentui/react-northstar/src/components/Tree/hooks/useTreeActiveState.tsx
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/react-northstar/src/components/Tree/hooks/useTreeActiveState.tsx
+++ packages/fluentui/react-northstar/src/components/Tree/hooks/useTreeActiveState.tsx
@@ -33,7 +33,7 @@ export function useTreeActiveState(
       }
 
       setActiveItemIdsState(activeItemIds => {
-        let nextActiveItemIds;
+        let nextActiveItemIds: string[];
         const index = activeItemIds.indexOf(idToToggle);
 
         if (index >= 0) {
