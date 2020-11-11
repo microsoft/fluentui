d3e6cd5b9bce81fb331cc77759efdb9481b6730d
Amber <yuanboxue@outlook.com>
2020-11-11 15:44:18 +0100

    Update packages/fluentui/react-northstar/src/components/Tree/Tree.tsx
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/react-northstar/src/components/Tree/Tree.tsx
+++ packages/fluentui/react-northstar/src/components/Tree/Tree.tsx
@@ -195,7 +195,7 @@ export const Tree: ComponentWithAs<'div', TreeProps> &
             index,
             treeSize,
             selectable: selectable ? item.item.selectable !== false : false,
-            renderItemTitle: item.item.renderItemTitle || props.renderItemTitle,
+            renderItemTitle: props.renderItemTitle,
           }),
       });
     });
