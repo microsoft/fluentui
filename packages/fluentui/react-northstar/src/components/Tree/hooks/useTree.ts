38452a35b819a6351152078b574195845b218893
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-11-18 14:15:26 +0100

    one hook to rule them all

--- packages/fluentui/react-northstar/src/components/Tree/hooks/useTree.ts
+++ packages/fluentui/react-northstar/src/components/Tree/hooks/useTree.ts
@@ -1,11 +1,41 @@
 import * as React from 'react';
 import { useAutoControlled } from '@fluentui/react-bindings';
-import { useStableProps } from './useStableProps';
 import { ObjectShorthandCollection } from '../../../types';
 import { TreeItemProps } from '../TreeItem';
-import { FlatTreeItem, flattenTree } from './flattenTree';
-import { useGetItemById } from './useGetItemById';
+import { FlatTreeItem, flattenTree, FlatTree } from './flattenTree';
 import * as _ from 'lodash';
+
+type GetItemById = (id: string) => FlatTreeItem;
+
+/**
+ * This hook returns a stable `getItemById()` function that will lookup in latest `flatTree`.
+ * This is used used to have stable callbacks that can be passed to React's Context.
+ */
+function useGetItemById(flatTree: FlatTree): GetItemById {
+  // An exception is thrown there to ensure that a proper callback will assigned to ref
+  const callbackRef = React.useRef<GetItemById>(() => {
+    throw new Error('Callback is not assigned yet');
+  });
+
+  // We are assigning a callback during render as it can be used during render and in event handlers. In dev mode we
+  // are freezing objects to prevent their mutations
+  callbackRef.current = itemId =>
+    process.env.NODE === 'production' ? flatTree[itemId] : Object.freeze(flatTree[itemId]);
+
+  return React.useCallback<GetItemById>((...args) => {
+    return callbackRef.current(...args);
+  }, []);
+}
+
+function useStableProps<P>(props: P) {
+  const stableProps = React.useRef<P>(props);
+
+  React.useEffect(() => {
+    stableProps.current = props;
+  });
+
+  return stableProps;
+}
 
 export interface UseTreeOptions {
   /** Shorthand array of props for Tree. */
