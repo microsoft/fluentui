5ea5799983f4640bb79b84dc3c8790ea42e9c149
stefaniehansen <stefhan@microsoft.com>
2020-11-19 14:45:48 -0800

    Only attempt to handle keydown event for tooltip close if the tooltip is currently open

--- packages/react-internal/src/components/Tooltip/TooltipHost.base.tsx
+++ packages/react-internal/src/components/Tooltip/TooltipHost.base.tsx
@@ -223,7 +223,7 @@ export class TooltipHostBase extends React.Component<ITooltipHostProps, ITooltip
   };
 
   private _onTooltipKeyDown = (ev: React.KeyboardEvent<HTMLElement>): void => {
-    if (ev.which === KeyCodes.escape || ev.ctrlKey) {
+    if ((ev.which === KeyCodes.escape || ev.ctrlKey) && this.state.isTooltipVisible) {
       this._hideTooltip();
       ev.stopPropagation();
     }
