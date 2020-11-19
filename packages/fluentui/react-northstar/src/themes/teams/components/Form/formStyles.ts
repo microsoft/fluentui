2e552285531faf0d1bb2380464b0041e784ec209
Charles Assuncao <junioassuncaocharles@gmail.com>
2020-11-19 13:28:45 -0300

    chore(Grid): revert grid IE11 support

--- packages/fluentui/react-northstar/src/themes/teams/components/Form/formStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Form/formStyles.ts
@@ -1,14 +1,17 @@
 import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
 import { FormStylesProps } from '../../../../components/Form/Form';
 import { FormVariables } from './formVariables';
+import { pxToRem } from '../../../../utils';
 
 export const formStyles: ComponentSlotStylesPrepared<FormStylesProps, FormVariables> = {
   root: ({ props, variables: v }): ICSSInJSStyle => ({
     height: '100%',
     width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    display: ['grid', '-ms-grid'],
+    gridTemplateColumns: '1fr',
+    msGridColumns: '1fr',
     justifyContent: 'space-evenly',
+    gridGap: pxToRem(20),
     justifyItems: 'start',
     '> *:not(:last-child)': {
       marginBottom: v.fieldsMarginBottom,
