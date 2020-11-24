5adfeff3d1ef72ab3621b8de323185b4855816b7
Elizabeth Craig <elcraig@microsoft.com>
2020-11-23 18:14:49 -0800

    Fix Callout example targets and simplify styling (#15941)
    * Fix syntax error in readme
    
    * Callout examples: fix targets and simplify styling
    
    * Add interface for onRestoreFocus params
    
    * fix docs
    
    * fix date example

--- packages/react-examples/src/react/Callout/Callout.FocusTrap.Example.tsx
+++ packages/react-examples/src/react/Callout/Callout.FocusTrap.Example.tsx
@@ -1,91 +1,57 @@
 import * as React from 'react';
 import { FocusTrapCallout, Stack, FocusZone, mergeStyleSets, FontWeights, Text } from '@fluentui/react';
-import { useBoolean } from '@fluentui/react-hooks';
+import { useBoolean, useId } from '@fluentui/react-hooks';
 import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/compat/Button';
-
-const styles = mergeStyleSets({
-  buttonArea: {
-    verticalAlign: 'top',
-    display: 'inline-block',
-    textAlign: 'center',
-    margin: '0 100px',
-    minWidth: 130,
-    height: 32,
-  },
-  callout: {
-    maxWidth: 300,
-  },
-  header: {
-    padding: '18px 24px 12px',
-  },
-  title: [
-    {
-      margin: 0,
-      fontWeight: FontWeights.semilight,
-    },
-  ],
-  inner: {
-    height: '100%',
-    padding: '0 24px 20px',
-  },
-  actions: {
-    position: 'relative',
-    marginTop: 20,
-    width: '100%',
-    whiteSpace: 'nowrap',
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    padding: '0 24px 24px',
-  },
-  subtext: [
-    {
-      margin: 0,
-      fontWeight: FontWeights.semilight,
-    },
-  ],
-});
 
 export const CalloutFocusTrapExample: React.FunctionComponent = () => {
   const [isCalloutVisible, { toggle: toggleIsCalloutVisible }] = useBoolean(false);
+  const buttonId = useId('callout-button');
+
   return (
     <>
-      <div className={styles.buttonArea}>
-        <DefaultButton
-          onClick={toggleIsCalloutVisible}
-          text={isCalloutVisible ? 'Hide FocusTrapCallout' : 'Show FocusTrapCallout'}
-        />
-      </div>
+      <DefaultButton id={buttonId} onClick={toggleIsCalloutVisible} text="Show callout" />
       {isCalloutVisible ? (
-        <div>
-          <FocusTrapCallout
-            role="alertdialog"
-            className={styles.callout}
-            gapSpace={0}
-            target={`.${styles.buttonArea}`}
-            onDismiss={toggleIsCalloutVisible}
-            setInitialFocus
-          >
-            <div className={styles.header}>
-              <Text className={styles.title}>Callout title here</Text>
-            </div>
-            <div className={styles.inner}>
-              <div>
-                <Text className={styles.subtext}>
-                  Content is wrapped in a FocusTrapZone so that user cannot accidentally tab out of this callout.
-                </Text>
-              </div>
-            </div>
-            <FocusZone>
-              <Stack className={styles.buttons} gap={8} horizontal>
-                <PrimaryButton onClick={toggleIsCalloutVisible}>Button 1</PrimaryButton>
-                <DefaultButton onClick={toggleIsCalloutVisible}>Button 2</DefaultButton>
-              </Stack>
-            </FocusZone>
-          </FocusTrapCallout>
-        </div>
+        <FocusTrapCallout
+          role="alertdialog"
+          className={styles.callout}
+          gapSpace={0}
+          target={`#${buttonId}`}
+          onDismiss={toggleIsCalloutVisible}
+          setInitialFocus
+        >
+          <Text block variant="xLarge" className={styles.title}>
+            Focus trapping callout
+          </Text>
+          <Text block variant="small">
+            Content is wrapped in a FocusTrapZone so the user cannot accidentally tab or focus out of this callout. Use
+            the buttons to close.
+          </Text>
+          {/* This FocusZone allows the user to go between buttons with the arrow keys.
+              It's not related to or required for FocusTrapCallout's built-in focus trapping. */}
+          <FocusZone>
+            <Stack className={styles.buttons} gap={8} horizontal>
+              <PrimaryButton onClick={toggleIsCalloutVisible}>Done</PrimaryButton>
+              <DefaultButton onClick={toggleIsCalloutVisible}>Cancel</DefaultButton>
+            </Stack>
+          </FocusZone>
+        </FocusTrapCallout>
       ) : null}
     </>
   );
 };
+
+const styles = mergeStyleSets({
+  callout: {
+    width: 320,
+    padding: '20px 24px',
+  },
+  title: {
+    marginBottom: 12,
+    fontWeight: FontWeights.semilight,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: 20,
+  },
+});
