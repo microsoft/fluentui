ef8231d0ae83ecb9e3d68783c430e7718b870347
Elizabeth Craig <elcraig@microsoft.com>
2020-11-23 17:08:31 -0800

    Add re-exports for date-time-utilities

---
+++ packages/office-ui-fabric-react/src/DateTimeUtilities.ts
@@ -0,0 +1,5 @@
+// NOTE: This is not a full re-export because date-time-utilities includes some additional stuff
+// which is exported elsewhere, causes conflicts, or isn't needed.
+export * from '@fluentui/date-time-utilities/lib/dateMath/dateMath';
+export * from '@fluentui/date-time-utilities/lib/dateValues/dateValues';
+export * from '@fluentui/date-time-utilities/lib/dateValues/timeConstants';
