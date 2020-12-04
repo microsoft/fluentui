763354dcad49dd243dbcccf4194499b45573af65
Elizabeth Craig <elcraig@microsoft.com>
2020-12-03 18:46:52 -0800

    add example

--- packages/react-examples/src/react/SpinButton/SpinButton.doc.tsx
+++ packages/react-examples/src/react/SpinButton/SpinButton.doc.tsx
@@ -7,12 +7,14 @@ import { SpinButtonCustomStyledExample } from './SpinButton.CustomStyled.Example
 import { SpinButtonIconExample } from './SpinButton.Icon.Example';
 import { SpinButtonSuffixExample } from './SpinButton.Suffix.Example';
 import { SpinButtonTopPositionExample } from './SpinButton.TopPosition.Example';
+import { SpinButtonControlledExample } from './SpinButton.Controlled.Example';
 
 const SpinButtonBasicExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.Basic.Example.tsx') as string;
 const SpinButtonCustomStyledExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.CustomStyled.Example.tsx') as string;
 const SpinButtonIconExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.Icon.Example.tsx') as string;
 const SpinButtonSuffixExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.Suffix.Example.tsx') as string;
 const SpinButtonTopPositionExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.TopPosition.Example.tsx') as string;
+const SpinButtonControlledExampleCode = require('!raw-loader!@fluentui/react-examples/src/react/SpinButton/SpinButton.Controlled.Example.tsx') as string;
 
 export const SpinButtonPageProps: IDocPageProps = {
   title: 'SpinButton',
@@ -35,7 +37,7 @@ export const SpinButtonPageProps: IDocPageProps = {
       view: <SpinButtonTopPositionExample />,
     },
     {
-      title: 'SpinButton with suffix',
+      title: 'SpinButton with suffix and custom handlers',
       code: SpinButtonSuffixExampleCode,
       view: <SpinButtonSuffixExample />,
     },
@@ -43,6 +45,11 @@ export const SpinButtonPageProps: IDocPageProps = {
       title: 'Custom styled SpinButton',
       code: SpinButtonCustomStyledExampleCode,
       view: <SpinButtonCustomStyledExample />,
+    },
+    {
+      title: 'Controlled SpinButton',
+      code: SpinButtonControlledExampleCode,
+      view: <SpinButtonControlledExample />,
     },
   ],
   overview: require<string>('!raw-loader!@fluentui/react-examples/src/react/SpinButton/docs/SpinButtonOverview.md'),
