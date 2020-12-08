08835118df0bfde7911abeac5957b21d6d7d62f5
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-12-08 20:31:43 +0100

    WIP

---
+++ packages/fluentui/e2e/tests/treeVirtualizedKeyboardNavigation-example.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { VirtualizedTreePrototype } from '@fluentui/react-northstar-prototypes';
+import { Loader } from '@fluentui/react-northstar';
+
+const treeVirtualizedKeyboardNavigation = () => (
+  <React.Suspense fallback={<Loader />}>
+    <VirtualizedTreePrototype />
+  </React.Suspense>
+);
+export default treeVirtualizedKeyboardNavigation;
