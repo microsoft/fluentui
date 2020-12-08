c92a63a3a73e73b26eb18150cd31167b7121de28
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-12-08 16:22:45 +0100

    remove prorotypes from @fluentui/docs

--- packages/fluentui/react-northstar-prototypes/src/index.ts
+++ packages/fluentui/react-northstar-prototypes/src/index.ts
@@ -1,57 +1,59 @@
-// prototypes
+import * as React from 'react';
 
-export * from './prototypes/alerts/BannerAlerts';
-
-export * from './prototypes/AsyncShorthand/index';
-
-export * from './prototypes/chatMessages/ChatMessageWithPopover';
-export * from './prototypes/chatMessages/ControlMessages';
-export * from './prototypes/chatMessages/ImportantAndMentionMessages';
-export * from './prototypes/chatMessages/ThreadedMessages';
-
-export * from './prototypes/chatPane/index';
-
-export * from './prototypes/CopyToClipboard/index';
-
-export * from './prototypes/customScrollbar/index';
-
-export * from './prototypes/customToolbar/index';
-
-export * from './prototypes/dropdowns/index';
-
-export * from './prototypes/EditorToolbar/index';
-
-export * from './prototypes/employeeCard/index';
-
-export * from './prototypes/FormValidation/FormHooks';
-export * from './prototypes/FormValidation/Formik';
-
-export * from './prototypes/hexagonalAvatar/index';
-
-export * from './prototypes/meetingOptions/index';
-
-export * from './prototypes/mentions/MentionsDropdown';
-export * from './prototypes/mentions/MentionsEditor';
-
-export { MenuButton } from './prototypes/menuList/MenuButton';
-
-export * from './prototypes/NestedPopupsAndDialogs/index';
-
-export * from './prototypes/ParticipantsList/index';
-
-export * from './prototypes/popups/index';
-
-export * from './prototypes/SearchPage/index';
-
-export * from './prototypes/table/AdvancedTable';
-export * from './prototypes/table/InteractiveTable';
-export * from './prototypes/table/ResponsiveTableContainer';
-export * from './prototypes/table/SelectableTable';
-
-export * from './prototypes/TextAreaAutoSize/index';
-
-export * from './prototypes/VirtualizedTable/VirtualizedTable';
-export * from './prototypes/VirtualizedTable/VirtualizedTables';
-
-export * from './prototypes/VirtualizedTree/VirtualTree';
-export * from './prototypes/VirtualizedTree/VirtualTreePrototype';
+export const CustomToolbarPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/customToolbar'),
+);
+export const ChatPanePrototype = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/chatPane'));
+export const ChatMessagesPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/chatMessages'),
+);
+export const AsyncShorthandPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/AsyncShorthand'),
+);
+export const EmployeeCardPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/employeeCard'),
+);
+export const MeetingOptionsPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/meetingOptions'),
+);
+export const SearchPagePrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/SearchPage'),
+);
+export const MentionsPrototype = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/mentions'));
+export const DropdownsPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/dropdowns'),
+);
+export const PopupsPrototype = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/popups'));
+export const AlertsPrototype = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/alerts'));
+export const NestedPopupsAndDialogsPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/NestedPopupsAndDialogs'),
+);
+export const VirtualizedTreePrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/VirtualizedTree'),
+);
+export const CopyToClipboardPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/CopyToClipboard'),
+);
+export const ParticipantsListPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/ParticipantsList'),
+);
+export const CustomScrollbarPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/customScrollbar'),
+);
+export const EditorToolbarPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/EditorToolbar'),
+);
+export const HexagonalAvatarPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/hexagonalAvatar'),
+);
+export const TablePrototype = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/table'));
+export const VirtualizedTablePrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/VirtualizedTable'),
+);
+export const FormValidationPrototype = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/FormValidation'),
+);
+export const TextAreaAutoSize = React.lazy(() =>
+  import(/* webpackChunkName: "prototypes" */ './prototypes/TextAreaAutoSize'),
+);
+export const MenuList = React.lazy(() => import(/* webpackChunkName: "prototypes" */ './prototypes/MenuList'));
