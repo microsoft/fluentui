1b494c62eb2bcfcdbacd98b9d33791f1e5383632
YuanboXue-Amber <yuanboxue@microsoft.com>
2020-12-09 11:36:41 +0100

    remove pressKey wip

--- packages/fluentui/e2e/e2eApi.ts
+++ packages/fluentui/e2e/e2eApi.ts
@@ -138,10 +138,7 @@ export class E2EApi {
     modifier?: 'Control' | 'Shift',
   ): Promise<void> => {
     await this.getElement(selector);
-    await this.pressKey(key, modifier);
-  };
 
-  public pressKey = async (key: E2EKeys, modifier?: 'Control' | 'Shift'): Promise<void> => {
     if (modifier) {
       await this.page.keyboard.down(modifier);
     }
