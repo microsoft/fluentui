ac1f257bc4b9b55c471e2bbda2a577ced5deb3d7
Juraj Kapsiar <jukapsia@microsoft.com>
2020-12-11 15:59:43 +0100

    fix grid template in checkbox

--- packages/fluentui/docs/src/examples/components/Checkbox/Visual/index.tsx
+++ packages/fluentui/docs/src/examples/components/Checkbox/Visual/index.tsx
@@ -3,10 +3,10 @@ import * as React from 'react';
 import ComponentExample from '../../../../components/ComponentDoc/ComponentExample';
 import NonPublicSection from '../../../../components/ComponentDoc/NonPublicSection';
 
-const Types = () => (
+const Visual = () => (
   <NonPublicSection title="Visual tests">
     <ComponentExample examplePath="components/Checkbox/Visual/CheckboxExampleLabelFlexColumn" />
   </NonPublicSection>
 );
 
-export default Types;
+export default Visual;
