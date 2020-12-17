88823ea9fa1a5c076ace43952ac5d45e129a793c
Lingfan Gao <lingfan.gao@microsoft.com>
2020-12-17 10:07:48 +0100

    fix jest config

--- packages/fluentui/styles/jest.config.js
+++ packages/fluentui/styles/jest.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  ...require('@uifabric/build/jest'),
+const commonConfig = require('@uifabric/build/jest');
+
+const config = commonConfig({
   name: 'styles',
   moduleNameMapper: require('lerna-alias').jest({
     directory: require('@uifabric/build/monorepo/findGitRoot')(),
   }),
-};
+});
+
+module.exports = config;
