89e84145eda2c754e2aa2b170437b166451ec674
Milan Turon <mituron@microsoft.com>
2020-12-30 12:10:05 +0100

    Revert "saving branch in development still"
    This reverts commit 9a8f5c61bd0ee7951a8cb6930937fb8c55531003.

--- packages/fluentui/accessibility/src/behaviors/Input/inputBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Input/inputBehavior.ts
@@ -8,7 +8,7 @@ import { keyboardKey } from '@fluentui/keyboard-key';
  */
 export const inputBehavior: Accessibility<InputBehaviorProps> = props => ({
   attributes: {
-    input: {
+    root: {
       'aria-disabled': props.disabled,
       ...(props.required && { 'aria-required': true }),
       ...(props.error && { 'aria-invalid': true }),
