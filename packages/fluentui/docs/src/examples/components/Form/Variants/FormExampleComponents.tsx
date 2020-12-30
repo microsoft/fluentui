4e238ec7bd59c9137d6b8344147cc5bdb75734ba
Milan Turon <mituron@microsoft.com>
2020-12-30 12:10:05 +0100

    Revert "saving branch in development still"
    This reverts commit 9a8f5c61bd0ee7951a8cb6930937fb8c55531003.

--- packages/fluentui/docs/src/examples/components/Form/Variants/FormExampleComponents.tsx
+++ packages/fluentui/docs/src/examples/components/Form/Variants/FormExampleComponents.tsx
@@ -1,4 +1,3 @@
-import { useBooleanKnob } from '@fluentui/docs-components';
 import * as React from 'react';
 import { Form } from '@fluentui/react-northstar';
 
@@ -24,54 +23,16 @@ const items = [
   },
 ];
 
-const FormExampleComponents = () => {
-  const [visible1, setVisible1] = useBooleanKnob({ name: 'visible1', initialValue: false });
-  const [visible2, setVisible2] = useBooleanKnob({ name: 'visible2', initialValue: false });
-
-  const setError1 = e => {
-    if (e.keyCode === 65) {
-      setVisible1(true);
-      return;
-    }
-    setVisible1(false);
-  };
-
-  const setError2 = e => {
-    if (e.keyCode === 65) {
-      setVisible2(true);
-      return;
-    }
-    setVisible2(false);
-  };
-
-  return (
-    <Form>
-      <Form.Input
-        onKeyDown={e => setError1(e)}
-        label="First name"
-        name="firstName"
-        required
-        errorMessage={visible1 && 'input error message'}
-      />
-      <Form.Dropdown
-        label="City"
-        items={['prague', 'new york']}
-        errorMessage={visible2 && 'input error message'}
-        onKeyDown={e => setError2(e)}
-      />
-      <Form.RadioGroup
-        label="Pizza"
-        errorMessage="error radioGroup"
-        vertical
-        defaultCheckedValue="prosciutto"
-        items={items}
-      />
-      <Form.Slider label="Bid the price" errorMessage="error slider" />
-      <Form.Checkbox errorMessage="error checkbox" label="I agree to the Terms and Conditions" />
-      <Form.Datepicker label="Select a date" errorMessage="You can not fix this error" />
-      <Form.Button content="Submit" />
-    </Form>
-  );
-};
+const FormExampleComponents = () => (
+  <Form>
+    <Form.Input label="First name" name="firstName" required />
+    <Form.Dropdown label="City" items={['prague', 'new york']} />
+    <Form.RadioGroup label="Pizza" vertical defaultCheckedValue="prosciutto" items={items} />
+    <Form.Slider label="Bid the price" />
+    <Form.Checkbox label="I agree to the Terms and Conditions" />
+    <Form.Datepicker label="Select a date" />
+    <Form.Button content="Submit" />
+  </Form>
+);
 
 export default FormExampleComponents;
