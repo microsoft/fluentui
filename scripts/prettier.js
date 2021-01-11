e0712596a21ac5f4d38c946221c356e76c005019
Martin Hochel <martinhochel@microsoft.com>
2021-01-11 14:16:11 +0100

    chore(scripts): improve prettier DX

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -6,47 +6,20 @@ const { runPrettier, runPrettierForFolder, prettierExtensions } = require('./pre
 const { default: PQueue } = require('p-queue');
 const findGitRoot = require('./monorepo/findGitRoot');
 
-const runOnAllFiles = require('yargs').argv.all;
-
+const parsedArgs = parseArgs();
 const numberOfCpus = os.cpus().length / 2;
-console.log(`Running prettier on ${runOnAllFiles ? 'all' : 'changed'} files (on ${numberOfCpus} processes):`);
 
-async function runPrettierForAll() {
+async function main() {
+  const runOnAllFiles = parsedArgs.all;
+
+  console.log(`Running prettier on ${runOnAllFiles ? 'all' : 'changed'} files (on ${numberOfCpus} processes):`);
+
   const queue = new PQueue({ concurrency: numberOfCpus });
+
   if (runOnAllFiles) {
-    // Run on groups of files so that the operations can run in parallel
-    const root = findGitRoot();
-    await queue.add(() => runPrettierForFolder(root, true, true));
-    await queue.addAll(
-      ['apps', 'packages/!(fluentui)', 'packages/fluentui', '{.*,scripts,typings}'].map(name => () =>
-        runPrettierForFolder(name),
-      ),
-    );
+    runOnAll(queue);
   } else {
-    const prettierIntroductionCommit = 'HEAD~1';
-    const passedDiffTarget = process.argv.slice(2).length ? process.argv.slice(2)[0] : prettierIntroductionCommit;
-
-    const projectPath = path.resolve(__dirname, '..');
-    const cmd = `git --no-pager diff ${passedDiffTarget} --diff-filter=AM --name-only --stat-name-width=0`;
-
-    const gitDiffOutput = execSync(cmd, { cwd: projectPath });
-    const prettierExtRegex = new RegExp(`\\.(${prettierExtensions.join('|')})$`);
-    const files = gitDiffOutput
-      .toString('utf8')
-      .split(os.EOL)
-      .filter(fileName => prettierExtRegex.test(fileName));
-
-    const fileGroups = [];
-    for (let chunkStart = 0; chunkStart < files.length; chunkStart += numberOfCpus) {
-      fileGroups.push(files.slice(chunkStart, chunkStart + numberOfCpus));
-    }
-
-    await queue.addAll(
-      fileGroups.map(group => () => {
-        console.log(`Running for ${group.length} files!`);
-        runPrettier(group, true /*async*/);
-      }),
-    );
+    runOnChanged(queue);
   }
 
   await queue.onEmpty().catch(error => {
@@ -55,4 +28,67 @@ async function runPrettierForAll() {
   });
 }
 
-runPrettierForAll();
+function parseArgs() {
+  return require('yargs')
+    .usage('Usage: prettier [commitHash] [options]')
+    .example('prettier', 'run prettier only on changed files')
+    .example('prettier HEAD~3', 'run prettier only on changed files since HEAD~3')
+    .options({
+      all: {
+        description: 'Run prettier on all projects',
+        boolean: true,
+      },
+    })
+    .alias('h', 'help').argv;
+}
+
+/**
+ *
+ * @param {PQueue} queue
+ */
+async function runOnChanged(queue) {
+  const prettierIntroductionCommit = 'HEAD~1';
+  const passedDiffTarget = parsedArgs._.length ? parsedArgs._[0] : prettierIntroductionCommit;
+
+  const projectRootPath = path.resolve(__dirname, '..');
+  const cmd = `git --no-pager diff ${passedDiffTarget} --diff-filter=AM --name-only --stat-name-width=0`;
+
+  const gitDiffOutput = execSync(cmd, { cwd: projectRootPath });
+  const prettierExtRegex = new RegExp(`\\.(${prettierExtensions.join('|')})$`);
+  const files = gitDiffOutput
+    .toString('utf8')
+    .split(os.EOL)
+    .filter(fileName => prettierExtRegex.test(fileName));
+
+  const fileGroups = [];
+  for (let chunkStart = 0; chunkStart < files.length; chunkStart += numberOfCpus) {
+    fileGroups.push(files.slice(chunkStart, chunkStart + numberOfCpus));
+  }
+
+  console.log(files);
+  process.exit(1);
+
+  await queue.addAll(
+    fileGroups.map(group => () => {
+      console.log(`Running for ${group.length} files!`);
+      runPrettier(group, true /*async*/);
+    }),
+  );
+}
+
+/**
+ *
+ * @param {PQueue} queue
+ */
+async function runOnAll(queue) {
+  // Run on groups of files so that the operations can run in parallel
+  const root = findGitRoot();
+  await queue.add(() => runPrettierForFolder(root, true, true));
+  await queue.addAll(
+    ['apps', 'packages/!(fluentui)', 'packages/fluentui', '{.*,scripts,typings}'].map(name => () =>
+      runPrettierForFolder(name),
+    ),
+  );
+}
+
+main();
