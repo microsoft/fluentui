7d8e4b68fe2107d1894d04960aa1ca7e54b8092d
KHMakoto <humbertomakotomorimoto@gmail.com>
2021-01-12 09:52:17 -0800

    Fixing ssr-test failure.

--- packages/make-styles/src/constants.ts
+++ packages/make-styles/src/constants.ts
@@ -1,7 +1,12 @@
 import { getWindow } from '@fluentui/utilities';
 import { MakeStylesLookupEntry } from './types';
 
-export const CAN_USE_CSS_VARIABLES = (window || getWindow()).CSS && CSS.supports('color', 'var(--c)');
+let window;
+if (window === undefined) {
+  window = getWindow() as Window & typeof globalThis;
+}
+
+export const CAN_USE_CSS_VARIABLES = window ? window.CSS && CSS.supports('color', 'var(--c)') : undefined;
 
 export const HASH_PREFIX = 'f';
 export const RTL_PREFIX = 'r';
