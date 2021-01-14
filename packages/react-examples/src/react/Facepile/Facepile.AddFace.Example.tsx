e59281517b19d5a36799316393e35447dee017a2
André Dias <andredias@microsoft.com>
2021-01-14 18:27:32 +0100

    fix(react-examples): Improved information for screen readers (microsoftdesign/fluentui#10254) Added Announced for the adding person action Changed button label to improve clarity

--- packages/react-examples/src/react/Facepile/Facepile.AddFace.Example.tsx
+++ packages/react-examples/src/react/Facepile/Facepile.AddFace.Example.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Facepile, OverflowButtonType } from '@fluentui/react/lib/Facepile';
 import { facepilePersonas } from '@fluentui/example-data';
+import { Announced } from '@fluentui/react';
 
 const overflowButtonProps = {
   ariaLabel: 'More users',
@@ -9,26 +10,36 @@ const overflowButtonProps = {
 
 export const FacepileAddFaceExample: React.FunctionComponent = () => {
   const [numberOfFaces, setNumberOfFaces] = React.useState(0);
+  const [addFaceAnnounced, setAddFaceAnnounced] = React.useState<JSX.Element | undefined>();
 
   const personas = React.useMemo(() => facepilePersonas.slice(0, numberOfFaces), [numberOfFaces]);
 
   const addButtonProps = React.useMemo(
     () => ({
-      ariaLabel: 'Add a new person',
-      onClick: (ev: React.MouseEvent<HTMLButtonElement>) => setNumberOfFaces(numberOfFaces + 1),
+      ariaLabel: 'Add a new person to the Facepile',
+      onClick: (ev: React.MouseEvent<HTMLButtonElement>) => {
+        const currentFaces = numberOfFaces + 1;
+        setNumberOfFaces(currentFaces);
+        setAddFaceAnnounced(
+          <Announced message={`New person added, currently ${currentFaces} in the Facepile.`} aria-live="assertive" />,
+        );
+      },
     }),
     [numberOfFaces],
   );
 
   return (
-    <Facepile
-      personas={personas}
-      maxDisplayablePersonas={5}
-      overflowButtonProps={overflowButtonProps}
-      overflowButtonType={OverflowButtonType.descriptive}
-      showAddButton
-      addButtonProps={addButtonProps}
-      ariaDescription="To move through the items use left and right arrow keys."
-    />
+    <>
+      {addFaceAnnounced}
+      <Facepile
+        personas={personas}
+        maxDisplayablePersonas={5}
+        overflowButtonProps={overflowButtonProps}
+        overflowButtonType={OverflowButtonType.descriptive}
+        showAddButton
+        addButtonProps={addButtonProps}
+        ariaDescription="To move through the items use left and right arrow keys."
+      />
+    </>
   );
 };
