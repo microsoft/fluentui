c45c1759f7152d8556abde4ba8709df807c636c7
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-01-14 06:37:38 -0300

    chore(Form): fix inline

--- packages/fluentui/docs/src/examples/components/Form/Types/FormExampleInline.tsx
+++ packages/fluentui/docs/src/examples/components/Form/Types/FormExampleInline.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Form } from '@fluentui/react-northstar';
+import { Form, Input } from '@fluentui/react-northstar';
 
 const FormExample = () => (
   <Form
@@ -7,15 +7,29 @@ const FormExample = () => (
       alert('Form submitted');
     }}
   >
-    <Form.Input
+    {/* TODO: Remove these Form.Field as soon inline is fixed in Form.Input */}
+    <Form.Field
       label="First name"
       name="firstName"
       id="first-name-inline"
       inline
       required
-      showSuccessIndicator={false}
+      control={{
+        as: Input,
+        showSuccessIndicator: false,
+      }}
     />
-    <Form.Input label="Last name" name="lastName" id="last-name-inline" inline required showSuccessIndicator={false} />
+    <Form.Field
+      label="Last name"
+      name="lastName"
+      id="last-name-inline"
+      inline
+      required
+      control={{
+        as: Input,
+        showSuccessIndicator: false,
+      }}
+    />
     <Form.Checkbox label="I agree to the Terms and Conditions" id="conditions-inline" />
     <Form.Button content="Submit" />
   </Form>
