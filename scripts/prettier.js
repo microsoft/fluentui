293ccd438449fe6cf99b176b5e3a141cbe62ed60
Martin Hochel <martinhochel@microsoft.com>
2021-01-14 13:52:23 +0100

    chore(scripts): add --check support for root prettier task (#16446)

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -13,9 +13,13 @@ const numberOfCpus = os.cpus().length / 2;
  */
 
 async function main() {
-  const runOnAllFiles = parsedArgs.all;
+  const { all: runOnAllFiles, check: checkMode } = parsedArgs;
 
-  console.log(`Running prettier on ${runOnAllFiles ? 'all' : 'changed'} files (on ${numberOfCpus} processes):`);
+  console.log(
+    `Running prettier on ${runOnAllFiles ? 'all' : 'changed'} files (on ${numberOfCpus} processes | in ${
+      checkMode ? 'check' : 'write'
+    } mode)`,
+  );
 
   /**
    * @type {Paths}
@@ -44,6 +48,10 @@ function parseArgs() {
     .options({
       all: {
         description: 'Run prettier on all files',
+        boolean: true,
+      },
+      check: {
+        description: 'Run prettier in check mode. useful for CI',
         boolean: true,
       },
     })
@@ -76,7 +84,7 @@ async function runOnChanged(options) {
   await queue.addAll(
     fileGroups.map(group => () => {
       console.log(`Running for ${group.length} files!`);
-      runPrettier(group, true /*async*/);
+      runPrettier(group, { runAsync: true, check: parsedArgs.check });
     }),
   );
 }
@@ -89,10 +97,12 @@ async function runOnAll(options) {
   const { queue, paths } = options;
 
   // Run on groups of files so that the operations can run in parallel
-  await queue.add(() => runPrettierForFolder(paths.root, true, true));
+  await queue.add(() =>
+    runPrettierForFolder(paths.root, { runAsync: true, nonRecursive: true, check: parsedArgs.check }),
+  );
   await queue.addAll(
     ['apps', 'packages/!(fluentui)', 'packages/fluentui', '{.*,scripts,typings}'].map(name => () =>
-      runPrettierForFolder(name),
+      runPrettierForFolder(name, { check: parsedArgs.check }),
     ),
   );
 }
