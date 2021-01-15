f7b01aa2e913733c602007fcfdd056fb03ede11b
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-01-15 09:08:15 -0300

    fix(CarouselPaddles): fix type

--- packages/fluentui/react-northstar/src/components/Carousel/CarouselPaddlesContainer.tsx
+++ packages/fluentui/react-northstar/src/components/Carousel/CarouselPaddlesContainer.tsx
@@ -29,7 +29,7 @@ export const carouselPaddlesContainerClassName = 'ui-carrouselpaddles_container'
 /**
  * A CarouselPaddlesContainer is a container for the Carousel Paddles.
  */
-export const CarouselPaddlesContainer: ComponentWithAs<'span', CarouselPaddlesContainerProps> &
+export const CarouselPaddlesContainer: ComponentWithAs<'div', CarouselPaddlesContainerProps> &
   FluentComponentStaticProps<CarouselPaddlesContainerProps> = props => {
   const context = useFluentContext();
   const { setStart, setEnd } = useTelemetry(CarouselPaddlesContainer.displayName, context.telemetry);
