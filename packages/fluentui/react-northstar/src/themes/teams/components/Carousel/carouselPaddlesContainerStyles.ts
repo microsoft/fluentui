a349979d5033f22262f69ffb587af879b83e1d82
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-01-15 09:51:37 -0300

    fix(CarouselPaddles): add variables

--- packages/fluentui/react-northstar/src/themes/teams/components/Carousel/carouselPaddlesContainerStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Carousel/carouselPaddlesContainerStyles.ts
@@ -1,13 +1,15 @@
 import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
 import { CarouselVariables } from './carouselVariables';
 import { CarouselPaddlesContainerStylesProps } from '../../../../components/Carousel/CarouselPaddlesContainer';
+import { pxToRem } from '../../../../utils';
 
 export const carouselPaddlesContainerStyles: ComponentSlotStylesPrepared<
   CarouselPaddlesContainerStylesProps,
   CarouselVariables
 > = {
-  root: (): ICSSInJSStyle => ({
+  root: ({ variables: v }): ICSSInJSStyle => ({
     display: 'flex',
+    width: pxToRem(v.width),
     justifyContent: 'space-between',
     alignItems: 'center',
     position: 'absolute',
