3fa9177530f439d44b3a55c8b12d0f41f111775a
Martin Hochel <martinhochel@microsoft.com>
2021-01-19 13:11:20 +0100

    chore(scripts): add support to standard jestTask arguments when executing our custom jest scripts

--- scripts/tasks/jest.ts
+++ scripts/tasks/jest.ts
@@ -1,10 +1,24 @@
-import { jestTask, argv } from 'just-scripts';
+import { jestTask, argv, JestTaskOptions } from 'just-scripts';
 import * as path from 'path';
 
-const commonArgs = () => {
+/**
+ * Partial support of native jest CLI arguments https://jestjs.io/docs/en/cli
+ * > Why partial support only? just jestTask limits support to only those specified as return value of this function
+ */
+const commonArgs = (): JestTaskOptions => {
   return {
-    ...((process.env.TF_BUILD || process.env.LAGE_PACKAGE_NAME) && { runInBand: true }),
-    ...(argv().u || argv().updateSnapshot ? { updateSnapshot: true } : undefined),
+    ...((process.env.TF_BUILD || process.env.LAGE_PACKAGE_NAME || argv().runInBand) && { runInBand: true }),
+    ...((argv().u || argv().updateSnapshot) && { updateSnapshot: true }),
+    clearCache: argv().clearCache,
+    config: argv().config,
+    watch: argv().watch,
+    coverage: argv().coverage,
+    passWithNoTests: argv().coverage,
+    testNamePattern: argv().testNamePattern,
+    testPathPattern: argv().testPathPattern,
+
+    // Just specific config
+    nodeArgs: argv().nodeArgs,
   };
 };
 
@@ -28,7 +42,7 @@ export const jestWatch = () => {
   return jestTask({
     ...commonArgs(),
     watch: true,
-    _: ['-i', ...(argv()._ || []).filter(arg => arg !== 'jest-watch')],
+    _: [...(argv()._ || []).filter(arg => arg !== 'jest-watch')],
     env: {
       ...process.env,
       PACKAGE_NAME: argv().package,
