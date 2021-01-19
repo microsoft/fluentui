beefd488994ffd6734acbf0b0263fdb1150c9e15
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2021-01-19 12:42:22 +0100

    Update packages/fluentui/e2e/tests/menuDismissOnItemClick-test.ts

--- packages/fluentui/e2e/tests/menuDismissOnItemClick-test.ts
+++ packages/fluentui/e2e/tests/menuDismissOnItemClick-test.ts
@@ -47,9 +47,11 @@ describe('Dismiss Menu on Item Click', () => {
   describe('Keyboard interactions', () => {
     it('Should close on click', async () => {
       await e2e.expectHidden(firstSubmenu);
+
       await e2e.focusOn(menuItem);
       await e2e.waitForSelectorAndPressKey(menuItem, 'Enter');
       await e2e.exists(firstSubmenu);
+
       await e2e.waitForSelectorAndPressKey(firstSubmenuItem, 'Enter');
       await e2e.expectHidden(firstSubmenu);
     });
