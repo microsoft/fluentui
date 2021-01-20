3a4d1234f89ae3e7a66ca14c07b714017f81cf1f
Tomi Olubeko <ololubek@microsoft.com>
2021-01-20 09:43:46 -0800

    fix comments and naming

--- packages/react-icon-provider/src/IconProvider.tsx
+++ packages/react-icon-provider/src/IconProvider.tsx
@@ -3,18 +3,17 @@ import * as React from 'react';
 import { IconProviderProps } from './IconProvider.types';
 
 /**
- * Context for providing the window.
+ * Context for providing the IconSubset.
  */
 export const IconContext = React.createContext<IIconSubset | undefined>(undefined);
 
 /**
- * Hook to access the Icon object. This can be overridden contextually using the `IconProvider`.
+ * Hook to access the Icon subset. This can be overridden contextually using the `IconProvider`.
  */
 export const useIcon = (): IIconSubset | undefined => React.useContext(IconContext);
 
 /**
- * Component to provide the icon override object contextually. This is useful when rendering a different
- * Icon from the default svg Icon provided
+ * Component to contextually override one or more of the default SVG icons
  */
 export const IconProvider: React.FunctionComponent<IconProviderProps> = props => {
   return <IconContext.Provider value={props.icons}>{props.children}</IconContext.Provider>;
