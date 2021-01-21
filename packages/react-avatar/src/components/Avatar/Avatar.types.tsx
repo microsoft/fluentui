4f59950a47b7dc3280275ff5c4911130c1595c63
Ben Howell <behowell@microsoft.com>
2021-01-20 17:59:06 -0700

    Remove unused classes prop from AvatarProps

--- packages/react-avatar/src/components/Avatar/Avatar.types.tsx
+++ packages/react-avatar/src/components/Avatar/Avatar.types.tsx
@@ -55,9 +55,6 @@ export interface AvatarProps extends ComponentProps, React.HTMLAttributes<HTMLEl
   /** Custom method for generating the initials from the name property, which is shown if no image is provided. */
   getInitials?: (name: string, isRtl: boolean) => string;
 
-  /** Classes for the parts of the component */
-  classes?: { [key: string]: string };
-
   /** Style tokens */
   tokens?: AvatarTokenSet;
 }
