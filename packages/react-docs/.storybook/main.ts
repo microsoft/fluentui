f7854dde0eec71370d78716ab95e2ce390b1ca19
Miroslav Stastny <miroslav.stastny@microsoft.com>
2021-01-27 20:50:06 +0100

    Add sourcemaps for dependencies

--- packages/react-docs/.storybook/main.ts
+++ packages/react-docs/.storybook/main.ts
@@ -12,6 +12,13 @@ module.exports = {
 
   webpackFinal: async (config) => {
     // do mutation to the config
+
+    config.module.rules.push({
+      test: /\.js$/,
+      enforce: 'pre',
+      use: ['source-map-loader'],
+    });
+
     return config;
   },
 
