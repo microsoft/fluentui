67d350e7b3486f7013aac3793f811be544a6be39
Oleksandr Fediashov <olfedias@microsoft.com>
2021-01-27 12:46:07 +0100

    cleanup

--- packages/make-styles/src/ax.test.ts
+++ packages/make-styles/src/ax.test.ts
@@ -11,6 +11,12 @@ const options: MakeStylesOptions<{}> = {
 describe('ax', () => {
   it('handles non makeStyles classes', () => {
     expect(ax('ui-button')).toBe('ui-button');
+    expect(ax('ui-button', 'ui-button-content')).toBe('ui-button ui-button-content');
+  });
+
+  it('handles empty params', () => {
+    expect(ax('ui-button', undefined)).toBe('ui-button');
+    expect(ax(undefined, undefined)).toBe('');
   });
 
   it('performs deduplication for multiple arguments', () => {
@@ -24,15 +30,29 @@ describe('ax', () => {
     expect(ax(className1, className2, className3, className4)).toBe(resultClassName);
   });
 
-  xit('performs deduplication for RTL classes', () => {
-    const ltrClassName = makeStyles([[null, { borderLeft: '5px' }]])({}, options);
-    // property names are the same for flipped classes except the RTL prefix
-    const rtlClassName = makeStyles([[null, { borderLeft: '5px' }]])({}, { ...options, rtl: true });
+  it('order of classes is not important', () => {
+    const className = makeStyles([[null, { display: 'block' }]])({}, options);
 
-    expect(ax(ltrClassName, rtlClassName)).toBe(rtlClassName);
+    expect(ax('ui-button', className, 'ui-button-content')).toBe(`ui-button ui-button-content ${className}`);
   });
 
-  it('TODO MULTILEVEL', () => {
+  it('order of classes is not important for multilevel overrides', () => {
+    const className1 = ax('ui-button', makeStyles([[null, { display: 'block' }]])({}, options), 'ui-button-content');
+    const className2 = makeStyles([[null, { display: 'grid' }]])({}, options);
+
+    expect(ax(className1, className2)).toBe(`ui-button ui-button-content ${className2}`);
+  });
+
+  // TODO: consider a proper approach for this
+  // xit('performs deduplication for RTL classes', () => {
+  //   const ltrClassName = makeStyles([[null, { borderLeft: '5px' }]])({}, options);
+  //   // property names are the same for flipped classes except the RTL prefix
+  //   const rtlClassName = makeStyles([[null, { borderLeft: '5px' }]])({}, { ...options, rtl: true });
+  //
+  //   expect(ax(ltrClassName, rtlClassName)).toBe(rtlClassName);
+  // });
+
+  it('merges multi-level overrides properly', () => {
     const className1 = makeStyles([[null, { display: 'block' }]])({}, options);
     const className2 = makeStyles([[null, { display: 'flex' }]])({}, options);
 
@@ -50,13 +70,26 @@ describe('ax', () => {
     expect(sequence3).toBe('ui-button ui-flex __xzc3aa0 f13qh94s f1sbtcvk fwiuce90 fdghr900 f15vdbe4 f1rqyxcv');
   });
 
-  // xdescribe('unstable functionality', () => {
-  //   it('deduplicates classes with mixed priority', () => {
-  //     // Classnames with numeric suffix has increased specificity
-  //     const className1 = HASH_PREFIX + hashString('display' + 'flex') + '1';
-  //     const className2 = HASH_PREFIX + hashString('display' + 'grid') + '2';
-  //
-  //     expect(ax(className1, className2)).toBe(className2);
-  //   });
-  // });
+  it('warns if strings are not properly merged', () => {
+    const error = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
+
+    const className1 = makeStyles([[null, { display: 'block' }]])({}, options);
+    const className2 = makeStyles([[null, { display: 'flex' }]])({}, options);
+
+    ax(className1 + ' ' + className2);
+
+    expect(error).toHaveBeenCalledWith(
+      expect.stringMatching(/a passed string contains multiple identifiers of atomic classes/),
+    );
+  });
+
+  describe('unstable functionality', () => {
+    it('deduplicates classes with mixed priority', () => {
+      // Classnames with numeric suffix has increased specificity
+      const className1 = makeStyles([[null, { display: 'grid' }]])({}, options);
+      const className2 = makeStyles([[null, { display: 'flex' }]], 1)({}, options);
+
+      expect(ax(className1, className2)).toBe(className2);
+    });
+  });
 });
