43191591b71d7d16205d0ffda65f30eae9d0e360
Lingfan Gao <lingfan.gao@microsoft.com>
2021-01-29 16:32:26 +0000

    init lib

---
+++ packages/react-examples/src/react-menu/MenuList.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+import { MenuList } from '@fluentui/react-menu';
+
+export const MenuListExample = () => <MenuList />;
---
+++ packages/react-menu/src/MenuList.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const MenuList = () => {
+  return <div>Hello world!</div>;
+};
+
+MenuList.displayName = 'MenuList';
