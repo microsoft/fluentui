665bb8704b35af212267f569c25420e2b15bbd48
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-02-01 14:44:44 -0800

    allow passing native properties to the root element

--- packages/office-ui-fabric-react/src/components/Stack/StackItem/StackItem.types.ts
+++ packages/office-ui-fabric-react/src/components/Stack/StackItem/StackItem.types.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { IComponentStyles, IHTMLSlot, IComponent, ISlotProp, IStyleableComponentProps } from '../../../Foundation';
 
 /**
@@ -36,7 +37,8 @@ export type IStackItemStylesReturnType = ReturnType<Extract<IStackItemComponent[
  */
 export interface IStackItemProps
   extends IStackItemSlots,
-    IStyleableComponentProps<IStackItemProps, IStackItemTokens, IStackItemStyles> {
+    IStyleableComponentProps<IStackItemProps, IStackItemTokens, IStackItemStyles>,
+    React.HTMLAttributes<HTMLElement> {
   /**
    * Defines a CSS class name used to style the StackItem.
    */
