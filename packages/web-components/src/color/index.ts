e4079adb2ab4fb09f58c71192665ff0b81e5f5ec
Chris Holt <chhol@microsoft.com>
2021-02-02 14:47:52 -0800

    feat: add color recipes to web component packages (#16755)
    * feat: move color recipes into fluent web component package
    
    * Change files
    
    * udpate recipe paths
    
    * ensure tests and syntax are correct
    
    * add more typedoc comments
    
    * update FAST deps
    
    * add generated file to prettier ignore
    
    * Change files

---
+++ packages/web-components/src/color/index.ts
@@ -0,0 +1,109 @@
+/**
+ * Text exports
+ */
+export {
+  neutralForeground,
+  neutralForegroundRest,
+  neutralForegroundHover,
+  neutralForegroundActive,
+} from './neutral-foreground';
+
+export { neutralForegroundToggle, neutralForegroundToggleLarge } from './neutral-foreground-toggle';
+
+export { accentForegroundCut, accentForegroundCutLarge } from './accent-foreground-cut';
+
+export { neutralForegroundHint, neutralForegroundHintLarge } from './neutral-foreground-hint';
+
+export {
+  accentForeground,
+  accentForegroundRest,
+  accentForegroundHover,
+  accentForegroundActive,
+  accentForegroundLarge,
+  accentForegroundLargeRest,
+  accentForegroundLargeHover,
+  accentForegroundLargeActive,
+} from './accent-foreground';
+
+/**
+ * Fill exports
+ */
+export { neutralFill, neutralFillRest, neutralFillHover, neutralFillActive, neutralFillSelected } from './neutral-fill';
+
+export {
+  neutralFillStealth,
+  neutralFillStealthRest,
+  neutralFillStealthHover,
+  neutralFillStealthActive,
+  neutralFillStealthSelected,
+} from './neutral-fill-stealth';
+
+export {
+  neutralFillToggle,
+  neutralFillToggleRest,
+  neutralFillToggleHover,
+  neutralFillToggleActive,
+} from './neutral-fill-toggle';
+
+export {
+  neutralFillInput,
+  neutralFillInputRest,
+  neutralFillInputHover,
+  neutralFillInputActive,
+  neutralFillInputSelected,
+} from './neutral-fill-input';
+
+export {
+  accentFill,
+  accentFillRest,
+  accentFillHover,
+  accentFillActive,
+  accentFillSelected,
+  accentFillLarge,
+  accentFillLargeRest,
+  accentFillLargeHover,
+  accentFillLargeActive,
+  accentFillLargeSelected,
+} from './accent-fill';
+
+export { neutralFillCard } from './neutral-fill-card';
+
+/**
+ * Border exports
+ */
+export {
+  neutralOutlineContrast,
+  neutralOutlineContrastRest,
+  neutralOutlineContrastHover,
+  neutralOutlineContrastActive,
+} from './neutral-outline-contrast';
+
+export { neutralOutline, neutralOutlineRest, neutralOutlineHover, neutralOutlineActive } from './neutral-outline';
+
+export { neutralDividerRest } from './neutral-divider';
+
+/**
+ * App layer exports
+ */
+export {
+  neutralLayerFloating,
+  neutralLayerCard,
+  neutralLayerCardContainer,
+  neutralLayerL1,
+  neutralLayerL1Alt,
+  neutralLayerL2,
+  neutralLayerL3,
+  neutralLayerL4,
+  StandardLuminance,
+} from './neutral-layer';
+
+/**
+ * Focus colors
+ */
+export { neutralFocus, neutralFocusInnerAccent } from './neutral-focus';
+
+/**
+ * Export supporting types
+ */
+export { isDarkMode, palette, PaletteType, Palette } from './palette';
+export { createColorPalette } from './create-color-palette';
