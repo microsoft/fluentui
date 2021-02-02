31ba28c988e311f47327e5f2f6b6efcddccf12c1
Elizabeth Craig <elcraig@microsoft.com>
2021-02-01 22:58:43 -0800

    Move related links to arrays

--- apps/public-docsite/src/pages/Controls/SpinnerPage/SpinnerPage.doc.ts
+++ apps/public-docsite/src/pages/Controls/SpinnerPage/SpinnerPage.doc.ts
@@ -1,7 +1,12 @@
 import { TFabricPlatformPageProps } from '../../../interfaces/Platforms';
 import { SpinnerPageProps as ExternalProps } from '@fluentui/react-examples/lib/react/Spinner/Spinner.doc';
+import { ISideRailLink } from '@fluentui/react-docsite-components/lib/index2';
 
-const related = require('!raw-loader?esModule=false!@fluentui/public-docsite/src/pages/Controls/SpinnerPage/docs/SpinnerRelated.md') as string;
+const related: ISideRailLink[] = [
+  { text: 'Web Spinner', url: '#/controls/web/spinner' },
+  { text: 'iOS Spinner', url: '#/controls/ios/spinner' },
+  { text: 'Android Spinner', url: '#/controls/android/spinner' },
+];
 const componentUrl =
   'https://github.com/microsoft/fluentui/tree/master/apps/public-docsite/src/pages/Controls/SpinnerPage';
 
