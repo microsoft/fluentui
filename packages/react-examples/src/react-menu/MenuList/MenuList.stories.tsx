af7e7af345330f0d965dd2493b350f7348c4307e
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-03 21:46:55 +0000

    add menu stories

--- packages/react-examples/src/react-menu/MenuList/MenuList.stories.tsx
+++ packages/react-examples/src/react-menu/MenuList/MenuList.stories.tsx
@@ -1,43 +1,79 @@
 import * as React from 'react';
 
-import { MenuList, MenuItem, MenuGroup, MenuGroupHeader, MenuItemCheckbox, MenuItemRadio } from '@fluentui/react-menu';
+import {
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuGroupHeader,
+  MenuItemCheckbox,
+  MenuItemRadio,
+  MenuGroupDivider,
+} from '@fluentui/react-menu';
 import { teamsLightTheme } from '@fluentui/react-theme';
 import { FluentProvider } from '@fluentui/react-provider';
 import { CutIcon, PasteIcon, EditIcon, AcceptIcon } from '@fluentui/react-icons-mdl2';
+import { makeStyles } from '@fluentui/react-make-styles';
+
+const useContainerStyles = makeStyles([
+  // This should eventually be the popup container styles
+  [
+    null,
+    theme => ({
+      backgroundColor: theme.neutralColorTokens.neutralBackground1,
+      minWidth: '128px',
+      minHeight: '48px',
+      maxWidth: '128px',
+      boxShadow: `${theme.shadowLevels?.shadow16.ambient}, ${theme.shadowLevels?.shadow16.key}`,
+      paddingTop: '4px',
+      paddingBottom: '4px',
+    }),
+  ],
+]);
+const Container = (props: { children: React.ReactNode }) => {
+  const classNames = useContainerStyles({});
+  return <div className={classNames}>{props.children}</div>;
+};
 
 export const MenuListExample = () => (
   <FluentProvider theme={teamsLightTheme}>
-    <MenuList>
-      <MenuItem icon={<CutIcon />}>Item</MenuItem>
-      <MenuItem icon={<PasteIcon />}>Item</MenuItem>
-      <MenuItem icon={<EditIcon />}>Item</MenuItem>
-    </MenuList>
+    <Container>
+      <MenuList>
+        <MenuItem icon={<CutIcon />}>Item</MenuItem>
+        <MenuItem icon={<PasteIcon />}>Item</MenuItem>
+        <MenuItem icon={<EditIcon />}>Item</MenuItem>
+      </MenuList>
+    </Container>
   </FluentProvider>
 );
 
 export const MenuListWithIconsExample = () => (
   <FluentProvider theme={teamsLightTheme}>
-    <MenuList>
-      <MenuItem icon="X">Item</MenuItem>
-      <MenuItem icon="X">Item</MenuItem>
-      <MenuItem icon="X">Item</MenuItem>
-    </MenuList>
+    <Container>
+      <MenuList>
+        <MenuItem icon={<CutIcon />}>Item</MenuItem>
+        <MenuItem icon={<PasteIcon />}>Item</MenuItem>
+        <MenuItem icon={<EditIcon />}>Item</MenuItem>
+      </MenuList>
+    </Container>
   </FluentProvider>
 );
 
 export const MenuListWithGroupExample = () => (
   <FluentProvider theme={teamsLightTheme}>
-    <MenuList>
-      <MenuItem icon="X">Item</MenuItem>
-      <MenuItem icon="X">Item</MenuItem>
-      <MenuItem icon="X">Item</MenuItem>
-      <MenuGroup>
-        <MenuGroupHeader>Header</MenuGroupHeader>
-        <MenuItem icon="X">Item</MenuItem>
-        <MenuItem icon="X">Item</MenuItem>
-        <MenuItem icon="X">Item</MenuItem>
-      </MenuGroup>
-    </MenuList>
+    <Container>
+      <MenuList>
+        <MenuItem icon={<CutIcon />}>Item</MenuItem>
+        <MenuItem icon={<PasteIcon />}>Item</MenuItem>
+        <MenuItem icon={<EditIcon />}>Item</MenuItem>
+        <MenuGroupDivider />
+        <MenuGroup>
+          <MenuGroupHeader>Section header</MenuGroupHeader>
+          <MenuItem icon={<CutIcon />}>Item</MenuItem>
+          <MenuItem icon={<PasteIcon />}>Item</MenuItem>
+          <MenuItem icon={<EditIcon />}>Item</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Container>
   </FluentProvider>
 );
 
@@ -50,17 +86,19 @@ export const MenuListWithCheckboxes = () => {
 
   return (
     <FluentProvider theme={teamsLightTheme}>
-      <MenuList checkedValues={checkedValues} onCheckedValuesChange={onChange}>
-        <MenuItemCheckbox name="checkbox" value="1" checkmark={checkmark}>
-          Item
-        </MenuItemCheckbox>
-        <MenuItemCheckbox name="checkbox" value="2" checkmark={checkmark}>
-          Item
-        </MenuItemCheckbox>
-        <MenuItemCheckbox name="checkbox" value="3" checkmark={checkmark}>
-          Item
-        </MenuItemCheckbox>
-      </MenuList>
+      <Container>
+        <MenuList checkedValues={checkedValues} onCheckedValuesChange={onChange}>
+          <MenuItemCheckbox name="checkbox" value="1" checkmark={checkmark}>
+            Item
+          </MenuItemCheckbox>
+          <MenuItemCheckbox name="checkbox" value="2" checkmark={checkmark}>
+            Item
+          </MenuItemCheckbox>
+          <MenuItemCheckbox name="checkbox" value="3" checkmark={checkmark}>
+            Item
+          </MenuItemCheckbox>
+        </MenuList>
+      </Container>
     </FluentProvider>
   );
 };
@@ -74,17 +112,19 @@ export const MenuListWithRadios = () => {
 
   return (
     <FluentProvider theme={teamsLightTheme}>
-      <MenuList checkedValues={checkedValues} onCheckedValuesChange={onChange}>
-        <MenuItemRadio name="checkbox" value="1" checkmark={checkmark}>
-          Item
-        </MenuItemRadio>
-        <MenuItemRadio name="checkbox" value="2" checkmark={checkmark}>
-          Item
-        </MenuItemRadio>
-        <MenuItemRadio name="checkbox" value="3" checkmark={checkmark}>
-          Item
-        </MenuItemRadio>
-      </MenuList>
+      <Container>
+        <MenuList checkedValues={checkedValues} onCheckedValuesChange={onChange}>
+          <MenuItemRadio name="checkbox" value="1" checkmark={checkmark}>
+            Item
+          </MenuItemRadio>
+          <MenuItemRadio name="checkbox" value="2" checkmark={checkmark}>
+            Item
+          </MenuItemRadio>
+          <MenuItemRadio name="checkbox" value="3" checkmark={checkmark}>
+            Item
+          </MenuItemRadio>
+        </MenuList>
+      </Container>
     </FluentProvider>
   );
 };
