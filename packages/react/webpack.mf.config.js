e6a82d775f70ae734255bcd53423bdf5a1a5d71b
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2021-02-02 20:08:36 -0800

    Fixes MF to actually run with latest webpack-dev-server version (4.0.0-beta.x) (#16773)
    * adding a flag to skip typecheck during webpack serve
    
    * Change files
    
    * adding comments

--- packages/react/webpack.mf.config.js
+++ packages/react/webpack.mf.config.js
@@ -1,3 +1,7 @@
+/**
+ * This webpack config creates an innerloop experience on port 2345 with the help of module federation feature
+ */
+
 const path = require('path');
 const fs = require('fs');
 const webpack = require('webpack');
@@ -5,11 +9,13 @@ const getResolveAlias = require('@fluentui/scripts/webpack/getResolveAlias');
 const { createServeConfig } = require('@fluentui/scripts/webpack/webpack-resources');
 const { webpackMerge } = require('just-scripts');
 
+// These shared are known vendor dependencies for the module federation plugin
 const shared = {
   react: { singleton: true },
   'react-dom': { singleton: true },
 };
 
+// These rootComponents are part of the "public API" of the @fluentui/react module federated bundle
 const rootComponents = fs
   .readdirSync(path.join(__dirname, 'src'))
   .filter(filename => filename[0].toUpperCase() === filename[0])
@@ -19,9 +25,12 @@ for (const component of rootComponents) {
   rootComponentsExposes[`./lib/${component}`] = `@fluentui/react/src/${component}`;
 }
 
-const myConfig = {
+// TODO: fork-ts-checker-webpack-plugin needs to be taught about the resolve alias -> pathmapping in order for the type checking to work
+process.env.SKIP_TYPECHECK = true;
+
+const mfConfig = {
   output: {},
-  entry: {},
+  entry: './empty-entrypoint.js',
   mode: 'development',
   devtool: 'cheap-module-source-map',
   optimization: {
@@ -48,10 +57,10 @@ const myConfig = {
   },
   devServer: {
     port: 2345,
-    contentBase: 'dist',
+    static: './dist',
   },
 };
 
-const metaConfig = webpackMerge.merge(createServeConfig(), myConfig);
+const metaConfig = webpackMerge.merge(createServeConfig(), mfConfig);
 
 module.exports = metaConfig;
