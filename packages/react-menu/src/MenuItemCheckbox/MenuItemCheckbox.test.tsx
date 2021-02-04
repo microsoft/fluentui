88ff6ebda62fa017232b109b7735aa3308082f00
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-04 17:35:03 +0000

    checkbox tests

--- packages/react-menu/src/MenuItemCheckbox/MenuItemCheckbox.test.tsx
+++ packages/react-menu/src/MenuItemCheckbox/MenuItemCheckbox.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { MenuListContext, MenuListProvider } from '../menuListContext';
 import { MenuItemCheckbox } from './MenuItemCheckbox';
 
@@ -28,5 +28,61 @@ describe('MenuItemCheckbox', () => {
 
     // Assert
     expect(getByText(checkmark)).not.toBeNull();
+  });
+
+  it('should render icon slot', () => {
+    // Arrange
+    const icon = 'xxx';
+    const { getByText } = render(
+      <TestMenuListContext>
+        <MenuItemCheckbox name="test" value="1" icon={icon}>
+          Checkbox
+        </MenuItemCheckbox>
+      </TestMenuListContext>,
+    );
+
+    // Assert
+    expect(getByText(icon)).not.toBeNull();
+  });
+
+  it('should set aria-checked value to true if value is checked', () => {
+    // Arrange
+    const checkedValues = { test: ['1'] };
+    const checkmark = 'xxx';
+    const { container } = render(
+      <TestMenuListContext context={{ checkedValues }}>
+        <MenuItemCheckbox name="test" value="1" checkmark={checkmark}>
+          Checkbox
+        </MenuItemCheckbox>
+      </TestMenuListContext>,
+    );
+
+    // Assert
+    expect(container.querySelector('[role="menuitemcheckbox"')?.getAttribute('aria-checked')).toEqual('true');
+  });
+
+  it.each([
+    ['uncheck', ['1'], []],
+    ['check', [], ['1']],
+  ])('should %s checkbox on click', (_, checkedItems, expectedResult) => {
+    // Arrange
+    const checkboxName = 'name';
+    const checkedValues = { [checkboxName]: checkedItems };
+    const spy = jest.fn();
+    const { container } = render(
+      <TestMenuListContext context={{ checkedValues, onCheckedValuesChange: spy }}>
+        <MenuItemCheckbox name={checkboxName} value={'1'}>
+          Checkbox
+        </MenuItemCheckbox>
+      </TestMenuListContext>,
+    );
+
+    // Act
+    const menuitem = container.querySelector('[role="menuitemcheckbox"');
+    menuitem && fireEvent.click(menuitem);
+
+    // Assert
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(checkboxName, [...expectedResult]);
   });
 });
