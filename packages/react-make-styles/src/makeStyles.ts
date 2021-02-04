8d491a194477c132634ccce34c48591e745e2017
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-04 16:02:02 +0000

    Do not throw makestyles in tests

--- packages/react-make-styles/src/makeStyles.ts
+++ packages/react-make-styles/src/makeStyles.ts
@@ -19,6 +19,10 @@ function useRenderer(document: Document | undefined): MakeStylesRenderer {
 export function makeStyles<Selectors>(definitions: MakeStylesDefinition<Selectors, Theme>[]) {
   const getStyles = vanillaMakeStyles(definitions);
 
+  if (process.env.NODE_ENV === 'test') {
+    return () => 'makestyles';
+  }
+
   return function useClasses(selectors: Selectors) {
     const { dir, document } = useFluent();
     const theme = useTheme();
