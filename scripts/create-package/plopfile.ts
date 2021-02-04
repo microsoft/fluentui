5b560ce0354c2fcd8b07b85d64c40e7738673f66
Elizabeth Craig <elcraig@microsoft.com>
2021-02-04 14:58:48 -0800

    allow multiple reference packages

--- scripts/create-package/plopfile.ts
+++ scripts/create-package/plopfile.ts
@@ -3,6 +3,8 @@ import { Actions } from 'node-plop';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as jju from 'jju';
+import _ from 'lodash';
+import chalk from 'chalk';
 import { spawnSync } from 'child_process';
 import { findGitRoot, PackageJson } from '../monorepo/index';
 
@@ -130,9 +132,9 @@ module.exports = (plop: NodePlopAPI) => {
           type: 'modify',
           path: `${destination}/package.json`,
           transform: packageJsonContents => {
-            const [result, hasUpdateError] = updatePackageJson(packageJsonContents, answers);
+            const { newPackageJson, hasError: hasUpdateError } = updatePackageJson(packageJsonContents, answers);
             hasError = hasError || hasUpdateError;
-            return result;
+            return newPackageJson;
           },
         },
         {
@@ -155,15 +157,21 @@ module.exports = (plop: NodePlopAPI) => {
         },
         () => {
           if (hasError) {
-            console.error('There were one or more errors creating the package.');
-            console.error('Please look at the logs above, fix the issues, and then run `yarn` to link.');
+            console.error(
+              chalk.red.bold(
+                '\nThere were one or more errors creating the package.\n' +
+                  'Please look at the logs above, fix the issues, and then run `yarn` to link.',
+              ),
+            );
             return;
           }
 
           console.log('\nPackage files created! Running yarn to link...\n');
           const yarnResult = spawnSync('yarn', ['--ignore-scripts'], { cwd: root, stdio: 'inherit', shell: true });
           if (yarnResult.status !== 0) {
-            console.error('Something went wrong with running yarn. Please check previous logs for details');
+            console.error(
+              chalk.red.bold('Something went wrong with running yarn. Please check previous logs for details'),
+            );
             process.exit(1);
           }
           return 'Packages linked!';
@@ -176,35 +184,46 @@ module.exports = (plop: NodePlopAPI) => {
 };
 
 /**
- * Replace empty version specs in `newPackageJson` with versions from `referencePackage`.
+ * Replace empty version specs in `newPackageJson` with versions from `referencePackages`.
  * Returns true if all versions were replaced, false if not.
  */
 function replaceVersionsFromReference(
-  referencePackage: string,
+  referencePackages: string[],
   newPackageJson: PackageJson,
   answers: Answers,
 ): boolean {
   const { hasTests } = answers;
+  const depTypes = ['dependencies', 'devDependencies', 'peerDependencies'] as const;
 
-  const referencePackageJson: PackageJson = fs.readJSONSync(
-    path.join(root, 'packages', referencePackage, 'package.json'),
+  // Read the package.json files of the given reference packages and combine into one object.
+  // This way if a dep is defined in any of them, it can easily be copied to newPackageJson.
+  const referenceDeps: Pick<PackageJson, 'dependencies' | 'devDependencies' | 'peerDependencies'> = _.merge(
+    {},
+    ...referencePackages.map(pkg =>
+      _.pick(fs.readJSONSync(path.join(root, 'packages', pkg, 'package.json')), ...depTypes),
+    ),
   );
+  console.dir(referenceDeps);
 
   let hasError = false;
 
-  for (const depType of ['dependencies', 'devDependencies', 'peerDependencies']) {
+  for (const depType of depTypes) {
     if (!newPackageJson[depType]) {
       continue;
     }
     for (const depPkg of Object.keys(newPackageJson[depType])) {
-      if (!hasTests && /\b(jest|enzyme|test)\b/.test(depPkg)) {
+      if (!hasTests && /\b(jest|enzyme|test|react-conformance)\b/.test(depPkg)) {
         delete newPackageJson[depType][depPkg];
-      } else if (referencePackageJson[depType]?.[depPkg]) {
-        newPackageJson[depType][depPkg] = referencePackageJson[depType][depPkg];
+      } else if (referenceDeps[depType]?.[depPkg]) {
+        newPackageJson[depType][depPkg] = referenceDeps[depType][depPkg];
       } else {
         hasError = true;
         console.warn(
-          `Couldn't determine appropriate ${depType} version of ${depPkg} from ${referencePackage} package.json`,
+          chalk.yellow(
+            `Couldn't determine appropriate ${depType} version of ${depPkg} from ${referencePackages.join(
+              ' or ',
+            )} package.json`,
+          ),
         );
       }
     }
@@ -212,20 +231,15 @@ function replaceVersionsFromReference(
   return hasError;
 }
 
-/**
- * Update new package's package.json.
- * Returns `[updatedPackageJsonContents, hasError]`.
- */
-function updatePackageJson(packageJsonContents: string, answers: Answers): [string, boolean] {
+function updatePackageJson(packageJsonContents: string, answers: Answers) {
   const { target, hasTests, publish } = answers;
 
-  // Copy dep versions in package.json from actual current versions.
+  // Copy dep versions in package.json from actual current version specs.
   // This is preferable over hardcoding dependency versions to keep things in sync.
-  // As of writing, @fluentui/react-image also depends on all the packages the React template needs,
-  // so we grab the current versions from there (or @fluentui/codemods for the node template).
+  // The reference package(s) may need to be updated over time as dependency lists change.
   const newPackageJson: PackageJson = JSON.parse(packageJsonContents);
-  const referencePackage = target === 'node' ? 'codemods' : 'react-image';
-  const hasReplaceError = replaceVersionsFromReference(referencePackage, newPackageJson, answers);
+  const referencePackages = target === 'node' ? ['codemods'] : ['react-button', 'react-image'];
+  const hasError = replaceVersionsFromReference(referencePackages, newPackageJson, answers);
 
   if (!hasTests) {
     delete newPackageJson.scripts['start-test'];
@@ -237,7 +251,7 @@ function updatePackageJson(packageJsonContents: string, answers: Answers): [stri
     delete newPackageJson.private;
   }
 
-  return [JSON.stringify(newPackageJson, null, 2), hasReplaceError];
+  return { newPackageJson: JSON.stringify(newPackageJson, null, 2), hasError };
 }
 
 function updateExamplePackageJson(packageJsonContents: string, packageNpmName: string): string {
