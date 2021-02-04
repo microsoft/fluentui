03c0e67f6f2542676c0dee083382fa116fe8cfc3
Elizabeth Craig <elcraig@microsoft.com>
2021-02-04 10:35:14 -0800

    fix coachmark test

--- packages/react-internal/src/components/Coachmark/Coachmark.test.tsx
+++ packages/react-internal/src/components/Coachmark/Coachmark.test.tsx
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import { Coachmark } from './Coachmark';
 import { isConformant } from '../../common/isConformant';
 
@@ -5,6 +6,7 @@ describe('Coachmark', () => {
   isConformant({
     Component: Coachmark,
     displayName: 'Coachmark',
+    componentPath: path.join(__dirname, 'Coachmark.ts'),
     disabledTests: ['component-handles-classname', 'component-has-root-ref', 'component-handles-ref'],
   });
 });
