bca3d0d79accb3675bf3dfd49c0297e646b91d2c
Miroslav Stastny <mistastn@microsoft.com>
2021-02-05 19:15:00 +0100

    chore(react-theme): update theme shape, add stories (#16844)
    * initial commit bringing over theme stories
    
    * Update theme shape, fix build
    
    * Alias color story
    
    * All theme stories added with hardcoded theme
    
    * Added units to theme values
    
    * Update dependencies, yarn.lock
    
    * Remove cruft
    
    * Make lint happy
    
    * Change files
    
    * Address PR comments
    
    Co-authored-by: Levi Thomason <me@levithomason.com>

---
+++ packages/react-examples/src/react-theme/Global/global-stroke-widths.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { StrokeWidths, teamsLightTheme } from '@fluentui/react-theme';
+
+export default {
+  title: 'Fluent UI Theme/Global/Stroke Width',
+};
+
+const global = teamsLightTheme.global;
+
+export const StrokeWidth = () => (
+  <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', gap: '10px', alignItems: 'center' }}>
+    {Object.keys(global.strokeWidth).map((strokeWidth: keyof StrokeWidths) => [
+      <div key={strokeWidth}>{strokeWidth}</div>,
+      <div key={`${strokeWidth}-value`} style={{ borderBottom: `${global.strokeWidth[strokeWidth]} solid black` }} />,
+    ])}
+  </div>
+);
