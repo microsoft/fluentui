e565e5afaf38cb205a22b05e840f3f3b43d16048
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-02-05 09:33:34 -0300

    feat(Checkbox): remove inderminate from state

--- packages/fluentui/state/src/managers/checkboxManager.ts
+++ packages/fluentui/state/src/managers/checkboxManager.ts
@@ -3,12 +3,10 @@ import { Manager, ManagerConfig } from '../types';
 
 export type CheckboxActions = {
   toggle: (checked: boolean) => void;
-  offIndeterminate: () => void;
 };
 
 export type CheckboxState = {
   checked: boolean;
-  indeterminate: boolean;
 };
 
 export type CheckboxManager = Manager<CheckboxState, CheckboxActions>;
@@ -24,7 +22,6 @@ export const createCheckboxManager = (
     },
     actions: {
       toggle: checked => () => ({ checked }),
-      offIndeterminate: () => () => ({ indeterminate: false }),
       ...config.actions,
     },
   });
