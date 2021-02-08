31fa82d859d775e3c8f04c8d49e0c91cf9abd15e
tomi-msft <66456876+tomi-msft@users.noreply.github.com>
2021-02-08 13:29:28 -0600

    Update packages/office-ui-fabric-react/src/components/Panel/Panel.types.ts
    Co-authored-by: Xu Gao <xugao0131@hotmail.com>

--- packages/office-ui-fabric-react/src/components/Panel/Panel.types.ts
+++ packages/office-ui-fabric-react/src/components/Panel/Panel.types.ts
@@ -200,7 +200,7 @@ export interface IPanelProps extends React.HTMLAttributes<PanelBase> {
   /**
    * Optional custom function to handle clicks outside this component
    */
-  onOuterClick?: (ev?: React.MouseEvent<HTMLDivElement>) => boolean | void;
+  onOuterClick?: (ev?: React.MouseEvent<HTMLDivElement>) => void;
 
   /**
    * Optional custom renderer navigation region. Replaces the region that contains the close button.
