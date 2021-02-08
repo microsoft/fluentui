ad3f46d9867e6b0bc20cecf82ae94e251212abb9
Levi Thomason <me@levithomason.com>
2021-02-08 14:31:24 -0800

    begin adding react-button converged tokens

--- packages/react-button/src/components/Button/Button.tsx
+++ packages/react-button/src/components/Button/Button.tsx
@@ -5,15 +5,66 @@ import { ButtonProps, ButtonState } from './Button.types';
 // import { useButtonClasses } from './useButtonClasses';
 import { renderButton } from './renderButton';
 import { makeStyles, ax } from '@fluentui/react-make-styles';
+import { Theme } from '@fluentui/react-theme';
 
-const useClasses = makeStyles([[null, { background: 'red' }]]);
-
-const useButtonClasses = (state: ButtonState, matcher: Partial<ButtonProps>) => {
-  // TODO: don't require "selectors"
-  // TODO: rename "selectors" to "matchers", its CSS in JS and not CSS
-  const classes = useClasses(matcher);
-  state.className = ax(state.className, classes);
+type ButtonSelectors = {
+  textOnly?: boolean;
+  iconOnly?: boolean;
 };
+
+const useRootClasses = makeStyles<ButtonSelectors>([
+  [
+    null,
+    // TODO: why is theme not typed here?
+    (theme: Theme) => ({
+      // TODO: how to define and access component variables that do not have globals/aliases?
+      //       consider IE11 friendly way
+      '--button-height': '32px',
+      '--button-paddingX': '12px',
+      '--button-minWidth': '96px',
+      '--button-maxWidth': '280px',
+
+      '--button-iconOnly-paddingX': '6px',
+
+      display: 'flex',
+      alignItems: 'center',
+
+      padding: '0 var(--button-paddingX)',
+      height: 'var(--button-height)',
+    }),
+  ],
+  [
+    // TODO: why do I have to type selectors when the generic is passed to makeStyles<ButtonSelectors>?
+    ({ textOnly }: ButtonSelectors) => textOnly,
+    {
+      // TODO:  CSS in JS styles are not typed
+      minWidth: `var(--button-minWidth)`,
+      maxWidth: `var(--button-maxWidth)`,
+    },
+  ],
+  [
+    // TODO: why do I have to type selectors when the generic is passed to makeStyles<ButtonSelectors>?
+    ({ iconOnly }: ButtonSelectors) => iconOnly,
+    {
+      // TODO: Update button variables here or make dedicated iconOnly variables?
+      padding: `var(--button-iconOnly-paddingX)`,
+    },
+  ],
+]);
+
+const useContentClasses = makeStyles([
+  [
+    null,
+    (theme: Theme) => ({
+      textOverflow: 'ellipsis',
+      display: 'inline-block',
+      border: '1px solid gray',
+      width: '100%',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+    }),
+  ],
+]);
 
 /**
  * Define a styled Button, using the `useButton` hook.
@@ -21,7 +72,17 @@ const useButtonClasses = (state: ButtonState, matcher: Partial<ButtonProps>) => 
  */
 export const Button = React.forwardRef<HTMLElement, ButtonProps>((props, ref) => {
   const state = useButton(props, ref);
-  useButtonClasses(state, {});
+  const styleSelectors = {
+    // TODO: iconOnly selector not working...
+    iconOnly: !!state.icon && !(state.content || state.children),
+    textOnly: !!(state.content || state.children) && !state.icon,
+  };
+
+  // TODO: don't require "selectors"
+  // TODO: rename "selectors" to "matchers", its CSS in JS and not CSS
+
+  state.className = ax(state.className, useRootClasses(styleSelectors));
+  state.content.className = ax(state.content.className, useContentClasses(styleSelectors));
 
   // useButtonClasses(state);
   // useInlineTokens(state, '--button');
