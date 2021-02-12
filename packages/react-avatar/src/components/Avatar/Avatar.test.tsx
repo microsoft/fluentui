e44325bc3bdfd0e89315e600ef9ce0a6974512d1
Oleksandr Fediashov <olfedias@microsoft.com>
2021-02-12 12:21:14 +0100

    remove test for `tokens`

--- packages/react-avatar/src/components/Avatar/Avatar.test.tsx
+++ packages/react-avatar/src/components/Avatar/Avatar.test.tsx
@@ -106,12 +106,4 @@ describe('Avatar', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-
-  it('handles customSize', () => {
-    const component = renderer.create(
-      <Avatar name="First Last" size={32} tokens={{ width: '33px', height: '33px' }} />,
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 });
