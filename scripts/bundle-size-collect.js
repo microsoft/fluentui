59546788f874cee648f432f5b4749df52122e3bd
Petr Duda <petrduda@microsoft.com>
2021-02-15 11:57:04 +0100

    Change devopsAssemblyArtifactName to drop

--- scripts/bundle-size-collect.js
+++ scripts/bundle-size-collect.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 const distRoot = path.resolve(__dirname, '../apps/test-bundles/dist');
 const sizes = {};
-const outputFilename = 'bundlesize.json';
+const outputFilename = 'bundlesizes.json';
 
 var items = fs.readdirSync(distRoot);
 items.forEach(item => {
