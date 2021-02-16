8c65a716f02bb953639851f4d14707bb8f92efb5
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-16 11:07:11 +0000

    rename context value type

--- packages/react-menu/src/menuGroupContext.ts
+++ packages/react-menu/src/menuGroupContext.ts
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
-const MenuGroupContext = React.createContext<MenuGroupContext>({ headerId: '' });
+const MenuGroupContext = React.createContext<MenuGroupContextValue>({ headerId: '' });
 
 /**
  * Context used to guarantee correct aria-relationship between header
  * and group information
  */
-export interface MenuGroupContext {
+export interface MenuGroupContextValue {
   /**
    * Element id applied to the `MenuGroupHeader` component
    */
