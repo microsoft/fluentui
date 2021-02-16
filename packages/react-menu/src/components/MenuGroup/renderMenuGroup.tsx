40bc05803dc3dfcfd2d7055c9baf42a8fb2bc360
ling1726 <lingfangao@hotmail.com>
2021-02-16 10:29:02 +0000

    Update packages/react-menu/src/components/MenuGroup/renderMenuGroup.tsx

--- packages/react-menu/src/components/MenuGroup/renderMenuGroup.tsx
+++ packages/react-menu/src/components/MenuGroup/renderMenuGroup.tsx
@@ -11,6 +11,7 @@ export const renderMenuGroup = (state: MenuGroupState) => {
   const { slots, slotProps } = getSlots(state);
 
   return (
+    // TODO introduce context selector pattern to avoid unnecessary rerenders
     <MenuGroupContextProvider value={{ headerId: state.headerId }}>
       <slots.root {...slotProps.root}>{state.children}</slots.root>
     </MenuGroupContextProvider>
