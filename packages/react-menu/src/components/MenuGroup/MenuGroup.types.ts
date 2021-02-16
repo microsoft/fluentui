9bf5fd2a9f08accd30179b31725b6a3468d36d04
ling1726 <lingfangao@hotmail.com>
2021-02-16 10:27:53 +0000

    Update packages/react-menu/src/components/MenuGroup/MenuGroup.types.ts
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-menu/src/components/MenuGroup/MenuGroup.types.ts
+++ packages/react-menu/src/components/MenuGroup/MenuGroup.types.ts
@@ -10,7 +10,7 @@ export type MenuGroupProps = ComponentProps & React.HTMLAttributes<HTMLElement> 
  * {@docCategory MenuGroup}
  */
 export interface MenuGroupState extends MenuGroupProps {
-  ref?: React.MutableRefObject<HTMLElement>;
+  ref: React.MutableRefObject<HTMLElement>;
 
   /**
    * id applied to the DOM element of `MenuGroupHeader`
