02f41566952ebf94f2e13d2db09c6c67eabfa011
KHMakoto <humbertomakotomorimoto@gmail.com>
2021-02-18 12:12:43 -0800

    Adding missing update.

--- apps/fabric-website/src/pages/Styles/ThemeSlotsPage/ThemeSlotsPage.doc.ts
+++ apps/fabric-website/src/pages/Styles/ThemeSlotsPage/ThemeSlotsPage.doc.ts
@@ -2,12 +2,10 @@ import { TFabricPlatformPageProps } from '../../../interfaces/Platforms';
 
 const componentUrl =
   'https://github.com/microsoft/fluentui/tree/7.0/apps/fabric-website/src/pages/Styles/ThemeSlotsPage';
-const related = require('!raw-loader!@uifabric/fabric-website/src/pages/Styles/ThemeSlotsPage/docs/ThemeSlotsRelated.md');
 
 export const ThemeSlotsPageProps: TFabricPlatformPageProps = {
   web: {
     componentUrl,
-    related,
     overview: require('!raw-loader!@uifabric/fabric-website/src/pages/Styles/ThemeSlotsPage/docs/web/ThemeSlotsOverview.md') as string,
   },
 };
