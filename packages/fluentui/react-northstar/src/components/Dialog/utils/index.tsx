f959e5e2bebdcd624ffa6e83d267330888dd044a
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-02-18 10:31:37 -0300

    fix(Dialog): remove touch event prevent default

--- packages/fluentui/react-northstar/src/components/Dialog/utils/index.tsx
+++ packages/fluentui/react-northstar/src/components/Dialog/utils/index.tsx
@@ -1,18 +1,12 @@
-const _disableIosBodyScroll = (event: TouchEvent) => {
-  event.preventDefault();
-};
-
 export function enableBodyScroll(target: HTMLElement): void {
   if (target) {
     target.style.overflow = 'unset';
-    target.removeEventListener('touchmove', _disableIosBodyScroll);
   }
 }
 
 export function disableBodyScroll(target: HTMLElement): void {
   if (target) {
     target.style.overflow = 'hidden';
-    target.addEventListener('touchmove', _disableIosBodyScroll, { passive: false, capture: false });
   }
 }
 
