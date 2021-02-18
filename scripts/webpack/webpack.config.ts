28e5bee8119a1dedb2aecf3ba0287d6f44bef9e6
ling1726 <lingfangao@hotmail.com>
2021-02-18 12:08:57 +0000

    Fixes webpack vFile fix for windows
    Use `path.resolve` to compat with unix and windows builds

--- scripts/webpack/webpack.config.ts
+++ scripts/webpack/webpack.config.ts
@@ -6,6 +6,7 @@ import webpack from 'webpack';
 import TerserPlugin from 'terser-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import * as path from 'path';
 
 import config from '../config';
 import getDefaultEnvironmentVars from './getDefaultEnvironmentVars';
@@ -65,7 +66,7 @@ const webpackConfig: webpack.Configuration = {
       // https://github.com/remarkjs/react-markdown/issues/339#issuecomment-683199835
       // https://github.com/vfile/vfile/issues/38
       {
-        test: /node_modules\/vfile\/core\.js/,
+        test: path.resolve(__dirname, '../../node_modules/vfile/core.js'),
         use: [
           {
             loader: 'imports-loader',
