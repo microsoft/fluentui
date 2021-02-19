e2956276b55a0714cc0d55587d698942b2d3b8ad
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-19 09:51:27 +0000

    Add useEventCallback hook
    This hook is documented in React's FAQ, that describes how to handle
    situations where `useCallback` gets invalidated too often by unstable
    dependencies

---
+++ packages/react-hooks/src/useEventCallback.ts
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+/**
+ * https://reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback
+ *
+ * Modified `useCallback` that can be used when dependencies change too frequently. Can occur when
+ * e.g. user props are depedencies which could change on every render
+ * e.g. volatile values (i.e. useState/useDispatch) are dependencies which could change frequently
+ *
+ * This should not be used often, but can be a useful re-render optimization since the callback is a ref and
+ * will not be invalidated between rerenders
+ *
+ * @param fn The callback function that will be used
+ * @param dependencies Exists only for completeness, can be used if there are dependencies not used by the callback
+ */
+export const useEventCallback = <Args extends unknown[], Return>(
+  fn: (...args: Args) => Return,
+  dependencies: React.DependencyList = [],
+) => {
+  const callbackRef = React.useRef<typeof fn>(() => {
+    throw new Error('Cannot call an event handler while rendering');
+  });
+
+  React.useEffect(
+    () => {
+      callbackRef.current = fn;
+    },
+    // no exhaustive deps is the main goal of this hook :)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [fn, ...dependencies],
+  );
+
+  return React.useCallback(
+    (...args: Args) => {
+      const callback = callbackRef.current;
+      return callback(...args);
+    },
+    [callbackRef],
+  );
+};
