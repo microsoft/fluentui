aafa83d81733e74a141773df6fd38d935739ff17
Charles Assunção <junioassuncaocharles@gmail.com>
2021-02-19 13:03:14 -0300

    feat(Badge): Add basic converged badge (#16868)
    * feat(Badge): scaffolding
    
    * feat(Badge): add sizes and status variations
    
    * feat(Badge): add spec
    
    * feat(Badge): add spec specific
    
    * feat(Badge): add epic
    
    * feat(Badge): remove min-width
    
    * Update packages/react-examples/package.json
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/README.md
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/jest.config.js
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/jest.config.js
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/src/Badge/useBadgeStyles.ts
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/src/Badge/useBadgeStyles.ts
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Update packages/react-badge/src/Badge/Badge.types.ts
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * feat(Badge): restore spec
    
    * feat(Badge): SizeValue
    
    * feat(Badge): remove cruft file
    
    * feat(Badge): support icon
    
    * feat(Badge): codeowner
    
    * feat(Badge): rever menu item change
    
    * feat(Badge): badge package
    
    * feat(Badge): badge package
    
    * Update packages/react-badge/package.json
    
    Co-authored-by: ling1726 <lingfangao@hotmail.com>
    
    * feat(Badge): add comments
    
    * feat(Badge): change
    
    * Change files
    
    * Update @fluentui-react-examples-f2754dfb-a65e-4601-87ba-8a00a3d506b9.json
    
    * Update @fluentui-react-theme-53128ace-2db6-46a6-9a26-f5c034dc6ac9.json
    
    * feat(Badge): api
    
    * feat(Badge): remove color
    
    * Update packages/react-badge/src/components/Badge/useBadge.ts
    
    Co-authored-by: Ben Howell <48106640+behowell@users.noreply.github.com>
    
    * Update @fluentui-react-badge-633e119f-2ee1-4826-8b29-484db1fea21d.json
    
    * Update @fluentui-react-examples-f2754dfb-a65e-4601-87ba-8a00a3d506b9.json
    
    * Update @fluentui-react-theme-53128ace-2db6-46a6-9a26-f5c034dc6ac9.json
    
    * feat(Badge): remove backslash
    
    * feat(Badge): remove comment
    
    * feat(Badge): variant to appearance
    
    * feat(Badge): variant to appearance
    
    * feat(Badge): update api
    
    * feat(Badge): added appearence variations
    
    * feat(Badge): update api
    
    * feat(Badge): fix import
    
    * feat(Badge): update versions
    
    * feat(Badge): fix import
    
    * feat(Badge): fix test
    
    * feat(Badge): story providers
    
    * feat(Badge): size values
    
    * feat(Badge): children to content
    
    * feat(Badge): font family
    
    * Update packages/react-badge/src/components/Badge/useBadge.ts
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * feat(Badge): remove -utils
    
    * feat(Badge): update api
    
    * feat(Badge): update packages
    
    * feat(Badge): update packages
    
    * feat(Badge): update deps
    
    * feat(Badge): update deps
    
    * feat(Badge): update deps
    
    * feat(Badge): remove jest dep
    
    * Update package.json
    
    * feat(Badge): yarn lock
    
    * feat(Badge): doc comment
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    Co-authored-by: ling1726 <lingfangao@hotmail.com>
    Co-authored-by: Ben Howell <48106640+behowell@users.noreply.github.com>

---
+++ packages/react-badge/src/components/Badge/Badge.types.ts
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { ComponentProps, ShorthandProps, ObjectShorthandProps } from '@fluentui/react-utilities';
+
+/**
+ * {@docCategory Badge}
+ */
+export type SizeValue = 'smallest' | 'smaller' | 'small' | 'medium' | 'large' | 'larger' | 'largest';
+
+/**
+ * {@docCategory Badge}
+ */
+export type BadgeStatus =
+  | 'success'
+  | 'warning'
+  | 'severe'
+  | 'accent'
+  | 'danger'
+  | 'important'
+  | 'informative'
+  | 'subtle';
+
+/**
+ * {@docCategory Badge}
+ */
+export type BadgeAppearances = 'filled' | 'outline' | 'ghost' | 'tint';
+
+/**
+ * {@docCategory Badge}
+ */
+export type BadgeShape = 'rounded' | 'square' | 'circular';
+
+/**
+ * {@docCategory Badge}
+ */
+export interface BadgeProps extends ComponentProps, React.HTMLAttributes<HTMLElement> {
+  /**
+   * A Badge can be sized.
+   */
+  size?: SizeValue;
+
+  /**
+   * A Badge can have status styles variants
+   */
+  status?: BadgeStatus;
+
+  /**
+   * A Badge can be square, circular or rounded
+   */
+  shape?: BadgeShape;
+
+  /**
+   * A Badge can be filled, outline, ghost, inverted
+   */
+  appearance?: BadgeAppearances;
+
+  /**
+   * Icon slot
+   */
+  icon?: ShorthandProps<HTMLElement>;
+
+  /**
+   * Position for Icon to be rendered
+   */
+  iconPosition?: 'before' | 'after';
+}
+
+/**
+ * {@docCategory Badge}
+ */
+export interface BadgeState extends BadgeProps {
+  /**
+   * Ref to the root slot
+   */
+  ref: React.MutableRefObject<HTMLElement>;
+  /**
+   * Icon slot when processed by internal state
+   */
+  icon?: ObjectShorthandProps<HTMLSpanElement>;
+}
