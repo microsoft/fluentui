1d3819595519b9c8a9a8e7641137474841f2d931
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-23 14:12:10 +0000

    narrow props

--- packages/react-focus-management/src/hooks/useFocusFinders.ts
+++ packages/react-focus-management/src/hooks/useFocusFinders.ts
@@ -1,12 +1,16 @@
 import { useFocusManagementContext } from '../focusManagementContext';
-import { Types as AHTypes } from 'ability-helpers';
 
+/**
+ * Returns a set of helper functions that will traverse focusable elements in the context of a root DOM element
+ */
 export const useFocusFinders = () => {
   const { ahInstance } = useFocusManagementContext();
 
-  const findAll = (...args: Parameters<AHTypes.FocusableAPI['findAll']>) => ahInstance.focusable.findAll(...args);
-  const findFirst = (...args: Parameters<AHTypes.FocusableAPI['findFirst']>) => ahInstance.focusable.findFirst(...args);
-  const findLast = (...args: Parameters<AHTypes.FocusableAPI['findLast']>) => ahInstance.focusable.findLast(...args);
+  // Narrow props for now until there is a need for modalizer/groupper
+  const findAll = (root: HTMLElement, matcher: (el: HTMLElement) => boolean) =>
+    ahInstance.focusable.findAll(root, matcher);
+  const findFirst = (root: HTMLElement) => ahInstance.focusable.findFirst(root);
+  const findLast = (root: HTMLElement) => ahInstance.focusable.findLast(root);
 
   return {
     findAll,
