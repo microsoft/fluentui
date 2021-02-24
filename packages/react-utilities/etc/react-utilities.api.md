2641f80031eaf4f033aa1e20f4ab13a97edf2fb2
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-24 10:39:06 +0000

    remove dep array from useEventCallback

--- packages/react-utilities/etc/react-utilities.api.md
+++ packages/react-utilities/etc/react-utilities.api.md
@@ -94,7 +94,7 @@ export function useControllableValue<TValue, TElement extends HTMLElement>(contr
 export function useControllableValue<TValue, TElement extends HTMLElement, TEvent extends React.SyntheticEvent<TElement> | undefined>(controlledValue: TValue | undefined, defaultUncontrolledValue: TValue | undefined, onChange: ChangeCallback<TElement, TValue, TEvent> | undefined): Readonly<[TValue | undefined, (update: React.SetStateAction<TValue | undefined>, ev?: React.FormEvent<TElement>) => void]>;
 
 // @public
-export const useEventCallback: <Args extends unknown[], Return>(fn: (...args: Args) => Return, dependencies: React.DependencyList) => (...args: Args) => Return;
+export const useEventCallback: <Args extends unknown[], Return>(fn: (...args: Args) => Return) => (...args: Args) => Return;
 
 // @public
 export function useId(prefix?: string, providedId?: string): string;
--- packages/react-utilities/src/hooks/useEventCallback.ts
+++ packages/react-utilities/src/hooks/useEventCallback.ts
@@ -13,21 +13,14 @@ import * as React from 'react';
  * @param fn The callback function that will be used
  * @param dependencies Shouldn't be needed since the callback is ref based, but good to behave like native
  */
-export const useEventCallback = <Args extends unknown[], Return>(
-  fn: (...args: Args) => Return,
-  dependencies: React.DependencyList,
-) => {
+export const useEventCallback = <Args extends unknown[], Return>(fn: (...args: Args) => Return) => {
   const callbackRef = React.useRef<typeof fn>(() => {
     throw new Error('Cannot call an event handler while rendering');
   });
 
-  React.useEffect(
-    () => {
-      callbackRef.current = fn;
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [fn, ...dependencies],
-  );
+  React.useEffect(() => {
+    callbackRef.current = fn;
+  }, [fn]);
 
   return React.useCallback(
     (...args: Args) => {
