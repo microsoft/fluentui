b8c361da073227e4050586dc8aef00c0667e8e70
Lingfan Gao <lingfan.gao@microsoft.com>
2021-02-24 11:32:37 +0000

    fix tsdoc

--- packages/react-utilities/src/hooks/useEventCallback.ts
+++ packages/react-utilities/src/hooks/useEventCallback.ts
@@ -10,8 +10,8 @@ import * as React from 'react';
  * This should not be used often, but can be a useful re-render optimization since the callback is a ref and
  * will not be invalidated between rerenders
  *
- * @param fn The callback function that will be used
- * @param dependencies Shouldn't be needed since the callback is ref based, but good to behave like native
+ * @param fn - The callback function that will be used
+ * @param dependencies - Shouldn't be needed since the callback is ref based, but good to behave like native
  */
 export const useEventCallback = <Args extends unknown[], Return>(fn: (...args: Args) => Return) => {
   const callbackRef = React.useRef<typeof fn>(() => {
