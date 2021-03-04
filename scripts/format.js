05f2ffd9694006335963c524d0aa354bed088a97
Elizabeth Craig <elcraig@microsoft.com>
2021-03-04 13:20:03 -0800

    more comments

--- scripts/format.js
+++ scripts/format.js
@@ -1,4 +1,8 @@
 // @ts-check
+
+// This script is not meant as a general-purpose replacement for prettier.
+// It only handles certain cases that prettier doesn't support as well/at all natively.
+
 const { execSync } = require('child_process');
 const os = require('os');
 const { default: PQueue } = require('p-queue');
@@ -47,11 +51,11 @@ async function main() {
 
 function parseArgs() {
   // Use yargs config to enforce that script must be run with exactly one of `--all` or
-  // `--since [target]` (--check is accepted with either one)
+  // `--since [target]` (--check is accepted with either one).
   return require('yargs')
-    .usage('Usage: format [options]')
-    .example('format --since HEAD~3', 'Run prettier only on changed files since HEAD~3')
-    .example('format --all', 'Run prettier on all files')
+    .usage('Usage: format [options]\n\nRuns prettier on all files or since a certain commit (using parallel processes)')
+    .example('format --since HEAD~3', 'Run `prettier --write` only on changed files since HEAD~3')
+    .example('format --all', 'Run `prettier --write` on all files')
     .options({
       since: {
         description: 'Run prettier on files since commit',
@@ -62,7 +66,7 @@ function parseArgs() {
         boolean: true,
       },
       check: {
-        description: 'Run prettier in check mode (useful for CI)',
+        description: 'Run prettier in check mode (useful for CI). Runs in write mode if not specified.',
         boolean: true,
       },
     })
@@ -75,7 +79,9 @@ function parseArgs() {
         throw new Error('To format individual files, run `yarn prettier` directly');
       }
       if (!argv.all && !argv.since) {
-        throw new Error('Must specify `--all` or `--since [target]`');
+        throw new Error(
+          'Must specify `--all` or `--since [target]` (for other use cases, run `yarn prettier` directly)',
+        );
       }
       return true;
     }).argv;
