615cb3e3dfeb74e338711ea81b00a6427448d806
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-03-03 16:09:30 -0800

    updated Rating examples to remove 'select' from ariaLabelFormat and make use of new ariaLabel prop if not in readOnly mode

--- packages/react-examples/src/react/Rating/Rating.ButtonControlled.Example.tsx
+++ packages/react-examples/src/react/Rating/Rating.ButtonControlled.Example.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Rating } from '@fluentui/react';
+
 import { PrimaryButton } from '@fluentui/react/lib/Button';
 
 export const RatingButtonControlledExample: React.FunctionComponent = () => {
   const [currentRating, setCurrentRating] = React.useState(5);
   const currentRatingToggle = React.useCallback(() => {
-    setCurrentRating(oldRating => (oldRating === 0 ? 5 : 0));
+    setCurrentRating((oldRating) => (oldRating === 0 ? 5 : 0));
   }, []);
 
   const getRatingAriaLabel = React.useCallback(() => `Rating value is ${currentRating} of 5`, [currentRating]);
@@ -18,7 +19,7 @@ export const RatingButtonControlledExample: React.FunctionComponent = () => {
         readOnly
         allowZeroStars
         getAriaLabel={getRatingAriaLabel}
-        ariaLabelFormat="Select {0} of {1} stars"
+        ariaLabelFormat="{0} of {1} stars"
       />
       <PrimaryButton onClick={currentRatingToggle}>Click to change rating to {5 - currentRating}</PrimaryButton>
     </div>
