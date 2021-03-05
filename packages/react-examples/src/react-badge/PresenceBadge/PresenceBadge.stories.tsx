27f8b147c39aedba65459c7f5199bfb485e62eb4
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-05 14:49:55 -0300

    feat(Badge): add presence badge

---
+++ packages/react-examples/src/react-badge/PresenceBadge/PresenceBadge.stories.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { PresenceBadge } from '@fluentui/react-badge';
+import { SkypeMinusIcon, SkypeClockIcon, SkypeCheckIcon, CancelIcon, SkypeArrowIcon } from '@fluentui/react-icons-mdl2';
+
+export const PresenceBadgeStatus = () => (
+  <>
+    <h2>Circular (default)</h2>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: 200,
+      }}
+    >
+      <PresenceBadge status="busy" icon={<SkypeMinusIcon />} />
+      <PresenceBadge status="away" icon={<SkypeClockIcon />} />
+      <PresenceBadge status="available" icon={<SkypeCheckIcon />} />
+      <PresenceBadge status="oof" icon={<SkypeArrowIcon />} />
+      <PresenceBadge status="offline" icon={<CancelIcon />} />
+    </div>
+  </>
+);
