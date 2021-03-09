444b075a04a5b8abfe3431154661ae734afcdded
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-03-09 15:15:19 -0300

    Updates DefaultExpandIcon internal Chevron

--- packages/react-accordion/src/components/AccordionHeader/DefaultExpandIcon.tsx
+++ packages/react-accordion/src/components/AccordionHeader/DefaultExpandIcon.tsx
@@ -1,51 +1,33 @@
 import * as React from 'react';
 import { AccordionHeaderExpandIconProps } from './AccordionHeader.types';
 
-const ChevronRightIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
-  <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 2048 2048" ref={ref} {...props}>
-    <path d="M515 1955l930-931L515 93l90-90 1022 1021L605 2045l-90-90z" />
-  </svg>
-));
-
-ChevronRightIcon.displayName = 'ChevronRightIcon';
-
-const ChevronDownIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
-  <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 2048 2048" ref={ref} {...props}>
-    <path d="M1939 467l90 90-1005 1005L19 557l90-90 915 915 915-915z" />
-  </svg>
-));
-
-ChevronDownIcon.displayName = 'ChevronDownIcon';
-
-const ChevronUpIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
-  <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 2048 2048" ref={ref} {...props}>
-    <path d="M1955 1533l-931-930-931 930-90-90L1024 421l1021 1022-90 90z" />
-  </svg>
-));
-
-ChevronUpIcon.displayName = 'ChevronUpIcon';
-
 export const DefaultExpandIcon = React.forwardRef<HTMLSpanElement, AccordionHeaderExpandIconProps>(
   ({ open, expandIconPosition, children, ...rest }, ref) => (
     <span {...rest} ref={ref}>
-      {children ?? mapStateToIcon({ open, expandIconPosition })}
+      {children ?? (
+        <svg
+          fill="currentColor"
+          height="1em"
+          width="1em"
+          viewBox="0 0 2048 2048"
+          transform={`rotate(${mapStateToRotation({ open, expandIconPosition })})`}
+        >
+          <path d="M515 1955l930-931L515 93l90-90 1022 1021L605 2045l-90-90z" />
+        </svg>
+      )}
     </span>
   ),
 );
 
-function mapStateToIcon({
+function mapStateToRotation({
   open,
   expandIconPosition,
 }: Pick<AccordionHeaderExpandIconProps, 'open' | 'expandIconPosition'>) {
-  switch (`${open}-${expandIconPosition}`) {
-    case 'false-start':
-      return <ChevronRightIcon />;
-    case 'true-end':
-      return <ChevronUpIcon />;
-    case 'true-start':
-    case 'false-end':
-      return <ChevronDownIcon />;
-    default:
-      return null;
+  if (open && expandIconPosition === 'end') {
+    return '-90';
   }
+  if ((!open && expandIconPosition === 'end') || (open && expandIconPosition === 'start')) {
+    return '90';
+  }
+  return '0';
 }
