daa35ec1417555f0e566f1fe61e837858999917a
Adam Samec <adam.samec@gmail.com>
2021-03-16 21:24:22 +0100

    Add aria-live prototype and changes so more prototypes can be added easily

--- packages/fluentui/react-northstar-prototypes/src/prototypes/AccessibleWizard/AccessibleWizard.tsx
+++ packages/fluentui/react-northstar-prototypes/src/prototypes/AccessibleWizard/AccessibleWizard.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { Provider, teamsTheme, Form, Input, TextArea, RadioGroup, Dropdown, Checkbox, Button} from '@fluentui/react-northstar';
+import { Provider, teamsTheme } from '@fluentui/react-northstar';
+import WizardContent from './WizardContent';
 
+let timeout;
 const narrate = (message, priority = 'polite') => {
   const element = document.createElement('div');
   element.setAttribute(
@@ -10,162 +12,87 @@ const narrate = (message, priority = 'polite') => {
   element.setAttribute('aria-live', priority);
   document.body.appendChild(element);
 
-  setTimeout(() => {
+  timeout = setTimeout(() => {
     element.innerText = message;
-  }, 2000); // End setTimeout 1
+  }, 1000); // End setTimeout 1
 
   setTimeout(() => {
     document.body.removeChild(element);
-  }, 2300); // End setTimeout 1
+  }, 1300); // End setTimeout 1
 }; // End narrate
 
 const AccessibleWizard: React.FunctionComponent = () => {
-  const [currentStepIndex, setCurrentStepIndex] = React.useState(0);
-narrate('');
-const firstFocusables = ['firstName', 'quote'];
+  const handleWizardFocus = React.useCallback(
+  message => {
+clearTimeout(timeout);
+narrate(message);
+}, []);
 
-  React.useEffect(() => {
-  // Focus the first focusable form element
-  (document.getElementById(firstFocusables[currentStepIndex]) as HTMLElement).focus();
-}, [currentStepIndex]); // End useEffect
-
-const wizardSteps = [
-// Step 1
-(
-<div role="dialog" aria-labelledby="step1Heading">
-<h2 id="step1Heading">Step 1: Personal details</h2>
-            <Form>
-
-            <label htmlFor="firstName" id="firstNameLabel">First name:</label>
-                        <Input
-            name="firstName"
-            id="firstName"
-            />
-            
-            <label htmlFor="lastName" id="lastNameLabel">Last name:</label>
-                        <Input
-            name="lastName"
-            id="lastName"
-            />
-            
-                        <label id="genderLabel">Gender:</label>
-            <RadioGroup
-aria-labelledby="genderLabel"
-            items={[
-                    {
-            name: 'gender',
-            key: 'Male',
-            label: 'Male',
-            value: 'male',
-            },
-                    {
-            name: 'gender',
-            key: 'Female',
-            label: 'Female',
-            value: 'female',
-            },
-                    ]}
-            />
-            
-                        <label id="countryLabel">Country of residence</label>
-            <Dropdown
-aria-labelledby="countryLabel"
-            search
-            items={[
-                    'Czech Republic',
-                    'Slovakia',
-                    'Germany',
-                    'Poland',
-                    'Austria',
-                    'Other',
-                    ]}
-            placeholder="Please select"
-            getA11ySelectionMessage={{
-    onAdd: item => `${item} has been selected.`,
-    }}
-            />
-            
-            <Checkbox
-id="adult"
-label={<label htmlFor="adult" id="adultLabel">I am 18+ years old:</label>}
+// Wizard 1
+  const [wizard1StepIndex, setWizard1StepIndex] = React.useState(0);
+const wizard1Steps = [
+( // Step 1
+<div role="region" aria-labelledby="wizard1-step1-heading">
+<h3 id="wizard1-step1-heading">Step 1: Personal details</h3>
+<WizardContent
+name="wizard1"
+stepIndex={0}
+setStepIndex={setWizard1StepIndex}
 />
-
-            <Button
-            onClick={() => {setCurrentStepIndex(1)}}
-            >Next step</Button>
-            </Form>
+</div>
+), ( // Step 2
+<div role="region" aria-labelledby="wizard1-step2-heading">
+<h3 id="wizard1-step2-heading">Step 2: Favourite Sci-Fi</h3>
+<WizardContent
+name="wizard1"
+stepIndex={1}
+setStepIndex={setWizard1StepIndex}
+/>
 </div>
 ),
-// Step 2
-(
-<div role="dialog" aria-labelledby="step2Heading">
-<h2 id="step2Heading">Step 2: Favourite Sci-Fi</h2>
-            <Form>
+];
 
-            <label htmlFor="quote" id="quoteLabel">Type your favourite quote:</label>
-                        <TextArea
-            name="quote"
-            id="quote"
-            />
-            
-                        <label id="technologyLabel">Which technology would you like to have the most:</label>
-            <RadioGroup
-aria-labelledby="technologyLabel"
-            items={[
-                    {
-            name: 'technology',
-            key: 'stargate',
-            label: 'Star Gate',
-            value: 'stargate',
-            },
-                    {
-            name: 'technology',
-            key: 'holodeck',
-            label: 'Holodeck',
-            value: 'holodeck',
-            },
-                    {
-            name: 'technology',
-            key: 'lightsaber',
-            label: 'Lightsaber',
-            value: 'lightsaber',
-            },
-            ]}
-            />
-            
-                        <label id="captainLabel">Your favourite captain:</label>
-            <Dropdown
-aria-labelledby="  captainLabel"
-            search
-            items={[
-                    'Samantha Carter',
-                    'Jean-Luc Picard',
-                    'Captain America',
-                    'Jonathan Archer',
-                    'James T. Kirk',
-                    'Captain Power',
-                    ]}
-            placeholder="Please select"
-            getA11ySelectionMessage={{
-    onAdd: item => `${item} has been selected.`,
-    }}
-            />
-            
-            <Button
-            onClick={() => {setCurrentStepIndex(0)}}
-            >Previous step</Button>
-            </Form>
+// Wizard 2
+  const [wizard2StepIndex, setWizard2StepIndex] = React.useState(0);
+const wizard2Steps = [
+( // Step 1
+<div onFocus={event => {
+handleWizardFocus('Step 1: Personal details');
+}}
+>
+<h3 id="wizard2-step1-heading">Step 1: Personal details</h3>
+<WizardContent
+name="wizard2"
+stepIndex={0}
+setStepIndex={setWizard2StepIndex}
+/>
+</div>
+), ( // Step 2
+<div onFocus={event => {
+handleWizardFocus('Step 2: Favourite Sci-Fi');
+}}
+>
+<h3 id="wizard2-step2-heading">Step 2: Favourite Sci-Fi</h3>
+<WizardContent
+name="wizard2"
+stepIndex={1}
+setStepIndex={setWizard2StepIndex}
+/>
 </div>
 ),
 ];
 
   return (
-    <>
                 <Provider theme={teamsTheme}>
-            <h1>Accessible wizard prototype</h1>
-{wizardSteps[currentStepIndex]}
+            <h1>Accessible wizard prototypes</h1>
+
+                        <h2>Prototype #1 - role=region and aria-label</h2>
+{wizard1Steps[wizard1StepIndex]}
+
+                        <h2>Prototype #2 - aria-live</h2>
+{wizard2Steps[wizard2StepIndex]}
+
                         </Provider>
-    </>
   );
 }; // End AccessibleWizard
 
---
+++ packages/fluentui/react-northstar-prototypes/src/prototypes/AccessibleWizard/WizardContent.tsx
@@ -0,0 +1,143 @@
+import * as React from 'react';
+import { Form, Input, TextArea, RadioGroup, Dropdown, Checkbox, Button} from '@fluentui/react-northstar';
+
+type WizardContentProps = {
+name: string;
+stepIndex: number;
+setStepIndex: (index: number) => void;
+};
+
+const WizardContent: React.FunctionComponent<WizardContentProps> = ({name, stepIndex, setStepIndex}) => {
+const firstFocusables = ['firstName', 'quote'];
+
+  React.useEffect(() => {
+  // Focus the first focusable form element
+  const id = name + '-' + firstFocusables[stepIndex];
+  (document.getElementById(id) as HTMLElement)?.focus();
+}, [stepIndex]); // End useEffect
+
+const wizardSteps = [
+( // Step 1
+            <Form>
+            <label htmlFor={name + '-firstName'}>First name:</label>
+                        <Input
+            id={name + '-firstName'}
+            />
+            
+            <label htmlFor={name + '-lastName'}>Last name:</label>
+                        <Input
+            id={name + '-lastName'}
+            />
+            
+                        <label id={name + '-genderLabel'}>Gender:</label>
+            <RadioGroup
+aria-labelledby={name + '-genderLabel'}
+            items={[
+                    {
+            name: 'gender',
+            key: 'Male',
+            label: 'Male',
+            value: 'male',
+            },
+                    {
+            name: 'gender',
+            key: 'Female',
+            label: 'Female',
+            value: 'female',
+            },
+                    ]}
+            />
+            
+                        <label id={name + '-countryLabel'}>Country of residence</label>
+            <Dropdown
+aria-labelledby={name + '-countryLabel'}
+            search
+            items={[
+                    'Czech Republic',
+                    'Slovakia',
+                    'Germany',
+                    'Poland',
+                    'Austria',
+                    'Other',
+                    ]}
+            placeholder="Please select"
+            getA11ySelectionMessage={{
+    onAdd: item => `${item} has been selected.`,
+    }}
+            />
+            
+            <Checkbox
+id={name + '-adult'}
+label={<label htmlFor={name + '-adult'}>I am 18+ years old:</label>}
+/>
+
+            <Button
+            onClick={() => {setStepIndex(1)}}
+            >Next step</Button>
+            </Form>
+), ( // Step 2
+            <Form>
+
+            <label htmlFor={name + '-quote'}>Type your favourite quote:</label>
+                        <TextArea
+            id={name + '-quote'}
+            />
+            
+                        <label id={name + '-technologyLabel'}>Which technology would you like to have the most:</label>
+            <RadioGroup
+aria-labelledby={name + '-technologyLabel'}
+            items={[
+                    {
+            name: 'technology',
+            key: 'stargate',
+            label: 'Star Gate',
+            value: 'stargate',
+            },
+                    {
+            name: 'technology',
+            key: 'holodeck',
+            label: 'Holodeck',
+            value: 'holodeck',
+            },
+                    {
+            name: 'technology',
+            key: 'lightsaber',
+            label: 'Lightsaber',
+            value: 'lightsaber',
+            },
+            ]}
+            />
+            
+                        <label id={name + '-captainLabel'}>Your favourite captain:</label>
+            <Dropdown
+aria-labelledby={name + '-captainLabel'}
+            search
+            items={[
+                    'Samantha Carter',
+                    'Jean-Luc Picard',
+                    'Captain America',
+                    'Jonathan Archer',
+                    'James T. Kirk',
+                    'Captain Power',
+                    ]}
+            placeholder="Please select"
+            getA11ySelectionMessage={{
+    onAdd: item => `${item} has been selected.`,
+    }}
+            />
+            
+            <Button
+            onClick={() => {setStepIndex(0)}}
+            >Previous step</Button>
+            </Form>
+),
+];
+
+  return (
+    <>
+{wizardSteps[stepIndex]}
+    </>
+  );
+}; // End WizardContent
+
+export default WizardContent;
