f359e44f97b9c4aff32d65afc3cc581f33a7f749
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-03-16 05:22:01 -0300

    react-flex - Updates styles from makeStylesCompat to makeStyles

--- packages/react-flex/src/components/Flex/useFlexStyles.ts
+++ packages/react-flex/src/components/Flex/useFlexStyles.ts
@@ -1,43 +1,22 @@
-import { makeStylesCompat, ax } from '@fluentui/react-make-styles';
+import { ax, makeStyles } from '@fluentui/react-make-styles';
 import { FlexState } from './Flex.types';
 
-/**
- * Styles for the root slot
- */
-const useRootStyles = makeStylesCompat<FlexState>([
-  [
-    null,
-    theme => ({
-      // TODO Add default styles
-    }),
-  ],
-  // TODO Add styles conditioned on FlexState props, for example:
-  // [
-  //   s => s.exampleProp === 'exampleValue',
-  //   theme => ({
-  //
-  //   }),
-  // ],
-]);
-
-// TODO Add styles for any other slots, for example:
-// const useExampleSlotStyles = makeStyles<FlexState>([
-//   [
-//     null,
-//     theme => ({
-//
-//     }),
-//   ],
-// ]);
+const useStyles = makeStyles({
+  // TODO: Add default styles
+  root: {},
+  // TODO: Add styles conditioned on FlexState props, for example:
+  // TODO: Add styles for any other slots, for example:
+});
 
 /**
  * Apply styling to the Flex slots based on the state
  * {@docCategory Flex }
  */
 export const useFlexStyles = (state: FlexState): FlexState => {
-  state.className = ax(useRootStyles(state), state.className);
+  const styles = useStyles();
+  state.className = ax(styles.root, state.className);
 
-  // TODO Hook up slot styles, for example:
+  // TODO: Hook up slot styles, for example:
   // const exampleSlotClassName = useExampleSlotStyles(state);
   // if (state.exampleSlot) {
   //   state.exampleSlot.className = ax(exampleSlotClassName, state.exampleSlot.className);
