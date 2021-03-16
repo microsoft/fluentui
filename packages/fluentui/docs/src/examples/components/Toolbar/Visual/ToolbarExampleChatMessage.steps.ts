c74b63e55ab481f36dcad4c065d1ce84e1e14718
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-16 18:01:04 +0000

    fix test

--- packages/fluentui/docs/src/examples/components/Toolbar/Visual/ToolbarExampleChatMessage.steps.ts
+++ packages/fluentui/docs/src/examples/components/Toolbar/Visual/ToolbarExampleChatMessage.steps.ts
@@ -1,12 +1,7 @@
 import { ScreenerTestsConfig } from '@fluentui/scripts/screener';
 
 const config: ScreenerTestsConfig = {
-  steps: [
-    builder =>
-      builder
-        .executeScript('document.querySelector(\'[title="more"\').click()')
-        .snapshot('Toolbar fits within bounds of the chat message'),
-  ],
+  steps: [builder => builder.click('[title="more"').snapshot('Toolbar fits within bounds of the chat message')],
 };
 
 export default config;
