8eee8373e28e5e1f6a586e43b63cbdebbb399d33
Elizabeth Craig <elcraig@microsoft.com>
2021-03-17 13:36:52 -0700

    consolidate pr-deploy-site code and ensure it uses es5

--- apps/pr-deploy-site/pr-deploy-site.js
+++ apps/pr-deploy-site/pr-deploy-site.js
@@ -1,7 +1,10 @@
+// @ts-check
 // If you are adding a new tile into this site, place make sure it is also being copied from `just.config.ts`
 
-// Syntax here MUST BE IE11-COMPATIBLE because this file is run without transpiling, and needs to
-// work in all browsers including IE 11 as long as we still support it.
+// A build step will replace this with the list of actual built packages
+/** @type {string[]} */
+var packages;
+
 var siteInfo = [
   {
     package: '@fluentui/public-docsite-resources',
@@ -59,25 +62,40 @@ var siteInfo = [
   },
 ];
 
+var hrefMatch = window.location.href.match(/refs\/(pull|heads)\/([^/]+)/);
+var repoUrl = 'https://github.com/microsoft/fluentui';
+if (hrefMatch) {
+  var link = /** @type {HTMLAnchorElement} */ (document.getElementById('prLink'));
+  if (hrefMatch[1] === 'heads') {
+    // master or other branch CI
+    link.innerHTML = hrefMatch[2];
+    link.href = repoUrl + '/tree/' + hrefMatch[2];
+    // remove the PR-specific explanation
+    var prExplanation = document.getElementById('prExplanation');
+    prExplanation.parentElement.removeChild(prExplanation);
+  } else {
+    // PR
+    link.innerHTML = 'PR #' + hrefMatch[2];
+    link.href = repoUrl + '/pull/' + hrefMatch[2];
+  }
+}
+
 var siteLink = document.getElementById('site-list');
 
-window.renderSiteLinks = function(packages) {
-  siteInfo.forEach(function(info) {
-    if (packages.indexOf(info.package) > -1) {
-      var li = document.createElement('LI');
-      li.className = 'Tile';
-      // Syntax here MUST BE IE11-COMPATIBLE (no backticks)
-      li.innerHTML =
-        '<a href="' +
-        info.link +
-        '" class="Tile-link">' +
-        '<i class="ms-Icon ms-Icon--' +
-        info.icon +
-        '"></i>' +
-        info.title +
-        '</a>';
+siteInfo.forEach(function(info) {
+  if (packages.indexOf(info.package) > -1) {
+    var li = document.createElement('LI');
+    li.className = 'Tile';
+    li.innerHTML =
+      '<a href="' +
+      info.link +
+      '" class="Tile-link">' +
+      '<i class="ms-Icon ms-Icon--' +
+      info.icon +
+      '"></i>' +
+      info.title +
+      '</a>';
 
-      siteLink.appendChild(li);
-    }
-  });
-};
+    siteLink.appendChild(li);
+  }
+});
