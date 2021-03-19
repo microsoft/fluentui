2e8644be7755484c23b774de760c37ad5185462f
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-19 10:49:37 -0300

    feat(Pills): add unstable comment

--- packages/fluentui/react-northstar/src/components/Pill/Pill.tsx
+++ packages/fluentui/react-northstar/src/components/Pill/Pill.tsx
@@ -66,6 +66,7 @@ export type PillStylesProps = Required<Pick<PillProps, 'appearance' | 'size' | '
 export const pillClassName = 'ui-pill';
 
 /**
+ * THIS COMPONENT IS UNSTABLE
  * Pills should be used when representing an input, as a way to filter content, or to represent an attribute.
  */
 export const Pill: ComponentWithAs<'span', PillProps> & FluentComponentStaticProps<PillProps> = props => {
