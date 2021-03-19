137d0a51bffa241a1ad62ffbfb0899ff706da304
Aidan Muller-Cohn <aidanmc95@gmail.com>
2021-03-18 17:55:21 -0700

    Switch to fluent for Stories

--- packages/react-examples/src/azure-themes/stories/components/tooltip.stories.tsx
+++ packages/react-examples/src/azure-themes/stories/components/tooltip.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { TooltipHost, ITooltipHostStyles } from 'office-ui-fabric-react/lib/Tooltip';
-import { useId } from '@uifabric/react-hooks';
+import { DefaultButton } from '@fluentui/react/lib/Button';
+import { TooltipHost, ITooltipHostStyles } from '@fluentui/react/lib/Tooltip';
+import { useId } from '@fluentui/react-hooks';
 
 const calloutProps = { gapSpace: 0 };
 // The TooltipHost root uses display: inline by default.
