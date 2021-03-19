6db570b277e562a4a9a286a6356ac782646505c9
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-19 16:32:47 -0300

    feat(Pills): fix example

--- packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleAppearance.tsx
+++ packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleAppearance.tsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 import { Pill, Flex } from '@fluentui/react-northstar';
 
 const PillAppearanceExample = () => (
-  <Flex
-    gap="gap.medium"
-    style={{
-      background: 'rgba(0,0,0,.2)',
-    }}
-  >
+  <Flex gap="gap.medium">
     <Pill appearance="filled">Filled Pill (Default)</Pill>
     <Pill appearance="inverted">Inverted Pill</Pill>
     <Pill appearance="outline">Outlined Pill</Pill>
