faccce594a4a2885a2b8af72877bc7e087f451e6
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-19 11:26:56 -0300

    feat(Pills): Address review comments

--- packages/fluentui/react-northstar/src/components/Pill/Pill.tsx
+++ packages/fluentui/react-northstar/src/components/Pill/Pill.tsx
@@ -1,17 +1,8 @@
-import * as customPropTypes from '@fluentui/react-proptypes';
-import * as _ from 'lodash';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-
-import {
-  UIComponentProps,
-  ContentComponentProps,
-  commonPropTypes,
-  createShorthandFactory,
-  SizeValue,
-  createShorthand,
-} from '../../utils';
-import { ComponentEventHandler, ShorthandValue, FluentComponentStaticProps } from '../../types';
+import * as customPropTypes from '@fluentui/react-proptypes';
+import { UIComponentProps, ContentComponentProps, commonPropTypes, SizeValue, createShorthand } from '../../utils';
+import { ShorthandValue, FluentComponentStaticProps } from '../../types';
 import { BoxProps } from '../Box/Box';
 
 import {
@@ -40,22 +31,6 @@ export interface PillProps extends UIComponentProps, ContentComponentProps<Short
    * A Pill can be filled, inverted or outline
    */
   appearance?: 'filled' | 'inverted' | 'outline';
-
-  /**
-   * Media slot
-   */
-  media?: ShorthandValue<BoxProps>;
-
-  /**
-   * Details that will appear as second line
-   */
-  details?: ShorthandValue<BoxProps>;
-
-  /** A Pill can be disabled. */
-  disabled?: boolean;
-
-  /** A Pill can be selected. */
-  selected?: boolean;
 }
 
 export type PillStylesProps = Required<Pick<PillProps, 'appearance' | 'size' | 'rounded'>>;
@@ -71,7 +46,7 @@ export const Pill: ComponentWithAs<'span', PillProps> & FluentComponentStaticPro
   const { setStart, setEnd } = useTelemetry(Pill.displayName, context.telemetry);
   setStart();
 
-  const { className, design, styles, variables, appearance, size, rounded, children } = props;
+  const { className, design, styles, variables, appearance, size, rounded, children, content } = props;
 
   const ElementType = getElementType(props);
   const unhandledProps = useUnhandledProps(Pill.handledProps, props);
@@ -98,33 +73,19 @@ export const Pill: ComponentWithAs<'span', PillProps> & FluentComponentStaticPro
     rtl: context.rtl,
   });
 
-  const handleClick = (e: React.SyntheticEvent, props: PillProps) => {
-    _.invoke(props, 'onClick', e, props);
-  };
-
-  const handleFocus = (e: React.SyntheticEvent) => {
-    _.invoke(props, 'onFocus', e, props);
-  };
-
   const element = getA11yProps.unstable_wrapWithFocusZone(
     <ElementType
       {...getA11yProps('root', {
         className: classes.root,
-        onFocus: handleFocus,
-        onClick: handleClick,
         ...unhandledProps,
       })}
     >
-      {createShorthand(
-        PillContent,
-        {},
-        {
-          defaultProps: () => ({
-            children,
-            size,
-          }),
-        },
-      )}
+      {createShorthand(PillContent, content, {
+        defaultProps: () => ({
+          children,
+          size,
+        }),
+      })}
     </ElementType>,
   );
 
@@ -135,25 +96,17 @@ export const Pill: ComponentWithAs<'span', PillProps> & FluentComponentStaticPro
 
 Pill.defaultProps = {
   as: 'span',
+  content: {},
 };
 
 Pill.propTypes = {
-  ...commonPropTypes.createCommon({
-    content: false,
-  }),
+  ...commonPropTypes.createCommon(),
+  content: customPropTypes.shorthandAllowingChildren,
   size: PropTypes.oneOf(['small', 'smaller', 'medium']),
   rounded: PropTypes.bool,
   appearance: PropTypes.oneOf(['filled', 'inverted', 'outline']),
-  media: customPropTypes.itemShorthand,
-  selected: PropTypes.bool,
-  details: customPropTypes.itemShorthand,
-  disabled: PropTypes.bool,
 };
 
 Pill.displayName = 'Pill';
 
 Pill.handledProps = Object.keys(Pill.propTypes) as any;
-
-Pill.create = createShorthandFactory({
-  Component: Pill,
-});
--- packages/fluentui/react-northstar/src/themes/teams/components/Pill/pillStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Pill/pillStyles.ts
@@ -7,6 +7,9 @@ export const pillStyles: ComponentSlotStylesPrepared<PillStylesProps, PillVariab
   root: ({ props: p, variables: v, theme: { siteVariables } }): ICSSInJSStyle => {
     return {
       display: 'inline-flex',
+      width: 'fit-content',
+      height: v.height,
+      maxHeight: v.height,
       borderRadius: v.borderRadiusCircular,
       background: v.background,
       margin: `${v.horizontalMargin} ${v.verticalMargin}`,
@@ -26,11 +29,15 @@ export const pillStyles: ComponentSlotStylesPrepared<PillStylesProps, PillVariab
       ...(p.size === 'small' && {
         minWidth: v.minWidthSmall,
         margin: `${v.horizontalMarginSmall} ${v.verticalMarginSmall}`,
+        height: v.heightSmall,
+        maxHeight: v.heightSmall,
       }),
 
       ...(p.size === 'medium' && {
         minWidth: v.minWidthMedium,
         margin: `${v.horizontalMarginMedium} ${v.verticalMarginMedium}`,
+        height: v.heightMedium,
+        maxHeight: v.heightMedium,
       }),
 
       ...getBorderFocusStyles({ variables: siteVariables }),
