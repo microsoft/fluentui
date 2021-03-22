8a84e070bbfe071b442951a80625501a27b63b48
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-22 10:16:00 -0300

    feat(Pills): update example

--- packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleRounded.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Pill } from '@fluentui/react-northstar';
-
-const PillRoundedExample = () => <Pill rounded>Pill Content</Pill>;
-
-export default PillRoundedExample;
