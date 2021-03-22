917375b37baeb79a73ed5b74761a491f22db2ded
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-22 07:01:55 -0300

    feat(Pills): update example

--- packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleSizes.tsx
+++ packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleSizes.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import { Pill, Flex, PillProps } from '@fluentui/react-northstar';
 
 const PillSizesExample = () => (
-  <Flex gap="gap.medium" column>
-    <Flex gap="gap.medium">
-      {['smaller', 'small', 'medium'].map((size: PillProps['size']) => (
-        <Pill key={size} size={size}>
-          {size} pill
-        </Pill>
-      ))}
-    </Flex>
+  <Flex gap="gap.medium">
+    {['smaller', 'small', 'medium'].map((size: PillProps['size']) => (
+      <Pill key={size} size={size}>
+        {size} pill
+      </Pill>
+    ))}
   </Flex>
 );
 
