c644196f37bfd52b1ad36c2abb88dfbef6b2ac6a
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-24 21:26:17 +0000

    fix export

--- packages/react-utilities/src/index.ts
+++ packages/react-utilities/src/index.ts
@@ -2,4 +2,4 @@ export * from './compose/index';
 export * from './descendants/index';
 export * from './hooks/index';
 export * from './utils/index';
-export * from './descendants';
+export * from './descendants/index';
