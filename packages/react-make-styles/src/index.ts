82897e01ceba9e49b9cfd9a27ea33e04c68b305a
Oleksandr Fediashov <olfedias@microsoft.com>
2021-03-24 17:28:12 +0100

    fix test, add export to avoid explosion in all components

--- packages/react-make-styles/src/index.ts
+++ packages/react-make-styles/src/index.ts
@@ -2,4 +2,8 @@ export { createDOMRenderer } from '@fluentui/make-styles';
 
 export { makeStyles } from './makeStyles';
 export { makeStaticStyles } from './makeStaticStyles';
-export { useAx } from './useAx';
+export {
+  useAx,
+  // TODO: this export will be removed in a follow up PR
+  useAx as ax,
+} from './useAx';
