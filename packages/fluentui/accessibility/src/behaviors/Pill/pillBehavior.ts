354835598c278ae2a9a45bab8ebf30201292bcc7
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-03-25 09:19:35 -0300

    feat(Pill): remove docblock

--- packages/fluentui/accessibility/src/behaviors/Pill/pillBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Pill/pillBehavior.ts
@@ -1,11 +1,6 @@
 import { Accessibility } from '../../types';
 import { IS_FOCUSABLE_ATTRIBUTE } from '../../attributes';
 
-/**
- * @description
- * Add tabIndex to 0 if actionable and -1 if not.
- * Set role to button if actionable and to none if not.
- */
 export const pillBehavior: Accessibility<PillBehaviorProps> = p => ({
   attributes: {
     root: {
