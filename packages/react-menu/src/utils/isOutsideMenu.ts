29f9dbdc5a608a79cef0d235c579abe6416222d9
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-25 09:00:32 +0000

    add doc

--- packages/react-menu/src/utils/isOutsideMenu.ts
+++ packages/react-menu/src/utils/isOutsideMenu.ts
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { MenuContextValue } from '../contexts/menuContext';
 
+/**
+ * Helper function that checks if a focus event moves focus outside of the menu trigger and menu popup
+ */
 export const isOutsideMenu = ({
   triggerRef,
   menuPopupRef,
