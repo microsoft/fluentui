f629ad0b758e2f26646ecbf155f225b5627f584a
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-26 09:14:32 +0000

    add menu trigger context

--- packages/react-menu/src/components/MenuItem/useMenuItemOnClickDismiss.ts
+++ packages/react-menu/src/components/MenuItem/useMenuItemOnClickDismiss.ts
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { useEventCallback } from '@fluentui/react-utilities';
 import { useMenuContext } from '../../contexts/menuContext';
+import { useMenuTriggerContext } from '../../contexts/menuTriggerContext';
 
 /**
  * Helper hook that adds menu dismiss to a menu item's onClick handler
  */
 export const useMenuItemOnClickDismiss = <T extends React.HTMLAttributes<HTMLElement>>(state: T) => {
   const setOpen = useMenuContext(context => context.setOpen);
+  const isMenuTrigger = useMenuTriggerContext();
 
   const { onClick: onClickOriginal } = state;
   state.onClick = useEventCallback((e: React.MouseEvent<HTMLElement>) => {
-    setOpen(false);
+    if (!isMenuTrigger) {
+      setOpen(false);
+    }
 
     // Call original onClick after above
     // MenuTrigger will override the click on dismiss behaviour if the menu item is a trigger for a submenu
