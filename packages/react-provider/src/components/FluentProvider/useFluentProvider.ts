7f47ac5c7202096a4498cbf39f56e1398528c66b
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-03-29 09:23:37 -0300

    Fix Lint errors

--- packages/react-provider/src/components/FluentProvider/useFluentProvider.ts
+++ packages/react-provider/src/components/FluentProvider/useFluentProvider.ts
@@ -32,8 +32,11 @@ export const useFluentProvider = (
   );
 
   const parentContext = useFluent();
-  // TODO: add merge functions to "dir" merge, nesting providers with the same "dir" should not add additional attributes to DOM
-  // see https://github.com/microsoft/fluentui/blob/0dc74a19f3aa5a058224c20505016fbdb84db172/packages/fluentui/react-northstar/src/utils/mergeProviderContexts.ts#L89-L93
+  /**
+   * TODO: add merge functions to "dir" merge,
+   * nesting providers with the same "dir" should not add additional attributes to DOM
+   * see https://github.com/microsoft/fluentui/blob/0dc74a19f3aa5a058224c20505016fbdb84db172/packages/fluentui/react-northstar/src/utils/mergeProviderContexts.ts#L89-L93
+   */
   state.document = state.document ?? parentContext.document;
   state.dir = state.dir ?? parentContext.dir;
 
