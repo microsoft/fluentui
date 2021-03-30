290bf77e7647715dc8bddd12579419804f2d0fca
Charles Assunção <junioassuncaocharles@gmail.com>
2021-03-30 12:15:55 -0300

    feat(Pill): add Pills container (#17601)
    * feat(Pill): add Pills container
    
    * feat(Pill): add Pills example
    
    * Update packages/fluentui/docs/src/examples/components/Pill/Types/index.tsx
    
    Co-authored-by: ling1726 <lingfangao@hotmail.com>
    
    * feat(Pill): use native context
    
    * feat(Pill): export PillsStylesProps in types
    
    * feat(Pill): memo childProps
    
    * feat(Pill): use childbeaviors
    
    * feat(Pill): remove missing examples warning
    
    * Update packages/fluentui/react-northstar/src/components/Pill/Pills.tsx
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * feat(Pill): remove esc handle
    
    * feat(Pill): context type
    
    * feat(Pill): fix behavior
    
    * feat(Pill): rename Pills
    
    * feat(Pill): rename Pills
    
    * feat(Pill): added aria label
    
    * feat(Pill): added best practices
    
    * feat(Pill): childBehaviors?
    
    * feat(Pill): add changelog
    
    * feat(Pill): classname
    
    * feat(Pill): behavior test depending on context
    
    Co-authored-by: ling1726 <lingfangao@hotmail.com>
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/accessibility/src/behaviors/Pill/pillOptionBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Pill/pillOptionBehavior.ts
@@ -2,12 +2,11 @@ import { Accessibility, AriaRole } from '../../types';
 import { keyboardKey } from '../../keyboard-key';
 import { IS_FOCUSABLE_ATTRIBUTE } from '../../attributes';
 
-export const pillBehavior: Accessibility<PillBehaviorProps> = p => ({
+export const pillOptionBehavior: Accessibility<PillBehaviorProps> = p => ({
   attributes: {
     root: {
-      role: p.actionable ? 'button' : 'none',
-      tabIndex: p.actionable ? 0 : -1,
-      [IS_FOCUSABLE_ATTRIBUTE]: p.actionable || p.role === 'option',
+      role: 'option',
+      [IS_FOCUSABLE_ATTRIBUTE]: true,
     },
   },
   keyActions: {
