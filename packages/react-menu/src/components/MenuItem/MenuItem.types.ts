8e1f490aca93bf46c8c168c61a9bf8aff0f4de1a
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-31 16:10:03 +0100

    Add slots for menu item content

--- packages/react-menu/src/components/MenuItem/MenuItem.types.ts
+++ packages/react-menu/src/components/MenuItem/MenuItem.types.ts
@@ -13,6 +13,16 @@ export interface MenuItemProps extends ComponentProps, React.HTMLAttributes<HTML
   submenuIndicator?: ShorthandProps<React.HTMLAttributes<HTMLElement>>;
 
   /**
+   * Slot for the component children, avoid in favour of children and classnames for customization
+   */
+  content?: ShorthandProps<React.HTMLAttributes<HTMLElement>>;
+
+  /**
+   * Secondary content rendered opposite the primary content (e.g Shortcut text)
+   */
+  secondaryContent?: ShorthandProps<React.HTMLAttributes<HTMLElement>>;
+
+  /**
    * If the menu item is a trigger for a submenu
    */
   hasSubmenu?: boolean;
@@ -28,6 +38,7 @@ export interface MenuItemState extends MenuItemProps {
    * Ref to the root slot
    */
   ref: React.MutableRefObject<HTMLElement>;
+
   /**
    * Icon slot when processed by internal state
    */
@@ -37,4 +48,14 @@ export interface MenuItemState extends MenuItemProps {
    * Icon slot that shows the indicator for a submenu
    */
   submenuIndicator?: ObjectShorthandProps<React.HTMLAttributes<HTMLElement>>;
+
+  /**
+   * Slot for the component children, avoid in favour of children and classnames for customization
+   */
+  content: ObjectShorthandProps<React.HTMLAttributes<HTMLElement>>;
+
+  /**
+   * Secondary content rendered opposite the primary content (e.g Shortcut text)
+   */
+  secondaryContent: ObjectShorthandProps<React.HTMLAttributes<HTMLElement>>;
 }
