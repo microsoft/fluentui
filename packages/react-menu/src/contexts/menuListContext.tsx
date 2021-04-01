238367897e2ed562be34d672d406b2dcaa2772ad
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-01 14:51:53 +0100

    Bump react-menu to alpha and preview
    * Adds missing context exports to react-menu
    * Bumps react-menu to v9-alpha
    * Exports react-menu from react-components

--- packages/react-menu/src/contexts/menuListContext.tsx
+++ packages/react-menu/src/contexts/menuListContext.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createContext, useContextSelector, ContextSelector } from '@fluentui/react-context-selector';
-import { SelectableHandler } from './selectable/index';
+import { SelectableHandler } from '../selectable/index';
 
 const MenuListContext = createContext<MenuListContextValue>({
   checkedValues: {},
