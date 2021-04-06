b997279cbd1d86b205f8a57c99e5e68f13af1e53
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-06 16:43:19 +0200

    fix ax() call

--- packages/react-menu/src/utils/DefaultIcons.tsx
+++ packages/react-menu/src/utils/DefaultIcons.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ax, makeStyles } from '@fluentui/react-make-styles';
+import { makeStyles, useAx } from '@fluentui/react-make-styles';
 import { getNativeProps, htmlElementProperties } from '@fluentui/react-utilities';
 
 const useStyles = makeStyles({
@@ -48,7 +48,7 @@ export const renderIcon = (
     {
       ...containerProps,
       ...nativeProps,
-      className: ax(rootClasses, props.className),
+      className: useAx(rootClasses, props.className),
     },
     <SVGElement svgClasses={svgClasses} />,
   );
