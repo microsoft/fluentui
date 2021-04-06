aa9c8ebb50c8a119a4d4f0acad37aacedb55e6d1
Tristan <tristan.watanabe@gmail.com>
2021-04-05 23:41:18 -0400

    SplitButton: when primary button is disabled, menu button is now focusable and explicitly added to tab order (#17698)
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Fixes [bug-11037](https://dev.azure.com/microsoftdesign/fluent-ui/_workitems/edit/11037)
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    - Added conditional which removes `tabIndex` from splitButton div container if `primaryDisabled` .
    - When `primaryDisabled`, the menu button's `tabIndex` is set to 0.
    - Added `splitButtonMenuFocused` styling to add focus border to menu button when it's in focus when `primaryDisabled` and the whole button is `!disabled`. 
    
    **Menu Button Keyboard Focus:**
    ![splitbutton-focus-1](https://user-images.githubusercontent.com/8649804/113614802-d0fb4e00-9607-11eb-8aff-c51fd506bf9c.JPG)
    **Menu Button Keyboard Activate:**
    ![splitbutton-focus-2](https://user-images.githubusercontent.com/8649804/113616068-7c58d280-9609-11eb-864a-3cfadcbe68a7.JPG)

--- packages/react/src/components/Button/BaseButton.tsx
+++ packages/react/src/components/Button/BaseButton.tsx
@@ -629,7 +629,7 @@ export class BaseButton extends React.Component<IBaseButtonProps, IBaseButtonSta
         ref={this._splitButtonContainer}
         data-is-focusable={true}
         onClick={!disabled && !primaryDisabled ? this._onSplitButtonPrimaryClick : undefined}
-        tabIndex={!disabled || allowDisabledFocus ? 0 : undefined}
+        tabIndex={(!disabled && !primaryDisabled) || allowDisabledFocus ? 0 : undefined}
         aria-roledescription={buttonProps['aria-roledescription']}
         onFocusCapture={this._onSplitContainerFocusCapture}
       >
@@ -689,7 +689,14 @@ export class BaseButton extends React.Component<IBaseButtonProps, IBaseButtonSta
     classNames: ISplitButtonClassNames | undefined,
     keytipAttributes: any,
   ): JSX.Element {
-    const { allowDisabledFocus, checked, disabled, splitButtonMenuProps, splitButtonAriaLabel } = this.props;
+    const {
+      allowDisabledFocus,
+      checked,
+      disabled,
+      splitButtonMenuProps,
+      splitButtonAriaLabel,
+      primaryDisabled,
+    } = this.props;
     const { menuHidden } = this.state;
     let menuIconProps = this.props.menuIconProps;
 
@@ -720,7 +727,7 @@ export class BaseButton extends React.Component<IBaseButtonProps, IBaseButtonSta
         {...splitButtonProps}
         data-ktp-execute-target={keytipAttributes ? keytipAttributes['data-ktp-execute-target'] : keytipAttributes}
         onMouseDown={this._onMouseDown}
-        tabIndex={-1}
+        tabIndex={primaryDisabled && !allowDisabledFocus ? 0 : -1}
       />
     );
   }
