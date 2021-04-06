c0b551a0e0818759aaa3e92055fd07c6e42047d3
Charles Assunção <junioassuncaocharles@gmail.com>
2021-04-06 08:24:07 -0300

    Update packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleSelectable.tsx
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleSelectable.tsx
+++ packages/fluentui/docs/src/examples/components/Pill/Variations/PillExampleSelectable.tsx
@@ -4,7 +4,7 @@ import { Pill, CalendarIcon, Flex } from '@fluentui/react-northstar';
 const PillSelectableExample = () => (
   <Flex>
     <Pill selectable image="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobertTolbert.jpg">
-      Pill with image
+      Pill with an image
     </Pill>
     <Pill selectable icon={<CalendarIcon />}>
       Pill with image
