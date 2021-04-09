26b6757beba3f3bf9abd8f1d13ae2ea87c686bc9
André Dias <andredias@microsoft.com>
2021-04-09 19:04:31 +0200

    Update apps/vr-tests/screener.local.config.js
    Co-authored-by: Elizabeth Craig <ecraig12345@gmail.com>

--- apps/vr-tests/screener.local.config.js
+++ apps/vr-tests/screener.local.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   projectRepo: 'microsoft/fluentui',
   storybookConfigDir: '.storybook',
-  apiKey: '[Screener Key here]',
+  apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
   baseBranch: 'master',
   branch: 'localTest',
