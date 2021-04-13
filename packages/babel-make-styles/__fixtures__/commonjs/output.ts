d5f0563c477e3b784b1f66021a10d08d4cb1db86
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-13 17:31:30 +0200

    add missing fixture

---
+++ packages/babel-make-styles/__fixtures__/commonjs/output.ts
@@ -0,0 +1,12 @@
+import { Theme } from '@fluentui/react-theme';
+
+const react_make_styles_1 = require('@fluentui/react-make-styles');
+
+const useStyles = react_make_styles_1.prebuildStyles({
+  root: {
+    fontSize: ['', 'flcnb000', '.flcnb000{font-size:var(--global-type-fontSizes-base-300);}'],
+    lineHeight: ['', 'f1syuwty', '.f1syuwty{line-height:var(--global-type-lineHeights-base-300);}'],
+    fontWeight: ['', 'f1du0uop', '.f1du0uop{font-weight:var(--global-type-fontWeights-regular);}'],
+  },
+});
+console.log(useStyles);
