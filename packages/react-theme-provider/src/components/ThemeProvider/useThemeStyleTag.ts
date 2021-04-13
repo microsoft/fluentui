57f40e97ea3271c5151acde3a84cbca2c0131c70
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-13 10:53:23 +0200

    use targetDocument internally

--- packages/react-theme-provider/src/components/ThemeProvider/useThemeStyleTag.ts
+++ packages/react-theme-provider/src/components/ThemeProvider/useThemeStyleTag.ts
@@ -4,14 +4,17 @@ import { themeToCSSVariables } from '@fluentui/react-theme';
 import { ThemeProviderState } from './ThemeProvider.types';
 
 /**
- * Writes a theme as css variables in a style tag on the provided document as a rule applied to a CSS class
+ * Writes a theme as css variables in a style tag on the provided targetDocument as a rule applied to a CSS class
  *
  * @param theme Theme values that are written as css variables in style rule
  * @returns CSS class to apply the rule
  */
-export const useThemeStyleTag = (theme: ThemeProviderState['theme'], document: ThemeProviderState['document']) => {
+export const useThemeStyleTag = (
+  theme: ThemeProviderState['theme'],
+  targetDocument: ThemeProviderState['document'],
+) => {
   const styleTagId = useId('theme-provider');
-  const styleTag = getOrCreateStyleTag(styleTagId);
+  const styleTag = getOrCreateStyleTag(styleTagId, targetDocument);
 
   const cssRule = React.useMemo(() => {
     const cssVars = themeToCSSVariables(theme);
@@ -25,7 +28,7 @@ export const useThemeStyleTag = (theme: ThemeProviderState['theme'], document: T
   }, [theme, styleTagId]);
 
   useIsomorphicLayoutEffect(() => {
-    if (document === null || document === undefined) {
+    if (!targetDocument || !styleTag) {
       return;
     }
     const sheet = styleTag.sheet as CSSStyleSheet;
@@ -37,10 +40,10 @@ export const useThemeStyleTag = (theme: ThemeProviderState['theme'], document: T
     sheet.insertRule(cssRule, 0);
   }, [cssRule, styleTag]);
 
-  // Removes the style tag from the document on unmount or if the id of the tag changes
+  // Removes the style tag from the targetDocument on unmount or if the id of the tag changes
   useIsomorphicLayoutEffect(() => {
     return () => {
-      const styleTagCleanup = document?.getElementById(styleTagId);
+      const styleTagCleanup = targetDocument?.getElementById(styleTagId);
       if (styleTagCleanup) {
         // IE11 safe node removal, otherwise use node.remove()
         styleTagCleanup.parentElement?.removeChild(styleTagCleanup);
@@ -51,14 +54,18 @@ export const useThemeStyleTag = (theme: ThemeProviderState['theme'], document: T
   return styleTagId;
 };
 
-const getOrCreateStyleTag = (id: string): HTMLStyleElement => {
-  let styleTag = document.getElementById(id) as HTMLStyleElement;
+const getOrCreateStyleTag = (id: string, targetDocument: ThemeProviderState['document']): HTMLStyleElement | null => {
+  if (!targetDocument) {
+    return null;
+  }
+
+  let styleTag = targetDocument.getElementById(id) as HTMLStyleElement;
   if (styleTag) {
     return styleTag;
   }
 
-  styleTag = document.createElement('style');
+  styleTag = targetDocument.createElement('style');
   styleTag.setAttribute('id', id);
-  document.head.appendChild(styleTag);
+  targetDocument.head.appendChild(styleTag);
   return styleTag;
 };
