fd4ec8e45bf5e8ca1638a2ae2a11250f5338748d
Ben Howell <48106640+behowell@users.noreply.github.com>
2021-04-15 02:53:28 -0700

    Apply suggestions from code review
    Co-authored-by: Elizabeth Craig <ecraig12345@gmail.com>

--- packages/react-tooltip/src/common/isConformant.ts
+++ packages/react-tooltip/src/common/isConformant.ts
@@ -1,10 +1,12 @@
 import { isConformant as baseIsConformant, IsConformantOptions } from '@fluentui/react-conformance';
 
-export function isConformant<TProps>(testInfo: Omit<IsConformantOptions<TProps>, 'componentPath'>) {
-  const defaultOptions = {
+export function isConformant<TProps = {}>(
+  testInfo: Omit<IsConformantOptions<TProps>, 'componentPath'> & { componentPath?: string },
+) {
+  const defaultOptions: Partial<IsConformantOptions<TProps>> = {
     asPropHandlesRef: true,
     componentPath: module!.parent!.filename.replace('.test', ''),
   };
 
-  baseIsConformant<TProps>(defaultOptions, testInfo);
+  baseIsConformant(defaultOptions, testInfo);
 }
