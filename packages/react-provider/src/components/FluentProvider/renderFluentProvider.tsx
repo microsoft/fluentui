cd0cdb18546e8f62d07a5807f77ccd6cc16c23d5
Ben Howell <48106640+behowell@users.noreply.github.com>
2021-04-15 16:51:33 -0700

    Implement Tooltip (#17812)

--- packages/react-provider/src/components/FluentProvider/renderFluentProvider.tsx
+++ packages/react-provider/src/components/FluentProvider/renderFluentProvider.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { FluentProviderState } from './FluentProvider.types';
 import { ProviderContext } from '@fluentui/react-shared-contexts';
+import { TooltipProvider } from '@fluentui/react-tooltip';
 import { TabsterProvider } from '@fluentui/react-tabster';
 import { ThemeProvider } from '@fluentui/react-theme-provider';
 
@@ -16,9 +17,11 @@ export const renderFluentProvider = (state: FluentProviderState) => {
   return (
     <ThemeProvider {...state} theme={theme}>
       <ProviderContext.Provider value={value}>
-        <TabsterProvider document={document} dir={dir}>
-          {state.children}
-        </TabsterProvider>
+        <TooltipProvider>
+          <TabsterProvider document={document} dir={dir}>
+            {state.children}
+          </TabsterProvider>
+        </TooltipProvider>
       </ProviderContext.Provider>
     </ThemeProvider>
   );
