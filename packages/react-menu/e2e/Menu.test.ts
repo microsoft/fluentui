01d7942fb0da8a70e894204684c92b9a95b8fb3e
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-19 19:31:46 +0200

    add tests

--- packages/react-menu/e2e/Menu.test.ts
+++ packages/react-menu/e2e/Menu.test.ts
@@ -1,10 +1,43 @@
-describe('MenuList', () => {
-  it('should open item by clicking on trigger', () => {
+describe('Menu', () => {
+  it('should open when trigger is clicked', () => {
+    cy.visitStory('Menu', 'TextOnly')
+      .get('[aria-haspopup="true"]')
+      .click()
+      .get('[role="menu"]')
+      .should('be.visible');
+  });
+
+  ['downarrow', 'enter', ' '].forEach(key => {
+    it(`should open when ${key === ' ' ? 'space' : key} key is used with trigger`, () => {
+      cy.visitStory('Menu', 'TextOnly')
+        .get('[aria-haspopup="true"]')
+        .focus()
+        .type(`{${key}}`)
+        .get('[role="menu"]')
+        .should('be.visible');
+    });
+  });
+
+  it('should close when a menu item is clicked', () => {
     cy.visitStory('Menu', 'TextOnly')
       .get('[aria-haspopup="true"]')
       .trigger('click')
-      .get('[role="menu"]')
-      .should('be.visible');
+      .get('[role="menuitem"]')
+      .first()
+      .click();
+
+    cy.get('[role="menu"]').should('not.be.exist');
+  });
+
+  it('should not close when a disabled menu item is clicked', () => {
+    cy.visitStory('Menu', 'TextOnly')
+      .get('[aria-haspopup="true"]')
+      .trigger('click')
+      .get('[aria-disabled="true"]')
+      .first()
+      .click();
+
+    cy.get('[role="menu"]').should('be.visible');
   });
 
   it('should focus each menu item on hover', () => {
