0435f60ed0441efa576543b261ee33270b3aa586
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-19 10:46:37 +0200

    add test

--- packages/react-menu/e2e/Menu.test.ts
+++ packages/react-menu/e2e/Menu.test.ts
@@ -1,4 +1,12 @@
 describe('MenuList', () => {
+  it('should open item by clicking on trigger', () => {
+    cy.visitStory('Menu', 'TextOnly')
+      .get('[aria-haspopup="true"]')
+      .trigger('click')
+      .get('[role="menu"]')
+      .should('be.visible');
+  });
+
   it('should focus each menu item on hover', () => {
     cy.visitStory('Menu', 'TextOnly')
       .get('[aria-haspopup="true"]')
