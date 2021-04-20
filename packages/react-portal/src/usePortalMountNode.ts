ccdb146eb2a43431a9a9067428c3a0b7029a74f1
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-20 08:41:21 +0200

    remove guard

--- packages/react-portal/src/usePortalMountNode.ts
+++ packages/react-portal/src/usePortalMountNode.ts
@@ -24,7 +24,7 @@ export const usePortalMountNode = (options: UsePortalMountNodeOptions) => {
     const newElement = targetDocument.createElement('div');
     newElement.setAttribute('class', themeClassName);
     newElement.setAttribute('dir', dir);
-    targetDocument?.body.appendChild(newElement);
+    targetDocument.body.appendChild(newElement);
 
     return newElement;
   }, [targetDocument, themeClassName, dir, options.disabled]);
