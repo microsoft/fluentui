093f6a387560630de1e0d0b02c6bb94ca2a59487
Chris Holt <chhol@microsoft.com>
2021-04-20 14:03:58 -0700

    correct style exports

--- packages/web-components/src/accordion/accordion-item/index.ts
+++ packages/web-components/src/accordion/accordion-item/index.ts
@@ -1,26 +1,23 @@
-import {
-  AccordionItem,
-  accordionItemTemplate as template,
-} from "@microsoft/fast-foundation";
-import { accordionItemStyles as styles } from "./accordion-item.styles";
+import { AccordionItem, accordionItemTemplate as template } from '@microsoft/fast-foundation';
+import { accordionItemStyles as styles } from './accordion-item.styles';
 
 /**
-* The Fluent Accordion Item Element. Implements {@link @microsoft/fast-foundation#AccordionItem},
-* {@link @microsoft/fast-foundation#accordionItemTemplate}
-*
-*
-* @public
-* @remarks
-* HTML Element: \<fast-accordion-item\>
-*/
+ * The Fluent Accordion Item Element. Implements {@link @microsoft/fast-foundation#AccordionItem},
+ * {@link @microsoft/fast-foundation#accordionItemTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<fast-accordion-item\>
+ */
 export const fluentAccordionItem = AccordionItem.compose({
-  baseName: "accordion-item",
+  baseName: 'accordion-item',
   template,
   styles,
 });
 
 /**
-* Styles for AccordionItem
-* @public
-*/
-export const AccordionItemStyles = styles;
+ * Styles for AccordionItem
+ * @public
+ */
+export const accordionItemStyles = styles;
--- packages/web-components/src/accordion/index.ts
+++ packages/web-components/src/accordion/index.ts
@@ -1,7 +1,7 @@
-import { Accordion, accordionTemplate as template } from "@microsoft/fast-foundation";
-import { accordionStyles as styles } from "./accordion.styles";
+import { Accordion, accordionTemplate as template } from '@microsoft/fast-foundation';
+import { accordionStyles as styles } from './accordion.styles';
 
-export * from "../accordion-item/index";
+export * from '../accordion-item/index';
 
 /**
  * The Fluent Accordion Element. Implements {@link @microsoft/fast-foundation#Accordion},
@@ -13,13 +13,13 @@ export * from "../accordion-item/index";
  * HTML Element: \<fast-accordion\>
  */
 export const fluentAccordion = Accordion.compose({
-    baseName: "accordion",
-    template,
-    styles,
+  baseName: 'accordion',
+  template,
+  styles,
 });
 
 /**
  * Styles for Accordion
  * @public
  */
-export const AccordionStyles = styles;
+export const accordionStyles = styles;
