42b061df458ecc17b4710bfa03c4e45ae747cfce
Chris Holt <chhol@microsoft.com>
2021-04-20 14:23:27 -0700

    update checkbox to extend foundation element

--- packages/web-components/src/checkbox/index.ts
+++ packages/web-components/src/checkbox/index.ts
@@ -1,28 +1,23 @@
-import { customElement } from '@microsoft/fast-element';
 import { Checkbox, CheckboxTemplate as template } from '@microsoft/fast-foundation';
-import { CheckboxStyles as styles } from './checkbox.styles';
+import { checkboxStyles as styles } from './checkbox.styles';
 
 /**
  * The Fluent Checkbox Element. Implements {@link @microsoft/fast-foundation#Checkbox},
- * {@link @microsoft/fast-foundation#CheckboxTemplate}
+ * {@link @microsoft/fast-foundation#checkboxTemplate}
  *
  *
  * @public
  * @remarks
- * HTML Element: \<fluent-checkbox\>
+ * HTML Element: \<fast-checkbox\>
  */
-@customElement({
-  name: 'fluent-checkbox',
+export const fastCheckbox = Checkbox.compose({
+  baseName: 'checkbox',
   template,
   styles,
-  shadowOptions: {
-    mode: 'closed',
-  },
-})
-export class FluentCheckbox extends Checkbox {}
+});
 
 /**
  * Styles for Checkbox
  * @public
  */
-export const CheckboxStyles = styles;
+export const checkboxStyles = styles;
