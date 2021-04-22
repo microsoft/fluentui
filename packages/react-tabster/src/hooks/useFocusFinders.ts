e9ef2a1d7e2d57a5b52642dd35243cb6a8600237
ling1726 <lingfan.gao@microsoft.com>
2021-04-22 13:34:48 +0200

    Replace `TabsterProvider` with `useTabster` (#17885)
    * Replace `TabsterProvider` with `useTabster`
    
    replaces `TabsterProvider` with the `useTabster` hook and removes the
    `react-tabster` dep from `react-provider`
    
    Open to opinions on whether to make `useTabster` public
    
    * use hook
    
    * remove provider code
    
    * update md
    
    * Change files
    
    * comments
    
    * Add types
    
    * remove dep from test
    
    * useTabster public, update accordion
    
    * add comment
    
    * Change files
    
    * useTabsterAttrbiutes
    
    * useTabsterAttribute more
    
    * fix tsdoc warning

--- packages/react-tabster/src/hooks/useFocusFinders.ts
+++ packages/react-tabster/src/hooks/useFocusFinders.ts
@@ -1,16 +1,16 @@
-import { useTabsterContext } from '../TabsterContext';
+import { useTabster } from './useTabster';
 
 /**
  * Returns a set of helper functions that will traverse focusable elements in the context of a root DOM element
  */
 export const useFocusFinders = () => {
-  const ctx = useTabsterContext();
+  const tabster = useTabster();
 
   // Narrow props for now and let need dictate additional props in the future
   const findAllFocusable = (root: HTMLElement, matcher: (el: HTMLElement) => boolean) =>
-    ctx?.focusable.findAll(root, matcher) || [];
-  const findFirstFocusable = (root: HTMLElement) => ctx?.focusable.findFirst(root);
-  const findLastFocusable = (root: HTMLElement) => ctx?.focusable.findLast(root);
+    tabster?.focusable.findAll(root, matcher) || [];
+  const findFirstFocusable = (root: HTMLElement) => tabster?.focusable.findFirst(root);
+  const findLastFocusable = (root: HTMLElement) => tabster?.focusable.findLast(root);
 
   return {
     findAllFocusable,
