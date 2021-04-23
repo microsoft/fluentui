32a0b3bdbbc8eec7f21f278a15e130be9d9c943f
Adam <adam.samec@gmail.com>
2021-04-23 14:42:25 +0200

    Make disabled items in menu as toolbar and menu as tablist focusable

--- packages/fluentui/react-northstar-prototypes/src/prototypes/DisabledMenuitem/DisabledMenuitem.tsx
+++ packages/fluentui/react-northstar-prototypes/src/prototypes/DisabledMenuitem/DisabledMenuitem.tsx
@@ -1,7 +1,29 @@
 import * as React from 'react';
-import { Provider, teamsTheme, Menu } from '@fluentui/react-northstar';
+import {
+  Provider,
+  teamsTheme,
+  Menu,
+  tabListBehavior,
+  menuAsToolbarBehavior,
+  MenuItemProps,
+  ShorthandCollection,
+} from '@fluentui/react-northstar';
+import {
+  FormatIcon,
+  PaperclipIcon,
+  EmojiIcon,
+  GiphyIcon,
+  StickerIcon,
+  VideoCameraEmphasisIcon,
+  SettingsIcon,
+  MoreIcon,
+  BookmarkIcon,
+  TranslationIcon,
+  TrashCanIcon,
+  MarkAsUnreadIcon,
+} from '@fluentui/react-icons-northstar';
 
-const items = [
+const menuItems = [
   {
     key: 'item1',
     content: 'Item #1',
@@ -17,13 +39,162 @@ const items = [
   },
 ];
 
-const DisabledMenuitem: React.FunctionComponent = () => {
+const tabItems = [
+  { key: 'editorials', content: 'Editorials' },
+  { key: 'review', content: 'Reviews', disabled: true },
+  { key: 'events', content: 'Upcoming Events' },
+];
 
+const toolbarItems: ShorthandCollection<MenuItemProps> = [
+  {
+    icon: (
+      <FormatIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'format',
+    'aria-label': 'Format Tool',
+  },
+  {
+    icon: (
+      <PaperclipIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'paperclip',
+    'aria-label': 'Paperclip Tool',
+  },
+  {
+    icon: (
+      <EmojiIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'emoji',
+    disabled: true,
+    'aria-label': 'Emoji Tool',
+  },
+  {
+    icon: (
+      <GiphyIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'giphy',
+    'aria-label': 'Giphy tool',
+  },
+  {
+    icon: (
+      <StickerIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'sticker',
+    'aria-label': 'Sticker Tool',
+  },
+  {
+    icon: (
+      <VideoCameraEmphasisIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'meetup',
+    'aria-label': 'Meetup Tool',
+  },
+  {
+    icon: (
+      <SettingsIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'settings',
+    'aria-label': 'Settings',
+  },
+  {
+    icon: (
+      <MoreIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'menuButton2',
+    'aria-label': 'More options',
+    indicator: false,
+    menu: {
+      items: [
+        {
+          key: '5',
+          content: 'item1',
+          icon: <BookmarkIcon outline />,
+        },
+        {
+          key: 'divider',
+          kind: 'divider',
+        },
+        {
+          key: '6',
+          content: 'item2',
+          icon: <MarkAsUnreadIcon outline />,
+        },
+        {
+          key: '7',
+          content: 'item3',
+          disabled: true,
+          icon: <TranslationIcon outline />,
+        },
+        {
+          key: 'divider2',
+          kind: 'divider',
+        },
+        {
+          key: '8',
+          content: 'item3',
+          icon: <TrashCanIcon outline />,
+        },
+      ],
+    },
+  },
+];
+
+const DisabledMenuitem: React.FunctionComponent = () => {
   return (
-                  <Provider theme={teamsTheme}>
-            <h1>Disabled Menuitem Prototype</h1>
-<Menu defaultActiveIndex={0} items={items} />
-                        </Provider>
+    <Provider theme={teamsTheme}>
+      <h1>Disabled Menuitem Prototype</h1>
+
+      <Menu defaultActiveIndex={0} items={menuItems} />
+
+      <Menu
+        defaultActiveIndex={0}
+        items={tabItems}
+        underlined
+        primary
+        accessibility={tabListBehavior}
+        aria-label="Today's events"
+      />
+
+      <Menu
+        defaultActiveIndex={0}
+        items={toolbarItems}
+        iconOnly
+        accessibility={menuAsToolbarBehavior}
+        aria-label="Compose Editor"
+      />
+    </Provider>
   );
 }; // End DisabledMenuitem
 
