c24f7045399a16e4ae7fe905a8417335d8d78d64
Andrew Martin <andmarti@microsoft.com>
2021-04-25 13:42:24 -0700

    updating font

--- packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
+++ packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
@@ -3,23 +3,17 @@ import { FontFaces } from '@fluentui/styles';
 export const fontFaces: FontFaces = [
   {
     name: 'Segoe UI',
-    paths: [
-      'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/test-segoeui-regular.woff2',
-    ],
+    paths: [],
     props: { fontWeight: 400 },
   },
   {
     name: 'Segoe UI',
-    paths: [
-      'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/test-segoeui-semibold.woff2',
-    ],
+    paths: [],
     props: { fontWeight: 600 },
   },
   {
     name: 'Segoe UI',
-    paths: [
-      'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/test-segoeui-bold.woff2',
-    ],
+    paths: [],
     props: { fontWeight: 700 },
   },
 ];
