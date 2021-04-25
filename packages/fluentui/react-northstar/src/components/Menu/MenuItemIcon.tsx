4529c3e904c44d66a43b4d369b9787f89064790f
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-04-25 11:52:17 +0200

    refact(MenuItemIcon): remove compose

--- packages/fluentui/react-northstar/src/components/Menu/MenuItemIcon.tsx
+++ packages/fluentui/react-northstar/src/components/Menu/MenuItemIcon.tsx
@@ -1,7 +1,18 @@
-import { compose } from '@fluentui/react-bindings';
+import * as React from 'react';
+import {
+  ComponentWithAs,
+  useFluentContext,
+  useTelemetry,
+  useStyles,
+  useAccessibility,
+  getElementType,
+  useUnhandledProps,
+  childrenExist,
+} from '@fluentui/react-bindings';
 import * as PropTypes from 'prop-types';
 import { commonPropTypes } from '../../utils';
-import { Box, BoxProps } from '../Box/Box';
+import { BoxProps } from '../Box/Box';
+import { FluentComponentStaticProps } from '../../types';
 
 interface MenuItemIconOwnProps {
   /** Indicates if the parent menu item may have just icons. */
@@ -19,23 +30,61 @@ export const menuItemIconClassName = 'ui-menu__itemicon';
 /**
  * A MenuItemIcon allows a user to have a dedicated component that can be targeted from the theme.
  */
-export const MenuItemIcon = compose<'span', MenuItemIconProps, MenuItemIconStylesProps, BoxProps, {}>(Box, {
-  className: menuItemIconClassName,
-  displayName: 'MenuItemIcon',
-  mapPropsToStylesProps: props => ({ hasContent: props.hasContent, iconOnly: props.iconOnly }),
-  handledProps: ['hasContent', 'iconOnly'],
+export const MenuItemIcon: ComponentWithAs<'span', MenuItemIconProps> &
+  FluentComponentStaticProps<MenuItemIconProps> = props => {
+  const context = useFluentContext();
+  const { setStart, setEnd } = useTelemetry(MenuItemIcon.displayName, context.telemetry);
+  setStart();
 
-  overrideStyles: true,
-  shorthandConfig: {
-    mappedProp: 'content',
-  },
-});
+  const { className, children, design, styles, variables, content, hasContent, iconOnly } = props;
+
+  const { classes } = useStyles<MenuItemIconStylesProps>(MenuItemIcon.displayName, {
+    className: menuItemIconClassName,
+    mapPropsToStyles: () => ({
+      hasContent,
+      iconOnly,
+    }),
+    mapPropsToInlineStyles: () => ({
+      className,
+      design,
+      styles,
+      variables,
+    }),
+    rtl: context.rtl,
+  });
+
+  const getA11Props = useAccessibility(props.accessibility, {
+    debugName: MenuItemIcon.displayName,
+    rtl: context.rtl,
+  });
+
+  const ElementType = getElementType(props);
+  const unhandledProps = useUnhandledProps(MenuItemIcon.handledProps, props);
+
+  const element = (
+    <ElementType {...getA11Props('root', { className: classes.root, ...unhandledProps })}>
+      {childrenExist(children) ? children : content}
+    </ElementType>
+  );
+  setEnd();
+
+  return element;
+};
+
+MenuItemIcon.displayName = 'MenuItemIcon';
 
 MenuItemIcon.defaultProps = {
   as: 'span',
 };
+
 MenuItemIcon.propTypes = {
   ...commonPropTypes.createCommon(),
   hasContent: PropTypes.bool,
   iconOnly: PropTypes.bool,
+};
+
+MenuItemIcon.handledProps = Object.keys(MenuItemIcon.propTypes) as any;
+
+MenuItemIcon.shorthandConfig = {
+  mappedProp: 'content',
 };
