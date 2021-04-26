a325eee4e7975d01feb62ae8411e5f70e71748e0
André Dias <andredias@microsoft.com>
2021-04-26 13:11:29 +0200

    Switch story styles for makeStyles

--- packages/react-examples/src/react-flex/Flex/Flex.stories.tsx
+++ packages/react-examples/src/react-flex/Flex/Flex.stories.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { Flex } from '@fluentui/react-flex';
-import * as classes from './Flex.stories.scss';
 import { select, text, boolean, number } from '@storybook/addon-knobs';
-import {
-  FlexDirectionProperty,
-  JustifyContentProperty,
-  AlignItemsProperty,
-  MarginProperty,
-  GlobalsNumber,
-} from 'csstype';
-import { useState } from '@storybook/addons';
+import { FlexDirectionProperty, JustifyContentProperty, AlignItemsProperty } from 'csstype';
+import { makeStyles } from '@fluentui/react-make-styles';
+
+const useStyles = makeStyles({
+  root: {
+    border: '1px solid black',
+    height: '200px',
+    '> *': {
+      border: '1px solid black',
+      backgroundColor: 'white',
+      padding: '5px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+  },
+});
 
 const directionOptions: FlexDirectionProperty[] = ['row', 'row-reverse', 'column', 'column-reverse'];
 const alignmentOptions: JustifyContentProperty[] | AlignItemsProperty[] = [
@@ -21,19 +28,23 @@ const alignmentOptions: JustifyContentProperty[] | AlignItemsProperty[] = [
   'space-between',
   'space-evenly',
 ];
-export const Default = () => (
-  <Flex
-    direction={select('Direction', directionOptions, 'row')}
-    horizontalAlign={select('Horizontal Alignment', alignmentOptions, 'normal')}
-    verticalAlign={select('Vertical Alignment', alignmentOptions, 'normal')}
-    gap={text('Gap', '10px')}
-    wrap={boolean('Wrap', false)}
-    grow={number('Grow', 0)}
-    shrink={number('Shrink', 1)}
-    inline={boolean('Inline', false)}
-    className={classes.wrapper}
-  >
-    <span>Item A</span>
-    <span>Item B</span>
-  </Flex>
-);
+export const Default = () => {
+  const styles = useStyles();
+
+  return (
+    <Flex
+      direction={select('Direction', directionOptions, 'row')}
+      horizontalAlign={select('Horizontal Alignment', alignmentOptions, 'normal')}
+      verticalAlign={select('Vertical Alignment', alignmentOptions, 'normal')}
+      gap={text('Gap', '10px')}
+      wrap={boolean('Wrap', false)}
+      grow={number('Grow', 0)}
+      shrink={number('Shrink', 1)}
+      inline={boolean('Inline', false)}
+      className={styles.root}
+    >
+      <span>Item A</span>
+      <span>Item B</span>
+    </Flex>
+  );
+};
