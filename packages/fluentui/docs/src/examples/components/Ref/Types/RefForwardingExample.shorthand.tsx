f481168d8ef00cc9f312e41ace84de5275eb26e8
JD Huntington <jdh@microsoft.com>
2021-04-26 20:52:29 +0000

    Revert change, update api docs

--- packages/fluentui/docs/src/examples/components/Ref/Types/RefForwardingExample.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Ref/Types/RefForwardingExample.shorthand.tsx
@@ -5,9 +5,7 @@ type RefForwardingExampleState = {
   isMounted: boolean;
 };
 
-const ExampleButton: React.FunctionComponent<{ children: React.ReactNode } & React.RefAttributes<
-  HTMLButtonElement
->> = React.forwardRef<HTMLButtonElement, { children: React.ReactNode }>((props, ref) => (
+const ExampleButton = React.forwardRef<HTMLButtonElement, { children: React.ReactNode }>((props, ref) => (
   <div>
     <button {...props} ref={ref} />
   </div>
