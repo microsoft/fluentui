cabcb25b3329ad3e8536f798ceaabcd1ee93e1b4
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-27 18:04:37 +0200

    chore: bump tabster in react-tabster (#17971)
    * chore: bump tabster in react-tabster
    
    * Change files
    
    * update API file
    
    * fix types
    
    * fix types

--- packages/react-tabster/src/hooks/useTabsterAttributes.ts
+++ packages/react-tabster/src/hooks/useTabsterAttributes.ts
@@ -1,14 +1,13 @@
-import { getTabsterAttribute } from 'tabster';
+import { getTabsterAttribute, Types as TabsterTypes } from 'tabster';
 import { useTabster } from './useTabster';
 
 /**
  * Hook that returns tabster attributes while ensuring tabster exists
- * @param args - same arguments as `getTabsterAttribute`
  */
-export const useTabsterAttributes = (...args: Parameters<typeof getTabsterAttribute>) => {
+export const useTabsterAttributes = (props: TabsterTypes.TabsterAttributeProps): TabsterTypes.TabsterDOMAttribute => {
   // A tabster instance is not necessary to generate tabster attributes
   // but calling the hook will ensure that a tabster instance exists internally and avoids consumers doing the same
   useTabster();
 
-  return getTabsterAttribute(...args);
+  return getTabsterAttribute(props, false) as TabsterTypes.TabsterDOMAttribute;
 };
