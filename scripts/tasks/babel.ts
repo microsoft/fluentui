5f8f166902704a6ecf281ad1557fe3b1796b02c8
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-28 18:10:49 +0200

    uncomment rc option

--- scripts/tasks/babel.ts
+++ scripts/tasks/babel.ts
@@ -26,7 +26,8 @@ export async function babel() {
 
       babelrc: true,
       // to avoid leaking of global configs
-      // babelrcRoots: [process.cwd()],
+      babelrcRoots: [process.cwd()],
+
       caller: { name: 'just-scripts' },
       filename: filePath,
 
