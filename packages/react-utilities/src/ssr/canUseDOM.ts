10fabaa02866565ef4df5176e8aa91fd24972002
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-28 10:59:20 +0200

    silence ESLint rule

--- packages/react-utilities/src/ssr/canUseDOM.ts
+++ packages/react-utilities/src/ssr/canUseDOM.ts
@@ -2,5 +2,12 @@
  * Verifies if an application can use DOM.
  */
 export function canUseDOM(): boolean {
-  return typeof window !== 'undefined' && !!(window.document && window.document.createElement);
+  return (
+    typeof window !== 'undefined' &&
+    !!(
+      window.document &&
+      // eslint-disable-next-line deprecation/deprecation
+      window.document.createElement
+    )
+  );
 }
