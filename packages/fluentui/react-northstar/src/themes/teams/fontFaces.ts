62f02f7e651325cb4befe0b34facbdc2a25b48dc
Andrew Martin <andmarti@microsoft.com>
2021-04-29 13:49:00 -0700

    updating local reference

--- packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
+++ packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
@@ -3,17 +3,27 @@ import { FontFaces } from '@fluentui/styles';
 export const fontFaces: FontFaces = [
   {
     name: 'Segoe UI',
-    paths: [],
+    paths: ['local("Segoe UI Light")'],
+    props: { fontWeight: 200 },
+  },
+  {
+    name: 'Segoe UI',
+    paths: ['local("Segoe UI Semilight")'],
+    props: { fontWeight: 300 },
+  },
+  {
+    name: 'Segoe UI',
+    paths: ['local("Segoe UI")'],
     props: { fontWeight: 400 },
   },
   {
     name: 'Segoe UI',
-    paths: [],
+    paths: ['local("Segoe UI Semibold")'],
     props: { fontWeight: 600 },
   },
   {
     name: 'Segoe UI',
-    paths: [],
+    paths: ['local("Segoe UI Bold")'],
     props: { fontWeight: 700 },
   },
 ];
