ea858c18477ea46c331078ff2cbf6979f4c4bf68
Lingfan Gao <lingfan.gao@microsoft.com>
2021-04-30 15:50:43 +0200

    remove failing test

--- packages/react-menu/e2e/MenuList.e2e.ts
+++ packages/react-menu/e2e/MenuList.e2e.ts
@@ -1,7 +1,4 @@
 describe('MenuList', () => {
-  it('should fail', () => {
-    expect(true).equals(false);
-  });
   it('should focus each menu item on hover', () => {
     cy.visitStory('MenuList', 'TextOnly')
       .get('[role="menuitem"]')
