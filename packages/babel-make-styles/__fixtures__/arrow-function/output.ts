427fcf0fffb899e5ffcac4dfa61fca5d22ee176c
Oleksandr Fediashov <olfedias@microsoft.com>
2021-05-04 19:39:33 +0200

    chore(makeStyles): hash properties (#18038)
    * chore(makeStyles): hash properties
    
    * Change files
    
    * update a comment

--- packages/babel-make-styles/__fixtures__/arrow-function/output.ts
+++ packages/babel-make-styles/__fixtures__/arrow-function/output.ts
@@ -1,11 +1,11 @@
 import { __styles } from '@fluentui/react-make-styles';
 export const useStyles = __styles({
   root: {
-    backgroundColor: ['', 'fbrlg6g0', '.fbrlg6g0{background-color:var(--global-color-black);}'],
-    color: ['', 'fk38h1u0', '.fk38h1u0{color:var(--alias-color-blue-border2);}'],
-    display: ['', 'f22iagw0', '.f22iagw0{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}'],
+    '3e3pzq0': ['', 'fbrlg6g0', '.fbrlg6g0{background-color:var(--global-color-black);}'],
+    sj55zd0: ['', 'fk38h1u0', '.fk38h1u0{color:var(--alias-color-blue-border2);}'],
+    mc9l5x0: ['', 'f22iagw0', '.f22iagw0{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}'],
   },
   rootPrimary: {
-    color: ['', 'f1qa0pc1', '.f1qa0pc1{color:var(--alias-color-brand-brandBackground);}'],
+    sj55zd0: ['', 'f1qa0pc1', '.f1qa0pc1{color:var(--alias-color-brand-brandBackground);}'],
   },
 });
--- packages/babel-make-styles/__fixtures__/commonjs/output.ts
+++ packages/babel-make-styles/__fixtures__/commonjs/output.ts
@@ -4,9 +4,9 @@ const react_make_styles_1 = require('@fluentui/react-make-styles');
 
 const useStyles = react_make_styles_1.__styles({
   root: {
-    fontSize: ['', 'flcnb000', '.flcnb000{font-size:var(--global-type-fontSizes-base-300);}'],
-    lineHeight: ['', 'f1syuwty', '.f1syuwty{line-height:var(--global-type-lineHeights-base-300);}'],
-    fontWeight: ['', 'f1du0uop', '.f1du0uop{font-weight:var(--global-type-fontWeights-regular);}'],
+    '1e2twd7': ['', 'flcnb000', '.flcnb000{font-size:var(--global-type-fontSizes-base-300);}'],
+    '1g96gwp': ['', 'f1syuwty', '.f1syuwty{line-height:var(--global-type-lineHeights-base-300);}'],
+    '1hrd7zp': ['', 'f1du0uop', '.f1du0uop{font-weight:var(--global-type-fontWeights-regular);}'],
   },
 });
 
