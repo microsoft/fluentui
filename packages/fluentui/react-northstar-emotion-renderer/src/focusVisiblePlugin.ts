c69eb96b137d89ae230baebbda983dfc9ed734a3
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-05-06 10:19:09 +0200

    chore: rename

--- packages/fluentui/react-northstar-emotion-renderer/src/focusVisiblePlugin.ts
+++ packages/fluentui/react-northstar-emotion-renderer/src/focusVisiblePlugin.ts
@@ -1,15 +1,15 @@
 const notFocusVisibleRgxp = /:not\(:focus-visible\)/g;
 const focusVisibleRgxp = /:focus-visible/g;
-const trim = (selector: string) => selector.trim().replace(/\s+/g, ' ');
+const toSingleSpace = (selector: string) => selector.trim().replace(/\s+/g, ' ');
 export function focusVisiblePlugin(context: number, content: string, selectors: Array<string>) {
   if (context === 2) {
     selectors.forEach((selector, index) => {
       if (selector.match(notFocusVisibleRgxp)) {
         const cleanSelector = selector.replace(notFocusVisibleRgxp, ':focus');
-        selectors[index] = trim(`[data-whatinput]:not([data-whatinput="keyboard"]) ${cleanSelector}`);
+        selectors[index] = toSingleSpace(`[data-whatinput]:not([data-whatinput="keyboard"]) ${cleanSelector}`);
       } else if (selector.match(focusVisibleRgxp)) {
         const cleanSelector = selector.replace(focusVisibleRgxp, ':focus');
-        selectors[index] = trim(`[data-whatinput="keyboard"] ${cleanSelector}`);
+        selectors[index] = toSingleSpace(`[data-whatinput="keyboard"] ${cleanSelector}`);
       }
     });
   }
