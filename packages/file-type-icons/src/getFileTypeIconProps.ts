99a01767790f42793bc82cdda34a688074f90fbe
Hjortur Jonasson <hjorturj@microsoft.com>
2021-05-06 09:30:19 +0200

    wrong PR

--- packages/file-type-icons/src/getFileTypeIconProps.ts
+++ packages/file-type-icons/src/getFileTypeIconProps.ts
@@ -9,7 +9,6 @@ const SHARED_FOLDER = 'sharedfolder';
 const DOCSET_FOLDER = 'docset';
 const LIST_ITEM = 'splist';
 const MULTIPLE_ITEMS = 'multiple';
-const NEWS = 'sponews';
 const STREAM = 'stream';
 const DEFAULT_ICON_SIZE: FileTypeIconSize = 16;
 
@@ -74,8 +73,6 @@ export function getFileTypeIconProps(options: IFileTypeIconOptions): { iconName:
       case FileIconType.multiple:
         iconBaseName = MULTIPLE_ITEMS;
         break;
-      case FileIconType.news:
-        iconBaseName = NEWS;
     }
   }
 
