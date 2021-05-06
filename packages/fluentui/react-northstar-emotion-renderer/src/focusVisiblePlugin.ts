ed9f37f8eae41c877ba71043a5e0edefe3209e1a
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-05-06 10:53:54 +0200

    chore: update focusVisiblePlugin

--- packages/fluentui/react-northstar-emotion-renderer/src/focusVisiblePlugin.ts
+++ packages/fluentui/react-northstar-emotion-renderer/src/focusVisiblePlugin.ts
@@ -1,6 +1,11 @@
 const notFocusVisibleRgxp = /:not\(:focus-visible\)/g;
 const focusVisibleRgxp = /:focus-visible/g;
 const toSingleSpace = (selector: string) => selector.trim().replace(/\s+/g, ' ');
+
+/**
+ * This is an custom implementation from original FocusVisiblePlugin
+ * see https://github.com/quid/refraction/blob/master/packages/stylis-plugin-focus-visible/src/index.js
+ */
 export function focusVisiblePlugin(context: number, content: string, selectors: Array<string>) {
   if (context === 2) {
     selectors.forEach((selector, index) => {
