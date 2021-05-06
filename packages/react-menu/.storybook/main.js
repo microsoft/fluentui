0054e6b42b386e0ab2197327e2881a9734a49fc9
Martin Hochel <martinhochel@microsoft.com>
2021-05-06 15:00:11 +0200

    chore(react-menu): consume properly babel config in storybook

--- packages/react-menu/.storybook/main.js
+++ packages/react-menu/.storybook/main.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 const rootMain = require('../../../.storybook/main');
 
-module.exports = /** @type {Pick<import('../../../.storybook/main').StorybookConfig,'addons'|'stories'|'webpackFinal'>} */ ({
+const babelConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../.babelrc.json'), 'utf-8'));
+
+module.exports = /** @type {Pick<import('../../../.storybook/main').StorybookConfig,'addons'|'stories'|'webpackFinal' |'babel'>} */ ({
   stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: [...rootMain.addons],
+  babel: async config => {
+    if (config.plugins) {
+      config.plugins.push(...babelConfig.plugins);
+    }
+
+    console.log({ babelConfig }, config);
+
+    return { ...config };
+  },
   webpackFinal: (config, options) => {
     const localConfig = { ...rootMain.webpackFinal(config, options) };
 
