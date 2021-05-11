e0b7d62f54c1b6e7ff8847ec60307ebac692ff7e
Ben Howell <48106640+behowell@users.noreply.github.com>
2021-05-11 14:02:06 -0700

    Refactor Tooltip to simplify API and remove dependency in FluentProvider (#18043)
    This is a significant refactoring of Tooltip, to simplify the API and remove the FluentProvider's dependence on the react-tooltip package. Highlights include:
    
    * Remove the `TooltipTrigger` component, and combine its functionality into `Tooltip`. The API changes like so:
       ```diff
      - <TooltipTrigger tooltip="..."><button /></TooltipTrigger>
      + <Tooltip content="..."><button /></Tooltip>
      ```
    * As a perf improvement in the common case where a tooltip is a simple string that is being used as its trigger's label, the tooltip's content is set as the trigger's `aria-label` and is only rendered when it is visible. 
      * If the tooltip content is more complex (has HTML elements), or is used as the trigger's description, it is still always rendered even if hidden, so that it can be referred to by ID using `aria-labelledby` or `aria-describedby`.
    * Remove the `TooltipManager`, and instead have tooltips manage their own showing and hiding internally. When a tooltip is visible, it'll register a function to hide it on the `TooltipContext`. Then if another tooltip is shown before the first one is hidden, the new tooltip can immediately hide the old one when it's shown.
    * Remove the dependence on react-tooltip from react-provider. Now rather than the heavyweight `TooltipProvider` component, it has a thin `TooltipContext.Provider` that just provides the context for tooltips to use, without pulling in the full tooltip implementation.

--- packages/react-tooltip/src/components/TooltipProvider/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './TooltipProvider';
-export * from './TooltipProvider.types';
-export * from './renderTooltipProvider';
-export * from './useTooltipContext';
-export * from './useTooltipManager';
-export * from './useTooltipProvider';
