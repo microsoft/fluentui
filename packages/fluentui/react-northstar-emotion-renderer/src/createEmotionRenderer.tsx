f3a645ac6336bb85550b9255b1d13f9865bd7a69
Oleksandr Fediashov <olfedias@microsoft.com>
2021-05-11 13:41:02 +0200

    fix TS type

--- packages/fluentui/react-northstar-emotion-renderer/src/createEmotionRenderer.tsx
+++ packages/fluentui/react-northstar-emotion-renderer/src/createEmotionRenderer.tsx
@@ -1,5 +1,5 @@
 import createCache, { StylisPlugin } from '@emotion/cache';
-import { ObjectInterpolation, serializeStyles } from '@emotion/serialize';
+import { CSSObject, serializeStyles } from '@emotion/serialize';
 import { StyleSheet } from '@emotion/sheet';
 import { EmotionCache, insertStyles } from '@emotion/utils';
 import {
@@ -95,7 +95,7 @@ export function createEmotionRenderer(options: CreateEmotionRendererOptions = {}
         }
 
         const serializedStyles = serializeStyles(
-          [{ [selector]: (styles as unknown) as ObjectInterpolation<{}> }],
+          [{ [selector]: (styles as unknown) as CSSObject }],
           // This looks as a bug in typings as in Emotion code this function can be used with a single param.
           // https://github.com/emotion-js/emotion/blob/a076e7fa5f78fec6515671b78801cfc9d6cf1316/packages/core/src/global.js#L45
           // @ts-ignore
