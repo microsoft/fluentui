4e69f9f9902e5bf394dbe47d8624e66c59f69d8c
Oleksandr Fediashov <olfedias@microsoft.com>
2021-05-12 12:20:22 +0200

    add jsdoc

--- packages/make-styles/src/runtime/expandShorthand.ts
+++ packages/make-styles/src/runtime/expandShorthand.ts
@@ -1,6 +1,10 @@
 import { expandProperty } from 'inline-style-expand-shorthand';
 import { MakeStyles } from '../types';
 
+/**
+ * A function that expands longhand properties ("margin", "padding") to their shorthand versions ("margin-left", etc.).
+ * Follows CSS-like order in expansion i.e. last defined property wins.
+ */
 export function expandShorthand(style: MakeStyles, result: MakeStyles = {}): MakeStyles {
   // eslint-disable-next-line guard-for-in
   for (const property in style) {
