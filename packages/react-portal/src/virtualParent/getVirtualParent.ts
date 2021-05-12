e0a2a0629b1e6c6aa745e488925931c442138709
Lingfan Gao <lingfan.gao@microsoft.com>
2021-05-12 09:45:10 +0200

    remove package, refactor vparent

--- packages/react-portal/src/virtualParent/getVirtualParent.ts
+++ packages/react-portal/src/virtualParent/getVirtualParent.ts
@@ -1,8 +1,6 @@
 import { isVirtualElement } from './isVirtualElement';
 /**
  * Gets the virtual parent given the child element, if it exists.
- *
- * @public
  */
 export function getVirtualParent(child: HTMLElement): HTMLElement | undefined {
   let parent: HTMLElement | undefined;
