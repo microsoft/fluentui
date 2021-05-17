27efcf89f488176ccdc83a7bd9adc7b71961a283
Chris Holt <chhol@microsoft.com>
2021-05-17 10:37:58 -0700

    update tooltip to leverage tokens

--- packages/web-components/src/tooltip/index.ts
+++ packages/web-components/src/tooltip/index.ts
@@ -1,5 +1,25 @@
-import { tooltipTemplate as template, Tooltip } from '@microsoft/fast-foundation';
+import { DI, DesignToken, Tooltip as FoundationTooltip, tooltipTemplate as template } from '@microsoft/fast-foundation';
 import { tooltipStyles as styles } from './tooltip.styles';
+import { white } from '../color-vNext/utilities/color-constants';
+import { fillColor, NeutralContrastFill } from '../design-tokens';
+
+/**
+ * The Fluent tooltip class
+ * @internal
+ */
+export class Tooltip extends FoundationTooltip {
+  /**
+   * @internal
+   */
+  public connectedCallback(): void {
+    super.connectedCallback();
+
+    const recipe = DI.getOrCreateDOMContainer(this.parentElement!).get(NeutralContrastFill);
+    const parentFill = fillColor.getValueFor(this.parentElement!);
+
+    fillColor.setValueFor(this, (target: HTMLElement) => recipe(target, parentFill).rest);
+  }
+}
 
 /**
  * The Fluent Tooltip Custom Element. Implements {@link @microsoft/fast-foundation#Tooltip},
