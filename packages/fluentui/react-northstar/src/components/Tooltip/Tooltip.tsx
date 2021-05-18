aed6e6e6aa5cd50f837203397bdd742c0139ce3f
Charles Assuncao <junioassuncaocharles@gmail.com>
2021-05-18 14:26:28 +0200

    chore: update

--- packages/fluentui/react-northstar/src/components/Tooltip/Tooltip.tsx
+++ packages/fluentui/react-northstar/src/components/Tooltip/Tooltip.tsx
@@ -232,7 +232,7 @@ export const Tooltip: React.FC<TooltipProps> &
     <>
       {triggerElement && (
         <Ref innerRef={triggerRef}>
-          {React.cloneElement(triggerElement, { ...getA11Props('trigger', triggerProps), ...triggerElement.props })}
+          {React.cloneElement(triggerElement, getA11Props('trigger', { ...triggerElement.props, ...triggerProps }))}
         </Ref>
       )}
       <PortalInner mountNode={mountNode}>
