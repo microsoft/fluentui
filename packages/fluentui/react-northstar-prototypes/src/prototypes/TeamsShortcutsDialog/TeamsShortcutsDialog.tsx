f7b280ead048b1faacdb4d20c613b2c4f1b5880b
Adam <adam.samec@gmail.com>
2021-05-20 13:09:33 +0200

    Add header, footer, first text focusing and some other things for keyboard sohrtcutsshortcuts dialog

--- packages/fluentui/react-northstar-prototypes/src/prototypes/TeamsShortcutsDialog/TeamsShortcutsDialog.tsx
+++ packages/fluentui/react-northstar-prototypes/src/prototypes/TeamsShortcutsDialog/TeamsShortcutsDialog.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Provider, teamsTheme, Header, Button, Dialog, Text, Table } from '@fluentui/react-northstar';
+import { CloseIcon } from '@fluentui/react-icons-northstar';
 
 const tableHeader = {
   items: ['Action', 'Keys'],
@@ -23,25 +24,31 @@ const TeamsShortcutsDialog: React.FunctionComponent = () => {
       <div>
         <Header content="Microsoft Teams Keyboard Shortcuts Dialog Prototype" />
         <Dialog
+          trapFocus={{ firstFocusableSelector: '#first-text' }}
           open={dialogOpened}
-          onOpen={() => {
-            setDialogOpened(true);
-          }}
-          onCancel={() => {
-            setDialogOpened(false);
-          }}
+          onOpen={() => setDialogOpened(true)}
+          onCancel={() => setDialogOpened(false)}
           header="Keyboard shortcuts"
+          headerAction={{ icon: <CloseIcon />, title: 'Close dialog', onClick: () => setDialogOpened(false) }}
+          footer={{
+            children: (Component, props) => (
+              <>
+                <Text as="a" href="">
+                  See shortcuts for all platforms
+                </Text>
+                <Text as="a" href="">
+                  Office Accessibility Center
+                </Text>
+              </>
+            ),
+          }}
           trigger={<Button content="Open keyboard shortcuts dialog" />}
           content={
             <div>
-              <Button
-                content="Close dialog"
-                onClick={() => {
-                  setDialogOpened(false);
-                }}
-              />
-              <Text>Keyboard language is: English (United States).</Text>
-              <Header as={'h3'} content="General" />
+              <Text id="first-text" tabIndex={-1}>
+                Keyboard language is: English (United States).
+              </Text>
+              <Header as="h3" content="General" />
               <Table header={tableHeader} rows={generalTableRows} />
             </div>
           }
