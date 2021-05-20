f000b9188a2dfe1d6477c1aa46ebdfe6e9cc9c4f
Chris Holt <chhol@microsoft.com>
2021-05-20 16:58:17 -0700

    update height number and direction

--- packages/web-components/src/styles/size.ts
+++ packages/web-components/src/styles/size.ts
@@ -1,5 +1,31 @@
+import { Behavior, CSSDirective, FASTElement } from '@microsoft/fast-element';
+import { baseHeightMultiplier, density, designUnit } from '../design-tokens';
+
 /**
- * The height of height of a standard control (expressed as a number) to be used in CSS.
- * @public
+ * A formula to retrieve the control height.
+ * Use this as the value of any CSS property that
+ * accepts a pixel size.
  */
-export const heightNumber = '(var(--base-height-multiplier) + var(--density)) * var(--design-unit)';
+class HeightNumber extends CSSDirective implements Behavior {
+  private behaviors: Behavior[] = [
+    baseHeightMultiplier.createBehavior(),
+    density.createBehavior(),
+    designUnit.createBehavior(),
+  ].filter(x => x !== void 0) as Behavior[];
+  createCSS() {
+    return `(${baseHeightMultiplier.createCSS()} + ${density.createCSS()}) * ${designUnit.createCSS()}`;
+  }
+  createBehavior() {
+    return this;
+  }
+
+  bind(element: FASTElement) {
+    element.$fastController.addBehaviors(this.behaviors);
+  }
+
+  unbind(element: FASTElement) {
+    element.$fastController.removeBehaviors(this.behaviors);
+  }
+}
+
+export const heightNumber = new HeightNumber();
