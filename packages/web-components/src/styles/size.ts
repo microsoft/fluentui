5c7aaad02404057c9fde54445735b0b371daef31
Chris Holt <chhol@microsoft.com>
2021-05-20 19:01:35 -0700

    update heightNumber to be css partial

--- packages/web-components/src/styles/size.ts
+++ packages/web-components/src/styles/size.ts
@@ -1,4 +1,4 @@
-import { Behavior, CSSDirective, FASTElement } from '@microsoft/fast-element';
+import { cssPartial } from '@microsoft/fast-element';
 import { baseHeightMultiplier, density, designUnit } from '../design-tokens';
 
 /**
@@ -6,26 +6,4 @@ import { baseHeightMultiplier, density, designUnit } from '../design-tokens';
  * Use this as the value of any CSS property that
  * accepts a pixel size.
  */
-class HeightNumber extends CSSDirective implements Behavior {
-  private behaviors: Behavior[] = [
-    baseHeightMultiplier.createBehavior(),
-    density.createBehavior(),
-    designUnit.createBehavior(),
-  ].filter(x => x !== void 0) as Behavior[];
-  createCSS() {
-    return `(${baseHeightMultiplier.createCSS()} + ${density.createCSS()}) * ${designUnit.createCSS()}`;
-  }
-  createBehavior() {
-    return this;
-  }
-
-  bind(element: FASTElement) {
-    element.$fastController.addBehaviors(this.behaviors);
-  }
-
-  unbind(element: FASTElement) {
-    element.$fastController.removeBehaviors(this.behaviors);
-  }
-}
-
-export const heightNumber = new HeightNumber();
+export const heightNumber = cssPartial`(${baseHeightMultiplier} + ${density}) * ${designUnit}`;
