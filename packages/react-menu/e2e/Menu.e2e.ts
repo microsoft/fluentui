6666855f133c44f2aa3b6d49a56ef7d1e156dbd9
Charles Assunção <junioassuncaocharles@gmail.com>
2021-05-24 09:15:36 +0200

    Update Menu.e2e.ts

--- packages/react-menu/e2e/Menu.e2e.ts
+++ packages/react-menu/e2e/Menu.e2e.ts
@@ -39,7 +39,11 @@ describe('MenuTrigger', () => {
 
 describe('Custom Trigger', () => {
   it('should open menu when clicked', () => {
-    cy.visitStory('Menu', customTriggerStory).contains('Custom Trigger').click().get(menuSelector).should('be.visible');
+    cy.visitStory('Menu', customTriggerStory)
+      .contains('Custom Trigger')
+      .click()
+      .get(menuSelector)
+      .should('be.visible');
   });
 
   it('should dismiss the menu when click outside', () => {
@@ -55,7 +59,12 @@ describe('Custom Trigger', () => {
 
 describe('MenuItem', () => {
   it('should close the menu when clicked', () => {
-    cy.visitStory('Menu', defaultStory).get(menuTriggerSelector).trigger('click').get(menuItemSelector).first().click();
+    cy.visitStory('Menu', defaultStory)
+      .get(menuTriggerSelector)
+      .trigger('click')
+      .get(menuItemSelector)
+      .first()
+      .click();
 
     cy.get(menuSelector).should('not.be.exist');
   });
@@ -77,7 +86,9 @@ describe('MenuItem', () => {
       .trigger('click')
       .get(menuItemSelector)
       .each(el => {
-        cy.wrap(el).trigger('mouseover').should('be.focused');
+        cy.wrap(el)
+          .trigger('mouseover')
+          .should('be.focused');
       });
   });
 });
@@ -157,11 +168,22 @@ describe('MenuItemRadio', () => {
       .first()
       .click();
 
-    cy.get(menuTriggerSelector).trigger('click').get(menuItemRadioSelector).eq(1).click();
+    cy.get(menuTriggerSelector)
+      .trigger('click')
+      .get(menuItemRadioSelector)
+      .eq(1)
+      .click();
 
-    cy.get(menuTriggerSelector).trigger('click').get(menuItemRadioSelector).eq(2).click();
+    cy.get(menuTriggerSelector)
+      .trigger('click')
+      .get(menuItemRadioSelector)
+      .eq(2)
+      .click();
 
-    cy.get(menuTriggerSelector).trigger('click').get('[aria-checked="true"]').should('have.length', 1);
+    cy.get(menuTriggerSelector)
+      .trigger('click')
+      .get('[aria-checked="true"]')
+      .should('have.length', 1);
   });
 });
 
@@ -177,7 +199,9 @@ describe('Menu', () => {
   });
 
   it('should be dismissed on outside click', () => {
-    cy.visitStory('Menu', defaultStory).get(menuTriggerSelector).click();
+    cy.visitStory('Menu', defaultStory)
+      .get(menuTriggerSelector)
+      .click();
 
     cy.get('body').click('bottomRight');
 
@@ -220,7 +244,9 @@ describe('Menu', () => {
               .type(key)
               .get(menuSelector)
               .within(() => {
-                cy.get(menuItemSelector).first().should('be.focused');
+                cy.get(menuItemSelector)
+                  .first()
+                  .should('be.focused');
               });
           })
           .get(menuSelector)
@@ -250,7 +276,10 @@ describe('Menu', () => {
           .type('{rightarrow}')
           .get(menuSelector)
           .within(() => {
-            cy.get(menuTriggerSelector).type('{rightarrow}').focused().type(key);
+            cy.get(menuTriggerSelector)
+              .type('{rightarrow}')
+              .focused()
+              .type(key);
           })
           .get(menuSelector)
           .should('have.length', 1);
