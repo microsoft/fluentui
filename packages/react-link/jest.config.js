4edbec44e0121f8cc557da845e82ca70e57011db
Oleksandr Fediashov <olfedias@microsoft.com>
2021-05-26 13:14:49 +0200

    chore: enable Jest aliases for converged packages

--- packages/react-link/jest.config.js
+++ packages/react-link/jest.config.js
@@ -2,6 +2,7 @@ const { createConfig } = require('@fluentui/scripts/jest/jest-resources');
 const path = require('path');
 
 const config = createConfig({
+  moduleNameMapper: require('lerna-alias').jest(),
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: ['@fluentui/jest-serializer-make-styles'],
 });
--- packages/react-shared-contexts/jest.config.js
+++ packages/react-shared-contexts/jest.config.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let { createConfig } = require('@fluentui/scripts/jest/jest-resources');
 module.exports = createConfig({
+  moduleNameMapper: require('lerna-alias').jest(),
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
 });
