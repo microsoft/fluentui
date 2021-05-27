843d6b465e134bb0972b846998d2a180afa5aa23
Jared Ivey <jaredi@microsoft.com>
2021-05-26 17:38:13 -0700

    cleanup comments

--- packages/react-experiments/src/components/UnifiedPicker/UnifiedPicker.tsx
+++ packages/react-experiments/src/components/UnifiedPicker/UnifiedPicker.tsx
@@ -298,8 +298,8 @@ export const UnifiedPicker = <T extends {}>(props: IUnifiedPickerProps<T>): JSX.
       dropItemsAtDeferred = undefined;
     }
 
-    // Because these calls are async, it's possible for us to get the drag end call while we're in
-    // the middle of a drop action (BETWEEN wells), so don't run the delete code if that's the case
+    // Because these calls are async, it's possible for us to get the drag end call while
+    // we're in the middle of a drop action, so don't run the delete code if that's the case
     if (event && !isInDropAction) {
       // If we have a move event, and we still have selected items (indicating that we
       // haven't already moved items within the well) we should remove the item(s)
