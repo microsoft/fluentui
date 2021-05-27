2e4f218bb2106f15f0f25de93cad67b471b7fed0
Lingfan Gao <lingfan.gao@microsoft.com>
2021-05-27 17:00:25 +0200

    add missing props

--- packages/react-popover/src/components/PopoverContent/usePopoverContent.ts
+++ packages/react-popover/src/components/PopoverContent/usePopoverContent.ts
@@ -30,10 +30,14 @@ export const usePopoverContent = (
   const arrowRef = usePopoverContext(context => context.arrowRef);
   const size = usePopoverContext(context => context.size);
   const noArrow = usePopoverContext(context => context.noArrow);
+  const brand = usePopoverContext(context => context.brand);
+  const inverted = usePopoverContext(context => context.inverted);
   const { modalAttributes } = useModalAttributes();
 
   const state = mergeProps(
     {
+      brand,
+      inverted,
       noArrow,
       size,
       arrowRef,
