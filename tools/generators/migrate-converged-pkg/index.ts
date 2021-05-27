fb91173159a8485ee8275f5b05cc8af395338dbc
Martin Hochel <martinhochel@microsoft.com>
2021-05-27 16:31:09 +0200

    feat(tools): implement jest updates for migration generator
    Co-authored-by: ling1726 <ling1726@users.noreply.github.com>
    Co-authored-by: André <andrefcdias@users.noreply.github.com>

--- tools/generators/migrate-converged-pkg/index.ts
+++ tools/generators/migrate-converged-pkg/index.ts
@@ -5,13 +5,20 @@ import {
   readProjectConfiguration,
   readWorkspaceConfiguration,
   joinPathFragments,
+  readJson,
+  getProjects,
+  stripIndents,
 } from '@nrwl/devkit';
+import { serializeJson } from '@nrwl/workspace';
+import { updateJestConfig } from '@nrwl/jest/src/generators/jest-project/lib/update-jestconfig';
+
+import { TsConfig } from '../../types';
 
 import { MigrateConvergedPkgGeneratorSchema } from './schema';
 
 /**
  * TASK:
- * 1. migrate to typescript path aliases - #18343 ✅ (partially done)
+ * 1. migrate to typescript path aliases - #18343 ✅
  * 2. migrate to use standard jest powered by TS path aliases
  * 3. setup docs task to run api-extractor for local changes verification
  * 4. bootstrap new storybook config
@@ -26,6 +33,10 @@ export default async function (tree: Tree, schema: MigrateConvergedPkgGeneratorS
   // 1. update TsConfigs
   updatedLocalTsConfig(tree, options);
   updatedBaseTsConfig(tree, options);
+
+  // 2. update Jest
+  updateLocalJestConfig(tree, options);
+  updateRootJestConfig(tree, options);
 
   formatFiles(tree);
 }
@@ -50,7 +61,29 @@ const templates = {
     },
     include: ['src'],
   },
-  jest: {},
+  jest: (options: { pkgName: string }) => stripIndents`
+      // @ts-check
+
+      /**
+      * @type {jest.InitialOptions}
+      */
+      module.exports = {
+        displayName: '${options.pkgName}',
+        preset: '../../jest.preset.js',
+        globals: {
+          'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.json',
+            diagnostics: false,
+          },
+        },
+        transform: {
+          '^.+\\.tsx?$': 'ts-jest',
+        },
+        coverageDirectory: './coverage',
+        setupFilesAfterEnv: ['./config/tests.js'],
+        snapshotSerializers: ['@fluentui/jest-serializer-make-styles'],
+      };
+  `,
 };
 
 function normalizeOptions(host: Tree, options: MigrateConvergedPkgGeneratorSchema) {
@@ -74,8 +107,17 @@ function normalizeOptions(host: Tree, options: MigrateConvergedPkgGeneratorSchem
   };
 }
 
-function serializeJson(value: unknown) {
-  return JSON.stringify(value, null, 2);
+function updateLocalJestConfig(tree: Tree, options: NormalizedSchema) {
+  const normalizedPkgName = options.name.replace(`@${options.workspaceConfig.npmScope}/`, '');
+  tree.write(options.paths.jestConfig, templates.jest({ pkgName: normalizedPkgName }));
+
+  return tree;
+}
+
+function updateRootJestConfig(tree: Tree, options: NormalizedSchema) {
+  updateJestConfig(tree, { project: options.name });
+
+  return tree;
 }
 
 function updatedLocalTsConfig(tree: Tree, options: NormalizedSchema) {
@@ -85,9 +127,24 @@ function updatedLocalTsConfig(tree: Tree, options: NormalizedSchema) {
 }
 
 function updatedBaseTsConfig(tree: Tree, options: NormalizedSchema) {
-  // @TODO: add missing dependencies from migrated package to path aliases
-  updateJson(tree, options.paths.rootTsconfig, json => {
+  const workspaceConfig = readWorkspaceConfiguration(tree);
+  const allProjects = getProjects(tree);
+
+  const projectPkgJson = readJson<{ dependencies: Record<string, string> }>(tree, options.paths.packageJson);
+
+  const depsThatNeedToBecomeAliases = Object.keys(projectPkgJson.dependencies)
+    .filter(pkgName => pkgName.startsWith(`@${workspaceConfig.npmScope}`))
+    .reduce((acc, pkgName) => {
+      acc[pkgName] = [`${allProjects.get(pkgName)?.root}/src/index.ts`];
+
+      return acc;
+    }, {} as Required<Pick<TsConfig['compilerOptions'], 'paths'>>['paths']);
+
+  updateJson<TsConfig, TsConfig>(tree, options.paths.rootTsconfig, json => {
+    json.compilerOptions.paths = json.compilerOptions.paths ?? {};
     json.compilerOptions.paths[options.name] = [`${options.projectConfig.root}/src/index.ts`];
+
+    Object.assign(json.compilerOptions.paths, depsThatNeedToBecomeAliases);
 
     return json;
   });
