537a4d15e1ecdbd9784ba763319d6aa5dd1ffbba
André Dias <andredias@microsoft.com>
2021-05-27 15:09:51 +0200

    Add eslint-disable for JSX key on tests

--- packages/react-examples/src/react-components/Migrations/utils.test.tsx
+++ packages/react-examples/src/react-components/Migrations/utils.test.tsx
@@ -41,7 +41,8 @@ test('renders Markdown source blocks', () => {
   const result = renderer.getRenderOutput();
 
   expect(result.props).toEqual({
-    children: [<h3 key="1">JavaScript</h3>, <Source key="2" code={`console.log("test");`} language="js" />],
+    // eslint-disable-next-line react/jsx-key
+    children: [<h3>JavaScript</h3>, <Source code={`console.log("test");`} language="js" />],
   });
 });
 
@@ -51,7 +52,8 @@ test('uses JSX for no header JSX source code blocks', () => {
   const result = renderer.getRenderOutput();
 
   expect(result.props).toEqual({
-    children: [<h3 key="1">React</h3>, <Source key="2" code={`<Test title={\"Example\"} />`} language="jsx" />],
+    // eslint-disable-next-line react/jsx-key
+    children: [<h3>React</h3>, <Source code={`<Test title={\"Example\"} />`} language="jsx" />],
   });
 });
 
@@ -74,7 +76,8 @@ test.each([
   const result = renderer.getRenderOutput();
 
   expect(result.props).toEqual({
-    children: [<h3 key="1">{expectedHeader}</h3>, <Source key="2" code={`Code`} language={language} />],
+    // eslint-disable-next-line react/jsx-key
+    children: [<h3>{expectedHeader}</h3>, <Source code={`Code`} language={language} />],
   });
 });
 
@@ -92,6 +95,7 @@ test('overrides the default title', () => {
   const result = renderer.getRenderOutput();
 
   expect(result.props).toEqual({
-    children: [<h3 key="1">Custom title</h3>, <Source key="2" code={`Code`} language="js" />],
+    // eslint-disable-next-line react/jsx-key
+    children: [<h3>Custom title</h3>, <Source code={`Code`} language="js" />],
   });
 });
