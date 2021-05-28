9e93eaeb5fc97f51e6d9566855acc4325e064bfc
Esteban Munoz <esteban.230@hotmail.com>
2021-05-28 13:06:07 -0600

    Adding customizable example

--- packages/react-examples/src/react-label/Label/Label.stories.tsx
+++ packages/react-examples/src/react-label/Label/Label.stories.tsx
@@ -1,43 +1,121 @@
 import * as React from 'react';
 import { Label } from '@fluentui/react-label';
 import { makeStyles } from '@fluentui/react-make-styles';
+import { Checkbox, Dropdown, IDropdownOption, TextField } from '@fluentui/react';
 
 const useStyles = makeStyles({
-  examplesContainer: {
+  exampleContainer: {
     display: 'flex',
     flexDirection: 'row',
-    margin: '20px',
     gap: '20px',
+  },
+
+  labelContainer: {
+    backgroundColor: 'whitesmoke',
+    padding: '10px',
+    alignSelf: 'center',
+  },
+
+  configurationContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '20px',
+    backgroundColor: 'whitesmoke',
+    padding: '20px',
+  },
+
+  textField: {
+    height: '20px',
+    width: '150px',
+  },
+
+  checkbox: {
+    display: 'flex',
+    flexDirection: 'row',
   },
 });
 
+const SizeOptions = [
+  { key: 'small', text: 'Small' },
+  { key: 'medium', text: 'Medium' },
+  { key: 'large', text: 'Large' },
+];
+
 export const BasicLabelExample = () => {
   const styles = useStyles();
+  const [labelText, setLabelText] = React.useState("I'm a label");
+  const [requiredText, setRequiredText] = React.useState<string | undefined>();
+  const [strong, setStrong] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(false);
+  const [required, setRequired] = React.useState(false);
+  const [size, setSize] = React.useState('medium');
+
+  const updateLabelText = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, val?: string) => {
+    if (val) {
+      setLabelText(val);
+    }
+  };
+
+  const updateRequiredText = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, val?: string) => {
+    setRequiredText(val);
+  };
+
+  const updateStrong = (ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
+    setStrong(checked!);
+  };
+
+  const updateDisabled = (ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
+    setDisabled(checked!);
+  };
+
+  const updateRequired = (ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean) => {
+    setRequired(checked!);
+  };
+
+  const updateSize = (ev?: React.FormEvent<HTMLDivElement>, item?: IDropdownOption) => {
+    if (item && typeof item.key === 'string') {
+      setSize(item.key);
+    }
+  };
 
   return (
-    <div className={styles.examplesContainer}>
-      <Label>Label</Label>
-      <Label disabled>Label</Label>
-      <Label required>Label</Label>
-      <Label required="**">Label</Label>
-      <Label size="small" required>
-        Label
-      </Label>
-      <Label size="small" required="**">
-        Label
-      </Label>
-      <Label size="large" required>
-        Label
-      </Label>
-      <Label size="large" required="**">
-        Label
-      </Label>
-      <Label strong required>
-        Label
-      </Label>
-      <Label strong required="**">
-        Label
-      </Label>
+    <div className={styles.exampleContainer}>
+      <div className={styles.configurationContainer}>
+        <div>
+          <Label strong>Text</Label>
+          <TextField placeholder="I'm a label" onChange={updateLabelText} className={styles.textField} />
+        </div>
+        <div className={styles.checkbox}>
+          <Checkbox onChange={updateStrong} />
+          <Label strong>Strong</Label>
+        </div>
+        <div className={styles.checkbox}>
+          <Checkbox onChange={updateDisabled} />
+          <Label strong>Disabled</Label>
+        </div>
+        <div className={styles.checkbox}>
+          <Checkbox onChange={updateRequired} />
+          <Label strong>Required</Label>
+        </div>
+        <div>
+          <Label strong>Required Text</Label>
+          <TextField onChange={updateRequiredText} className={styles.textField} />
+        </div>
+        <div>
+          <Label strong>Size</Label>
+          <Dropdown defaultSelectedKey={'medium'} options={SizeOptions} onChange={updateSize} />
+        </div>
+      </div>
+      <div className={styles.labelContainer}>
+        <Label
+          required={required && requiredText !== '' ? requiredText : required}
+          size={size}
+          disabled={disabled}
+          strong={strong}
+        >
+          {labelText}
+        </Label>
+      </div>
     </div>
   );
 };
