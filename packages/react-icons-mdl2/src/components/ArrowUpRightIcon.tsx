0281d15480d47ce28edaebbf0a7fd7438fa92a63
tomi-msft <66456876+tomi-msft@users.noreply.github.com>
2021-06-04 13:49:17 -0700

    Add focusable attribute to SVG MDL2 icons (#18429)
    * Add focusable prop to SVG MDL2 icons
    
    * Change files
    
    * update snapshots
    
    * Change files
    
    * Reverting prettier change

--- packages/react-icons-mdl2/src/components/ArrowUpRightIcon.tsx
+++ packages/react-icons-mdl2/src/components/ArrowUpRightIcon.tsx
@@ -3,7 +3,7 @@ import createSvgIcon from '../utils/createSvgIcon';
 
 const ArrowUpRightIcon = createSvgIcon({
   svg: ({ classes }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" className={classes.svg}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" className={classes.svg} focusable="false">
       <path d="M384 256h1408v1408h-128V475L347 1792l-91-91L1573 384H384V256z" />
     </svg>
   ),
--- packages/react-icons-mdl2/src/components/CancelIcon.tsx
+++ packages/react-icons-mdl2/src/components/CancelIcon.tsx
@@ -3,7 +3,7 @@ import createSvgIcon from '../utils/createSvgIcon';
 
 const CancelIcon = createSvgIcon({
   svg: ({ classes }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" className={classes.svg}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" className={classes.svg} focusable="false">
       <path d="M1115 1024l690 691-90 90-691-690-691 690-90-90 690-691-690-691 90-90 691 690 691-690 90 90-690 691z" />
     </svg>
   ),
