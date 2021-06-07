c95784bf90af998f343331ebf4e929686d3f106e
Shi Cheng <shi.cheng@microsoft.com>
2021-06-07 17:09:01 -0400

    Add the ability to generate default time and dropdown options with increments

--- packages/react-examples/src/react/TimePicker/TimePicker.Example.tsx
+++ packages/react-examples/src/react/TimePicker/TimePicker.Example.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { ComboBox, IComboBox, IComboBoxOption } from '@fluentui/react';
 import { TimeConstants } from '@fluentui/date-time-utilities';
 
+// This should be added to TimeConstants
+const HOURS_IN_ONE_DAY = 24;
+
 interface ITimePickerProps {
   /**
    * Label of the component
@@ -26,29 +29,14 @@ interface ITimePickerProps {
   range?: any;
 }
 
-// TODO: Make sure how many options we are going to show
-const OPTION_NUMBER = 10;
-
-// This functions should be moved to date-time-utilities eventually
-const addMinutes = (date: Date, minutes: number): Date => {
-  const result = new Date(date.getTime());
-  result.setTime(result.getTime() + minutes * TimeConstants.MinutesInOneHour * TimeConstants.MillisecondsIn1Sec);
-  return result;
-};
-
-// This functions needs to be reimplemented later with proper handling of user region/timezone
-const formatTimeString = (date: Date): string => {
-  return date.toLocaleTimeString();
-};
-
 const TimePicker = ({ label, increments = 30 }: ITimePickerProps) => {
-  const now = new Date();
-
-  const timePickerOptions: IComboBoxOption[] = Array(OPTION_NUMBER)
+  const defaultTime = generateDefaultTime(increments);
+  const optionsCount = getDropdownOptionsCount(increments);
+  const timePickerOptions: IComboBoxOption[] = Array(optionsCount)
     .fill(0)
     .map((_, index) => ({
       key: index,
-      text: formatTimeString(addMinutes(now, increments * index)),
+      text: formatTimeString(addMinutes(defaultTime, increments * index)),
     }));
 
   const comboBoxRef = React.useRef<IComboBox>(null);
@@ -69,4 +57,41 @@ const TimePicker = ({ label, increments = 30 }: ITimePickerProps) => {
 
 export const TimePickerBasicExample: React.FC = () => {
   return <TimePicker label={'TimePicker basic example'} />;
+};
+
+const generateDefaultTime = (increments: number) => {
+  const now = new Date();
+  if (!(TimeConstants.MinutesInOneHour % increments)) {
+    const minute = roundMinute(now.getMinutes(), increments);
+    now.setMinutes(minute);
+  }
+  return now;
+};
+
+const roundMinute = (minute: number, increments: number) => {
+  if (minute === 0) return minute;
+  const times = TimeConstants.MinutesInOneHour / increments;
+  let rounded;
+  for (let i = 1; i <= times; i++) {
+    if (minute > increments * (i - 1) && minute <= increments * i) {
+      rounded = increments * i;
+      return rounded;
+    }
+  }
+};
+
+const getDropdownOptionsCount = (increments: number) => {
+  return Math.floor((TimeConstants.MinutesInOneHour * HOURS_IN_ONE_DAY) / increments);
+};
+
+// This functions should be moved to date-time-utilities eventually
+const addMinutes = (date: Date, minutes: number): Date => {
+  const result = new Date(date.getTime());
+  result.setTime(result.getTime() + minutes * TimeConstants.MinutesInOneHour * TimeConstants.MillisecondsIn1Sec);
+  return result;
+};
+
+// This functions needs to be reimplemented later with proper handling of user region/timezone
+const formatTimeString = (date: Date): string => {
+  return date.toLocaleTimeString();
 };
