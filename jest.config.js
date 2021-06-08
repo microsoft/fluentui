081a27d0bf865ffbbb1f9bfe0ee6d5bebd7d9faa
Lingfan Gao <lingfan.gao@microsoft.com>
2021-06-08 15:57:48 +0200

    chore(Portal): Migrate to new DX
    used `nx workspace-generator migrate-converged-pkg` to migrate
    `react-portal` to the new DX

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/tools', '<rootDir>/packages/react-menu', '<rootDir>/packages/react-storybook'],
+  projects: ['<rootDir>/tools', '<rootDir>/packages/react-menu', '<rootDir>/packages/react-storybook',"<rootDir>/packages/react-portal"],
 };
