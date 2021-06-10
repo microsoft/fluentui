5e09e2e859464d280d1f415396cf402db5ca7617
Karlo Sudec <karlo.sudec@gmail.com>
2021-06-10 14:04:58 +0200

    (GroupedList) fixed getGroupHeight function when group has children

--- packages/react/src/components/GroupedList/GroupedList.base.tsx
+++ packages/react/src/components/GroupedList/GroupedList.base.tsx
@@ -287,6 +287,7 @@ export class GroupedListBase extends React.Component<IGroupedListProps, IGrouped
   }
 
   private _getDefaultGroupItemLimit = (group: IGroup): number => {
+    return group.count;
     return group.children && group.children.length > 0 ? group.children.length : group.count;
   };
 
@@ -300,8 +301,9 @@ export class GroupedListBase extends React.Component<IGroupedListProps, IGrouped
 
   private _getGroupHeight = (group: IGroup): number => {
     const rowHeight = this.props.compact ? COMPACT_ROW_HEIGHT : ROW_HEIGHT;
+    const rowCount = group.children?.length > 0 ? group.children.length : this._getGroupItemLimit(group);
 
-    return rowHeight + (group.isCollapsed ? 0 : rowHeight * this._getGroupItemLimit(group));
+    return rowHeight + (group.isCollapsed ? 0 : rowHeight * rowCount);
   };
 
   private _getPageHeight: IListProps['getPageHeight'] = (itemIndex: number) => {
