cabb2460cb3243a9a7ab33aca7372cb4d081d229
James Wu <jamwu@microsoft.com>
2021-06-11 12:02:01 -0400

    Re-added TimePicker example.

--- packages/react-examples/src/react/TimePicker/TimePicker.Example.tsx
+++ packages/react-examples/src/react/TimePicker/TimePicker.Example.tsx
@@ -1,28 +1,28 @@
-// import * as React from 'react';
-// import { TimePicker } from '@fluentui/react/lib/TimePicker';
+import * as React from 'react';
+import { TimeRange, TimePicker } from '@fluentui/react/lib/TimePicker';
 
-// export const TimePickerBasicExample: React.FC = () => {
-//   const timeRange: TimeRange = {
-//     start: 2,
-//     end: 20,
-//   };
-//   return (
-//     <TimePicker
-//       styles={{
-//         optionsContainerWrapper: {
-//           height: '500px',
-//         },
-//         root: {
-//           width: '50%',
-//         },
-//       }}
-//       showSeconds
-//       allowFreeform
-//       autoComplete="on"
-//       label={'TimePicker basic example'}
-//       showDurationIndicator
-//       useComboBoxAsMenuWidth
-//       timeRange={timeRange}
-//     />
-//   );
-// };
+export const TimePickerBasicExample: React.FC = () => {
+  const timeRange: TimeRange = {
+    start: 2,
+    end: 20,
+  };
+  return (
+    <TimePicker
+      styles={{
+        optionsContainerWrapper: {
+          height: '500px',
+        },
+        root: {
+          width: '50%',
+        },
+      }}
+      showSeconds
+      allowFreeform
+      autoComplete="on"
+      label={'TimePicker basic example'}
+      showDurationIndicator
+      useComboBoxAsMenuWidth
+      timeRange={timeRange}
+    />
+  );
+};
