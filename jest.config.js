cbd0626efddf0810c500ad92c3e55670f9783132
Lingfan Gao <lingfan.gao@microsoft.com>
2021-06-14 21:09:24 +0200

    solve conflicts

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-<<<<<<< HEAD
-  projects: ['<rootDir>/tools', '<rootDir>/packages/react-menu', '<rootDir>/packages/react-storybook',"<rootDir>/packages/react-portal"],
-=======
   projects: [
     '<rootDir>/tools',
     '<rootDir>/packages/react-menu',
     '<rootDir>/packages/react-storybook',
     '<rootDir>/packages/react-accordion',
+    '<rootDir>/packages/react-portal',
   ],
->>>>>>> master
 };
