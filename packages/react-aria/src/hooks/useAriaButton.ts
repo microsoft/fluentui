07c83b9a7e9d0477d7788096247894ffc1545220
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-06-15 13:50:31 +0200

    short-circuit in button case

--- packages/react-aria/src/hooks/useAriaButton.ts
+++ packages/react-aria/src/hooks/useAriaButton.ts
@@ -8,14 +8,7 @@ enum KeyboardEventKeys {
 }
 
 export function useARIAButton(shorthand: ObjectShorthandProps<React.ButtonHTMLAttributes<HTMLElement>>) {
-  if (!shorthand.hasOwnProperty('as')) {
-    shorthand.as = 'button';
-  }
-  if (!shorthand.hasOwnProperty('children')) {
-    shorthand.children = null;
-  }
-
-  const { onClick, onKeyDown, onKeyUp, as, disabled } = shorthand;
+  const { onClick, onKeyDown, onKeyUp, disabled } = shorthand;
 
   const onClickHandler = useEventCallback((ev: React.MouseEvent<HTMLElement>) => {
     if (disabled) {
@@ -61,28 +54,33 @@ export function useARIAButton(shorthand: ObjectShorthandProps<React.ButtonHTMLAt
     }
   });
 
+  if (!shorthand.hasOwnProperty('as') || shorthand.as === 'button') {
+    shorthand.as = 'button';
+    return; // there's nothing to be done if as prop === 'button'
+  }
+  if (!shorthand.hasOwnProperty('children')) {
+    shorthand.children = null;
+  }
   /**
    * TODO: Ideally this is unnecessary after implementation of as-prop RFC.
    * The way to go is to have an assertion method to ensure types,
    * in the case of button we'd like to limit it for: button, div, span, a
    */
-  if (typeof as === 'string') {
+  if (typeof shorthand.as === 'string') {
     // Add 'role=button' and 'tabIndex=0' for all non-button elements.
-    if (as !== 'button') {
-      if (!shorthand.hasOwnProperty('role')) {
-        shorthand.role = 'button';
-      }
+    if (!shorthand.hasOwnProperty('role')) {
+      shorthand.role = 'button';
+    }
 
-      shorthand.onClick = onClickHandler;
+    shorthand.onClick = onClickHandler;
 
-      // Add keydown event handler for all other non-anchor elements.
-      if (as !== 'a') {
-        if (!shorthand.hasOwnProperty('tabIndex')) {
-          shorthand.tabIndex = disabled ? undefined : 0;
-        }
-        shorthand.onKeyDown = onKeyDownHandler;
-        shorthand.onKeyUp = onKeyupHandler;
+    // Add keydown event handler for all other non-anchor elements.
+    if (shorthand.as !== 'a') {
+      if (!shorthand.hasOwnProperty('tabIndex')) {
+        shorthand.tabIndex = disabled ? undefined : 0;
       }
+      shorthand.onKeyDown = onKeyDownHandler;
+      shorthand.onKeyUp = onKeyupHandler;
     }
   }
   // Add keydown event handler, 'role=button' and 'tabIndex=0' for all other elements.
