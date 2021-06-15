da095b1a70b752a849e8a457e2c4e9ec99042539
Lingfan Gao <lingfan.gao@microsoft.com>
2021-06-15 08:51:49 +0200

    improve docs

--- packages/react-positioning/src/usePopperMouseTarget.ts
+++ packages/react-positioning/src/usePopperMouseTarget.ts
@@ -6,6 +6,8 @@ import * as PopperJs from '@popperjs/core';
  * A state hook that manages a popper virtual element from mouseevents.
  * Useful for scenarios where a component needs to be positioned by mouse click (e.g. contextmenu)
  * React synthetic events are not persisted by this hook
+ *
+ * @param initialState - initializes a user provided state similare to useState
  * @returns state and dispatcher for a Popper virtual element that uses native/synthetic mouse events
  */
 export const usePopperMouseTarget = (initialState?: PopperJs.VirtualElement | (() => PopperJs.VirtualElement)) => {
