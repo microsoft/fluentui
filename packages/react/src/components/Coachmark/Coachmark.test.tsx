348e7862f1fd35a104dccf321e95229adffcf906
czearing <czearing@outlook.com>
2021-06-21 15:28:27 -0700

    Adding render tests to Coachmark.

--- packages/react/src/components/Coachmark/Coachmark.test.tsx
+++ packages/react/src/components/Coachmark/Coachmark.test.tsx
@@ -14,25 +14,77 @@ import { ICoachmark } from './Coachmark.types';
 const ReactDOM = require('react-dom');
 
 describe('Coachmark', () => {
+  // Conformance Tests:
+  isConformant({
+    Component: Coachmark,
+    displayName: 'Coachmark',
+    componentPath: path.join(__dirname, 'Coachmark.ts'),
+    disabledTests: ['component-handles-classname', 'component-has-root-ref', 'component-handles-ref'],
+  });
+
   // Render Tests:
-  it('renders Coachmark correctly', () => {
+  it('renders Coachmark (correctly)', () => {
     const createPortal = ReactDOM.createPortal;
     ReactDOM.createPortal = jest.fn(element => element);
 
-    safeCreate(<Coachmark target="test">Content</Coachmark>, component => {
+    safeCreate(<Coachmark target="test" />, component => {
       const tree = component!.toJSON();
       expect(tree).toMatchSnapshot();
       ReactDOM.createPortal = createPortal;
     });
   });
 
-  it('renders closed (isCollapsed = false) Coachmark correctly', () => {
+  it('renders Coachmark (className)', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Coachmark className={'myClassName'} target="test" />, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+      ReactDOM.createPortal = createPortal;
+    });
+  });
+
+  it('renders Coachmark (isCollapsed)', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Coachmark isCollapsed={false} target="test" />, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+      ReactDOM.createPortal = createPortal;
+    });
+  });
+
+  it('renders Coachmark (color)', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Coachmark color="red" target="test" />, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+      ReactDOM.createPortal = createPortal;
+    });
+  });
+
+  it('renders Coachmark (beaconColorOne and beaconColorTwo)', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Coachmark beaconColorOne="red" beaconColorTwo="blue" target="test" />, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+      ReactDOM.createPortal = createPortal;
+    });
+  });
+
+  it('renders Coachmark (children)', () => {
     const createPortal = ReactDOM.createPortal;
     ReactDOM.createPortal = jest.fn(element => element);
 
     safeCreate(
-      <Coachmark isCollapsed={false} target="test">
-        Content
+      <Coachmark target="test">
+        <div className="test">Test</div>
       </Coachmark>,
       component => {
         const tree = component!.toJSON();
@@ -42,11 +94,24 @@ describe('Coachmark', () => {
     );
   });
 
-  // Conformance Tests:
-  isConformant({
-    Component: Coachmark,
-    displayName: 'Coachmark',
-    componentPath: path.join(__dirname, 'Coachmark.ts'),
-    disabledTests: ['component-handles-classname', 'component-has-root-ref', 'component-handles-ref'],
+  it('renders Coachmark (aria)', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(
+      <Coachmark
+        ariaAlertText="Test: ariaAlertText"
+        ariaLabelledBy="Test: ariaLabelledBy"
+        ariaLabelledByText="Test: ariaLabelledByText"
+        ariaDescribedBy="Test: ariaDescribedBy"
+        ariaDescribedByText="Test: ariaDescribedBy"
+        target="test"
+      />,
+      component => {
+        const tree = component!.toJSON();
+        expect(tree).toMatchSnapshot();
+        ReactDOM.createPortal = createPortal;
+      },
+    );
   });
 });
