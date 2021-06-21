8e0583100bd4071c13ba1366768124341f3602cd
czearing <czearing@outlook.com>
2021-06-21 12:37:04 -0700

    Adding resetId to Coachmark

--- packages/react/src/components/Coachmark/Coachmark.test.tsx
+++ packages/react/src/components/Coachmark/Coachmark.test.tsx
@@ -1,8 +1,40 @@
-import * as path from 'path';
+import * as React from 'react';
+import { ReactTestRenderer } from 'react-test-renderer';
+import { create } from '@fluentui/utilities/lib/test';
+import { mount, ReactWrapper } from 'enzyme';
+
 import { Coachmark } from './Coachmark';
+import * as path from 'path';
 import { isConformant } from '../../common/isConformant';
+import { IRefObject, resetIds } from '@fluentui/utilities';
+import { ICoachmark } from './Coachmark.types';
+
+let coachmark: ICoachmark | undefined;
+/** Use this as the componentRef when rendering a Coachmark. */
+const coachmarkRef: IRefObject<ICoachmark> = (ref: ICoachmark | null) => {
+  coachmark = ref!;
+};
 
 describe('Coachmark', () => {
+  let renderedComponent: ReactTestRenderer | undefined;
+  let component: ReactWrapper | undefined;
+
+  beforeEach(() => {
+    resetIds();
+  });
+
+  afterEach(() => {
+    coachmark = undefined;
+    if (renderedComponent) {
+      renderedComponent.unmount();
+      renderedComponent = undefined;
+    }
+    if (component) {
+      component.unmount();
+      component = undefined;
+    }
+  });
+
   isConformant({
     Component: Coachmark,
     displayName: 'Coachmark',
