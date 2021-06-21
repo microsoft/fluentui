7a09542ef3bf8f378200d9b90f41f4aea9ea7ad1
czearing <czearing@outlook.com>
2021-06-21 14:04:01 -0700

    Adding Coachmark snapshot test

--- packages/react/src/components/Coachmark/Coachmark.test.tsx
+++ packages/react/src/components/Coachmark/Coachmark.test.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import * as ReactTestUtils from 'react-dom/test-utils';
 import { ReactTestRenderer } from 'react-test-renderer';
 import { create } from '@fluentui/utilities/lib/test';
 import { mount, ReactWrapper } from 'enzyme';
+import { safeCreate } from '@fluentui/test-utilities';
 
 import { Coachmark } from './Coachmark';
 import * as path from 'path';
@@ -9,8 +11,11 @@ import { isConformant } from '../../common/isConformant';
 import { IRefObject, resetIds } from '@fluentui/utilities';
 import { ICoachmark } from './Coachmark.types';
 
+const ReactDOM = require('react-dom');
+
 let coachmark: ICoachmark | undefined;
-/** Use this as the componentRef when rendering a Coachmark. */
+
+// /** Use this as the componentRef when rendering a Coachmark. */
 const coachmarkRef: IRefObject<ICoachmark> = (ref: ICoachmark | null) => {
   coachmark = ref!;
 };
@@ -35,6 +40,20 @@ describe('Coachmark', () => {
     }
   });
 
+  // Render Tests:
+
+  it('renders Coachmark correctly', () => {
+    const createPortal = ReactDOM.createPortal;
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Coachmark target="test">Content</Coachmark>, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+      ReactDOM.createPortal = createPortal;
+    });
+  });
+
+  // Conformance Tests:
   isConformant({
     Component: Coachmark,
     displayName: 'Coachmark',
