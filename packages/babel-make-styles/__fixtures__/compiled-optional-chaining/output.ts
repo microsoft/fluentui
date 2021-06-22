fa663839f891a39a0e0b6a7d3cd16809c08a2548
Oleksandr Fediashov <olfedias@microsoft.com>
2021-06-22 10:48:57 +0200

    update fixtures after merge

--- packages/babel-make-styles/__fixtures__/compiled-optional-chaining/output.ts
+++ packages/babel-make-styles/__fixtures__/compiled-optional-chaining/output.ts
@@ -4,7 +4,7 @@ export var makeButtonTokens = (theme: Theme) => {
   return {
     primary: {
       hovered: {
-        background: theme.alias.color.brand.brandBackgroundHover,
+        background: theme.alias.color.neutral.brandBackgroundHover,
       },
     },
   };
@@ -13,10 +13,10 @@ export var useStyles = __styles(
   {
     rootPrimary: {
       sj55zd: 'fe3e8s9',
-      ayd6f0: 'f1pzj0k4',
+      ayd6f0: 'fcz74vz',
     },
   },
   {
-    d: ['.fe3e8s9{color:red;}', '.f1pzj0k4{background:var(--alias-color-brand-brandBackgroundHover);}'],
+    d: ['.fe3e8s9{color:red;}', '.fcz74vz{background:var(--alias-color-neutral-brandBackgroundHover);}'],
   },
 );
--- packages/babel-make-styles/__fixtures__/function/output.ts
+++ packages/babel-make-styles/__fixtures__/function/output.ts
@@ -7,7 +7,7 @@ export const useStyles = __styles(
       mc9l5x: 'f22iagw',
     },
     rootPrimary: {
-      sj55zd: 'f1qa0pc1',
+      sj55zd: 'f1aydz7j',
     },
   },
   {
@@ -15,7 +15,7 @@ export const useStyles = __styles(
       '.fbrlg6g{background-color:var(--global-color-black);}',
       '.fk38h1u{color:var(--alias-color-blue-border2);}',
       '.f22iagw{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;}',
-      '.f1qa0pc1{color:var(--alias-color-brand-brandBackground);}',
+      '.f1aydz7j{color:var(--alias-color-neutral-brandBackground);}',
     ],
   },
 );
