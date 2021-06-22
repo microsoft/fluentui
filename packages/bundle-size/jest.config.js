94c59453919d29fa0751144114ed6d67b566af14
Oleksandr Fediashov <olfedias@microsoft.com>
2021-06-22 17:06:09 +0200

    run NX migration on bundle-size

---
+++ packages/bundle-size/jest.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {jest.InitialOptions}
+ */
+module.exports = {
+  displayName: 'bundle-size',
+  preset: '../../jest.preset.js',
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+};
