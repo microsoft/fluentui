b48d0649edf8c2155e4c925682f034a29190c716
czearing <czearing@outlook.com>
2021-06-23 14:25:34 -0700

    Adding snapshot test to Check component.

--- packages/react/src/components/Check/Check.test.tsx
+++ packages/react/src/components/Check/Check.test.tsx
@@ -1,9 +1,35 @@
+import * as React from 'react';
 import { Check } from './Check';
+import { safeCreate } from '@fluentui/test-utilities';
+import { resetIds } from '@fluentui/utilities';
 import { isConformant } from '../../common/isConformant';
 
+const ReactDOM = require('react-dom');
+
 describe('Check', () => {
+  const createPortal = ReactDOM.createPortal;
+
+  beforeEach(() => {
+    resetIds();
+  });
+
+  afterEach(() => {
+    ReactDOM.createPortal = createPortal;
+  });
+
+  // Conformance Tests:
   isConformant({
     Component: Check,
     displayName: 'Check',
+  });
+
+  // Snapshot Tests:
+  it('renders Check (correctly)', () => {
+    ReactDOM.createPortal = jest.fn(element => element);
+
+    safeCreate(<Check checked={true} className={'test-className'} />, component => {
+      const tree = component!.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
