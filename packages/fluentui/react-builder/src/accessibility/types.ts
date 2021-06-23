15b1d71d36412f2ae2533e5b2f04f296238c5705
Emily <erl3193@rit.edu>
2021-06-23 15:22:00 -0400

    fixed formatting on debug panel and navigator tab

--- packages/fluentui/react-builder/src/accessibility/types.ts
+++ packages/fluentui/react-builder/src/accessibility/types.ts
@@ -1,9 +1,9 @@
 export type AccessibilityErrorsBySource = {
   source: string;
   errors: string[];
-}
+};
 
 export type AccessibilityErrors = {
   elementUuid: string | number;
   accessibilityErrors: AccessibilityErrorsBySource[];
-}
+};
