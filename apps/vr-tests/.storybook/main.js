1508c239d03f13dc4346bace5bb8f05c0a1e59cc
Oleksandr Fediashov <olfedias@microsoft.com>
2021-06-24 14:55:21 +0200

    restore a change

--- apps/vr-tests/.storybook/main.js
+++ apps/vr-tests/.storybook/main.js
@@ -3,6 +3,7 @@ const custom = require('@fluentui/scripts/storybook/webpack.config');
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
+  babel: {},
   typescript: {
     // disable react-docgen-typescript (totally not needed here, slows things down a lot)
     reactDocgen: false,
