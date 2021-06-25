d39b73fc7bdf404a546e96230efbb0ece3745cfc
czearing <czearing@outlook.com>
2021-06-24 17:48:25 -0700

    Adding tests for onChange and onChanged.

--- packages/react/src/components/SearchBox/SearchBox.test.tsx
+++ packages/react/src/components/SearchBox/SearchBox.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ReactTestRenderer } from 'react-test-renderer';
+import { act, ReactTestRenderer } from 'react-test-renderer';
 import { create } from '@fluentui/utilities/lib/test';
 import { mount, ReactWrapper } from 'enzyme';
 import { SearchBox } from './SearchBox';
@@ -171,6 +171,30 @@ describe('SearchBox', () => {
     wrapper = mount(<SearchBox value={0 as any} />);
     // this is not allowed per typings, but users might do it anyway
     expect(wrapper.find('input').getDOMNode().getAttribute('value')).toBe('0');
+  });
+
+  it('handles onChange', () => {
+    const onChange = jest.fn();
+
+    act(() => {
+      wrapper = mount(<SearchBox onChange={onChange} />);
+      expect(onChange).toHaveBeenCalledTimes(0);
+
+      wrapper.find('input').simulate('change', { target: { value: 'New value' } });
+
+      expect(onChange).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('handles onChanged', () => {
+    const onChanged = jest.fn();
+
+    wrapper = mount(<SearchBox onChanged={onChanged} />);
+    expect(onChanged).toHaveBeenCalledTimes(0);
+
+    wrapper.find('input').simulate('change', { target: { value: 'New value' } });
+
+    expect(onChanged).toHaveBeenCalledTimes(1);
   });
 
   it('invokes onEscape callback on escape keydown', () => {
