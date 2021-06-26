59db9a805d62c3dc739f9db06de7d75869819291
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-06-25 21:24:12 -0700

    ts-ignore react-provider import

--- packages/react-make-styles/src/RTL.stories.tsx
+++ packages/react-make-styles/src/RTL.stories.tsx
@@ -1,4 +1,6 @@
 import { makeStyles, mergeClasses } from './index';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - (see https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982)
 import { FluentProvider } from '@fluentui/react-provider';
 import { webLightTheme } from '@fluentui/react-theme';
 import * as React from 'react';
