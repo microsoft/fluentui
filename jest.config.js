a53006e183a22e060e76f9bfad01399853ce903c
ling1726 <lingfan.gao@microsoft.com>
2021-06-28 18:08:16 +0200

    chore(react-tabster): Migrate to new DX (#18699)
    * chore(react-tabster): Migrate to new DX
    
    * Change files
    
    * Update packages/react-tabster/tsconfig.json
    
    * fix jest config
    
    * pass with no tests

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     '<rootDir>/packages/react-popover',
     '<rootDir>/packages/react-link',
     '<rootDir>/packages/react-divider',
+    '<rootDir>/packages/react-tabster',
     '<rootDir>/packages/react-label',
     '<rootDir>/packages/make-styles',
     '<rootDir>/packages/jest-serializer-make-styles',
