eb48f4970149f96a2f7b5f253d913aceef757870
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-06-29 18:15:24 -0700

    Remove console log

--- packages/react/src/components/CalendarDayGrid/CalendarDayGrid.styles.ts
+++ packages/react/src/components/CalendarDayGrid/CalendarDayGrid.styles.ts
@@ -48,7 +48,7 @@ export const styles = (props: ICalendarDayGridStyleProps): ICalendarDayGridStyle
     animationDirection,
   } = props;
   const { palette } = theme;
-  console.log('theme ', theme);
+
   const classNames = getGlobalClassNames(GlobalClassNames, theme);
 
   let rowAnimationStyle: IRawStyle = {};
