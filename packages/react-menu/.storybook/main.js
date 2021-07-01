9e87e609863e0de635cc363da0341ede9a46b9f4
Lingfan Gao <lingfan.gao@microsoft.com>
2021-07-01 22:24:36 +0200

    ignore internal stories glob

--- packages/react-menu/.storybook/main.js
+++ packages/react-menu/.storybook/main.js
@@ -1,7 +1,9 @@
 const rootMain = require('../../../.storybook/main');
 
+const testGlob = '../src/stories/!(*.internal).stories.@(ts|tsx)';
+
 module.exports = /** @type {Pick<import('../../../.storybook/main').StorybookConfig,'addons'|'stories'|'webpackFinal'>} */ ({
-  stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  stories: [...rootMain.stories, '../src/**/*.stories.mdx', testGlob],
   addons: [...rootMain.addons],
   webpackFinal: (config, options) => {
     const localConfig = { ...rootMain.webpackFinal(config, options) };
