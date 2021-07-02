bf0ded4636b6efc5bb8a52d78bd073dae0269898
André Dias <andredias@microsoft.com>
2021-07-02 12:51:56 +0200

    Text implementation (#18590)
    Co-authored-by: Tringa Krasniqi <tkrasniqi@microsoft.com>
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-text/src/components/Text/Text.types.ts
+++ packages/react-text/src/components/Text/Text.types.ts
@@ -5,17 +5,88 @@ import { ComponentPropsCompat, ComponentStateCompat } from '@fluentui/react-util
  * Text Props
  */
 export interface TextProps extends ComponentPropsCompat, React.HTMLAttributes<HTMLElement> {
-  /*
-   * TODO Add props and slots here
-   * Any slot property should be listed in the textShorthandProps array below
-   * Any property that has a default value should be listed in TextDefaultedProps as e.g. 'size' | 'icon'
+  /**
+   * Wraps the text content on white spaces.
+   *
+   * @defaultValue true
    */
+  wrap?: boolean;
+
+  /**
+   * Truncate overflowing text for block displays.
+   *
+   * @defaultValue false
+   */
+  truncate?: boolean;
+
+  /**
+   * Applies a block display for the content.
+   *
+   * @defaultValue false
+   */
+  block?: boolean;
+
+  /**
+   * Applies the italic font style to the content.
+   *
+   * @defaultValue false
+   */
+  italic?: boolean;
+
+  /**
+   * Applies the underline text decoration to the content.
+   *
+   * @defaultValue false
+   */
+  underline?: boolean;
+
+  /**
+   * Applies the strikethrough text decoration to the content.
+   *
+   * @defaultValue false
+   */
+  strikethrough?: boolean;
+
+  /**
+   * Applies font size and line height based on the theme tokens.
+   *
+   * @defaultValue 300
+   */
+  size?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000;
+
+  /**
+   * Applies the font family to the content.
+   *
+   * @defaultValue base
+   */
+  font?: 'base' | 'monospace' | 'numeric';
+
+  /**
+   * Applies font weight to the content.
+   *
+   * @defaultValue regular
+   */
+  weight?: 'regular' | 'medium' | 'semibold';
+
+  /**
+   * Aligns text based on the parent container.
+   *
+   * @defaultValue start
+   */
+  align?: 'start' | 'center' | 'end' | 'justify';
+
+  /**
+   * Component to be rendered as.
+   *
+   * @defaultValue span
+   */
+  as?: 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'pre';
 }
 
 /**
  * Names of TextProps that have a default value in useText
  */
-export type TextDefaultedProps = never; // TODO add names of properties with default values
+export type TextDefaultedProps = never;
 
 /**
  * State used in rendering Text
