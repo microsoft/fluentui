d3c14cc33719cb9a7cfb1ae27011ec07febb3719
Chris Holt <chhol@microsoft.com>
2021-07-02 14:45:04 -0700

    update dependencies and lint-staged config (#18822)
    * update dependencies and lint-staged config
    
    * fix neutral fill layer spec
    
    * Change files

--- packages/web-components/src/color/recipes/neutral-fill-layer.spec.ts
+++ packages/web-components/src/color/recipes/neutral-fill-layer.spec.ts
@@ -7,15 +7,16 @@ import { neutralFillLayer } from './neutral-fill-layer';
 const neutralPalette = PaletteRGB.create(middleGrey);
 
 describe('neutralFillLayer', (): void => {
-  it('should get darker when the index of the backgroundColor is lower than the offset index', (): void => {
+  it('should be lighter or equal when the index of the fill color is lower than the offset index', (): void => {
     const delta = 3;
     for (let i: number = 0; i < delta; i++) {
       const color = neutralFillLayer(neutralPalette, neutralPalette.get(i), delta);
-      const resolved = neutralPalette.get(delta + i);
+      const resolved = neutralPalette.get(i - delta);
+
       expect(color).to.equal(resolved);
     }
   });
-  it('should return the color at three steps lower than the background color', (): void => {
+  it('should return the color at three steps lower than the fill color', (): void => {
     const delta = 3;
 
     for (let i: number = delta; i < neutralPalette.swatches.length; i++) {
