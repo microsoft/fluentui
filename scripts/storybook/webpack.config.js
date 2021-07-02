ce485096489a070dce738694a531075e7d71a24b
Elizabeth Craig <elcraig@microsoft.com>
2021-07-02 10:26:32 -0700

    remove watch code

--- scripts/storybook/webpack.config.js
+++ scripts/storybook/webpack.config.js
@@ -1,13 +1,10 @@
 const IgnoreNotFoundExportWebpackPlugin = require('ignore-not-found-export-webpack-plugin');
-const fs = require('fs');
 const path = require('path');
 const findGitRoot = require('../monorepo/findGitRoot');
 const getResolveAlias = require('../webpack/getResolveAlias');
 const webpack = require('webpack');
 
 module.exports = (/** @type {webpack.Configuration} */ config) => {
-  const gitRoot = findGitRoot();
-
   config.resolveLoader = {
     ...config.resolveLoader,
     modules: [
@@ -77,7 +74,7 @@ module.exports = (/** @type {webpack.Configuration} */ config) => {
   config.resolve.alias = {
     // Use the aliases for react-examples since the examples and demo may depend on some things
     // that the package itself doesn't (and it will include the aliases for all the package's deps)
-    ...getResolveAlias(false, path.join(gitRoot, 'packages/react-examples')),
+    ...getResolveAlias(false, path.join(findGitRoot(), 'packages/react-examples')),
   };
 
   config.plugins.push(new IgnoreNotFoundExportWebpackPlugin({ include: [/\.tsx?$/] }));
@@ -95,21 +92,6 @@ module.exports = (/** @type {webpack.Configuration} */ config) => {
       // Plugins have circular references, and they're probably not the issue here
       JSON.stringify(config, (key, value) => (key === 'plugins' ? undefined : value), 2),
     );
-    const northstarPath = path.join(gitRoot, 'packages/fluentui/react-northstar');
-    const filesToWatch = [
-      path.join(gitRoot, 'tsconfig.json'),
-      path.join(northstarPath, 'tsconfig.json'),
-      path.join(northstarPath, 'package.json'),
-    ];
-    for (const file of filesToWatch) {
-      fs.watchFile(file, (curr, prev) => {
-        // Get the stack but remove the first line about it being an error
-        const stack = new Error().stack.split(/\r?\n/g).slice(1).join('\n');
-        const isAccess = curr.mtime.getTime() === prev.mtime.getTime();
-        console.log(`${isAccess ? 'Accessed' : 'Modified'} ${file}:`);
-        console.log(stack);
-      });
-    }
   }
 
   return config;
