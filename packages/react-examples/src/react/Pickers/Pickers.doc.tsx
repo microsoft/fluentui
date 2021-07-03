fbc2f693ae3641a10b213d9c3343435b918e489d
shujathkhan <shujathlive@gmail.com>
2021-07-04 00:33:02 +0530

    Package namespace and snapshot update for custom icon story

--- packages/react-examples/src/react/Pickers/Pickers.doc.tsx
+++ packages/react-examples/src/react/Pickers/Pickers.doc.tsx
@@ -9,7 +9,7 @@ import { TagPickerInlineExample } from './TagPicker.Inline.Example';
 const TagPickerExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react/Pickers/TagPicker.Basic.Example.tsx') as string;
 const TagPickerInlineExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react/Pickers/TagPicker.Inline.Example.tsx') as string;
 const PickerCustomResultExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react/Pickers/Picker.CustomResult.Example.tsx') as string;
-const TagPickerCustomIconExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react/Pickers/Picker.CustomIcon.Example.tsx') as string;
+const TagPickerCustomIconExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react/Pickers/TagPicker.CustomIcon.Example.tsx') as string;
 
 export const PickersPageProps: IDocPageProps = {
   title: 'Pickers',
