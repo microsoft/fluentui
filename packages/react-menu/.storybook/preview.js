8687b764c575793a1f78ca7bfe1496468ccc0a5b
Lingfan Gao <lingfan.gao@microsoft.com>
2021-07-07 21:51:07 +0200

    revert preview.js change

--- packages/react-menu/.storybook/preview.js
+++ packages/react-menu/.storybook/preview.js
@@ -1,20 +1,3 @@
 import * as rootPreview from '../../../.storybook/preview';
 
 export const decorators = [...rootPreview.decorators];
-
-export const parameters = {
-  options: {
-    storySort: (a, b) => {
-      if (a[1].name === 'Default') {
-        console.log('xxx');
-        return -1;
-      }
-
-      if (b[1].name === 'Default') {
-        return 1;
-      }
-
-      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
-    },
-  },
-};
