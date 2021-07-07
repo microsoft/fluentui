71f5d172139cbc8815638d2c31cba8bce922a412
Lingfan Gao <lingfan.gao@microsoft.com>
2021-07-07 21:47:05 +0200

    revert main.js change

--- packages/react-menu/.storybook/main.js
+++ packages/react-menu/.storybook/main.js
@@ -1,7 +1,5 @@
 const rootMain = require('../../../.storybook/main');
 
-const testGlob = '../src/stories/!(*.internal).stories.@(ts|tsx)';
-
 module.exports = /** @type {Pick<import('../../../.storybook/main').StorybookConfig,'addons'|'stories'|'webpackFinal'>} */ ({
   stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: [...rootMain.addons],
