61fe7eed7d0330a4dd94f8ce86c687bdfd0f0dac
Martin Hochel <martinhochel@microsoft.com>
2021-07-08 17:58:28 +0200

    chore: move all babel deps to single version policy

--- scripts/babel/index.js
+++ scripts/babel/index.js
@@ -32,9 +32,11 @@ module.exports = api => {
   ];
   const plugins = [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
     ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
     ['@babel/plugin-proposal-optional-chaining', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     '@babel/plugin-syntax-dynamic-import',
     ['@babel/plugin-transform-runtime', { useESModules }],
 
