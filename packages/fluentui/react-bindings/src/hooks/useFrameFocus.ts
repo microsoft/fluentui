ac0f538c8be5f6ae9191f22919725f5b44cb17f9
chassunc <86579954+chassunc@users.noreply.github.com>
2021-07-12 05:29:10 -0300

    Update packages/fluentui/react-bindings/src/hooks/useFrameFocus.ts
    Co-authored-by: ling1726 <lingfangao@hotmail.com>

--- packages/fluentui/react-bindings/src/hooks/useFrameFocus.ts
+++ packages/fluentui/react-bindings/src/hooks/useFrameFocus.ts
@@ -1,7 +1,7 @@
 import { useFrameFocusDispatch } from './useFrameFocusDispatch';
 import { useFrameListener } from './useFrameListener';
 
-export const useFrameFocus = (enableFrameFocusDispatch: boolean, $document: Document, cb: (e: Event) => void) => {
+export const useOnFrameFocus = (enableFrameFocusDispatch: boolean, $document: Document, cb: (e: Event) => void) => {
   useFrameFocusDispatch(enableFrameFocusDispatch, $document);
   useFrameListener(cb, $document);
 };
