36c81f20d6cba467d301783fe19990d28e631f3c
Charles Assuncao <charles.assuncao@microsoft.com>
2021-07-12 06:13:36 -0300

    address comments review

--- packages/fluentui/react-bindings/src/hooks/useOnFrameFocus.ts
+++ packages/fluentui/react-bindings/src/hooks/useOnFrameFocus.ts
@@ -1,6 +1,9 @@
 import { useFrameFocusDispatch } from './useFrameFocusDispatch';
 import { useFrameListener } from './useFrameListener';
 
+/**
+ * It enabables a interval to check if the iframe is focused and executes the callback function.
+ */
 export const useOnFrameFocus = (enableFrameFocusDispatch: boolean, $document: Document, cb: (e: Event) => void) => {
   useFrameFocusDispatch(enableFrameFocusDispatch, $document);
   useFrameListener(enableFrameFocusDispatch, cb, $document);
