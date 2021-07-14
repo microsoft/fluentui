336f319449d10604dd7c1183e952800c15dfcd3f
Jan Pilzer <japilzer@microsoft.com>
2021-07-14 15:23:22 -0700

    Add Screener test for Chat for V2 themes

--- packages/fluentui/docs/src/examples/components/Chat/Types/ChatExampleCompact.shorthand.steps.ts
+++ packages/fluentui/docs/src/examples/components/Chat/Types/ChatExampleCompact.shorthand.steps.ts
@@ -1,5 +1,9 @@
 import { ScreenerTestsConfig } from '@fluentui/scripts/screener';
+import { chatMessageClassName } from '@fluentui/react-northstar';
 
-const config: ScreenerTestsConfig = { themes: ['teams', 'teamsDark', 'teamsHighContrast'] };
+const config: ScreenerTestsConfig = {
+  themes: ['teams', 'teamsDark', 'teamsHighContrast', 'teamsV2', 'teamsDarkV2'],
+  steps: [builder => builder.hover(chatMessageClassName).snapshot('Mouse hover on first message')],
+};
 
 export default config;
