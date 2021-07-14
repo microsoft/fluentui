f945ced1cd9fc16c7c1afc4cc4c71d2f5adb0b0a
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-07-14 10:09:06 -0700

    update buttonBaseProps based on changes made in #18563

--- packages/react-button/src/buttonBaseProps.stories.ts
+++ packages/react-button/src/buttonBaseProps.stories.ts
@@ -4,8 +4,11 @@ import { PropDefinition } from './Playground.types.stories';
 type ExampleProps = { iconOnly?: string };
 
 export const buttonBaseProps: PropDefinition<ButtonProps & ExampleProps>[] = [
+  { propName: 'block', propType: 'boolean' },
+  { propName: 'circular', propType: 'boolean' },
   { propName: 'content', propType: 'string', defaultValue: 'This is a button', dependsOnProps: ['~iconOnly'] },
-  { propName: 'disabled', propType: 'boolean' },
+  { propName: 'disabled', propType: 'boolean', dependsOnProps: ['~disabledFocusable'] },
+  { propName: 'disabledFocusable', propType: 'boolean', dependsOnProps: ['~disabled'] },
   { propName: 'icon', propType: 'boolean' },
   { propName: 'iconOnly', propType: 'boolean', dependsOnProps: ['icon'] },
   {
@@ -14,8 +17,9 @@ export const buttonBaseProps: PropDefinition<ButtonProps & ExampleProps>[] = [
     defaultValue: 'before',
     dependsOnProps: ['icon', '~iconOnly'],
   },
-  { propName: 'primary', propType: 'boolean', dependsOnProps: ['~subtle', '~transparent'] },
+  { propName: 'outline', propType: 'boolean', dependsOnProps: ['~primary', '~subtle', '~transparent'] },
+  { propName: 'primary', propType: 'boolean', dependsOnProps: ['~outline', '~subtle', '~transparent'] },
   { propName: 'size', propType: ['small', 'medium', 'large'], defaultValue: 'medium' },
-  { propName: 'subtle', propType: 'boolean', dependsOnProps: ['~primary', '~transparent'] },
-  { propName: 'transparent', propType: 'boolean', dependsOnProps: ['~primary', '~subtle'] },
+  { propName: 'subtle', propType: 'boolean', dependsOnProps: ['~outline', '~primary', '~transparent'] },
+  { propName: 'transparent', propType: 'boolean', dependsOnProps: ['~outline', '~primary', '~subtle'] },
 ];
