b33747106ad308bdf1a748f28c2621b3c9cdda49
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-07-15 08:03:00 -0700

    add crypto and stream polyfills to react-examples SB webpack config

--- packages/react-examples/.storybook/main.js
+++ packages/react-examples/.storybook/main.js
@@ -25,7 +25,14 @@ export default {
       loader: path.resolve(__dirname, 'preview-loader.js'),
     });
 
-    return customConfig;
+    return {
+      ...customConfig,
+      resolve: {
+        ...customConfig.resolve,
+        // @ts-ignore
+        fallback: { crypto: require.resolve('crypto-browserify'), stream: require.resolve('stream-browserify') },
+      },
+    };
   },
 
   core: {
