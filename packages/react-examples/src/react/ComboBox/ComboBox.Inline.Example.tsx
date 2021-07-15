544e4df4db5a6149245097dc03c73e5a9e40d816
Sarah Higley <sarah.higley@microsoft.com>
2021-07-15 16:40:57 -0700

    add extra whitespace for inline combo

--- packages/react-examples/src/react/ComboBox/ComboBox.Inline.Example.tsx
+++ packages/react-examples/src/react/ComboBox/ComboBox.Inline.Example.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
-import { ComboBox, IComboBoxOption, IComboBoxStyles, SelectableOptionMenuItemType } from '@fluentui/react';
+import { ComboBox, IComboBoxOption, IComboBoxStyles } from '@fluentui/react';
 
 const options: IComboBoxOption[] = [
-  { key: 'Header1', text: 'First heading', itemType: SelectableOptionMenuItemType.Header },
   { key: 'A', text: 'Option A' },
   { key: 'B', text: 'Option B' },
   { key: 'C', text: 'Option C' },
   { key: 'D', text: 'Option D' },
-  { key: 'divider', text: '-', itemType: SelectableOptionMenuItemType.Divider },
-  { key: 'Header2', text: 'Second heading', itemType: SelectableOptionMenuItemType.Header },
-  { key: 'E', text: 'Option E' },
-  { key: 'F', text: 'Option F', disabled: true },
-  { key: 'G', text: 'Option G' },
-  { key: 'H', text: 'Option H' },
-  { key: 'I', text: 'Option I' },
-  { key: 'J', text: 'Option J' },
 ];
 // Optional styling to make the example look nicer
 const comboBoxStyles: Partial<IComboBoxStyles> = { root: { maxWidth: 300 } };
@@ -28,6 +19,11 @@ export const ComboBoxInlineExample: React.FunctionComponent = () => {
         options={options}
         styles={comboBoxStyles}
         calloutProps={{ doNotLayer: true }}
+      />
+      <div
+        // since this example is an inline picker, it needs some forced space below
+        // so when embedded in an iframe in the website, the dropdown shows up
+        style={{ height: '10em' }}
       />
     </div>
   );
