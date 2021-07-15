0cd0a13f391021534ea0ee2e2b68632951d6f0de
André Dias <andredias@microsoft.com>
2021-07-15 16:43:02 +0200

    Add Title1 wrapper
    Co-authored-by: Tringa Krasniqi <tkrasniqi@microsoft.com>

---
+++ packages/react-text/src/components/Title1/Title1.types.ts
@@ -0,0 +1,6 @@
+import { TextProps } from '../Text/index';
+
+/**
+ * Title1 Props
+ */
+export interface Title1Props extends Omit<TextProps, 'font' | 'size'> {}
--- packages/react-text/src/index.ts
+++ packages/react-text/src/index.ts
@@ -2,3 +2,4 @@ export * from './Text';
 export * from './typographyStyles';
 export * from './Display';
 export * from './LargeTitle';
+export * from './Title1';
