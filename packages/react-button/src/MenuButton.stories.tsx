ed0485d70c308ad5b2c92c02b325e0c4ccbafcab
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-07-19 08:44:28 -0700

    add ts-ignore on react-menu import

--- packages/react-button/src/MenuButton.stories.tsx
+++ packages/react-button/src/MenuButton.stories.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { Menu, MenuItem, MenuList, MenuProps, MenuTrigger } from '@fluentui/react-menu';
 import { MenuButton, MenuButtonProps } from './MenuButton';
 import { Playground } from './Playground.stories';
