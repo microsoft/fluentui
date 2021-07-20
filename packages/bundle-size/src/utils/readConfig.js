7de7cc768ef53b4f1af1b7bf6e28cc5d08f23742
Lingfan Gao <lingfan.gao@microsoft.com>
2021-07-20 12:05:11 +0200

    fix type

--- packages/bundle-size/src/utils/readConfig.js
+++ packages/bundle-size/src/utils/readConfig.js
@@ -19,7 +19,7 @@ let cache;
 /**
  * @returns {Promise<Config>}
  *
- * @param {Boolean} quiet
+ * @param {Boolean | undefined} quiet
  */
 async function readConfig(quiet) {
   // don't use the cache in tests
