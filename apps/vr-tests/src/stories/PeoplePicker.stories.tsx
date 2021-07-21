422e2076c4ac1ed076538e69632d9f3b5cb2c26f
Caleb Zearing <czearing@outlook.com>
2021-07-21 14:46:19 -0700

    Changing the location of the resolveDelay prop.

--- apps/vr-tests/src/stories/PeoplePicker.stories.tsx
+++ apps/vr-tests/src/stories/PeoplePicker.stories.tsx
@@ -185,8 +185,8 @@ storiesOf('PeoplePicker', module).addStory('Normal with text', () => (
         onEmptyInputFocus={getPeople}
         getTextFromItem={getTextFromItem}
         className={'ms-PeoplePicker'}
-        resolveDelay={300}
         pickerSuggestionsProps={suggestionProps}
+        resolveDelay={300}
       />
     </Fabric>
   </Screener>
