c5947ae33e315c6db23e8f2f67be0a90f518d7bf
Charles Assuncao <chassunc@microsoft.com>
2021-07-23 09:42:50 -0300

    remove provider wrapper

--- packages/fluentui/docs/src/examples/components/Loader/Variations/LoaderExampleSecondary.tsx
+++ packages/fluentui/docs/src/examples/components/Loader/Variations/LoaderExampleSecondary.tsx
@@ -2,21 +2,19 @@ import { Loader, Flex, Provider } from '@fluentui/react-northstar';
 import * as React from 'react';
 
 const LoaderExampleSecondary: React.FC = () => (
-  <Provider>
-    <Provider.Consumer
-      render={theme => (
-        <Flex
-          style={{
-            backgroundColor: theme.siteVariables.colorScheme.brand.background,
-            padding: 8,
-            width: 'fit-content',
-          }}
-        >
-          <Loader secondary />
-        </Flex>
-      )}
-    />
-  </Provider>
+  <Provider.Consumer
+    render={theme => (
+      <Flex
+        style={{
+          backgroundColor: theme.siteVariables.colorScheme.brand.background,
+          padding: 8,
+          width: 'fit-content',
+        }}
+      >
+        <Loader secondary />
+      </Flex>
+    )}
+  />
 );
 
 export default LoaderExampleSecondary;
