ecbf6c0fa4376e6659d0739be0e48f9479af1683
yifei <yifei.huang@microsoft.com>
2021-07-22 17:10:39 -0700

    move flag to carousel

--- packages/fluentui/react-northstar/src/components/Carousel/CarouselNavigation.tsx
+++ packages/fluentui/react-northstar/src/components/Carousel/CarouselNavigation.tsx
@@ -59,9 +59,6 @@ export interface CarouselNavigationProps extends UIComponentProps, ChildrenCompo
 
   /** A vertical carousel navigation displays elements vertically. */
   vertical?: boolean;
-
-  /** A navigation may be clickable */
-  disableClickableNav?: boolean;
 }
 
 export type CarouselNavigationStylesProps = Required<
@@ -92,7 +89,6 @@ export const CarouselNavigation: ComponentWithAs<'ul', CarouselNavigationProps> 
     vertical,
     thumbnails,
     styles,
-    disableClickableNav,
   } = props;
   const ElementType = getElementType(props);
   const unhandledProps = useUnhandledProps(CarouselNavigation.handledProps, props);
@@ -120,16 +116,13 @@ export const CarouselNavigation: ComponentWithAs<'ul', CarouselNavigationProps> 
     rtl: context.rtl,
   });
 
-  const handleItemOverrides = (variables: any, disableClickableNav: boolean) => predefinedProps =>
-    disableClickableNav
-      ? undefined
-      : {
-          onClick: (e, itemProps) => {
-            _.invoke(props, 'onItemClick', e, itemProps);
-            _.invoke(predefinedProps, 'onClick', e, itemProps);
-          },
-          variables: mergeVariablesOverrides(variables, predefinedProps.variables),
-        };
+  const handleItemOverrides = (variables: any) => predefinedProps => ({
+    onClick: (e, itemProps) => {
+      _.invoke(props, 'onItemClick', e, itemProps);
+      _.invoke(predefinedProps, 'onClick', e, itemProps);
+    },
+    variables: mergeVariablesOverrides(variables, predefinedProps.variables),
+  });
 
   const renderItems = () => {
     return _.map(items, (item, index) =>
@@ -144,7 +137,7 @@ export const CarouselNavigation: ComponentWithAs<'ul', CarouselNavigationProps> 
             vertical,
             thumbnails,
           }),
-        overrideProps: handleItemOverrides(variables, disableClickableNav),
+        overrideProps: handleItemOverrides(variables),
       }),
     );
   };
