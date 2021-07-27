879c2b35ff1786390cd94556a5967da2b94b666b
Sagar Charde <v-scharde@microsoft.com>
2021-07-27 20:19:09 +0530

    Accessibility change for HeatMap chart

--- packages/react-examples/src/charting/HeatMapChart/HeatMapChartPage.tsx
+++ packages/react-examples/src/charting/HeatMapChart/HeatMapChartPage.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 
 import { ComponentPage, ExampleCard, IComponentDemoPageProps, PropertiesTableSet } from '@uifabric/example-app-base';
 import { HeatMapChartBasicExample } from './HeatMapChartBasic.Example';
+import { HeatMapChartCustomAccessibilityExample } from './HeatMapChartBasic.CustomAccessibility.Example';
 
 const HeatMapChartBasicExampleCode = require('!raw-loader!@fluentui/react-examples/src/charting/HeatMapChart/HeatMapChartBasic.Example.tsx');
-
+const HeatMapChartCustomAccessibilityExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/HeatMapChart/HeatMapChartBasic.CustomAccessibility.Example.tsx');
 export class HeatMapChart extends React.Component<IComponentDemoPageProps, {}> {
   public render(): React.ReactNode {
     return (
@@ -15,6 +16,9 @@ export class HeatMapChart extends React.Component<IComponentDemoPageProps, {}> {
           <div>
             <ExampleCard title="Heat Map Basic" code={HeatMapChartBasicExampleCode}>
               <HeatMapChartBasicExample />
+            </ExampleCard>
+            <ExampleCard title="Heat Map Custom Accessibility" code={HeatMapChartCustomAccessibilityExampleCode}>
+              <HeatMapChartCustomAccessibilityExample />
             </ExampleCard>
           </div>
         }
