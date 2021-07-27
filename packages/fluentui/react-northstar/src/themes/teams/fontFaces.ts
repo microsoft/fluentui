aa2ca3d0d3134fc2750aebd95125b66040d84ba4
Andrew Martin <andmarti@microsoft.com>
2021-07-26 17:24:00 -0700

    updating font stack

--- packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
+++ packages/fluentui/react-northstar/src/themes/teams/fontFaces.ts
@@ -2,21 +2,33 @@ import { FontFaces } from '@fluentui/styles';
 
 export const fontFaces: FontFaces = [
   {
-    name: 'Segoe UI',
+    name: 'Segoe UI Web',
+    paths: ['https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-light.woff2'],
+    props: { fontWeight: 100 },
+  },
+  {
+    name: 'Segoe UI Web',
+    paths: [
+      'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semilight.woff2',
+    ],
+    props: { fontWeight: 300 },
+  },
+  {
+    name: 'Segoe UI Web',
     paths: [
       'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-regular.woff2',
     ],
     props: { fontWeight: 400 },
   },
   {
-    name: 'Segoe UI',
+    name: 'Segoe UI Web',
     paths: [
       'https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semibold.woff2',
     ],
     props: { fontWeight: 600 },
   },
   {
-    name: 'Segoe UI',
+    name: 'Segoe UI Web',
     paths: ['https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-bold.woff2'],
     props: { fontWeight: 700 },
   },
