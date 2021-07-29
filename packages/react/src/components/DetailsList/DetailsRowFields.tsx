6804578694de9ae2f724bfb9d22bd227fa2ba51e
Jen Downs <jendowns@microsoft.com>
2021-07-28 17:47:36 -0700

    DetailsRowFields: export getCellText

--- packages/react/src/components/DetailsList/DetailsRowFields.tsx
+++ packages/react/src/components/DetailsList/DetailsRowFields.tsx
@@ -4,7 +4,7 @@ import { css } from '../../Utilities';
 import { IDetailsRowFieldsProps } from './DetailsRowFields.types';
 import { DEFAULT_CELL_STYLE_PROPS } from './DetailsRow.styles';
 
-const getCellText = (item: any, column: IColumn): string => {
+export const getCellText = (item: any, column: IColumn): string => {
   let value = item && column && column.fieldName ? item[column.fieldName] : '';
 
   if (value === null || value === undefined) {
