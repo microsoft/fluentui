9a17deba01dee2358e668ff6fef3e6aadf248b23
André Dias <andredias@microsoft.com>
2021-08-04 17:01:06 +0200

    Add config

--- apps/vr-tests/screener.local.config.js
+++ apps/vr-tests/screener.local.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   projectRepo: 'microsoft/fluentui',
   storybookConfigDir: '.storybook',
+  storybookVersion: '5',
+  storybookBinPath: '../../node_modules/.bin/',
   apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
   baseBranch: 'master',
