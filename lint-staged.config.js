dbd24e03d189dc12b750b1d6ef5a6a78d9558275
Martin Hochel <martinhochel@microsoft.com>
2021-08-04 20:15:23 +0200

    chore: revamp & simplify prettier & lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,18 @@
 // @ts-check
 
+const commands = {
+  format: 'prettier --write',
+  /**
+   * Run eslint in fix mode for applicable files followed by prettier.
+   * - The eslint wrapper handles filtering which files should be linted, since we need to both:
+   *   - respect ignore files (which eslint doesn't do by default when passed a specific file path)
+   *   - match the set of files that are linted by the package's normal `lint` command
+   */
+  lint: 'node ./scripts/lint-staged/eslint',
+};
+
 // https://www.npmjs.com/package/lint-staged
 module.exports = {
-  // Run eslint in fix mode for applicable files followed by prettier.
-  // - The eslint wrapper handles filtering which files should be linted, since we need to both:
-  //   - respect ignore files (which eslint doesn't do by default when passed a specific file path)
-  //   - match the set of files that are linted by the package's normal `lint` command
-  // - Prettier must be run in sequence after eslint since both of them can modify files
-  '*': ['node ./scripts/lint-staged/eslint', 'prettier --write'],
+  '**/*.{js,jsx,ts,tsx,json,html,yml,css,scss,md,mdx}': [commands.format],
+  '**/*.{js,jsx,ts,tsx}': [commands.lint],
 };
