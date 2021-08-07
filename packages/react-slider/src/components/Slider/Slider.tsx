c8aa3a59fbe9a59ca90077418d7c82de9f31ee13
Caleb Zearing <czearing@outlook.com>
2021-08-07 15:08:09 -0700

    (react-slider) - Implementing basic Slider component (#19187)
    * Implementing a basic Slider component.
    
    * Adding stories and a few fixes.
    
    * Adding test file.
    
    * Adding more tests
    
    * Updating a test
    
    * More tests
    
    * More tests
    
    * Adding stories and accessibility tests
    
    * Switching left to translateX and adding a container around the Thumb.
    
    * Styling update
    
    * bug fix
    
    * Fixing a bug in preventDefault and stopPropagation.
    
    * Updating the Slider component.
    
    * Adding translateX to the track.
    
    * Cleaning up styling
    
    * Cleaning up render.
    
    * Removing unused utility methods.
    
    * Styling fix and updating stories.
    
    * Adding the snap feature.
    
    * Updating tests
    
    * Updating the API and cleaning up stories.
    
    * Switching to pointer events
    
    * Updating stories and fixing a styling bug in the minWidth for the track.
    
    * High Contrast styling update.
    
    * Adding transition var to styles
    
    * Adding public api
    
    * Updating the API
    
    * Updating deps
    
    * Fixing a event bug.
    
    * Fixing an event bug
    
    * Fixing a stopPropagation bug
    
    * Merge master
    
    * Fixing a styling bug.
    
    * Change files
    
    * Updating pacakge deps
    
    * Removing unused style from Slider stories.
    
    * PR feedback
    
    * Change files
    
    * PR feedback
    
    * Updating styles and package.
    
    * Updating deps and improving types
    
    * Updating deps
    
    * Updating types for the useControllableValue in useSlider
    
    * Separating useSlider into useSliderState and adding tests for root callback methods.
    
    * Update packages/react-slider/src/components/Slider/useSliderState.tsx
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>
    
    * Updating package
    
    * Updating snapshots
    
    * Simplifying prop merging
    
    * Removing unused change files
    
    * Removing classname usage from tests.
    
    * Adding a wrapper around the thumb to simplify its styling. Removing ::after from styles.
    
    * Updating public focus method and adding tests.
    
    * Clamping Slider's controlled value onMount with useControllableState implemented.
    
    * Removing test-utilities usage within Slider and switching it to use test-library.
    
    * Updating package deps.
    
    * Switching to ev.key and removing keyboard-key from Slider package.
    
    * Switching Slider's tests to use resetIdsForTests instead of getId and removing @fluentui/utilities from deps.
    
    * Updating Slider design
    
    * Adding a linear gradient border to the thumb.
    
    * Updating drop shadow radius to match design spec
    
    * Styling fix
    
    * Adding a test for controlled out of bonds values.
    
    * Updating styles and adding a production warning for unclamped controlled values.
    
    * Updating deps
    
    * 1. Changing currentTarget to target to allow for mobile pointer capture.
    2. Updating the styles to add a round focus border during mobile selection.
    
    * Removing snapToStep
    
    * Updating the api
    
    * Removing unused style
    
    * Cleaning up styles
    
    * PR feedback
    
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-slider/src/components/Slider/Slider.tsx
+++ packages/react-slider/src/components/Slider/Slider.tsx
@@ -5,13 +5,15 @@ import { renderSlider } from './renderSlider';
 import { useSliderStyles } from './useSliderStyles';
 
 /**
- * Slider component
+ * Define a styled Slider, using the `useSlider` hook.
+ * {@docCategory Slider}
  */
-export const Slider = React.forwardRef<HTMLElement, SliderProps>((props, ref) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Slider = React.forwardRef<HTMLElement, SliderProps>((props, ref: any) => {
   const state = useSlider(props, ref);
 
   useSliderStyles(state);
+
   return renderSlider(state);
 });
-
 Slider.displayName = 'Slider';
