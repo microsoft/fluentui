a5d7d23b2fbed6e9b5e5976239281b5be74df653
Dexter Lesaca <v-dlesac@microsoft.com>
2021-08-09 15:44:41 -0700

    update combobox story

--- packages/web-components/src/combobox/combobox.stories.ts
+++ packages/web-components/src/combobox/combobox.stories.ts
@@ -9,15 +9,41 @@ export default {
       control: { type: 'radio' },
     },
     autocomplete: {
-      options: ['inline', 'list', 'both'],
+      options: ['inline', 'list', 'none', 'both'],
       control: { type: 'radio' },
+    },
+    position: {
+      options: ['above', 'below'],
+      control: { type: 'radio' },
+    },
+    required: {
+      control: { type: 'boolean' },
     },
   },
 };
 
-const ComboboxTemplate = ({ appearance, value }) => `<fluent-combobox>
-<fluent-option>Judge</fluent-option>
-<fluent-combobox/>`;
+const ComboboxTemplate = ({ appearance, autocomplete, position, required }) => `
+  <fluent-combobox
+    appearance="${appearance}"
+    autocomplete="${autocomplete}"
+    ${required ? 'required' : ''}
+    position="${position}"
+  >
+    <fluent-option>Please Please Me</fluent-option>
+    <fluent-option>With The Beatles</fluent-option>
+    <fluent-option>A Hard Day's Night</fluent-option>
+    <fluent-option>Beatles for Sale</fluent-option>
+    <fluent-option>Help!</fluent-option>
+    <fluent-option>Rubber Soul</fluent-option>
+    <fluent-option>Revolver</fluent-option>
+    <fluent-option>Sgt. Pepper's Lonely Hearts Club Band</fluent-option>
+    <fluent-option>Magical Mystery Tour</fluent-option>
+    <fluent-option>The Beatles</fluent-option>
+    <fluent-option>Yellow Submarine</fluent-option>
+    <fluent-option>Abbey Road</fluent-option>
+    <fluent-option>Let It Be</fluent-option>
+  <fluent-combobox/>
+`;
 
 export const Combobox = ComboboxTemplate.bind({});
 
