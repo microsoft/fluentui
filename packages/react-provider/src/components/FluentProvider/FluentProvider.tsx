c408b03461eee7df06fed3e159baea557a28f484
Oleksandr Fediashov <olfedias@microsoft.com>
2021-08-10 16:26:25 +0200

    chore(FluentProvider): implement useContextValues() pattern

--- packages/react-provider/src/components/FluentProvider/FluentProvider.tsx
+++ packages/react-provider/src/components/FluentProvider/FluentProvider.tsx
@@ -4,12 +4,15 @@ import { FluentProviderProps } from './FluentProvider.types';
 import { renderFluentProvider } from './renderFluentProvider';
 import { useFluentProvider } from './useFluentProvider';
 import { useFluentProviderStyles } from './useFluentProviderStyles';
+import { useFluentProviderContextValues } from './useFluentProviderContextValues';
 
 export const FluentProvider = React.forwardRef<HTMLElement, FluentProviderProps>((props, ref) => {
   const state = useFluentProvider(props, ref);
+  const contextValues = useFluentProviderContextValues(state);
+
   useFluentProviderStyles(state);
 
-  return renderFluentProvider(state);
+  return renderFluentProvider(state, contextValues);
 });
 
 FluentProvider.displayName = 'FluentProvider';
