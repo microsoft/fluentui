38c34b3c07a06c2bc098eae607b57d9e88250cd5
Oleksandr Fediashov <olfedias@microsoft.com>
2021-08-12 14:47:48 +0200

    add snapshot for test

--- packages/react-menu/src/components/Menu/useMenuContextValues.test.tsx
+++ packages/react-menu/src/components/Menu/useMenuContextValues.test.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { useMenu } from './useMenu';
 import { useMenuContextValues } from './useMenuContextValues';
 
-describe('useAccordionContextValues', () => {
+describe('useMenuContextValues', () => {
   it('should return a value for "menu"', () => {
     const { result } = renderHook(() => {
       const state = useMenu({ children: <span /> });
@@ -12,6 +12,28 @@ describe('useAccordionContextValues', () => {
       return useMenuContextValues(state);
     });
 
-    expect(result.current.menu).toBeDefined();
+    expect(result.current.menu).toMatchInlineSnapshot(`
+      Object {
+        "checkedValues": Object {},
+        "defaultCheckedValues": undefined,
+        "hasCheckmarks": undefined,
+        "hasIcons": undefined,
+        "inline": undefined,
+        "isSubmenu": false,
+        "menuPopoverRef": Object {
+          "current": null,
+        },
+        "onCheckedValueChange": [Function],
+        "open": false,
+        "openOnContext": undefined,
+        "openOnHover": false,
+        "persistOnItemClick": undefined,
+        "setOpen": [Function],
+        "triggerId": "menu1",
+        "triggerRef": Object {
+          "current": null,
+        },
+      }
+    `);
   });
 });
