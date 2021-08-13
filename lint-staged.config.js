34b02abd1390e07e1a1bc5a1b43be1fa9f3d43da
Martin Hochel <hochelmartin@gmail.com>
2021-08-13 10:37:56 +0200

    docs update
    Co-authored-by: Elizabeth Craig <ecraig12345@gmail.com>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,9 +6,9 @@ const commands = {
   format: 'prettier --write',
   /**
    * Run eslint in fix mode for applicable files followed by prettier.
-   * - The eslint wrapper handles filtering which files should be linted, since we need to both:
-   *   - respect ignore files (which eslint doesn't do by default when passed a specific file path)
-   *   - match the set of files that are linted by the package's normal `lint` command
+   * The eslint wrapper handles filtering which files should be linted, since we need to both:
+   * - respect ignore files (which eslint doesn't do by default when passed a specific file path)
+   * - match the set of files that are linted by the package's normal `lint` command
    */
   lint: 'node ./scripts/lint-staged/eslint',
 };
