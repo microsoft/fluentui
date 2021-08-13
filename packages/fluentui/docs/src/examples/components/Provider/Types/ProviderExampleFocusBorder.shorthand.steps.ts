eb54610727e81826a518f396f35b2456f5cfa34e
Anna Kellerstein <annkelle@microsoft.com>
2021-08-13 14:13:33 +0200

    fix sharthand test

--- packages/fluentui/docs/src/examples/components/Provider/Types/ProviderExampleFocusBorder.shorthand.steps.ts
+++ packages/fluentui/docs/src/examples/components/Provider/Types/ProviderExampleFocusBorder.shorthand.steps.ts
@@ -3,6 +3,7 @@ import { ScreenerTestsConfig } from '@fluentui/scripts/screener';
 
 const config: ScreenerTestsConfig = {
   steps: [
+    builder => builder.click('#text-focus'),
     (builder, keys) => builder.keys(`.${buttonClassName}`, keys.tab).snapshot('Show overridden border focused styles'),
   ],
 };
