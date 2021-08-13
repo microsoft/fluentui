296de1e8d5b81ef001724131e2c1c7d7a33547df
Caleb Zearing <czearing@outlook.com>
2021-08-12 18:47:03 -0700

    doc update

--- packages/react-slider/src/components/Slider/Slider.types.ts
+++ packages/react-slider/src/components/Slider/Slider.types.ts
@@ -110,7 +110,7 @@ export interface SliderCommon extends Omit<React.HTMLAttributes<HTMLDivElement>,
    * must be in numerical order.
    *
    * 1. `Boolean`: If true marks are visible.
-   * 2. `Number[]`: Marks will be displayed at each provided number.
+   * 2. `number[]`: Marks will be displayed at each provided number.
    * 3. `number | {label: string, value: number}[]`: Marks are shown at the value location and display the custom label.
    */
   marks?: boolean | number[] | (number | { value: number; label?: string | JSX.Element; mark?: JSX.Element })[];
