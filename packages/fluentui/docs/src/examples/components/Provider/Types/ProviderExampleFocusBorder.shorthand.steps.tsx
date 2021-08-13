5864984627118ff1fc625089d62896ae4ca2d428
Anna Kellerstein <annkelle@microsoft.com>
2021-08-13 09:06:46 +0200

    send tab key for screener test

--- packages/fluentui/docs/src/examples/components/Provider/Types/ProviderExampleFocusBorder.shorthand.steps.tsx
+++ packages/fluentui/docs/src/examples/components/Provider/Types/ProviderExampleFocusBorder.shorthand.steps.tsx
@@ -1,8 +1,11 @@
-import { ScreenerTestsConfig } from '@fluentui/scripts/screener';
 import { buttonClassName } from '@fluentui/react-northstar';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ScreenerTestsConfig } from '@fluentui/scripts/screener';
 
 const config: ScreenerTestsConfig = {
-  steps: [builder => builder.focus(`.${buttonClassName}`).snapshot('Show overridden border focused styles')],
+  steps: [
+    (builder, keys) => builder.keys(`.${buttonClassName}`, keys.tab).snapshot('Show overridden border focused styles'),
+  ],
 };
 
 export default config;
