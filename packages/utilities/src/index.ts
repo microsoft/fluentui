f6b43972394855731835dff4b4fbb57e489b5324
dzearing <dzearing@hotmail.com>
2021-08-17 10:53:16 -0700

    Fixing lint comment.

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -69,11 +69,11 @@ export * from './styled';
 export * from './warn';
 export * from './ie11Detector';
 export * from './getPropsWithDefaults';
-// eslint-disable-next-line deprecation/deprecation
 export { setFocusVisibility, IsFocusVisibleClassName } from './setFocusVisibility';
 export { setSSR } from './dom/setSSR';
 export { createMergedRef } from './createMergedRef';
 
 import './version';
 
+// eslint-disable-next-line deprecation/deprecation
 export type { IStyleFunctionOrObject, Omit } from '@fluentui/merge-styles';
