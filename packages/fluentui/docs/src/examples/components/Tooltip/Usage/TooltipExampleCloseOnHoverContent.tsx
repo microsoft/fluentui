4bb95dee9470d664809e9e9ea6c0643295e47b38
Charles Assuncao <chassunc@microsoft.com>
2021-08-18 08:48:55 -0300

    e2e, example and changelog

--- packages/fluentui/docs/src/examples/components/Tooltip/Usage/TooltipExampleCloseOnHoverContent.tsx
+++
@@ -1,8 +0,0 @@
-import { Button, Tooltip } from '@fluentui/react-northstar';
-import * as React from 'react';
-
-const TooltipExampleDialogContentShorthand: React.FC = () => (
-  <Tooltip dismissOnContentMouseEnter trigger={<Button content="Click me!" />} content="Hello from tooltip!" />
-);
-
-export default TooltipExampleDialogContentShorthand;
