152c4bb0d4f3d7d779afc29bc716164bb223f8a8
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-08-24 10:53:19 +0200

    Reverting to compat

--- packages/react-switch/src/components/Switch/renderSwitch.tsx
+++ packages/react-switch/src/components/Switch/renderSwitch.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { getSlots } from '@fluentui/react-utilities';
+import { getSlotsCompat } from '@fluentui/react-utilities';
 import { switchShorthandProps } from './useSwitch';
-import type { SwitchSlots, SwitchState } from './Switch.types';
+import type { SwitchState } from './Switch.types';
 
 /**
  * Render the final JSX of Switch
  */
 export const renderSwitch = (state: SwitchState) => {
-  const { slots, slotProps } = getSlots<SwitchSlots>(state, switchShorthandProps);
+  const { slots, slotProps } = getSlotsCompat(state, switchShorthandProps);
 
   return <slots.root {...slotProps.root}>{state.children}</slots.root>;
 };
