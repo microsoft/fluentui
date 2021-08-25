da90afe0eb50b77d3c33ac2123918210845aa1fd
Oleksandr Fediashov <olfedias@microsoft.com>
2021-08-25 12:43:11 +0200

    remove .only()

--- packages/react-conformance-make-styles/src/classNameWins.ts
+++ packages/react-conformance-make-styles/src/classNameWins.ts
@@ -47,7 +47,7 @@ export const classNameWins: ConformanceTest = (componentInfo, testInfo) => {
     container = null;
   });
 
-  it.only('"className" passed last wins', async () => {
+  it('"className" passed last wins', async () => {
     // To mock mergeClasses() we need require a component again, this could be done via "jest.isolateModules()"
     // but a bug that prevents it was fixed only in Jest 27.
     // https://github.com/facebook/jest/pull/10963
