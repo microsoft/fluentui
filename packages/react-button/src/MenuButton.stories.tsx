3a82b18a2b30a3275f1a4f90b57dfed0a6cfb549
Martin Hochel <martinhochel@microsoft.com>
2021-08-27 17:19:47 +0200

    fixup! fixup! revert(react-button): remove vNext components used in SB  from devDeps that are stale

--- packages/react-button/src/MenuButton.stories.tsx
+++ packages/react-button/src/MenuButton.stories.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 import { Menu, MenuItem, MenuList, MenuTrigger } from '@fluentui/react-menu';
+import type { MenuProps } from '@fluentui/react-menu';
 // @ts-ignore
-import type { MenuButtonProps } from './MenuButton';
 /* eslint-enable @typescript-eslint/ban-ts-comment */
 
 import { MenuButton } from './MenuButton';
 import { Playground } from './Playground.stories';
 import { buttonBaseProps } from './buttonBaseProps.stories';
-import type { MenuProps } from '@fluentui/react-menu';
+import type { MenuButtonProps } from './MenuButton';
 import type { PlaygroundProps } from './Playground.types.stories';
 
 const ExampleMenu = (props: MenuButtonProps): JSX.Element => (
