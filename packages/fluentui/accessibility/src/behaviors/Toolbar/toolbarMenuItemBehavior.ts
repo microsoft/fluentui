fc951cd26f5aed5626887b6390778042547c9287
chassunc <86579954+chassunc@users.noreply.github.com>
2021-08-28 10:15:28 -0300

    Update toolbarMenuItemBehavior.ts

--- packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Toolbar/toolbarMenuItemBehavior.ts
@@ -14,15 +14,20 @@ import { menuItemBehavior, MenuItemBehaviorProps } from '../Menu/menuItemBehavio
  * Adds attribute 'aria-describedby' based on the property 'aria-describedby' to 'root' slot.
  * Adds attribute 'aria-expanded=true' based on the property 'menuOpen' if the component has 'hasMenu' property to 'root' slot.
  * Adds attribute 'aria-haspopup=true' to 'root' slot if 'hasMenu' property is set.
- * Adds attribute 'aria-disabled=true' based on the property 'disabled'. This can be overriden by providing 'aria-disabled' property directly to the component.
  * Triggers 'performClick' action with 'Enter' or 'Spacebar' on 'root'.
  * Triggers 'closeMenuAndFocusTrigger' action with 'Escape' on 'wrapper'.
  * Triggers 'closeAllMenusAndFocusNextParentItem' action with 'ArrowRight' on 'wrapper'.
  * Triggers 'closeMenu' action with 'ArrowLeft' on 'wrapper'.
  * Triggers 'openMenu' action with 'ArrowRight' on 'wrapper'.
+ * Adds attribute 'disabled=true' based on the property 'disabled'.
+ * Adds attribute 'aria-disabled=true' based on the property 'disabledFocusable'.
  */
 export const toolbarMenuItemBehavior: Accessibility<ToolbarMenuItemBehaviorProps> = props => {
-  return menuItemBehavior({ ...props, vertical: true });
+  return menuItemBehavior({ 
+    ...props, vertical: true, 
+    disabled: props.disabled,
+    'aria-disabled': props.disabledFocusable,
+  });
 };
 
 export type ToolbarMenuItemBehaviorProps = Omit<MenuItemBehaviorProps, 'vertical'>;
