9b9135323a15377c47df7285562c4f476c8ea2a9
Peter Dräxler <peter@draxler.ml>
2021-09-01 14:27:34 +0200

    fix pipeline

--- packages/react-button/src/components/Button/Button.stories.tsx
+++ packages/react-button/src/components/Button/Button.stories.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
-import { Button } from '../../Button';
+import { Button } from '@fluentui/react-button';
 import { Meta } from '@storybook/react';
 
 import descriptionMd from './ButtonDescription.md';
 import bestPracticesMd from './ButtonBestPractices.md';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Emphasis } from './ButtonEmphasis.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Default } from './ButtonDefault.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Disabled } from './ButtonDisabled.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { WithIcon } from './ButtonWithIcon.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Circular } from './ButtonCircular.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Size } from './ButtonSize.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Block } from './ButtonBlock.stories';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { WithLongText } from './ButtonWithLongText.stories';
 
 export default {
--- packages/react-button/src/components/Button/ButtonDefault.stories.tsx
+++ packages/react-button/src/components/Button/ButtonDefault.stories.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
-import { Button, ButtonProps } from '@fluentui/react-button';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
+import { Button } from '@fluentui/react-button';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
+import { ButtonProps } from './Button.types';
 
 export const Default = (props: ButtonProps) => <Button {...props}>Button</Button>;
