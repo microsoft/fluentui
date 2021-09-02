34830ee5eaf75dbaef10c90d6db1a1020ba12414
Lingfan Gao <lingfangao@hotmail.com>
2021-09-02 11:47:31 +0200

    chore(react-positioning): Add documentation for FLuent positioning

---
+++ packages/react-positioning/src/positioningBestPractices.md
@@ -0,0 +1,3 @@
+# Best practices
+
+⚠️ Simpler configuration is better. Positioning is complicated, do not make your life harder by using it too much ⚠️
---
+++ packages/react-positioning/src/utils.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { PositioningProps } from './types';
+
+export const Positioning: React.FC<PositioningProps> = props => {
+  return <div />;
+};
