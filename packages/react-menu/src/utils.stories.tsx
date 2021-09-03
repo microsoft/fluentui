a19b84212a7fdecf386ed7d1c615c032cc3b2d47
Lingfan Gao <lingfangao@hotmail.com>
2021-09-03 17:07:30 +0200

    update comments

--- packages/react-menu/src/utils.stories.tsx
+++ packages/react-menu/src/utils.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// Need to disable compilation for aliases: https://github.com/microsoft/fluentui/pull/16976/files#r575447074
+// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { Button as ReactButton, ButtonProps } from '@fluentui/react-button';
@@ -7,7 +7,7 @@ export const Button = React.forwardRef<HTMLElement, ButtonProps>((props, ref) =>
   return <ReactButton ref={ref} {...props} />;
 });
 
-// Need to disable compilation for aliases: https://github.com/microsoft/fluentui/pull/16976/files#r575447074
+// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { makeStyles } from '@fluentui/react-make-styles';
