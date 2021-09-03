bcd6b9f45523259732c9186aaf61f15e2f2ce295
Caleb Zearing <czearing@outlook.com>
2021-09-03 15:25:03 -0700

    (react-slider) - Adding a hidden input element (#19509)
    * Adding hidden input
    
    * Adding the hidden input element
    
    * Adding initial changes
    
    * Updating tests
    
    * Updating tests
    
    * Quick fix
    
    * Quick fix
    
    * Alphabetizing ShorthandProps
    
    * Adding negative step prop test back
    
    * Updating the API
    
    * Adding cursor styling for the input element
    
    * Updating styles
    
    * Adding more tests to validate input props
    
    * PR feedback and fixing a rounding bug related to an optimization.
    
    * Fixing a logic bug in uncontrolled scenarios
    
    * Quick update
    
    * Reverting changes
    
    * Adding a test for clamping value inbetween steps

--- packages/react-slider/src/components/Slider/useSlider.ts
+++ packages/react-slider/src/components/Slider/useSlider.ts
@@ -7,14 +7,15 @@ import { SliderProps, SliderShorthandProps, SliderState } from './Slider.types';
  * Array of all shorthand properties listed in SliderShorthandProps
  */
 export const sliderShorthandProps: SliderShorthandProps[] = [
+  'activeRail',
+  'input',
   'rail',
   'sliderWrapper',
-  'trackWrapper',
-  'track',
-  'marksWrapper',
-  'thumbWrapper',
   'thumb',
-  'activeRail',
+  'thumbWrapper',
+  'track',
+  'trackWrapper',
+  'marksWrapper',
 ];
 
 /**
@@ -28,6 +29,7 @@ export const useSlider = (props: SliderProps, ref: React.Ref<HTMLElement>, defau
   const state = mergeProps(
     {
       ref,
+      as: 'div',
       sliderWrapper: { as: 'div', children: null },
       rail: { as: 'div', children: null },
       trackWrapper: { as: 'div', children: null },
@@ -36,6 +38,7 @@ export const useSlider = (props: SliderProps, ref: React.Ref<HTMLElement>, defau
       marksWrapper: { as: 'div', children: null },
       thumb: { as: 'div', children: null },
       activeRail: { as: 'div', children: null },
+      input: { as: 'input', type: 'range', children: null },
     },
     defaultProps && resolveShorthandProps(defaultProps, sliderShorthandProps),
     resolveShorthandProps(props, sliderShorthandProps),
