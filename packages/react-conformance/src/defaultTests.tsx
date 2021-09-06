37e0f35eb706d40c4537010de97db94a831022d3
Oleksandr Fediashov <olfedias@microsoft.com>
2021-09-06 18:01:53 +0200

    feat(react-conformance): add test for callback arguments (#19369)
    * feat(react-conformance): add test for callback arguments
    
    * Change files
    
    * lint fixes
    
    * remove cast
    
    * handle undefined
    
    * disable tests in FocusZone
    
    * Change files
    
    * handle arrays
    
    * disable test for MenuList
    
    * Change files
    
    * disable test for Menu
    
    * disable tests
    
    * Change files
    
    * disable test in N*
    
    * add error messages
    
    * update changelog entries
    
    * fix comment
    
    * address PR review comments
    
    * fix test
    
    * restore changes for Menu as tests were fixed
    
    * Update change/@fluentui-react-conformance-1461c075-e920-4b98-ad27-2adbf47103e0.json

--- packages/react-conformance/src/defaultTests.tsx
+++ packages/react-conformance/src/defaultTests.tsx
@@ -2,6 +2,7 @@ import { TestObject, IsConformantOptions } from './types';
 import { defaultErrorMessages } from './defaultErrorMessages';
 import { ComponentDoc } from 'react-docgen-typescript';
 import { getComponent } from './utils/getComponent';
+import { getCallbackArguments } from './utils/getCallbackArguments';
 import { mount, ReactWrapper } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import parseDocblock from './utils/parseDocblock';
@@ -10,6 +11,8 @@ import * as React from 'react';
 import * as _ from 'lodash';
 import * as path from 'path';
 import consoleUtil from './utils/consoleUtil';
+
+const CALLBACK_REGEX = /^on(?!Render[A-Z])[A-Z]/;
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
@@ -314,7 +317,7 @@ export const defaultTests: TestObject = {
       const ignoreProps = testOptions['consistent-callback-names']?.ignoreProps || [];
 
       const invalidProps = propNames.filter(propName => {
-        if (!ignoreProps.includes(propName) && /^on(?!Render[A-Z])[A-Z]/.test(propName)) {
+        if (!ignoreProps.includes(propName) && CALLBACK_REGEX.test(propName)) {
           const words = propName.slice(2).match(/[A-Z][a-z]+/g);
           if (words) {
             // Make sure last word doesn't end with ed
@@ -329,6 +332,60 @@ export const defaultTests: TestObject = {
         expect(invalidProps).toEqual([]);
       } catch (e) {
         throw new Error(defaultErrorMessages['consistent-callback-names'](testInfo, invalidProps));
+      }
+    });
+  },
+
+  /** Ensures that components have consistent callback arguments (ev, data) */
+  'consistent-callback-args': (componentInfo, testInfo, tsProgram) => {
+    it('has consistent custom callback arguments (consistent-callback-args)', () => {
+      const { testOptions = {} } = testInfo;
+
+      const propNames = Object.keys(componentInfo.props);
+      const ignoreProps = testOptions['consistent-callback-args']?.ignoreProps || [];
+
+      const invalidProps = propNames.filter(propName => {
+        if (!ignoreProps.includes(propName) && CALLBACK_REGEX.test(propName)) {
+          const propInfo = componentInfo.props[propName];
+
+          if (!propInfo.declarations) {
+            throw new Error(
+              [
+                `Definition for "${propName}" does not have ".declarations" produced by "react-docgen-typescript".`,
+                'Please report a bug in Fluent UI repo if this happens. Include in a bug report details about file',
+                'where it happens and used interfaces.',
+              ].join(' '),
+            );
+          }
+
+          if (propInfo.declarations.length !== 1) {
+            throw new Error(
+              [
+                `Definition for "${propName}" has multiple elements in ".declarations" produced by `,
+                `"react-docgen-typescript".`,
+                'Please report a bug in Fluent UI repo if this happens. Include in a bug report details about file',
+                'where it happens and used interfaces.',
+              ].join(' '),
+            );
+          }
+
+          const rootFileName = propInfo.declarations[0].fileName;
+          const typeName = propInfo.declarations[0].name;
+
+          const handlerArguments = getCallbackArguments(tsProgram, rootFileName, typeName, propName);
+
+          if (Object.keys(handlerArguments).length !== 2) {
+            return true;
+          }
+        }
+
+        return false;
+      });
+
+      try {
+        expect(invalidProps).toEqual([]);
+      } catch (e) {
+        throw new Error(defaultErrorMessages['consistent-callback-args'](testInfo, invalidProps));
       }
     });
   },
