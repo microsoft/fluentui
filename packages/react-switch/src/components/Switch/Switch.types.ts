d19921c55cdfb58b8e24d511730813310d4f85c4
Caleb Zearing <czearing@outlook.com>
2021-09-08 12:47:17 -0700

    Cleaning up types files

--- packages/react-switch/src/components/Switch/Switch.types.ts
+++ packages/react-switch/src/components/Switch/Switch.types.ts
@@ -4,92 +4,58 @@ import type { ComponentProps, ComponentState, ObjectShorthandProps } from '@flue
 
 export type SwitchSlots = {
   /**
-
    * The root of the Switch.
-
    */
-
   root: ObjectShorthandProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
 
   /**
-
    * The bar indicating the status of the Switch.
-
    */
-
   track: React.HTMLAttributes<HTMLElement>;
 
   /**
-
    * The wrapper around the thumb. It is used as the active area for the thumb to position itself.
-
    */
-
   thumbWrapper: React.HTMLAttributes<HTMLElement>;
 
   /**
-
    * The circular icon indicating the status of the Switch.
-
    */
-
   thumb: React.HTMLAttributes<HTMLElement>;
 
   /**
-
    * The hidden input that handles the Switch's internal functionality.
-
    */
-
   input: ObjectShorthandProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 };
 
 export interface SwitchCommons {
   /**
-
    * The starting value for a uncontrolled Switch. If `true` then the Switch will be enabled.
-
    * Mutually exclusive with `checked` prop.
-
    *
-
    * @default false
-
    */
-
   defaultChecked?: boolean;
 
   /**
-
    * The current value for a controlled Switch. If `true` then the Switch will be enabled.
-
    * Mutually exclusive with `defaultChecked` prop.
-
    */
-
   checked?: boolean;
 
   /**
-
    * Whether the Switch should be disabled.
-
    *
-
    * @default false
-
    */
-
   disabled?: boolean;
 
   /**
-
    * Callback to be called when the `checked` value changes.
-
    */
-
   onChange?: (
     ev: React.ChangeEvent<HTMLInputElement>,
-
     data: {
       checked: boolean;
     },
