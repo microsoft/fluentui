7beb614d1eefdaed4c810ebb18178b0a760fd272
Caleb Zearing <czearing@outlook.com>
2021-09-08 12:51:27 -0700

    Cleaning up useSwitchState

--- packages/react-switch/src/components/Switch/useSwitchState.ts
+++ packages/react-switch/src/components/Switch/useSwitchState.ts
@@ -1,33 +1,24 @@
 import * as React from 'react';
-
 import { useControllableState, useEventCallback, useMergedRefs } from '@fluentui/react-utilities';
-
 import { useFluent } from '@fluentui/react-shared-contexts';
-
 import type { SwitchState } from './Switch.types';
 
 export const useSwitchState = (state: SwitchState) => {
   const { defaultChecked = false, checked, disabled = false, onChange } = state;
 
   const { dir } = useFluent();
-
   const inputRef = useMergedRefs(state.input.ref);
-
   const [internalValue, setInternalValue] = useControllableState({
     defaultState: defaultChecked,
-
     state: checked,
-
     initialState: false,
   });
 
   const setChecked = React.useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>, incomingValue: boolean) => {
       onChange?.(ev, { checked: incomingValue });
-
       setInternalValue(incomingValue);
     },
-
     [onChange, setInternalValue],
   );
 
@@ -35,9 +26,7 @@ export const useSwitchState = (state: SwitchState) => {
 
   const onInputChange = useEventCallback((ev: React.ChangeEvent<HTMLInputElement>) => {
     ev.stopPropagation();
-
     userOnChange?.(ev);
-
     setChecked(ev, ev.currentTarget.checked);
   });
 
@@ -53,17 +42,12 @@ export const useSwitchState = (state: SwitchState) => {
   };
 
   // Input Props
-
   state.input.onChange = onInputChange;
-
   state.input.checked = internalValue;
-
   state.input.disabled = disabled;
-
   state.input.ref = inputRef;
 
   // thumbContainer Props
-
   state.thumbWrapper.style = thumbWrapperStyles;
 
   return state;
