e9d49763bf0f26370b0d9dab2816b5bd9d3ed429
Charles Assuncao <chassunc@microsoft.com>
2021-09-11 10:19:43 -0300

    chore: improve accordion docs

---
+++ packages/react-accordion/src/stories/AccordionSizes.stories.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { AccordionItem, AccordionHeader, AccordionPanel, Accordion } from '../index';
+
+export const Sizes = () => (
+  <>
+    <Accordion>
+      <AccordionItem value="1">
+        <AccordionHeader size="small">Accordion Header 1</AccordionHeader>
+        <AccordionPanel>
+          <div>Accordion Panel 1</div>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+    <Accordion>
+      <AccordionItem value="1">
+        <AccordionHeader size="medium">Accordion Header 1</AccordionHeader>
+        <AccordionPanel>
+          <div>Accordion Panel 1</div>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+    <Accordion>
+      <AccordionItem value="1">
+        <AccordionHeader size="large">Accordion Header 1</AccordionHeader>
+        <AccordionPanel>
+          <div>Accordion Panel 1</div>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+    <Accordion>
+      <AccordionItem value="1">
+        <AccordionHeader size="extra-large">Accordion Header 1</AccordionHeader>
+        <AccordionPanel>
+          <div>Accordion Panel 1</div>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  </>
+);
---
+++ packages/react-accordion/src/stories/AccordionWithIcon.stories.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { RocketIcon } from '../icons.stories';
+import { AccordionItem, AccordionHeader, AccordionPanel, Accordion, AccordionHeaderProps } from '../index';
+
+export const WithIcon = (args: AccordionHeaderProps) => (
+  <Accordion>
+    <AccordionItem value="1">
+      <AccordionHeader {...args}>Accordion Header 1</AccordionHeader>
+      <AccordionPanel>
+        <div>Accordion Panel 1</div>
+      </AccordionPanel>
+    </AccordionItem>
+    <AccordionItem value="2">
+      <AccordionHeader {...args}>Accordion Header 2</AccordionHeader>
+      <AccordionPanel>
+        <div>Accordion Panel 2</div>
+      </AccordionPanel>
+    </AccordionItem>
+    <AccordionItem value="3">
+      <AccordionHeader {...args}>Accordion Header 3</AccordionHeader>
+      <AccordionPanel>
+        <div>Accordion Panel 3</div>
+      </AccordionPanel>
+    </AccordionItem>
+  </Accordion>
+);
+
+WithIcon.args = {
+  icon: <RocketIcon />,
+} as AccordionHeaderProps;
