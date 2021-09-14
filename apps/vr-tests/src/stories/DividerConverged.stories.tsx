1e5b888607626cae4ff69b5b9055bf8b9537c45b
Caleb Zearing <czearing@outlook.com>
2021-09-14 16:53:14 -0700

    (vr-tests) - Enabling converged components to test RTL through config prop (#19670)
    * Adding context
    
    * Test
    
    * Rewriting converged VR tests
    
    * Cleaning up preview.js imports
    
    * Updating ImageNext stories
    
    * Updating Divider stories
    
    * Updating Switch vr tests
    
    * Switching converged vr tests to contain converged in their title.
    
    * Adding "Converged" title to a converged Accordian story.

--- apps/vr-tests/src/stories/DividerConverged.stories.tsx
+++ apps/vr-tests/src/stories/DividerConverged.stories.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
-import { FluentProviderDecorator } from '../utilities/index';
 import { mergeStyles } from '@fluentui/react';
 import { Divider } from '@fluentui/react-divider';
 
@@ -13,33 +12,48 @@ const horizontalStyles = mergeStyles({
   width: '400px',
 });
 
-storiesOf('Divider', module)
-  .addDecorator(FluentProviderDecorator)
+storiesOf('Divider Converged', module)
   .addDecorator(story => (
     <Screener steps={new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>
       {story()}
     </Screener>
   ))
-  .addStory('Horizontal without content', () => (
-    <div className={horizontalStyles}>
-      <Divider />
-    </div>
-  ))
-  .addStory('Horizontal with content', () => (
-    <div className={horizontalStyles}>
-      <Divider>Today</Divider>
-    </div>
-  ))
-  .addStory('Horizontal Start Aligned', () => (
-    <div className={horizontalStyles}>
-      <Divider alignContent="start">Today</Divider>
-    </div>
-  ))
-  .addStory('Horizontal End Aligned', () => (
-    <div className={horizontalStyles}>
-      <Divider alignContent="end">Today</Divider>
-    </div>
-  ))
+  .addStory(
+    'Horizontal without content',
+    () => (
+      <div className={horizontalStyles}>
+        <Divider />
+      </div>
+    ),
+    { rtl: true },
+  )
+  .addStory(
+    'Horizontal with content',
+    () => (
+      <div className={horizontalStyles}>
+        <Divider>Today</Divider>
+      </div>
+    ),
+    { rtl: true },
+  )
+  .addStory(
+    'Horizontal Start Aligned',
+    () => (
+      <div className={horizontalStyles}>
+        <Divider alignContent="start">Today</Divider>
+      </div>
+    ),
+    { rtl: true },
+  )
+  .addStory(
+    'Horizontal End Aligned',
+    () => (
+      <div className={horizontalStyles}>
+        <Divider alignContent="end">Today</Divider>
+      </div>
+    ),
+    { rtl: true },
+  )
   .addStory('Vertical Center Aligned', () => (
     <div className={verticalStyles}>
       <Divider vertical>Today</Divider>
