79941b8ebfd53bfdc6f6a6896b53519f29988f7a
Caleb Zearing <czearing@outlook.com>
2021-09-14 16:48:47 -0700

    P1 - Renaming rtl config to includeRtl

--- apps/vr-tests/src/stories/AvatarConverged.stories.tsx
+++ apps/vr-tests/src/stories/AvatarConverged.stories.tsx
@@ -95,7 +95,7 @@ storiesOf('Avatar Converged', module)
         <Avatar name={examples.name[7]} image={examples.image[7]} badge="available" />
       </>
     ),
-    { rtl: true },
+    { includeRtl: true },
   )
   .addStory('size+name', () => <AvatarList names={examples.name} />)
   .addStory('size+icon+badge+square', () => <AvatarList badge="outOfOffice" square />)
