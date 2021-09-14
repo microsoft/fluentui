66b00f15d3c665e1548039fb1e152303d2ca8b6b
Peter Dräxler <peter@draxler.ml>
2021-09-14 14:33:21 +0200

    add comments to babel plugin

--- .storybook/babel.plugin.js
+++ .storybook/babel.plugin.js
@@ -1,8 +1,14 @@
+// This Babel plugin adds context.parameters.fullSource property to Storybook stories,
+// which contains source of of the file where story is present.
+// It’s utilized by Export to CodeSandbox.
+
+// @ts-ignore
 module.exports = function (babel) {
   const { types: t } = babel;
   return {
     name: 'literal-replacer',
     visitor: {
+      // @ts-ignore
       MemberExpression(path) {
         if (path.node.property.name === 'parameters' && path.parentPath.isAssignmentExpression()) {
           const storyName = path.node.object.name;
