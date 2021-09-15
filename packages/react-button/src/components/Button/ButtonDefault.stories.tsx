bec2094727bc8f5dc2a217280b80ebaba7d5fbd6
Peter Dräxler <peter@draxler.ml>
2021-09-15 19:48:24 +0200

    remove relative import

--- packages/react-button/src/components/Button/ButtonDefault.stories.tsx
+++ packages/react-button/src/components/Button/ButtonDefault.stories.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
-import { Button } from '@fluentui/react-button';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
-import { ButtonProps } from './Button.types';
+import { Button, ButtonProps } from '@fluentui/react-button';
 
 export const Default = (props: ButtonProps) => (
   <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
