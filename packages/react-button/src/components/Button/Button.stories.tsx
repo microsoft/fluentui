ddc0d31b18a82814fc8629d26a962eedf6e90002
Peter Dräxler <peter@draxler.ml>
2021-09-16 12:26:30 +0200

    remove unnecessary ts-ignores

--- packages/react-button/src/components/Button/Button.stories.tsx
+++ packages/react-button/src/components/Button/Button.stories.tsx
@@ -6,36 +6,14 @@ import { Meta } from '@storybook/react';
 import descriptionMd from './ButtonDescription.md';
 import bestPracticesMd from './ButtonBestPractices.md';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Emphasis } from './ButtonEmphasis.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Default } from './ButtonDefault.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Disabled } from './ButtonDisabled.stories';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { WithIcon } from './ButtonWithIcon.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Circular } from './ButtonCircular.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Size } from './ButtonSize.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { Block } from './ButtonBlock.stories';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - add link to this comment please  (see https://github.com/microsoft/fluentui/pull/18695)
 export { WithLongText } from './ButtonWithLongText.stories';
 
 export default {
