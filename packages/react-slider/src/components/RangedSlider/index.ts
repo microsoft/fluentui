18f71fa727221e0d1cdf559a49f0c90175762db7
Caleb Zearing <czearing@outlook.com>
2021-09-16 17:27:01 -0700

    Updating index.ts

--- packages/react-slider/src/components/RangedSlider/index.ts
+++ packages/react-slider/src/components/RangedSlider/index.ts
@@ -2,5 +2,5 @@ export * from './RangedSlider';
 export * from './RangedSlider.types';
 export * from './renderRangedSlider';
 export * from './useRangedSlider';
-// export * from './useRangedSliderState';
+export * from './useRangedSliderState';
 export * from './useRangedSliderStyles';
