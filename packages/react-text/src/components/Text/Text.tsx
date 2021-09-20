a608d3171e06206924220375ad59846f9a43667d
André Dias <andredias@microsoft.com>
2021-09-20 13:57:43 +0200

    Simplify API

--- packages/react-text/src/components/Text/Text.tsx
+++ packages/react-text/src/components/Text/Text.tsx
@@ -7,7 +7,7 @@ import type { TextProps } from './Text.types';
 /**
  * Typography and styling abstraction component used to ensure consistency of text.
  */
-export const Text = React.forwardRef<
+export const Text: React.FunctionComponent<TextProps> = React.forwardRef<
   HTMLSpanElement | HTMLParagraphElement | HTMLHeadingElement | HTMLPreElement,
   TextProps
 >((props, ref) => {
