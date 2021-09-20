484a74a8c8cdee3186678d714481f876228fb2ae
Lingfan Gao <lingfangao@hotmail.com>
2021-09-20 12:25:53 +0200

    clean up beachball config

---
+++ scripts/beachball/getScopes.ts
@@ -0,0 +1,55 @@
+import { AllPackageInfo, getAllPackageInfo } from '../monorepo/index';
+
+// Northstar is never published with beachbal
+const ignoreNorthstarScope = '!packages/fluentui/*';
+
+/**
+ * Reads package info from the monorepo and generates the scopes for beachball bump and release
+ * @returns Appropriate scoped packages for beachball
+ */
+export function getScopes() {
+  const allPackageInfo = getAllPackageInfo();
+
+  if (process.env.RELEASE_VNEXT) {
+    return [...getVNextPackagePaths(allPackageInfo), ...getSharedPackagePaths(allPackageInfo)];
+  }
+
+  const ignoreVNextScope = getVNextPackagePaths(allPackageInfo).map(path => `!${path}`);
+  return [ignoreNorthstarScope, ignoreVNextScope];
+}
+
+function getVNextPackagePaths(allPackageInfo: AllPackageInfo) {
+  return Object.values(allPackageInfo)
+    .map(packageInfo => {
+      if (packageInfo.packageJson.version.startsWith('9.')) {
+        return packageInfo.packagePath;
+      }
+
+      // These packages depend on converged packages, but are private
+      // Can be included in the publish scope so that dependencies are bumped correctly.
+      const privateNonConverged = ['perf-test', 'vr-tests'];
+
+      if (privateNonConverged.includes(packageInfo.packageJson.name)) {
+        return packageInfo.packagePath;
+      }
+
+      return false;
+    })
+    .filter(Boolean) as string[];
+}
+
+function getSharedPackagePaths(allPackageInfo: AllPackageInfo) {
+  return Object.values(allPackageInfo)
+    .map(packageInfo => {
+      // These packages depend on converged packages, but are private
+      // Can be included in the publish scope so that dependencies are bumped correctly.
+      const privateNonConverged = ['perf-test', 'vr-tests'];
+
+      if (privateNonConverged.includes(packageInfo.packageJson.name)) {
+        return packageInfo.packagePath;
+      }
+
+      return false;
+    })
+    .filter(Boolean) as string[];
+}
--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -1,32 +1,7 @@
 import { BeachballConfig } from 'beachball';
-import { getAllPackageInfo } from '../monorepo/index';
 import { renderHeader, renderEntry } from './customRenderers';
-
-const allPackageInfo = getAllPackageInfo();
-const fluentConvergedPackagePaths = Object.values(allPackageInfo)
-  .map(packageInfo => {
-    if (packageInfo.packageJson.version.startsWith('9.')) {
-      return packageInfo.packagePath;
-    }
-
-    // These packages depend on converged packages, but are private
-    // Can be included in the publish scope so that dependencies are bumped correctly.
-    const privateNonConverged = ['perf-test', 'vr-tests'];
-
-    if (privateNonConverged.includes(packageInfo.packageJson.name)) {
-      return packageInfo.packagePath;
-    }
-
-    return false;
-  })
-  .filter(Boolean) as string[];
-
-const ignoreFluentConvergedScope = fluentConvergedPackagePaths.map(path => `!${path}`);
-// Northstar is never published with beachball
-const ignoreNorthstarScope = '!packages/fluentui/*';
-
-// Default scope used to publish Fluent UI v8 and non-v9 packages
-const defaultScope = [ignoreNorthstarScope, ...ignoreFluentConvergedScope];
+import { getScopes } from './getScopes';
+import { getVNextChangelogGroups } from './getVNextChangelogGroups';
 
 export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
@@ -34,18 +9,12 @@ export const config: BeachballConfig = {
   generateChangelog: true,
   // https://github.com/microsoft/beachball/pull/599
   // @ts-ignore
-  scope: process.env.RELEASE_VNEXT ? fluentConvergedPackagePaths : defaultScope,
+  scope: getScopes(),
   changelog: {
     customRenderers: {
       renderHeader,
       renderEntry,
     },
-    groups: [
-      {
-        masterPackageName: '@fluentui/react-components',
-        changelogPath: 'packages/react-components',
-        include: fluentConvergedPackagePaths,
-      },
-    ],
+    groups: [getVNextChangelogGroups()],
   },
 };
