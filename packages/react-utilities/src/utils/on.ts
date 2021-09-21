95faf99b338160b25270a772faf94a6216c5eb17
Caleb Zearing <czearing@outlook.com>
2021-09-21 12:56:48 -0700

    PR feedback

--- packages/react-utilities/src/utils/on.ts
+++ packages/react-utilities/src/utils/on.ts
@@ -1,9 +1,17 @@
+/**
+ * Adds and removes a given event listener on an element.
+ *
+ * @param element the element to listen to.
+ * @param eventName the event to watch.
+ * @param callback the callback to call when the eventName is triggered.
+ * @param useCapture whether the events should be dispatched to the callback first before other EventTargets.
+ */
 export const on = <T>(
   element: Element | Window | Document,
   eventName: string,
   callback: (ev: T) => void,
-  options?: boolean,
+  useCapture?: boolean,
 ) => {
-  element.addEventListener(eventName, (callback as unknown) as (ev: Event) => void, options);
-  return () => element.removeEventListener(eventName, (callback as unknown) as (ev: Event) => void, options);
+  element.addEventListener(eventName, (callback as unknown) as (ev: Event) => void, useCapture);
+  return () => element.removeEventListener(eventName, (callback as unknown) as (ev: Event) => void, useCapture);
 };
