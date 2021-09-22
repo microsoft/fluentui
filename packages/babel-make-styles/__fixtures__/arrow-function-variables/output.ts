6a6787570751a47832ff54c4277436390b61b002
Elizabeth Craig <elcraig@microsoft.com>
2021-09-21 21:34:23 -0700

    add arrow-function-variables test

---
+++ packages/babel-make-styles/__fixtures__/arrow-function-variables/output.ts
@@ -0,0 +1,13 @@
+import { __styles } from '@fluentui/react-make-styles';
+const colors = {
+  black: '#000',
+};
+// TODO: update result once test passes
+export const useStyles = __styles(
+  {
+    root: {},
+  },
+  {
+    d: [],
+  },
+);
