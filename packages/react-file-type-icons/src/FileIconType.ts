06679a61ee0cc3fcaa46ceb2efc95c06d6d0031a
Chris H <60440259+hoorncj@users.noreply.github.com>
2021-09-23 17:14:15 -0700

    Update FileIconType.ts

--- packages/react-file-type-icons/src/FileIconType.ts
+++ packages/react-file-type-icons/src/FileIconType.ts
@@ -18,8 +18,7 @@ export enum FileIconType {
   documentsFolder = 10,
   picturesFolder = 11,
   linkedFolder = 12,
-  list = 13,
-  form = 14
+  list = 13
 }
 
 export type FileIconTypeInput = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
