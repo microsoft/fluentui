23b55f03920cbc35d38e6b3cca96d2321d69b934
ling1726 <lingfan.gao@microsoft.com>
2021-09-24 14:27:56 +0200

    fix(Popup): Should not dismiss when an iframe content is focused inside (#19955)
    * fix(Popup): Should not dismiss when an iframe content is focused inside
    
    * Add test
    
    * Add changelog entry
    
    * only check inside content
    
    * improve test flakiness

--- packages/fluentui/react-northstar/src/components/Popup/Popup.tsx
+++ packages/fluentui/react-northstar/src/components/Popup/Popup.tsx
@@ -184,6 +184,12 @@ export const Popup: React.FC<PopupProps> &
   const rightClickReferenceObject = React.useRef<PopperJs.VirtualElement | null>();
 
   useOnIFrameFocus(open, context.target, (e: Event) => {
+    const iframeInsidePopup = elementContains(popupContentRef.current, e.target as HTMLElement);
+
+    if (iframeInsidePopup) {
+      return;
+    }
+
     setOpen(__ => {
       _.invoke(props, 'onOpenChange', e, { ...props, ...{ open: false } });
       return false;
