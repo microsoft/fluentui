80a0b6c1617fc2e3eb4b6fb3ecc2ce3849f8cd63
Peter Dräxler <peter@draxler.ml>
2021-09-24 16:19:13 +0200

    Button.stories.tsx - revert disscet, update

--- packages/react-button/src/components/Button/ButtonDefault.stories.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Button, ButtonProps } from '../../Button'; // codesandbox-dependency: @fluentui/react-button ^9.0.0-alpha
-
-export const Default = (props: ButtonProps) => (
-  <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-    <Button {...props}>Button</Button>
-  </div>
-);
