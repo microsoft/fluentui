439617dad0e72e26a03af0c4ef9d0c0225fe8935
Peter Dräxler <peter@draxler.ml>
2021-09-24 16:09:47 +0200

    fix types for babel plugin

--- .storybook/babel.plugin.js
+++ .storybook/babel.plugin.js
@@ -1,16 +1,23 @@
-// This Babel plugin adds context.parameters.fullSource property to Storybook stories,
-// which contains source of of the file where story is present.
-// It’s utilized by Export to CodeSandbox.
-
-// @ts-ignore
+/**
+ * This Babel plugin adds `context.parameters.fullSource` property to Storybook stories,
+ * which contains source of of the file where story is present.
+ * It’s utilized by Export to CodeSandbox.
+ * @param {import('@babel/core')} babel
+ * @returns {import('@babel/core').PluginObj}
+ */
 module.exports = function (babel) {
   const { types: t } = babel;
+
   return {
     name: 'literal-replacer',
     visitor: {
-      // @ts-ignore
       MemberExpression(path) {
-        if (path.node.property.name === 'parameters' && path.parentPath.isAssignmentExpression()) {
+        if (
+          t.isIdentifier(path.node.property) &&
+          t.isIdentifier(path.node.object) &&
+          path.node.property.name === 'parameters' &&
+          path.parentPath.isAssignmentExpression()
+        ) {
           const storyName = path.node.object.name;
           const expression = t.expressionStatement(
             t.assignmentExpression(
@@ -22,7 +29,6 @@ module.exports = function (babel) {
               t.identifier('__STORY__'),
             ),
           );
-          // @ts-ignore
           const expressionStatement = path.findParent(p => p.isExpressionStatement());
           expressionStatement.insertAfter(expression);
           path.stop();
