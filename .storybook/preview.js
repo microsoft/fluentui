a1e04027d482c72487bbed813a1c74dc08f7fd37
Peter Dräxler <peter@draxler.ml>
2021-09-24 16:05:12 +0200

    update addon to 0.4.0

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { withFluentProvider, withStrictMode } from '@fluentui/react-storybook';
 import 'cypress-storybook/react';
+import dedent from 'dedent';
 
 /** @type {NonNullable<import('@storybook/react').Story['decorators']>} */
 export const decorators = [withFluentProvider, withStrictMode];
@@ -14,5 +15,25 @@ export const parameters = {
     source: {
       excludeDecorators: true,
     },
+  },
+  exportToCodeSandbox: {
+    requiredDependencies: {
+      'react-dom': 'latest', // for React
+      'react-scripts': 'latest', // necessary when using typescript in CodeSandbox
+      '@fluentui/react-components': '^9.0.0-alpha', // necessary for FluentProvider
+    },
+    indexTsx: dedent`
+          import * as ReactDOM from 'react-dom';
+          import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+          import { STORY_NAME as Example } from './example';
+          //
+          // You can edit this example in "example.tsx".
+          //
+          ReactDOM.render(
+              <FluentProvider theme={webLightTheme}>
+                  <Example />
+              </FluentProvider>,
+              document.getElementById('root'),
+          );`,
   },
 };
