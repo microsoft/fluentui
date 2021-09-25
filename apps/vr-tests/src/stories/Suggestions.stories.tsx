c68c3b807eee6b14c6f31d2521977aa0323a98c4
Caleb Zearing <czearing@outlook.com>
2021-09-25 16:40:22 -0700

    Removing a sporadic test

--- apps/vr-tests/src/stories/Suggestions.stories.tsx
+++ apps/vr-tests/src/stories/Suggestions.stories.tsx
@@ -84,7 +84,6 @@ export class SimpleSuggestionsExample extends React.Component<{}, { Provinces: P
   public render(): JSX.Element {
     return (
       <div
-        className="rootElement"
         style={{
           height: '80vh',
           position: 'relative',
@@ -129,21 +128,6 @@ export class SimpleSuggestionsExample extends React.Component<{}, { Provinces: P
 
 storiesOf('(Dev-Only) Suggestions', module)
   .addDecorator(FabricDecorator)
-  .addDecorator(story => (
-    <Screener
-      steps={new Screener.Steps()
-        .snapshot('default', { cropTo: '.rootElement' })
-        .hover('#province-fake-long-province')
-        .snapshot('Hovering over a wide suggestion element', { cropTo: '.rootElement' })
-        .hover('#sug-0+.ms-Suggestions-closeButton')
-        .snapshot('Hovering over the X button on a wide suggestion element', {
-          cropTo: '.rootElement',
-        })
-        .end()}
-    >
-      {story()}
-    </Screener>
-  ))
   .addStory('Test of closeButton with overflowing wide custom element', () => (
     <SimpleSuggestionsExample />
   ));
