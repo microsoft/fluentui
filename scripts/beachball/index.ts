8b950ae9a07e1e53445b9f0b8ab582723e0ddc23
Elizabeth Craig <elcraig@microsoft.com>
2021-09-28 14:40:19 -0700

    Update beachball to enabled grouped change files (#20000)

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -7,6 +7,7 @@ export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
   tag: 'latest',
   generateChangelog: true,
+  groupChanges: true,
   scope: getScopes(),
   changelog: {
     customRenderers: {
