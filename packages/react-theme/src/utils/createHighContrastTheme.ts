ede1575b3a5c8f893124af9415c53968564fb923
Miroslav Stastny <mistastn@microsoft.com>
2021-09-29 09:41:46 +0200

    feat(react-theme): Use direct values in alias tokens (#19660)
    * feat(react-theme): Use direct values in alias tokens
    
    * Change files
    
    * fix snapshot in UT

--- packages/react-theme/src/utils/createHighContrastTheme.ts
+++ packages/react-theme/src/utils/createHighContrastTheme.ts
@@ -1,18 +1,20 @@
 import { createShadowLevelTokens } from './shadows';
-import { generateSharedColorTokens, neutralColorTokens } from '../alias/highContrast';
+import { generateSharedColorTokens, generateNeutralColorTokens } from '../alias/highContrast';
 import { createGlobalTheme } from '../global/utils';
+import { sharedColors as globalSharedColors } from '../global/colors';
 import type { BrandVariants, Theme } from '../types';
 
 export const createHighContrastTheme: (brand: BrandVariants) => Theme = brand => {
-  const global = createGlobalTheme(brand);
+  const global = createGlobalTheme(brand, globalSharedColors);
+  const neutral = generateNeutralColorTokens(global);
   return {
     global,
     alias: {
       color: {
-        ...generateSharedColorTokens(global.palette),
-        neutral: neutralColorTokens,
+        ...generateSharedColorTokens(globalSharedColors, global.color),
+        neutral,
       } as Theme['alias']['color'],
-      shadow: createShadowLevelTokens(neutralColorTokens.neutralShadowAmbient, neutralColorTokens.neutralShadowKey),
+      shadow: createShadowLevelTokens(neutral.neutralShadowAmbient, neutral.neutralShadowKey),
     },
   };
 };
