d6484a6a23756d0821b04f8d21a30aa3aa734126
Oleksandr Fediashov <olfedias@microsoft.com>
2021-09-30 14:01:47 +0200

    fix a test

--- packages/react-theme/src/utils/themeToCSSVariables.test.ts
+++ packages/react-theme/src/utils/themeToCSSVariables.test.ts
@@ -4,9 +4,9 @@ import { themeToCSSVariables } from './themeToCSSVariables';
 describe('themeToCSSVariables', () => {
   it('returns a flat object with CSS variables', () => {
     expect(themeToCSSVariables(teamsLightTheme)).toMatchObject({
-      '--colorBlueForeground1': '#006cbf',
-      '--colorBlueForeground2': '#004377',
-      '--colorBlueForeground3': '#0078d4',
+      '--colorPaletteBlueForeground1': '#006cbf',
+      '--colorPaletteBlueForeground2': '#004377',
+      '--colorPaletteBlueForeground3': '#0078d4',
     });
   });
 });
