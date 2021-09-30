9710458fc7a5987b114c07c0d4af44a75ebc2d39
Miroslav Stastny <miroslav.stastny@microsoft.com>
2021-09-30 11:50:39 +0200

    fix codemod build

--- packages/codemods/src/codeMods/mods/v9ThemeFlattening/v9ThemeFlattening.mod.ts
+++ packages/codemods/src/codeMods/mods/v9ThemeFlattening/v9ThemeFlattening.mod.ts
@@ -1,6 +1,6 @@
 import { SourceFile, ts } from 'ts-morph';
 
-import { CodeMod, ModError, NoOp } from '../../types';
+import { CodeMod, ModError, ModResult, NoOp } from '../../types';
 import { Err, Ok, Result } from '../../../helpers/result';
 
 function capitalizeToken(value: string): string {
@@ -102,7 +102,7 @@ function renameToken(token: string): string {
 }
 
 const v9ThemeFlattening: CodeMod = {
-  run: (file: SourceFile): Result<string, NoOp | ModError> => {
+  run: (file: SourceFile): Result<ModResult, NoOp | ModError> => {
     try {
       file.transform(traversal => {
         const node = traversal.visitChildren();
@@ -128,15 +128,14 @@ const v9ThemeFlattening: CodeMod = {
         return node;
       });
 
-      return Ok('Successfully renamed the given props.');
+      return Ok({ logs: ['Successfully renamed theme tokens.'] });
     } catch (e) {
-      console.log(e);
-      return Err<string, ModError>({ error: e });
+      return Err<ModResult, ModError>({ error: e });
     }
   },
   version: '100000',
   name: 'v9ThemeFlattening',
-  enabled: false, // No longer needed; remains for demo purposes
+  enabled: true,
 };
 
 export default v9ThemeFlattening;
--- packages/codemods/src/codeMods/tests/mock/v9/mMakeStylesDeep.ts
+++ packages/codemods/src/codeMods/tests/mock/v9/mMakeStylesDeep.ts
@@ -2,26 +2,37 @@ import { makeStyles } from '@fluentui/react-make-styles';
 
 export const useStylesA = makeStyles({
   neutral: theme => ({
+    // @ts-expect-error
     border: `5px solid ${theme.alias.color.neutral.neutralStroke1}`,
+    // @ts-expect-error
     color: theme.alias.color.neutral.neutralForeground1,
   }),
   colors: theme => ({
+    // @ts-expect-error
     border: `5px solid ${theme.alias.color.blue.border2}`,
+    // @ts-expect-error
     color: theme.alias.color.marigold.foreground2,
   }),
   shadow: theme => ({
+    // @ts-expect-error
     boxShadow: theme.alias.shadow.shadow8,
   }),
   border: theme => ({
+    // @ts-expect-error
     borderRadius: theme.global.borderRadius.circular,
   }),
   stroke: theme => ({
+    // @ts-expect-error
     borderBottomWidth: theme.global.strokeWidth.thin,
   }),
   type: theme => ({
+    // @ts-expect-error
     fontFamily: theme.global.type.fontFamilies.base,
+    // @ts-expect-error
     fontSize: theme.global.type.fontSizes.base[300],
+    // @ts-expect-error
     lineHeight: theme.global.type.lineHeights.base[300],
+    // @ts-expect-error
     fontWeight: theme.global.type.fontWeights.regular,
   }),
 });
