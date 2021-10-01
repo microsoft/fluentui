9dbc739fb0354804a10306d002aed751eaa77ad1
ling1726 <lingfan.gao@microsoft.com>
2021-10-01 11:30:44 +0200

    feat(Menu): Update styles from design spec (#19990)
    * feat(Menu): Update styles from design spec
    
    Recent design spec changes incorporated into current menu
    
    * Change files
    
    * add divider styles
    
    * update border radius
    
    * update md
    
    * remove inset box shadow
    
    * update snapshot
    
    * use foreground 2
    
    * revert background
    
    * revert background
    
    * revert background
    
    * revert background
    
    * don't use theme
    
    * remove disabled background
    
    * fix story

--- packages/react-menu/src/components/MenuList/MenuList.tsx
+++ packages/react-menu/src/components/MenuList/MenuList.tsx
@@ -3,6 +3,7 @@ import { useMenuList } from './useMenuList';
 import { renderMenuList } from './renderMenuList';
 import { useMenuListContextValues } from './useMenuListContextValues';
 import type { MenuListProps } from './MenuList.types';
+import { useMenuListStyles } from './useMenuListStyles';
 
 /**
  * Define a styled MenuList, using the `useMenuList` hook.
@@ -10,6 +11,7 @@ import type { MenuListProps } from './MenuList.types';
 export const MenuList = React.forwardRef<HTMLDivElement, MenuListProps>((props, ref) => {
   const state = useMenuList(props, ref);
   const contextValues = useMenuListContextValues(state);
+  useMenuListStyles(state);
 
   return renderMenuList(state, contextValues);
 });
