9b1ea8c87aca59298974a262c949ee1c5f83cbf3
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2021-10-02 02:06:48 +0200

    react-avatar: Fix stories `getFilenameFromUrl` implementation (#20063)
    * Fix getFilenameFromUrl implementation
    
    * Change files

--- packages/react-avatar/src/Avatar.stories.tsx
+++ packages/react-avatar/src/Avatar.stories.tsx
@@ -300,7 +300,7 @@ export const AvatarPlayground = () => {
       'image',
       [{ src: nameAndImage.image }, nextNameAndImage, prevNameAndImage],
       true,
-      getFilenameFromUrl as () => string,
+      getFilenameFromUrl,
     ),
     useValueSelector('color', useValueSelectorState([...examples.color, ...examples.namedColors])),
     useValueSelector('active', useValueSelectorState(['active', 'inactive'] as const)),
@@ -364,7 +364,9 @@ const AvatarExampleList: React.FC<
 
 const badgeToString = (badge: typeof examples.badge[number] | undefined): string =>
   typeof badge === 'object' ? `{ status: '${badge.status}', outOfOffice: ${badge.outOfOffice} }` : `${badge}`;
-const getFilenameFromUrl = (url: string) => url.substring(url.lastIndexOf('/') + 1);
+
+const getFilenameFromUrl = (image: AvatarProps['image']) =>
+  image?.src?.substring(image?.src.lastIndexOf('/') + 1) ?? '';
 
 type ValueSelectorState<T> = [/*value:*/ T, /*next:*/ () => void, /*prev:*/ () => void];
 
