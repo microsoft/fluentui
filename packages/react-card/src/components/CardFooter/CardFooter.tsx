c11f5b4e89ca7cca1062f31a4094998d72d07a7f
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2021-10-04 22:10:33 +0200

    react-card: Adjust signature to use ForwardRefComponent (#20090)
    * Adds ForwardRefComponent to react-card components declaration
    
    * Change files

--- packages/react-card/src/components/CardFooter/CardFooter.tsx
+++ packages/react-card/src/components/CardFooter/CardFooter.tsx
@@ -3,11 +3,12 @@ import { useCardFooter } from './useCardFooter';
 import { renderCardFooter } from './renderCardFooter';
 import { useCardFooterStyles } from './useCardFooterStyles';
 import type { CardFooterProps } from './CardFooter.types';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * Component to render Button actions in a Card component.
  */
-export const CardFooter = React.forwardRef<HTMLElement, CardFooterProps>((props, ref) => {
+export const CardFooter: ForwardRefComponent<CardFooterProps> = React.forwardRef((props, ref) => {
   const state = useCardFooter(props, ref);
 
   useCardFooterStyles(state);
--- packages/react-card/src/components/CardHeader/CardHeader.tsx
+++ packages/react-card/src/components/CardHeader/CardHeader.tsx
@@ -3,11 +3,12 @@ import { useCardHeader } from './useCardHeader';
 import { renderCardHeader } from './renderCardHeader';
 import { useCardHeaderStyles } from './useCardHeaderStyles';
 import type { CardHeaderProps } from './CardHeader.types';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * Component to render an image, text and an action in a Card component.
  */
-export const CardHeader = React.forwardRef<HTMLElement, CardHeaderProps>((props, ref) => {
+export const CardHeader: ForwardRefComponent<CardHeaderProps> = React.forwardRef((props, ref) => {
   const state = useCardHeader(props, ref);
 
   useCardHeaderStyles(state);
