7d145c8a69f2d28e3c88e7c0af46ea7ce8eaa862
Elizabeth Craig <elcraig@microsoft.com>
2021-10-04 01:54:41 -0700

    Add ForwardRefComponent helper type (#19923)
    * Add ForwardRefComponent helper type
    
    * Change files
    
    * update hack event name

--- packages/react-link/src/components/Link/Link.tsx
+++ packages/react-link/src/components/Link/Link.tsx
@@ -3,16 +3,18 @@ import { useLink } from './useLink';
 import { useLinkStyles } from './useLinkStyles';
 import { renderLink } from './renderLink';
 import type { LinkProps } from './Link.types';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * A Link is a reference to data that a user can follow by clicking or tapping it.
  */
-export const Link = React.forwardRef<HTMLAnchorElement | HTMLButtonElement, LinkProps>((props, ref) => {
+export const Link: ForwardRefComponent<LinkProps> = React.forwardRef((props, ref) => {
   const state = useLink(props, ref);
 
   useLinkStyles(state);
 
   return renderLink(state);
-});
+  // Work around some small mismatches in inferred types which don't matter in practice
+}) as ForwardRefComponent<LinkProps>;
 
 Link.displayName = 'Link';
--- packages/react-text/src/components/wrapper.tsx
+++ packages/react-text/src/components/wrapper.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { mergeClasses } from '@fluentui/react-make-styles';
 import { renderText, useText, useTextStyles } from '../Text';
 import type { TextProps } from '../Text';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 export type TextWrapperProps = Omit<TextProps, 'font' | 'size'>;
 
@@ -10,7 +11,7 @@ export function createWrapper(options: {
   useStyles: () => Record<'root', string>;
 }): React.FunctionComponent<TextWrapperProps> {
   const { useStyles, displayName } = options;
-  const Wrapper = React.forwardRef<HTMLElement, TextWrapperProps>((props, ref) => {
+  const Wrapper: ForwardRefComponent<TextWrapperProps> = React.forwardRef((props, ref) => {
     const styles = useStyles();
     const state = useText(props as TextProps, ref);
 
