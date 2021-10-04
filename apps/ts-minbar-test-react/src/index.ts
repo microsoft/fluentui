e84335f938ed40532a3df1c6e5888ddbf149e6bc
Tristan Watanabe <tristan.watanabe@gmail.com>
2021-10-04 14:47:04 -0700

    updates

---
+++ apps/ts-minbar-test-react/src/index.ts
@@ -0,0 +1,16 @@
+import { typings } from './typings';
+
+async function performTest() {
+  try {
+    await typings();
+  } catch (e) {
+    console.log(e);
+
+    console.log('');
+    console.log('@fluentui/ts-minbar-test-react: The test suite failed.');
+
+    process.exit(1);
+  }
+}
+
+performTest();
