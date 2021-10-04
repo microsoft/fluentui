23cef964767ec372eddf7e783c6e77a788ff0339
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2021-10-04 13:54:29 +0200

    react-menu: Adjust signature to use ForwardRefComponent (#20087)
    * Adds ForwardRefComponent to react-menu components declaration
    
    * Change files

--- packages/react-menu/src/components/MenuDivider/MenuDivider.tsx
+++ packages/react-menu/src/components/MenuDivider/MenuDivider.tsx
@@ -3,11 +3,12 @@ import { useMenuDivider } from './useMenuDivider';
 import { useMenuDividerStyles } from './useMenuDividerStyles';
 import { renderMenuDivider } from './renderMenuDivider';
 import type { MenuDividerProps } from './MenuDivider.types';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * Define a styled MenuDivider, using the `useMenuDivider` hook.
  */
-export const MenuDivider = React.forwardRef<HTMLDivElement, MenuDividerProps>((props, ref) => {
+export const MenuDivider: ForwardRefComponent<MenuDividerProps> = React.forwardRef((props, ref) => {
   const state = useMenuDivider(props, ref);
   useMenuDividerStyles(state);
 
--- packages/react-menu/src/components/MenuItemCheckbox/MenuItemCheckbox.tsx
+++ packages/react-menu/src/components/MenuItemCheckbox/MenuItemCheckbox.tsx
@@ -3,11 +3,12 @@ import { useMenuItemCheckbox } from './useMenuItemCheckbox';
 import { renderMenuItemCheckbox } from './renderMenuItemCheckbox';
 import { useMenuItemCheckBoxStyles } from './useMenuItemCheckboxStyles';
 import type { MenuItemCheckboxProps } from './MenuItemCheckbox.types';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * Define a styled MenuItemCheckbox, using the `useMenuItemCheckbox` hook.
  */
-export const MenuItemCheckbox = React.forwardRef<HTMLElement, MenuItemCheckboxProps>((props, ref) => {
+export const MenuItemCheckbox: ForwardRefComponent<MenuItemCheckboxProps> = React.forwardRef((props, ref) => {
   const state = useMenuItemCheckbox(props, ref);
   useMenuItemCheckBoxStyles(state);
 
