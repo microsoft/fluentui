82af47717f0a1490704984e80ff3448d5ebdea08
tringakrasniqi <tkrasniqi@microsoft.com>
2021-10-04 17:09:36 +0200

    Added description and removed Spec file

--- packages/react-radio/src/components/Radio/Radio.tsx
+++ packages/react-radio/src/components/Radio/Radio.tsx
@@ -5,7 +5,7 @@ import { renderRadio } from './renderRadio';
 import { useRadioStyles } from './useRadioStyles';
 
 /**
- * Radio component enables select a single option from two or more choices.
+ * A Radio component presents a set of options where only one option can be selected.
  */
 export const Radio = React.forwardRef<HTMLElement, RadioProps>((props, ref) => {
   const state = useRadio(props, ref);
