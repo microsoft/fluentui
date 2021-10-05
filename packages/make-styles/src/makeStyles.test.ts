4544fe9836da1bf114152ae169c0eab99e72f55f
Oleksandr Fediashov <olfedias@microsoft.com>
2021-10-05 11:50:46 +0200

    fix(makeStyles): fix insertion of keyframes (#20095)
    * fix insertion of keyframes
    
    * Change files
    
    * fix snapshot
    
    * fix more UTs
    
    * add an example with multiple animations
    
    * fix multiple animations

--- packages/make-styles/src/makeStyles.test.ts
+++ packages/make-styles/src/makeStyles.test.ts
@@ -129,6 +129,34 @@ describe('makeStyles', () => {
           transform: rotate(-360deg);
         }
       }
+      @keyframes f1q8eu9e {
+        from {
+          -webkit-transform: rotate(0deg);
+          -moz-transform: rotate(0deg);
+          -ms-transform: rotate(0deg);
+          transform: rotate(0deg);
+        }
+        to {
+          -webkit-transform: rotate(360deg);
+          -moz-transform: rotate(360deg);
+          -ms-transform: rotate(360deg);
+          transform: rotate(360deg);
+        }
+      }
+      @keyframes f55c0se {
+        from {
+          -webkit-transform: rotate(0deg);
+          -moz-transform: rotate(0deg);
+          -ms-transform: rotate(0deg);
+          transform: rotate(0deg);
+        }
+        to {
+          -webkit-transform: rotate(-360deg);
+          -moz-transform: rotate(-360deg);
+          -ms-transform: rotate(-360deg);
+          transform: rotate(-360deg);
+        }
+      }
       .f1g6ul6r {
         -webkit-animation-name: f1q8eu9e;
         animation-name: f1q8eu9e;
