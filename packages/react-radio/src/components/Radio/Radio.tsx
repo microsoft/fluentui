8f6d0f50d50a4934d27fffbe8b604f24c4efd220
tringakrasniqi <tkrasniqi@microsoft.com>
2021-10-05 15:17:17 +0200

    Added the ForwardRefComponent

--- packages/react-radio/src/components/Radio/Radio.tsx
+++ packages/react-radio/src/components/Radio/Radio.tsx
@@ -3,11 +3,12 @@ import { useRadio } from './useRadio';
 import { RadioProps } from './Radio.types';
 import { renderRadio } from './renderRadio';
 import { useRadioStyles } from './useRadioStyles';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * A Radio component presents a set of options where only one option can be selected.
  */
-export const Radio = React.forwardRef<HTMLElement, RadioProps>((props, ref) => {
+export const Radio: ForwardRefComponent<RadioProps> = React.forwardRef((props, ref) => {
   const state = useRadio(props, ref);
 
   useRadioStyles(state);
