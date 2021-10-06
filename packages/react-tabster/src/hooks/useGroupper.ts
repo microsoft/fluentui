a51e802580c2faa97408f2979ed6fcb256fd2fdc
André Dias <andredias@microsoft.com>
2021-10-06 20:07:19 +0200

    Add useGroupper

---
+++ packages/react-tabster/src/hooks/useGroupper.ts
@@ -0,0 +1,43 @@
+import { Types, getGroupper } from 'tabster';
+import { useTabsterAttributes } from './useTabsterAttributes';
+import { useTabster } from './useTabster';
+
+export enum GroupperTabbability {
+  /**
+   * Tab will cycle into and out of the groupper content.
+   */
+  Unlimited = Types.GroupperTabbabilities.Unlimited,
+  /**
+   * Tab will cycle out of the container, but not into it.
+   */
+  Limited = Types.GroupperTabbabilities.Limited,
+  /**
+   * Tab only cycles the inner elements.
+   */
+  LimitedTrapFocus = Types.GroupperTabbabilities.LimitedTrapFocus,
+}
+
+export interface UseGroupperOptions {
+  /**
+   * Type of TAB key interaction.
+   */
+  tabbability?: GroupperTabbability;
+}
+
+/**
+ * A hook that returns the necessary tabster attributes to support groupping.
+ * @param options - Options to configure keyboard navigation
+ */
+export const useGroupper = (options?: UseGroupperOptions) => {
+  const tabster = useTabster();
+
+  if (tabster) {
+    getGroupper(tabster);
+  }
+
+  return useTabsterAttributes({
+    groupper: {
+      tabbability: options?.tabbability as Types.GroupperTabbability,
+    },
+  });
+};
