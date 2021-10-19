4ed662ed307a980de955494c5f61f6fbde468f06
GeoffCoxMSFT <gcox@microsoft.com>
2021-10-19 14:37:19 -0700

    Removed duplicate 'Image' from story names

--- packages/react-image/src/stories/Image.stories.tsx
+++ packages/react-image/src/stories/Image.stories.tsx
@@ -4,12 +4,12 @@ import descriptionMd from './ImageDescription.md';
 import bestPracticesMd from './ImageBestPractices.md';
 
 export { Default } from './ImageDefault.stories';
-export { ImageShape } from './ImageShape.stories';
-export { ImageBorderVariations } from './ImageBorderVariations.stories';
-export { ImageFallback } from './ImageFallback.stories';
-export { ImageBlock } from './ImageBlock.stories';
-export { ImageLayoutFit } from './ImageLayoutFit.stories';
-export { ImageShadow } from './ImageShadow.stories';
+export { Shape } from './ImageShape.stories';
+export { Bordered } from './ImageBordered.stories';
+export { Fallback } from './ImageFallback.stories';
+export { Block } from './ImageBlock.stories';
+export { Fit } from './ImageFit.stories';
+export { Shadow } from './ImageShadow.stories';
 
 export default {
   title: 'Components/Image',
