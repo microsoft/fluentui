020bd80321424cb6be445f8f6ef21b5800641a1e
Lingfan Gao <lingfangao@hotmail.com>
2021-10-19 20:29:37 +0200

    fix(react-components): Disable storybook canvas and default to docs page

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -23,4 +23,29 @@ const options = {
 export const decorators = [...rootPreview.decorators];
 
 /** @type {typeof rootPreview.parameters} */
-export const parameters = { ...rootPreview.parameters, options };
+export const parameters = { ...rootPreview.parameters, previewTabs: { canvas: { hidden: true } }, options };
+
+// Hacky way of clicking on Docs button on first load of page.
+// https://github.com/storybookjs/storybook/issues/13128
+function clickDocsButtonOnFirstLoad() {
+  window.removeEventListener('load', clickDocsButtonOnFirstLoad);
+
+  try {
+    const docsButtonSelector = window.parent.document.evaluate(
+      "//button[contains(., 'Docs')]",
+      window.parent.document,
+      null,
+      XPathResult.ANY_TYPE,
+      null,
+    );
+
+    const button = docsButtonSelector.iterateNext();
+    if (button) {
+      button.click();
+    }
+  } catch (error) {
+    // Do nothing if it wasn't able to click on Docs button.
+  }
+}
+
+window.addEventListener('load', clickDocsButtonOnFirstLoad);
