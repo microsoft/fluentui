e71393ebda85c52094c46d42d93af1e642f21293
Geoff Cox (Microsoft) <gcox@microsoft.com>
2021-10-20 13:08:32 -0700

    Removed duplicate 'Image' from story names (#20268)
    * Removed duplicate 'Image' from story names
    
    * Change files

--- packages/react-image/src/stories/ImageBlock.stories.tsx
+++ packages/react-image/src/stories/ImageBlock.stories.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Image } from '../index';
 
-export const ImageBlock = () => (
+export const Block = () => (
   <>
     <Image block src="https://via.placeholder.com/900x50" />
     <Image block src="https://via.placeholder.com/100x100" />
   </>
 );
-ImageBlock.parameters = {
+Block.parameters = {
   docs: {
     description: {
       story: 'An Image can be maximized in order to fill its parent container.',
--- packages/react-image/src/stories/ImageShadow.stories.tsx
+++ packages/react-image/src/stories/ImageShadow.stories.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Image } from '../index';
 
-export const ImageShadow = () => <Image shadow src="https://via.placeholder.com/300x300" />;
-ImageShadow.parameters = {
+export const Shadow = () => <Image shadow src="https://via.placeholder.com/300x300" />;
+Shadow.parameters = {
   docs: {
     description: {
       story: 'The shadow prop will apply box shadow styling to the image.',
