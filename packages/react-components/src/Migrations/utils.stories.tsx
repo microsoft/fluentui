4afccab7ca057afe035c40e16021feb5e37144f0
Lingfan Gao <lingfangao@hotmail.com>
2021-10-25 15:23:01 +0200

    remove old

--- packages/react-components/src/Migrations/utils.stories.tsx
+++ packages/react-components/src/Migrations/utils.stories.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { DocsContainer, DocsContainerProps, Source } from '@storybook/addon-docs';
 import { makeStyles } from '@fluentui/react-make-styles';
-import {
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuPopover,
-  MenuTrigger,
-  MenuButton,
-  FluentProvider,
-  webLightTheme,
-} from '../index';
 
 const useCodeComparisonStyles = makeStyles({
   root: {
@@ -66,56 +55,5 @@ export const CodeExample = (props: { title?: string; children: React.ReactElemen
       <h3>{title ?? codeLanguages[language]}</h3>
       <Source language={language} code={code} />
     </div>
-  );
-};
-
-interface VersionEntry {
-  version: string;
-  commit: string;
-}
-
-export const VersionPicker = () => {
-  const [versions, setVersions] = React.useState<VersionEntry[]>([]);
-
-  React.useEffect(() => {
-    fetch('https://fluentstorybookversion.azurewebsites.net/api/GetVersions')
-      .then(res => res.json())
-      .then(json => {
-        setVersions(json);
-      });
-  }, []);
-
-  const navigateHandler = (commit: string) => () => {
-    const href = `https://${commit}--6002298f95a00c00213f4d55.chromatic.com`;
-    window.parent.location.href = href;
-  };
-
-  return (
-    <FluentProvider theme={webLightTheme} style={{ position: 'sticky', top: 0 }}>
-      <Menu>
-        <MenuTrigger>
-          <MenuButton>Select version</MenuButton>
-        </MenuTrigger>
-        <MenuPopover>
-          <MenuList>
-            {versions.map(entry => (
-              <MenuItem onClick={navigateHandler(entry.commit)} key={entry.version}>
-                {entry.version}
-              </MenuItem>
-            ))}
-          </MenuList>
-        </MenuPopover>
-      </Menu>
-    </FluentProvider>
-  );
-};
-
-export const FluentDocsContainer = ({ children, context }: DocsContainerProps & { children: React.ReactNode }) => {
-  console.log(process.env.STORYBOOK_THEME);
-  return (
-    <>
-      <VersionPicker />
-      <DocsContainer context={context}>{children}</DocsContainer>
-    </>
   );
 };
