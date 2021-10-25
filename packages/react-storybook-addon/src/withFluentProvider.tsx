f26fb4c7be1a41f1832b82a25b448adc3b054977
Lingfan Gao <lingfangao@hotmail.com>
2021-10-25 15:34:07 +0200

    update deps

--- packages/react-storybook-addon/src/withFluentProvider.tsx
+++ packages/react-storybook-addon/src/withFluentProvider.tsx
@@ -5,6 +5,17 @@ import { themes, defaultTheme, FluentProvider } from './theme';
 import { THEME_ID } from './constants';
 import { Globals, StoryContext } from './hooks';
 
+import { makeStyles } from '@fluentui/react-make-styles';
+
+const useStyles = makeStyles({
+  root: theme => ({
+    padding: '10px',
+    fontFamily: theme.fontFamilyBase,
+    color: theme.colorNeutralForeground1,
+    background: theme.colorNeutralBackground1,
+  }),
+});
+
 const getActiveFluentTheme = (globals: Globals) => {
   const selectedThemeId = globals[THEME_ID];
   const { theme } = themes.find(value => value.id === selectedThemeId) ?? defaultTheme;
@@ -14,6 +25,15 @@ const getActiveFluentTheme = (globals: Globals) => {
 
 export const withFluentProvider = (StoryFn: StoryFunction<React.ReactElement>, context: StoryContext) => {
   const { theme } = getActiveFluentTheme(context.globals);
+  return (
+    <FluentProvider theme={theme}>
+      <FluentExampleContainer>{StoryFn()}</FluentExampleContainer>
+    </FluentProvider>
+  );
+};
 
-  return <FluentProvider theme={theme}>{StoryFn()}</FluentProvider>;
+const FluentExampleContainer: React.FC = props => {
+  const styles = useStyles();
+
+  return <div className={styles.root}>{props.children}</div>;
 };
