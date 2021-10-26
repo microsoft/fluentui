e79117f6c75938b22697bbd54a6a8d952bc63359
Lingfan Gao <lingfangao@hotmail.com>
2021-10-26 13:19:11 +0200

    remove unnecessary changes

--- packages/react-storybook-addon/src/constants.ts
+++ packages/react-storybook-addon/src/constants.ts
@@ -1,4 +1,2 @@
 export const ADDON_ID = 'storybook/fluentui-react-addon';
 export const THEME_ID = `${ADDON_ID}/theme` as const;
-export const PARAM_KEY = `myAddonParameter`;
-export const EVENTS = {} as const;
