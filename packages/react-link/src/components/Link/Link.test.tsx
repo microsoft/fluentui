63992af9d07ce018247983c2e1030e545238d531
KHMakoto <Humberto.Morimoto@microsoft.com>
2021-10-25 18:10:32 -0700

    Updating Link tests.

--- packages/react-link/src/components/Link/Link.test.tsx
+++ packages/react-link/src/components/Link/Link.test.tsx
@@ -6,12 +6,14 @@ import { Link } from './Link';
 import { LinkProps } from './Link.types';
 
 describe('Link', () => {
+  const testId = 'test-anchor';
+
   isConformant<LinkProps>({
     Component: Link,
     displayName: 'Link',
   });
 
-  describe('AccessibilityLinkBehavior', () => {
+  describe('meets accessibility requirements', () => {
     const testFacade = new ComponentTestFacade(Link, {});
     const errors = validateBehavior(linkBehaviorDefinition, testFacade);
     expect(errors).toEqual([]);
@@ -23,21 +25,21 @@ describe('Link', () => {
   });
 
   it('renders as a button if no href is provided', () => {
-    const result = render(<Link>This is a link</Link>);
-    const button = result.queryAllByRole('button');
-    const anchor = result.queryAllByRole('link');
-    expect(button.length).toBe(1);
-    expect(anchor.length).toBe(0);
+    const result = render(<Link data-testid={testId}>This is a link</Link>);
+    const button = result.getByTestId(testId);
+    expect(button.tagName).toBe('BUTTON');
 
     expect(result.container).toMatchSnapshot();
   });
 
   it('renders as an anchor when href is provided', () => {
-    const result = render(<Link href="https://www.bing.com">This is a link</Link>);
-    const button = result.queryAllByRole('button');
-    const anchor = result.queryAllByRole('link');
-    expect(button.length).toBe(0);
-    expect(anchor.length).toBe(1);
+    const result = render(
+      <Link data-testid={testId} href="https://www.bing.com">
+        This is a link
+      </Link>,
+    );
+    const anchor = result.getByTestId(testId);
+    expect(anchor.tagName).toBe('A');
 
     expect(result.container).toMatchSnapshot();
   });
