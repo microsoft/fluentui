089d5e34da1b3fa515177c2d57c15f2436c62155
ling1726 <lingfangao@hotmail.com>
2021-10-27 09:02:36 +0200

    Update packages/react-label/src/stories/LabelStrong.stories.tsx
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-label/src/stories/LabelStrong.stories.tsx
+++ packages/react-label/src/stories/LabelStrong.stories.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
-import { Label } from '../index'; // codesandbox-dependency: @fluentui/react-button ^9.0.0-beta
+import { Label } from '../index'; // codesandbox-dependency: @fluentui/react-label ^9.0.0-beta
 
 export const Strong = () => <Label strong>Strong label</Label>;
