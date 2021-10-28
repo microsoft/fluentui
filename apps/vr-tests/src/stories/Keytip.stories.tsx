ffdc5ad44e339c4cf0dbf1fccdd5473b6a15f063
REDMOND\jspurlin <jspurlin@microsoft.com>
2021-10-28 13:43:53 -0700

    actually I just noticed there was already a ktp target span, undoing my story update, adding some text to the keytipElement, and making the size of the area larger

--- apps/vr-tests/src/stories/Keytip.stories.tsx
+++ apps/vr-tests/src/stories/Keytip.stories.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import Screener from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
 import { FabricDecorator } from '../utilities/index';
-import { Keytip, DefaultButton } from '@fluentui/react';
+import { Keytip } from '@fluentui/react';
 
 storiesOf('Keytip', module)
   .addDecorator(story => (
-    <div style={{ width: '50px', height: '50px' }}>
-      <span data-ktp-target={'ktp-a'} />
+    <div style={{ width: '150px', height: '150px' }}>
+      <span data-ktp-target={'ktp-a'}>text</span>
       {story()}
     </div>
   ))
@@ -22,21 +22,10 @@ storiesOf('Keytip', module)
       {story()}
     </Screener>,
   )
-  .addStory('Root', () => (
-    <>
-      <DefaultButton data-ktp-target="ktp-a" data-ktp-execute-target="ktp-a" label="button" />
-      <Keytip content={'A'} keySequences={['a']} visible={true} />
-    </>
-  ))
+  .addStory('Root', () => <Keytip content={'A'} keySequences={['a']} visible={true} />)
   .addStory('Disabled', () => (
-    <>
-      <DefaultButton data-ktp-target="ktp-a" data-ktp-execute-target="ktp-a" label="button" />
-      <Keytip content={'A'} keySequences={['a']} visible={true} disabled={true} />
-    </>
+    <Keytip content={'A'} keySequences={['a']} visible={true} disabled={true} />
   ))
   .addStory('Offset', () => (
-    <>
-      <DefaultButton data-ktp-target="ktp-a" data-ktp-execute-target="ktp-a" label="button" />
-      <Keytip content={'A'} keySequences={['a']} visible={true} offset={{ x: 15, y: 15 }} />
-    </>
+    <Keytip content={'A'} keySequences={['a']} visible={true} offset={{ x: 15, y: 15 }} />
   ));
