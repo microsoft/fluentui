ab6fd655d1c5c801a47776d36fdae77e207830ed
Elizabeth Craig <elcraig@microsoft.com>
2021-10-27 20:57:01 -0700

    Stop publishing website packages in v7 (#20392)

--- scripts/lint-files/index.js
+++ scripts/lint-files/index.js
@@ -1,9 +1,10 @@
 // @ts-check
 const { lintFiles: lintTslintFiles } = require('./no-tslint');
 const { lintFiles: lintExampleFiles } = require('./no-old-example-paths');
+const { lintFiles: lintChangeFiles } = require('./no-example-change-files');
 
 if (require.main === module) {
-  if (lintTslintFiles() || lintExampleFiles()) {
+  if (lintTslintFiles() || lintExampleFiles() || lintChangeFiles()) {
     process.exit(1);
   }
 }
