e9e7f438f1530d5629108a27dfd1d81d7c107293
Chris Holt <chhol@microsoft.com>
2021-11-01 21:49:49 -0700

    (web components) fix cursor issue text field (#20445)
    * change input state styles to fix height and prevent z-index from blocking cursor
    
    * Change files
    
    * oopsie copy paste

--- packages/web-components/src/styles/patterns/input.styles.ts
+++ packages/web-components/src/styles/patterns/input.styles.ts
@@ -187,7 +187,7 @@ export const inputStateStyles: (
   :host(:not([disabled]):focus-within:not(:active))::after {
     content: '';
     position: absolute;
-    height: 100%;
+    height: calc(${focusStrokeWidth} * 1px);
     bottom: 0;
     border-bottom: calc(${focusStrokeWidth} * 1px) solid ${accentFillRest};
     border-bottom-left-radius: calc(${controlCornerRadius} * 1px);
