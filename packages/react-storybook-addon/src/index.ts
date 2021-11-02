c4bc7618cfc3290580eaa72e8a2aeb43a85eebd4
Lingfan Gao <lingfangao@hotmail.com>
2021-11-02 15:29:40 +0100

    export

--- packages/react-storybook-addon/src/index.ts
+++ packages/react-storybook-addon/src/index.ts
@@ -1,3 +1,4 @@
-export type { FluentGlobals, FluentStoryContext, setGlobalTheme } from './hooks';
+export type { FluentGlobals, FluentStoryContext } from './hooks';
+export { setGlobalTheme } from './hooks';
 export { themes } from './theme';
 export { THEME_ID } from './constants';
