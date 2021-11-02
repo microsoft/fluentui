14071aa6c609ea52025b97c3a2c73f45543c92ff
Levi Thomason <me@levithomason.com>
2021-11-01 17:18:05 -0700

    split more examples, add more stories

--- packages/react-avatar/src/AvatarColorBrand.stories.tsx
+++ packages/react-avatar/src/AvatarColorBrand.stories.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { Avatar, AvatarProps } from './index';
 
-export const ColorBrand = (props: Partial<AvatarProps>) => (
-  <>
-    <Avatar color="brand" name="Lydia Bauer" badge={{ status: 'doNotDisturb' }} {...props} />
-    <Avatar color="brand" badge={{ status: 'available' }} {...props} />
-  </>
-);
+export const ColorBrand = (props: Partial<AvatarProps>) => <Avatar {...props} color="brand" />;
 
-ColorBrand.storyName = 'Color: neutral, brand';
+ColorBrand.storyName = 'Color: brand';
