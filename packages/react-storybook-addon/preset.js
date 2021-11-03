b25633b39926c0f297f97c146d830dc923eb5fe5
Lingfan Gao <lingfangao@hotmail.com>
2021-11-03 13:04:49 +0100

    remove babel register

--- packages/react-storybook-addon/preset.js
+++ packages/react-storybook-addon/preset.js
@@ -1,10 +1,14 @@
-require('@babel/register');
-
 function config(entry = []) {
+  // This entrypoint is deliberately set to ts for monorepo DX
+  // However this is bad for publishing
+  // https://github.com/microsoft/fluentui/issues/20474
   return [...entry, require.resolve('./src/preset/preview.ts')];
 }
 
 function managerEntries(entry = []) {
+  // This entrypoint is deliberately set to ts for monorepo DX
+  // However this is bad for publishing
+  // https://github.com/microsoft/fluentui/issues/20474
   return [...entry, require.resolve('./src/preset/manager.ts')];
 }
 
