c5b65ba66866cab9fcfdac145a368c250c39d2f5
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-03 12:20:17 +0100

    use star export

--- packages/react-tooltip/src/components/Tooltip/index.ts
+++ packages/react-tooltip/src/components/Tooltip/index.ts
@@ -2,4 +2,4 @@ export * from './Tooltip';
 export * from './Tooltip.types';
 export * from './renderTooltip';
 export * from './useTooltip';
-export { tooltipClassName, useTooltipStyles } from './useTooltipStyles';
+export * from './useTooltipStyles';
