1d6c3d004654729d1a4d24bad198c45eab4b8b58
Lingfan Gao <lingfangao@hotmail.com>
2021-11-03 11:18:20 +0100

    chore(release-vnext): Postpublish step to tag prereleases
    Following #20352, all v9 packages will be released under tag `latest`,
    adds a postpublish hook that will tag each of the released packages with
    the appropriate prerelease version

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -2,6 +2,7 @@ import { BeachballConfig } from 'beachball';
 import { renderHeader, renderEntry } from './customRenderers';
 import { getScopes } from './getScopes';
 import { getVNextChangelogGroups } from './getVNextChangelogGroups';
+import { execSync } from 'child_process';
 
 export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
@@ -14,5 +15,24 @@ export const config: BeachballConfig = {
       renderEntry,
     },
     groups: [getVNextChangelogGroups()],
+  },
+  hooks: {
+    postpublish: (packagePath, name, version) => {
+      // Following https://github.com/microsoft/fluentui/pull/20352 all prerelease versions are released with `latest`
+      // Adds a post publish hook to continue to tag prerelease versions with appropriate tags
+      if (version.indexOf('beta') !== -1 && process.env.RELEASE_VNEXT) {
+        const tag = 'beta';
+        try {
+          console.log(`tagging ${name}@${version} with ${tag}`);
+          const res = execSync(`npm dist-tag add ${name}@${version} ${tag}`);
+          console.log(res.toString());
+        } catch (err) {
+          console.error(`failed to tag ${name} with ${tag}`);
+          if (err.stderr) {
+            console.error(err.stderr);
+          }
+        }
+      }
+    },
   },
 };
