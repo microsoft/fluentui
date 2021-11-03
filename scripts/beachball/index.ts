65b4a7a837d14c257d6ba10be130dadffd512ecf
Lingfan Gao <lingfangao@hotmail.com>
2021-11-03 15:42:49 +0100

    use async, use npm token auth

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -2,7 +2,12 @@ import { BeachballConfig } from 'beachball';
 import { renderHeader, renderEntry } from './customRenderers';
 import { getScopes } from './getScopes';
 import { getVNextChangelogGroups } from './getVNextChangelogGroups';
-import { execSync } from 'child_process';
+import { writeFile } from 'fs-extra';
+import { exec as execCb } from 'child_process';
+import { promisify } from 'util';
+import * as path from 'path';
+
+const exec = promisify(execCb);
 
 export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
@@ -17,21 +22,21 @@ export const config: BeachballConfig = {
     groups: [getVNextChangelogGroups()],
   },
   hooks: {
-    postpublish: (packagePath, name, version) => {
+    postpublish: async (packagePath, name, version) => {
+      await writeFile(path.resolve(packagePath, '.npmrc'), '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
+
       // Following https://github.com/microsoft/fluentui/pull/20352 all prerelease versions are released with `latest`
       // Adds a post publish hook to continue to tag prerelease versions with appropriate tags
       // can be removed once v9 is fully released
       const tag = 'beta';
       if (version.includes(tag) && process.env.RELEASE_VNEXT) {
-        try {
-          console.log(`tagging ${name}@${version} with ${tag}`);
-          const res = execSync(`npm dist-tag add ${name}@${version} ${tag}`);
-          console.log(res.toString());
-        } catch (err) {
+        console.log(`tagging ${name}@${version} with ${tag}`);
+        const res = await exec(`npm dist-tag add ${name}@${version} ${tag}`);
+        console.log(res.stdout);
+
+        if (res.stderr) {
           console.error(`failed to tag ${name} with ${tag}`);
-          if (err.stderr) {
-            console.error(err.stderr);
-          }
+          console.error(res.stderr);
         }
       }
     },
