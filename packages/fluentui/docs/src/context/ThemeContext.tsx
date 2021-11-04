86d6649544c09e4db47086782e7c3ae2fd507e75
David Zukowski <david@zuko.me>
2021-11-04 11:57:58 -0500

    make teamsV2Theme the default docs theme (#20442)
    * make teamsV2Theme the default docs theme
    * use teamsTheme (v1) for screener tests

--- packages/fluentui/docs/src/context/ThemeContext.tsx
+++ packages/fluentui/docs/src/context/ThemeContext.tsx
@@ -15,7 +15,7 @@ export type ThemeContextData = {
 };
 
 export const themeContextDefaults: ThemeContextData = {
-  themeName: 'teamsTheme',
+  themeName: 'teamsV2Theme',
   themeOptions: [
     { text: 'Teams', value: 'teamsTheme' },
     { text: 'Teams Dark', value: 'teamsDarkTheme' },
@@ -25,5 +25,12 @@ export const themeContextDefaults: ThemeContextData = {
   ],
   changeTheme: () => {},
 };
+
+// Existing screener tests assume that they're running with the v1 theme, so
+// set that as the default theme when inside screener. This check is safe
+// because this context is only used in the documentation website.
+if (location.pathname.includes('/react-northstar-screener')) {
+  themeContextDefaults.themeName = 'teamsTheme';
+}
 
 export const ThemeContext = React.createContext<ThemeContextData>(themeContextDefaults);
