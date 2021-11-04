d58cedfb2a9ea9495526fb89034bcf60809ff7fe
Milan Turon <milanturon@centrum.cz>
2021-11-04 13:29:02 +0100

    fix typings and let container build

--- packages/fluentui/react-northstar-prototypes/src/prototypes/fztesting/example.tsx
+++ packages/fluentui/react-northstar-prototypes/src/prototypes/fztesting/example.tsx
@@ -1,126 +1,15 @@
 // import React from 'react';
 import * as React from 'react';
-import { Avatar, Chat, Divider, Provider, teamsTheme } from '@fluentui/react-northstar';
+import {
+  Avatar,
+  Chat,
+  Divider,
+  Provider,
+  teamsTheme,
+  ShorthandCollection,
+  ChatItemProps,
+} from '@fluentui/react-northstar';
 // import { AcceptIcon } from '@fluentui/react-icons-northstar';
-
-const robinAvatar = {
-  image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobinCounts.jpg',
-  status: {
-    color: 'green',
-    // icon: <AcceptIcon />,
-  },
-};
-const items = oneMoreMessage => [
-  {
-    message: <Chat.Message content="Hello" author="Cecil Folk" timestamp="Yesterday, 10:15 PM" mine />,
-    contentPosition: 'end',
-    attached: 'top',
-    key: 'message-id-1',
-  },
-  {
-    message: <Chat.Message content="I'm back!" author="Cecil Folk" timestamp="Yesterday, 10:15 PM" mine />,
-    contentPosition: 'end',
-    attached: true,
-    key: 'message-id-2',
-  },
-  {
-    message: (
-      <Chat.Message
-        content={{
-          content: (
-            <div>
-              What do you think about{' '}
-              <a href="#" tabIndex={0}>
-                www.tabindexFood.com
-              </a>
-              ?
-            </div>
-          ),
-        }}
-        author="Cecil Folk"
-        timestamp="Yesterday, 10:15 PM"
-        mine
-      />
-    ),
-    contentPosition: 'end',
-    attached: 'bottom',
-    key: 'message-id-3',
-  },
-  {
-    gutter: <Avatar {...robinAvatar} />,
-    message: <Chat.Message content="Hi" author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
-    attached: 'top',
-    key: 'message-id-4',
-  },
-  {
-    gutter: <Avatar {...robinAvatar} />,
-    message: <Chat.Message content="Looks good!" author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
-    attached: true,
-    key: 'message-id-5',
-  },
-  oneMoreMessage && {
-    gutter: <Avatar {...robinAvatar} />,
-    message: (
-      <Chat.Message
-        content={
-          <div>
-            I also like{' '}
-            <a href="#" tabIndex={0}>
-              www.laterTabindex.com
-            </a>
-            .
-          </div>
-        }
-        author="Robin Counts"
-        timestamp="Yesterday, 10:15 PM"
-      />
-    ),
-    attached: 'bottom',
-    key: 'message-id-6',
-  },
-  {
-    gutter: <Avatar {...robinAvatar} />,
-    message: (
-      <Chat.Message
-        content={
-          <div>
-            I also like <a href="#">www.goodFood2.com</a>.
-          </div>
-        }
-        author="Robin Counts"
-        timestamp="Yesterday, 10:15 PM"
-      />
-    ),
-    attached: 'bottom',
-    key: 'message-id-6',
-  },
-  {
-    message: (
-      <Chat.Message
-        content="Would you like to grab lunch there?"
-        author="Cecil Folk"
-        timestamp="Yesterday, 10:16 PM"
-        mine
-      />
-    ),
-    contentPosition: 'end',
-    key: 'message-id-7',
-  },
-  {
-    gutter: <Avatar {...robinAvatar} />,
-    message: <Chat.Message content="Sure! Let's try it." author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
-    key: 'message-id-8',
-  },
-  {
-    children: <Divider content="Today" color="brand" important />,
-    key: 'message-id-9',
-  },
-  {
-    message: <Chat.Message content="Ok, let's go." author="Cecil Folk" timestamp="Today, 11:15 PM" mine />,
-    contentPosition: 'end',
-    key: 'message-id-10',
-  },
-];
 
 const ChatExample = () => {
   let [oneMoreMessage, setOneMoreMessage] = React.useState(false);
@@ -128,9 +17,131 @@ const ChatExample = () => {
     setTimeout(() => {
       setOneMoreMessage(true);
     }, 3000);
+
+  const robinAvatar = {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobinCounts.jpg',
+    status: {
+      color: 'green',
+      // icon: <AcceptIcon />,
+    },
+  };
+
+  const items: ShorthandCollection<ChatItemProps> = [
+    {
+      message: <Chat.Message content="Hello" author="Cecil Folk" timestamp="Yesterday, 10:15 PM" mine />,
+      contentPosition: 'end',
+      attached: 'top',
+      key: 'message-id-1',
+    },
+    {
+      message: <Chat.Message content="I'm back!" author="Cecil Folk" timestamp="Yesterday, 10:15 PM" mine />,
+      contentPosition: 'end',
+      attached: true,
+      key: 'message-id-2',
+    },
+    {
+      message: (
+        <Chat.Message
+          content={{
+            content: (
+              <div>
+                <button>how are</button>
+                What do you think about{' '}
+                <a href="#" tabIndex={0}>
+                  www.tabindexFood.com
+                </a>
+                ?
+              </div>
+            ),
+          }}
+          author="Cecil Folk"
+          timestamp="Yesterday, 10:15 PM"
+          mine
+        />
+      ),
+      contentPosition: 'end',
+      attached: 'bottom',
+      key: 'message-id-3',
+    },
+    {
+      gutter: <Avatar {...robinAvatar} />,
+      message: <Chat.Message content="Hi" author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
+      attached: 'top',
+      key: 'message-id-4',
+    },
+    {
+      gutter: <Avatar {...robinAvatar} />,
+      message: <Chat.Message content="Looks good!" author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
+      attached: true,
+      key: 'message-id-5',
+    },
+    oneMoreMessage && {
+      gutter: <Avatar {...robinAvatar} />,
+      message: (
+        <Chat.Message
+          content={
+            <div>
+              I also like{' '}
+              <a href="#" tabIndex={0}>
+                www.laterTabindex.com
+              </a>
+              .
+            </div>
+          }
+          author="Robin Counts"
+          timestamp="Yesterday, 10:15 PM"
+        />
+      ),
+      attached: 'bottom',
+      key: 'message-id-6',
+    },
+    {
+      gutter: <Avatar {...robinAvatar} />,
+      message: (
+        <Chat.Message
+          content={
+            <div>
+              I also like <a href="#">www.goodFood2.com</a>.
+            </div>
+          }
+          author="Robin Counts"
+          timestamp="Yesterday, 10:15 PM"
+        />
+      ),
+      attached: 'bottom',
+      key: 'message-id-6',
+    },
+    {
+      message: (
+        <Chat.Message
+          content="Would you like to grab lunch there?"
+          author="Cecil Folk"
+          timestamp="Yesterday, 10:16 PM"
+          mine
+        />
+      ),
+      contentPosition: 'end',
+      key: 'message-id-7',
+    },
+    {
+      gutter: <Avatar {...robinAvatar} />,
+      message: <Chat.Message content="Sure! Let's try it." author="Robin Counts" timestamp="Yesterday, 10:15 PM" />,
+      key: 'message-id-8',
+    },
+    {
+      children: <Divider content="Today" color="brand" important />,
+      key: 'message-id-9',
+    },
+    {
+      message: <Chat.Message content="Ok, let's go." author="Cecil Folk" timestamp="Today, 11:15 PM" mine />,
+      contentPosition: 'end',
+      key: 'message-id-10',
+    },
+  ];
+
   return (
     <Provider theme={teamsTheme}>
-      <Chat items={items(oneMoreMessage)} />
+      <Chat items={items} />
     </Provider>
   );
 };
