cc239c7200f0861d12a48e4a6ffd5a8e3cc16436
Micah Godbolt <mgodbolt@microsoft.com>
2021-11-05 14:54:16 -0700

    fix missing type

--- packages/react-button/src/components/SplitButton/stories/SplitButtonShape.stories.tsx
+++ packages/react-button/src/components/SplitButton/stories/SplitButtonShape.stories.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { Menu, MenuItem, MenuList, MenuPopover, MenuTrigger } from '@fluentui/react-menu';
-import { SplitButton } from '../../../SplitButton'; // codesandbox-dependency: @fluentui/react-button ^9.0.0-beta
+import { SplitButton, MenuButtonProps } from '../../../index'; // codesandbox-dependency: @fluentui/react-button ^9.0.0-beta
 
 export const Shape = () => (
   <>
