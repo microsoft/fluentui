452cf097c44829ccf7f8a02ff83fb6b14240c732
ling1726 <lingfan.gao@microsoft.com>
2021-11-05 14:34:54 +0100

    chore(react-components): Theme picker for storybook `--docs` mode (#20448)
    * experiment(storybook): Version picker
    
    Chromatic stores published storybooks with commits, we can simply build
    an azure function to keep the version -> commit mapping and integrate a
    picker into the docs page
    
    * Change files
    
    * use fluent menu
    
    * cleanup
    
    * add network fetch
    
    * use addon
    
    * remove old
    
    * update deps
    
    * update stypes
    
    * update md
    
    * remove export
    
    * chore(storybook): Add theme picker to storybook toolbar
    
    Adds the theme picker to the storybook toolbar for internal development.
    The global types are exported since they will be used for the docs page.
    
    * remove unnecessary changes
    
    * update change
    
    * chore(storybook): Add custom DocsContainer to react-components storybook
    
    Initialize a custom `FluentDocsContainer` and  FluentDocsHeader` to the
    react-components storybook for future toolbar items like theme and
    version picker.
    
    This custom header is required since our future plans to build with
    `--docs` will remove the native storybook toolbar
    
    * Change files
    
    * add docstring
    
    * remove font family style
    
    * init theme picker
    
    * revert manager
    
    * revert .storybook
    
    * remove dupe export
    
    * export
    
    * README
    
    * use active theme
    
    * add docstring
    
    * Change files
    
    * fix md
    
    * drop icon
    
    * update md
    
    * set global theme to react-components
    
    * add container to page
    
    * use type
    
    * header styling
    
    * styling updates

--- packages/react-components/src/DocsComponents/FluentDocsHeader.stories.tsx
+++ packages/react-components/src/DocsComponents/FluentDocsHeader.stories.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
+import { FluentGlobals, THEME_ID } from '@fluentui/react-storybook-addon';
 import { makeStyles } from '../index';
+import { ThemePicker } from './ThemePicker.stories';
 
 const useStyles = makeStyles({
-  root: {
-    position: 'sticky',
+  root: theme => ({
+    position: 'fixed',
     top: 0,
     padding: '5px',
-  },
+    width: '100%',
+    background: theme.colorNeutralBackground3,
+    boxShadow: `${theme.shadow8}`,
+    borderBottom: `1px solid ${theme.colorTransparentStroke}`,
+    zIndex: 1000,
+  }),
 });
 
 /**
  * Sticky header over the entire docs page
  */
-export const FluentDocsHeader = () => {
+export const FluentDocsHeader: React.FC<{ storybookGlobals: FluentGlobals }> = ({ storybookGlobals }) => {
   const styles = useStyles();
-  return <div className={styles.root}>{/** TODO add theme+version picker */}</div>;
+  return (
+    <div className={styles.root}>
+      <ThemePicker selectedThemeId={storybookGlobals[THEME_ID]} />
+    </div>
+  );
 };
