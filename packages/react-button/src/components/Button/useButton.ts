5c06cd31cd090a92e340ec9a70434a840492d357
Levi Thomason <me@levithomason.com>
2021-11-08 10:05:51 -0800

    begin button refactor

--- packages/react-button/src/components/Button/useButton.ts
+++ packages/react-button/src/components/Button/useButton.ts
@@ -2,13 +2,17 @@ import * as React from 'react';
 import { useARIAButton } from '@fluentui/react-aria';
 import { getNativeElementProps, resolveShorthand } from '@fluentui/react-utilities';
 import type { ButtonProps, ButtonState } from './Button.types';
+import { useButtonStyles } from './useButtonStyles';
+import { renderButton } from './renderButton';
 
 /**
  * Given user props, defines default props for the Button, calls useButtonState, and returns processed state.
  * @param props - User provided props to the Button component.
  * @param ref - User provided ref to be passed to the Button component.
  */
-export const useButton = (props: ButtonProps, ref: React.Ref<HTMLButtonElement | HTMLAnchorElement>): ButtonState => {
+type RenderButton = (state: ButtonState) => React.ReactElement;
+
+const useButtonState = (props: ButtonProps) => {
   const {
     appearance,
     as,
@@ -22,7 +26,7 @@ export const useButton = (props: ButtonProps, ref: React.Ref<HTMLButtonElement |
   } = props;
   const iconShorthand = resolveShorthand(icon);
 
-  return {
+  const state: ButtonState = {
     // Props passed at the top-level
     appearance,
     block,
@@ -41,17 +45,43 @@ export const useButton = (props: ButtonProps, ref: React.Ref<HTMLButtonElement |
       icon: 'span',
     },
 
-    root: getNativeElementProps(
-      as || 'button',
-      useARIAButton(props, {
-        required: true,
-        defaultProps: {
-          // useARIAButton isn't working with React.Ref<HTMLButtonElement | HTMLAnchorElement>
-          ref: ref as React.Ref<HTMLButtonElement>,
-          type: 'button', // This is added because the default for type is 'submit'
-        },
-      }),
-    ),
+    root: getNativeElementProps(as || 'button', props),
     icon: iconShorthand,
   };
+
+  return state;
+};
+
+const useButtonARIA = (
+  state: ButtonState,
+  props: ButtonProps,
+  ref: React.Ref<HTMLButtonElement | HTMLAnchorElement>,
+) => {
+  const ariaProps = getNativeElementProps(
+    props.as || 'button',
+    useARIAButton(props, {
+      required: true,
+      defaultProps: {
+        // useARIAButton isn't working with React.Ref<HTMLButtonElement | HTMLAnchorElement>
+        ref: ref as React.Ref<HTMLButtonElement>,
+        type: 'button', // This is added because the default for type is 'submit'
+      },
+    }),
+  );
+
+  state.root = {
+    ...state.root,
+    ...ariaProps,
+  };
+};
+
+export const useButton = (
+  props: ButtonProps,
+  ref: React.Ref<HTMLButtonElement | HTMLAnchorElement>,
+): { state: ButtonState; render: RenderButton } => {
+  const state = useButtonState(props);
+  useButtonARIA(state, props, ref);
+  useButtonStyles(state);
+
+  return { state, render: renderButton };
 };
