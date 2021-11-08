5e04b6208619318aa256768635101d5e64b96cc5
ling1726 <lingfan.gao@microsoft.com>
2021-11-08 13:41:24 +0100

    chore(Text): Add docs for each prop and new typography story (#20502)
    * chore(Text): Add docs for each prop and new typography story
    
    * remove mdx
    
    * Change files
    
    * add codesandbox dep for typography
    
    * Update change/@fluentui-react-text-8e9536d4-ec33-44d4-abc3-f2cc51633f53.json
    
    * Update change/@fluentui-react-text-8e9536d4-ec33-44d4-abc3-f2cc51633f53.json

---
+++ packages/react-text/src/stories/TextStrikeThrough.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { Text } from '../index'; // codesandbox-dependency: @fluentui/react-text ^9.0.0-beta
+
+export const StrikeThrough = () => <Text strikethrough>Strikethrough text</Text>;
