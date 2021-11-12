3f362fe801eac1e5f5e35563939059481414c638
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-12 11:09:42 +0100

    fix(react-conformance): update paths generated in defaultErrorMessages (#20580)
    * fix
    
    * fix paths
    
    * Change files
    
    * Update change/@fluentui-react-conformance-90ba2958-fc3c-4b00-9180-740ecb0fb58e.json
    
    Co-authored-by: Elizabeth Craig <elcraig@microsoft.com>
    
    Co-authored-by: Elizabeth Craig <elcraig@microsoft.com>

--- packages/react-conformance/src/defaultTests.tsx
+++ packages/react-conformance/src/defaultTests.tsx
@@ -567,7 +567,7 @@ export const defaultTests: TestObject = {
   },
 };
 
-function getPackagePath(componentPath: string) {
+export function getPackagePath(componentPath: string) {
   // Use lastIndexOf in case anyone has all their repos under a folder called "src" (it happens)
   const srcIndex = componentPath.replace(/\\/g, '/').lastIndexOf('/src/');
   return componentPath.slice(0, srcIndex);
