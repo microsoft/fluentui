cc40532b254089f862921e0473533948b9a37dc4
ling1726 <lingfan.gao@microsoft.com>
2021-11-12 11:00:57 +0100

    chore(release-vnext): Postpublish step to tag prereleases (#20471)
    * chore(react-components): Remove doc related changelog entries
    
    Documentation changes are not code that is shipped with the published
    package, and should not pollute the changelog
    
    * chore(release-vnext): Postpublish step to tag prereleases
    
    Following #20352, all v9 packages will be released under tag `latest`,
    adds a postpublish hook that will tag each of the released packages with
    the appropriate prerelease version
    
    * add docstring
    
    * pr feedback
    
    * use async, use npm token auth
    
    * add codesandbox links
    
    * check status code
    
    * revert avatar changes
    
    * use correct cwd

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -2,6 +2,9 @@ import { BeachballConfig } from 'beachball';
 import { renderHeader, renderEntry } from './customRenderers';
 import { getScopes } from './getScopes';
 import { getVNextChangelogGroups } from './getVNextChangelogGroups';
+import { writeFile } from 'fs-extra';
+import { exec } from 'child_process';
+import * as path from 'path';
 
 export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
@@ -14,5 +17,32 @@ export const config: BeachballConfig = {
       renderEntry,
     },
     groups: [getVNextChangelogGroups()],
+  },
+  hooks: {
+    postpublish: async (packagePath, name, version) => {
+      await writeFile(path.resolve(packagePath, '.npmrc'), '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
+
+      // Following https://github.com/microsoft/fluentui/pull/20352 all prerelease versions are released with `latest`
+      // Adds a post publish hook to continue to tag prerelease versions with appropriate tags
+      // can be removed once v9 is fully released
+      const tag = 'beta';
+      if (version.includes(tag) && process.env.RELEASE_VNEXT) {
+        return new Promise((resolve, reject) => {
+          console.log(`tagging ${name}@${version} with ${tag}`);
+
+          exec(`npm dist-tag add ${name}@${version} ${tag}`, { cwd: packagePath }, (error, stdout, stderr) => {
+            if (error && error.code !== 0) {
+              console.error(`failed to tag ${name} with ${tag}`);
+              console.error(stderr);
+              reject();
+              return;
+            }
+
+            console.log(stdout);
+            resolve();
+          });
+        });
+      }
+    },
   },
 };
