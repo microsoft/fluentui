a4d8d1b9fc94900822b65bb601d66970245c176d
GeoffCoxMSFT <gcox@microsoft.com>
2021-11-11 16:18:04 -0800

    Added size stories

--- packages/react-tabs/src/stories/TabListSize.stories.tsx
+++ packages/react-tabs/src/stories/TabListSize.stories.tsx
@@ -12,80 +12,72 @@ const useStyles = makeStyles({
     padding: '50px 20px',
     rowGap: '20px',
   },
+  sizeExamples: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    rowGap: '20px',
+    padding: '20px 0',
+  },
 });
 
 export const Size = (props: Partial<TabProps>) => {
   const styles = useStyles();
 
+  const renderTabs = () => {
+    return (
+      <>
+        <Tab icon={<CalendarMonth24Regular />} value="tab1">
+          First Tab
+        </Tab>
+        <Tab icon={<CalendarMonth24Regular />} value="tab2">
+          Second Tab
+        </Tab>
+        <Tab icon={<CalendarMonth24Regular />} value="tab3">
+          Third Tab
+        </Tab>
+        <Tab icon={<CalendarMonth24Regular />} value="tab4">
+          Fourth Tab
+        </Tab>
+      </>
+    );
+  };
+
   return (
     <div className={styles.root}>
-      <h3>small</h3>
-      <TabList defaultSelectedKey="tab2" size="small">
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="small" vertical>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="small" verticalTabContent>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="small" vertical verticalTabContent>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-
-      <h3>medium (default)</h3>
-      <TabList defaultSelectedKey="tab2" size="medium">
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="medium" vertical>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="medium" verticalTabContent>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
-      <TabList defaultSelectedKey="tab2" size="medium" vertical verticalTabContent>
-        <Tab value="tab1">First Tab</Tab>
-        <Tab icon={<CalendarMonth24Regular />} value="tab2">
-          Second Tab
-        </Tab>
-        <Tab value="tab3">Third Tab</Tab>
-        <Tab value="tab4">Fourth Tab</Tab>
-      </TabList>
+      <div className={styles.sizeExamples}>
+        <TabList defaultSelectedKey="tab2" size="small">
+          {renderTabs()}
+        </TabList>
+        <TabList defaultSelectedKey="tab2" size="medium">
+          {renderTabs()}
+        </TabList>
+      </div>
+      <div className={styles.sizeExamples}>
+        <TabList defaultSelectedKey="tab2" size="small" vertical>
+          {renderTabs()}
+        </TabList>
+        <TabList defaultSelectedKey="tab2" size="medium" vertical>
+          {renderTabs()}
+        </TabList>
+      </div>
+      <div className={styles.sizeExamples}>
+        <TabList defaultSelectedKey="tab2" size="small" verticalTabContent>
+          {renderTabs()}
+        </TabList>
+        <TabList defaultSelectedKey="tab2" size="medium" verticalTabContent>
+          {renderTabs()}
+        </TabList>
+      </div>
+      <div className={styles.sizeExamples}>
+        <TabList defaultSelectedKey="tab2" size="small" vertical verticalTabContent>
+          {renderTabs()}
+        </TabList>
+        <TabList defaultSelectedKey="tab2" size="medium" vertical verticalTabContent>
+          {renderTabs()}
+        </TabList>
+      </div>
     </div>
   );
 };
