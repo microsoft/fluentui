623b51377429c946a28e755c08a544af9d65547d
Elizabeth Craig <elcraig@microsoft.com>
2021-11-16 13:01:58 -0800

    Delete unused a11y-tests package

--- apps/a11y-tests/jest.config.js
+++
@@ -1,8 +0,0 @@
-let { createConfig } = require('@fluentui/scripts/jest/jest-resources');
-let path = require('path');
-
-const config = createConfig({
-  setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
-});
-
-module.exports = config;
