21ee8527c706ce4fb9c68b192812070c512675c6
Martin Hochel <martinhochel@microsoft.com>
2021-11-16 19:38:02 +0100

    feat(typings): create environment package to provide strict type checking for process.env (#20603)
    * feat(typings): create environment package to provide strict type checking for process.env
    
    * feat(tools): add new 'environment' global type to migration generator
    
    * fixup! feat(typings): create environment package to provide strict type checking for process.env
    
    * fixup! fixup! feat(typings): create environment package to provide strict type checking for process.env
    
    * fixup! feat(tools): add new 'environment' global type to migration generator
    
    * chore(typings): use solution config file to proerly check extensions/declaration without leaking globals
    
    * feat(typings): make environment globals work everywhere with proper DX

--- tools/generators/migrate-converged-pkg/index.ts
+++ tools/generators/migrate-converged-pkg/index.ts
@@ -163,7 +163,7 @@ const templates = {
             lib: ['ES2019'],
             outDir: 'dist',
             declaration: true,
-            types: ['static-assets'],
+            types: ['static-assets', 'environment'],
           } as TsConfig['compilerOptions'],
           exclude: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.test.ts', '**/*.test.tsx'],
           include: ['./src/**/*.ts', './src/**/*.tsx'],
@@ -269,10 +269,7 @@ const templates = {
         allowJs: true,
         checkJs: true,
       },
-      exclude: [
-        /* added programmatically */
-      ],
-      include: ['../src/**/*', '*.js'],
+      include: ['../src/**/*.stories.ts', '../src/**/*.stories.tsx', '*.js'],
     },
   },
   e2e: {
@@ -575,12 +572,6 @@ function setupStorybook(tree: Tree, options: NormalizedSchema) {
 
       json.compilerOptions.types.push(...(libTsConfig.compilerOptions.types || []), 'storybook__addons');
       json.compilerOptions.types = uniqueArray(json.compilerOptions.types);
-
-      json.exclude = json.exclude || [];
-      const transformedExcludePaths = (libTsConfig.exclude || []).map(excludePath =>
-        transformRelativePath(excludePath),
-      );
-      json.exclude.push(...transformedExcludePaths);
 
       return json;
     });
