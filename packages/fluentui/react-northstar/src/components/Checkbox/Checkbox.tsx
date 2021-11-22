57233e300779e9ca7124b939c6a7bd5e9099fe56
Charles <chassunc@microsoft.com>
2021-11-22 08:01:55 -0300

    fix: checkbox component to properly pass down ref

--- packages/fluentui/react-northstar/src/components/Checkbox/Checkbox.tsx
+++ packages/fluentui/react-northstar/src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,5 @@
 import { Accessibility, checkboxBehavior, CheckboxBehaviorProps } from '@fluentui/accessibility';
 import {
-  ComponentWithAs,
   getElementType,
   useUnhandledProps,
   useAccessibility,
@@ -8,6 +7,7 @@ import {
   useFluentContext,
   useStyles,
   useTelemetry,
+  ForwardRefWithAs,
 } from '@fluentui/react-bindings';
 import * as customPropTypes from '@fluentui/react-proptypes';
 import { createCheckboxManager } from '@fluentui/state';
@@ -79,7 +79,7 @@ export const checkboxSlotClassNames: CheckboxSlotClassNames = {
  * @accessibility
  * Implements [ARIA Checkbox](https://www.w3.org/TR/wai-aria-practices-1.1/#checkbox) design pattern.
  */
-export const Checkbox: ComponentWithAs<'div', CheckboxProps> & FluentComponentStaticProps<CheckboxProps> = props => {
+export const Checkbox = (React.forwardRef<HTMLDivElement, CheckboxProps>((props, ref) => {
   const context = useFluentContext();
   const { setStart, setEnd } = useTelemetry(Checkbox.displayName, context.telemetry);
   setStart();
@@ -173,6 +173,7 @@ export const Checkbox: ComponentWithAs<'div', CheckboxProps> & FluentComponentSt
         className: classes.root,
         onClick: handleClick,
         onChange: handleChange,
+        ref,
         ...unhandledProps,
       })}
     >
@@ -190,14 +191,14 @@ export const Checkbox: ComponentWithAs<'div', CheckboxProps> & FluentComponentSt
   setEnd();
 
   return element;
-};
+}) as unknown) as ForwardRefWithAs<'div', HTMLDivElement, CheckboxProps> & FluentComponentStaticProps<CheckboxProps>;
 
 Checkbox.displayName = 'Checkbox';
 
 Checkbox.defaultProps = {
   accessibility: checkboxBehavior,
   indicator: {},
-  labelPosition: 'end',
+  labelPosition: 'end' as const,
 };
 Checkbox.propTypes = {
   ...commonPropTypes.createCommon({
