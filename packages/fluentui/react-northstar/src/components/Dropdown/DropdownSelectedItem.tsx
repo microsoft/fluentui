e25356771ec8c80098a0dbbc586f6e0844e3f583
Charles <chassunc@microsoft.com>
2021-11-22 09:42:33 -0300

    fix: Dropdown components to pass ref

--- packages/fluentui/react-northstar/src/components/Dropdown/DropdownSelectedItem.tsx
+++ packages/fluentui/react-northstar/src/components/Dropdown/DropdownSelectedItem.tsx
@@ -22,13 +22,13 @@ import { createShorthandFactory, commonPropTypes } from '../../utils';
 import { Image, ImageProps } from '../Image/Image';
 import { Box, BoxProps } from '../Box/Box';
 import {
-  ComponentWithAs,
   useUnhandledProps,
   useStyles,
   useFluentContext,
   useTelemetry,
   getElementType,
   useAccessibility,
+  ForwardRefWithAs,
 } from '@fluentui/react-bindings';
 
 export interface DropdownSelectedItemSlotClassNames {
@@ -92,8 +92,7 @@ export type DropdownSelectedItemStylesProps = { hasImage: boolean };
 /**
  * A DropdownSelectedItem represents a selected item of 'multiple-selection' Dropdown.
  */
-export const DropdownSelectedItem: ComponentWithAs<'span', DropdownSelectedItemProps> &
-  FluentComponentStaticProps<DropdownSelectedItemProps> = props => {
+export const DropdownSelectedItem = (React.forwardRef<HTMLSpanElement, DropdownSelectedItemProps>(props => {
   const context = useFluentContext();
   const { setStart, setEnd } = useTelemetry(DropdownSelectedItem.displayName, context.telemetry);
   setStart();
@@ -206,7 +205,8 @@ export const DropdownSelectedItem: ComponentWithAs<'span', DropdownSelectedItemP
 
   setEnd();
   return element;
-};
+}) as unknown) as ForwardRefWithAs<'span', HTMLSpanElement, DropdownSelectedItemProps> &
+  FluentComponentStaticProps<DropdownSelectedItemProps>;
 
 DropdownSelectedItem.displayName = 'DropdownSelectedItem';
 
