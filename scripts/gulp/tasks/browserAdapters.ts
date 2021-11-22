3f20cc983e2a0b40c1df947a75caa69f3874fef5
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-22 10:47:12 +0100

    revert change

--- scripts/gulp/tasks/browserAdapters.ts
+++ scripts/gulp/tasks/browserAdapters.ts
@@ -40,8 +40,6 @@ export async function createChrome(): Promise<Browser> {
     openPage: async url => {
       const page = await browser.newPage();
 
-      page.emulateCPUThrottling();
-
       await page.goto(url);
 
       return {
