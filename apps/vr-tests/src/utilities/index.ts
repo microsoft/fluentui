4b55f424f1d74685e9589218e5b11be722ec5772
Elizabeth Craig <elcraig@microsoft.com>
2021-11-23 13:31:40 -0800

    Improve vr-tests addStory typings (#20684)

--- apps/vr-tests/src/utilities/index.ts
+++ apps/vr-tests/src/utilities/index.ts
@@ -1,18 +1,15 @@
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { initializeFolderCovers } from '@fluentui/react-experiments/lib/FolderCover';
+import { ExtendedStoryApi } from './types';
 
 initializeIcons();
 initializeFolderCovers();
 
-export interface IStoryConfig {
-  rtl?: boolean;
-}
-
-declare module '@storybook/addons/dist/ts3.9/types' {
+declare module '@storybook/addons' {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  interface StoryApi<StoryFnReturnType = unknown> {
+  interface StoryApi {
     /** adds a story, but via VR Tests' addon which auto adds variants like RTL */
-    addStory: this['add'];
+    addStory: ExtendedStoryApi['addStory'];
   }
 }
 
