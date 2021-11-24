ea427b410bb9841b6211ff5ce7369ac446f64524
ling1726 <lingfan.gao@microsoft.com>
2021-11-24 16:19:14 +0100

    Migrate Popover, Accordion, Menu, Portal to solution ts configs (#20692)
    * Migrate Popover, Accordion, Menu, Portal to solution ts configs
    
    * Change files
    
    * fix types
    
    * fix types
    
    * remove isConformant from react-porta
    
    * update imports
    
    * restore storybook layout
    
    * disable isolated modules for e2e tests
    
    * disabled islated modules for menu

--- packages/react-menu/src/stories/MenuControlledRadioItems.stories.tsx
+++ packages/react-menu/src/stories/MenuControlledRadioItems.stories.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { Menu, MenuTrigger, MenuList, MenuItemRadio, MenuPopover, MenuProps } from '@fluentui/react-menu';
 
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { Menu, MenuTrigger, MenuList, MenuItemRadio, MenuPopover, MenuProps } from '../index';
+
 import { Button } from '@fluentui/react-button';
 import {
   Cut20Regular as CutIcon,
@@ -14,20 +9,9 @@ import {
   Edit20Regular as EditIcon,
 } from '@fluentui/react-icons';
 
-// FIXME need to redeclare types since type import is under a @ts-ignore
-type OnCheckedValueChange = (
-  e: React.MouseEvent | React.KeyboardEvent,
-  data: {
-    /** The name of the value */
-    name: string;
-    /** The items for this value that are checked */
-    checkedItems: string[];
-  },
-) => void;
-
 export const ControlledRadioItems = () => {
   const [checkedValues, setCheckedValues] = React.useState<Record<string, string[]>>({ checkbox: ['2'] });
-  const onChange: OnCheckedValueChange = (e: React.SyntheticEvent, { name, checkedItems }) => {
+  const onChange: MenuProps['onCheckedValueChange'] = (e, { name, checkedItems }) => {
     setCheckedValues(s => ({ ...s, [name]: checkedItems }));
   };
 
