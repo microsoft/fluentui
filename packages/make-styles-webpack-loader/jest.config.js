2042bb86936576a478b00e700315af65676d6006
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-24 14:31:08 +0100

    chore: migrate @fluentui/make-styles-webpack-loader

--- packages/make-styles-webpack-loader/jest.config.js
+++ packages/make-styles-webpack-loader/jest.config.js
@@ -8,15 +8,13 @@ module.exports = {
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
+      tsConfig: '<rootDir>/tsconfig.spec.json',
       diagnostics: false,
     },
   },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testEnvironment: 'node',
   coverageDirectory: './coverage',
   setupFilesAfterEnv: ['./config/tests.js'],
 };
