8a141d14f3a14de2e1c4691dad908622f310a9d1
ling1726 <lingfan.gao@microsoft.com>
2021-11-24 16:19:14 +0100

    Migrate Popover, Accordion, Menu, Portal to solution ts configs (#20692)
    * Migrate Popover, Accordion, Menu, Portal to solution ts configs
    
    * Change files
    
    * fix types
    
    * fix types
    
    * remove isConformant from react-porta
    
    * update imports
    
    * restore storybook layout
    
    * disable isolated modules for e2e tests
    
    * disabled islated modules for menu

--- packages/react-menu/src/stories/MenuAnchorToTarget.stories.tsx
+++ packages/react-menu/src/stories/MenuAnchorToTarget.stories.tsx
@@ -1,26 +1,13 @@
 import * as React from 'react';
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { Menu, MenuList, MenuItem, MenuPopover, MenuProps } from '@fluentui/react-menu';
 
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { Menu, MenuList, MenuItem, MenuPopover, MenuProps } from '../index';
+
 import { Button } from '@fluentui/react-button';
-
-// FIXME need to redeclare types since type import is under a @ts-ignore
-type MenuOpenEvents =
-  | MouseEvent
-  | TouchEvent
-  | React.MouseEvent<HTMLElement>
-  | React.KeyboardEvent<HTMLElement>
-  | React.FocusEvent<HTMLElement>;
 
 export const AnchorToCustomTarget = () => {
   const [open, setOpen] = React.useState(false);
   const [target, setTarget] = React.useState<HTMLElement | null>(null);
-  const onOpenChange = (e: MenuOpenEvents, data: { open: boolean }) => {
+  const onOpenChange: MenuProps['onOpenChange'] = (e, data) => {
     setOpen(data.open);
   };
 
--- packages/react-menu/src/stories/MenuListDefault.stories.tsx
+++ packages/react-menu/src/stories/MenuListDefault.stories.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { MenuList, MenuItem } from '@fluentui/react-menu';
-// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { MenuList, MenuItem } from '../index';
+
 import { makeStyles } from '@fluentui/react-make-styles';
 
 export const useMenuListContainerStyles = makeStyles({
