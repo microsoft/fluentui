5acbc8904a509109e5e9f39461dfb87fdf6e906c
David Zearing <dzearing@microsoft.com>
2021-11-23 16:14:52 -0800

    Removing cross-library export *s (#20665)
    * Adding export * rule and applying fixes.
    
    * First pass at linting.
    
    * Moving codemods back.
    
    * Change files
    
    * Updating to node 14, updating createTheme to be exported from the theme package.
    
    * revert upgrade.js phantom change
    
    * dedupe deps, update eslint-plugin-react to match, work around jsx-no-bind changes
    
    * Change files
    
    * fix syncpack
    
    * Adding makestyle as a devdep to codemods.
    
    * Change files
    
    * oops.
    
    * Revert "oops."
    
    This reverts commit 623926a875d55a2a06458bf216284de2c098e0a7.
    
    * Removing rnx-kit/eslint-plugin from the root package.json since it's now in the @fluentui/eslint-plugin dependencies list.
    
    * Fixing dep issue.
    
    * Fixing Icons package to not export a const enum.
    
    * Update change/@fluentui-eslint-plugin-4301dbea-1be5-436e-bf0e-89ed473cd0b7.json
    
    Co-authored-by: Elizabeth Craig <elcraig@microsoft.com>
    Co-authored-by: Elizabeth Craig <ecraig12345@gmail.com>

--- packages/eslint-plugin/src/configs/react-legacy.js
+++ packages/eslint-plugin/src/configs/react-legacy.js
@@ -6,6 +6,7 @@ const configHelpers = require('../utils/configHelpers');
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [path.join(__dirname, 'react')],
+
   rules: {
     ...configHelpers.getNamingConventionRule(true /* prefixWithI */),
     'jsdoc/check-tag-names': 'off',
