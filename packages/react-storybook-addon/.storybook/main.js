b1acb503c1def4a210103ee1214cba033185b935
André Dias (he/him) <andredias@microsoft.com>
2021-11-26 13:26:41 +0100

    Update packages/react-storybook-addon/.storybook/main.js
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

--- packages/react-storybook-addon/.storybook/main.js
+++ packages/react-storybook-addon/.storybook/main.js
@@ -3,7 +3,7 @@ const rootMain = require('../../../.storybook/main');
 module.exports = /** @type {Omit<import('../../../.storybook/main'), 'typescript'|'babel'>} */ ({
   ...rootMain,
   stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-  addons: [...rootMain.addons],
+  addons: ['../preset.js', ...rootMain.addons],
   webpackFinal: (config, options) => {
     const localConfig = { ...rootMain.webpackFinal(config, options) };
 
