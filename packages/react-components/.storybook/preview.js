fb859f240a37dc06953d8d9d28ba8538b60ff831
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-26 18:13:41 +0100

    chore: migrate @fluentui/react-components (#20749)
    * chore: migrate @fluentui/react-components
    
    * Change files
    
    * restore script
    
    * restore changes
    
    * fix config
    
    * update configs to include both JS & TS
    
    * fix typings errors
    
    * fix SB config
    
    * bump pkg version
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hochel <martinhochel@microsoft.com>
    
    * fix fmt
    
    * run generator once more
    
    * apply review suggestions
    
    Co-authored-by: Martin Hochel <martinhochel@microsoft.com>

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -36,7 +36,6 @@ export const parameters = {
     ...rootPreview.parameters.docs,
     container: FluentDocsContainer,
     page: FluentDocsPage,
-    // container: FluentDocsContainer,
   },
   options,
 };
