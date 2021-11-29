d88738b67f924523c73a6174b21f69404394e7de
Oleksandr Fediashov <olfedias@microsoft.com>
2021-11-29 11:56:11 +0100

    chore(perf-test,vr-tests): update styles to not use CSS shorthands (#20821)

--- apps/vr-tests/src/stories/MakeStyles.stories.tsx
+++ apps/vr-tests/src/stories/MakeStyles.stories.tsx
@@ -1,4 +1,4 @@
-import { mergeClasses, makeStyles } from '@fluentui/react-make-styles';
+import { shorthands, mergeClasses, makeStyles } from '@fluentui/react-make-styles';
 import { FluentProvider } from '@fluentui/react-provider';
 import { webLightTheme } from '@fluentui/react-theme';
 import { storiesOf } from '@storybook/react';
@@ -7,23 +7,23 @@ import Screener, { Steps } from 'screener-storybook/src/screener';
 
 const useStyles = makeStyles({
   box: theme => ({
-    border: `5px solid ${theme.colorNeutralStroke1}`,
-    borderLeft: `20px solid ${theme.colorPaletteBlueBorder2}`,
+    ...shorthands.border('5px', 'solid', theme.colorNeutralStroke1),
+    ...shorthands.borderLeft('20px', 'solid', theme.colorPaletteBlueBorder2),
 
     backgroundColor: theme.colorNeutralBackground1,
     color: theme.colorNeutralForeground1,
 
-    margin: '5px',
-    padding: '5px',
+    ...shorthands.margin('5px'),
+    ...shorthands.padding('5px'),
     paddingLeft: '50px',
   }),
 
   container: theme => ({
-    border: `5px solid ${theme.colorNeutralStroke1}`,
-    borderLeft: `20px solid ${theme.colorPaletteBlueBorder2}`,
+    ...shorthands.border('5px', 'solid', theme.colorNeutralStroke1),
+    ...shorthands.borderLeft('20px', 'solid', theme.colorPaletteBlueBorder2),
 
-    margin: '5px',
-    padding: '5px',
+    ...shorthands.margin('5px'),
+    ...shorthands.padding('5px'),
   }),
   containerPrimary: theme => ({
     borderLeftColor: theme.colorPaletteDarkOrangeBorder2,
@@ -36,8 +36,8 @@ const useStyles = makeStyles({
 
 const useFocusStylesA = makeStyles({
   root: {
-    border: '3px solid blue',
-    padding: '10px',
+    ...shorthands.border('3px', 'solid', 'blue'),
+    ...shorthands.padding('10px'),
 
     ':focus': {
       color: 'red',
@@ -49,8 +49,8 @@ const useFocusStylesA = makeStyles({
 });
 const useFocusStylesB = makeStyles({
   root: {
-    border: '3px solid orange',
-    padding: '10px',
+    ...shorthands.border('3px', 'solid', 'orange'),
+    ...shorthands.padding('10px'),
 
     ':hover': {
       color: 'orange',
