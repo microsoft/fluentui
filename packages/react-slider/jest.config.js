6382d6c3f627355d46eb5c9d0e81d920c07f6a9b
André Dias (he/him) <andredias@microsoft.com>
2021-11-30 20:32:58 +0100

    chore: run migration for @fluentui/react-slider (#20856)

--- packages/react-slider/jest.config.js
+++ packages/react-slider/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
+      tsConfig: '<rootDir>/tsconfig.spec.json',
       diagnostics: false,
     },
   },
