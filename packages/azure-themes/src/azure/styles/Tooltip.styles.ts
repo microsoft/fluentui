1af1946f532cbc088e1ee345909d4dd5a79d44e7
Jacqueline Gaherity <30805892+Jacqueline-ms@users.noreply.github.com>
2021-11-30 11:07:29 -0800

    Tooltip dark theme fix

--- packages/azure-themes/src/azure/styles/Tooltip.styles.ts
+++ packages/azure-themes/src/azure/styles/Tooltip.styles.ts
@@ -1,9 +1,19 @@
 import { ITooltipStyles, ITooltipStyleProps } from 'office-ui-fabric-react';
+import { IExtendedSemanticColors } from '../IExtendedSemanticColors';
 
 export const TooltipStyles = (props: ITooltipStyleProps): Partial<ITooltipStyles> => {
+  const { theme } = props;
+  const { semanticColors } = theme;
+  const extendedSemanticColors = semanticColors as IExtendedSemanticColors;
+
   return {
     root: {
       maxWidth: '480px',
+      padding: '0',
+    },
+    content: {
+      backgroundColor: extendedSemanticColors.controlBackground,
+      padding: 8,
     },
   };
 };
