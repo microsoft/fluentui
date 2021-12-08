e20d0b9e44b60b7279ed368f64de032d36be3b23
Lingfan Gao <lingfangao@hotmail.com>
2021-12-08 14:45:42 +0100

    fix(Badge): Add uniform padding and modify gaps
    Adds uniform padding around the badge to achieve the desired effect for
    circular icon and one character badges.
    
    The wider variants still look OK. I'm not sure if they are pixel
    perfect, but it's the only way I could find that would achieve the
    desired effect for icon only badges 🤷

--- packages/react-badge/src/stories/BadgeDefault.stories.tsx
+++ packages/react-badge/src/stories/BadgeDefault.stories.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
 
 import { Badge, BadgeProps } from '../index';
+import { EditFilled } from '@fluentui/react-icons';
 
-export const Default = (props: BadgeProps) => <Badge {...props} />;
+const size = 'large';
+export const Default = (props: BadgeProps) => (
+  <>
+    <Badge size={size} shape="rounded" icon={<EditFilled />} />
+    <Badge size={size} shape="rounded" icon={<EditFilled />}>
+      999+
+    </Badge>
+    <Badge size={size} shape="rounded" iconPosition="after" icon={<EditFilled />}>
+      999+
+    </Badge>
+    <Badge size={size} {...props}>
+      0
+    </Badge>
+    <Badge size={size} {...props}>
+      0000
+    </Badge>
+  </>
+);
