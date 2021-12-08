156cee8494a9e3360b3f36a65d444d4df0432874
Lingfan Gao <lingfangao@hotmail.com>
2021-12-08 14:49:24 +0100

    revert bagde stories

--- packages/react-badge/src/stories/BadgeDefault.stories.tsx
+++ packages/react-badge/src/stories/BadgeDefault.stories.tsx
@@ -1,23 +1,5 @@
 import * as React from 'react';
 
 import { Badge, BadgeProps } from '../index';
-import { EditFilled } from '@fluentui/react-icons';
 
-const size = 'large';
-export const Default = (props: BadgeProps) => (
-  <>
-    <Badge size={size} shape="rounded" icon={<EditFilled />} />
-    <Badge size={size} shape="rounded" icon={<EditFilled />}>
-      999+
-    </Badge>
-    <Badge size={size} shape="rounded" iconPosition="after" icon={<EditFilled />}>
-      999+
-    </Badge>
-    <Badge size={size} {...props}>
-      0
-    </Badge>
-    <Badge size={size} {...props}>
-      0000
-    </Badge>
-  </>
-);
+export const Default = (props: BadgeProps) => <Badge {...props} />;
