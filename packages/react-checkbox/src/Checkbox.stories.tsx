070746ef0e37b149290a536714807b8685d44a34
Ben Howell <behowell@microsoft.com>
2021-12-09 05:29:56 -0800

    Split stories into their own files and update descriptions

--- packages/react-checkbox/src/Checkbox.stories.tsx
+++ packages/react-checkbox/src/Checkbox.stories.tsx
@@ -1,128 +1,26 @@
 import * as React from 'react';
-import { Checkbox, CheckboxProps } from './index';
+import { Meta } from '@storybook/react';
+import { Checkbox } from './index';
 
-export const Default = (props: CheckboxProps) => <Checkbox {...props} />;
-Default.argTypes = {
-  label: {
-    control: 'text',
-    defaultValue: 'Checkbox',
-    type: 'string',
-  },
-  checked: {
-    control: {
-      type: 'inline-radio',
-      options: [undefined, false, true, 'mixed'],
-    },
-  },
-  size: {
-    control: {
-      type: 'inline-radio',
-    },
-  },
-  defaultChecked: {
-    control: {
-      type: 'inline-radio',
-      options: [false, true, 'mixed'],
-    },
-  },
-};
-
-export const Label = () => <Checkbox label="Labeled checkbox" />;
-Label.parameters = {
-  docs: {
-    description: {
-      story:
-        'A checkbox can have a label with the `label` slot instead of an external `<label>` element. The benefit of ' +
-        'using the slot is that styling such as padding, and hover/disabled colors are handled automatically.',
-    },
-  },
-};
-
-export const Large = () => <Checkbox size="large" label="Large checkbox" />;
-Large.parameters = {
-  docs: {
-    description: {
-      story: 'A checkbox can be large in size.',
-    },
-  },
-};
-
-export const Circular = () => <Checkbox circular label="Circular checkbox" />;
-Circular.parameters = {
-  docs: {
-    description: {
-      story:
-        'A checkbox can have a circular shape. This variant is **strongly discourged** outside of a tasks ' +
-        'experience, since it could be confused for a `RadioItem`.',
-    },
-  },
-};
-
-export const LabelBefore = () => <Checkbox labelPosition="before" label="Label before the checkbox" />;
-LabelBefore.parameters = {
-  docs: {
-    description: {
-      story: 'The label can be placed in front of the checkbox.',
-    },
-  },
-};
-
-export const Required = () => <Checkbox required label="Required checkbox" />;
-Required.parameters = {
-  docs: {
-    description: {
-      story: 'When a checkbox is marked as `required`, its label also gets the required styling.',
-    },
-  },
-};
-
-export const Disabled = () => <Checkbox disabled label="Disabled checkbox" />;
-Disabled.parameters = {
-  docs: {
-    description: {
-      story: 'A checkbox can be disabled.',
-    },
-  },
-};
-
-export const Checked = () => <Checkbox defaultChecked label="Checked checkbox" />;
-Checked.parameters = {
-  docs: {
-    description: {
-      story: 'A checkbox can be initially checked using `defaultChecked`, or controlled via the `checked` property.',
-    },
-  },
-};
-
-export const Mixed = () => <Checkbox defaultChecked="mixed" label="Mixed checkbox" />;
-Mixed.parameters = {
-  docs: {
-    description: {
-      story:
-        'A checkbox can be initially mixed (also known as indeterminate) using `defaultChecked="mixed"`, ' +
-        'or controlled via `checked="mixed"`',
-    },
-  },
-};
-
-export const ExternalLabel = () => (
-  <div style={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-    <Checkbox id="exampleCheckbox" />
-    <label htmlFor="exampleCheckbox">This is an external label</label>
-  </div>
-);
-ExternalLabel.parameters = {
-  docs: {
-    description: {
-      story:
-        "An external `<label>` element or `<Label>` component can be used via the checkbox's `id` and the label's " +
-        "`htmlFor` property, just as with any HTML checkbox. However, the external label won't have padding or other " +
-        'styling applied.',
-    },
-  },
-};
+export { Default } from './Checkbox.Default.stories';
+export { Checked } from './Checkbox.Checked.stories';
+export { Mixed } from './Checkbox.Mixed.stories';
+export { Disabled } from './Checkbox.Disabled.stories';
+export { Large } from './Checkbox.Large.stories';
+export { LabelBefore } from './Checkbox.LabelBefore.stories';
+export { LabelWrapping } from './Checkbox.LabelWrapping.stories';
+export { Required } from './Checkbox.Required.stories';
+export { Circular } from './Checkbox.Circular.stories';
 
 export default {
   title: 'Components/Checkbox',
   component: Checkbox,
-};
+
+  decorators: [
+    Story => (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
