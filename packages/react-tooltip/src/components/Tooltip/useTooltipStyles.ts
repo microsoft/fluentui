2c9beb52b9b746ef0450cad8befc68f823fee457
KHMakoto <Humberto.Morimoto@microsoft.com>
2021-12-15 00:05:41 -0800

    react-tooltip: Replacing use of functions in makeStyles with direct use of tokens.

--- packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
+++ packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
@@ -1,5 +1,6 @@
 import { shorthands, makeStyles, mergeClasses } from '@fluentui/react-make-styles';
 import { createArrowStyles } from '@fluentui/react-positioning';
+import { tokens } from '@fluentui/react-theme';
 import type { TooltipState } from './Tooltip.types';
 
 export const tooltipClassName = 'fui-Tooltip';
@@ -8,35 +9,35 @@ export const tooltipClassName = 'fui-Tooltip';
  * Styles for the tooltip
  */
 const useStyles = makeStyles({
-  root: theme => ({
+  root: {
     display: 'none',
     ...shorthands.padding('5px', '12px', '7px', '12px'),
     maxWidth: '240px',
     cursor: 'default',
-    fontFamily: theme.fontFamilyBase,
-    fontSize: theme.fontSizeBase200,
-    lineHeight: theme.lineHeightBase200,
+    fontFamily: tokens.fontFamilyBase,
+    fontSize: tokens.fontSizeBase200,
+    lineHeight: tokens.lineHeightBase200,
 
     // Update tooltipBorderRadius in useTooltip.tsx if this changes
-    ...shorthands.borderRadius(theme.borderRadiusMedium),
+    ...shorthands.borderRadius(tokens.borderRadiusMedium),
 
-    backgroundColor: theme.colorNeutralBackground1,
-    color: theme.colorNeutralForeground1,
+    backgroundColor: tokens.colorNeutralBackground1,
+    color: tokens.colorNeutralForeground1,
 
-    // TODO need to add versions of theme.alias.shadow.shadow8, etc. that work with filter
+    // TODO need to add versions of tokens.alias.shadow.shadow8, etc. that work with filter
     filter:
-      `drop-shadow(0 0 2px ${theme.colorNeutralShadowAmbient}) ` +
-      `drop-shadow(0 4px 8px ${theme.colorNeutralShadowKey})`,
-  }),
+      `drop-shadow(0 0 2px ${tokens.colorNeutralShadowAmbient}) ` +
+      `drop-shadow(0 4px 8px ${tokens.colorNeutralShadowKey})`,
+  },
 
   visible: {
     display: 'block',
   },
 
-  inverted: theme => ({
-    backgroundColor: theme.colorNeutralBackgroundInverted,
-    color: theme.colorNeutralForegroundInverted,
-  }),
+  inverted: {
+    backgroundColor: tokens.colorNeutralBackgroundInverted,
+    color: tokens.colorNeutralForegroundInverted,
+  },
 
   // width and height = arrowHeight * sqrt(2)
   // Update arrowHeight in useTooltip.tsx if this changes
