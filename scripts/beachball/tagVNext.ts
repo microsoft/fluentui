8b9fd9038e0bfb7b07c7a3036f55cf5b1e53633c
Lingfan Gao <lingfangao@hotmail.com>
2021-12-16 15:04:22 +0100

    use tmp npmrc

--- scripts/beachball/tagVNext.ts
+++ scripts/beachball/tagVNext.ts
@@ -1,7 +1,9 @@
 import { getScopes } from './getScopes';
 import { execSync } from 'child_process';
 import * as semver from 'semver';
+import * as tmp from 'tmp';
 import { AllPackageInfo, getAllPackageInfo, isConvergedPackage } from '../monorepo/index';
+import * as fs from 'fs';
 
 process.env.RELEASE_VNEXT = 'true';
 const vNextScope = getScopes();
@@ -17,9 +19,18 @@ const packagesToTag = Object.values(packageInfos)
   })
   .filter(Boolean);
 
+const tmpDir = tmp.dirSync();
+
+const npmrc = tmp.fileSync({
+  name: '.npmrc',
+  dir: tmpDir.name,
+});
+
+fs.writeFileSync(npmrc.name, '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
+
 packagesToTag.forEach(pkg => {
   const prereleaseTag = semver.parse(pkg.version).prerelease[0];
-  const command = `npm dist-tag add ${pkg.name}@${pkg.version} ${prereleaseTag}`;
+  const command = `npm dist-tag add ${pkg.name}@${pkg.version} ${prereleaseTag} --userconfig ${npmrc.name}`;
   console.log(command);
   try {
     const res = execSync(command);
@@ -29,3 +40,6 @@ packagesToTag.forEach(pkg => {
     console.error(e);
   }
 });
+
+npmrc.removeCallback();
+tmpDir.removeCallback();
