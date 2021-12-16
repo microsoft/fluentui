06f5f609f4e59d6fb14a6f12f412edc72fe0c61d
Lingfan Gao <lingfangao@hotmail.com>
2021-12-16 15:55:06 +0100

    cleanup

--- scripts/beachball/tagVNext.ts
+++ scripts/beachball/tagVNext.ts
@@ -5,41 +5,63 @@ import * as tmp from 'tmp';
 import { AllPackageInfo, getAllPackageInfo, isConvergedPackage } from '../monorepo/index';
 import * as fs from 'fs';
 
-process.env.RELEASE_VNEXT = 'true';
-const vNextScope = getScopes();
-const packageInfos: AllPackageInfo = getAllPackageInfo();
-const packagesToTag = Object.values(packageInfos)
-  .map(packageInfo => {
-    if (vNextScope.includes(packageInfo.packagePath) && isConvergedPackage(packageInfo.packageJson)) {
-      return {
-        name: packageInfo.packageJson.name,
-        version: packageInfo.packageJson.version,
-      };
-    }
-  })
-  .filter(Boolean);
+(function () {
+  const packagesToTag = getPackagesToTag();
+  const { path: npmrcPath, cleanup: cleanupTmpFiles } = createTmpNpmrc();
 
-const tmpDir = tmp.dirSync();
+  packagesToTag.forEach(pkg => {
+    tagPackage(pkg.name, pkg.version, npmrcPath);
+  });
 
-const npmrc = tmp.fileSync({
-  name: '.npmrc',
-  dir: tmpDir.name,
-});
+  cleanupTmpFiles();
+})();
 
-fs.writeFileSync(npmrc.name, '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
-
-packagesToTag.forEach(pkg => {
-  const prereleaseTag = semver.parse(pkg.version).prerelease[0];
-  const command = `npm dist-tag add ${pkg.name}@${pkg.version} ${prereleaseTag} --userconfig ${npmrc.name} --registry https://registry.npmjs.org/`;
+function tagPackage(name: string, version: string, npmrcPath: string) {
+  const prereleaseTag = semver.parse(version).prerelease[0];
+  const command = `npm dist-tag add ${name}@${version} ${prereleaseTag} --userconfig ${npmrcPath} --registry https://registry.npmjs.org/`;
   console.log(command);
   try {
     const res = execSync(command);
     console.log(res.toString());
   } catch (e) {
-    console.error(`failed to tag ${pkg.name}@${pkg.version}`);
+    console.error(`failed to tag ${name}@${version}`);
     console.error(e);
   }
-});
+}
 
-npmrc.removeCallback();
-tmpDir.removeCallback();
+function getPackagesToTag() {
+  const vNextScope = getScopes();
+  const packageInfos: AllPackageInfo = getAllPackageInfo();
+  return Object.values(packageInfos)
+    .map(packageInfo => {
+      if (
+        !packageInfo.packageJson.private &&
+        isConvergedPackage(packageInfo.packageJson) &&
+        vNextScope.includes(packageInfo.packagePath)
+      ) {
+        return {
+          name: packageInfo.packageJson.name,
+          version: packageInfo.packageJson.version,
+        };
+      }
+    })
+    .filter(Boolean);
+}
+
+function createTmpNpmrc() {
+  const tmpDir = tmp.dirSync();
+
+  const npmrc = tmp.fileSync({
+    name: '.npmrc',
+    dir: tmpDir.name,
+  });
+
+  fs.writeFileSync(npmrc.name, '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
+
+  const cleanup = () => {
+    npmrc.removeCallback();
+    tmpDir.removeCallback();
+  };
+
+  return { path: npmrc.name, cleanup };
+}
