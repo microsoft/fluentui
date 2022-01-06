1b2f2648a0e8183b455e118fa4f51bb0547a8ce0
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-01-06 14:36:29 -0800

    react-radio: Replacing use of functions in makeStyles with direct use of tokens (#21053)
    * react-theme: Exporting token to css variable mapping.
    
    * Change files
    
    * Updating packages based on changes to focusIndicator functions to remove functions from makeStyles in @fluentui/react-tabster.
    
    * Change files
    
    * react-radio: Replacing use of functions in makeStyles with direct use of tokens.
    
    * Moving tokens.ts into src/ and out of src/global.
    
    * Moving tokens.ts into src/ and out of src/global.
    
    * Removing change files for private packages.
    
    * Readding unintended deleted file.
    
    * Fixing build error.

--- packages/react-radio/src/components/Radio/useRadioStyles.ts
+++ packages/react-radio/src/components/Radio/useRadioStyles.ts
@@ -7,9 +7,9 @@ export const radioClassName = 'fui-Radio';
  * Styles for the root slot
  */
 const useStyles = makeStyles({
-  root: theme => ({
+  root: {
     // TODO Add default styles for the root element
-  }),
+  },
 
   // TODO add additional classes for different states and/or slots
 });
