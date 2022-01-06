9204703d0a42ee34a20ae4cf28952e13872647ba
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-01-06 13:27:22 -0800

    react-tooltip: Replacing use of functions in makeStyles with direct use of tokens (#21058)
    * react-theme: Exporting token to css variable mapping.
    
    * Change files
    
    * react-tooltip: Replacing use of functions in makeStyles with direct use of tokens.
    
    * Change files
    
    * Moving tokens.ts into src/ and out of src/global.
    
    * Update @fluentui-react-tooltip-3983a8eb-9223-4f77-8e3f-30c0082715c6.json

--- packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
+++ packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
@@ -1,5 +1,6 @@
 import { shorthands, makeStyles, mergeClasses } from '@fluentui/react-make-styles';
 import { createArrowStyles } from '@fluentui/react-positioning';
+import { tokens } from '@fluentui/react-theme';
 import type { TooltipState } from './Tooltip.types';
 
 export const tooltipClassName = 'fui-Tooltip';
@@ -8,35 +9,35 @@ export const tooltipClassName = 'fui-Tooltip';
  * Styles for the tooltip
  */
 const useStyles = makeStyles({
-  root: theme => ({
+  root: {
     display: 'none',
     boxSizing: 'border-box',
     maxWidth: '240px',
     cursor: 'default',
-    fontFamily: theme.fontFamilyBase,
-    fontSize: theme.fontSizeBase200,
-    lineHeight: theme.lineHeightBase200,
+    fontFamily: tokens.fontFamilyBase,
+    fontSize: tokens.fontSizeBase200,
+    lineHeight: tokens.lineHeightBase200,
 
-    ...shorthands.borderRadius(theme.borderRadiusMedium), // Must match tooltipBorderRadius in useTooltip.tsx
-    ...shorthands.border('1px', 'solid', theme.colorTransparentStroke),
+    ...shorthands.borderRadius(tokens.borderRadiusMedium), // Must match tooltipBorderRadius in useTooltip.tsx
+    ...shorthands.border('1px', 'solid', tokens.colorTransparentStroke),
     ...shorthands.padding('4px', '11px', '6px', '11px'), // '5px 12px 7px 12px' minus the border width '1px'
-    backgroundColor: theme.colorNeutralBackground1,
-    color: theme.colorNeutralForeground1,
+    backgroundColor: tokens.colorNeutralBackground1,
+    color: tokens.colorNeutralForeground1,
 
-    // TODO need to add versions of theme.alias.shadow.shadow8, etc. that work with filter
+    // TODO need to add versions of tokens.alias.shadow.shadow8, etc. that work with filter
     filter:
-      `drop-shadow(0 0 2px ${theme.colorNeutralShadowAmbient}) ` +
-      `drop-shadow(0 4px 8px ${theme.colorNeutralShadowKey})`,
-  }),
+      `drop-shadow(0 0 2px ${tokens.colorNeutralShadowAmbient}) ` +
+      `drop-shadow(0 4px 8px ${tokens.colorNeutralShadowKey})`,
+  },
 
   visible: {
     display: 'block',
   },
 
-  inverted: theme => ({
-    backgroundColor: theme.colorNeutralBackgroundInverted,
-    color: theme.colorNeutralForegroundInverted,
-  }),
+  inverted: {
+    backgroundColor: tokens.colorNeutralBackgroundInverted,
+    color: tokens.colorNeutralForegroundInverted,
+  },
 
   arrow: createArrowStyles({ arrowHeight: 6 }), // Must match arrowHeight in useTooltip.tsx
 });
