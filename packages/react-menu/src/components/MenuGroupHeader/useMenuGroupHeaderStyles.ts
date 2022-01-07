878f69b76bf7a01f434f8ff8de98d97b2b373c8d
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-01-07 10:12:12 -0800

    react-menu: Replacing use of functions in makeStyles with direct use of tokens (#21048)
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Implements part of #20651
    - [x] Include a change request file using `$ yarn change`
    
    #### Description of changes
    
    _This PR is part of a series of PRs that split #20918 into smaller PRs._
    _This PR follows #21026 and #21035._
    
    This PR replaces the use of functions in `makeStyles` calls in the `@fluentui/react-accordion` package with direct use of the `tokens` exported from `@fluentui/react-theme`.

--- packages/react-menu/src/components/MenuGroupHeader/useMenuGroupHeaderStyles.ts
+++ packages/react-menu/src/components/MenuGroupHeader/useMenuGroupHeaderStyles.ts
@@ -1,19 +1,21 @@
+import * as React from 'react';
 import { mergeClasses, makeStyles } from '@fluentui/react-make-styles';
+import { tokens } from '@fluentui/react-theme';
 import type { MenuGroupHeaderState } from './MenuGroupHeader.types';
 
 export const menuGroupHeaderClassName = 'fui-MenuGroupHeader';
 
 const useStyles = makeStyles({
-  root: theme => ({
-    fontSize: theme.fontSizeBase200,
-    color: theme.colorNeutralForeground3,
+  root: {
+    fontSize: tokens.fontSizeBase200,
+    color: tokens.colorNeutralForeground3,
     paddingLeft: '12px',
     paddingRight: '12px',
-    fontWeight: theme.fontWeightSemibold,
+    fontWeight: tokens.fontWeightSemibold as React.CSSProperties['fontWeight'],
     height: '32px',
     display: 'flex',
     alignItems: 'center',
-  }),
+  },
 });
 
 export const useMenuGroupHeaderStyles = (state: MenuGroupHeaderState) => {
