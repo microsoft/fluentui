7204663c4625f9f53f07214b4c3f128a1561104a
Ben Howell <behowell@microsoft.com>
2022-01-19 12:43:57 -0800

    Update positioning stories to include icon

--- packages/react-tooltip/src/stories/TooltipPositioning.stories.tsx
+++ packages/react-tooltip/src/stories/TooltipPositioning.stories.tsx
@@ -1,43 +1,50 @@
 import * as React from 'react';
 import { Tooltip } from '../Tooltip';
 import { Button } from '@fluentui/react-button';
+import { ArrowUpRegular, ArrowEnterUpRegular } from '@fluentui/react-icons';
 
 export const Positioning = () => {
+  /* eslint-disable react/jsx-key */
   const positions = [
-    'above-start',
-    'above',
-    'above-end',
-    'before-top',
-    'before',
-    'before-bottom',
-    'after-top',
-    'after',
-    'after-bottom',
-    'below-start',
-    'below',
-    'below-end',
+    ['above-start', <ArrowEnterUpRegular />],
+    ['above', <ArrowUpRegular />],
+    ['above-end', <ArrowEnterUpRegular transform="scale(-1 1)" />],
+
+    ['before-top', <ArrowEnterUpRegular transform="rotate(-90) scale(-1 1)" />],
+    ['before', <ArrowUpRegular transform="rotate(-90)" />],
+    ['before-bottom', <ArrowEnterUpRegular transform="rotate(-90)" />],
+
+    ['after-top', <ArrowEnterUpRegular transform="rotate(90)" />],
+    ['after', <ArrowUpRegular transform="rotate(90)" />],
+    ['after-bottom', <ArrowEnterUpRegular transform="rotate(90) scale(-1 1)" />],
+
+    ['below-start', <ArrowEnterUpRegular transform="rotate(180) scale(-1 1)" />],
+    ['below', <ArrowUpRegular transform="rotate(180)" />],
+    ['below-end', <ArrowEnterUpRegular transform="rotate(180)" />],
   ] as const;
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gap: '4px',
-        gridTemplateColumns: 'repeat(5, 1fr)',
-        gridTemplateAreas:
-          '".             above-start   above         above-end     .            "' +
-          '"before-top    .             .             .             after-top    "' +
-          '"before        .             .             .             after        "' +
-          '"before-bottom .             .             .             after-bottom "' +
-          '".             below-start   below         below-end     .            "',
-      }}
-    >
-      {positions.map(position => (
-        <Tooltip withArrow key={position} positioning={position} content={position}>
-          <Button size="small" style={{ gridArea: position, height: '40px', minWidth: '40px' }} />
-        </Tooltip>
-      ))}
-    </div>
+    <>
+      <div
+        style={{
+          display: 'grid',
+          margin: '24px 128px',
+          gap: '4px',
+          gridTemplateAreas:
+            '".             above-start   above         above-end     .            "' +
+            '"before-top    .             .             .             after-top    "' +
+            '"before        .             .             .             after        "' +
+            '"before-bottom .             .             .             after-bottom "' +
+            '".             below-start   below         below-end     .            "',
+        }}
+      >
+        {positions.map(([position, icon]) => (
+          <Tooltip withArrow positioning={position} content={position}>
+            <Button style={{ gridArea: position, minWidth: '64px', height: '64px' }} icon={icon} />
+          </Tooltip>
+        ))}
+      </div>
+    </>
   );
 };
 
