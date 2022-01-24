629a8a6a234e806bc10702601ee2b60ef803bd29
Oleksandr Fediashov <olfedias@microsoft.com>
2022-01-24 14:36:48 +0100

    chore: migrate react-switch to use Griffel

--- packages/react-switch/src/components/Switch/useSwitchStyles.ts
+++ packages/react-switch/src/components/Switch/useSwitchStyles.ts
@@ -1,4 +1,4 @@
-import { shorthands, makeStyles, mergeClasses } from '@fluentui/react-make-styles';
+import { shorthands, makeStyles, mergeClasses } from '@griffel/react';
 import { createFocusOutlineStyle } from '@fluentui/react-tabster';
 import { tokens } from '@fluentui/react-theme';
 import type { SwitchState } from './Switch.types';
