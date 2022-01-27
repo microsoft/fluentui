ff119c346a5cfcf4b7a7fb754ac9b22b87afef93
Martin Hochel <martinhochel@microsoft.com>
2022-01-27 16:08:55 +0100

    fixup! feat(scripts): improve pre and post install hooks DX

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,14 +4,18 @@ const chalk = require('chalk');
 
 const COMMAND_PREFIX = `${chalk.cyan('>')} ${chalk.inverse(chalk.bold(chalk.cyan(' GETTING STARTED ')))}`;
 
-// git v2.9.0 supports a custom hooks directory. This means we just need to check in the hooks scripts.
-spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
+main();
 
-console.log('\n');
-console.log(COMMAND_PREFIX);
-console.log('✅ All dependencies are installed! This repo no longer requires a build to start the inner loop.');
-console.log(`🎬 For inner loop development, run: ${chalk.yellow('yarn start')}`);
-console.log(`💡 To learn more about all the commands that this monorepo supports, see the wiki:
+function main() {
+  // git v2.9.0 supports a custom hooks directory. This means we just need to check in the hooks scripts.
+  spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
+
+  console.log('\n');
+  console.log(COMMAND_PREFIX);
+  console.log('✅ All dependencies are installed! This repo no longer requires a build to start the inner loop.');
+  console.log(`🎬 For inner loop development, run: ${chalk.yellow('yarn start')}`);
+  console.log(`💡 To learn more about all the commands that this monorepo supports, see the wiki:
 
   https://github.com/microsoft/fluentui/wiki/Build-Commands
 `);
+}
