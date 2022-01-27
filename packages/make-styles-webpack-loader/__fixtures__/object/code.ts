c630a77b3ce88f650a07252aa9c1871472da6edb
Oleksandr Fediashov <olfedias@microsoft.com>
2022-01-27 16:12:27 +0100

    chore: remove make-styles-webpack-loader

--- packages/make-styles-webpack-loader/__fixtures__/object/code.ts
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@fluentui/react-make-styles';
-import { tokens } from '@fluentui/react-theme';
-
-const styles = makeStyles({
-  root: { color: tokens.colorBrandStroke1 },
-});
-
-console.log(styles);
