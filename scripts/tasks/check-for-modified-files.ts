ead8ba386e377a16a7f45f46e1a2e13de4060394
Elizabeth Craig <elcraig@microsoft.com>
2022-01-31 13:42:02 -0800

    include staged and unstaged

--- scripts/tasks/check-for-modified-files.ts
+++ scripts/tasks/check-for-modified-files.ts
@@ -10,11 +10,13 @@ export function checkForModifiedFiles() {
 
   if (hasChangedFiles) {
     logger.error('This build has files that are tracked by git that resulted in modified files.');
-    logger.error('Check the following output and resolve the problem that caused these files to change.');
+    logger.error('Check the following output and resolve the problem that caused these files to change .');
     logger.error('Most likely you committed your files with --no-verify.');
     logger.error(gitStatusOutput);
 
-    execSync('git --no-pager diff', { stdio: 'inherit' });
+    // Diffing against HEAD will include both unstaged and staged files
+    // (some scripts, such as gulp build:docs:toc, automatically stage the modified files)
+    execSync('git diff HEAD', { stdio: 'inherit' });
 
     throw new Error('Found modified files');
   }
