b51d4d9b5a0e19ebd302ccd3306cef5bbe3e2b3b
Ben Howell <behowell@microsoft.com>
2022-02-01 06:54:56 -0800

    Tweak stories

--- packages/react-avatar/src/AvatarIcon.stories.tsx
+++ packages/react-avatar/src/AvatarIcon.stories.tsx
@@ -4,7 +4,7 @@ import { GuestRegular } from '@fluentui/react-icons';
 import { Avatar } from './index';
 
 export const Icon = () => {
-  return <Avatar icon={<GuestRegular />} aria-label="Guest icon" />;
+  return <Avatar icon={<GuestRegular />} aria-label="Guest Avatar" />;
 };
 
 Icon.parameters = {
