3e0ccfe911e6bf8cdfe32fc42c92be74a5adafa9
Ben Howell <behowell@microsoft.com>
2022-02-01 07:38:21 -0800

    Update stories

--- packages/react-avatar/src/AvatarBadge.stories.tsx
+++ packages/react-avatar/src/AvatarBadge.stories.tsx
@@ -3,42 +3,27 @@ import { Avatar } from './index';
 
 export const Badge = () => (
   <div style={{ display: 'grid', gap: '10px', gridTemplateColumns: 'repeat(6, 32px)' }}>
-    <span>
-      <Avatar badge={{ status: 'available' }} aria-label="available avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'busy' }} aria-label="busy avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'outOfOffice' }} aria-label="outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'away' }} aria-label="away avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'offline' }} aria-label="offline avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'doNotDisturb' }} aria-label="doNotDisturb avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'available', outOfOffice: true }} aria-label="available outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'busy', outOfOffice: true }} aria-label="busy outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'outOfOffice', outOfOffice: true }} aria-label="outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'away', outOfOffice: true }} aria-label="away outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'offline', outOfOffice: true }} aria-label="offline outOfOffice avatar" />
-    </span>
-    <span>
-      <Avatar badge={{ status: 'doNotDisturb', outOfOffice: true }} aria-label="doNotDisturb outOfOffice avatar" />
-    </span>
+    <Avatar name="Lydia Bauer" badge={{ status: 'available', 'aria-label': 'available' }} />
+    <Avatar name="Amanda Brady" badge={{ status: 'busy', 'aria-label': 'busy' }} />
+    <Avatar name="Henry Brill" badge={{ status: 'outOfOffice', 'aria-label': 'out of office' }} />
+    <Avatar name="Robin Counts" badge={{ status: 'away', 'aria-label': 'away' }} />
+    <Avatar name="Tim Deboer" badge={{ status: 'offline', 'aria-label': 'offline' }} />
+    <Avatar name="Cameron Evans" badge={{ status: 'doNotDisturb', 'aria-label': 'do not disturb' }} />
+    <Avatar
+      name="Mona Kane"
+      badge={{ status: 'available', outOfOffice: true, 'aria-label': 'available out of office' }}
+    />
+    <Avatar name="Allan Munger" badge={{ status: 'busy', outOfOffice: true, 'aria-label': 'busy out of office' }} />
+    <Avatar name="Erik Nason" badge={{ status: 'outOfOffice', outOfOffice: true, 'aria-label': 'out of office' }} />
+    <Avatar name="Daisy Phillips" badge={{ status: 'away', outOfOffice: true, 'aria-label': 'away out of office' }} />
+    <Avatar
+      name="Kevin Sturgis"
+      badge={{ status: 'offline', outOfOffice: true, 'aria-label': 'offline out of office' }}
+    />
+    <Avatar
+      name="Elliot Woodward"
+      badge={{ status: 'doNotDisturb', outOfOffice: true, 'aria-label': 'do not disturb out of office' }}
+    />
   </div>
 );
 
--- packages/react-avatar/src/AvatarIcon.stories.tsx
+++ packages/react-avatar/src/AvatarIcon.stories.tsx
@@ -4,7 +4,7 @@ import { GuestRegular } from '@fluentui/react-icons';
 import { Avatar } from './index';
 
 export const Icon = () => {
-  return <Avatar icon={<GuestRegular />} aria-label="Guest Avatar" />;
+  return <Avatar icon={<GuestRegular />} aria-label="Guest" />;
 };
 
 Icon.parameters = {
