543b3ec5208eaa3988e78c543d8a5a1831ae223b
Micah Godbolt <mgodbolt@microsoft.com>
2022-02-02 15:07:16 -0800

    return card to normal

--- packages/react-card/src/Card.stories.tsx
+++ packages/react-card/src/Card.stories.tsx
@@ -1,4 +1,5 @@
 import { Card, CardFooter, CardHeader, CardPreview } from './index';
+import descriptionMd from './CardDescription.md';
 
 export { Default } from './stories/CardDefault.stories';
 export { ActionCard } from './stories/CardAction.stories';
@@ -7,5 +8,12 @@ export { GridCard } from './stories/CardGrid.stories';
 export default {
   title: 'Preview Components/Card',
   component: Card,
+  parameters: {
+    docs: {
+      description: {
+        component: [descriptionMd].join('\n'),
+      },
+    },
+  },
   subcomponents: { CardHeader, CardPreview, CardFooter },
 };
