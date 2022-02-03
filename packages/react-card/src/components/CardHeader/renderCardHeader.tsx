a9d2b268f65b2c3b6fca027c6d1fb34749862a2b
Bernardo Sunderhus <bsunderhus@microsoft.com>
2022-02-03 13:56:01 +0100

    Updates components with nullRender changes

--- packages/react-card/src/components/CardHeader/renderCardHeader.tsx
+++ packages/react-card/src/components/CardHeader/renderCardHeader.tsx
@@ -11,11 +11,13 @@ export const renderCardHeader_unstable = (state: CardHeaderState) => {
   return (
     <slots.root {...slotProps.root}>
       <slots.image {...slotProps.image} />
-      <slots.content {...slotProps.content}>
-        <slots.header {...slotProps.header} />
-        <slots.description {...slotProps.description} />
-      </slots.content>
-      <slots.action {...slotProps.action} />
+      {slots.content && (
+        <slots.content {...slotProps.content}>
+          <slots.header {...slotProps.header} />
+          <slots.description {...slotProps.description} />
+        </slots.content>
+      )}
+      {slots.action && <slots.action {...slotProps.action} />}
     </slots.root>
   );
 };
