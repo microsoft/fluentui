97c3818f7faf8733a0cfc9c91f5e94ddb8652d3a
Ben Howell <48106640+behowell@users.noreply.github.com>
2022-02-03 18:09:27 -0800

    Tooltip: Update Documentation in Storybook (#21298)
    Rewrite most of the tooltip stories for clarity and style.

---
+++ packages/react-tooltip/src/stories/TooltipTarget.stories.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Tooltip } from '../Tooltip';
+import { Button } from '@fluentui/react-button';
+import { ArrowRoutingRegular } from '@fluentui/react-icons';
+
+export const Target = () => {
+  const [icon, setIconRef] = React.useState<HTMLSpanElement | null>(null);
+  return (
+    <Tooltip
+      positioning={{ target: icon }}
+      withArrow
+      content="This tooltip points to the icon"
+      relationship="description"
+    >
+      <Button icon={{ ref: setIconRef, children: <ArrowRoutingRegular /> }}>Button with icon</Button>
+    </Tooltip>
+  );
+};
+
+Target.parameters = {
+  docs: {
+    description: {
+      story: `The tooltip can be placed relative to a custom element using \`positioning.target\`. In this example, the
+        tooltip points to the icon inside the button, but it could point to any element.`,
+    },
+  },
+};
