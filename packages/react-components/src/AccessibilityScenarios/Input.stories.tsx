d3716440cb2ff0c434a090ebf5c5e11ddfa50706
adam.samec <adam.samec@gmail.com>
2022-02-08 14:08:13 +0100

    Add uncomplete Input scenario

---
+++ packages/react-components/src/AccessibilityScenarios/Input.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Input } from '@fluentui/react-components/unstable';
+
+// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Label } from '@fluentui/react-label';
+
+// https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Button } from '@fluentui/react-button';
+
+import { Scenario } from './utils';
+
+export const RegistrationFormInputsAccessibilityScenario: React.FunctionComponent = () => {
+  return (
+    <Scenario pageTitle="Registration form inputs">
+      <h1>Registration form</h1>
+
+      <Label htmlFor="name">Name:</Label>
+      <Input type="text" id="name" name="name" />
+
+      <Label htmlFor="password">Password:</Label>
+      <Input type="password" id="password" name="password" />
+
+      <Label htmlFor="confirmPassword">Confirm password:</Label>
+      <Input type="password" id="confirmPassword" name="confirmPassword" />
+    </Scenario>
+  );
+};
+
+export default {
+  title: 'Accessibility Scenarios/ Registration form inputs',
+  id: 'input-accessibility-scenario',
+};
