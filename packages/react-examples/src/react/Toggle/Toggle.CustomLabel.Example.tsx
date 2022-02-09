9e8efc1714680c85743071d62dbc7fdc35220b49
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-02-09 15:44:38 -0800

    Wrap icon in a button

--- packages/react-examples/src/react/Toggle/Toggle.CustomLabel.Example.tsx
+++ packages/react-examples/src/react/Toggle/Toggle.CustomLabel.Example.tsx
@@ -3,35 +3,39 @@ import { Icon } from '@fluentui/react/lib/Icon';
 import { Stack, IStackTokens } from '@fluentui/react/lib/Stack';
 import { Toggle } from '@fluentui/react/lib/Toggle';
 import { TooltipHost } from '@fluentui/react/lib/Tooltip';
+import { useId } from '@fluentui/react-hooks';
 
 const stackTokens: IStackTokens = { childrenGap: 10 };
+const buttonStyles = {
+  background: 'transparent',
+  border: 'none',
+};
 
 export const ToggleCustomLabelExample: React.FunctionComponent = () => {
+  const [showTooltip, setShowTooltip] = React.useState(false);
+  const tooltipId = useId('tooltipId');
+
+  const iconWithTooltip = (
+    <>
+      <TooltipHost content={showTooltip ? 'Info tooltip' : undefined} id={tooltipId}>
+        <button
+          style={buttonStyles}
+          aria-label={showTooltip ? 'Close Tooltip' : 'Open Tooltip'}
+          onClick={() => setShowTooltip(!showTooltip)}
+          aria-describedby={showTooltip ? tooltipId : undefined}
+        >
+          <Icon iconName="Info" />
+        </button>
+      </TooltipHost>
+    </>
+  );
+
   return (
     <Stack tokens={stackTokens}>
-      <Toggle
-        label={
-          <div>
-            Custom label{' '}
-            <TooltipHost content="Info tooltip">
-              <Icon iconName="Info" aria-label="Info tooltip" />
-            </TooltipHost>
-          </div>
-        }
-        onText="On"
-        offText="Off"
-        onChange={_onChange}
-      />
+      <Toggle label={<div>Custom label {iconWithTooltip}</div>} onText="On" offText="Off" onChange={_onChange} />
 
       <Toggle
-        label={
-          <div>
-            Custom inline label{' '}
-            <TooltipHost content="Info tooltip">
-              <Icon iconName="Info" aria-label="Info tooltip" />
-            </TooltipHost>
-          </div>
-        }
+        label={<div>Custom inline label {iconWithTooltip}</div>}
         inlineLabel
         onText="On"
         offText="Off"
