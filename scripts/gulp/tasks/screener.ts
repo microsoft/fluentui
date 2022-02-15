f279999cd27b84d96a2dc33c86d71b3642c2274d
Lingfan Gao <lingfangao@hotmail.com>
2022-02-15 16:04:56 +0000

    use modern js

--- scripts/gulp/tasks/screener.ts
+++ scripts/gulp/tasks/screener.ts
@@ -38,7 +38,7 @@ task('screener:runner', cb => {
       });
 
   const screenerConfig = require(screenerConfigPath);
-  const isPrBuild = process.env.BUILD_SOURCEBRANCH && process.env.BUILD_SOURCEBRANCH.indexOf('refs/pull');
+  const isPrBuild = process.env.BUILD_SOURCEBRANCH && process.env.BUILD_SOURCEBRANCH.includes('refs/pull');
 
   if (changedPackages.has(docsPackageName) && isPrBuild) {
     handlePromiseExit(screenerRunner(screenerConfig));
