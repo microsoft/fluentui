1bba930fdc1cb7160349ae60d409e18d04a6d0b0
Lingfan Gao <lingfangao@hotmail.com>
2022-02-15 12:22:13 +0000

    modernize eslint

--- apps/vr-tests-react-components/src/utilities/index.ts
+++ apps/vr-tests-react-components/src/utilities/index.ts
@@ -1,7 +1,6 @@
 import { ExtendedStoryApi } from './types';
 
 declare module '@storybook/addons' {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   interface StoryApi {
     /** adds a story, but via VR Tests' addon which auto adds variants like RTL */
     addStory: ExtendedStoryApi['addStory'];
--- apps/vr-tests-react-components/src/utilities/types.ts
+++ apps/vr-tests-react-components/src/utilities/types.ts
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { StoryApi, StoryName, LegacyStoryFn } from '@storybook/addons';
 
 /** Extra parameters provided by our addon (see `.storybook/preview.js`) */
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export interface AddStoryConfig {
   /** Whether to include an RTL version of the story */
   includeRtl?: boolean;
@@ -16,7 +15,6 @@ export type ExtendedStoryFnReturnType = React.ReactElement<unknown>;
 
 export type ExtendedStoryFn = LegacyStoryFn<ExtendedStoryFnReturnType>;
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export interface ExtendedStoryApi extends StoryApi<ExtendedStoryFnReturnType> {
   addStory: (storyName: StoryName, storyFn: ExtendedStoryFn, config?: AddStoryConfig) => ExtendedStoryApi;
 
