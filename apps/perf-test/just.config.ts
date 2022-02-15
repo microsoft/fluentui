aac13d4aa9d111c09c8d0716b89161fece5aab49
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-02-15 15:57:58 -0800

    add conditionals and run-lage task

--- apps/perf-test/just.config.ts
+++ apps/perf-test/just.config.ts
@@ -1,7 +1,32 @@
 import { getPerfRegressions } from './tasks/perf-test';
-import { preset, task, series } from '@fluentui/scripts';
+import { preset, task, series, condition } from '@fluentui/scripts';
+import { getAffectedPackages, findGitRoot } from '../../scripts/monorepo';
+import { spawnSync } from 'child_process';
 
 preset();
 
+task('run-lage', () => {
+  console.log('Affected packages ', getAffectedPackages());
+  const gitRoot = findGitRoot();
+  const result = spawnSync('yarn', ['lage', 'build', '--to', 'perf-test', '--verbose'], {
+    cwd: gitRoot,
+    shell: true,
+    encoding: 'utf-8',
+  });
+  if (result.status !== 0) {
+    throw new Error(`yarn lage build --to -perf-test --verbose failed with status ${result.status}`);
+  } else {
+    console.log(result.stderr);
+  }
+});
 task('run-perf-test', getPerfRegressions);
-task('perf-test', series('build', 'bundle', 'run-perf-test'));
+
+task(
+  'perf-test',
+  series(
+    condition('run-lage', () => getAffectedPackages().has('@fluentui/react')),
+    condition('build', () => getAffectedPackages().has('@fluentui/react')),
+    condition('bundle', () => getAffectedPackages().has('@fluentui/react')),
+    condition('run-perf-test', () => getAffectedPackages().has('@fluentui/react')),
+  ),
+);
--- packages/fluentui/perf-test-northstar/just.config.ts
+++ packages/fluentui/perf-test-northstar/just.config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
-import { preset, series, task, argv } from '@fluentui/scripts';
+import { preset, series, task, argv, condition } from '@fluentui/scripts';
 import { getResolveLoaderDirs } from '@fluentui/scripts/webpack/webpack-resources';
-
+import { getAffectedPackages, findGitRoot } from '../../../scripts/monorepo';
+import { spawnSync } from 'child_process';
 preset();
 
 // TODO: FUR integration issues
@@ -33,9 +34,33 @@ task('perf-test:run', () => {
   return runPerfTest(argv().base);
 });
 
+task('run-lage', () => {
+  console.log('Affected packages ', getAffectedPackages());
+
+  const gitRoot = findGitRoot();
+  const result = spawnSync('yarn', ['lage', 'build', '--to', '@fluentui/perf-test-northstar', '--verbose'], {
+    cwd: gitRoot,
+    shell: true,
+    encoding: 'utf-8',
+  });
+  if (result.status !== 0) {
+    throw new Error(`yarn lage build --to @fluentui/perf-test-northstar --verbose failed with status ${result.status}`);
+  } else {
+    console.log(result.stderr);
+  }
+});
+
 // TOOD: is build doing anything meaningful? only if there's source that's not a just script?
 // TODO: if stories/scenarios are added in this package, make sure build catches type errors
-task('perf-test', series('build', 'perf-test:bundle', 'perf-test:run'));
+task(
+  'perf-test',
+  series(
+    condition('run-lage', () => getAffectedPackages().has('@fluentui/react-northstar')),
+    condition('build', () => getAffectedPackages().has('@fluentui/react-northstar')),
+    condition('perf-test:bundle', () => getAffectedPackages().has('@fluentui/react-northstar')),
+    condition('perf-test:run', () => getAffectedPackages().has('@fluentui/react-northstar')),
+  ),
+);
 
 // TODO: Uncomment once stories can be referred to in a dependency.
 // This command will not be reliable until perf stories are in a package that can be set as a dep.
