f26edaba9dcafdbcf39e3dc645e480fad5d58723
Micah Godbolt <mgodbolt@microsoft.com>
2022-02-17 15:46:38 -0800

    another try

--- apps/vr-tests/src/utilities/types.ts
+++ apps/vr-tests/src/utilities/types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StoryApi, StoryName, StoryFn, StoryContext } from '@storybook/addons';
+import { StoryApi, StoryName, StoryFn, StoryContext, Parameters } from '@storybook/addons';
 
 /** Extra parameters provided by our addon (see `.storybook/preview.js`) */
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -26,5 +26,9 @@ export interface ExtendedStoryApi extends StoryApi<ExtendedStoryFnReturnType> {
     config?: AddStoryConfig,
   ) => ExtendedStoryApi;
 
-  add: (storyName: StoryName, storyFn: ExtendedStoryFn) => StoryApi<ExtendedStoryFnReturnType>;
+  add: (
+    storyName: StoryName,
+    storyFn: StoryFn<ExtendedStoryFnReturnType>,
+    parameters?: Parameters | undefined,
+  ) => StoryApi<ExtendedStoryFnReturnType>;
 }
