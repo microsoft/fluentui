d3f76f43ba23e11556f60fad1f081375274c4a6e
AtishayMsft <atishay.jain@microsoft.com>
2022-02-17 12:19:46 +0530

    Update comment length

--- packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
+++ packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
@@ -280,10 +280,13 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
       For this case there will be no callout updation for the event.
       This condition has been added to prevent repeated callout flashing.
       Currently there is a fraction of second delay between hover event and subsequent callout refresh.
-      In the meantime if another event is received, the callout continues to flash for the set of intermediate hover events.
+      In the meantime if another event is received, the callout continues to flash for the set of 
+      intermediate hover events.
 
-      This does not cause any issue as the user interaction takes atleast a fraction of second and the final callout state is ultimately achieved.
-      If a user performs very swift mouse maneuver, the intermediate events will be lost but the callout experience remains smooth.
+      This does not cause any issue as the user interaction takes atleast a fraction of second and the final 
+      callout state is ultimately achieved.
+      If a user performs very swift mouse maneuver, the intermediate events will be lost but the callout experience
+      remains smooth.
       */
     }
     if (!found) {
