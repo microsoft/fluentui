9886d37b8ef565cb4ca5a229961ef4cee83c4d25
Charles <chassunc@microsoft.com>
2022-02-21 10:13:09 -0300

    chore: remove other imports

--- packages/react-toolbar/src/components/Toolbar/ToolbarContext.tsx
+++ packages/react-toolbar/src/components/Toolbar/ToolbarContext.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { createContext } from '@fluentui/react-context-selector';
-import type { Context } from '@fluentui/react-context-selector';
 import type { ToolbarContextValue } from './Toolbar.types';
 
 export const ToolbarContext = React.createContext<ToolbarContextValue>({
