4907b251cf5076082e911e392ff9577da4511d6f
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-02-23 11:30:46 -0800

    refactor

--- scripts/monorepo/checkIfPackagesAffected.js
+++ scripts/monorepo/checkIfPackagesAffected.js
@@ -6,13 +6,21 @@ const FLUENTUI_REACT_NORTHSTAR = '@fluentui/react-northstar';
 const packages = [FLUENTUI_REACT, FLUENTUI_REACT_COMPONENTS, FLUENTUI_REACT_NORTHSTAR];
 
 const isPackageAffected = packagesToCheck => {
+  const affectedPackages = getAffectedPackages();
+
   for (const pkg of packagesToCheck) {
-    if (getAffectedPackages().has(pkg)) {
-      if (pkg === FLUENTUI_REACT || pkg === FLUENTUI_REACT_COMPONENTS) {
-        console.log(`##vso[task.setvariable variable=V8PackageAffected;isOutput=true]true`);
-      } else if (pkg === FLUENTUI_REACT_NORTHSTAR) {
-        console.log(`##vso[task.setvariable variable=NorthstarPackageAffected;isOutput=true]true`);
-      }
+    if (!affectedPackages.has(pkg)) {
+      continue;
+    }
+
+    if (pkg === FLUENTUI_REACT || pkg === FLUENTUI_REACT_COMPONENTS) {
+      console.log(`##vso[task.setvariable variable=V8PackageAffected;isOutput=true]true`);
+      continue;
+    }
+
+    if (pkg === FLUENTUI_REACT_NORTHSTAR) {
+      console.log(`##vso[task.setvariable variable=NorthstarPackageAffected;isOutput=true]true`);
+      continue;
     }
   }
 };
