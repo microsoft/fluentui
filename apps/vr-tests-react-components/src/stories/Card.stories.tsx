b1c023b534fe9f86f71028183c2f150c59c0b325
André <39736248+andrefcdias@users.noreply.github.com>
2022-02-23 18:23:29 +0100

    chore: migrate vr tests to proper app

--- apps/vr-tests-react-components/src/stories/Card.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Card.stories.tsx
@@ -2,16 +2,19 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Screener from 'screener-storybook/src/screener';
 import { Card, CardHeader, CardFooter } from '@fluentui/react-card';
-import { Open16Regular, Share16Regular } from '@fluentui/react-icons';
+import { MoreVerticalRegular, Open16Regular, Share16Regular } from '@fluentui/react-icons';
 import { Body, Caption } from '@fluentui/react-text';
 import { Button } from '@fluentui/react-button';
 import { action } from '@storybook/addon-actions';
+import { makeStyles, shorthands } from '@griffel/react';
+import { tokens } from '@fluentui/react-theme';
 
 const ASSET_URL = 'https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-card';
 
 const powerpointLogoURL = ASSET_URL + '/assets/powerpoint_logo.svg';
+const avatarMauricioURL = ASSET_URL + '/assets/avatar_mauricio.svg';
 
-const SampleCardContent = () => (
+const SampleAppearanceContent = () => (
   <>
     <CardHeader
       image={<img src={powerpointLogoURL} alt="Microsoft PowerPoint logo" />}
@@ -32,7 +35,37 @@ const SampleCardContent = () => (
   </>
 );
 
-storiesOf('Card Converged', module)
+const SampleScaleContent = () => (
+  <CardHeader
+    image={<img src={avatarMauricioURL} alt="Face of a person" />}
+    header={
+      <Body>
+        <b>Mauricio August</b> + 7 others edited
+      </Body>
+    }
+    description={<Caption>Artificial Intelligence Deck</Caption>}
+    action={<Button appearance="transparent" icon={<MoreVerticalRegular fontSize={20} />} />}
+  />
+);
+
+const useScaleStyles = makeStyles({
+  grid: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    ...shorthands.gap('24px'),
+  },
+  container: {
+    width: '500px',
+    height: '200px',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...shorthands.border('5px', 'solid', tokens.colorNeutralStrokeAccessible),
+  },
+});
+
+storiesOf('Card', module)
   .addDecorator(story => (
     <Screener steps={new Screener.Steps().snapshot('normal', { cropTo: '.testWrapper' }).end()}>
       <div className="testWrapper" style={{ width: '300px' }}>
@@ -47,25 +80,25 @@ storiesOf('Card Converged', module)
         <div>
           <h1>Filled</h1>
           <Card appearance="filled">
-            <SampleCardContent />
+            <SampleAppearanceContent />
           </Card>
         </div>
         <div>
           <h1>Filled alternative</h1>
           <Card appearance="filled-alternative">
-            <SampleCardContent />
+            <SampleAppearanceContent />
           </Card>
         </div>
         <div>
           <h1>Outline</h1>
           <Card appearance="outline">
-            <SampleCardContent />
+            <SampleAppearanceContent />
           </Card>
         </div>
         <div>
           <h1>Subtle</h1>
           <Card appearance="subtle">
-            <SampleCardContent />
+            <SampleAppearanceContent />
           </Card>
         </div>
       </div>
@@ -75,9 +108,73 @@ storiesOf('Card Converged', module)
       includeHighContrast: true,
       includeDarkMode: true,
     },
-  );
+  )
+  .addStory('scale', () => {
+    const styles = useScaleStyles();
 
-storiesOf('Card Converged', module)
+    return (
+      <div className={styles.grid}>
+        <div>
+          <h1>Default (fixed size)</h1>
+          <div className={styles.container}>
+            <Card style={{ width: '400px', height: '75px', justifyContent: 'center' }}>
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Auto width</h1>
+          <div className={styles.container}>
+            <Card scale="auto-width" style={{ height: '75px', justifyContent: 'center' }}>
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Auto height</h1>
+          <div className={styles.container}>
+            <Card scale="auto-height" style={{ width: '400px' }}>
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Auto</h1>
+          <div className={styles.container}>
+            <Card scale="auto">
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Fluid width</h1>
+          <div className={styles.container}>
+            <Card scale="fluid-width">
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Fluid height</h1>
+          <div className={styles.container}>
+            <Card scale="fluid-height" style={{ justifyContent: 'center' }}>
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <h1>Fluid</h1>
+          <div className={styles.container}>
+            <Card scale="fluid" style={{ justifyContent: 'center' }}>
+              <SampleScaleContent />
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+storiesOf('Card', module)
   .addDecorator(story => (
     <Screener
       steps={new Screener.Steps()
@@ -97,7 +194,7 @@ storiesOf('Card Converged', module)
     'appearance interactive - Filled',
     () => (
       <Card onClick={action('filled card clicked')} appearance="filled">
-        <SampleCardContent />
+        <SampleAppearanceContent />
       </Card>
     ),
     {
@@ -110,7 +207,7 @@ storiesOf('Card Converged', module)
     'appearance interactive - Filled Alternative',
     () => (
       <Card onClick={action('filled alternative card clicked')} appearance="filled-alternative">
-        <SampleCardContent />
+        <SampleAppearanceContent />
       </Card>
     ),
     {
@@ -123,7 +220,7 @@ storiesOf('Card Converged', module)
     'appearance interactive - Outline',
     () => (
       <Card onClick={action('outline card clicked')} appearance="outline">
-        <SampleCardContent />
+        <SampleAppearanceContent />
       </Card>
     ),
     {
@@ -136,7 +233,7 @@ storiesOf('Card Converged', module)
     'appearance interactive - Subtle',
     () => (
       <Card onClick={action('subtle card clicked')} appearance="subtle">
-        <SampleCardContent />
+        <SampleAppearanceContent />
       </Card>
     ),
     {
