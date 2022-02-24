80af4a7b1bb485927893c9ad5cb88fc833dbd209
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-02-24 15:31:54 -0800

    set output variables in pipeline

--- scripts/monorepo/checkIfPackagesAffected.js
+++ scripts/monorepo/checkIfPackagesAffected.js
@@ -1,30 +1,20 @@
 const getAffectedPackages = require('./getAffectedPackages');
 
-const FLUENTUI_REACT = '@fluentui/react';
-const FLUENTUI_REACT_COMPONENTS = '@fluentui/react-components';
-const FLUENTUI_REACT_NORTHSTAR = '@fluentui/react-northstar';
-const packages = [FLUENTUI_REACT, FLUENTUI_REACT_COMPONENTS, FLUENTUI_REACT_NORTHSTAR];
+const packagesToCheck = process.argv.filter(pkg => pkg.startsWith('--')).map(pkg => pkg.substring(2));
 
 const isPackageAffected = packagesToCheck => {
   const affectedPackages = getAffectedPackages();
 
   for (const pkg of packagesToCheck) {
-    if (!affectedPackages.has(pkg)) {
-      continue;
-    }
-
-    if (pkg === FLUENTUI_REACT || pkg === FLUENTUI_REACT_COMPONENTS) {
-      // this creates ADO variables which are used within condition checks
-      // @see https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands?view=azure-devops&tabs=bash
-      console.log(`##vso[task.setvariable variable=V8PackageAffected;isOutput=true]true`);
-      continue;
-    }
-
-    if (pkg === FLUENTUI_REACT_NORTHSTAR) {
-      console.log(`##vso[task.setvariable variable=NorthstarPackageAffected;isOutput=true]true`);
-      continue;
+    if (affectedPackages.has(pkg)) {
+      return true;
     }
   }
+  return false;
 };
 
-isPackageAffected(packages);
+/**
+ * In order for pipeline to capture and save the return value from a node script,
+ * the output needs to be printed.
+ */
+console.log(isPackageAffected(packagesToCheck));
