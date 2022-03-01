d2cdd82e8b9129e75d0926c738d282b29f8f98c4
ling1726 <lingfangao@hotmail.com>
2022-03-01 18:02:03 +0100

    Update packages/react-components/src/Concepts/Positioning/PositioningImperativePositionUpdate.stories.tsx
    Co-authored-by: André Dias (he/him) <39736248+andrefcdias@users.noreply.github.com>

--- packages/react-components/src/Concepts/Positioning/PositioningImperativePositionUpdate.stories.tsx
+++ packages/react-components/src/Concepts/Positioning/PositioningImperativePositionUpdate.stories.tsx
@@ -46,7 +46,7 @@ ImperativePositionUpdate.parameters = {
         'The `popperRef` positioning prop provides an [imperative handle](https://reactjs.org/docs/hooks-reference.html#useimperativehandle)',
         'to reposition the positioned element. This can be useful for scenarios where content is dynamically loaded.',
         '',
-        'In this example, move your mouse in the red boundary and the tooltip will follow the mouse cursor',
+        'In this example, you can move your mouse in the red boundary and the tooltip will follow the mouse cursor',
       ].join('\n'),
     },
   },
