134b28ade70a7ea4c0c46f541296efeb2182c36b
Ben Howell <behowell@microsoft.com>
2022-03-01 02:15:19 -0800

    Align Labeled to best practices for label text

--- packages/react-radio/src/stories/RadioGroupLabeled.stories.tsx
+++ packages/react-radio/src/stories/RadioGroupLabeled.stories.tsx
@@ -8,7 +8,7 @@ export const Labeled = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
       <Label id={labelId} required>
-        Pick an option:
+        Selected option
       </Label>
       <RadioGroup aria-labelledby={labelId}>
         <Radio value="A" label="Option A" />
@@ -24,7 +24,7 @@ Labeled.parameters = {
     description: {
       story:
         'Use the `aria-labelledby` property on RadioGroup to associate a label with the group.<br />' +
-        '**Note**: The `<label>` attribute `for` does _not_ work with RadioGroup, as the group is not ' +
+        '**Note**: The `<label>` attribute `htmlFor` does _not_ work with RadioGroup, as the group is not ' +
         'an input element. You must use `aria-labelledby` instead.',
     },
   },
