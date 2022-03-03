da9157a5a4c155aaae1f03895ebdf0474313b35e
Charles <chassunc@microsoft.com>
2022-03-03 13:42:00 -0300

    chore: fix typo

--- packages/fluentui/react-northstar/src/components/Carousel/utils.ts
+++ packages/fluentui/react-northstar/src/components/Carousel/utils.ts
@@ -8,14 +8,13 @@ export function getAnimationName({
 }) {
   const initialMounting = typeof dir === 'undefined';
 
-
   if (!initialMounting) {
     if (!active) {
       return dir === 'start' ? animationExitToPrev : animationExitToNext;
     }
 
     if (active) {
-      return = dir === 'start' ? animationEnterFromNext : animationEnterFromPrev;
+      return dir === 'start' ? animationEnterFromNext : animationEnterFromPrev;
     }
   }
 
