41b1727090b32c5d84f1e02c6d019a27ebf8e160
Lingfan Gao <lingfangao@hotmail.com>
2022-03-04 12:37:02 +0100

    init: @fluentui/priority-overflow

---
+++ packages/priority-overflow/src/debounce.ts
@@ -0,0 +1,20 @@
+/**
+ * Microtask debouncer
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTML_DOM_API/Microtask_guide
+ * @param fn - Function to debounce
+ * @returns debounced function
+ */
+export default function debounce(fn: Function) {
+  let pending: boolean;
+  return () => {
+    if (!pending) {
+      pending = true;
+      queueMicrotask(() => {
+        // Need to set pending to `false` before the debounced function is run.
+        // React can actually interrupt the function while it's running!
+        pending = false;
+        fn();
+      });
+    }
+  };
+}
