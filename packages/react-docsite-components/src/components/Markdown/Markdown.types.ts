61d1ec16271c2beff80f449c35b10e0b8a1817b5
Micah Godbolt <mgodbolt@microsoft.com>
2022-03-08 13:51:40 -0800

    Upgrade storybook to 6.4.x (#21780)
    * basic storybook upgrade without the new story store format
    
    * Change files
    
    * add storybook/html to satisfied ignore
    
    * move html back into WC
    
    * deduplicate deps
    
    * skip html in satisfied
    
    * update vr tests typings
    
    * update vr tests typings
    
    * remove params from add in storyApi
    
    * fix add storyFn type
    
    * another try
    
    * return type
    
    * back to legacy
    
    * knobs
    
    * fix lock
    
    * fix 2nd vr tests
    
    * add nohoist for storybook/router's react-router deps
    
    * Update markdown-to-jsx to version 7
    
    * theming-designer skipLibCheck
    
    * fix lint issue
    
    * fix typings in FluentDocsPage
    
    * fix story decorators
    
    * fix decorators on react components
    
    * add decorator to slider
    
    * remove temp test wrapper
    
    * remove dup wrapper in doc card
    
    * remove slider wrapper
    
    * change test wrappers
    
    * fix test widths and set waits on button tests
    
    * fix stories
    
    * use classes for test
    
    * use class for link tests
    
    * change link as button too
    
    * try id based selection for buttons
    
    * add a second wait
    
    * remove the delay and move a test around
    
    * give each failing test its own storiesOf
    
    * start migrating button over to CSF
    
    * remove extra decorator
    
    * update react versions
    
    * upgrade screener storybook
    
    * remove CSF exploration
    
    * lock file
    
    * comment out offending tests
    
    * remove a few more
    
    Co-authored-by: Elizabeth Craig <elcraig@microsoft.com>

--- packages/react-docsite-components/src/components/Markdown/Markdown.types.ts
+++ packages/react-docsite-components/src/components/Markdown/Markdown.types.ts
@@ -3,9 +3,10 @@ import { IMarkdownHeaderStyleProps } from './MarkdownHeader';
 import { IMarkdownParagraphStyleProps } from './MarkdownParagraph';
 import { IMarkdownCodeStyleProps } from './MarkdownCode';
 import { IMarkdownTableStyleProps } from '../MarkdownTable/index';
-import { MarkdownOptions } from 'markdown-to-jsx';
+import { MarkdownToJSX } from 'markdown-to-jsx';
 
 export interface IMarkdownProps {
+  /** CSS class to apply to the component root */
   className?: string;
 
   /**
@@ -15,7 +16,7 @@ export interface IMarkdownProps {
   enableRenderHtmlBlock?: boolean;
 
   /** Additional component overrides for markdown rendering */
-  overrides?: MarkdownOptions['overrides'];
+  overrides?: MarkdownToJSX.Overrides;
 
   /** Theme provided by higher-order component. */
   theme?: ITheme;
