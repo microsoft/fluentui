076fb57cc64603de5e02caa4ad0fd8f527f8996d
Micah Godbolt <mgodbolt@microsoft.com>
2022-03-08 09:28:24 -0800

    add a styled demo to tooltip

---
+++ packages/react-tooltip/src/stories/TooltipStyled.stories.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import { Tooltip, TooltipProps } from '../Tooltip';
+import { Button } from '@fluentui/react-button';
+import { SlideTextRegular } from '@fluentui/react-icons';
+import { makeStyles } from '@griffel/react';
+
+const getStyles = makeStyles({
+  toolTip: {
+    backgroundColor: 'green',
+    color: 'white',
+  },
+});
+
+export const Styled = (props: Partial<TooltipProps>) => {
+  const styles = getStyles();
+  return (
+    <Tooltip
+      withArrow
+      content={{ children: 'Example tooltip', className: styles.toolTip }}
+      relationship="label"
+      {...props}
+    >
+      <Button icon={<SlideTextRegular />} size="large" />
+    </Tooltip>
+  );
+};
+
+Styled.parameters = {
+  docs: {
+    description: {
+      story: `To style a tooltip, classNames must be passed through the content slot.`,
+    },
+  },
+};
