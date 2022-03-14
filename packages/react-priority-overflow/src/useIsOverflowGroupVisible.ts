cb099aa8c08c3d67feec3f007a446065c7f7bd53
Lingfan Gao <lingfangao@hotmail.com>
2022-03-14 11:25:47 +0100

    nit

--- packages/react-priority-overflow/src/useIsOverflowGroupVisible.ts
+++ packages/react-priority-overflow/src/useIsOverflowGroupVisible.ts
@@ -1,9 +1,10 @@
+import { OverflowGroupState } from '@fluentui/priority-overflow';
 import { useOverflowContext } from './overflowContext';
 
 /**
  * @param id - unique identifier for a group of overflow items
  * @returns visibility state of the group
  */
-export function useIsOverflowGroupVisible(id: string | number): boolean {
-  return !!useOverflowContext(ctx => ctx.groupVisibility[id + '']);
+export function useIsOverflowGroupVisible(id: string | number): OverflowGroupState {
+  return useOverflowContext(ctx => ctx.groupVisibility[id + '']);
 }
