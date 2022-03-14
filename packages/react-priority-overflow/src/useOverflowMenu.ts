b38ec75129268fb982cae950236e4e47310c80a1
Lingfan Gao <lingfangao@hotmail.com>
2022-03-14 12:27:25 +0100

    useId in useOverflowMenu

--- packages/react-priority-overflow/src/useOverflowMenu.ts
+++ packages/react-priority-overflow/src/useOverflowMenu.ts
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { useIsomorphicLayoutEffect } from '@fluentui/react-utilities';
+import { useId, useIsomorphicLayoutEffect } from '@fluentui/react-utilities';
 import { useOverflowContext } from './overflowContext';
 import { useOverflowCount } from './useOverflowCount';
 import { DATA_OVERFLOW_MENU } from './constants';
 
-export function useOverflowMenu<TElement extends HTMLElement>(id = 'overflow-menu') {
+export function useOverflowMenu<TElement extends HTMLElement>(id?: string) {
+  const elementId = useId('overflow-menu', id);
   const overflowCount = useOverflowCount();
   const registerItem = useOverflowContext(v => v.registerItem);
   const updateOverflow = useOverflowContext(v => v.updateOverflow);
@@ -17,7 +18,7 @@ export function useOverflowMenu<TElement extends HTMLElement>(id = 'overflow-men
     if (element) {
       deregisterItem = registerItem({
         element,
-        id,
+        id: elementId,
         priority: Infinity,
       });
       element.setAttribute(DATA_OVERFLOW_MENU, '');
@@ -27,7 +28,7 @@ export function useOverflowMenu<TElement extends HTMLElement>(id = 'overflow-men
       element?.removeAttribute(DATA_OVERFLOW_MENU);
       deregisterItem();
     };
-  }, [registerItem, isOverflowing, id]);
+  }, [registerItem, isOverflowing, elementId]);
 
   React.useEffect(() => {
     if (isOverflowing) {
