4eb71b3e911bea7908895f5b41fed3412ec0fe12
KHMakoto <Humberto.Morimoto@microsoft.com>
2022-03-15 17:42:36 -0600

    Removing star exports from @fluentui/react-badge.

--- packages/react-badge/src/index.ts
+++ packages/react-badge/src/index.ts
@@ -1,5 +1,31 @@
-export * from './components/Badge/index';
-export * from './Badge';
-export * from './PresenceBadge';
-export * from './components/CounterBadge/index';
-export * from './CounterBadge';
+export {
+  Badge,
+  badgeClassName,
+  renderBadge_unstable,
+  useBadgeStyles_unstable,
+  useBadge_unstable,
+} from './components/Badge/index';
+export type { BadgeProps, BadgeSlots, BadgeState } from './components/Badge/index';
+export { Badge, badgeClassName, renderBadge_unstable, useBadgeStyles_unstable, useBadge_unstable } from './Badge';
+export type { BadgeProps, BadgeSlots, BadgeState } from './Badge';
+export {
+  PresenceBadge,
+  presenceBadgeClassName,
+  usePresenceBadgeStyles_unstable,
+  usePresenceBadge_unstable,
+} from './PresenceBadge';
+export type { PresenceBadgeProps, PresenceBadgeState, PresenceBadgeStatus } from './PresenceBadge';
+export {
+  CounterBadge,
+  counterBadgeClassName,
+  useCounterBadgeStyles_unstable,
+  useCounterBadge_unstable,
+} from './components/CounterBadge/index';
+export type { CounterBadgeProps, CounterBadgeState } from './components/CounterBadge/index';
+export {
+  CounterBadge,
+  counterBadgeClassName,
+  useCounterBadgeStyles_unstable,
+  useCounterBadge_unstable,
+} from './CounterBadge';
+export type { CounterBadgeProps, CounterBadgeState } from './CounterBadge';
