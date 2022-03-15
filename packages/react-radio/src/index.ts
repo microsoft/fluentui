c1bf998fba9db80554902cd1d2590263a513f8e0
Sean Monahan <seanmonahan@microsoft.com>
2022-03-15 13:11:58 -0700

    Remove star exports from react-radio
    Removes star exports from src/index.ts in the react-radio package.

--- packages/react-radio/src/index.ts
+++ packages/react-radio/src/index.ts
@@ -1,3 +1,12 @@
-export * from './RadioGroup';
-export * from './Radio';
-export * from './contexts/RadioGroupContext';
+export {
+  RadioGroup,
+  radioGroupClassName,
+  renderRadioGroup_unstable,
+  useRadioGroupStyles_unstable,
+  useRadioGroup_unstable,
+} from './RadioGroup';
+export type { RadioGroupOnChangeData, RadioGroupProps, RadioGroupSlots, RadioGroupState } from './RadioGroup';
+export { Radio, radioClassName, renderRadio_unstable, useRadioStyles_unstable, useRadio_unstable } from './Radio';
+export type { RadioProps, RadioSlots, RadioState } from './Radio';
+export { RadioGroupContext } from './contexts/RadioGroupContext';
+export type { RadioGroupContextValue } from './contexts/RadioGroupContext';
