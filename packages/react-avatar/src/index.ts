34f141b61bb0b392e9d3944f0f883038024bc994
KHMakoto <Humberto.Morimoto@microsoft.com>
2022-03-15 17:38:52 -0600

    Removing star exports from @fluentui/react-avatar.

--- packages/react-avatar/src/index.ts
+++ packages/react-avatar/src/index.ts
@@ -1,2 +1,3 @@
-export * from './Avatar';
-export * from './utils/index';
+export { Avatar, avatarClassName, renderAvatar_unstable, useAvatarStyles_unstable, useAvatar_unstable } from './Avatar';
+export type { AvatarNamedColor, AvatarProps, AvatarSlots, AvatarState } from './Avatar';
+export { getInitials } from './utils/index';
