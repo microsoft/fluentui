726cd1dce72e363e231922db8723a0f26826eff1
Martin Hochel <martinhochel@microsoft.com>
2022-03-15 18:22:35 +0100

    test(scripts): add beachball config creation coverage

---
+++ scripts/beachball/config.test.ts
@@ -0,0 +1,68 @@
+/// <reference types="jest" />
+
+import { config as sharedConfig } from './shared.config';
+import v8Config from './release-v8.config';
+import vNextConfig from './release-vNext.config';
+
+describe(`beachball configs`, () => {
+  it(`should generate shared config`, () => {
+    expect(sharedConfig).toEqual({
+      changehint: "Run 'yarn change' to generate a change file",
+      disallowedChangeTypes: ['major', 'prerelease'],
+      generateChangelog: true,
+      ignorePatterns: [
+        '**/*.{shot,snap}',
+        '**/*.{test,spec}.{ts,tsx}',
+        '**/*.stories.tsx',
+        '**/.eslintrc.*',
+        '**/__fixtures__/**',
+        '**/__mocks__/**',
+        '**/common/isConformant.ts',
+        '**/jest.config.js',
+        '**/SPEC*.md',
+        '**/tests/**',
+      ],
+      scope: ['!packages/fluentui/*'],
+      tag: 'latest',
+      changelog: {
+        customRenderers: {
+          renderEntry: expect.any(Function),
+          renderHeader: expect.any(Function),
+        },
+      },
+    });
+  });
+
+  it(`should generate v8 release config`, () => {
+    expect(v8Config.scope).toEqual(
+      expect.arrayContaining([
+        '!packages/fluentui/*',
+        '!apps/perf-test-react-components',
+        '!apps/vr-tests-react-components',
+      ]),
+    );
+    expect(v8Config.scope.some(scope => scope.startsWith('!packages/react-'))).toBe(true);
+    expect(v8Config.changelog).toEqual(sharedConfig.changelog);
+  });
+
+  it(`should generate vNext release config`, () => {
+    const specialPackages = ['!packages/fluentui/*', 'apps/perf-test', 'apps/vr-tests'];
+
+    expect(vNextConfig.scope).toEqual(
+      expect.arrayContaining([...specialPackages, 'apps/perf-test-react-components', 'apps/vr-tests-react-components']),
+    );
+
+    expect(vNextConfig.scope.some(scope => scope.startsWith('packages/react-'))).toBe(true);
+
+    const includeScopes = vNextConfig.scope.filter(scope => !specialPackages.includes(scope));
+
+    expect(vNextConfig.changelog.customRenderers).toEqual(sharedConfig.changelog.customRenderers);
+    expect(vNextConfig.changelog.groups).toEqual([
+      {
+        changelogPath: 'packages/react-components',
+        masterPackageName: '@fluentui/react-components',
+        include: includeScopes,
+      },
+    ]);
+  });
+});
