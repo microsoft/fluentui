0b582572ff3f886cc0fe55f76aab1c57ee826dc2
Martin Hochel <martinhochel@microsoft.com>
2022-03-16 11:59:25 +0100

    refactor: autofix no export star at index file for cxe-prg packages

--- packages/react-dialog/src/index.ts
+++ packages/react-dialog/src/index.ts
@@ -1,3 +1,11 @@
 // TODO: replace with real exports
 export {};
-export * from './Dialog';
+export {
+  Dialog,
+  dialogClassName,
+  dialogClassNames,
+  renderDialog_unstable,
+  useDialogStyles_unstable,
+  useDialog_unstable,
+} from './Dialog';
+export type { DialogProps, DialogSlots, DialogState } from './Dialog';
