6bee76a70a7490557cd2b5c2b5639ab6bed2e6a1
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-03-16 13:31:33 -0600

    Removing star exports from @fluentui/react-button (#22111)
    * Removing star exports from @fluentui/react-button.
    
    * Adding change files.

--- packages/react-button/src/index.ts
+++ packages/react-button/src/index.ts
@@ -1,5 +1,45 @@
-export * from './Button';
-export * from './CompoundButton';
-export * from './MenuButton';
-export * from './SplitButton';
-export * from './ToggleButton';
+export {
+  Button,
+  buttonClassName,
+  buttonClassNames,
+  renderButton_unstable,
+  useButtonStyles_unstable,
+  useButton_unstable,
+} from './Button';
+export type { ButtonProps, ButtonSlots, ButtonState } from './Button';
+export {
+  CompoundButton,
+  compoundButtonClassName,
+  compoundButtonClassNames,
+  renderCompoundButton_unstable,
+  useCompoundButtonStyles_unstable,
+  useCompoundButton_unstable,
+} from './CompoundButton';
+export type { CompoundButtonProps, CompoundButtonSlots, CompoundButtonState } from './CompoundButton';
+export {
+  MenuButton,
+  menuButtonClassName,
+  menuButtonClassNames,
+  renderMenuButton_unstable,
+  useMenuButtonStyles_unstable,
+  useMenuButton_unstable,
+} from './MenuButton';
+export type { MenuButtonProps, MenuButtonSlots, MenuButtonState } from './MenuButton';
+export {
+  SplitButton,
+  renderSplitButton_unstable,
+  splitButtonClassName,
+  splitButtonClassNames,
+  useSplitButtonStyles_unstable,
+  useSplitButton_unstable,
+} from './SplitButton';
+export type { SplitButtonProps, SplitButtonSlots, SplitButtonState } from './SplitButton';
+export {
+  ToggleButton,
+  renderToggleButton_unstable,
+  toggleButtonClassName,
+  toggleButtonClassNames,
+  useToggleButtonStyles_unstable,
+  useToggleButton_unstable,
+} from './ToggleButton';
+export type { ToggleButtonProps, ToggleButtonState } from './ToggleButton';
