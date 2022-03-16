1942c0a0da76cb65991fbbb02f25750e5d21308a
Martin Hochel <martinhochel@microsoft.com>
2022-03-16 14:25:18 +0100

    fixup! refactor: autofix no export star at index file for cxe-prg packages

--- packages/react-dialog/src/index.ts
+++ packages/react-dialog/src/index.ts
@@ -1,7 +1,6 @@
-// TODO: replace with real exports
-export {};
 export {
   Dialog,
+  // eslint-disable-next-line deprecation/deprecation -- https://github.com/microsoft/fluentui/pull/21960#issuecomment-1068991851
   dialogClassName,
   dialogClassNames,
   renderDialog_unstable,
