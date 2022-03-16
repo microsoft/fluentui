d0cf279eb3b935f134a55d1273ebcfa370e708ac
Martin Hochel <martinhochel@microsoft.com>
2022-03-16 21:26:25 +0100

    style: fix lint errors

--- scripts/tasks/eslint.ts
+++ scripts/tasks/eslint.ts
@@ -6,7 +6,7 @@ export const eslint = eslintTask({
   // TODO: also lint config files?
   files: [path.join(process.cwd(), constants.directory)],
   extensions: constants.extensions,
-  cache: true, // only lint files changed since last lint
+  // cache: true, // only lint files changed since last lint
   fix: process.argv.includes('--fix'),
   // If requested, display a table with 10 slowest rules after running.
   // (to display more, edit the `slice` call in the `display` function of node_modules/eslint/lib/linter/timing.js)
