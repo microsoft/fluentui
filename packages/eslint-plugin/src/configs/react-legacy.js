36d650c034c8246db815607e5bf72343a7b59a7c
Martin Hochel <martinhochel@microsoft.com>
2022-03-16 19:20:29 +0100

    feat: create core base for react/node/legacy lint configs

--- packages/eslint-plugin/src/configs/react-legacy.js
+++ packages/eslint-plugin/src/configs/react-legacy.js
@@ -5,10 +5,10 @@ const configHelpers = require('../utils/configHelpers');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [path.join(__dirname, 'react')],
+  extends: [path.join(__dirname, 'core'), path.join(__dirname, 'react-config')],
 
   rules: {
-    ...configHelpers.getNamingConventionRule(true /* prefixWithI */),
+    ...configHelpers.getNamingConventionRule(true),
     'jsdoc/check-tag-names': 'off',
   },
 };
