f7dd4e16f0cc3089188429a2598529cc51cc04a8
Micah Godbolt <mgodbolt@microsoft.com>
2022-03-17 10:46:21 -0700

    fix stories with * export

--- packages/react-input/src/stories/Input.stories.tsx
+++ packages/react-input/src/stories/Input.stories.tsx
@@ -3,16 +3,16 @@ import { Meta } from '@storybook/react';
 import { Input } from '../index';
 
 import descriptionMd from './InputDescription.md';
-export * from './InputDefault.stories';
-export * from './InputAppearance.stories';
-export * from './InputContentBeforeAfter.stories';
-export * from './InputDisabled.stories';
-export * from './InputInline.stories';
-export * from './InputPlaceholder.stories';
-export * from './InputSize.stories';
-export * from './InputType.stories';
-export * from './InputUncontrolled.stories';
-export * from './InputControlled.stories';
+export { Default } from './InputDefault.stories';
+export { Appearance } from './InputAppearance.stories';
+export { ContentBeforeAfter } from './InputContentBeforeAfter.stories';
+export { Disabled } from './InputDisabled.stories';
+export { Inline } from './InputInline.stories';
+export { Placeholder } from './InputPlaceholder.stories';
+export { Size } from './InputSize.stories';
+export { Type } from './InputType.stories';
+export { Uncontrolled } from './InputUncontrolled.stories';
+export { Controlled } from './InputControlled.stories';
 
 const meta: Meta = {
   title: 'Preview Components/Input',
