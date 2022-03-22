ae9e8f52ad045aa8f6616eb30b46dd02adcddf53
Charles <chassunc@microsoft.com>
2022-03-21 21:36:26 -0300

    chore: reuse components in toolbar

--- packages/react-toolbar/src/components/ToolbarDivider/ToolbarDivider.tsx
+++ packages/react-toolbar/src/components/ToolbarDivider/ToolbarDivider.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { useToolbarDivider_unstable } from './useToolbarDivider';
 import { renderToolbarDivider_unstable } from './renderToolbarDivider';
 import { useToolbarDividerStyles_unstable } from './useToolbarDividerStyles';
 import type { ToolbarDividerProps } from './ToolbarDivider.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useDivider_unstable } from '@fluentui/react-divider';
 
 /**
  * ToolbarDivider component
  */
 export const ToolbarDivider: ForwardRefComponent<ToolbarDividerProps> = React.forwardRef((props, ref) => {
-  const state = useToolbarDivider_unstable(props, ref);
+  const state = useDivider_unstable({ vertical: true, ...props }, ref);
   useToolbarDividerStyles_unstable(state);
   return renderToolbarDivider_unstable(state);
 });
