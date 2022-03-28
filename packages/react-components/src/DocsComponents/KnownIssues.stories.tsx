67cb97df3ad535612437f6247c8b034f5494405e
admin <peter@draxler.ml>
2022-03-28 16:26:49 +0200

    fix property name

--- packages/react-components/src/DocsComponents/KnownIssues.stories.tsx
+++ packages/react-components/src/DocsComponents/KnownIssues.stories.tsx
@@ -111,7 +111,7 @@ export const KnownIssues: React.FC<{ componentName: string }> = ({ componentName
                 >
                   <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
                   />
                 </svg>
