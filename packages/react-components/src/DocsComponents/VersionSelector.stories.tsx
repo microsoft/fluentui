82cfc4a50c4e9e37caccb2c6e8caa83ff48c7bd4
admin <peter@draxler.ml>
2022-03-30 17:29:51 +0200

    sorting, version extraction

--- packages/react-components/src/DocsComponents/VersionSelector.stories.tsx
+++ packages/react-components/src/DocsComponents/VersionSelector.stories.tsx
@@ -11,6 +11,7 @@ import {
   MenuItem,
 } from '../index';
 import { version as packageJsonVersion } from '../../package.json';
+import * as semver from 'semver';
 
 const useStyles = makeStyles({
   menuButton: {
@@ -37,12 +38,8 @@ const useStyles = makeStyles({
 });
 
 const onCheckedValueChange: MenuProps['onCheckedValueChange'] = (e, data) => {
-  const selectedUrl = data.checkedItems[0] + window.location.search;
-  if (window.top) {
-    window.top.location.href = selectedUrl;
-  } else {
-    window.location.href = selectedUrl;
-  }
+  const selectedUrl = data.checkedItems[0] + (window.top || window).location.search;
+  (window.top || window).location.href = selectedUrl;
 };
 
 /**
@@ -58,8 +55,23 @@ export const VersionSelector: React.FC = () => {
     // https://master--5ccbc373887ca40020446347.chromatic.com/metadata.json
     fetch('/metadata.json').then(async response => {
       if (response.ok) {
-        const metadata = await response.json();
-        setVersions(Object.entries(metadata.versions));
+        const versionsCleaned: string[][] = [];
+        const tagUrlMap: { [key: string]: string } = (await response.json()).versions;
+
+        Object.entries(tagUrlMap).forEach(([gitTag, url]) => {
+          // The metadata.json contains mapping from git tags to published docs URLs.
+          // Our git tags have the following format in v9: @fluentui/react-icons_v0.4.3
+          //
+          // We try to extract just the version number (e.g. 0.4.3), so only the number
+          // would be visible in the version selector. As a fallback, we use the whole tag.
+
+          const versionDirty = gitTag.split('_').pop() ?? '';
+          const versionClean = semver.clean(versionDirty, true);
+          versionsCleaned.push([versionClean ?? gitTag, url]);
+        });
+
+        const versionsSorted = versionsCleaned.sort(([a], [b]) => semver.compare(b, a));
+        setVersions(versionsSorted);
       }
     });
   }, []);
