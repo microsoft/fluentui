17a6bca86191e5734ce200b70180262a4b0c3964
Tomi Olubeko <ololubek@microsoft.com>
2022-03-31 15:30:10 -0700

    Add svg className

--- packages/react-spinner/src/components/Spinner/DefaultSvg.tsx
+++ packages/react-spinner/src/components/Spinner/DefaultSvg.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export const DefaultSvg = () => (
-  <svg role="progressbar" /*className="fui-Spinner__Progressbar"*/>
+  <svg role="progressbar" className="fui-Spinner__Progressbar">
     <circle className="fui-Spinner__Track" />
     <circle className="fui-Spinner__Tail" />
   </svg>
