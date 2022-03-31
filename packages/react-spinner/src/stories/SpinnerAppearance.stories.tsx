68f30dd16b6bec2ce54355f6786e95d1a6da2093
Tomi Olubeko <ololubek@microsoft.com>
2022-03-30 17:45:46 -0700

    Update appearance story

--- packages/react-spinner/src/stories/SpinnerAppearance.stories.tsx
+++ packages/react-spinner/src/stories/SpinnerAppearance.stories.tsx
@@ -1,28 +1,21 @@
 import * as React from 'react';
-import { useId } from '@fluentui/react-utilities';
-import { Label } from '@fluentui/react-label';
 import { makeStyles, shorthands } from '@griffel/react';
 import { Spinner } from '../index';
 
 const useStyles = makeStyles({
   container: {
     '> div': { ...shorthands.padding('20px') },
-    '& label': { display: 'block', marginBottom: '10px' },
   },
 });
 
 export const Appearance = () => {
-  const invertedId = useId('inverted');
-  const primaryId = useId('primary');
   const styles = useStyles();
 
   return (
     <div className={styles.container}>
-      <Label htmlFor={primaryId}>Primary Appearance</Label>
-      <Spinner appearance="primary" id={primaryId} />
+      <Spinner appearance="primary" label="Primary Spinner" />
 
-      <Label htmlFor={invertedId}>Inverted Appearance</Label>
-      <Spinner appearance="inverted" id={invertedId} />
+      <Spinner appearance="inverted" label="Inverted Spinner" />
     </div>
   );
 };
