f7a4cdc96b70042909d6d28b806e924c26431016
AtishayMsft <atishay.jain@microsoft.com>
2022-04-01 16:30:59 +0530

    Add sankey chart to documentation

--- packages/react-examples/src/react-charting/SankeyChart/SankeyChartPage.tsx
+++ packages/react-examples/src/react-charting/SankeyChart/SankeyChartPage.tsx
@@ -11,7 +11,7 @@ import { SankeyChartBasicExample } from './SankeyChart.Basic.Example';
 
 const SankeyChartBasicExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/SankeyChart/SankeyChart.Basic.Example.tsx') as string;
 
-export class PieChartPage extends React.Component<IComponentDemoPageProps, {}> {
+export class SankeyChartPage extends React.Component<IComponentDemoPageProps, {}> {
   public render(): JSX.Element {
     return (
       <ComponentPage
@@ -32,6 +32,16 @@ export class PieChartPage extends React.Component<IComponentDemoPageProps, {}> {
           />
         }
         isHeaderVisible={this.props.isHeaderVisible}
+        overview={
+          <div>
+            <p>
+              A sankey diagram is a visualization used to depict a flow from one set of values to another. The things
+              being connected are called nodes and the connections are called links. Sankeys are best used when you want
+              to show a many-to-many mapping between two domains (e.g., universities and majors) or multiple paths
+              through a set of stages
+            </p>
+          </div>
+        }
       />
     );
   }
