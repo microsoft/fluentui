a1e388cc3138b7dd7eaa0f454393954f55202fa2
Martin Hochel <martinhochel@microsoft.com>
2022-04-04 15:57:14 +0200

    fixup! try stuff - ast in progress

--- tools/generators/migrate-v8-pkg/lib/utils.ts
+++ tools/generators/migrate-v8-pkg/lib/utils.ts
@@ -1,70 +1,52 @@
-// use this module to define any kind of generic utilities that are used in more than 1 place within the generator implementation
-
-import * as ts  from "typescript";
-
-export function dummyHelper() {
-  return;
-}
-
-
+import { stripIndents } from '@nrwl/devkit';
+import * as ts from 'typescript';
 
 /**
- * Should be used to get the jest config object.
+ * Should be used to get the tooling config object when JS configuration is used
  *
  * @param host
  * @param path
  */
-export function eslintConfigObjectAst(
-  fileContent: string
-): ts.ObjectLiteralExpression {
-
-  const sourceFile = ts.createSourceFile(
-    '.eslintrc.js',
-    fileContent,
-    ts.ScriptTarget.Latest,
-    true
-  );
+export function getCjsConfigObjectAst(fileContent: string): ts.ObjectLiteralExpression {
+  const sourceFile = ts.createSourceFile('file-config.js', fileContent, ts.ScriptTarget.Latest, true);
 
   const moduleExportsStatement = sourceFile.statements.find(
-    (statement) =>
+    statement =>
       ts.isExpressionStatement(statement) &&
       ts.isBinaryExpression(statement.expression) &&
       statement.expression.left.getText() === 'module.exports' &&
-      statement.expression.operatorToken.kind === ts.SyntaxKind.EqualsToken
+      statement.expression.operatorToken.kind === ts.SyntaxKind.EqualsToken,
   );
 
-  const moduleExports = (moduleExportsStatement as ts.ExpressionStatement)
-    .expression as ts.BinaryExpression;
+  const moduleExports = (moduleExportsStatement as ts.ExpressionStatement).expression as ts.BinaryExpression;
 
   if (!moduleExports) {
     throw new Error(
-      `
-       The provided jest config file does not have the expected 'module.exports' expression.
-       See https://jestjs.io/docs/en/configuration for more details.`
+      stripIndents`
+       The provided config file does not have the expected 'module.exports' expression.
+      `,
     );
   }
 
   if (!ts.isObjectLiteralExpression(moduleExports.right)) {
-    throw new Error(
-      `The 'module.exports' expression is not an object literal.`
-    );
+    throw new Error(`The 'module.exports' expression is not an object literal.`);
   }
 
   return moduleExports.right as ts.ObjectLiteralExpression;
 }
 
+export function getASTconfigObjectProp(object: ts.ObjectLiteralExpression, propName: string) {
+  const extendsProperty = object.properties.find(value => {
+    if (!ts.isPropertyAssignment(value)) {
+      return;
+    }
+    if (!ts.isIdentifier(value.name)) {
+      return;
+    }
+    if (value.name.escapedText === propName) {
+      return true;
+    }
+  });
 
-export function getExtendsProp(object: ts.ObjectLiteralExpression){
-  // const extendsProperty = object.properties.find(value=>{
-  //   //  if(ts.isIdentifier(value) && value.text === 'extends'){
-
-  //   //    return tsNode.
-  //   //  }
-  //   if(ts.isPropertyName(value.name) === 'extends'){
-  //     return value
-  //   }
-  // })
-    // return ts.isIdentifier()
-
-    return ts.factory.createObjectLiteralExpression(object.properties)
+  return extendsProperty ? (extendsProperty as ts.PropertyAssignment).initializer.getText() : undefined;
 }
