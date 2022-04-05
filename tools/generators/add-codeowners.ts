46de5543ec99e761992ddb79b2a90086e9a4fac2
Martin Hochel <martinhochel@microsoft.com>
2022-04-05 18:16:07 +0200

    refactor: extract validation logic of codeowners modification

--- tools/generators/add-codeowners.ts
+++ tools/generators/add-codeowners.ts
@@ -12,8 +12,8 @@ export function addCodeowner(tree: Tree, options: Options) {
   processCodeowners(tree, { ...project, owner: options.owner });
 }
 
-function processCodeowners(tree: Tree, options: ReturnType<typeof getProjectConfig> & Pick<Options, 'owner'>) {
-  const placeholder = '# <%= NX-CODEOWNER-PLACEHOLDER %>';
+const placeholder = '# <%= NX-CODEOWNER-PLACEHOLDER %>';
+export function assertCodeownersSetup(tree: Tree) {
   const codeownersPath = joinPathFragments('/.github', 'CODEOWNERS');
 
   if (!tree.exists(codeownersPath)) {
@@ -22,6 +22,16 @@ function processCodeowners(tree: Tree, options: ReturnType<typeof getProjectConf
 
   const codeownersContent = tree.read(codeownersPath, 'utf8') as string;
   const placeholderPosition = codeownersContent.indexOf(placeholder);
+
+  if (placeholderPosition === -1) {
+    throw new Error(`CODEOWNERS is missing '${placeholder}' placeholder`);
+  }
+
+  return { content: codeownersContent, placeholderPosition, path: codeownersPath };
+}
+
+function processCodeowners(tree: Tree, options: ReturnType<typeof getProjectConfig> & Pick<Options, 'owner'>) {
+  const { content, placeholderPosition } = assertCodeownersSetup(tree);
 
   if (placeholderPosition === -1) {
     throw new Error(`CODEOWNERS is missing '${placeholder}' placeholder`);
@@ -45,7 +55,7 @@ function processCodeowners(tree: Tree, options: ReturnType<typeof getProjectConf
     },
   ];
 
-  const newContents = applyChangesToString(codeownersContent, changes);
+  const newContents = applyChangesToString(content, changes);
 
-  tree.write(codeownersPath, newContents);
+  tree.write(content, newContents);
 }
