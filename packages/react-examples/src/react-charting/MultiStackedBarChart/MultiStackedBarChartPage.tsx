3d68eb86d4c5e7fbfe4600653fa23795aa9a98cd
AtishayMsft <atishay.jain@microsoft.com>
2022-04-05 16:35:48 +0530

    Update multi stacked bar chart base page

--- packages/react-examples/src/react-charting/MultiStackedBarChart/MultiStackedBarChartPage.tsx
+++ packages/react-examples/src/react-charting/MultiStackedBarChart/MultiStackedBarChartPage.tsx
@@ -1,31 +1,46 @@
 import * as React from 'react';
-import { ComponentPage, ExampleCard } from '@fluentui/react-docsite-components';
+import {
+  ComponentPage,
+  ExampleCard,
+  IComponentDemoPageProps,
+  PropertiesTableSet,
+} from '@fluentui/react-docsite-components';
 
 import { MultiStackedBarChartExample } from './MultiStackedBarChart.Example';
 import { MultiStackedBarChartWithPlaceholderExample } from './MultiStackedBarChartWithPlaceHolder.Example';
 
-const MultiStackedBarChartExampleCode = require('!raw-loader?esModule=false!./MultiStackedBarChart.Example.tsx') as string;
-const MultiStackedBarChartWithPlaceholderExampleCode = require('!raw-loader?esModule=false!./MultiStackedBarChartWithPlaceHolder.Example.tsx') as string;
+const MultiStackedBarChartExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/MultiStackedBarChart/MultiStackedBarChart.Example.tsx') as string;
+const MultiStackedBarChartWithPlaceholderExampleCode = require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/MultiStackedBarChart/MultiStackedBarChartWithPlaceHolder.Example.tsx') as string;
 
-export const MultiStackedBarChartPage: React.FunctionComponent = () => {
-  return (
-    <ComponentPage
-      title="Multiple Stacked Bar Chart"
-      componentName="MultiStackedBarChart"
-      exampleCards={
-        <>
-          <ExampleCard title="Multi Stacked Bar Chart" code={MultiStackedBarChartExampleCode}>
-            <MultiStackedBarChartExample />
-          </ExampleCard>
-          <ExampleCard
-            title="Multi Stacked Bar Chart With Placeholder"
-            code={MultiStackedBarChartWithPlaceholderExampleCode}
-          >
-            <MultiStackedBarChartWithPlaceholderExample />
-          </ExampleCard>
-        </>
-      }
-      overview={<div />}
-    />
-  );
-};
+export class MultiStackedBarChartPage extends React.Component<IComponentDemoPageProps, {}> {
+  public render(): JSX.Element {
+    return (
+      <ComponentPage
+        title="Multiple Stacked Bar Chart"
+        componentName="MultiStackedBarChart"
+        exampleCards={
+          <>
+            <ExampleCard title="Multi Stacked Bar Chart" code={MultiStackedBarChartExampleCode}>
+              <MultiStackedBarChartExample />
+            </ExampleCard>
+            <ExampleCard
+              title="Multi Stacked Bar Chart With Placeholder"
+              code={MultiStackedBarChartWithPlaceholderExampleCode}
+            >
+              <MultiStackedBarChartWithPlaceholderExample />
+            </ExampleCard>
+          </>
+        }
+        propertiesTables={
+          <PropertiesTableSet
+            sources={[
+              require<string>('!raw-loader?esModule=false!@fluentui/react-charting/src/components/StackedBarChart/MultiStackedBarChart.types.ts'),
+            ]}
+          />
+        }
+        isHeaderVisible={this.props.isHeaderVisible}
+        overview={<div />}
+      />
+    );
+  }
+}
