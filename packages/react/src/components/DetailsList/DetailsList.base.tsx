9a2d895b555c67f9e1f3e89b8b1ced75cfc0b47a
Sarah Higley <sarah.higley@microsoft.com>
2022-04-08 13:58:32 -0700

    fix ariaLabel and ariaLabelForGrid weirdness, add deprecation warning for shouldApplyApplicationRole

--- packages/react/src/components/DetailsList/DetailsList.base.tsx
+++ packages/react/src/components/DetailsList/DetailsList.base.tsx
@@ -659,20 +659,19 @@ const DetailsListInner: React.ComponentType<IDetailsListInnerProps> = (
   );
 
   return (
-    // If shouldApplyApplicationRole is true, role application will be applied to make arrow keys work
-    // with JAWS.
     <div
       ref={rootRef}
       className={classNames.root}
       data-automationid="DetailsList"
       data-is-scrollable="false"
-      aria-label={ariaLabel}
       {...(shouldApplyApplicationRole ? { role: 'application' } : {})}
     >
       <FocusRects />
       <div
         role={role}
-        aria-label={ariaLabelForGrid}
+        // ariaLabel is a legacy prop that used to be applied on the root node, which has poor AT support
+        // it is now treated as a fallback to ariaLabelForGrid for legacy support
+        aria-label={ariaLabelForGrid || ariaLabel}
         aria-rowcount={isPlaceholderData ? -1 : rowCount}
         aria-colcount={colCount}
         aria-readonly="true"
