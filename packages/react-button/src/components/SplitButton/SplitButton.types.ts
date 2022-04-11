e118be251415c0d0abc3c9a6d23c66cc3f2ea572
KHMakoto <Humberto.Morimoto@microsoft.com>
2022-04-11 11:10:55 -0500

    Adding change files.

--- packages/react-button/src/components/SplitButton/SplitButton.types.ts
+++ packages/react-button/src/components/SplitButton/SplitButton.types.ts
@@ -3,7 +3,6 @@ import { MenuButton } from '../MenuButton/MenuButton';
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
 import type { ButtonProps, ButtonState } from '../Button/Button.types';
 import type { MenuButtonProps, MenuButtonState } from '../MenuButton/MenuButton.types';
-// import * as React from 'react';
 
 export type SplitButtonSlots = {
   /**
@@ -28,13 +27,3 @@ export type SplitButtonProps = ComponentProps<SplitButtonSlots> &
 export type SplitButtonState = ComponentState<SplitButtonSlots> &
   Omit<ButtonState, 'components' | 'iconOnly' | 'root'> &
   Omit<MenuButtonState, 'components' | 'iconOnly' | 'root'>;
-
-// type MenuButtonSlot = SplitButtonSlots['menuButton'];
-// type TestType = ExtractSlotProps<SplitButtonSlots['menuButton']>;
-// type OmittedComponentType<T extends { as?: unknown }> = Omit<T, 'as'> & { as?: Exclude<T['as'],
-// React.ComponentType> };
-
-// export type As1 = TestType['as'];
-// export type As2 = OmittedComponentType<TestType>['as'];
-// export const test: React.ComponentType<TestType> = MenuButton;
-// export const test2: React.ComponentType<OmittedComponentType<TestType>> = MenuButton;
