e72718ca34dc69e1e89519fbb1a6b0a91984ecd2
Adam Samec <adam.samec@gmail.com>
2022-04-11 16:40:26 +0200

    Remove the password hint scenario for Tooltip component

--- packages/react-components/src/AccessibilityScenarios/Tooltip.stories.tsx
+++ packages/react-components/src/AccessibilityScenarios/Tooltip.stories.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 
 import { Button } from '@fluentui/react-button';
-import { Label } from '@fluentui/react-label';
-import { Tooltip, TooltipProps } from '@fluentui/react-tooltip';
+import { Tooltip } from '@fluentui/react-tooltip';
 
 // https://github.com/microsoft/fluentui/pull/18695#issuecomment-868432982
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -12,26 +11,8 @@ import { TextItalic24Regular, TextUnderline24Regular, TextBold24Regular } from '
 import { Scenario } from './utils';
 
 export const ButtonsWithTooltipAccessibilityScenario: React.FunctionComponent = () => {
-  const [tooltipVisible, setTooltipVisible] = React.useState(false);
-
-  const onVisibleChange: TooltipProps['onVisibleChange'] = (event, { visible }) => {
-    setTooltipVisible(visible);
-  };
-
   return (
     <Scenario pageTitle="Buttons with tooltip">
-      <h2>Tooltip as a password requirements</h2>
-      <Label htmlFor="password">Password</Label>
-      <Tooltip
-        relationship="label"
-        onVisibleChange={onVisibleChange}
-        visible={tooltipVisible}
-        content="Password must be at least 8 characters long, contain a capital letter, and a number."
-      >
-        <Button onClick={() => setTooltipVisible(visible => !visible)}>Password requirements</Button>
-      </Tooltip>
-      <input type="password" id="password" name="password" />
-
       <h2>Tooltips for text formatting icon-only buttons</h2>
       <Tooltip relationship="label" content="Make text bold">
         <Button icon={<TextBold24Regular />} />
