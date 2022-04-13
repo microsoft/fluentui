0ff83f5313f01f6c97be0d9035eeae41cb2fa5b1
tomi-msft <66456876+tomi-msft@users.noreply.github.com>
2022-04-13 13:05:39 -0700

    Update initializeIcons to add user defined URL in argument (v7) (#22415)
    * Cherrypick update initializeIcons call
    
    * Change files
    
    * Add FabricConfig existence check
    
    * Use useWindow to check for window in server environment
    
    * use getWindow()
    
    * Update change/@uifabric-icons-388eaadd-8734-4c42-a630-e91ca6353c2f.json
    
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>
    
    * Update packages/icons/src/index.ts
    
    Co-authored-by: Sean Monahan <seanmonahan@microsoft.com>
    
    * Remove tsignore
    
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>
    Co-authored-by: Sean Monahan <seanmonahan@microsoft.com>

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -20,9 +20,23 @@ import { initializeIcons as i17 } from './fabric-icons-17';
 
 import { IIconOptions } from '@uifabric/styling';
 import { registerIconAliases } from './iconAliases';
+import { getWindow } from '@uifabric/utilities';
 const DEFAULT_BASE_URL = 'https://spoppe-b.azureedge.net/files/fabric-cdn-prod_20210407.001/assets/icons/';
 
-export function initializeIcons(baseUrl: string = DEFAULT_BASE_URL, options?: IIconOptions): void {
+declare global {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  interface Window {
+    FabricConfig?: {
+      fontBaseUrl?: string;
+    };
+  }
+}
+
+const win = getWindow();
+export function initializeIcons(
+  baseUrl: string = win?.FabricConfig?.fontBaseUrl ?? DEFAULT_BASE_URL,
+  options?: IIconOptions,
+): void {
   [
     i,
     i0,
