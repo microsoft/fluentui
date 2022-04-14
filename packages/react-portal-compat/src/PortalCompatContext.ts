949c95948d26294067a940b8edfb4c104f68a16e
Oleksandr Fediashov <olfedias@microsoft.com>
2022-04-14 12:32:10 +0200

    fix lint issue

--- packages/react-portal-compat/src/PortalCompatContext.ts
+++ packages/react-portal-compat/src/PortalCompatContext.ts
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { RegisterPortalFn } from './types';
 
-export const PortalCompatContext = React.createContext<RegisterPortalFn>(() => () => {});
+export const PortalCompatContext = React.createContext<RegisterPortalFn>(
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  () => () => {},
+);
 
 export function usePortalCompat() {
   return React.useContext(PortalCompatContext);
