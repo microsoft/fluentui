9c8592decacb9f7b1dc608a03c9f991918806008
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-04-19 13:34:13 -0500

    Re-exporting commonly used things from @fluentui/react-tabster and @fluentui/react-utilities in @fluentui/react-components (#22367)
    * Re-exporting everything from @fluentui/react-tabster and @fluentui/react-utilities in @fluentui/react-components.
    
    * Adding change files.
    
    * Adding missing dependency.
    
    * Cutting back on the things exported by react-components.
    
    * Cutting back on more things exported by react-components.
    
    * Cutting back on more things exported by react-components.
    
    * Cutting back on more things exported by react-components.

--- packages/react-tabster/src/index.ts
+++ packages/react-tabster/src/index.ts
@@ -1,1 +1,18 @@
-export * from './hooks/index';
+export {
+  createCustomFocusIndicatorStyle,
+  createFocusOutlineStyle,
+  useArrowNavigationGroup,
+  useFocusableGroup,
+  useFocusFinders,
+  useKeyboardNavAttribute,
+  useModalAttributes,
+  useTabsterAttributes,
+} from './hooks/index';
+export type {
+  CreateFocusIndicatorStyleRuleOptions,
+  FocusOutlineOffset,
+  FocusOutlineStyleOptions,
+  UseArrowNavigationGroupOptions,
+  UseFocusableGroupOptions,
+  UseModalAttributesOptions,
+} from './hooks/index';
