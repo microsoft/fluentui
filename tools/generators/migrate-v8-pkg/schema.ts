4ea0368952d16e98af729fdfe9144e0ee8a2f9aa
Martin Hochel <martinhochel@microsoft.com>
2022-04-20 19:28:47 +0200

    docs(tools): udpate schema docs

--- tools/generators/migrate-v8-pkg/schema.ts
+++ tools/generators/migrate-v8-pkg/schema.ts
@@ -1,6 +1,6 @@
 export interface MigrateV8PkgGeneratorSchema {
   /**
-   * Library name
+   * Library name or comma delimited library names to execute migration on multiple libraries.
    */
   name?: string;
   /**
