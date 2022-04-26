bf2970a1ed078128202555edd1eed818761475dc
André <39736248+andrefcdias@users.noreply.github.com>
2022-04-26 19:08:29 +0200

    chore: add documentation to `focusable`

--- packages/react-card/src/components/Card/Card.types.ts
+++ packages/react-card/src/components/Card/Card.types.ts
@@ -6,6 +6,21 @@ export type CardSlots = {
 
 export type CardCommons = {
   appearance: 'filled' | 'filled-alternative' | 'outline' | 'subtle';
+
+  /**
+   * Sets the focus behavior for the card. If `true`, the card will use the `noTab` focus behavior.
+   *
+   * `noTab`
+   * This behaviour traps the focus inside of the Card when pressing the Enter key and will only release focus when pressing the Escape key.
+   *
+   * `tabExit`
+   * This behaviour traps the focus inside of the Card when pressing the Enter key but will release focus when pressing the Tab key on the last inner element.
+   *
+   * `tabOnly`
+   * This behaviour will cycle through all elements inside of the Card when pressing the Tab key and then release focus after the last inner element.
+   *
+   * @defaultvalue false
+   */
   focusable: boolean | 'no-tab' | 'tab-exit' | 'tab-only';
 };
 
