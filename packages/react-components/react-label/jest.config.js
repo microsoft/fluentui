efd366b04be1ce0b44b55dbf77cf94a429091662
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-04-28 06:50:34 -0700

    move react-label

--- packages/react-components/react-label/jest.config.js
+++ packages/react-components/react-label/jest.config.js
@@ -5,7 +5,7 @@
  */
 module.exports = {
   displayName: 'react-label',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
