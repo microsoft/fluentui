39406a4e0022849982202615e9e21f137fbc748f
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-04-28 07:00:39 -0700

    move react-provider

--- packages/react-components/react-provider/jest.config.js
+++ packages/react-components/react-provider/jest.config.js
@@ -5,7 +5,7 @@
  */
 module.exports = {
   displayName: 'react-provider',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
