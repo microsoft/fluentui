55d617650a9bb0bce741f1816b2c595d03fcf95a
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-05-02 15:58:00 -0700

    Update packages/react-components/react-link/src/stories/LinkInline.stories.tsx
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>

--- packages/react-components/react-link/src/stories/LinkInline.stories.tsx
+++ packages/react-components/react-link/src/stories/LinkInline.stories.tsx
@@ -3,7 +3,7 @@ import { Link } from '../index';
 
 export const Inline = () => (
   <div>
-    This is an{' '}
+    This is an
     <Link href="https://www.bing.com" inline>
       inline link
     </Link>
