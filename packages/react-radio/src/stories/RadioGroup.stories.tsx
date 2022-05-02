47a859111d7d53fd16d5cfe18506fbb6161b72c1
Sean Monahan <seanmonahan@microsoft.com>
2022-05-02 15:39:34 -0700

    react-radio: update labels in examples

--- packages/react-radio/src/stories/RadioGroup.stories.tsx
+++ packages/react-radio/src/stories/RadioGroup.stories.tsx
@@ -8,6 +8,7 @@ export { Horizontal } from './RadioGroupHorizontal.stories';
 export { HorizontalStacked } from './RadioGroupHorizontalStacked.stories';
 export { ControlledValue } from './RadioGroupControlledValue.stories';
 export { UncontrolledValue } from './RadioGroupUncontrolledValue.stories';
+export { Required } from './RadioGroupRequired.stories';
 export { Disabled } from './RadioGroupDisabled.stories';
 export { DisabledItem } from './RadioGroupDisabledItem.stories';
 export { LabelSubtext } from './RadioGroupLabelSubtext.stories';
