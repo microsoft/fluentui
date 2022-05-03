28c08c1e1e9c6feaf13c5df54a03bc15bf00ae9e
KHMakoto <humberto_makoto@hotmail.com>
2022-05-03 14:24:02 -0700

    Removing star exports from @fluentui/react-select.

--- packages/react-components/react-select/src/index.ts
+++ packages/react-components/react-select/src/index.ts
@@ -1,1 +1,9 @@
-export * from './Select';
+export {
+  Select,
+  renderSelect_unstable,
+  selectClassName,
+  selectClassNames,
+  useSelectStyles_unstable,
+  useSelect_unstable,
+} from './Select';
+export type { SelectCommons, SelectProps, SelectSlots, SelectState } from './Select';
