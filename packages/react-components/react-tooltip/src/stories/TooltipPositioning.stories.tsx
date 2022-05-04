2af65fc29bde3dfac8177db7acef39075e28b264
Oleksandr Fediashov <olfedias@microsoft.com>
2022-05-04 09:40:08 +0200

    docs(Tooltip): fix React keys in stories (#22787)
    * docs(Tooltip): fix React keys in stories
    
    * Update packages/react-components/react-tooltip/src/stories/TooltipPositioning.stories.tsx
    
    Co-authored-by: Makoto Morimoto <humbertomakotomorimoto@gmail.com>
    
    * Update packages/react-components/react-tooltip/src/stories/TooltipPositioning.stories.tsx
    
    Co-authored-by: Makoto Morimoto <humbertomakotomorimoto@gmail.com>

--- packages/react-components/react-tooltip/src/stories/TooltipPositioning.stories.tsx
+++ packages/react-components/react-tooltip/src/stories/TooltipPositioning.stories.tsx
@@ -38,7 +38,7 @@ export const Positioning = () => {
       }}
     >
       {positions.map(([position, icon]) => (
-        <Tooltip withArrow positioning={position} content={position} relationship="label">
+        <Tooltip key={position} withArrow positioning={position} content={position} relationship="label">
           <Button icon={icon} size="large" style={{ gridArea: position, minWidth: '64px', height: '64px' }} />
         </Tooltip>
       ))}
