3a642907cfdad8c377e0466b47d66c97fe54f79b
GeoffCoxMSFT <gcox@microsoft.com>
2022-05-09 11:56:34 -0700

    Added aria roles and labelledby to tab panels story

--- packages/react-components/react-tabs/src/stories/TabListWithPanels.stories.tsx
+++ packages/react-components/react-tabs/src/stories/TabListWithPanels.stories.tsx
@@ -41,92 +41,98 @@ export const WithPanels = () => {
   };
 
   const Arrivals = React.memo(() => (
-    <table>
-      <thead>
-        <th>Origin</th>
-        <th>Gate</th>
-        <th>ETA</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>DEN</td>
-          <td>C3</td>
-          <td>12:40 PM</td>
-        </tr>
-        <tr>
-          <td>SMF</td>
-          <td>D1</td>
-          <td>1:18 PM</td>
-        </tr>
-        <tr>
-          <td>SFO</td>
-          <td>E18</td>
-          <td>1:42 PM</td>
-        </tr>
-      </tbody>
-    </table>
+    <div role="tabpanel" aria-labelledby="Arrivals" tabIndex={0}>
+      <table>
+        <thead>
+          <th>Origin</th>
+          <th>Gate</th>
+          <th>ETA</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>DEN</td>
+            <td>C3</td>
+            <td>12:40 PM</td>
+          </tr>
+          <tr>
+            <td>SMF</td>
+            <td>D1</td>
+            <td>1:18 PM</td>
+          </tr>
+          <tr>
+            <td>SFO</td>
+            <td>E18</td>
+            <td>1:42 PM</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   ));
 
   const Departures = React.memo(() => (
-    <table>
-      <thead>
-        <th>Destination</th>
-        <th>Gate</th>
-        <th>ETD</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>MSP</td>
-          <td>A7</td>
-          <td>8:26 AM</td>
-        </tr>
-        <tr>
-          <td>DCA</td>
-          <td>N2</td>
-          <td>9:03 AM</td>
-        </tr>
-        <tr>
-          <td>LAS</td>
-          <td>E15</td>
-          <td>2:36 PM</td>
-        </tr>
-      </tbody>
-    </table>
+    <div role="tabpanel" aria-labelledby="Departures" tabIndex={0}>
+      <table>
+        <thead>
+          <th>Destination</th>
+          <th>Gate</th>
+          <th>ETD</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>MSP</td>
+            <td>A7</td>
+            <td>8:26 AM</td>
+          </tr>
+          <tr>
+            <td>DCA</td>
+            <td>N2</td>
+            <td>9:03 AM</td>
+          </tr>
+          <tr>
+            <td>LAS</td>
+            <td>E15</td>
+            <td>2:36 PM</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   ));
 
   const Conditions = React.memo(() => (
-    <table className={styles.propsTable}>
-      <tbody>
-        <tr>
-          <td>Time</td>
-          <td>6:45 AM</td>
-        </tr>
-        <tr>
-          <td>Temperature</td>
-          <td>68F / 20C</td>
-        </tr>
-        <tr>
-          <td>Forecast</td>
-          <td>Overcast</td>
-        </tr>
-        <tr>
-          <td>Visibility</td>
-          <td>0.5 miles, 1800 ft runway visual range</td>
-        </tr>
-      </tbody>
-    </table>
+    <div role="tabpanel" aria-labelledby="Conditions" tabIndex={0}>
+      <table className={styles.propsTable}>
+        <tbody>
+          <tr>
+            <td>Time</td>
+            <td>6:45 AM</td>
+          </tr>
+          <tr>
+            <td>Temperature</td>
+            <td>68F / 20C</td>
+          </tr>
+          <tr>
+            <td>Forecast</td>
+            <td>Overcast</td>
+          </tr>
+          <tr>
+            <td>Visibility</td>
+            <td>0.5 miles, 1800 ft runway visual range</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   ));
 
   return (
     <div className={styles.root}>
       <TabList selectedValue={selectedValue} onTabSelect={onTabSelect}>
-        <Tab icon={<AirplaneRegular />} value="arrivals">
+        <Tab id="Arrivals" icon={<AirplaneRegular />} value="arrivals">
           Arrivals
         </Tab>
-        <Tab icon={<AirplaneTakeOffRegular />} value="departures">
+        <Tab id="Departures" icon={<AirplaneTakeOffRegular />} value="departures">
           Departures
         </Tab>
-        <Tab icon={<TimeAndWeatherRegular />} value="conditions">
+        <Tab id="Conditions" icon={<TimeAndWeatherRegular />} value="conditions">
           Conditions
         </Tab>
       </TabList>
