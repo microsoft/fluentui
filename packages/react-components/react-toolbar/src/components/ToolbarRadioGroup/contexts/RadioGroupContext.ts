5aaa95847bc4fa369db6b59617d7c6ef4b3fc38a
Charles <chassunc@microsoft.com>
2022-05-10 11:38:10 -0300

    chore: remove create context

--- packages/react-components/react-toolbar/src/components/ToolbarRadioGroup/contexts/RadioGroupContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from '@fluentui/react-context-selector';
-import type { Context } from '@fluentui/react-context-selector';
-import { RadioGroupContextValue } from '../ToolbarRadioGroup.types';
-
-/**
- * RadioGroupContext is provided by RadioGroup, and is consumed by Radio to determine default values of some props.
- */
-export const RadioGroupContext: Context<RadioGroupContextValue> = createContext({});
