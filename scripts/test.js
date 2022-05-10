14fc6485b10780588d4fafb228406ad44a7e0de3
Lingfan Gao <lingfangao@hotmail.com>
2022-05-10 13:39:21 +0200

    stuf

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
-const out = execSync('git rev-list origin --max-count=1 --skip=1');
+const out = execSync('git rev-list HEAD --max-count=1 --skip=1');
 const commitSha = out.toString().trim();
 
 execSync(`yarn lage info --since ${out}`);
