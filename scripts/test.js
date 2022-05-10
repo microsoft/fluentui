1f330026db6b4d4dc18aa564cfb00d3816793c91
Lingfan Gao <lingfangao@hotmail.com>
2022-05-10 15:14:03 +0200

    revert changes

--- scripts/test.js
+++
@@ -1,6 +0,0 @@
-const execSync = require('child_process').execSync;
-
-const out = execSync('git rev-list HEAD --max-count=1 --skip=1');
-const commitSha = out.toString().trim();
-
-execSync(`yarn lage info --since ${out}`);
