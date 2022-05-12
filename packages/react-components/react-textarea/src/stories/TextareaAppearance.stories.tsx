9e82e9158b5c4f7cc047676c729261832c3b0304
Esteban Munoz <esteban.230@hotmail.com>
2022-05-12 15:35:27 -0600

    adding contrast guidance

--- packages/react-components/react-textarea/src/stories/TextareaAppearance.stories.tsx
+++ packages/react-components/react-textarea/src/stories/TextareaAppearance.stories.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { useId } from '@fluentui/react-utilities';
-import { Textarea } from '../index';
 import { Label } from '@fluentui/react-label';
 import { makeStyles, shorthands } from '@griffel/react';
+import { Text } from '@fluentui/react-text';
+import { Textarea } from '../index';
+import { useId } from '@fluentui/react-utilities';
 
 const useStyles = makeStyles({
   container: {
@@ -11,8 +12,11 @@ const useStyles = makeStyles({
     ...shorthands.gap('10px'),
     '& > div': { display: 'flex', flexDirection: 'column', ...shorthands.gap('10px'), ...shorthands.padding('10px') },
   },
+  filledLighter: {
+    backgroundColor: '#8a8a8a',
+  },
   filledDarker: {
-    backgroundColor: 'white',
+    backgroundColor: '#8a8a8a',
   },
 });
 
@@ -28,13 +32,24 @@ export const Appearance = () => {
         <Label htmlFor={outlineId}>Textarea with Outline appearance.</Label>
         <Textarea id={outlineId} appearance="outline" placeholder="type here..." resize="both" />
       </div>
-      <div className={styles.filledDarker}>
+      <div>
         <Label htmlFor={filledDarkerId}>Textarea with Filled Darker appearance.</Label>
-        <Textarea id={filledDarkerId} appearance="filledDarker" placeholder="type here..." resize="both" />
+        <div className={styles.filledDarker}>
+          <Textarea id={filledDarkerId} appearance="filledDarker" placeholder="type here..." resize="both" />
+        </div>
       </div>
       <div>
         <Label htmlFor={filledLighterId}>Textarea with Filled Lighter appearance.</Label>
-        <Textarea id={filledLighterId} appearance="filledLighter" placeholder="type here..." resize="both" />
+        <div className={styles.filledLighter}>
+          <Textarea id={filledLighterId} appearance="filledLighter" placeholder="type here..." resize="both" />
+        </div>
+      </div>
+      <div>
+        <Text>
+          The colors adjacent to the Textarea should have a sufficient contrast. Particularly, the color of input with
+          filled darker and lighter styles needs to provide a contrast ratio greater than 3 to 1 against the immediate
+          surrounding color to pass accessibility requirement.
+        </Text>
       </div>
     </div>
   );
