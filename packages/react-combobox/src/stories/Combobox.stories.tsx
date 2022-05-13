134e944612a5d282e0e17f22e9524da4a35b99cc
Sarah Higley <sarah.higley@microsoft.com>
2022-05-13 11:25:22 -0700

    add grouped combo story

--- packages/react-combobox/src/stories/Combobox.stories.tsx
+++ packages/react-combobox/src/stories/Combobox.stories.tsx
@@ -7,6 +7,7 @@ import bestPracticesMd from './ComboboxBestPractices.md';
 export { Default } from './ComboboxDefault.stories';
 export { CustomOptions } from './ComboboxCustomOptions.stories';
 export { Multiselect } from './ComboboxMultiselect.stories';
+export { Grouped } from './ComboboxGrouped.stories';
 export { Appearance } from './ComboboxAppearance.stories';
 export { Size } from './ComboboxSize.stories';
 
