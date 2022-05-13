3c20f25b2750e054e1568f98f31dceab5e85db16
Sean Monahan <seanmonahan@microsoft.com>
2022-05-13 14:24:43 -0700

    react-input: update unions to use spinal-case
    BREAKING
    
    Per https://github.com/microsoft/fluentui/pull/22596 all string
    unions are being updated to use spinal-case

--- packages/react-components/react-input/src/components/Input/useInputStyles.ts
+++ packages/react-components/react-input/src/components/Input/useInputStyles.ts
@@ -148,10 +148,10 @@ const useRootStyles = makeStyles({
       ...shorthands.borderColor(tokens.colorTransparentStrokeInteractive),
     },
   },
-  filledDarker: {
+  'filled-darker': {
     backgroundColor: tokens.colorNeutralBackground3,
   },
-  filledLighter: {
+  'filled-lighter': {
     backgroundColor: tokens.colorNeutralBackground1,
   },
   disabled: {
