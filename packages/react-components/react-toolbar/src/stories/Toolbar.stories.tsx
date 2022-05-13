52fe6da8b121cbf384bf975c046842f453086c6a
Charles <chassunc@microsoft.com>
2022-05-12 22:10:55 -0300

    feat: add subtle and popover stories for toolbar

--- packages/react-components/react-toolbar/src/stories/Toolbar.stories.tsx
+++ packages/react-components/react-toolbar/src/stories/Toolbar.stories.tsx
@@ -6,6 +6,8 @@ import bestPracticesMd from './ToolbarBestPractices.md';
 export { Default } from './ToolbarDefault.stories';
 export { Small } from './ToolbarSmall.stories';
 export { WithTooltip } from './ToolbarWithTooltip.stories';
+export { WithPopover } from './ToolbarWithPopover.stories';
+export { Subtle } from './ToolbarSubtle.stories';
 
 export default {
   title: 'Components/Toolbar',
