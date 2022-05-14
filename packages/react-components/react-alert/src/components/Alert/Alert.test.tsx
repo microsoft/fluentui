9b26c84edfc364037e64cc2ae41393f994ecb04b
Rohit Pagariya <rohitpag@microsoft.com>
2022-05-13 21:31:24 -0700

    six

--- packages/react-components/react-alert/src/components/Alert/Alert.test.tsx
+++ packages/react-components/react-alert/src/components/Alert/Alert.test.tsx
@@ -3,17 +3,30 @@ import * as React from 'react';
 import { Delete16Filled } from '@fluentui/react-icons';
 import { render, screen } from '@testing-library/react';
 
-// import { isConformant } from '../../common/isConformant';
+import { isConformant } from '../../common/isConformant';
 import { Alert } from './Alert';
 import { intentClassName } from './useAlertStyles';
 
 describe('Alert', () => {
-  // Todo - Follow up in the next PR
-
-  // isConformant({
-  //   Component: Alert,
-  //   displayName: 'Alert',
-  // });
+  isConformant({
+    Component: Alert,
+    displayName: 'Alert',
+    // TODO - Follow up on the best option here
+    // testOptions: {
+    //   'has-static-classnames': [
+    //     {
+    //       props: {
+    //         action: { children: 'Test button ' },
+    //       },
+    //     },
+    //   ],
+    // },
+    disabledTests: [
+      // 'component-has-static-classname',
+      // 'component-has-static-classname-exported',
+      'component-has-static-classnames-object',
+    ],
+  });
 
   // TODO - create visual regression tests in /apps/vr-tests
   // TODO - add tests for aria attributes or any a11y behaviors
