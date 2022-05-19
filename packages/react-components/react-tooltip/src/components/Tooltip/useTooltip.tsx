61cd9f1a09c61d5b77c668a6a6a2dcfa927c54a7
Sean Monahan <seanmonahan@microsoft.com>
2022-05-19 16:57:41 -0700

    react-tooltip: update react-positioning

--- packages/react-components/react-tooltip/src/components/Tooltip/useTooltip.tsx
+++ packages/react-components/react-tooltip/src/components/Tooltip/useTooltip.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { mergeArrowOffset, resolvePositioningShorthand, usePopper } from '@fluentui/react-positioning';
+import { mergeArrowOffset, resolvePositioningShorthand, usePositioning } from '@fluentui/react-positioning';
 import { TooltipContext, useFluent } from '@fluentui/react-shared-contexts';
 import {
   applyTriggerPropsToChildren,
@@ -15,6 +15,7 @@ import {
 } from '@fluentui/react-utilities';
 import type { TooltipProps, TooltipState, TooltipTriggerProps } from './Tooltip.types';
 import { arrowHeight, tooltipBorderRadius } from './private/constants';
+import type { Offset } from '@fluentui/react-positioning';
 
 /**
  * Create the state required to render Tooltip.
@@ -86,7 +87,7 @@ export const useTooltip_unstable = (props: TooltipProps): TooltipState => {
     arrowPadding: 2 * tooltipBorderRadius,
     position: 'above' as const,
     align: 'center' as const,
-    offset: [0, 4] as [number, number],
+    offset: { crossAxis: 0, mainAxis: 4 } as Offset,
     ...resolvePositioningShorthand(state.positioning),
   };
 
@@ -102,7 +103,7 @@ export const useTooltip_unstable = (props: TooltipProps): TooltipState => {
     targetRef: React.MutableRefObject<unknown>;
     containerRef: React.MutableRefObject<HTMLDivElement>;
     arrowRef: React.MutableRefObject<HTMLDivElement>;
-  } = usePopper(popperOptions);
+  } = usePositioning(popperOptions);
 
   state.content.ref = useMergedRefs(state.content.ref, containerRef);
   state.arrowRef = arrowRef;
