10aae1f0eb1cfdd5f42b03f01d53fd36c90ea3eb
Martin Hochel <martinhochel@microsoft.com>
2022-05-20 16:16:07 +0200

    fixup! fixup! feat: final fantasy ?

--- packages/react-components/react-utilities/src/utils/getNativeElementProps.ts
+++ packages/react-components/react-utilities/src/utils/getNativeElementProps.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { IntrisicElementProps } from '../compose';
+import type { IntrisicElementProps } from '../compose/index';
 
 import {
   labelProperties,
