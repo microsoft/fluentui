c03feb6688a25d3cbbacc871ed98f7163076ac19
AnnapareddyHarsha <v-hannapared@microsoft.com>
2022-05-20 12:57:32 +0530

    reverting color contrast issue changes

--- packages/react-charting/src/components/LineChart/eventAnnotation/LabelLink.tsx
+++ packages/react-charting/src/components/LineChart/eventAnnotation/LabelLink.tsx
@@ -56,12 +56,13 @@ export const LabelLink: React.FunctionComponent<ILabelLinkProps> = props => {
   }
 
   let text: string;
-  const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
-  const fill: string | undefined = darkThemeMq.matches ? 'rgb(255,255,255)' : props.textColor;
+  let fill: string | undefined;
   if (props.labelDef.aggregatedIdx.length === 1) {
     text = props.lineDefs[props.labelDef.aggregatedIdx[0]].event;
+    fill = props.textColor;
   } else {
     text = props.mergedLabel(props.labelDef.aggregatedIdx.length);
+    fill = props.textColor;
   }
 
   return (
