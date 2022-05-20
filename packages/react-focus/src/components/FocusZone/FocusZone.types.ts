21e970cd321970f825691c6bb57cfa23c6360e47
Sarah Higley <smhigley@users.noreply.github.com>
2022-05-19 17:36:27 -0700

    spelling
    Co-authored-by: Sean Monahan <seanmonahan@microsoft.com>

--- packages/react-focus/src/components/FocusZone/FocusZone.types.ts
+++ packages/react-focus/src/components/FocusZone/FocusZone.types.ts
@@ -217,7 +217,7 @@ export interface IFocusZoneProps extends React.HTMLAttributes<HTMLElement> {
 
   /**
    * A callback method to determine if an input or textarea element should lose focus on arrow keys
-   *  @param inputElement - The input or textarea element which is to loose focus.
+   *  @param inputElement - The input or textarea element which is to lose focus.
    *  @returns True if input element should loose focus or false otherwise.
    */
   shouldInputLoseFocusOnArrowKey?: (inputElement: HTMLInputElement | HTMLTextAreaElement) => boolean;
