48230861e5b0a67085c3d5674d2f68c6523b9540
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2022-05-23 16:35:04 -0700

    Theme Designer: creating child components and basic layout (#23158)
    * creating child components and basic layout
    
    * change container to root
    
    * Update packages/react-components/theme-designer/src/components/Body/Body.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * Update packages/react-components/theme-designer/src/components/Nav/Nav.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * Update packages/react-components/theme-designer/src/components/Nav/Nav.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * Update packages/react-components/theme-designer/src/components/Body/Body.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * Update packages/react-components/theme-designer/src/ThemeDesigner.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * update root for top level component
    
    * Update packages/react-components/theme-designer/src/ThemeDesigner.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * basic scaffold
    
    * fix componentprop names
    
    * remove classname requirement on themedesigner
    
    * change 'Body' to 'Content'
    
    * Update packages/react-components/theme-designer/src/ThemeDesigner.types.ts
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * folder based imports to file based imports
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>

---
+++ packages/react-components/theme-designer/src/components/Nav/Nav.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface NavProps {
+  className?: String;
+}
+
+export const Nav: React.FC<NavProps> = props => {
+  return <div>Nav</div>;
+};
---
+++ packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface SidebarProps {
+  className?: String;
+}
+
+export const Sidebar: React.FC<SidebarProps> = props => {
+  return <div>Sidebar</div>;
+};
