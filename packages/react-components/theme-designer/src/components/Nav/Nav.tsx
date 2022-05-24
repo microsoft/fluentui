4e7ea9f0ffc218b8e0a94a52869c40fb6b2e123f
Emma Jiang <eyj.thunder@gmail.com>
2022-05-24 11:31:57 -0700

    starting to build out styles for layout of the theme designer

--- packages/react-components/theme-designer/src/components/Nav/Nav.tsx
+++ packages/react-components/theme-designer/src/components/Nav/Nav.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import { makeStyles, mergeClasses } from '@griffel/react';
 
 export interface NavProps {
-  className?: String;
+  className: string;
 }
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'left',
+    borderBottomWidth: '1px',
+    borderBottomColor: '#D1D1D1',
+    borderBottomStyle: 'solid',
+  },
+});
+
 export const Nav: React.FC<NavProps> = props => {
-  return <div>Nav</div>;
+  const styles = useStyles();
+  return <div className={mergeClasses(styles.root, props.className)}>Nav</div>;
 };
--- packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
+++ packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import { makeStyles, mergeClasses } from '@griffel/react';
 
 export interface SidebarProps {
-  className?: String;
+  className: string;
 }
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRightWidth: '1px',
+    borderRightColor: '#D1D1D1',
+    borderRightStyle: 'solid',
+  },
+});
+
 export const Sidebar: React.FC<SidebarProps> = props => {
-  return <div>Sidebar</div>;
+  const styles = useStyles();
+  return <div className={mergeClasses(styles.root, props.className)}>Sidebar</div>;
 };
