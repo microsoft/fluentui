ae2db3b290cd09cd912e904a50b2b78f4856473b
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2022-05-24 15:17:15 -0700

    Theme Designer: starting to build out styles for layout (#23195)
    * starting to build out styles for layout of the theme designer
    
    * Update packages/react-components/theme-designer/src/components/Nav/Nav.tsx
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>
    
    * added shorthands
    
    * make classNames optional
    
    Co-authored-by: Micah Godbolt <micahgodbolt@gmail.com>

--- packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
+++ packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
+import { makeStyles, mergeClasses, shorthands } from '@griffel/react';
 
 export interface SidebarProps {
-  className?: String;
+  className?: string;
 }
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...shorthands.borderRight('1px', 'solid', '#D1D1D1'),
+  },
+});
+
 export const Sidebar: React.FC<SidebarProps> = props => {
-  return <div>Sidebar</div>;
+  const styles = useStyles();
+  return <div className={mergeClasses(styles.root, props.className)}>Sidebar</div>;
 };
