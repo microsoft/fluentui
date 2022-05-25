fdf2022f62be66002345181c495fdffec8b1ea45
Lingfan Gao <lingfangao@hotmail.com>
2022-05-25 09:49:29 +0200

    type aware rules for base

--- packages/eslint-plugin/src/configs/base.js
+++ packages/eslint-plugin/src/configs/base.js
@@ -1,8 +1,14 @@
 // @ts-check
 
 const path = require('path');
+const configHelpers = require('../utils/configHelpers');
 
 const { getNamingConventionRule } = require('../utils/configHelpers');
+
+/** @type {import("eslint").Linter.RulesRecord} */
+const typeAwareRules = {
+  '@fluentui/ban-context-export': ['error', { exclude: ['**/react-shared-contexts/**'] }],
+};
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
@@ -15,12 +21,13 @@ module.exports = {
     ...getNamingConventionRule(),
   },
   overrides: [
+    // Enable rules requiring type info only for appropriate files/circumstances
+    ...configHelpers.getTypeInfoRuleOverrides(typeAwareRules),
     {
       files: '**/src/index.{ts,tsx,js}',
       rules: {
         // TODO: propagate to `error` once all packages barrel files have been fixed
         '@rnx-kit/no-export-all': ['warn', { expand: 'all' }],
-        '@fluentui/ban-context-export': ['error', { exclude: ['**/react-shared-contexts/**'] }],
       },
     },
   ],
