4922a8a8b9e52ef8cc7f299771d983a6a09fa861
chajun <86579954+chpalac@users.noreply.github.com>
2022-05-25 08:09:33 -0300

    feat: add Toolbar overflow stories (#23077)
    * chore: delete not needed tests
    
    * feat: add Toolbar small storie and Radio small support
    
    * chore: update api
    
    * chore: initial toolbar overflow work
    
    * chore: finish overflow storie example
    
    * chore: rename test items
    
    * chore: remove space
    
    * chore: rename menu divider
    
    * chore: overflow button example

--- packages/react-components/react-toolbar/src/stories/Toolbar.stories.tsx
+++ packages/react-components/react-toolbar/src/stories/Toolbar.stories.tsx
@@ -5,6 +5,7 @@ import bestPracticesMd from './ToolbarBestPractices.md';
 
 export { Default } from './ToolbarDefault.stories';
 export { Small } from './ToolbarSmall.stories';
+export { OverflowItems } from './ToolbarOverflow.stories';
 export { WithTooltip } from './ToolbarWithTooltip.stories';
 export { WithPopover } from './ToolbarWithPopover.stories';
 export { Subtle } from './ToolbarSubtle.stories';
