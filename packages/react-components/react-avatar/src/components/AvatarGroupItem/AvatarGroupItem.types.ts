fdf33def6d5e1b5522c224f0ac7ac44b18e09fec
Esteban Munoz <esteban.230@hotmail.com>
2022-05-26 17:38:50 -0600

    removing long comment

--- packages/react-components/react-avatar/src/components/AvatarGroupItem/AvatarGroupItem.types.ts
+++ packages/react-components/react-avatar/src/components/AvatarGroupItem/AvatarGroupItem.types.ts
@@ -13,5 +13,3 @@ export type AvatarGroupItemProps = ComponentProps<AvatarGroupItemSlots> & {};
  * State used in rendering AvatarGroupItem
  */
 export type AvatarGroupItemState = ComponentState<AvatarGroupItemSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from AvatarGroupItemProps.
-// & Required<Pick<AvatarGroupItemProps, 'propName'>>
