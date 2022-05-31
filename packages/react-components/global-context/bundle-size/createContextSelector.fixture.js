6da6dce4e12754b7762bd42b09fe9ffb2d517497
ling1726 <lingfangao@hotmail.com>
2022-05-31 16:37:49 +0200

    Update packages/react-components/global-context/bundle-size/createContextSelector.fixture.js
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-components/global-context/bundle-size/createContextSelector.fixture.js
+++ packages/react-components/global-context/bundle-size/createContextSelector.fixture.js
@@ -2,5 +2,5 @@ import { createContextSelector } from '@fluentui/global-context';
 console.log(createContextSelector);
 
 export default {
-  name: 'createContextSelector implementations',
+  name: 'createContextSelector',
 };
