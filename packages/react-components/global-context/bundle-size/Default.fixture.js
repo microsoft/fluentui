081d35e43b5cf9330d620a6232350d502c70b41a
Lingfan Gao <lingfangao@hotmail.com>
2022-05-31 16:33:31 +0200

    custom webpack config

--- packages/react-components/global-context/bundle-size/Default.fixture.js
+++
@@ -1,6 +0,0 @@
-import { createContext, createContextSelector } from '@fluentui/global-context';
-console.log(createContext, createContextSelector);
-
-export default {
-  name: 'createContext and createContextSelector',
-};
