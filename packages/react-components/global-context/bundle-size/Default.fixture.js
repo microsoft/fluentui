82e44941130d79eb7e2f5c95d4f22191e7bf2939
ling1726 <lingfangao@hotmail.com>
2022-05-31 16:23:54 +0200

    Update packages/react-components/global-context/bundle-size/Default.fixture.js
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-components/global-context/bundle-size/Default.fixture.js
+++ packages/react-components/global-context/bundle-size/Default.fixture.js
@@ -2,5 +2,5 @@ import { createContext, createContextSelector } from '@fluentui/global-context';
 console.log(createContext, createContextSelector);
 
 export default {
-  name: 'createContext and createContextSelector implementations',
+  name: 'createContext and createContextSelector',
 };
