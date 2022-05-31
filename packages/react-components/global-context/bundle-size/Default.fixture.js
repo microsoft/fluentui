fd4998d8262245a875b011397bd8ba6d0199d2d1
Lingfan Gao <lingfangao@hotmail.com>
2022-05-31 16:21:30 +0200

    add bundlesize

---
+++ packages/react-components/global-context/bundle-size/Default.fixture.js
@@ -0,0 +1,6 @@
+import { createContext, createContextSelector } from '@fluentui/global-context';
+console.log(createContext, createContextSelector);
+
+export default {
+  name: 'createContext and createContextSelector implementations',
+};
