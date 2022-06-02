75013797f234d41c67b1cf41c1025636ac18a5d8
Esteban Munoz <esteban.230@hotmail.com>
2022-06-02 13:02:15 -0600

    removing styling for trigger

--- packages/react-components/react-avatar/src/components/AvatarGroup/useAvatarGroupStyles.ts
+++ packages/react-components/react-avatar/src/components/AvatarGroup/useAvatarGroupStyles.ts
@@ -20,27 +20,6 @@ const useStyles = makeStyles({
   },
 });
 
-/**
- * Styles for the PopoverTrigger.
- */
-const usePopoverTriggerStyles = makeStyles({
-  base: {
-    flexShrink: 0,
-    minWidth: 0,
-    minHeight: 0,
-    maxWidth: 'unset',
-    maxHeight: 'unset',
-    ...shorthands.padding(0),
-  },
-  pie: {
-    backgroundColor: 'transparent',
-    ...shorthands.borderColor('transparent'),
-    ':focus': {
-      backgroundColor: 'transparent',
-    },
-  },
-});
-
 const usePopoverSurfaceListStyles = makeStyles({
   base: {
     listStyleType: 'none',
@@ -53,10 +32,9 @@ const usePopoverSurfaceListStyles = makeStyles({
  * Apply styling to the AvatarGroup slots based on the state
  */
 export const useAvatarGroupStyles_unstable = (state: AvatarGroupState): AvatarGroupState => {
-  const { layout, size } = state;
+  const { size } = state;
   const styles = useStyles();
   const sizeStyles = useSizeStyles();
-  const popoverTriggerStyles = usePopoverTriggerStyles();
   const popoverSurfaceListStyles = usePopoverSurfaceListStyles();
 
   state.root.className = mergeClasses(avatarGroupClassNames.root, styles.base, state.root.className);
@@ -76,9 +54,7 @@ export const useAvatarGroupStyles_unstable = (state: AvatarGroupState): AvatarGr
   if (state.popoverTrigger) {
     state.popoverTrigger.className = mergeClasses(
       avatarGroupClassNames.popoverTrigger,
-      popoverTriggerStyles.base,
       sizeStyles[size],
-      layout === 'pie' && popoverTriggerStyles.pie,
       state.popoverTrigger.className,
     );
   }
