4ac322fec1b0d13732ab944c9336a34e3e06db0b
Emma Jiang <eyj.thunder@gmail.com>
2022-06-02 14:39:33 -0700

    fixing styles

--- packages/react-components/theme-designer/src/components/Nav/Nav.tsx
+++ packages/react-components/theme-designer/src/components/Nav/Nav.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { makeStyles } from '@griffel/react';
+import { makeStyles, mergeClasses } from '@griffel/react';
 import { FluentProvider, webDarkTheme } from '@fluentui/react-components';
 import { CircleRegular, ChevronRightRegular } from '@fluentui/react-icons';
 import { Text, Menu, MenuTrigger, MenuButton, MenuPopover, MenuList, MenuItem } from '@fluentui/react-components';
@@ -9,7 +9,7 @@ export interface NavProps {
 }
 
 const useStyles = makeStyles({
-  content: {
+  root: {
     alignItems: 'center',
     display: 'grid',
     gridTemplateColumns: '1fr 2fr 3fr 3fr',
@@ -66,20 +66,16 @@ export const ExportButton = () => {
 export const Nav: React.FC<NavProps> = props => {
   const styles = useStyles();
   return (
-    <div className={props.className}>
-      <FluentProvider theme={webDarkTheme}>
-        <div className={styles.content}>
-          <div className={styles.logo}>
-            <CircleRegular />
-            <Text>Color Tool</Text>
-          </div>
-          <div className={styles.element}>
-            UI Colors <ChevronRightRegular /> New palette
-          </div>
-          <Name />
-          <ExportButton />
-        </div>
-      </FluentProvider>
-    </div>
+    <FluentProvider theme={webDarkTheme} className={mergeClasses(styles.root, props.className)}>
+      <div className={styles.logo}>
+        <CircleRegular />
+        <Text>Color Tool</Text>
+      </div>
+      <div className={styles.element}>
+        UI Colors <ChevronRightRegular /> New palette
+      </div>
+      <Name />
+      <ExportButton />
+    </FluentProvider>
   );
 };
