43b2213f6d468ae0c745f1696c8ebe7a97cf5e5c
Esteban Munoz <esteban.230@hotmail.com>
2022-06-02 10:41:37 -0600

    updating stories

--- packages/react-components/react-avatar/src/stories/AvatarGroupDefault.stories.tsx
+++ packages/react-components/react-avatar/src/stories/AvatarGroupDefault.stories.tsx
@@ -1,37 +1,20 @@
 import * as React from 'react';
 import { AvatarGroup } from '../AvatarGroup';
 import type { AvatarGroupProps } from '../AvatarGroup';
-import { Avatar } from '../Avatar';
-import { Label } from '@fluentui/react-label';
-import { makeStyles, shorthands } from '@griffel/react';
-import { useId } from '@fluentui/react-utilities';
-
-const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    ...shorthands.gap('10px'),
-  },
-});
+import { AvatarGroupItem } from '../AvatarGroupItem';
 
 export const Default = (props: Partial<AvatarGroupProps>) => {
-  const avatarGroupId = useId('textarea');
-  const styles = useStyles();
-
   return (
-    <div className={styles.container}>
-      <Label htmlFor={avatarGroupId}>Default AvatarGroup</Label>
-      <AvatarGroup id={avatarGroupId} {...props}>
-        <Avatar name="Katri Athokas" />
-        <Avatar name="Elvia Atkins" />
-        <Avatar name="Cameron Evans" />
-        <Avatar name="Wanda Howard" />
-        <Avatar name="Mona Kane" />
-        <Avatar name="Allan Munger" />
-        <Avatar name="Daisy Phillips" />
-        <Avatar name="Robert Tolbert" />
-        <Avatar name="Kevin Sturgis" />
-      </AvatarGroup>
-    </div>
+    <AvatarGroup {...props}>
+      <AvatarGroupItem name="Katri Athokas" />
+      <AvatarGroupItem name="Elvia Atkins" />
+      <AvatarGroupItem name="Cameron Evans" />
+      <AvatarGroupItem name="Wanda Howard" />
+      <AvatarGroupItem name="Mona Kane" />
+      <AvatarGroupItem name="Allan Munger" />
+      <AvatarGroupItem name="Daisy Phillips" />
+      <AvatarGroupItem name="Robert Tolbert" />
+      <AvatarGroupItem name="Kevin Sturgis" />
+    </AvatarGroup>
   );
 };
