195f887f5d9d12bdbbe2e3e613ef1a1e41466c71
Sarah Higley <smhigley@users.noreply.github.com>
2022-06-02 16:55:17 -0700

    react-select: add tests (#23374)

--- packages/react-components/react-select/src/components/Select/Select.test.tsx
+++ packages/react-components/react-select/src/components/Select/Select.test.tsx
@@ -11,10 +11,46 @@ describe('Select', () => {
     primarySlot: 'select',
   });
 
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
+  // Note for Select tests: avoid using getByRole;
+  // The accessibility role mapping for <select> differs between Windows and macOS
 
   it('renders the default state', () => {
     const result = render(<Select />);
     expect(result.container).toMatchSnapshot();
+  });
+
+  it('renders a custom icon slot', () => {
+    const result = render(<Select icon="x" />);
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it('renders option children', () => {
+    const result = render(
+      <Select>
+        <option>A</option>
+        <option>B</option>
+        <option>C</option>
+      </Select>,
+    );
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it('handles the disabled attribute', () => {
+    const { getByTestId } = render(<Select data-testid="select" disabled />);
+    expect((getByTestId('select') as HTMLSelectElement).disabled).toBeTruthy();
+  });
+
+  it('forwards ref to the select element', () => {
+    const ref = React.createRef<HTMLSelectElement>();
+    const { getByTestId } = render(<Select ref={ref} data-testid="select" />);
+    expect(getByTestId('select')).toEqual(ref.current);
+  });
+
+  it('forwards id and aria-* to the select element', () => {
+    const { getByTestId } = render(<Select id="select" aria-label="test" data-testid="select" />);
+    const select = getByTestId('select');
+
+    expect(select.id).toEqual('select');
+    expect(select.getAttribute('aria-label')).toEqual('test');
   });
 });
