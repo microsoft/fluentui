332452210ee0cd2989700bed7655c4521ee942d2
André <39736248+andrefcdias@users.noreply.github.com>
2022-06-06 14:41:32 +0200

    chore: add documentation

--- packages/react-components/react-card/src/components/Card/Card.types.ts
+++ packages/react-components/react-card/src/components/Card/Card.types.ts
@@ -17,6 +17,10 @@ export type CardSlots = {
    * Root element of the component.
    */
   root: Slot<'div'>;
+
+  /**
+   * Checkbox slot used when `selectable` prop is enabled.
+   */
   select?: Slot<typeof Checkbox>;
 };
 
@@ -62,12 +66,30 @@ export type CardProps = ComponentProps<CardSlots> & {
    */
   size?: 'small' | 'medium' | 'large';
 
+  /**
+   * Enables selection of the card.
+   *
+   * @default false
+   */
   selectable?: boolean;
 
+  /**
+   * Defines the controlled selected state of the card.
+   *
+   * @default false
+   */
   selected?: boolean;
 
+  /**
+   * Defines whether the card is initially in a selected state or not when rendered.
+   *
+   * @default false
+   */
   defaultSelected?: boolean;
 
+  /**
+   * Callback to be called when the selected state value changes.
+   */
   onCardSelect?: (event: React.MouseEvent | React.KeyboardEvent, data: CardOnSelectData) => void;
 };
 
