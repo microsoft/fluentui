d0bb11717a0607738c7d4bfe2893f91f6bdee457
André <39736248+andrefcdias@users.noreply.github.com>
2022-06-07 23:50:10 +0200

    chore: add app card

--- packages/react-components/react-card/src/stories/CardTemplates.stories.tsx
+++ packages/react-components/react-card/src/stories/CardTemplates.stories.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
-import { makeStyles } from '@griffel/react';
+import { makeStyles, shorthands } from '@griffel/react';
 import { tokens } from '@fluentui/react-theme';
 import { MoreHorizontal16Filled } from '@fluentui/react-icons';
 import { Button } from '@fluentui/react-button';
 import { Text, Caption1 } from '@fluentui/react-text';
 import { Card, CardHeader } from '../';
+import AppLogo from '../../assets/app_logo.svg';
 
 export const ASSET_URL =
   'https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-components/react-card';
@@ -15,13 +16,22 @@ const powerpointLogoURL = ASSET_URL + '/assets/powerpoint_logo.svg';
 const useStyles = makeStyles({
   container: {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
+    ...shorthands.gap('16px'),
   },
-  example1: {
+  size: {
     width: '280px',
   },
-  example1Caption: {
+  caption: {
     color: tokens.colorNeutralForeground3,
+  },
+  headerImage: {
+    maxWidth: '44px',
+    maxHeight: '44px',
+
+    [`> img`]: {
+      ...shorthands.borderRadius('4px'),
+    },
   },
 });
 
@@ -30,11 +40,11 @@ export const Templates = () => {
 
   return (
     <div className={styles.container}>
-      <Card className={styles.example1}>
+      <Card className={styles.size}>
         <CardHeader
           image={<img src={powerpointLogoURL} />}
           header={<Text weight="semibold">Team offsite 2020</Text>}
-          description={<Caption1 className={styles.example1Caption}>Onedrive &gt; Files</Caption1>}
+          description={<Caption1 className={styles.caption}>Onedrive &gt; Files</Caption1>}
           action={
             <Button
               appearance="transparent"
@@ -43,6 +53,24 @@ export const Templates = () => {
             />
           }
         />
+      </Card>
+      <Card className={styles.size}>
+        <CardHeader
+          image={{ className: styles.headerImage, children: <img src={AppLogo} /> }}
+          header={<Text weight="semibold">App Name</Text>}
+          description={<Caption1 className={styles.caption}>Developer</Caption1>}
+          action={
+            <Button
+              appearance="transparent"
+              icon={<MoreHorizontal16Filled />}
+              onClick={action('Example 1 button pressed')}
+            />
+          }
+        />
+        <span>
+          Donut chocolate bar oat cake. Dragée tiramisu lollipop bear claw. Marshmallow pastry jujubes toffee sugar
+          plum.
+        </span>
       </Card>
     </div>
   );
