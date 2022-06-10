acf1753ea566d89131058f5415961bfdafa2c742
Esteban Munoz Facusse <esteban.230@hotmail.com>
2022-06-10 15:54:27 -0600

    docs(react-avatar): Removing AvatarGroupItem stories (#23497)
    * removing AvatarGroupItem stories
    
    * change files
    
    * adding requested changes

--- packages/react-components/react-avatar/src/stories/AvatarGroupItem/AvatarGroupItemOverflow.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { AvatarGroupItem, AvatarGroupItemProps } from '../../AvatarGroupItem';
-import { AvatarGroupContext } from '../../contexts/AvatarGroupContext';
-
-export const Overflow = (props: Partial<AvatarGroupItemProps>) => (
-  <AvatarGroupContext.Provider value={{ isOverflow: true }}>
-    <AvatarGroupItem name="Katri Athokas" {...props} />
-  </AvatarGroupContext.Provider>
-);
