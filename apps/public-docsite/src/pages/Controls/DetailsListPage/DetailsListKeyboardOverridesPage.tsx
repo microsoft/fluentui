2dfb3976fc751afd9bf5ca555a02f31eea20e6ea
Sarah Higley <sarah.higley@microsoft.com>
2022-06-10 15:37:36 -0700

    update docsite detailslist page files

--- apps/public-docsite/src/pages/Controls/DetailsListPage/DetailsListKeyboardOverridesPage.tsx
+++ apps/public-docsite/src/pages/Controls/DetailsListPage/DetailsListKeyboardOverridesPage.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import { ControlsAreaPage, IControlsPageProps } from '../ControlsAreaPage';
 import { DetailsListKeyboardOverridesProps } from './DetailsListKeyboardOverrides.doc';
 
-export const DetailsListKeyboardOverrides: React.FunctionComponent<IControlsPageProps> = props => {
+export const DetailsListKeyboardOverridesPage: React.FunctionComponent<IControlsPageProps> = props => {
   return <ControlsAreaPage {...props} {...DetailsListKeyboardOverridesProps[props.platform]} />;
 };
