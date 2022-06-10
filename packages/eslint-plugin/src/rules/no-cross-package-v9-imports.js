a092b25f912cddb0985299cbee84305b6243a125
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-06-10 11:32:46 -0700

    refactor to return array of fixers

--- packages/eslint-plugin/src/rules/no-cross-package-v9-imports.js
+++ packages/eslint-plugin/src/rules/no-cross-package-v9-imports.js
@@ -1,9 +1,6 @@
 // @ts-check
 const { AST_NODE_TYPES } = require('@typescript-eslint/experimental-utils');
 const createRule = require('../utils/createRule');
-/**
- * @typedef {import("@typescript-eslint/typescript-estree").TSESTree.Identifier} Identifier
- */
 
 module.exports = createRule({
   name: 'no-cross-package-v9-imports',
@@ -16,7 +13,7 @@ module.exports = createRule({
     },
     messages: {
       crossPackageImport:
-        'Cross package import from {{ importedPkg }} detected. Please import only from @fluentui/react-components',
+        'Cross package import from {{ packageName }} detected. Please import only from @fluentui/react-components',
     },
     fixable: 'code',
     schema: [],
@@ -28,23 +25,20 @@ module.exports = createRule({
      */
     let reactComponentsImportNodeRange;
 
-    /**
-     * @type {import("@typescript-eslint/types/dist/ts-estree").ImportClause[]}
-     */
-    const importedIdentifiers = [];
-
     return {
       ImportDeclaration: imprt => {
         if (typeof imprt.source.value !== 'string') {
           return;
         }
 
+        const specifiers = imprt.specifiers;
+
         if (
           imprt.source &&
           imprt.source.type === AST_NODE_TYPES.Literal &&
           imprt.source.value === '@fluentui/react-components'
         ) {
-          reactComponentsImportNodeRange = imprt.specifiers[imprt.specifiers.length - 1].range;
+          reactComponentsImportNodeRange = specifiers[specifiers.length - 1].range;
         }
 
         if (
@@ -52,14 +46,14 @@ module.exports = createRule({
             imprt.source.type === AST_NODE_TYPES.Literal &&
             imprt.source.value !== '@fluentui/react-components' &&
             imprt.source.value.includes('@fluentui/react-')) ||
-          imprt.source.value.includes('@griffel')
+          imprt.source.value.includes('@griffel/react')
         ) {
           if (!reactComponentsImportNodeRange) {
-            reactComponentsImportNodeRange = imprt.specifiers[imprt.specifiers.length - 1].range;
+            reactComponentsImportNodeRange = specifiers[specifiers.length - 1].range;
             context.report({
               node: imprt,
               messageId: 'crossPackageImport',
-              data: { importedPkg: imprt.source.value },
+              data: { packageName: imprt.source.value },
               fix: fixer => {
                 const [start, end] = imprt.source.range;
                 return fixer.replaceTextRange([start + 1, end - 1], '@fluentui/react-components');
@@ -69,25 +63,13 @@ module.exports = createRule({
             context.report({
               node: imprt,
               messageId: 'crossPackageImport',
-              data: { importedPkg: imprt.source.value },
+              data: { packageName: imprt.source.value },
               fix: fixer => {
-                importedIdentifiers.push(...imprt.specifiers);
-                return fixer.remove(imprt);
+                const importsToAdd = `, ${imprt.specifiers.map(specifier => specifier.local.name).join(', ')}`;
+                return [fixer.insertTextAfterRange(reactComponentsImportNodeRange, importsToAdd), fixer.remove(imprt)];
               },
             });
           }
-        }
-      },
-      onCodePathEnd: (/** @type {any} */ codePath, /** @type {Identifier} */ node) => {
-        if (importedIdentifiers.length) {
-          const importsToAdd = importedIdentifiers.map(specifier => specifier.local.name).join(', ');
-          context.report({
-            node: node,
-            messageId: 'crossPackageImport',
-            fix: fixer => {
-              return fixer.insertTextAfterRange(reactComponentsImportNodeRange, `, ${importsToAdd}`);
-            },
-          });
         }
       },
     };
