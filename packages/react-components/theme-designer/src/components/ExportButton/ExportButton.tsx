a374af505a79f7548fd62ae1d0fc7ce7944fe6ed
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2022-06-13 16:42:24 -0700

    theme designer: export theme to codesandbox (#23522)
    * temp progress (broken)
    
    * export to codesandbox
    
    * suggested fixes
    
    * codesandbox to exportlink
    
    * delete lockfile
    
    * updated lock file
    
    * fix nav
    
    Co-authored-by: Micah Godbolt <mgodbolt@microsoft.com>

---
+++ packages/react-components/theme-designer/src/components/ExportButton/ExportButton.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { makeStyles } from '@griffel/react';
+import {
+  BrandVariants,
+  Menu,
+  MenuTrigger,
+  MenuButton,
+  MenuPopover,
+  MenuList,
+  MenuItem,
+} from '@fluentui/react-components';
+import { ExportLink } from './ExportLink';
+
+export interface ExportProps {
+  className?: string;
+  brand: BrandVariants;
+}
+
+const useStyles = makeStyles({
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingRight: '20px',
+  },
+});
+
+export const ExportButton: React.FC<ExportProps> = props => {
+  const styles = useStyles();
+  return (
+    <div className={styles.root}>
+      <Menu>
+        <MenuTrigger>
+          <MenuButton size="small" appearance="outline">
+            Save
+          </MenuButton>
+        </MenuTrigger>
+
+        <MenuPopover>
+          <MenuList>
+            <MenuItem>
+              <ExportLink brand={props.brand} isLightTheme={true} />
+            </MenuItem>
+            <MenuItem>
+              <ExportLink brand={props.brand} isLightTheme={false} />
+            </MenuItem>
+          </MenuList>
+        </MenuPopover>
+      </Menu>
+    </div>
+  );
+};
