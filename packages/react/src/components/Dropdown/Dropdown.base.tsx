82337ca348673485e78b9072431c00c222cd985a
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2022-06-14 12:23:07 -0700

    fix: Adding close button to Dropdown list when it opens as a panel. (#23524)
    * fix: Adding close button to Dropdown list when it opens as a panel.
    
    * Adding change file.
    
    * Addressing PR feedback.

--- packages/react/src/components/Dropdown/Dropdown.base.tsx
+++ packages/react/src/components/Dropdown/Dropdown.base.tsx
@@ -48,6 +48,7 @@ import type { IPanelStyleProps, IPanelStyles } from '../../Panel';
 import type { IWithResponsiveModeState } from '../../ResponsiveMode';
 import type { ISelectableDroppableTextProps } from '../../SelectableOption';
 import type { ICheckboxStyleProps, ICheckboxStyles } from '../../Checkbox';
+import { IFocusTrapZoneProps } from '../FocusTrapZone/FocusTrapZone.types';
 
 const COMPONENT_NAME = 'Dropdown';
 const getClassNames = classNamesFunction<IDropdownStyleProps, IDropdownStyles>();
@@ -594,6 +595,7 @@ class DropdownInternal extends React.Component<IDropdownInternalProps, IDropdown
 
     const isSmall = responsiveMode! <= ResponsiveMode.medium;
 
+    const focusTrapZoneProps: IFocusTrapZoneProps = { firstFocusableTarget: `#${this._listId}1` };
     const panelStyles = this._classNames.subComponentStyles
       ? (this._classNames.subComponentStyles.panel as IStyleFunctionOrObject<IPanelStyleProps, IPanelStyles>)
       : undefined;
@@ -608,10 +610,12 @@ class DropdownInternal extends React.Component<IDropdownInternalProps, IDropdown
 
     return isSmall ? (
       <Panel
+        closeButtonAriaLabel="Close"
+        focusTrapZoneProps={focusTrapZoneProps}
+        hasCloseButton
         isOpen={true}
         isLightDismiss={true}
         onDismiss={this._onDismiss}
-        hasCloseButton={false}
         styles={panelStyles}
         {...panelProps}
       >
