5b8b939fba992f489f587c15c619aa9f0bcea2f8
KHMakoto <Humberto.Morimoto@microsoft.com>
2022-06-15 11:53:56 -0700

    Removing irrelevant changes.

--- packages/dom-utilities/src/elementContains.ts
+++ packages/dom-utilities/src/elementContains.ts
@@ -1,5 +1,4 @@
 import { getParent } from './getParent';
-
 /**
  * Determines whether or not a parent element contains a given child element.
  * If `allowVirtualParents` is true, this method may return `true` if the child
