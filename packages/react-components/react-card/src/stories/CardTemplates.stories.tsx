1d2a51f1c31413250df31ec926057235df5ed4d2
André <39736248+andrefcdias@users.noreply.github.com>
2022-06-16 16:45:13 +0200

    chore: add new template

--- packages/react-components/react-card/src/stories/CardTemplates.stories.tsx
+++ packages/react-components/react-card/src/stories/CardTemplates.stories.tsx
@@ -7,6 +7,8 @@ import { Button } from '@fluentui/react-button';
 import { Text, Caption1 } from '@fluentui/react-text';
 import { Card, CardHeader } from '../';
 import AppLogo from '../../assets/app_logo.svg';
+import Logo1 from '../../assets/logo.svg';
+import Logo2 from '../../assets/logo2.svg';
 
 export const ASSET_URL =
   'https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-components/react-card';
@@ -32,6 +34,22 @@ const useStyles = makeStyles({
     [`> img`]: {
       ...shorthands.borderRadius('4px'),
     },
+  },
+  example3Header: {
+    display: 'flex',
+    flexDirection: 'row',
+    ...shorthands.gap('4px'),
+    justifyContent: 'flex-start',
+
+    '> *': {
+      height: '32px',
+      ...shorthands.borderRadius('4px'),
+    },
+  },
+  example3Footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
@@ -71,6 +89,20 @@ export const Templates = () => {
           Donut chocolate bar oat cake. Dragée tiramisu lollipop bear claw. Marshmallow pastry jujubes toffee sugar
           plum.
         </span>
+      </Card>
+      <Card className={styles.size}>
+        <div className={styles.example3Header}>
+          <img src={Logo1} />
+          <img src={Logo2} />
+        </div>
+        <CardHeader
+          header={<Text weight="semibold">Alert in Teams when a new document is uploaded in channel</Text>}
+          description={<Caption1 className={styles.caption}>By Microsoft</Caption1>}
+        />
+        <div className={styles.example3Footer}>
+          <span>Automated</span>
+          <span>3290</span>
+        </div>
       </Card>
     </div>
   );
