5f0c1dd31dcb388d57ee5f2b3190fadf5a1be76e
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2022-06-16 11:25:55 -0700

    theme designer: edit tab (sidebar) (#23571)
    * to sentence case
    
    * to sentence case
    
    * accessibility list changes
    
    * get edit tab of sidebar online
    
    * Update Sidebar.tsx

--- packages/react-components/theme-designer/src/ThemeDesigner.tsx
+++ packages/react-components/theme-designer/src/ThemeDesigner.tsx
@@ -4,6 +4,7 @@ import type { ThemeDesignerProps } from './ThemeDesigner.types';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
 import { createLightTheme, createDarkTheme } from '@fluentui/react-theme';
 import { getBrandTokensFromPalette } from './utils/getBrandTokensFromPalette';
+import { brandWeb, brandTeams, brandOffice } from './utils/brandColors';
 import { useDebounce } from './utils/useDebounce';
 
 import { Nav } from './components/Nav/Nav';
@@ -40,23 +41,68 @@ const useStyles = makeStyles({
 export const ThemeDesigner: React.FC<ThemeDesignerProps> = props => {
   const styles = useStyles();
 
+  const [theme, setTheme] = React.useState<string>('Custom');
   const [keyColor, setKeyColor] = React.useState<string>('#006bc7');
+  const [hueTorsion, setHueTorsion] = React.useState<number>(0);
+  const [darkCp, setDarkCp] = React.useState<number>(2 / 3);
+  const [lightCp, setLightCp] = React.useState<number>(1 / 3);
+
   const debouncedKeyColor = useDebounce(keyColor, 400);
 
-  const themes = React.useMemo(() => {
-    const brand = getBrandTokensFromPalette(debouncedKeyColor);
+  const customTheme = React.useMemo(() => {
+    const brand = getBrandTokensFromPalette(debouncedKeyColor, {
+      hueTorsion: hueTorsion,
+      darkCp: darkCp,
+      lightCp: lightCp,
+    });
     return {
       brand: brand,
       lightTheme: createLightTheme(brand),
       darkTheme: createDarkTheme(brand),
     };
-  }, [debouncedKeyColor]);
+  }, [hueTorsion, darkCp, lightCp, debouncedKeyColor]);
+
+  const themes = React.useMemo(() => {
+    if (theme === 'Teams') {
+      return {
+        brand: brandTeams,
+        lightTheme: createLightTheme(brandTeams),
+        darkTheme: createDarkTheme(brandTeams),
+      };
+    } else if (theme === 'Web') {
+      return {
+        brand: brandWeb,
+        lightTheme: createLightTheme(brandWeb),
+        darkTheme: createDarkTheme(brandWeb),
+      };
+    } else if (theme === 'Office') {
+      return {
+        brand: brandOffice,
+        lightTheme: createLightTheme(brandOffice),
+        darkTheme: createDarkTheme(brandOffice),
+      };
+    } else {
+      return customTheme;
+    }
+  }, [customTheme, theme]);
 
   return (
     <FluentProvider theme={teamsLightTheme}>
       <div className={styles.root}>
         <Nav className={styles.nav} brand={themes.brand} />
-        <Sidebar className={styles.sidebar} keyColor={debouncedKeyColor} setKeyColor={setKeyColor} />
+        <Sidebar
+          className={styles.sidebar}
+          keyColor={debouncedKeyColor}
+          setKeyColor={setKeyColor}
+          hueTorsion={hueTorsion}
+          setHueTorsion={setHueTorsion}
+          darkCp={darkCp}
+          setDarkCp={setDarkCp}
+          lightCp={lightCp}
+          setLightCp={setLightCp}
+          theme={theme}
+          setTheme={setTheme}
+        />
         <Content
           className={styles.content}
           brand={themes.brand}
--- packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
+++ packages/react-components/theme-designer/src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,36 @@
 import * as React from 'react';
 import { makeStyles, mergeClasses, shorthands } from '@griffel/react';
-import { Button, TabList, Tab, Label, Input, useId, Subtitle2, tokens } from '@fluentui/react-components';
-import { AddCircleRegular } from '@fluentui/react-icons';
+import {
+  Menu,
+  MenuTrigger,
+  Button,
+  MenuPopover,
+  MenuList,
+  MenuItemRadio,
+  MenuDivider,
+  MenuProps,
+  TabValue,
+  TabList,
+  Tab,
+  Label,
+  Input,
+  useId,
+  tokens,
+  Slider,
+} from '@fluentui/react-components';
 
 export interface SidebarProps {
   className?: string;
   keyColor: string;
   setKeyColor: React.Dispatch<React.SetStateAction<string>>;
+  hueTorsion: number;
+  setHueTorsion: React.Dispatch<React.SetStateAction<number>>;
+  darkCp: number;
+  setDarkCp: React.Dispatch<React.SetStateAction<number>>;
+  lightCp: number;
+  setLightCp: React.Dispatch<React.SetStateAction<number>>;
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const useStyles = makeStyles({
@@ -41,6 +65,12 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     alignItems: 'flex-start',
   },
+  inlineInputs: {
+    display: 'flex',
+    columnGap: '1em',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   keyColor: {
     paddingLeft: '0px',
   },
@@ -66,88 +96,118 @@ const useStyles = makeStyles({
 export const Sidebar: React.FC<SidebarProps> = props => {
   const styles = useStyles();
 
+  const [tab, setTab] = React.useState<TabValue>('use');
+  const handleTabChange = React.useCallback(
+    (event, data) => {
+      props.setTheme('Custom');
+      setTab(data.value);
+    },
+    [props],
+  );
+
   const keyColorId = useId();
-  const lightThemeId = useId();
-  const darkThemeId = useId();
-
-  const [lightTheme, setLightTheme] = React.useState<string>('#FFFFFF');
-  const changeLightTheme = React.useCallback(e => setLightTheme(e.target.value), [setLightTheme]);
-
-  const [darkTheme, setDarkTheme] = React.useState<string>('#000000');
-  const changeDarkTheme = React.useCallback(e => setDarkTheme(e.target.value), [setDarkTheme]);
-
   const handleOnChange = React.useCallback(e => props.setKeyColor(e.target.value), [props]);
 
-  return (
-    <div className={mergeClasses(styles.root, props.className)}>
-      <TabList className={styles.tabs} size="medium" defaultSelectedValue="use">
-        <Tab className={styles.tab} value="use">
-          Use
-        </Tab>
-        <Tab disabled className={styles.tab} value="edit">
-          Edit
-        </Tab>
-      </TabList>
-      <div className={styles.content}>
-        <div className={styles.inputs}>
-          <Label htmlFor={keyColorId}>Key color value</Label>
-          <div className={styles.labels}>
-            <Input
-              className={styles.keyColor}
-              size="large"
-              appearance="underline"
-              id={keyColorId}
-              value={props.keyColor}
-              onChange={handleOnChange}
-            />
-            <div className={styles.colorPicker} style={{ backgroundColor: props.keyColor }}>
-              <input className={styles.color} type="color" id={keyColorId} onChange={handleOnChange} />
-            </div>
-          </div>
-        </div>
-        <Subtitle2>Contrast references</Subtitle2>
-        <div className={styles.inputs}>
-          <Label htmlFor={lightThemeId}>Light theme</Label>
-          <div className={styles.labels}>
-            <Input
-              size="small"
-              appearance="underline"
-              id={lightThemeId}
-              value={lightTheme}
-              onChange={changeLightTheme}
-            />
-            <div className={styles.colorPicker} style={{ backgroundColor: lightTheme }}>
-              <input
-                disabled={true}
-                className={styles.color}
-                type="color"
-                id={lightThemeId}
-                value={lightTheme}
-                onChange={changeLightTheme}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.inputs}>
-          <Label htmlFor={darkThemeId}>Dark theme</Label>
-          <div className={styles.labels}>
-            <Input size="small" appearance="underline" id={darkThemeId} value={darkTheme} onChange={changeDarkTheme} />
-            <div className={styles.colorPicker} style={{ backgroundColor: darkTheme }}>
-              <input
-                disabled={true}
-                className={styles.color}
-                type="color"
-                id={darkThemeId}
-                value={darkTheme}
-                onChange={changeDarkTheme}
-              />
-            </div>
+  const hueTorsionId = useId();
+  const handleHueTorsionChange = React.useCallback(e => props.setHueTorsion(e.target.value / 10), [props]);
+
+  const lightCpId = useId();
+  const handleLightCpChange = React.useCallback(e => props.setLightCp(e.target.value / 100), [props]);
+
+  const darkCpId = useId();
+  const handleDarkCpChange = React.useCallback(e => props.setDarkCp(e.target.value / 100), [props]);
+
+  const themeId = useId();
+  const handleThemeChange: MenuProps['onCheckedValueChange'] = React.useCallback(
+    (e, data) => {
+      props.setTheme(data.checkedItems[0] as string);
+    },
+    [props],
+  );
+
+  const Use = React.memo(() => (
+    <div className={styles.content} role="tabpanel" aria-labelledby="Use">
+      <div className={styles.inlineInputs}>
+        <Label htmlFor={themeId}>Theme</Label>
+        <Menu onCheckedValueChange={handleThemeChange}>
+          <MenuTrigger>
+            <Button>{props.theme}</Button>
+          </MenuTrigger>
+          <MenuPopover>
+            <MenuList>
+              <MenuItemRadio name="Teams" value="Teams">
+                Teams
+              </MenuItemRadio>
+              <MenuItemRadio name="Web" value="Web">
+                Web
+              </MenuItemRadio>
+              <MenuItemRadio name="Office" value="Office">
+                Office
+              </MenuItemRadio>
+              <MenuDivider />
+              <MenuItemRadio name="Custom" value="Custom">
+                Custom
+              </MenuItemRadio>
+            </MenuList>
+          </MenuPopover>
+        </Menu>
+      </div>
+    </div>
+  ));
+
+  const Edit = React.memo(() => (
+    <div className={styles.content} role="tabpanel" aria-labelledby="Edit">
+      <div className={styles.inputs}>
+        <Label htmlFor={keyColorId}>Key color value</Label>
+        <div className={styles.labels}>
+          <Input
+            className={styles.keyColor}
+            size="large"
+            appearance="underline"
+            id={keyColorId}
+            value={props.keyColor}
+            onChange={handleOnChange}
+          />
+          <div className={styles.colorPicker} style={{ backgroundColor: props.keyColor }}>
+            <input className={styles.color} type="color" id={keyColorId} onChange={handleOnChange} />
           </div>
         </div>
       </div>
-      <Button appearance="transparent" icon={<AddCircleRegular />} iconPosition="before">
-        Add background colors
-      </Button>
+      <Label htmlFor={hueTorsionId}>Hue Torsion</Label>
+      <Slider
+        size="small"
+        min={-50}
+        max={50}
+        id={hueTorsionId}
+        value={props.hueTorsion * 10}
+        onChange={handleHueTorsionChange}
+      />
+      <Label htmlFor={lightCpId}>Light Control Point</Label>
+      <Slider
+        size="small"
+        min={0}
+        max={100}
+        id={lightCpId}
+        value={props.lightCp * 100}
+        onChange={handleLightCpChange}
+      />
+      <Label htmlFor={darkCpId}>Dark Control Point</Label>
+      <Slider size="small" min={0} max={100} id={darkCpId} value={props.darkCp * 100} onChange={handleDarkCpChange} />
+    </div>
+  ));
+
+  return (
+    <div className={mergeClasses(styles.root, props.className)}>
+      <TabList className={styles.tabs} size="medium" selectedValue={tab} onTabSelect={handleTabChange}>
+        <Tab className={styles.tab} value="use">
+          Use
+        </Tab>
+        <Tab className={styles.tab} value="edit">
+          Edit
+        </Tab>
+      </TabList>
+      {tab === 'use' && <Use />}
+      {tab === 'edit' && <Edit />}
     </div>
   );
 };
