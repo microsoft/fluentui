22542e9e7fa76cb4f1a3c35c2b760c0d727d6cd5
Sean Monahan <seanmonahan@microsoft.com>
2022-06-16 15:40:47 -0700

    update docs import paths

--- apps/fabric-website/src/pages/Controls/DetailsListPage/DetailsListKeyboardOverrides.doc.ts
+++ apps/fabric-website/src/pages/Controls/DetailsListPage/DetailsListKeyboardOverrides.doc.ts
@@ -1,5 +1,5 @@
 import { TFabricPlatformPageProps } from '../../../interfaces/Platforms';
-import { DetailsListKeyboardOverridesProps as ExternalProps } from '@fluentui/react-examples/lib/react/DetailsList/DetailsList.doc';
+import { DetailsListKeyboardOverridesProps as ExternalProps } from '@fluentui/react-examples/lib/office-ui-fabric-react/DetailsList/DetailsList.doc';
 
 export const DetailsListKeyboardOverridesProps: TFabricPlatformPageProps = {
   web: {
