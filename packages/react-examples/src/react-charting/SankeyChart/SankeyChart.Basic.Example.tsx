a9cb2f3bf15a129519f6de57240f0a0830a78c02
AnnapareddyHarsha <v-hannapared@microsoft.com>
2022-06-22 11:52:48 +0530

    labels are changed to sentence case

--- packages/react-examples/src/react-charting/SankeyChart/SankeyChart.Basic.Example.tsx
+++ packages/react-examples/src/react-charting/SankeyChart/SankeyChart.Basic.Example.tsx
@@ -116,9 +116,9 @@ export class SankeyChartBasicExample extends React.Component<{}, ISankeyChartBas
 
     return (
       <>
-        <label>change Width:</label>
+        <label>Change Width:</label>
         <input type="range" value={this.state.width} min={200} max={1000} onChange={this._onWidthChange} />
-        <label>change Height:</label>
+        <label>Change Height:</label>
         <input type="range" value={this.state.height} min={200} max={1000} onChange={this._onHeightChange} />
         <div style={rootStyle}>
           <SankeyChart
