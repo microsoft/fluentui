008c392ad05377e422dd887ccb3ff96e8c3722c3
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-06-23 11:01:28 -0700

    Update main.js & move stories to stories/Dialog

--- packages/react-components/react-dialog/src/stories/Dialog/DialogDefault.stories.tsx
+++ packages/react-components/react-dialog/src/stories/Dialog/DialogDefault.stories.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
-import { Dialog, DialogProps } from '../index';
+import { Dialog, DialogProps } from '@fluentui/react-dialog';
 
 export const Default = (props: Partial<DialogProps>) => <Dialog {...props} />;
