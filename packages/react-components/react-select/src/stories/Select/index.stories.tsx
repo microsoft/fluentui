f692f0f52e3cdf6f624f5140a01b8a103de33df3
Micah Godbolt <mgodbolt@microsoft.com>
2022-06-23 08:47:53 -0700

    fix markdown import

--- packages/react-components/react-select/src/stories/Select/index.stories.tsx
+++ packages/react-components/react-select/src/stories/Select/index.stories.tsx
@@ -1,5 +1,5 @@
 import { Select } from '@fluentui/react-select';
-import selectDescription from '../SelectDescription.md';
+import selectDescription from './SelectDescription.md';
 
 import bestPracticesMd from './SelectBestPractices.md';
 
