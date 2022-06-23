a25f3a930c21e8311a7e14ad2ce02946b2b572dd
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-06-23 12:41:23 -0700

    chore: Update story title

--- packages/react-components/react-theme/src/stories/Theme/index.stories.tsx
+++ packages/react-components/react-theme/src/stories/Theme/index.stories.tsx
@@ -9,7 +9,7 @@ export { Spacing } from './ThemeSpacing.stories';
 export { StrokeWidths } from './ThemeStrokeWidths.stories';
 
 export default {
-  title: 'Theme',
+  title: 'Theme/Theme',
   parameters: {
     docs: {
       description: {},
