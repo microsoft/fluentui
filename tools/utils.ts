23c8000d2b094b6843dad8271a9ec9baa9e100c3
Lingfan Gao <lingfangao@hotmail.com>
2022-06-24 15:22:45 +0200

    use semver to parse version string

--- tools/utils.ts
+++ tools/utils.ts
@@ -11,6 +11,7 @@ import {
   readJson,
 } from '@nrwl/devkit';
 import { PackageJson } from './types';
+import * as semver from 'semver';
 
 /**
  * CLI prompts abstraction to trigger dynamic prompts within a generator
@@ -183,8 +184,12 @@ export function hasSchemaFlag<T, K extends keyof T>(schema: T, flag: K): schema 
   return Boolean(schema[flag]);
 }
 
-export function isPackageVersionConverged(version: string) {
-  return version.startsWith('9.');
+export function isPackageVersionConverged(versionString: string) {
+  const version = semver.parse(versionString);
+  if (version === null) {
+    throw new Error(`${versionString} is not a valid semver version`);
+  }
+  return version.major === 9;
 }
 
 export function isPackageConverged(tree: Tree, project: ProjectConfiguration) {
