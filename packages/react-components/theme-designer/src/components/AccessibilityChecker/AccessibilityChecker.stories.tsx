6f4c32825d6a316e8bc72c675cd43c05001b086b
Emma Jiang <eyj.thunder@gmail.com>
2022-06-27 10:03:58 -0700

    update defaults

--- packages/react-components/theme-designer/src/components/AccessibilityChecker/AccessibilityChecker.stories.tsx
+++ packages/react-components/theme-designer/src/components/AccessibilityChecker/AccessibilityChecker.stories.tsx
@@ -1,9 +1,9 @@
 import { AccessibilityChecker } from './AccessibilityChecker';
 import { getBrandTokensFromPalette } from '../../utils/getBrandTokensFromPalette';
-import { createDarkTheme } from '@fluentui/react-components';
+import { createLightTheme } from '@fluentui/react-components';
 export default { component: AccessibilityChecker };
 
 const brand = getBrandTokensFromPalette('#006bc7');
-const lightTheme = createDarkTheme(brand);
+const lightTheme = createLightTheme(brand);
 
 export const Default = { args: { theme: lightTheme } };
