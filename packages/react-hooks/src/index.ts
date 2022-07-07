d31db166e558eaf61e4ef7ae8e61cc02e3fa033f
Thomas Michon <tmichon@microsoft.com>
2022-07-07 13:58:35 -0700

    Fix FocusTrapZone tab looping out of an iframe (#23792)
    * Handle when FocusTrapZone wraps an iframe
    
    * Update changelogs
    
    * Fix linter issues

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -4,6 +4,7 @@ export * from './useBoolean';
 export * from './useConst';
 export * from './useConstCallback';
 export * from './useControllableValue';
+export * from './useEventCallback';
 export * from './useForceUpdate';
 export * from './useId';
 export * from './useMergedRefs';
