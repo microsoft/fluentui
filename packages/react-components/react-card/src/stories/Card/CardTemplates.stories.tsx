3fe43d06e72b6d4ebc916e2fdc3def3c84f14d25
André <39736248+andrefcdias@users.noreply.github.com>
2022-07-07 19:56:38 +0200

    chore: fix imports and CardHeader usage

--- packages/react-components/react-card/src/stories/Card/CardTemplates.stories.tsx
+++ packages/react-components/react-card/src/stories/Card/CardTemplates.stories.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
-import { makeStyles, shorthands } from '@griffel/react';
-import { tokens } from '@fluentui/react-theme';
+import { makeStyles, shorthands, tokens, Button, Text, Caption1 } from '@fluentui/react-components';
 import { MoreHorizontal16Filled } from '@fluentui/react-icons';
-import { Button } from '@fluentui/react-button';
-import { Text, Caption1 } from '@fluentui/react-text';
-import { Card, CardHeader } from '../';
-import AppLogo from '../../assets/app_logo.svg';
-import Logo1 from '../../assets/logo.svg';
-import Logo2 from '../../assets/logo2.svg';
+import { Card, CardHeader } from '@fluentui/react-card';
+import AppLogo from '../../../assets/app_logo.svg';
+import Logo1 from '../../../assets/logo.svg';
+import Logo2 from '../../../assets/logo2.svg';
 
 export const ASSET_URL =
   'https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-components/react-card';
@@ -30,10 +27,7 @@ const useStyles = makeStyles({
   headerImage: {
     maxWidth: '44px',
     maxHeight: '44px',
-
-    [`> img`]: {
-      ...shorthands.borderRadius('4px'),
-    },
+    ...shorthands.borderRadius('4px'),
   },
   example3Header: {
     display: 'flex',
@@ -60,7 +54,7 @@ export const Templates = () => {
     <div className={styles.container}>
       <Card className={styles.size}>
         <CardHeader
-          image={<img src={powerpointLogoURL} />}
+          image={{ as: 'img', src: powerpointLogoURL }}
           header={<Text weight="semibold">Team offsite 2020</Text>}
           description={<Caption1 className={styles.caption}>Onedrive &gt; Files</Caption1>}
           action={
@@ -74,7 +68,7 @@ export const Templates = () => {
       </Card>
       <Card className={styles.size}>
         <CardHeader
-          image={{ className: styles.headerImage, children: <img src={AppLogo} /> }}
+          image={{ as: 'img', className: styles.headerImage, src: AppLogo }}
           header={<Text weight="semibold">App Name</Text>}
           description={<Caption1 className={styles.caption}>Developer</Caption1>}
           action={
