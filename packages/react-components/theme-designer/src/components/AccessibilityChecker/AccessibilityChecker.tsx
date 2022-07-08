9010153202c7efdcbf5e63cf61c3f838c1f11753
Emma Jiang <eyj.thunder@gmail.com>
2022-07-08 08:52:45 -0700

    revert to old contrast

--- packages/react-components/theme-designer/src/components/AccessibilityChecker/AccessibilityChecker.tsx
+++ packages/react-components/theme-designer/src/components/AccessibilityChecker/AccessibilityChecker.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AccessibilityList } from './AccessibilityList';
-import { contrast, hex_to_sRGB, Vec3 } from '@fluent-blocks/colors';
+import { hex_to_sRGB, Vec3 } from '@fluent-blocks/colors';
+import { contrast } from '../../utils/csswg';
 import { Caption1, Theme } from '@fluentui/react-components';
 
 export interface AccessibilityCheckerProps {
