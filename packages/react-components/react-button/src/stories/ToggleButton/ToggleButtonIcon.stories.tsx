bc1d2c1f353b95222b0632145c6dff5c0ed0f12c
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-11 12:09:21 -0400

    Update packages/react-components/react-button/src/stories/ToggleButton/ToggleButtonIcon.stories.tsx
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-button/src/stories/ToggleButton/ToggleButtonIcon.stories.tsx
+++ packages/react-components/react-button/src/stories/ToggleButton/ToggleButtonIcon.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
-import { Tooltip, ToggleButton } from '@fluentui/react-components';
+import { ToggleButton, Tooltip } from '@fluentui/react-components';
 
 const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
 
