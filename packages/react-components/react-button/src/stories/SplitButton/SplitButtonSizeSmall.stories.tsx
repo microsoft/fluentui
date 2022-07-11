efde7923b64065548d9f5eb9ee4716662c7b84ef
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-11 12:09:02 -0400

    Update packages/react-components/react-button/src/stories/SplitButton/SplitButtonSizeSmall.stories.tsx
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-button/src/stories/SplitButton/SplitButtonSizeSmall.stories.tsx
+++ packages/react-components/react-button/src/stories/SplitButton/SplitButtonSizeSmall.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
-import { Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, Tooltip, SplitButton } from '@fluentui/react-components';
+import { Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, SplitButton, Tooltip } from '@fluentui/react-components';
 import type { MenuButtonProps } from '@fluentui/react-components';
 
 const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
