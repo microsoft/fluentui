deb104a3992a55980a488011c3f25fd1130dc310
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-11 12:06:47 -0400

    Update packages/react-components/react-button/src/stories/Button/ButtonIcon.stories.tsx
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-button/src/stories/Button/ButtonIcon.stories.tsx
+++ packages/react-components/react-button/src/stories/Button/ButtonIcon.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
-import { Tooltip, Button } from '@fluentui/react-components';
+import { Button, Tooltip } from '@fluentui/react-components';
 
 const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
 
