f9363b742b2354cbd5f5c5d1e2c67451c59844ae
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-11 10:08:21 -0700

    Update packages/react-components/react-checkbox/src/stories/Checkbox/index.stories.tsx
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-checkbox/src/stories/Checkbox/index.stories.tsx
+++ packages/react-components/react-checkbox/src/stories/Checkbox/index.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Meta } from '@storybook/react';
-import { Checkbox, tokens } from '@fluentui/react-components';
+import { tokens, Checkbox } from '@fluentui/react-components';
 
 export { Default } from './CheckboxDefault.stories';
 export { Checked } from './CheckboxChecked.stories';
