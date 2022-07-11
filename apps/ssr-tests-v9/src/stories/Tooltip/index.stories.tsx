d7a5fbff09f0beda7aabfcd17429b11054bcef9c
Lingfan Gao <lingfangao@hotmail.com>
2022-07-11 12:34:27 +0200

    chore: Add internal SSR test stories to ssr-tests-v9
    Adds the capability to have internal SSR tests that will not appear as a
    part of the public documentation

---
+++ apps/ssr-tests-v9/src/stories/Tooltip/index.stories.tsx
@@ -0,0 +1,1 @@
+export { DefaultOpen } from './DefaultOpen.stories';
--- packages/react-components/react-theme/src/stories/Theme/index.stories.tsx
+++ packages/react-components/react-theme/src/stories/Theme/index.stories.tsx
@@ -2,8 +2,13 @@ import { Meta } from '@storybook/react';
 
 export { BorderRadii } from './ThemeBorderRadii.stories';
 export { Colors } from './ThemeColors.stories';
-export { FontFamily, FontSize, FontWeight, LineHeight, TypographyStyles } from './ThemeTypography.stories';
-export { MotionCurves, MotionDuration } from './ThemeMotion.stories';
+export { FontFamily } from './ThemeTypography.stories';
+export { FontSize } from './ThemeTypography.stories';
+export { FontWeight } from './ThemeTypography.stories';
+export { LineHeight } from './ThemeTypography.stories';
+export { TypographyStyles } from './ThemeTypography.stories';
+export { MotionCurves } from './ThemeMotion.stories';
+export { MotionDuration } from './ThemeMotion.stories';
 export { Shadows } from './ThemeShadows.stories';
 export { Spacing } from './ThemeSpacing.stories';
 export { StrokeWidths } from './ThemeStrokeWidths.stories';
