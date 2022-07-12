6f3ab09c3de072b456b8bfbb25af8202418b3c69
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-12 13:16:42 -0400

    Menu: update stories to import from @fluentui/react-components (#23873)

--- packages/react-components/react-menu/src/stories/Menu/MenuGroupingItems.stories.tsx
+++ packages/react-components/react-menu/src/stories/Menu/MenuGroupingItems.stories.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {
+  Button,
   Menu,
   MenuTrigger,
   MenuList,
@@ -8,9 +9,7 @@ import {
   MenuDivider,
   MenuGroupHeader,
   MenuPopover,
-} from '@fluentui/react-menu';
-
-import { Button } from '@fluentui/react-button';
+} from '@fluentui/react-components';
 import {
   bundleIcon,
   CutRegular,
