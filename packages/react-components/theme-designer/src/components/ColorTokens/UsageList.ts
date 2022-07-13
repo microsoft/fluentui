9ae180b3facb824a2a5118861fdc58eb1dfc13f5
Emma Jiang <eyj.thunder@gmail.com>
2022-07-12 17:15:45 -0700

    fix string length

--- packages/react-components/theme-designer/src/components/ColorTokens/UsageList.ts
+++ packages/react-components/theme-designer/src/components/ColorTokens/UsageList.ts
@@ -13,12 +13,12 @@ export const usageList = {
   colorBrandForegroundLinkHover: 'Link hover',
   colorBrandForegroundLinkPressed: 'Link pressed',
   colorBrandForegroundLinkSelected: 'Link selected',
-  colorCompoundBrandForeground1:
-    'Checkbox mixed icon, Menu custom icon active, Menu custom icon, RadioGroup checked filled rest, Tab transparent selected icon rest, Tab subtle selected icon rest',
-  colorCompoundBrandForeground1Hover:
-    'Checkbox mixed icon hover, Menu custom icon hover, RadioGroup checked filled hover, Tab transparent selected icon hover, Tab subtle selected icon hover',
-  colorCompoundBrandForeground1Pressed:
-    'Checkbox mixed icon pressed, Menu custom icon pressed, RadioGroup checked filled pressed, Tab transparent selected icon pressed, Tab subtle selected icon pressed',
+  colorCompoundBrandForeground1: `Checkbox mixed icon, Menu custom icon active, Menu custom icon, RadioGroup checked
+  filled rest, Tab transparent selected icon rest, Tab subtle selected icon rest`,
+  colorCompoundBrandForeground1Hover: `Checkbox mixed icon hover, Menu custom icon hover, RadioGroup checked filled
+    hover, Tab transparent selected icon hover, Tab subtle selected icon hover`,
+  colorCompoundBrandForeground1Pressed: `Checkbox mixed icon pressed, Menu custom icon pressed, RadioGroup checked
+    filled pressed, Tab transparent selected icon pressed, Tab subtle selected icon pressed`,
   colorBrandForeground1: 'Badge outline, Badge ghost, Divider brand content',
   colorBrandForeground2: 'Badge tint',
   colorBrandForegroundInverted: 'Unused',
@@ -34,10 +34,10 @@ export const usageList = {
   colorBrandBackgroundSelected: 'Button primary selected',
   colorCompoundBrandBackground:
     'Checkbox checked icon, Checkbox checked base, Slider rail rest, Slider thumb rest, Switch checked rest',
-  colorCompoundBrandBackgroundHover:
-    'Checkbox checked icon hover, Checkbox checked base hover, Slider rail hover, Slider thumb hover, Switch checked hover',
-  colorCompoundBrandBackgroundPressed:
-    'Checkbox checked icon pressed, Checkbox checked base pressed, Slider rail pressed, Slider thumb pressed, Switch checked pressed',
+  colorCompoundBrandBackgroundHover: `Checkbox checked icon hover, Checkbox checked base hover, Slider rail hover,
+    Slider thumb hover, Switch checked hover`,
+  colorCompoundBrandBackgroundPressed: `Checkbox checked icon pressed, Checkbox checked base pressed, Slider rail
+    pressed, Slider thumb pressed, Switch checked pressed`,
   colorBrandBackgroundStatic: 'Avatar brand',
   colorBrandBackground2: 'Badge tint',
   colorBrandBackgroundInvertedHover: 'Unused',
@@ -46,10 +46,12 @@ export const usageList = {
   colorNeutralStrokeAccessibleSelected: 'Unused',
   colorBrandStroke1: 'Avatar active, Divider brand base, Spinner primary track',
   colorBrandStroke2: 'Unused',
-  colorCompoundBrandStroke:
-    'Checkbox mixed border, Input filled darker in focus, Input filled lighter in focus, Input underline in focus, Input outline in focus, RadioGroup checked rest, TabList transparent selected indicator rest, TabList subtle selected indicator rest, TextArea filled darker in focus, TextArea filled lighter in focus, TextArea outline in focus',
-  colorCompoundBrandStrokeHover:
-    'Checkbox mixed border hover, RadioGroup checked hover, TabList transparent selected indicator hover, Tablist subtle selected indicator hover',
-  colorCompoundBrandStrokePressed:
-    'Checkbox mixed border pressed, RadioGroup checked pressed, TabList transparent selected indicator pressed, Tablist subtle selected indicator pressed',
+  colorCompoundBrandStroke: `Checkbox mixed border, Input filled darker in focus, Input filled lighter in focus, Input
+    underline in focus, Input outline in focus, RadioGroup checked rest, TabList transparent selected indicator rest,
+    TabList subtle selected indicator rest, TextArea filled darker in focus, TextArea filled lighter in focus, TextArea
+    outline in focus`,
+  colorCompoundBrandStrokeHover: `Checkbox mixed border hover, RadioGroup checked hover, TabList transparent selected
+    indicator hover, Tablist subtle selected indicator hover`,
+  colorCompoundBrandStrokePressed: `Checkbox mixed border pressed, RadioGroup checked pressed, TabList transparent
+    selected indicator pressed, Tablist subtle selected indicator pressed`,
 };
