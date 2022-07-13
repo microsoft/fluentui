e70d4d0f96163d021627b0c8786e78019beb00c6
Micah Godbolt <mgodbolt@microsoft.com>
2022-07-13 14:55:30 -0700

    add all components and add some test stories

--- apps/react18-test/src/App.tsx
+++ apps/react18-test/src/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
-import { Badge } from '@fluentui/react-badge';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { Default } from '../../../packages/react-components/react-button/src/stories/Button/ButtonDefault.stories';
+import { Appearance } from '../../../packages/react-components/react-button/src/stories/Button/ButtonAppearance.stories';
 
 export const App = () => {
-  return <Badge />;
+  return (
+    <FluentProvider theme={webLightTheme}>
+      <Default />
+      <Appearance />
+    </FluentProvider>
+  );
 };
