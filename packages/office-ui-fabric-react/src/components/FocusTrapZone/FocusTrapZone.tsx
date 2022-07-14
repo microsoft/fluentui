f5e1436635c4cd0c109a1f1571ea305268a356b8
Germey <cqc@cuiqingcai.com>
2022-07-15 07:59:54 +0800

    revert wrong logic in _forceClickInTrap

--- packages/office-ui-fabric-react/src/components/FocusTrapZone/FocusTrapZone.tsx
+++ packages/office-ui-fabric-react/src/components/FocusTrapZone/FocusTrapZone.tsx
@@ -359,18 +359,8 @@ export class FocusTrapZone extends React.Component<IFocusTrapZoneProps, {}> impl
       const clickedElement = ev.target as HTMLElement;
 
       if (clickedElement && !elementContains(this._root.current, clickedElement)) {
-        const doc = this._getDocument();
-        if (doc && doc.activeElement === doc.body) {
-          setTimeout(() => {
-            if (doc && doc.activeElement === doc.body) {
-              this.focus();
-              this._hasFocus = true;
-            }
-          }, 0);
-        } else {
-          this.focus();
-          this._hasFocus = true; // set focus here since we stop event propagation
-        }
+        this.focus();
+        this._hasFocus = true; // set focus here since we stop event propagation
         ev.preventDefault();
         ev.stopPropagation();
       }
