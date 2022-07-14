fb8376322f3c656d82117e15e99cf2eb6e2163b7
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-14 11:47:02 -0400

    Label: update stories to import from @fluentui/react-components (#23907)

--- packages/react-components/react-label/src/stories/Label/LabelWeight.stories.tsx
+++ packages/react-components/react-label/src/stories/Label/LabelWeight.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Label } from '@fluentui/react-label';
+import { Label } from '@fluentui/react-components';
 
 export const Weight = () => <Label weight="semibold">Strong label</Label>;
 
