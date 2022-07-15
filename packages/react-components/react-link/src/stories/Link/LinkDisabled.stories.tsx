474fc692b5ccc08b9033d48aecab15cbc3b74d2d
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-15 09:35:15 -0400

    Link: update stories to import from @fluentui/react-components (#23908)

--- packages/react-components/react-link/src/stories/Link/LinkDisabled.stories.tsx
+++ packages/react-components/react-link/src/stories/Link/LinkDisabled.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from '@fluentui/react-link';
+import { Link } from '@fluentui/react-components';
 
 export const Disabled = () => (
   <Link disabled href="https://www.bing.com">
