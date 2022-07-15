c4dc12ffe0512ca091d87d0088b2a8eccb3d31da
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-07-15 08:20:25 -0700

    import from react-components

--- packages/react-components/react-spinner/src/stories/Spinner/SpinnerSize.stories.tsx
+++ packages/react-components/react-spinner/src/stories/Spinner/SpinnerSize.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { makeStyles, shorthands } from '@griffel/react';
-import { Spinner } from '@fluentui/react-spinner';
+import { makeStyles, shorthands, Spinner } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
   container: {
