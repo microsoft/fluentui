d12bf72cbb28fb230f634839d2172225c2864806
Juraj Kapsiar <jukapsia@microsoft.com>
2022-07-19 16:38:28 +0200

    keep query on click outside of dropdown

--- packages/fluentui/react-northstar/src/components/Dropdown/Dropdown.tsx
+++ packages/fluentui/react-northstar/src/components/Dropdown/Dropdown.tsx
@@ -999,7 +999,17 @@ export const Dropdown = (React.forwardRef<HTMLDivElement, DropdownProps>((props,
       case Downshift.stateChangeTypes.mouseUp:
         if (open) {
           newState.open = false;
-          newState.highlightedIndex = null;
+          if (allowFreeform) {
+            const itemIndex = items.findIndex(i => itemToString(i)?.startsWith(searchQuery));
+
+            // if there is an item that starts with searchQuery, still apply the search query
+            // to do auto complete (you enter '12:', can be completed to '12:00')
+            if (itemIndex !== -1) {
+              newState.searchQuery = itemToString(items[itemIndex]);
+            }
+          } else {
+            newState.highlightedIndex = null;
+          }
         }
 
         break;
@@ -1019,16 +1029,16 @@ export const Dropdown = (React.forwardRef<HTMLDivElement, DropdownProps>((props,
             listRef.current.focus();
           }
         } else if (allowFreeform) {
-            const itemIndex = items.findIndex(i => itemToString(i)?.startsWith(searchQuery));
+          const itemIndex = items.findIndex(i => itemToString(i)?.startsWith(searchQuery));
 
-            // if there is an item that starts with searchQuery, still apply the search query
-            // to do auto complete (you enter '12:', can be completed to '12:00')
-            if (itemIndex !== -1) {
-              newState.searchQuery = itemToString(items[itemIndex]);
-            }
-          } else {
-            newState.highlightedIndex = null;
+          // if there is an item that starts with searchQuery, still apply the search query
+          // to do auto complete (you enter '12:', can be completed to '12:00')
+          if (itemIndex !== -1) {
+            newState.searchQuery = itemToString(items[itemIndex]);
           }
+        } else {
+          newState.highlightedIndex = null;
+        }
         break;
       case Downshift.stateChangeTypes.itemMouseEnter:
         newState.highlightedIndex = changes.highlightedIndex;
