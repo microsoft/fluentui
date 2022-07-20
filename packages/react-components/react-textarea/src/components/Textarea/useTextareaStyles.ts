4f51549aade9c0c802a36476efc244bc0cf75ab3
Esteban Munoz Facusse <esteban.230@hotmail.com>
2022-07-20 13:24:36 -0600

    Adding story for styling Textarea's height

--- packages/react-components/react-textarea/src/components/Textarea/useTextareaStyles.ts
+++ packages/react-components/react-textarea/src/components/Textarea/useTextareaStyles.ts
@@ -120,22 +120,6 @@ const useRootStyles = makeStyles({
       borderBottomColor: tokens.colorCompoundBrandStroke,
     },
   },
-
-  small: {
-    height: textareaHeight.small,
-    maxHeight: '200px',
-    minHeight: '40px',
-  },
-  medium: {
-    height: textareaHeight.medium,
-    maxHeight: '260px',
-    minHeight: '52px',
-  },
-  large: {
-    height: textareaHeight.large,
-    maxHeight: '320px',
-    minHeight: '64px',
-  },
 });
 
 /**
@@ -169,6 +153,8 @@ const useTextareaStyles = makeStyles({
   // The padding style adds both content and regular padding (from design spec), this is because the handle is not
   // affected by changing the padding of the root.
   small: {
+    height: textareaHeight.small,
+    minHeight: '40px',
     ...shorthands.padding(
       tokens.spacingVerticalXS,
       `calc(${tokens.spacingHorizontalSNudge} + ${tokens.spacingHorizontalXXS})`,
@@ -176,6 +162,8 @@ const useTextareaStyles = makeStyles({
     ...typographyStyles.caption1,
   },
   medium: {
+    height: textareaHeight.medium,
+    minHeight: '52px',
     ...shorthands.padding(
       tokens.spacingVerticalSNudge,
       `calc(${tokens.spacingHorizontalMNudge} + ${tokens.spacingHorizontalXXS})`,
@@ -183,6 +171,8 @@ const useTextareaStyles = makeStyles({
     ...typographyStyles.body1,
   },
   large: {
+    height: textareaHeight.large,
+    minHeight: '64px',
     ...shorthands.padding(
       tokens.spacingVerticalS,
       `calc(${tokens.spacingHorizontalM} + ${tokens.spacingHorizontalXXS})`,
@@ -222,7 +212,6 @@ export const useTextareaStyles_unstable = (state: TextareaState): TextareaState 
     textareaClassNames.root,
     rootStyles.base,
     rootStyles[appearance],
-    rootStyles[size],
     disabled && rootStyles.disabled,
     !disabled && rootStyles.interactive,
     !disabled && appearance === 'outline' && rootStyles.outlineInteractive,
