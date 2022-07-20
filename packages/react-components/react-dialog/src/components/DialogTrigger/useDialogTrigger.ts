f510aae1872b24279d0b828cf249b9d046f29de1
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-07-20 08:31:23 +0000

    Adds DialogTrigger implementation

---
+++ packages/react-components/react-dialog/src/components/DialogTrigger/useDialogTrigger.ts
@@ -0,0 +1,61 @@
+import { useModalAttributes } from '@fluentui/react-tabster';
+import { applyTriggerPropsToChildren, getTriggerChild, useEventCallback } from '@fluentui/react-utilities';
+import * as React from 'react';
+import { useDialogContext_unstable } from '../../contexts/dialogContext';
+import { isTargetDisabled } from '../../utils/isTargetDisabled';
+import {
+  DialogTriggerChildProps,
+  DialogTriggerProps,
+  DialogTriggerState,
+  DialogTriggerAction,
+} from './DialogTrigger.types';
+
+/**
+ * Create the state required to render DialogTrigger.
+ * Clones the only child component and adds necessary event handling behaviours to open a popup Dialog
+ *
+ * @param props - props from this instance of DialogTrigger
+ */
+export const useDialogTrigger_unstable = (props: DialogTriggerProps): DialogTriggerState => {
+  const { children, action = 'toggle' } = props;
+
+  const child = React.isValidElement(children) ? getTriggerChild(children) : undefined;
+
+  const requestOpenChange = useDialogContext_unstable(ctx => ctx.requestOpenChange);
+
+  const { triggerAttributes } = useModalAttributes();
+
+  const handleClick = useEventCallback((event: React.MouseEvent<HTMLElement>) => {
+    if (isTargetDisabled(event)) {
+      return;
+    }
+    child?.props.onClick?.(event);
+    if (!event.isDefaultPrevented()) {
+      requestOpenChange({
+        event,
+        type: 'triggerClick',
+        open: updateOpen(action),
+      });
+    }
+  });
+
+  return {
+    children: applyTriggerPropsToChildren<DialogTriggerChildProps>(children, {
+      'aria-haspopup': 'dialog',
+      ref: child?.ref as React.Ref<never>,
+      onClick: handleClick,
+      ...triggerAttributes,
+    }),
+  };
+};
+
+function updateOpen(type: DialogTriggerAction): React.SetStateAction<boolean> {
+  switch (type) {
+    case 'close':
+      return false;
+    case 'open':
+      return true;
+    case 'toggle':
+      return curr => !curr;
+  }
+}
--- packages/react-components/react-dialog/src/index.ts
+++ packages/react-components/react-dialog/src/index.ts
@@ -6,3 +6,11 @@ export {
   useDialog_unstable,
 } from './Dialog';
 export type { DialogProps, DialogOpenChangeData, DialogOpenChangeEvent, DialogSlots, DialogState } from './Dialog';
+
+export { DialogTrigger, useDialogTrigger_unstable, renderDialogTrigger_unstable } from './DialogTrigger';
+export type {
+  DialogTriggerProps,
+  DialogTriggerChildProps,
+  DialogTriggerState,
+  DialogTriggerAction,
+} from './DialogTrigger';
