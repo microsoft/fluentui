463fd4815d42b6bc22c8d3623ee5b09b34231df9
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-07-21 14:20:01 +0000

    changes

--- packages/react-components/react-menu/src/contexts/menuTriggerContext.ts
+++ packages/react-components/react-menu/src/contexts/menuTriggerContext.ts
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
-const newLocal = false;
 /**
  * Context used communicate with a child menu item that it is a trigger for a submenu
  */
 // eslint-disable-next-line @fluentui/no-context-default-value
-const MenuTriggerContext = React.createContext<boolean>(newLocal);
+const MenuTriggerContext = React.createContext<boolean>(false);
 
 export const MenuTriggerContextProvider = MenuTriggerContext.Provider;
 export const useMenuTriggerContext_unstable = () => React.useContext(MenuTriggerContext);
