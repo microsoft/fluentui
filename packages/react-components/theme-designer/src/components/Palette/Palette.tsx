028557ac8a81153d342cbda644a942d43f220a7c
Emma Jiang <eyj.thunder@gmail.com>
2022-07-21 12:31:50 -0700

    update palette with hex codes

--- packages/react-components/theme-designer/src/components/Palette/Palette.tsx
+++ packages/react-components/theme-designer/src/components/Palette/Palette.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { makeStyles, mergeClasses } from '@griffel/react';
-import { Text, Caption1 } from '@fluentui/react-components';
+import { Button, Caption1, Text } from '@fluentui/react-components';
 import { Brands, BrandVariants } from '@fluentui/react-theme';
+import { contrast, hex_to_sRGB } from '@fluent-blocks/colors';
+import { CopyRegular } from '@fluentui/react-icons';
 
 export interface PaletteProps {
   className?: string;
@@ -16,9 +18,29 @@ const useStyles = makeStyles({
   },
   block: {
     justifyContent: 'left',
+    display: 'grid',
+    gridTemplateColumns: '0.5em auto',
+    gridTemplateRows: '0.5em 1fr 1fr 0.5em',
+  },
+  hexCopy: {
+    justifyContent: 'space-between',
+    gridColumnStart: 2,
+    gridRowStart: 2,
+  },
+  hexColor: {
+    justifyContent: 'left',
+    alignItems: 'flex-start',
+  },
+  copyButton: {
+    justifyContent: 'right',
+    alignItems: 'flex-end',
+  },
+  brandKey: {
+    justifyContent: 'left',
     display: 'flex',
     alignItems: 'flex-end',
-    flexGrow: 1,
+    gridColumnStart: 2,
+    gridRowStart: 3,
   },
 });
 
@@ -29,19 +51,42 @@ const getBrands = (colors: BrandVariants): Brands[] => {
 export const Palette: React.FC<PaletteProps> = props => {
   const styles = useStyles();
 
+  const [shownHex, setShownHex] = React.useState<number>(0);
+
   return (
     <div>
       <Caption1>Generated palette</Caption1>
       <div className={mergeClasses(styles.root, props.className)}>
         {getBrands(props.brandColors).map(brandKey => {
           const brandColor = props.brandColors[brandKey];
+          const textColor = contrast(hex_to_sRGB(brandColor), hex_to_sRGB('#FFFFFF')) <= 4.5 ? 'black' : 'white';
           return (
             <div
               key={brandKey}
               className={styles.block}
-              style={{ backgroundColor: brandColor, color: brandKey <= 100 ? 'white' : 'black' }}
+              style={{
+                backgroundColor: brandColor,
+                color: textColor,
+                flexGrow: shownHex === brandKey ? 0 : 1,
+              }}
+              onMouseEnter={() => setShownHex(brandKey)}
+              onMouseLeave={() => setShownHex(0)}
             >
-              <Text>{brandKey}</Text>
+              {shownHex === brandKey ? (
+                <div className={styles.hexCopy}>
+                  <Text className={styles.hexColor}>{brandColor}</Text>
+                  <Button
+                    size="small"
+                    className={styles.copyButton}
+                    appearance="transparent"
+                    icon={<CopyRegular color={textColor} />}
+                    onClick={() => navigator.clipboard.writeText(brandColor)} // eslint-disable-line react/jsx-no-bind
+                  />
+                </div>
+              ) : (
+                <div />
+              )}
+              <Text className={styles.brandKey}>{brandKey}</Text>
             </div>
           );
         })}
