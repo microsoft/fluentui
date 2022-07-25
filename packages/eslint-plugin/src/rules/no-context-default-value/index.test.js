14d4a9d884d7c84b4fbc51a87b73a3546917172d
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-07-25 07:32:46 +0000

    Fix typo

--- packages/eslint-plugin/src/rules/no-context-default-value/index.test.js
+++ packages/eslint-plugin/src/rules/no-context-default-value/index.test.js
@@ -17,46 +17,46 @@ ruleTester.run('no-context-default-value', rule, {
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
     },
     {
       code: `
         import {createContext as createContext1} from 'react'
-        import {createContext as createContext2} from '@fluenui/react-context-selector'
+        import {createContext as createContext2} from '@fluentui/react-context-selector'
         const context1 = createContext1(undefined);
         const context2 = createContext2(undefined);
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
     },
     {
       code: `
         import * as React from 'react'
-        import * as ContextSelector from '@fluenui/react-context-selector'
+        import * as ContextSelector from '@fluentui/react-context-selector'
         const context1 = React.createContext(undefined);
         const context2 = ContextSelector.createContext(undefined);
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
     },
     {
       code: `
         import React from 'react'
-        import ContextSelector from '@fluenui/react-context-selector'
+        import ContextSelector from '@fluentui/react-context-selector'
         const context1 = React.createContext(undefined);
         const context2 = ContextSelector.createContext(undefined);
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
     },
@@ -65,13 +65,13 @@ ruleTester.run('no-context-default-value', rule, {
     {
       code: `
         import {createContext as createContext1} from 'react'
-        import {createContext as createContext2} from '@fluenui/react-context-selector'
+        import {createContext as createContext2} from '@fluentui/react-context-selector'
         const context1 = createContext1(null);
         const context2 = createContext2(null);
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
       errors: [{ messageId: 'invalidDefaultValue' }, { messageId: 'invalidDefaultValue' }],
@@ -79,13 +79,13 @@ ruleTester.run('no-context-default-value', rule, {
     {
       code: `
         import {createContext as createContext1} from 'react'
-        import {createContext as createContext2} from '@fluenui/react-context-selector'
+        import {createContext as createContext2} from '@fluentui/react-context-selector'
         const context1 = createContext1({});
         const context2 = createContext2({});
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
       errors: [{ messageId: 'invalidDefaultValue' }, { messageId: 'invalidDefaultValue' }],
@@ -93,13 +93,13 @@ ruleTester.run('no-context-default-value', rule, {
     {
       code: `
         import {createContext as createContext1} from 'react'
-        import {createContext as createContext2} from '@fluenui/react-context-selector'
+        import {createContext as createContext2} from '@fluentui/react-context-selector'
         const context1 = createContext1(defaultValue);
         const context2 = createContext2(defaultValue);
       `,
       options: [
         {
-          imports: ['react', '@fluenui/react-context-selector'],
+          imports: ['react', '@fluentui/react-context-selector'],
         },
       ],
       errors: [{ messageId: 'invalidDefaultValue' }, { messageId: 'invalidDefaultValue' }],
