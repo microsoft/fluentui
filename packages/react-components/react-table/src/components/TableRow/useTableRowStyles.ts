aa606161c778cb5733a4ecc8e1f9dee7fca727e5
Lingfan Gao <lingfangao@hotmail.com>
2022-07-25 20:33:53 +0200

    feat: implement TableContext with handling for rows + cells
    Implements `TableContext` and handle the `size` and `noNativeElements`

--- packages/react-components/react-table/src/components/TableRow/useTableRowStyles.ts
+++ packages/react-components/react-table/src/components/TableRow/useTableRowStyles.ts
@@ -14,12 +14,25 @@ export const tableRowClassNames: SlotClassNames<TableRowSlots> = {
 const useStyles = makeStyles({
   root: {
     display: 'flex',
-    minHeight: '44px',
     color: tokens.colorNeutralForeground1,
     ':hover': {
       backgroundColor: tokens.colorNeutralBackground1Hover,
     },
+  },
+
+  medium: {
+    minHeight: '44px',
     ...shorthands.borderBottom('1px', 'solid', tokens.colorNeutralStroke2),
+  },
+
+  small: {
+    minHeight: '34px',
+    ...shorthands.borderBottom('1px', 'solid', tokens.colorNeutralStroke2),
+  },
+
+  smaller: {
+    minHeight: '24px',
+    fontSize: '12px',
   },
 });
 
@@ -28,7 +41,7 @@ const useStyles = makeStyles({
  */
 export const useTableRowStyles_unstable = (state: TableRowState): TableRowState => {
   const styles = useStyles();
-  state.root.className = mergeClasses(tableRowClassNames.root, styles.root, state.root.className);
+  state.root.className = mergeClasses(tableRowClassNames.root, styles.root, styles[state.size], state.root.className);
 
   return state;
 };
--- packages/react-components/react-table/src/stories/Table/index.stories.tsx
+++ packages/react-components/react-table/src/stories/Table/index.stories.tsx
@@ -1,5 +1,7 @@
 export { Default } from './Default.stories';
 export { NonNativeElements } from './NonNativeElements.stories';
+export { SizeSmall } from './SizeSmall.stories';
+export { SizeSmaller } from './SizeSmaller.stories';
 
 export default {
   title: 'Preview Components/Table',
