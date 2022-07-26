a0fa24b4f7a71937b70af488a7bb5e838f38c359
GeoffCoxMSFT <gcox@microsoft.com>
2022-07-26 13:48:30 -0700

    Added exports for decorators missing from v7

---
+++ packages/office-ui-fabric-react/src/utilities/decorators/index.ts
@@ -0,0 +1,13 @@
+export {
+  getInitialResponsiveMode,
+  getResponsiveMode,
+  initializeResponsiveMode,
+  setResponsiveMode,
+  withResponsiveMode,
+} from './withResponsiveMode';
+
+export type { IWithResponsiveModeState, ResponsiveMode } from './withResponsiveMode';
+
+export { withViewport } from './withViewport';
+
+export type { IViewport, IWithViewportProps, IWithViewportState } from './withViewport';
