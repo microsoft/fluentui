61b35deb3af56b0c80b8c62244bbf1a20f1d8c9c
Lingfan Gao <lingfangao@hotmail.com>
2022-07-28 20:35:51 +0200

    remove unused prop

--- packages/react-components/react-table/src/components/Table/useTable.ts
+++ packages/react-components/react-table/src/components/Table/useTable.ts
@@ -26,6 +26,5 @@ export const useTable_unstable = (props: TableProps, ref: React.Ref<HTMLElement>
     size: props.size ?? 'medium',
     noNativeElements: props.noNativeElements ?? false,
     items: props.items ?? [],
-    columns: props.columns ?? [],
   };
 };
