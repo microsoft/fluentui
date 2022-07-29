2e1fa2b6b62803bead236314e96f4f8a0c1c870d
ling1726 <lingfangao@hotmail.com>
2022-07-29 13:07:28 +0200

    feat: Implement TableHeader and TableHeaderCell components (#24139)
    Implements the table header components without sorting support.

---
+++ packages/react-components/react-table/src/components/TableHeader/TableHeader.test.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { TableHeader } from './TableHeader';
+import { isConformant } from '../../common/isConformant';
+import { TableHeaderProps } from './TableHeader.types';
+import { TableContextProvider } from '../../contexts/tableContext';
+
+describe('TableHeader', () => {
+  const table = document.createElement('table');
+  beforeEach(() => {
+    document.body.appendChild(table);
+  });
+  isConformant({
+    Component: TableHeader as React.FC<TableHeaderProps>,
+    displayName: 'TableHeader',
+    renderOptions: {
+      container: table,
+    },
+  });
+
+  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
+
+  it('renders a default state', () => {
+    const result = render(
+      <TableHeader>
+        <tr />
+      </TableHeader>,
+      { container: table },
+    );
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it('renders as div if `noNativeElements` is set', () => {
+    const { container } = render(
+      <TableContextProvider value={{ size: 'medium', noNativeElements: true }}>
+        <TableHeader>
+          <div />
+        </TableHeader>
+      </TableContextProvider>,
+    );
+    expect(container.firstElementChild?.tagName).toEqual('DIV');
+    expect(container.firstElementChild?.getAttribute('role')).toEqual('rowgroup');
+  });
+});
