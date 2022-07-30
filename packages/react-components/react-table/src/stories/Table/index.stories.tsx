598a43ebaf2eb54ad92933a80df0850c56dc3099
Lingfan Gao <lingfangao@hotmail.com>
2022-07-30 09:29:27 +0200

    update stories

--- packages/react-components/react-table/src/stories/Table/index.stories.tsx
+++ packages/react-components/react-table/src/stories/Table/index.stories.tsx
@@ -1,10 +1,10 @@
 import { Table } from '../..';
 
 export { Default } from './Default.stories';
+export { Sort } from './Sort.stories';
 export { NonNativeElements } from './NonNativeElements.stories';
 export { SizeSmall } from './SizeSmall.stories';
 export { SizeSmaller } from './SizeSmaller.stories';
-export { Sort } from './Sort.stories';
 
 export default {
   title: 'Preview Components/Table',
