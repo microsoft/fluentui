b67323e77fe054dfaba09be0e22fb14bcbffa897
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-08-02 18:42:02 +0200

    chore(react-dialog): Adds e2e tests (#24167)
    * chore: Adds e2e tests for `react-dialog`
    
    * cange file
    
    * Adds more tests
    
    * Adds proper selectors to e2e

--- packages/react-components/react-dialog/e2e/Dialog.e2e.tsx
+++ packages/react-components/react-dialog/e2e/Dialog.e2e.tsx
@@ -4,7 +4,9 @@ import { mount as mountBase } from '@cypress/react';
 import { FluentProvider } from '@fluentui/react-provider';
 import { teamsLightTheme } from '@fluentui/react-theme';
 
-import { Dialog } from '@fluentui/react-dialog';
+import { Dialog, DialogActions, DialogBody, DialogSurface, DialogTitle, DialogTrigger } from '@fluentui/react-dialog';
+import { Button } from '@fluentui/react-components';
+import { dialogSurfaceSelector, dialogTriggerCloseSelector, dialogTriggerOpenSelector } from './selectors';
 
 const mount = (element: JSX.Element) => mountBase(<FluentProvider theme={teamsLightTheme}>{element}</FluentProvider>);
 
@@ -12,11 +14,209 @@ describe('Dialog', () => {
   it('should be closed by default', () => {
     mount(
       <Dialog>
-        <div>
-          <button id="close-btn">close</button>
-        </div>
+        <DialogTrigger>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface>
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+            est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+            cumque eaque?
+          </DialogBody>
+          <DialogActions>
+            <DialogTrigger>
+              <Button appearance="secondary">Close</Button>
+            </DialogTrigger>
+            <Button appearance="primary">Do Something</Button>
+          </DialogActions>
+        </DialogSurface>
       </Dialog>,
     );
-    cy.get('#close-btn').should('not.exist');
+    cy.get(dialogTriggerOpenSelector).should('exist');
+    cy.get(dialogSurfaceSelector).should('not.exist');
+  });
+  it('should open when trigger is clicked', () => {
+    mount(
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface>
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+            est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+            cumque eaque?
+          </DialogBody>
+          <DialogActions>
+            <DialogTrigger>
+              <Button appearance="secondary">Close</Button>
+            </DialogTrigger>
+            <Button appearance="primary">Do Something</Button>
+          </DialogActions>
+        </DialogSurface>
+      </Dialog>,
+    );
+    cy.get(dialogTriggerOpenSelector).click();
+    cy.get(dialogSurfaceSelector).should('exist');
+  });
+  it('should focus on first focusabled element when opened', () => {
+    mount(
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface>
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+            est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+            cumque eaque?
+          </DialogBody>
+          <DialogActions>
+            <DialogTrigger>
+              <Button id="close-btn" appearance="secondary">
+                Close
+              </Button>
+            </DialogTrigger>
+            <Button appearance="primary">Do Something</Button>
+          </DialogActions>
+        </DialogSurface>
+      </Dialog>,
+    );
+    cy.get(dialogTriggerOpenSelector).click();
+    cy.get(dialogTriggerCloseSelector).should('be.focused');
+  });
+  it('should focus on body if no focusabled element in dialog', () => {
+    mount(
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface id="dialog-surface">
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+            est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+            cumque eaque?
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>,
+    );
+    cy.get(dialogTriggerOpenSelector).click();
+    cy.focused().should('not.exist');
+  });
+  it('should focus back on trigger when dialog closed', () => {
+    mount(
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface>
+          <DialogTitle>Dialog title</DialogTitle>
+          <DialogBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+            est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+            cumque eaque?
+          </DialogBody>
+          <DialogActions>
+            <DialogTrigger>
+              <Button id="close-btn" appearance="secondary">
+                Close
+              </Button>
+            </DialogTrigger>
+            <Button appearance="primary">Do Something</Button>
+          </DialogActions>
+        </DialogSurface>
+      </Dialog>,
+    );
+    cy.get(dialogTriggerOpenSelector).click();
+    cy.get(dialogTriggerCloseSelector).click();
+    cy.get(dialogTriggerOpenSelector).should('be.focused');
+  });
+  describe('modalType = modal', () => {
+    it('should close with escape keydown', () => {
+      mount(
+        <Dialog modalType="modal">
+          <DialogTrigger>
+            <Button>Open dialog</Button>
+          </DialogTrigger>
+          <DialogSurface>
+            <DialogTitle>Dialog title</DialogTitle>
+            <DialogBody>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+              est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+              cumque eaque?
+            </DialogBody>
+            <DialogActions>
+              <DialogTrigger>
+                <Button appearance="secondary">Close</Button>
+              </DialogTrigger>
+              <Button appearance="primary">Do Something</Button>
+            </DialogActions>
+          </DialogSurface>
+        </Dialog>,
+      );
+      cy.get(dialogTriggerOpenSelector).click();
+      cy.focused().type('{esc}');
+      cy.get(dialogSurfaceSelector).should('not.exist');
+    });
+  });
+  describe('modalType = non-modal', () => {
+    it('should close with escape keydown', () => {
+      mount(
+        <Dialog modalType="non-modal">
+          <DialogTrigger>
+            <Button>Open dialog</Button>
+          </DialogTrigger>
+          <DialogSurface>
+            <DialogTitle>Dialog title</DialogTitle>
+            <DialogBody>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+              est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+              cumque eaque?
+            </DialogBody>
+            <DialogActions>
+              <DialogTrigger>
+                <Button appearance="secondary">Close</Button>
+              </DialogTrigger>
+              <Button appearance="primary">Do Something</Button>
+            </DialogActions>
+          </DialogSurface>
+        </Dialog>,
+      );
+      cy.get(dialogTriggerOpenSelector).click();
+      cy.focused().type('{esc}');
+      cy.get(dialogSurfaceSelector).should('not.exist');
+    });
+  });
+  describe('modalType = alert', () => {
+    it('should not close with escape keydown', () => {
+      mount(
+        <Dialog modalType="alert">
+          <DialogTrigger>
+            <Button>Open dialog</Button>
+          </DialogTrigger>
+          <DialogSurface>
+            <DialogTitle>Dialog title</DialogTitle>
+            <DialogBody>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem cumque repellendus eaque
+              est dolor eius expedita nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates in natus iure
+              cumque eaque?
+            </DialogBody>
+            <DialogActions>
+              <DialogTrigger>
+                <Button appearance="secondary">Close</Button>
+              </DialogTrigger>
+              <Button appearance="primary">Do Something</Button>
+            </DialogActions>
+          </DialogSurface>
+        </Dialog>,
+      );
+      cy.get(dialogTriggerOpenSelector).click();
+      cy.focused().type('{esc}');
+      cy.get(dialogSurfaceSelector).should('exist');
+    });
   });
 });
