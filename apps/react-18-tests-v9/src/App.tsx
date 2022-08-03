3ba6bf3f9aecf3251781a3f40aeadc7122625ffc
Micah Godbolt <mgodbolt@microsoft.com>
2022-08-03 13:43:58 -0700

    rename test file

--- apps/react-18-tests-v9/src/App.tsx
+++ apps/react-18-tests-v9/src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { FluentProvider, webLightTheme, Button } from '@fluentui/react-components';
 
+// This app is here as a simple sandbox to render v9 controls inside of React 18 environement.
+
 export const App = () => {
   return (
     <FluentProvider theme={webLightTheme}>
