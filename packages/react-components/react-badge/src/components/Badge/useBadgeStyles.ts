a8f97329e085d829c9f91cca5ff5eb32f14d8651
Sean Monahan <seanmonahan@microsoft.com>
2022-08-04 15:24:31 -0700

    badge: update color token for ghost/subtle appearance

--- packages/react-components/react-badge/src/components/Badge/useBadgeStyles.ts
+++ packages/react-components/react-badge/src/components/Badge/useBadgeStyles.ts
@@ -155,7 +155,7 @@ const useRootStyles = makeStyles({
     color: tokens.colorPaletteDarkOrangeForeground3,
   },
   'ghost-subtle': {
-    color: tokens.colorNeutralForegroundInverted,
+    color: tokens.colorNeutralForegroundInvertedStatic,
   },
   'ghost-success': {
     color: tokens.colorPaletteGreenForeground3,
