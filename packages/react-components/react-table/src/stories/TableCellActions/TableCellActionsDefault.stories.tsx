ddafaa8cfe0421b341acadab8cf334863ceda580
Lingfan Gao <lingfangao@hotmail.com>
2022-08-04 20:06:35 +0200

    remove unnecessary

--- packages/react-components/react-table/src/stories/TableCellActions/TableCellActionsDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { TableCellActions, TableCellActionsProps } from '@fluentui/react-table';
-
-export const Default = (props: Partial<TableCellActionsProps>) => <TableCellActions {...props} />;
--- packages/react-components/react-table/src/stories/TablePrimaryCell/TablePrimaryCellDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { TablePrimaryCell, TablePrimaryCellProps } from '@fluentui/react-table';
-
-export const Default = (props: Partial<TablePrimaryCellProps>) => <TablePrimaryCell {...props} />;
