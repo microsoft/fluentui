84843d9e36a84d9e67cf556c445f16ab877610e8
Martin Hochel <martinhochel@microsoft.com>
2022-08-05 09:19:16 +0200

    chore(storybook-addon): use nx module registration with SWC

--- packages/react-components/react-storybook-addon/preset.js
+++ packages/react-components/react-storybook-addon/preset.js
@@ -1,8 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./preset.prod');
 } else {
-  const { appRootPath: workspaceRoot } = require('@nrwl/tao/src/utils/app-root');
-  const { registerTsProject } = require('./register');
+  const { workspaceRoot } = require('nx/src/utils/app-root');
+  const { registerTsProject } = require('nx/src/utils/register');
+
   registerTsProject(workspaceRoot, 'tsconfig.base.json');
   module.exports = require('./preset.dev');
 }
