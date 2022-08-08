f02d178f48c307fe159e1c8c2451e480633fe104
Sean Monahan <seanmonahan@microsoft.com>
2022-08-08 15:45:29 -0700

    add options to perf "buildConfig" script

--- apps/stress-test/benchmarks/simple-stress/buildConfig.js
+++ apps/stress-test/benchmarks/simple-stress/buildConfig.js
@@ -1,6 +1,22 @@
 const fs = require('fs-extra');
 const { getBrowsers } = require('../browsers');
 const path = require('path');
+const { program } = require('commander');
+
+program
+  .option('-s, --size <number>', 'Number of components to use in the test', 100)
+  .option('-a, --add-nodes <number>', 'Number of components to add in the test')
+  .option('-r, --remove-nodes <number>', 'Number of components to remove in the test');
+
+program.parse(process.argv);
+const options = program.opts();
+if (!options.addNodes) {
+  options.addNodes = options.size;
+}
+
+if (!options.removeNodes) {
+  options.removeNodes = options.size - 1;
+}
 
 const makeConfigJson = (browser, testCase, sampleSize = 25) => {
   const json = `
@@ -22,15 +38,15 @@ const makeConfigJson = (browser, testCase, sampleSize = 25) => {
             "expand": [
               {
                 "name": "v8 - ${testCase}",
-                "url": "http://localhost:8080/v8/simple-stress/?test=${testCase}&numStartNodes=1000&numAddNodes=1000&numRemoveNodes=999"
+                "url": "http://localhost:8080/v8/simple-stress/?test=${testCase}&numStartNodes=${options.size}&numAddNodes=${options.addNodes}&numRemoveNodes=${options.removeNodes}"
               },
               {
                 "name": "v9 - ${testCase}",
-                "url": "http://localhost:8080/v9/simple-stress/?test=${testCase}&numStartNodes=1000&numAddNodes=1000&numRemoveNodes=999"
+                "url": "http://localhost:8080/v9/simple-stress/?test=${testCase}&numStartNodes=${options.size}&numAddNodes=${options.addNodes}&numRemoveNodes=${options.removeNodes}"
               },
               {
                 "name": "wc - ${testCase}",
-                "url": "http://localhost:8080/wc/simple-stress/?test=${testCase}&numStartNodes=1000&numAddNodes=1000&numRemoveNodes=999"
+                "url": "http://localhost:8080/wc/simple-stress/?test=${testCase}&numStartNodes=${options.size}&numAddNodes=${options.addNodes}&numRemoveNodes=${options.removeNodes}"
               }
             ]
           }
