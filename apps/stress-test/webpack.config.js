7c1ad24fa0c2f7ddbefe2bbe629e3543471eddff
Sean Monahan <seanmonahan@microsoft.com>
2022-08-10 14:59:29 -0700

    stress-test: update webpack to automatically load pages
    Developers can just add pages without having to touch the
    Webpack config.

--- apps/stress-test/webpack.config.js
+++ apps/stress-test/webpack.config.js
@@ -1,39 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { GriffelCSSExtractionPlugin } = require('@griffel/webpack-extraction-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const { configurePages } = require('./pageConfig');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const pages = [
-  {
-    name: 'v8-simple-stress',
-    path: './src/v8/simple-stress/index.tsx',
-    template: './src/v8/simple-stress/index.html',
-    output: 'v8/simple-stress/index.html',
-  },
-  {
-    name: 'v9-simple-stress',
-    path: './src/v9/simple-stress/index.tsx',
-    template: './src/v9/simple-stress/index.html',
-    output: 'v9/simple-stress/index.html',
-  },
-  {
-    name: 'wc-simple-stress',
-    path: './src/wc/simple-stress/index.wc.ts',
-    template: './src/wc/simple-stress/index.html',
-    output: 'wc/simple-stress/index.html',
-  },
-];
-
 const config = {
   mode: isProd ? 'production' : 'development',
-  entry: pages.reduce((config, page) => {
-    config[page.name] = page.path;
-    return config;
-  }, {}),
   output: {
     filename: '[name].[contenthash].bundle.js',
     sourceMapFilename: '[name].[contenthash].map',
@@ -98,17 +73,8 @@ const config = {
       },
     ],
   },
-  plugins: [].concat(
-    pages.map(page => {
-      return new HtmlWebpackPlugin({
-        inject: 'body',
-        template: page.template,
-        filename: page.output,
-        chunks: [page.name],
-      });
-    }),
-    [new MiniCssExtractPlugin(), new GriffelCSSExtractionPlugin(), new CleanWebpackPlugin()],
-  ),
+  plugins: [new MiniCssExtractPlugin(), new GriffelCSSExtractionPlugin(), new CleanWebpackPlugin()],
+
   optimization: {
     splitChunks: {
       chunks: 'all',
@@ -125,4 +91,4 @@ if (!isProd) {
   };
 }
 
-module.exports = config;
+module.exports = configurePages(config);
