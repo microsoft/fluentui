c14cc2e529071f8c615b4360086e13476a964b77
Sean Monahan <seanmonahan@microsoft.com>
2022-08-11 09:41:42 -0700

    stress-test: move dev dependencies to root
    Moves all dev dependencies from stress test app to root package.json.
    This adds some new root deps.
    
    Also drops usage of commander in favor of yargs as yargs was already
    present in the workspace and commander was not.

--- apps/stress-test/benchmarks/simple-stress/buildConfig.js
+++ apps/stress-test/benchmarks/simple-stress/buildConfig.js
@@ -1,15 +1,26 @@
 const fs = require('fs-extra');
 const { getBrowsers } = require('../browsers');
 const path = require('path');
-const { program } = require('commander');
+const yargs = require('yargs');
 
-program
-  .option('-s, --size <number>', 'Number of components to use in the test', 100)
-  .option('-a, --add-nodes <number>', 'Number of components to add in the test')
-  .option('-r, --remove-nodes <number>', 'Number of components to remove in the test');
+const options = yargs
+  .options({
+    size: {
+      alias: 's',
+      describe: 'Number of components to use in the test',
+      default: 100,
+    },
+    'add-nodes': {
+      alias: 'a',
+      describe: 'Number of components to add in the test',
+    },
+    'remove-nodes': {
+      alias: 'r',
+      describe: 'Number of components to remove in the test',
+    },
+  })
+  .parse();
 
-program.parse(process.argv);
-const options = program.opts();
 if (!options.addNodes) {
   options.addNodes = options.size;
 }
