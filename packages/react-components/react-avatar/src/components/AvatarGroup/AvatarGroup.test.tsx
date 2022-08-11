1ca5c41015eb5297fa979affa7cce3e0c2ce288a
Esteban Munoz <esteban.230@hotmail.com>
2022-08-11 14:40:23 -0600

    adding more tests

--- packages/react-components/react-avatar/src/components/AvatarGroup/AvatarGroup.test.tsx
+++ packages/react-components/react-avatar/src/components/AvatarGroup/AvatarGroup.test.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { AvatarGroup } from './AvatarGroup';
 import { AvatarGroupItem } from '../AvatarGroupItem';
 import { isConformant } from '../../common/isConformant';
-import { render, screen } from '@testing-library/react';
 import { AvatarGroupPopover } from '../AvatarGroupPopover/AvatarGroupPopover';
 
 describe('AvatarGroup', () => {
@@ -27,25 +26,5 @@ describe('AvatarGroup', () => {
         </>
       ),
     },
-  });
-
-  it('renders an icon overflow indicator when size is less than 24', () => {
-    render(
-      <AvatarGroup size={20}>
-        <AvatarGroupItem name="Mona Kane" />
-        <AvatarGroupItem name="Allan Munger" />
-        <AvatarGroupItem name="Daisy Phillips" />
-        <AvatarGroupItem name="Robert Tolbert" />
-        <AvatarGroupItem name="Kevin Sturgis" />
-        <AvatarGroupPopover>
-          <AvatarGroupItem name="Allan Munger" />
-          <AvatarGroupItem name="Daisy Phillips" />
-          <AvatarGroupItem name="Robert Tolbert" />
-          <AvatarGroupItem name="Kevin Sturgis" />
-        </AvatarGroupPopover>
-      </AvatarGroup>,
-    );
-
-    expect(screen.getByRole('button').textContent).toBe('');
   });
 });
