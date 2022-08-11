5998553e027cf09e09d4062180106d8b0731df9b
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-08-11 10:56:36 +0000

    chore: removes memoization

--- packages/react-components/react-popover/src/components/PopoverTrigger/PopoverTrigger.tsx
+++ packages/react-components/react-popover/src/components/PopoverTrigger/PopoverTrigger.tsx
@@ -7,11 +7,11 @@ import type { PopoverTriggerProps } from './PopoverTrigger.types';
 /**
  * Wraps a trigger element as an only child and adds the necessary event handling to open a popover.
  */
-export const PopoverTrigger: React.FC<PopoverTriggerProps> & FluentTriggerComponent = React.memo(props => {
+export const PopoverTrigger: React.FC<PopoverTriggerProps> & FluentTriggerComponent = props => {
   const state = usePopoverTrigger_unstable(props);
 
   return renderPopoverTrigger_unstable(state);
-});
+};
 
 PopoverTrigger.displayName = 'PopoverTrigger';
 PopoverTrigger.isFluentTriggerComponent = true;
