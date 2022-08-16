c3dc22a593e0bfde8b47143e8a08fb7777bfc199
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2022-08-16 18:37:59 +0530

    Add code blocks in explaining variants

--- packages/react-examples/src/react-charting/LineChart/LineChartPage.tsx
+++ packages/react-examples/src/react-charting/LineChart/LineChartPage.tsx
@@ -65,22 +65,43 @@ export class LineChartPage extends React.Component<IComponentDemoPageProps, {}> 
               amount of lines (data sets) depend on the attributes selected during the report creation.
             </p>
             <p>The line graph thickness will vary depending on the number of data sets and data increments.</p>
-            <h3>Different variants of line chart</h3>
+
+            <h3>Variant details</h3>
             <h4>Event annotations</h4>
-            <p>Data can be annotated using vertical lines representing the events of interest.</p>
+            <p>
+              Event annotations are used to highlight instances of datetime and annotate them using messages.
+              Annotations are represented by vertical line markers to mark the date and callouts to represent the
+              message. Events can be added by using <code>eventAnnotationProps</code> prop. Each event contains a{' '}
+              <code>date</code>, <code>event message</code> and event detail callout callback
+              <code>onRenderCard</code>
+            </p>
             <h4>Gaps</h4>
             <p>
-              The chart can have gaps in between. This is to represent missing data. The gaps can also be replaced with
-              dashed or dotted lines for specific scenarios.
+              The chart can have gaps/breaks in between. This is to represent missing data. The gaps can also be
+              replaced with dashed or dotted lines for specific scenarios. For eg: Representing low confidence
+              predictions for a time series forecast graph.
             </p>
-            **Line border** * Each line in the chart can contain a 2 px border for better highlighting of the line when
-            there are multiple items in the chart.
+            <h4>Line border</h4>
+            <p>
+              Each line in the chart can contain a 2 px border for better highlighting of the line when there are
+              multiple items in the chart. The border will be of the background theme color. Lines will be highlighted
+              in order of their appearance in legends.
+            </p>
             <h4>Lines with large dataset</h4>
             <p>
               We use a path based rendering technique to create datasets with large number of points (greater than 1k).
               Using this technique datasets with over 10k points can be rendered easily. Enable this rendering method by
-              setting the `optimizeLargeData` prop to true.
+              setting the <code>optimizeLargeData</code> prop to true.
             </p>
+            <h4>Custom accessibility</h4>
+            <p>
+              Line chart provides a bunch of props to enable custom accessibility messages. Use{' '}
+              <code>xAxisCalloutAccessibilityData</code>
+              and <code>callOutAccessibilityData</code> to configure x axis and y axis accessibility messages
+              respectively.
+            </p>
+            <h4>Styled lines</h4>
+            <p>Fill in the details</p>
           </div>
         }
       />
