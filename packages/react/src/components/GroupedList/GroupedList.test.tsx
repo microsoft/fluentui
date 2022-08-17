df602db552cade02f2a0922e7ba829975232c7dc
Tony Hallett <tonyhallett74@gmail.com>
2022-08-17 21:18:56 +0100

    Fix grouped list derived state from props (#24385)
    * add items to next state
    
    * remove duplicate code from before
    
    * change files
    
    * update snapshot for component that uses GroupedList
    
    * review.  add intermediate assertion

--- packages/react/src/components/GroupedList/GroupedList.test.tsx
+++ packages/react/src/components/GroupedList/GroupedList.test.tsx
@@ -307,4 +307,35 @@ describe('GroupedList', () => {
     expect(onRenderCheckboxMock).toHaveBeenCalledTimes(1);
     expect(onRenderCheckboxMock.mock.calls[0][0]).toEqual({ checked: false, theme: getTheme() });
   });
+
+  it('re-renders when items change back to the initial items', () => {
+    const initialItems: Array<{ key: string }> = [{ key: 'initial' }];
+    const nextItems: Array<{ key: string }> = [{ key: 'changed' }];
+    const _groups: Array<IGroup> = [
+      {
+        count: 1,
+        hasMoreData: true,
+        isCollapsed: false,
+        key: 'group0',
+        name: 'group 0',
+        startIndex: 0,
+        level: 0,
+      },
+    ];
+
+    function _onRenderCell(nestingDepth: number, item: { key: string }, itemIndex: number): JSX.Element {
+      const id = `rendered-item-${item.key}`;
+      return <div id={id} />;
+    }
+
+    const wrapper = mount(<GroupedList items={initialItems} groups={_groups} onRenderCell={_onRenderCell} />);
+    expect(wrapper.contains(<div id="rendered-item-initial" />)).toEqual(true);
+
+    wrapper.setProps({ items: nextItems });
+    expect(wrapper.contains(<div id="rendered-item-changed" />)).toEqual(true);
+    expect(wrapper.contains(<div id="rendered-item-initial" />)).toEqual(false);
+
+    wrapper.setProps({ items: initialItems });
+    expect(wrapper.contains(<div id="rendered-item-initial" />)).toEqual(true);
+  });
 });
