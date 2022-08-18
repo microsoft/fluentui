1d0e8d9e8fd5cef7b27fca549bc5d2b269f94cea
Micah Godbolt <mgodbolt@microsoft.com>
2022-08-18 11:02:28 -0700

    simplify src index file

--- packages/react-charting/src/index.ts
+++ packages/react-charting/src/index.ts
@@ -1,16 +1,2 @@
-export * from './HorizontalBarChart';
-export * from './DonutChart';
-export * from './Legends';
-export * from './LineChart';
-export * from './AreaChart';
-export * from './PieChart';
-export * from './StackedBarChart';
-export * from './VerticalBarChart';
-export * from './VerticalStackedBarChart';
-export * from './GroupedVerticalBarChart';
-export * from './CartesianChart';
-export * from '@uifabric/charting/lib/types';
-export * from '@uifabric/charting/lib/utilities/ChartHoverCard';
-export * from './HeatMapChart';
-
 import './version';
+export * from '@uifabric/charting';
