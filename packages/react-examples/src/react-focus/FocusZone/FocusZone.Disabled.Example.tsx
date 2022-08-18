05bc78fbc04ad67c0df93cf94d2ef59ded9d58b5
REDMOND\jspurlin <jspurlin@microsoft.com>
2022-08-18 14:06:56 -0700

    remove exmaple update that was used for testing

--- packages/react-examples/src/react-focus/FocusZone/FocusZone.Disabled.Example.tsx
+++ packages/react-examples/src/react-focus/FocusZone/FocusZone.Disabled.Example.tsx
@@ -9,14 +9,11 @@ const textFieldStyles: Partial<ITextFieldStyles> = { root: { width: 200 } };
 
 export const FocusZoneDisabledExample: React.FunctionComponent = () => {
   const tokens = { childrenGap: 20 };
-  const fzRef = React.useRef<FocusZone>(null);
   return (
     <Stack tokens={tokens} horizontalAlign="start">
-      {/* <FocusZone direction={FocusZoneDirection.horizontal} componentRef={ref => (fzRef.current = ref || undefined)}> */}
-      <FocusZone direction={FocusZoneDirection.horizontal} ref={fzRef}>
+      <FocusZone direction={FocusZoneDirection.horizontal}>
         <Stack tokens={tokens} horizontal verticalAlign="center">
           <span>Enabled FocusZone: </span>
-          <DefaultButton style={{ visibility: 'hidden' }}>Button 0</DefaultButton>
           <DefaultButton>Button 1</DefaultButton>
           <DefaultButton>Button 2</DefaultButton>
           <TextField placeholder="FocusZone TextField" styles={textFieldStyles} ariaLabel="FocusZone TextField" />
@@ -32,10 +29,6 @@ export const FocusZoneDisabledExample: React.FunctionComponent = () => {
         </Stack>
       </FocusZone>
       <TextField placeholder="Tabbable Element 2" ariaLabel="Tabbable Element 2" />
-      <DefaultButton onClick={() => fzRef.current?.focus(true)}>Set Focus to the first Focus Zone - OLD</DefaultButton>
-      <DefaultButton onClick={() => fzRef.current?.focus(true, true)}>
-        Set Focus to the first Focus Zone - Bypass hidden
-      </DefaultButton>
     </Stack>
   );
 };
