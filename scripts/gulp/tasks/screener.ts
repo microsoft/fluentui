1e24a9e15b73d369883eef8160c03e7e87c34530
Raluca Pelin <t-rpelin@microsoft.com>
2022-08-19 17:58:28 +0300

    Solve conflict

--- scripts/gulp/tasks/screener.ts
+++ scripts/gulp/tasks/screener.ts
@@ -2,8 +2,8 @@ import { task, series } from 'gulp';
 import { argv } from 'yargs';
 
 import config from '../../config';
-import { getAffectedPackages, getAllPackageInfo, getNthCommit } from '../../monorepo';
-import { screenerRunner, cancelScreenerRun } from '../../screener/screener.runner';
+import { getAllPackageInfo } from '../../monorepo';
+import { screenerRunner } from '../../screener/screener.runner';
 
 const { paths } = config;
 
@@ -37,34 +37,9 @@ task('screener:runner', cb => {
       });
 
   const screenerConfig = require(screenerConfigPath);
-  let affectedPackages = new Set<string>();
-  const isPrBuild = process.env.BUILD_SOURCEBRANCH && process.env.BUILD_SOURCEBRANCH.includes('refs/pull');
 
-  if (isPrBuild) {
-    affectedPackages = getAffectedPackages();
-  } else {
-    // master CI build,
-    const previousMasterCommit = getNthCommit();
-    affectedPackages = getAffectedPackages(previousMasterCommit);
-  }
-
-  debugAffectedGraph(affectedPackages);
-
-  if (!affectedPackages.has(docsPackageName)) {
-    handlePromiseExit(cancelScreenerRun(screenerConfig, 'skipped'));
-  } else {
-    handlePromiseExit(screenerRunner(screenerConfig));
-  }
+  handlePromiseExit(screenerRunner(screenerConfig));
 });
-
-/**
- * Outputs debug output for the affected packages graph
- * @param affectedPackages  - set of affected packages
- */
-function debugAffectedGraph(affectedPackages: Set<string>) {
-  console.log('affected package tree');
-  console.log(Array.from(affectedPackages.values()));
-}
 
 // ----------------------------------------
 // Default
