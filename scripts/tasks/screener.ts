e8b303a4eed400638ea7c84f874145b7babc9409
Raluca Pelin <t-rpelin@microsoft.com>
2022-08-19 15:50:52 +0300

    Remove log

--- scripts/tasks/screener.ts
+++ scripts/tasks/screener.ts
@@ -15,7 +15,6 @@ export async function screener() {
   console.log(JSON.stringify(screenerConfig, null, 2));
 
   try {
-    console.log(process.env.skipScreenerBuild);
     if (process.env.skipScreenerBuild !== 'true') {
       console.log('Running screener test:');
       const screenerStates = await getScreenerStates(screenerConfig);
