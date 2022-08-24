cecb705e25b993a39768745c84839646c25a749e
Sean Monahan <seanmonahan@microsoft.com>
2022-08-24 13:18:34 -0700

    WIP

--- packages/react-components/react-spinbutton/src/stories/SpinButton/SpinButtonAppearance.stories.tsx
+++ packages/react-components/react-spinbutton/src/stories/SpinButton/SpinButtonAppearance.stories.tsx
@@ -18,13 +18,13 @@ const useStyles = makeStyles({
   filledLighter: {
     backgroundColor: tokens.colorNeutralBackgroundInverted,
     '> label': {
-      color: tokens.colorNeutralForegroundInverted,
+      color: tokens.colorNeutralForegroundInvertedStatic,
     },
   },
   filledDarker: {
     backgroundColor: tokens.colorNeutralBackgroundInverted,
     '> label': {
-      color: tokens.colorNeutralForegroundInverted,
+      color: tokens.colorNeutralForegroundInvertedStatic,
     },
   },
 });
