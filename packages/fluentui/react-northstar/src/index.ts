a43315219cba4f0e26a079708afb90ae6a6ac8f7
Raluca Pelin <t-rpelin@microsoft.com>
2022-08-31 16:19:47 +0300

    test react components and N*

--- packages/fluentui/react-northstar/src/index.ts
+++ packages/fluentui/react-northstar/src/index.ts
@@ -287,3 +287,4 @@ export const FocusZoneUtilities = {
   getPreviousElement,
   focusAsync,
 };
+//  comment
--- packages/react-components/babel-preset-global-context/src/index.ts
+++ packages/react-components/babel-preset-global-context/src/index.ts
@@ -5,3 +5,4 @@ export default function preset() {
     plugins: [[transformPlugin]],
   };
 }
+//comment
