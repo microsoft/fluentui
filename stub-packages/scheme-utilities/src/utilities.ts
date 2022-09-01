b5c66b24e4278362331231d30759caabba02fc2a
Geoff Cox <gcox@microsoft.com>
2022-08-31 17:59:13 -0700

    Added stub scheme-utilities

---
+++ stub-packages/scheme-utilities/src/utilities.ts
@@ -0,0 +1,1 @@
+export * from '@uifabric/variants/lib/utilities';
---
+++ stub-packages/scheme-utilities/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const resources = require('../../scripts/webpack/webpack-resources');
+const webpack = resources.webpack;
+
+const BUNDLE_NAME = 'scheme-utilities';
+
+module.exports = resources.createConfig(BUNDLE_NAME, true, {
+  entry: {
+    [BUNDLE_NAME]: './lib/index.js',
+  },
+
+  output: {
+    libraryTarget: 'var',
+    library: 'SchemeUtilities',
+  },
+});
