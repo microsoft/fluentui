39a09be9c0e8cecc12e8f6f5baa13987d3e668bd
Charles <chassunc@microsoft.com>
2022-09-01 09:34:59 -0300

    chore: move size to the use hook

--- packages/react-components/react-toolbar/src/components/ToolbarToggleButton/ToolbarToggleButton.tsx
+++ packages/react-components/react-toolbar/src/components/ToolbarToggleButton/ToolbarToggleButton.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import type { ToolbarToggleButtonProps } from './ToolbarToggleButton.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
 import { renderToggleButton_unstable, useToggleButtonStyles_unstable } from '@fluentui/react-button';
-import { useToolbarContext } from '../Toolbar/ToolbarContext';
 import { useToolbarToggleButton_unstable } from './useToolbarToggleButton';
 
 /**
  * ToolbarToggleButton component
  */
 export const ToolbarToggleButton: ForwardRefComponent<ToolbarToggleButtonProps> = React.forwardRef((props, ref) => {
-  const { size } = useToolbarContext();
-  const state = useToolbarToggleButton_unstable({ size, ...props }, ref);
+  const state = useToolbarToggleButton_unstable(props, ref);
 
   useToggleButtonStyles_unstable(state);
   return renderToggleButton_unstable(state);
