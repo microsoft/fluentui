6ed1c3e08cbb374007534b8aebfd2bd649bf58f9
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-09-14 15:50:13 +0200

    remove function in chatmsgcontentstyles

--- packages/fluentui/react-northstar/src/themes/teams/components/Chat/chatMessageContentStyles.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Chat/chatMessageContentStyles.ts
@@ -1,44 +1,15 @@
-import { ComponentSlotStyleFunction, ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
+import { ComponentSlotStylesPrepared, ICSSInJSStyle } from '@fluentui/styles';
 
 import { ChatMessageContentStylesProps } from '../../../../components/Chat/ChatMessageContent';
 import { ChatMessageVariables } from './chatMessageVariables';
 import { pxToRem } from '../../../../utils';
-
-const getChatMessageVariantStyles: ComponentSlotStyleFunction<ChatMessageContentStylesProps, ChatMessageVariables> = ({
-  props: p,
-  variables: v,
-  theme,
-}) => {
-  return {
-    ...(p.layout === 'refresh' &&
-      p.density === 'comfy' && {
-        color: v.contentColor,
-        wordBreak: 'break-word',
-        wordWrap: 'break-word',
-        '& a': {
-          color: 'inherit',
-          textDecoration: 'underline',
-          wordBreak: 'break-all',
-          '&:hover': { textDecorationStyle: 'double' },
-          '&:focus': { textDecorationStyle: 'double' },
-        },
-        ...(p.failed && {
-          color: theme.siteVariables.colorScheme.default.foreground,
-        }),
-      }),
-
-    ...(p.density === 'comfy' && {
-      ...(p.hasBadge && p.badgePosition === 'end' && { marginRight: pxToRem(4) }),
-    }),
-  };
-};
 
 export const chatMessageContentStyles: ComponentSlotStylesPrepared<
   ChatMessageContentStylesProps,
   ChatMessageVariables
 > = {
   root: (componentStyleFunctionParam): ICSSInJSStyle => {
-    const { props: p, variables: v } = componentStyleFunctionParam;
+    const { props: p, variables: v, theme } = componentStyleFunctionParam;
     return {
       color: v.contentColor,
       display: 'block',
@@ -49,7 +20,27 @@ export const chatMessageContentStyles: ComponentSlotStylesPrepared<
           textDecoration: 'underline',
         },
       },
-      ...getChatMessageVariantStyles(componentStyleFunctionParam),
+
+      ...(p.layout === 'refresh' &&
+        p.density === 'comfy' && {
+          color: v.contentColor,
+          wordBreak: 'break-word',
+          wordWrap: 'break-word',
+          '& a': {
+            color: 'inherit',
+            textDecoration: 'underline',
+            wordBreak: 'break-all',
+            '&:hover': { textDecorationStyle: 'double' },
+            '&:focus': { textDecorationStyle: 'double' },
+          },
+          ...(p.failed && {
+            color: theme.siteVariables.colorScheme.default.foreground,
+          }),
+        }),
+
+      ...(p.density === 'comfy' && {
+        ...(p.hasBadge && p.badgePosition === 'end' && { marginRight: pxToRem(4) }),
+      }),
     };
   },
 };
