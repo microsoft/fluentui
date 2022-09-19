c168d0012e78c927844dd1b736a4ee4a872f4345
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-09-19 08:06:43 +0000

    chore: updates API

--- packages/react-components/react-dialog/src/components/DialogContent/DialogContent.types.ts
+++ packages/react-components/react-dialog/src/components/DialogContent/DialogContent.types.ts
@@ -13,5 +13,3 @@ export type DialogContentProps = ComponentProps<DialogContentSlots> & {};
  * State used in rendering DialogContent
  */
 export type DialogContentState = ComponentState<DialogContentSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from DialogContentProps.
-// & Required<Pick<DialogContentProps, 'propName'>>
