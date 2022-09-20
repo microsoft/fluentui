08b8920b818d1102d4a2fd3e4e34d59053309051
Lingfan Gao <lingfangao@hotmail.com>
2022-09-20 14:51:33 +0200

    remove doc

--- packages/react-components/react-shared-contexts/src/AvatarContext.ts
+++ packages/react-components/react-shared-contexts/src/AvatarContext.ts
@@ -2,10 +2,6 @@ import * as React from 'react';
 
 const avatarContext = React.createContext<AvatarContextValue | undefined>(undefined);
 
-/**
- * Sizes that can be used for the Avatar
- * Copied from the react-avatar package, if the sizes are incompatible then TS should fail
- */
 export type AvatarSizes = 16 | 20 | 24 | 28 | 32 | 36 | 40 | 48 | 56 | 64 | 72 | 96 | 120 | 128;
 
 /**
--- packages/react-components/react-table/src/components/TableCellLayout/useTableCellLayoutContextValues.ts
+++ packages/react-components/react-table/src/components/TableCellLayout/useTableCellLayoutContextValues.ts
@@ -2,13 +2,13 @@ import * as React from 'react';
 import type { TableCellLayoutState, TableCellLayoutContextValues } from './TableCellLayout.types';
 
 export function useTableCellLayoutContextValues_unstable(state: TableCellLayoutState): TableCellLayoutContextValues {
-  const { size: tableSize } = state;
+  const { avatarSize } = state;
 
   const avatar = React.useMemo(
     () => ({
-      size: tableAvatarSizeMap[tableSize],
+      size: avatarSize,
     }),
-    [tableSize],
+    [avatarSize],
   );
 
   return {
