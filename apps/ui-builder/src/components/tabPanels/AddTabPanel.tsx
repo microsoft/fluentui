7619d02ff5b6dacd47a8c969ccf2335e95d276c0
Ligia Popescu <ligiapopescu@microsoft.com>
2022-09-21 13:48:03 +0300

    Add category for recommended components

--- apps/ui-builder/src/components/tabPanels/AddTabPanel.tsx
+++ apps/ui-builder/src/components/tabPanels/AddTabPanel.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { ComponentList } from '../ComponentList';
+import { ComponentInfo } from '../../componentInfo/types';
 
 export type AddTabPanelProps = {
   onDragStart?: (info: any, e: any) => void;
+  selectedComponentInfo?: ComponentInfo;
 };
 
-export const AddTabPanel: React.FunctionComponent<AddTabPanelProps> = ({ onDragStart }) => {
+export const AddTabPanel: React.FunctionComponent<AddTabPanelProps> = ({ onDragStart, selectedComponentInfo }) => {
   return (
     <div>
-      <ComponentList style={{ overflowY: 'auto' }} onDragStart={onDragStart} />
+      <ComponentList
+        style={{ overflowY: 'auto' }}
+        onDragStart={onDragStart}
+        selectedComponentInfo={selectedComponentInfo}
+      />
     </div>
   );
 };
