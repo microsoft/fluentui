7f14f0acdc5de2c60396e75a52528d4bca418e3c
fogelek <michal@fogelman.pl>
2022-09-21 15:47:08 +0200

    InsertComponent: - Improved dialog content positioning

--- apps/ui-builder/src/components/InsertComponent.tsx
+++ apps/ui-builder/src/components/InsertComponent.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
-import { Dialog, Dropdown } from '@fluentui/react-northstar';
+import { Dropdown } from '@fluentui/react-northstar';
+import { Button, makeStyles, shorthands } from '@fluentui/react-components';
+import { Dialog, DialogSurface } from '@fluentui/react-components/unstable';
 import { componentInfoContext } from '../componentInfo/componentInfoContext';
 
+const useStyles = makeStyles({
+  surface: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 'fit-content',
+    rowGap: '2rem',
+    zIndex: 10,
+  },
+  header: {
+    ...shorthands.margin(0),
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
 export const InsertComponent = ({ onComponentAdded, onDismiss }) => {
+  const styles = useStyles();
   const [selectedComponent, setSelectedComponent] = React.useState('');
   const confirm = React.useCallback(() => {
     const component = componentInfoContext.byDisplayName[selectedComponent];
@@ -13,25 +34,22 @@ export const InsertComponent = ({ onComponentAdded, onDismiss }) => {
     onDismiss && onDismiss();
   }, [onDismiss]);
 
+  const onChange = React.useCallback((e, data) => setSelectedComponent(String(data.value)), [setSelectedComponent]);
+
   const items = Object.keys(componentInfoContext.byDisplayName);
 
   return (
-    <Dialog
-      header="Insert component"
-      confirmButton="Insert"
-      cancelButton="Cancel"
-      open={true}
-      content={
-        <Dropdown
-          search
-          highlightFirstItemOnOpen
-          placeholder="Choose component"
-          items={items}
-          onChange={(e, data) => setSelectedComponent(String(data.value))}
-        />
-      }
-      onConfirm={confirm}
-      onCancel={dismiss}
-    />
+    <Dialog open={true}>
+      <DialogSurface className={styles.surface}>
+        <h2 className={styles.header}>Insert component</h2>
+        <Dropdown search highlightFirstItemOnOpen placeholder="Choose component" items={items} onChange={onChange} />
+        <div className={styles.buttonContainer}>
+          <Button onClick={dismiss}>Cancel</Button>
+          <Button onClick={confirm} appearance="primary">
+            Insert
+          </Button>
+        </div>
+      </DialogSurface>
+    </Dialog>
   );
 };
