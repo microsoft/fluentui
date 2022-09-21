ce5f1ec6a0c0f766679ea708c40538fc2d56c6d9
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-09-21 15:05:28 +0200

    chore(react-aria): restructure folder organization (#24884)

--- packages/react-components/react-aria/src/index.ts
+++ packages/react-components/react-aria/src/index.ts
@@ -1,4 +1,4 @@
-export { useARIAButtonShorthand, useARIAButtonProps } from './hooks/useARIAButton/index';
+export { useARIAButtonShorthand, useARIAButtonProps } from './button/index';
 export type {
   ARIAButtonSlotProps,
   ARIAButtonProps,
@@ -6,4 +6,4 @@ export type {
   ARIAButtonType,
   ARIAButtonElement,
   ARIAButtonElementIntersection,
-} from './hooks/useARIAButton/index';
+} from './button/index';
