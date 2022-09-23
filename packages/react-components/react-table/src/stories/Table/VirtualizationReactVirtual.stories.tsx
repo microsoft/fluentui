f19cf60c159f5c2c97ffa02cb8bca63d69bbac43
Lingfan Gao <lingfangao@hotmail.com>
2022-09-23 17:46:00 +0200

    add sort and selection

--- packages/react-components/react-table/src/stories/Table/VirtualizationReactVirtual.stories.tsx
+++ packages/react-components/react-table/src/stories/Table/VirtualizationReactVirtual.stories.tsx
@@ -1,6 +1,39 @@
 import * as React from 'react';
-import { Table, TableHeader, TableHeaderCell, TableCell, TableBody, TableRow } from '../..';
+import { Table, TableHeader, TableHeaderCell, TableCell, TableBody, TableRow, TableSelectionCell } from '../..';
 import { useVirtualizer } from '@tanstack/react-virtual';
+import { useTable } from '../../hooks/useTable';
+import { useSelection } from '../../hooks/useSelection';
+import { useSort } from '../../hooks/useSort';
+import { ColumnDefinition, ColumnId } from '../../hooks/types';
+
+const columns: ColumnDefinition<{ index: number }>[] = [
+  {
+    columnId: 'first',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'second',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'third',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'fourth',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+];
+
+const items = new Array(1000).fill(0).map((_, i) => ({ index: i }));
 
 export const VirtualizationReactVirtual = () => {
   // The scrollable element for your list
@@ -13,14 +46,53 @@ export const VirtualizationReactVirtual = () => {
     estimateSize: () => 35,
   });
 
+  const {
+    getRows,
+    sort: { getSortDirection, toggleColumnSort, sort },
+    selection: { allRowsSelected, someRowsSelected, toggleAllRows, toggleRow, isRowSelected },
+  } = useTable(
+    {
+      columns,
+      items,
+    },
+    [
+      tableState => useSelection(tableState, { selectionMode: 'multiselect' }),
+      tableState => useSort(tableState, { defaultSortState: { sortColumn: 'first', sortDirection: 'ascending' } }),
+    ],
+  );
+
+  let rows = getRows(row => ({
+    ...row,
+    onClick: () => toggleRow(row.rowId),
+    onKeyDown: (e: React.KeyboardEvent) => {
+      if (e.key === ' ' || e.key === 'Enter') {
+        toggleRow(row.rowId);
+      }
+    },
+    selected: isRowSelected(row.rowId),
+  }));
+
+  rows = sort(rows);
+
+  const headerSortProps = (columnId: ColumnId) => ({
+    onClick: () => {
+      toggleColumnSort(columnId);
+    },
+    sortDirection: getSortDirection(columnId),
+  });
+
   return (
     <Table layoutType="flex">
       <TableHeader style={{ paddingRight: 16 }}>
         <TableRow>
-          <TableHeaderCell>First</TableHeaderCell>
-          <TableHeaderCell>Second</TableHeaderCell>
-          <TableHeaderCell>Third</TableHeaderCell>
-          <TableHeaderCell>Fourth</TableHeaderCell>
+          <TableSelectionCell
+            checked={allRowsSelected ? true : someRowsSelected ? 'mixed' : false}
+            onClick={toggleAllRows}
+          />
+          <TableHeaderCell {...headerSortProps('first')}>First</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('second')}>Second</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('third')}>Third</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('fourt')}>Fourth</TableHeaderCell>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -41,24 +113,32 @@ export const VirtualizationReactVirtual = () => {
             }}
           >
             {/* Only the visible items in the virtualizer, manually positioned to be in view */}
-            {rowVirtualizer.getVirtualItems().map(virtualItem => (
-              <TableRow
-                key={virtualItem.key}
-                style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
-                  height: `${virtualItem.size}px`,
-                  transform: `translateY(${virtualItem.start}px)`,
-                }}
-              >
-                <TableCell>First {virtualItem.key} </TableCell>
-                <TableCell>Second</TableCell>
-                <TableCell>Third</TableCell>
-                <TableCell>Fourth</TableCell>
-              </TableRow>
-            ))}
+            {rowVirtualizer.getVirtualItems().map(virtualItem => {
+              const {
+                selected,
+                onClick,
+                item: { index: userIndex },
+              } = rows[virtualItem.index];
+
+              const style = {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              };
+
+              return (
+                <TableRow style={style} onClick={onClick} key={virtualItem.key}>
+                  <TableSelectionCell checked={selected} />
+                  <TableCell>First {userIndex}</TableCell>
+                  <TableCell>Second {userIndex}</TableCell>
+                  <TableCell>Third {userIndex}</TableCell>
+                  <TableCell>Fourth {userIndex}</TableCell>
+                </TableRow>
+              );
+            })}
           </div>
         </div>
       </TableBody>
--- packages/react-components/react-table/src/stories/Table/VirtualizationReactWindow.stories.tsx
+++ packages/react-components/react-table/src/stories/Table/VirtualizationReactWindow.stories.tsx
@@ -3,40 +3,110 @@ import { Table, TableHeader, TableHeaderCell, TableCell, TableBody, TableRow } f
 import { FixedSizeList as List } from 'react-window';
 import { useTable } from '../../hooks/useTable';
 import { useColumnSizing } from '../../hooks/useColumnSizing';
+import { useSelection } from '../../hooks/useSelection';
+import { useSort } from '../../hooks/useSort';
+import { TableSelectionCell } from '../../components/TableSelectionCell/TableSelectionCell';
+import { ColumnDefinition, ColumnId } from '../../hooks/types';
 
-const Row = ({ index, style }) => (
-  <TableRow style={style}>
-    <TableCell>First {index}</TableCell>
-    <TableCell>Second</TableCell>
-    <TableCell>Third</TableCell>
-    <TableCell>Fourth</TableCell>
-  </TableRow>
-);
+const columns: ColumnDefinition<{ index: number }>[] = [
+  {
+    columnId: 'first',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'second',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'third',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+  {
+    columnId: 'fourth',
+    compare: (a, b) => {
+      return a.index - b.index;
+    },
+  },
+];
+
+const Row = ({ index, style, data }) => {
+  const {
+    selected,
+    onClick,
+    item: { index: userIndex },
+  } = data[index];
+  return (
+    <TableRow style={style} onClick={onClick}>
+      <TableSelectionCell checked={selected} />
+      <TableCell>First {userIndex}</TableCell>
+      <TableCell>Second {userIndex}</TableCell>
+      <TableCell>Third {userIndex}</TableCell>
+      <TableCell>Fourth {userIndex}</TableCell>
+    </TableRow>
+  );
+};
+
+const items = new Array(1000).fill(0).map((_, i) => ({ index: i }));
 
 export const VirtualizationReactWindow = () => {
   const {
     columnSizing: { getTotalWidth },
     tableRef,
+    getRows,
+    sort: { getSortDirection, toggleColumnSort, sort },
+    selection: { allRowsSelected, someRowsSelected, toggleAllRows, toggleRow, isRowSelected },
   } = useTable(
     {
-      columns: [{ columnId: 'first' }, { columnId: 'second' }, { columnId: 'third' }, { columnId: 'fourth' }],
-      items: [],
+      columns,
+      items,
     },
-    [useColumnSizing],
+    [
+      useColumnSizing,
+      tableState => useSelection(tableState, { selectionMode: 'multiselect' }),
+      tableState => useSort(tableState, { defaultSortState: { sortColumn: 'first', sortDirection: 'ascending' } }),
+    ],
   );
+
+  const rows = getRows(row => ({
+    ...row,
+    onClick: () => toggleRow(row.rowId),
+    onKeyDown: (e: React.KeyboardEvent) => {
+      if (e.key === ' ' || e.key === 'Enter') {
+        toggleRow(row.rowId);
+      }
+    },
+    selected: isRowSelected(row.rowId),
+  }));
+
+  const headerSortProps = (columnId: ColumnId) => ({
+    onClick: () => {
+      toggleColumnSort(columnId);
+    },
+    sortDirection: getSortDirection(columnId),
+  });
 
   return (
     <Table ref={tableRef} noNativeElements layoutType="flex">
       <TableHeader style={{ paddingRight: 14 }}>
         <TableRow>
-          <TableHeaderCell>First</TableHeaderCell>
-          <TableHeaderCell>Second</TableHeaderCell>
-          <TableHeaderCell>Third</TableHeaderCell>
-          <TableHeaderCell>Fourth</TableHeaderCell>
+          <TableSelectionCell
+            checked={allRowsSelected ? true : someRowsSelected ? 'mixed' : false}
+            onClick={toggleAllRows}
+          />
+          <TableHeaderCell {...headerSortProps('first')}>First</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('second')}>Second</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('third')}>Third</TableHeaderCell>
+          <TableHeaderCell {...headerSortProps('fourt')}>Fourth</TableHeaderCell>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <List height={600} itemCount={1000} itemSize={50} width={getTotalWidth()}>
+        <List height={600} itemCount={1000} itemSize={50} width={getTotalWidth()} itemData={sort(rows)}>
           {Row}
         </List>
       </TableBody>
