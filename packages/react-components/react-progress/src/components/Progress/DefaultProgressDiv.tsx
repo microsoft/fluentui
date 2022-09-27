75243b1e8268ffe1c7582eb3e2504086aabbd0b0
Tomi Olubeko <ololubek@microsoft.com>
2022-09-27 13:41:27 -0700

    remove unused file

--- packages/react-components/react-progress/src/components/Progress/DefaultProgressDiv.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export const DefaultProgressDiv = () => (
-  <div className="fui-Progress__Container">
-    <div className="fui-Progress__Track" />
-    <div className="fui-Progress__Bar" />
-  </div>
-);
