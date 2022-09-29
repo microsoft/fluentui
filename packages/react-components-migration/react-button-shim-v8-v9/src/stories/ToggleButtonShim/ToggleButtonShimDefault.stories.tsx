9506f4aed579d13ca7c3c1fc7d478bab988d0166
GeoffCoxMSFT <gcox@microsoft.com>
2022-09-29 14:55:22 -0700

    Moved shim code to shim packages

---
+++ packages/react-components-migration/react-button-shim-v8-v9/src/stories/ToggleButtonShim/ToggleButtonShimDefault.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { ToggleButtonShim, ToggleButtonShimProps } from '@fluentui/react-button-shim-v8-v9';
+
+export const Default = (props: Partial<ToggleButtonShimProps>) => <ToggleButtonShim {...props} />;
---
+++ packages/react-components-migration/react-stack-shim-v8-v9/src/stories/StackShimV8V9/StackShimDefault.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { StackShim, StackShimProps } from '@fluentui/react-stack-shim-v8-v9';
+
+export const Default = (props: Partial<StackShimProps>) => <StackShim {...props} />;
