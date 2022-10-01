eae0266abfb7fb271d1e683eb96cf3ece6409083
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-10-01 02:08:48 -0700

    nit: dont have stories as constant

--- scripts/tasks/eslint.ts
+++ scripts/tasks/eslint.ts
@@ -4,7 +4,7 @@ import * as constants from './eslint-constants';
 import * as fs from 'fs';
 
 const files = [path.join(process.cwd(), constants.directory)];
-const storiesPath = path.join(process.cwd(), constants.stories);
+const storiesPath = path.join(process.cwd(), 'stories');
 
 if (fs.existsSync(storiesPath)) {
   files.push(storiesPath);
