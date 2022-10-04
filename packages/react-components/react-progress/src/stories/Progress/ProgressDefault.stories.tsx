07d65df009a26d899d90d204954f2dd5fca8840e
Tomi Olubeko <ololubek@microsoft.com>
2022-10-04 13:46:26 -0700

    update stories

--- packages/react-components/react-progress/src/stories/Progress/ProgressDefault.stories.tsx
+++ packages/react-components/react-progress/src/stories/Progress/ProgressDefault.stories.tsx
@@ -4,11 +4,3 @@ import { Progress, ProgressProps } from '@fluentui/react-progress';
 export const Default = (props: Partial<ProgressProps>) => {
   return <Progress {...props} value={0.5} />;
 };
-
-Default.parameters = {
-  docs: {
-    description: {
-      story: `Determinate Progress bar`,
-    },
-  },
-};
