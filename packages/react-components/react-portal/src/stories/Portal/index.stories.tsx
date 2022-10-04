27fbb128b89ef21531d9249bb33c3230ab7eda99
Micah Godbolt <micahgodbolt@gmail.com>
2022-10-04 11:04:16 -0700

    Update packages/react-components/react-portal/src/stories/Portal/index.stories.tsx
    Co-authored-by: Tristan Watanabe <tristan.watanabe@gmail.com>

--- packages/react-components/react-portal/src/stories/Portal/index.stories.tsx
+++ packages/react-components/react-portal/src/stories/Portal/index.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Meta } from '@storybook/react';
-import { Portal } from '@fluentui/react-portal';
+import { Portal } from '@fluentui/react-components';
 import descriptionMd from './PortalDescription.md';
 export { Default } from './PortalDefault.stories';
 
