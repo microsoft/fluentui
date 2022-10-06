6fed3619838c479dc2a30eb6cbad46ed5ec62e01
Charles <chassunc@microsoft.com>
2022-10-06 09:32:56 -0300

    chore: remove context call from ToolbarDivider

--- packages/react-components/react-toolbar/src/components/ToolbarDivider/ToolbarDivider.tsx
+++ packages/react-components/react-toolbar/src/components/ToolbarDivider/ToolbarDivider.tsx
@@ -3,15 +3,13 @@ import { useToolbarDividerStyles_unstable } from './useToolbarDividerStyles';
 import type { ToolbarDividerProps } from './ToolbarDivider.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
 import { renderDivider_unstable } from '@fluentui/react-divider';
-import { useToolbarContext } from '../Toolbar/ToolbarContext';
 import { useToolbarDivider_unstable } from './useToolbarDivider';
 
 /**
  * ToolbarDivider component
  */
 export const ToolbarDivider: ForwardRefComponent<ToolbarDividerProps> = React.forwardRef((props, ref) => {
-  const { vertical } = useToolbarContext();
-  const state = useToolbarDivider_unstable({ vertical: !vertical, ...props }, ref);
+  const state = useToolbarDivider_unstable(props, ref);
   useToolbarDividerStyles_unstable(state);
   return renderDivider_unstable(state);
 });
