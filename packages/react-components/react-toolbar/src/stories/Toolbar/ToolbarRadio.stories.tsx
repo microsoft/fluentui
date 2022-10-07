4bf9366965790125ca738b26ccdb888b9dc78594
Charles <chassunc@microsoft.com>
2022-10-07 09:57:10 -0300

    chore: remove unnecesary storie file

--- packages/react-components/react-toolbar/src/stories/Toolbar/ToolbarRadio.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ToolbarRadio } from '../../ToolbarRadio';
-
-export { Radio } from './ToolbarRadioDefault.stories';
-
-export default {
-  title: 'Components/ToolbarRadio',
-  component: ToolbarRadio,
-};
