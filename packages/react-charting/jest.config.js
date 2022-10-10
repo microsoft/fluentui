beadede0c8d4ea3a07be3d1aec643e0cf8b279fa
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2022-10-10 20:53:10 +0530

    Add transform ignore pattern

--- packages/react-charting/jest.config.js
+++ packages/react-charting/jest.config.js
@@ -1,14 +1,16 @@
 let { createConfig, resolveMergeStylesSerializer } = require('@fluentui/scripts/jest/jest-resources');
 let path = require('path');
 
+const esModules = ['@d3-scale', 'd3-scale'].join('|');
+
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: [resolveMergeStylesSerializer(), 'enzyme-to-json/serializer'],
-  transform: {},
-  moduleNameMapper: {
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+  /*moduleNameMapper: {
     // '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js',
     '^d3-scale$': require.resolve('d3-scale'),
-  },
+  },*/
 });
 
 module.exports = config;
