9f8288fa8fea1bd381ecce6e86aaf30e4e16f4a1
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2022-10-10 20:23:17 +0530

    Test jest config change

--- packages/react-charting/jest.config.js
+++ packages/react-charting/jest.config.js
@@ -5,6 +5,10 @@ const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: [resolveMergeStylesSerializer(), 'enzyme-to-json/serializer'],
   transform: {},
+  moduleNameMapper: {
+    // '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js',
+    '^d3-scale$': require.resolve('d3-scale'),
+  },
 });
 
 module.exports = config;
