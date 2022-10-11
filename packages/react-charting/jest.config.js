d502474618bd8e28c1aa4ceb1e547fe4fdca54e2
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2022-10-11 19:27:56 +0530

    Revert unnecessary changes

--- packages/react-charting/jest.config.js
+++ packages/react-charting/jest.config.js
@@ -1,8 +1,6 @@
 let { createConfig, resolveMergeStylesSerializer } = require('@fluentui/scripts/jest/jest-resources');
 let path = require('path');
 
-const esModules = ['@d3-scale', 'd3-scale'].join('|');
-
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: [resolveMergeStylesSerializer(), 'enzyme-to-json/serializer'],
