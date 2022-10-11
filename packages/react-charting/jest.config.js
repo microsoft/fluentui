b609fc71e96d8f884875fd70632931c41e94c31c
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2022-10-11 15:02:12 +0530

    Update jest config

--- packages/react-charting/jest.config.js
+++ packages/react-charting/jest.config.js
@@ -6,10 +6,14 @@ const esModules = ['@d3-scale', 'd3-scale'].join('|');
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: [resolveMergeStylesSerializer(), 'enzyme-to-json/serializer'],
-  transformIgnorePatterns: ['/node_modules/(?!(d3-scale)/)'],
   moduleNameMapper: {
-    // '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js',
-    '^d3-scale$': require.resolve('d3-scale'),
+    'd3-scale': 'd3-scale/dist/d3-scale.min.js',
+    'd3-array': 'd3-scale/node_modules/d3-array/dist/d3-array.min.js',
+    'd3-interpolate': 'd3-scale/node_modules/d3-interpolate/dist/d3-interpolate.min.js',
+    'd3-color': 'd3-scale/node_modules/d3-color/dist/d3-color.min.js',
+    'd3-format': 'd3-scale/node_modules/d3-format/dist/d3-format.min.js',
+    'd3-time-format': 'd3-scale/node_modules/d3-time-format/dist/d3-time-format.min.js',
+    'd3-time': 'd3-scale/node_modules/d3-time/dist/d3-time.min.js',
   },
 });
 
