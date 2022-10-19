64295e1393870afa784166ee8b0c31ecafa1e5d0
EnricoGianoglio <egianoglio@microsoft.com>
2022-10-19 14:26:50 +0200

    Change svg heights

--- packages/fluentui/react-northstar/src/themes/teams/components/Loader/loaderVariables.ts
+++ packages/fluentui/react-northstar/src/themes/teams/components/Loader/loaderVariables.ts
@@ -33,13 +33,13 @@ export const loaderVariables = (siteVariables: any): LoaderVariables => ({
   },
 
   svgHeights: {
-    smaller: pxToRem(1464),
-    smallest: pxToRem(1464),
-    small: pxToRem(1464),
-    medium: pxToRem(2196),
-    large: pxToRem(4392),
-    larger: pxToRem(4392),
-    largest: pxToRem(4392),
+    smallest: pxToRem(24),
+    smaller: pxToRem(24),
+    small: pxToRem(24),
+    medium: pxToRem(36),
+    large: pxToRem(72),
+    larger: pxToRem(72),
+    largest: pxToRem(72),
   },
   svgTranslatePosition: {
     smallest: pxToRem(-1440),
