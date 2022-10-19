f11d29e54d6d9ae0fdfe9dce1b5c70368f082128
Adam Samec <adam.samec@gmail.com>
2022-10-19 14:04:09 +0200

    Complete the Card scenario

--- packages/react-components/react-components/src/AccessibilityScenarios/Card.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityScenarios/Card.stories.tsx
@@ -1,39 +1,69 @@
 import * as React from 'react';
 
 import { Card, CardHeader, CardFooter } from '@fluentui/react-components/unstable';
-
 import { Button } from '@fluentui/react-components';
 
 import { Scenario } from './utils';
 
-export const UserProfileCards: React.FunctionComponent = () => {
-  return (
-    <Scenario pageTitle="User profile cards">
-      <div role="group" aria-label="Press enter to enter card contents, press Escape to leave it">
-        <Card focusMode="no-tab" aria-label="Bruce Wayne card">
-          <CardHeader
-            image={{ as: 'img', src: '#', alt: 'Face of a person' }}
-            header={<h2>Bruce Wayne</h2>}
-            description="Last seen 5 hours ago."
-          />
-          <CardFooter>
-            <Button>Private message</Button>
-            <Button aria-haspopup="menu">More options</Button>
-          </CardFooter>
-        </Card>
+type FocusModeType = 'off' | 'no-tab' | 'tab-exit' | 'tab-only';
 
-        <Card focusMode="no-tab" aria-label="Clark Kent card">
-          <CardHeader
-            image={{ as: 'img', src: '#', alt: 'Face of a person' }}
-            header={<h2>Clark Kent</h2>}
-            description="Last seen 30 minutes ago."
-          />
-          <CardFooter>
-            <Button>Private message</Button>
-            <Button aria-haspopup="menu">More options</Button>
-          </CardFooter>
-        </Card>
-      </div>
-    </Scenario>
+interface CardTemplateProps {
+  cardId: string;
+  name: string;
+  lastSeen: string;
+  focusMode: FocusModeType;
+}
+const CardTemplate: React.FunctionComponent<CardTemplateProps> = ({ cardId, name, lastSeen, focusMode }) => {
+  const messageDescId = `${cardId}-message-desc`;
+  return (
+    <Card focusMode={focusMode} aria-label={`${name} card`}>
+      <CardHeader
+        image={{ as: 'img', src: '#', alt: 'Face of a person' }}
+        header={{ as: 'h3', children: name }}
+        description={`Last seen ${lastSeen} ago.`}
+      />
+      <CardFooter>
+        <Button aria-describedby={messageDescId}>Private message</Button>
+        <span id={messageDescId} style={{ display: 'none' }}>
+          Send a private message to {name}.
+        </span>
+        <Button aria-haspopup="menu">More options</Button>
+      </CardFooter>
+    </Card>
   );
 };
+
+interface CardGroupProps {
+  groupId: string;
+  focusMode: FocusModeType;
+}
+const CardGroup: React.FunctionComponent<CardGroupProps> = ({ groupId, focusMode }) => (
+  <>
+    <CardTemplate cardId={`card1-${groupId}`} name="Bruce wayne" lastSeen="4 hours" focusMode={focusMode} />
+    <CardTemplate cardId={`card2-${groupId}`} name="Clark Kent" lastSeen="30 minutes" focusMode={focusMode} />
+    <CardTemplate cardId={`card3-${groupId}`} name="Peter Parker" lastSeen="2 days" focusMode={focusMode} />
+  </>
+);
+
+export const UserProfileCards: React.FunctionComponent = () => (
+  <Scenario pageTitle="User profile cards">
+    <h2>Variant 1: No card focus with Tab</h2>
+    <CardGroup groupId="group1" focusMode="off" />
+
+    <h2>Variant 2: Enter card with Enter and leave with Escape only</h2>
+    <div role="group" aria-label="Press enter to enter card contents, press Escape to leave it">
+      <CardGroup groupId="group2" focusMode="no-tab" />
+    </div>
+
+    <h2>Variant 3: Enter card with Enter and leave with Escape or Tab</h2>
+    <div
+      role="group"
+      aria-label="Press enter to enter card contents, press Escape or Tab when on last element to leave it"
+    >
+      <CardGroup groupId="group3" focusMode="tab-exit" />
+    </div>
+
+    <h2>Variant 4: Focus card with Tab, but no enter or leave</h2>
+    <CardGroup groupId="group4" focusMode="tab-only" />
+  </Scenario>
+);
