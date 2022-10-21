3b2b61c05be205223a93fa74b8fb7cd78931afe9
EnricoGianoglio <egianoglio@microsoft.com>
2022-10-22 01:04:04 +0200

    Set black background to HC secondary example

--- packages/fluentui/docs/src/examples/components/Loader/Variations/LoaderExampleSecondary.tsx
+++ packages/fluentui/docs/src/examples/components/Loader/Variations/LoaderExampleSecondary.tsx
@@ -1,20 +1,27 @@
 import { Loader, Flex, Provider } from '@fluentui/react-northstar';
 import * as React from 'react';
 
-const LoaderExampleSecondary: React.FC = () => (
-  <Provider.Consumer
-    render={theme => (
-      <Flex
-        style={{
-          backgroundColor: theme.siteVariables.colorScheme.brand.background,
-          padding: 8,
-          width: 'fit-content',
-        }}
-      >
-        <Loader secondary />
-      </Flex>
-    )}
-  />
-);
+const LoaderExampleSecondary: React.FC = () => {
+  return (
+    <Provider.Consumer
+      render={theme => {
+        const themeBackgroundColor = theme.siteVariables.colorScheme.brand.background;
+        const siteVariablesColors = theme.siteVariables.colors;
+        return (
+          <Flex
+            style={{
+              backgroundColor:
+                themeBackgroundColor !== siteVariablesColors.white ? themeBackgroundColor : siteVariablesColors.black,
+              padding: 8,
+              width: 'fit-content',
+            }}
+          >
+            <Loader secondary />
+          </Flex>
+        );
+      }}
+    />
+  );
+};
 
 export default LoaderExampleSecondary;
