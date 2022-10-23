f1f59baccbc2187b5e89112eb6bc65d2e4415cfa
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-10-22 20:55:02 -0700

    chore(Checkbox): convert Checkbox stories to CSF

--- apps/vr-tests-react-components/src/stories/Checkbox/Checkbox.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Checkbox/Checkbox.stories.tsx
@@ -1,64 +1,116 @@
 import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
-import { storiesOf } from '@storybook/react';
 import { Checkbox } from '@fluentui/react-checkbox';
 import { TestWrapperDecoratorFixedWidth } from '../../utilities/TestWrapperDecorator';
+import { ComponentMeta } from '@storybook/react';
+import { getStoryVariant, RTL } from '../../utilities';
 
-storiesOf('Checkbox Converged', module)
-  .addDecorator(TestWrapperDecoratorFixedWidth)
-  .addDecorator(story => (
-    <Screener steps={new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>{story()}</Screener>
-  ))
-  .addStory('disabled+checked', () => <Checkbox disabled checked label="Disabled checked" />)
-  .addStory('disabled+mixed', () => <Checkbox disabled checked="mixed" label="Disabled mixed" />)
-  .addStory('no-label', () => <Checkbox />)
-  .addStory('label-before', () => <Checkbox labelPosition="before" label="Label before" />, { includeRtl: true })
-  .addStory(
-    'label-wrapping',
-    () => (
-      <Checkbox
-        label={
-          <>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua
-          </>
-        }
-      />
-    ),
-    { includeRtl: true },
-  )
-  .addStory('required', () => <Checkbox required label="Required" />)
-  .addStory('required+label-before', () => (
-    <Checkbox required labelPosition="before" label="Required with label before" />
-  ))
-  .addStory('circular', () => <Checkbox shape="circular" label="Circular" />)
-  .addStory('circular+checked', () => <Checkbox shape="circular" checked label="Circular checked" />)
-  .addStory('circular+mixed', () => <Checkbox shape="circular" checked="mixed" label="Circular mixed" />)
-  //
-  // large variants
-  //
-  .addStory('large', () => <Checkbox size="large" label="Large" />, { includeRtl: true })
-  .addStory('large+checked', () => <Checkbox size="large" checked label="Large checked" />)
-  .addStory('large+mixed', () => <Checkbox size="large" checked="mixed" label="Large mixed" />)
-  .addStory('large+circular', () => <Checkbox size="large" shape="circular" label="Large circular" />)
-  .addStory('large+circular+checked', () => (
-    <Checkbox size="large" shape="circular" checked label="Large circular checked" />
-  ))
-  .addStory('large+circular+mixed', () => (
-    <Checkbox size="large" shape="circular" checked="mixed" label="Large circular mixed" />
-  ))
-  .addStory(
-    'large+label-wrapping',
-    () => (
-      <Checkbox
-        size="large"
-        label={
-          <>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua
-          </>
-        }
-      />
-    ),
-    { includeRtl: true },
-  );
+export default {
+  title: 'Checkbox Converged',
+
+  decorators: [
+    TestWrapperDecoratorFixedWidth,
+    story => <Screener steps={new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>{story()}</Screener>,
+  ],
+} as ComponentMeta<typeof Checkbox>;
+
+export const DisabledChecked = () => <Checkbox disabled checked label="Disabled checked" />;
+
+DisabledChecked.storyName = 'disabled+checked';
+
+export const DisabledMixed = () => <Checkbox disabled checked="mixed" label="Disabled mixed" />;
+
+DisabledMixed.storyName = 'disabled+mixed';
+
+export const NoLabel = () => <Checkbox />;
+
+NoLabel.storyName = 'no-label';
+
+export const LabelBefore = () => <Checkbox labelPosition="before" label="Label before" />;
+
+LabelBefore.storyName = 'label-before';
+
+export const LabelBeforeRTL = getStoryVariant(LabelBefore, RTL);
+
+export const LabelWrapping = () => (
+  <Checkbox
+    label={
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua
+      </>
+    }
+  />
+);
+
+LabelWrapping.storyName = 'label-wrapping';
+
+export const LabelWrappingRTL = getStoryVariant(LabelWrapping, RTL);
+
+export const Required = () => <Checkbox required label="Required" />;
+
+Required.storyName = 'required';
+
+export const RequiredLabelBefore = () => (
+  <Checkbox required labelPosition="before" label="Required with label before" />
+);
+
+RequiredLabelBefore.storyName = 'required+label-before';
+
+export const Circular = () => <Checkbox shape="circular" label="Circular" />;
+
+Circular.storyName = 'circular';
+
+export const CircularChecked = () => <Checkbox shape="circular" checked label="Circular checked" />;
+
+CircularChecked.storyName = 'circular+checked';
+
+export const CircularMixed = () => <Checkbox shape="circular" checked="mixed" label="Circular mixed" />;
+
+CircularMixed.storyName = 'circular+mixed';
+
+export const Large = () => <Checkbox size="large" label="Large" />;
+
+Large.storyName = 'large';
+
+export const LargeRTL = getStoryVariant(Large, RTL);
+
+export const LargeChecked = () => <Checkbox size="large" checked label="Large checked" />;
+
+LargeChecked.storyName = 'large+checked';
+
+export const LargeMixed = () => <Checkbox size="large" checked="mixed" label="Large mixed" />;
+
+LargeMixed.storyName = 'large+mixed';
+
+export const LargeCircular = () => <Checkbox size="large" shape="circular" label="Large circular" />;
+
+LargeCircular.storyName = 'large+circular';
+
+export const LargeCircularChecked = () => (
+  <Checkbox size="large" shape="circular" checked label="Large circular checked" />
+);
+
+LargeCircularChecked.storyName = 'large+circular+checked';
+
+export const LargeCircularMixed = () => (
+  <Checkbox size="large" shape="circular" checked="mixed" label="Large circular mixed" />
+);
+
+LargeCircularMixed.storyName = 'large+circular+mixed';
+
+export const LargeLabelWrapping = () => (
+  <Checkbox
+    size="large"
+    label={
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua
+      </>
+    }
+  />
+);
+
+LargeLabelWrapping.storyName = 'large+label-wrapping';
+
+export const LargeLabelWrappingRTL = getStoryVariant(LargeLabelWrapping, RTL);
--- apps/vr-tests-react-components/src/stories/Checkbox/CheckboxInteractions.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Checkbox/CheckboxInteractions.stories.tsx
@@ -1,25 +1,38 @@
 import * as React from 'react';
-import Screener, { Steps } from 'screener-storybook/src/screener';
-import { storiesOf } from '@storybook/react';
+import { Steps } from 'screener-storybook/src/screener';
 import { Checkbox } from '@fluentui/react-checkbox';
 import { TestWrapperDecoratorFixedWidth } from '../../utilities/TestWrapperDecorator';
+import { ComponentMeta } from '@storybook/react';
+import { getStoryVariant, withScreenerSteps, RTL } from '../../utilities';
 
-storiesOf('Checkbox Converged', module)
-  .addDecorator(TestWrapperDecoratorFixedWidth)
-  .addDecorator(story => (
-    <Screener
-      steps={new Steps()
-        .snapshot('rest', { cropTo: '.testWrapper' })
-        .hover('input')
-        .snapshot('hover', { cropTo: '.testWrapper' })
-        .mouseDown('input')
-        .snapshot('active', { cropTo: '.testWrapper' })
-        .end()}
-    >
-      {story()}
-    </Screener>
-  ))
-  .addStory('unchecked', () => <Checkbox label="Unchecked" />, { includeRtl: true })
-  .addStory('checked', () => <Checkbox checked label="Checked" />)
-  .addStory('mixed', () => <Checkbox checked="mixed" label="Mixed" />)
-  .addStory('disabled', () => <Checkbox disabled label="Disabled" />);
+const steps = new Steps()
+  .snapshot('rest', { cropTo: '.testWrapper' })
+  .hover('input')
+  .snapshot('hover', { cropTo: '.testWrapper' })
+  .mouseDown('input')
+  .snapshot('active', { cropTo: '.testWrapper' })
+  .end();
+
+export default {
+  title: 'Checkbox Converged',
+
+  decorators: [TestWrapperDecoratorFixedWidth, story => withScreenerSteps({ steps, story })],
+} as ComponentMeta<typeof Checkbox>;
+
+export const Unchecked = () => <Checkbox label="Unchecked" />;
+
+Unchecked.storyName = 'unchecked';
+
+export const UncheckedRTL = getStoryVariant(Unchecked, RTL);
+
+export const Checked = () => <Checkbox checked label="Checked" />;
+
+Checked.storyName = 'checked';
+
+export const Mixed = () => <Checkbox checked="mixed" label="Mixed" />;
+
+Mixed.storyName = 'mixed';
+
+export const Disabled = () => <Checkbox disabled label="Disabled" />;
+
+Disabled.storyName = 'disabled';
