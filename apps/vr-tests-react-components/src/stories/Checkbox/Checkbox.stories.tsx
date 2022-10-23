322859018ea732b20bbef5420f6d1d424c68b241
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-10-22 20:42:04 -0700

    chore(Checkbox): split Checkbox stories to different files

--- apps/vr-tests-react-components/src/stories/Checkbox/Checkbox.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Checkbox/Checkbox.stories.tsx
@@ -2,27 +2,7 @@ import * as React from 'react';
 import Screener, { Steps } from 'screener-storybook/src/screener';
 import { storiesOf } from '@storybook/react';
 import { Checkbox } from '@fluentui/react-checkbox';
-import { TestWrapperDecoratorFixedWidth } from '../utilities/TestWrapperDecorator';
-
-storiesOf('Checkbox Converged', module)
-  .addDecorator(TestWrapperDecoratorFixedWidth)
-  .addDecorator(story => (
-    <Screener
-      steps={new Steps()
-        .snapshot('rest', { cropTo: '.testWrapper' })
-        .hover('input')
-        .snapshot('hover', { cropTo: '.testWrapper' })
-        .mouseDown('input')
-        .snapshot('active', { cropTo: '.testWrapper' })
-        .end()}
-    >
-      {story()}
-    </Screener>
-  ))
-  .addStory('unchecked', () => <Checkbox label="Unchecked" />, { includeRtl: true })
-  .addStory('checked', () => <Checkbox checked label="Checked" />)
-  .addStory('mixed', () => <Checkbox checked="mixed" label="Mixed" />)
-  .addStory('disabled', () => <Checkbox disabled label="Disabled" />);
+import { TestWrapperDecoratorFixedWidth } from '../../utilities/TestWrapperDecorator';
 
 storiesOf('Checkbox Converged', module)
   .addDecorator(TestWrapperDecoratorFixedWidth)
