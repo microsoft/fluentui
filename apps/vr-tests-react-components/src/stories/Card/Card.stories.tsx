574a39d790cdfd038386f2e2b6f2955f665f6ee7
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-10-22 19:01:11 -0700

    chore(Card): convert Card stories to CSF

--- apps/vr-tests-react-components/src/stories/Card/Card.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Card/Card.stories.tsx
@@ -1,4 +1,3 @@
-import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Screener from 'screener-storybook/src/screener';
 import { Card, CardHeader, CardPreview } from '@fluentui/react-card';
@@ -6,159 +5,177 @@ import { MoreHorizontal24Filled } from '@fluentui/react-icons';
 import { Body1, Caption1 } from '@fluentui/react-text';
 import { Button } from '@fluentui/react-button';
 import { powerpointLogoURL, salesPresentationTemplateURL, SampleCardContent } from './utils';
+import { ComponentMeta } from '@storybook/react';
+import { getStoryVariant, DARK_MODE, HIGH_CONTRAST, RTL } from '../../utilities';
 
-storiesOf('Card Converged', module)
-  .addDecorator(story => (
-    <Screener steps={new Screener.Steps().snapshot('normal', { cropTo: '.testWrapper' }).end()}>
-      <div className="testWrapper" style={{ width: '600px' }}>
-        {story()}
-      </div>
-    </Screener>
-  ))
-  .addStory('card templates', () => (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <Card>
-        <CardPreview>
-          <img src={salesPresentationTemplateURL} alt="sales presentation preview" />
-        </CardPreview>
-        <CardHeader
-          image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
-          header={
-            <Body1>
-              <b>Sales analysis 2019 presentation</b>
-            </Body1>
-          }
-          description={<Caption1>Folder &gt; Presentations</Caption1>}
-        />
-      </Card>
-    </div>
-  ))
-  .addStory(
-    'appearance',
-    () => (
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-        <div>
-          <h1>Filled</h1>
-          <Card appearance="filled">
-            <SampleCardContent />
-          </Card>
+export default {
+  title: 'Card Converged',
+
+  decorators: [
+    story => (
+      <Screener steps={new Screener.Steps().snapshot('normal', { cropTo: '.testWrapper' }).end()}>
+        <div className="testWrapper" style={{ width: '600px' }}>
+          {story()}
         </div>
-        <div>
-          <h1>Filled alternative</h1>
-          <Card appearance="filled-alternative">
-            <SampleCardContent />
-          </Card>
-        </div>
-        <div>
-          <h1>Outline</h1>
-          <Card appearance="outline">
-            <SampleCardContent />
-          </Card>
-        </div>
-        <div>
-          <h1>Subtle</h1>
-          <Card appearance="subtle">
-            <SampleCardContent />
-          </Card>
-        </div>
-      </div>
+      </Screener>
     ),
-    {
-      includeRtl: true,
-      includeHighContrast: true,
-      includeDarkMode: true,
-    },
-  )
-  .addStory('size', () => (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <Card size="small">
-        <SampleCardContent />
-      </Card>
-      <Card size="medium">
-        <SampleCardContent />
-      </Card>
-      <Card size="large">
-        <SampleCardContent />
-      </Card>
-    </div>
-  ))
-  .addStory('orientation', () => (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <div>
-        <h1>Vertical</h1>
-        <Card orientation="vertical">
-          <SampleCardContent />
-        </Card>
-      </div>
-      <div>
-        <h1>Horizontal</h1>
-        <Card orientation="horizontal">
-          <SampleCardContent />
-        </Card>
-      </div>
-    </div>
-  ))
-  .addStory('CardHeader', () => (
+  ],
+} as ComponentMeta<typeof Card>;
+
+export const CardTemplates = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
     <Card>
+      <CardPreview>
+        <img src={salesPresentationTemplateURL} alt="sales presentation preview" />
+      </CardPreview>
       <CardHeader
         image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
         header={
           <Body1>
-            <b>App Name</b>
+            <b>Sales analysis 2019 presentation</b>
           </Body1>
         }
-        description={<Caption1>Developer</Caption1>}
-        action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
-      />
-      <CardHeader
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
-        description={<Caption1>Developer</Caption1>}
-        action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
-      />
-      <CardHeader
-        image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
-        action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
-      />
-      <CardHeader
-        image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
-        description={<Caption1>Developer</Caption1>}
-      />
-      <CardHeader
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
-        action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
-      />
-      <CardHeader
-        image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
-      />
-      <CardHeader
-        header={
-          <Body1>
-            <b>App Name</b>
-          </Body1>
-        }
+        description={<Caption1>Folder &gt; Presentations</Caption1>}
       />
     </Card>
-  ));
+  </div>
+);
+
+CardTemplates.storyName = 'card templates';
+
+export const Appearance = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+    <div>
+      <h1>Filled</h1>
+      <Card appearance="filled">
+        <SampleCardContent />
+      </Card>
+    </div>
+    <div>
+      <h1>Filled alternative</h1>
+      <Card appearance="filled-alternative">
+        <SampleCardContent />
+      </Card>
+    </div>
+    <div>
+      <h1>Outline</h1>
+      <Card appearance="outline">
+        <SampleCardContent />
+      </Card>
+    </div>
+    <div>
+      <h1>Subtle</h1>
+      <Card appearance="subtle">
+        <SampleCardContent />
+      </Card>
+    </div>
+  </div>
+);
+
+Appearance.storyName = 'appearance';
+
+export const AppearanceDarkMode = getStoryVariant(Appearance, DARK_MODE);
+export const AppearanceHighContrast = getStoryVariant(Appearance, HIGH_CONTRAST);
+export const AppearanceRTL = getStoryVariant(Appearance, RTL);
+
+export const Size = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+    <Card size="small">
+      <SampleCardContent />
+    </Card>
+    <Card size="medium">
+      <SampleCardContent />
+    </Card>
+    <Card size="large">
+      <SampleCardContent />
+    </Card>
+  </div>
+);
+
+Size.storyName = 'size';
+
+export const Orientation = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+    <div>
+      <h1>Vertical</h1>
+      <Card orientation="vertical">
+        <SampleCardContent />
+      </Card>
+    </div>
+    <div>
+      <h1>Horizontal</h1>
+      <Card orientation="horizontal">
+        <SampleCardContent />
+      </Card>
+    </div>
+  </div>
+);
+
+Orientation.storyName = 'orientation';
+
+export const _CardHeader = () => (
+  <Card>
+    <CardHeader
+      image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+      description={<Caption1>Developer</Caption1>}
+      action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
+    />
+    <CardHeader
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+      description={<Caption1>Developer</Caption1>}
+      action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
+    />
+    <CardHeader
+      image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+      action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
+    />
+    <CardHeader
+      image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+      description={<Caption1>Developer</Caption1>}
+    />
+    <CardHeader
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+      action={<Button appearance="transparent" icon={<MoreHorizontal24Filled />} />}
+    />
+    <CardHeader
+      image={{ as: 'img', src: powerpointLogoURL, alt: 'Microsoft PowerPoint logo' }}
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+    />
+    <CardHeader
+      header={
+        <Body1>
+          <b>App Name</b>
+        </Body1>
+      }
+    />
+  </Card>
+);
+
+_CardHeader.storyName = 'CardHeader';
--- apps/vr-tests-react-components/src/stories/Card/CardInteractions.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Card/CardInteractions.stories.tsx
@@ -1,75 +1,115 @@
-import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Screener from 'screener-storybook/src/screener';
 import { Card } from '@fluentui/react-card';
 import { action } from '@storybook/addon-actions';
 import { SampleCardContent } from './utils';
+import { ComponentMeta } from '@storybook/react';
+import { getStoryVariant, DARK_MODE, HIGH_CONTRAST, RTL } from '../../utilities';
 
-storiesOf('Card Converged', module)
-  .addDecorator(story => (
-    <Screener
-      steps={new Screener.Steps()
-        .snapshot('normal', { cropTo: '.testWrapper' })
-        .hover('[role="group"]')
-        .snapshot('focused', { cropTo: '.testWrapper' })
-        .mouseDown('[role="group"]')
-        .snapshot('clicked', { cropTo: '.testWrapper' })
-        .end()}
-    >
-      <div className="testWrapper" style={{ width: '300px' }}>
-        {story()}
-      </div>
-    </Screener>
-  ))
-  .addStory(
-    'appearance interactive - Filled',
-    () => (
-      <Card onClick={action('filled card clicked')} appearance="filled">
-        <SampleCardContent />
-      </Card>
+export default {
+  title: 'Card Converged',
+
+  decorators: [
+    story => (
+      <Screener
+        steps={new Screener.Steps()
+          .snapshot('normal', { cropTo: '.testWrapper' })
+          .hover('[role="group"]')
+          .snapshot('focused', { cropTo: '.testWrapper' })
+          .mouseDown('[role="group"]')
+          .snapshot('clicked', { cropTo: '.testWrapper' })
+          .end()}
+      >
+        <div className="testWrapper" style={{ width: '300px' }}>
+          {story()}
+        </div>
+      </Screener>
     ),
-    {
-      includeRtl: true,
-      includeHighContrast: true,
-      includeDarkMode: true,
-    },
-  )
-  .addStory(
-    'appearance interactive - Filled Alternative',
-    () => (
-      <Card onClick={action('filled alternative card clicked')} appearance="filled-alternative">
-        <SampleCardContent />
-      </Card>
-    ),
-    {
-      includeRtl: true,
-      includeHighContrast: true,
-      includeDarkMode: true,
-    },
-  )
-  .addStory(
-    'appearance interactive - Outline',
-    () => (
-      <Card onClick={action('outline card clicked')} appearance="outline">
-        <SampleCardContent />
-      </Card>
-    ),
-    {
-      includeRtl: true,
-      includeHighContrast: true,
-      includeDarkMode: true,
-    },
-  )
-  .addStory(
-    'appearance interactive - Subtle',
-    () => (
-      <Card onClick={action('subtle card clicked')} appearance="subtle">
-        <SampleCardContent />
-      </Card>
-    ),
-    {
-      includeRtl: true,
-      includeHighContrast: true,
-      includeDarkMode: true,
-    },
-  );
+  ],
+} as ComponentMeta<typeof Card>;
+
+export const AppearanceInteractiveFilled = () => (
+  <Card onClick={action('filled card clicked')} appearance="filled">
+    <SampleCardContent />
+  </Card>
+);
+
+AppearanceInteractiveFilled.story = {
+  name: 'appearance interactive - Filled',
+
+  parameters: {
+    includeRtl: true,
+    includeHighContrast: true,
+    includeDarkMode: true,
+  },
+};
+
+export const AppearanceInteractiveFilledDarkMode = getStoryVariant(AppearanceInteractiveFilled, DARK_MODE);
+export const AppearanceInteractiveFilledHighContrast = getStoryVariant(AppearanceInteractiveFilled, HIGH_CONTRAST);
+export const AppearanceInteractiveFilledRTL = getStoryVariant(AppearanceInteractiveFilled, RTL);
+
+export const AppearanceInteractiveFilledAlternative = () => (
+  <Card onClick={action('filled alternative card clicked')} appearance="filled-alternative">
+    <SampleCardContent />
+  </Card>
+);
+
+AppearanceInteractiveFilledAlternative.story = {
+  name: 'appearance interactive - Filled Alternative',
+
+  parameters: {
+    includeRtl: true,
+    includeHighContrast: true,
+    includeDarkMode: true,
+  },
+};
+
+export const AppearanceInteractiveFilledAlternativeDarkMode = getStoryVariant(
+  AppearanceInteractiveFilledAlternative,
+  DARK_MODE,
+);
+export const AppearanceInteractiveFilledAlternativeHighContrast = getStoryVariant(
+  AppearanceInteractiveFilledAlternative,
+  HIGH_CONTRAST,
+);
+export const AppearanceInteractiveFilledAlternativeRTL = getStoryVariant(AppearanceInteractiveFilledAlternative, RTL);
+
+export const AppearanceInteractiveOutline = () => (
+  <Card onClick={action('outline card clicked')} appearance="outline">
+    <SampleCardContent />
+  </Card>
+);
+
+AppearanceInteractiveOutline.story = {
+  name: 'appearance interactive - Outline',
+
+  parameters: {
+    includeRtl: true,
+    includeHighContrast: true,
+    includeDarkMode: true,
+  },
+};
+
+export const AppearanceInteractiveOutlineDarkMode = getStoryVariant(AppearanceInteractiveOutline, DARK_MODE);
+export const AppearanceInteractiveOutlineHighContrast = getStoryVariant(AppearanceInteractiveOutline, HIGH_CONTRAST);
+export const AppearanceInteractiveOutlineRTL = getStoryVariant(AppearanceInteractiveOutline, RTL);
+
+export const AppearanceInteractiveSubtle = () => (
+  <Card onClick={action('subtle card clicked')} appearance="subtle">
+    <SampleCardContent />
+  </Card>
+);
+
+AppearanceInteractiveSubtle.story = {
+  name: 'appearance interactive - Subtle',
+
+  parameters: {
+    includeRtl: true,
+    includeHighContrast: true,
+    includeDarkMode: true,
+  },
+};
+
+export const AppearanceInteractiveSubtleDarkMode = getStoryVariant(AppearanceInteractiveSubtle, DARK_MODE);
+export const AppearanceInteractiveSubtleHighContrast = getStoryVariant(AppearanceInteractiveSubtle, HIGH_CONTRAST);
+export const AppearanceInteractiveSubtleRTL = getStoryVariant(AppearanceInteractiveSubtle, RTL);
