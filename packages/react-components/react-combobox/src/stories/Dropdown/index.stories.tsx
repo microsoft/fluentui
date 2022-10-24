cf90935c601b04f625c33811f0a16822e93db7f2
Sarah Higley <sarah.higley@microsoft.com>
2022-10-24 15:05:53 -0700

    feat: add Dropdown stories

--- packages/react-components/react-combobox/src/stories/Dropdown/index.stories.tsx
+++ packages/react-components/react-combobox/src/stories/Dropdown/index.stories.tsx
@@ -5,6 +5,11 @@ import descriptionMd from './DropdownDescription.md';
 import bestPracticesMd from './DropdownBestPractices.md';
 
 export { Default } from './DropdownDefault.stories';
+export { Appearance } from './DropdownAppearance.stories';
+export { Grouped } from './DropdownGrouped.stories';
+export { CustomOptions } from './DropdownCustomOptions.stories';
+export { Multiselect } from './DropdownMultiselect.stories';
+export { Size } from './DropdownSize.stories';
 
 export default {
   title: 'Preview Components/Dropdown',
