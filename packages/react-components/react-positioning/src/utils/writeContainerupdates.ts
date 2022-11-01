92e4f17324fd1a459de4dce8b930a5621025207b
Lingfan Gao <lingfangao@hotmail.com>
2022-11-01 13:20:57 +0100

    fix blurriness

--- packages/react-components/react-positioning/src/utils/writeContainerupdates.ts
+++ packages/react-components/react-positioning/src/utils/writeContainerupdates.ts
@@ -49,8 +49,11 @@ export function writeContainerUpdates(options: {
     container.setAttribute(DATA_POSITIONING_HIDDEN, '');
   }
 
+  // decmial translate values can cause blurriness
+  const floorX = Math.floor(x);
+  const floorY = Math.floor(y);
   Object.assign(container.style, {
-    transform: lowPPI ? `translate(${x}px, ${y}px)` : `translate3d(${x}px, ${y}px, 0)`,
+    transform: lowPPI ? `translate(${floorX}px, ${floorY}px)` : `translate3d(${floorX}px, ${floorY}px, 0)`,
     position: strategy,
   });
 }
