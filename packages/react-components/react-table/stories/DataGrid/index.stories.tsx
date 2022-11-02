48c85202ce3c230fdc83bde3ce494f952632b16f
ling1726 <lingfangao@hotmail.com>
2022-11-02 14:54:34 +0100

    feat: Implement child render function for DataGrid rows (#25476)
    * feat: Implement child render function for DataGrid rows
    
    * changefile
    
    * update package.json
    
    * update unit tests

---
+++ packages/react-components/react-table/stories/DataGrid/index.stories.tsx
@@ -0,0 +1,12 @@
+import { DataGrid } from '../../src/components/DataGrid/DataGrid';
+
+export { Default } from './Default.stories';
+export default {
+  title: 'Preview Components/DataGrid',
+  component: DataGrid,
+  parameters: {
+    docs: {
+      description: {},
+    },
+  },
+};
