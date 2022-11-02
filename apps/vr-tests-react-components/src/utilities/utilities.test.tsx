781e403f9b5618858c9ce9104437d949e202b994
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-11-01 20:33:25 -0700

    test: add test for story naming

--- apps/vr-tests-react-components/src/utilities/utilities.test.tsx
+++ apps/vr-tests-react-components/src/utilities/utilities.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { getStoryVariant, DARK_MODE, HIGH_CONTRAST, RTL } from './getStoryVariant';
 import { Button } from '@fluentui/react-button';
+import { ComponentStory } from '@storybook/react';
 
 describe('utility functions', () => {
   describe('getStoryVariant', () => {
@@ -21,6 +22,27 @@ describe('utility functions', () => {
 
       expect(darkModeStory.parameters.fluentTheme).toBe('web-dark');
       expect(highContrastStory.parameters.fluentTheme).toBe('teams-high-contrast');
+    });
+
+    it('should set the correct name for story', () => {
+      const darkModeStory = getStoryVariant(DefaultStory, DARK_MODE);
+      const highContrastStory = getStoryVariant(DefaultStory, HIGH_CONTRAST);
+      const rtlStory = getStoryVariant(DefaultStory, RTL);
+
+      expect(darkModeStory.storyName).toEqual('Default Story - Dark Mode');
+      expect(highContrastStory.storyName).toEqual('Default Story - High Contrast');
+      expect(rtlStory.storyName).toEqual('Default Story - RTL');
+
+      const buttonStory: ComponentStory<any> = DefaultStory;
+      buttonStory.storyName = 'button';
+
+      const buttonDarkModeStory = getStoryVariant(buttonStory, DARK_MODE);
+      const buttonHighContrastStory = getStoryVariant(buttonStory, HIGH_CONTRAST);
+      const buttonRtlStory = getStoryVariant(buttonStory, RTL);
+
+      expect(buttonDarkModeStory.storyName).toEqual('button - Dark Mode');
+      expect(buttonHighContrastStory.storyName).toEqual('button - High Contrast');
+      expect(buttonRtlStory.storyName).toEqual('button - RTL');
     });
   });
 });
