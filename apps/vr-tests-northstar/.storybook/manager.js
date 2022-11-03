b17fafaf978a65e772d7ce692a3f63c5e3221c83
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-11-02 18:36:30 -0700

    scaffold v0 vr tests package

---
+++ apps/vr-tests-northstar/.storybook/manager.js
@@ -0,0 +1,9 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Fabric',
+  }),
+});
