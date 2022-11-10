bb488a3c03a789d6cbfee5fc7ebc7061df5f2fac
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-11-10 03:12:46 -0800

    Add Accordion stories

---
+++ packages/fluentui/docs/src/vr-tests/Accordion/AccordionExampleCustomTitle.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Screener, { Steps } from 'screener-storybook/src/screener';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta } from '@storybook/react';
+import { Accordion, accordionTitleSlotClassNames } from '@fluentui/react-northstar';
+import AccordionExampleCustomTitle from '../../examples/components/Accordion/Visual/AccordionExampleCustomTitle.shorthand';
+
+export default {
+  component: Accordion,
+  title: 'Accordion',
+  decorators: [
+    story => (
+      <Screener
+        steps={new Steps()
+          .focus(`.${accordionTitleSlotClassNames.contentWrapper}`)
+          .snapshot('Focuses the accordion title')}
+      >
+        {story()}
+      </Screener>
+    ),
+  ],
+} as ComponentMeta<typeof Accordion>;
+
+export { AccordionExampleCustomTitle };
