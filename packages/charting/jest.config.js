f43e69007441fe096e42b8ac6441577d0aaee289
Atishay Jain <atisjai@microsoft.com>
2022-11-11 12:32:09 +0530

    Update deps

--- packages/charting/jest.config.js
+++ packages/charting/jest.config.js
@@ -24,8 +24,11 @@ const config = createConfig({
   moduleNameMapper: {
     'd3-scale': 'd3-scale/dist/d3-scale.min.js',
     'd3-array': 'd3-scale/node_modules/d3-array/dist/d3-array.min.js',
+    'd3-interpolate': 'd3-interpolate/dist/d3-interpolate.min.js',
+    'd3-color': 'd3-color/dist/d3-color.min.js',
     'd3-format': 'd3-scale/node_modules/d3-format/dist/d3-format.min.js',
     'd3-time-format': 'd3-scale/node_modules/d3-time-format/dist/d3-time-format.min.js',
+    'd3-time': 'd3-time/dist/d3-time.min.js',
   },
 });
 
