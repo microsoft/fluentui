7faf03956fea4b58d15dba88b4b2772da2609612
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2022-11-12 09:56:47 +0530

    Update d3 scale version from 2.0.0 to 4.0.0 (#25618)
    * Update d3 scale version
    
    * Change files
    
    * Update d3 scale to 4.0.0
    
    * Get cjs mapping for esm
    
    * Update module name mapper
    
    * Update deps
    
    * Update deps
    
    * Move cjs files to function
    
    * Update deps
    
    * Disable d3 interpolate
    
    * Cleanup
    
    * Add d3 color
    
    * Add missing mappings
    
    Co-authored-by: Atishay Jain <atisjai@microsoft.com>

--- stub-packages/react-charting/jest.config.js
+++ stub-packages/react-charting/jest.config.js
@@ -1,9 +1,33 @@
 let { createConfig, resolveMergeStylesSerializer } = require('@uifabric/build/jest/jest-resources');
 let path = require('path');
 
+function getEsmOnlyPackagesToCjsMapping() {
+  /**
+   * relative path to monorepo root
+   */
+  const prefix = `<rootDir>/../../`;
+  /**
+   * map of packages that ship only as ESM
+   */
+  const cjsPathsToEsmOnlyPackages = {
+    '^d3-scale$': prefix + 'node_modules/d3-scale/dist/d3-scale.min.js',
+    '^d3-array$': prefix + 'node_modules/d3-scale/node_modules/d3-array/dist/d3-array.min.js',
+    '^d3-interpolate$': prefix + 'node_modules/d3-interpolate/dist/d3-interpolate.min.js',
+    '^d3-color$': prefix + 'node_modules/d3-color/dist/d3-color.min.js',
+    '^d3-format$': prefix + 'node_modules/d3-scale/node_modules/d3-format/dist/d3-format.min.js',
+    '^d3-time-format$': prefix + 'node_modules/d3-scale/node_modules/d3-time-format/dist/d3-time-format.min.js',
+    '^d3-time$': prefix + 'node_modules/d3-time/dist/d3-time.min.js',
+  };
+  return cjsPathsToEsmOnlyPackages;
+}
+
 const config = createConfig({
   setupFiles: [path.resolve(path.join(__dirname, 'config', 'tests.js'))],
   snapshotSerializers: [resolveMergeStylesSerializer()],
+
+  moduleNameMapper: {
+    ...getEsmOnlyPackagesToCjsMapping(),
+  },
 });
 
 module.exports = config;
