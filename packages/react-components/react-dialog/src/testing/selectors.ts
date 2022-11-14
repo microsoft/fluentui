a510bccdb429d1a038f5c28b84bf3e98b51906b3
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-11-14 13:54:52 +0100

    feat(react-dialog): removes `aria-haspopup` (#25611)
    * feat(react-dialog): removes aria-haspopup
    
    * chore: updates snapshot
    
    * chore: updates e2e trigger selectors

--- packages/react-components/react-dialog/src/testing/selectors.ts
+++ packages/react-components/react-dialog/src/testing/selectors.ts
@@ -1,10 +1,11 @@
 import { dialogSurfaceClassNames, dialogTitleClassNames } from '@fluentui/react-dialog';
 
+export const dialogTriggerOpenId = 'open-btn';
 export const dialogTriggerCloseId = 'close-btn';
 
 export const dialogBackdropSelector = `.${dialogSurfaceClassNames.backdrop}`;
 export const dialogSurfaceSelector = `.${dialogSurfaceClassNames.root}`;
-export const dialogTriggerOpenSelector = `[aria-haspopup="dialog"]`;
+export const dialogTriggerOpenSelector = `#${dialogTriggerOpenId}`;
 export const dialogTriggerCloseSelector = `#${dialogTriggerCloseId}`;
 export const dialogTitleSelector = `.${dialogTitleClassNames.root}`;
 export const dialogActionSelector = `.${dialogTitleClassNames.action}`;
