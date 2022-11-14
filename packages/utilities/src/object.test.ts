129c4cf91d5127e1eb251148e4816c417e4764a6
Sarah Higley <smhigley@users.noreply.github.com>
2022-11-14 10:43:54 -0800

    fix: handle null and undefined in @fluentui/utilities shallowCompare (#25623)

--- packages/utilities/src/object.test.ts
+++ packages/utilities/src/object.test.ts
@@ -1,4 +1,73 @@
-import { assign, filteredAssign, mapEnumByName, values, omit } from './object';
+import { assign, filteredAssign, mapEnumByName, values, omit, shallowCompare } from './object';
+
+describe('shallowCompare', () => {
+  it('returns true for matching objects', () => {
+    const a = {
+      a: 1,
+      b: 'string',
+      c: {
+        d: 2,
+      },
+    };
+
+    const b = { ...a };
+
+    expect(shallowCompare(a, b)).toBeTruthy();
+  });
+
+  it('returns false when one object is a superset of the other', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const a: { [key: string]: any } = {
+      a: 1,
+      b: 'string',
+      c: {
+        d: 2,
+      },
+    };
+
+    const b = { ...a, e: 'extra' };
+
+    expect(shallowCompare(a, b)).toBeFalsy();
+
+    a.e = 'extra';
+    a.f = 3;
+
+    expect(shallowCompare(a, b)).toBeFalsy();
+  });
+
+  it('returns false when nested objects are not strictly equal', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const a: { [key: string]: any } = {
+      a: 1,
+      b: 'string',
+      c: {
+        d: 2,
+      },
+    };
+
+    const b = { ...a, c: { ...a.c } };
+
+    expect(shallowCompare(a, b)).toBeFalsy();
+  });
+
+  it('returns true for two empty objects', () => {
+    expect(shallowCompare({}, {})).toBeTruthy();
+  });
+
+  it('returns true for two falsy values', () => {
+    expect(shallowCompare(null, null)).toBeTruthy();
+    expect(shallowCompare(undefined, undefined)).toBeTruthy();
+    expect(shallowCompare(null, undefined)).toBeTruthy();
+    expect(shallowCompare(0, '')).toBeTruthy();
+    expect(shallowCompare(null, '')).toBeTruthy();
+    expect(shallowCompare(0, undefined)).toBeTruthy();
+  });
+
+  it('returns false when comparing null or undefined against an object', () => {
+    expect(shallowCompare(null, { a: 1 })).toBeFalsy();
+    expect(shallowCompare(undefined, { a: 1 })).toBeFalsy();
+  });
+});
 
 describe('assign', () => {
   it('can copy an object', () => {
