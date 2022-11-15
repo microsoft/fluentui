3507eb7c91a1265b5b57213f25a58768c48f6a9d
Esteban Munoz <esteban.230@hotmail.com>
2022-11-15 14:50:12 -0700

    adding dependency

--- apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
+++ apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
@@ -7,15 +7,24 @@ const infoButtonContent = 'This is the content of an InfoButton.';
 
 storiesOf('InfoButton', module)
   .addDecorator(story => (
-    <Screener steps={new Screener.Steps().click('.show-content').snapshot('infoButtonOpen').end()}>{story()}</Screener>
+    <Screener
+      steps={new Screener.Steps()
+        .click('.show-content-small')
+        .click('.show-content-medium')
+        .click('.show-content-large')
+        .snapshot('infoButtonOpen')
+        .end()}
+    >
+      {story()}
+    </Screener>
   ))
   .addStory(
     'size',
     () => (
       <div style={{ display: 'flex', flexDirection: 'column', padding: '25px', gap: '80px', alignItems: 'start' }}>
-        <InfoButton className="show-content" size="small" content={infoButtonContent} />
-        <InfoButton className="show-content" size="medium" content={infoButtonContent} />
-        <InfoButton className="show-content" size="large" content={infoButtonContent} />
+        <InfoButton id="show-content-small" size="small" content={infoButtonContent} />
+        <InfoButton id="show-content-medium" size="medium" content={infoButtonContent} />
+        <InfoButton id="show-content-large" size="large" content={infoButtonContent} />
       </div>
     ),
     {
