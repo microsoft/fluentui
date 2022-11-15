6c4619be9c454698bb38657b5d9aa8a29d90796d
Sean Monahan <seanmonahan@microsoft.com>
2022-11-15 13:16:26 -0800

    groupedlistv2: make some props optional

--- packages/react/src/components/GroupedList/GroupedListV2.types.ts
+++ packages/react/src/components/GroupedList/GroupedListV2.types.ts
@@ -5,14 +5,14 @@ import type { IGroup } from './GroupedList.types';
 
 export interface IGroupedListV2Props extends IGroupedListProps {
   /** Ref to the underlying List controle */
-  listRef: React.Ref<List>;
+  listRef?: React.Ref<List>;
 
   /**
    * For perf reasons, GroupedList avoids re-rendering unless certain props have changed.
    * Use this prop if you need to force it to re-render in other cases. You can pass any type of
    * value as long as it only changes (`===` comparison) when a re-render should happen.
    */
-  version: {};
+  version?: {};
 
   /**
    * For perf reasons, GrouopedList avoids re-rendering unless certain props have changed.
@@ -20,7 +20,7 @@ export interface IGroupedListV2Props extends IGroupedListProps {
    * You can pass any type of value as long as it only changes (`===` comparison)
    * when a re-render should happen.
    */
-  groupExpandedVersion: {};
+  groupExpandedVersion?: {};
 
   /** Rendering callback to render the group items. */
   onRenderCell: (nestingDepth?: number, item?: any, index?: number, group?: IGroup) => React.ReactNode;
