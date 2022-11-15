00ff9b596d711d565bbc91f567f5f2e960c85ffb
Petr Duda <petrduda@microsoft.com>
2022-11-15 12:53:49 +0100

    types

--- packages/react-components/react-tree/src/index.ts
+++ packages/react-components/react-tree/src/index.ts
@@ -1,3 +1,8 @@
 export { Tree, treeClassNames } from './Tree';
+export type { TreeProps, TreeState, TreeSlots } from './Tree';
+
 export { TreeBranch, treeBranchClassNames } from './TreeBranch';
+export type { TreeBranchProps, TreeBranchState, TreeBranchSlots } from './TreeBranch';
+
 export { TreeLeaf, treeLeafClassNames } from './TreeLeaf';
+export type { TreeLeafProps, TreeLeafState, TreeLeafSlots } from './TreeLeaf';
