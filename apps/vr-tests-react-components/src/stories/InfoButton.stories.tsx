514232208429700034ee0795980795e4dc9c35cb
Esteban Munoz <esteban.230@hotmail.com>
2022-11-15 15:19:56 -0700

    splitting open vs closed size stories

--- apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
+++ apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react';
-import Screener from 'screener-storybook/src/screener';
 import { InfoButton } from '@fluentui/react-infobutton';
 import { storiesOf } from '@storybook/react';
+import { PopoverProps } from '@fluentui/react-popover';
 
 const infoButtonContent = 'This is the content of an InfoButton.';
 
 storiesOf('InfoButton', module)
-  .addDecorator(story => (
-    <Screener
-      steps={new Screener.Steps()
-        .click('.show-content-small')
-        .click('.show-content-medium')
-        .click('.show-content-large')
-        .snapshot('infoButtonOpen')
-        .end()}
-    >
-      {story()}
-    </Screener>
-  ))
   .addStory(
     'size',
     () => (
@@ -25,6 +13,35 @@ storiesOf('InfoButton', module)
         <InfoButton id="show-content-small" size="small" content={infoButtonContent} />
         <InfoButton id="show-content-medium" size="medium" content={infoButtonContent} />
         <InfoButton id="show-content-large" size="large" content={infoButtonContent} />
+      </div>
+    ),
+    {
+      includeHighContrast: true,
+      includeDarkMode: true,
+    },
+  )
+  .addStory(
+    'size-open',
+    () => (
+      <div style={{ display: 'flex', flexDirection: 'column', padding: '25px', gap: '80px', alignItems: 'start' }}>
+        <InfoButton
+          id="show-content-small"
+          size="small"
+          content={{ children: infoButtonContent }}
+          popover={{ open: true } as PopoverProps}
+        />
+        <InfoButton
+          id="show-content-medium"
+          size="medium"
+          content={{ children: infoButtonContent }}
+          popover={{ open: true } as PopoverProps}
+        />
+        <InfoButton
+          id="show-content-large"
+          size="large"
+          content={{ children: infoButtonContent }}
+          popover={{ open: true } as PopoverProps}
+        />
       </div>
     ),
     {
