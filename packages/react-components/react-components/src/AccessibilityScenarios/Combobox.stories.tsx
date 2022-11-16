06235884c117bf1f240184bfc3b1453d4526a5d8
Adam Samec <adam.samec@gmail.com>
2022-11-16 12:40:38 +0100

    Complete the Combobox accessibility scenario

--- packages/react-components/react-components/src/AccessibilityScenarios/Combobox.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityScenarios/Combobox.stories.tsx
@@ -13,8 +13,8 @@ export const FavoritesComboboxes: React.FunctionComponent = () => {
     <Scenario pageTitle="Comboboxes in questionnaire about your favorites">
       <h1>Questionnaire about your favorites</h1>
       <form>
-        <Label htmlFor="color">Most favorite color</Label>
-        <Combobox id="color">
+        <Label htmlFor="colors">Most favorite color</Label>
+        <Combobox id="colors">
           <Option>White</Option>
           <Option>Yellow</Option>
           <Option>Orange</Option>
@@ -33,22 +33,50 @@ export const FavoritesComboboxes: React.FunctionComponent = () => {
           label="I am a vegetarian"
         />
 
-        <Label htmlFor="food">Which food do you like?</Label>
-        <Combobox id="food" multiselect placeholder="You can select multiple items">
-          <OptionGroup label="Fruit and vegetables" />
+        <Label htmlFor="ingredients">Which food ingredients do you like?</Label>
+        <Combobox
+        id="ingredients"
+        multiselect
+        placeholder="You can select multiple ingredients"
+        >
+          <OptionGroup label="Fruit and vegetables">
           <Option>Apple</Option>
           <Option>Orange</Option>
           <Option>Banana</Option>
           <Option>Carrot</Option>
           <Option>Cucumber</Option>
           <Option>Tomato</Option>
-          <OptionGroup label="Meat" />
+          </OptionGroup>
+
+          <OptionGroup label="Meat">
           <Option disabled={isVegetarianSelected}>Beef</Option>
           <Option disabled={isVegetarianSelected}>Poultry</Option>
           <Option disabled={isVegetarianSelected}>Pork</Option>
-          <OptionGroup label="Legume" />
+</OptionGroup>
+
+          <OptionGroup label="Legume">
           <Option>Beans</Option>
+          <Option>Peas</Option>
           <Option>Lentils</Option>
+          </OptionGroup>
+        </Combobox>
+
+<Label htmlFor="animals">Which animals would you like to pet?</Label>
+        <Combobox
+        id="animals"
+        multiselect
+        freeform
+        placeholder="You can select multiple animals or type your own ones"
+        >
+          <Option>Cats</Option>
+          <Option>Dog</Option>
+          <Option>Fish</Option>
+          <Option>Hamster</Option>
+          <Option>Lion</Option>
+          <Option>Mouse</Option>
+          <Option>Parrot</Option>
+          <Option>Snake</Option>
+          <Option>Turtle</Option>
         </Combobox>
 
         <Button type="submit">Submit</Button>
