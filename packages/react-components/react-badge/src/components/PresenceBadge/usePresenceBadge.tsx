a3f3a260a29dd6c8e268bce328940586d9e17c91
KHMakoto <humberto_makoto@hotmail.com>
2022-11-16 10:51:13 -0800

    Addressing PR feedback.

--- packages/react-components/react-badge/src/components/PresenceBadge/usePresenceBadge.tsx
+++ packages/react-components/react-badge/src/components/PresenceBadge/usePresenceBadge.tsx
@@ -14,11 +14,7 @@ import {
 import { useBadge_unstable } from '../Badge/index';
 import type { PresenceBadgeProps, PresenceBadgeState } from './PresenceBadge.types';
 
-const iconMap = (
-  status: PresenceBadgeState['status'],
-  outOfOffice: boolean,
-  size: PresenceBadgeState['size'],
-): React.FunctionComponent | null => {
+const iconMap = (status: PresenceBadgeState['status'], outOfOffice: boolean, size: PresenceBadgeState['size']) => {
   switch (status) {
     case 'available':
       return outOfOffice ? presenceAvailableRegular[size] : presenceAvailableFilled[size];
@@ -54,9 +50,7 @@ export const usePresenceBadge_unstable = (
   props: PresenceBadgeProps,
   ref: React.Ref<HTMLElement>,
 ): PresenceBadgeState => {
-  const size = props.size ?? 'medium';
-  const status = props.status ?? 'available';
-  const outOfOffice = props.outOfOffice ?? false;
+  const { size = 'medium', status = 'available', outOfOffice = false } = props;
 
   const statusText = DEFAULT_STRINGS[status];
   const oofText = props.outOfOffice && props.status !== 'out-of-office' ? ` ${DEFAULT_STRINGS['out-of-office']}` : '';
