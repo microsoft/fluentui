1aaea9b440f1477a076a4f3c75f22a43ecbea6cf
Esteban Munoz <esteban.230@hotmail.com>
2022-11-16 15:27:56 -0700

    adding more stories

--- apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
+++ apps/vr-tests-react-components/src/stories/InfoButton.stories.tsx
@@ -1,18 +1,52 @@
 import * as React from 'react';
+import Screener from 'screener-storybook/src/screener';
 import { InfoButton } from '@fluentui/react-infobutton';
 import { storiesOf } from '@storybook/react';
 import { PopoverProps } from '@fluentui/react-popover';
-
-const infoButtonContent = 'This is the content of an InfoButton.';
+import { TestWrapperDecoratorFixedWidth } from '../utilities/TestWrapperDecorator';
 
 storiesOf('InfoButton', module)
+  .addDecorator(TestWrapperDecoratorFixedWidth)
+  .addDecorator(story => (
+    <Screener
+      steps={new Screener.Steps()
+        .snapshot('rest', { cropTo: '.testWrapper' })
+        .hover('button')
+        .snapshot('hover', { cropTo: '.testWrapper' })
+        .focus('button')
+        .snapshot('focus', { cropTo: '.testWrapper' })
+        .mouseDown('button')
+        .snapshot('active', { cropTo: '.testWrapper' })
+        .end()}
+    >
+      {story()}
+    </Screener>
+  ))
+  .addStory(
+    'size',
+    () => (
+      <div style={{ padding: '60px' }}>
+        <InfoButton content="This is the content of an InfoButton." />
+      </div>
+    ),
+    {
+      includeHighContrast: true,
+      includeDarkMode: true,
+    },
+  );
+
+storiesOf('InfoButton - sizes', module)
+  .addDecorator(TestWrapperDecoratorFixedWidth)
+  .addDecorator(story => (
+    <Screener steps={new Screener.Steps().snapshot('active', { cropTo: '.testWrapper' }).end()}>{story()}</Screener>
+  ))
   .addStory(
     'size',
     () => (
       <div style={{ display: 'flex', flexDirection: 'column', padding: '60px', gap: '80px', alignItems: 'start' }}>
-        <InfoButton id="show-content-small" size="small" content={infoButtonContent} />
-        <InfoButton id="show-content-medium" size="medium" content={infoButtonContent} />
-        <InfoButton id="show-content-large" size="large" content={infoButtonContent} />
+        <InfoButton size="small" content="This is the content of an InfoButton." />
+        <InfoButton size="medium" content="This is the content of an InfoButton." />
+        <InfoButton size="large" content="This is the content of an InfoButton." />
       </div>
     ),
     {
@@ -25,21 +59,18 @@ storiesOf('InfoButton', module)
     () => (
       <div style={{ display: 'flex', flexDirection: 'column', padding: '60px', gap: '80px', alignItems: 'start' }}>
         <InfoButton
-          id="show-content-small"
           size="small"
-          content={{ children: infoButtonContent }}
+          content="This is the content of an InfoButton."
           popover={{ open: true } as PopoverProps}
         />
         <InfoButton
-          id="show-content-medium"
           size="medium"
-          content={{ children: infoButtonContent }}
+          content="This is the content of an InfoButton."
           popover={{ open: true } as PopoverProps}
         />
         <InfoButton
-          id="show-content-large"
           size="large"
-          content={{ children: infoButtonContent }}
+          content="This is the content of an InfoButton."
           popover={{ open: true } as PopoverProps}
         />
       </div>
