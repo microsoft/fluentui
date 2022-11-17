df844929f8c3ccb71ce23be404d6b2326b41509b
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-11-17 09:35:19 -0800

    fix (Alert): remove unneeded StoryWright steps

--- packages/fluentui/docs/src/vr-tests/Alert/AlertExampleDanger.stories.tsx
+++ packages/fluentui/docs/src/vr-tests/Alert/AlertExampleDanger.stories.tsx
@@ -1,20 +1,12 @@
-import * as React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { StoryWright } from 'storywright';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta } from '@storybook/react';
 import { Alert } from '@fluentui/react-northstar';
-import { getFocusStoryWrightSteps, getHoverStoryWrightSteps } from './commonStoryWrightSteps';
 import { getThemeStoryVariant } from '../utilities/getThemeStoryVariant';
 import AlertExampleDanger from '../../examples/components/Alert/Variations/AlertExampleDanger.shorthand';
 
 export default {
   component: Alert,
   title: 'Alert',
-  decorators: [
-    story => <StoryWright steps={getFocusStoryWrightSteps}>{story()}</StoryWright>,
-    story => <StoryWright steps={getHoverStoryWrightSteps}>{story()}</StoryWright>,
-  ],
 } as ComponentMeta<typeof Alert>;
 
 const AlertExampleDangerTeams = getThemeStoryVariant(AlertExampleDanger, 'teamsV2');
--- packages/fluentui/docs/src/vr-tests/Alert/AlertExampleInfo.stories.tsx
+++ packages/fluentui/docs/src/vr-tests/Alert/AlertExampleInfo.stories.tsx
@@ -1,20 +1,12 @@
-import * as React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { StoryWright } from 'storywright';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta } from '@storybook/react';
 import { Alert } from '@fluentui/react-northstar';
-import { getFocusStoryWrightSteps, getHoverStoryWrightSteps } from './commonStoryWrightSteps';
 import { getThemeStoryVariant } from '../utilities/getThemeStoryVariant';
 import AlertExampleInfo from '../../examples/components/Alert/Variations/AlertExampleInfo.shorthand';
 
 export default {
   component: Alert,
   title: 'Alert',
-  decorators: [
-    story => <StoryWright steps={getFocusStoryWrightSteps}>{story()}</StoryWright>,
-    story => <StoryWright steps={getHoverStoryWrightSteps}>{story()}</StoryWright>,
-  ],
 } as ComponentMeta<typeof Alert>;
 
 const AlertExampleInfoTeams = getThemeStoryVariant(AlertExampleInfo, 'teamsV2');
