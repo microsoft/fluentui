99a0f3e17c2b685abfeda1d04af31a28b2bc9a74
Sarah Higley <sarah.higley@microsoft.com>
2022-11-18 15:20:51 -0800

    use more varied option names in free input example

--- packages/react-examples/src/react/ComboBox/ComboBox.FreeInput.Example.tsx
+++ packages/react-examples/src/react/ComboBox/ComboBox.FreeInput.Example.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import { ComboBox, SelectableOptionMenuItemType } from '@fluentui/react';
+import { ComboBox } from '@fluentui/react';
 import type { IComboBoxOption, IComboBoxStyles } from '@fluentui/react';
 
 const options: IComboBoxOption[] = [
-  { key: 'Header1', text: 'First heading', itemType: SelectableOptionMenuItemType.Header },
-  { key: 'A', text: 'Option A' },
-  { key: 'B', text: 'Option B' },
-  { key: 'C', text: 'Option C' },
-  { key: 'D', text: 'Option D' },
-  { key: 'divider', text: '-', itemType: SelectableOptionMenuItemType.Divider },
-  { key: 'Header2', text: 'Second heading', itemType: SelectableOptionMenuItemType.Header },
-  { key: 'E', text: 'Option E' },
-  { key: 'F', text: 'Option F', disabled: true },
-  { key: 'G', text: 'Option G' },
-  { key: 'H', text: 'Option H' },
-  { key: 'I', text: 'Option I' },
-  { key: 'J', text: 'Option J' },
+  { key: 'black', text: 'Black' },
+  { key: 'blue', text: 'Blue' },
+  { key: 'brown', text: 'Brown' },
+  { key: 'cyan', text: 'Cyan' },
+  { key: 'green', text: 'Green' },
+  { key: 'magenta', text: 'Magenta', disabled: true },
+  { key: 'mauve', text: 'Mauve' },
+  { key: 'orange', text: 'Orange' },
+  { key: 'pink', text: 'Pink' },
+  { key: 'purple', text: 'Purple' },
+  { key: 'red', text: 'Red' },
+  { key: 'rose', text: 'Rose' },
+  { key: 'violet', text: 'Violet' },
+  { key: 'white', text: 'White' },
+  { key: 'yellow', text: 'Yellow' },
 ];
 // Optional styling to make the example look nicer
 const comboBoxStyles: Partial<IComboBoxStyles> = { root: { maxWidth: 300 } };
