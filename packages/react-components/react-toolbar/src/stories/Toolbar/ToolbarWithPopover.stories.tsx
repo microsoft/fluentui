3bffa8e4d154b546f8d1e968ecb34a236299d9f8
chajun <86579954+chpalac@users.noreply.github.com>
2022-11-29 13:42:31 -0300

    chore: add few improvements to toolbar stories (#25635)
    * chore: add few improvements to toolbar stories
    
    * chore: remove snooze from examples
    
    * chore: add label for more meu trigger
    
    * chore: add few examples
    
    * chore: change approach on the popover example
    
    * chore: add new round of feedback
    
    * chore: update toolbar popover example
    
    * chore: update toolbar popover example
    
    * chore: fix controlled radio example
    
    * chore: update controlled radio example
    
    * chore: update with tooltip example

--- packages/react-components/react-toolbar/src/stories/Toolbar/ToolbarWithPopover.stories.tsx
+++ packages/react-components/react-toolbar/src/stories/Toolbar/ToolbarWithPopover.stories.tsx
@@ -2,42 +2,82 @@ import * as React from 'react';
 import { Toolbar, ToolbarButton, ToolbarDivider } from '@fluentui/react-toolbar';
 import type { ToolbarProps } from '@fluentui/react-toolbar';
 import { Popover, PopoverSurface, PopoverTrigger, Button } from '@fluentui/react-components';
-import { CalendarMonthRegular, Accessibility24Filled } from '@fluentui/react-icons';
+import { MathFormatLinear24Regular, Image24Regular, Table24Filled } from '@fluentui/react-icons';
 
-export const WithPopover = (props: Partial<ToolbarProps>) => (
-  <Toolbar {...props} size="small">
-    <Popover withArrow>
-      <PopoverTrigger disableButtonEnhancement>
-        <ToolbarButton appearance="primary">See more...</ToolbarButton>
-      </PopoverTrigger>
-      <PopoverSurface>
-        <div>
-          <h3>Popover content</h3>
-          <Button>Action</Button>
-        </div>
-      </PopoverSurface>
-    </Popover>
-    <ToolbarDivider />
-    <Popover withArrow>
-      <PopoverTrigger disableButtonEnhancement>
-        <ToolbarButton aria-label="Popover trigger - Calendar" icon={<CalendarMonthRegular />} />
-      </PopoverTrigger>
-      <PopoverSurface>
-        <div>
-          <h3>Popover content</h3>
-          <Button>Action</Button>
-        </div>
-      </PopoverSurface>
-    </Popover>
-    <Popover withArrow>
-      <PopoverTrigger disableButtonEnhancement>
-        <ToolbarButton aria-label="Popover trigger - Accessibility" icon={<Accessibility24Filled />} />
-      </PopoverTrigger>
-      <PopoverSurface>
-        <div>
-          <h3>Popover content - Accessibility</h3> <Button>Action</Button>
-        </div>
-      </PopoverSurface>
-    </Popover>
-  </Toolbar>
-);
+export const WithPopover = (props: Partial<ToolbarProps>) => {
+  const [open, setOpen] = React.useState<'first' | 'second' | 'third' | 'fourth' | undefined>();
+
+  return (
+    <Toolbar {...props} size="small">
+      <Popover
+        withArrow
+        trapFocus
+        open={open === 'first'}
+        onOpenChange={(_, data) => setOpen(data.open ? 'first' : undefined)}
+      >
+        <PopoverTrigger disableButtonEnhancement>
+          <ToolbarButton aria-label="Insert image" icon={<Image24Regular />} />
+        </PopoverTrigger>
+        <PopoverSurface>
+          <div>
+            <h3>Insert Image</h3>
+            <Button>Example 1</Button>
+            <Button onClick={() => setOpen(undefined)}>Close</Button>
+          </div>
+        </PopoverSurface>
+      </Popover>
+      <Popover
+        withArrow
+        trapFocus
+        open={open === 'second'}
+        onOpenChange={(_, data) => setOpen(data.open ? 'second' : undefined)}
+      >
+        <PopoverTrigger disableButtonEnhancement>
+          <ToolbarButton appearance="primary" icon={<Table24Filled />} aria-label="Insert Table" />
+        </PopoverTrigger>
+        <PopoverSurface>
+          <div>
+            <h3>Insert Table</h3>
+            <Button>Example 2</Button>
+            <Button onClick={() => setOpen(undefined)}>Close</Button>
+          </div>
+        </PopoverSurface>
+      </Popover>
+      <Popover
+        withArrow
+        trapFocus
+        open={open === 'third'}
+        onOpenChange={(_, data) => setOpen(data.open ? 'third' : undefined)}
+      >
+        <PopoverTrigger disableButtonEnhancement>
+          <ToolbarButton aria-label="Insert Formula" icon={<MathFormatLinear24Regular />} />
+        </PopoverTrigger>
+        <PopoverSurface>
+          <div>
+            <h3>Insert Formula</h3>
+            <Button>Example 3</Button>
+            <Button onClick={() => setOpen(undefined)}>Close</Button>
+          </div>
+        </PopoverSurface>
+      </Popover>
+      <ToolbarDivider />
+      <Popover
+        withArrow
+        trapFocus
+        open={open === 'fourth'}
+        onOpenChange={(_, data) => setOpen(data.open ? 'fourth' : undefined)}
+      >
+        <PopoverTrigger disableButtonEnhancement>
+          <ToolbarButton>Quick Actions</ToolbarButton>
+        </PopoverTrigger>
+        <PopoverSurface>
+          <div>
+            <h3>Quick Actions</h3>
+            <Button>Example 4</Button>
+            <Button onClick={() => setOpen(undefined)}>Close</Button>
+          </div>
+        </PopoverSurface>
+      </Popover>
+    </Toolbar>
+  );
+};
