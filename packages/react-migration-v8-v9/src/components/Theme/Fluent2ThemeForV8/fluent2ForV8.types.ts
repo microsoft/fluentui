6323b20aba37624375f6c8f00225daad846b071c
Mason Tejera <matejera@microsoft.com>
2022-11-30 14:53:38 -0800

    lintar

--- packages/react-migration-v8-v9/src/components/Theme/Fluent2ThemeForV8/fluent2ForV8.types.ts
+++ packages/react-migration-v8-v9/src/components/Theme/Fluent2ThemeForV8/fluent2ForV8.types.ts
@@ -1,5 +1,6 @@
 import { IEffects } from '@fluentui/react';
 
+/* eslint-disable @typescript-eslint/naming-convention */
 export interface IExtendedEffects extends IEffects {
   roundedCorner8?: string;
   roundedCornerCircle?: string;
