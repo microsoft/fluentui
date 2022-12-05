b9a98280e03919c11b341fb27e9e243422a90cdc
Lingfan Gao <lingfangao@hotmail.com>
2022-12-05 16:52:04 +0100

    remove virtualizer

--- packages/react-components/virtualizer/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Virtualizer } from './Virtualizer';
-export { useVirtualizer_unstable } from './useVirtualizer';
-export { renderVirtualizer_unstable } from './renderVirtualizer';
-export { useVirtualizerStyles_unstable } from './useVirtualizerStyles';
-export type { VirtualizerProps, VirtualizerState, VirtualizerSlots } from './Virtualizer.types';
