a6566b9ccd30d99faa7615d2da38a419e19ca60e
Marcos Moura <marcosvmmoura@gmail.com>
2022-12-05 15:04:53 +0100

    docs: add example of selectable card without header

--- packages/react-components/react-card/stories/Card/CardSelectableIndicator.stories.tsx
+++ packages/react-components/react-card/stories/Card/CardSelectableIndicator.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { makeStyles, shorthands, Button, Caption1, tokens, Checkbox, Text } from '@fluentui/react-components';
 import { MoreHorizontal20Filled } from '@fluentui/react-icons';
-import { Card, CardHeader, CardPreview, CardProps } from '@fluentui/react-card';
+import { Card, CardHeader, CardPreview } from '@fluentui/react-card';
 
 const resolveAsset = (asset: string) => {
   const ASSET_URL =
@@ -10,10 +10,19 @@ const resolveAsset = (asset: string) => {
   return `${ASSET_URL}${asset}`;
 };
 
+const flex = {
+  ...shorthands.gap('16px'),
+  display: 'flex',
+};
+
 const useStyles = makeStyles({
   main: {
-    ...shorthands.gap('16px'),
-    display: 'flex',
+    ...flex,
+    flexDirection: 'column',
+  },
+
+  row: {
+    ...flex,
     flexWrap: 'wrap',
   },
 
@@ -41,50 +50,96 @@ const useStyles = makeStyles({
     backgroundColor: '#FFF',
     boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.14), 0px 0px 2px rgba(0, 0, 0, 0.12)',
   },
+
+  actions: {
+    display: 'flex',
+  },
 });
-
-const CardExample = (props: CardProps) => {
-  const styles = useStyles();
-
-  return (
-    <Card className={styles.card} {...props}>
-      <CardPreview
-        className={styles.grayBackground}
-        logo={<img className={styles.logoBadge} alt="app logo" src={resolveAsset('logo3.svg')} />}
-      >
-        <img alt="Presentation Preview" src={resolveAsset('office1.png')} className={styles.smallRadius} />
-      </CardPreview>
-
-      <CardHeader
-        header={<Text weight="semibold">iOS App Prototype</Text>}
-        description={<Caption1 className={styles.caption}>You created 53m ago</Caption1>}
-        action={<Button appearance="transparent" icon={<MoreHorizontal20Filled />} aria-label="More actions" />}
-      />
-    </Card>
-  );
-};
 
 export const SelectableIndicator = () => {
   const styles = useStyles();
 
   const [selected1, setSelected1] = React.useState(false);
   const [selected2, setSelected2] = React.useState(false);
+  const [selected3, setSelected3] = React.useState(false);
+  const [selected4, setSelected4] = React.useState(false);
 
   const onFirstCardSelected = React.useCallback((_, { selected }) => setSelected1(selected), [setSelected1]);
   const onSecondCardSelected = React.useCallback((_, { selected }) => setSelected2(selected), [setSelected2]);
+  const onThirdCardSelected = React.useCallback((_, { selected }) => setSelected3(selected), [setSelected3]);
+  const onForthCardSelected = React.useCallback((_, { selected }) => setSelected4(selected), [setSelected4]);
 
   return (
     <div className={styles.main}>
-      <CardExample
-        floatingAction={<Checkbox checked={selected1} />}
-        selected={selected1}
-        onSelectionChange={onFirstCardSelected}
-      />
-      <CardExample
-        floatingAction={<Checkbox checked={selected2} />}
-        selected={selected2}
-        onSelectionChange={onSecondCardSelected}
-      />
+      <div className={styles.row}>
+        <Card
+          className={styles.card}
+          floatingAction={<Checkbox checked={selected1} />}
+          selected={selected1}
+          onSelectionChange={onFirstCardSelected}
+        >
+          <CardPreview
+            className={styles.grayBackground}
+            logo={<img className={styles.logoBadge} alt="app logo" src={resolveAsset('logo3.svg')} />}
+          >
+            <img alt="Presentation Preview" src={resolveAsset('office1.png')} className={styles.smallRadius} />
+          </CardPreview>
+
+          <CardHeader
+            header={<Text weight="semibold">iOS App Prototype</Text>}
+            description={<Caption1 className={styles.caption}>You created 53m ago</Caption1>}
+            action={<Button appearance="transparent" icon={<MoreHorizontal20Filled />} aria-label="More actions" />}
+          />
+        </Card>
+
+        <Card
+          className={styles.card}
+          floatingAction={<Checkbox checked={selected2} />}
+          selected={selected2}
+          onSelectionChange={onSecondCardSelected}
+        >
+          <CardPreview
+            className={styles.grayBackground}
+            logo={<img className={styles.logoBadge} alt="app logo" src={resolveAsset('logo3.svg')} />}
+          >
+            <img alt="Presentation Preview" src={resolveAsset('office1.png')} className={styles.smallRadius} />
+          </CardPreview>
+
+          <CardHeader
+            header={<Text weight="semibold">iOS App Prototype</Text>}
+            description={<Caption1 className={styles.caption}>You created 53m ago</Caption1>}
+            action={<Button appearance="transparent" icon={<MoreHorizontal20Filled />} aria-label="More actions" />}
+          />
+        </Card>
+      </div>
+
+      <div className={styles.row}>
+        <Card className={styles.card} selected={selected3} onSelectionChange={onThirdCardSelected}>
+          <CardHeader
+            header={<Text weight="semibold">iOS App Prototype</Text>}
+            description={<Caption1 className={styles.caption}>You created 53m ago</Caption1>}
+            action={
+              <div className={styles.actions}>
+                <Button appearance="transparent" icon={<MoreHorizontal20Filled />} aria-label="More actions" />
+                <Checkbox checked={selected3} />
+              </div>
+            }
+          />
+        </Card>
+
+        <Card className={styles.card} selected={selected4} onSelectionChange={onForthCardSelected}>
+          <CardHeader
+            header={<Text weight="semibold">iOS App Prototype</Text>}
+            description={<Caption1 className={styles.caption}>You created 53m ago</Caption1>}
+            action={
+              <div className={styles.actions}>
+                <Button appearance="transparent" icon={<MoreHorizontal20Filled />} aria-label="More actions" />
+                <Checkbox checked={selected4} />
+              </div>
+            }
+          />
+        </Card>
+      </div>
     </div>
   );
 };
