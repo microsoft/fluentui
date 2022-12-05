58ea8dbaa1977370fca092f86b215aa5df79d100
Adam Samec <adam.samec@gmail.com>
2022-12-05 16:11:48 +0100

    Correct placeholders, add aria-describedby and add form submission for Combobox and Dropdown scenarios

--- packages/react-components/react-components/src/AccessibilityScenarios/Combobox.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityScenarios/Combobox.stories.tsx
@@ -8,79 +8,90 @@ import { Scenario } from './utils';
 
 export const FavoritesComboboxes: React.FunctionComponent = () => {
   const [isVegetarianSelected, setIsVegetarianSelected] = React.useState(false);
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isSubmitted) {
+      document.getElementById('formSubmittedText')?.focus();
+    }
+  }, [isSubmitted]);
+
+  const onSubmit = (event: React.BaseSyntheticEvent) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
 
   return (
     <Scenario pageTitle="Comboboxes in questionnaire about your favorites">
       <h1>Questionnaire about your favorites</h1>
-      <form>
-        <Label htmlFor="colors">Most favorite color</Label>
-        <Combobox id="colors">
-          <Option>White</Option>
-          <Option>Yellow</Option>
-          <Option>Orange</Option>
-          <Option>Pink</Option>
-          <Option>Violet</Option>
-          <Option>Red</Option>
-          <Option>Blue</Option>
-          <Option>Green</Option>
-          <Option>Brown</Option>
-          <Option>Black</Option>
-        </Combobox>
+      {!isSubmitted ? (
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="colors">The most favorite color</Label>
+          <Combobox id="colors" required>
+            <Option>White</Option>
+            <Option>Yellow</Option>
+            <Option>Orange</Option>
+            <Option>Pink</Option>
+            <Option>Violet</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Green</Option>
+            <Option>Brown</Option>
+            <Option>Black</Option>
+          </Combobox>
 
-        <Checkbox
-          checked={isVegetarianSelected}
-          onChange={() => setIsVegetarianSelected(checked => !checked)}
-          label="I am a vegetarian"
-        />
+          <Checkbox
+            checked={isVegetarianSelected}
+            onChange={() => setIsVegetarianSelected(checked => !checked)}
+            label="I am a vegetarian"
+          />
 
-        <Label htmlFor="ingredients">Which food ingredients do you like?</Label>
-        <Combobox
-        id="ingredients"
-        multiselect
-        placeholder="You can select multiple ingredients"
-        >
-          <OptionGroup label="Fruit and vegetables">
-          <Option>Apple</Option>
-          <Option>Orange</Option>
-          <Option>Banana</Option>
-          <Option>Carrot</Option>
-          <Option>Cucumber</Option>
-          <Option>Tomato</Option>
-          </OptionGroup>
+          <Label htmlFor="ingredients">Which food ingredients do you like?</Label>
+          <Combobox id="ingredients" multiselect placeholder="Please choose wisely" aria-describedby="ingredientsHint">
+            <OptionGroup label="Fruit and vegetables">
+              <Option>Apple</Option>
+              <Option>Orange</Option>
+              <Option>Banana</Option>
+              <Option>Carrot</Option>
+              <Option>Cucumber</Option>
+              <Option>Tomato</Option>
+            </OptionGroup>
 
-          <OptionGroup label="Meat">
-          <Option disabled={isVegetarianSelected}>Beef</Option>
-          <Option disabled={isVegetarianSelected}>Poultry</Option>
-          <Option disabled={isVegetarianSelected}>Pork</Option>
-</OptionGroup>
+            <OptionGroup label="Meat">
+              <Option disabled={isVegetarianSelected}>Beef</Option>
+              <Option disabled={isVegetarianSelected}>Poultry</Option>
+              <Option disabled={isVegetarianSelected}>Pork</Option>
+            </OptionGroup>
 
-          <OptionGroup label="Legume">
-          <Option>Beans</Option>
-          <Option>Peas</Option>
-          <Option>Lentils</Option>
-          </OptionGroup>
-        </Combobox>
+            <OptionGroup label="Legume">
+              <Option>Beans</Option>
+              <Option>Peas</Option>
+              <Option>Lentils</Option>
+            </OptionGroup>
+          </Combobox>
+          <p id="ingredientsHint">You can select more than one ingredient.</p>
 
-<Label htmlFor="animals">Which animals would you like to pet?</Label>
-        <Combobox
-        id="animals"
-        multiselect
-        freeform
-        placeholder="You can select multiple animals or type your own ones"
-        >
-          <Option>Cats</Option>
-          <Option>Dog</Option>
-          <Option>Fish</Option>
-          <Option>Hamster</Option>
-          <Option>Lion</Option>
-          <Option>Mouse</Option>
-          <Option>Parrot</Option>
-          <Option>Snake</Option>
-          <Option>Turtle</Option>
-        </Combobox>
+          <Label htmlFor="animals">Which animals do you like to pet the most?</Label>
+          <Combobox id="animals" freeform placeholder="Please choose wisely" aria-describedby="animalsHint">
+            <Option>Cats</Option>
+            <Option>Dog</Option>
+            <Option>Fish</Option>
+            <Option>Hamster</Option>
+            <Option>Lion</Option>
+            <Option>Mouse</Option>
+            <Option>Parrot</Option>
+            <Option>Snake</Option>
+            <Option>Turtle</Option>
+          </Combobox>
+          <p id="animalsHint">Please select one of the options or type your own animal.</p>
 
-        <Button type="submit">Submit</Button>
-      </form>
+          <Button type="submit">Submit</Button>
+        </form>
+      ) : (
+        <p id="formSubmittedText" tabIndex={-1}>
+          The form would have been submitted.
+        </p>
+      )}
     </Scenario>
   );
 };
--- packages/react-components/react-components/src/AccessibilityScenarios/Dropdown.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityScenarios/Dropdown.stories.tsx
@@ -8,57 +8,76 @@ import { Scenario } from './utils';
 
 export const FavoritesDropdowns: React.FunctionComponent = () => {
   const [isVegetarianSelected, setIsVegetarianSelected] = React.useState(false);
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isSubmitted) {
+      document.getElementById('formSubmittedText')?.focus();
+    }
+  }, [isSubmitted]);
+
+  const onSubmit = (event: React.BaseSyntheticEvent) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
 
   return (
     <Scenario pageTitle="Dropdowns in questionnaire about your favorites">
       <h1>Questionnaire about your favorites</h1>
-      <form>
-        <Label htmlFor="colors">Most favorite color</Label>
-        <Dropdown id="colors">
-          <Option>White</Option>
-          <Option>Yellow</Option>
-          <Option>Orange</Option>
-          <Option>Pink</Option>
-          <Option>Violet</Option>
-          <Option>Red</Option>
-          <Option>Blue</Option>
-          <Option>Green</Option>
-          <Option>Brown</Option>
-          <Option>Black</Option>
-        </Dropdown>
-
-        <Checkbox
-          checked={isVegetarianSelected}
-          onChange={() => setIsVegetarianSelected(checked => !checked)}
-          label="I am a vegetarian"
-        />
-
-        <Label htmlFor="ingredients">Which food ingredients do you like?</Label>
-        <Dropdown id="ingredients" multiselect placeholder="You can select multiple ingredients">
-          <OptionGroup label="Fruit and vegetables">
-            <Option>Apple</Option>
+      {!isSubmitted ? (
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="colors">The most favorite color</Label>
+          <Dropdown id="colors">
+            <Option>White</Option>
+            <Option>Yellow</Option>
             <Option>Orange</Option>
-            <Option>Banana</Option>
-            <Option>Carrot</Option>
-            <Option>Cucumber</Option>
-            <Option>Tomato</Option>
-          </OptionGroup>
+            <Option>Pink</Option>
+            <Option>Violet</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Green</Option>
+            <Option>Brown</Option>
+            <Option>Black</Option>
+          </Dropdown>
 
-          <OptionGroup label="Meat">
-            <Option disabled={isVegetarianSelected}>Beef</Option>
-            <Option disabled={isVegetarianSelected}>Poultry</Option>
-            <Option disabled={isVegetarianSelected}>Pork</Option>
-          </OptionGroup>
+          <Checkbox
+            checked={isVegetarianSelected}
+            onChange={() => setIsVegetarianSelected(checked => !checked)}
+            label="I am a vegetarian"
+          />
 
-          <OptionGroup label="Legume">
-            <Option>Beans</Option>
-            <Option>Peas</Option>
-            <Option>Lentils</Option>
-          </OptionGroup>
-        </Dropdown>
+          <Label htmlFor="ingredients">Which food ingredients do you like?</Label>
+          <Dropdown id="ingredients" multiselect placeholder="Please choose wisely" aria-describedby="ingredientsHint">
+            <OptionGroup label="Fruit and vegetables">
+              <Option>Apple</Option>
+              <Option>Orange</Option>
+              <Option>Banana</Option>
+              <Option>Carrot</Option>
+              <Option>Cucumber</Option>
+              <Option>Tomato</Option>
+            </OptionGroup>
 
-        <Button type="submit">Submit</Button>
-      </form>
+            <OptionGroup label="Meat">
+              <Option disabled={isVegetarianSelected}>Beef</Option>
+              <Option disabled={isVegetarianSelected}>Poultry</Option>
+              <Option disabled={isVegetarianSelected}>Pork</Option>
+            </OptionGroup>
+
+            <OptionGroup label="Legume">
+              <Option>Beans</Option>
+              <Option>Peas</Option>
+              <Option>Lentils</Option>
+            </OptionGroup>
+          </Dropdown>
+          <p id="ingredientsHint">You can select more than one ingredient.</p>
+
+          <Button type="submit">Submit</Button>
+        </form>
+      ) : (
+        <p id="formSubmittedText" tabIndex={-1}>
+          The form would have been submitted.
+        </p>
+      )}
     </Scenario>
   );
 };
