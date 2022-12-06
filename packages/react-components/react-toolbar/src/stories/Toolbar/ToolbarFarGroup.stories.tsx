b23abc785a88f05892d840b1a9de463ffff6f51c
Charles <chassunc@microsoft.com>
2022-12-06 12:18:30 -0300

    chore: remove sized icons

--- packages/react-components/react-toolbar/src/stories/Toolbar/ToolbarFarGroup.stories.tsx
+++ packages/react-components/react-toolbar/src/stories/Toolbar/ToolbarFarGroup.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { makeStyles } from '@fluentui/react-components';
-import { FontIncrease24Regular, FontDecrease24Regular, TextFont24Regular } from '@fluentui/react-icons';
+import { FontIncreaseRegular, FontDecreaseRegular, TextFontRegular } from '@fluentui/react-icons';
 import { Toolbar, ToolbarButton, ToolbarDivider, ToolbarGroup } from '@fluentui/react-toolbar';
 import type { ToolbarProps } from '@fluentui/react-toolbar';
 
@@ -15,18 +15,18 @@ export const FarGroup = (props: Partial<ToolbarProps>) => {
   return (
     <Toolbar {...props} className={farGroupStyles.toolbar}>
       <ToolbarGroup role="presentation">
-        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncrease24Regular />} />
-        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecrease24Regular />} />
-        <ToolbarButton aria-label="Reset Font Size" icon={<TextFont24Regular />} />
+        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncreaseRegular />} />
+        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecreaseRegular />} />
+        <ToolbarButton aria-label="Reset Font Size" icon={<TextFontRegular />} />
         <ToolbarDivider />
-        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncrease24Regular />} />
-        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecrease24Regular />} />
-        <ToolbarButton aria-label="Reset Font Size" icon={<TextFont24Regular />} />
+        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncreaseRegular />} />
+        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecreaseRegular />} />
+        <ToolbarButton aria-label="Reset Font Size" icon={<TextFontRegular />} />
       </ToolbarGroup>
       <ToolbarGroup role="presentation">
-        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncrease24Regular />} />
-        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecrease24Regular />} />
-        <ToolbarButton aria-label="Reset Font Size" icon={<TextFont24Regular />} />
+        <ToolbarButton aria-label="Increase Font Size" appearance="primary" icon={<FontIncreaseRegular />} />
+        <ToolbarButton aria-label="Decrease Font Size" icon={<FontDecreaseRegular />} />
+        <ToolbarButton aria-label="Reset Font Size" icon={<TextFontRegular />} />
       </ToolbarGroup>
     </Toolbar>
   );
