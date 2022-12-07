e1502496772bda6e31940ddbc4450157a5f8d09e
Jacqueline Gaherity <30805892+Jacqueline-ms@users.noreply.github.com>
2022-12-07 15:00:32 -0800

    Fixing merge

--- packages/azure-themes/src/azure/Constants.ts
+++ packages/azure-themes/src/azure/Constants.ts
@@ -5,7 +5,7 @@ import {
   getScreenSelector,
 } from 'office-ui-fabric-react/lib/Styling';
 
-export const inputHeight = '18px
+export const inputHeight = '18px';
 export const buttonPadding = '0px 16px';
 export const commandBarHeight = '36px';
 export const borderWidth = '1px';
