2b9e9fa45cd40a4b15a84180b196c8a7ad714818
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-12-07 06:39:58 -0800

    chore: run migration generator

--- packages/react-components/react-toolbar/src/components/ToolbarButton/ToolbarButton.test.tsx
+++ packages/react-components/react-toolbar/src/components/ToolbarButton/ToolbarButton.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { ToolbarButton } from './ToolbarButton';
-import { isConformant } from '../../common/isConformant';
+import { isConformant } from '../../testing/isConformant';
 import { ButtonProps } from '@fluentui/react-button';
 
 describe('ToolbarButton', () => {
