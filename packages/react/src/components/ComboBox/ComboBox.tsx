c9da576a886bfc534d1690e991940087573a0b18
KHMakoto <Humberto.Morimoto@microsoft.com>
2022-12-06 16:15:48 -0800

    fix: Stopping autocomplete in Combobox from suggesting disabled options.

--- packages/react/src/components/ComboBox/ComboBox.tsx
+++ packages/react/src/components/ComboBox/ComboBox.tsx
@@ -833,7 +833,10 @@ class ComboBoxInternal extends React.Component<IComboBoxInternalProps, IComboBox
       const items = currentOptions
         .map((item, index) => ({ ...item, index }))
         .filter(
-          option => isNormalOption(option) && getPreviewText(option).toLocaleLowerCase().indexOf(updatedValue) === 0,
+          option =>
+            isNormalOption(option) &&
+            !option.disabled &&
+            getPreviewText(option).toLocaleLowerCase().indexOf(updatedValue) === 0,
         );
       if (items.length > 0) {
         // use ariaLabel as the value when the option is set
@@ -893,7 +896,10 @@ class ComboBoxInternal extends React.Component<IComboBoxInternalProps, IComboBox
         const items = currentOptions
           .map((item, i) => ({ ...item, index: i }))
 
-          .filter(option => isNormalOption(option) && option.text.toLocaleLowerCase().indexOf(updatedValue) === 0);
+          .filter(
+            option =>
+              isNormalOption(option) && !option.disabled && option.text.toLocaleLowerCase().indexOf(updatedValue) === 0,
+          );
 
         // If we found a match, update the state
         if (items.length > 0) {
