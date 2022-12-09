e05318e29847a69b251cff98a3d29e7038284509
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-12-09 07:27:24 -0800

    fix(Video): wait 1 second before taking snapshot

--- packages/fluentui/docs/src/vr-tests/Video/Video.stories.tsx
+++ packages/fluentui/docs/src/vr-tests/Video/Video.stories.tsx
@@ -1,8 +1,15 @@
+import * as React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta } from '@storybook/react';
 import { Video } from '@fluentui/react-northstar';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { StoryWright, Steps } from 'storywright';
 import VideoExample from '../../examples/components/Video/Types/VideoExample.shorthand';
 
-export default { component: Video, title: 'Video' } as ComponentMeta<typeof Video>;
+export default {
+  component: Video,
+  title: 'Video',
+  decorators: [story => <StoryWright steps={new Steps().wait(3000).snapshot('Video').end()}>{story()}</StoryWright>],
+} as ComponentMeta<typeof Video>;
 
 export { VideoExample };
