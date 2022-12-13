104503657311537adc8b68938d93068a1cd5c685
Sarah Higley <sarah.higley@microsoft.com>
2022-12-13 01:19:45 -0800

    chore: update freeform example to show typed string

--- packages/react-components/react-combobox/src/stories/Combobox/ComboboxFreeform.stories.tsx
+++ packages/react-components/react-combobox/src/stories/Combobox/ComboboxFreeform.stories.tsx
@@ -16,11 +16,45 @@ const useStyles = makeStyles({
 export const Freeform = (props: Partial<ComboboxProps>) => {
   const comboId = useId('combo-default');
   const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
+  const [customSearch, setCustomSearch] = React.useState<string | undefined>();
   const styles = useStyles();
+
+  const onChange: ComboboxProps['onChange'] = event => {
+    const value = event.target.value.trim();
+    const matches = options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) === 0);
+    if (value.length && matches.length < 1) {
+      setCustomSearch(value);
+    } else {
+      setCustomSearch(undefined);
+    }
+  };
+
+  const onOptionSelect: ComboboxProps['onOptionSelect'] = (event, data) => {
+    const matchingOption = data.optionText && options.includes(data.optionText);
+    if (matchingOption) {
+      setCustomSearch(undefined);
+    } else {
+      setCustomSearch(data.optionText);
+    }
+  };
+
   return (
     <div className={styles.root}>
       <label id={comboId}>Best pet</label>
-      <Combobox aria-labelledby={comboId} freeform placeholder="Select an animal" {...props}>
+      <Combobox
+        aria-labelledby={comboId}
+        freeform
+        placeholder="Select an animal"
+        onChange={onChange}
+        onOptionSelect={onOptionSelect}
+        {...props}
+      >
+        {/* We recommend displaying the custom freeform string, if it doesn't match an animal */}
+        {customSearch ? (
+          <Option key="freeform" text={customSearch}>
+            "{customSearch}"
+          </Option>
+        ) : null}
         {options.map(option => (
           <Option key={option} disabled={option === 'Ferret'}>
             {option}
