2317ef147d6d3a455919d8624a4648a3250fa9aa
mituron <mituron@microsoft.com>
2022-12-13 17:38:47 +0100

    adding tests

--- packages/fluentui/accessibility/src/behaviors/Carousel/carouselBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Carousel/carouselBehavior.ts
@@ -10,6 +10,8 @@ import { keyboardKey, SpacebarKey } from '../../keyboard-key';
  * Adds attribute 'aria-label' to 'itemsContainer' slot if 'navigation' property is true. Does not set the attribute otherwise.
  * Adds attribute 'role=region' to 'itemsContainer' slot if 'navigation' property is true.  Set 'role=none' otherwise.
  * Adds attribute 'tabIndex=-1' to 'itemsContainer' slot if 'navigation' property is false. Does not set the attribute otherwise.
+ * Adds attribute 'data-is-visible=false' to 'paddleNext' slot if 'paddleNextHidden' property is true. Does not set the attribute otherwise.
+ * Adds attribute 'data-is-visible=false' to 'paddlePrevious' slot if 'paddlePreviousHidden' property is false. Does not set the attribute otherwise.
  * @specification
  * Adds attribute 'role=region' to 'root' slot.
  * Adds attribute 'aria-live=polite' to 'itemsContainerWrapper' slot if 'ariaLiveOn' property is true. Sets the attribute to 'off' otherwise.
@@ -46,6 +48,7 @@ export const carouselBehavior: Accessibility<CarouselBehaviorProps> = props => (
         'aria-hidden': 'true',
       }),
       ...(props.paddleNextHidden && {
+        // we need the attribute when carousel is inside FZ
         'data-is-visible': 'false',
       }),
     },
@@ -55,6 +58,7 @@ export const carouselBehavior: Accessibility<CarouselBehaviorProps> = props => (
         'aria-hidden': 'true',
       }),
       ...(props.paddlePreviousHidden && {
+        // we need the attribute when carousel is inside FZ
         'data-is-visible': 'false',
       }),
     },
