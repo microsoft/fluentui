db7d2bd7289ea50c31cab1e0cab561d66eae4926
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-12-16 18:10:53 -0500

    chore: remove unneed eslint rule disable comments

--- packages/fluentui/docs/src/vr-tests/Button/Button.stories.tsx
+++ packages/fluentui/docs/src/vr-tests/Button/Button.stories.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta } from '@storybook/react';
 import { Button } from '@fluentui/react-northstar';
 import ButtonDefaultBsize from '../../examples/components/Button/Performance/ButtonDefault.bsize';
