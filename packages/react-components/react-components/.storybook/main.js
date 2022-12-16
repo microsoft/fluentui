aa1efbe284434e089fbecc084096fac233bedbbb
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-12-16 04:59:47 -0500

    fix: restore staticDirs in main.js

--- packages/react-components/react-components/.storybook/main.js
+++ packages/react-components/react-components/.storybook/main.js
@@ -3,6 +3,7 @@ const rootMain = require('../../../../.storybook/main');
 module.exports = /** @type {Omit<import('../../../../.storybook/main'), 'typescript'|'babel'>} */ ({
   ...rootMain,
   stories: [...rootMain.stories, '../stories/**/*.stories.mdx', '../stories/**/index.stories.@(ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [...rootMain.addons],
   webpackFinal: (config, options) => {
     const localConfig = { ...rootMain.webpackFinal(config, options) };
