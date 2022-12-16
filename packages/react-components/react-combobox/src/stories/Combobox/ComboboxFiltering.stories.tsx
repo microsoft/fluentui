89e32644a242da17b50fe8f75c113c4f3320e26d
Sarah Higley <smhigley@users.noreply.github.com>
2022-12-16 14:01:08 -0800

    feat: more Combobox examples (#26023)

---
+++ packages/react-components/react-combobox/src/stories/Combobox/ComboboxFiltering.stories.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { makeStyles, shorthands, useId } from '@fluentui/react-components';
+import { Combobox, ComboboxProps, Option } from '@fluentui/react-combobox';
+
+const useStyles = makeStyles({
+  root: {
+    // Stack the label above the field with a gap
+    display: 'grid',
+    gridTemplateRows: 'repeat(1fr)',
+    justifyItems: 'start',
+    ...shorthands.gap('2px'),
+    maxWidth: '400px',
+  },
+});
+
+export const Filtering = (props: Partial<ComboboxProps>) => {
+  const comboId = useId('combo-default');
+  const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
+  const [matchingOptions, setMatchingOptions] = React.useState([...options]);
+  const styles = useStyles();
+
+  const onChange: ComboboxProps['onChange'] = event => {
+    const value = event.target.value.trim();
+    const matches = options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) === 0);
+    setMatchingOptions(matches);
+  };
+
+  return (
+    <div className={styles.root}>
+      <label id={comboId}>Best pet</label>
+      <Combobox aria-labelledby={comboId} placeholder="Select an animal" onChange={onChange} {...props}>
+        {matchingOptions.map(option => (
+          <Option key={option} disabled={option === 'Ferret'}>
+            {option}
+          </Option>
+        ))}
+      </Combobox>
+    </div>
+  );
+};
+
+Filtering.parameters = {
+  docs: {
+    description: {
+      story: 'Filtering based on the user-typed string can be achieved by modifying the child Options directly.',
+    },
+  },
+};
--- packages/react-components/react-combobox/src/stories/Combobox/ComboboxFreeform.stories.tsx
+++ packages/react-components/react-combobox/src/stories/Combobox/ComboboxFreeform.stories.tsx
@@ -15,16 +15,68 @@ const useStyles = makeStyles({
 
 export const Freeform = (props: Partial<ComboboxProps>) => {
   const comboId = useId('combo-default');
-  const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
+  const options = [
+    'Cat',
+    'Caterpillar',
+    'Catfish',
+    'Cheetah',
+    'Chicken',
+    'Cockatiel',
+    'Cow',
+    'Dog',
+    'Dolphin',
+    'Ferret',
+    'Firefly',
+    'Fish',
+    'Fox',
+    'Fox Terrier',
+    'Frog',
+    'Hamster',
+    'Snake',
+  ];
+  const [matchingOptions, setMatchingOptions] = React.useState([...options]);
+  const [customSearch, setCustomSearch] = React.useState<string | undefined>();
   const styles = useStyles();
+
+  const onChange: ComboboxProps['onChange'] = event => {
+    const value = event.target.value.trim();
+    const matches = options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) === 0);
+    setMatchingOptions(matches);
+    if (value.length && matches.length < 1) {
+      setCustomSearch(value);
+    } else {
+      setCustomSearch(undefined);
+    }
+  };
+
+  const onOptionSelect: ComboboxProps['onOptionSelect'] = (event, data) => {
+    const matchingOption = data.optionText && options.includes(data.optionText);
+    if (matchingOption) {
+      setCustomSearch(undefined);
+    } else {
+      setCustomSearch(data.optionText);
+    }
+  };
+
   return (
     <div className={styles.root}>
       <label id={comboId}>Best pet</label>
-      <Combobox aria-labelledby={comboId} freeform placeholder="Select an animal" {...props}>
-        {options.map(option => (
-          <Option key={option} disabled={option === 'Ferret'}>
-            {option}
+      <Combobox
+        aria-labelledby={comboId}
+        freeform
+        placeholder="Select an animal"
+        onChange={onChange}
+        onOptionSelect={onOptionSelect}
+        {...props}
+      >
+        {/* We recommend displaying a custom freeform string, if the user input doesn't match an animal */}
+        {customSearch ? (
+          <Option key="freeform" text={customSearch}>
+            "{customSearch}"
           </Option>
+        ) : null}
+        {matchingOptions.map(option => (
+          <Option key={option}>{option}</Option>
         ))}
       </Combobox>
     </div>
@@ -36,7 +88,7 @@ Freeform.parameters = {
     description: {
       story:
         'Combobox supports the `freeform` prop, which allows freeform text input. ' +
-        'The input value will be reset on blur to reflect the selected options.',
+        'Implementing filtering together with `freeform` is generally recommended.',
     },
   },
 };
