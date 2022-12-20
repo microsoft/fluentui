5bca96e1cab7c6dfc604356d6316405ba0c89b87
Oleksandr Fediashov <olfedias@microsoft.com>
2022-12-20 13:32:53 +0100

    feat: FluentProvider applies styleAttributes defined by a renderer

--- packages/react-components/react-provider/src/components/FluentProvider/useFluentProviderThemeStyleTag.test.tsx
+++ packages/react-components/react-provider/src/components/FluentProvider/useFluentProviderThemeStyleTag.test.tsx
@@ -1,8 +1,10 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { createDOMRenderer, RendererProvider } from '@griffel/react';
+import type { Theme } from '@fluentui/react-theme';
 import { resetIdsForTests } from '@fluentui/react-utilities';
+import { renderHook } from '@testing-library/react-hooks';
+import * as React from 'react';
 
 import { useFluentProviderThemeStyleTag } from './useFluentProviderThemeStyleTag';
-import type { Theme } from '@fluentui/react-theme';
 
 jest.mock('@fluentui/react-theme');
 
@@ -69,5 +71,20 @@ describe('useFluentProviderThemeStyleTag', () => {
     const rule = sheet.cssRules[0] as CSSStyleRule;
     expect(rule.selectorText).toEqual(`.${result.current}`);
     expect(rule.cssText).toMatchInlineSnapshot(`".fui-FluentProvider1 {--css-variable-update: xxx;}"`);
+  });
+
+  it('should update style tag on theme change', () => {
+    const renderer = createDOMRenderer(document, {
+      styleElementAttributes: { nonce: 'random' },
+    });
+
+    const { result } = renderHook(
+      () => useFluentProviderThemeStyleTag({ theme: defaultTheme, targetDocument: document }),
+      { wrapper: props => <RendererProvider renderer={renderer}>{props.children}</RendererProvider> },
+    );
+    const tag = document.getElementById(result.current) as HTMLStyleElement;
+
+    expect(tag.getAttribute('id')).toBe('fui-FluentProvider1');
+    expect(tag.getAttribute('nonce')).toBe('random');
   });
 });
