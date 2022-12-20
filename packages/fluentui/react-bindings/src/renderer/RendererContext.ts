14a41a34bb847641f742f07a2f2bbec5570bce64
Oleksandr Fediashov <olfedias@microsoft.com>
2022-12-20 18:16:56 +0100

    feat: add support for nonce in Fela renderer (#25992)

--- packages/fluentui/react-bindings/src/renderer/RendererContext.ts
+++ packages/fluentui/react-bindings/src/renderer/RendererContext.ts
@@ -2,4 +2,4 @@ import { createFelaRenderer } from '@fluentui/react-northstar-fela-renderer';
 import { CreateRenderer } from '@fluentui/react-northstar-styles-renderer';
 import * as React from 'react';
 
-export const RendererContext = React.createContext<CreateRenderer>(createFelaRenderer);
+export const RendererContext = React.createContext<CreateRenderer>(createFelaRenderer());
