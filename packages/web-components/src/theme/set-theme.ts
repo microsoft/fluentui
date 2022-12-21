8d5c2b6e4939b364dfdfd48e691acb2220cbed28
Miroslav Stastny <miroslav.stastny@microsoft.com>
2022-12-21 18:59:51 +0100

    feat(wc): Add v9-compatible themes (#25660)
    * bump FAST dependencies
    
    * Add theme tokens (from react-theme)
    
    * Add theme switcher to storybook
    
    * deduplicate packages
    
    * fix yarn.lock
    
    * Replace @fluentui/react-theme with @fluentui/tokens
    
    * change file
    
    * address PR comments

---
+++ packages/web-components/src/theme/set-theme.ts
@@ -0,0 +1,14 @@
+import type { Theme } from '@fluentui/tokens';
+import * as tokens from './design-tokens';
+
+const tokenNames = Object.keys(tokens);
+
+/**
+ * Sets the theme tokens on defaultNode.
+ * @param theme Flat object of theme token values.
+ */
+export const setTheme = (theme: Theme) => {
+  for (const t of tokenNames) {
+    tokens[t].withDefault(theme[t]);
+  }
+};
