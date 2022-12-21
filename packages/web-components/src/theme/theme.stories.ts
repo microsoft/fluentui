3010a9d02d8e3f34841d493a9846f414a6fcfa0c
Miroslav Stastny <miroslav.stastny@microsoft.com>
2022-12-21 18:59:51 +0100

    feat(wc): Add v9-compatible themes (#25660)
    * bump FAST dependencies
    
    * Add theme tokens (from react-theme)
    
    * Add theme switcher to storybook
    
    * deduplicate packages
    
    * fix yarn.lock
    
    * Replace @fluentui/react-theme with @fluentui/tokens
    
    * change file
    
    * address PR comments

---
+++ packages/web-components/src/theme/theme.stories.ts
@@ -0,0 +1,24 @@
+import { DesignToken } from '@microsoft/fast-foundation';
+import * as tokens from '../theme/design-tokens';
+
+DesignToken.registerDefaultStyleTarget();
+
+export default {
+  title: 'Theme/Tokens',
+};
+
+export const Tokens = () => `
+<div>
+  <h3>Theme Tokens</h3>
+  <p>Debug story which uses theme tokens to style the element below.</p>
+  <div style="
+    font-family: ${tokens.fontFamilyBase.createCSS()};
+    font-size: ${tokens.fontSizeBase300.createCSS()};
+    background: var(${tokens.colorBrandBackground.cssCustomProperty});
+    color: ${tokens.colorNeutralForegroundOnBrand.createCSS()};
+    border: ${tokens.strokeWidthThicker.createCSS()} solid ${tokens.colorNeutralStroke1.createCSS()};
+    padding: ${tokens.spacingVerticalS.createCSS()} ${tokens.spacingHorizontalM.createCSS()};
+    box-shadow: ${tokens.shadow28.createCSS()};
+  ">colorNeutralForegroundOnBrand on colorBrandBackground with shadow28</div>
+</div>
+`;
