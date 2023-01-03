2a77e849e42a2600fa14d31fd562de108a43c2ab
ling1726 <lingfangao@hotmail.com>
2023-01-03 09:38:46 +0100

    fix: Autosize middleware should set box-sizing border-box (#26094)
    * fix: Autosize should set box-sizing value
    
    The root cause of the problem is that floating ui (and popper) don't
    take into account borders/padding when the box sizing is `content-box`.
    The fix here is to explicitly set `box-sizing: border-box`since
    in the middleware instead of on the component styles since autosize
    will never work properly with content-box anyway.
    
    Also removes our custom middleware in favour of the official floating ui
    size middleware.
    
    * changefile
    
    * update test

--- packages/react-components/react-positioning/src/middleware/maxSize.ts
+++ packages/react-components/react-positioning/src/middleware/maxSize.ts
@@ -1,39 +1,39 @@
-import { detectOverflow } from '@floating-ui/dom';
-import type { Middleware, Side } from '@floating-ui/dom';
+import { size } from '@floating-ui/dom';
+import type { Middleware } from '@floating-ui/dom';
 import type { PositioningOptions } from '../types';
-import { parseFloatingUIPlacement } from '../utils/index';
+import { getBoundary } from '../utils/getBoundary';
+export interface MaxSizeMiddlewareOptions extends Pick<PositioningOptions, 'overflowBoundary'> {
+  container: HTMLElement | null;
+}
 
-export function maxSize(autoSize: PositioningOptions['autoSize']): Middleware {
-  return {
-    name: 'maxSize',
-    fn: async middlewareArguments => {
-      const { placement, rects, elements, middlewareData } = middlewareArguments;
-      const basePlacement = parseFloatingUIPlacement(placement).side;
-
-      const overflow = await detectOverflow(middlewareArguments);
-      const { x, y } = middlewareData.shift || { x: 0, y: 0 };
-      const { width, height } = rects.floating;
-
-      const widthProp: Side = basePlacement === 'left' ? 'left' : 'right';
-      const heightProp: Side = basePlacement === 'top' ? 'top' : 'bottom';
-
+export function maxSize(autoSize: PositioningOptions['autoSize'], options: MaxSizeMiddlewareOptions): Middleware {
+  const { container, overflowBoundary } = options;
+  return size({
+    ...(overflowBoundary && { altBoundary: true, boundary: getBoundary(container, overflowBoundary) }),
+    apply({ availableHeight, availableWidth, elements, rects }) {
       const applyMaxWidth =
         autoSize === 'always' ||
         autoSize === 'width-always' ||
-        (overflow[widthProp] > 0 && (autoSize === true || autoSize === 'width'));
+        (rects.floating.width > availableWidth && (autoSize === true || autoSize === 'width'));
+
       const applyMaxHeight =
         autoSize === 'always' ||
         autoSize === 'height-always' ||
-        (overflow[heightProp] > 0 && (autoSize === true || autoSize === 'height'));
+        (rects.floating.height > availableHeight && (autoSize === true || autoSize === 'height'));
+
+      if (applyMaxHeight) {
+        Object.assign(elements.floating.style, {
+          maxHeight: `${availableHeight}px`,
+          boxSizing: 'border-box',
+        });
+      }
 
       if (applyMaxWidth) {
-        elements.floating.style.maxWidth = `${width - overflow[widthProp] - x}px`;
+        Object.assign(elements.floating.style, {
+          maxWidth: `${availableWidth}px`,
+          boxSizing: 'border-box',
+        });
       }
-      if (applyMaxHeight) {
-        elements.floating.style.maxHeight = `${height - overflow[heightProp] - y}px`;
-      }
-
-      return {};
     },
-  };
+  });
 }
