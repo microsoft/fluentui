cd40c7b674965ff2d7a4c4c2615cc3ecf447c8ae
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-01-04 14:18:04 +0000

    feat(react-portal): elementContains supports Node types

--- packages/react-components/react-portal/src/virtualParent/getParent.ts
+++ packages/react-components/react-portal/src/virtualParent/getParent.ts
@@ -4,6 +4,6 @@ import { getVirtualParent } from './getVirtualParent';
  * Gets the element which is the parent of a given element.
  * This method prefers the virtual parent over real DOM parent when present.
  */
-export function getParent(child: HTMLElement | null): HTMLElement | null {
-  return (child && getVirtualParent(child)) || (child?.parentNode as HTMLElement | null);
+export function getParent(child: Node | null): Node | null {
+  return ((child && getVirtualParent(child)) || child?.parentNode) ?? null;
 }
