d62c74b6fcf39f89d90b77dae948e145dc803d3b
Lingfan Gao <lingfangao@hotmail.com>
2023-01-05 16:14:37 +0100

    implement cache

--- packages/react-components/react-utilities/src/hooks/useScrollbarWidth.ts
+++ packages/react-components/react-utilities/src/hooks/useScrollbarWidth.ts
@@ -1,13 +1,31 @@
 import * as React from 'react';
 
+const cache = new WeakMap<Document, number>();
+
+interface UseScrollbarWidthOptions {
+  /**
+   * Reference document to measure the scrollbar width
+   */
+  targetDocument: Document | null | undefined;
+
+  /**
+   * Does not use the cache and recalculates the scrollbar width
+   */
+  force?: boolean;
+}
+
 /**
  * @returns The width in pixels of the scrollbar in the user agent
  */
-export function useScrollbarWidth(options: { targetDocument: Document | undefined | null }) {
-  const { targetDocument } = options;
+export function useScrollbarWidth(options: UseScrollbarWidthOptions) {
+  const { targetDocument, force } = options;
   return React.useMemo(() => {
     if (!targetDocument) {
       return 0;
+    }
+
+    if (!force && cache.has(targetDocument)) {
+      return cache.get(targetDocument);
     }
 
     const outer = document.createElement('div');
@@ -20,6 +38,7 @@ export function useScrollbarWidth(options: { targetDocument: Document | undefine
     targetDocument.body.appendChild(outer);
     const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
     outer.remove();
+    cache.set(targetDocument, scrollbarWidth);
     return scrollbarWidth;
-  }, [targetDocument]);
+  }, [targetDocument, force]);
 }
