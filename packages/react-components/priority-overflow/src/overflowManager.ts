microsoft/fluentui

commit 6440417cb5db157acfb33d9f2c93de9bf7493791

feat: Overflow menu should be registered in overflowManager (#25091)
* feat: Overflow menu should be registered in overflowManager

Fixes #25090

The overflow manager treats the overflow menu like any other overflow
item. This causes the unfortunate bug that there is no way to whether
the last invisible item can simply replace the overflow menu.

Adds special treatment to the overflow menu so it is a special kind of
item in the overflow manager.

Updates `processOverflow` so that there is a special case at the end to
check for the case that the overflow menu can be replaced with the last
overflow item.

* changefile

* update md

* add test

* revert

* remove cruft

* rename overflowMenuSize to overflowMenuOffset

packages/react-components/priority-overflow/src/overflowManager.ts
@@ -8,6 +8,7 @@ import type { OverflowGroupState, OverflowItemEntry, OverflowManager, ObserveOpt
  */
 export function createOverflowManager(): OverflowManager {
   let container: HTMLElement | undefined;
+  let overflowMenu: HTMLElement | undefined;
   let observing = false;
   const options: Required<ObserveOptions> = {
     padding: 10,
@@ -122,6 +123,8 @@ export function createOverflowManager(): OverflowManager {
       return;
     }
 
+    const overflowMenuOffset = overflowMenu ? getOffsetSize(overflowMenu) : 0;
+
     // Snapshot of the visible/invisible state to compare for updates
     const visibleTop = visibleItemQueue.peek();
     const invisibleTop = invisibleItemQueue.peek();
@@ -142,6 +145,10 @@ export function createOverflowManager(): OverflowManager {
         break;
       }
       currentWidth -= makeItemInvisible();
+    }
+
+    if (invisibleItemQueue.size() > 0 && currentWidth + overflowMenuOffset > availableSize) {
+      makeItemInvisible();
     }
 
     // only update when the state of visible/invisible items has changed
@@ -176,6 +183,10 @@ export function createOverflowManager(): OverflowManager {
   };
 
   const addItem: OverflowManager['addItem'] = item => {
+    if (overflowItems[item.id]) {
+      return;
+    }
+
     overflowItems[item.id] = item;
 
     // some options can affect priority which are only set on `observe`
@@ -197,7 +208,19 @@ export function createOverflowManager(): OverflowManager {
     update();
   };
 
+  const addOverflowMenu: OverflowManager['addOverflowMenu'] = el => {
+    overflowMenu = el;
+  };
+
+  const removeOverflowMenu: OverflowManager['removeOverflowMenu'] = () => {
+    overflowMenu = undefined;
+  };
+
   const removeItem: OverflowManager['removeItem'] = itemId => {
+    if (!overflowItems[itemId]) {
+      return;
+    }
+
     const item = overflowItems[itemId];
     visibleItemQueue.remove(itemId);
     invisibleItemQueue.remove(itemId);
@@ -218,5 +241,7 @@ export function createOverflowManager(): OverflowManager {
     observe,
     removeItem,
     update,
+    addOverflowMenu,
+    removeOverflowMenu,
   };
 }

packages/react-components/priority-overflow/src/priorityQueue.ts
@@ -88,13 +88,14 @@ export function createPriorityQueue<T>(compare: PriorityQueueCompareFn<T>): Prio
   };
 
   const contains = (item: T) => {
-    return arr.indexOf(item) >= 0;
+    const index = arr.indexOf(item);
+    return index >= 0 && index < size;
   };
 
   const remove = (item: T) => {
     const i = arr.indexOf(item);
 
-    if (i === -1) {
+    if (i === -1 || i >= size) {
       return;
     }
 

packages/react-components/priority-overflow/src/types.ts
@@ -104,4 +104,15 @@ export interface OverflowManager {
    * Manually update the overflow sync
    */
   forceUpdate: () => void;
+
+  /**
+   * Adds an element that opens an overflow menu. This is used to calculate
+   * available space and check if additional items need to overflow
+   */
+  addOverflowMenu: (element: HTMLElement) => void;
+
+  /**
+   * Unsets the overflow menu element
+   */
+  removeOverflowMenu: () => void;
 }

packages/react-components/react-overflow/e2e/Overflow.e2e.tsx
@@ -48,7 +48,8 @@ const setContainerSize = (size: number) => {
     .then(container => {
       container.css('width', `${size}px`);
     })
-    .log(`Setting container size to ${size}px`);
+    .log(`Setting container size to ${size}px`)
+    .wait(1);
 };
 
 const Item: React.FC<{ children?: React.ReactNode; width?: number } & Omit<OverflowItemProps, 'children'>> = ({
@@ -413,5 +414,25 @@ describe('Overflow', () => {
     setContainerSize(200);
     cy.get(`[${selectors.divider}="1"]`).should('not.exist');
     cy.get(`[${selectors.divider}]`).should('have.length', 1);
+  });
+
+  it.only('should remove overflow menu if the last overflowed item can take its place', () => {
+    const mapHelper = new Array(10).fill(0).map((_, i) => i);
+    mount(
+      <Container>
+        {mapHelper.map(i => (
+          <Item key={i} id={i.toString()}>
+            {i}
+          </Item>
+        ))}
+        <Menu />
+      </Container>,
+    );
+
+    setContainerSize(497);
+    setContainerSize(498);
+    setContainerSize(499);
+    setContainerSize(500);
+    cy.get(`[${selectors.menu}]`).should('not.exist');
   });
 });

packages/react-components/react-overflow/src/components/Overflow.tsx
@@ -55,7 +55,7 @@ export const Overflow = React.forwardRef((props: OverflowProps, ref) => {
     setGroupVisibility(data.groupVisibility);
   };
 
-  const { containerRef, registerItem, updateOverflow } = useOverflowContainer(update, {
+  const { containerRef, registerItem, updateOverflow, registerOverflowMenu } = useOverflowContainer(update, {
     overflowDirection,
     overflowAxis,
     padding,
@@ -76,6 +76,7 @@ export const Overflow = React.forwardRef((props: OverflowProps, ref) => {
         hasOverflow,
         registerItem,
         updateOverflow,
+        registerOverflowMenu,
       }}
     >
       {clonedChild}

packages/react-components/react-overflow/src/overflowContext.ts
@@ -6,6 +6,7 @@ export interface OverflowContextValue {
   groupVisibility: Record<string, OverflowGroupState>;
   hasOverflow: boolean;
   registerItem: (item: OverflowItemEntry) => () => void;
+  registerOverflowMenu: (el: HTMLElement) => () => void;
   updateOverflow: (padding?: number) => void;
 }
 
@@ -19,6 +20,7 @@ const overflowContextDefaultValue: OverflowContextValue = {
   hasOverflow: false,
   registerItem: () => () => null,
   updateOverflow: () => null,
+  registerOverflowMenu: () => () => null,
 };
 
 export const useOverflowContext = <SelectedValue>(selector: ContextSelector<OverflowContextValue, SelectedValue>) =>

packages/react-components/react-overflow/src/stories/Overflow/ReverseDomOrder.stories.tsx
@@ -20,10 +20,10 @@ export const ReverseDomOrder = () => {
   return (
     <Overflow overflowDirection="start">
       <div className={styles.container}>
+        <OverflowMenu itemIds={itemIds} />
         {itemIds.map(i => (
           <TestOverflowItem key={i} id={i} />
         ))}
-        <OverflowMenu itemIds={itemIds} />
       </div>
     </Overflow>
   );

packages/react-components/react-overflow/src/types.ts
@@ -4,17 +4,10 @@ import { OverflowContextValue } from './overflowContext';
 /**
  * @internal
  */
-export interface UseOverflowContainerReturn<TElement extends HTMLElement> {
+export interface UseOverflowContainerReturn<TElement extends HTMLElement>
+  extends Pick<OverflowContextValue, 'registerItem' | 'updateOverflow' | 'registerOverflowMenu'> {
   /**
    * Ref to apply to the container that will overflow
    */
   containerRef: React.RefObject<TElement>;
-  /**
-   * Registers and overflow item
-   */
-  registerItem: OverflowContextValue['registerItem'];
-  /**
-   * Imperative function to trigger overflow update
-   */
-  updateOverflow: OverflowContextValue['updateOverflow'];
 }

commit 76122f9e3c70bf8b5ed1c3908216866ac6444446

fix: Minimum visible overflow items should be respected
This fixes a regression from #25091, the updated mechanism to handle
overflow menu was inadvertedly breaking the minumum visible options
because it was still making items invisible without considering whether
the minimum number of items was reached.

packages/react-components/priority-overflow/src/overflowManager.ts
@@ -135,19 +135,25 @@ export function createOverflowManager(): OverflowManager {
       return sum + getOffsetSize(child);
     }, 0);
 
-    // Add items until available width is filled
+    // Add items until available width is filled - can result in overflow
     while (currentWidth < availableSize && invisibleItemQueue.size() > 0) {
       currentWidth += makeItemVisible();
     }
-    // Remove items until there's no more overlap
+
+    // Remove items until there's no more overflow
     while (currentWidth > availableSize && visibleItemQueue.size() > 0) {
-      if (visibleItemQueue.size() === options.minimumVisible) {
+      if (visibleItemQueue.size() <= options.minimumVisible) {
         break;
       }
       currentWidth -= makeItemInvisible();
     }
 
-    if (invisibleItemQueue.size() > 0 && currentWidth + overflowMenuOffset > availableSize) {
+    // make sure the overflow menu can fit
+    if (
+      visibleItemQueue.size() > options.minimumVisible &&
+      invisibleItemQueue.size() > 0 &&
+      currentWidth + overflowMenuOffset > availableSize
+    ) {
       makeItemInvisible();
     }
 