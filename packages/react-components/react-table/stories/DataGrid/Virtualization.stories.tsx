a535835768c5ac6f3c74d5c07901a047683e3b55
Lingfan Gao <lingfangao@hotmail.com>
2023-01-06 14:39:31 +0100

    useScrollbarWidth

--- packages/react-components/react-table/stories/DataGrid/Virtualization.stories.tsx
+++ packages/react-components/react-table/stories/DataGrid/Virtualization.stories.tsx
@@ -8,7 +8,7 @@ import {
   DocumentPdfRegular,
   VideoRegular,
 } from '@fluentui/react-icons';
-import { PresenceBadgeStatus, Avatar, useFluent } from '@fluentui/react-components';
+import { PresenceBadgeStatus, Avatar, useScrollbarWidth } from '@fluentui/react-components';
 import { TableCellLayout } from '@fluentui/react-components/unstable';
 import {
   DataGridRow,
@@ -47,27 +47,6 @@ type Item = {
   author: AuthorCell;
   lastUpdated: LastUpdatedCell;
   lastUpdate: LastUpdateCell;
-};
-
-const useScrollbarWidth = () => {
-  const { targetDocument } = useFluent();
-  return React.useMemo(() => {
-    if (!targetDocument) {
-      return 0;
-    }
-
-    const outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.overflow = 'scroll';
-
-    const inner = document.createElement('div');
-    outer.appendChild(inner);
-
-    targetDocument.body.appendChild(outer);
-    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
-    outer.remove();
-    return scrollbarWidth;
-  }, [targetDocument]);
 };
 
 export const Virtualization = () => {
