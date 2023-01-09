microsoft/fluentui

commit 41b0dbe63181a473ea54811fffd5a9582d7b28b6

chore(react-menu): removes prop spreading on MenuList state (#25672)
* chore(react-menu): removes prop spreading on MenuList state

* chore: deprecates unused type UninitializedMenuListState

packages/react-components/react-menu/src/components/MenuList/MenuList.types.ts
@@ -47,7 +47,6 @@ export type MenuListProps = ComponentProps<MenuListSlots> & {
 };
 
 export type MenuListState = ComponentState<MenuListSlots> &
-  Pick<MenuListProps, 'defaultCheckedValues' | 'onCheckedValueChange'> &
   Required<Pick<MenuListProps, 'checkedValues' | 'hasCheckmarks' | 'hasIcons'>> & {
     /**
      * Selects a radio item, will de-select the currently selected ratio item
@@ -63,12 +62,31 @@ export type MenuListState = ComponentState<MenuListSlots> &
      * Toggles the state of a checkbox item
      */
     toggleCheckbox: SelectableHandler;
+
+    /**
+     * Default values to be checked on mount
+     * @deprecated this property is not used internally anymore,
+     * the signature remains just to avoid breaking changes
+     */
+    defaultCheckedValues?: Record<string, string[]>;
+    /**
+     * Callback when checked items change for value with a name
+     *
+     * @param event - React's original SyntheticEvent
+     * @param data - A data object with relevant information
+     * @deprecated this property is not used internally anymore,
+     * the signature remains just to avoid breaking changes
+     */
+    onCheckedValueChange?: (e: MenuCheckedValueChangeEvent, data: MenuCheckedValueChangeData) => void;
   };
 
 export type MenuListContextValues = {
   menuList: MenuListContextValue;
 };
 
+/**
+ * @deprecated this type is not being used internally anymore
+ */
 export type UninitializedMenuListState = Omit<
   MenuListState,
   'checkedValues' | 'selectRadio' | 'setFocusByFirstCharacter' | 'toggleCheckbox'

packages/react-components/react-menu/src/components/MenuList/useMenuList.ts
@@ -9,7 +9,7 @@ import { useArrowNavigationGroup, useFocusFinders } from '@fluentui/react-tabste
 import { useHasParentContext } from '@fluentui/react-context-selector';
 import { useMenuContext_unstable } from '../../contexts/menuContext';
 import { MenuContext } from '../../contexts/menuContext';
-import type { MenuListProps, MenuListState, UninitializedMenuListState } from './MenuList.types';
+import type { MenuListProps, MenuListState } from './MenuList.types';
 
 /**
  * Returns the props and state required to render the component
@@ -27,22 +27,6 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
   }
 
   const innerRef = React.useRef<HTMLElement>(null);
-  const initialState: UninitializedMenuListState = {
-    components: {
-      root: 'div',
-    },
-    root: getNativeElementProps('div', {
-      ref: useMergedRefs(ref, innerRef),
-      role: 'menu',
-      'aria-labelledby': menuContext.triggerId,
-      ...focusAttributes,
-      ...props,
-    }),
-    hasIcons: menuContext.hasIcons || false,
-    hasCheckmarks: menuContext.hasCheckmarks || false,
-    ...(hasMenuContext && menuContext),
-    ...props,
-  };
 
   const setFocusByFirstCharacter = React.useCallback(
     (e: React.KeyboardEvent<HTMLElement>, itemEl: HTMLElement) => {
@@ -91,12 +75,13 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
   );
 
   const [checkedValues, setCheckedValues] = useControllableState({
-    state: initialState.checkedValues,
-    defaultState: initialState.defaultCheckedValues,
+    state: hasMenuContext ? menuContext.checkedValues : props.checkedValues,
+    defaultState: props.defaultCheckedValues,
     initialState: {},
   });
 
-  const { onCheckedValueChange } = initialState;
+  const handleCheckedValueChange = hasMenuContext ? menuContext.onCheckedValueChange : props.onCheckedValueChange;
+
   const toggleCheckbox = useEventCallback(
     (e: React.MouseEvent | React.KeyboardEvent, name: string, value: string, checked: boolean) => {
       const checkedItems = checkedValues?.[name] || [];
@@ -107,7 +92,7 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
         newCheckedItems.push(value);
       }
 
-      onCheckedValueChange?.(e, { name, checkedItems: newCheckedItems });
+      handleCheckedValueChange?.(e, { name, checkedItems: newCheckedItems });
       setCheckedValues(s => ({ ...s, [name]: newCheckedItems }));
     },
   );
@@ -115,18 +100,27 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
   const selectRadio = useEventCallback((e: React.MouseEvent | React.KeyboardEvent, name: string, value: string) => {
     const newCheckedItems = [value];
     setCheckedValues(s => ({ ...s, [name]: newCheckedItems }));
-    onCheckedValueChange?.(e, { name, checkedItems: newCheckedItems });
+    handleCheckedValueChange?.(e, { name, checkedItems: newCheckedItems });
   });
 
-  const state = {
-    ...initialState,
+  return {
+    components: {
+      root: 'div',
+    },
+    root: getNativeElementProps('div', {
+      ref: useMergedRefs(ref, innerRef),
+      role: 'menu',
+      'aria-labelledby': menuContext.triggerId,
+      ...focusAttributes,
+      ...props,
+    }),
+    hasIcons: menuContext.hasIcons || false,
+    hasCheckmarks: menuContext.hasCheckmarks || false,
+    checkedValues,
     setFocusByFirstCharacter,
     selectRadio,
     toggleCheckbox,
-    checkedValues: checkedValues ?? {},
   };
-
-  return state;
 };
 
 /**
@@ -135,7 +129,6 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
 const useMenuContextSelectors = () => {
   const checkedValues = useMenuContext_unstable(context => context.checkedValues);
   const onCheckedValueChange = useMenuContext_unstable(context => context.onCheckedValueChange);
-  const defaultCheckedValues = useMenuContext_unstable(context => context.defaultCheckedValues);
   const triggerId = useMenuContext_unstable(context => context.triggerId);
   const hasIcons = useMenuContext_unstable(context => context.hasIcons);
   const hasCheckmarks = useMenuContext_unstable(context => context.hasCheckmarks);
@@ -143,7 +136,6 @@ const useMenuContextSelectors = () => {
   return {
     checkedValues,
     onCheckedValueChange,
-    defaultCheckedValues,
     triggerId,
     hasIcons,
     hasCheckmarks,

packages/react-components/react-components/src/index.ts
@@ -484,6 +484,8 @@ export type {
   MenuTriggerProps,
   MenuTriggerState,
   SelectableHandler,
+  // UninitializedMenuListState is deprecated but removing it would be a breaking change
+  // eslint-disable-next-line deprecation/deprecation
   UninitializedMenuListState,
 } from '@fluentui/react-menu';
 export {

packages/react-components/react-menu/src/components/Menu/Menu.types.ts
@@ -22,13 +22,6 @@ export type MenuProps = ComponentProps<MenuSlots> &
     children: [JSX.Element, JSX.Element] | JSX.Element;
 
     /**
-     * Whether the popup is open by default
-     *
-     * @default false
-     */
-    defaultOpen?: boolean;
-
-    /**
      * Sets the delay for mouse open/close for the popover one mouse enter/leave
      */
     hoverDelay?: number;
@@ -53,6 +46,13 @@ export type MenuProps = ComponentProps<MenuSlots> &
      * @default false
      */
     open?: boolean;
+
+    /**
+     * Whether the popup is open by default
+     *
+     * @default false
+     */
+    defaultOpen?: boolean;
 
     /**
      * Opens the menu on right click (context menu), removes all other menu open interactions
@@ -89,7 +89,6 @@ export type MenuProps = ComponentProps<MenuSlots> &
   };
 
 export type MenuState = ComponentState<MenuSlots> &
-  Pick<MenuProps, 'onOpenChange' | 'defaultCheckedValues'> &
   Required<
     Pick<
       MenuProps,
@@ -150,6 +149,20 @@ export type MenuState = ComponentState<MenuSlots> &
      * The ref for the MenuTrigger, used for popup positioning
      */
     triggerRef: React.MutableRefObject<HTMLElement>;
+
+    /**
+     * Call back when the component requests to change value
+     * The `open` value is used as a hint when directly controlling the component
+     * @deprecated this property is not used internally anymore,
+     * the signature remains just to avoid breaking changes
+     */
+    onOpenChange?: (e: MenuOpenEvent, data: MenuOpenChangeData) => void;
+    /**
+     * Default values to be checked on mount
+     @deprecated this property is not used internally anymore,
+     * the signature remains just to avoid breaking changes
+     */
+    defaultCheckedValues?: Record<string, string[]>;
   };
 
 export type MenuContextValues = {

packages/react-components/react-menu/src/components/Menu/useMenu.tsx
@@ -41,11 +41,11 @@ export const useMenu_unstable = (props: MenuProps): MenuState => {
   const [contextTarget, setContextTarget] = usePositioningMouseTarget();
 
   const positioningState = {
-    position: isSubmenu ? ('after' as const) : ('below' as const),
-    align: isSubmenu ? ('top' as const) : ('start' as const),
+    position: isSubmenu ? 'after' : 'below',
+    align: isSubmenu ? 'top' : 'start',
     target: props.openOnContext ? contextTarget : undefined,
     ...resolvePositioningShorthand(props.positioning),
-  };
+  } as const;
 
   const children = React.Children.toArray(props.children) as React.ReactElement[];
 
@@ -69,10 +69,10 @@ export const useMenu_unstable = (props: MenuProps): MenuState => {
   } else if (children.length === 1) {
     menuPopover = children[0];
   }
+
   const { targetRef: triggerRef, containerRef: menuPopoverRef } = usePositioning(positioningState);
 
   // TODO Better way to narrow types ?
-
   const [open, setOpen] = useMenuOpenState({
     hoverDelay,
     isSubmenu,
@@ -112,7 +112,6 @@ export const useMenu_unstable = (props: MenuProps): MenuState => {
     open,
     setOpen,
     checkedValues,
-    defaultCheckedValues,
     onCheckedValueChange,
     persistOnItemClick,
   };
@@ -123,22 +122,20 @@ export const useMenu_unstable = (props: MenuProps): MenuState => {
  * i.e checkboxes and radios
  */
 const useMenuSelectableState = (
-  state: Pick<MenuProps, 'checkedValues' | 'defaultCheckedValues' | 'onCheckedValueChange'>,
+  props: Pick<MenuProps, 'checkedValues' | 'defaultCheckedValues' | 'onCheckedValueChange'>,
 ) => {
   const [checkedValues, setCheckedValues] = useControllableState({
-    state: state.checkedValues,
-    defaultState: state.defaultCheckedValues,
+    state: props.checkedValues,
+    defaultState: props.defaultCheckedValues,
     initialState: {},
   });
-  const { onCheckedValueChange: onCheckedValueChangeOriginal } = state;
   const onCheckedValueChange: MenuState['onCheckedValueChange'] = useEventCallback((e, { name, checkedItems }) => {
-    if (onCheckedValueChangeOriginal) {
-      onCheckedValueChangeOriginal(e, { name, checkedItems });
-    }
+    props.onCheckedValueChange?.(e, { name, checkedItems });
 
-    setCheckedValues(s => {
-      return s ? { ...s, [name]: checkedItems } : { [name]: checkedItems };
-    });
+    setCheckedValues(currentValue => ({
+      ...currentValue,
+      [name]: checkedItems,
+    }));
   });
 
   return [checkedValues, onCheckedValueChange] as const;
@@ -149,18 +146,17 @@ const useMenuOpenState = (
     MenuState,
     | 'isSubmenu'
     | 'menuPopoverRef'
-    | 'onOpenChange'
     | 'setContextTarget'
     | 'triggerRef'
     | 'openOnContext'
     | 'closeOnScroll'
     | 'hoverDelay'
   > &
-    Pick<MenuProps, 'open' | 'defaultOpen'>,
+    Pick<MenuProps, 'open' | 'defaultOpen' | 'onOpenChange'>,
 ) => {
   const { targetDocument } = useFluent();
   const parentSetOpen = useMenuContext_unstable(context => context.setOpen);
-  const onOpenChange: MenuState['onOpenChange'] = useEventCallback((e, data) => state.onOpenChange?.(e, data));
+  const onOpenChange: MenuProps['onOpenChange'] = useEventCallback((e, data) => state.onOpenChange?.(e, data));
 
   const shouldHandleCloseRef = React.useRef(false);
   const shouldHandleTabRef = React.useRef(false);

packages/react-components/react-menu/src/components/Menu/useMenuContextValues.test.tsx
@@ -15,7 +15,6 @@ describe('useMenuContextValues_unstable', () => {
     expect(result.current.menu).toMatchInlineSnapshot(`
       Object {
         "checkedValues": Object {},
-        "defaultCheckedValues": undefined,
         "hasCheckmarks": false,
         "hasIcons": false,
         "inline": false,

packages/react-components/react-menu/src/components/Menu/useMenuContextValues.ts
@@ -3,7 +3,6 @@ import type { MenuContextValues, MenuState } from './Menu.types';
 export function useMenuContextValues_unstable(state: MenuState): MenuContextValues {
   const {
     checkedValues,
-    defaultCheckedValues,
     hasCheckmarks,
     hasIcons,
     inline,
@@ -22,7 +21,6 @@ export function useMenuContextValues_unstable(state: MenuState): MenuContextValu
   // This context is created with "@fluentui/react-context-selector", these is no sense to memoize it
   const menu = {
     checkedValues,
-    defaultCheckedValues,
     hasCheckmarks,
     hasIcons,
     inline,

packages/react-components/react-menu/src/components/MenuGroup/useMenuGroup.ts
@@ -18,6 +18,6 @@ export function useMenuGroup_unstable(props: MenuGroupProps, ref: React.Ref<HTML
       role: 'group',
       ...props,
     }),
-    headerId: headerId,
+    headerId,
   };
 }

commit 1c6638309c560acb0592e718c55a80fd9f6aa38c

fix: MenuList props should win over context props
Fixes the regression introduced in #25672 so that prop values win over
context props with the same name.

Fixes #

packages/react-components/react-menu/src/components/MenuList/MenuList.types.ts
@@ -47,7 +47,8 @@ export type MenuListProps = ComponentProps<MenuListSlots> & {
 };
 
 export type MenuListState = ComponentState<MenuListSlots> &
-  Required<Pick<MenuListProps, 'checkedValues' | 'hasCheckmarks' | 'hasIcons'>> & {
+  Required<Pick<MenuListProps, 'checkedValues' | 'hasCheckmarks' | 'hasIcons'>> &
+  Pick<MenuListProps, 'defaultCheckedValues' | 'onCheckedValueChange'> & {
     /**
      * Selects a radio item, will de-select the currently selected ratio item
      */
@@ -62,22 +63,6 @@ export type MenuListState = ComponentState<MenuListSlots> &
      * Toggles the state of a checkbox item
      */
     toggleCheckbox: SelectableHandler;
-
-    /**
-     * Default values to be checked on mount
-     * @deprecated this property is not used internally anymore,
-     * the signature remains just to avoid breaking changes
-     */
-    defaultCheckedValues?: Record<string, string[]>;
-    /**
-     * Callback when checked items change for value with a name
-     *
-     * @param event - React's original SyntheticEvent
-     * @param data - A data object with relevant information
-     * @deprecated this property is not used internally anymore,
-     * the signature remains just to avoid breaking changes
-     */
-    onCheckedValueChange?: (e: MenuCheckedValueChangeEvent, data: MenuCheckedValueChangeData) => void;
   };
 
 export type MenuListContextValues = {

packages/react-components/react-menu/src/components/MenuList/useMenuList.ts
@@ -75,12 +75,13 @@ export const useMenuList_unstable = (props: MenuListProps, ref: React.Ref<HTMLEl
   );
 
   const [checkedValues, setCheckedValues] = useControllableState({
-    state: hasMenuContext ? menuContext.checkedValues : props.checkedValues,
+    state: props.checkedValues ?? (hasMenuContext ? menuContext.checkedValues : undefined),
     defaultState: props.defaultCheckedValues,
     initialState: {},
   });
 
-  const handleCheckedValueChange = hasMenuContext ? menuContext.onCheckedValueChange : props.onCheckedValueChange;
+  const handleCheckedValueChange =
+    props.onCheckedValueChange ?? (hasMenuContext ? menuContext.onCheckedValueChange : undefined);
 
   const toggleCheckbox = useEventCallback(
     (e: React.MouseEvent | React.KeyboardEvent, name: string, value: string, checked: boolean) => {

packages/react-components/react-menu/stories/Menu/MenuControlledCheckboxItems.stories.tsx
@@ -25,12 +25,12 @@ export const ControlledCheckboxItems = () => {
   };
 
   return (
-    <Menu>
+    <Menu checkedValues={checkedValues} onCheckedValueChange={onChange}>
       <MenuTrigger disableButtonEnhancement>
         <Button>Toggle menu</Button>
       </MenuTrigger>
       <MenuPopover>
-        <MenuList checkedValues={checkedValues} onCheckedValueChange={onChange}>
+        <MenuList>
           <MenuItemCheckbox icon={<CutIcon />} name="edit" value="cut">
             Cut
           </MenuItemCheckbox>

packages/react-components/react-menu/stories/Menu/MenuControlledRadioItems.stories.tsx
@@ -23,12 +23,12 @@ export const ControlledRadioItems = () => {
   };
 
   return (
-    <Menu>
+    <Menu checkedValues={checkedValues} onCheckedValueChange={onChange}>
       <MenuTrigger disableButtonEnhancement>
         <Button>Toggle menu</Button>
       </MenuTrigger>
       <MenuPopover>
-        <MenuList checkedValues={checkedValues} onCheckedValueChange={onChange}>
+        <MenuList>
           <MenuItemRadio icon={<CutIcon />} name="font" value="segoe">
             Segoe
           </MenuItemRadio>

packages/react-components/react-menu/stories/MenuList/MenuListCheckboxItems.stories.tsx
@@ -1,0 +1,49 @@
+import * as React from 'react';
+
+import { MenuList, MenuItemCheckbox, makeStyles, tokens } from '@fluentui/react-components';
+import {
+  bundleIcon,
+  CutRegular,
+  CutFilled,
+  ClipboardPasteRegular,
+  ClipboardPasteFilled,
+  EditRegular,
+  EditFilled,
+} from '@fluentui/react-icons';
+
+const CutIcon = bundleIcon(CutFilled, CutRegular);
+const PasteIcon = bundleIcon(ClipboardPasteFilled, ClipboardPasteRegular);
+const EditIcon = bundleIcon(EditFilled, EditRegular);
+
+export const useMenuListContainerStyles = makeStyles({
+  container: {
+    backgroundColor: tokens.colorNeutralBackground1,
+    minWidth: '128px',
+    minHeight: '48px',
+    maxWidth: '300px',
+    width: 'max-content',
+    boxShadow: `${tokens.shadow16}`,
+    paddingTop: '4px',
+    paddingBottom: '4px',
+  },
+});
+
+export const CheckboxItems = () => {
+  const styles = useMenuListContainerStyles();
+
+  return (
+    <div className={styles.container}>
+      <MenuList>
+        <MenuItemCheckbox icon={<CutIcon />} name="edit" value="cut">
+          Cut
+        </MenuItemCheckbox>
+        <MenuItemCheckbox icon={<PasteIcon />} name="edit" value="paste">
+          Paste
+        </MenuItemCheckbox>
+        <MenuItemCheckbox icon={<EditIcon />} name="edit" value="edit">
+          Edit
+        </MenuItemCheckbox>
+      </MenuList>
+    </div>
+  );
+};

packages/react-components/react-menu/stories/MenuList/MenuListControlledCheckboxItems.stories.tsx
@@ -1,0 +1,56 @@
+import * as React from 'react';
+
+import { MenuList, MenuItemCheckbox, makeStyles, tokens } from '@fluentui/react-components';
+import {
+  bundleIcon,
+  CutRegular,
+  CutFilled,
+  ClipboardPasteRegular,
+  ClipboardPasteFilled,
+  EditRegular,
+  EditFilled,
+} from '@fluentui/react-icons';
+import type { MenuProps } from '@fluentui/react-components';
+
+const CutIcon = bundleIcon(CutFilled, CutRegular);
+const PasteIcon = bundleIcon(ClipboardPasteFilled, ClipboardPasteRegular);
+const EditIcon = bundleIcon(EditFilled, EditRegular);
+
+export const useMenuListContainerStyles = makeStyles({
+  container: {
+    backgroundColor: tokens.colorNeutralBackground1,
+    minWidth: '128px',
+    minHeight: '48px',
+    maxWidth: '300px',
+    width: 'max-content',
+    boxShadow: `${tokens.shadow16}`,
+    paddingTop: '4px',
+    paddingBottom: '4px',
+  },
+});
+
+export const ControlledCheckboxItems = () => {
+  const styles = useMenuListContainerStyles();
+  const [checkedValues, setCheckedValues] = React.useState<Record<string, string[]>>({ edit: ['cut', 'paste'] });
+  const onChange: MenuProps['onCheckedValueChange'] = (e, { name, checkedItems }) => {
+    setCheckedValues(s => {
+      return s ? { ...s, [name]: checkedItems } : { [name]: checkedItems };
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <MenuList checkedValues={checkedValues} onCheckedValueChange={onChange}>
+        <MenuItemCheckbox icon={<CutIcon />} name="edit" value="cut">
+          Cut
+        </MenuItemCheckbox>
+        <MenuItemCheckbox icon={<PasteIcon />} name="edit" value="paste">
+          Paste
+        </MenuItemCheckbox>
+        <MenuItemCheckbox icon={<EditIcon />} name="edit" value="edit">
+          Edit
+        </MenuItemCheckbox>
+      </MenuList>
+    </div>
+  );
+};

packages/react-components/react-menu/stories/MenuList/MenuListControlledRadioItems.stories.tsx
@@ -1,0 +1,54 @@
+import * as React from 'react';
+
+import { MenuList, MenuItemRadio, makeStyles, tokens } from '@fluentui/react-components';
+import {
+  bundleIcon,
+  CutRegular,
+  CutFilled,
+  ClipboardPasteRegular,
+  ClipboardPasteFilled,
+  EditRegular,
+  EditFilled,
+} from '@fluentui/react-icons';
+import type { MenuProps } from '@fluentui/react-components';
+
+const CutIcon = bundleIcon(CutFilled, CutRegular);
+const PasteIcon = bundleIcon(ClipboardPasteFilled, ClipboardPasteRegular);
+const EditIcon = bundleIcon(EditFilled, EditRegular);
+
+export const useMenuListContainerStyles = makeStyles({
+  container: {
+    backgroundColor: tokens.colorNeutralBackground1,
+    minWidth: '128px',
+    minHeight: '48px',
+    maxWidth: '300px',
+    width: 'max-content',
+    boxShadow: `${tokens.shadow16}`,
+    paddingTop: '4px',
+    paddingBottom: '4px',
+  },
+});
+
+export const ControlledRadioItems = () => {
+  const styles = useMenuListContainerStyles();
+  const [checkedValues, setCheckedValues] = React.useState<Record<string, string[]>>({ font: ['calibri'] });
+  const onChange: MenuProps['onCheckedValueChange'] = (e, { name, checkedItems }) => {
+    setCheckedValues(s => ({ ...s, [name]: checkedItems }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <MenuList checkedValues={checkedValues} onCheckedValueChange={onChange}>
+        <MenuItemRadio icon={<CutIcon />} name="font" value="segoe">
+          Segoe
+        </MenuItemRadio>
+        <MenuItemRadio icon={<PasteIcon />} name="font" value="calibri">
+          Calibri
+        </MenuItemRadio>
+        <MenuItemRadio icon={<EditIcon />} name="font" value="arial">
+          Arial
+        </MenuItemRadio>
+      </MenuList>
+    </div>
+  );
+};

packages/react-components/react-menu/stories/MenuList/MenuListRadioItems.stories.tsx
@@ -1,0 +1,48 @@
+import * as React from 'react';
+
+import { MenuList, MenuItemRadio, makeStyles, tokens } from '@fluentui/react-components';
+import {
+  bundleIcon,
+  CutRegular,
+  CutFilled,
+  ClipboardPasteRegular,
+  ClipboardPasteFilled,
+  EditRegular,
+  EditFilled,
+} from '@fluentui/react-icons';
+
+const CutIcon = bundleIcon(CutFilled, CutRegular);
+const PasteIcon = bundleIcon(ClipboardPasteFilled, ClipboardPasteRegular);
+const EditIcon = bundleIcon(EditFilled, EditRegular);
+
+export const useMenuListContainerStyles = makeStyles({
+  container: {
+    backgroundColor: tokens.colorNeutralBackground1,
+    minWidth: '128px',
+    minHeight: '48px',
+    maxWidth: '300px',
+    width: 'max-content',
+    boxShadow: `${tokens.shadow16}`,
+    paddingTop: '4px',
+    paddingBottom: '4px',
+  },
+});
+
+export const RadioItems = () => {
+  const styles = useMenuListContainerStyles();
+  return (
+    <div className={styles.container}>
+      <MenuList>
+        <MenuItemRadio icon={<CutIcon />} name="font" value="segoe">
+          Segoe
+        </MenuItemRadio>
+        <MenuItemRadio icon={<PasteIcon />} name="font" value="calibri">
+          Calibri
+        </MenuItemRadio>
+        <MenuItemRadio icon={<EditIcon />} name="font" value="arial">
+          Arial
+        </MenuItemRadio>
+      </MenuList>
+    </div>
+  );
+};

packages/react-components/react-menu/stories/MenuList/index.stories.tsx
@@ -3,6 +3,10 @@ import descriptionMd from './MenuListDescription.md';
 
 export { Default } from './MenuListDefault.stories';
 export { MenuListWithNestedSubmenus } from './MenuListNestedSubmenus.stories';
+export { CheckboxItems } from './MenuListCheckboxItems.stories';
+export { RadioItems } from './MenuListRadioItems.stories';
+export { ControlledCheckboxItems } from './MenuListControlledCheckboxItems.stories';
+export { ControlledRadioItems } from './MenuListControlledRadioItems.stories';
 
 export default {
   title: 'Components/Menu/MenuList',

packages/react-components/react-menu/src/components/Menu/Menu.test.tsx
@@ -284,4 +284,118 @@ describe('Menu', () => {
     // Assert
     expect(container.querySelector('[role="menu"]')).not.toBeNull();
   });
+
+  it('should call onCheckedValueChange when item is selected', () => {
+    const onCheckedValueChange = jest.fn();
+    const { getAllByRole } = render(
+      <Menu open inline onCheckedValueChange={onCheckedValueChange}>
+        <MenuTrigger disableButtonEnhancement>
+          <button>MenuTrigger</button>
+        </MenuTrigger>
+
+        <MenuPopover>
+          <MenuList>
+            <MenuItemCheckbox name="test" value="first">
+              First
+            </MenuItemCheckbox>
+            <MenuItemCheckbox name="test" value="second">
+              Second
+            </MenuItemCheckbox>
+            <MenuItemCheckbox name="test" value="third">
+              Third
+            </MenuItemCheckbox>
+          </MenuList>
+        </MenuPopover>
+      </Menu>,
+    );
+
+    const checkboxes = getAllByRole('menuitemcheckbox');
+    fireEvent.click(checkboxes[0]);
+    expect(onCheckedValueChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('should control checked items with checkedValues prop', () => {
+    const { container } = render(
+      <Menu open inline checkedValues={{ test: ['second'] }}>
+        <MenuTrigger disableButtonEnhancement>
+          <button>MenuTrigger</button>
+        </MenuTrigger>
+
+        <MenuPopover>
+          <MenuList>
+            <MenuItemCheckbox id="first" name="test" value="first">
+              First
+            </MenuItemCheckbox>
+            <MenuItemCheckbox id="second" name="test" value="second">
+              Second
+            </MenuItemCheckbox>
+            <MenuItemCheckbox id="third" name="test" value="third">
+              Third
+            </MenuItemCheckbox>
+          </MenuList>
+        </MenuPopover>
+      </Menu>,
+    );
+
+    expect(container.querySelector('#first')?.getAttribute('aria-checked')).toBe('false');
+    expect(container.querySelector('#second')?.getAttribute('aria-checked')).toBe('true');
+    expect(container.querySelector('#third')?.getAttribute('aria-checked')).toBe('false');
+  });
+
+  it('should call onCheckedValueChange (applied to MenuList) when item is selected', () => {
+    const onCheckedValueChange = jest.fn();
+    const { getAllByRole } = render(
+      <Menu open inline>
+        <MenuTrigger disableButtonEnhancement>
+          <button>MenuTrigger</button>
+        </MenuTrigger>
+
+        <MenuPopover>
+          <MenuList onCheckedValueChange={onCheckedValueChange}>
+            <MenuItemCheckbox name="test" value="first">
+              First
+            </MenuItemCheckbox>
+            <MenuItemCheckbox name="test" value="second">
+              Second
+            </MenuItemCheckbox>
+            <MenuItemCheckbox name="test" value="third">
+              Third
+            </MenuItemCheckbox>
+          </MenuList>
+        </MenuPopover>
+      </Menu>,
+    );
+
+    const checkboxes = getAllByRole('menuitemcheckbox');
+    fireEvent.click(checkboxes[0]);
+    expect(onCheckedValueChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('should control checked items with checkedValues prop (applied to MenuList)', () => {
+    const { container } = render(
+      <Menu open inline>
+        <MenuTrigger disableButtonEnhancement>
+          <button>MenuTrigger</button>
+        </MenuTrigger>
+
+        <MenuPopover>
+          <MenuList checkedValues={{ test: ['second'] }}>
+            <MenuItemCheckbox id="first" name="test" value="first">
+              First
+            </MenuItemCheckbox>
+            <MenuItemCheckbox id="second" name="test" value="second">
+              Second
+            </MenuItemCheckbox>
+            <MenuItemCheckbox id="third" name="test" value="third">
+              Third
+            </MenuItemCheckbox>
+          </MenuList>
+        </MenuPopover>
+      </Menu>,
+    );
+
+    expect(container.querySelector('#first')?.getAttribute('aria-checked')).toBe('false');
+    expect(container.querySelector('#second')?.getAttribute('aria-checked')).toBe('true');
+    expect(container.querySelector('#third')?.getAttribute('aria-checked')).toBe('false');
+  });
 });