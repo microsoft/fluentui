4a2e2bab02065f4da9f788b5b201763921d61971
ling1726 <lingfangao@hotmail.com>
2023-01-11 13:26:58 +0100

    docs: Improve a11y for Table/DataGrid examples (#26218)
    Fixes #26210
    Fixes #26208
    Fixes #26197
    Fixes #26209

--- packages/react-components/react-table/stories/Table/SingleSelectControlled.stories.tsx
+++ packages/react-components/react-table/stories/Table/SingleSelectControlled.stories.tsx
@@ -166,12 +166,20 @@ export const SingleSelectControlled = () => {
             aria-selected={selected}
             appearance={appearance}
           >
-            <TableSelectionCell checked={selected} type="radio" />
+            <TableSelectionCell checked={selected} type="radio" radioIndicator={{ 'aria-label': 'Select row' }} />
             <TableCell>
               <TableCellLayout media={item.file.icon}>{item.file.label}</TableCellLayout>
             </TableCell>
             <TableCell>
-              <TableCellLayout media={<Avatar badge={{ status: item.author.status }} />}>
+              <TableCellLayout
+                media={
+                  <Avatar
+                    aria-label={item.author.label}
+                    name={item.author.label}
+                    badge={{ status: item.author.status }}
+                  />
+                }
+              >
                 {item.author.label}
               </TableCellLayout>
             </TableCell>
