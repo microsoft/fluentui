8c2b3b9b087fd909d905ae3ca49146a248a42c9e
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-01-11 16:54:34 -0700

    fix(react-infobutton): Change role of PopoverSurface to note and fix API to not require cast (#26300)
    * update types to make the API more friendly for popover
    
    * change files
    
    * api file
    
    * updating types to keep slots
    
    * updating query by dialog to note

--- packages/react-components/react-infobutton/src/components/InfoButton/InfoButton.test.tsx
+++ packages/react-components/react-infobutton/src/components/InfoButton/InfoButton.test.tsx
@@ -4,20 +4,20 @@ import { infoButtonClassNames } from './useInfoButtonStyles';
 import type { RenderResult } from '@testing-library/react';
 
 // testing-library's queryByRole function doesn't look inside portals
-function queryByRoleDialog(result: RenderResult) {
-  const dialogs = result.baseElement.querySelectorAll('[role="dialog"]');
-  if (!dialogs?.length) {
+function queryByRoleNote(result: RenderResult) {
+  const notes = result.baseElement.querySelectorAll('[role="note"]');
+  if (!notes?.length) {
     return null;
   } else {
-    expect(dialogs.length).toBe(1);
-    return dialogs.item(0) as HTMLElement;
+    expect(notes.length).toBe(1);
+    return notes.item(0) as HTMLElement;
   }
 }
 
 const getPopoverSurfaceElement = (result: RenderResult) => {
   // button needs to be clicked otherwise content won't be rendered.
   result.getByRole('button').click();
-  const dialog = queryByRoleDialog(result);
+  const dialog = queryByRoleNote(result);
   expect(dialog).not.toBeNull();
   return dialog!;
 };
