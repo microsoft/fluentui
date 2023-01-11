ffa133f0a331ee22034e3f640fff0282a3b943e8
ling1726 <lingfangao@hotmail.com>
2023-01-11 13:26:58 +0100

    docs: Improve a11y for Table/DataGrid examples (#26218)
    Fixes #26210
    Fixes #26208
    Fixes #26197
    Fixes #26209

--- packages/react-components/react-table/stories/DataGrid/SelectionAppearance.stories.tsx
+++ packages/react-components/react-table/stories/DataGrid/SelectionAppearance.stories.tsx
@@ -113,7 +113,15 @@ export const SelectionAppearance = () => {
         },
         renderCell: item => {
           return (
-            <TableCellLayout media={<Avatar badge={{ status: item.author.status }} />}>
+            <TableCellLayout
+              media={
+                <Avatar
+                  aria-label={item.author.label}
+                  name={item.author.label}
+                  badge={{ status: item.author.status }}
+                />
+              }
+            >
               {item.author.label}
             </TableCellLayout>
           );
@@ -159,7 +167,7 @@ export const SelectionAppearance = () => {
       defaultSelectedItems={defaultSelectedItems}
     >
       <DataGridHeader>
-        <DataGridRow>
+        <DataGridRow selectionCell={{ 'aria-label': 'Select all rows' }}>
           {({ renderHeaderCell }: ColumnDefinition<Item>) => (
             <DataGridHeaderCell>{renderHeaderCell()}</DataGridHeaderCell>
           )}
@@ -167,7 +175,7 @@ export const SelectionAppearance = () => {
       </DataGridHeader>
       <DataGridBody>
         {({ item, rowId }: RowState<Item>) => (
-          <DataGridRow key={rowId}>
+          <DataGridRow key={rowId} selectionCell={{ 'aria-label': 'Select row' }}>
             {({ renderCell }: ColumnDefinition<Item>) => <DataGridCell>{renderCell(item)}</DataGridCell>}
           </DataGridRow>
         )}
