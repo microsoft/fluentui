4f8c169d723821db762bb003e485188556a02b64
Martin Hochel <martinhochel@microsoft.com>
2023-01-11 19:13:31 +0100

    chore: setup typescript 4.7 for web-components package (#26299)
    * chore: setup typescript 4.7 for web-components package
    
    * chore: generate change-file
    
    * fixup! chore: setup typescript 4.7 for web-components package
    
    * fixup! fixup! chore: setup typescript 4.7 for web-components package

--- packages/web-components/.storybook/main.js
+++ packages/web-components/.storybook/main.js
@@ -1,5 +1,7 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 
+const tsBin = require.resolve('typescript');
+
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|mdx)'],
   staticDirs: ['../public'],
@@ -22,16 +24,29 @@ module.exports = {
     },
   ],
   webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.ts$/,
-      use: [
-        {
-          loader: 'ts-loader',
+    config.resolve.extensionAlias = {
+      '.js': ['.ts', '.js'],
+      '.mjs': ['.mts', '.mjs'],
+    };
+    config.module.rules.push(
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+        sideEffects: true,
+        options: {
+          transpileOnly: true,
+          compiler: tsBin,
         },
-      ],
-    });
-    config.resolve.extensions.push('.ts');
-    config.resolve.extensions.push('.js');
+      },
+      // Following config is needed to be able to resolve @storybook packages imported in specified files that don't ship valid ESM
+      // It also enables importing other packages without proper ESM extensions, but that should be avoided !
+      // @see https://webpack.js.org/configuration/module/#resolvefullyspecified
+      {
+        test: /\.storybook\/.+\.m?js/,
+        resolve: { fullySpecified: false },
+      },
+    );
+    config.resolve.extensions.push(...['.ts', '.js']);
     config.plugins.push(
       new CircularDependencyPlugin({
         exclude: /node_modules/,
