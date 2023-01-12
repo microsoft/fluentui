e5f9e8c419030430c20a9ff374c70562412ef3b9
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-01-12 12:26:38 -0700

    chore(react-datepicker): Create Calendar components and DatePicker component (#26325)
    * add Calendar components and DatePicker component
    
    * fix lint error
    
    * missed one

---
+++ packages/react-components/react-datepicker/src/components/CalendarPicker/index.ts
@@ -0,0 +1,5 @@
+export * from './CalendarPicker';
+export * from './CalendarPicker.types';
+export * from './renderCalendarPicker';
+export * from './useCalendarPicker';
+export * from './useCalendarPickerStyles';
---
+++ packages/react-components/react-datepicker/stories/CalendarDay/CalendarDayDefault.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { CalendarDay, CalendarDayProps } from '@fluentui/react-datepicker';
+
+export const Default = (props: Partial<CalendarDayProps>) => <CalendarDay {...props} />;
