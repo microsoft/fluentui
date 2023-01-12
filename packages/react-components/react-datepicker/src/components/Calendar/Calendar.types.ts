62dd9d21ffe11fb611053474f629aa61345fab42
Esteban Munoz <esteban.230@hotmail.com>
2023-01-12 10:26:22 -0700

    missed one

--- packages/react-components/react-datepicker/src/components/Calendar/Calendar.types.ts
+++ packages/react-components/react-datepicker/src/components/Calendar/Calendar.types.ts
@@ -13,5 +13,4 @@ export type CalendarProps = ComponentProps<CalendarSlots> & {};
  * State used in rendering Calendar
  */
 export type CalendarState = ComponentState<CalendarSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from CalendarProps.
 // & Required<Pick<CalendarProps, 'propName'>>
