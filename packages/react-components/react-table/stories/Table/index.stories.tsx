eed4cadc848e02bd21e66c45cf733e22b6a2eab9
ling1726 <lingfangao@hotmail.com>
2023-01-17 13:12:30 +0100

    fix(TableCellActions): Background should match rown on hover and active (#26373)
    * fix(TableCellActions): Background should match rown on hover and active
    
    * changefile
    
    * remove unnecessary styles, inherit background color

--- packages/react-components/react-table/stories/Table/index.stories.tsx
+++ packages/react-components/react-table/stories/Table/index.stories.tsx
@@ -25,6 +25,7 @@ export { SingleSelect } from './SingleSelect.stories';
 export { MultipleSelectControlled } from './MultipleSelectControlled.stories';
 export { SingleSelectControlled } from './SingleSelectControlled.stories';
 export { SubtleSelection } from './SubtleSelection.stories';
+export { SelectionWithCellActions } from './SelectionWithCellActions.stories';
 export { Virtualization } from './Virtualization.stories';
 export { DataGrid } from './DataGrid.stories';
 
