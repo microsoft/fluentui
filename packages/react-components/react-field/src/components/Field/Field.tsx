51b4dcaf8aed5ffeb3df90acf45a1211562ebf1c
Ben Howell <behowell@microsoft.com>
2023-01-18 16:15:13 -0800

    Organize imports

--- packages/react-components/react-field/src/components/Field/Field.tsx
+++ packages/react-components/react-field/src/components/Field/Field.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import type { FieldProps } from './Field.types';
 import { renderField_unstable } from './renderField';
 import { useField_unstable } from './useField';
 import { useFieldStyles_unstable } from './useFieldStyles';
-import type { FieldProps } from './Field.types';
-import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 export const Field: ForwardRefComponent<FieldProps> = React.forwardRef((props, ref) => {
   const state = useField_unstable(props, ref);
