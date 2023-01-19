ed1fd9e907ccf177170b79e65f6a9a488cc0d2e1
Marcos Moura <marcosvmmoura@gmail.com>
2023-01-19 17:23:05 +0100

    fix: move token generation outside the component for better perf

--- packages/react-components/react-theme/stories/Theme/typography/ThemeTypography.stories.tsx
+++ packages/react-components/react-theme/stories/Theme/typography/ThemeTypography.stories.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { makeStyles, Subtitle2Stronger, Text, typographyStyles } from '@fluentui/react-components';
 import type { TypographyStyles } from '@fluentui/react-components';
 
+type TypographyTokens = [token: keyof TypographyStyles, tokenName: string, values: string[]][];
+
 const useStyles = makeStyles({
   container: {
     rowGap: '24px',
@@ -14,33 +16,34 @@ const useStyles = makeStyles({
   ...typographyStyles,
 });
 
+const tokenOrder: (keyof TypographyStyles)[] = [
+  'caption2',
+  'caption2Strong',
+  'caption1',
+  'caption1Strong',
+  'caption1Stronger',
+  'body1',
+  'body1Strong',
+  'body1Stronger',
+  'body2',
+  'subtitle2',
+  'subtitle2Stronger',
+  'subtitle1',
+  'title3',
+  'title2',
+  'title1',
+  'largeTitle',
+  'display',
+];
+
+const tokens: TypographyTokens = tokenOrder.map(token => [
+  token,
+  token.replace(/([A-Z\d])/g, ' $1').replace(/^(.)/, firstChar => firstChar.toUpperCase()),
+  Object.values(typographyStyles[token]).map(v => v.replace(/var\(--(.+)\)/, '$1')),
+]);
+
 export const Typography = () => {
   const styles = useStyles();
-  const tokenOrder: (keyof TypographyStyles)[] = [
-    'caption2',
-    'caption2Strong',
-    'caption1',
-    'caption1Strong',
-    'caption1Stronger',
-    'body1',
-    'body1Strong',
-    'body1Stronger',
-    'body2',
-    'subtitle2',
-    'subtitle2Stronger',
-    'subtitle1',
-    'title3',
-    'title2',
-    'title1',
-    'largeTitle',
-    'display',
-  ];
-
-  const tokens: [keyof TypographyStyles, string, string[]][] = tokenOrder.map(token => [
-    token,
-    token.replace(/([A-Z\d])/g, ' $1').replace(/^(.)/, firstChar => firstChar.toUpperCase()),
-    Object.values(typographyStyles[token]).map(v => v.replace(/var\(--(.+)\)/, '$1')),
-  ]);
 
   return (
     <div className={styles.container}>
