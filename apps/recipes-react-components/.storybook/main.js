18caef3344f989d552ae3f47f6afdffe11414a3b
Esteban Munoz <esteban.230@hotmail.com>
2023-01-19 12:41:48 -0700

    adding requested changeS

--- apps/recipes-react-components/.storybook/main.js
+++ apps/recipes-react-components/.storybook/main.js
@@ -1,51 +1,6 @@
-const path = require('path');
-const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const rootMain = require('../../../.storybook/main');
 
-module.exports = /** @type {import('../../../.storybook/main').StorybookBaseConfig} */ ({
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs/preset',
-    'storybook-addon-performance',
-    '@fluentui/react-storybook-addon',
-  ],
-
+module.exports = /** @type {Omit<import('../../../.storybook/main'), 'typescript'|'babel'>} */ ({
+  ...rootMain,
   stories: ['../src/**/*.stories.mdx'],
-  core: {
-    builder: 'webpack5',
-    disableTelemetry: true,
-  },
-  babel: {},
-  typescript: {
-    // disable react-docgen-typescript (totally not needed here, slows things down a lot)
-    reactDocgen: false,
-  },
-  webpackFinal: config => {
-    const tsPaths = new TsconfigPathsPlugin({
-      configFile: path.resolve(__dirname, '../../../tsconfig.base.json'),
-    });
-
-    if (config.resolve) {
-      config.resolve.plugins ? config.resolve.plugins.push(tsPaths) : (config.resolve.plugins = [tsPaths]);
-    }
-
-    if (config.module) {
-      config.module.rules?.unshift({
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: '@griffel/webpack-loader',
-            options: {
-              babelOptions: {
-                presets: ['@babel/preset-typescript'],
-              },
-            },
-          },
-        ],
-      });
-    }
-
-    return config;
-  },
 });
