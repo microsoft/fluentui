25f6b220b80bfc55da76ed9a0f5ba8d335c9a9c5
GeoffCoxMSFT <gcox@microsoft.com>
2023-01-20 11:26:59 -0800

    Update theme shim stories

--- packages/react-migration-v8-v9/src/stories/ThemeShim/createV8Theme/index.stories.tsx
+++ packages/react-migration-v8-v9/src/stories/ThemeShim/createV8Theme/index.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Theme as ThemeV8 } from '@fluentui/react';
+import { createTheme, Theme as ThemeV8, ThemeProvider, initializeIcons } from '@fluentui/react';
 import {
   Button,
   makeStyles,
@@ -12,11 +12,16 @@ import {
   Theme as ThemeV9,
   createLightTheme,
   createDarkTheme,
-  Checkbox,
+  tokens,
+  FluentProvider,
 } from '@fluentui/react-components';
 import { brandWeb, createV8Theme } from '../../../components/Theme/index';
 import descriptionMd from './Description.md';
 import { Meta } from '@storybook/react';
+import { ThemePreviewV8 } from '../ThemePreviewV8';
+import { ThemePreviewV9 } from '../ThemePreviewV9';
+
+initializeIcons();
 
 const useStyles = makeStyles({
   root: {
@@ -26,6 +31,9 @@ const useStyles = makeStyles({
     gridRowGap: '10px',
     justifyItems: 'start',
     ...shorthands.padding('5px'),
+  },
+  instructions: {
+    color: tokens.colorNeutralForeground2,
   },
   editor: {
     width: '400px',
@@ -38,6 +46,9 @@ const useStyles = makeStyles({
     [`& > *`]: {
       ...shorthands.margin('5px'),
     },
+  },
+  error: {
+    color: 'red',
   },
   result: {
     display: 'grid',
@@ -54,37 +65,24 @@ const useStyles = makeStyles({
   },
 });
 
-export const Default = () => {
+export const Playground = () => {
   const styles = useStyles();
 
   const defaultBrandVariantText = JSON.stringify(brandWeb, null, 4);
-  const defaultV9ThemeText = JSON.stringify(webLightTheme, null, 4);
-
-  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
   const [brandVariantText, setBrandVariantText] = React.useState(defaultBrandVariantText);
+  const defaultV9Theme = webLightTheme;
+  const defaultV9ThemeText = JSON.stringify(defaultV9Theme, null, 4);
+  const [v9Theme, setV9Theme] = React.useState<ThemeV9>(defaultV9Theme);
   const [v9ThemeText, setV9ThemeText] = React.useState(defaultV9ThemeText);
-  const [v8Theme, setV8Theme] = React.useState<ThemeV8>();
+  const [createV9ThemeError, setCreateV9ThemeError] = React.useState('');
+  const [previewV9ThemeError, setPreviewV9ThemeError] = React.useState('');
 
-  const v8Effects = React.useMemo(() => ((v8Theme ? v8Theme.effects : {}) as unknown) as Record<string, string>, [
-    v8Theme,
-  ]);
-
-  const v8Fonts = React.useMemo(() => ((v8Theme ? v8Theme.fonts : {}) as unknown) as Record<string, string>, [v8Theme]);
-
-  const v8Spacing = React.useMemo(() => ((v8Theme ? v8Theme.spacing : {}) as unknown) as Record<string, string>, [
-    v8Theme,
-  ]);
-
-  const v8Palette = React.useMemo(() => ((v8Theme ? v8Theme.palette : {}) as unknown) as Record<string, string>, [
-    v8Theme,
-  ]);
-
-  const v8SemanticColors = React.useMemo(
-    () => ((v8Theme ? v8Theme.semanticColors : {}) as unknown) as Record<string, string>,
-    [v8Theme],
-  );
-
-  const [message, setMessage] = React.useState('');
+  const defaultV8Theme = createTheme();
+  const defaultV8ThemeText = JSON.stringify(defaultV8Theme, null, 4);
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+  const [v8Theme, setV8Theme] = React.useState<ThemeV8>(defaultV8Theme);
+  const [v8ThemeText, setV8ThemeText] = React.useState(defaultV8ThemeText);
+  const [createV8ThemeError, setCreateV8ThemeError] = React.useState('');
 
   const onBrandVariantTextChange: TextareaProps['onChange'] = (_, data) => {
     setBrandVariantText(data.value);
@@ -98,50 +96,99 @@ export const Default = () => {
     setBrandVariantText(defaultBrandVariantText);
   };
 
-  const onResetTheme = () => {
+  const onResetV9Theme = () => {
+    setV9Theme(defaultV9Theme);
     setV9ThemeText(defaultV9ThemeText);
+    setCreateV9ThemeError('');
+    setPreviewV9ThemeError('');
   };
 
-  const onSetLightBrandTheme = React.useCallback(() => {
-    const brandVariants = JSON.parse(brandVariantText) as BrandVariants;
-    const v9Theme = createLightTheme(brandVariants);
-    setV9ThemeText(JSON.stringify(v9Theme, null, 4));
-    setIsDarkTheme(false);
-  }, [brandVariantText]);
-
-  const onSetDarkBrandTheme = React.useCallback(() => {
-    const brandVariants = JSON.parse(brandVariantText) as BrandVariants;
-    const v9Theme = createDarkTheme(brandVariants);
-    setV9ThemeText(JSON.stringify(v9Theme, null, 4));
-    setIsDarkTheme(true);
-  }, [brandVariantText]);
-
-  const onCreateTheme = React.useCallback(() => {
+  const onCreateLightV9Theme = React.useCallback(() => {
     try {
+      setCreateV9ThemeError('');
       const brandVariants = JSON.parse(brandVariantText) as BrandVariants;
-      const v9Theme = JSON.parse(v9ThemeText) as ThemeV9;
-      const newV8Theme = createV8Theme(brandVariants, v9Theme, isDarkTheme);
-      setV8Theme(newV8Theme);
+      const newV9Theme = createLightTheme(brandVariants);
+      setV9Theme(newV9Theme);
+      setV9ThemeText(JSON.stringify(newV9Theme, null, 4));
+      setIsDarkTheme(false);
     } catch (e) {
-      setMessage((e as Error).message);
+      setCreateV9ThemeError((e as Error).message);
+    }
+  }, [brandVariantText]);
+
+  const onCreateDarkv9Theme = React.useCallback(() => {
+    try {
+      setCreateV9ThemeError('');
+      const brandVariants = JSON.parse(brandVariantText) as BrandVariants;
+      const newV9Theme = createDarkTheme(brandVariants);
+      setV9Theme(newV9Theme);
+      setV9ThemeText(JSON.stringify(newV9Theme, null, 4));
+      setIsDarkTheme(true);
+    } catch (e) {
+      setCreateV9ThemeError((e as Error).message);
+    }
+  }, [brandVariantText]);
+
+  const onPreviewV9Theme = React.useCallback(() => {
+    try {
+      setPreviewV9ThemeError('');
+      const newV9Theme = JSON.parse(v9ThemeText) as ThemeV9;
+      setV9Theme(newV9Theme);
+    } catch (e) {
+      setPreviewV9ThemeError((e as Error).message);
+    }
+  }, [v9ThemeText]);
+
+  const onCreateV8Theme = React.useCallback(() => {
+    try {
+      setCreateV8ThemeError('');
+      const newV9Theme = JSON.parse(v9ThemeText) as ThemeV9;
+      setV9Theme(newV9Theme);
+      const brandVariants = JSON.parse(brandVariantText) as BrandVariants;
+      const newV8Theme = createV8Theme(brandVariants, newV9Theme, isDarkTheme);
+      setV8Theme(newV8Theme);
+      setV8ThemeText(JSON.stringify(newV8Theme, null, 4));
+    } catch (e) {
+      setCreateV8ThemeError((e as Error).message);
     }
   }, [brandVariantText, isDarkTheme, v9ThemeText]);
 
+  const onResetV8Theme = () => {
+    setV8Theme(defaultV8Theme);
+    setV8ThemeText(defaultV8ThemeText);
+    setCreateV8ThemeError('');
+  };
+
   return (
     <div className={styles.root}>
-      <h2>Brand</h2>
+      <h2>1. Define Brand Variants</h2>
+      <div className={styles.instructions}>
+        <p>Adjust the brand variants (aka brand ramp). You can reset to the default brand colors.</p>
+        <p>
+          If you want to generate entirely different brand variants or v9 theme, use the&nbsp;
+          <a href="https://aka.ms/themedesigner-v9">v9 Theme Designer</a>.
+        </p>
+      </div>
+      <div className={styles.actions}>
+        <Button onClick={onResetBrand}>Reset</Button>
+      </div>
       <Textarea
         resize="both"
         textarea={{ className: styles.editor }}
         value={brandVariantText}
         onChange={onBrandVariantTextChange}
       />
-      <div className={styles.actions}>
-        <Button onClick={onResetBrand}>Reset</Button>
-        <Button onClick={onSetLightBrandTheme}>Set Light v9 Theme from Brand</Button>
-        <Button onClick={onSetDarkBrandTheme}>Set Dark v9 Theme from Brand</Button>
+      <h2>2. Create a v9 Theme</h2>
+      <div className={styles.instructions}>
+        Create a light or dark v9 theme from the brand variants. You can adjust the theme and click preview anytime.
+        them.
       </div>
-      <h2>v9 Theme</h2>
+      <div className={styles.actions}>
+        <Button onClick={onResetV9Theme}>Reset</Button>
+        <Button onClick={onCreateLightV9Theme}>Create Light Theme</Button>
+        <Button onClick={onCreateDarkv9Theme}>Create Dark Theme</Button>
+      </div>
+      <div className={styles.error}>{createV9ThemeError}</div>
       <Textarea
         resize="both"
         textarea={{ className: styles.editor }}
@@ -149,77 +196,30 @@ export const Default = () => {
         onChange={onV9ThemeTextChange}
       />
       <div className={styles.actions}>
-        <Button onClick={onResetTheme}>Reset</Button>
-        <Button onClick={onCreateTheme}>Create</Button>
-        <Checkbox
-          label="Dark Theme"
-          checked={isDarkTheme}
-          onChange={(_, data) => setIsDarkTheme(data.checked === true)}
-        />
+        <Button onClick={onPreviewV9Theme}>Preview</Button>
       </div>
-      <h2>v8 Theme</h2>
-      <div>{message}</div>
-      <h3>effects</h3>
-      <div className={styles.result}>
-        {Object.keys(v8Effects)
-          .sort()
-          .map(key => (
-            <>
-              <div>{key}</div>
-              <div>{v8Effects[key]}</div>
-            </>
-          ))}
+      <div className={styles.error}>{previewV9ThemeError}</div>
+      <FluentProvider theme={v9Theme}>
+        <ThemePreviewV9 />
+      </FluentProvider>
+      <h2>3. Create a v8 Theme</h2>
+      <div className={styles.instructions}>
+        Create a v8 theme from the v9 theme. You can copy the result to use it in your application.
       </div>
-      <h3>fonts</h3>
-      <div className={styles.result}>
-        {Object.keys(v8Fonts)
-          .sort()
-          .map(key => (
-            <>
-              <div>{key}</div>
-              <div>{JSON.stringify(v8Fonts[key])}</div>
-            </>
-          ))}
+      <div className={styles.actions}>
+        <Button onClick={onResetV8Theme}>Reset</Button>
+        <Button onClick={onCreateV8Theme}>Create</Button>
       </div>
-      <h3>palette</h3>
-      <div className={styles.result}>
-        {Object.keys(v8Palette)
-          .sort()
-          .map(key => (
-            <>
-              <div>{key}</div>
-              <div>
-                {v8Palette[key]}&nbsp;
-                <span className={styles.colorBlock} style={{ backgroundColor: v8Palette[key] }} />
-              </div>
-            </>
-          ))}
+      <div className={styles.error}>{createV8ThemeError}</div>
+      <div>
+        <ThemeProvider theme={v8Theme}>
+          <ThemePreviewV8
+            backgroundColor={v8Theme?.semanticColors.bodyBackground || 'white'}
+            textColor={v8Theme?.semanticColors.bodyText || 'black'}
+          />
+        </ThemeProvider>
       </div>
-      <h3>semanticColors</h3>
-      <div className={styles.result}>
-        {Object.keys(v8SemanticColors)
-          .sort()
-          .map(key => (
-            <>
-              <div>{key}</div>
-              <div>
-                {v8SemanticColors[key]}&nbsp;
-                <span className={styles.colorBlock} style={{ backgroundColor: v8SemanticColors[key] }} />
-              </div>
-            </>
-          ))}
-      </div>
-      <h3>spacing</h3>
-      <div className={styles.result}>
-        {Object.keys(v8Spacing)
-          .sort()
-          .map(key => (
-            <>
-              <div>{key}</div>
-              <div>{v8Spacing[key]}</div>
-            </>
-          ))}
-      </div>
+      <Textarea resize="both" readOnly textarea={{ className: styles.editor }} value={v8ThemeText} />
     </div>
   );
 };
