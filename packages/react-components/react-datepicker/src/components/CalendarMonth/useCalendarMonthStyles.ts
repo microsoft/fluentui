4e8e21970639bc322699e1eb5b6832afe6493e65
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-01-24 13:22:57 -0700

    chore(react-datepicker): Copy over files to package from personal branch (#26472)
    * copy over files from local branch
    
    * copy over stories and src
    
    * updating deps
    
    * adding missing dep
    
    * adding missing dep
    
    * fix typecheck errors

--- packages/react-components/react-datepicker/src/components/CalendarMonth/useCalendarMonthStyles.ts
+++ packages/react-components/react-datepicker/src/components/CalendarMonth/useCalendarMonthStyles.ts
@@ -1,33 +1,15 @@
-import { makeStyles, mergeClasses } from '@griffel/react';
-import type { CalendarMonthSlots, CalendarMonthState } from './CalendarMonth.types';
+import { useCalendarPickerStyles_unstable } from '../CalendarPicker/useCalendarPickerStyles';
 import type { SlotClassNames } from '@fluentui/react-utilities';
+import type { CalendarMonthSlots, CalendarMonthStyleProps, CalendarMonthStyles } from './CalendarMonth.types';
 
 export const calendarMonthClassNames: SlotClassNames<CalendarMonthSlots> = {
   root: 'fui-CalendarMonth',
-  // TODO: add class names for all slots on CalendarMonthSlots.
-  // Should be of the form `<slotName>: 'fui-CalendarMonth__<slotName>`
 };
-
-/**
- * Styles for the root slot
- */
-const useStyles = makeStyles({
-  root: {
-    // TODO Add default styles for the root element
-  },
-
-  // TODO add additional classes for different states and/or slots
-});
-
 /**
  * Apply styling to the CalendarMonth slots based on the state
  */
-export const useCalendarMonthStyles_unstable = (state: CalendarMonthState): CalendarMonthState => {
-  const styles = useStyles();
-  state.root.className = mergeClasses(calendarMonthClassNames.root, styles.root, state.root.className);
-
-  // TODO Add class names to slots, for example:
-  // state.mySlot.className = mergeClasses(styles.mySlot, state.mySlot.className);
-
-  return state;
+export const useCalendarMonthStyles_unstable = (
+  props: CalendarMonthStyleProps,
+): Record<keyof CalendarMonthStyles, string> => {
+  return useCalendarPickerStyles_unstable(props);
 };
