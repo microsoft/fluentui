b2c09944b5d60625c147c6c07b1ad626db2a2f58
Oleksandr Fediashov <olfedias@microsoft.com>
2023-01-24 16:47:24 +0100

    fix: update elementContains() to handle circular references

--- packages/react-components/react-portal/src/virtualParent/getParent.ts
+++ packages/react-components/react-portal/src/virtualParent/getParent.ts
@@ -1,9 +1,28 @@
-import { getVirtualParent } from './getVirtualParent';
+import { isVirtualElement } from './isVirtualElement';
+
+/**
+ * Gets the virtual parent given the child element, if it exists.
+ */
+function getVirtualParent(child: Node): Node | null {
+  return isVirtualElement(child) ? child._virtual.parent || null : null;
+}
 
 /**
  * Gets the element which is the parent of a given element.
  * This method prefers the virtual parent over real DOM parent when present.
  */
-export function getParent(child: Node | null): Node | null {
-  return ((child && getVirtualParent(child)) || child?.parentNode) ?? null;
+export function getParent(child: Node | null, skipVirtual: boolean = false): Node | null {
+  if (!child) {
+    return null;
+  }
+
+  if (!skipVirtual) {
+    const virtualParent = getVirtualParent(child);
+
+    if (virtualParent) {
+      return virtualParent;
+    }
+  }
+
+  return child?.parentNode || null;
 }
