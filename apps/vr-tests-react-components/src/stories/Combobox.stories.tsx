0e222da0e5831279393939de322a9d06298ed904
Sarah Higley <sarah.higley@microsoft.com>
2023-01-25 15:45:07 -0800

    add padding to force listbox into screenshot

--- apps/vr-tests-react-components/src/stories/Combobox.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Combobox.stories.tsx
@@ -123,47 +123,57 @@ storiesOf('Combobox Converged', module)
     </StoryWright>
   ))
   .addStory('Open', () => (
-    <Combobox open>
-      <Option>Red</Option>
-      <Option>Green</Option>
-      <Option>Blue</Option>
-    </Combobox>
-  ))
-  .addStory('Open with inlinePopup', () => (
-    <Combobox open inlinePopup>
-      <Option>Red</Option>
-      <Option>Green</Option>
-      <Option>Blue</Option>
-    </Combobox>
-  ))
-  .addStory('Option with long content', () => (
-    <Combobox open>
-      <Option>
-        Blue indigo periwinkle azure sapphire ultramarine teal cerulean navy turquoise cyan cobalt blue indigo
-        periwinkle azure sapphire ultramarine teal cerulean navy turquoise cyan cobalt
-      </Option>
-      <Option>Red</Option>
-      <Option>Green</Option>
-    </Combobox>
-  ))
-  .addStory('Disabled option', () => (
-    <Combobox open>
-      <Option disabled>Red</Option>
-      <Option>Green</Option>
-      <Option>Blue</Option>
-    </Combobox>
-  ))
-  .addStory('Open with grouped options', () => (
-    <Combobox open>
-      <OptionGroup label="Colors">
+    <div style={{ paddingBottom: '120px' }}>
+      <Combobox open>
         <Option>Red</Option>
         <Option>Green</Option>
         <Option>Blue</Option>
-      </OptionGroup>
-      <OptionGroup label="Shapes">
-        <Option>Circle</Option>
-        <Option>Square</Option>
-        <Option>Oval</Option>
-      </OptionGroup>
-    </Combobox>
+      </Combobox>
+    </div>
+  ))
+  .addStory('Open with inlinePopup', () => (
+    <div style={{ paddingBottom: '120px' }}>
+      <Combobox open inlinePopup>
+        <Option>Red</Option>
+        <Option>Green</Option>
+        <Option>Blue</Option>
+      </Combobox>
+    </div>
+  ))
+  .addStory('Option with long content', () => (
+    <div style={{ paddingBottom: '240px' }}>
+      <Combobox open>
+        <Option>
+          Blue indigo periwinkle azure sapphire ultramarine teal cerulean navy turquoise cyan cobalt blue indigo
+          periwinkle azure sapphire ultramarine teal cerulean navy turquoise cyan cobalt
+        </Option>
+        <Option>Red</Option>
+        <Option>Green</Option>
+      </Combobox>
+    </div>
+  ))
+  .addStory('Disabled option', () => (
+    <div style={{ paddingBottom: '120px' }}>
+      <Combobox open>
+        <Option disabled>Red</Option>
+        <Option>Green</Option>
+        <Option>Blue</Option>
+      </Combobox>
+    </div>
+  ))
+  .addStory('Open with grouped options', () => (
+    <div style={{ paddingBottom: '300px' }}>
+      <Combobox open>
+        <OptionGroup label="Colors">
+          <Option>Red</Option>
+          <Option>Green</Option>
+          <Option>Blue</Option>
+        </OptionGroup>
+        <OptionGroup label="Shapes">
+          <Option>Circle</Option>
+          <Option>Square</Option>
+          <Option>Oval</Option>
+        </OptionGroup>
+      </Combobox>
+    </div>
   ));
