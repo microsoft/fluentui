13ab5ff3916ec7642086d109e4e5f8c7c9129cd0
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-01-25 12:32:39 +0000

    feat(react-tree): starts using ButtonContext to modify default button size

--- packages/react-components/react-tree/src/components/TreeItem/useTreeItemContextValues.ts
+++ packages/react-components/react-tree/src/components/TreeItem/useTreeItemContextValues.ts
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import { TreeItemContextValue } from '../../contexts';
+import type { TreeItemContextValue } from '../../contexts';
 import type { TreeItemContextValues, TreeItemState } from './TreeItem.types';
+import type { ButtonContextValue } from '@fluentui/react-button';
 
 export function useTreeItemContextValues_unstable(state: TreeItemState): TreeItemContextValues {
-  const { isActionsVisible } = state;
+  const { isActionsVisible, buttonSize } = state;
 
-  const treeItem: TreeItemContextValue = React.useMemo(
-    () => ({
-      isActionsVisible,
-    }),
-    [isActionsVisible],
-  );
+  const treeItem = React.useMemo<TreeItemContextValue>(() => ({ isActionsVisible }), [isActionsVisible]);
+  const button = React.useMemo<ButtonContextValue>(() => ({ size: buttonSize }), [buttonSize]);
 
-  return { treeItem };
+  return { treeItem, button };
 }
