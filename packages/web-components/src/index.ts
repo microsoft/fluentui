08ede9b5c994144c3514156f6bc29509903ef595
Miroslav Stastny <miroslav.stastny@microsoft.com>
2023-01-25 17:17:34 +0100

    feat(web-components): export theme (#26500)
    * feat: export theme
    
    * change file

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -2,3 +2,5 @@ export * from './badge/index.js';
 export * from './counter-badge/index.js';
 export * from './progress-bar/index.js';
 export * from './text/index.js';
+
+export * from './theme/index.js';
