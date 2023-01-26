d783421eb54f373dc2a4e429823171e8fc04642a
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-01-26 11:07:11 -0300

    Update packages/react-components/react-tree/src/components/TreeItem/useTreeItemContextValues.ts
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-tree/src/components/TreeItem/useTreeItemContextValues.ts
+++ packages/react-components/react-tree/src/components/TreeItem/useTreeItemContextValues.ts
@@ -4,7 +4,7 @@ import type { TreeItemContextValue } from '../../contexts';
 import type { TreeItemContextValues, TreeItemState } from './TreeItem.types';
 
 export function useTreeItemContextValues_unstable(state: TreeItemState): TreeItemContextValues {
-  const { isActionsVisible, buttonSize } = state;
+  const { buttonSize, isActionsVisible } = state;
 
   const treeItem = React.useMemo<TreeItemContextValue>(() => ({ isActionsVisible }), [isActionsVisible]);
   const button = React.useMemo<ButtonContextValue>(() => ({ size: buttonSize }), [buttonSize]);
