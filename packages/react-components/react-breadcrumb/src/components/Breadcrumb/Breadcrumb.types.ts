3a21d173fe7ba2ec0baa6abfe2b4ba5d0cde317b
Valentyna <vkozlova@microsoft.com>
2023-01-27 18:31:33 +0100

    Fixed lint errors and warnings

--- packages/react-components/react-breadcrumb/src/components/Breadcrumb/Breadcrumb.types.ts
+++ packages/react-components/react-breadcrumb/src/components/Breadcrumb/Breadcrumb.types.ts
@@ -13,5 +13,3 @@ export type BreadcrumbProps = ComponentProps<BreadcrumbSlots> & {};
  * State used in rendering Breadcrumb
  */
 export type BreadcrumbState = ComponentState<BreadcrumbSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from BreadcrumbProps.
-// & Required<Pick<BreadcrumbProps, 'propName'>>
--- packages/react-components/react-breadcrumb/src/index.ts
+++ packages/react-components/react-breadcrumb/src/index.ts
@@ -1,3 +1,8 @@
-// TODO: replace with real exports
-export {};
-export * from './Breadcrumb';
+export {
+  Breadcrumb,
+  renderBreadcrumb_unstable,
+  useBreadcrumb_unstable,
+  useBreadcrumbStyles_unstable,
+  breadcrumbClassNames,
+} from './Breadcrumb';
+export type { BreadcrumbSlots, BreadcrumbProps, BreadcrumbState } from './Breadcrumb';
