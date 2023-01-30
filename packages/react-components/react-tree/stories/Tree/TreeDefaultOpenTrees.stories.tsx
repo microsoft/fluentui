3c3a1a095511432e5c80a3d9ea8acf083818f287
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-01-30 22:00:01 +0000

    chore(react-tree): adds TreeItemLayout to all stories

--- packages/react-components/react-tree/stories/Tree/TreeDefaultOpenTrees.stories.tsx
+++ packages/react-components/react-tree/stories/Tree/TreeDefaultOpenTrees.stories.tsx
@@ -1,24 +1,38 @@
 import * as React from 'react';
-import { Tree, TreeItem } from '@fluentui/react-tree';
+import { Tree, TreeItem, TreeItemLayout } from '@fluentui/react-tree';
 
 export const DefaultOpenTrees = () => {
   const defaultOpenTrees = ['default-subtree-1', 'default-subtree-2', 'default-subtree-2-1'];
 
   return (
     <Tree aria-label="Tree" defaultOpenItems={defaultOpenTrees}>
-      <TreeItem aria-owns="default-subtree-1">level 1, item 1</TreeItem>
-      <Tree id="default-subtree-1">
-        <TreeItem>level 2, item 1</TreeItem>
-        <TreeItem>level 2, item 2</TreeItem>
-        <TreeItem>level 2, item 3</TreeItem>
-      </Tree>
-      <TreeItem aria-owns="default-subtree-2">level 1, item 2</TreeItem>
-      <Tree id="default-subtree-2">
-        <TreeItem aria-owns="default-subtree-2-1">level 2, item 1</TreeItem>
-        <Tree id="default-subtree-2-1">
-          <TreeItem>level 3, item 1</TreeItem>
+      <TreeItem>
+        <TreeItemLayout>level 1, item 1</TreeItemLayout>
+        <Tree>
+          <TreeItem>
+            <TreeItemLayout>level 2, item 1</TreeItemLayout>
+          </TreeItem>
+          <TreeItem>
+            <TreeItemLayout>level 2, item 2</TreeItemLayout>
+          </TreeItem>
+          <TreeItem>
+            <TreeItemLayout>level 2, item 3</TreeItemLayout>
+          </TreeItem>
         </Tree>
-      </Tree>
+      </TreeItem>
+      <TreeItem>
+        <TreeItemLayout>level 1, item 2</TreeItemLayout>
+        <Tree>
+          <TreeItem>
+            <TreeItemLayout>level 2, item 1</TreeItemLayout>
+            <Tree>
+              <TreeItem>
+                <TreeItemLayout>level 3, item 1</TreeItemLayout>
+              </TreeItem>
+            </Tree>
+          </TreeItem>
+        </Tree>
+      </TreeItem>
     </Tree>
   );
 };
