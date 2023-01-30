d40e97063e3b080f4590253c34600cb7da3896e6
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-01-30 16:01:08 +0100

    type

--- packages/react-components/react-table/src/hooks/useMeasureElement.ts
+++ packages/react-components/react-table/src/hooks/useMeasureElement.ts
@@ -8,23 +8,22 @@ import { useFluent_unstable as useFluent } from '@fluentui/react-shared-contexts
  * `width` - current element width (0 by default),
  * `measureElementRef` - a ref function to be passed as `ref` to the element you want to measure
  */
-export function useMeasureElement() {
+export function useMeasureElement<TElement extends HTMLElement = HTMLElement>() {
   const [width, setWidth] = React.useState(0);
   const container = React.useRef<HTMLElement | undefined>(undefined);
 
-  const fluent = useFluent();
-  const { targetDocument } = fluent;
+  const { targetDocument } = useFluent();
 
+  // the handler for resize observer
   const handleResize = React.useCallback(() => {
-    const w = container.current?.getBoundingClientRect().width;
-    setWidth(w || 0);
+    const containerWidth = container.current?.getBoundingClientRect().width;
+    setWidth(containerWidth || 0);
   }, []);
 
   // Keep the reference of ResizeObserver in the state, as it should live through renders
   const [resizeObserver] = React.useState(canUseDOM() ? new ResizeObserver(handleResize) : undefined);
-
   const measureElementRef = React.useCallback(
-    (el: HTMLElement | null) => {
+    (el: TElement | null) => {
       if (!targetDocument || !resizeObserver) {
         return;
       }
@@ -33,11 +32,10 @@ export function useMeasureElement() {
       if (container.current) {
         resizeObserver.unobserve(container.current);
         container.current.remove();
-        container.current = undefined;
       }
 
       if (el) {
-        container.current = targetDocument?.createElement('div');
+        container.current = targetDocument.createElement('div');
         el.insertAdjacentElement('beforebegin', container.current);
         resizeObserver.observe(container.current);
         handleResize();
@@ -45,6 +43,10 @@ export function useMeasureElement() {
     },
     [targetDocument, resizeObserver, handleResize],
   );
+
+  React.useEffect(() => {
+    return () => resizeObserver?.disconnect();
+  }, [resizeObserver]);
 
   return { width, measureElementRef };
 }
