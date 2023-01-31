6b6af66e5edcbf6ef6c246f897575bbbc6940226
Brian Brady <brianbrady@microsoft.com>
2023-01-31 11:16:56 -0800

    adds docs

--- packages/web-components/src/accordion/accordion-item/accordion-item.definition.ts
+++ packages/web-components/src/accordion/accordion-item/accordion-item.definition.ts
@@ -3,6 +3,15 @@ import { AccordionItem } from './accordion-item.js';
 import { styles } from './accordion-item.styles.js';
 import { template } from './accordion-item.template.js';
 
+/**
+ * The Fluent AccordionItem Element. Implements {@link @microsoft/fast-foundation#AccordionItem },
+ * {@link @microsoft/fast-foundation#accordionItemTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<fluent-accordion-item\>
+ */
 export const definition = AccordionItem.compose({
   name: `${FluentDesignSystem.prefix}-accordion-item`,
   template,
--- packages/web-components/src/accordion/accordion/accordion.definition.ts
+++ packages/web-components/src/accordion/accordion/accordion.definition.ts
@@ -3,6 +3,15 @@ import { Accordion } from './accordion.js';
 import { styles } from './accordion.styles.js';
 import { template } from './accordion.template.js';
 
+/**
+ * The Fluent Accordion Element. Implements {@link @microsoft/fast-foundation#Accordion },
+ * {@link @microsoft/fast-foundation#accordionTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<fluent-accordion\>
+ */
 export const definition = Accordion.compose({
   name: `${FluentDesignSystem.prefix}-accordion`,
   template,
