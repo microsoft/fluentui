d67f1578965b99d3f6f82bbe54fc433bbeecd87a
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-02-01 14:24:54 +0100

    tablecell header

--- packages/react-components/react-table/src/components/TableHeaderCell/TableHeaderCell.test.tsx
+++ packages/react-components/react-table/src/components/TableHeaderCell/TableHeaderCell.test.tsx
@@ -20,7 +20,7 @@ describe('TableHeaderCell', () => {
         {
           props: {
             sortIcon: 'Test Icon',
-            resizeHandle: 'Test resizeHandle',
+            aside: 'Test aside',
           },
         },
       ],
