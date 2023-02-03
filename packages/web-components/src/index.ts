30d287e691bfe538360a310b4d7e4e3b6e380478
Brian Brady <brianbrady@microsoft.com>
2023-02-03 11:27:04 -0800

    fixes exports

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -2,7 +2,7 @@ export * from './badge/index.js';
 export * from './counter-badge/index.js';
 export * from './progress-bar/index.js';
 export * from './text/index.js';
-export * from './accordion/accordion/accordion.js';
-export * from './accordion/accordion-item/accordion-item.js';
+export * from './accordion/accordion/index.js';
+export * from './accordion/accordion-item/index.js';
 
 export * from './theme/index.js';
