f7c86b2f3807ad875c040b4d619994283c9757d5
KHMakoto <Humberto.Morimoto@microsoft.com>
2023-02-07 11:21:07 -0800

    Fixing storybook error.

--- packages/react-components/react-skeleton/stories/Skeleton/SkeletonDefault.stories.tsx
+++ packages/react-components/react-skeleton/stories/Skeleton/SkeletonDefault.stories.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Skeleton, SkeletonLine, SkeletonProps } from '@fluentui/react-skeleton';
 
-export const Default = (props: Partial<SkeletonProps>) => {
+export const Default = (props: Partial<SkeletonProps>) => (
   <Skeleton {...props}>
     <SkeletonLine />
-  </Skeleton>;
-};
+  </Skeleton>
+);
