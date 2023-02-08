48add2383858c851ae609e7d27b95aa5af8d8dda
Marcos Moura <marcosvmmoura@gmail.com>
2023-02-08 14:03:04 +0100

    fix: remove unused import

--- apps/vr-tests-react-components/src/stories/Card/Card.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Card/Card.stories.tsx
@@ -7,7 +7,7 @@ import { Button } from '@fluentui/react-button';
 import { powerpointLogoURL, salesPresentationTemplateURL, SampleCardContent, appLogoUrl } from './utils';
 import { ComponentMeta } from '@storybook/react';
 import { getStoryVariant, DARK_MODE, HIGH_CONTRAST, RTL } from '../../utilities';
-import { makeStyles, shorthands } from '@griffel/react';
+import { makeStyles } from '@griffel/react';
 import { tokens } from '@fluentui/react-theme';
 
 export default {
