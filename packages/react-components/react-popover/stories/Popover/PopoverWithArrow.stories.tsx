1467e1f11dc30623499be35fb80fc545145df64d
Ben Howell <behowell@microsoft.com>
2023-02-09 10:45:52 -0800

    Example repro with the fix

--- packages/react-components/react-popover/stories/Popover/PopoverWithArrow.stories.tsx
+++ packages/react-components/react-popover/stories/Popover/PopoverWithArrow.stories.tsx
@@ -1,32 +1,40 @@
 import * as React from 'react';
 import { makeStyles, Button, Popover, PopoverTrigger, PopoverSurface } from '@fluentui/react-components';
 
+const supportsBackdropFilter = '@supports (backdrop-filter: blur(100px))';
+
 const useStyles = makeStyles({
   contentHeader: {
     marginTop: '0',
+  },
+  surface: {
+    backgroundColor: 'white',
+    [supportsBackdropFilter]: {
+      backdropFilter: 'blur(100px)',
+      color: '#FF0000',
+      backgroundColor: '#FFFFFF80',
+    },
   },
 });
 
 const ExampleContent = () => {
   const styles = useStyles();
   return (
-    <div>
+    <PopoverSurface className={styles.surface}>
       <h3 className={styles.contentHeader}>Popover content</h3>
 
       <div>This popover has an arrow pointing to its target</div>
-    </div>
+    </PopoverSurface>
   );
 };
 
 export const WithArrow = () => (
-  <Popover withArrow>
+  <Popover open withArrow>
     <PopoverTrigger disableButtonEnhancement>
       <Button>Popover trigger</Button>
     </PopoverTrigger>
 
-    <PopoverSurface>
-      <ExampleContent />
-    </PopoverSurface>
+    <ExampleContent />
   </Popover>
 );
 
