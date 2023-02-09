9c1bb7084485ad1825634193028fa6a01c68e08d
Brian Brady <brianbrady@microsoft.com>
2023-02-09 12:43:42 -0800

    updates jsdocs

--- packages/web-components/src/menu/menu-item/menu-item.definition.ts
+++ packages/web-components/src/menu/menu-item/menu-item.definition.ts
@@ -4,13 +4,13 @@ import { styles } from './menu-item.styles.js';
 import { template } from './menu-item.template.js';
 
 /**
- * The Fluent Menu Element. Implements {@link @microsoft/fast-foundation#MenuItem },
+ * The Fluent Menu Item Element. Implements {@link @microsoft/fast-foundation#MenuItem },
  * {@link @microsoft/fast-foundation#menuItemTemplate}
  *
  *
  * @public
  * @remarks
- * HTML Element: <fluent-menu>
+ * HTML Element: <fluent-menu-item>
  */
 export const definition = MenuItem.compose({
   name: `${FluentDesignSystem.prefix}-menu-item`,
