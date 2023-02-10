cf4af110618703d81fc87e3d6eea45bcce355126
Lingfan Gao <lingfangao@hotmail.com>
2023-02-10 12:20:37 +0100

    eslint pragma

--- packages/react-components/babel-preset-storybook-full-source/src/index.dev.js
+++ packages/react-components/babel-preset-storybook-full-source/src/index.dev.js
@@ -1,10 +1,10 @@
 // This is internal code and should be a dev dependency
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 const { registerTsProject } = require('nx/src/utils/register');
 
 // This is internal code and should be a dev dependency
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { workspaceRoot } = require('@nrwl/devkit');
+/* eslint-enable import/no-extraneous-dependencies */
 
 registerTsProject(workspaceRoot, 'tsconfig.base.json');
 
