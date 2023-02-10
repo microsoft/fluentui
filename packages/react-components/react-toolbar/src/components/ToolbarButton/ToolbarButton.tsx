cbba19f7ed8a619b5fa856575fde872e79be99b9
Martin Hochel <martinhochel@microsoft.com>
2023-02-10 15:08:56 +0100

    fix(scripts-format): make prettier fail process instead of silently failing (#26806)
    * fix(scripts-format): make prettier fail process instead of silently failing
    
    * feat(scripts-format): make prettier not fail on huge file lists with --since
    
    * chore: update prettierignore
    
    * style: fix formatting in files that were silently passed previously
    
    * generate change files
    
    * fixup! style: fix formatting in files that were silently passed previously

--- packages/react-components/react-toolbar/src/components/ToolbarButton/ToolbarButton.tsx
+++ packages/react-components/react-toolbar/src/components/ToolbarButton/ToolbarButton.tsx
@@ -12,7 +12,7 @@ import { useToolbarButton_unstable } from './useToolbarButton';
 export const ToolbarButton: ForwardRefComponent<ToolbarButtonProps> = React.forwardRef((props, ref) => {
   const state = useToolbarButton_unstable(props, ref);
   useToolbarButtonStyles_unstable(state);
-  
+
   return renderButton_unstable(state);
   // Casting is required due to lack of distributive union to support unions on @types/react
 }) as ForwardRefComponent<ToolbarButtonProps>;
