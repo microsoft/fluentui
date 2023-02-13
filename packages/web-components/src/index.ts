a49bf1e3e0ffec5312d2215abacf2245b5d1da48
Brian Brady <brianbrady@microsoft.com>
2023-02-13 10:48:07 -0800

    sorts exports alphabetically

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -1,8 +1,8 @@
+export * from './accordion/accordion/index.js';
+export * from './accordion/accordion-item/index.js';
 export * from './badge/index.js';
 export * from './counter-badge/index.js';
 export * from './progress-bar/index.js';
 export * from './text/index.js';
-export * from './accordion/accordion/index.js';
-export * from './accordion/accordion-item/index.js';
 
 export * from './theme/index.js';
