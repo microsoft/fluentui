5f13fa401a48388dc336f4aa4957dcce3f77ddf8
chajun <86579954+chpalac@users.noreply.github.com>
2023-02-13 08:58:00 -0300

    chore: add segment shim (#26431)
    * chore: add segment shim
    
    * chore: update api

--- packages/react-components/react-migration-v0-v9/src/index.ts
+++ packages/react-components/react-migration-v0-v9/src/index.ts
@@ -1,3 +1,4 @@
 export { GridShim, grid, gridClassName, useGridStyles } from './components/Grid/index';
 export type { GridShimProps } from './components/Grid/index';
 export { FormFieldShim } from './components/FormField';
+export { Segment } from './components/Segment';
