30a876b039f89eb16b66738bf69e7f670d9db953
Ryan Merrill <ryanmerrill@microsoft.com>
2023-02-14 18:23:24 +0000

    Adds basic implementation of Spinner component (#26392)
    * Adds basic implementation of Spinner component
    
    * Adds inverted Story to Spinner
    
    * Cleans up some typos
    
    * Generates API report
    
    * Adds change file
    
    * Removes kebab-case for key in Spinner in favor of camelCase; Adds spinner export to package.json; Properly comments spinner options with type docs
    
    * Address feedback in PR
    
    * Minor typo cleanup
    
    * Rebuilds

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -1,6 +1,7 @@
 export * from './badge/index.js';
 export * from './counter-badge/index.js';
 export * from './progress-bar/index.js';
+export * from './spinner/index.js';
 export * from './text/index.js';
 
 export * from './theme/index.js';
---
+++ packages/web-components/src/spinner/spinner.ts
@@ -0,0 +1,30 @@
+import { attr } from '@microsoft/fast-element';
+import { FASTProgressRing } from '@microsoft/fast-foundation';
+import type { SpinnerAppearance, SpinnerSize } from './spinner.options.js';
+
+/**
+ * The base class used for constructing a fluent-spinner custom element
+ * @public
+ */
+export class Spinner extends FASTProgressRing {
+  /**
+   * The size of the spinner
+   *
+   * @public
+   * @default 'medium'
+   * @remarks
+   * HTML Attribute: size
+   */
+  @attr
+  public size: SpinnerSize;
+
+  /**
+   * The appearance of the spinner
+   * @public
+   * @default 'primary'
+   * @remarks
+   * HTML Attribute: appearance
+   */
+  @attr
+  public appearance: SpinnerAppearance;
+}
