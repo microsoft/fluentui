ba0e2828223192f9a49a0da769710740238ba89d
Ryan Merrill <ryanmerrill@microsoft.com>
2023-02-14 18:23:24 +0000

    Adds basic implementation of Spinner component (#26392)
    * Adds basic implementation of Spinner component
    
    * Adds inverted Story to Spinner
    
    * Cleans up some typos
    
    * Generates API report
    
    * Adds change file
    
    * Removes kebab-case for key in Spinner in favor of camelCase; Adds spinner export to package.json; Properly comments spinner options with type docs
    
    * Address feedback in PR
    
    * Minor typo cleanup
    
    * Rebuilds

---
+++ packages/web-components/src/spinner/spinner.template.ts
@@ -0,0 +1,24 @@
+import type { ElementViewTemplate } from '@microsoft/fast-element';
+import { progressRingTemplate } from '@microsoft/fast-foundation';
+import { Spinner } from './spinner.js';
+
+export const template: ElementViewTemplate<Spinner> = progressRingTemplate({
+  indeterminateIndicator: `
+    <svg class="progress" part="progress" viewBox="0 0 16 16">
+        <circle
+            class="background"
+            part="background"
+            cx="8px"
+            cy="8px"
+            r="7px"
+        ></circle>
+        <circle
+            class="indeterminate-indicator-1"
+            part="indeterminate-indicator-1"
+            cx="8px"
+            cy="8px"
+            r="7px"
+        ></circle>
+    </svg>
+  `,
+});
