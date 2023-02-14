99e6e27c7fcb62754a7184208a3b390d9f8a82df
ankityadav4 <ankityadav@microsoft.com>
2023-02-14 15:14:09 +0530

    Optimizing one function for performance improvement

--- packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
+++ packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
@@ -55,6 +55,10 @@ export interface IDPointType {
   xVal: number | Date;
 }
 
+export interface IMapXToDataSet {
+  [key: number | string]: ILineChartDataPoint[];
+}
+
 export interface IAreaChartState extends IBasestate {
   lineXValue: number;
   displayOfLine: InterceptVisibility;
@@ -369,25 +373,49 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
         allChartPoints.push(...singleChartPoint.data);
       });
 
-    let tempArr = allChartPoints;
-    while (tempArr.length) {
-      const valToCheck = tempArr[0].x instanceof Date ? tempArr[0].x.toLocaleString() : tempArr[0].x;
-      const filteredChartPoints: ILineChartDataPoint[] = tempArr.filter(
-        (point: ILineChartDataPoint) => (point.x instanceof Date ? point.x.toLocaleString() : point.x) === valToCheck,
-      );
+    //let tempArr = allChartPoints;
+    const mapOfXvalToListOfDataPoints: IMapXToDataSet = {};
+    allChartPoints.forEach((dataPoint: ILineChartDataPoint) => {
+      const xValue = dataPoint.x instanceof Date ? dataPoint.x.toLocaleString() : dataPoint.x;
+      // map of x value to the list of data points which share the same x value .
+      if (mapOfXvalToListOfDataPoints[xValue]) {
+        mapOfXvalToListOfDataPoints[xValue].push(dataPoint);
+      } else {
+        mapOfXvalToListOfDataPoints[xValue] = [dataPoint];
+      }
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+    Object.keys(mapOfXvalToListOfDataPoints).forEach((key: number | string) => {
+      const value: ILineChartDataPoint[] = mapOfXvalToListOfDataPoints[key];
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const singleDataset: any = {};
-      filteredChartPoints.forEach((singleDataPoint: ILineChartDataPoint, index: number) => {
+      value.forEach((singleDataPoint: ILineChartDataPoint, index: number) => {
         singleDataset.xVal = singleDataPoint.x;
         singleDataset[`chart${index}`] = singleDataPoint.y;
       });
       dataSet.push(singleDataset);
-      // removing compared objects from array
-      const val = tempArr[0].x instanceof Date ? tempArr[0].x.toLocaleString() : tempArr[0].x;
-      tempArr = tempArr.filter(
-        (point: ILineChartDataPoint) => (point.x instanceof Date ? point.x.toLocaleString() : point.x) !== val,
-      );
-    }
+    });
+
+    // while (tempArr.length) {
+    //   const valToCheck = tempArr[0].x instanceof Date ? tempArr[0].x.toLocaleString() : tempArr[0].x;
+    //   const filteredChartPoints: ILineChartDataPoint[] = tempArr.filter(
+    //     (point: ILineChartDataPoint) =>
+    //               (point.x instanceof Date ? point.x.toLocaleString() : point.x) === valToCheck,
+    //   );
+    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //   const singleDataset: any = {};
+    //   filteredChartPoints.forEach((singleDataPoint: ILineChartDataPoint, index: number) => {
+    //     singleDataset.xVal = singleDataPoint.x;
+    //     singleDataset[`chart${index}`] = singleDataPoint.y;
+    //   });
+    //   dataSet.push(singleDataset);
+    //   // removing compared objects from array
+    //   const val = tempArr[0].x instanceof Date ? tempArr[0].x.toLocaleString() : tempArr[0].x;
+    //   tempArr = tempArr.filter(
+    //     (point: ILineChartDataPoint) => (point.x instanceof Date ? point.x.toLocaleString() : point.x) !== val,
+    //   );
+    // }
 
     // get keys from dataset, used to create stacked data
     const keysLength: number = dataSet && Object.keys(dataSet[0])!.length;
