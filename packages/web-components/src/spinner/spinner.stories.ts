b9851d092c1eeb06214ffb619f9c433fc08de8af
Ryan Merrill <ryanmerrill@microsoft.com>
2023-02-14 18:23:24 +0000

    Adds basic implementation of Spinner component (#26392)
    * Adds basic implementation of Spinner component
    
    * Adds inverted Story to Spinner
    
    * Cleans up some typos
    
    * Generates API report
    
    * Adds change file
    
    * Removes kebab-case for key in Spinner in favor of camelCase; Adds spinner export to package.json; Properly comments spinner options with type docs
    
    * Address feedback in PR
    
    * Minor typo cleanup
    
    * Rebuilds

---
+++ packages/web-components/src/spinner/spinner.stories.ts
@@ -0,0 +1,53 @@
+import { html } from '@microsoft/fast-element';
+import type { Args, Meta } from '@storybook/html';
+import { renderComponent } from '../__test__/helpers.js';
+import { SpinnerAppearance, SpinnerSize } from './spinner.options.js';
+import './define.js';
+
+type SpinnerStoryArgs = Args;
+type SpinnerStoryMeta = Meta<SpinnerStoryArgs>;
+
+const storyTemplate = html<SpinnerStoryArgs>`
+  <fluent-spinner appearance=${x => x.appearance} size=${x => x.size}></fluent-spinner>
+`;
+
+export default {
+  title: 'Components/Spinner',
+  argTypes: {
+    appearance: {
+      description: 'The appearance of the spinner',
+      table: {
+        defaultValue: { summary: 'primary' },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(SpinnerAppearance),
+      },
+      defaultValue: 'primary',
+    },
+    size: {
+      description: 'The size of the spinner',
+      table: {
+        defaultValue: { summary: 'medium' },
+      },
+      control: {
+        type: 'select',
+        options: Object.values(SpinnerSize),
+      },
+      defaultValue: 'medium',
+    },
+  },
+  parameters: {
+    status: {
+      type: 'experimental',
+    },
+  },
+} as SpinnerStoryMeta;
+
+export const Spinner = renderComponent(storyTemplate).bind({});
+
+export const SpinnerInverted = renderComponent(html<SpinnerStoryArgs>`
+  <div style="background-color: #0f6cbd; padding: 20px;">
+    <fluent-spinner appearance="inverted" size="medium"></fluent-spinner>
+  </div>
+`);
