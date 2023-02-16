527451f1d40bc7a88fe16028d5acda3f5a40396b
Lingfan Gao <lingfangao@hotmail.com>
2023-02-16 12:02:18 +0100

    feat: Set overflow on positioned element when `autosize` is applied
    Adds `overflowX` and `overflowY` to the positioned element when the
    autosize dimension is used. It's counterintuitive that components
    should need to apply overflow styles for this.

--- packages/react-components/react-positioning/src/middleware/maxSize.ts
+++ packages/react-components/react-positioning/src/middleware/maxSize.ts
@@ -25,6 +25,7 @@ export function maxSize(autoSize: PositioningOptions['autoSize'], options: MaxSi
         Object.assign(elements.floating.style, {
           maxHeight: `${availableHeight}px`,
           boxSizing: 'border-box',
+          overflowY: 'auto',
         });
       }
 
@@ -32,6 +33,7 @@ export function maxSize(autoSize: PositioningOptions['autoSize'], options: MaxSi
         Object.assign(elements.floating.style, {
           maxWidth: `${availableWidth}px`,
           boxSizing: 'border-box',
+          overflowX: 'auto',
         });
       }
     },
