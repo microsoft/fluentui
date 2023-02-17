3cb33fd02dcff03030611ef098b28de5d5079a2a
Ryan Merrill <ryan@ryanmerrill.net>
2023-02-16 19:53:04 -0500

    Starts work on CSS

--- packages/web-components/src/slider/slider.styles.ts
+++ packages/web-components/src/slider/slider.styles.ts
@@ -1,48 +1,62 @@
 import { css } from '@microsoft/fast-element';
 import { display } from '@microsoft/fast-foundation';
-// import { } from '../theme/design-tokens.js';
+import {
+  borderRadiusCircular,
+  borderRadiusMedium,
+  colorBrandBackground,
+  colorCompoundBrandBackground,
+  colorNeutralBackground1,
+  colorNeutralStroke1,
+  colorNeutralStrokeAccessible,
+} from '../theme/design-tokens.js';
 
 /** Text styles
  * @public
  */
 export const styles = css`
   ${display('inline-grid')} :host {
-    --thumb-size: 16;
+    --cursor-height: 20px;
+    --cursor-width: 20px;
+    --track-height: 4px;
     align-items: center;
     width: 100%;
-    user-select: none;
     box-sizing: border-box;
     outline: none;
     cursor: pointer;
+    user-select: none;
+    border-radius: ${borderRadiusMedium};
+  }
+
+  .positioning-region {
+    position: relative;
+    margin: 0 8px;
+    display: grid;
+    grid-template-rows: 16px 1fr;
   }
 
   .track {
-    background: #000;
-    height: 2px;
+    background: ${colorNeutralStrokeAccessible};
+    height: var(--track-height);
+    border-radius: ${borderRadiusMedium};
   }
 
   .thumb-container {
     position: absolute;
+    left: 0;
+    background: ${colorCompoundBrandBackground};
+    height: var(--track-height);
+    border-radius: ${borderRadiusMedium};
   }
 
   .thumb-cursor {
-    height: 20px;
-    width: 20px;
-    border-radius: 40px;
-    background: red;
-  }
-
-  :host([orientation='horizontal']) .positioning-region {
-    position: relative;
-    margin: 0 8px;
-    display: grid;
-    grid-template-rows: calc(var(--thumb-size) * 1px) 1fr;
-  }
-  :host([orientation='vertical']) .positioning-region {
-    position: relative;
-    margin: 0 8px;
-    display: grid;
-    height: 100%;
-    grid-template-columns: calc(var(--thumb-size) * 1px) 1fr;
+    position: absolute;
+    right: 0;
+    height: var(--cursor-height);
+    width: var(--cursor-width);
+    border-radius: ${borderRadiusCircular};
+    background-color: ${colorBrandBackground};
+    box-shadow: inset 0 0 0 3px ${colorNeutralBackground1};
+    border: 1px solid ${colorNeutralStroke1};
+    transform: translateY(calc((var(--track-height) * 2) * -1));
   }
 `;
--- packages/web-components/src/slider/slider.ts
+++ packages/web-components/src/slider/slider.ts
@@ -1,22 +1,12 @@
 import { attr } from '@microsoft/fast-element';
 import { FASTSlider } from '@microsoft/fast-foundation';
-import type { SliderMode, SliderSize } from './slider.options.js';
+import type { SliderSize } from './slider.options.js';
 
 /**
  * The base class used for constructing a fluent-slider custom element
  * @public
  */
 export class Slider extends FASTSlider {
-  /**
-   * The mode of the slider
-   *
-   * @public
-   * @remarks
-   * HTML Attribute: mode
-   */
-  @attr
-  public mode: SliderMode;
-
   /**
    * The size of the slider
    * @public
