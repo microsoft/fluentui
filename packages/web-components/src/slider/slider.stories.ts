a7841c7b4a33fa0d71e86d3de95791b272ba5cb4
Ryan Merrill <ryan@ryanmerrill.net>
2023-02-17 13:08:31 -0500

    More work on Stories

--- packages/web-components/src/slider/slider.stories.ts
+++ packages/web-components/src/slider/slider.stories.ts
@@ -1,16 +1,57 @@
 import { html } from '@microsoft/fast-element';
 import type { Args, Meta } from '@storybook/html';
 import { renderComponent } from '../helpers.stories.js';
+import { SliderSize } from './slider.options.js';
 import type { Slider as FluentSlider } from './slider.js';
 import './define.js';
 
 type SliderStoryArgs = Args & FluentSlider;
 type SliderStoryMeta = Meta<SliderStoryArgs>;
 
-const storyTemplate = html<SliderStoryArgs>` <fluent-slider min=${x => x.max} max=${x => x.max}></fluent-slider> `;
+const storyTemplate = html<SliderStoryArgs>`
+  <fluent-slider min=${x => x.max} max=${x => x.max} current-value=${x => x.currentValue}> </fluent-slider>
+`;
 
 export default {
   title: 'Components/Slider',
+  args: {
+    min: 0,
+    max: 100,
+    currentValue: '50',
+    size: SliderSize.medium,
+  },
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: Object.values(SliderSize),
+      },
+    },
+    min: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+    },
+    max: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+    },
+    currentValue: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1,
+      },
+    },
+  },
 } as SliderStoryMeta;
 
 export const Slider = renderComponent(storyTemplate).bind({});
