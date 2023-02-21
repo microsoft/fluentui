572c926f7efc2a6e07448bc7b26bcb2e535db823
BrdyBrn <brian.christopher.brady@gmail.com>
2023-02-21 09:14:56 -0800

    web component accordion (#26547)
    * accordion and accordion item init
    
    * yarn change
    
    * updates docs
    
    * updates accordion item styles
    
    * code formatting
    
    * updates imports
    
    * adds v9 appearance sizes
    
    * updates attribute name to align with fluent v9
    
    * adds alternative stories for accordion with custom icons
    
    * adds docs
    
    * updates storybook
    
    * updates changelog
    
    * removes dead code
    
    * removes dynamic icons from sb
    
    * removes dead code
    
    * adds accordion README
    
    * moves accordion readme to accordion directory
    
    * updates docs
    
    * updates accordion docs
    
    * updates docs
    
    * updates docs
    
    * fixes docs
    
    * fixes exports
    
    * adds display helper to styles
    
    * optimizes styles
    
    * updates html helper to use html.partial
    
    * optimizes styles
    
    * exports directly on class
    
    * removes html helper
    
    * removes arg tables from stories
    
    * sorts exports alphabetically
    
    * api report
    
    * flattens accordion componnent folders
    
    * removes comment
    
    * updates styles
    
    * optimizes styles
    
    * updates attributes
    
    * removes renaming of definition import
    
    * removes attribute syntax
    
    * adds attr
    
    * reverts api-report
    
    * removes dead code
    
    * updates styles
    
    * consolidates stories into one
    
    * updates docs
    
    * adds display helper to accordion styles

---
+++ packages/web-components/src/accordion-item/index.ts
@@ -0,0 +1,5 @@
+export * from './accordion-item.js';
+export * from './accordion-item.options.js';
+export { styles as accordionItemStyles } from './accordion-item.styles.js';
+export { definition as accordionItemDefinition } from './accordion-item.definition.js';
+export { template as accordionItemTemplate } from './accordion-item.template.js';
