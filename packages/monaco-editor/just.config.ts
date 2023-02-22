df6f27ce9807da1bfbdc2af5190b0d5637988e44
Martin Hochel <martinhochel@microsoft.com>
2023-02-22 19:04:08 +0100

    fix(scripts-tasks): format and update API usage which was modified by eslint export * rule unwrap autofix

--- packages/monaco-editor/just.config.ts
+++ packages/monaco-editor/just.config.ts
@@ -1,4 +1,4 @@
-import { preset, task, series, parallel, cleanTask, postprocessTask } from '@fluentui/scripts-tasks';
+import { preset, task, series, parallel, cleanTask, postprocessTask, defaultLibPaths } from '@fluentui/scripts-tasks';
 
 import * as path from 'path';
 import { transformCssTask } from './tasks/transformCssTask';
@@ -7,7 +7,7 @@ preset();
 
 task('clean', cleanTask({ paths: ['esm', 'lib', 'lib-commonjs'].map(p => path.join(process.cwd(), p)) }));
 task('transform-css', transformCssTask);
-task('ts:postprocess', postprocessTask([...postprocessTask.defaultLibPaths, 'esm/**/*.d.ts']));
+task('ts:postprocess', postprocessTask([...defaultLibPaths, 'esm/**/*.d.ts']));
 task('ts', series(parallel('ts:esm', 'ts:commonjs'), 'ts:postprocess'));
 
 task('build', series('clean', 'copy', 'transform-css', 'ts', 'lint-imports:all')).cached!();
