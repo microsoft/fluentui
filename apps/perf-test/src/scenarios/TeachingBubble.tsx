995fb2afaa341c43647a3ab2a27650ab65c3b73c
Tristan Watanabe <tristan.watanabe@gmail.com>
2023-02-22 04:07:47 -0500

    temp fix: delete v8 TeachingBubble perf test

--- apps/perf-test/src/scenarios/TeachingBubble.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { TeachingBubble } from '@fluentui/react';
-
-// This test hangs with puppeteer 19, commenting out for now.
-// const Scenario = () => <TeachingBubble />;
-const Scenario = () => <div />;
-
-export default Scenario;
