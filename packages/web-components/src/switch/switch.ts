02e273f4e480c0a5462ba10e1967bb90bb119e27
BrdyBrn <brian.christopher.brady@gmail.com>
2023-02-23 16:28:56 -0800

    Web Component Switch (#26517)
    * switch init
    
    * provides export path
    
    * yarn change
    
    * removes dead code
    
    * adds table docs
    
    * updates attribute to use attr syntax
    
    * removes on/off text for label only
    
    * addresses PR feedback
    
    * updates label font-weight
    
    * removes dead code
    
    * updates styles
    
    * adds required styles for switch
    
    * Update change/@fluentui-web-components-a74d6518-0d94-4e22-bd9f-70ea63a066ab.json
    
    Co-authored-by: Miroslav Stastny <miroslav.stastny@microsoft.com>
    
    * removes unuses import
    
    * api report
    
    * removes dead code
    
    * updates styles and story
    
    * optimizes styles
    
    * reverts api report
    
    * removes unchecked checked message styles
    
    * updates labelPosition type to include undefined
    
    * updates helper import
    
    * optimizes css
    
    * cleans up styles
    
    * optimizes styles
    
    * uses design tokens
    
    * removes dead import
    
    * removes dead code
    
    * removes required styling from label
    
    * adds switch readme
    
    * fixes docs
    
    * fixes switch hover styles
    
    * formats switch styles
    
    * alphabetizes exports
    
    ---------
    
    Co-authored-by: Miroslav Stastny <miroslav.stastny@microsoft.com>

---
+++ packages/web-components/src/switch/switch.ts
@@ -0,0 +1,16 @@
+import { attr } from '@microsoft/fast-element';
+import { FASTSwitch } from '@microsoft/fast-foundation';
+import { SwitchLabelPosition } from './switch.options.js';
+
+export class Switch extends FASTSwitch {
+  /**
+   * The label position of the switch
+   *
+   * @public
+   * @default 'after'
+   * @remarks
+   * HTML Attribute: labelposition
+   */
+  @attr({ attribute: 'label-position' })
+  public labelPosition: SwitchLabelPosition | undefined;
+}
