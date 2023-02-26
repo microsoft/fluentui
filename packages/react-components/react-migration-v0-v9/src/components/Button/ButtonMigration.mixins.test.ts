6a893734e7fbeb80a0d6190f28a07359cae5443a
Charles <chassunc@microsoft.com>
2023-02-26 16:55:50 -0300

    chore: use snapshoot

--- packages/react-components/react-migration-v0-v9/src/components/Button/ButtonMigration.mixins.test.ts
+++ packages/react-components/react-migration-v0-v9/src/components/Button/ButtonMigration.mixins.test.ts
@@ -1,15 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { GriffelStyle, shorthands } from '@fluentui/react-components';
 import { iconFilledClassName, iconRegularClassName } from '@fluentui/react-icons';
 import { buttonMigrationStyles } from './ButtonMigration.mixins';
-
-const testMixin = (mixin: GriffelStyle | undefined, expectedStyle: GriffelStyle | undefined) => {
-  const name = expectedStyle ? JSON.stringify(expectedStyle) : 'empty';
-  test(name, () => {
-    const result = { ...mixin };
-    expect(result).toEqual(expectedStyle || {});
-  });
-};
 
 describe('ButtonMigration.mixins', () => {
   describe('classNames check', () => {
@@ -21,86 +11,35 @@ describe('ButtonMigration.mixins', () => {
     });
   });
   describe('v0Icon', () => {
-    const styles = {
-      '& .ui-icon__outline': {
-        display: 'block',
-      },
-      '& .ui-icon__filled': {
-        display: 'none',
-      },
-      '&:hover': {
-        '& .ui-icon__outline': {
-          display: 'none',
+    expect(buttonMigrationStyles.v0Icon()).toMatchInlineSnapshot(`
+      Object {
+        "& .ui-icon__filled": Object {
+          "display": "none",
         },
-        '& .ui-icon__filled': {
-          display: 'block',
+        "& .ui-icon__outline": Object {
+          "display": "block",
         },
-      },
-    };
-    testMixin(buttonMigrationStyles.v0Icon(), styles);
+        "&:hover": Object {
+          "& .ui-icon__filled": Object {
+            "display": "block",
+          },
+          "& .ui-icon__outline": Object {
+            "display": "none",
+          },
+        },
+      }
+    `);
   });
 
   describe('v9Icon', () => {
-    const styles = {
-      '& svg': {
-        width: '100%',
-        paddingBottom: '100%',
-        ...shorthands.margin('-4px', 0, 0, '-4px'),
-        ...shorthands.overflow('visible'),
-      },
-      '& .fui-Icon-filled': {
-        display: 'none',
-      },
-      '& .fui-Icon-regular': {
-        display: 'inline',
-      },
-      '&:hover': {
-        '& .fui-Icon-filled': {
-          display: 'inline',
-        },
-        '& .fui-Icon-regular': {
-          display: 'none',
-        },
-      },
-    };
-    testMixin(buttonMigrationStyles.v9Icon(), styles);
+    expect(buttonMigrationStyles.v9Icon()).toMatchSnapshot();
   });
 
   describe('v9CustomSizeIcon', () => {
-    const styles = {
-      '& svg': {
-        ...shorthands.overflow('visible'),
-      },
-      '& .fui-Icon-filled': {
-        display: 'none',
-      },
-      '& .fui-Icon-regular': {
-        display: 'inline',
-      },
-      '&:hover': {
-        '& .fui-Icon-filled': {
-          display: 'inline',
-        },
-        '& .fui-Icon-regular': {
-          display: 'none',
-        },
-      },
-    };
-    testMixin(buttonMigrationStyles.v9CustomSizeIcon(), styles);
+    expect(buttonMigrationStyles.v9CustomSizeIcon()).toMatchSnapshot();
   });
 
   describe('v9DisabledCursor', () => {
-    const styles = {
-      '&:disabled': {
-        cursor: 'default',
-        '&:hover': {
-          cursor: 'default',
-        },
-        '&:hover:active': {
-          cursor: 'default',
-        },
-      },
-    };
-    testMixin(buttonMigrationStyles.v9DisabledCursor(), styles);
+    expect(buttonMigrationStyles.v9DisabledCursor()).toMatchSnapshot();
   });
 });
