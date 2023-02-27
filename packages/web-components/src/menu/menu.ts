5ae8347b316cd43c491331a4a8b592faf994279b
Chris Holt <chhol@microsoft.com>
2023-02-27 12:00:08 -0800

    dynamically set the menu item icon attribute when icons is true for menu

--- packages/web-components/src/menu/menu.ts
+++ packages/web-components/src/menu/menu.ts
@@ -1,5 +1,5 @@
 import { FASTMenu } from '@microsoft/fast-foundation';
-import { attr } from '@microsoft/fast-element';
+import { attr, Updates } from '@microsoft/fast-element';
 
 /**
  * The base class used for constructing a fluent-menu custom element
@@ -15,4 +15,29 @@ export class Menu extends FASTMenu {
    */
   @attr({ mode: 'boolean' })
   public icons: boolean = false;
+  protected iconsChanged(): void {
+    if (!this.$fastController.isConnected) {
+      return;
+    }
+
+    this.menuItems?.filter(this.isMenuItemElement).forEach((item: HTMLElement, index: number) => {
+      if (this.icons) {
+        item.setAttribute('icon', '');
+      } else {
+        item.removeAttribute('icon');
+      }
+    });
+  }
+
+  public connectedCallback() {
+    super.connectedCallback();
+
+    if (this.icons) {
+      Updates.enqueue(() => {
+        // wait until children have had a chance to
+        // connect before setting/checking their props/attributes
+        this.iconsChanged();
+      });
+    }
+  }
 }
