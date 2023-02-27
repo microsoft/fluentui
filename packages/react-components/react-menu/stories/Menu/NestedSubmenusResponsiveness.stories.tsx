912219cdef69607420af85acfa04be3340f5fa59
Lingfan Gao <lingfangao@hotmail.com>
2023-02-27 18:52:23 +0100

    docsite polish

--- packages/react-components/react-menu/stories/Menu/NestedSubmenusResponsiveness.stories.tsx
+++ packages/react-components/react-menu/stories/Menu/NestedSubmenusResponsiveness.stories.tsx
@@ -12,21 +12,23 @@ import {
   shorthands,
   tokens,
   mergeClasses,
+  MenuProps,
 } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
   container: {
     width: '500px',
+    height: '400px',
+  },
+
+  resizableArea: {
+    width: '500px',
     ...shorthands.border('2px', 'dashed', 'red'),
     height: '400px',
     ...shorthands.padding('10px'),
     position: 'relative',
-  },
-
-  resizableArea: {
     ...shorthands.border('2px', 'solid', tokens.colorBrandBackground),
     ...shorthands.padding('20px', '10px', '10px', '10px'),
-    position: 'relative',
     resize: 'both',
     backgroundImage: `linear-gradient(-45deg, ${tokens.colorBrandBackground} 20px, transparent 20px)`,
     ...shorthands.overflow('hidden'),
@@ -49,7 +51,8 @@ const useStyles = makeStyles({
 
 export const NestedSubmenusResponsiveness = () => {
   const styles = useStyles();
-  const [overflowBoundary, setBoundary] = React.useState<HTMLElement | null>(null);
+  const [open, setOpen] = React.useState(false);
+  const [boundary, setBoundary] = React.useState<HTMLElement | null>(null);
   const positioningRefSubmenu = React.useRef<PositioningImperativeRef>(null);
   const positioningRefRoot = React.useRef<PositioningImperativeRef>(null);
 
@@ -64,53 +67,97 @@ export const NestedSubmenusResponsiveness = () => {
   )[0];
 
   React.useEffect(() => {
-    if (overflowBoundary) {
-      resizeObserver.observe(overflowBoundary);
-      return () => resizeObserver.unobserve(overflowBoundary);
+    if (boundary) {
+      resizeObserver.observe(boundary);
+      return () => resizeObserver.unobserve(boundary);
     }
-  }, [overflowBoundary, resizeObserver]);
+  }, [boundary, resizeObserver]);
 
   React.useEffect(() => {
-    positioningRefSubmenu.current?.updatePosition();
-    positioningRefRoot.current?.updatePosition();
     return () => {
       resizeObserver.disconnect();
     };
   }, [resizeObserver]);
 
+  React.useEffect(() => {
+    if (open) {
+      // defer position update of the nested menu the root menu hasn't been positioned yet
+      const timeout = setTimeout(() => positioningRefSubmenu.current?.updatePosition());
+      positioningRefRoot.current?.updatePosition();
+
+      return () => clearTimeout(timeout);
+    }
+  }, [open]);
+
+  const onOpenChange: MenuProps['onOpenChange'] = (e, data) => {
+    if (boundary?.contains(e.target as HTMLElement)) {
+      setOpen(true);
+    } else {
+      setOpen(data.open);
+    }
+  };
+
   return (
-    <div id="boundary" className={mergeClasses(styles.container, styles.resizableArea)} ref={setBoundary}>
-      <Menu open positioning={{ positioningRef: positioningRefRoot, overflowBoundary, flipBoundary: overflowBoundary }}>
-        <MenuTrigger disableButtonEnhancement>
-          <Button style={{ position: 'absolute', left: '40%' }}>Menu</Button>
-        </MenuTrigger>
+    <div className={styles.container}>
+      <div id="boundary" className={mergeClasses(styles.container, styles.resizableArea)} ref={setBoundary}>
+        <Menu
+          open={open}
+          onOpenChange={onOpenChange}
+          positioning={{ positioningRef: positioningRefRoot, overflowBoundary: boundary, flipBoundary: boundary }}
+        >
+          <MenuTrigger disableButtonEnhancement>
+            <Button style={{ position: 'absolute', left: '40%' }}>Menu</Button>
+          </MenuTrigger>
 
-        <MenuPopover>
-          <MenuList>
-            <MenuItem>New </MenuItem>
-            <MenuItem>New Window</MenuItem>
-            <MenuItem disabled>Open File</MenuItem>
-            <MenuItem>Open Folder</MenuItem>
-            <Menu
-              open
-              positioning={{ overflowBoundary, flipBoundary: overflowBoundary, positioningRef: positioningRefSubmenu }}
-            >
-              <MenuTrigger disableButtonEnhancement>
-                <MenuItem>Toggle menu</MenuItem>
-              </MenuTrigger>
+          <MenuPopover>
+            <MenuList>
+              <MenuItem>New </MenuItem>
+              <MenuItem>New Window</MenuItem>
+              <MenuItem disabled>Open File</MenuItem>
+              <MenuItem>Open Folder</MenuItem>
+              <Menu
+                open
+                positioning={{
+                  overflowBoundary: boundary,
+                  flipBoundary: boundary,
+                  positioningRef: positioningRefSubmenu,
+                }}
+              >
+                <MenuTrigger disableButtonEnhancement>
+                  <MenuItem>Toggle menu</MenuItem>
+                </MenuTrigger>
 
-              <MenuPopover>
-                <MenuList>
-                  <MenuItem>New </MenuItem>
-                  <MenuItem>New Window</MenuItem>
-                  <MenuItem disabled>Open File</MenuItem>
-                  <MenuItem>Open Folder</MenuItem>
-                </MenuList>
-              </MenuPopover>
-            </Menu>
-          </MenuList>
-        </MenuPopover>
-      </Menu>
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>New </MenuItem>
+                    <MenuItem>New Window</MenuItem>
+                    <MenuItem disabled>Open File</MenuItem>
+                    <MenuItem>Open Folder</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+            </MenuList>
+          </MenuPopover>
+        </Menu>
+      </div>
     </div>
   );
+};
+
+NestedSubmenusResponsiveness.parameters = {
+  docs: {
+    description: {
+      story: [
+        'Nested submenus have some limited responsiveness built in. If the boundaries of the container/viewport',
+        'get smaller, nested submenus will try to position themselves accordingly. Below is the order or',
+        'fallbacks that will happen:',
+        '- Move alignment of the nested menu higher',
+        '- Flip the position of the nested menu',
+        '- Position the nested menu above the parent menu',
+        '',
+        'You can use the resizable container below to try this out.',
+        '**(Click outside the resizable area to dismiss the menus)**',
+      ].join('\n'),
+    },
+  },
 };
