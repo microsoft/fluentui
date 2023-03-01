607fb16f8c2695dfa10d283bc2d29d97ebe9a27a
Micah Godbolt <mgodbolt@microsoft.com>
2023-02-28 17:17:53 -0800

    add raw html

--- packages/react-components/react-tags/src/components/Tag/renderTag.tsx
+++ packages/react-components/react-tags/src/components/Tag/renderTag.tsx
@@ -9,5 +9,23 @@ export const renderTag_unstable = (state: TagState) => {
   const { slots, slotProps } = getSlots<TagSlots>(state);
 
   // TODO Add additional slots in the appropriate place
-  return <slots.root {...slotProps.root} />;
+  return (
+    <slots.root {...slotProps.root}>
+      <span className="content">
+        <div className="persona">
+          <img
+            height="32"
+            width="32"
+            src="https://res-1.cdn.office.net/files/fabric-cdn-prod_20221209.001/office-ui-fabric-react-assets/persona-male.png"
+          />
+        </div>
+        <div className="icon">i</div>
+        <div className="primary">Primary Text</div>
+        <div className="secondary">Secondary Text</div>
+      </span>
+      <div className="dismiss">
+        <button>x</button>
+      </div>
+    </slots.root>
+  );
 };
--- packages/react-components/react-tags/src/components/TagButton/renderTagButton.tsx
+++ packages/react-components/react-tags/src/components/TagButton/renderTagButton.tsx
@@ -9,5 +9,23 @@ export const renderTagButton_unstable = (state: TagButtonState) => {
   const { slots, slotProps } = getSlots<TagButtonSlots>(state);
 
   // TODO Add additional slots in the appropriate place
-  return <slots.root {...slotProps.root} />;
+  return (
+    <slots.root {...slotProps.root}>
+      <button className="content">
+        <div className="persona">
+          <img
+            height="32"
+            width="32"
+            src="https://res-1.cdn.office.net/files/fabric-cdn-prod_20221209.001/office-ui-fabric-react-assets/persona-male.png"
+          />
+        </div>
+        <div className="icon">i</div>
+        <div className="primary">Primary Text</div>
+        <div className="secondary">Secondary Text</div>
+      </button>
+      <div className="dismiss">
+        <button>x</button>
+      </div>
+    </slots.root>
+  );
 };
