c958b843fdb2c20302644f4581d08b45ff3a7b9e
Martin Hochel <martinhochel@microsoft.com>
2023-03-01 12:48:58 +0100

    style: apply 2.5 on whole codebase

--- packages/fluentui/accessibility/src/behaviors/Datepicker/datepickerCalendarCellButtonBehavior.ts
+++ packages/fluentui/accessibility/src/behaviors/Datepicker/datepickerCalendarCellButtonBehavior.ts
@@ -9,17 +9,18 @@ import { Accessibility } from '../../types';
  * Adds attribute 'aria-selected=true' based on the property 'selected'.
  * Adds role='gridcell'.
  */
-export const datepickerCalendarCellButtonBehavior: Accessibility<DatepickerCalendarCellButtonBehaviorProps> =
-  props => ({
-    attributes: {
-      root: {
-        role: 'gridcell',
-        ...(props.selected && { 'aria-selected': true }),
-        ...(props.disabled && { 'aria-disabled': true }),
-        ...(props.today && { 'aria-current': 'date' }),
-      },
+export const datepickerCalendarCellButtonBehavior: Accessibility<
+  DatepickerCalendarCellButtonBehaviorProps
+> = props => ({
+  attributes: {
+    root: {
+      role: 'gridcell',
+      ...(props.selected && { 'aria-selected': true }),
+      ...(props.disabled && { 'aria-disabled': true }),
+      ...(props.today && { 'aria-current': 'date' }),
     },
-  });
+  },
+});
 
 export type DatepickerCalendarCellButtonBehaviorProps = {
   /** Cell can be selected. */
