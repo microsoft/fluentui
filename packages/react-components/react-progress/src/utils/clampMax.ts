711ab4738ea1ca2c096a6f6af6083497d0df4ff1
Tomi Olubeko <66456876+tomi-msft@users.noreply.github.com>
2023-03-03 13:47:44 -0800

    Update packages/react-components/react-progress/src/utils/clampMax.ts
    Co-authored-by: Ben Howell <48106640+behowell@users.noreply.github.com>

--- packages/react-components/react-progress/src/utils/clampMax.ts
+++ packages/react-components/react-progress/src/utils/clampMax.ts
@@ -2,7 +2,7 @@ export const clampMax = (max: number): number => {
   const internalMax = max <= 0 ? 1 : max;
 
   if (process.env.NODE_ENV !== 'production') {
-    if (max !== undefined && max <= 0) {
+    if (max <= 0) {
       // eslint-disable-next-line no-console
       console.error(`The prop 'max' must be greater than 0. Received max: ${max}`);
     }
