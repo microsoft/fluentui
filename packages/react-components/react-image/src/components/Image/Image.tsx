e5fe01b444689fa5306a58e7b1367975f3c46981
GeoffCoxMSFT <gcox@microsoft.com>
2023-03-04 09:22:11 -0800

    Custom style react-image

--- packages/react-components/react-image/src/components/Image/Image.tsx
+++ packages/react-components/react-image/src/components/Image/Image.tsx
@@ -4,13 +4,18 @@ import { useImage_unstable } from './useImage';
 import { useImageStyles_unstable } from './useImageStyles';
 import type { ImageProps } from './Image.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHooks_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * The Image component ensures the consistent styling of images.
  */
 export const Image: ForwardRefComponent<ImageProps> = React.forwardRef((props, ref) => {
   const state = useImage_unstable(props, ref);
+
   useImageStyles_unstable(state);
+
+  const { useImageStyles_unstable: useCustomStyles } = useCustomStyleHooks_unstable();
+  useCustomStyles(state);
 
   return renderImage_unstable(state);
 });
