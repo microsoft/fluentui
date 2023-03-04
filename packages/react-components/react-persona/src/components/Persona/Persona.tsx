dd4b5b776f40b23311d3d437b4c66f50d11966ee
GeoffCoxMSFT <gcox@microsoft.com>
2023-03-04 09:44:21 -0800

    Custom style react-persona

--- packages/react-components/react-persona/src/components/Persona/Persona.tsx
+++ packages/react-components/react-persona/src/components/Persona/Persona.tsx
@@ -4,6 +4,7 @@ import { usePersona_unstable } from './usePersona';
 import { usePersonaStyles_unstable } from './usePersonaStyles';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
 import type { PersonaProps } from './Persona.types';
+import { useCustomStyleHooks_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * A Persona is a visual representation of a person or status that showcases an Avatar, PresenceBadge,
@@ -13,6 +14,10 @@ export const Persona: ForwardRefComponent<PersonaProps> = React.forwardRef((prop
   const state = usePersona_unstable(props, ref);
 
   usePersonaStyles_unstable(state);
+
+  const { usePersonaStyles_unstable: useCustomStyles } = useCustomStyleHooks_unstable();
+  useCustomStyles(state);
+
   return renderPersona_unstable(state);
 });
 
