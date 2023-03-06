d489c1a10a96019bbcc3c811f7c853980b727c67
Geoff Cox (Microsoft) <gcox@microsoft.com>
2023-03-06 08:37:27 -0800

    Custom style react-input (#27065)

--- packages/react-components/react-input/src/components/Input/Input.tsx
+++ packages/react-components/react-input/src/components/Input/Input.tsx
@@ -4,6 +4,7 @@ import { renderInput_unstable } from './renderInput';
 import { useInputStyles_unstable } from './useInputStyles';
 import type { InputProps } from './Input.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHooks_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * The Input component allows people to enter and edit text.
@@ -12,6 +13,10 @@ export const Input: ForwardRefComponent<InputProps> = React.forwardRef((props, r
   const state = useInput_unstable(props, ref);
 
   useInputStyles_unstable(state);
+
+  const { useInputStyles_unstable: useCustomStyles } = useCustomStyleHooks_unstable();
+  useCustomStyles(state);
+
   return renderInput_unstable(state);
 });
 
