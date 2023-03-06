478cb631eebeb90d1f96a4c51feae22cb1e7bd84
Mason Tejera <matejera@microsoft.com>
2023-03-06 12:46:32 -0800

    Exports IExtendedSemanticColors

--- apps/public-docsite/src/components/Site/AppThemes.ts
+++ apps/public-docsite/src/components/Site/AppThemes.ts
@@ -3,10 +3,10 @@ import { Fluent2WebLightTheme, Fluent2WebDarkTheme } from '@fluentui/fluent2-the
 import { IAppThemes, IExampleCardTheme } from '@fluentui/react-docsite-components';
 
 const exampleCardTheme: IExampleCardTheme[] = [
-  { title: 'Default', theme: DefaultTheme },
-  { title: 'Dark', theme: DarkTheme },
   { title: 'Fluent 2 Web Light', theme: Fluent2WebLightTheme },
   { title: 'Fluent 2 Web Dark', theme: Fluent2WebDarkTheme },
+  { title: 'Default', theme: DefaultTheme },
+  { title: 'Dark', theme: DarkTheme },
 ];
 
 export const AppThemes: IAppThemes = {
