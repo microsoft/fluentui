c9b9249ba984bb69977ba72097533a647e2d7c18
Brian Brady <brianbrady@microsoft.com>
2023-03-07 11:41:42 -0800

    adds radio group attributes

--- packages/web-components/src/radio-group/radio-group.ts
+++ packages/web-components/src/radio-group/radio-group.ts
@@ -7,13 +7,22 @@ import { FASTRadioGroup } from '@microsoft/fast-foundation';
  */
 export class RadioGroup extends FASTRadioGroup {
   /**
+   * Describes the ID of the element that labels the current element, as used for accessibility with screen readers.
+   *
+   * @public
+   * @remarks
+   * HTML Attribute: aria-labelledby
+   */
+  @attr({ attribute: 'aria-labelledby' })
+  public ariaLabelledby: string = '';
+
+  /**
    * sets radio layout styles
    *
    * @public
    * @remarks
    * HTML Attribute: stacked
    */
-
   @attr({ attribute: 'stacked', mode: 'boolean' })
   public stacked: boolean = false;
 
@@ -32,9 +41,28 @@ export class RadioGroup extends FASTRadioGroup {
     }
   }
 
+  protected disableChanged(): void {
+    console.log('disableChanged');
+    if (!this.$fastController.isConnected) {
+      return;
+    }
+    this.slottedRadioButtons.forEach((item: HTMLElement, index: number) => {
+      if (this.disabled) {
+        item.setAttribute('disabled', '');
+      }
+    });
+  }
+
   protected slottedRadioButtonsChanged(oldValue: HTMLElement[], newValue: HTMLElement[]): void {
     super.slottedRadioButtonsChanged(oldValue, newValue);
+    console.log('slottedRadioButtonsChanged');
+    if (this.disabled) {
+      console.log('slottedRadioButtonsChanged: disabled');
+      this.disableChanged();
+    }
     if (this.stacked) {
+      console.log('slottedRadioButtonsChanged: layoutChanged');
+
       this.layoutChanged();
     }
   }
