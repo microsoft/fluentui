3b7d670261a16e26ace34b4e73a8d7488cd11d68
Marcos Moura <marcosvmmoura@gmail.com>
2023-03-08 19:19:55 +0100

    feat: Release Card as stable

--- packages/react-components/react-components/src/unstable/index.ts
+++ packages/react-components/react-components/src/unstable/index.ts
@@ -8,44 +8,6 @@ export {
   useAlert_unstable,
 } from '@fluentui/react-alert';
 export type { AlertProps, AlertSlots, AlertState } from '@fluentui/react-alert';
-export {
-  Card,
-  CardFooter,
-  CardHeader,
-  CardPreview,
-  cardClassNames,
-  cardCSSVars,
-  cardFooterClassNames,
-  cardHeaderClassNames,
-  cardHeaderCSSVars,
-  cardPreviewClassNames,
-  renderCard_unstable,
-  renderCardFooter_unstable,
-  renderCardHeader_unstable,
-  renderCardPreview_unstable,
-  useCard_unstable,
-  useCardFooter_unstable,
-  useCardFooterStyles_unstable,
-  useCardHeader_unstable,
-  useCardHeaderStyles_unstable,
-  useCardPreview_unstable,
-  useCardPreviewStyles_unstable,
-  useCardStyles_unstable,
-} from '@fluentui/react-card';
-export type {
-  CardFooterProps,
-  CardFooterSlots,
-  CardFooterState,
-  CardHeaderProps,
-  CardHeaderSlots,
-  CardHeaderState,
-  CardPreviewProps,
-  CardPreviewSlots,
-  CardPreviewState,
-  CardProps,
-  CardSlots,
-  CardState,
-} from '@fluentui/react-card';
 
 export {
   InfoButton,
