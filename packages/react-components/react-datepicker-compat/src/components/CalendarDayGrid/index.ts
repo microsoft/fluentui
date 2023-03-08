d1941f63a9f01cfd22e5d25bee729db501f55458
Esteban Munoz <esteban.230@hotmail.com>
2023-03-08 14:46:41 -0800

    minor format change

--- packages/react-components/react-datepicker-compat/src/components/CalendarDayGrid/index.ts
+++ packages/react-components/react-datepicker-compat/src/components/CalendarDayGrid/index.ts
@@ -1,5 +1,5 @@
 export * from './CalendarDayGrid';
 export * from './CalendarDayGrid.types';
 export * from './useCalendarDayGridStyles';
-export type { WeekCorners } from './useWeekCornerStyles';
 export { calendarDayGridClassNames, extraCalendarDayGridClassNames } from './useCalendarDayGridStyles';
+export type { WeekCorners } from './useWeekCornerStyles';
