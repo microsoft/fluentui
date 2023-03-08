a09fedfabffd50dbe407b74ed0db2e339ea0e913
Levi Thomason <me@levithomason.com>
2023-03-08 08:27:29 -0800

    Web Component v3 Stories (#27049)
    * Flatten badge stories in sidebar
    
    * undo badge flattening
    
    * add divider stories
    
    * progress stories
    
    * spinner stories
    
    * switch stories
    
    * text stories
    
    * remove text as story

--- packages/web-components/src/progress-bar/progress-bar.stories.ts
+++ packages/web-components/src/progress-bar/progress-bar.stories.ts
@@ -64,6 +64,76 @@ export default {
 
 export const Progress = renderComponent(storyTemplate).bind({});
 
-export const ProgressIndeterminate = renderComponent(html<ProgressStoryArgs>`
-  <fluent-progress-bar title="Indeterminate Bar" aria-label="Indeterminate progress bar"></fluent-progress-bar>
+//
+// Attribute stories
+//
+
+export const Max = renderComponent(html<ProgressStoryArgs>`
+  <div>
+    <p>
+      <code>3 of 10</code>
+      <fluent-progress-bar value="3" aria-valuenow="3" max="10" aria-valuemax="10"></fluent-progress-bar>
+    </p>
+    <p>
+      <code>3 o 5</code>
+      <fluent-progress-bar value="3" aria-valuenow="3" max="5" aria-valuemax="5"></fluent-progress-bar>
+    </p>
+  </div>
+`);
+
+export const Value = renderComponent(html<ProgressStoryArgs>`
+  <div>
+    <code>0</code><fluent-progress-bar value="0" aria-valuenow="0"></fluent-progress-bar>
+    <code>25</code>
+    <fluent-progress-bar value="25" aria-valuenow="25"></fluent-progress-bar>
+    <code>50</code>
+    <fluent-progress-bar value="50" aria-valuenow="50"></fluent-progress-bar>
+    <code>75</code>
+    <fluent-progress-bar value="75" aria-valuenow="75"></fluent-progress-bar>
+    <code>100</code>
+    <fluent-progress-bar value="100" aria-valuenow="100"></fluent-progress-bar>
+  </div>
+`);
+
+export const Thickness = renderComponent(html<ProgressStoryArgs>`
+  <div>
+    <p>
+      <code>medium</code>
+      <fluent-progress-bar thickness="medium"></fluent-progress-bar>
+    </p>
+    <p>
+      <code>large</code>
+      <fluent-progress-bar thickness="large"></fluent-progress-bar>
+    </p>
+  </div>
+`);
+
+export const Shape = renderComponent(html<ProgressStoryArgs>`
+  <div>
+    <p>
+      <code>rounded</code>
+      <fluent-progress-bar shape="rounded" thickness="large"></fluent-progress-bar>
+    </p>
+    <p>
+      <code>square</code>
+      <fluent-progress-bar shape="square" thickness="large"></fluent-progress-bar>
+    </p>
+  </div>
+`);
+
+export const ValidationState = renderComponent(html<ProgressStoryArgs>`
+  <div>
+    <p>
+      <code>success</code>
+      <fluent-progress-bar validation-state="success"></fluent-progress-bar>
+    </p>
+    <p>
+      <code>warning</code>
+      <fluent-progress-bar validation-state="warning"></fluent-progress-bar>
+    </p>
+    <p>
+      <code>error</code>
+      <fluent-progress-bar validation-state="error"></fluent-progress-bar>
+    </p>
+  </div>
 `);
