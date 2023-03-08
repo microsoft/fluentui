7d1a2cd4144a808c85b77d32130b19655678f804
Esteban Munoz <esteban.230@hotmail.com>
2023-03-07 16:48:39 -0800

    revert example

--- packages/react-components/react-datepicker-compat/stories/Calendar/CalendarDefault.stories.tsx
+++ packages/react-components/react-datepicker-compat/stories/Calendar/CalendarDefault.stories.tsx
@@ -1,16 +1,4 @@
 import * as React from 'react';
-import { Calendar, CalendarProps, DateRangeType } from '@fluentui/react-datepicker-compat';
-import { tokens } from '@fluentui/react-components';
-import { makeResetStyles } from '@griffel/react';
+import { Calendar, CalendarProps } from '@fluentui/react-datepicker-compat';
 
-const useStyles = makeResetStyles({
-  backgroundColor: tokens.colorNeutralBackground1,
-  padding: '30px',
-});
-
-export const Default = (props: Partial<CalendarProps>) => (
-  <div className={useStyles()}>
-    {/* <Calendar {...props} /> */}
-    <Calendar dateRangeType={DateRangeType.Month} highlightSelectedMonth showGoToToday {...props} />
-  </div>
-);
+export const Default = (props: Partial<CalendarProps>) => <Calendar {...props} />;
