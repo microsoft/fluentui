cb36fda9e47e3244dec3525f7ed0145423d74b6a
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2023-03-09 11:19:01 -0800

    Adds Fluent 2 focus styles to search box (#27144)
    * search box focus styles, create utils
    
    * change file

--- packages/fluent2-theme/src/componentStyles/SearchBox.styles.ts
+++ packages/fluent2-theme/src/componentStyles/SearchBox.styles.ts
@@ -1,32 +1,50 @@
 import type { ISearchBoxStyleProps, ISearchBoxStyles, IStyleFunctionOrObject } from '@fluentui/react';
-import { getInputFocusStyle } from '@fluentui/react';
+import { getFluent2InputDisabledStyles, getFluent2InputFocusStyles } from './inputStyleHelpers.utils';
 
 export function getSearchBoxStyles(
   props: ISearchBoxStyleProps,
 ): IStyleFunctionOrObject<ISearchBoxStyleProps, ISearchBoxStyles> {
-  const { hasFocus, underlined, theme } = props;
+  const { hasFocus, underlined, disabled, theme } = props;
+  const { palette } = theme;
+
+  const restBottomBorder = `1px solid ${palette.neutralPrimary}`;
 
   const styles: Partial<ISearchBoxStyles> = {
     root: [
       {
         borderRadius: underlined ? 0 : theme.effects.roundedCorner4,
+        borderBottom: restBottomBorder,
       },
 
       hasFocus && [
         'is-active',
         {
           position: 'relative',
+          borderColor: 'unset',
         },
-        getInputFocusStyle(
-          theme.semanticColors.inputFocusBorderAlt,
-          underlined ? 0 : theme.effects.roundedCorner4,
-          underlined ? 'borderBottom' : 'border',
-        ),
+        getFluent2InputFocusStyles(theme),
+      ],
+
+      disabled && ['is-disabled', getFluent2InputDisabledStyles(theme)],
+    ],
+    field: [
+      disabled && {
+        '::placeholder': {
+          color: palette.neutralQuaternaryAlt,
+        },
+      },
+    ],
+    iconContainer: [
+      {
+        color: theme.palette.neutralPrimary,
+      },
+      disabled && [
+        'is-disabled',
+        {
+          color: palette.neutralQuaternaryAlt,
+        },
       ],
     ],
-    iconContainer: {
-      color: theme.palette.neutralPrimary,
-    },
   };
 
   return styles;
