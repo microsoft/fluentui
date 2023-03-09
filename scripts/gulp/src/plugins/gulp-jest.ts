053eb560ffb286508701da27cdd231ef87a5177d
Martin Hochel <martinhochel@microsoft.com>
2023-03-09 10:51:59 +0100

    test: improve jest execution speeds by removing -i and --coverage from v8,v0

--- scripts/gulp/src/plugins/gulp-jest.ts
+++ scripts/gulp/src/plugins/gulp-jest.ts
@@ -2,6 +2,7 @@ import { sh } from '@fluentui/scripts-utils';
 
 export type JestPluginConfig = {
   config: string;
+  cache?: boolean;
   coverage?: boolean;
   detectLeaks?: boolean;
   maxWorkers?: number;
@@ -25,12 +26,12 @@ const jest = (config: JestPluginConfig) => () => {
     config.coverage && '--coverage',
     config.watchAll && '--watchAll',
     config.runInBand && '--runInBand',
-    // eslint-disable-next-line eqeqeq
-    config.maxWorkers != null && `--maxWorkers=${config.maxWorkers}`,
+    config.maxWorkers && `--maxWorkers=${config.maxWorkers}`,
     config.detectLeaks && '--detectLeaks',
     config.testNamePattern && `--testNamePattern="${config.testNamePattern}"`,
     config.rootDir && `--rootDir ${config.rootDir}`,
     config.verbose && '--verbose',
+    config.cache === true ? '' : '--no-cache',
     config.testFilePattern, // !!! THIS ITEM MUST GO LAST IN THE ARRAY !!!
   ]
     .filter(Boolean)
