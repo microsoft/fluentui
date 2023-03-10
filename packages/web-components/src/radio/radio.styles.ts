f273e93e14f3cf2097e0a3901688310e35932f79
Brian Brady <brianbrady@microsoft.com>
2023-03-09 16:38:15 -0800

    merge

--- packages/web-components/src/radio/radio.styles.ts
+++ packages/web-components/src/radio/radio.styles.ts
@@ -33,6 +33,8 @@ export const styles = css`
 
   :host {
     align-items: center;
+    cursor: pointer;
+    display: flex;
     flex-direction: row;
     outline: none;
     position: relative;
@@ -41,60 +43,51 @@ export const styles = css`
   .label {
     color: ${colorNeutralForeground3};
     cursor: pointer;
-    padding: ${spacingVerticalS} ${spacingHorizontalS} ${spacingVerticalS} ${spacingHorizontalXS};
     font-family: ${fontFamilyBase};
     font-size: ${fontSizeBase300};
     font-weight: ${fontWeightRegular};
     line-height: ${lineHeightBase300};
+    padding: ${spacingVerticalS} ${spacingHorizontalS} ${spacingVerticalS} ${spacingHorizontalXS};
   }
-  :host(:hover) .label {
-    color: ${colorNeutralForeground2};
-  }
+
   .label__hidden {
     display: none;
     visibility: hidden;
   }
-  .control,
-  .checked-indicator {
-    flex-shrink: 0;
-  }
   .control {
-    display: flex;
-    justify-content: center;
     align-items: center;
+    border: 1px solid ${colorNeutralStrokeAccessible};
+    border-radius: 50%;
+    display: flex;
+    height: 16px;
+    justify-content: center;
+    margin: ${spacingVerticalS} ${spacingHorizontalS};
     position: relative;
     width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    border: 1px solid ${colorNeutralStrokeAccessible};
-    margin: ${spacingVerticalS} ${spacingHorizontalS};
-  }
-  :host(:hover) .control {
-    border-color: ${colorNeutralStrokeAccessibleHover};
   }
   .checked-indicator {
+    border-radius: 5px;
+    height: 10px;
     opacity: 0;
     width: 10px;
-    height: 10px;
-    border-radius: 5px;
-    background-color: #ccc;
   }
 
   :host(:focus-visible)::after {
-    content: '';
-    position: absolute;
-    inset: 0px;
-    cursor: pointer;
-    border-radius: ${borderRadiusMedium};
-    outline: none;
     border: 2px solid ${colorStrokeFocus1};
+    border-radius: ${borderRadiusMedium};
     box-shadow: inset 0 0 0 1px ${colorStrokeFocus2};
+    content: '';
+    cursor: pointer;
+    inset: 0px;
+    outline: none;
+    position: absolute;
   }
 
   :host([stack]) {
     flex-direction: column;
     justify-content: center;
   }
+
   :host([stack]) .label {
     padding: ${spacingVerticalXS} ${spacingHorizontalS} ${spacingVerticalS} ${spacingHorizontalS};
   }
@@ -106,13 +99,15 @@ export const styles = css`
   :host(:not([disabled]):hover) .label {
     color: ${colorNeutralForeground2};
   }
-  :host(:not([disabled])) .control:active {
-    background: gray;
-    border-color: darkgray;
+
+  :host(:not([disabled]):active) .label {
+    color: ${colorNeutralForeground1};
   }
+
   :host(:not([disabled]):hover) .control {
     border-color: ${colorNeutralStrokeAccessibleHover};
   }
+
   :host(:not([disabled]):active) .control {
     border-color: ${colorNeutralStrokeAccessiblePressed};
   }
@@ -124,20 +119,24 @@ export const styles = css`
   :host([aria-checked='true']:not([disabled])) .label {
     color: ${colorNeutralForeground1};
   }
+
   :host([aria-checked='true']:not([disabled])) .control {
     border-color: ${colorCompoundBrandStroke};
   }
+
   :host([aria-checked='true']:not([disabled])) .checked-indicator {
-    display: block;
     background-color: ${colorCompoundBrandForeground1};
+    display: block;
   }
 
   :host([aria-checked='true']:hover:not([disabled])) .control {
     border-color: ${colorCompoundBrandStrokeHover};
   }
+
   :host([aria-checked='true']:hover:not([disabled])) .checked-indicator {
     border-color: ${colorCompoundBrandStrokeHover};
   }
+
   :host([aria-checked='true']:hover:not([disabled])) .label {
     border-color: ${colorNeutralForeground1};
   }
@@ -145,9 +144,11 @@ export const styles = css`
   :host([aria-checked='true']:active:not([disabled])) .label {
     color: ${colorNeutralForeground1};
   }
+
   :host([aria-checked='true']:active:not([disabled])) .control {
     border-color: ${colorCompoundBrandStrokePressed};
   }
+
   :host([aria-checked='true']:active:not([disabled])) .checked-indicator {
     background: ${colorCompoundBrandForeground1Pressed};
   }
@@ -155,6 +156,7 @@ export const styles = css`
   :host([disabled][aria-checked='true']) .control {
     border-color: ${colorNeutralForegroundDisabled};
   }
+
   :host([disabled][aria-checked='true']) .checked-indicator {
     background-color: ${colorNeutralForegroundDisabled};
   }
@@ -163,14 +165,17 @@ export const styles = css`
   :host([readonly]) {
     pointer-events: none;
   }
+
   :host([disabled]) .label,
   :host([readonly]) .label {
     color: ${colorNeutralForegroundDisabled};
   }
+
   :host([readonly]) .control,
   :host([disabled]) .control {
     border-color: ${colorNeutralForegroundDisabled};
   }
+
   :host([disabled]) .checked-indicator,
   :host([readonly]) .checked-indicator {
     background-color: ${colorNeutralForegroundDisabled};
