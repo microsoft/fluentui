c15a61d8dff8ebb9e25a51c8d0c8ca807b4a333d
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-03-10 18:58:06 +0000

    chore: removes unnecessary null verification

--- packages/react-components/react-utilities/src/utils/isHTMLElement.test.ts
+++ packages/react-components/react-utilities/src/utils/isHTMLElement.test.ts
@@ -17,27 +17,26 @@ const customDocument = {
 describe('isHTMLElement', () => {
   it('should behave the same as instanceof HTMLElement for elements on the same realm', () => {
     const element = document.createElement('div');
-    expect(isHTMLElement(element) === element instanceof HTMLElement).toBe(true);
-    expect(isHTMLElement({}) === {} instanceof HTMLElement).toBe(true);
+    expect(isHTMLElement(element)).toBe(element instanceof HTMLElement);
+    expect(isHTMLElement({})).toBe({} instanceof HTMLElement);
   });
   it('should behave the same as instanceof HTMLInputElement for elements on the same realm', () => {
     const element = document.createElement('input');
     expect(
       isHTMLElement(element, {
         constructorName: 'HTMLInputElement',
-      }) ===
-        element instanceof HTMLInputElement,
-    ).toBe(true);
-    expect(isHTMLElement({}, { constructorName: 'HTMLInputElement' }) === {} instanceof HTMLInputElement).toBe(true);
+      }),
+    ).toBe(element instanceof HTMLInputElement);
+    expect(isHTMLElement({}, { constructorName: 'HTMLInputElement' })).toBe({} instanceof HTMLInputElement);
   });
   it('should return true for instances of HTMLElement on different realms', () => {
     const element = new CustomHTMLElement();
-    expect(isHTMLElement(element)).toBe(true);
     expect(element instanceof HTMLElement).toBe(false);
+    expect(isHTMLElement(element)).toBe(true);
   });
   it('should return true for instances of HTMLInputElement on different realms', () => {
     const element = new CustomHTMLInputElement();
-    expect(isHTMLElement(element, { constructorName: 'HTMLInputElement' })).toBe(true);
     expect(element instanceof HTMLInputElement).toBe(false);
+    expect(isHTMLElement(element, { constructorName: 'HTMLInputElement' })).toBe(true);
   });
 });
