00c509a1992b83a28eaef80a5c2eb19cf19a4c9d
Ben Howell <behowell@microsoft.com>
2023-03-14 13:46:46 -0700

    Add tests

--- packages/react-components/react-infobutton/src/components/InfoLabel/InfoLabel.test.tsx
+++ packages/react-components/react-infobutton/src/components/InfoLabel/InfoLabel.test.tsx
@@ -20,8 +20,23 @@ describe('InfoLabel', () => {
     },
   });
 
-  it('sets the infoButton aria-labelledby to the label and infoButton', () => {
+  it('renders an InfoButton when content is set', () => {
+    const result = render(<InfoLabel content="Test">Test label</InfoLabel>);
+    expect(result.getByRole('button')).toBeTruthy();
+  });
+
+  it("renders an InfoButton when the infoButton slot's content is set", () => {
     const result = render(<InfoLabel infoButton={{ content: 'Test' }}>Test label</InfoLabel>);
+    expect(result.getByRole('button')).toBeTruthy();
+  });
+
+  it('does not render an InfoButton when content is not set', () => {
+    const result = render(<InfoLabel>Test label</InfoLabel>);
+    expect(result.queryByRole('button')).toBeNull();
+  });
+
+  it('sets the infoButton aria-labelledby to the label and infoButton', () => {
+    const result = render(<InfoLabel content="Test">Test label</InfoLabel>);
 
     const infoButton = result.getByRole('button');
     const label = result.getByText('Test label') as HTMLLabelElement;
