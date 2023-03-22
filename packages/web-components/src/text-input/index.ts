0040fbd61a38b8f5d5be9b6a32bb3bbd47721c25
Brian Brady <brianbrady@microsoft.com>
2023-03-22 12:07:03 -0700

    text input init

---
+++ packages/web-components/src/text-input/index.ts
@@ -0,0 +1,5 @@
+export * from './text-input.js';
+export * from './text-input.options.js';
+export { template as TextTemplate } from './text-input.template.js';
+export { styles as TextStyles } from './text-input.styles.js';
+export { definition as TextDefinition } from './text-input.definition.js';
