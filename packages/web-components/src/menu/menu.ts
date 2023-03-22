22009f787bff57e4aeeee77b7d616c8b238ab532
Brian Brady <brianbrady@microsoft.com>
2023-03-22 14:18:52 -0700

    adds docs to menu.ts

--- packages/web-components/src/menu/menu.ts
+++ packages/web-components/src/menu/menu.ts
@@ -5,10 +5,16 @@ import { MenuItem, MenuItemColumnCount } from '../index.js';
  * The base class used for constructing a fluent-menu custom element
  * @public
  */
+
 export class Menu extends FASTMenu {
   protected setItems(): void {
     super.setItems();
 
+    /**
+     * TSet the indent attribute on MenuItem elements based on their
+     * position in the Menu. Each MenuItem element has a data-indent attribute that is
+     * used to set the indent of the element's start slot content.
+     */
     const filteredMenuItems = this.menuItems?.filter(this.isMenuItemElement);
 
     filteredMenuItems?.forEach((item: HTMLElement, index: number) => {
