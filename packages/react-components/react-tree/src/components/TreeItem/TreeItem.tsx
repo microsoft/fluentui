38ea28a77927665cf032f616cd6ec9d40eae9dc6
Petr Duda <petrduda@microsoft.com>
2023-03-22 16:04:40 +0100

    fix treeitem description

--- packages/react-components/react-tree/src/components/TreeItem/TreeItem.tsx
+++ packages/react-components/react-tree/src/components/TreeItem/TreeItem.tsx
@@ -8,11 +8,14 @@ import { useTreeItemContextValues_unstable } from './useTreeItemContextValues';
 
 /**
  * The `TreeItem` component represents a single item in a tree.
- * It can contain child items in the form of another Tree component or standalone TreeItem.
- * The content and layout of a TreeItem can be defined using the `TreeItemLayout` or `TreeItemPersonaLayout` component,
+ * It expects a certain order of children to work properly: the first child should be the node itself,
+ * and the second child should be a nested subtree in the form of another Tree component or a standalone TreeItem.
+ * This order follows the same order as document traversal for the TreeWalker API in JavaScript:
+ * https://developer.mozilla.org/en-US/docs/Web/API/TreeWalker.
+ * The content and layout of a TreeItem can be defined using the TreeItemLayout or TreeItemPersonaLayout component,
  * which should be used as a direct child of TreeItem.
  *
- * When a TreeItem has child items, an expand/collapse control is displayed,
+ * When a TreeItem has nsted child subtree, an expand/collapse control is displayed,
  * allowing the user to show or hide the children.
  */
 export const TreeItem: ForwardRefComponent<TreeItemProps> = React.forwardRef((props, ref) => {
