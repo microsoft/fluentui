64d2ec1f40281eab9337df6d6065edef9ad0d312
ling1726 <lingfangao@hotmail.com>
2023-03-23 15:54:47 +0100

    feat: Render theme CSS variables in SSR style element (#27277)
    * feat: Render theme CSS variables in new SSR style slot
    
    Adds a new `serverStyle` slot to the `FluentProvider` that is only
    rendered when the provider is a child of an `SSRProvider`.
    
    Also updates `useFluentProviderThemeTag` to return the CSS rule that
    contains all CSS variables.
    
    This PR makes use of dangerous HTML, since react will output certain
    characters in unicode which can result in invalid CSS, [this approach is
    also taken by Emotion](https://github.com/emotion-js/emotion/blob/89b6dbb3c13d49ef1fa3d88888672d810853f05a/packages/react/src/emotion-element.js#L72-L78)
    
    * changefile
    
    * revert
    
    * remove mock
    
    * add explainer
    
    * update md
    
    * add nonce
    
    * make slot private
    
    * Remove SSR element on first render
    
    * remove changefile
    
    * pr feedback
    
    * update changfile
    
    * stop using query selector
    
    * pass renderer to useFluentpRoviderThemeStyleTag hook
    
    * rename rendererAttributes to attributes
    
    * simplify logic
    
    * remove data attribute
    
    * use PartialTheme type
    
    * remove SSRPRovider from tests
    
    * remove cruft
    
    * use ternary render
    
    * update md
    
    * only pass attributes
    
    * remove ssr provider
    
    * fix tests
    
    * update md

--- packages/react-components/react-provider/src/components/FluentProvider/FluentProvider.test.tsx
+++ packages/react-components/react-provider/src/components/FluentProvider/FluentProvider.test.tsx
@@ -1,10 +1,11 @@
 import { resetIdsForTests } from '@fluentui/react-utilities';
 import { render } from '@testing-library/react';
 import * as React from 'react';
-import * as renderer from 'react-test-renderer';
+import * as reactTestRenderer from 'react-test-renderer';
 
 import { FluentProvider } from './FluentProvider';
 import { isConformant } from '../../testing/isConformant';
+import { teamsLightTheme } from '@fluentui/react-theme';
 
 describe('FluentProvider', () => {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -28,11 +29,16 @@ describe('FluentProvider', () => {
    * Note: see more visual regression tests for FluentProvider in /apps/vr-tests.
    */
   it('renders a default state', () => {
-    const component = renderer.create(
+    const component = reactTestRenderer.create(
       <FluentProvider theme={{ colorBrandBackground2: '#fff' }}>Default FluentProvider</FluentProvider>,
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('does not render style element when not in SSR', () => {
+    const { container } = render(<FluentProvider theme={teamsLightTheme} />);
+    expect(container.querySelector('style')).toBeNull();
   });
 
   describe('applies "dir" attribute', () => {
