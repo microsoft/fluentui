c058a2e2c14fb15a08d3c294f65810d4e3cad746
Brian Brady <brianbrady@microsoft.com>
2023-03-23 14:42:58 -0700

    removes redundant stack attribute

--- packages/web-components/src/radio/radio.ts
+++ packages/web-components/src/radio/radio.ts
@@ -5,14 +5,4 @@ import { FASTRadio } from '@microsoft/fast-foundation';
  * The base class used for constructing a fluent-radio custom element
  * @public
  */
-export class Radio extends FASTRadio {
-  /**
-   * sets radio layout styles
-   *
-   * @public
-   * @remarks
-   * HTML Attribute: stack
-   */
-  @attr({ mode: 'boolean' })
-  public stack: boolean = false;
-}
+export class Radio extends FASTRadio {}
