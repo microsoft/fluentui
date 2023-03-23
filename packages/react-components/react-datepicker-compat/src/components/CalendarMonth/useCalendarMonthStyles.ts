34dec1d344736ea7be694d29c9429c40f5cb6c9e
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-03-23 13:58:07 -0700

    chore(react-datepicker-compat): Cleanup types, unused code, and restore utils tests (#27297)
    * updating specific path
    
    * Removing navigated icons strings
    
    * removing calendar exports and restoring utils tests
    
    * requested changes
    
    * updating @defaultValue to @default
    
    * disabling classnames exported at top level test

--- packages/react-components/react-datepicker-compat/src/components/CalendarMonth/useCalendarMonthStyles.ts
+++ packages/react-components/react-datepicker-compat/src/components/CalendarMonth/useCalendarMonthStyles.ts
@@ -1,11 +1,9 @@
 import { useCalendarPickerStyles_unstable } from '../CalendarPicker/useCalendarPickerStyles';
-import type { SlotClassNames } from '@fluentui/react-utilities';
-import type { CalendarMonthSlots, CalendarMonthStyleProps, CalendarMonthStyles } from './CalendarMonth.types';
+import type { CalendarMonthStyleProps, CalendarMonthStyles } from './CalendarMonth.types';
 
-export const calendarMonthClassNames: SlotClassNames<CalendarMonthSlots> = {
-  root: 'fui-CalendarMonth',
-};
 /**
+ *  @internal
+ *
  * Apply styling to the CalendarMonth slots based on the state
  */
 export const useCalendarMonthStyles_unstable = (props: CalendarMonthStyleProps): CalendarMonthStyles => {
--- packages/react-components/react-datepicker-compat/src/components/CalendarPicker/index.ts
+++ packages/react-components/react-datepicker-compat/src/components/CalendarPicker/index.ts
@@ -1,6 +1,2 @@
-// export * from './CalendarPicker';
-// export * from './CalendarPicker.styles';
 export * from './CalendarPicker.types';
-// export * from './renderCalendarPicker';
-// export * from './useCalendarPicker';
 export * from './useCalendarPickerStyles';
