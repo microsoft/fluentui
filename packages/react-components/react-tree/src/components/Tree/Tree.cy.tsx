f3c520f0ce019ca22ea99605e2fb09b4fcd8c35f
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-03-24 13:46:56 +0000

    chore: improves tests

--- packages/react-components/react-tree/src/components/Tree/Tree.cy.tsx
+++ packages/react-components/react-tree/src/components/Tree/Tree.cy.tsx
@@ -9,240 +9,298 @@ import {
   TreeProps,
   treeItemLayoutClassNames,
   treeItemClassNames,
+  useFlatTree_unstable,
 } from '@fluentui/react-tree';
 import { Button } from '@fluentui/react-button';
+import { flattenTreeFromElement } from '../../utils/flattenTree';
 
 const mount = (element: JSX.Element) => {
   mountBase(<FluentProvider theme={teamsLightTheme}>{element}</FluentProvider>);
 };
 
-const Example = (props: TreeProps) => {
+const NestedTree: React.FC<TreeProps> = props => {
   return (
     <Tree id="tree" aria-label="Tree" {...props}>
-      <TreeItem id="item1">
-        <TreeItemLayout>level 1, item 1</TreeItemLayout>
-        <Tree>
-          <TreeItem id="item1__item1">
-            <TreeItemLayout>level 2, item 1</TreeItemLayout>
-          </TreeItem>
-          <TreeItem id="item1__item2">
-            <TreeItemLayout>level 2, item 2</TreeItemLayout>
-          </TreeItem>
-          <TreeItem id="item1__item3">
-            <TreeItemLayout>level 2, item 3</TreeItemLayout>
-          </TreeItem>
-        </Tree>
-      </TreeItem>
-      <TreeItem id="item2">
-        <TreeItemLayout>level 1, item 2</TreeItemLayout>
-        <Tree>
-          <TreeItem id="item2__item1">
-            <TreeItemLayout>level 2, item 1</TreeItemLayout>
+      {props.children ?? (
+        <>
+          <TreeItem id="item1">
+            <TreeItemLayout>level 1, item 1</TreeItemLayout>
             <Tree>
-              <TreeItem id="item2__item1__item1">
-                <TreeItemLayout>level 3, item 1</TreeItemLayout>
+              <TreeItem id="item1__item1">
+                <TreeItemLayout>level 2, item 1</TreeItemLayout>
+              </TreeItem>
+              <TreeItem id="item1__item2">
+                <TreeItemLayout>level 2, item 2</TreeItemLayout>
+              </TreeItem>
+              <TreeItem id="item1__item3">
+                <TreeItemLayout>level 2, item 3</TreeItemLayout>
               </TreeItem>
             </Tree>
           </TreeItem>
-        </Tree>
-      </TreeItem>
+          <TreeItem id="item2">
+            <TreeItemLayout>level 1, item 2</TreeItemLayout>
+            <Tree>
+              <TreeItem id="item2__item1">
+                <TreeItemLayout>level 2, item 1</TreeItemLayout>
+                <Tree>
+                  <TreeItem id="item2__item1__item1">
+                    <TreeItemLayout>level 3, item 1</TreeItemLayout>
+                  </TreeItem>
+                </Tree>
+              </TreeItem>
+            </Tree>
+          </TreeItem>
+        </>
+      )}
     </Tree>
   );
 };
+NestedTree.displayName = 'NestedTree';
 
-describe('Tree', () => {
-  it('should have all but first level items hidden', () => {
-    mount(<Example />);
-    cy.get('#item1__item1').should('not.exist');
-    cy.get('#item1__item2').should('not.exist');
-    cy.get('#item1__item3').should('not.exist');
-    cy.get('#item2__item1').should('not.exist');
-    cy.get('#item2__item1__item1').should('not.exist');
-  });
-
-  it('should have all items visible', () => {
-    mount(<Example defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
-    cy.get('#item1__item1').should('exist');
-    cy.get('#item1__item2').should('exist');
-    cy.get('#item1__item3').should('exist');
-    cy.get('#item2__item1').should('exist');
-    cy.get('#item2__item1__item1').should('exist');
-  });
-
-  describe('Mouse interactions', () => {
-    it('should expand/collapse item on layout click', () => {
-      mount(<Example />);
-      cy.get('#item1__item1').should('not.exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
-      cy.get('#item1__item1').should('exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
-      cy.get('#item1__item1').should('not.exist');
-    });
-    it('should expand/collapse item on expandIcon click only', () => {
-      mount(
-        <Example
-          onOpenChange={(event, data) => {
-            if (data.type === 'Click') {
-              event.preventDefault();
-            }
-          }}
-        />,
-      );
-      cy.get('#item1__item1').should('not.exist');
-      cy.get(`#item1 .${treeItemClassNames.expandIcon}`).realClick();
-      cy.get('#item1__item1').should('exist');
-      cy.get(`#item1 .${treeItemClassNames.expandIcon}`).realClick();
-      cy.get('#item1__item1').should('not.exist');
-
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
-      cy.get('#item1__item1').should('not.exist');
-    });
-    it('should not expand/collapse item on actions click', () => {
-      mount(
-        <Tree id="tree" aria-label="Tree">
-          <TreeItem
-            actions={
-              <>
-                <Button id="action">action</Button>
-              </>
-            }
-            id="item1"
-          >
+const FlatTree: React.FC<TreeProps> = (props: TreeProps) => {
+  const flatTree = useFlatTree_unstable(
+    flattenTreeFromElement(
+      props.children ? (
+        <>{props.children}</>
+      ) : (
+        <>
+          <TreeItem id="item1">
             <TreeItemLayout>level 1, item 1</TreeItemLayout>
             <Tree>
               <TreeItem id="item1__item1">
                 <TreeItemLayout>level 2, item 1</TreeItemLayout>
               </TreeItem>
-            </Tree>
-          </TreeItem>
-        </Tree>,
-      );
-      cy.get('#item1__item1').should('not.exist');
-      cy.get('#item1').focus();
-      cy.get(`#action`).realClick();
-      cy.get('#item1__item1').should('not.exist');
-    });
-  });
-  describe('Keyboard interactions', () => {
-    it('should expand/collapse item on Enter key', () => {
-      mount(<Example />);
-      cy.get('#item1').focus();
-      cy.get('#item1__item1').should('not.exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{enter}');
-      cy.get('#item1__item1').should('exist');
-    });
-    it('should expand item on Right key', () => {
-      mount(<Example />);
-      cy.get('#item1').focus();
-      cy.get('#item1__item1').should('not.exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{rightarrow}');
-      cy.get('#item1__item1').should('exist');
-    });
-    it('should collapse item on Left key', () => {
-      mount(<Example />);
-      cy.get('#item1').focus();
-      cy.get('#item1__item1').should('not.exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{rightarrow}');
-      cy.get('#item1__item1').should('exist');
-      cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{leftarrow}');
-      cy.get('#item1__item1').should('not.exist');
-    });
-    it('should focus on actions when pressing tab key', () => {
-      mount(
-        <Tree id="tree" aria-label="Tree">
-          <TreeItem
-            actions={
-              <>
-                <Button id="action">action</Button>
-              </>
-            }
-            id="item1"
-          >
-            <TreeItemLayout>level 1, item 1</TreeItemLayout>
-            <Tree>
-              <TreeItem id="item1__item1">
-                <TreeItemLayout>level 2, item 1</TreeItemLayout>
+              <TreeItem id="item1__item2">
+                <TreeItemLayout>level 2, item 2</TreeItemLayout>
+              </TreeItem>
+              <TreeItem id="item1__item3">
+                <TreeItemLayout>level 2, item 3</TreeItemLayout>
               </TreeItem>
             </Tree>
           </TreeItem>
-        </Tree>,
-      );
-      cy.focused().should('not.exist');
-      cy.document().realPress('Tab');
-      cy.get('#item1').should('be.focused');
-      cy.document().realPress('Tab');
-      cy.get('#action').should('be.focused');
-    });
-    it('should not expand/collapse item on actions Enter/Space key', () => {
-      mount(
-        <Tree id="tree" aria-label="Tree">
-          <TreeItem
-            actions={
-              <>
-                <Button id="action">action</Button>
-              </>
-            }
-            id="item1"
-          >
-            <TreeItemLayout>level 1, item 1</TreeItemLayout>
+          <TreeItem id="item2">
+            <TreeItemLayout>level 1, item 2</TreeItemLayout>
             <Tree>
-              <TreeItem id="item1__item1">
+              <TreeItem id="item2__item1">
                 <TreeItemLayout>level 2, item 1</TreeItemLayout>
+                <Tree>
+                  <TreeItem id="item2__item1__item1">
+                    <TreeItemLayout>level 3, item 1</TreeItemLayout>
+                  </TreeItem>
+                </Tree>
               </TreeItem>
             </Tree>
           </TreeItem>
-        </Tree>,
-      );
-      cy.focused().should('not.exist');
-      cy.document().realPress('Tab');
-      cy.get('#item1').should('be.focused');
-      cy.document().realPress('Tab');
-      cy.get('#action').should('be.focused').realPress('{enter}');
+        </>
+      ),
+    ),
+    props,
+  );
+  return (
+    <Tree {...props} {...flatTree.getTreeProps()} id="tree" aria-label="Tree">
+      {Array.from(flatTree.items(), item => (
+        <TreeItem key={item.id} {...item.getTreeItemProps()} />
+      ))}
+    </Tree>
+  );
+};
+FlatTree.displayName = 'FlatTree';
+
+for (const TreeTest of [NestedTree, FlatTree]) {
+  describe(TreeTest.displayName!, () => {
+    it('should have all but first level items hidden', () => {
+      mount(<TreeTest />);
       cy.get('#item1__item1').should('not.exist');
-      cy.get('#action').should('be.focused').realPress('Space');
-      cy.get('#item1__item1').should('not.exist');
+      cy.get('#item1__item2').should('not.exist');
+      cy.get('#item1__item3').should('not.exist');
+      cy.get('#item2__item1').should('not.exist');
+      cy.get('#item2__item1__item1').should('not.exist');
     });
-    it('should focus on first item when pressing tab key', () => {
-      mount(<Example />);
-      cy.focused().should('not.exist');
-      cy.document().realPress('Tab');
-      cy.get('#item1').should('be.focused');
+
+    it('should have all items visible', () => {
+      mount(<TreeTest defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
+      cy.get('#item1__item1').should('exist');
+      cy.get('#item1__item2').should('exist');
+      cy.get('#item1__item3').should('exist');
+      cy.get('#item2__item1').should('exist');
+      cy.get('#item2__item1__item1').should('exist');
     });
-    it('should focus out of tree when pressing tab key inside tree.', () => {
-      mount(<Example />);
-      cy.focused().should('not.exist');
-      cy.document().realPress('Tab');
-      cy.get('#item1').should('be.focused');
-      cy.focused().realPress('Tab');
-      cy.focused().should('not.exist');
+
+    describe('Mouse interactions', () => {
+      it('should expand/collapse item on layout click', () => {
+        mount(<TreeTest />);
+        cy.get('#item1__item1').should('not.exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
+        cy.get('#item1__item1').should('exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
+        cy.get('#item1__item1').should('not.exist');
+      });
+      it('should expand/collapse item on expandIcon click only', () => {
+        mount(
+          <TreeTest
+            onOpenChange={(event, data) => {
+              if (data.type === 'Click') {
+                event.preventDefault();
+              }
+            }}
+          />,
+        );
+        cy.get('#item1__item1').should('not.exist');
+        cy.get(`#item1 .${treeItemClassNames.expandIcon}`).realClick();
+        cy.get('#item1__item1').should('exist');
+        cy.get(`#item1 .${treeItemClassNames.expandIcon}`).realClick();
+        cy.get('#item1__item1').should('not.exist');
+
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realClick();
+        cy.get('#item1__item1').should('not.exist');
+      });
+      it('should not expand/collapse item on actions click', () => {
+        mount(
+          <TreeTest id="tree" aria-label="Tree">
+            <TreeItem
+              actions={
+                <>
+                  <Button id="action">action</Button>
+                </>
+              }
+              id="item1"
+            >
+              <TreeItemLayout>level 1, item 1</TreeItemLayout>
+              <Tree>
+                <TreeItem id="item1__item1">
+                  <TreeItemLayout>level 2, item 1</TreeItemLayout>
+                </TreeItem>
+              </Tree>
+            </TreeItem>
+          </TreeTest>,
+        );
+        cy.get('#item1__item1').should('not.exist');
+        cy.get('#item1').focus();
+        cy.get(`#action`).realClick();
+        cy.get('#item1__item1').should('not.exist');
+      });
     });
-    describe('Navigation', () => {
-      it('should move with Up/Down keys', () => {
-        mount(<Example />);
-        cy.get('#item1').focus().realPress('{downarrow}');
-        cy.get('#item2').should('be.focused');
-        cy.focused().realPress('Tab').should('not.exist');
+    describe('Keyboard interactions', () => {
+      it('should expand/collapse item on Enter key', () => {
+        mount(<TreeTest />);
+        cy.get('#item1').focus();
+        cy.get('#item1__item1').should('not.exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{enter}');
+        cy.get('#item1__item1').should('exist');
       });
-      it('should move with Left/Right keys', () => {
-        mount(<Example defaultOpenItems={['item2', 'item2__item1']} />);
-        cy.get('#item1').focus().realPress('{downarrow}');
-        cy.get('#item2').should('be.focused').realPress('{rightarrow}');
-        cy.get('#item2__item1').should('be.focused').realPress('{rightarrow}');
-        cy.get('#item2__item1__item1').should('be.focused').realPress('{leftarrow}');
-        cy.get('#item2__item1').should('be.focused').realPress('{leftarrow}').realPress('{leftarrow}');
-        cy.get('#item2').should('be.focused');
+      it('should expand item on Right key', () => {
+        mount(<TreeTest />);
+        cy.get('#item1').focus();
+        cy.get('#item1__item1').should('not.exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{rightarrow}');
+        cy.get('#item1__item1').should('exist');
       });
-      it('should move to last item with End key', () => {
-        mount(<Example defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
-        cy.get('#item1').focus().realPress('{end}');
-        cy.get('#item2__item1__item1').should('be.focused');
+      it('should collapse item on Left key', () => {
+        mount(<TreeTest />);
+        cy.get('#item1').focus();
+        cy.get('#item1__item1').should('not.exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{rightarrow}');
+        cy.get('#item1__item1').should('exist');
+        cy.get(`#item1 .${treeItemLayoutClassNames.root}`).realPress('{leftarrow}');
+        cy.get('#item1__item1').should('not.exist');
       });
-      it('should move to first item with Home key', () => {
-        mount(<Example defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
-        cy.get('#item1').focus().realPress('{end}');
-        cy.get('#item2__item1__item1').should('be.focused').realPress('{home}');
+      it('should focus on actions when pressing tab key', () => {
+        mount(
+          <TreeTest id="tree" aria-label="Tree">
+            <TreeItem
+              actions={
+                <>
+                  <Button id="action">action</Button>
+                </>
+              }
+              id="item1"
+            >
+              <TreeItemLayout>level 1, item 1</TreeItemLayout>
+              <Tree>
+                <TreeItem id="item1__item1">
+                  <TreeItemLayout>level 2, item 1</TreeItemLayout>
+                </TreeItem>
+              </Tree>
+            </TreeItem>
+          </TreeTest>,
+        );
+        cy.focused().should('not.exist');
+        cy.document().realPress('Tab');
+        cy.get('#item1').should('be.focused');
+        cy.document().realPress('Tab');
+        cy.get('#action').should('be.focused');
+      });
+      it('should not expand/collapse item on actions Enter/Space key', () => {
+        mount(
+          <TreeTest id="tree" aria-label="Tree">
+            <TreeItem
+              actions={
+                <>
+                  <Button id="action">action</Button>
+                </>
+              }
+              id="item1"
+            >
+              <TreeItemLayout>level 1, item 1</TreeItemLayout>
+              <Tree>
+                <TreeItem id="item1__item1">
+                  <TreeItemLayout>level 2, item 1</TreeItemLayout>
+                </TreeItem>
+              </Tree>
+            </TreeItem>
+          </TreeTest>,
+        );
+        cy.focused().should('not.exist');
+        cy.document().realPress('Tab');
+        cy.get('#item1').should('be.focused');
+        cy.document().realPress('Tab');
+        cy.get('#action').should('be.focused').realPress('{enter}');
+        cy.get('#item1__item1').should('not.exist');
+        cy.get('#action').should('be.focused').realPress('Space');
+        cy.get('#item1__item1').should('not.exist');
+      });
+      it('should focus on first item when pressing tab key', () => {
+        mount(<TreeTest />);
+        cy.focused().should('not.exist');
+        cy.document().realPress('Tab');
         cy.get('#item1').should('be.focused');
       });
+      it('should focus out of tree when pressing tab key inside tree.', () => {
+        mount(<TreeTest />);
+        cy.focused().should('not.exist');
+        cy.document().realPress('Tab');
+        cy.get('#item1').should('be.focused');
+        cy.focused().realPress('Tab');
+        cy.focused().should('not.exist');
+      });
+      describe('Navigation', () => {
+        it('should move with Up/Down keys', () => {
+          mount(<TreeTest />);
+          cy.get('#item1').focus().realPress('{downarrow}');
+          cy.get('#item2').should('be.focused');
+          cy.focused().realPress('Tab').should('not.exist');
+        });
+        it('should move with Left/Right keys', () => {
+          mount(<TreeTest defaultOpenItems={['item2', 'item2__item1']} />);
+          cy.get('#item1').focus().realPress('{downarrow}');
+          cy.get('#item2').should('be.focused').realPress('{rightarrow}');
+          cy.get('#item2__item1').should('be.focused').realPress('{rightarrow}');
+          cy.get('#item2__item1__item1').should('be.focused').realPress('{leftarrow}');
+          cy.get('#item2__item1').should('be.focused').realPress('{leftarrow}').realPress('{leftarrow}');
+          cy.get('#item2').should('be.focused');
+        });
+        it('should move to last item with End key', () => {
+          mount(<TreeTest defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
+          cy.get('#item1').focus().realPress('{end}');
+          cy.get('#item2__item1__item1').should('be.focused');
+        });
+        it('should move to first item with Home key', () => {
+          mount(<TreeTest defaultOpenItems={['item1', 'item2', 'item2__item1']} />);
+          cy.get('#item1').focus().realPress('{end}');
+          cy.get('#item2__item1__item1').should('be.focused').realPress('{home}');
+          cy.get('#item1').should('be.focused');
+        });
+      });
     });
   });
-});
+}
--- packages/react-components/react-tree/src/utils/flattenTree.ts
+++ packages/react-components/react-tree/src/utils/flattenTree.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { FlatTreeItemProps } from '../hooks/useFlatTree';
 import { TreeItemProps } from '../TreeItem';
 
@@ -67,3 +68,37 @@ function flattenTreeRecursive(items: NestedTreeItem[], parent?: FlatTreeItemProp
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const flattenTree_unstable = (items: NestedTreeItem[]): FlatTreeItemProps[] => flattenTreeRecursive(items);
+
+/**
+ * @internal
+ */
+export const flattenTreeFromElement = (
+  root: React.ReactElement<{
+    children?: React.ReactElement<TreeItemProps> | React.ReactElement<TreeItemProps>[];
+  }>,
+  parent?: FlatTreeItemProps,
+  level = 1,
+): FlatTreeItemProps[] => {
+  const children = React.Children.toArray(root.props.children) as React.ReactElement<TreeItemProps>[];
+  return children.reduce<FlatTreeItemProps[]>((acc, curr, index) => {
+    const [content, subtree] = React.Children.toArray(curr.props.children) as [
+      React.ReactNode,
+      typeof root | undefined,
+    ];
+    const flatTreeItem: FlatTreeItemProps = {
+      'aria-level': level,
+      'aria-posinset': index + 1,
+      'aria-setsize': children.length,
+      parentId: parent?.id,
+      id: curr.props.id ?? `fui-FlatTreeItem-${count++}`,
+      leaf: subtree === undefined,
+      ...curr.props,
+      children: content,
+    };
+    acc.push(flatTreeItem);
+    if (subtree !== undefined) {
+      acc.push(...flattenTreeFromElement(subtree, flatTreeItem, level + 1));
+    }
+    return acc;
+  }, []);
+};
