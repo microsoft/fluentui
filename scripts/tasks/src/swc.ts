17943612afc70e16a0b605fb38fa0b0ad471e076
Tristan Watanabe <tristan.watanabe@gmail.com>
2023-03-25 02:05:24 -0400

    Apply PR review suggestions

--- scripts/tasks/src/swc.ts
+++ scripts/tasks/src/swc.ts
@@ -6,24 +6,28 @@ import type { Options as SwcOptions } from '@swc/core';
 import glob from 'glob';
 import * as match from 'micromatch';
 
-type Options = SwcOptions & { module: { type: 'es6' | 'commonjs' | 'amd' } };
+type Options = SwcOptions & { module: { type: 'es6' | 'commonjs' | 'amd' }; outputPath: string };
 
 async function swcTransform(options: Options) {
   const { outputPath, module } = options;
+  const packageRoot = process.cwd();
+  const sourceRootDirName = module.type === 'es6' ? 'src' : 'lib';
+
   let sourceFiles: string[] = [];
 
   if (module.type === 'es6') {
-    sourceFiles = glob.sync('src/**/*.{ts,tsx}');
+    sourceFiles = glob.sync(`${sourceRootDirName}/**/*.{ts,tsx}`);
   }
 
   if (module.type === 'commonjs' || module.type === 'amd') {
-    sourceFiles = glob.sync('lib/**/*.js');
+    sourceFiles = glob.sync(`${sourceRootDirName}/**/*.js`);
   }
 
-  const swcConfig = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '.swcrc'), 'utf-8'));
+  const swcConfig = JSON.parse(fs.readFileSync(path.resolve(packageRoot, '.swcrc'), 'utf-8'));
+  const tsFileExtensionRegex = /\.(tsx|ts)$/;
 
   for (const fileName of sourceFiles) {
-    const srcFilePath = path.resolve(process.cwd(), fileName);
+    const srcFilePath = path.resolve(packageRoot, fileName);
     const isFileExcluded = match.isMatch(srcFilePath, swcConfig.exclude, { contains: true });
 
     if (isFileExcluded) {
@@ -39,15 +43,12 @@ async function swcTransform(options: Options) {
       outputPath,
     });
 
-    const compiledFilePath = path.resolve(
-      process.cwd(),
-      fileName.replace(`${module.type === 'es6' ? 'src' : 'lib'}`, outputPath!),
-    );
+    const compiledFilePath = path.resolve(packageRoot, fileName.replace(`${sourceRootDirName}`, outputPath));
 
     //Create directory folder for new compiled file(s) to live in.
     await fs.promises.mkdir(compiledFilePath.replace(path.basename(compiledFilePath), ''), { recursive: true });
 
-    const compiledFilePathJS = `${compiledFilePath.replace(/\.(tsx|ts)$/, '.js')}`;
+    const compiledFilePathJS = `${compiledFilePath.replace(tsFileExtensionRegex, '.js')}`;
 
     await fs.promises.writeFile(compiledFilePathJS, result.code);
     if (result.map) {
