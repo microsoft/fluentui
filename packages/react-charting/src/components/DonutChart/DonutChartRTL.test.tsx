c73df2832aeb78972548557b52681ce95e209744
srmukher <srmukher@microsoft.com>
2023-03-27 15:22:22 +0530

    Resolving PR comments

--- packages/react-charting/src/components/DonutChart/DonutChartRTL.test.tsx
+++ packages/react-charting/src/components/DonutChart/DonutChartRTL.test.tsx
@@ -1,7 +1,10 @@
-import { render, screen, queryAllByAttribute, fireEvent } from '@testing-library/react';
+import { render, screen, queryAllByAttribute, fireEvent, act } from '@testing-library/react';
 import { chartPoints } from './DonutChart.test';
 import { DonutChart } from './index';
 import * as React from 'react';
+import { DarkTheme } from '@fluentui/theme-samples';
+import { ThemeProvider } from '@fluentui/react';
+import * as utils from '../../utilities/utilities';
 
 test('Should hide callout on mouse leave', () => {
   // Arrange
@@ -15,6 +18,7 @@ test('Should hide callout on mouse leave', () => {
 
   // Assert
   expect(getById(container, /callout/i)[0]).toHaveStyle('opacity: 0');
+  expect(container).toMatchSnapshot();
 });
 
 test('Should show callout on focus', () => {
@@ -105,4 +109,91 @@ test('Should show Pies with same opacity on mouse out of legends', () => {
   // Assert
   expect(getById(container, /Pie.*?first/i)[0]).toHaveAttribute('opacity', '1');
   expect(getById(container, /Pie.*?second/i)[0]).toHaveAttribute('opacity', '1');
+});
+
+test('Should display correct callout data on mouse move', () => {
+  // Arrange
+  const { container } = render(<DonutChart data={chartPoints} innerRadius={55} calloutProps={{ doNotLayer: true }} />);
+
+  // Act
+  const getById = queryAllByAttribute.bind(null, 'id');
+  fireEvent.mouseOver(getById(container, /Pie/i)[0]);
+  expect(getById(container, /callout/i)[0]).toHaveTextContent('20,000');
+  fireEvent.mouseLeave(getById(container, /Pie/i)[0]);
+  fireEvent.mouseOver(getById(container, /Pie/i)[1]);
+
+  // Assert
+  expect(getById(container, /callout/i)[0]).toHaveTextContent('39,000');
+});
+
+test('Should reflect theme change', () => {
+  // Arrange
+  const { container } = render(
+    <ThemeProvider theme={DarkTheme}>
+      <DonutChart culture={window.navigator.language} data={chartPoints} innerRadius={55} />
+    </ThemeProvider>,
+  );
+
+  // Assert
+  expect(container).toMatchSnapshot();
+});
+
+describe('Screen resolution', () => {
+  const originalInnerWidth = global.innerWidth;
+  const originalInnerHeight = global.innerHeight;
+  afterEach(() => {
+    global.innerWidth = originalInnerWidth;
+    global.innerHeight = originalInnerHeight;
+    act(() => {
+      global.dispatchEvent(new Event('resize'));
+    });
+  });
+
+  test('Should remain unchanged on zoom in', () => {
+    // Arrange
+    const { container } = render(<DonutChart data={chartPoints} innerRadius={55} width={300} height={300} />);
+
+    // Act
+    global.innerWidth = window.innerWidth / 2;
+    global.innerHeight = window.innerHeight / 2;
+    act(() => {
+      global.dispatchEvent(new Event('resize'));
+    });
+
+    // Assert
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should remain unchanged on zoom out', () => {
+    // Arrange
+    const { container } = render(<DonutChart data={chartPoints} innerRadius={55} width={300} height={300} />);
+
+    // Act
+    global.innerWidth = window.innerWidth * 2;
+    global.innerHeight = window.innerHeight * 2;
+    act(() => {
+      global.dispatchEvent(new Event('resize'));
+    });
+
+    // Assert
+    expect(container).toMatchSnapshot();
+  });
+});
+
+test('Should change value inside donut with the legend value on mouseOver legend ', () => {
+  // Mock the implementation of wrapTextInsideDonut as it internally calls a Browser Function like
+  // getComputedTextLength() which will otherwise lead to a crash if mounted
+  jest.spyOn(utils, 'wrapTextInsideDonut').mockImplementation(() => '1000');
+  // Arrange
+  const { container } = render(
+    <DonutChart data={chartPoints} innerRadius={55} hideLegend={false} valueInsideDonut={1000} />,
+  );
+  const getByClass = queryAllByAttribute.bind(null, 'class');
+
+  // Act
+  fireEvent.mouseOver(screen.getByText('first'));
+
+  // Assert
+  expect(getByClass(container, /insideDonutString.*?/)[0].textContent).toBe('20,000');
+  expect(getByClass(container, /insideDonutString.*?/)[1].textContent).toBe('20,000');
 });
