1f6d54802313af52f9ac9d1fcb85e6e2932837b8
Ryan Merrill <ryanmerrill@microsoft.com>
2023-03-27 11:38:42 -0400

    Adds Slider as a web component (#27165)
    * Initial commit of styles and markup for slider
    
    * Cleans up some styles and adds Stories for states
    
    * More Slider styles and stories
    
    * Adds stripes to slider for vertical/horizontal. Deletes SliderLabel as we don't need it
    
    * Fixes silly math
    
    * Cleans up CSS
    
    * Cleans up CSS and Stories for Slider
    
    * Initial commit of styles and markup for slider
    
    * More Slider styles and stories
    
    * Adds stripes to slider for vertical/horizontal. Deletes SliderLabel as we don't need it
    
    * Fixes silly math
    
    * Fixes some styles on checkmarks
    
    * Adds check to see if Step was set
    
    * Generates API report
    
    * Removes some duplicate CSS and cleans up spec
    
    * Adds slider to index.ts and package.json
    
    * Addresses initial round of feedback to PR
    
    * Renames sizeChanged to stepChanged. Duh.
    
    * Adds JSDOC comments
    
    * update slider step rate code
    
    * Adds export to SliderOrientation
    
    * Adds new focus style and fixes overflow visual bug
    
    * Fixes build errors
    
    * Adds export to SliderOrientation
    
    * Adds new focus style and fixes overflow visual bug
    
    * Fixes build errors
    
    * Runs API report
    
    ---------
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>

---
+++ packages/web-components/src/slider/slider.styles.ts
@@ -0,0 +1,202 @@
+import { css } from '@microsoft/fast-element';
+import { display } from '@microsoft/fast-foundation';
+import {
+  borderRadiusCircular,
+  borderRadiusMedium,
+  borderRadiusSmall,
+  colorBrandBackground,
+  colorCompoundBrandBackground,
+  colorCompoundBrandBackgroundHover,
+  colorCompoundBrandBackgroundPressed,
+  colorNeutralBackground1,
+  colorNeutralBackgroundDisabled,
+  colorNeutralForegroundDisabled,
+  colorNeutralStroke1,
+  colorNeutralStrokeAccessible,
+  colorNeutralStrokeDisabled,
+  colorStrokeFocus1,
+  colorStrokeFocus2,
+} from '../theme/design-tokens.js';
+
+/** Text styles
+ * @public
+ */
+export const styles = css`
+  ${display('inline-grid')} :host {
+    --thumb-size: 18px;
+    --thumb-padding: 3px;
+    --thumb-translate: calc(var(--thumb-size) * -0.5 + var(--track-width) / 2);
+    --track-overhang: -2px;
+    --track-width: 4px;
+    --fast-slider-height: calc(var(--thumb-size) * 10);
+    --slider-direction: 90deg;
+    align-items: center;
+    box-sizing: border-box;
+    outline: none;
+    cursor: pointer;
+    user-select: none;
+    border-radius: ${borderRadiusSmall};
+    touch-action: pan-y;
+    min-width: calc(var(--thumb-size) * 1px);
+    width: 100%;
+  }
+
+  :host([size='small']) {
+    --thumb-size: 14px;
+    --track-width: 2px;
+    --thumb-padding: 3px;
+  }
+
+  :host([orientation='vertical']) {
+    --slider-direction: 0deg;
+    height: 160px;
+    min-height: var(--thumb-size);
+    touch-action: pan-x;
+    padding: 8px 0;
+    width: auto;
+    min-width: auto;
+  }
+
+  :host([disabled]:hover) {
+    cursor: initial;
+  }
+
+  :host(:focus-visible) {
+    box-shadow: 0 0 0 2pt ${colorStrokeFocus2};
+    outline: 1px solid ${colorStrokeFocus1};
+  }
+
+  .thumb-cursor:focus {
+    outline: 0;
+  }
+
+  /* Thumb Container and Cursor */
+  .thumb-container {
+    position: absolute;
+    height: var(--thumb-size);
+    width: var(--thumb-size);
+    transition: all 0.2s ease;
+  }
+
+  .thumb-container {
+    transform: translateX(calc(var(--thumb-size) * 0.5)) translateY(calc(var(--thumb-translate) * -1.5));
+  }
+
+  :host([size='small']) .thumb-container {
+    transform: translateX(calc(var(--thumb-size) * 0.5)) translateY(calc(var(--thumb-translate) * -1.35));
+  }
+
+  :host([orientation='vertical']) .thumb-container {
+    transform: translateX(calc(var(--thumb-translate) * -1.5)) translateY(calc(var(--thumb-size) * -0.5));
+  }
+
+  :host([orientation='vertical'][size='small']) .thumb-container {
+    transform: translateX(calc(var(--thumb-translate) * -1.35)) translateY(calc(var(--thumb-size) * -0.5));
+  }
+
+  .thumb-cursor {
+    height: var(--thumb-size);
+    width: var(--thumb-size);
+    background-color: ${colorBrandBackground};
+    border-radius: ${borderRadiusCircular};
+    box-shadow: inset 0 0 0 var(--thumb-padding) ${colorNeutralBackground1}, 0 0 0 1px ${colorNeutralStroke1};
+  }
+  .thumb-cursor:hover {
+    background-color: ${colorCompoundBrandBackgroundHover};
+  }
+  .thumb-cursor:active {
+    background-color: ${colorCompoundBrandBackgroundPressed};
+  }
+  :host([disabled]) .thumb-cursor {
+    background-color: ${colorNeutralForegroundDisabled};
+    box-shadow: inset 0 0 0 var(--thumb-padding) ${colorNeutralBackground1}, 0 0 0 1px ${colorNeutralStrokeDisabled};
+  }
+
+  /* Positioning Region */
+  .positioning-region {
+    position: relative;
+    display: grid;
+  }
+
+  :host([orientation='horizontal']) .positioning-region {
+    margin: 0 8px;
+    grid-template-rows: var(--thumb-size) var(--thumb-size);
+  }
+  :host([orientation='vertical']) .positioning-region {
+    margin: 8px 0;
+    height: 100%;
+    grid-template-columns: var(--thumb-size) var(--thumb-size);
+  }
+
+  /* Track */
+  .track {
+    align-self: start;
+    position: absolute;
+    background-color: ${colorNeutralStrokeAccessible};
+    border-radius: ${borderRadiusMedium};
+    overflow: hidden;
+  }
+
+  :host([step]) .track::after {
+    content: '';
+    position: absolute;
+    border-radius: ${borderRadiusMedium};
+    width: 100%;
+    inset: 0 2px;
+    background-image: repeating-linear-gradient(
+      var(--slider-direction),
+      #0000 0%,
+      #0000 calc(var(--step-rate) - 1px),
+      ${colorNeutralBackground1} calc(var(--step-rate) - 1px),
+      ${colorNeutralBackground1} var(--step-rate)
+    );
+  }
+
+  :host([orientation='vertical'][step]) .track::after {
+    inset: -2px 0;
+  }
+
+  :host([disabled]) .track {
+    background-color: ${colorNeutralBackgroundDisabled};
+  }
+
+  :host([orientation='horizontal']) .track {
+    right: var(--track-overhang);
+    left: var(--track-overhang);
+    align-self: start;
+    height: var(--track-width);
+    grid-row: 2 / auto;
+  }
+
+  :host([orientation='vertical']) .track {
+    top: var(--track-overhang);
+    bottom: var(--track-overhang);
+    width: var(--track-width);
+    height: 100%;
+    grid-column: 2 / auto;
+  }
+  .track-start {
+    background-color: ${colorCompoundBrandBackground};
+    position: absolute;
+    height: 100%;
+    left: 0;
+    border-radius: ${borderRadiusMedium};
+  }
+  :host([disabled]) .track-start {
+    background-color: ${colorNeutralForegroundDisabled};
+  }
+  :host(:hover) .track-start {
+    background-color: ${colorCompoundBrandBackgroundHover};
+  }
+  :host([disabled]:hover) .track-start {
+    background-color: ${colorNeutralForegroundDisabled};
+  }
+  .track-start:active {
+    background-color: ${colorCompoundBrandBackgroundPressed};
+  }
+  :host([orientation='vertical']) .track-start {
+    height: auto;
+    width: 100%;
+    bottom: 0;
+  }
+`;
---
+++ packages/web-components/src/slider/slider.ts
@@ -0,0 +1,76 @@
+import { attr, css, Observable } from '@microsoft/fast-element';
+import type { ElementStyles } from '@microsoft/fast-element';
+import { FASTSlider } from '@microsoft/fast-foundation';
+import type { SliderSize } from './slider.options.js';
+
+/**
+ * The base class used for constructing a fluent-slider custom element
+ * @public
+ */
+export class Slider extends FASTSlider {
+  /**
+   * The size of the slider
+   * @public
+   * @remarks
+   * HTML Attribute: size
+   */
+  @attr
+  public size?: SliderSize;
+
+  public handleChange(source: any, propertyName: string): void {
+    switch (propertyName) {
+      case 'min':
+      case 'max':
+      case 'step':
+        this.handleStepStyles();
+        break;
+      default:
+        break;
+    }
+  }
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+
+    Observable.getNotifier(this).subscribe(this, 'max');
+    Observable.getNotifier(this).subscribe(this, 'min');
+    Observable.getNotifier(this).subscribe(this, 'step');
+
+    this.handleStepStyles();
+  }
+
+  public disconnectedCallback(): void {
+    super.disconnectedCallback();
+
+    Observable.getNotifier(this).unsubscribe(this, 'max');
+    Observable.getNotifier(this).unsubscribe(this, 'min');
+    Observable.getNotifier(this).unsubscribe(this, 'step');
+  }
+
+  private stepStyles?: ElementStyles;
+
+  /**
+   * Handles changes to step styling based on the step value
+   * NOTE: This function is not a changed callback, stepStyles is not observable
+   */
+  private handleStepStyles(): void {
+    if (this.step) {
+      const totalSteps = (100 / Math.floor((this.max - this.min) / this.step)) as any;
+
+      if (this.stepStyles !== undefined) {
+        this.$fastController.removeStyles(this.stepStyles);
+      }
+
+      this.stepStyles = css/**css*/ `
+        :host {
+          --step-rate: ${totalSteps}%;
+          color: blue;
+        }
+      `;
+
+      this.$fastController.addStyles(this.stepStyles);
+    } else if (this.stepStyles !== undefined) {
+      this.$fastController.removeStyles(this.stepStyles);
+    }
+  }
+}
