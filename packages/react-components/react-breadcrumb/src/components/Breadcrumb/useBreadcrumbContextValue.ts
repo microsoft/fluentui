89b7465459f29b3ae66bc072843731a0708f2674
Valentina <v.kozlova13@gmail.com>
2023-03-27 15:49:02 +0200

    Update packages/react-components/react-breadcrumb/src/components/Breadcrumb/useBreadcrumbContextValue.ts
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-components/react-breadcrumb/src/components/Breadcrumb/useBreadcrumbContextValue.ts
+++ packages/react-components/react-breadcrumb/src/components/Breadcrumb/useBreadcrumbContextValue.ts
@@ -1,5 +1,9 @@
 import type { BreadcrumbContextValue, BreadcrumbState } from './Breadcrumb.types';
 
-export function useBreadcrumbContextValue(props: BreadcrumbState): BreadcrumbContextValue {
-  return props;
+export function useBreadcrumbContextValue(state: BreadcrumbState): BreadcrumbContextValues {
+  const { size, dividerType } = state
+
+  const breadcrumb = React.useMemo(() => ({ size, dividerType }, [size, dividerType])
+  
+  return { breadcrumb }
 }
