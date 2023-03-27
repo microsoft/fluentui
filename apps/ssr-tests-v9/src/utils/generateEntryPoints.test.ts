861ab3852f3fa19e614335d8e2694c2b18e02279
Oleksandr Fediashov <olfedias@microsoft.com>
2023-03-27 15:01:26 +0200

    chore(ssr-tests-v9): improve "ignore" functionality

--- apps/ssr-tests-v9/src/utils/generateEntryPoints.test.ts
+++ apps/ssr-tests-v9/src/utils/generateEntryPoints.test.ts
@@ -22,7 +22,12 @@ describe('generateEntryPoints', () => {
 `,
     );
 
-    await generateEntryPoints({ cjsEntryPoint, esmEntryPoint, storiesGlobs: [`${storiesDir}/*.ts`] });
+    await generateEntryPoints({
+      cjsEntryPoint,
+      esmEntryPoint,
+      storiesGlobs: [`${storiesDir}/*.ts`],
+      ignore: [],
+    });
 
     const storiesFile = await fs.promises.readFile(cjsEntryPoint, { encoding: 'utf8' });
     const appFile = await fs.promises.readFile(esmEntryPoint, { encoding: 'utf8' });
@@ -63,6 +68,64 @@ describe('generateEntryPoints', () => {
           <App />
         </RendererProvider>,
         document.querySelector(\\"#root\\")
+      );
+      "
+    `);
+  });
+
+  it('skips globs specified in "ignore"', async () => {
+    const filesDir = tmp.dirSync({ unsafeCleanup: true }).name;
+
+    const cjsEntryPoint = path.resolve(filesDir, 'cjs.js');
+    const esmEntryPoint = path.resolve(filesDir, 'esm.js');
+
+    const storiesDir = path.resolve(filesDir, 'stories', 'Module');
+
+    await fs.promises.mkdir(storiesDir, { recursive: true });
+    await fs.promises.writeFile(
+      path.resolve(storiesDir, 'index.stories.ts'),
+      `
+    export { Foo } from './Foo'
+    export { Bar } from './Bar'
+    export { Baz } from './Baz'
+    export { Qux } from './Qux'
+`,
+    );
+
+    const { ignoredStories } = await generateEntryPoints({
+      cjsEntryPoint,
+      esmEntryPoint,
+      storiesGlobs: [`${storiesDir}/*.ts`],
+      ignore: ['**/Module/Bar', '**/Module/Baz'],
+    });
+
+    const storiesFile = await fs.promises.readFile(cjsEntryPoint, { encoding: 'utf8' });
+    const ignoredStoriesNormalized = ignoredStories.map(filepath => filepath.replace(filesDir, '').replace(/\\/g, '/'));
+
+    expect(ignoredStoriesNormalized).toMatchInlineSnapshot(`
+      Array [
+        "/stories/Module/Bar",
+        "/stories/Module/Baz",
+      ]
+    `);
+    expect(storiesFile).toMatchInlineSnapshot(`
+      "import {
+        FluentProvider,
+        teamsLightTheme,
+        SSRProvider,
+      } from \\"@fluentui/react-components\\";
+      import * as React from \\"react\\";
+
+      import { Foo as ModuleFoo } from \\"stories/Module/Foo\\";
+      import { Qux as ModuleQux } from \\"stories/Module/Qux\\";
+
+      export const App = () => (
+        <SSRProvider>
+          <FluentProvider id=\\"root-provider\\" theme={teamsLightTheme}>
+            <ModuleFoo />
+            <ModuleQux />
+          </FluentProvider>
+        </SSRProvider>
       );
       "
     `);
