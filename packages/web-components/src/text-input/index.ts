7a001f5ea30b7d840cd98ca280beb5a57ae1df8f
Brian Brady <brianbrady@microsoft.com>
2023-03-27 14:42:52 -0700

    text input: fixes export names

--- packages/web-components/src/text-input/index.ts
+++ packages/web-components/src/text-input/index.ts
@@ -1,6 +1,6 @@
 export * from './text-input.js';
 export * from './text-input.options.js';
-export { template as TextTemplate } from './text-input.template.js';
-export { styles as TextStyles } from './text-input.styles.js';
-export { definition as TextDefinition } from './text-input.definition.js';
+export { template as TextInputTemplate } from './text-input.template.js';
+export { styles as TextInputStyles } from './text-input.styles.js';
+export { definition as TextInputDefinition } from './text-input.definition.js';
 export { TextFieldType as TextInputType } from '@microsoft/fast-foundation';
