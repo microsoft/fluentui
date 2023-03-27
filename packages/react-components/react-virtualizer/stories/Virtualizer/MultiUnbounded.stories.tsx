639f3421a4d2fbad04408658fb11875c40b0e90a
Martin Hochel <martinhochel@microsoft.com>
2023-03-27 16:49:07 +0200

    fixup! fix(reat-virtualizer): remove disallowed v9 react-fabric/v8 from dependency to mitigate dep-tree creep

--- packages/react-components/react-virtualizer/stories/Virtualizer/MultiUnbounded.stories.tsx
+++ packages/react-components/react-virtualizer/stories/Virtualizer/MultiUnbounded.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Virtualizer, useStaticVirtualizerMeasure } from '@fluentui/react-components/unstable';
 import { makeStyles, useFluent } from '@fluentui/react-components';
+// @ts-expect-error - we don support path aliases for libraries with both v8 and v9 as dependencies
 import { ThemeProvider } from '@fluentui/react';
 
 const useStyles = makeStyles({
