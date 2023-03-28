15a7df992f08fe39be7ac85868927022e51e20c0
Albert Calzaretto <acalzaretto@microsoft.com>
2023-03-28 09:47:27 -0400

    Added title & description

--- packages/react-components/theme-designer/src/components/Content/Content.tsx
+++ packages/react-components/theme-designer/src/components/Content/Content.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { Divider, FluentProvider, makeStyles, mergeClasses, shorthands, tokens } from '@fluentui/react-components';
+import {
+  Divider,
+  FluentProvider,
+  makeStyles,
+  mergeClasses,
+  shorthands,
+  tokens,
+  Text,
+} from '@fluentui/react-components';
 import { Alert } from '@fluentui/react-alert';
 import { Demo } from '../Demo/Demo';
 import { Palette } from '../Palette/Palette';
@@ -36,6 +44,13 @@ export const Content: React.FC<ContentProps> = props => {
         This tool is still a work in progress - colors are still subject to adjustment.
       </Alert>
       <div className={mergeClasses(styles.root, props.className)}>
+        <h1 style={{ marginBottom: 0 }}>Fluent Theme Designer</h1>
+        <Text>
+          Welcome to the Fluent Theme Designer tool. This tool offers a step-by-step process to help you implement your
+          organization’s brand colors within Microsoft products using Fluent 2. Inputting your organization’s key color
+          value will output a 16-color ramp that passes contrast checks when used in tokens and implemented with Fluent
+          2 components.
+        </Text>
         <Palette />
         <KeyColorBanner keyColor={brand[80]} />
         <Demo />
