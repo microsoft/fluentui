b9c2eb46938150b789ee23aca7a38b9d47485b92
Esteban Munoz <esteban.230@hotmail.com>
2023-03-29 14:52:59 -0700

    cropping examples and fixing today

--- apps/vr-tests-react-components/src/stories/DatePickerCompat.stories.tsx
+++ apps/vr-tests-react-components/src/stories/DatePickerCompat.stories.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import { Steps, StoryWright } from 'storywright';
 import { DatePicker as DatePickerBase, DateRangeType } from '@fluentui/react-datepicker-compat';
 import { storiesOf } from '@storybook/react';
-import { TestWrapperDecoratorFixedWidth } from '../utilities/TestWrapperDecorator';
+import { TestWrapperDecorator } from '../utilities/TestWrapperDecorator';
 import type { DatePickerProps } from '@fluentui/react-datepicker-compat';
 
-const DatePicker = (props: DatePickerProps) => <DatePickerBase today={new Date('3/15/2023')} {...props} />;
+const DatePicker = (props: DatePickerProps) => {
+  const today = new Date('3/15/2023');
+  return (
+    <div style={{ width: '500px', height: '330px', padding: '10px' }}>
+      <DatePickerBase value={today} today={today} {...props} />
+    </div>
+  );
+};
 
 storiesOf('DatePicker Compat', module)
-  .addDecorator(TestWrapperDecoratorFixedWidth)
+  .addDecorator(TestWrapperDecorator)
   .addDecorator(story => (
     <StoryWright
       steps={new Steps()
@@ -23,10 +30,7 @@ storiesOf('DatePicker Compat', module)
   .addStory('default', () => <DatePicker input={{ className: 'datepicker-input' }} />, {
     includeHighContrast: true,
     includeDarkMode: true,
-  });
-
-storiesOf('DatePicker Compat', module)
-  .addDecorator(TestWrapperDecoratorFixedWidth)
+  })
   .addStory(
     'DateRange: day',
     () => <DatePicker calendar={{ dateRangeType: DateRangeType.Day }} popover={{ open: true }} />,
