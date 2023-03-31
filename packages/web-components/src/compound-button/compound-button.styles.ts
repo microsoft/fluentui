c1ae39ee8b5659f1edfa14453a8149ceda614df7
Chris Holt <chhol@microsoft.com>
2023-03-31 13:19:37 -0700

    add compound button as a new web component

---
+++ packages/web-components/src/compound-button/compound-button.styles.ts
@@ -0,0 +1,123 @@
+import { css } from '@microsoft/fast-element';
+import { styles as ButtonStyles } from '../button/button.styles.js';
+import {
+  colorNeutralForeground2,
+  colorNeutralForeground2BrandHover,
+  colorNeutralForeground2BrandPressed,
+  colorNeutralForeground2Hover,
+  colorNeutralForeground2Pressed,
+  colorNeutralForegroundDisabled,
+  colorNeutralForegroundOnBrand,
+  fontSizeBase200,
+  fontSizeBase300,
+  fontSizeBase400,
+  fontWeightRegular,
+  lineHeightBase300,
+  lineHeightBase400,
+  spacingHorizontalS,
+  spacingHorizontalSNudge,
+  spacingHorizontalXS,
+} from '../theme/design-tokens.js';
+
+// Need to support icon hover styles
+export const styles = css`
+  ${ButtonStyles}
+
+  :host .control,
+  :host(:is([size])) .control {
+    gap: 12px;
+    height: auto;
+    padding-top: 14px;
+    padding-inline-end: 12px;
+    padding-bottom: 16px;
+    padding-inline-start: 12px;
+    font-size: ${fontSizeBase300};
+    line-height: ${lineHeightBase300};
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+  }
+
+  ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2};
+    line-height: 100%;
+    font-size: ${fontSizeBase200};
+    font-weight: ${fontWeightRegular};
+  }
+
+  :host ::slotted(svg),
+  :host([size='large']) ::slotted(svg) {
+    font-size: 40px !important;
+    height: 40px !important;
+    width: 40px !important;
+  }
+
+  :host(:hover) ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2Hover};
+  }
+
+  :host(:active) ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2Pressed};
+  }
+
+  :host([disabled]) ::slotted([slot='description']) {
+    color: ${colorNeutralForegroundDisabled};
+  }
+
+  :host(:is([appearance='primary'], [appearance='primary']:hover, [appearance='primary']:active))
+    ::slotted([slot='description']) {
+    color: ${colorNeutralForegroundOnBrand};
+  }
+
+  :host(:is([appearance='subtle'], [appearance='subtle']:hover, [appearance='subtle']:active))
+    ::slotted([slot='description']),
+  :host([appearance='transparent']) ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2};
+  }
+
+  :host([appearance='transparent']:hover) ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2BrandHover};
+  }
+
+  :host([appearance='transparent']:active) ::slotted([slot='description']) {
+    color: ${colorNeutralForeground2BrandPressed};
+  }
+
+  :host([size='small']) .control {
+    padding: 8px;
+    padding-bottom: 10px;
+  }
+
+  :host([icon-only]) .control {
+    min-width: 52px;
+    max-width: 52px;
+    padding: ${spacingHorizontalSNudge};
+  }
+
+  :host([icon-only][size='small']) .control {
+    min-width: 48px;
+    max-width: 48px;
+    padding: ${spacingHorizontalXS};
+  }
+
+  :host([icon-only][size='large']) .control {
+    min-width: 56px;
+    max-width: 56px;
+    padding: ${spacingHorizontalS};
+  }
+
+  :host([size='large']) .control {
+    padding-top: 18px;
+    padding-inline-end: 16px;
+    padding-bottom: 20px;
+    padding-inline-start: 16px;
+    font-size: ${fontSizeBase400};
+    line-height: ${lineHeightBase400};
+  }
+  :host([size='large']) ::slotted([slot='description']) {
+    font-size: ${fontSizeBase300};
+  }
+`;
