448e17a054e1be1a1c3d228c6b44524a81161a8b
ankityadav4 <ankityadav@microsoft.com>
2023-04-01 19:12:07 +0530

    Creating new Pr deploy site

---
+++ packages/react-examples/src/react-charting-perf/demo/AppDefinition.tsx
@@ -0,0 +1,34 @@
+import { IAppDefinition } from '@fluentui/react-docsite-components';
+import { AppThemes } from './AppThemes';
+
+export const AppDefinition: IAppDefinition = {
+  appTitle: 'Fluent UI React - Charting Performance',
+  themes: AppThemes,
+  testPages: [],
+  examplePages: [
+    {
+      links: [
+        {
+          component: require<any>('../PerformanceDataSet1/LineChartPage').LineChartPerfDataOne,
+          key: 'LineChart Perf Data Set One',
+          name: 'Line Chart Perf Data One',
+          url: '#/examples/linechartperfone',
+        },
+      ],
+    },
+  ],
+  headerLinks: [
+    {
+      name: 'Getting started',
+      url: '#/',
+    },
+    {
+      name: 'Fabric',
+      url: 'https://developer.microsoft.com/en-us/fluentui',
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/microsoft/fluentui/tree/master/packages/react-charting',
+    },
+  ],
+};
