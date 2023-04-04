42104e45c9e2dabb410741486fcaf9a2c3e15f98
Marcos Moura <marcosvmmoura@gmail.com>
2023-04-04 18:58:17 +0200

    test: disable same tests as Dialog

--- packages/react-components/react-drawer/src/components/Drawer/Drawer.test.tsx
+++ packages/react-components/react-drawer/src/components/Drawer/Drawer.test.tsx
@@ -2,25 +2,36 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Drawer } from './Drawer';
 import { isConformant } from '../../testing/isConformant';
+import { resetIdsForTests } from '@fluentui/react-utilities';
+import { DrawerProps } from './Drawer.types';
 
 describe('Drawer', () => {
+  const testid = 'test';
+  const props = {
+    'data-testid': testid,
+  } as DrawerProps;
+
+  beforeEach(() => {
+    resetIdsForTests();
+  });
+
   isConformant({
     Component: Drawer,
     displayName: 'Drawer',
+    disabledTests: [
+      'component-handles-ref',
+      'component-has-root-ref',
+      'component-handles-classname',
+      'component-has-static-classnames-object',
+      'consistent-callback-args',
+      'make-styles-overrides-win',
+    ],
+    requiredProps: props,
+    getTargetElement: result => result.getByTestId(testid),
   });
-
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
 
   it('renders a default state', () => {
     const result = render(<Drawer>Default Drawer</Drawer>);
-    expect(result.container).toMatchInlineSnapshot(`
-      <div>
-        <div
-          class="fui-Drawer"
-        >
-          Default Drawer
-        </div>
-      </div>
-    `);
+    expect(result.container).toMatchInlineSnapshot(`<div />`);
   });
 });
