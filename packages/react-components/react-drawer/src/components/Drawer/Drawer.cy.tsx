0e700fc1959267ab5bfac8aa64e5fae3ce505b23
Marcos Moura <marcosvmmoura@gmail.com>
2023-04-05 13:18:12 +0200

    test: add test for the `open` prop

--- packages/react-components/react-drawer/src/components/Drawer/Drawer.cy.tsx
+++ packages/react-components/react-drawer/src/components/Drawer/Drawer.cy.tsx
@@ -11,8 +11,29 @@ const mountFluent = (element: JSX.Element) => {
 
 describe('Drawer', () => {
   it('render drawer component', () => {
-    mountFluent(<Drawer open id="drawer" />);
+    mountFluent(<Drawer id="drawer" />);
 
+    cy.get('#drawer').should('not.exist');
+  });
+
+  it('should toggle drawer visibility on open', () => {
+    const ExampleDrawer = () => {
+      const [open, setOpen] = React.useState(false);
+
+      return (
+        <>
+          <Drawer open={open} id="drawer" />
+          <button id="button" onClick={() => setOpen(true)}>
+            Open
+          </button>
+        </>
+      );
+    };
+
+    mountFluent(<ExampleDrawer />);
+
+    cy.get('#drawer').should('not.exist');
+    cy.get('#button').click();
     cy.get('#drawer').should('exist');
   });
 });
