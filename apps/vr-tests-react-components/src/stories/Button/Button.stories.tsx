934be466b5feb0da9fcd5f4cb9f696685fbc7407
piyush-2712 <piyush.2712@gmail.com>
2023-04-08 22:56:44 +0530

    Update Button.stories.tsx

--- apps/vr-tests-react-components/src/stories/Button/Button.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Button/Button.stories.tsx
@@ -35,7 +35,7 @@ export const Circular = () => (
 
 export const Outline = () => (
   <Button id={buttonId} appearance="outline">
-    Hello, world, Hello, world, This is i. Please1 1Woxrksssa
+    Hello, world, Hello, world, This is i. Please1Woxrksssa
   </Button>
 );
 
