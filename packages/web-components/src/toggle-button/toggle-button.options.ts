2ae63de044db0a8aad442cb5d2faff6d69535ade
Chris Holt <chhol@microsoft.com>
2023-04-11 15:12:08 -0700

    add toggle button as new v3 web component (#27405)
    * add toggle button as a new web component
    
    * address ts and lint errors
    
    * remove unnecessary css reassignment
    
    * remove unnecessary icon attr from stories
    
    * add checked stories and fix subtle appearance

---
+++ packages/web-components/src/toggle-button/toggle-button.options.ts
@@ -0,0 +1,39 @@
+import { ButtonOptions, ValuesOf } from '@microsoft/fast-foundation';
+import { ButtonAppearance, ButtonShape, ButtonSize } from '../button/button.options.js';
+
+/**
+ * Toggle Button Appearance constants
+ * @public
+ */
+export const ToggleButtonAppearance = ButtonAppearance;
+/**
+ * A Toggle Button can be secondary, primary, outline, subtle, transparent
+ * @public
+ */
+export type ToggleButtonAppearance = ValuesOf<typeof ToggleButtonAppearance>;
+
+/**
+ * A Toggle Button can be square, circular or rounded.
+ * @public
+ */
+export const ToggleButtonShape = ButtonShape;
+
+/**
+ * A Toggle Button can be square, circular or rounded
+ * @public
+ */
+export type ToggleButtonShape = ValuesOf<typeof ToggleButtonShape>;
+
+/**
+ * A Toggle Button can be a size of small, medium or large.
+ * @public
+ */
+export const ToggleButtonSize = ButtonSize;
+
+/**
+ * A Toggle Button can be on of several preset sizes.
+ * @public
+ */
+export type ToggleButtonSize = ValuesOf<typeof ToggleButtonSize>;
+
+export { ButtonOptions as ToggleButtonOptions };
