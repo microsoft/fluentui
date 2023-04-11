947ef6704f3504a0efe77220b366aa04a0dd9672
Chris Holt <chhol@microsoft.com>
2023-04-11 15:12:08 -0700

    add toggle button as new v3 web component (#27405)
    * add toggle button as a new web component
    
    * address ts and lint errors
    
    * remove unnecessary css reassignment
    
    * remove unnecessary icon attr from stories
    
    * add checked stories and fix subtle appearance

---
+++ packages/web-components/src/toggle-button/toggle-button.template.ts
@@ -0,0 +1,9 @@
+import { ElementViewTemplate } from '@microsoft/fast-element';
+import { buttonTemplate } from '@microsoft/fast-foundation';
+import type { ToggleButton } from './toggle-button.js';
+
+/**
+ * The template for the ToggleButton component.
+ * @public
+ */
+export const template: ElementViewTemplate<ToggleButton> = buttonTemplate();
