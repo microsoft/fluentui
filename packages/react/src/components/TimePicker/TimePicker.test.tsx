4d5879890a82a896780ec2f60ce1874d12868b2b
James Wu <jamwu@microsoft.com>
2023-04-12 10:44:12 -0400

    Updated TimePicker tests and added test for new controlled component variant

--- packages/react/src/components/TimePicker/TimePicker.test.tsx
+++ packages/react/src/components/TimePicker/TimePicker.test.tsx
@@ -1,31 +1,81 @@
 import * as React from 'react';
 import { TimePicker } from './TimePicker';
-// import { ITimeRange } from './TimePicker.types';
-// import { create } from '@fluentui/test-utilities';
+import { ITimeRange } from './TimePicker.types';
+import { create } from '@fluentui/test-utilities';
 import { mount } from 'enzyme';
 import type { IComboBox } from '../ComboBox/ComboBox.types';
 import { KeyCodes } from '../../Utilities';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('TimePicker', () => {
-  // TODO: times in this snapshot test changed and failed builds
-  // it('renders correctly', () => {
-  //   const timeRange: ITimeRange = {
-  //     start: 0,
-  //     end: 5,
-  //   };
-  //   const component = create(<TimePicker label="I am a TimePicker" timeRange={timeRange} />);
-  //   const tree = component.toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('renders correctly', () => {
+    const timeRange: ITimeRange = {
+      start: 0,
+      end: 5,
+    };
+    const component = create(<TimePicker label="I am a TimePicker" timeRange={timeRange} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   it('generates the formatted option', () => {
     const onFormatDate = (date: Date) => {
       return 'custom date option';
     };
     const timePicker = React.createRef<IComboBox>();
+    const dateAnchor = new Date('November 25, 2021 09:00:00');
 
-    mount(<TimePicker label="I am a TimePicker" onFormatDate={onFormatDate} componentRef={timePicker} />);
+    mount(
+      <TimePicker
+        label="I am a TimePicker"
+        onFormatDate={onFormatDate}
+        dateAnchor={dateAnchor}
+        defaultValue={dateAnchor}
+        componentRef={timePicker}
+      />,
+    );
+
     expect(timePicker!.current!.selectedOptions[0].text).toBe('custom date option');
+  });
+
+  it('shows controlled time correctly', () => {
+    let _selectedTime = new Date('February 27, 2023 10:00:00');
+    const onChange = (ev: React.FormEvent<IComboBox>, time: Date | undefined): void => {
+      if (time) {
+        _selectedTime = time;
+      }
+    };
+    const dateAnchor = new Date('February 27, 2023 08:00:00');
+
+    const { getByRole, getAllByRole } = render(
+      <TimePicker
+        showSeconds
+        allowFreeform={false}
+        increments={15}
+        autoComplete="on"
+        label="I am a controlled TimePicker"
+        dateAnchor={dateAnchor}
+        value={_selectedTime}
+        onChange={onChange}
+      />,
+    );
+
+    const timePickerComboBox = getByRole('combobox') as HTMLInputElement;
+    expect(timePickerComboBox.value).toEqual('10:00:00');
+
+    userEvent.click(timePickerComboBox);
+    const timePickerOptions = getAllByRole('option') as HTMLInputElement[];
+    userEvent.click(timePickerOptions[1], undefined, { skipPointerEventsCheck: true });
+
+    const formattedSelectedTime = _selectedTime.toLocaleTimeString([], {
+      hour: 'numeric',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+
+    const expectedTime = '8:15:00 AM';
+    expect(formattedSelectedTime).toEqual(expectedTime);
   });
 
   describe('validates entered text when', () => {
