46c89e8e688152ab8d0ed44ed79572508e4fb50e
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-04-15 11:37:34 +0200

    chore(react-dialog): adopts custom JSX pragma (#27475)

--- packages/react-components/react-dialog/src/components/Dialog/renderDialog.tsx
+++ packages/react-components/react-dialog/src/components/Dialog/renderDialog.tsx
@@ -1,4 +1,8 @@
-import * as React from 'react';
+/** @jsxRuntime classic */
+/** @jsx createElement */
+
+import { createElement } from '@fluentui/react-jsx-runtime';
+
 import { DialogProvider, DialogSurfaceProvider } from '../../contexts';
 import type { DialogState, DialogContextValues } from './Dialog.types';
 
