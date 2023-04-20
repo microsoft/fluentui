f801e34cdc25ba01ccf3b2baa9fac1080d987e9b
BrdyBrn <brian.christopher.brady@gmail.com>
2023-04-20 13:13:30 -0700

    Adds Menu and MenuItem as new web components (#26765)
    * menu init
    
    * updates docs
    
    * menu init
    
    * menu item init
    
    * registers menu-item
    
    * adds some menu styles
    
    * adds default menu item icons
    
    * adds menu stories
    
    * updates default menu icons
    
    * styles menu and menu item
    
    * updates readme
    
    * swaps part for class css
    
    * removes dead code
    
    * adds menu and menu item to package.json
    
    * removes dead code
    
    * updates menu styles
    
    * yarn change
    
    * adds menu header
    
    * updates jsdocs
    
    * removes dead file
    
    * removes dead code
    
    * removes split button styling
    
    * adds display helper
    
    * api report
    
    * flattens menu and menu item directories
    
    * removes element specification from slotted selector
    
    * formatting
    
    * optimizes styles
    
    * optmizes styles
    
    * updates disabled css selector to bool
    
    * removes dead code
    
    * consolidates menu docs into one story
    
    * updates menu storybook content
    
    * optimizes styling
    
    * revert api-report
    
    * updates disabled pseudo selector
    
    * fixes disabled state
    
    * fixes css syntax error
    
    * updates storybook content
    
    * updates storybook copy for consistency
    
    * revert api report
    
    * updates yarn change message
    
    * maps component to MenuList rather than Menu
    
    * fixes focus state
    
    * updates styles
    
    * divides accordion and accordion item readme
    
    * removes dead imports
    
    * fixes submenu positioning
    
    * adds styles for icon alignment
    
    * reverts Menu component name
    
    * cleans up storybook
    
    * dynamically set the menu item icon attribute when icons is true for menu (#26998)
    
    * dynamically set the menu item icon attribute when icons is true for menu
    
    * move icons check to set items to ensure we update on item change and init
    
    * set items should be protected
    
    * updates Menu styles
    
    * updates icon alignment styles and docs
    
    * updates menu docs
    
    * adds checkmarks attribute and styles menu items
    
    * adds conditional styling for when icons are present
    
    * updates Menu and MenuItem docs
    
    * updates deltas in docs
    
    * updates docs
    
    * optimizes styles
    
    * optimizes css
    
    * optimizes css
    
    * optimizes css
    
    * reverts json export
    
    * working (#27293)
    
    * optimizes styles
    
    * adds docs to menu.ts
    
    * updates menu logic for readability
    
    * menu: fixes style syntax error
    
    * menu: consolidates styles
    
    * menu: updates styling
    
    * menu: updates styling
    
    * yarn change
    
    * menulist, menuitem: changes component name to MenuList
    
    * menulist, menuitem, updates readme
    
    * menu, menuitem: fixes circular dep
    
    * menu, menuitem: alphabetize index.js
    
    ---------
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>
    Co-authored-by: Jeff Smith <37851214+eljefe223@users.noreply.github.com>

---
+++ packages/web-components/src/menu-item/menu-item.styles.ts
@@ -0,0 +1,203 @@
+import { css } from '@microsoft/fast-element';
+import { display } from '@microsoft/fast-foundation';
+import {
+  borderRadiusMedium,
+  colorCompoundBrandForeground1Hover,
+  colorCompoundBrandForeground1Pressed,
+  colorNeutralBackground1,
+  colorNeutralBackground1Hover,
+  colorNeutralBackground1Selected,
+  colorNeutralBackgroundDisabled,
+  colorNeutralForeground2,
+  colorNeutralForeground2Hover,
+  colorNeutralForeground2Pressed,
+  colorNeutralForeground3,
+  colorNeutralForegroundDisabled,
+  fontFamilyBase,
+  fontSizeBase200,
+  fontSizeBase300,
+  fontSizeBase500,
+  fontWeightRegular,
+  lineHeightBase200,
+  lineHeightBase300,
+} from '../theme/design-tokens.js';
+
+/** MenuItem styles
+ * @public
+ */
+export const styles = css`
+  ${display('grid')}
+
+  :host {
+    grid-template-columns: 20px 20px auto 20px;
+    align-items: center;
+    grid-gap: 4px;
+    height: 32px;
+    background: ${colorNeutralBackground1};
+    font: ${fontWeightRegular} ${fontSizeBase300} / ${lineHeightBase300} ${fontFamilyBase};
+    border-radius: ${borderRadiusMedium};
+    color: ${colorNeutralForeground2};
+    padding: 0 10px;
+    cursor: pointer;
+    overflow: visible;
+    contain: layout;
+  }
+
+  :host(:hover) {
+    background: ${colorNeutralBackground1Hover};
+  }
+
+  .content {
+    white-space: nowrap;
+    flex-grow: 1;
+    grid-column: auto / span 2;
+    padding: 0 2px;
+  }
+
+  .checkbox,
+  .radio {
+    display: none;
+  }
+
+  .input-container,
+  .expand-collapse-glyph-container,
+  ::slotted([slot='start']),
+  ::slotted([slot='end']),
+  :host([checked]) .checkbox,
+  :host([checked]) .radio {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    color: ${colorNeutralForeground2};
+  }
+
+  .expand-collapse-glyph-container,
+  ::slotted([slot='start']),
+  ::slotted([slot='end']) {
+    height: 32px;
+    font-size: ${fontSizeBase500};
+    width: fit-content;
+  }
+
+  .input-container {
+    width: 20px;
+  }
+
+  ::slotted([slot='end']) {
+    color: ${colorNeutralForeground3};
+    font: ${fontWeightRegular} ${fontSizeBase200} / ${lineHeightBase200} ${fontFamilyBase};
+    white-space: nowrap;
+    grid-column: 4 / span 1;
+    justify-self: flex-end;
+  }
+
+  .expand-collapse-glyph-container {
+    grid-column: 4 / span 1;
+    justify-self: flex-end;
+  }
+
+  :host(:hover) .input-container,
+  :host(:hover) .expand-collapse-glyph-container,
+  :host(:hover) .content {
+    color: ${colorNeutralForeground2Hover};
+  }
+
+  :host([icon]:hover) ::slotted([slot='start']) {
+    color: ${colorCompoundBrandForeground1Hover};
+  }
+
+  :host(:active) {
+    background-color: ${colorNeutralBackground1Selected};
+  }
+
+  :host(:active) .input-container,
+  :host(:active) .expand-collapse-glyph-container,
+  :host(:active) .content {
+    color: ${colorNeutralForeground2Pressed};
+  }
+
+  :host(:active) ::slotted([slot='start']) {
+    color: ${colorCompoundBrandForeground1Pressed};
+  }
+
+  :host([disabled]) {
+    background-color: ${colorNeutralBackgroundDisabled};
+  }
+
+  :host([disabled]) .content,
+  :host([disabled]) .expand-collapse-glyph-container,
+  :host([disabled]) ::slotted([slot='end']),
+  :host([disabled]) ::slotted([slot='start']) {
+    color: ${colorNeutralForegroundDisabled};
+  }
+
+  :host([data-indent]) {
+    display: grid;
+  }
+
+  :host([data-indent='1']) .content {
+    grid-column: 2 / span 1;
+  }
+
+  :host([data-indent='1'][role='menuitemcheckbox']) {
+    display: grid;
+  }
+
+  :host([data-indent='2'][aria-haspopup='menu']) ::slotted([slot='end']) {
+    grid-column: 4 / span 1;
+  }
+
+  :host([data-indent='2'][aria-haspopup='menu']) .expand-collapse-glyph-container {
+    grid-column: 5 / span 1;
+  }
+
+  :host([data-indent='1']) .content {
+    grid-column: 2 / span 1;
+  }
+
+  :host([data-indent='1'][role='menuitemcheckbox']) .content,
+  :host([data-indent='1'][role='menuitemradio']) .content {
+    grid-column: auto / span 1;
+  }
+
+  :host([icon]) ::slotted([slot='end']),
+  :host([data-indent='1']) ::slotted([slot='end']) {
+    grid-column: 4 / span 1;
+    justify-self: flex-end;
+  }
+
+  :host([data-indent='2']) {
+    display: grid;
+    grid-template-columns: 20px 20px auto auto;
+  }
+
+  :host([data-indent='2']) .content {
+    grid-column: 3 / span 1;
+  }
+
+  :host([data-indent='2']) .input-container {
+    grid-column: 1 / span 1;
+  }
+
+  :host([data-indent='2']) ::slotted([slot='start']) {
+    grid-column: 2 / span 1;
+  }
+
+  :host([aria-haspopup='menu']) {
+    grid-template-columns: 20px auto auto 20px;
+  }
+
+  :host([data-indent='2'][aria-haspopup='menu']) {
+    grid-template-columns: 20px 20px auto auto 20px;
+  }
+
+  :host([aria-haspopup='menu']) ::slotted([slot='end']) {
+    grid-column: 3 / span 1;
+    justify-self: flex-end;
+  }
+
+  :host([data-indent='2'][aria-haspopup='menu']) ::slotted([slot='end']) {
+    grid-column: 4 / span 1;
+    justify-self: flex-end;
+  }
+`;
