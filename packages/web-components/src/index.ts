0f8c7ac1da374b121485e8bbdd37d8f7131558b0
BrdyBrn <brian.christopher.brady@gmail.com>
2023-04-20 13:13:30 -0700

    Adds Menu and MenuItem as new web components (#26765)
    * menu init
    
    * updates docs
    
    * menu init
    
    * menu item init
    
    * registers menu-item
    
    * adds some menu styles
    
    * adds default menu item icons
    
    * adds menu stories
    
    * updates default menu icons
    
    * styles menu and menu item
    
    * updates readme
    
    * swaps part for class css
    
    * removes dead code
    
    * adds menu and menu item to package.json
    
    * removes dead code
    
    * updates menu styles
    
    * yarn change
    
    * adds menu header
    
    * updates jsdocs
    
    * removes dead file
    
    * removes dead code
    
    * removes split button styling
    
    * adds display helper
    
    * api report
    
    * flattens menu and menu item directories
    
    * removes element specification from slotted selector
    
    * formatting
    
    * optimizes styles
    
    * optmizes styles
    
    * updates disabled css selector to bool
    
    * removes dead code
    
    * consolidates menu docs into one story
    
    * updates menu storybook content
    
    * optimizes styling
    
    * revert api-report
    
    * updates disabled pseudo selector
    
    * fixes disabled state
    
    * fixes css syntax error
    
    * updates storybook content
    
    * updates storybook copy for consistency
    
    * revert api report
    
    * updates yarn change message
    
    * maps component to MenuList rather than Menu
    
    * fixes focus state
    
    * updates styles
    
    * divides accordion and accordion item readme
    
    * removes dead imports
    
    * fixes submenu positioning
    
    * adds styles for icon alignment
    
    * reverts Menu component name
    
    * cleans up storybook
    
    * dynamically set the menu item icon attribute when icons is true for menu (#26998)
    
    * dynamically set the menu item icon attribute when icons is true for menu
    
    * move icons check to set items to ensure we update on item change and init
    
    * set items should be protected
    
    * updates Menu styles
    
    * updates icon alignment styles and docs
    
    * updates menu docs
    
    * adds checkmarks attribute and styles menu items
    
    * adds conditional styling for when icons are present
    
    * updates Menu and MenuItem docs
    
    * updates deltas in docs
    
    * updates docs
    
    * optimizes styles
    
    * optimizes css
    
    * optimizes css
    
    * optimizes css
    
    * reverts json export
    
    * working (#27293)
    
    * optimizes styles
    
    * adds docs to menu.ts
    
    * updates menu logic for readability
    
    * menu: fixes style syntax error
    
    * menu: consolidates styles
    
    * menu: updates styling
    
    * menu: updates styling
    
    * yarn change
    
    * menulist, menuitem: changes component name to MenuList
    
    * menulist, menuitem, updates readme
    
    * menu, menuitem: fixes circular dep
    
    * menu, menuitem: alphabetize index.js
    
    ---------
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>
    Co-authored-by: Jeff Smith <37851214+eljefe223@users.noreply.github.com>

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -10,6 +10,8 @@ export * from './divider/index.js';
 export * from './image/index.js';
 export * from './label/index.js';
 export * from './menu-button/index.js';
+export * from './menu-item/index.js';
+export * from './menu-list/index.js';
 export * from './progress-bar/index.js';
 export * from './slider/index.js';
 export * from './spinner/index.js';
---
+++ packages/web-components/src/menu-item/index.ts
@@ -0,0 +1,4 @@
+export * from './menu-item.js';
+export { template as MenuItemTemplate } from './menu-item.template.js';
+export { styles as MenuItemStyles } from './menu-item.styles.js';
+export { definition as MenuItemDefinition } from './menu-item.definition.js';
