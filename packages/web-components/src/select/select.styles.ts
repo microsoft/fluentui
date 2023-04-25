e5df3d9ce38dcb6ec9e86a8b6804d0a10857965d
Ryan Merrill <ryan@ryanmerrill.net>
2023-04-25 13:26:53 -0400

    Adds focus styles to Select box

--- packages/web-components/src/select/select.styles.ts
+++ packages/web-components/src/select/select.styles.ts
@@ -2,15 +2,25 @@ import { css } from '@microsoft/fast-element';
 import { display } from '@microsoft/fast-foundation';
 import {
   borderRadiusMedium,
+  colorCompoundBrandStroke,
   colorNeutralBackground1,
   colorNeutralForeground1,
   colorNeutralStroke1,
   colorNeutralStrokeAccessible,
+  curveAccelerateMid,
+  curveDecelerateMid,
+  durationNormal,
+  durationUltraFast,
   fontFamilyBase,
+  fontSizeBase200,
   fontSizeBase300,
+  fontSizeBase400,
   fontWeightRegular,
+  lineHeightBase200,
   lineHeightBase300,
+  lineHeightBase400,
   spacingHorizontalMNudge,
+  spacingHorizontalXXS,
 } from '../theme/design-tokens.js';
 
 /** Text styles
@@ -22,15 +32,19 @@ export const styles = css`
   }
 
   label {
+    font-family: ${fontFamilyBase};
+    font-size: ${fontSizeBase300};
+    line-height: ${lineHeightBase300};
     display: block;
     width: 100%;
+    color: ${colorNeutralForeground1};
   }
 
   select {
     display: block;
-    width: 100%;
     height: 32px;
-    padding: 0 ${spacingHorizontalMNudge};
+    padding: 0;
+    flex-grow: 1;
     font-size: ${fontSizeBase300};
     font-weight: ${fontWeightRegular};
     line-height: ${lineHeightBase300};
@@ -42,6 +56,8 @@ export const styles = css`
     appearance: none;
     -webkit-appearance: none;
     -moz-appearance: none;
+    padding-right: calc(${spacingHorizontalMNudge} + 20px + ${spacingHorizontalXXS} + ${spacingHorizontalXXS});
+    padding-left: calc(${spacingHorizontalMNudge} + ${spacingHorizontalXXS});
   }
 
   :host([size='small']) select {
@@ -52,32 +68,80 @@ export const styles = css`
     height: 40px;
   }
 
-  svg {
-    width: 24px;
-    height: 24px;
-  }
-
   select:focus {
-    outline: none;
+    outline-color: transparent;
+    outline-style: solid;
+    outline-width: 2px;
   }
 
-  select::after {
+  :host([size='small']) select {
+    font-size: ${fontSizeBase200};
+    line-height: ${lineHeightBase200};
+  }
+
+  :host([size='large']) select {
+    font-size: ${fontSizeBase400};
+    line-height: ${lineHeightBase400};
+  }
+
+  .select-wrapper {
+    display: flex;
+    flex-wrap: no-wrap;
+    position: relative;
+    align-items: center;
+  }
+
+  .select-wrapper::after {
+    display: block;
+    position: absolute;
     content: '';
     border-radius: 0 0 ${borderRadiusMedium} ${borderRadiusMedium};
+    transform: scaleX(0);
+    transition-delay: ${curveAccelerateMid};
+    transition-duration: ${durationUltraFast};
+    transition-property: transform;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: ${borderRadiusMedium};
     background-image: linear-gradient(
       0deg,
-      var(--colorCompoundBrandStroke) 0%,
-      var(--colorCompoundBrandStroke) 50%,
+      ${colorCompoundBrandStroke} 0%,
+      ${colorCompoundBrandStroke} 50%,
       transparent 50%,
       transparent 100%
     );
   }
 
-  select:focus::after {
-    height: ${borderRadiusMedium};
+  .select-wrapper:focus-within::after {
+    transform: scaleX(1);
+    transition-property: transform;
+    transition-delay: ${curveDecelerateMid};
+    transition-duration: ${durationNormal};
+  }
+
+  .dropdown-arrow {
     position: absolute;
-    bottom: 0;
-    right: 0;
-    left: 0;
+    right: ${spacingHorizontalMNudge};
+    pointer-events: none;
+    width: 20px;
+    height: 20px;
+    font-size: 20px;
+  }
+
+  .dropdown-arrow svg {
+    color: ${colorNeutralStrokeAccessible};
+  }
+
+  :host([size='small']) .dropdown-arrow {
+    width: 16px;
+    height: 16px;
+    font-size: 16px;
+  }
+
+  :host([size='large']) .dropdown-arrow {
+    width: 24px;
+    height: 24px;
+    font-size: 24px;
   }
 `;
--- packages/web-components/src/select/select.template.ts
+++ packages/web-components/src/select/select.template.ts
@@ -10,37 +10,40 @@ export function selectTemplate<T extends Select>(): ElementViewTemplate<T> {
   return html<T>`
     <template>
       <label part="label"><slot name="label"></slot></label>
-      <select
-        part="control"
-        :autofocus="${x => x.autofocus}"
-        :autocomplete="${x => x.autocomplete}"
-        ?disabled="${x => x.disabled}"
-        @change="${x => x.handleChange()}"
-        @input="${x => x.handleInput()}"
-      >
-        ${repeat(
-          x => x.options,
-          html`<option ?selected=${opt => opt.selected} value=${opt => opt.value}>${opt => opt.innerText}</option>`,
-        )}
-      </select>
+      <div class="select-wrapper" part="select-wrapper">
+        <select
+          part="control"
+          :autofocus="${x => x.autofocus}"
+          :autocomplete="${x => x.autocomplete}"
+          ?disabled="${x => x.disabled}"
+          @change="${x => x.handleChange()}"
+          @input="${x => x.handleInput()}"
+        >
+          ${repeat(
+            x => x.options,
+            html`<option ?selected=${opt => opt.selected} value=${opt => opt.value}>${opt => opt.innerText}</option>`,
+          )}
+        </select>
+        <span class="dropdown-arrow" part="dropdown-arrow">
+          <slot name="dropdown-arrow">
+            <svg
+              fill="currentColor"
+              aria-hidden="true"
+              width="1em"
+              height="1em"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.85 7.65c.2.2.2.5 0 .7l-5.46 5.49a.55.55 0 0 1-.78 0L4.15 8.35a.5.5 0 1 1 .7-.7L10 12.8l5.15-5.16c.2-.2.5-.2.7 0Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </slot>
+        </span>
+      </div>
       <span hidden>
         <slot @slotchange=${x => x.handleSlotChange}></slot>
-      </span>
-      <span part="dropdown-arrow">
-        <slot name="dropdown-arrow">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-chevron-down"
-          >
-            <path d="M6 9l6 6 6-6" />
-          </svg>
-        </slot>
       </span>
     </template>
   `;
