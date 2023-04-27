8e61ff1103cbb9d524203001eabccf5847319370
Brian Brady <brianbrady@microsoft.com>
2023-04-27 11:52:24 -0700

    radiogroup, radio: removes whitespace

--- packages/web-components/src/radio/radio.template.ts
+++ packages/web-components/src/radio/radio.template.ts
@@ -3,5 +3,5 @@ import { radioTemplate } from '@microsoft/fast-foundation';
 import type { Radio } from './radio.js';
 
 export const template: ElementViewTemplate<Radio> = radioTemplate({
-  checkedIndicator: html` <div part="checked-indicator" class="checked-indicator"></div> `,
+  checkedIndicator: html`<div part="checked-indicator" class="checked-indicator"></div>`,
 });
