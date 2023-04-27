7035ce5a092e6a379c9bdf97b0fb5b77dee43f13
Marcos Moura <marcosvmmoura@gmail.com>
2023-04-27 16:02:51 +0200

    feat: add support to override drawer styles

--- packages/react-components/react-drawer/src/components/Drawer/Drawer.tsx
+++ packages/react-components/react-drawer/src/components/Drawer/Drawer.tsx
@@ -4,6 +4,7 @@ import { renderDrawer_unstable } from './renderDrawer';
 import { useDrawerStyles_unstable } from './useDrawerStyles.styles';
 import type { DrawerProps } from './Drawer.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * Drawer contains supplementary content and are used for complex creation, edit, or management experiences.
@@ -12,6 +13,8 @@ export const Drawer: ForwardRefComponent<DrawerProps> = React.forwardRef((props,
   const state = useDrawer_unstable(props, ref);
 
   useDrawerStyles_unstable(state);
+  useCustomStyleHook_unstable('useDrawerStyles_unstable')(state);
+
   return renderDrawer_unstable(state);
 });
 
