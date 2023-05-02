ef6d38662c3e00d89f1fcecd5f864ebbf1ee5478
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-05-02 12:22:17 +0800

    revert change in useTagButton_unstable

--- packages/react-components/react-tags/src/components/TagButton/useTagButton.tsx
+++ packages/react-components/react-tags/src/components/TagButton/useTagButton.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { resolveShorthand } from '@fluentui/react-utilities';
 import type { TagButtonProps, TagButtonState } from './TagButton.types';
 import { useTag_unstable } from '../Tag/index';
 
@@ -12,5 +13,12 @@ import { useTag_unstable } from '../Tag/index';
  * @param ref - reference to root HTMLElement of TagButton
  */
 export const useTagButton_unstable = (props: TagButtonProps, ref: React.Ref<HTMLElement>): TagButtonState => {
-  return useTag_unstable({ ...props, content: { tabIndex: 0, ...props.content } }, ref);
+  const state = useTag_unstable(props, ref);
+  state.content = resolveShorthand(props.content, {
+    required: true,
+    defaultProps: {
+      tabIndex: 0,
+    },
+  });
+  return state;
 };
