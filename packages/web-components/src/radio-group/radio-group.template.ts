9a6705fc5e6a8b5982be92088357018bbffa3edd
Brian Brady <brianbrady@microsoft.com>
2023-05-05 11:52:18 -0700

    radiogroup, radio: fixes import

--- packages/web-components/src/radio-group/radio-group.template.ts
+++ packages/web-components/src/radio-group/radio-group.template.ts
@@ -1,5 +1,5 @@
 import type { ElementViewTemplate } from '@microsoft/fast-element';
 import { radioGroupTemplate } from '@microsoft/fast-foundation';
-import type { Radio } from './radio-group.js';
+import type { RadioGroup } from './radio-group.js';
 
-export const template: ElementViewTemplate<Radio> = radioGroupTemplate();
+export const template: ElementViewTemplate<RadioGroup> = radioGroupTemplate();
