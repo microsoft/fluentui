2472098f39b2ca738b837e35f2bd94cf0c2b5587
Ben Howell <behowell@microsoft.com>
2023-05-08 14:52:08 -0700

    Crop new storywright stories

--- apps/vr-tests-react-components/src/stories/PresenceBadge.stories.tsx
+++ apps/vr-tests-react-components/src/stories/PresenceBadge.stories.tsx
@@ -2,6 +2,8 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { PresenceBadge } from '@fluentui/react-badge';
 import { tokens } from '@fluentui/react-theme';
+import { Steps, StoryWright } from 'storywright';
+import { TestWrapperDecorator } from '../utilities/TestWrapperDecorator';
 
 const statuses = ['available', 'away', 'busy', 'do-not-disturb', 'offline', 'out-of-office', 'unknown'] as const;
 const sizes = ['tiny', 'extra-small', 'small', 'medium', 'large', 'extra-large'] as const;
@@ -42,30 +44,35 @@ storiesOf('PresenceBadge Converged - sizes', module).addStory(
   { includeRtl: true },
 );
 
-storiesOf('PresenceBadge Converged - inverted background', module).addStory(
-  'default',
-  () => (
-    <div
-      style={{
-        display: 'inline-grid',
-        gridTemplateColumns: `repeat(${2 * statuses.length}, auto)`,
-        placeItems: 'start',
-        gap: '10px',
-        padding: '16px',
-        backgroundColor: tokens.colorNeutralBackgroundInverted,
-      }}
-    >
-      {sizes.map(size => (
-        <>
-          {statuses.map(status => (
-            <PresenceBadge key={size + status} size={size} status={status} />
-          ))}
-          {statuses.map(status => (
-            <PresenceBadge key={size + status + 'OOO'} size={size} status={status} outOfOffice />
-          ))}
-        </>
-      ))}
-    </div>
-  ),
-  { includeHighContrast: true, includeDarkMode: true },
-);
+storiesOf('PresenceBadge Converged - inverted background', module)
+  .addDecorator(TestWrapperDecorator)
+  .addDecorator(story => (
+    <StoryWright steps={new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>{story()}</StoryWright>
+  ))
+  .addStory(
+    'default',
+    () => (
+      <div
+        style={{
+          display: 'inline-grid',
+          gridTemplateColumns: `repeat(${2 * statuses.length}, auto)`,
+          placeItems: 'start',
+          gap: '10px',
+          padding: '16px',
+          backgroundColor: tokens.colorNeutralBackgroundInverted,
+        }}
+      >
+        {sizes.map(size => (
+          <>
+            {statuses.map(status => (
+              <PresenceBadge key={size + status} size={size} status={status} />
+            ))}
+            {statuses.map(status => (
+              <PresenceBadge key={size + status + 'OOO'} size={size} status={status} outOfOffice />
+            ))}
+          </>
+        ))}
+      </div>
+    ),
+    { includeHighContrast: true, includeDarkMode: true },
+  );
