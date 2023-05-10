d77f18c06f674adf2a3cedf4cb11df37a14863b6
Lingfan Gao <lingfangao@hotmail.com>
2023-05-10 11:54:47 +0200

    stop leaking react

--- packages/react-components/react-toast/src/state/vanilla/dispatchToast.ts
+++ packages/react-components/react-toast/src/state/vanilla/dispatchToast.ts
@@ -1,10 +1,9 @@
-import * as React from 'react';
 import { ToastOptions } from '../types';
 import { EVENTS } from '../constants';
 
 let counter = 0;
 
-export function dispatchToast(content: React.ReactNode, options: ToastOptions = {}, targetDocument: Document) {
+export function dispatchToast(content: unknown, options: ToastOptions = {}, targetDocument: Document) {
   options.toastId ??= (counter++).toString();
   const event = new CustomEvent(EVENTS.show, { bubbles: false, cancelable: false, detail: { ...options, content } });
   targetDocument.dispatchEvent(event);
