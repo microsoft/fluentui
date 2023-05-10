26d32a4d4201df03fe28e908c56ac4561f8f0260
Marcos Moura <marcosvmmoura@gmail.com>
2023-05-10 15:58:06 +0200

    fix: remove unused stories

--- packages/react-components/react-drawer/stories/DrawerFooter/DrawerFooterDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { DrawerFooter, DrawerFooterProps } from '@fluentui/react-drawer';
-
-export const Default = (props: Partial<DrawerFooterProps>) => <DrawerFooter {...props} />;
