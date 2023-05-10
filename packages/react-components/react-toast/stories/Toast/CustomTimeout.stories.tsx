9c3566ca6314d67eb6f30e5304f57337ecad35aa
Lingfan Gao <lingfangao@hotmail.com>
2023-05-10 11:53:30 +0200

    fix cruft

--- packages/react-components/react-toast/stories/Toast/CustomTimeout.stories.tsx
+++ packages/react-components/react-toast/stories/Toast/CustomTimeout.stories.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { Toaster, useToastController } from '@fluentui/react-toast';
 
-let toastId = 0;
-
 export const CustomTimeout = () => {
   const { dispatchToast } = useToastController();
-  const notify = () => dispatchToast('This is a toast', { toastId: (toastId++).toString(), timeout: 1000 });
+  const notify = () => dispatchToast('This is a toast', { timeout: 1000 });
 
   return (
     <>
