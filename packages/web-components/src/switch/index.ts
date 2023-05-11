fabd248e5d3e185e551fe7713227653d4d307c65
Ryan Merrill <ryan@ryanmerrill.net>
2023-05-11 16:32:57 -0400

    Correct export naming in Switch component

--- packages/web-components/src/switch/index.ts
+++ packages/web-components/src/switch/index.ts
@@ -1,5 +1,5 @@
 export * from './switch.js';
 export * from './switch.options.js';
-export { definition } from './switch.definition.js';
+export { definition as SwitchDefinition } from './switch.definition.js';
 export { styles as switchStyles } from './switch.styles.js';
 export { template as switchTemplate } from './switch.template.js';
