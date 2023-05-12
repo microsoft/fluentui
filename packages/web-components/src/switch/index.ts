9465cc4b0fd981220e25b92eb051d370b7a0330c
Ryan Merrill <ryanmerrill@microsoft.com>
2023-05-12 14:52:37 -0400

    Correct export naming in Switch component (#27821)
    * Correct export naming in Switch component
    
    * Formats capitalization
    
    * Adds change file

--- packages/web-components/src/switch/index.ts
+++ packages/web-components/src/switch/index.ts
@@ -1,5 +1,5 @@
 export * from './switch.js';
 export * from './switch.options.js';
-export { definition } from './switch.definition.js';
-export { styles as switchStyles } from './switch.styles.js';
-export { template as switchTemplate } from './switch.template.js';
+export { definition as SwitchDefinition } from './switch.definition.js';
+export { styles as SwitchStyles } from './switch.styles.js';
+export { template as SwitchTemplate } from './switch.template.js';
