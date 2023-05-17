de522113197a7598f675695ccb7e6478836d4f4e
Oleksandr Fediashov <olfedias@microsoft.com>
2023-05-17 16:36:15 +0200

    chore: move styles to .styles.ts files

--- apps/public-docsite-v9/src/Concepts/Poster/OverviewCard.tsx
+++ apps/public-docsite-v9/src/Concepts/Poster/OverviewCard.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-
-import { Image, makeStyles, shorthands, tokens, typographyStyles } from '@fluentui/react-components';
+import { Image } from '@fluentui/react-components';
+import { useOverviewCardStyles } from './OverviewCard.styles';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -18,56 +18,8 @@ import starsImageSrc from '../../../public/fluent9-stars.png';
 // @ts-ignore
 import springImageSrc from '../../../public/fluent9-spring.png';
 
-const useStyles = makeStyles({
-  root: {
-    ...shorthands.border(tokens.strokeWidthThick, 'solid', tokens.colorNeutralBackground3),
-    ...shorthands.borderRadius(tokens.borderRadiusMedium),
-    ...shorthands.overflow('visible'),
-    ...shorthands.padding('40px'),
-    boxSizing: 'border-box',
-    boxShadow: tokens.shadow16,
-    ...shorthands.margin('50px'),
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  header: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  title: {
-    ...typographyStyles.title1,
-  },
-  tagline: {
-    ...typographyStyles.caption1,
-  },
-  features: {
-    display: 'flex',
-    alignItems: 'center',
-    alignContent: 'center',
-  },
-  logo: {
-    alignSelf: 'center',
-    width: '400px',
-  },
-  featuresList: {
-    display: 'grid',
-    gridTemplateRows: '50px auto 50px auto 50px auto',
-    gridTemplateColumns: '180px',
-    alignItems: 'center',
-    justifyItems: 'center',
-    justifyContent: 'center',
-    rowGap: '5px',
-    ...shorthands.padding('30px', 0, 0, '30px'),
-  },
-  featureText: {
-    ...typographyStyles.caption2,
-    ...shorthands.margin('10px'),
-  },
-});
-
 export const OverviewCard: React.FunctionComponent = () => {
-  const styles = useStyles();
+  const styles = useOverviewCardStyles();
 
   return (
     <div className={styles.root}>
