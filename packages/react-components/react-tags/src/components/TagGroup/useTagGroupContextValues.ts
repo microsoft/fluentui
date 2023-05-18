56767eb8512043844b89fb3e894b41dbfe391fa9
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-05-18 10:56:14 +0200

    prettier after merge suggestion

--- packages/react-components/react-tags/src/components/TagGroup/useTagGroupContextValues.ts
+++ packages/react-components/react-tags/src/components/TagGroup/useTagGroupContextValues.ts
@@ -3,5 +3,5 @@ import type { TagGroupContextValues, TagGroupState } from './TagGroup.types';
 
 export function useTagGroupContextValues_unstable(state: TagGroupState): TagGroupContextValues {
   const { size } = state;
-  return {tagGroup: React.useMemo(() => ({ size }), [size])};
+  return { tagGroup: React.useMemo(() => ({ size }), [size]) };
 }
