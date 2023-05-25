f6d270cddecb869339409722aa659e8b300f0aba
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-05-25 13:23:49 +0200

    combobox with tag story update

--- packages/react-components/react-combobox/stories/Combobox/ComboboxMultiselectWithTags.stories.tsx
+++ packages/react-components/react-combobox/stories/Combobox/ComboboxMultiselectWithTags.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Button, Combobox, makeStyles, Option, shorthands, tokens, useId } from '@fluentui/react-components';
+import { Combobox, makeStyles, Option, shorthands, tokens, useId } from '@fluentui/react-components';
 import type { ComboboxProps } from '@fluentui/react-components';
-import { Dismiss12Regular } from '@fluentui/react-icons';
+import { Tag, TagGroup, TagGroupProps } from '@fluentui/react-tags';
 
 const useStyles = makeStyles({
   root: {
@@ -27,10 +27,6 @@ export const MultiselectWithTags = (props: Partial<ComboboxProps>) => {
   const comboId = useId('combo-multi');
   const selectedListId = `${comboId}-selection`;
 
-  // refs for managing focus when removing tags
-  const selectedListRef = React.useRef<HTMLUListElement>(null);
-  const comboboxInputRef = React.useRef<HTMLInputElement>(null);
-
   const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
   const styles = useStyles();
 
@@ -42,18 +38,8 @@ export const MultiselectWithTags = (props: Partial<ComboboxProps>) => {
     setSelectedOptions(data.selectedOptions);
   };
 
-  const onTagClick = (option: string, index: number) => {
-    // remove selected option
-    setSelectedOptions(selectedOptions.filter(o => o !== option));
-
-    // focus previous or next option, defaulting to focusing back to the combo input
-    const indexToFocus = index === 0 ? 1 : index - 1;
-    const optionToFocus = selectedListRef.current?.querySelector(`#${comboId}-remove-${indexToFocus}`);
-    if (optionToFocus) {
-      (optionToFocus as HTMLButtonElement).focus();
-    } else {
-      comboboxInputRef.current?.focus();
-    }
+  const onTagDismiss: TagGroupProps['onDismiss'] = (_e, { dismissedTagValue }) => {
+    setSelectedOptions(selectedOptions.filter(o => o !== dismissedTagValue));
   };
 
   const labelledBy = selectedOptions.length > 0 ? `${comboId} ${selectedListId}` : comboId;
@@ -62,28 +48,13 @@ export const MultiselectWithTags = (props: Partial<ComboboxProps>) => {
     <div className={styles.root}>
       <label id={comboId}>Best pets</label>
       {selectedOptions.length ? (
-        <ul id={selectedListId} className={styles.tagsList} ref={selectedListRef}>
-          {/* The "Remove" span is used for naming the buttons without affecting the Combobox name */}
-          <span id={`${comboId}-remove`} hidden>
-            Remove
-          </span>
-          {selectedOptions.map((option, i) => (
-            <li key={option}>
-              <Button
-                size="small"
-                shape="circular"
-                appearance="primary"
-                icon={<Dismiss12Regular />}
-                iconPosition="after"
-                onClick={() => onTagClick(option, i)}
-                id={`${comboId}-remove-${i}`}
-                aria-labelledby={`${comboId}-remove ${comboId}-remove-${i}`}
-              >
-                {option}
-              </Button>
-            </li>
+        <TagGroup id={selectedListId} onDismiss={onTagDismiss}>
+          {selectedOptions.map(option => (
+            <Tag key={option} value={option}>
+              {option}
+            </Tag>
           ))}
-        </ul>
+        </TagGroup>
       ) : null}
       <Combobox
         aria-labelledby={labelledBy}
@@ -91,7 +62,6 @@ export const MultiselectWithTags = (props: Partial<ComboboxProps>) => {
         placeholder="Select one or more animals"
         selectedOptions={selectedOptions}
         onOptionSelect={onSelect}
-        ref={comboboxInputRef}
         {...props}
       >
         {options.map(option => (
