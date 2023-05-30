89152ac317904b5214f4fe350a64ca3be53a4cdb
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-05-30 11:52:03 +0200

    og datagrid

--- packages/react-components/react-data-grid-react-window/src/components/DataGrid/DataGrid.tsx
+++ packages/react-components/react-data-grid-react-window/src/components/DataGrid/DataGrid.tsx
@@ -6,10 +6,10 @@ import {
   useDataGridContextValues_unstable,
 } from '@fluentui/react-table';
 import type { DataGridProps } from '@fluentui/react-table';
-import { ForwardRefComponent } from '@fluentui/react-utilities';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 export const DataGrid: ForwardRefComponent<DataGridProps> = React.forwardRef((props, ref) => {
-  const state = useDataGrid_unstable({ ...props }, ref);
+  const state = useDataGrid_unstable(props, ref);
 
   useDataGridStyles_unstable(state);
   return renderDataGrid_unstable(state, useDataGridContextValues_unstable(state));
