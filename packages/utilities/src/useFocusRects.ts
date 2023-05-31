6b2a3785755531f77b7e5ea5fdf96c83150c94c1
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-05-31 16:13:07 -0400

    Avoid inlining setCallbackMap with terser.

--- packages/utilities/src/useFocusRects.ts
+++ packages/utilities/src/useFocusRects.ts
@@ -110,7 +110,9 @@ export function useFocusRects(rootRef?: React.RefObject<HTMLElement>): void {
     let onKeyUp: (ev: KeyboardEvent) => void;
     if (context?.providerRef?.current) {
       el = context.providerRef.current;
-      const callbacks = setCallbackMap(context);
+      // The NOINLINE directive tells terser not to move the setCallbackMap implementation into the call site during minification.
+      // This is prevents the function from capturing additional variables in the closure, which has been a source of memory leaks.
+      const callbacks = /*@__NOINLINE__*/ setCallbackMap(context);
       onMouseDown = callbacks.onMouseDown;
       onPointerDown = callbacks.onPointerDown;
       onKeyDown = callbacks.onKeyDown;
