2dee58b6f8adb8d1764547e10aaef60be9aa6025
ankityadav4 <ankityadav@microsoft.com>
2023-05-31 16:25:23 +0530

    test changes

--- packages/react-examples/src/react-charting-perf/demo/index.tsx
+++ packages/react-examples/src/react-charting-perf/demo/index.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+// import App from './App';
+import { ThemeProvider } from '@fluentui/react/lib/utilities/ThemeProvider/ThemeProvider';
+import { LineChartBasicExample } from '../PerformanceDataSet1/LineChart.Basic.Example';
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+// const rootElement = document.getElementById('root');
+// render(<App />, rootElement);
+
+const LineChartPageWrapper = () => (
+  <ThemeProvider>
+    <LineChartBasicExample />
+  </ThemeProvider>
+);
+render(<LineChartPageWrapper />, document.getElementById('content'));
