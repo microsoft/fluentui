a5fad01fd95a406c647597f342957b84ab3e709b
ankityadav4 <ankityadav@microsoft.com>
2023-05-31 16:50:38 +0530

    test changes

--- packages/react-examples/src/react-charting-perf/demo/index.tsx
+++ packages/react-examples/src/react-charting-perf/demo/index.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOM from 'react-dom';
+//import { render } from 'react-dom';
 // import App from './App';
 import { ThemeProvider } from '@fluentui/react/lib/utilities/ThemeProvider/ThemeProvider';
 import { LineChartBasicExample } from '../PerformanceDataSet1/LineChart.Basic.Example';
+//import ReactDOM = require('react-dom');
 
 // const rootElement = document.getElementById('root');
 // render(<App />, rootElement);
 
-const LineChartPageWrapper = () => (
-  <ThemeProvider>
-    <LineChartBasicExample />
-  </ThemeProvider>
-);
-render(<LineChartPageWrapper />, document.getElementById('content'));
+// const LineChartPageWrapper = () => (
+//   <ThemeProvider>
+//     <LineChartBasicExample />
+//   </ThemeProvider>
+// );
+// render(<LineChartPageWrapper />, document.getElementById('content'));
+let rootElement: HTMLElement | null;
+
+function _onLoad(): void {
+  const LineChartPageWrapper = () => (
+    <ThemeProvider>
+      <LineChartBasicExample />
+    </ThemeProvider>
+  );
+  ReactDOM.render(<LineChartPageWrapper />, document.getElementById('content'));
+  rootElement = rootElement || document.getElementById('content');
+}
+
+function _onUnload(): void {
+  if (rootElement) {
+    ReactDOM.unmountComponentAtNode(rootElement);
+  }
+}
+
+const isReady = document.readyState === 'interactive' || document.readyState === 'complete';
+
+if (isReady) {
+  _onLoad();
+} else {
+  window.onload = _onLoad;
+}
+
+window.onunload = _onUnload;
