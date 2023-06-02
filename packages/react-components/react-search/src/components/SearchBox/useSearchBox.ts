c9783cd14647f3d68ec81e23bc930c65f9b83229
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2023-06-02 10:56:38 -0700

    set up structure and slots of SearchBox using input as a starting point

--- packages/react-components/react-search/src/components/SearchBox/useSearchBox.ts
+++ packages/react-components/react-search/src/components/SearchBox/useSearchBox.ts
@@ -1,6 +1,13 @@
 import * as React from 'react';
-import { getNativeElementProps } from '@fluentui/react-utilities';
+import { useFieldControlProps_unstable } from '@fluentui/react-field';
+import {
+  getPartitionedNativeProps,
+  resolveShorthand,
+  useControllableState,
+  useEventCallback,
+} from '@fluentui/react-utilities';
 import type { SearchBoxProps, SearchBoxState } from './SearchBox.types';
+import { useOverrides_unstable as useOverrides } from '@fluentui/react-shared-contexts';
 
 /**
  * Create the state required to render SearchBox.
@@ -9,20 +16,60 @@ import type { SearchBoxProps, SearchBoxState } from './SearchBox.types';
  * before being passed to renderSearchBox_unstable.
  *
  * @param props - props from this instance of SearchBox
- * @param ref - reference to root HTMLElement of SearchBox
+ * @param ref - reference to `<input>` element of SearchBox
  */
-export const useSearchBox_unstable = (props: SearchBoxProps, ref: React.Ref<HTMLElement>): SearchBoxState => {
-  return {
-    // TODO add appropriate props/defaults
+export const useSearchBox_unstable = (props: SearchBoxProps, ref: React.Ref<HTMLInputElement>): SearchBoxState => {
+  props = useFieldControlProps_unstable(props, { supportsLabelFor: true, supportsRequired: true, supportsSize: true });
+
+  const overrides = useOverrides();
+
+  const { size = 'medium', appearance = overrides.inputDefaultAppearance ?? 'outline', onChange } = props;
+
+  const [value, setValue] = useControllableState({
+    state: props.value,
+    defaultState: props.defaultValue,
+    initialState: '',
+  });
+
+  const nativeProps = getPartitionedNativeProps({
+    props,
+    primarySlotTagName: 'input',
+    excludedPropNames: ['size', 'onChange', 'value', 'defaultValue'],
+  });
+
+  const state: SearchBoxState = {
+    size,
+    appearance,
     components: {
-      // TODO add each slot's element type or component
-      root: 'div',
+      root: 'span',
+      input: 'input',
+      contentBefore: 'span',
+      contentAfter: 'span',
+      dismiss: 'span',
     },
-    // TODO add appropriate slots, for example:
-    // mySlot: resolveShorthand(props.mySlot),
-    root: getNativeElementProps('div', {
-      ref,
-      ...props,
+    input: resolveShorthand(props.input, {
+      required: true,
+      defaultProps: {
+        type: 'search',
+        ref,
+        ...nativeProps.primary,
+      },
+    }),
+    contentBefore: resolveShorthand(props.contentBefore),
+    contentAfter: resolveShorthand(props.contentAfter),
+    dismiss: resolveShorthand(props.dismiss),
+    root: resolveShorthand(props.root, {
+      required: true,
+      defaultProps: nativeProps.root,
     }),
   };
+
+  state.input.value = value;
+  state.input.onChange = useEventCallback(ev => {
+    const newValue = ev.target.value;
+    onChange?.(ev, { value: newValue });
+    setValue(newValue);
+  });
+
+  return state;
 };
