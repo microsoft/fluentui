77ca96d98e53f8821d4842e8d98d6d1c88aea538
Marcos Moura <marcosvmmoura@gmail.com>
2023-06-04 15:14:34 +0200

    docs: create motion documentation

--- packages/react-components/react-drawer/stories/Drawer/index.stories.tsx
+++ packages/react-components/react-drawer/stories/Drawer/index.stories.tsx
@@ -18,10 +18,12 @@ export { Position } from './DrawerPosition.stories';
 export { Size } from './DrawerSize.stories';
 export { CustomSize } from './DrawerCustomSize.stories';
 export { Separator } from './DrawerSeparator.stories';
-export { AlwaysOpen } from './DrawerAlwaysOpen.stories';
-export { PreventClose } from './DrawerPreventClose.stories';
 export { WithNavigation } from './DrawerWithNavigation.stories';
 export { WithScroll } from './DrawerWithScroll.stories';
+export { DisabledTransition } from './DrawerDisabledTransition.stories';
+export { CustomTransition } from './DrawerCustomTransition.stories';
+export { AlwaysOpen } from './DrawerAlwaysOpen.stories';
+export { PreventClose } from './DrawerPreventClose.stories';
 export { Responsive } from './DrawerResponsive.stories';
 export { Resizable } from './DrawerResizable.stories';
 
