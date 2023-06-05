0d7c485d51ff889bf1eca94f7ecec5176190129b
brianchristopherbrady <brian.christopher.brady@gmail.com>
2023-06-05 13:46:27 -0700

    drawer: removes unused files

--- packages/web-components/src/drawer-trigger/drawer-trigger.styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@microsoft/fast-element';
-import { display } from '@microsoft/fast-foundation';
-
-/** drawer-trigger styles
- * @public
- */
-export const styles = css`
-  ${display('flex')}
-`;
--- packages/web-components/src/drawer-trigger/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './drawer-trigger.js';
-export { definition as DrawerTriggerDefinition } from './drawer-trigger.definition.js';
-export { styles as DrawerTriggerStyles } from './drawer-trigger.styles.js';
-export { template as DrawerTriggerTemplate } from './drawer-trigger.template.js';
