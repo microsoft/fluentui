ac7f17ec809a50b1b709e5fd66e8566b4181f08e
BrdyBrn <brian.christopher.brady@gmail.com>
2023-06-06 14:14:19 -0700

    Adds TextInput as a new web component (#27348)
    * radio init
    
    * styles radio
    
    * reverts branch
    
    * input spec init
    
    * cleans up spec
    
    * formatting
    
    * updates component name to text input
    
    * updates component name in spec
    
    * text input init
    
    * adds text input options
    
    * styles text input
    
    * updates text input styles
    
    * adds input size attribute and styles
    
    * updates text input storybook
    
    * text input: adds properties to class
    
    * text-input: re-exports TextFieldType as TextInputType
    
    * text input: fixes export names
    
    * text input: updates underline styles
    
    * text-input: exports text-input from package.json
    
    * removes dead code
    
    * text-input: removes default attr values
    
    * text-input: updates icon sizes
    
    * text-input: updates block vs inline implimentation
    
    * text-input: updates docs
    
    * text-input: adds styling support for icons and text passed through start/end slots
    
    * text-input: updates border styles
    
    * input: updates storybook content
    
    * updates README
    
    * text-input: adds filled non shadow variants
    
    * text-input: modifies storybook examples
    
    * text-input: removes dead import
    
    * addresses PR feedback
    
    * text input: updates styles
    
    * text input: yarn change
    
    * text input: updates README and styles
    
    * text input: shorthands css property
    
    * textinput: fixes ts errors
    
    * textinput: updates attribute name
    
    * textinput: uses fluent-label in stories
    
    * textinput: updates control size type name
    
    * textinput: updates storybook
    
    * textinput: updates type
    
    * textinput: updates story styles
    
    * Update change/@fluentui-web-components-ec003fd2-a453-4e83-b620-9ec9f3ddffa0.json
    
    Co-authored-by: Miroslav Stastny <mistastn@microsoft.com>
    
    * text-input: addresses feedback
    
    * text-input: alphabetize exports, removes setting default attr value
    
    ---------
    
    Co-authored-by: Miroslav Stastny <mistastn@microsoft.com>

---
+++ packages/web-components/src/text-input/index.ts
@@ -0,0 +1,6 @@
+export * from './text-input.js';
+export * from './text-input.options.js';
+export { template as TextInputTemplate } from './text-input.template.js';
+export { styles as TextInputStyles } from './text-input.styles.js';
+export { definition as TextInputDefinition } from './text-input.definition.js';
+export { TextFieldType as TextInputType } from '@microsoft/fast-foundation';
