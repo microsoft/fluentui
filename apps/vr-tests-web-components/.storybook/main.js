3805a51b3050fe0efcfaf39921a89b2bdc0c5306
Tristan Watanabe <tristan.watanabe@gmail.com>
2023-06-10 10:28:19 -0400

    chore: update storybook configs to match web-components package

--- apps/vr-tests-web-components/.storybook/main.js
+++ apps/vr-tests-web-components/.storybook/main.js
@@ -1,8 +1,29 @@
 const path = require('path');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
+const tsBin = require.resolve('typescript');
+const tsConfigPath = path.resolve(__dirname, '../../../tsconfig.base.wc.json');
+
+const tsPaths = new TsconfigPathsPlugin({
+  configFile: tsConfigPath,
+});
+
 module.exports = /** @type {import('../../../.storybook/main').StorybookBaseConfig} */ ({
-  addons: ['@fluentui/react-storybook-addon'],
+  addons: [
+    {
+      name: '@storybook/addon-docs',
+    },
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+        viewport: false,
+        toolbars: false,
+        actions: false,
+      },
+    },
+  ],
 
   stories: ['../src/**/*.stories.tsx'],
   core: {
@@ -14,29 +35,49 @@ module.exports = /** @type {import('../../../.storybook/main').StorybookBaseConf
     // disable react-docgen-typescript (totally not needed here, slows things down a lot)
     reactDocgen: false,
   },
-  webpackFinal: config => {
-    const tsPaths = new TsconfigPathsPlugin({
-      configFile: path.resolve(__dirname, '../../../tsconfig.base.json'),
-    });
+  webpackFinal: async config => {
+    config.resolve = config.resolve ?? {};
+    config.resolve.extensions = config.resolve.extensions ?? [];
+    config.resolve.plugins = config.resolve.plugins ?? [];
+    config.module = config.module ?? {};
+    config.plugins = config.plugins ?? [];
 
-    if (config.resolve) {
-      config.resolve.plugins ? config.resolve.plugins.push(tsPaths) : (config.resolve.plugins = [tsPaths]);
-    }
+    config.resolve.extensionAlias = {
+      '.js': ['.ts', '.js'],
+      '.mjs': ['.mts', '.mjs'],
+    };
+    config.resolve.extensions.push(...['.ts', '.js']);
+    config.resolve.plugins.push(tsPaths);
+    config.module.rules = config.module.rules ?? [];
+    config.module.rules.push(
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+        sideEffects: true,
+        options: {
+          transpileOnly: true,
+          compiler: tsBin,
+        },
+      },
+      // Following config is needed to be able to resolve @storybook packages imported in specified files that don't ship valid ESM
+      // It also enables importing other packages without proper ESM extensions, but that should be avoided !
+      // @see https://webpack.js.org/configuration/module/#resolvefullyspecified
+      {
+        test: /\.m?js/,
+        resolve: { fullySpecified: false },
+      },
+    );
 
-    if (config.module) {
-      config.module.rules?.unshift({
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: '@griffel/webpack-loader',
-            options: {
-              babelOptions: {
-                presets: ['@babel/preset-typescript'],
-              },
-            },
-          },
-        ],
-      });
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+        failOnError: process.env.NODE_ENV === 'production',
+      }),
+    );
+
+    // Disable ProgressPlugin which logs verbose webpack build progress. Warnings and Errors are still logged.
+    if (process.env.TF_BUILD || process.env.LAGE_PACKAGE_NAME) {
+      config.plugins = config.plugins.filter(({ constructor }) => constructor.name !== 'ProgressPlugin');
     }
 
     return config;
--- apps/vr-tests-web-components/.storybook/manager.js
+++ apps/vr-tests-web-components/.storybook/manager.js
@@ -1,9 +1,11 @@
 import { addons } from '@storybook/addons';
-import { create } from '@storybook/theming';
 
 addons.setConfig({
-  theme: create({
-    base: 'light',
-    brandTitle: 'Fabric',
-  }),
+  previewTabs: {
+    canvas: { hidden: true },
+  },
+  enableShortcuts: false,
+  sidebar: {
+    showRoots: true,
+  },
 });
