39f1ed2bd38b4480e325c15637405e9d4bbe05d0
Juraj Kapsiar <jukapsia@microsoft.com>
2023-06-11 15:46:02 -0700

    pretty

--- packages/fluentui/docs/src/examples/components/Datepicker/Types/DatepickerExampleOpen.shorthand.tsx
+++ packages/fluentui/docs/src/examples/components/Datepicker/Types/DatepickerExampleOpen.shorthand.tsx
@@ -4,8 +4,12 @@ import { Datepicker } from '@fluentui/react-northstar';
 
 const DatepickerExampleOpen = () => {
   const [open, setOpen] = useBooleanKnob({ name: 'open' });
-
-  return <Datepicker calendarOpenState={open} onCalendarOpenStateChange={(_, { calendarOpenState }) => setOpen(calendarOpenState)}/>;
+  return (
+    <Datepicker
+      calendarOpenState={open}
+      onCalendarOpenStateChange={(_, { calendarOpenState }) => setOpen(calendarOpenState)}
+    />
+  );
 };
 
 export default DatepickerExampleOpen;
