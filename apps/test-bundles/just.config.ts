182b89c0011405d7b6fedbf521e435587aeccd17
Martin Hochel <martinhochel@microsoft.com>
2023-06-13 16:28:04 +0200

    feat(test-bundles): replace jq cryptic script with custom merge script

--- apps/test-bundles/just.config.ts
+++ apps/test-bundles/just.config.ts
@@ -5,6 +5,7 @@ import { preset, task, series } from '@fluentui/scripts-tasks';
 import { run } from 'parallel-webpack';
 
 import { bundleSizeCollect } from './scripts/bundle-size-collect';
+import { mergeBundleSizes } from './scripts/merge-bundlesizes';
 
 preset();
 
@@ -21,6 +22,14 @@ task('bundle', done => {
   const configPath = path.join(__dirname, 'webpack.config.js');
   const options = {};
   run(configPath, options, done);
+});
+
+task('bundle-size-merge', () => {
+  const root = path.join(__dirname, 'dist');
+  mergeBundleSizes(
+    [path.join(root, 'react/bundlesize.json'), path.join(root, 'react-northstar/bundlesize.json')],
+    path.join(root, 'bundlesizes.json'),
+  );
 });
 
 task('bundle-size', series('bundle', 'bundle-size-collect'));
