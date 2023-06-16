0164af3076885cfe386375bf548b95151c7e0151
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-06-16 12:48:12 +0200

    stories

--- packages/react-components/react-tags/stories/TagGroup/TagGroupSizes.stories.tsx
+++ packages/react-components/react-tags/stories/TagGroup/TagGroupSizes.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { TagGroup, Tag, TagButton } from '@fluentui/react-tags';
 import { Avatar, makeStyles } from '@fluentui/react-components';
-import { Calendar3Day20Regular } from '@fluentui/react-icons';
+import { Calendar3DayRegular } from '@fluentui/react-icons';
 import { TagSize } from '../../src/Tag';
 
 const useContainerStyles = makeStyles({
@@ -14,7 +14,6 @@ const useContainerStyles = makeStyles({
 
 export const Sizes = () => {
   const containerStyles = useContainerStyles();
-  // TODO add one more size
   const sizes: TagSize[] = ['medium', 'small', 'extra-small'];
   return (
     <div className={containerStyles.root}>
@@ -22,17 +21,16 @@ export const Sizes = () => {
         <div key={size}>
           {`${size}: `}
           <TagGroup size={size}>
-            <Tag>Tag 1</Tag>
-            <Tag media={<Avatar name="Katri Athokas" />}>Tag 1</Tag>
-            <Tag icon={<Calendar3Day20Regular />}>Tag 1</Tag>
-
-            <Tag dismissible>Tag 1</Tag>
+            <Tag>{size}</Tag>
             <Tag dismissible media={<Avatar name="Katri Athokas" />}>
-              Tag 1
+              {size}
             </Tag>
-            <Tag dismissible icon={<Calendar3Day20Regular />}>
-              Tag 1
+            <Tag icon={<Calendar3DayRegular />} shape="circular">
+              {size}
             </Tag>
+            <TagButton dismissible icon={<Calendar3DayRegular />}>
+              {size}
+            </TagButton>
           </TagGroup>
         </div>
       ))}
@@ -44,7 +42,7 @@ Sizes.storyName = 'Sizes';
 Sizes.parameters = {
   docs: {
     description: {
-      story: 'A TagGroup supports different sizes',
+      story: 'A TagGroup supports `medium`, `small` and `extra-small` size. Default size is `medium`.',
     },
   },
 };
