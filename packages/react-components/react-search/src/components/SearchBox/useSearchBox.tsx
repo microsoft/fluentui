a147f8f1bca857357b181cf59b9fb0e96f5afabe
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2023-06-19 13:34:50 -0700

    Update packages/react-components/react-search/src/components/SearchBox/useSearchBox.tsx
    Co-authored-by: Sean Monahan <seanmonahan@microsoft.com>

--- packages/react-components/react-search/src/components/SearchBox/useSearchBox.tsx
+++ packages/react-components/react-search/src/components/SearchBox/useSearchBox.tsx
@@ -39,7 +39,7 @@ export const useSearchBox_unstable = (props: SearchBoxProps, ref: React.Ref<HTML
   });
 
   const onBlur: React.FocusEventHandler<HTMLSpanElement> = useEventCallback(ev => {
-    setFocused(searchBoxRootRef.current!.contains(ev.relatedTarget));
+    setFocused(searchBoxRootRef.current?.contains(ev.relatedTarget) ?? false);
   });
 
   const state: SearchBoxState = {
