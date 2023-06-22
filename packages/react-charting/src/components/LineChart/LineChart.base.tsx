8d368aab540b3261b3fc79de84b47706de3ff25f
Esteban Munoz <esteban.230@hotmail.com>
2023-06-22 12:01:16 -0700

    revert charting change

--- packages/react-charting/src/components/LineChart/LineChart.base.tsx
+++ packages/react-charting/src/components/LineChart/LineChart.base.tsx
@@ -264,8 +264,8 @@ export class LineChartBase extends React.Component<ILineChartProps, ILineChartSt
       ...this.props.calloutProps,
     };
     const tickParams = {
-      tickValues,
-      tickFormat,
+      tickValues: tickValues,
+      tickFormat: tickFormat,
     };
 
     return !this.state.emptyChart ? (
@@ -410,7 +410,7 @@ export class LineChartBase extends React.Component<ILineChartProps, ILineChartSt
       // mapping data to the format Legends component needs
       const legend: ILegend = {
         title: point.legend!,
-        color,
+        color: color,
         action: () => {
           if (isLegendMultiSelectEnabled) {
             this._handleMultipleLineLegendSelectionAction(point);
