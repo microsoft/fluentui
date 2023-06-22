875efbba9ecf9de8757c4c1ecac109075783c8e1
Devjoyti Barman <53661347+Devjoyti-Barman@users.noreply.github.com>
2023-06-22 06:57:58 +0000

    Added testing

--- packages/react-components/react-select/src/components/Select/useSelectStyles.styles.ts
+++ packages/react-components/react-select/src/components/Select/useSelectStyles.styles.ts
@@ -10,16 +10,16 @@ export const selectClassNames: SlotClassNames<SelectSlots> = {
 };
 
 const iconSizes = {
-  small: '16px',
-  medium: '20px',
-  large: '24px',
+  small: '36px',
+  medium: '50px',
+  large: '74px',
 };
 
 //TODO: Should fieldHeights be a set of global design tokens or constants?
 const fieldHeights = {
-  small: '24px',
-  medium: '32px',
-  large: '40px',
+  small: '44px',
+  medium: '52px',
+  large: '70px',
 };
 
 /* Since the <select> element must span the full width and cannot have children,
