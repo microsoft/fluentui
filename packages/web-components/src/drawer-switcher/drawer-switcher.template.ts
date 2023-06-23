791f3144a0eb038d586594f035eb02a994b88887
brianchristopherbrady <brian.christopher.brady@gmail.com>
2023-06-22 18:26:48 -0700

    drawer: removes dead import

--- packages/web-components/src/drawer-switcher/drawer-switcher.template.ts
+++ packages/web-components/src/drawer-switcher/drawer-switcher.template.ts
@@ -1,5 +1,4 @@
 import { html, repeat } from '@microsoft/fast-element';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import type { Drawer } from '../drawer/drawer.js';
 import type { DrawerSwitcher } from './drawer-switcher.js';
 
@@ -9,7 +8,7 @@ export const template = html<DrawerSwitcher>`
       x => x.drawerElements,
       html<Drawer>`
         <fluent-button icon-only @click="${(x, ctx) => ctx.parent.toggleDrawer(x)}">
-          ${x => unsafeHTML(x.getIcon())}
+          ${x => x.getIcon()}
         </fluent-button>
       `,
     )}
