1c7e6451a11fb9c59e344df616394a320e8031c4
ling1726 <lingfangao@hotmail.com>
2023-06-27 12:25:26 +0200

    feat: Observed element hooks (#28291)
    * feat: Observed element hooks
    
    Implements hooks that enable the use of `observedElement` API in
    react-tabster
    
    Fixes #28030
    
    * changefiles
    
    * fix md
    
    * Add tabster documentation
    
    * add useModalAttribute docs
    
    * improve observable examples
    
    * improve focusable group examples
    
    * improve arrow navigation group
    
    * add focus finders
    
    * Update packages/react-components/react-components/stories/Concepts/FocusManagement/useFocusFinders/FindPrevious.stories.tsx
    
    * fix type check
    
    * improve styles
    
    * improve docs
    
    * Add utilities section

---
+++ packages/react-components/react-components/stories/Concepts/FocusManagement/useFocusFinders/FindFirst.stories.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {
+  TextBoldRegular,
+  TextUnderlineRegular,
+  TextItalicRegular,
+  TextAlignLeftRegular,
+  TextAlignCenterRegular,
+  TextAlignRightRegular,
+  CopyRegular,
+  ClipboardPasteRegular,
+  CutRegular,
+} from '@fluentui/react-icons';
+import {
+  Button,
+  useArrowNavigationGroup,
+  makeStyles,
+  shorthands,
+  useFocusFinders,
+  tokens,
+} from '@fluentui/react-components';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: '5px',
+    display: 'flex',
+    ...shorthands.gap('5px'),
+    ...shorthands.padding('10px'),
+    ...shorthands.border('2px', 'solid', tokens.colorBrandStroke1),
+    ...shorthands.borderRadius(tokens.borderRadiusMedium),
+  },
+});
+
+export const FindFirst = () => {
+  const styles = useStyles();
+  const { findFirstFocusable } = useFocusFinders();
+  const toolbarRef = React.useRef<HTMLDivElement>(null);
+  const attributes = useArrowNavigationGroup({ axis: 'horizontal' });
+
+  const onClick = () => {
+    if (toolbarRef.current) {
+      findFirstFocusable(toolbarRef.current)?.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Button onClick={onClick}>Focus first</Button>
+      </div>
+      <div
+        ref={toolbarRef}
+        aria-label="Editor toolbar example"
+        role="toolbar"
+        {...attributes}
+        className={styles.container}
+      >
+        <Button disabled aria-label="Bold" icon={<TextBoldRegular />} />
+        <Button appearance="primary" aria-label="Underline" icon={<TextUnderlineRegular />} />
+        <Button aria-label="Italic" icon={<TextItalicRegular />} />
+        <Button aria-label="Align Left" icon={<TextAlignLeftRegular />} />
+        <Button aria-label="Align Center" icon={<TextAlignCenterRegular />} />
+        <Button aria-label="Align Right" icon={<TextAlignRightRegular />} />
+        <Button aria-label="Copy" icon={<CopyRegular />} />
+        <Button aria-label="Cut" icon={<CutRegular />} />
+        <Button aria-label="Paste" icon={<ClipboardPasteRegular />} />
+      </div>
+    </>
+  );
+};
