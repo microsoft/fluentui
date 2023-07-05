cc29777c8ee4a213a7aae63cb35923c8cb0c9049
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2023-07-05 16:28:34 +0530

    Fix react examples charting path

--- apps/public-docsite-resources/src/components/pages/LegendsPage.tsx
+++ apps/public-docsite-resources/src/components/pages/LegendsPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { DemoPage } from '../DemoPage';
 
-import { LegendsPageProps } from '@fluentui/react-examples/lib/react/Legends/Legends.doc';
+import { LegendsPageProps } from '@fluentui/react-examples/lib/react-charting/Legends/Legends.doc';
 
 export const LegendsPage = (props: { isHeaderVisible: boolean }) => (
   <DemoPage jsonDocs={require('../../../dist/api/react/Legends.page.json')} {...{ ...LegendsPageProps, ...props }} />
