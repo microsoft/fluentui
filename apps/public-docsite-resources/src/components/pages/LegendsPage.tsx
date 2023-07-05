3ee697f54e7cec2f5559745b7646f8b3b8e9c547
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2023-07-05 16:39:20 +0530

    Fix charting path in docs

--- apps/public-docsite-resources/src/components/pages/LegendsPage.tsx
+++ apps/public-docsite-resources/src/components/pages/LegendsPage.tsx
@@ -4,5 +4,8 @@ import { DemoPage } from '../DemoPage';
 import { LegendsPageProps } from '@fluentui/react-examples/lib/react-charting/Legends/Legends.doc';
 
 export const LegendsPage = (props: { isHeaderVisible: boolean }) => (
-  <DemoPage jsonDocs={require('../../../dist/api/react/Legends.page.json')} {...{ ...LegendsPageProps, ...props }} />
+  <DemoPage
+    jsonDocs={require('../../../dist/api/react-charting/Legends.page.json')}
+    {...{ ...LegendsPageProps, ...props }}
+  />
 );
