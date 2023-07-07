e79075445ff159f134ee6312a24dd713c70aa2e6
Esteban Munoz <esteban.230@hotmail.com>
2023-07-06 21:09:21 -0700

    fix(react-datepicker-compat, Calendar): Add hover styles for gototoday button.

--- packages/react/src/components/Calendar/Calendar.styles.ts
+++ packages/react/src/components/Calendar/Calendar.styles.ts
@@ -1,4 +1,4 @@
-import { normalize, FontSizes, getFocusStyle } from '@fluentui/style-utilities';
+import { normalize, FontSizes, getFocusStyle, HighContrastSelector } from '@fluentui/style-utilities';
 import type { ICalendarStyleProps, ICalendarStyles } from './Calendar.types';
 
 export const styles = (props: ICalendarStyleProps): ICalendarStyles => {
@@ -42,7 +42,7 @@ export const styles = (props: ICalendarStyleProps): ICalendarStyles => {
         lineHeight: 30,
         backgroundColor: 'transparent',
         border: 'none',
-        boxSizing: 'content-box',
+        boxSizing: 'border-box',
         padding: '0 4px',
         alignSelf: 'flex-end',
         marginRight: 16,
@@ -58,6 +58,12 @@ export const styles = (props: ICalendarStyleProps): ICalendarStyles => {
             color: palette.themePrimary,
             backgroundColor: 'transparent',
             cursor: 'pointer',
+            selectors: {
+              [HighContrastSelector]: {
+                outline: '1px solid Buttontext',
+                borderRadius: '2px',
+              },
+            },
           },
           '&:active': {
             color: palette.themeDark,
