930e58165e840485efde35de208b0d359846902b
srmukher <srmukher@microsoft.com>
2023-07-11 14:09:53 +0530

    Fixing build and test errors

--- packages/react-charting/src/components/SankeyChart/SankeyChartRTL.test.tsx
+++ packages/react-charting/src/components/SankeyChart/SankeyChartRTL.test.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { queryAllByAttribute, render, waitFor } from '@testing-library/react';
 import { data } from './SankeyChart.test';
 import { IChartProps, SankeyChart } from './index';
+import { resetIds } from '../../Utilities';
 
 const emptyChartPoints: IChartProps = {
   chartData: [],
 };
 
+function sharedBeforeEach() {
+  resetIds();
+}
+
 describe('Sankey chart rendering', () => {
+  beforeEach(sharedBeforeEach);
   test('Should re-render the Sankey chart with data', async () => {
     // Arrange
     const { container, rerender } = render(<SankeyChart data={emptyChartPoints} />);
