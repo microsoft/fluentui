fadf588a1856008bc06e37e8dda2cd159c825cce
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-07-12 10:50:20 +0200

    remove VR test for deprecated style

--- apps/vr-tests-react-components/src/stories/Table.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Table.stories.tsx
@@ -634,50 +634,6 @@ const Truncate: React.FC<SharedVrTestArgs & { truncate?: boolean }> = ({ noNativ
   </Table>
 );
 
-const KeyboardColumnResizingStyle: React.FC<SharedVrTestArgs> = ({ noNativeElements }) => {
-  return (
-    <Table noNativeElements={noNativeElements}>
-      <TableHeader>
-        <TableRow>
-          {columns.map(column => (
-            <TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {items.map((item, i) => (
-          <TableRow key={item.file.label} className={`row-${i}`}>
-            <TableCell>
-              <TableCellLayout media={item.file.icon}>
-                {item.file.label}
-                <TableCellActions>
-                  <Button icon={<EditRegular />} appearance="subtle" />
-                  <Button icon={<MoreHorizontalRegular />} appearance="subtle" />
-                </TableCellActions>
-              </TableCellLayout>
-            </TableCell>
-            <TableCell>
-              <TableCellLayout
-                media={
-                  <Avatar name={item.author.label} badge={{ status: item.author.status as PresenceBadgeStatus }} />
-                }
-              >
-                {item.author.label}
-              </TableCellLayout>
-            </TableCell>
-            <TableCell>
-              <TableCellLayout>{item.lastUpdated.label}</TableCellLayout>
-            </TableCell>
-            <TableCell>
-              <TableCellLayout media={item.lastUpdate.icon}>{item.lastUpdate.label}</TableCellLayout>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
-
 ([true, false] as const).forEach(noNativeElements => {
   const layoutName = noNativeElements ? 'flex' : 'table';
   storiesOf(`Table layout ${layoutName} - cell actions`, module)
@@ -773,10 +729,7 @@ const KeyboardColumnResizingStyle: React.FC<SharedVrTestArgs> = ({ noNativeEleme
       includeDarkMode: true,
       includeHighContrast: true,
       includeRtl: true,
-    })
-    .addStory('keyboard column resizing style', () => (
-      <KeyboardColumnResizingStyle noNativeElements={noNativeElements} />
-    ));
+    });
 
   storiesOf(`Table ${layoutName} - subtle selection`, module)
     .addDecorator(story => (
