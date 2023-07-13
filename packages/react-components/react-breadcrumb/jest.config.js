2af17c02dc62fea518c100d63ebb8fdfa3793944
Valentyna <vkozlova@microsoft.com>
2023-07-13 11:19:44 +0200

    more renames

--- packages/react-components/react-breadcrumb/jest.config.js
+++ packages/react-components/react-breadcrumb/jest.config.js
@@ -4,7 +4,7 @@
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-  displayName: 'react-breadcrumb',
+  displayName: 'react-breadcrumb-preview',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.tsx?$': [
