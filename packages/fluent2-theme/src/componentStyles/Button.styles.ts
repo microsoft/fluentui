6c966d5c81ef2cb06c867c28e11e0dcd6096f44b
Mitch-At-Work <mifraser@microsoft.com>
2023-07-17 09:33:26 -0700

    Normalize indent

--- packages/fluent2-theme/src/componentStyles/Button.styles.ts
+++ packages/fluent2-theme/src/componentStyles/Button.styles.ts
@@ -14,7 +14,7 @@ const getFocusStyle = (borderColor: string, borderRadius: string, outlineColor: 
   '.ms-Fabric--isFocusVisible &:focus::after': {
     border: `1px solid ${borderColor}`,
     borderRadius: '2px',
-    inset: '1px 0.5px 0.5px 0.5px',
+    inset: '1px 1px 1px 1px',
     outline: `2px solid ${outlineColor}`,
   },
 });
