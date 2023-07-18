915e1c35272f10603ebed1ee80721dee410c844c
BrdyBrn <brian.christopher.brady@gmail.com>
2023-07-18 09:44:18 -0700

    Adds Checkbox as a new web component (#27450)
    * radio init
    
    * styles radio
    
    * reverts branch
    
    * input spec init
    
    * cleans up spec
    
    * formatting
    
    * updates component name to text input
    
    * updates component name in spec
    
    * checkbox init
    
    * updats checkbox styles
    
    * checkbox: adds attributes to Checkbox
    
    * checkbox: updates checked-indicator svg and storybook templates
    
    * checkbox: updates styles
    
    * checkbox: adds exports
    
    * checkbox: organizes css
    
    * checkbox: updates styles
    
    * checkbox: adds README
    
    * checkbox: formats README
    
    * deletes dead file
    
    * checkbox: cleans up
    
    * checkbox: updates storybook content
    
    * checkbox: removes file
    
    * checkbox: yarn change
    
    * checkbox: updates README
    
    * checkbox: updates styles
    
    * checkbox: fixes styling bug
    
    * checkbox: optimze css
    
    * checkbox: updates storybook
    
    * checkbox: adds required story
    
    * checkbox: updates stories
    
    * Update change/@fluentui-web-components-eba99a5e-947a-4735-815b-86d613000d31.json
    
    Co-authored-by: Miroslav Stastny <mistastn@microsoft.com>
    
    * checkbox: updates README, adds focus styles
    
    * checkbox: api-report
    
    * reverts api report
    
    * checkbox: updates styles
    
    ---------
    
    Co-authored-by: Miroslav Stastny <mistastn@microsoft.com>

---
+++ packages/web-components/src/checkbox/checkbox.ts
@@ -0,0 +1,42 @@
+import { attr } from '@microsoft/fast-element';
+import { FASTCheckbox } from '@microsoft/fast-foundation';
+import { CheckboxLabelPosition, CheckboxShape, CheckboxSize } from './checkbox.options.js';
+
+/**
+ * The base class used for constucting a fluent checkbox custom element
+ * @public
+ */
+export class Checkbox extends FASTCheckbox {
+  /**
+   * Sets shape of the checkbox.
+   *
+   * @public
+   * @default 'square'
+   * @remarks
+   * HTML Attribute: shape
+   */
+  @attr
+  public shape?: CheckboxShape;
+
+  /**
+   * Sets size of the checkbox.
+   *
+   * @public
+   * @default 'medium'
+   * @remarks
+   * HTML Attribute: size
+   */
+  @attr
+  public size?: CheckboxSize;
+
+  /**
+   * Sets position of the label relative to the input
+   *
+   * @public
+   * @default 'after'
+   * @remarks
+   * HTML Attribute: label-position
+   */
+  @attr({ attribute: 'label-position' })
+  public labelPosition?: CheckboxLabelPosition;
+}
