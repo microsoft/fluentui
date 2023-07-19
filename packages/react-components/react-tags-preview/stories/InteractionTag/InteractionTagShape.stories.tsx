def3c87e738532330879d05bc0206b9a1b0cf766
Amber <yuanboxue@microsoft.com>
2023-07-19 11:59:27 +0200

    react-tags: fill icon on hover for outlined InteractionTag (#28558)
    * chg
    
    * use bundled icon

--- packages/react-components/react-tags-preview/stories/InteractionTag/InteractionTagShape.stories.tsx
+++ packages/react-components/react-tags-preview/stories/InteractionTag/InteractionTagShape.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Avatar, makeStyles } from '@fluentui/react-components';
-import { Calendar3Day20Regular } from '@fluentui/react-icons';
+import { CalendarMonthRegular } from '@fluentui/react-icons';
 
 import { InteractionTag } from '@fluentui/react-tags-preview';
 
@@ -22,10 +22,10 @@ export const Shape = () => {
         Circular
       </InteractionTag>
 
-      <InteractionTag dismissible icon={<Calendar3Day20Regular />} secondaryText="Secondary text">
+      <InteractionTag dismissible icon={<CalendarMonthRegular />} secondaryText="Secondary text">
         Rounded
       </InteractionTag>
-      <InteractionTag shape="circular" dismissible icon={<Calendar3Day20Regular />} secondaryText="Secondary text">
+      <InteractionTag shape="circular" dismissible icon={<CalendarMonthRegular />} secondaryText="Secondary text">
         Circular
       </InteractionTag>
     </div>
