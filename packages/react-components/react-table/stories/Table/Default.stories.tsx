4d2efffb43b9ea9e75aff5a056e7b9f1d4d56641
Lingfan Gao <lingfan.gao@microsoft.com>
2023-07-19 12:56:28 +0000

    feat: allSelectedRows and someSelectedRows should be more reliable
    We took the decision to decouple the selection state from the data. This
    means that the selection state does not always stay in sync with the
    data, but the resulting output should be correct.
    
    In this case `allSeletedRows` and `someSelectedRows` were calculated
    based on the size of the selected rows which was wrong, because there
    can be outdated values in there. Updates the `useTableSelection` hook
    to make sure that those states reflect the what is in the data. This
    will cause extra computation (which is memoized) but there is no other
    way to maintain consistency.
    
    It might be possible to update `useSelection` accept the set of
    selectable items, but the extra computation will still need to be there.
    
    Fixes #28456

--- packages/react-components/react-table/stories/Table/Default.stories.tsx
+++ packages/react-components/react-table/stories/Table/Default.stories.tsx
@@ -1,3 +1,20 @@
+import {
+  PresenceBadgeStatus,
+  Avatar,
+  TableBody,
+  TableCell,
+  TableRow,
+  Table,
+  TableHeader,
+  TableHeaderCell,
+  TableSelectionCell,
+  TableCellLayout,
+  useTableFeatures,
+  TableColumnDefinition,
+  useTableSelection,
+  createTableColumn,
+  Button,
+} from '@fluentui/react-components';
 import * as React from 'react';
 import {
   FolderRegular,
@@ -8,23 +25,39 @@ import {
   DocumentPdfRegular,
   VideoRegular,
 } from '@fluentui/react-icons';
-import {
-  TableBody,
-  TableCell,
-  TableRow,
-  Table,
-  TableHeader,
-  TableHeaderCell,
-  TableCellLayout,
-  PresenceBadgeStatus,
-  Avatar,
-} from '@fluentui/react-components';
 
-const items = [
+type FileCell = {
+  label: string;
+  icon: JSX.Element;
+};
+
+type LastUpdatedCell = {
+  label: string;
+  timestamp: number;
+};
+
+type LastUpdateCell = {
+  label: string;
+  icon: JSX.Element;
+};
+
+type AuthorCell = {
+  label: string;
+  status: PresenceBadgeStatus;
+};
+
+type Item = {
+  file: FileCell;
+  author: AuthorCell;
+  lastUpdated: LastUpdatedCell;
+  lastUpdate: LastUpdateCell;
+};
+
+let data: Item[] = [
   {
     file: { label: 'Meeting notes', icon: <DocumentRegular /> },
     author: { label: 'Max Mustermann', status: 'available' },
-    lastUpdated: { label: '7h ago', timestamp: 1 },
+    lastUpdated: { label: '7h ago', timestamp: 3 },
     lastUpdate: {
       label: 'You edited this',
       icon: <EditRegular />,
@@ -51,7 +84,7 @@ const items = [
   {
     file: { label: 'Purchase order', icon: <DocumentPdfRegular /> },
     author: { label: 'Jane Doe', status: 'offline' },
-    lastUpdated: { label: 'Tue at 9:30 AM', timestamp: 3 },
+    lastUpdated: { label: 'Tue at 9:30 AM', timestamp: 1 },
     lastUpdate: {
       label: 'You shared this in a Teams chat',
       icon: <PeopleRegular />,
@@ -59,49 +92,119 @@ const items = [
   },
 ];
 
-const columns = [
-  { columnKey: 'file', label: 'File' },
-  { columnKey: 'author', label: 'Author' },
-  { columnKey: 'lastUpdated', label: 'Last updated' },
-  { columnKey: 'lastUpdate', label: 'Last update' },
+const columns: TableColumnDefinition<Item>[] = [
+  createTableColumn<Item>({
+    columnId: 'file',
+  }),
+  createTableColumn<Item>({
+    columnId: 'author',
+  }),
+  createTableColumn<Item>({
+    columnId: 'lastUpdated',
+  }),
+  createTableColumn<Item>({
+    columnId: 'lastUpdate',
+  }),
 ];
 
 export const Default = () => {
+  const [items, setItems] = React.useState<Item[]>(data);
+
+  const {
+    getRows,
+    selection: { allRowsSelected, someRowsSelected, toggleAllRows, toggleRow, isRowSelected },
+  } = useTableFeatures(
+    {
+      columns,
+      items,
+      getRowId: item => item.file.label,
+    },
+    [
+      useTableSelection({
+        selectionMode: 'multiselect',
+        defaultSelectedItems: new Set([0, 1]),
+      }),
+    ],
+  );
+
+  let rows = getRows(row => {
+    const selected = isRowSelected(row.rowId);
+    return {
+      ...row,
+      onClick: (e: React.MouseEvent) => toggleRow(e, row.rowId),
+      onKeyDown: (e: React.KeyboardEvent) => {
+        if (e.key === ' ') {
+          e.preventDefault();
+          toggleRow(e, row.rowId);
+        }
+      },
+      selected,
+      appearance: selected ? ('brand' as const) : ('none' as const),
+    };
+  });
+
+  const toggleAllKeydown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+      if (e.key === ' ') {
+        toggleAllRows(e);
+        e.preventDefault();
+      }
+    },
+    [toggleAllRows],
+  );
+
+  const onDelete = () => {
+    data = data.filter((x, index) => index !== 0);
+    setItems(data);
+  };
+
   return (
-    <Table arial-label="Default table">
-      <TableHeader>
-        <TableRow>
-          {columns.map(column => (
-            <TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {items.map(item => (
-          <TableRow key={item.file.label}>
-            <TableCell>
-              <TableCellLayout media={item.file.icon}>{item.file.label}</TableCellLayout>
-            </TableCell>
-            <TableCell>
-              <TableCellLayout
-                media={
-                  <Avatar
-                    aria-label={item.author.label}
-                    name={item.author.label}
-                    badge={{ status: item.author.status as PresenceBadgeStatus }}
-                  />
-                }
-              >
-                {item.author.label}
-              </TableCellLayout>
-            </TableCell>
-            <TableCell>{item.lastUpdated.label}</TableCell>
-            <TableCell>
-              <TableCellLayout media={item.lastUpdate.icon}>{item.lastUpdate.label}</TableCellLayout>
-            </TableCell>
+    <>
+      <Button onClick={onDelete}>deleted</Button>
+      <Table aria-label="Table with multiselect">
+        <TableHeader>
+          <TableRow>
+            <TableSelectionCell
+              checked={allRowsSelected ? true : someRowsSelected ? 'mixed' : false}
+              onClick={toggleAllRows}
+              onKeyDown={toggleAllKeydown}
+              checkboxIndicator={{ 'aria-label': 'Select all rows ' }}
+            />
+
+            <TableHeaderCell>File</TableHeaderCell>
+            <TableHeaderCell>Author</TableHeaderCell>
+            <TableHeaderCell>Last updated</TableHeaderCell>
+            <TableHeaderCell>Last update</TableHeaderCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {rows.map(({ item, selected, onClick, onKeyDown, appearance }) => (
+            <TableRow
+              key={item.file.label}
+              onClick={onClick}
+              onKeyDown={onKeyDown}
+              aria-selected={selected}
+              appearance={appearance}
+            >
+              <TableSelectionCell checked={selected} checkboxIndicator={{ 'aria-label': 'Select row' }} />
+              <TableCell>
+                <TableCellLayout media={item.file.icon}>{item.file.label}</TableCellLayout>
+              </TableCell>
+              <TableCell>
+                <TableCellLayout
+                  media={<Avatar aria-label={item.author.label} badge={{ status: item.author.status }} />}
+                >
+                  {item.author.label}
+                </TableCellLayout>
+              </TableCell>
+              <TableCell>{item.lastUpdated.label}</TableCell>
+              <TableCell>
+                <TableCellLayout media={item.lastUpdate.icon}>{item.lastUpdate.label}</TableCellLayout>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 };
