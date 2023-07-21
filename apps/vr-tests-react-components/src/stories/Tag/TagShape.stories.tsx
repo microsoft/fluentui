7bd3501d9e52d7b44a6b631f22d9f8052f7b7785
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-07-21 14:23:07 +0200

    remove id from non-focusable-tag

--- apps/vr-tests-react-components/src/stories/Tag/TagShape.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Tag/TagShape.stories.tsx
@@ -18,18 +18,12 @@ export default {
   decorators: [story => withStoryWrightSteps({ story, steps })],
 } as ComponentMeta<typeof Tag>;
 
-export const Rounded = () => <Tag id={tagId}>Primary Text</Tag>;
+export const Rounded = () => <Tag>Primary Text</Tag>;
 
-export const RoundedWithSecondaryText = () => (
-  <Tag id={tagId} secondaryText="Secondary Text">
-    Primary Text
-  </Tag>
-);
+export const RoundedWithSecondaryText = () => <Tag secondaryText="Secondary Text">Primary Text</Tag>;
 
 export const RoundedWithMedia = () => (
-  <Tag id={tagId} media={<Avatar name="Lydia Bauer" badge={{ status: 'available' }} />}>
-    Primary Text
-  </Tag>
+  <Tag media={<Avatar name="Lydia Bauer" badge={{ status: 'available' }} />}>Primary Text</Tag>
 );
 export const RoundedWithMediaRTL = getStoryVariant(RoundedWithMedia, RTL);
 
@@ -40,20 +34,12 @@ export const RoundedDismissible = () => (
 );
 export const RoundedDismissibleRTL = getStoryVariant(RoundedDismissible, RTL);
 
-export const Circular = () => (
-  <Tag id={tagId} shape="circular">
-    Primary Text
-  </Tag>
-);
+export const Circular = () => <Tag shape="circular">Primary Text</Tag>;
 
-export const CircularWithSecondaryText = () => (
-  <Tag id={tagId} secondaryText="Secondary Text">
-    Primary Text
-  </Tag>
-);
+export const CircularWithSecondaryText = () => <Tag secondaryText="Secondary Text">Primary Text</Tag>;
 
 export const CircularWithMedia = () => (
-  <Tag id={tagId} shape="circular" media={<Avatar name="Lydia Bauer" badge={{ status: 'available' }} />}>
+  <Tag shape="circular" media={<Avatar name="Lydia Bauer" badge={{ status: 'available' }} />}>
     Primary Text
   </Tag>
 );
