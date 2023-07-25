9a1c9f38fc69142927b7af02f4c1e25929efe4bb
srmukher <srmukher@microsoft.com>
2023-07-25 14:47:37 +0530

    Fixing Sankey chart test failures

--- packages/react-charting/src/components/SankeyChart/SankeyChartRTL.test.tsx
+++ packages/react-charting/src/components/SankeyChart/SankeyChartRTL.test.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react';
 import { queryAllByAttribute, render, waitFor } from '@testing-library/react';
 import { data } from './SankeyChart.test';
 import { IChartProps, SankeyChart } from './index';
 import { resetIds } from '../../Utilities';
+import { SankeyChartBase } from './SankeyChart.base';
 
 const emptyChartPoints: IChartProps = {
   chartData: [],
@@ -15,6 +17,8 @@ function sharedBeforeEach() {
 describe('Sankey chart rendering', () => {
   beforeEach(sharedBeforeEach);
   test('Should re-render the Sankey chart with data', async () => {
+    jest.spyOn(SankeyChartBase.prototype as any, '_truncateText').mockImplementation(() => 'test');
+    jest.spyOn(SankeyChartBase.prototype as any, '_createNodes').mockImplementation(() => []);
     // Arrange
     const { container, rerender } = render(<SankeyChart data={emptyChartPoints} />);
     const getById = queryAllByAttribute.bind(null, 'id');
