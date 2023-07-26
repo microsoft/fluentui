2f5a77db15de52ca80907fc6bc5a56601ec234bf
Yush Singla <yushsingla@microsoft.com>
2023-07-26 13:23:53 +0530

    line chart vr test

---
+++ apps/vr-tests/src/stories/LineChart.stories.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Steps, StoryWright } from 'storywright';
+import { storiesOf } from '@storybook/react';
+import { TestWrapperDecorator } from '../utilities/index';
+import { LineChartBasicExample } from '@fluentui/react-examples/lib/react-charting/LineChart/LineChart.Basic.Example';
+
+storiesOf('LineChart', module)
+  .addDecorator(TestWrapperDecorator)
+  .addDecorator(story =>
+    // prettier-ignore
+    <StoryWright
+      steps={new Steps()
+        .snapshot('default', { cropTo: '.testWrapper' })
+        .end()}
+    >
+      {story()}
+    </StoryWright>,
+  )
+  .addStory('Root', () => {
+    return <LineChartBasicExample />;
+  });
