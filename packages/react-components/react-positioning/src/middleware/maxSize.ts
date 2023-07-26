7f86bb816831bb9d0356b574c46d51d682428a78
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-07-26 12:10:33 +0200

    try to make it better

--- packages/react-components/react-positioning/src/middleware/maxSize.ts
+++ packages/react-components/react-positioning/src/middleware/maxSize.ts
@@ -29,31 +29,28 @@ const CSS_VAR_OVERFLOW_Y = '--maxsize-overflow-y';
 
 export const resetMaxSizeStyles = (floatingElementStyle: CSSStyleDeclaration) => {
   if (floatingElementStyle.boxSizing === `var(${CSS_VAR_BOX_SIZING})`) {
-    floatingElementStyle.removeProperty(CSS_VAR_BOX_SIZING);
     floatingElementStyle.removeProperty('box-sizing');
-  }
 
-  if (floatingElementStyle.maxWidth === `var(${CSS_VAR_AVAILABLE_WIDTH})`) {
-    floatingElementStyle.removeProperty('max-width');
-    floatingElementStyle.removeProperty(CSS_VAR_AVAILABLE_WIDTH);
+    if (floatingElementStyle.maxWidth === `var(${CSS_VAR_AVAILABLE_WIDTH})`) {
+      floatingElementStyle.removeProperty('max-width');
+    }
     if (floatingElementStyle.width === `var(${CSS_VAR_AVAILABLE_WIDTH})`) {
       floatingElementStyle.removeProperty('width');
     }
-  }
 
-  if (floatingElementStyle.maxHeight === `var(${CSS_VAR_AVAILABLE_HEIGHT})`) {
-    floatingElementStyle.removeProperty('max-height');
-    floatingElementStyle.removeProperty(CSS_VAR_AVAILABLE_HEIGHT);
+    if (floatingElementStyle.maxHeight === `var(${CSS_VAR_AVAILABLE_HEIGHT})`) {
+      floatingElementStyle.removeProperty('max-height');
+    }
     if (floatingElementStyle.height === `var(${CSS_VAR_AVAILABLE_HEIGHT})`) {
       floatingElementStyle.removeProperty('height');
     }
-  }
 
-  if (floatingElementStyle.overflowX === `var(${CSS_VAR_OVERFLOW_X})`) {
-    floatingElementStyle.removeProperty('overflow-x');
-  }
-  if (floatingElementStyle.overflowY === `var(${CSS_VAR_OVERFLOW_Y})`) {
-    floatingElementStyle.removeProperty('overflow-y');
+    if (floatingElementStyle.overflowX === `var(${CSS_VAR_OVERFLOW_X})`) {
+      floatingElementStyle.removeProperty('overflow-x');
+    }
+    if (floatingElementStyle.overflowY === `var(${CSS_VAR_OVERFLOW_Y})`) {
+      floatingElementStyle.removeProperty('overflow-y');
+    }
   }
 };
 
@@ -79,8 +76,10 @@ export function maxSize(autoSize: PositioningOptions['autoSize'], options: MaxSi
         elements.floating.style.setProperty('max-width', `var(${CSS_VAR_AVAILABLE_WIDTH})`);
         if (widthOverflow) {
           elements.floating.style.setProperty('width', `var(${CSS_VAR_AVAILABLE_WIDTH})`);
-          elements.floating.style.setProperty(CSS_VAR_OVERFLOW_X, 'auto');
-          elements.floating.style.setProperty('overflow-x', `var(${CSS_VAR_OVERFLOW_X})`);
+          if (!elements.floating.style.overflowX) {
+            elements.floating.style.setProperty(CSS_VAR_OVERFLOW_X, 'auto');
+            elements.floating.style.setProperty('overflow-x', `var(${CSS_VAR_OVERFLOW_X})`);
+          }
         }
       }
 
@@ -89,8 +88,10 @@ export function maxSize(autoSize: PositioningOptions['autoSize'], options: MaxSi
         elements.floating.style.setProperty('max-height', `var(${CSS_VAR_AVAILABLE_HEIGHT})`);
         if (heightOverflow) {
           elements.floating.style.setProperty('height', `var(${CSS_VAR_AVAILABLE_HEIGHT})`);
-          elements.floating.style.setProperty(CSS_VAR_OVERFLOW_Y, 'auto');
-          elements.floating.style.setProperty('overflow-y', `var(${CSS_VAR_OVERFLOW_Y})`);
+          if (!elements.floating.style.overflowY) {
+            elements.floating.style.setProperty(CSS_VAR_OVERFLOW_Y, 'auto');
+            elements.floating.style.setProperty('overflow-y', `var(${CSS_VAR_OVERFLOW_Y})`);
+          }
         }
       }
     },
