53594633ed73bbc100c0bba43f4f301df9d67f47
Yush Singla <yushsingla@microsoft.com>
2023-07-27 14:55:14 +0530

    testing vr-test for nested folder

--- apps/vr-tests/src/stories/react-charting/LineChart.stories.tsx
+++ apps/vr-tests/src/stories/react-charting/LineChart.stories.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Steps, StoryWright } from 'storywright';
 import { storiesOf } from '@storybook/react';
-import { TestWrapperDecorator } from '../utilities/index';
+import { TestWrapperDecorator } from '../../utilities/index';
 import { LineChartBasicExample } from '@fluentui/react-examples/lib/react-charting/LineChart/LineChart.Basic.Example';
+import { LineChartEventsExample } from '@fluentui/react-examples/src/react-charting/LineChart/LineChart.Events.Example';
 
 storiesOf('LineChart', module)
   .addDecorator(TestWrapperDecorator)
@@ -18,4 +19,7 @@ storiesOf('LineChart', module)
   )
   .addStory('Root', () => {
     return <LineChartBasicExample />;
+  })
+  .addStory('Events', () => {
+    return <LineChartEventsExample />;
   });
