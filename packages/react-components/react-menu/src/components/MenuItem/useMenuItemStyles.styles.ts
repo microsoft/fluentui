d2d69704c4e2828a4a958b91374846522b08b873
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-07-31 15:17:38 +0200

    disable browser focus style

--- packages/react-components/react-menu/src/components/MenuItem/useMenuItemStyles.styles.ts
+++ packages/react-components/react-menu/src/components/MenuItem/useMenuItemStyles.styles.ts
@@ -1,6 +1,6 @@
 import { mergeClasses, makeStyles, shorthands } from '@griffel/react';
 import { iconFilledClassName, iconRegularClassName } from '@fluentui/react-icons';
-import { createFocusOutlineStyle } from '@fluentui/react-tabster';
+import { createCustomFocusIndicatorStyle } from '@fluentui/react-tabster';
 import { tokens } from '@fluentui/react-theme';
 import { useCheckmarkStyles_unstable } from '../../selectable/index';
 import type { MenuItemCheckboxState } from '../MenuItemCheckbox/index';
@@ -17,11 +17,18 @@ export const menuItemClassNames: SlotClassNames<MenuItemSlots> = {
 };
 
 const useStyles = makeStyles({
-  // focusIndicator: createCustomFocusIndicatorStyle({
-  //   ...shorthands.borderRadius(tokens.borderRadiusMedium),
-  //   ...shorthands.outline(tokens.strokeWidthThick, 'solid', tokens.colorStrokeFocus2),
-  // }),
-  focusIndicator: createFocusOutlineStyle(),
+  focusIndicator: {
+    ':focus': {
+      outlineStyle: 'none',
+    },
+    ':focus-visible': {
+      outlineStyle: 'none',
+    },
+    ...createCustomFocusIndicatorStyle({
+      ...shorthands.borderRadius(tokens.borderRadiusMedium),
+      ...shorthands.outline(tokens.strokeWidthThick, 'solid', tokens.colorStrokeFocus2),
+    }),
+  },
   // TODO: this should be extracted to another package
   root: {
     ...shorthands.borderRadius(tokens.borderRadiusMedium),
