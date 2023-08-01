7807a270b27ff923b6c8260d9e2074929cce0162
yush singla <yuhshsingla@microsoft.com>
2023-08-01 19:23:10 +0530

    vr-tests fix

--- apps/vr-tests/src/stories/react-charting/LineChart.stories.tsx
+++ apps/vr-tests/src/stories/react-charting/LineChart.stories.tsx
@@ -2,29 +2,22 @@ import * as React from 'react';
 import { Steps, StoryWright } from 'storywright';
 import { storiesOf } from '@storybook/react';
 import { TestWrapperDecorator } from '../../utilities/index';
-import { IChartProps } from '../../../../../packages/react-charting/src/HorizontalBarChart';
-import { ILineChartPoints, LineChart } from '../../../../../packages/react-charting/src/LineChart';
-import { DefaultPalette } from '../../../../../packages/theme/src/index';
-import { mergeStyles } from '../../../../../packages/merge-styles/src/mergeStyles';
-import {
-  DataVizPalette,
-  ICustomizedCalloutData,
-} from '../../../../../packages/react-charting/src/index';
+import { ILineChartPoints, LineChart, IChartProps } from '@fluentui/react-charting';
+import { mergeStyles, DefaultPalette } from '@fluentui/react';
+import { DataVizPalette, ICustomizedCalloutData } from '@fluentui/react-charting';
 
 storiesOf('react-charting/LineChart', module)
   .addDecorator(TestWrapperDecorator)
-  .addDecorator(story =>
-    // prettier-ignore
-    <StoryWright
-      steps={new Steps()
-        .snapshot('default', { cropTo: '.testWrapper' })
-        .end()}
-    >
-      {story()}
-    </StoryWright>,
-  )
+  .addDecorator(story => {
+    const steps =
+      story.name === 'Basic'
+        ? new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()
+        : new Steps().snapshot('default', { cropTo: '.testWrapper' }).end();
+
+    return <StoryWright steps={steps}>{story()}</StoryWright>;
+  })
   .addStory(
-    'Root',
+    'Basic',
     () => {
       const margins = { left: 35, top: 20, bottom: 35, right: 20 };
       const data: IChartProps = {
@@ -826,141 +819,6 @@ storiesOf('react-charting/LineChart', module)
             />
           </div>
         </>
-      );
-    },
-    { includeRtl: true, includeDarkMode: true },
-  )
-  .addStory(
-    'Date_Axis',
-    () => {
-      const data: IChartProps = {
-        chartTitle: 'Line Chart',
-        lineChartData: [
-          {
-            legend: 'From_Legacy_to_O365',
-            data: [
-              {
-                x: new Date('2020-03-03T00:00:00.000Z'),
-                y: 216000,
-                onDataPointClick: () => alert('click on 217000'),
-              },
-              {
-                x: new Date('2020-04-03T10:00:00.000Z'),
-                y: 218123,
-                onDataPointClick: () => alert('click on 217123'),
-              },
-              {
-                x: new Date('2020-05-05T11:00:00.000Z'),
-                y: 217124,
-                onDataPointClick: () => alert('click on 217124'),
-              },
-              {
-                x: new Date('2020-07-14T00:00:00.000Z'),
-                y: 248000,
-                onDataPointClick: () => alert('click on 248000'),
-              },
-              {
-                x: new Date('2020-11-15T00:00:00.000Z'),
-                y: 252000,
-                onDataPointClick: () => alert('click on 252000'),
-              },
-              {
-                x: new Date('2020-12-06T00:00:00.000Z'),
-                y: 274000,
-                onDataPointClick: () => alert('click on 274000'),
-              },
-              {
-                x: new Date('2021-01-07T00:00:00.000Z'),
-                y: 260000,
-                onDataPointClick: () => alert('click on 260000'),
-              },
-              {
-                x: new Date('2021-02-14T00:00:00.000Z'),
-                y: 304000,
-                onDataPointClick: () => alert('click on 300000'),
-              },
-              {
-                x: new Date('2021-03-09T00:00:00.000Z'),
-                y: 218000,
-                onDataPointClick: () => alert('click on 218000'),
-              },
-            ],
-            color: DefaultPalette.blue,
-            lineOptions: {
-              lineBorderWidth: '4',
-            },
-            onLineClick: () => console.log('From_Legacy_to_O365'),
-          },
-          {
-            legend: 'All',
-            data: [
-              {
-                x: new Date('2020-03-03T00:00:00.000Z'),
-                y: 297000,
-              },
-              {
-                x: new Date('2020-04-04T00:00:00.000Z'),
-                y: 284000,
-              },
-              {
-                x: new Date('2020-05-05T00:00:00.000Z'),
-                y: 282000,
-              },
-              {
-                x: new Date('2020-06-06T00:00:00.000Z'),
-                y: 294000,
-              },
-              {
-                x: new Date('2020-09-16T00:00:00.000Z'),
-                y: 224000,
-              },
-              {
-                x: new Date('2021-02-08T00:00:00.000Z'),
-                y: 300000,
-              },
-              {
-                x: new Date('2021-03-09T00:00:00.000Z'),
-                y: 298000,
-              },
-            ],
-            color: DefaultPalette.green,
-            lineOptions: {
-              lineBorderWidth: '4',
-            },
-          },
-          {
-            legend: 'single point',
-            data: [
-              {
-                x: new Date('2020-07-05T00:00:00.000Z'),
-                y: 282000,
-              },
-            ],
-            color: DefaultPalette.yellow,
-          },
-        ],
-      };
-
-      const rootStyle = { width: `${700}px`, height: `${300}px` };
-      const margins = { left: 35, top: 20, bottom: 35, right: 20 };
-
-      return (
-        <div style={rootStyle}>
-          <LineChart
-            culture={'rs-ss'}
-            data={data}
-            legendsOverflowText={'Overflow Items'}
-            yMinValue={200}
-            yMaxValue={301}
-            height={300}
-            width={700}
-            margins={margins}
-            xAxisTickCount={10}
-            allowMultipleShapesForPoints={false}
-            rotateXAxisLables={true}
-            enablePerfOptimization={true}
-          />
-        </div>
       );
     },
     { includeRtl: true, includeDarkMode: true },
