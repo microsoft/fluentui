dcbe49a29a349915c7fb4be177c905ff5ade1928
Lingfan Gao <lingfangao@hotmail.com>
2023-08-01 19:18:57 +0200

    revert breaking change

--- packages/react-components/react-toast/src/components/Toaster/Toaster.types.ts
+++ packages/react-components/react-toast/src/components/Toaster/Toaster.types.ts
@@ -32,7 +32,8 @@ export type ToasterProps = Omit<ComponentProps<ToasterSlots>, 'children'> &
  * State used in rendering Toaster
  */
 export type ToasterState = ComponentState<ToasterSlotsInternal> &
-  Pick<AriaLiveProps, 'announceRef'> & {
+  Pick<AriaLiveProps, 'announceRef'> &
+  Pick<Required<ToasterProps>, 'announce'> & {
     offset: ToasterOptions['offset'] | undefined;
     renderAriaLive: boolean;
     dir: 'rtl' | 'ltr';
