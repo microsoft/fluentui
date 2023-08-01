889af91146beaa27609a504931a1fa47d283395c
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-08-01 10:55:46 +0200

    type test

--- packages/react-components/react-positioning/src/middleware/maxSize.test.ts
+++ packages/react-components/react-positioning/src/middleware/maxSize.test.ts
@@ -1,8 +1,8 @@
 import { Platform, computePosition } from '@floating-ui/dom';
 import { resetMaxSize } from './maxSize';
 
-const referenceRect = { x: 0, y: 0, width: 100, height: 100 };
-const floatingRect = { x: 0, y: 0, width: 50, height: 50 };
+const referenceRect: Partial<DOMRect> = { x: 0, y: 0, width: 100, height: 100 };
+const floatingRect: Partial<DOMRect> = { x: 0, y: 0, width: 50, height: 50 };
 const mockGetElementRects = jest.fn(() =>
   Promise.resolve({
     reference: referenceRect,
@@ -11,10 +11,11 @@ const mockGetElementRects = jest.fn(() =>
 );
 const platform = {
   getElementRects: mockGetElementRects,
-} as unknown as Platform;
+} as Partial<Platform> as Platform;
 
 describe('maxSize', () => {
   it('resetMaxSize reset once per life cycle', async () => {
+    expect.hasAssertions();
     const button = document.createElement('div');
     const tooltip = document.createElement('div');
     const autoSize = { applyMaxHeight: true, applyMaxWidth: true };
