192f8383d9ea548022745d2e89c6fc4837abb2c5
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-08-02 12:28:53 +0200

    Update packages/react-components/react-tree/bundle-size/PersonaTree.fixture.js
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-components/react-tree/bundle-size/PersonaTree.fixture.js
+++ packages/react-components/react-tree/bundle-size/PersonaTree.fixture.js
@@ -3,5 +3,5 @@ import { Tree, TreeItem, TreeItemPersonaLayout } from '@fluentui/react-tree';
 console.log(Tree, TreeItem, TreeItemPersonaLayout);
 
 export default {
-  name: 'Tree',
+  name: 'PersonaTree',
 };
