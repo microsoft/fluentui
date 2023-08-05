4f79a23b5f661ba9e27fcd15904c3efeaae7b1ab
Miroslav Stastny <miroslav.stastny@microsoft.com>
2023-08-04 22:03:42 -0700

    update the vr story to match component story

--- apps/vr-tests-web-components/src/stories/accordion/accordion.stories.tsx
+++ apps/vr-tests-web-components/src/stories/accordion/accordion.stories.tsx
@@ -22,20 +22,20 @@ export default {
   ],
 };
 
-const add20Filled = `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style="fill: currentColor;">
+const add20Filled = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path
     d="M10.5 2.75C10.5 2.33579 10.1642 2 9.75 2C9.33579 2 9 2.33579 9 2.75V9H2.75C2.33579 9 2 9.33579 2 9.75C2 10.1642 2.33579 10.5 2.75 10.5H9V16.75C9 17.1642 9.33579 17.5 9.75 17.5C10.1642 17.5 10.5 17.1642 10.5 16.75V10.5H16.75C17.1642 10.5 17.5 10.1642 17.5 9.75C17.5 9.33579 17.1642 9 16.75 9H10.5V2.75Z"
+    fill="#212121"
   />
 </svg>`;
-
 const subtract20Filled = `<svg
   width="20"
   height="20"
   viewBox="0 0 20 20"
+  fill="none"
   xmlns="http://www.w3.org/2000/svg"
-  style="fill: currentColor;"
 >
-  <rect x="3" y="9.25" width="14" height="1.5" rx="0.75" />
+  <rect x="3" y="9.25" width="14" height="1.5" rx="0.75" fill="#212121" />
 </svg>`;
 
 export const Size = () =>
