80ef0097a345bd2f77203922b4df1145ffc4cd4f
Geoff Cox (Microsoft) <gcox@microsoft.com>
2023-08-08 14:22:48 -0700

    Update packages/react-components/react-datepicker-compat/src/components/DatePicker/DatePicker.types.ts
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-datepicker-compat/src/components/DatePicker/DatePicker.types.ts
+++ packages/react-components/react-datepicker-compat/src/components/DatePicker/DatePicker.types.ts
@@ -3,7 +3,7 @@ import { Input } from '@fluentui/react-input';
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
 import type { CalendarProps } from '../Calendar/Calendar.types';
 import type { CalendarStrings, DateFormatting } from '../../utils';
-import type { PortalProps } from '@fluentui/react-portal';
+import type { PortalProps, PortalState } from '@fluentui/react-portal';
 import type { PositioningProps } from '@fluentui/react-positioning';
 
 export type DatePickerSlots = {
