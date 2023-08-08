405adc0f15862c74784dd7576ca6d22b47adf012
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-08-08 17:04:03 +0200

    bugfix: Breaking Change, onHeaderClick erroneously removed (#28764)

--- packages/react-components/react-accordion/src/components/AccordionHeader/useAccordionHeader.test.tsx
+++ packages/react-components/react-accordion/src/components/AccordionHeader/useAccordionHeader.test.tsx
@@ -4,28 +4,22 @@ import * as React from 'react';
 import { useAccordionHeader_unstable } from './useAccordionHeader';
 import { AccordionProvider } from '../../contexts/accordion';
 import { AccordionItemProvider } from '../../contexts/accordionItem';
+import { mockAccordionContextValue, mockAccordionItemContextValue } from '../../testing/mockContextValue';
 
 describe('useAccordionHeader_unstable', () => {
   it('should return button props as disabled even when it is not disabled (forceDisabled)', () => {
     const ref = React.createRef<HTMLElement>();
     const wrapper: React.FC = ({ children }) => (
       <AccordionProvider
-        value={{
-          collapsible: false,
-          multiple: false,
-          navigation: undefined,
+        value={mockAccordionContextValue({
           openItems: [1],
-          requestToggle: () => {
-            /* ... */
-          },
-        }}
+        })}
       >
         <AccordionItemProvider
-          value={{
-            disabled: false,
+          value={mockAccordionItemContextValue({
             open: true,
             value: 1,
-          }}
+          })}
         >
           {children}
         </AccordionItemProvider>
