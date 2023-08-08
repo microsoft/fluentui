79d6ed6bd2ef22ba1761a911a475f7df4c92b15e
Jacqueline Gaherity <30805892+Jacqueline-ms@users.noreply.github.com>
2023-08-08 11:22:07 -0700

    Contrast ratio a11y bug fix for detailslist

--- packages/azure-themes/src/azure/AzureColors.ts
+++ packages/azure-themes/src/azure/AzureColors.ts
@@ -796,7 +796,7 @@ export const LightSemanticColors: IAzureSemanticColors = {
     background: BaseColors.WHITE,
     disabled: BaseColors.GRAY_C8C6C4,
     hover: BaseColors.GRAY_605E5C,
-    accent: BaseColors.BLUE_0078D4,
+    accent: BaseColors.GRAY_605E5C,
     focus: BaseColors.BLUE_0078D4,
     error: BaseColors.RED_A4262C,
     dirty: BaseColors.PURPLE_8A2DA5,
