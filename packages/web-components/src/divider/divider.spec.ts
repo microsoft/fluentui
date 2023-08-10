2588c519e254f0e816d101b5f636d608b1faa6f5
Hale Rankin <harankin@microsoft.com>
2023-08-10 11:44:01 -0700

    Updates test.

--- packages/web-components/src/divider/divider.spec.ts
+++ packages/web-components/src/divider/divider.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 import type { Locator, Page } from '@playwright/test';
 import { fixtureURL } from '../helpers.tests.js';
 import type { Divider } from './divider.js';
-import { DividerOrientation, DividerRole } from './divider.options.js';
+import { lightModeStylesheetBehavior } from '@microsoft/fast-foundation';
 
 test.describe('Divider', () => {
   let page: Page;
@@ -30,70 +30,111 @@ test.describe('Divider', () => {
             `;
     });
 
-    await expect(element).toHaveAttribute('role', DividerRole.separator);
+    await expect(element).toHaveAttribute('role', 'separator');
   });
 
-  // test("should set the `role` attribute equal to the role provided", async () => {
-  //     await root.evaluate(node => {
-  //         node.innerHTML = /* html */ `
-  //             <fluent-divider role="presentation"></fluent-divider>
-  //         `;
-  //     });
+  test('should set the `role` attribute equal to the role provided', async () => {
+    await root.evaluate(node => {
+      node.innerHTML = /* html */ `
+                <fluent-divider role="presentation"></fluent-divider>
+            `;
+    });
 
-  //     await expect(element).toHaveAttribute("role", "presentation");
+    await expect(element).toHaveAttribute('role', 'presentation');
 
-  //     await element.evaluate((node: Divider, DividerRole) => {
-  //         node.role = DividerRole.separator;
-  //     }, DividerRole);
+    await element.evaluate((node: Divider) => {
+      node.role = 'separator';
+    });
 
-  //     await expect(element).toHaveAttribute("role", DividerRole.separator);
-  // });
+    await expect(element).toHaveAttribute('role', 'separator');
+  });
 
-  // test("should set the `aria-orientation` attribute equal to the `orientation` value", async () => {
-  //     await root.evaluate(node => {
-  //         node.innerHTML = /* html */ `
-  //             <fluent-divider orientation="vertical"></fluent-divider>
-  //         `;
-  //     });
+  test('should set the `aria-orientation` attribute equal to the `orientation` value', async () => {
+    await root.evaluate(node => {
+      node.innerHTML = /* html */ `
+                <fluent-divider orientation="vertical"></fluent-divider>
+            `;
+    });
 
-  //     await expect(element).toHaveAttribute(
-  //         "aria-orientation",
-  //         DividerOrientation.vertical
-  //     );
+    await expect(element).toHaveJSProperty('ariaOrientation', 'vertical');
 
-  //     await element.evaluate((node: Divider, DividerOrientation) => {
-  //         node.orientation = DividerOrientation.horizontal;
-  //     }, DividerOrientation);
+    await element.evaluate((node: Divider) => {
+      node.orientation = 'horizontal';
+    });
 
-  //     await expect(element).toHaveAttribute(
-  //         "aria-orientation",
-  //         DividerOrientation.horizontal
-  //     );
-  // });
+    await expect(element).toHaveJSProperty('ariaOrientation', 'horizontal');
+  });
 
-  // test("should NOT set the `aria-orientation` attribute equal to the `orientation` value if the `role` is presentational", async () => {
-  //     await root.evaluate(node => {
-  //         node.innerHTML = /* html */ `
-  //             <fluent-divider orientation="vertical"></fluent-divider>
-  //         `;
-  //     });
+  // These test fail for Fluent Divider because aria-orientation is set regardless of role. Something is broken.
+  test('should NOT set the `aria-orientation` attribute equal to the `orientation` value if the `role` is presentational', async () => {
+    await root.evaluate(node => {
+      node.innerHTML = /* html */ `
+                <fluent-divider orientation="vertical"></fluent-divider>
+            `;
+    });
 
-  //     await expect(element).toHaveAttribute(
-  //         "aria-orientation",
-  //         DividerOrientation.vertical
-  //     );
+    await expect(element).toHaveAttribute('aria-orientation', 'vertical');
 
-  //     await element.evaluate((node: Divider, DividerRole) => {
-  //         node.role = DividerRole.presentation;
-  //     }, DividerRole);
+    await element.evaluate((node: Divider) => {
+      node.role = 'presentation';
+    });
 
-  //     await expect(element).not.toHaveAttribute(
-  //         "aria-orientation",
-  //         DividerOrientation.horizontal
-  //     );
-  //     await expect(element).not.toHaveAttribute(
-  //         "aria-orientation",
-  //         DividerOrientation.vertical
-  //     );
-  // });
+    await expect(element).not.toHaveAttribute('aria-orientation', 'horizontal');
+    await expect(element).not.toHaveAttribute('aria-orientation', 'vertical');
+  });
+
+  test('should initialize to the provided value attribute if set post-connection', async () => {
+    await root.evaluate(node => {
+      node.innerHTML = /* html */ `
+              <fluent-divider></fluent-divider>
+          `;
+    });
+
+    await element.evaluate((node: Divider) => {
+      node.alignContent = 'start';
+    });
+
+    await expect(element).toHaveJSProperty('alignContent', 'start');
+
+    await element.evaluate((node: Divider) => {
+      node.alignContent = 'center';
+    });
+
+    await expect(element).toHaveJSProperty('alignContent', 'center');
+
+    await element.evaluate((node: Divider) => {
+      node.alignContent = 'end';
+    });
+
+    await expect(element).toHaveJSProperty('alignContent', 'end');
+
+    await element.evaluate((node: Divider) => {
+      node.appearance = 'default';
+    });
+
+    await expect(element).toHaveJSProperty('appearance', 'default');
+
+    await element.evaluate((node: Divider) => {
+      node.appearance = 'strong';
+    });
+
+    await expect(element).toHaveJSProperty('appearance', 'strong');
+
+    await element.evaluate((node: Divider) => {
+      node.appearance = 'brand';
+    });
+
+    await expect(element).toHaveJSProperty('appearance', 'brand');
+    await element.evaluate((node: Divider) => {
+      node.appearance = 'subtle';
+    });
+
+    await expect(element).toHaveJSProperty('appearance', 'subtle');
+
+    await element.evaluate((node: Divider) => {
+      node.inset = true;
+    });
+
+    await expect(element).toHaveJSProperty('inset', true);
+  });
 });
