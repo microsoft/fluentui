92701764f0afe9547ff4f6df27afa04fee61b888
brianchristopherbrady <brian.christopher.brady@gmail.com>
2023-08-14 15:52:54 -0700

    menu: removes block styling

--- packages/web-components/src/menu/menu.styles.ts
+++ packages/web-components/src/menu/menu.styles.ts
@@ -1,12 +1,10 @@
 import { css } from '@microsoft/fast-element';
-import { display } from '@microsoft/fast-foundation';
 import {} from '../theme/design-tokens.js';
 
 /** Menu styles
  * @public
  */
 export const styles = css`
-  ${display('block')}
   :host {
     position: relative;
   }
