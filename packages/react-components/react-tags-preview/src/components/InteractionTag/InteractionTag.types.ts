09266bc14f11f28175e0c409ac7fbaf601b2c0ba
Amber <yuanboxue@microsoft.com>
2023-08-15 10:35:28 +0200

    BREAKING(react-tags-preview): use JSX children instead of slots for InteractionTag's buttons (#28739)
    * fix
    
    * chg
    
    * wip
    
    * story upd
    
    * story upd
    
    * story
    
    * vr
    
    * test
    
    * complete storybook
    
    * story
    
    * polish
    
    * chg
    
    * retry rename file to lowercase
    
    * api
    
    * UT
    
    * fix crud in vr
    
    * fix crud in vr
    
    * migrate to new slot api
    
    * small cleanup
    
    * small cleanup
    
    * small cleanup
    
    * Update packages/react-components/react-tags-preview/src/components/Secondary/Secondary.types.ts
    
    Co-authored-by: Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
    
    * avatar context -> tag avatar context
    
    * api
    
    * add hasSecondaryAction prop
    
    * export context hooks
    
    * rename Primary to InteractionTagPrimary
    
    * rename Secondary to InteractionTagSecondary
    
    * rename cleanup
    
    * update vr test after rename
    
    * todo
    
    * cleanup after rename
    
    * api
    
    ---------
    
    Co-authored-by: Bernardo Sunderhus <bernardo.sunderhus@gmail.com>

--- packages/react-components/react-tags-preview/src/components/InteractionTag/InteractionTag.types.ts
+++ packages/react-components/react-tags-preview/src/components/InteractionTag/InteractionTag.types.ts
@@ -1,21 +1,40 @@
+import * as React from 'react';
 import { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
-import { TagContextValues, TagProps, TagSlots, TagState } from '../Tag/index';
+import { InteractionTagContextValue } from '../../contexts/interactionTagContext';
+import { TagAppearance, TagShape, TagSize } from '../../utils/types';
 
-export type InteractionTagContextValues = TagContextValues;
+export type InteractionTagContextValues = {
+  interactionTag: InteractionTagContextValue;
+};
 
-export type InteractionTagSlots = Omit<TagSlots, 'root' | 'dismissIcon'> & {
+export type InteractionTagSlots = {
   root: NonNullable<Slot<'div'>>;
-  dismissButton?: Slot<'button'>;
-  content: NonNullable<Slot<'button'>>;
 };
 
 /**
  * InteractionTag Props
  */
-export type InteractionTagProps = ComponentProps<Partial<InteractionTagSlots>> & Omit<TagProps, 'root' | 'dismissIcon'>;
+export type InteractionTagProps<Value = string> = ComponentProps<Partial<InteractionTagSlots>> & {
+  // TODO add comments with default value
+
+  appearance?: TagAppearance;
+  disabled?: boolean;
+  shape?: TagShape;
+  size?: TagSize;
+
+  /**
+   * Unique value identifying the tag within a TagGroup
+   */
+  value?: Value;
+};
 
 /**
  * State used in rendering InteractionTag
  */
-export type InteractionTagState = ComponentState<InteractionTagSlots> &
-  Omit<TagState, 'components' | 'root' | 'dismissIcon'>;
+export type InteractionTagState<Value = string> = ComponentState<InteractionTagSlots> &
+  Required<Pick<InteractionTagProps, 'appearance' | 'disabled' | 'shape' | 'size' | 'value'>> & {
+    /**
+     * Event handler from TagGroup context that allows TagGroup to dismiss the tag
+     */
+    handleTagDismiss: (e: React.MouseEvent | React.KeyboardEvent, value: Value) => void;
+  };
