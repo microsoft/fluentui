7287927b377501dbb3ae877d9f1742a5aba4af9f
Makoto Morimoto <humbertomakotomorimoto@gmail.com>
2023-08-17 13:26:23 -0700

    docs: Updating version of CDN used and replacing usage of via.placeholder.com with SVG from CDN (#28895)
    * docs: Updating version of CDN used and replacing usage of via.placeholder.com with SVG from CDN.
    
    * Adding change files.
    
    * Updating snapshots.

--- packages/react-examples/src/react/Shimmer/Shimmer.LoadData.Example.tsx
+++ packages/react-examples/src/react/Shimmer/Shimmer.LoadData.Example.tsx
@@ -54,7 +54,7 @@ export const ShimmerLoadDataExample: React.FunctionComponent = () => {
       !isDataLoadedTwo
         ? {
             imageUrl:
-              'https://res-1.cdn.office.net/files/fabric-cdn-prod_20221209.001/office-ui-fabric-react-assets/persona-female.png',
+              'https://res-1.cdn.office.net/files/fabric-cdn-prod_20230815.002/office-ui-fabric-react-assets/persona-female.png',
             imageInitials: 'AL',
             primaryText: 'Annie Lindqvist',
             secondaryText: 'Software Engineer',
