612de8dc3530315729b59805ebcbe42f296256d6
Marcos Moura <marcosvmmoura@gmail.com>
2023-08-24 02:30:27 +0200

    fix: remove duplicated styles
    (cherry picked from commit 8db133d683000a7ac407107130a0ff735516439f)

--- packages/react-components/react-drawer/src/components/DrawerInline/useDrawerInlineStyles.styles.ts
+++ packages/react-components/react-drawer/src/components/DrawerInline/useDrawerInlineStyles.styles.ts
@@ -4,7 +4,7 @@ import type { SlotClassNames } from '@fluentui/react-utilities';
 import { tokens } from '@fluentui/react-theme';
 
 import type { DrawerInlineSlots, DrawerInlineState } from './DrawerInline.types';
-import { drawerCSSVars, useDrawerBaseClassNames, useDrawerDurationStyles } from '../../util/useDrawerBaseStyles.styles';
+import { drawerCSSVars, useDrawerBaseClassNames } from '../../util/useDrawerBaseStyles.styles';
 
 export const drawerInlineClassNames: SlotClassNames<DrawerInlineSlots> = {
   root: 'fui-DrawerInline',
@@ -51,7 +51,6 @@ const useDrawerRootStyles = makeStyles({
 export const useDrawerInlineStyles_unstable = (state: DrawerInlineState): DrawerInlineState => {
   const baseClassNames = useDrawerBaseClassNames(state);
   const rootStyles = useDrawerRootStyles();
-  const durationStyles = useDrawerDurationStyles();
 
   const separatorClass = React.useMemo(() => {
     if (!state.separator) {
@@ -66,7 +65,6 @@ export const useDrawerInlineStyles_unstable = (state: DrawerInlineState): Drawer
     baseClassNames,
     rootStyles.root,
     separatorClass,
-    durationStyles[state.size],
     !state.motion.active && rootStyles[state.position],
     state.motion.active && rootStyles.visible,
     state.root.className,
