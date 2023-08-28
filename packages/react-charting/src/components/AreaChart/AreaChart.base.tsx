b37b1065a14f809c07409320e8b34715abe2362f
Marcos Moura <marcosvmmoura@gmail.com>
2023-08-28 17:18:31 +0200

    Discard changes to packages/react-charting/src/components/AreaChart/AreaChart.base.tsx

--- packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
+++ packages/react-charting/src/components/AreaChart/AreaChart.base.tsx
@@ -293,7 +293,7 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
     // if no points need to be called out then don't show vertical line and callout card
     if (found && pointToHighlightUpdated && !this.state.isShowCalloutPending) {
       this.setState({
-        nearestCircleToHighlight,
+        nearestCircleToHighlight: nearestCircleToHighlight,
         isCalloutVisible: false,
         isShowCalloutPending: true,
         lineXValue: this._xAxisRectScale(pointToHighlight),
@@ -329,7 +329,7 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
     if (!found) {
       this.setState({
         isCalloutVisible: false,
-        nearestCircleToHighlight,
+        nearestCircleToHighlight: nearestCircleToHighlight,
         displayOfLine: InterceptVisibility.hide,
         isCircleClicked: false,
       });
@@ -553,7 +553,7 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
 
       const legend: ILegend = {
         title: singleChartData.legend,
-        color,
+        color: color,
         action: () => {
           this._onLegendClick(singleChartData.legend);
         },
