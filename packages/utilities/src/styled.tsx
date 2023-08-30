870fe426dd3662ab272c1ba3eea29b643c6ebd76
Sean Monahan <seanmonahan@microsoft.com>
2023-08-30 15:09:19 -0700

    mergeStyles: initial shadow DOM and constructable stylesheets implementation (#28906)
    * feat: update mergeStyles to support shadow DOM
    
    NOTE: this is stil a work in progress.
    
    - Refactors Stylesheet.ts to support shadow DOM
    - Adds React context and hooks for opting in to
      shadow DOM
    - Adds an API for projecting styles to child windows.
    
    * refactor and docs
    
    - Refactors stylesheet shadow DOM implementation a bit.
    - Adds a new docs page.
    
    * refactor to clean up types
    
    - Fixes various Typescript errors
    - Adds APIs to better encapsulate shadow DOM features
    
    * change files
    
    * add pipeline trigger
    
    * update test
    
    * fix typescript error
    
    * improve 'ShadowConfig' check
    
    * export ShadowConfig type
    
    * add constant for '__global__'
    
    * add default ShadowConfig
    
    * remove commented out line
    
    * better change file messages
    
    * remove IStyleSheetKey type
    
    * clean up
    
    * fix import path
    
    * add null checks before inserting styles
    
    * remove commented out code
    
    * refactor IStyleOptions to optionally include shadowConfig
    
    Adds an optional `shadowConfig` object to IStyleOptions. This
    allows us to pass the shadow DOM configuration object through to
    all the places we need it without modifying APIs to add a parameter
    for "shadowConfig".
    
    Aligns naming. Previously we had a type `ShadowConfig` and mostly referred
    to objects as "shadowConfig" but there were some instances of "shadowDom".
    Everything is now referred to by "shadowConfig/ShadowConfig".
    
    * update styleToClassName tests
    
    * update customizable tests
    
    * update component types for shadow DOM
    
    - remove "shadowDom" prop
    - update syles types to include "__shadowConfig__" key
    
    * remove unused variable
    
    * fix types in shadow dom example
    
    * add guard to calling requestAnimationFrame in Stylesheet
    
    This fix is for ssr-tests.

--- packages/utilities/src/styled.tsx
+++ packages/utilities/src/styled.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { concatStyleSetsWithProps } from '@fluentui/merge-styles';
+import {
+  useAdoptedStylesheet_unstable,
+  useHasMergeStylesShadowRootContext,
+} from './shadowDom/MergeStylesShadowRootContext';
 import { useCustomizationSettings } from './customizations/useCustomizationSettings';
-import type { IStyleSet, IStyleFunctionOrObject } from '@fluentui/merge-styles';
+import type { IStyleSet, IStyleFunctionOrObject, ShadowConfig } from '@fluentui/merge-styles';
+import { getWindow } from './dom/getWindow';
+// eslint-disable-next-line
+import { useWindow } from '@fluentui/react-window-provider';
 
 export interface IPropsWithStyles<TStyleProps, TStyleSet extends IStyleSet<TStyleSet>> {
   styles?: IStyleFunctionOrObject<TStyleProps, TStyleSet>;
@@ -28,6 +35,9 @@ export type StyleFunction<TStyleProps, TStyleSet> = IStyleFunctionOrObject<TStyl
 
   /** True if no styles prop or styles from Customizer is passed to wrapped component. */
   __noStyleOverride__: boolean;
+
+  /** Shadow DOM configuration object */
+  __shadowConfig__?: ShadowConfig;
 };
 
 /**
@@ -95,6 +105,22 @@ export function styled<
     const { styles: customizedStyles, dir, ...rest } = settings;
     const additionalProps = getProps ? getProps(props) : undefined;
 
+    const win = useWindow() ?? getWindow();
+    const inShadow = useHasMergeStylesShadowRootContext();
+    const shadowConfig = React.useRef<ShadowConfig>({ stylesheetKey: scope, inShadow });
+    if (
+      shadowConfig.current.stylesheetKey !== scope ||
+      shadowConfig.current.inShadow !== inShadow ||
+      shadowConfig.current.window !== win
+      // false
+    ) {
+      shadowConfig.current = {
+        stylesheetKey: scope,
+        inShadow,
+        window: win,
+      };
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const cache = (styles.current && (styles.current as any).__cachedInputs__) || [];
     const propStyles = props.styles;
@@ -118,6 +144,10 @@ export function styled<
 
       styles.current = concatenatedStyles as StyleFunction<TStyleProps, TStyleSet>;
     }
+
+    styles.current.__shadowConfig__ = shadowConfig.current;
+
+    useAdoptedStylesheet_unstable(scope);
 
     return <Component ref={forwardedRef} {...rest} {...additionalProps} {...props} styles={styles.current} />;
   });
