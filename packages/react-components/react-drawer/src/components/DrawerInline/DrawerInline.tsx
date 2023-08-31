4ee67c1a26d28f01fce4c26c93d733e4116ef977
Marcos Moura <marcosvmmoura@gmail.com>
2023-08-31 16:54:44 +0200

    feat(react-drawer): add custom styling hooks for Drawer subcomponents

--- packages/react-components/react-drawer/src/components/DrawerInline/DrawerInline.tsx
+++ packages/react-components/react-drawer/src/components/DrawerInline/DrawerInline.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
+
+import type { DrawerInlineProps } from './DrawerInline.types';
 import { useDrawerInline_unstable } from './useDrawerInline';
 import { renderDrawerInline_unstable } from './renderDrawerInline';
 import { useDrawerInlineStyles_unstable } from './useDrawerInlineStyles.styles';
-import type { DrawerInlineProps } from './DrawerInline.types';
-import type { ForwardRefComponent } from '@fluentui/react-utilities';
 
 /**
  * DrawerInline is often used for navigation that is not dismissible. As it is on the same level as
@@ -13,6 +15,8 @@ export const DrawerInline: ForwardRefComponent<DrawerInlineProps> = React.forwar
   const state = useDrawerInline_unstable(props, ref);
 
   useDrawerInlineStyles_unstable(state);
+  useCustomStyleHook_unstable('useDrawerInlineStyles_unstable')(state);
+
   return renderDrawerInline_unstable(state);
 });
 
