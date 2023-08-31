8d320769ca23ea1b8c97e10c6e28924e4e56d6e8
Marcos Moura <marcosvmmoura@gmail.com>
2023-08-31 19:36:01 +0200

    test(react-drawer): add renderization tests for drawer subcomponents

---
+++ packages/react-components/react-drawer/src/components/DrawerHeaderTitle/DrawerHeaderTitle.cy.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { mount } from '@cypress/react';
+import { FluentProvider } from '@fluentui/react-provider';
+import { webLightTheme } from '@fluentui/react-theme';
+
+import { DrawerHeaderTitle } from './DrawerHeaderTitle';
+
+const mountFluent = (element: JSX.Element) => {
+  mount(<FluentProvider theme={webLightTheme}>{element}</FluentProvider>);
+};
+
+describe('DrawerHeaderTitle', () => {
+  it('should render drawer title with correct tag name', () => {
+    mountFluent(<DrawerHeaderTitle id="drawer-title">Content</DrawerHeaderTitle>);
+
+    cy.get('#drawer-title').should('exist');
+    cy.get('#drawer-title').should('match', 'div');
+  });
+
+  it('should render children H2 tag by default', () => {
+    mountFluent(<DrawerHeaderTitle id="drawer-title">Content</DrawerHeaderTitle>);
+
+    cy.get('#drawer-title').contains('h2', 'Content');
+  });
+
+  it('should render different heading when provided', () => {
+    mountFluent(
+      <DrawerHeaderTitle id="drawer-title" heading="h1">
+        Content
+      </DrawerHeaderTitle>,
+    );
+
+    cy.get('#drawer-title').contains('h1', 'Content');
+  });
+});
