db27d1495879a049acfde3d4283edfb1e07dbd3c
Simon Fisher <syphe@outlook.com>
2023-08-31 09:55:07 +0100

    Update SearchBoxControlled.stories.tsx

--- packages/react-components/react-search-preview/stories/SearchBox/SearchBoxControlled.stories.tsx
+++ packages/react-components/react-search-preview/stories/SearchBox/SearchBoxControlled.stories.tsx
@@ -16,13 +16,17 @@ export const Controlled = () => {
     }
   };
 
+  const onDismiss = React.useCallback(() => {
+    setValue('');
+  }, [setValue]);
+
   return (
     <Field
       label="Controlled SearchBox limiting the value to 20 characters"
       validationState={valid ? 'none' : 'warning'}
       validationMessage={valid ? '' : 'Input is limited to 20 characters.'}
     >
-      <SearchBox value={value} onChange={onChange} />
+      <SearchBox value={value} onChange={onChange} onDismis={onDismiss} />
     </Field>
   );
 };
