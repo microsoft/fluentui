e475b224ca38f2f3f8da72acf633ab9d52b1f924
Lingling Becker <litong@microsoft.com>
2023-09-07 14:18:53 -0700

    change

--- packages/react-experiments/src/components/FloatingSuggestionsComposite/FloatingPeopleSuggestions/FloatingPeopleSuggestions.types.ts
+++
@@ -1,4 +0,0 @@
-import type { IBaseFloatingSuggestionsProps } from '../FloatingSuggestions.types';
-import type { IPersonaProps } from '@fluentui/react/lib/Persona';
-
-export type IFloatingPeopleSuggestionsProps = IBaseFloatingSuggestionsProps<IPersonaProps>;
