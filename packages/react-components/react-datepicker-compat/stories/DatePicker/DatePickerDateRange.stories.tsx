bd6bf41cdabf49f48733bae4177b933235e42a8d
Esteban Munoz <esteban.230@hotmail.com>
2023-09-07 13:09:19 -0700

    adding requested changes

--- packages/react-components/react-datepicker-compat/stories/DatePicker/DatePickerDateRange.stories.tsx
+++ packages/react-components/react-datepicker-compat/stories/DatePicker/DatePickerDateRange.stories.tsx
@@ -1,24 +1,53 @@
 import * as React from 'react';
-import { Field, makeStyles } from '@fluentui/react-components';
+import { Field, Label, makeStyles, Select } from '@fluentui/react-components';
 import { DatePicker, DateRangeType } from '@fluentui/react-datepicker-compat';
 
 const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: '20px',
+  },
+
   control: {
     maxWidth: '300px',
   },
 });
 
+const dateRangeOptions = {
+  Day: DateRangeType.Day,
+  'Work Week': DateRangeType.WorkWeek,
+  Week: DateRangeType.Week,
+  Month: DateRangeType.Month,
+};
+
 export const DateRange = () => {
   const styles = useStyles();
+  const [dateRangeType, setDateRangeType] = React.useState('Week');
 
   return (
-    <Field label="Select a date">
-      <DatePicker
-        calendar={{ dateRangeType: DateRangeType.Week }}
-        placeholder="Select a date..."
-        className={styles.control}
-      />
-    </Field>
+    <div className={styles.container}>
+      <Field label="Select a date">
+        <DatePicker
+          calendar={{ dateRangeType: dateRangeOptions[dateRangeType as keyof typeof dateRangeOptions] }}
+          placeholder="Select a date..."
+          className={styles.control}
+        />
+      </Field>
+      <div>
+        <Label htmlFor="select-daterange-id">Select a DateRangeType</Label>
+        <Select
+          onChange={(ev, data) => setDateRangeType(data.value)}
+          value={dateRangeType}
+          className={styles.control}
+          id="select-daterange-id"
+        >
+          {Object.keys(dateRangeOptions).map(key => (
+            <option key={key}>{key}</option>
+          ))}
+        </Select>
+      </div>
+    </div>
   );
 };
 
