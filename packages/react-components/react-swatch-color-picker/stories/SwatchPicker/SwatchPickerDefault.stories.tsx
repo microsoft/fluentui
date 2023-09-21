cad9a0b2b7ba198e8cee5cc257a11953ad63bbd2
Valentyna <vkozlova@microsoft.com>
2023-09-21 18:10:42 +0200

    Added more examples

--- packages/react-components/react-swatch-color-picker/stories/SwatchPicker/SwatchPickerDefault.stories.tsx
+++ packages/react-components/react-swatch-color-picker/stories/SwatchPicker/SwatchPickerDefault.stories.tsx
@@ -7,40 +7,112 @@ const customStyles = makeStyles({
     ...shorthands.borderRadius('8px'),
     boxShadow: 'rgb(46 46 46 / 43%) 0px 1px 2px 0px',
   },
+  picker: {
+    gridTemplateColumns: `repeat(3, 30px)`,
+    columnGap: '4px',
+    rowGap: '4px',
+  },
 });
 
 const colors = [
   {
     id: 0,
-    color: 'red',
+    swatch: 'red',
   },
   {
     id: 1,
-    color: 'rgb(189, 255, 104)',
+    swatch: 'rgb(189, 255, 104)',
   },
   {
     id: 2,
-    color: 'rgba(189, 255, 104,.4)',
+    swatch: 'rgba(189, 255, 104,.4)',
   },
   {
     id: 3,
-    color: '#f09',
+    swatch: '#f09',
   },
   {
     id: 4,
-    color: 'yellow',
+    swatch: 'yellow',
   },
   {
     id: 5,
-    color: 'cyan',
+    swatch: 'cyan',
   },
   {
     id: 6,
-    color: 'purple',
+    swatch: 'purple',
   },
   {
     id: 7,
-    color: 'magenta',
+    swatch: 'magenta',
+  },
+];
+
+const colorsLarge = [
+  {
+    id: 0,
+    swatch: 'red',
+  },
+  {
+    id: 1,
+    swatch: 'rgb(189, 255, 104)',
+  },
+  {
+    id: 2,
+    swatch: 'rgba(189, 255, 104,.4)',
+  },
+  {
+    id: 3,
+    swatch: '#f09',
+  },
+  {
+    id: 4,
+    swatch: 'yellow',
+  },
+  {
+    id: 5,
+    swatch: 'cyan',
+  },
+  {
+    id: 6,
+    swatch: 'purple',
+  },
+  {
+    id: 7,
+    swatch: 'magenta',
+  },
+  {
+    id: 8,
+    swatch: 'red',
+  },
+  {
+    id: 9,
+    swatch: 'rgb(189, 255, 104)',
+  },
+  {
+    id: 10,
+    swatch: 'rgba(189, 255, 104,.4)',
+  },
+  {
+    id: 11,
+    swatch: '#f09',
+  },
+  {
+    id: 12,
+    swatch: 'yellow',
+  },
+  {
+    id: 13,
+    swatch: 'cyan',
+  },
+  {
+    id: 14,
+    swatch: 'purple',
+  },
+  {
+    id: 15,
+    swatch: 'magenta',
   },
 ];
 
@@ -52,14 +124,14 @@ export const Default = (props: Partial<SwatchPickerProps>) => {
       <h2>row</h2>
       <SwatchPicker layout="row" value={color} onChange={(_, data) => setColor(data.value)} aria-labelledby="colors">
         {colors.map(item => (
-          <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.color} color={item.color} />
+          <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} color={item.swatch} />
         ))}
       </SwatchPicker>
       <h2>grid</h2>
       <div style={{ width: '100px' }}>
         <SwatchPicker layout="grid" value={color} onChange={(_, data) => setColor(data.value)} aria-labelledby="colors">
           {colors.map(item => (
-            <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.color} color={item.color} />
+            <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} color={item.swatch} />
           ))}
         </SwatchPicker>
       </div>
@@ -77,20 +149,38 @@ export const Default = (props: Partial<SwatchPickerProps>) => {
             aria-labelledby="colors"
           >
             {colors.map(item => (
-              <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.color} color={item.color} />
+              <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} color={item.swatch} />
             ))}
           </SwatchPicker>
         </PopoverSurface>
       </Popover>
+      <h2>Grid with a lot of colors</h2>
+      <SwatchPicker
+        layout="grid"
+        value={color}
+        onChange={(_, data) => setColor(data.value)}
+        aria-labelledby="colors"
+        style={{ gridTemplateColumns: `repeat(4, 30px)` }}
+      >
+        {colorsLarge.map(item => (
+          <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} color={item.swatch} />
+        ))}
+      </SwatchPicker>
       <h2>custom cell</h2>
-      <SwatchPicker layout="row" value={color} onChange={(_, data) => setColor(data.value)} aria-labelledby="colors">
+      <SwatchPicker
+        layout="grid"
+        value={color}
+        onChange={(_, data) => setColor(data.value)}
+        aria-labelledby="colors"
+        className={{}}
+      >
         {colors.map(item => (
           <SwatchColorPikerCell
             id={item.id}
             key={item.id}
             name="color"
-            value={item.color}
-            color={item.color}
+            value={item.swatch}
+            color={item.swatch}
             className={styles.root}
           />
         ))}
