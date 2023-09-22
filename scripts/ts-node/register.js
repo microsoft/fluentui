46af2bc585dd285962d233070ada71506554990a
Martin Hochel <martinhochel@microsoft.com>
2023-09-22 14:32:30 +0200

    fix(scripts-ts-node): disable swc which doesnt work with current ts-node

--- scripts/ts-node/register.js
+++ scripts/ts-node/register.js
@@ -3,5 +3,6 @@ const tsNode = require('ts-node');
 tsNode.register({
   // https://github.com/TypeStrong/ts-node#skipproject - don't read tsconfig within ts-node
   skipProject: true,
-  swc: true,
+  // @deprecated: we cannot use this until new version of ts-node is released https://github.com/TypeStrong/ts-node/pull/2062
+  // swc: true,
 });
