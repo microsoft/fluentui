5f24114444234756e6941c182950734d46da8611
ankityadav4 <ankityadav@microsoft.com>
2023-09-25 18:44:01 +0530

    Removing other examples for d site

--- packages/react-examples/src/react-charting/TreeChart/TreeChart.doc.tsx
+++ packages/react-examples/src/react-charting/TreeChart/TreeChart.doc.tsx
@@ -2,19 +2,19 @@ import * as React from 'react';
 
 import { IDocPageProps } from '@fluentui/react/lib/common/DocPage.types';
 
-import { TreeChartTwoLayerExample } from './TreeChart.TwoLayer.Example';
-import { TreeChartThreeLayerLongExample } from './TreeChart.ThreeLayerLong.Example';
-import { TreeChartThreeLayerCompactExample } from './TreeChart.ThreeLayerCompact.Example';
-import { TreeChartThreeLayerExample } from './TreeChart.ThreeLayer.Example';
+import { TreeChartThreeLayerCompactDocSiteExample } from './TreeChart.ThreeLayerCompactDocSite.Example';
+import { TreeChartThreeLayerDocSiteExample } from './TreeChart.ThreeLayerDoc.Example';
+// import { TreeChartThreeLayerCompactExample } from './TreeChart.ThreeLayerCompact.Example';
+// import { TreeChartThreeLayerExample } from './TreeChart.ThreeLayer.Example';
 
-const TreeChartTwoLayerExampleCode =
-  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.TwoLayer.Example.tsx') as string;
-const TreeChartThreeLayerLongExampleCode =
-  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayerLong.Example.tsx') as string;
-const TreeChartThreeLayerCompactExampleCode =
-  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayerCompact.Example.tsx') as string;
-const TreeChartThreeLayerExampleCode =
-  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayer.Example.tsx') as string;
+const TreeChartThreeLayerCompactDocExampleCode =
+  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayerCompactDocSite.Example') as string;
+const TreeChartThreeLayerDocExampleCode =
+  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayerDoc.Example') as string;
+// const TreeChartThreeLayerCompactExampleCode =
+//   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayerCompact.Example.tsx') as string;
+// const TreeChartThreeLayerExampleCode =
+//   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/TreeChart.ThreeLayer.Example.tsx') as string;
 
 export const TreeChartPageProps: IDocPageProps = {
   title: 'TreeChart',
@@ -23,24 +23,24 @@ export const TreeChartPageProps: IDocPageProps = {
   examples: [
     {
       title: 'TreeChart two layer',
-      code: TreeChartTwoLayerExampleCode,
-      view: <TreeChartTwoLayerExample />,
+      code: TreeChartThreeLayerCompactDocExampleCode,
+      view: <TreeChartThreeLayerCompactDocSiteExample />,
     },
     {
       title: 'TreeChart three layer',
-      code: TreeChartThreeLayerExampleCode,
-      view: <TreeChartThreeLayerExample />,
+      code: TreeChartThreeLayerDocExampleCode,
+      view: <TreeChartThreeLayerDocSiteExample />,
     },
-    {
-      title: 'TreeChart three layer long variant',
-      code: TreeChartThreeLayerLongExampleCode,
-      view: <TreeChartThreeLayerLongExample />,
-    },
-    {
-      title: 'TreeChart three layer compact variant',
-      code: TreeChartThreeLayerCompactExampleCode,
-      view: <TreeChartThreeLayerCompactExample />,
-    },
+    // {
+    //   title: 'TreeChart three layer long variant',
+    //   code: TreeChartThreeLayerLongExampleCode,
+    //   view: <TreeChartThreeLayerLongExample />,
+    // },
+    // {
+    //   title: 'TreeChart three layer compact variant',
+    //   code: TreeChartThreeLayerCompactExampleCode,
+    //   view: <TreeChartThreeLayerCompactExample />,
+    // },
   ],
   overview: require<string>('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/docs/TreeChartOverview.md'),
   bestPractices: require<string>('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/TreeChart/docs/TreeChartBestPractices.md'),
