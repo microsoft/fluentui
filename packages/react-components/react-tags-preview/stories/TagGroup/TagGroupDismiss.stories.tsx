a6b240db0cf5835fd8980d93613910f485bc27c4
Amber <yuanboxue@outlook.com>
2023-09-25 12:00:41 +0200

    Update packages/react-components/react-tags-preview/stories/TagGroup/TagGroupDismiss.stories.tsx
    Co-authored-by: Oleksandr Fediashov <alexander.mcgarret@gmail.com>

--- packages/react-components/react-tags-preview/stories/TagGroup/TagGroupDismiss.stories.tsx
+++ packages/react-components/react-tags-preview/stories/TagGroup/TagGroupDismiss.stories.tsx
@@ -79,6 +79,7 @@ const DismissWithTags = () => {
         ref={resetButtonRef}
         disabled={visibleTags.length !== 0}
         className={styles.resetButton}
+        size="small"
       >
         Reset the example
       </Button>
