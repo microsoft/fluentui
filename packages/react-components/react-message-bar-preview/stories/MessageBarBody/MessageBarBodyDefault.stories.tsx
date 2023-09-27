0c03bfaada3eb6232a6c5fb6958f4e894d665bd9
Lingfan Gao <lingfan.gao@microsoft.com>
2023-09-27 15:48:03 +0000

    remove stories

--- packages/react-components/react-message-bar-preview/stories/MessageBarBody/MessageBarBodyDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { MessageBarBody, MessageBarBodyProps } from '@fluentui/react-message-bar-preview';
-
-export const Default = (props: Partial<MessageBarBodyProps>) => <MessageBarBody {...props} />;
