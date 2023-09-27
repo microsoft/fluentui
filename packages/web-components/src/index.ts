a83af28b43c6ad571f2fb762647ad1d4fcebe766
BrdyBrn <brian.christopher.brady@gmail.com>
2023-09-27 11:57:55 -0700

    Menu Web Component [ PRIORITY 1 ] (#27906)
    * radio init
    
    * styles radio
    
    * reverts branch
    
    * input spec init
    
    * cleans up spec
    
    * formatting
    
    * updates component name to text input
    
    * updates component name in spec
    
    * menu: menu init
    
    * menu: updates README
    
    * menu: updates template, styles
    
    * menu: updates menu and stories
    
    * menu: removes dead code
    
    * menu: removes folder and file
    
    * yarn change
    
    * menu: removes tab index on template slot
    
    * menu changes based on feedback
    
    * menu: updates stories
    
    * menu, emits event on expanded change
    
    * menu: updatews attr/property name to align with fluent ( expand --> open )
    
    * menu: cleans up code
    
    * reverts dead file
    
    * menu: removes block styling
    
    * enhances cleanup of autoUpdate, conditionally remove mouseover event listener
    
    * Add defaultPrevented check to handleTriggerKeydown function
    
    * menu: adds open/close features
    
    * menu: fixes spelling error in readme
    
    * menu: adds part selector to positioningRegion
    
    * menu: adds z-index css variable, updates docs accordingly
    
    * menu: adds attr changed callback for closeOnScroll to manage event listeners
    
    * menu: adds exports

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -10,6 +10,7 @@ export * from './counter-badge/index.js';
 export * from './divider/index.js';
 export * from './image/index.js';
 export * from './label/index.js';
+export * from './menu/index.js';
 export * from './menu-button/index.js';
 export * from './menu-item/index.js';
 export * from './menu-list/index.js';
