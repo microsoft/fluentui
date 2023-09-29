7c0148720b7cf603e7bdaf8433bc54f71a8eb694
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-09-29 11:48:04 +0000

    chore: replace makeStyles with makeResetStyles

--- packages/react-components/react-tree/src/components/TreeItem/useTreeItemStyles.styles.ts
+++ packages/react-components/react-tree/src/components/TreeItem/useTreeItemStyles.styles.ts
@@ -1,4 +1,4 @@
-import { GriffelStyle, makeStyles, mergeClasses } from '@griffel/react';
+import { GriffelStyle, makeResetStyles, makeStyles, mergeClasses } from '@griffel/react';
 import type { TreeItemCSSProperties, TreeItemSlots, TreeItemState } from './TreeItem.types';
 import type { SlotClassNames } from '@fluentui/react-utilities';
 import { tokens } from '@fluentui/react-theme';
@@ -9,42 +9,43 @@ export const treeItemClassNames: SlotClassNames<TreeItemSlots> = {
   root: 'fui-TreeItem',
 };
 
+const useBaseStyles = makeResetStyles({
+  position: 'relative',
+  cursor: 'pointer',
+  display: 'flex',
+  flexDirection: 'column',
+  boxSizing: 'border-box',
+  backgroundColor: tokens.colorSubtleBackground,
+  color: tokens.colorNeutralForeground2,
+  paddingRight: tokens.spacingHorizontalNone,
+  ...createFocusOutlineStyle(),
+});
+
 type StaticLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 type StaticLevelProperty = `level${StaticLevel}`;
 
-const useRootStyles = makeStyles({
+const useStyles = makeStyles({
   ...(Object.fromEntries(
     Array.from<never, [StaticLevelProperty, TreeItemCSSProperties]>({ length: 10 }, (_, index) => [
       `level${(index + 1) as StaticLevel}`,
       { [treeItemLevelToken]: index + 1 },
     ]),
   ) as Record<StaticLevelProperty, GriffelStyle>),
-  base: {
-    position: 'relative',
-    cursor: 'pointer',
-    display: 'flex',
-    flexDirection: 'column',
-    boxSizing: 'border-box',
-    backgroundColor: tokens.colorSubtleBackground,
-    color: tokens.colorNeutralForeground2,
-    paddingRight: tokens.spacingHorizontalNone,
-  },
-  focusIndicator: createFocusOutlineStyle(),
 });
 
 /**
  * Apply styling to the TreeItem slots based on the state
  */
 export const useTreeItemStyles_unstable = (state: TreeItemState): TreeItemState => {
-  const rootStyles = useRootStyles();
+  const baseStyles = useBaseStyles();
+  const styles = useStyles();
 
   const { level } = state;
 
   state.root.className = mergeClasses(
     treeItemClassNames.root,
-    isStaticallyDefinedLevel(level) && rootStyles[`level${level}` as StaticLevelProperty],
-    rootStyles.base,
-    rootStyles.focusIndicator,
+    baseStyles,
+    isStaticallyDefinedLevel(level) && styles[`level${level}` as StaticLevelProperty],
     state.root.className,
   );
 
