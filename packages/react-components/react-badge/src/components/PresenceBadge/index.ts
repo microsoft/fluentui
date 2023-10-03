909ff5e5cade0983f3a6d38669279ddd99a28666
Tomi Olubeko <ololubek@microsoft.com>
2023-10-03 12:25:29 -0700

    doc update

--- packages/react-components/react-badge/src/components/PresenceBadge/index.ts
+++ packages/react-components/react-badge/src/components/PresenceBadge/index.ts
@@ -2,4 +2,5 @@ export * from './PresenceBadge';
 export * from './PresenceBadge.types';
 export * from './usePresenceBadge';
 export * from './usePresenceBadgeStyles.styles';
+/** @deprecated */
 export * from './presenceIcons';
