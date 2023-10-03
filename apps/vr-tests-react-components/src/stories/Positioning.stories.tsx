618e88259a468d5b120358137cd84968ef863e2f
Lingfan Gao <lingfan.gao@microsoft.com>
2023-10-03 17:22:06 +0000

    fix: Consider all parents as scroll parents
    This PR introduces `listScrollParents` which will collect every scroll
    parent in DOM of both target and container.
    
    This behaviour already existed in poppver v2 https://github.com/floating-ui/floating-ui/blob/v2.x/src/dom-utils/listScrollParents.js

--- apps/vr-tests-react-components/src/stories/Positioning.stories.tsx
+++ apps/vr-tests-react-components/src/stories/Positioning.stories.tsx
@@ -1084,6 +1084,47 @@ const ScrollJumpContext = () => {
   );
 };
 
+const MultiScrollParent = () => {
+  const { targetRef, containerRef } = usePositioning({});
+  const scrollContainerRef = React.useRef<HTMLDivElement>(null);
+
+  const scroll = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ top: 100 });
+    }
+  };
+
+  return (
+    <>
+      <span>The popover should stay attached to the trigger</span>
+      <button id="scroll" onClick={scroll}>
+        scroll
+      </button>
+      <div ref={scrollContainerRef} style={{ border: '2px dashed green', height: 300, width: 400, overflow: 'auto' }}>
+        <div style={{ height: 5000, width: 300, position: 'relative' }}>
+          <div
+            style={{
+              overflow: 'auto',
+              border: '2px dashed red',
+              position: 'absolute',
+              top: 150,
+              left: 100,
+              padding: 20,
+            }}
+          >
+            <button id="target" ref={targetRef}>
+              Trigger
+            </button>
+          </div>
+        </div>
+      </div>
+      <div ref={containerRef} style={{ border: '2px solid blue', padding: 20, backgroundColor: 'white' }}>
+        Popover
+      </div>
+    </>
+  );
+};
+
 storiesOf('Positioning', module)
   .addDecorator(story => (
     <div
@@ -1161,7 +1202,12 @@ storiesOf('Positioning', module)
     'disable CSS transform with position fixed',
     () => <PositionAndAlignProps positionFixed useTransform={false} />,
     { includeRtl: true },
-  );
+  )
+  .addStory('Multiple scroll parents', () => (
+    <StoryWright steps={new Steps().click('#scroll').snapshot('container attached to target').end()}>
+      <MultiScrollParent />
+    </StoryWright>
+  ));
 
 storiesOf('Positioning (no decorator)', module)
   .addStory('scroll jumps', () => (
