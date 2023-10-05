fe05d189c90076c5648e7c3b8da26e9fea2680f8
Lingfan Gao <lingfangao@hotmail.com>
2023-10-05 10:07:43 +0200

    chore: Adds VR tests for MessageBar
    Addresses #22578

---
+++ apps/vr-tests-react-components/src/stories/MessageBar/MessageBar.stories.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import {
+  MessageBar,
+  MessageBarActions,
+  MessageBarBody,
+  MessageBarIntent,
+  MessageBarTitle,
+} from '@fluentui/react-message-bar-preview';
+import { ComponentMeta } from '@storybook/react';
+import { Steps } from 'storywright';
+import { Button } from '@fluentui/react-button';
+import { Link } from '@fluentui/react-link';
+import { DismissRegular } from '@fluentui/react-icons';
+import { getStoryVariant, withStoryWrightSteps, DARK_MODE, HIGH_CONTRAST, RTL } from '../../utilities';
+
+const steps = new Steps().snapshot('default', { cropTo: '.testWrapper' }).end();
+
+export default {
+  title: 'MessageBar',
+  Component: MessageBar,
+  decorators: [story => withStoryWrightSteps({ story, steps })],
+} as ComponentMeta<typeof MessageBar>;
+
+const intents: MessageBarIntent[] = ['info', 'warning', 'error', 'success'];
+
+export const Intents = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+    {intents.map(intent => (
+      <MessageBar key={intent} intent={intent}>
+        <MessageBarBody>
+          <MessageBarTitle>{intent}</MessageBarTitle>
+          Message providing information to the user with actionable insights. <Link>Link</Link>
+        </MessageBarBody>
+        <MessageBarActions containerAction={<Button appearance="transparent" icon={<DismissRegular />} />}>
+          <Button>Action</Button>
+          <Button>Action</Button>
+        </MessageBarActions>
+      </MessageBar>
+    ))}
+  </div>
+);
+
+export const IntentsRTL = getStoryVariant(Intents, RTL);
+export const IntentsDarkMode = getStoryVariant(Intents, DARK_MODE);
+export const IntentsHighContrast = getStoryVariant(Intents, HIGH_CONTRAST);
+
+export const Multiline = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', width: 500 }}>
+    {intents.map(intent => (
+      <MessageBar layout="multiline" key={intent} intent={intent}>
+        <MessageBarBody>
+          <MessageBarTitle>{intent}</MessageBarTitle>
+          Message providing information to the user with actionable insights. <Link>Link</Link>
+        </MessageBarBody>
+        <MessageBarActions containerAction={<Button appearance="transparent" icon={<DismissRegular />} />}>
+          <Button>Action</Button>
+          <Button>Action</Button>
+        </MessageBarActions>
+      </MessageBar>
+    ))}
+  </div>
+);
+
+export const Auto = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', width: 400 }}>
+      <MessageBar layout="auto">
+        <MessageBarBody>
+          <MessageBarTitle>Title</MessageBarTitle>
+          Message providing information to the user with actionable insights. <Link>Link</Link>
+        </MessageBarBody>
+        <MessageBarActions containerAction={<Button appearance="transparent" icon={<DismissRegular />} />}>
+          <Button>Action</Button>
+          <Button>Action</Button>
+        </MessageBarActions>
+      </MessageBar>
+    </div>
+  );
+};
