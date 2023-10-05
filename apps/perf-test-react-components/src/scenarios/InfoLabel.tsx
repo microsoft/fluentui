7502f790048882b052e49e77e6b5fcef4b74afd2
Esteban Munoz <esteban.230@hotmail.com>
2023-10-04 17:06:25 -0700

    updating perf tests

--- apps/perf-test-react-components/src/scenarios/InfoLabel.tsx
+++ apps/perf-test-react-components/src/scenarios/InfoLabel.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { InfoButton } from '@fluentui/react-infobutton';
+import { InfoLabel } from '@fluentui/react-infobutton';
 import { FluentProvider } from '@fluentui/react-provider';
 import { webLightTheme } from '@fluentui/react-theme';
 
-const Scenario = () => <InfoButton info={"This is an InfoButton's content."} />;
+const Scenario = () => <InfoLabel info="This is an InfoButton's content." label="Sample label text" />;
 
 Scenario.decorator = (props: { children: React.ReactNode }) => {
   <FluentProvider theme={webLightTheme}>{props.children}</FluentProvider>;
