7e6c2f82617d2570ed6724e591456aa0d7c63220
Oleksandr Fediashov <olfedias@microsoft.com>
2023-10-06 13:56:45 +0200

    revert default

--- packages/react-components/react-message-bar-preview/stories/MessageBar/Default.stories.tsx
+++ packages/react-components/react-message-bar-preview/stories/MessageBar/Default.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Link } from '@fluentui/react-components';
-import { MessageBar, MessageBarTitle, MessageBarBody } from '@fluentui/react-message-bar-preview';
+import { Button, Link } from '@fluentui/react-components';
+import { DismissRegular } from '@fluentui/react-icons';
+import { MessageBar, MessageBarActions, MessageBarTitle, MessageBarBody } from '@fluentui/react-message-bar-preview';
 
 export const Default = () => (
   <MessageBar>
@@ -8,5 +9,9 @@ export const Default = () => (
       <MessageBarTitle>Descriptive title</MessageBarTitle>
       Message providing information to the user with actionable insights. <Link>Link</Link>
     </MessageBarBody>
+    <MessageBarActions containerAction={<Button appearance="transparent" icon={<DismissRegular />} />}>
+      <Button>Action</Button>
+      <Button>Action</Button>
+    </MessageBarActions>
   </MessageBar>
 );
