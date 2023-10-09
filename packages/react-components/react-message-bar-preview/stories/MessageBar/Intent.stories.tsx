b22ee708e47c3e5e9cee60d1986df257e6c052c0
Lingfan Gao <lingfangao@hotmail.com>
2023-10-09 15:37:57 +0200

    revert

--- packages/react-components/react-message-bar-preview/stories/MessageBar/Intent.stories.tsx
+++ packages/react-components/react-message-bar-preview/stories/MessageBar/Intent.stories.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react';
-import { Button, Link, makeStyles, shorthands } from '@fluentui/react-components';
-import {
-  MessageBar,
-  MessageBarTitle,
-  MessageBarBody,
-  MessageBarIntent,
-  MessageBarActions,
-} from '@fluentui/react-message-bar-preview';
-import { DismissRegular } from '@fluentui/react-icons';
+import { Link, makeStyles, shorthands } from '@fluentui/react-components';
+import { MessageBar, MessageBarTitle, MessageBarBody, MessageBarIntent } from '@fluentui/react-message-bar-preview';
 
 const useClasses = makeStyles({
   container: {
     display: 'flex',
     flexDirection: 'column',
     ...shorthands.gap('10px'),
-    resize: 'horizontal',
-    ...shorthands.overflow('hidden'),
   },
 });
 const intents: MessageBarIntent[] = ['info', 'warning', 'error', 'success'];
@@ -31,10 +22,6 @@ export const Intent = () => {
             <MessageBarTitle>{intent}</MessageBarTitle>
             Message providing information to the user with actionable insights. <Link>Link</Link>
           </MessageBarBody>
-          <MessageBarActions containerAction={<Button icon={<DismissRegular />} />}>
-            <Button>Action</Button>
-            <Button>Action</Button>
-          </MessageBarActions>
         </MessageBar>
       ))}
     </div>
