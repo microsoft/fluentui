17ee53f3d4f94721f5ab27b13abfdef05d44b493
Lingfan Gao <lingfangao@hotmail.com>
2023-10-09 15:31:17 +0200

    fix: MessageBarActions should not overflow into grid padding
    CSS grid can cause its own padding to overflow cells. The only way to
    avoid this padding collapse (that I could think of) is to handle the spacing in the grid areas
    directly.
    
    Fixes #29423

--- packages/react-components/react-message-bar-preview/stories/MessageBar/Intent.stories.tsx
+++ packages/react-components/react-message-bar-preview/stories/MessageBar/Intent.stories.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import { Link, makeStyles, shorthands } from '@fluentui/react-components';
-import { MessageBar, MessageBarTitle, MessageBarBody, MessageBarIntent } from '@fluentui/react-message-bar-preview';
+import { Button, Link, makeStyles, shorthands } from '@fluentui/react-components';
+import {
+  MessageBar,
+  MessageBarTitle,
+  MessageBarBody,
+  MessageBarIntent,
+  MessageBarActions,
+} from '@fluentui/react-message-bar-preview';
+import { DismissRegular } from '@fluentui/react-icons';
 
 const useClasses = makeStyles({
   container: {
     display: 'flex',
     flexDirection: 'column',
     ...shorthands.gap('10px'),
+    resize: 'horizontal',
+    ...shorthands.overflow('hidden'),
   },
 });
 const intents: MessageBarIntent[] = ['info', 'warning', 'error', 'success'];
@@ -22,6 +31,10 @@ export const Intent = () => {
             <MessageBarTitle>{intent}</MessageBarTitle>
             Message providing information to the user with actionable insights. <Link>Link</Link>
           </MessageBarBody>
+          <MessageBarActions containerAction={<Button icon={<DismissRegular />} />}>
+            <Button>Action</Button>
+            <Button>Action</Button>
+          </MessageBarActions>
         </MessageBar>
       ))}
     </div>
