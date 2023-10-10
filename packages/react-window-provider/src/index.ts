157cb0813a0be4fa860bea1a2d5fb8a2a0320208
Sean Monahan <seanmonahan@microsoft.com>
2023-10-10 02:17:18 +0000

    add lint exception

--- packages/react-window-provider/src/index.ts
+++ packages/react-window-provider/src/index.ts
@@ -1,4 +1,10 @@
-export { WindowContext, useWindow, useDocument, WindowProvider } from './WindowProvider';
+export {
+  // eslint-disable-next-line @fluentui/ban-context-export
+  WindowContext,
+  useWindow,
+  useDocument,
+  WindowProvider,
+} from './WindowProvider';
 export type { WindowProviderProps } from './WindowProvider';
 
 import './version';
