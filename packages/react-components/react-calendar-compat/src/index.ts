8ab7301accea2915617cc11fa0bda943d4a551ea
Esteban Munoz Facusse <estebanmu@microsoft.com>
2023-10-10 08:09:00 -0700

    chore(react-calendar-compat): Copy Calendar files from datepicker-compat (#29466)
    * chore: Copy Calendar files from datepicker-compat.
    
    * updating deps

--- packages/react-components/react-calendar-compat/src/index.ts
+++ packages/react-components/react-calendar-compat/src/index.ts
@@ -1,1 +1,34 @@
-export {};
+export { AnimationDirection } from './Calendar';
+export type { CalendarProps, ICalendar } from './Calendar';
+
+export type { CalendarDayProps, ICalendarDay } from './CalendarDay';
+
+export type { CalendarMonthProps, ICalendarMonth } from './CalendarMonth';
+
+export {
+  DAYS_IN_WEEK,
+  DateRangeType,
+  DayOfWeek,
+  FirstWeekOfYear,
+  MonthOfYear,
+  TimeConstants,
+  addDays,
+  addMonths,
+  addWeeks,
+  addYears,
+  compareDatePart,
+  compareDates,
+  getDatePartHashValue,
+  getDateRangeArray,
+  getEndDateOfWeek,
+  getMonthEnd,
+  getMonthStart,
+  getStartDateOfWeek,
+  getWeekNumber,
+  getWeekNumbersInMonth,
+  getYearEnd,
+  getYearStart,
+  isInDateRangeArray,
+  setMonth,
+} from './utils';
+export type { CalendarStrings, DateFormatting, DateGridStrings } from './utils';
