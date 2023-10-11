33021c484083a8cc6917e75096c53c33a88fad53
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-10-11 12:13:09 +0000

    chore: remove mockFetch and useQuery from story

--- packages/react-components/react-tree/stories/Tree/TreeLazyLoading.stories.tsx
+++ packages/react-components/react-tree/stories/Tree/TreeLazyLoading.stories.tsx
@@ -4,27 +4,25 @@ import {
   FlatTreeItem,
   TreeItemLayout,
   TreeItemValue,
-  TreeOpenChangeData,
-  TreeOpenChangeEvent,
   Spinner,
   shorthands,
   makeStyles,
+  TreeItemOpenChangeData,
+  TreeItemOpenChangeEvent,
 } from '@fluentui/react-components';
-
-interface Result {
-  results: { name: string }[];
-}
+import { useQuery } from './utils/useQuery';
+import { mockFetch } from './utils/mockFetch';
 
 interface Entity {
   name: string;
   value: string;
-  itemType: 'branch' | 'leaf';
 }
 
-interface SubtreeProps {
-  openItems: Set<TreeItemValue>;
-  setAriaMessage: (message: string) => void;
-}
+type SubtreeProps = {
+  value: TreeItemValue;
+  onDataLoading?(): void;
+  onDataLoaded?(): void;
+};
 
 const useStyles = makeStyles({
   screenReadersOnly: {
@@ -39,20 +37,26 @@ const useStyles = makeStyles({
 });
 
 export const LazyLoading = () => {
-  const [openItems, setOpenItems] = React.useState<Set<TreeItemValue>>(() => new Set());
   const [ariaMessage, setAriaMessage] = React.useState('');
   const styles = useStyles();
-
-  const handleOpenChange = React.useCallback((_: TreeOpenChangeEvent, data: TreeOpenChangeData) => {
-    setOpenItems(data.openItems);
-  }, []);
-
   return (
     <>
-      <FlatTree openItems={openItems} onOpenChange={handleOpenChange} aria-label="Lazy Loading">
-        <PeopleSubtree openItems={openItems} setAriaMessage={setAriaMessage} />
-        <PlanetsSubtree openItems={openItems} setAriaMessage={setAriaMessage} />
-        <StarshipsSubtree openItems={openItems} setAriaMessage={setAriaMessage} />
+      <FlatTree aria-label="Lazy Loading">
+        <Subtree
+          value="People"
+          onDataLoaded={React.useCallback(() => setAriaMessage(`people items loaded`), [])}
+          onDataLoading={React.useCallback(() => setAriaMessage(`loading people items...`), [])}
+        />
+        <Subtree
+          value="Planet"
+          onDataLoaded={React.useCallback(() => setAriaMessage(`planet items loaded`), [])}
+          onDataLoading={React.useCallback(() => setAriaMessage(`loading planet items...`), [])}
+        />
+        <Subtree
+          value="Starship"
+          onDataLoaded={React.useCallback(() => setAriaMessage(`starship items loaded`), [])}
+          onDataLoading={React.useCallback(() => setAriaMessage(`loading starship items...`), [])}
+        />
       </FlatTree>
       <div aria-live="polite" aria-atomic="true" className={styles.screenReadersOnly}>
         {ariaMessage}
@@ -61,50 +65,62 @@ export const LazyLoading = () => {
   );
 };
 
-const PeopleSubtree: React.FC<SubtreeProps> = ({ openItems, setAriaMessage }) => {
-  const { isLoaded, isLoading, query } = useQuery<Entity[]>([]);
-  const [items, setItems] = React.useState<Entity[]>([]);
+const Subtree: React.FC<SubtreeProps> = ({ onDataLoaded, onDataLoading, value }) => {
+  const [open, setOpen] = React.useState(false);
+  // useQuery here is just a helper to simulate async data fetching
+  // you can use any other async data fetching library like react-query, swr, etc.
+  const { query, value: items, state } = useQuery<Entity[]>([]);
+
+  // we need to focus the first item when the subtree is opened
   const firstItemRef = React.useRef<HTMLDivElement>(null);
 
-  const handleDataFetch = React.useCallback(
-    async (type: string) => {
-      query(
-        async () => {
-          const json = await mockFetch(type);
-          return json.results.map<Entity>(entity => ({
-            value: `${type}/${entity.name}`,
-            name: entity.name,
-            itemType: 'leaf',
-          }));
-        },
-        data => {
-          setItems(data);
-          setAriaMessage(`people items loaded`);
-          firstItemRef.current?.focus();
-        },
-      );
+  const handleOpenChange = React.useCallback(
+    (e: TreeItemOpenChangeEvent, data: TreeItemOpenChangeData) => {
+      setOpen(data.open);
     },
-    [query, setAriaMessage],
+    [setOpen],
   );
 
   React.useEffect(() => {
-    if (openItems.has('people') && !isLoaded && !isLoading) {
-      setAriaMessage(`loading people items...`);
-      handleDataFetch('people');
+    if (open && state === 'idle') {
+      onDataLoading?.();
+      query(async () => {
+        // mockFetch is just a helper to simulate an API endpoint.
+        // you probably will be using some custom API endpoint here.
+        const json = await mockFetch(value.toString());
+        return json.results.map<Entity>(entity => ({ value: `${value}/${entity.name}`, name: entity.name }));
+      });
     }
-  }, [handleDataFetch, isLoaded, isLoading, openItems, setAriaMessage]);
+  }, [open, onDataLoading, query, state, value]);
+
+  React.useEffect(() => {
+    if (open && state === 'loaded') {
+      onDataLoaded?.();
+      firstItemRef.current?.focus();
+    }
+  }, [open, state, onDataLoaded]);
 
   return (
     <>
-      <FlatTreeItem value="people" aria-level={1} aria-setsize={3} aria-posinset={1} itemType="branch">
-        <TreeItemLayout expandIcon={isLoading ? <Spinner size="tiny" /> : undefined}>People</TreeItemLayout>
+      <FlatTreeItem
+        value={value}
+        aria-level={1}
+        aria-setsize={3}
+        aria-posinset={1}
+        itemType="branch"
+        open={open}
+        onOpenChange={handleOpenChange}
+      >
+        <TreeItemLayout expandIcon={state === 'loading' ? <Spinner size="tiny" /> : undefined}>
+          {value.toString()}
+        </TreeItemLayout>
       </FlatTreeItem>
-      {openItems.has('people') &&
-        items?.map((person, index) => (
+      {open &&
+        items.map((person, index) => (
           <FlatTreeItem
             key={person.value}
             ref={index === 0 ? firstItemRef : null}
-            parentValue="people"
+            parentValue={value}
             value={person.value}
             aria-level={2}
             aria-setsize={items.length}
@@ -116,151 +132,6 @@ const PeopleSubtree: React.FC<SubtreeProps> = ({ openItems, setAriaMessage }) =>
         ))}
     </>
   );
-};
-
-const PlanetsSubtree: React.FC<SubtreeProps> = ({ openItems, setAriaMessage }) => {
-  const { isLoaded, isLoading, query } = useQuery<Entity[]>([]);
-  const [items, setItems] = React.useState<Entity[]>([]);
-  const firstItemRef = React.useRef<HTMLDivElement>(null);
-
-  const handleDataFetch = React.useCallback(
-    async (type: string) => {
-      query(
-        async () => {
-          const json = await mockFetch(type);
-          return json.results.map<Entity>(entity => ({
-            value: `${type}/${entity.name}`,
-            name: entity.name,
-            itemType: 'leaf',
-          }));
-        },
-        data => {
-          setItems(data);
-          setAriaMessage(`planets items loaded`);
-          firstItemRef.current?.focus();
-        },
-      );
-    },
-    [query, setAriaMessage],
-  );
-
-  React.useEffect(() => {
-    if (openItems.has('planets') && !isLoaded && !isLoading) {
-      setAriaMessage(`loading planets items...`);
-      handleDataFetch('planets');
-    }
-  }, [handleDataFetch, isLoaded, isLoading, openItems, setAriaMessage]);
-
-  return (
-    <>
-      <FlatTreeItem value="planets" aria-level={1} aria-setsize={3} aria-posinset={2} itemType="branch">
-        <TreeItemLayout expandIcon={isLoading ? <Spinner size="tiny" /> : undefined}>Planets</TreeItemLayout>
-      </FlatTreeItem>
-      {openItems.has('planets') &&
-        items.map((planet, index) => (
-          <FlatTreeItem
-            key={planet.value}
-            ref={index === 0 ? firstItemRef : null}
-            parentValue="planets"
-            value={planet.value}
-            aria-level={2}
-            aria-setsize={items.length}
-            aria-posinset={index + 1}
-            itemType="leaf"
-          >
-            <TreeItemLayout>{planet.name}</TreeItemLayout>
-          </FlatTreeItem>
-        ))}
-    </>
-  );
-};
-
-const StarshipsSubtree: React.FC<SubtreeProps> = ({ openItems, setAriaMessage }) => {
-  const { isLoaded, isLoading, query } = useQuery<Entity[]>([]);
-  const [items, setItems] = React.useState<Entity[]>([]);
-  const firstItemRef = React.useRef<HTMLDivElement>(null);
-
-  const handleDataFetch = React.useCallback(
-    async (type: string) => {
-      query(
-        async () => {
-          const json = await mockFetch(type);
-          return json.results.map<Entity>(entity => ({
-            value: `${type}/${entity.name}`,
-            name: entity.name,
-            itemType: 'leaf',
-          }));
-        },
-        data => {
-          setItems(data);
-          setAriaMessage(`starships items loaded`);
-          firstItemRef.current?.focus();
-        },
-      );
-    },
-    [query, setAriaMessage],
-  );
-
-  React.useEffect(() => {
-    if (openItems.has('starships') && !isLoaded && !isLoading) {
-      setAriaMessage(`loading starships items...`);
-      handleDataFetch('starships');
-    }
-  }, [handleDataFetch, isLoaded, isLoading, openItems, setAriaMessage]);
-
-  return (
-    <>
-      <FlatTreeItem value="starships" aria-level={1} aria-setsize={3} aria-posinset={3} itemType="branch">
-        <TreeItemLayout expandIcon={isLoading ? <Spinner size="tiny" /> : undefined}>Starships</TreeItemLayout>
-      </FlatTreeItem>
-      {openItems.has('starships') &&
-        items.map((starship, index) => (
-          <FlatTreeItem
-            key={starship.value}
-            ref={index === 0 ? firstItemRef : null}
-            parentValue="starships"
-            value={starship.value}
-            aria-level={2}
-            aria-setsize={items.length}
-            aria-posinset={index + 1}
-            itemType="leaf"
-          >
-            <TreeItemLayout>{starship.name}</TreeItemLayout>
-          </FlatTreeItem>
-        ))}
-    </>
-  );
-};
-
-/**
- * This function is just for the sake of the example,
- * a library for fetching data (like react-query) might be a better option
- */
-function useQuery<Value>(initialValue: Value) {
-  const [queryResult, setQueryResult] = React.useState({ value: initialValue, isLoading: false, isLoaded: false });
-  const query = (fn: () => Promise<Value> | Value, onDone?: (data: Value) => void) => {
-    setQueryResult(curr => ({ ...curr, isLoading: true }));
-    Promise.resolve(fn()).then(nextValue => {
-      setQueryResult({ value: nextValue, isLoaded: true, isLoading: false });
-      if (onDone) {
-        onDone(nextValue);
-      }
-    });
-  };
-  return { ...queryResult, query } as const;
-}
-
-const mockFetch = (type: string) => {
-  return new Promise<Result>(resolve => {
-    setTimeout(() => {
-      const mockData: Result = {
-        results: Array.from({ length: 10 }, (_, index) => ({
-          name: `${type.charAt(0).toUpperCase() + type.slice(1)} ${index + 1}`,
-        })),
-      };
-      resolve(mockData);
-    }, 1000);
-  });
 };
 
 LazyLoading.parameters = {
