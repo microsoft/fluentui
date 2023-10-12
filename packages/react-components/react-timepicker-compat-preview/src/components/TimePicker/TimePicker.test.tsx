b03a47260f977d472f34e3402dc89d3e3d56f760
Amber <yuanboxue@microsoft.com>
2023-10-12 18:14:36 +0200

    feat(timepicker-compat): initial implementation (#29477)
    * initial
    
    * remove comments
    
    * selectedOptions memo
    
    * create TimePickerOption
    
    * add todo
    
    * useStableDateAnchor hook
    
    * Update packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/useTimePicker.tsx
    
    Co-authored-by: ling1726 <lingfangao@hotmail.com>
    
    * cleanup after apply comments
    
    ---------
    
    Co-authored-by: ling1726 <lingfangao@hotmail.com>

--- packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/TimePicker.test.tsx
+++ packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/TimePicker.test.tsx
@@ -5,5 +5,17 @@ describe('TimePicker', () => {
   isConformant({
     Component: TimePicker,
     displayName: 'TimePicker',
+    primarySlot: 'input',
+    testOptions: {
+      'has-static-classnames': [
+        {
+          props: {
+            open: true,
+            // Portal messes with the classNames test, so rendering the listbox inline here
+            inlinePopup: true,
+          },
+        },
+      ],
+    },
   });
 });
