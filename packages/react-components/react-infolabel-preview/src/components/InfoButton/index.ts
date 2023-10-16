217365dc3cd3d282ba9e18fe81f00027a29abffa
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-10-16 23:15:14 +0000

    feat: Copy over files from react-infobutton to react-infolabel-preview.

---
+++ packages/react-components/react-infolabel-preview/src/components/InfoButton/index.ts
@@ -0,0 +1,5 @@
+export * from './InfoButton';
+export * from './InfoButton.types';
+export * from './renderInfoButton';
+export * from './useInfoButton';
+export * from './useInfoButtonStyles.styles';
