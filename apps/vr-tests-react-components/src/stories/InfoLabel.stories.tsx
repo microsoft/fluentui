98acf6fcb45728dffccbfbac701d8c79fd6fe2a5
Esteban Munoz Facusse <esteban.230@hotmail.com>
2023-10-19 22:45:59 +0000

    chore: Update vr-tests to only use react-infolabel.

--- apps/vr-tests-react-components/src/stories/InfoLabel.stories.tsx
+++ apps/vr-tests-react-components/src/stories/InfoLabel.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { Steps, StoryWright } from 'storywright';
-import { InfoLabel } from '@fluentui/react-infobutton';
+import { InfoLabel } from '@fluentui/react-infolabel';
 import { storiesOf } from '@storybook/react';
 import { TestWrapperDecoratorFixedWidth } from '../utilities/TestWrapperDecorator';
 
@@ -41,3 +41,27 @@ storiesOf('InfoLabel', module)
       includeRtl: true,
     },
   );
+
+// Opened states
+storiesOf('InfoLabel', module)
+  .addDecorator(TestWrapperDecoratorFixedWidth)
+  .addDecorator(story => (
+    <StoryWright steps={new Steps().click('.testButton').snapshot('rest', { cropTo: '.testWrapper' }).end()}>
+      {story()}
+    </StoryWright>
+  ))
+  .addStory('size: small', () => (
+    <InfoLabel size="small" infoButton={{ className: 'testButton' }} info="Test">
+      This is an info label
+    </InfoLabel>
+  ))
+  .addStory('size: medium', () => (
+    <InfoLabel size="medium" infoButton={{ className: 'testButton' }} info="Test">
+      This is an info label
+    </InfoLabel>
+  ))
+  .addStory('size: large', () => (
+    <InfoLabel size="large" infoButton={{ className: 'testButton' }} info="Test">
+      This is an info label
+    </InfoLabel>
+  ));
