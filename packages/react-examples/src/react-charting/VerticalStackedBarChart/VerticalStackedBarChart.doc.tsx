0d2f2f16c74fda65596966f0177c3af005c88504
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2023-10-20 07:54:11 +0000

    Fix broken examples

--- packages/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.doc.tsx
+++ packages/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.doc.tsx
@@ -5,8 +5,8 @@ import { IDocPageProps } from '@fluentui/react/lib/common/DocPage.types';
 import { VerticalStackedBarChartBasicExample } from './VerticalStackedBarChart.Basic.Example';
 import { VerticalStackedBarChartStyledExample } from './VerticalStackedBarChart.Styled.Example';
 import { VerticalStackedBarChartCalloutExample } from './VerticalStackedBarChart.Callout.Example';
-import { VerticalStackedBarChartTooltipExample2 } from './VerticalStackedBarChart.AxisTooltip.Example2';
-//import { VerticalStackedBarChartCustomAccessibilityExample } from './VerticalStackedBarChart.CustomAccessibility.Example';
+import { VerticalStackedBarChartTooltipExample } from './VerticalStackedBarChart.AxisTooltip.Example';
+import { VerticalStackedBarChartCustomAccessibilityExample } from './VerticalStackedBarChart.CustomAccessibility.Example';
 
 const VerticalBarChartBasicExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.Basic.Example.tsx') as string;
@@ -14,10 +14,10 @@ const VerticalBarChartStyledExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.Styled.Example.tsx') as string;
 const VerticalBarChartCalloutExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.Callout.Example.tsx') as string;
-const VerticalBarChartTooltipExample2Code =
-  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.AxisTooltip.Example2') as string;
-//const VerticalBarChartCustomAccessibilityExampleCode =
-//  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.CustomAccessibility.Example') as string;
+const VerticalBarChartTooltipExampleCode =
+  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.AxisTooltip.Example.tsx') as string;
+const VerticalBarChartCustomAccessibilityExampleCode =
+  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/VerticalStackedBarChart.CustomAccessibility.Example.tsx') as string;
 
 export const VerticalStackedBarChartPageProps: IDocPageProps = {
   title: 'VerticalStackedBarChart',
@@ -42,14 +42,14 @@ export const VerticalStackedBarChartPageProps: IDocPageProps = {
     },
     {
       title: 'VerticalStackedBarChart tooltip',
-      code: VerticalBarChartTooltipExample2Code,
-      view: <VerticalStackedBarChartTooltipExample2 />,
+      code: VerticalBarChartTooltipExampleCode,
+      view: <VerticalStackedBarChartTooltipExample />,
     },
-    /* {
+    {
       title: 'VerticalStackedBarChart custom accessibility',
       code: VerticalBarChartCustomAccessibilityExampleCode,
       view: <VerticalStackedBarChartCustomAccessibilityExample />,
-    }, */
+    },
   ],
   overview: require<string>('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/docs/VerticalStackedBarChartOverview.md'),
   bestPractices: require<string>('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/VerticalStackedBarChart/docs/VerticalStackedBarChartBestPractices.md'),
