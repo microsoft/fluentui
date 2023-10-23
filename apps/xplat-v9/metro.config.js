9560192cbcfe32a4bbdd88e862c4fff54706b88f
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-10-23 19:02:58 +0200

    dedupe and fix bundling

--- apps/xplat-v9/metro.config.js
+++ apps/xplat-v9/metro.config.js
@@ -1,16 +1,25 @@
-const { makeMetroConfig, exclusionList } = require('@rnx-kit/metro-config');
+const { exclusionList, makeMetroConfig, resolveUniqueModule } = require('@rnx-kit/metro-config');
 const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+
+const [normalizeColorsPath, normalizeColorsPattern] = resolveUniqueModule(
+  '@react-native/normalize-colors',
+  require.resolve('react-native'),
+);
+
+const [reactIsPath, reactIsPattern] = resolveUniqueModule('react-is', require.resolve('react-test-renderer'));
 
 module.exports = makeMetroConfig({
   resolver: {
     resolveRequest: MetroSymlinksResolver(),
+    extraNodeModules: {
+      '@react-native/normalize-colors': normalizeColorsPath,
+      'react-is': reactIsPath,
+    },
     blockList: exclusionList([
       /.*\/__fixtures__\/.*/,
       /,*\/fake_node_modules\/.*/,
-      //    /.*\/babel-preset-global-context\/.*/,
-      //    /.*\/fluentui\/node_modules\/@babel\/runtime\/.*/,
-      //     /.*\/deprecated-react-native-prop-types\/node_modules\/@react-native\/normalize-colors\/.*/,
-      //    /.*\/node_modules\/prop-types\/node_modules\/react-is\/.*/,
+      normalizeColorsPattern,
+      reactIsPattern,
     ]),
   },
   transformer: {
