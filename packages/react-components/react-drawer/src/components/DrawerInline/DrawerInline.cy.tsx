dcf4daae946b6ad605b02eea3eaabdd4c0ae8436
Marcos Moura <marcosvmmoura@gmail.com>
2023-10-24 20:53:29 +0200

    fix: update cypress tests

--- packages/react-components/react-drawer/src/components/DrawerInline/DrawerInline.cy.tsx
+++ packages/react-components/react-drawer/src/components/DrawerInline/DrawerInline.cy.tsx
@@ -17,22 +17,22 @@ describe('DrawerInline', () => {
     it('should not render any border when separator is false', () => {
       mountFluent(<DrawerInline id="drawer" position="start" open />);
 
-      cy.get('#drawer').should('not.have.css', `border-right-width`, '1px');
-      cy.get('#drawer').should('not.have.css', `border-left-width`, '1px');
+      cy.get('#drawer').should('have.css', `border-right-color`, 'transparent');
+      cy.get('#drawer').should('have.css', `border-left-color`, 'transparent');
     });
 
     it('should render correct border when when position is `start`', () => {
       mountFluent(<DrawerInline id="drawer" position="start" separator open />);
 
-      cy.get('#drawer').should('not.have.css', `border-left-width`, '1px');
-      cy.get('#drawer').should('have.css', `border-right-width`, '1px');
+      cy.get('#drawer').should('have.css', `border-left-color`, 'transparent');
+      cy.get('#drawer').should('not.have.css', `border-right-color`, '1px');
     });
 
     it('should render correct border when when position is `end`', () => {
       mountFluent(<DrawerInline id="drawer" position="end" separator open />);
 
-      cy.get('#drawer').should('not.have.css', `border-right-width`, '1px');
-      cy.get('#drawer').should('have.css', `border-left-width`, '1px');
+      cy.get('#drawer').should('have.css', `border-right-color`, 'transparent');
+      cy.get('#drawer').should('not.have.css', `border-left-color`, 'transparent');
     });
   });
 });
