92906f55ce5c4e82654962f712d709757e263c15
Lin Jiang (SHE/HER) <jianlin@microsoft.com>
2023-10-24 11:18:23 +0800

    add v9 shim for v8 checkbox

---
+++ packages/react-components/react-migration-v8-v9/src/stories/CheckboxShim/index.stories.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+
+import descriptionMd from './Description.md';
+
+import { Checkbox } from '@fluentui/react';
+import {
+  FluentProvider,
+  Checkbox as CheckboxV9,
+  webLightTheme,
+  makeStyles,
+  shorthands,
+} from '@fluentui/react-components';
+import { CheckboxShim } from '../../components/Checkbox/index';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto auto',
+    gridTemplateRows: '1fr',
+    width: 'fit-content',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyItems: 'center',
+    columnGap: '10px',
+    rowGap: '10px',
+  },
+  componentName: {
+    justifySelf: 'end',
+    ...shorthands.margin(0, '10px', 0, 0),
+  },
+});
+
+export const Default = () => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.root}>
+      <h3>v8</h3>
+      <h3>shim</h3>
+      <h3>v9</h3>
+      <Checkbox label="checkbox" required={true} />
+      <FluentProvider theme={webLightTheme}>
+        <CheckboxShim label="checkbox" required={true} />
+      </FluentProvider>
+      <FluentProvider theme={webLightTheme}>
+        <CheckboxV9 label="checkbox" required={true} />
+      </FluentProvider>
+    </div>
+  );
+};
+
+export default {
+  title: 'Migration Shims/V8/CheckboxShim',
+  component: CheckboxShim,
+  parameters: {
+    docs: {
+      description: {
+        component: [descriptionMd].join('\n'),
+      },
+    },
+  },
+};
