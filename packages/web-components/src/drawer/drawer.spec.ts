567256104a9bebde6d3c6db01bae6707fd626ea0
brianchristopherbrady <brian.christopher.brady@gmail.com>
2023-10-25 14:13:11 -0700

    drawer: fixes tests

--- packages/web-components/src/drawer/drawer.spec.ts
+++ packages/web-components/src/drawer/drawer.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 import type { Locator, Page } from '@playwright/test';
 import { keyEscape } from '@microsoft/fast-web-utilities';
+import { Updates } from '@microsoft/fast-element';
+
 import { fixtureURL } from '../helpers.tests.js';
 import type { Drawer } from './drawer.js';
 
@@ -8,24 +10,17 @@ test.describe('Drawer', () => {
   let page: Page;
   let element: Locator;
   let root: Locator;
-  let overlay: Locator;
 
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage();
     root = page.locator('#root');
     element = page.locator('fluent-drawer');
-    overlay = element.locator('.overlay');
 
     await page.goto(fixtureURL('components-drawer--drawer'));
   });
 
   test.afterAll(async () => {
     await page.close();
-  });
-
-  test('should render without crashing', async () => {
-    await page.waitForSelector('fluent-drawer');
-    await expect(element).toBeVisible();
   });
 
   // eslint-disable-next-line playwright/no-focused-test
@@ -51,6 +46,12 @@ test.describe('Drawer', () => {
     });
     await expect(element).toHaveAttribute('size', 'large');
     await expect(element).toHaveJSProperty('size', 'large');
+
+    await element.evaluate((node: Drawer) => {
+      node.size = 'full';
+    });
+    await expect(element).toHaveAttribute('size', 'full');
+    await expect(element).toHaveJSProperty('size', 'full');
   });
 
   test('should reflect position attribute', async () => {
@@ -85,14 +86,14 @@ test.describe('Drawer', () => {
       node.modalType = 'non-modal';
     });
 
-    await expect(element).not.toHaveAttribute('modal-type', 'non-modal');
+    await expect(element).toHaveAttribute('modal-type', 'non-modal');
     await expect(element).toHaveJSProperty('modalType', 'non-modal');
 
     await element.evaluate((node: Drawer) => {
       node.modalType = 'alert';
     });
 
-    await expect(element).not.toHaveAttribute('modal-type', 'alert');
+    await expect(element).toHaveAttribute('modal-type', 'alert');
     await expect(element).toHaveJSProperty('modalType', 'alert');
   });
 
@@ -114,7 +115,7 @@ test.describe('Drawer', () => {
     await expect(element).toHaveJSProperty('open', false);
   });
 
-  test('should reflect ariaLabel attribute', async () => {
+  test('should reflect aria-label attribute', async () => {
     await root.evaluate(node => {
       node.innerHTML = /* html */ `
               <fluent-drawer aria-label="abc"></fluent-drawer>
@@ -166,39 +167,6 @@ test.describe('Drawer', () => {
 
     await expect(element).toHaveAttribute('aria-describedby', 'def');
     await expect(element).toHaveJSProperty('ariaDescribedby', 'def');
-  });
-
-  test('overlay should be hidden when drawer is open and the modal attribute is NOT present', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer open></fluent-drawer>
-        `;
-    });
-
-    await expect(overlay).toHaveAttribute('aria-hidden', 'true');
-    await expect(overlay).toHaveAttribute('hidden', '');
-  });
-
-  test('overlay should NOT be hidden when modal attribute is present and open', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer modal open></fluent-drawer>
-        `;
-    });
-
-    await expect(overlay).toHaveAttribute('aria-hidden', 'false');
-    await expect(overlay).not.toHaveAttribute('hidden', '');
-  });
-
-  test('overlay should be hidden when not open and modal attribute is present', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-              <fluent-drawer modal></fluent-drawer>
-          `;
-    });
-
-    await expect(overlay).toHaveAttribute('aria-hidden', 'true');
-    await expect(overlay).toHaveAttribute('hidden', '');
   });
 
   test('show and hide methods should toggle open state of drawer', async () => {
@@ -273,7 +241,7 @@ test.describe('Drawer', () => {
       element.evaluate(
         node =>
           new Promise(resolve => {
-            node.addEventListener('openChanged', () => resolve(true));
+            node.addEventListener('onOpenChange', () => resolve(true));
           }),
       ),
       await element.evaluate((node: Drawer) => {
@@ -284,163 +252,10 @@ test.describe('Drawer', () => {
     expect(wasOpened).toBe(true);
   });
 
-  test('openChanged event should include drawers position in detail', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer></fluent-drawer>
-        `;
-    });
-
-    const [right] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.position));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(right).toEqual('right');
-
-    const [left] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.position));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.position = 'start';
-        node.show();
-      }),
-    ]);
-    expect(left).toEqual('start');
-  });
-
-  test('openChanged event should include drawers open state in detail', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer></fluent-drawer>
-        `;
-    });
-
-    const [isOpen] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.open));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(isOpen).toEqual(true);
-
-    const [isOpen2] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.open));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.close();
-      }),
-    ]);
-    expect(isOpen2).not.toEqual(true);
-  });
-
-  test('openChanged event should include drawers width in detail', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer></fluent-drawer>
-        `;
-    });
-
-    const [medium] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.size));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(medium).toEqual(592);
-
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer size="small"></fluent-drawer>
-        `;
-    });
-
-    const [small] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.size));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(small).toEqual(320);
-
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer size="large"></fluent-drawer>
-        `;
-    });
-
-    const [large] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.size));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(large).toEqual(940);
-
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-            <fluent-drawer style="width: 222px;"></fluent-drawer>
-        `;
-    });
-
-    const [custom] = await Promise.all([
-      element.evaluate(
-        node =>
-          new Promise(resolve => {
-            node.addEventListener('openChanged', (event: Event) => resolve((event as CustomEvent).detail.size));
-          }),
-      ),
-      await element.evaluate((node: Drawer) => {
-        node.show();
-      }),
-    ]);
-
-    expect(custom).toEqual(222);
-  });
-
   test('drawer should close on escape keypress', async () => {
     await root.evaluate(node => {
       node.innerHTML = /* html */ `
-            <fluent-drawer open></fluent-drawer>
+            <fluent-drawer open tabindex="0"></fluent-drawer>
         `;
     });
 
