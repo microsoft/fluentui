8712d19dfacbab2f091ccfe90d1e97c5617c7c5b
BrdyBrn <brian.christopher.brady@gmail.com>
2023-10-25 11:43:59 -0700

    Web Component Dialog (#28569)
    * dialog init
    
    * dialog: updates stories, styles, and docs
    
    * dialog: updates styles
    
    * dialog: removes dead code, cleans up
    
    * dialog: updates styles
    
    * updates docs
    
    * deletes dead code
    
    * dialog: addresses PR feedback
    
    * reverts dead file
    
    * dialog: updates styles per feedback
    
    * drawer: updates template, styles
    
    * dialog: removes attr changed callback
    
    * dialog: updates slot names for react parity, updates docs
    
    * yarn change
    
    * dialog: addresses feedback
    
    * dialog: updates README
    
    * dialog: fixes responsive styles
    
    * dialog: removes dead styles
    
    * dialog: updates readme docs
    
    * dialog: reworks dialog to use navtive html dialog element
    
    * dialog: fixes jsdoc
    
    * dialog: updates slot names for closer parity with fv9, updates stories and docs
    
    * dialog: updates README
    
    * dialog: updates template class names
    
    * dialog: updates backdrop to use fluent token
    
    * dialog: adds two column layout story
    
    * dialog: adds additional stories
    
    * dialog: adds exports to index and package json
    
    * dialog: fixes style token imports
    
    * dialog: cleans up open state
    
    * dialog: updates template slot names
    
    * dialog: updates property name
    
    * dialog: removes change-focus
    
    * dialog: syncs open attr
    
    * dialog: sync open attribute
    
    * dialog: updates storybook docs
    
    * dialog: updates readme docs

---
+++ packages/web-components/src/dialog/index.ts
@@ -0,0 +1,4 @@
+export * from './dialog.js';
+export { definition as DialogDefinition } from './dialog.definition.js';
+export { template as DialogTemplate } from './dialog.template.js';
+export { styles as DialogStyles } from './dialog.styles.js';
