4611e31dd38799b5e7bd2faa3ff735398e176b03
Marcos Moura <marcosvmmoura@gmail.com>
2023-10-26 17:32:42 +0200

    feat(react-drawer): use useMotionClassNames to create drawer motion styles (#29662)

--- packages/react-components/react-drawer/src/components/OverlayDrawer/useOverlayDrawerStyles.styles.ts
+++ packages/react-components/react-drawer/src/components/OverlayDrawer/useOverlayDrawerStyles.styles.ts
@@ -12,6 +12,7 @@ import {
   useDrawerBaseClassNames,
   useDrawerDurationStyles,
 } from '../../shared/useDrawerBaseStyles.styles';
+import { useMotionClassNames } from '@fluentui/react-motion-preview';
 
 export const overlayDrawerClassNames: SlotClassNames<OverlayDrawerSurfaceSlots> = {
   root: 'fui-OverlayDrawer',
@@ -27,10 +28,6 @@ const useDrawerResetStyles = makeResetStyles({
   position: 'fixed',
   top: 0,
   bottom: 0,
-  opacity: 0,
-  boxShadow: `0px ${tokens.colorTransparentBackground}`,
-  transitionProperty: 'transform, box-shadow, opacity',
-  willChange: 'transform, box-shadow, opacity',
 });
 
 const useDrawerRootStyles = makeStyles({
@@ -41,9 +38,16 @@ const useDrawerRootStyles = makeStyles({
   end: {
     transform: `translate3D(calc(var(${drawerCSSVars.drawerSizeVar}) * 1), 0, 0)`,
   },
+});
 
-  /* Visible */
-  visible: {
+const useDrawerMotionStyles = makeStyles({
+  default: {
+    opacity: 0,
+    boxShadow: `0px ${tokens.colorTransparentBackground}`,
+    transitionProperty: 'transform, box-shadow, opacity',
+    willChange: 'transform, box-shadow, opacity',
+  },
+  enter: {
     opacity: 1,
     transform: 'translate3D(0, 0, 0)',
     boxShadow: tokens.shadow64,
@@ -53,19 +57,15 @@ const useDrawerRootStyles = makeStyles({
 /**
  * Styles for the backdrop slot
  */
-const useBackdropStyles = makeStyles({
-  backdrop: {
+const useBackdropMotionStyles = makeStyles({
+  default: {
     opacity: 0,
     transitionProperty: 'opacity',
     transitionTimingFunction: tokens.curveEasyEase,
     willChange: 'opacity',
-
-    '@media screen and (prefers-reduced-motion: reduce)': {
-      transitionDuration: '0.001ms',
-    },
   },
 
-  visible: {
+  enter: {
     opacity: 1,
   },
 });
@@ -77,8 +77,10 @@ export const useOverlayDrawerStyles_unstable = (state: OverlayDrawerState): Over
   const baseClassNames = useDrawerBaseClassNames(state);
   const resetStyles = useDrawerResetStyles();
   const rootStyles = useDrawerRootStyles();
-  const backdropStyles = useBackdropStyles();
   const durationStyles = useDrawerDurationStyles();
+
+  const drawerMotionClassNames = useMotionClassNames(state.motion, useDrawerMotionStyles());
+  const backdropMotionClassNames = useMotionClassNames(state.motion, useBackdropMotionStyles());
 
   const backdrop = state.root.backdrop as React.HTMLAttributes<HTMLDivElement> | undefined;
 
@@ -87,16 +89,15 @@ export const useOverlayDrawerStyles_unstable = (state: OverlayDrawerState): Over
     baseClassNames,
     resetStyles,
     rootStyles[state.position],
-    state.motion.active && rootStyles.visible,
+    drawerMotionClassNames,
     state.root.className,
   );
 
   if (backdrop) {
     backdrop.className = mergeClasses(
       overlayDrawerClassNames.backdrop,
-      backdropStyles.backdrop,
+      backdropMotionClassNames,
       durationStyles[state.size],
-      state.backdropMotion.active && backdropStyles.visible,
       backdrop.className,
     );
   }
