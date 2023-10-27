394b2bdad4b290e39967f376a127562db5c92392
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-10-27 18:26:12 +0200

    remove CustomValidation story because it makes more sense for later

--- packages/react-components/react-timepicker-compat-preview/stories/TimePicker/index.stories.tsx
+++ packages/react-components/react-timepicker-compat-preview/stories/TimePicker/index.stories.tsx
@@ -7,7 +7,6 @@ export { Default } from './TimePickerDefault.stories';
 export { Controlled } from './TimePickerControlled.stories';
 export { CustomTimeString } from './TimePickerCustomTimeString.stories';
 export { FreeformWithErrorHandling } from './TimePickerFreeform.stories';
-export { CustomValidation } from './TimePickerCustomValidation.stories';
 export { TimePickerWithDatePicker } from './TimePickerWithDatePicker.stories';
 
 export default {
