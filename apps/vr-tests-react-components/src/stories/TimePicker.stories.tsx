9182ed66677295b58fff289a202a8890f69d09e6
YuanboXue-Amber <yuanboxue@microsoft.com>
2023-11-02 13:52:37 +0100

    vr tests

---
+++ apps/vr-tests-react-components/src/stories/TimePicker.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Steps, StoryWright } from 'storywright';
+import { storiesOf } from '@storybook/react';
+import { TimePicker } from '@fluentui/react-timepicker-compat-preview';
+import { TestWrapperDecoratorFixedWidth } from '../utilities/TestWrapperDecorator';
+
+storiesOf('TimePicker compat', module)
+  .addDecorator(TestWrapperDecoratorFixedWidth)
+  .addDecorator(story => (
+    <StoryWright steps={new Steps().snapshot('default', { cropTo: '.testWrapper' }).end()}>{story()}</StoryWright>
+  ))
+  .addStory('Default', () => <TimePicker open startHour={11} endHour={13} />)
+  .addStory('With 12-hour format', () => <TimePicker open startHour={11} endHour={13} hourCycle="h11" />)
+  .addStory('Show seconds', () => <TimePicker open startHour={11} endHour={13} showSeconds />);
