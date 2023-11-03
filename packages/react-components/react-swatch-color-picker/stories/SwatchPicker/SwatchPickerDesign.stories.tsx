3022229efbfac43842c3ec44ac4906af4c37baaf
Valentyna <vkozlova@microsoft.com>
2023-11-03 19:05:51 +0100

    added new variant to the design

--- packages/react-components/react-swatch-color-picker/stories/SwatchPicker/SwatchPickerDesign.stories.tsx
+++ packages/react-components/react-swatch-color-picker/stories/SwatchPicker/SwatchPickerDesign.stories.tsx
@@ -1,62 +1,45 @@
 import * as React from 'react';
 import { SwatchPicker, SwatchPickerProps, SwatchColorPikerCell } from '@fluentui/react-swatch-color-picker';
-import { makeStyles, shorthands } from '@fluentui/react-components';
+import { makeStyles, shorthands, mergeClasses } from '@fluentui/react-components';
+import { background } from '@storybook/theming';
 
 const customStyles = makeStyles({
   root: {
     ...shorthands.borderRadius('8px'),
     boxShadow: 'rgb(46 46 46 / 43%) 0px 1px 2px 0px',
     ...shorthands.transition('all', '.5s', 'ease-in-out'),
-    // transition: 'all .3s ease-in-out',
     '&:hover': {
       transform: 'scale(1.3)',
     },
   },
-  // selected: {
-  //   ...shorthands.border('4px', 'solid', 'yellow'),
-  // },
+  selected: {
+    boxShadow: `inset 0px 0px 0px 2px #00ffff, inset 0px 0px 0px 6px black`,
+    ...shorthands.border('none'),
+    '&:hover': {
+      transform: 'scale(1.3)',
+      boxShadow: `inset 0px 0px 0px 2px #00ffff, inset 0px 0px 0px 5px black`,
+    },
+    '&:hover&:active': {
+      transform: 'scale(1.3)',
+      boxShadow: `inset 0px 0px 0px 3px #00ffff, inset 0px 0px 0px 6px black`,
+    },
+  },
   picker: {
-    gridTemplateColumns: `repeat(3, 30px)`,
+    gridTemplateColumns: `repeat(4, 30px)`,
     columnGap: '4px',
     rowGap: '4px',
   },
-  customHoverStyles: {},
+  swatch: {
+    '&:hover': {
+      transform: 'scale(1.1)',
+      boxShadow: `inset 0px 0px 0px 2px #00ffff, inset 0px 0px 0px 4px black`,
+    },
+    '&:hover&:active': {
+      transform: 'scale(1.2)',
+      boxShadow: `inset 0px 0px 0px 2px #00ffff, inset 0px 0px 0px 4px black`,
+    },
+  },
 });
-
-const colors = [
-  {
-    id: 0,
-    swatch: 'red',
-  },
-  {
-    id: 1,
-    swatch: 'rgb(189, 255, 104)',
-  },
-  {
-    id: 2,
-    swatch: 'rgba(189, 255, 104,.4)',
-  },
-  {
-    id: 3,
-    swatch: '#f09',
-  },
-  {
-    id: 4,
-    swatch: 'yellow',
-  },
-  {
-    id: 5,
-    swatch: 'cyan',
-  },
-  {
-    id: 6,
-    swatch: 'purple',
-  },
-  {
-    id: 7,
-    swatch: 'magenta',
-  },
-];
 
 const colorsLarge = [
   {
@@ -126,25 +109,11 @@ const colorsLarge = [
 ];
 
 export const DesignStories = (props: Partial<SwatchPickerProps>) => {
-  const [rowColor, setRowColor] = React.useState('red');
   const [gridColor, setGridColor] = React.useState('');
-  const [rowCustomColor, setRowCustomColor] = React.useState('red');
   const styles = customStyles();
+
   return (
     <>
-      <h2>row</h2>
-      <SwatchPicker
-        layout="row"
-        value={rowColor}
-        onChange={(_, data) => setRowColor(data.value)}
-        aria-labelledby="colors"
-      >
-        {colors.map(item => (
-          <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} swatch={item.swatch} />
-        ))}
-      </SwatchPicker>
-      Background can be changed
-      <div style={{ backgroundColor: rowColor, width: 50, height: 50 }} />
       <h2>Grid with a lots of colors</h2>
       <SwatchPicker
         layout="grid"
@@ -157,28 +126,60 @@ export const DesignStories = (props: Partial<SwatchPickerProps>) => {
           <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} swatch={item.swatch} />
         ))}
       </SwatchPicker>
-      Background can be changed
-      <div style={{ backgroundColor: gridColor, width: 50, height: 50 }} />
-      <h2>custom cell</h2>
+      <h2>New</h2>
       <SwatchPicker
-        layout="row"
-        value={rowCustomColor}
-        onChange={(_, data) => setRowCustomColor(data.value)}
+        layout="grid"
+        value={gridColor}
+        onChange={(_, data) => setGridColor(data.value)}
         aria-labelledby="colors"
+        className={styles.picker}
       >
-        {colors.map(item => (
+        {colorsLarge.map(item => (
           <SwatchColorPikerCell
-            id={item.id}
+            className={mergeClasses(styles.swatch, gridColor === item.swatch ? styles.selected : '')}
             key={item.id}
+            id={item.id}
             name="color"
             value={item.swatch}
             swatch={item.swatch}
-            className={styles.root}
           />
         ))}
       </SwatchPicker>
+      <h2>Dark Grid with a lots of colors</h2>
+      <div style={{ backgroundColor: 'black', padding: 40 }}>
+        <SwatchPicker
+          layout="grid"
+          value={gridColor}
+          onChange={(_, data) => setGridColor(data.value)}
+          aria-labelledby="colors"
+          style={{ gridTemplateColumns: `repeat(4, 30px)` }}
+        >
+          {colorsLarge.map(item => (
+            <SwatchColorPikerCell key={item.id} id={item.id} name="color" value={item.swatch} swatch={item.swatch} />
+          ))}
+        </SwatchPicker>
+        <h2>New</h2>
+        <SwatchPicker
+          layout="grid"
+          value={gridColor}
+          onChange={(_, data) => setGridColor(data.value)}
+          aria-labelledby="colors"
+          className={styles.picker}
+        >
+          {colorsLarge.map(item => (
+            <SwatchColorPikerCell
+              className={mergeClasses(styles.swatch, gridColor === item.swatch ? styles.selected : '')}
+              key={item.id}
+              id={item.id}
+              name="color"
+              value={item.swatch}
+              swatch={item.swatch}
+            />
+          ))}
+        </SwatchPicker>
+      </div>
       Background can be changed
-      <div style={{ backgroundColor: rowCustomColor, width: 50, height: 50 }} />
+      <div style={{ backgroundColor: gridColor, width: 50, height: 50 }} />
     </>
   );
 };
