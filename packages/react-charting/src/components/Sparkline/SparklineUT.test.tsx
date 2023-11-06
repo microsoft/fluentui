df0325275e4c916f80c7394bb4d20cadd34d74b8
srmukher <srmukher@microsoft.com>
2023-11-06 18:11:52 +0530

    Adding change file

--- packages/react-charting/src/components/Sparkline/SparklineUT.test.tsx
+++ packages/react-charting/src/components/Sparkline/SparklineUT.test.tsx
@@ -1,16 +1,8 @@
 /* eslint-disable import/no-absolute-path */
-// import { SparklineBase } from './Sparkline.base';
-
-// import { updateFile } from './script'
-// updateFile();
 
 describe('unit test', () => {
-  // beforeAll(() => updateFile());
   test('UT 1', () => {
     const rewire = require('rewire');
-    // const ChartRewired = rewire(
-    //   'D:/fluentui/packages/react-charting/lib-commonjs/components/Sparkline/Sparkline.base',
-    // );
     const ChartRewired = rewire('D:/fluentui/packages/react-charting/src/components/Sparkline/SparklineUpdated.js');
     const result = ChartRewired.__get__('SparklineBase.prototype._test')();
     expect(result).toEqual(100);
--- packages/react-charting/src/components/Sparkline/script.ts
+++ packages/react-charting/src/components/Sparkline/script.ts
@@ -22,23 +22,3 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     );
   }
 });
-// export const updateFile = () => {
-//   const filePath = 'D:/fluentui/packages/react-charting/lib-commonjs/components/Sparkline/Sparkline.base.js'
-//   // Read the file
-//   fs.readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(`Error reading file from disk: ${err}`);
-//     } else {
-//       // Replace the words
-//       let result = data.replace(/require\('d3-scale'\)/g, "import('d3-scale')");
-//       result = result.replace(/require\('d3-shape'\)/g, "import('d3-shape')");
-//       result = result.replace(/require\('d3-array'\)/g, "import('d3-array')");
-//       // Write the file back
-//       fs.writeFile('D:/fluentui/packages/react-charting/src/components/Sparkline/SparklineUpdated.js', result, 'utf8', (err) => {
-//         if (err) {
-//           console.error(`Error writing file to disk: ${err}`);
-//         }
-//       });
-//     }
-//   });
-// }
