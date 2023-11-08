580a64b330d8c33e2b5b5602477d05ad198a9d0f
srmukher <srmukher@microsoft.com>
2023-11-08 12:25:50 +0530

    Updating the timer

--- packages/react-charting/src/components/Sparkline/SparklineUT.test.tsx
+++ packages/react-charting/src/components/Sparkline/SparklineUT.test.tsx
@@ -4,17 +4,38 @@ import * as fs from 'fs';
 describe('unit test', () => {
   test('UT 1', () => {
     const rewire = require('rewire');
-    const ChartRewired = rewire('./SparklineBase.js');
-    const result = ChartRewired.__get__('SparklineBase.prototype._test')();
-    expect(result).toEqual(100);
+    fs.access('src/components/Sparkline/SparklineBase.js', fs.constants.F_OK, err => {
+      if (!err) {
+        const ChartRewired = rewire('./SparklineBase.js');
+        const result = ChartRewired.__get__('SparklineBase.prototype._test')();
+        expect(result).toEqual(100);
+      } else {
+        setTimeout(() => {
+          const ChartRewired = rewire('./SparklineBase.js');
+          const result = ChartRewired.__get__('SparklineBase.prototype._test')();
+          expect(result).toEqual(100);
+        }, 5000);
+      }
+    });
   });
 });
 
-// Delete the js file
-fs.unlink('./SparklineBase.js', err => {
-  if (err) {
-    console.error(`Error deleting file: ${err}`);
+// Check if the js file exists
+fs.access('./SparklineBase.js', fs.constants.F_OK, err => {
+  if (!err) {
+    // Delete the js file
+    fs.unlink('./SparklineBase.js', err => {
+      if (err) {
+        console.error(`Error deleting file: ${err}`);
+      }
+    });
   } else {
-    console.log('File deleted successfully!');
+    setTimeout(() => {
+      fs.unlink('./SparklineBase.js', err => {
+        if (err) {
+          console.error(`Error deleting file: ${err}`);
+        }
+      });
+    }, 5000);
   }
 });
