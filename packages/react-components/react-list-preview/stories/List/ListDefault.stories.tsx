6fd6224f2b56a1a517706b3a22000e25cb06e464
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-11-20 12:36:17 +0100

    fix unnecessary ul/li in exmample

--- packages/react-components/react-list-preview/stories/List/ListDefault.stories.tsx
+++ packages/react-components/react-list-preview/stories/List/ListDefault.stories.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { List, ListItem } from '@fluentui/react-list-preview';
 
 export const Default = () => (
-  <List as="ul">
-    <ListItem as="li">Asia</ListItem>
-    <ListItem as="li">Africa</ListItem>
-    <ListItem as="li">Europe</ListItem>
-    <ListItem as="li">North America</ListItem>
-    <ListItem as="li">South America</ListItem>
-    <ListItem as="li">Australia/Oceania</ListItem>
-    <ListItem as="li">Antarctica</ListItem>
+  <List>
+    <ListItem>Asia</ListItem>
+    <ListItem>Africa</ListItem>
+    <ListItem>Europe</ListItem>
+    <ListItem>North America</ListItem>
+    <ListItem>South America</ListItem>
+    <ListItem>Australia/Oceania</ListItem>
+    <ListItem>Antarctica</ListItem>
   </List>
 );
