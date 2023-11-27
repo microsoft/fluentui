62f3f7f40884978260885062e347ea64af1dd603
Tomi Olubeko <ololubek@microsoft.com>
2023-11-27 11:02:20 -0800

    Remove conformance tests from RatingItem

--- packages/react-components/react-rating-preview/src/components/RatingItem/RatingItem.test.tsx
+++ packages/react-components/react-rating-preview/src/components/RatingItem/RatingItem.test.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { isConformant } from '../../testing/isConformant';
 import { RatingItem } from './RatingItem';
 
 describe('RatingItem', () => {
-  isConformant({
-    Component: RatingItem,
-    displayName: 'RatingItem',
-  });
-
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
-
   it('renders a default state', () => {
     const result = render(<RatingItem>Default RatingItem</RatingItem>);
     expect(result.container).toMatchSnapshot();
