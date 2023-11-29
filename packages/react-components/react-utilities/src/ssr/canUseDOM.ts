ea89f19c6c52e55d72f243738c6ccf1f279fbb76
Sean Monahan <seanmonahan@microsoft.com>
2023-11-29 05:12:49 +0000

    update to use  from context

--- packages/react-components/react-utilities/src/ssr/canUseDOM.ts
+++ packages/react-components/react-utilities/src/ssr/canUseDOM.ts
@@ -3,10 +3,11 @@
  */
 export function canUseDOM(): boolean {
   return (
+    // eslint-disable-next-line no-restricted-globals
     typeof window !== 'undefined' &&
     !!(
-      window.document &&
-      // eslint-disable-next-line deprecation/deprecation
+      window.document && // eslint-disable-line no-restricted-globals
+      // eslint-disable-next-line deprecation/deprecation, no-restricted-globals
       window.document.createElement
     )
   );
