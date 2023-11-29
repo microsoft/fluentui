188cedd67a33fe276ea221721279b194bfefac8a
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-11-29 08:16:15 +0000

    feat: adds floating-ui-devtools support

---
+++ packages/react-components/react-positioning/src/utils/devtools.ts
@@ -0,0 +1,39 @@
+import { MiddlewareArguments } from '@floating-ui/dom';
+import { PositioningOptions } from '../types';
+import { FluentUI } from 'floating-ui-devtools';
+import { isHTMLElement } from '@fluentui/react-utilities';
+import { listScrollParents } from './listScrollParents';
+import { fromFloatingUIPlacement } from './fromFloatingUIPlacement';
+
+export const devtoolsCallback =
+  (options: PositioningOptions) =>
+  (middlewareState: MiddlewareArguments): FluentUI.MiddlewareData => {
+    const {
+      elements: { floating, reference },
+    } = middlewareState;
+    const scrollParentsSet = new Set<HTMLElement>();
+    if (isHTMLElement(reference)) {
+      listScrollParents(reference).forEach(scrollParent => scrollParentsSet.add(scrollParent));
+    }
+    listScrollParents(floating).forEach(scrollParent => scrollParentsSet.add(scrollParent));
+    const flipBoundaries: HTMLElement[] = Array.isArray(options.flipBoundary)
+      ? options.flipBoundary
+      : isHTMLElement(options.flipBoundary)
+      ? [options.flipBoundary]
+      : [];
+    const overflowBoundaries: HTMLElement[] = Array.isArray(options.overflowBoundary)
+      ? options.overflowBoundary
+      : isHTMLElement(options.overflowBoundary)
+      ? [options.overflowBoundary]
+      : [];
+    return {
+      type: 'FluentUIMiddleware',
+      middlewareState,
+      options,
+      initialPlacement: fromFloatingUIPlacement(middlewareState.initialPlacement),
+      placement: fromFloatingUIPlacement(middlewareState.placement),
+      flipBoundaries,
+      overflowBoundaries,
+      scrollParents: Array.from(scrollParentsSet),
+    };
+  };
