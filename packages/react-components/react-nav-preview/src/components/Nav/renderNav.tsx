72cb0d24dc78c39c2fb644a06285ec5bcb41e1e5
Mason Tejera <matejera@microsoft.com>
2023-12-01 14:51:13 -0800

    remove legacy patterns

--- packages/react-components/react-nav-preview/src/components/Nav/renderNav.tsx
+++ packages/react-components/react-nav-preview/src/components/Nav/renderNav.tsx
@@ -1,9 +1,6 @@
-/** @jsxRuntime classic */
-/** @jsx createElement */
-
-import { createElement } from '@fluentui/react-jsx-runtime';
 /** @jsxRuntime automatic */
 /** @jsxImportSource @fluentui/react-jsx-runtime */
+
 import { assertSlots } from '@fluentui/react-utilities';
 import type { NavState, NavSlots } from './Nav.types';
 import type { NavContextValues } from '../NavContext.types';
