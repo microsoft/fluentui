692e7a498491906665b7eadd23727ec1c3c75607
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2023-12-07 15:57:29 -0800

    Scaffolds Nav Context Provider (#29867)
    * sscaffolds context provider
    
    * Apply suggestions from code review
    
    Co-authored-by: Esteban Munoz Facusse <estebanmu@microsoft.com>
    Co-authored-by: Geoff Cox (Microsoft) <gcox@microsoft.com>
    
    * integrating feedback from PR
    
    * put react import back
    
    * add back conformance tests
    
    * update deps
    
    * remove nav link and nav link group stories
    
    * rename things to _unstable
    
    * address PR feedback
    
    * easy comments from Sean
    
    * trying to implement new event pattern
    
    * fix compilation error
    
    * fixing make reset styles
    
    * some comments from Ben
    
    * remove styles, add another example w/ default
    
    * move and rename onSelectData
    
    * blowing away contentReservedSpace
    
    * remove legacy patterns
    
    * addressing more feedback from ben
    
    * update dependencies
    
    * fix build breaks
    
    * fix ref
    
    * fix tests
    
    * opt out of ban context export
    
    * do the right thing
    
    * remove expprot
    
    * update api
    
    ---------
    
    Co-authored-by: Esteban Munoz Facusse <estebanmu@microsoft.com>
    Co-authored-by: Geoff Cox (Microsoft) <gcox@microsoft.com>

--- packages/react-components/react-nav-preview/src/components/Nav/Nav.types.ts
+++ packages/react-components/react-nav-preview/src/components/Nav/Nav.types.ts
@@ -1,17 +1,62 @@
+import * as React from 'react';
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
+import type { NavContextValue } from '../NavContext.types';
+import type { NavGroupValue } from '../NavGroup/NavGroup.types';
 
 export type NavSlots = {
-  root: Slot<'div'>;
+  root: NonNullable<Slot<'div'>>;
 };
 
 /**
  * Nav Props
  */
-export type NavProps = ComponentProps<NavSlots> & {};
+export type NavProps = ComponentProps<NavSlots> & {
+  /**
+   * Nav size may change between unselected and selected states.
+   * The default scenario is a selected NavGroup has bold text.
+   *
+   * When true, this property requests navGroups be the same size whether unselected or selected.
+   * @default true
+   */
+  reserveSelectedNavGroupSpace?: boolean;
+
+  /**
+   * The value of the navGroup to be selected by default.
+   * Typically useful when the selectedValue is uncontrolled.
+   *  Mutually exclusive with selectedValue.
+   */
+  defaultSelectedValue?: NavGroupValue;
+
+  /**
+   * Raised when a navGroup is selected.
+   */
+  onNavGroupSelect?: EventHandler<OnNavGroupSelectData>;
+
+  /**
+   * The value of the currently selected navGroup.
+   * Mutually exclusive with defaultSelectedValue.
+   */
+  selectedValue?: NavGroupValue;
+};
+
+export type OnNavGroupSelectData = EventData<'click', React.MouseEvent<HTMLButtonElement>> & {
+  /**
+   * The value of the selected navGroup.
+   */
+  value: NavGroupValue;
+};
 
 /**
  * State used in rendering Nav
  */
-export type NavState = ComponentState<NavSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from NavProps.
-// & Required<Pick<NavProps, 'propName'>>
+export type NavState = ComponentState<NavSlots> & NavContextValue;
+
+// Temporarily here until they go into @fluentui/react-utilities
+export type EventData<Type extends string, TEvent> =
+  | { type: undefined; event: React.SyntheticEvent | Event }
+  | { type: Type; event: TEvent };
+
+export type EventHandler<TData extends EventData<string, unknown>> = (
+  ev: React.SyntheticEvent | Event,
+  data: TData,
+) => void;
