6b195b736582b931e03f325a0d77e230c6b2b410
Lingfan Gao <lingfan.gao@microsoft.com>
2023-12-08 18:39:01 +0000

    revert

--- packages/react-components/react-combobox/stories/Combobox/ComboboxDefault.stories.tsx
+++ packages/react-components/react-combobox/stories/Combobox/ComboboxDefault.stories.tsx
@@ -17,13 +17,12 @@ export const Default = (props: Partial<ComboboxProps>) => {
   const comboId = useId('combo-default');
   const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
   const styles = useStyles();
-
   return (
     <div className={styles.root}>
       <label id={comboId}>Best pet</label>
       <Combobox aria-labelledby={comboId} placeholder="Select an animal" {...props}>
         {options.map(option => (
-          <Option style={{ minWidth: 300 }} key={option} disabled={option === 'Ferret'}>
+          <Option key={option} disabled={option === 'Ferret'}>
             {option}
           </Option>
         ))}
