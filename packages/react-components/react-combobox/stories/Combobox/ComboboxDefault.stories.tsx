72d876c69854ae85db8796e0bd88033059455098
Lingfan Gao <lingfan.gao@microsoft.com>
2023-12-08 11:05:17 +0000

    fix: Allows matchTargetSize middleware to be overidden with inline style
    `matchTargetSize` middleware applies styling through an inline css var
    so that it respects user inline style overrides
    
    Fixes

--- packages/react-components/react-combobox/stories/Combobox/ComboboxDefault.stories.tsx
+++ packages/react-components/react-combobox/stories/Combobox/ComboboxDefault.stories.tsx
@@ -17,12 +17,13 @@ export const Default = (props: Partial<ComboboxProps>) => {
   const comboId = useId('combo-default');
   const options = ['Cat', 'Dog', 'Ferret', 'Fish', 'Hamster', 'Snake'];
   const styles = useStyles();
+
   return (
     <div className={styles.root}>
       <label id={comboId}>Best pet</label>
       <Combobox aria-labelledby={comboId} placeholder="Select an animal" {...props}>
         {options.map(option => (
-          <Option key={option} disabled={option === 'Ferret'}>
+          <Option style={{ minWidth: 300 }} key={option} disabled={option === 'Ferret'}>
             {option}
           </Option>
         ))}
