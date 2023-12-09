9149d53016be73d8378d488477aaf6de6dd33b04
Tomi Olubeko <66456876+tomi-msft@users.noreply.github.com>
2023-12-08 23:24:27 -0800

    React rating initial implementation (#29490)
    * Rating initial implementation
    
    * Rating initial implementation
    
    * version fix
    
    * yarn lock update
    
    * Add carat
    
    * update exports
    
    * change checked to defaultChecked in RatingItem
    
    * add empty onChange to silence warnings
    
    * api update
    
    * add comments from code review
    
    * api updates
    
    * update Rating styles and stories
    
    * Update indicator api to add filled and unfilled icons
    
    * add shapes
    
    * Update shape api to use iconFilled and iconOutline
    
    * Default the color of the rating to neutralForeground1
    
    * Add outlineIcon and support filled unselected icons
    
    * Rename icon slots in RatingItem
    
    * Fix label text centering
    
    * update api
    
    * Update tabster dependency version
    
    * Update ratingitem styling and stories
    
    * Clean up shape story
    
    * Add outlineStyle prop and update api
    
    * Remove conformance tests from RatingItem
    
    * add suggestion from code review
    
    * api update
    
    * package update
    
    * Dependency mismatch
    
    * Temporarily remove conformance tests
    
    * update snapshots
    
    * update snapshots
    
    * apply suggestions from code review
    
    ---------
    
    Co-authored-by: Ben Howell <behowell@microsoft.com>

---
+++ packages/react-components/react-rating-preview/src/components/RatingItem/renderRatingItem.tsx
@@ -0,0 +1,22 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource @fluentui/react-jsx-runtime */
+
+import { assertSlots } from '@fluentui/react-utilities';
+import type { RatingItemState, RatingItemSlots } from './RatingItem.types';
+
+/**
+ * Render the final JSX of RatingItem
+ */
+export const renderRatingItem_unstable = (state: RatingItemState) => {
+  assertSlots<RatingItemSlots>(state);
+
+  return (
+    <state.root>
+      {state.halfValueInput && <state.halfValueInput />}
+      {state.fullValueInput && <state.fullValueInput />}
+      {state.unselectedFilledIcon && <state.unselectedFilledIcon />}
+      {state.unselectedOutlineIcon && <state.unselectedOutlineIcon />}
+      {state.selectedIcon && <state.selectedIcon />}
+    </state.root>
+  );
+};
--- packages/react-components/react-rating-preview/stories/Rating/RatingDefault.stories.tsx
+++ packages/react-components/react-rating-preview/stories/Rating/RatingDefault.stories.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
 import { Rating, RatingProps } from '@fluentui/react-rating-preview';
 
-export const Default = (props: Partial<RatingProps>) => <Rating {...props} />;
+export const Default = (props: Partial<RatingProps>) => {
+  return <Rating {...props} />;
+};
