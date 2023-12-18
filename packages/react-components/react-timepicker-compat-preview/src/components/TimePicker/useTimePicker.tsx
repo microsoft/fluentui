a657b210377643bdab23b145b47f715e1d7effa9
Amber <yuanboxue@microsoft.com>
2023-12-18 13:31:41 +0100

    fix(TimePickerCompat): letter key navigation should be case insensitive (#30072)
    * fix
    
    * test
    
    * test
    
    * fix crud

--- packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/useTimePicker.tsx
+++ packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/useTimePicker.tsx
@@ -163,7 +163,7 @@ const useSelectTimeFromValue = (state: TimePickerState, callback: TimePickerProp
   React.useEffect(() => {
     if (freeform && value) {
       setActiveOption(prevActiveOption => {
-        if (prevActiveOption?.text?.indexOf(value) === 0) {
+        if (prevActiveOption?.text && prevActiveOption.text.toLowerCase().indexOf(value.toLowerCase()) === 0) {
           return prevActiveOption;
         }
         return undefined;
