1ecb61b8bd9c661cfb242fb75f0f1282e9238cc9
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2023-12-21 13:02:12 +0000

    chore(react-teaching-popover): stop using old slot API

--- packages/react-components/react-teaching-popover-preview/src/components/TeachingPopoverTitle/renderTeachingPopoverTitle.tsx
+++ packages/react-components/react-teaching-popover-preview/src/components/TeachingPopoverTitle/renderTeachingPopoverTitle.tsx
@@ -1,19 +1,19 @@
 /** @jsxRuntime automatic */
 /** @jsxImportSource @fluentui/react-jsx-runtime */
 import type { TeachingPopoverTitleState } from './TeachingPopoverTitle.types';
-import { getSlotsNext } from '@fluentui/react-utilities';
+import { assertSlots } from '@fluentui/react-utilities';
 import { TeachingPopoverTitleSlots } from './TeachingPopoverTitle.types';
 
 /**
  * Render the final JSX of TeachingPopoverTitle
  */
 export const renderTeachingPopoverTitle_unstable = (state: TeachingPopoverTitleState) => {
-  const { slots, slotProps } = getSlotsNext<TeachingPopoverTitleSlots>(state);
+  assertSlots<TeachingPopoverTitleSlots>(state);
 
   return (
-    <slots.root {...slotProps.root}>
-      {slotProps.root.children}
-      {slots.dismissButton && <slots.dismissButton {...slotProps.dismissButton} />}
-    </slots.root>
+    <state.root>
+      {state.root.children}
+      {state.dismissButton && <state.dismissButton />}
+    </state.root>
   );
 };
