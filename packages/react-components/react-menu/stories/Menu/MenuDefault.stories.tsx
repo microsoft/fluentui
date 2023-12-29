8ed3df324d12337cde81fdd3af029197cfa7235a
Lingfan Gao <lingfangao@hotmail.com>
2023-12-29 10:28:30 +0100

    revert story

--- packages/react-components/react-menu/stories/Menu/MenuDefault.stories.tsx
+++ packages/react-components/react-menu/stories/Menu/MenuDefault.stories.tsx
@@ -2,29 +2,19 @@ import * as React from 'react';
 
 import { Button, Menu, MenuTrigger, MenuList, MenuItem, MenuPopover } from '@fluentui/react-components';
 
-export const Default = () => {
-  const [wide, setWide] = React.useState(false);
-  React.useEffect(() => {
-    setTimeout(() => setWide(true), 3000);
-  }, []);
+export const Default = () => (
+  <Menu>
+    <MenuTrigger disableButtonEnhancement>
+      <Button>Toggle menu</Button>
+    </MenuTrigger>
 
-  return (
-    <>
-      <div style={{ marginLeft: '200px', display: 'inline-block' }} />
-      <Menu positioning="below">
-        <MenuTrigger disableButtonEnhancement>
-          <Button>Toggle menu</Button>
-        </MenuTrigger>
-
-        <MenuPopover style={{ width: wide ? '200px' : undefined }}>
-          <MenuList>
-            <MenuItem>New </MenuItem>
-            <MenuItem>New Window</MenuItem>
-            <MenuItem disabled>Open File</MenuItem>
-            <MenuItem>Open Folder</MenuItem>
-          </MenuList>
-        </MenuPopover>
-      </Menu>
-    </>
-  );
-};
+    <MenuPopover>
+      <MenuList>
+        <MenuItem>New </MenuItem>
+        <MenuItem>New Window</MenuItem>
+        <MenuItem disabled>Open File</MenuItem>
+        <MenuItem>Open Folder</MenuItem>
+      </MenuList>
+    </MenuPopover>
+  </Menu>
+);
