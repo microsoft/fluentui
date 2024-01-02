d50ce8c818643442aa30a463a7aaceb09d41e15f
Martin Hochel <martinhochel@microsoft.com>
2024-01-02 15:40:15 +0100

    feat(workspace-plugin): add getWorkspaceConfig as source of truth to retrieve nx json + package.json metadata

--- tools/workspace-plugin/src/generators/rc-caret/index.spec.ts
+++ tools/workspace-plugin/src/generators/rc-caret/index.spec.ts
@@ -1,15 +1,9 @@
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import {
-  Tree,
-  readProjectConfiguration,
-  serializeJson,
-  addProjectConfiguration,
-  readJson,
-  readNxJson,
-} from '@nx/devkit';
+import { Tree, readProjectConfiguration, serializeJson, addProjectConfiguration, readJson } from '@nx/devkit';
 
 import generator from './index';
 import { VersionBumpGeneratorSchema } from './schema';
+import { getWorkspaceConfig } from '../../utils';
 
 const noop = () => null;
 
@@ -24,7 +18,7 @@ describe('rc-caret generator', () => {
     jest.spyOn(console, 'warn').mockImplementation(noop);
 
     tree = createTreeWithEmptyWorkspace();
-    npmScope = assertAndReadNxJson(tree).npmScope ?? '@proj';
+    npmScope = getWorkspaceConfig(tree).npmScope;
   });
 
   it('should work for dependencies', async () => {
@@ -131,7 +125,7 @@ function setupDummyPackage(
       projectConfiguration: Partial<ReturnType<typeof readProjectConfiguration>>;
     }>,
 ) {
-  const workspaceConfig = assertAndReadNxJson(tree);
+  const workspaceConfig = getWorkspaceConfig(tree);
   const defaults = {
     name: `@${workspaceConfig.npmScope}/react-components`,
     version: '9.0.0-alpha.40',
@@ -170,14 +164,4 @@ function setupDummyPackage(
   });
 
   return tree;
-}
-
-function assertAndReadNxJson(tree: Tree) {
-  const nxJson = readNxJson(tree);
-
-  if (!nxJson) {
-    throw new Error('nx.json doesnt exist');
-  }
-
-  return nxJson;
 }
