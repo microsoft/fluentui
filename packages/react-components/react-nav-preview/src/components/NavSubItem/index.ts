b51d4b83fa83de76d86bad11f9db98737b103428
Mason Tejera <matejera@microsoft.com>
2024-01-03 11:00:09 -0800

    update index files

--- packages/react-components/react-nav-preview/src/components/NavSubItem/index.ts
+++ packages/react-components/react-nav-preview/src/components/NavSubItem/index.ts
@@ -1,5 +1,5 @@
-export * from './NavSubItem';
-export * from './NavSubItem.types';
-export * from './renderNavSubItem';
-export * from './useNavSubItem';
-export * from './useNavSubItemStyles.styles';
+export { NavSubItem } from './NavSubItem';
+export type { NavSubItemSlots, NavSubItemProps, NavSubItemState } from './NavSubItem.types';
+export { renderNavSubItem_unstable } from './renderNavSubItem';
+export { useNavSubItem_unstable } from './useNavSubItem';
+export { useNavSubItemStyles_unstable, navSubItemClassNames } from './useNavSubItemStyles.styles';
