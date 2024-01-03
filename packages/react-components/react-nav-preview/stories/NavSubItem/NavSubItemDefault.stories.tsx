12ee1544a37c45af0ba948bab7ee7f3b7b50c6d7
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-01-03 14:29:40 -0800

    Update packages/react-components/react-nav-preview/stories/NavSubItem/NavSubItemDefault.stories.tsx
    Co-authored-by: Makoto Morimoto <humbertomakotomorimoto@gmail.com>

--- packages/react-components/react-nav-preview/stories/NavSubItem/NavSubItemDefault.stories.tsx
+++ packages/react-components/react-nav-preview/stories/NavSubItem/NavSubItemDefault.stories.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
-import { NavSubItem, NavSubItemProps } from '@fluentui/react-nav-preview';
+import { NavSubItem } from '@fluentui/react-nav-preview';
+import type { NavSubItemProps } from '@fluentui/react-nav-preview';
+
 
 export const Default = (props: Partial<NavSubItemProps>) => <NavSubItem {...props} />;
