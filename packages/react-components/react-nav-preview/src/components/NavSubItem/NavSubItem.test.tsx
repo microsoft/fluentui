bb44b5809b588ac3e1a112aafd1895168c941d83
Mason Tejera <matejera@microsoft.com>
2024-01-03 13:03:09 -0800

    update snap shot tests

--- packages/react-components/react-nav-preview/src/components/NavSubItem/NavSubItem.test.tsx
+++ packages/react-components/react-nav-preview/src/components/NavSubItem/NavSubItem.test.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { NavSubItem } from './NavSubItem';
 
@@ -7,12 +5,5 @@ describe('NavSubItem', () => {
   isConformant({
     Component: NavSubItem,
     displayName: 'NavSubItem',
-  });
-
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
-
-  it('renders a default state', () => {
-    const result = render(<NavSubItem>Default NavSubItem</NavSubItem>);
-    expect(result.container).toMatchSnapshot();
   });
 });
