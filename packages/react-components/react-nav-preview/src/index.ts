2075400bdfef416ebcaeeee9255fcbc436a4ee7a
Mason Tejera <matejera@microsoft.com>
2024-01-04 11:42:54 -0800

    Add new components

--- packages/react-components/react-nav-preview/src/index.ts
+++ packages/react-components/react-nav-preview/src/index.ts
@@ -27,3 +27,19 @@ export type { NavSubItemSlots, NavSubItemProps, NavSubItemState } from './compon
 export { renderNavSubItem_unstable } from './components/NavSubItem/renderNavSubItem';
 export { useNavSubItem_unstable } from './components/NavSubItem/useNavSubItem';
 export { useNavSubItemStyles_unstable, navSubItemClassNames } from './components/NavSubItem/useNavSubItemStyles.styles';
+
+export { NavSubItemGroup } from './components/NavSubItemGroup/NavSubItemGroup';
+export type {
+  NavSubItemGroupSlots,
+  NavSubItemGroupProps,
+  NavSubItemGroupState,
+} from './components/NavSubItemGroup/NavSubItemGroup.types';
+export { renderNavSubItemGroup_unstable } from './components/NavSubItemGroup/renderNavSubItemGroup';
+export { useNavSubItemGroup_unstable } from './components/NavSubItemGroup/useNavSubItemGroup';
+export { useNavSubItemGroupStyles_unstable } from './components/NavSubItemGroup/useNavSubItemGroupStyles.styles';
+
+export { NavCategory } from './components/NavCategory/NavCategory';
+export type { NavCategorySlots, NavCategoryProps, NavCategoryState } from './components/NavCategory/NavCategory.types';
+export { renderNavCategory_unstable } from './components/NavCategory/renderNavCategory';
+export { useNavCategory_unstable } from './components/NavCategory/useNavCategory';
+export { useNavCategoryStyles_unstable } from './components/NavCategory/useNavCategoryStyles.styles';
