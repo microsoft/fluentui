49ebeef3d5b5875438cff971fa9f6c4bb6cf2a26
YuanboXue-Amber <yuanboxue@microsoft.com>
2024-01-04 10:29:33 +0100

    more test

--- packages/react-components/react-timepicker-compat/src/components/TimePicker/TimePicker.test.tsx
+++ packages/react-components/react-timepicker-compat/src/components/TimePicker/TimePicker.test.tsx
@@ -36,6 +36,9 @@ describe('TimePicker', () => {
           },
         },
       ],
+      'consistent-callback-type': {
+        ignoreProps: ['onOpenChange', 'onTimeChange'],
+      },
     },
   });
 
