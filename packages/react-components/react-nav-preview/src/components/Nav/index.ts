9803a2d1f03b32f23e333e8c023c21f73a6bfb93
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-01-05 10:04:08 -0800

    Update packages/react-components/react-nav-preview/src/components/Nav/index.ts
    Co-authored-by: Makoto Morimoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-nav-preview/src/components/Nav/index.ts
+++ packages/react-components/react-nav-preview/src/components/Nav/index.ts
@@ -1,5 +1,4 @@
 export { Nav } from './Nav';
-// don't export evenData and EventHandler since those will be added to react-utilities
 export type { NavSlots, NavProps, OnNavItemSelectData, NavState } from './Nav.types';
 export { renderNav_unstable } from './renderNav';
 export { useNav_unstable } from './useNav';
