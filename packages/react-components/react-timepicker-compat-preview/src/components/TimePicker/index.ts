e6c386521b3c7039227779ca831b576083fe5280
YuanboXue-Amber <yuanboxue@microsoft.com>
2024-01-09 11:51:39 +0100

    remove timepicker-compat-preview

--- packages/react-components/react-timepicker-compat-preview/src/components/TimePicker/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './TimePicker';
-export * from './TimePicker.types';
-export * from './useTimePicker';
-export * from './useTimePickerStyles.styles';
-// eslint-disable-next-line deprecation/deprecation
-export { formatDateToTimeString } from './timeMath';
