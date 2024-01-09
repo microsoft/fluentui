6eaad68b1cbaeb5b4917fabe17e6fae7a851f780
Tomi Olubeko <66456876+tomi-msft@users.noreply.github.com>
2024-01-08 21:51:39 -0800

    v8 registerIcons compat (#30003)
    Co-authored-by: KHMakoto <Humberto.Morimoto@microsoft.com>

--- packages/react-components/react-icons-compat/src/index.ts
+++ packages/react-components/react-icons-compat/src/index.ts
@@ -1,1 +1,2 @@
-export {};
+export { registerIcons, registerIconAlias, unregisterIcons, getIcon, setIconOptions } from './icon';
+export type { IconRecord, IconOptions, IconSubset, IconSubsetRecord, IconRecords } from './icon';
