3be9c0d87c7c53343551000407a9d8f96d8ab5c0
Mason Tejera <matejera@microsoft.com>
2024-01-09 11:57:54 -0800

    fix story

--- packages/react-components/react-nav-preview/stories/NavItem/NavItemDefault.stories.tsx
+++ packages/react-components/react-nav-preview/stories/NavItem/NavItemDefault.stories.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import { NavItem, NavItemProps } from '@fluentui/react-nav-preview';
 
-export const Default = (props: Partial<NavItemProps>) => <NavItem {...props} />;
+export const Default = (props: Partial<NavItemProps>) => <NavItem value={'someValue'} {...props} />;
