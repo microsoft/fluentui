f0e1d972b4c5a255bb6f9f1898ae2d3ae238f991
Sarah Higley <sarah.higley@microsoft.com>
2024-01-11 12:58:31 -0800

    add test

--- packages/react-components/react-combobox/src/components/Combobox/Combobox.test.tsx
+++ packages/react-components/react-combobox/src/components/Combobox/Combobox.test.tsx
@@ -497,10 +497,29 @@ describe('Combobox', () => {
 
     expect((getByRole('combobox') as HTMLInputElement).value).toEqual('Red');
 
-    // arrow down + space
+    // space should select after an arrow down
     userEvent.keyboard('{ArrowDown} ');
 
     expect((getByRole('combobox') as HTMLInputElement).value).toEqual('Green');
+  });
+
+  it('does not select with space while typing', () => {
+    const onSelect = jest.fn();
+
+    const { getByTestId, getByRole } = render(
+      <Combobox open data-testid="combobox" onOptionSelect={onSelect}>
+        <Option>Red</Option>
+        <Option>Green</Option>
+        <Option>Blue</Option>
+      </Combobox>,
+    );
+
+    const combobox = getByTestId('combobox');
+
+    userEvent.type(combobox, 'abc def');
+
+    expect((getByRole('combobox') as HTMLInputElement).value).toEqual('abc def');
+    expect(onSelect).not.toHaveBeenCalled();
   });
 
   it('does not select a disabled option with the keyboard', () => {
