530128552886ae523648b87afb7ff9fa4de5db84
YuanboXue-Amber <yuanboxue@microsoft.com>
2024-01-12 14:46:06 +0100

    add unit test

---
+++ packages/eslint-plugin/src/rules/consistent-callback-type/index.test.js
@@ -0,0 +1,67 @@
+// @ts-nocheck
+const { ESLintUtils } = require('@typescript-eslint/experimental-utils');
+const rule = require('./index');
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+});
+
+ruleTester.run('consistent-callback-type', rule, {
+  valid: [
+    // Valid when prop is TSTypeAliasDeclaration and the callback uses EventHandler
+    {
+      code: `
+        import { EventHandler, EventData } from '@fluentui/react-utilities';
+        export type OnSomeEventData = EventData<'focus', React.FocusEvent<HTMLElement>> & {
+          open: boolean,
+        };
+        export type SomeProps = {
+          onSomeEvent?: EventHandler<OnSomeEventData>,
+        };
+      `,
+      filename: 'src/components/SomeComponent/SomeComponent.type.ts',
+    },
+    // Valid when prop is TSIntersectionType and the callback uses EventHandler
+    {
+      code: `
+        import { EventHandler, EventData } from '@fluentui/react-utilities';
+        export type OnSomeEventData = EventData<'focus', React.FocusEvent<HTMLElement>> & {
+          open: boolean,
+        };
+        export type SomeProps = SomeType & {
+          onSomeEvent?: EventHandler<OnSomeEventData>,
+        };
+      `,
+      filename: 'src/components/SomeComponent/SomeComponent.type.ts',
+    },
+    // Valid when prop has a function that is not callback, and not using EventHandler type
+    {
+      code: `
+        export type SomeProps = {
+          someFunction?: EventHandler<OnSomeEventData>,
+        };
+      `,
+      filename: 'src/components/SomeComponent/SomeComponent.type.ts',
+    },
+  ],
+  invalid: [
+    // Invalid when callback in props is not using EventHandler
+    {
+      code: `
+        export type SomeProps = {
+          onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: {}) => void;
+        };
+      `,
+      errors: [{ messageId: 'invalidType' }],
+    },
+    // Invalid when callback in props is not using EventHandler, and the props is TSIntersectionType
+    {
+      code: `
+        export type SomeProps = SomeType & {
+          onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: {}) => void;
+        };
+      `,
+      errors: [{ messageId: 'invalidType' }],
+    },
+  ],
+});
