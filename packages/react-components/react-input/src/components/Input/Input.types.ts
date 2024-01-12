69dc50399aee7c34b18d732160bd81dd2320167b
YuanboXue-Amber <yuanboxue@microsoft.com>
2024-01-12 12:08:27 +0100

    remove testing code in input.type.ts

--- packages/react-components/react-input/src/components/Input/Input.types.ts
+++ packages/react-components/react-input/src/components/Input/Input.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { ComponentProps, ComponentState, EventData, EventHandler, Slot } from '@fluentui/react-utilities';
+import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
 
 export type InputSlots = {
   /**
@@ -26,8 +26,6 @@ export type InputSlots = {
   /** Element after the input text, within the input border */
   contentAfter?: Slot<'span'>;
 };
-
-type OnOpenChangeData = EventData<'click', React.MouseEvent<HTMLInputElement>>;
 
 export type InputProps = Omit<
   ComponentProps<Partial<InputSlots>, 'input'>,
@@ -81,8 +79,6 @@ export type InputProps = Omit<
    */
   // eslint-disable-next-line @fluentui/consistent-callback-type
   onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => void;
-
-  onChange2?: EventHandler<OnOpenChangeData>;
 
   /**
    * An input can have different text-based [types](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input#input_types)
