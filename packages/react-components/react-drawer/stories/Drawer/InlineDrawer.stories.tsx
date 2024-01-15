b46558687dc1f99ce5d19047d862819412214fde
Marcos Moura <marcosvmmoura@gmail.com>
2024-01-15 15:32:46 +0100

    fix: code formatting

--- packages/react-components/react-drawer/stories/Drawer/InlineDrawer.stories.tsx
+++ packages/react-components/react-drawer/stories/Drawer/InlineDrawer.stories.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react"
 import {
   DrawerBody,
   DrawerHeader,
@@ -10,73 +10,73 @@ import {
   tokens,
   DrawerProps,
   mergeClasses,
-} from '@fluentui/react-components';
-import { Dismiss24Regular } from '@fluentui/react-icons';
+} from "@fluentui/react-components"
+import { Dismiss24Regular } from "@fluentui/react-icons"
 
 const useStyles = makeStyles({
   root: {
-    ...shorthands.border('2px', 'solid', '#ccc'),
-    ...shorthands.overflow('hidden'),
+    ...shorthands.border("2px", "solid", "#ccc"),
+    ...shorthands.overflow("hidden"),
 
-    display: 'flex',
-    height: '480px',
-    backgroundColor: '#fff',
+    display: "flex",
+    height: "480px",
+    backgroundColor: "#fff",
   },
 
   content: {
     ...shorthands.flex(1),
-    ...shorthands.padding('16px'),
-    ...shorthands.overflow('auto'),
+    ...shorthands.padding("16px"),
+    ...shorthands.overflow("auto"),
 
-    position: 'relative',
+    position: "relative",
   },
 
   flexColumn: {
-    flexDirection: 'column',
+    flexDirection: "column",
   },
 
   buttons: {
     ...shorthands.flex(1),
-    ...shorthands.padding('16px'),
+    ...shorthands.padding("16px"),
 
-    position: 'sticky',
-    top: '-16px',
-    right: '-16px',
-    left: '-16px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    position: "sticky",
+    top: "-16px",
+    right: "-16px",
+    left: "-16px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
     columnGap: tokens.spacingHorizontalXS,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     transitionDuration: tokens.durationFast,
   },
-});
+})
 
 type DrawerInlineExampleProps = {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-  position: DrawerProps['position'];
-};
+  open: boolean
+  setOpen: (open: boolean) => void
+  position: DrawerProps["position"]
+}
 
-const setButtonText = (open: boolean, position: DrawerProps['position']) => {
-  let buttonText = open ? 'Close' : 'Open';
+const setButtonText = (open: boolean, position: DrawerProps["position"]) => {
+  let buttonText = open ? "Close" : "Open"
 
   switch (position) {
-    case 'start':
-      buttonText = `${buttonText} left`;
+    case "start":
+      buttonText = `${buttonText} left`
 
-    case 'end':
-      buttonText = `${buttonText} right`;
+    case "end":
+      buttonText = `${buttonText} right`
 
-    case 'bottom':
-      buttonText = `${buttonText} bottom`;
+    case "bottom":
+      buttonText = `${buttonText} bottom`
 
     default:
-      buttonText = `${buttonText} drawer`;
+      buttonText = `${buttonText} drawer`
   }
 
   return buttonText
-};
+}
 
 const DrawerInlineExample: React.FC<DrawerInlineExampleProps> = ({ open, setOpen, position }) => {
   return (
@@ -84,7 +84,7 @@ const DrawerInlineExample: React.FC<DrawerInlineExampleProps> = ({ open, setOpen
       <DrawerHeader>
         <DrawerHeaderTitle
           action={
-            <Button appearance="subtle" aria-label="Close" icon={<Dismiss24Regular />} onClick={() => setOpen(false)} />
+            <Button appearance='subtle' aria-label='Close' icon={<Dismiss24Regular />} onClick={() => setOpen(false)} />
           }
         >
           {position} Inline Drawer
@@ -95,33 +95,33 @@ const DrawerInlineExample: React.FC<DrawerInlineExampleProps> = ({ open, setOpen
         <p>Drawer content</p>
       </DrawerBody>
     </InlineDrawer>
-  );
-};
+  )
+}
 
 export const Inline = () => {
-  const styles = useStyles();
+  const styles = useStyles()
 
-  const [leftOpen, setLeftOpen] = React.useState(false);
-  const [rightOpen, setRightOpen] = React.useState(false);
-  const [bottomOpen, setBottomOpen] = React.useState(false);
+  const [leftOpen, setLeftOpen] = React.useState(false)
+  const [rightOpen, setRightOpen] = React.useState(false)
+  const [bottomOpen, setBottomOpen] = React.useState(false)
 
   return (
     <div className={mergeClasses(styles.root, styles.flexColumn)}>
       <div className={styles.root}>
-        <DrawerInlineExample open={leftOpen} setOpen={setLeftOpen} position="start" />
+        <DrawerInlineExample open={leftOpen} setOpen={setLeftOpen} position='start' />
 
         <div className={styles.content}>
           <div className={styles.buttons}>
-            <Button appearance="primary" onClick={() => setLeftOpen(!leftOpen)}>
-              {setButtonText(leftOpen, 'start')}
+            <Button appearance='primary' onClick={() => setLeftOpen(!leftOpen)}>
+              {setButtonText(leftOpen, "start")}
             </Button>
 
-            <Button appearance="primary" onClick={() => setRightOpen(!rightOpen)}>
-              {setButtonText(rightOpen, 'end')}
+            <Button appearance='primary' onClick={() => setRightOpen(!rightOpen)}>
+              {setButtonText(rightOpen, "end")}
             </Button>
 
-            <Button appearance="primary" onClick={() => setBottomOpen(!bottomOpen)}>
-              {setButtonText(bottomOpen, 'bottom')}
+            <Button appearance='primary' onClick={() => setBottomOpen(!bottomOpen)}>
+              {setButtonText(bottomOpen, "bottom")}
             </Button>
           </div>
 
@@ -134,22 +134,22 @@ export const Inline = () => {
           ))}
         </div>
 
-        <DrawerInlineExample open={rightOpen} setOpen={setRightOpen} position="end" />
+        <DrawerInlineExample open={rightOpen} setOpen={setRightOpen} position='end' />
       </div>
 
-      <DrawerInlineExample open={bottomOpen} setOpen={setBottomOpen} position="bottom" />
+      <DrawerInlineExample open={bottomOpen} setOpen={setBottomOpen} position='bottom' />
     </div>
-  );
-};
+  )
+}
 
 Inline.parameters = {
   docs: {
     description: {
       story: [
-        'InlineDrawer is often used for navigation that is not dismissible.',
-        'As it is on the same level as the main surface, users can still interact with other UI elements.',
-        'This could be useful for swapping between different items in the main surface.',
-      ].join('\n'),
+        "InlineDrawer is often used for navigation that is not dismissible.",
+        "As it is on the same level as the main surface, users can still interact with other UI elements.",
+        "This could be useful for swapping between different items in the main surface.",
+      ].join("\n"),
     },
   },
-};
+}
