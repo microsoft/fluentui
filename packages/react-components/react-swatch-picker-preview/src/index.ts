7906e9a89ef60679ca64638044b04c148c288994
Ben Howell <behowell@microsoft.com>
2024-01-16 15:14:04 -0800

    Undo accidental, unrelated changes

--- packages/react-components/react-swatch-picker-preview/src/index.ts
+++ packages/react-components/react-swatch-picker-preview/src/index.ts
@@ -1,2 +1,1 @@
-export { SwatchPicker, renderSwatchPicker_unstable, swatchPickerClassNames, useSwatchPickerStyles_unstable, useSwatchPicker_unstable } from './SwatchPicker';
-export type { SwatchPickerProps, SwatchPickerSlots, SwatchPickerState } from './SwatchPicker';
+export * from './SwatchPicker';
