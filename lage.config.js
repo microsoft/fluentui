1b8eef3c16c382408073d498d0d8eac13be96780
Martin Hochel <martinhochel@microsoft.com>
2024-01-22 12:10:11 +0100

    chore: remove bundle-size-auditor package,its usage and replace it with monosize on ci

--- lage.config.js
+++ lage.config.js
@@ -5,7 +5,6 @@ module.exports = {
     'build:info': [],
     bundle: ['build'],
     'bundle-size': ['build'],
-    'bundle-size-auditor': ['build'],
     // adding temporary back until import plugin rule is resolved https://github.com/microsoft/fluentui/issues/27727
     lint: ['build'],
     clean: [],
--- scripts/bundle-size-auditor/src/typings.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'parallel-webpack' {
-  export async function run(configPath: string, options: {}, doneCallback?: () => void): void;
-}
