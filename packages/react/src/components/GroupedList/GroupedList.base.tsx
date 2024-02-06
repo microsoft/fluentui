4ce91446f3c4b19c255ae708d0a8d5b093e9cdf7
Sarah Higley <sarah.higley@microsoft.com>
2024-02-05 17:15:09 -0800

    fix: GroupedList sets correct focus attributes on List

--- packages/react/src/components/GroupedList/GroupedList.base.tsx
+++ packages/react/src/components/GroupedList/GroupedList.base.tsx
@@ -168,6 +168,7 @@ export class GroupedListBase extends React.Component<IGroupedListProps, IGrouped
             usePageCache={usePageCache}
             onShouldVirtualize={onShouldVirtualize}
             version={version}
+            renderEarly={true}
             {...rootListProps}
           />
         )}
