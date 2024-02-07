d9a773933d1e6c6c6cc22a671764cfe66b979ddd
Amber <yuanboxue@microsoft.com>
2024-02-07 10:18:47 +0100

    feat(react-conformance): Deprecate conformance test `consistent-callback-args` (#30301)

--- packages/react-conformance/src/types.ts
+++ packages/react-conformance/src/types.ts
@@ -137,6 +137,9 @@ export interface DefaultTestObject<TProps = {}> {
   'has-top-level-file': BaseConformanceTest<TProps>;
   'kebab-aria-attributes': ConformanceTest<TProps>;
   'consistent-callback-names': ConformanceTest<TProps>;
+  /**
+   * @deprecated this test is for existing callbacks. The newly added callbacks' type will be guarded by eslint rule consistent-callback-type
+   */
   'consistent-callback-args': ConformanceTest<TProps>;
   'primary-slot-gets-native-props': BaseConformanceTest<TProps>;
 }
