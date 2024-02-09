f5fdb107137d50b20cd5dc43d1a53b224b0e0d25
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-02-09 10:31:35 -0800

    add back nav slots

--- packages/react-components/react-nav-preview/src/components/NavCategory/NavCategory.types.ts
+++ packages/react-components/react-nav-preview/src/components/NavCategory/NavCategory.types.ts
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { NavItemValue } from '../NavContext.types';
 import { NavCategoryContextValue } from '../NavCategoryContext';
+import { ComponentProps } from '@fluentui/react-utilities';
+
+export type NavCategorySlots = {};
 
 /**
  * NavCategory Props
  */
-export type NavCategoryProps = {
+export type NavCategoryProps = ComponentProps<NavCategorySlots> & {
   /**
    * Required value that identifies this item inside an Nav component.
    */
