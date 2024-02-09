e3756b6fb47e75b025a32f2016e26866537b53d4
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-02-09 10:21:55 -0800

    updating type imports

--- packages/react-components/react-nav-preview/src/components/NavCategory/NavCategory.tsx
+++ packages/react-components/react-nav-preview/src/components/NavCategory/NavCategory.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { useNavCategory_unstable } from './useNavCategory';
-import { renderNavCategory_unstable } from './renderNavCategory';
-import { useNavCategoryContextValues_unstable } from '../useNavCategoryContextValues_unstable';
 
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
 import type { NavCategoryProps } from './NavCategory.types';
+
+import { useNavCategory_unstable } from './useNavCategory';
+import { renderNavCategory_unstable } from './renderNavCategory';
+import { useNavCategoryContextValues_unstable } from '../useNavCategoryContextValues_unstable';
 
 /**
  * NavCategory component - TODO: add more docs
