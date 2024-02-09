74ca8d2edeec01ff0f5d25fdd7710b5dc198263f
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-02-09 11:28:41 -0800

    updates

--- packages/react-components/react-nav-preview/stories/NavCategory/NavCategoryDefault.stories.tsx
+++ packages/react-components/react-nav-preview/stories/NavCategory/NavCategoryDefault.stories.tsx
@@ -1,5 +1,22 @@
 import * as React from 'react';
-import { NavCategory } from '@fluentui/react-nav-preview';
-import type { NavCategoryProps } from '@fluentui/react-nav-preview';
+import { Nav } from '../../src/components/Nav/Nav';
+import { NavCategory } from '../../src/components/NavCategory/NavCategory';
+import { NavCategoryItem } from '../../src/components/NavCategoryItem/NavCategoryItem';
+import { NavSubItemGroup } from '../../src/components/NavSubItemGroup/NavSubItemGroup';
 
-export const Default = (props: Partial<NavCategoryProps>) => <NavCategory {...props} />;
+export const Default = () => (
+  <Nav>
+    <NavCategory value="1">
+      <NavCategoryItem>NavCategoryItem 1</NavCategoryItem>
+      <NavSubItemGroup>
+        <div>Some div inside SubItemGroup</div>
+      </NavSubItemGroup>
+    </NavCategory>
+    <NavCategory value="2">
+      <NavCategoryItem>NavCategoryItem2</NavCategoryItem>
+      <NavSubItemGroup>
+        <div>Some other div inside SubItemGroup</div>
+      </NavSubItemGroup>
+    </NavCategory>
+  </Nav>
+);
