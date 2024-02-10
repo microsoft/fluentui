5908234de4a242a3d2a0f060b3b9964d088712d6
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-02-09 16:03:27 -0800

    successful copy paste

--- packages/react-components/react-nav-preview/src/components/NavCategoryItem/renderNavCategoryItem.tsx
+++ packages/react-components/react-nav-preview/src/components/NavCategoryItem/renderNavCategoryItem.tsx
@@ -3,19 +3,29 @@
 
 import { createElement } from '@fluentui/react-jsx-runtime';
 import { assertSlots } from '@fluentui/react-utilities';
-import type { NavCategoryItemState, NavCategoryItemSlots } from './NavCategoryItem.types';
+import type { NavCategoryItemState, NavCategoryItemSlots, NavCategoryItemContextValues } from './NavCategoryItem.types';
+import { NavCategoryItemProvider } from '../NavCategoryItemContext';
 
 /**
  * Render the final JSX of NavCategoryItem
  */
-export const renderNavCategoryItem_unstable = (state: NavCategoryItemState) => {
+export const renderNavCategoryItem_unstable = (
+  state: NavCategoryItemState,
+  contextValues: NavCategoryItemContextValues,
+) => {
   assertSlots<NavCategoryItemSlots>(state);
 
   return (
-    <state.root>
-      {/* TODO: light this up when we have design spec */}
-      {/* {state.icon && <state.icon />} */}
-      <state.content />
-    </state.root>
+    <NavCategoryItemProvider value={contextValues.navCategoryItem}>
+      <state.root>
+        {/* TODO: These were copied from AccordionHeader.
+        We need to decide if they are still applicable
+        in our scenario and how to adapt them. */}
+        {/* <state.button> */}
+        {/* Todo: {state.icon && <state.icon />} */}
+        {state.root.children}
+        {state.expandIcon && <state.expandIcon />} {/* </state.button> */}
+      </state.root>
+    </NavCategoryItemProvider>
   );
 };
