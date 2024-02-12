756b683a1f6d4520c3d20a0582bfb14864459d1d
Jason Morse <jasonmorse@Jasons-MacBook-Pro-2.local>
2024-02-11 22:11:52 -0800

    fix: add type for babel preset to unblock builds

--- packages/react-components/babel-preset-global-context/etc/babel-preset-global-context.api.md
+++ packages/react-components/babel-preset-global-context/etc/babel-preset-global-context.api.md
@@ -11,15 +11,7 @@ import { PluginObj } from '@babel/core';
 import { PluginPass } from '@babel/core';
 
 // @public (undocumented)
-function preset(): {
-    plugins: ((api: object, options: Record<string, any> | null | undefined, dirname: string) => PluginObj<PluginPass & {
-    importDeclarationPaths?: NodePath<ImportDeclaration>[] | undefined;
-    nativeExpressionPaths?: NodePath<CallExpression>[] | undefined;
-    contextSelectorExpressionPaths?: NodePath<CallExpression>[] | undefined;
-    nativeLocalName?: string | undefined;
-    contextSelectorLocalName?: string | undefined;
-    }>)[][];
-};
+function preset(): BabelPreset;
 export default preset;
 
 // (No @packageDocumentation comment for this package)
--- packages/react-components/babel-preset-global-context/src/index.ts
+++ packages/react-components/babel-preset-global-context/src/index.ts
@@ -1,6 +1,27 @@
 import { transformPlugin } from './transformPlugin';
 
-export default function preset() {
+import { type CallExpression, type ImportDeclaration } from '@babel/types';
+import { type NodePath } from '@babel/traverse';
+import { type PluginObj, type PluginPass } from '@babel/core';
+
+// @public (undocumented)
+type BabelPresetFunction = (
+  api: object,
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  options: Record<string, any> | null | undefined,
+  dirname: string,
+) => PluginObj<
+  PluginPass & {
+    importDeclarationPaths?: NodePath<ImportDeclaration>[] | undefined;
+    nativeExpressionPaths?: NodePath<CallExpression>[] | undefined;
+    contextSelectorExpressionPaths?: NodePath<CallExpression>[] | undefined;
+    nativeLocalName?: string | undefined;
+    contextSelectorLocalName?: string | undefined;
+  }
+>;
+type BabelPreset = { plugins: BabelPresetFunction[][] };
+
+export default function preset(): BabelPreset {
   return {
     plugins: [[transformPlugin]],
   };
