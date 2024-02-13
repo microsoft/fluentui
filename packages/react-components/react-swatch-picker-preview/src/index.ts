39fbd07b0943ff4499fa406b606ef78e76b02d93
Valentyna <vkozlova@microsoft.com>
2024-02-13 15:33:52 +0100

    Added Default stories for the SwatchPicker

--- packages/react-components/react-swatch-picker-preview/src/index.ts
+++ packages/react-components/react-swatch-picker-preview/src/index.ts
@@ -1,2 +1,14 @@
-export * from './SwatchPicker';
-export * from './ColorSwatch';
+export {
+  SwatchPicker,
+  renderSwatchPicker_unstable,
+  useSwatchPickerStyles_unstable,
+  useSwatchPicker_unstable,
+} from './SwatchPicker';
+export type { SwatchPickerProps, SwatchPickerSlots, SwatchPickerState } from './SwatchPicker';
+export {
+  ColorSwatch,
+  renderColorSwatch_unstable,
+  useColorSwatchStyles_unstable,
+  useColorSwatch_unstable,
+} from './ColorSwatch';
+export type { ColorSwatchProps, ColorSwatchSlots, ColorSwatchState } from './ColorSwatch';
--- packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerDefault.stories.tsx
+++ packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerDefault.stories.tsx
@@ -1,4 +1,19 @@
 import * as React from 'react';
-import { SwatchPicker, SwatchPickerProps } from '@fluentui/react-swatch-picker-preview';
+import { SwatchPicker, ColorSwatch } from '@fluentui/react-swatch-picker-preview';
 
-export const Default = (props: Partial<SwatchPickerProps>) => <SwatchPicker {...props} />;
+export const Default = () => {
+  return (
+    <SwatchPicker aria-label="SwatchPicker no layout">
+      <ColorSwatch color="#C11016" aria-label="dark red" role="radio" />
+      <ColorSwatch color="#FF1921" aria-label="red" role="radio" />
+      <ColorSwatch color="#FFC12E" aria-label="orange" role="radio" />
+      <ColorSwatch color="#FEFF37" aria-label="yellow" role="radio" />
+      <ColorSwatch color="#90D057" aria-label="light green" role="radio" />
+      <ColorSwatch color="#00B053" aria-label="green" role="radio" />
+      <ColorSwatch color="#00AFED" aria-label="light blue" role="radio" />
+      <ColorSwatch color="#006EBD" aria-label="blue" role="radio" />
+      <ColorSwatch color="#011F5E" aria-label="dark blue" role="radio" />
+      <ColorSwatch color="#712F9E" aria-label="purple" role="radio" />
+    </SwatchPicker>
+  );
+};
