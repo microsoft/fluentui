e7a2b4e884a2ead3f22804d6b68c85ede22089d0
Miroslav Stastny <miroslav.stastny@microsoft.com>
2024-02-22 14:59:31 -0800

    update colorStatusDangerForeground3 mapping in dark theme

--- packages/tokens/src/alias/darkColorPalette.ts
+++ packages/tokens/src/alias/darkColorPalette.ts
@@ -77,8 +77,7 @@ export const colorStatusTokens: ColorStatusTokens = Object.entries(statusColorMa
 );
 
 // one-off overrides for colorStatus tokens
-colorStatusTokens.colorStatusDangerForeground1 = mappedStatusColors[statusColorMapping.danger].tint40;
-colorStatusTokens.colorStatusDangerForeground3 = mappedStatusColors[statusColorMapping.danger].tint30;
+colorStatusTokens.colorStatusDangerForeground3 = mappedStatusColors[statusColorMapping.danger].tint40;
 colorStatusTokens.colorStatusDangerBorder2 = mappedStatusColors[statusColorMapping.danger].tint30;
 colorStatusTokens.colorStatusSuccessForeground3 = mappedStatusColors[statusColorMapping.success].tint40;
 colorStatusTokens.colorStatusSuccessBorder2 = mappedStatusColors[statusColorMapping.success].tint40;
