2e8b33675468ee9bbf88bb14dcca89a0173fb4a3
Shubhabrata Ghosh <35366351+Shubhabrata08@users.noreply.github.com>
2024-02-23 20:34:26 +0530

    [Fix] Zero values unavailable in DonutChart plot (#30237)

--- packages/react-charting/src/utilities/utilities.ts
+++ packages/react-charting/src/utilities/utilities.ts
@@ -1374,7 +1374,7 @@ export const getAccessibleDataObject = (
 
 type LocaleStringDataProps = number | string | Date | undefined;
 export const convertToLocaleString = (data: LocaleStringDataProps, culture?: string): LocaleStringDataProps => {
-  if (!data) {
+  if (data === undefined || data === null || Number.isNaN(data)) {
     return data;
   }
   culture = culture || undefined;
