c8994b49cdf21630ad9cdb0223c042a68b9918e1
v-baambati <132879294+v-baambati@users.noreply.github.com>
2024-02-27 13:50:12 +0530

    added unit test cases and test plan

--- packages/react-charting/UnitTests/GaugeChartUT.test.tsx
+++ packages/react-charting/UnitTests/GaugeChartUT.test.tsx
@@ -1,13 +1,9 @@
-import * as React from 'react';
-import { GaugeChart, GaugeChartVariant, GaugeValueFormat, IGaugeChartProps } from '../src/components/GaugeChart/index';
-import { render } from '@testing-library/react';
+import { IGaugeChartProps } from '../src/components/GaugeChart/index';
 import { GaugeChartBase } from '../src/components/GaugeChart/GaugeChart.base';
 import { DataVizPalette } from '../src/utilities/colors';
 import { DarkTheme } from '@fluentui/theme-samples';
-import { IFontStyles, IScheme } from '@fluentui/theme';
 
 const env = require('../config/tests');
-
 const runTest = env === 'TEST' ? describe : describe.skip;
 
 const segments = [
@@ -17,11 +13,38 @@ const segments = [
 ];
 
 const data: IGaugeChartProps = { segments: segments, chartValue: 100, theme: DarkTheme };
+const emptyData: IGaugeChartProps = { segments: [], chartValue: 100, theme: DarkTheme };
+
 runTest('_processProps', () => {
-  test('Should return proper legends data', () => {
+  test('Should return proper arcs data', () => {
     const instance = new GaugeChartBase(data);
     expect(instance).toBeDefined();
     const result = instance._processProps();
     expect(result.arcs).toHaveLength(3);
+    expect(result.arcs[0].segmentIndex).toEqual(0);
+    expect(result.arcs[1].segmentIndex).toEqual(1);
+    expect(result.arcs[2].segmentIndex).toEqual(2);
+    expect(result.arcs[0].d).toEqual('M0,0Z');
+    expect(result.arcs[1].d).toEqual('M0,0Z');
+    expect(result.arcs[2].d).toEqual('M0,0Z');
+  });
+
+  test('Should return zero arcs when there is no data', () => {
+    const instance = new GaugeChartBase(emptyData);
+    expect(instance).toBeDefined();
+    const result = instance._processProps();
+    expect(result.arcs).toHaveLength(0);
+  });
+});
+
+runTest('_getMargins', () => {
+  test('Should return zero arcs when there is no data', () => {
+    const instance = new GaugeChartBase(data);
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(16);
+    expect(result.top).toEqual(18);
+    expect(result.left).toEqual(56);
+    expect(result.right).toEqual(56);
   });
 });
---
+++ packages/react-charting/docs/TestPlans/GaugeChart/UnitTests.md
@@ -0,0 +1,9 @@
+**Unit Test Plan - Gauge Chart**
+
+|     **Test steps**      |                **Validation**                | **Tool used** |
+| :---------------------: | :------------------------------------------: | :-----------: |
+|  Test 1: Process Pros   |                                              |               |
+|  - With segments data   |        Should return proper arcs data        |      RTL      |
+| - Without segments data | Should not return arcs when there is no data |      RTL      |
+|   Test 2: Get Margins   |                                              |               |
+| - Without segment data  |     Should return margins data properly      |      RTL      |
