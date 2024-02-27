cda2e1efcfe1a61e549f4ace962161fe082a5588
Valentyna <vkozlova@microsoft.com>
2024-02-27 13:01:33 +0100

    added step fro building storybook

--- packages/react-components/react-swatch-picker-preview/src/components/ColorSwatch/ColorSwatch.test.tsx
+++ packages/react-components/react-swatch-picker-preview/src/components/ColorSwatch/ColorSwatch.test.tsx
@@ -2,40 +2,25 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { ColorSwatch } from './ColorSwatch';
-import { colorSwatchClassNames } from './useColorSwatchStyles.styles';
 
 describe('ColorSwatch', () => {
   isConformant({
     Component: ColorSwatch,
     displayName: 'ColorSwatch',
-    primarySlot: 'button',
-    testOptions: {
-      'has-static-classnames': [
-        {
-          props: {},
-          expectedClassNames: {
-            button: colorSwatchClassNames.button,
-          },
-        },
-      ],
-    },
   });
 
   it('renders a default state', () => {
     const result = render(<ColorSwatch color="#f09" value="#f09" />);
     expect(result.container).toMatchInlineSnapshot(`
       <div>
-        <div
+        <button
           aria-checked="false"
           class="fui-ColorSwatch"
           role="radio"
-          style="--fui-SwatchPicker--color: #f09;"
-        >
-          <button
-            class="fui-ColorSwatch__button"
-            type="button"
-          />
-        </div>
+          style="--fui-SwatchPicker--color: #f09; --fui-SwatchPicker--contrastColor: transparent;"
+          type="button"
+          value="#f09"
+        />
       </div>
     `);
   });
