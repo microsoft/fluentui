389a7ce0f6b283e7415d8383cfa30fac6cef9ee8
v-baambati <132879294+v-baambati@users.noreply.github.com>
2024-02-27 16:57:25 +0530

    added test cases for getMarging function

--- packages/react-charting/UnitTests/GaugeChartUT.test.tsx
+++ packages/react-charting/UnitTests/GaugeChartUT.test.tsx
@@ -12,7 +12,7 @@ const segments = [
   { size: 33, color: DataVizPalette.error, legend: 'High Risk' },
 ];
 
-const data: IGaugeChartProps = { segments: segments, chartValue: 100, theme: DarkTheme };
+const data: IGaugeChartProps = { segments: segments, chartValue: 100, theme: DarkTheme, hideMinMax: true };
 const emptyData: IGaugeChartProps = { segments: [], chartValue: 100, theme: DarkTheme };
 
 runTest('_processProps', () => {
@@ -38,13 +38,131 @@ runTest('_processProps', () => {
 });
 
 runTest('_getMargins', () => {
-  test('Should return zero arcs when there is no data', () => {
-    const instance = new GaugeChartBase(data);
+  test('Should return margins data properly when hideMinMax is false', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: false,
+    });
     expect(instance).toBeDefined();
     const result = instance._getMargins();
     expect(result.bottom).toEqual(16);
     expect(result.top).toEqual(18);
     expect(result.left).toEqual(56);
     expect(result.right).toEqual(56);
+  });
+
+  test('Should return margins data properly when hideMinMax is true', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: true,
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(16);
+    expect(result.top).toEqual(18);
+    expect(result.left).toEqual(16);
+    expect(result.right).toEqual(16);
+  });
+
+  test('Should return margins data properly when hideMinMax is false and chart title is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: false,
+      chartTitle: 'Gauge Chart',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(16);
+    expect(result.top).toEqual(43);
+    expect(result.left).toEqual(56);
+    expect(result.right).toEqual(56);
+  });
+
+  test('Should return margins data properly when hideMinMax is true and chart title is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: true,
+      chartTitle: 'Gauge Chart',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(16);
+    expect(result.top).toEqual(43);
+    expect(result.left).toEqual(16);
+    expect(result.right).toEqual(16);
+  });
+
+  test('Should return margins data properly when hideMinMax is false and sublabel text is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: false,
+      sublabel: 'Chart Data',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(36);
+    expect(result.top).toEqual(18);
+    expect(result.left).toEqual(56);
+    expect(result.right).toEqual(56);
+  });
+
+  test('Should return margins data properly when hideMinMax is true and sublabel text is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: true,
+      sublabel: 'Chart Data',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(36);
+    expect(result.top).toEqual(18);
+    expect(result.left).toEqual(16);
+    expect(result.right).toEqual(16);
+  });
+
+  test('Should return margins data properly when hideMinMax is false and chartTitle,sublabel text is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: false,
+      chartTitle: 'Gauge Chart',
+      sublabel: 'Chart data',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(36);
+    expect(result.top).toEqual(43);
+    expect(result.left).toEqual(56);
+    expect(result.right).toEqual(56);
+  });
+
+  test('Should return margins data properly when hideMinMax is true and chartTitle,sublabel text is available', () => {
+    const instance = new GaugeChartBase({
+      segments: segments,
+      chartValue: 100,
+      theme: DarkTheme,
+      hideMinMax: true,
+      chartTitle: 'Gauge Chart',
+      sublabel: 'Chart data',
+    });
+    expect(instance).toBeDefined();
+    const result = instance._getMargins();
+    expect(result.bottom).toEqual(36);
+    expect(result.top).toEqual(43);
+    expect(result.left).toEqual(16);
+    expect(result.right).toEqual(16);
   });
 });
