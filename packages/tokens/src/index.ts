7f42a904e1574bb46b2ca5c9fef3c56f32675451
adri <orantesadri1@gmail.com>
2024-02-29 11:45:19 -0800

    Revert "added azure light theme to storybook and theme folder"
    This reverts commit e74f1fa7dc511ecc4de32b2f3e393b6a031c8ec1.

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,5 +1,18 @@
-export { teamsDarkTheme, teamsHighContrastTheme, teamsLightTheme, webDarkTheme, webLightTheme } from './themes/index';
-export { createDarkTheme, createHighContrastTheme, createLightTheme, createTeamsDarkTheme } from './utils/index';
+export {
+  teamsDarkTheme,
+  teamsHighContrastTheme,
+  teamsLightTheme,
+  webDarkTheme,
+  webLightTheme,
+  azureDarkTheme,
+} from './themes/index';
+export {
+  createDarkTheme,
+  createHighContrastTheme,
+  createLightTheme,
+  createTeamsDarkTheme,
+  createAzureDarkTheme,
+} from './utils/index';
 
 export { themeToTokensObject } from './themeToTokensObject';
 export { tokens } from './tokens';
--- packages/tokens/src/themes/azure/lightThemeAzure.ts
+++
@@ -1,5 +0,0 @@
-import { createAzureLightTheme } from '../../utils/createAzureLightTheme';
-import { brandAzure } from '../../global/brandColors';
-import type { Theme } from '../../types';
-
-export const azureLightTheme: Theme = createAzureLightTheme(brandAzure);
