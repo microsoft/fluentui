bf8e3e02797abe5c54c96600e6f8b9524a12f426
Robert Penner <robert@robertpenner.com>
2024-02-29 18:02:36 +0000

    refactor(motion): give motionTokens both grouped export and flat export - Remove template literal string validation which isn't supported in TS 3.9

--- packages/react-components/react-motions-preview/src/index.ts
+++ packages/react-components/react-motions-preview/src/index.ts
@@ -1,11 +1,9 @@
-import * as motionTokens from './motions/motionTokens';
+export { motionTokens, durations, curves } from './motions/motionTokens';
 
 export { createMotionComponent } from './factories/createMotionComponent';
 export { createPresenceComponent } from './factories/createPresenceComponent';
 
 export { PresenceGroup } from './components/PresenceGroup';
-
-export { motionTokens };
 
 export type { AtomMotion, AtomMotionFn, PresenceMotion, PresenceMotionFn, MotionImperativeRef } from './types';
 
--- packages/react-components/react-motions-preview/src/motions/motionTokens.ts
+++ packages/react-components/react-motions-preview/src/motions/motionTokens.ts
@@ -1,45 +1,15 @@
-export const durationUltraFast = 50;
-export const durationFaster = 100;
-export const durationFast = 150;
-export const durationNormal = 200;
-export const durationSlow = 300;
-export const durationSlower = 400;
-export const durationUltraSlow = 500;
+export const durations = {
+  durationUltraFast: 50,
+  durationFaster: 100,
+  durationFast: 150,
+  durationNormal: 200,
+  durationGentle: 250,
+  durationSlow: 300,
+  durationSlower: 400,
+  durationUltraSlow: 500,
+} as const;
 
-export const easingAccelerateMax = 'cubic-bezier(0.9,0.1,1,0.2)';
-export const easingAccelerateMid = 'cubic-bezier(1,0,1,1)';
-export const easingAccelerateMin = 'cubic-bezier(0.8,0,0.78,1)';
-export const easingDecelerateMax = 'cubic-bezier(0.1,0.9,0.2,1)';
-export const easingDecelerateMid = 'cubic-bezier(0,0,0,1)';
-export const easingDecelerateMin = 'cubic-bezier(0.33,0,0.1,1)';
-export const easingEasyEaseMax = 'cubic-bezier(0.8,0,0.2,1)';
-export const easingEasyEase = 'cubic-bezier(0.33,0,0.67,1)';
-export const easingLinear = 'cubic-bezier(0,0,1,1)';
-
-// TODO: settle on a token naming and structure, i.e. flat vs objects
-// Below matches @fluentui/tokens, which currently only export these as CSS variables.
-// If we make @fluentui/tokens export the JS values as well, we wouldn't have to duplicate these here.
-// For simplicity, let's not rename `curve...` to `easing...`.
-// These tokens will be renamed to the new Fluent scheme anyway.
-
-type DurationKey = `duration${Capitalize<string>}`;
-type DurationValue = `${number}ms`;
-
-export const durations: Record<DurationKey, DurationValue> = {
-  durationUltraFast: '50ms',
-  durationFaster: '100ms',
-  durationFast: '150ms',
-  durationNormal: '200ms',
-  durationGentle: '250ms',
-  durationSlow: '300ms',
-  durationSlower: '400ms',
-  durationUltraSlow: '500ms',
-};
-
-type CurveKey = `curve${Capitalize<string>}`;
-type CurveValue = `cubic-bezier(${number},${number},${number},${number})`;
-
-export const curves: Record<CurveKey, CurveValue> = {
+export const curves = {
   curveAccelerateMax: 'cubic-bezier(0.9,0.1,1,0.2)',
   curveAccelerateMid: 'cubic-bezier(1,0,1,1)',
   curveAccelerateMin: 'cubic-bezier(0.8,0,0.78,1)',
@@ -49,4 +19,16 @@ export const curves: Record<CurveKey, CurveValue> = {
   curveEasyEaseMax: 'cubic-bezier(0.8,0,0.2,1)',
   curveEasyEase: 'cubic-bezier(0.33,0,0.67,1)',
   curveLinear: 'cubic-bezier(0,0,1,1)',
+} as const;
+
+export const motionTokens = {
+  // For flat lookup
+  ...durations,
+  ...curves,
 };
+
+// TODO: consider when TypeScript 4.4 is allowed:
+// type DurationKey = `duration${Capitalize<string>}`;
+// type DurationValue = `${number}ms`;
+// type CurveKey = `curve${Capitalize<string>}`;
+// type CurveValue = `cubic-bezier(${number},${number},${number},${number})`;
