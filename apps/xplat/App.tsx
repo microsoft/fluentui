4ee1cf8edb52be0e5ded4be918f7c475b8ddc63c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-02-29 11:48:31 +0100

    remove unused imports

--- apps/xplat/App.tsx
+++ apps/xplat/App.tsx
@@ -1,13 +1,7 @@
-import React from 'react';
 import type { PropsWithChildren } from 'react';
+import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
 import { StrictDomDemo } from './StrictDomDemo';
 
 type SectionProps = PropsWithChildren<{
