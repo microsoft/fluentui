ebaa4f3a81246de9311b72600e6d5dd98240c383
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2024-03-01 14:38:45 +0100

    feat(react-tag-picker-preview): initial implementation (#30659)

---
+++ packages/react-components/react-tag-picker-preview/src/components/TagPickerOption/TagPickerOption.test.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { isConformant } from '../../testing/isConformant';
+import { TagPickerOption } from './TagPickerOption';
+
+describe('TagPickerOption', () => {
+  isConformant({
+    Component: TagPickerOption,
+    displayName: 'TagPickerOption',
+    requiredProps: { value: 'value', media: <></>, secondaryContent: <></> },
+  });
+
+  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
+
+  it('renders a default state', () => {
+    const result = render(<TagPickerOption value="value">Default TagPickerOption</TagPickerOption>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
