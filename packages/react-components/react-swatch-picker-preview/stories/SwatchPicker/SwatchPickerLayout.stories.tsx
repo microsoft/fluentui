fb2afa4a15e3755aaa738c37303da8c8d24fecca
Valentyna <vkozlova@microsoft.com>
2024-03-06 09:51:14 +0100

    Added color sets in a popover

--- packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerLayout.stories.tsx
+++ packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerLayout.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { makeStyles, shorthands } from '@fluentui/react-components';
+import { makeStyles, shorthands, Button, Popover, PopoverSurface, PopoverTrigger } from '@fluentui/react-components';
 import {
   SwatchPicker,
   ColorSwatch,
@@ -14,6 +14,13 @@ const useStyles = makeStyles({
     ...shorthands.border('1px', 'solid', '#ccc'),
     ...shorthands.margin('20px', '0'),
   },
+  colorButton: {
+    minWidth: '32px',
+    height: '32px',
+    ...shorthands.borderRadius(0),
+    ...shorthands.border('none'),
+    ...shorthands.margin('0', '10px'),
+  },
 });
 
 export const SwatchPickerLayout = () => {
@@ -22,6 +29,13 @@ export const SwatchPickerLayout = () => {
   const handleSelect: SwatchPickerOnSelectEventHandler = (_, data) => {
     setSelectedValue(data.selectedValue);
     setSelectedColor(data.selectedColor);
+  };
+
+  const [selectedValue1, setSelectedValue1] = React.useState('00B053');
+  const [selectedColor1, setSelectedColor1] = React.useState('#00B053');
+  const handleSelect1: SwatchPickerOnSelectEventHandler = (_, data) => {
+    setSelectedValue1(data.selectedValue);
+    setSelectedColor1(data.selectedColor);
   };
 
   const styles = useStyles();
@@ -66,6 +80,36 @@ export const SwatchPickerLayout = () => {
           backgroundColor: selectedColor,
         }}
       />
+      <h3>Color sets</h3>
+      Click to change color
+      <Popover trapFocus>
+        <PopoverTrigger disableButtonEnhancement>
+          <Button className={styles.colorButton} style={{ backgroundColor: selectedColor1 }} />
+        </PopoverTrigger>
+        <PopoverSurface>
+          <SwatchPicker
+            aria-label="SwatchPicker default"
+            selectedValue={selectedValue1}
+            onSelectionChange={handleSelect1}
+          >
+            <SwatchPickerRow>
+              <ColorSwatch color="#FF1921" value="FF1921" aria-label="red" />
+              <ColorSwatch color="#FFC12E" value="FFC12E" aria-label="orange" />
+              <ColorSwatch color="#FEFF37" value="FEFF37" aria-label="yellow" />
+            </SwatchPickerRow>
+            <SwatchPickerRow>
+              <ColorSwatch color="#90D057" value="90D057" aria-label="light green" />
+              <ColorSwatch color="#00B053" value="00B053" aria-label="green" />
+              <ColorSwatch color="#00AFED" value="00AFED" aria-label="light blue" />
+            </SwatchPickerRow>
+            <SwatchPickerRow>
+              <ColorSwatch color="#006EBD" value="006EBD" aria-label="blue" />
+              <ColorSwatch color="#011F5E" value="011F5E" aria-label="dark blue" />
+              <ColorSwatch color="#712F9E" value="712F9E" aria-label="purple" />
+            </SwatchPickerRow>
+          </SwatchPicker>
+        </PopoverSurface>
+      </Popover>
     </>
   );
 };
