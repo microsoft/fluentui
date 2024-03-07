e8bd2da01d880451ad901601152cded568b5b5de
Robert Penner <robert@robertpenner.com>
2024-03-07 08:51:40 +0000

    refactor(motion): make motion tokens' structure & naming like @fluentui/tokens (durations & easing curves)

--- packages/react-components/react-motions-preview/src/index.ts
+++ packages/react-components/react-motions-preview/src/index.ts
@@ -1,10 +1,8 @@
-import * as motionTokens from './motions/motionTokens';
+export { motionTokens, durations, curves } from './motions/motionTokens';
 
 export { createMotionComponent } from './factories/createMotionComponent';
 export { createPresenceComponent } from './factories/createPresenceComponent';
 
 export { PresenceGroup } from './components/PresenceGroup';
-
-export { motionTokens };
 
 export type { AtomMotion, AtomMotionFn, PresenceMotion, PresenceMotionFn, MotionImperativeRef } from './types';
