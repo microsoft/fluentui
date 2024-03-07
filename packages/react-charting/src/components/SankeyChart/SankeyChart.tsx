728d7a3866f445ceecbaa8aa07b1b6193fc2dc2e
Laxmi Bhavani Malkareddy <lamalkar@microsoft.com>
2024-03-07 14:45:32 -0800

    test

--- packages/react-charting/src/components/SankeyChart/SankeyChart.tsx
+++ packages/react-charting/src/components/SankeyChart/SankeyChart.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
 import { styled } from '@fluentui/react/lib/Utilities';
-import { ISankeyChartProps, ISankeyChartStyleProps, ISankeyChartStyles } from './SankeyChart.types';
+import * as React from 'react';
 import { SankeyChartBase } from './SankeyChart.base';
 import { getStyles } from './SankeyChart.styles';
+import { ISankeyChartProps, ISankeyChartStyleProps, ISankeyChartStyles } from './SankeyChart.types';
 
 // Create a SankeyChart variant which uses these default styles and this styled subcomponent.
 /**
- * Sankeychart component
+ * Sankeychart component.
  * {@docCategory SankeyChart}
  */
 export const SankeyChart: React.FunctionComponent<ISankeyChartProps> = styled<
