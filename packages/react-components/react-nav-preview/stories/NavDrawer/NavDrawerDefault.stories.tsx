85f00294e86710f3a83b943d7c9bf839109a4ade
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-03-11 15:34:25 -0700

    pre-pr polishing

--- packages/react-components/react-nav-preview/stories/NavDrawer/NavDrawerDefault.stories.tsx
+++ packages/react-components/react-nav-preview/stories/NavDrawer/NavDrawerDefault.stories.tsx
@@ -9,31 +9,49 @@ import {
   NavSubItemGroup,
 } from '@fluentui/react-nav-preview';
 import { DrawerBody } from '@fluentui/react-drawer';
+import { makeStyles, shorthands } from '@griffel/react';
+import { tokens } from '@fluentui/react-theme';
+
+const useStyles = makeStyles({
+  root: {
+    ...shorthands.overflow('hidden'),
+    display: 'flex',
+    height: '480px', // arbitrary value, for demo purposes only
+    // arbitrary value, for dramatic effect only
+    // I know this is ugly and wrong, but styling is coming, I promise.
+
+    backgroundColor: tokens.colorBackgroundOverlay,
+  },
+});
 
 export const Default = (props: Partial<NavDrawerProps>) => {
+  const styles = useStyles();
+
   return (
-    <NavDrawer defaultSelectedValue={'10'} defaultSelectedCategoryValue={'8'} size="small" separator={true} open={true}>
-      <DrawerBody>
-        <NavItem value="1">First</NavItem>
-        <NavItem value="2">Second</NavItem>
-        <NavItem value="3">Third</NavItem>
-        <NavCategory value="4">
-          <NavCategoryItem>NavCategoryItem 1</NavCategoryItem>
-          <NavSubItemGroup>
-            <NavSubItem value="5">Five</NavSubItem>
-            <NavSubItem value="6">Six</NavSubItem>
-            <NavSubItem value="7">Seven</NavSubItem>
-          </NavSubItemGroup>
-        </NavCategory>
-        <NavCategory value="8">
-          <NavCategoryItem>NavCategoryItem2</NavCategoryItem>
-          <NavSubItemGroup>
-            <NavSubItem value="9">Nine</NavSubItem>
-            <NavSubItem value="10">Ten</NavSubItem>
-            <NavSubItem value="11">Eleven</NavSubItem>
-          </NavSubItemGroup>
-        </NavCategory>
-      </DrawerBody>
-    </NavDrawer>
+    <div className={styles.root}>
+      <NavDrawer defaultSelectedValue={'10'} defaultSelectedCategoryValue={'8'} size="small" open={true}>
+        <DrawerBody>
+          <NavItem value="1">First</NavItem>
+          <NavItem value="2">Second</NavItem>
+          <NavItem value="3">Third</NavItem>
+          <NavCategory value="4">
+            <NavCategoryItem>NavCategoryItem 1</NavCategoryItem>
+            <NavSubItemGroup>
+              <NavSubItem value="5">Five</NavSubItem>
+              <NavSubItem value="6">Six</NavSubItem>
+              <NavSubItem value="7">Seven</NavSubItem>
+            </NavSubItemGroup>
+          </NavCategory>
+          <NavCategory value="8">
+            <NavCategoryItem>NavCategoryItem2</NavCategoryItem>
+            <NavSubItemGroup>
+              <NavSubItem value="9">Nine</NavSubItem>
+              <NavSubItem value="10">Ten</NavSubItem>
+              <NavSubItem value="11">Eleven</NavSubItem>
+            </NavSubItemGroup>
+          </NavCategory>
+        </DrawerBody>
+      </NavDrawer>
+    </div>
   );
 };
