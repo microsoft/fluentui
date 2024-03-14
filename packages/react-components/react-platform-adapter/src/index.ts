68debe6bf57c298871929fcface637c2904fe20c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-03-14 21:50:33 +0100

    fix: ensure package is tree-shakeable (#30776)

--- packages/react-components/react-platform-adapter/src/index.ts
+++ packages/react-components/react-platform-adapter/src/index.ts
@@ -1,12 +1,12 @@
-export { XPlatProvider, suppressCssVariableInsertion, type XPlatProviderProps } from './XPlatProvider';
-export { jsxPlatformAdapter } from './jsx';
-export {
-  getStylesFromClassName,
-  makeStyles,
-  makeStylesCore,
-  makeResetStyles,
-  mergeClasses,
-  shorthands,
-  TextDirectionProvider,
-  useRenderer_unstable,
-} from './styling/index';
+export { XPlatProvider, suppressCssVariableInsertion } from './XPlatProvider/XPlatProvider';
+export type { XPlatProviderProps } from './XPlatProvider/XPlatProvider.types';
+export { jsxPlatformAdapter } from './jsx/jsxPlatformAdapter';
+export { getStylesFromClassName } from './styling/classNameMap';
+export { makeResetStyles } from './styling/makeResetStyles';
+export { makeStyles } from './styling/makeStyles';
+export { mergeClasses } from './styling/mergeClasses';
+export { shorthands } from './styling/shorthands';
+
+// re-export some griffel types to have fluent use the griffel adapter instead of griffel directly
+export { useRenderer_unstable, TextDirectionProvider } from '@griffel/react';
+export { makeStyles as makeStylesCore } from '@griffel/core';
--- packages/react-components/react-platform-adapter/src/jsx/jsxPlatformAdapter.ts
+++ packages/react-components/react-platform-adapter/src/jsx/jsxPlatformAdapter.ts
@@ -1,4 +1,4 @@
-import { JSXRuntime } from './types';
+import type { JSXRuntime } from './types';
 
 export const jsxPlatformAdapter = (reactJsx: JSXRuntime) => {
   return reactJsx;
