5770446fc25f9598993c1a534ff1a774ec2c8268
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-03-14 09:39:31 +0100

    fix: ensure package is tree-shakeable

--- packages/react-components/react-platform-adapter/src/styling/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './classNameMap';
-export * from './makeResetStyles';
-export * from './makeStyles';
-export * from './mergeClasses';
-export * from './shorthands';
-// re-export some griffel types to have fluent use the griffel adapter instead of griffel directly
-export { useRenderer_unstable, TextDirectionProvider } from '@griffel/react';
-export { makeStyles as makeStylesCore } from '@griffel/core';
