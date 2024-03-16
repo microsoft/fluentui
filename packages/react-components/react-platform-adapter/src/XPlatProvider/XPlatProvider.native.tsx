7e66dfac7740df4111b3eebc76d5d9701e2a37ba
Ben Howell <48106640+behowell@users.noreply.github.com>
2024-03-16 00:09:49 -0700

    fix(xplat): XPlatProvider correctly formats variable names for strict dom ThemeProvider (#30792)

--- packages/react-components/react-platform-adapter/src/XPlatProvider/XPlatProvider.native.tsx
+++ packages/react-components/react-platform-adapter/src/XPlatProvider/XPlatProvider.native.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { contexts } from 'react-strict-dom';
 import { type XPlatProviderProps } from './XPlatProvider.types';
-import { createCustomVariablesFromTheme } from './createCustomVariablesFromTheme';
 
 // css variables insertion should be suppressed in native
 export const suppressCssVariableInsertion = true;
 
 export const XPlatProvider: React.FunctionComponent<XPlatProviderProps> = props => {
-  const { theme, children } = props;
-  const customProps = React.useMemo(() => createCustomVariablesFromTheme(theme), [theme]);
+  const { theme = {}, children } = props;
 
-  // customProperties should be in the form of { '--camelCasedVariable': 'value' }
-  return <contexts.ThemeProvider customProperties={customProps} children={children} />;
+  // customProperties should be in the form of { 'camelCasedVariable': 'value' }
+  return <contexts.ThemeProvider customProperties={theme} children={children} />;
 };
