e866c29ba630a5b1011abced251b4bb3d65280f3
Ben Howell <48106640+behowell@users.noreply.github.com>
2024-03-16 00:11:35 -0700

    chore(xplat): Add xplat support to Button (#30793)
    Hook up Button to support cross-platform styles. Cross-plat reset styles don't support shorthands, so need to convert the styles to expand shorthands.

--- apps/xplat-v9/StrictDomDemo.tsx
+++ apps/xplat-v9/StrictDomDemo.tsx
@@ -1,34 +1,21 @@
 import * as React from 'react';
 
+import { Button } from '@fluentui/react-button';
 import { Label } from '@fluentui/react-label';
-import {
-  getStylesFromClassName,
-  makeResetStyles,
-  makeStyles,
-  mergeClasses,
-  shorthands,
-} from '@fluentui/react-platform-adapter';
-import { html } from 'react-strict-dom';
+import { getStylesFromClassName, makeStyles, shorthands } from '@fluentui/react-platform-adapter';
 import { FluentProvider } from '@fluentui/react-provider';
-import { webLightTheme } from '@fluentui/react-theme';
-
-const useBaseClassName = makeResetStyles({
-  marginBlock: '1rem',
-  padding: '10px',
-});
+import { tokens, webLightTheme } from '@fluentui/react-theme';
+import { html } from 'react-strict-dom';
 
 const useClassNames = makeStyles({
   root: {
     ...shorthands.border('2px', 'solid', 'red'),
-    color: '#222',
-    backgroundColor: '#EEE',
-    ':hover': {
-      color: 'blue',
-      backgroundColor: '#DDD',
-    },
-  },
-  cond: {
-    ...shorthands.borderWidth('5px'),
+    backgroundColor: tokens.colorNeutralBackground2,
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: '10px',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
@@ -39,10 +26,9 @@ export const StrictDomDemo = (props: { cond?: boolean }) => {
 
   return (
     <FluentProvider theme={webLightTheme}>
-      <html.div
-        style={getStylesFromClassName(mergeClasses(useBaseClassName(), classNames.root, cond && classNames.cond))}
-      >
-        <Label required>This is a label</Label>
+      <html.div style={getStylesFromClassName(classNames.root)}>
+        <Label required>Required label</Label>
+        <Button appearance="primary">Primary button</Button>
       </html.div>
     </FluentProvider>
   );
