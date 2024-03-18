f3a6a735635b03aece784b18f4c9917159f2117b
Valentyna <vkozlova@microsoft.com>
2024-03-18 11:17:34 +0100

    Updated tests

--- packages/react-components/react-swatch-picker-preview/src/components/ColorSwatch/ColorSwatch.test.tsx
+++ packages/react-components/react-swatch-picker-preview/src/components/ColorSwatch/ColorSwatch.test.tsx
@@ -14,7 +14,7 @@ describe('ColorSwatch', () => {
     expect(result.container).toMatchInlineSnapshot(`
       <div>
         <button
-          aria-selected="false"
+          aria-checked="false"
           class="fui-ColorSwatch"
           role="radio"
           style="--fui-SwatchPicker--color: #f09;"
