fd89630895495f072b46b3cfc8e6de8b255d4e92
Valentina <vkozlova@microsoft.com>
2024-03-18 14:25:00 +0100

    docs(react-swatch-picker): Added stories for Size, Shape and Spacing (#30781)

--- packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/index.stories.tsx
+++ packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/index.stories.tsx
@@ -4,6 +4,10 @@ import descriptionMd from './SwatchPickerDescription.md';
 import bestPracticesMd from './SwatchPickerBestPractices.md';
 
 export { Default } from './SwatchPickerDefault.stories';
+export { SwatchPickerSize } from './SwatchPickerSize.stories';
+export { SwatchPickerSpacing } from './SwatchPickerSpacing.stories';
+export { SwatchPickerShape } from './SwatchPickerShape.stories';
+export { SwatchPickerPopup } from './SwatchPickerPopup.stories';
 
 export default {
   title: 'Preview Components/SwatchPicker',
