dd286f8025c48aa16d8fde3e595d96df43e5ef72
Mitch-At-Work <mifraser@microsoft.com>
2024-03-20 11:25:04 -0700

    Update to enable arrow key navigation when nav group is focused or internal focused

--- packages/react-components/react-teaching-popover-preview/src/components/TeachingPopoverCarouselNav/useTeachingPopoverCarouselNav.tsx
+++ packages/react-components/react-teaching-popover-preview/src/components/TeachingPopoverCarouselNav/useTeachingPopoverCarouselNav.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { getIntrinsicElementProps, slot } from '@fluentui/react-utilities';
+import { getIntrinsicElementProps, mergeCallbacks, slot, useEventCallback } from '@fluentui/react-utilities';
 import type {
   TeachingPopoverCarouselNavProps,
   TeachingPopoverCarouselNavState,
 } from './TeachingPopoverCarouselNav.types';
+import { ArrowUp, ArrowDown, ArrowLeft, ArrowRight } from '@fluentui/keyboard-keys';
 
 import { useFocusableGroup } from '@fluentui/react-tabster';
 import { TeachingPopoverCarouselNavButton } from '../TeachingPopoverCarouselNavButton/index';
 import { useTeachingPopoverCarouselContext_unstable } from '../TeachingPopoverCarousel/TeachingPopoverCarouselContext';
+import { useRef } from 'react';
 
 /**
  * Returns the props and state required to render the component
@@ -21,6 +23,31 @@ export const useTeachingPopoverCarouselNav_unstable = (
   const focusableGroupAttr = useFocusableGroup({ tabBehavior: 'limited' });
   const totalPages = useTeachingPopoverCarouselContext_unstable(context => context.totalPages);
   const currentPage = useTeachingPopoverCarouselContext_unstable(context => context.currentPage);
+  const setCurrentPage = useTeachingPopoverCarouselContext_unstable(context => context.setCurrentPage);
+
+  let handleKeyDown = useEventCallback((ev: React.KeyboardEvent<HTMLDivElement>) => {
+    props.onKeyDown?.(ev);
+
+    if (ev.isDefaultPrevented()) {
+      return;
+    }
+
+    const key = ev.key;
+
+    if (key === ArrowUp || key === ArrowLeft) {
+      let prevPage = Math.max(0, currentPage - 1);
+      setCurrentPage(prevPage);
+    }
+
+    if (key === ArrowDown || key === ArrowRight) {
+      let nextPage = Math.min(totalPages - 1, currentPage + 1);
+      setCurrentPage(nextPage);
+    }
+  });
+
+  if (props.onKeyDown) {
+    handleKeyDown = mergeCallbacks(handleKeyDown, props.onKeyDown);
+  }
 
   // Generate the child TeachingPopoverCarouselNavButton and memoize them to prevent unnecessary re-rendering
   const rootChildren = React.useMemo(() => {
@@ -40,6 +67,7 @@ export const useTeachingPopoverCarouselNav_unstable = (
         tabIndex: 0,
         ...props,
         children: rootChildren,
+        onKeyDown: handleKeyDown,
         ...focusableGroupAttr,
       }),
       { elementType: 'div' },
