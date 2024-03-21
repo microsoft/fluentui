e5f3a519934bd89ffb3502312f63f460ad828a80
Robert Penner <robert@robertpenner.com>
2024-03-21 16:08:41 +0000

    chore(motion): comment out overrides in renderAccordionPanel

--- packages/react-components/react-accordion/src/components/AccordionPanel/renderAccordionPanel.tsx
+++ packages/react-components/react-accordion/src/components/AccordionPanel/renderAccordionPanel.tsx
@@ -3,13 +3,14 @@
 
 import { assertSlots } from '@fluentui/react-utilities';
 import type { AccordionPanelState, AccordionPanelSlots } from './AccordionPanel.types';
-import { Collapse, durations, curves } from '@fluentui/react-motions-preview';
+// import { Collapse, durations, curves } from '@fluentui/react-motions-preview';
+import { Collapse } from '@fluentui/react-motions-preview';
 
 // TODO: allow the motion to be injected and enable that from Storybook
 // TODO: unify this open/close duration with the header's chevron rotation duration
-const motionDuration = durations.durationNormal;
-const enterEasing = curves.curveDecelerateMid;
-const exitEasing = curves.curveDecelerateMid;
+// const motionDuration = durations.durationNormal;
+// const enterEasing = curves.curveDecelerateMid;
+// const exitEasing = curves.curveDecelerateMid;
 
 /**
  * Function that renders the final JSX of the component
@@ -18,15 +19,14 @@ export const renderAccordionPanel_unstable = (state: AccordionPanelState) => {
   assertSlots<AccordionPanelSlots>(state);
   // Wrap child content in a Collapse transition which manages show/hide
   return (
-    <Collapse
-      visible={state.open}
-      override={{
-        all: { duration: motionDuration },
-        enter: { easing: enterEasing },
-        exit: { easing: exitEasing },
-      }}
-    >
-      {<state.root>{state.root.children}</state.root>}
-    </Collapse>
+    // <Collapse
+    //   visible={state.open}
+    //   override={{
+    //     all: { duration: motionDuration },
+    //     enter: { easing: enterEasing },
+    //     exit: { easing: exitEasing },
+    //   }}
+    // >
+    <Collapse>{<state.root>{state.root.children}</state.root>}</Collapse>
   );
 };
