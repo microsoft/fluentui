b6e9fdefdfe32bd6e23c1ea0d854c5977b730ed8
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-09 09:35:06 -0700

    feat(react-nav-preview): Add styles to NavItem and add icon prop (#30955)

--- packages/react-components/react-nav-preview/src/components/NavItem/NavItem.test.tsx
+++ packages/react-components/react-nav-preview/src/components/NavItem/NavItem.test.tsx
@@ -1,9 +1,22 @@
 import { isConformant } from '../../testing/isConformant';
 import { NavItem } from './NavItem';
+import { navItemClassNames } from './useNavItemStyles.styles';
 
 describe('NavItem', () => {
   isConformant({
     Component: NavItem,
     displayName: 'NavItem',
+    testOptions: {
+      'has-static-classnames': [
+        {
+          props: { icon: 'Test Icon', content: 'Some Content' },
+          expectedClassNames: {
+            root: navItemClassNames.root,
+            content: navItemClassNames.content,
+            icon: navItemClassNames.icon,
+          },
+        },
+      ],
+    },
   });
 });
