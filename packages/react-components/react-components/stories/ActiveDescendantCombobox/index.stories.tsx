51797229e99f11f7de9358db140e095a8c85d001
Adam Samec <asamec@microsoft.com>
2024-04-16 18:44:26 +0200

    Fix export

--- packages/react-components/react-components/stories/ActiveDescendantCombobox/index.stories.tsx
+++ packages/react-components/react-components/stories/ActiveDescendantCombobox/index.stories.tsx
@@ -1,5 +1,6 @@
-export { GridActiveDescendantComboboxCellNavigation } from './GridActiveDescendantComboboxCellNavigation.stories';
+export { GridActiveDescendantComboboxCompositeNavigationRowNarration } from './GridActiveDescendantComboboxCompositeNavigationRowNarration.stories';
 export { GridActiveDescendantComboboxCompositeNavigation } from './GridActiveDescendantComboboxCompositeNavigation.stories';
+export { GridActiveDescendantComboboxCellNavigation } from './GridActiveDescendantComboboxCellNavigation.stories';
 
 export default {
   title: 'Concepts/Developer/Accessibility/ActiveDescendantComboboxes',
