0f9d062e7ddeeacb79703b6a25b34b2d73cf86e3
Valentina <vkozlova@microsoft.com>
2024-04-17 14:57:52 +0200

    fix(react-swatch-picker): a11y attrs for ImageSwatch (#31060)

--- packages/react-components/react-swatch-picker-preview/src/components/ImageSwatch/ImageSwatch.test.tsx
+++ packages/react-components/react-swatch-picker-preview/src/components/ImageSwatch/ImageSwatch.test.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { ImageSwatch } from './ImageSwatch';
+import { SwatchPickerProvider, swatchPickerContextDefaultValue } from '../../contexts/swatchPicker';
 
 describe('ImageSwatch', () => {
   isConformant({
@@ -14,10 +15,56 @@ describe('ImageSwatch', () => {
     expect(result.container).toMatchInlineSnapshot(`
       <div>
         <button
+          aria-checked="false"
           class="fui-ImageSwatch"
+          role="radio"
           style="background-image: url(path/img.png);"
         />
       </div>
     `);
+  });
+
+  it('selected when clicked', () => {
+    const onSelect = jest.fn();
+    const contextValue = {
+      ...swatchPickerContextDefaultValue,
+      requestSelectionChange: onSelect,
+    };
+
+    const result = render(
+      <SwatchPickerProvider value={contextValue}>
+        <ImageSwatch src="path/img.png" value="img" />
+      </SwatchPickerProvider>,
+    );
+
+    const swatch = result.getByRole('radio');
+    fireEvent.click(swatch);
+    expect(onSelect).toHaveBeenCalledWith(expect.anything(), { selectedSwatch: 'path/img.png', selectedValue: 'img' });
+  });
+
+  it('has correct a11y attributes in a row layout', () => {
+    const result = render(
+      <SwatchPickerProvider value={swatchPickerContextDefaultValue}>
+        <ImageSwatch src="path/img.png" value="img" />
+      </SwatchPickerProvider>,
+    );
+
+    const swatch = result.getByRole('radio');
+    expect(swatch.getAttribute('aria-checked')).toBe('false');
+  });
+
+  it('has correct a11y attributes in a grid layout', () => {
+    const contextValue = {
+      ...swatchPickerContextDefaultValue,
+      isGrid: true,
+    };
+    const result = render(
+      <SwatchPickerProvider value={contextValue}>
+        <ImageSwatch src="path/img.png" value="img" />
+      </SwatchPickerProvider>,
+    );
+
+    const swatch = result.getByRole('gridcell');
+    expect(swatch.getAttribute('aria-selected')).toBe('false');
   });
 });
