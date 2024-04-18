14587fc37dcb0dcc93d746c802cc63951d9ad69c
Lingfan Gao <lingfangao@hotmail.com>
2024-04-18 07:27:56 +0000

    remove cruf

--- packages/react-components/react-menu/stories/MenuItemSwitch/MenuItemSwitchDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { MenuItemSwitch, MenuItemSwitchProps } from '@fluentui/react-menu';
-
-export const Default = (props: Partial<MenuItemSwitchProps>) => <MenuItemSwitch {...props} />;
