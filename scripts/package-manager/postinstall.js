dd539658d5544a9be6bdf7450cfa96d6f737caa1
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-04-22 10:28:43 -0700

    Upgrade Playwright to 1.43.1
    - Add explainer to download required Playwright binaries

--- scripts/package-manager/postinstall.js
+++ scripts/package-manager/postinstall.js
@@ -1,4 +1,5 @@
 const { spawnSync } = require('child_process');
+
 const chalk = require('chalk');
 
 const COMMAND_PREFIX = `${chalk.cyan('>')} ${chalk.inverse(chalk.bold(chalk.cyan(' GETTING STARTED ')))}`;
@@ -9,12 +10,16 @@ function main() {
   // git v2.9.0 supports a custom hooks directory. This means we just need to check in the hooks scripts.
   spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
 
-  console.log('\n');
-  console.log(COMMAND_PREFIX);
-  console.log('✅ All dependencies are installed! This repo no longer requires a build to start the inner loop.');
-  console.log(`🎬 For inner loop development, run: ${chalk.yellow('yarn start')}`);
-  console.log(`💡 To learn more about all the commands that this monorepo supports, see the wiki:
+  console.log(`
+${COMMAND_PREFIX}
+✅ Dependencies are installed! This repo no longer requires a build to start the inner loop.
 
-  https://github.com/microsoft/fluentui/wiki/Build-Commands
+🎭 Some projects use Playwright for tests.
+\tTo install/upgrade Playwright, run: ${chalk.cyan('yarn playwright install --with-deps')}
+
+🎬 ${chalk.bold(`For inner loop development, run: ${chalk.yellow('yarn start')}`)}
+
+💡 To learn more about all the commands that this monorepo supports, see the wiki:
+\thttps://github.com/microsoft/fluentui/wiki/Build-Commands
 `);
 }
