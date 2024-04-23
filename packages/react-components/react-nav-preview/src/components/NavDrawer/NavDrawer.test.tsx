645f160a90bbd9f2c89fa6437d33c120425ff01a
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-23 09:33:06 -0700

    Disable conformance tests

--- packages/react-components/react-nav-preview/src/components/NavDrawer/NavDrawer.test.tsx
+++ packages/react-components/react-nav-preview/src/components/NavDrawer/NavDrawer.test.tsx
@@ -1,9 +1,21 @@
 import { isConformant } from '../../testing/isConformant';
 import { NavDrawer } from './NavDrawer';
 
+/// Why are these tests disabled?
+/// Because the Drawer component uses the Dialog under the hood and Dialog does not handle ref,
+// The underlying Drawer component tests also disable these.
+
 describe('NavDrawer', () => {
   isConformant({
     Component: NavDrawer,
     displayName: 'NavDrawer',
+    disabledTests: [
+      'component-handles-ref',
+      'component-has-root-ref',
+      'component-handles-classname',
+      'component-has-static-classnames-object',
+      'consistent-callback-args',
+      'make-styles-overrides-win',
+    ],
   });
 });
