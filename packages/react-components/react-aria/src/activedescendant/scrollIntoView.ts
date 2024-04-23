9b5e6087314cbcc0577e53539f30769e89acc86a
Steven Countryman <stevenco@microsoft.com>
2024-04-23 16:11:40 -0700

    Light refactor

--- packages/react-components/react-aria/src/activedescendant/scrollIntoView.ts
+++ packages/react-components/react-aria/src/activedescendant/scrollIntoView.ts
@@ -1,45 +1,3 @@
-const getIntValueOfComputedStyle = (computedStyle: string) => {
-  return computedStyle ? parseInt(computedStyle, 10) : 0;
-};
-
-const getScrollMargins = (element: HTMLElement) => {
-  const computedStyles = getComputedStyle(element);
-  const scrollMarginTop =
-    getIntValueOfComputedStyle(computedStyles.scrollMarginTop) ??
-    getIntValueOfComputedStyle(computedStyles.scrollMarginBlockStart);
-  const scrollMarginBottom =
-    getIntValueOfComputedStyle(computedStyles.scrollMarginBottom) ??
-    getIntValueOfComputedStyle(computedStyles.scrollMarginBlockEnd);
-  return {
-    scrollMarginTop,
-    scrollMarginBottom,
-  };
-};
-
-const findScrollableParent = (element: HTMLElement | null): HTMLElement | null => {
-  if (!element) {
-    return null;
-  }
-
-  if (element.scrollHeight > element.offsetHeight) {
-    return element;
-  }
-
-  return findScrollableParent(element.parentElement);
-};
-
-const getTotalOffsetTop = (element: HTMLElement, scrollParent: HTMLElement): number => {
-  if (!element || element === scrollParent) {
-    return 0;
-  }
-
-  if (element.contains(scrollParent)) {
-    return scrollParent.offsetTop * -1;
-  }
-
-  return element.offsetTop + getTotalOffsetTop(element.offsetParent as HTMLElement, scrollParent);
-};
-
 export const scrollIntoView = (target: HTMLElement | null | undefined) => {
   if (!target || !parent) {
     return;
@@ -67,4 +25,47 @@ export const scrollIntoView = (target: HTMLElement | null | undefined) => {
   } else if (isBelow) {
     scrollParent.scrollTo(0, offsetTop + offsetHeight + scrollMarginBottom - parentOffsetHeight + buffer);
   }
+};
+
+const findScrollableParent = (element: HTMLElement | null): HTMLElement | null => {
+  if (!element) {
+    return null;
+  }
+
+  if (element.scrollHeight > element.offsetHeight) {
+    return element;
+  }
+
+  return findScrollableParent(element.parentElement);
+};
+
+const getTotalOffsetTop = (element: HTMLElement, scrollParent: HTMLElement): number => {
+  if (!element || element === scrollParent) {
+    return 0;
+  }
+
+  if (element.contains(scrollParent)) {
+    // subtract the scroll parent's offset top from the running total if the offsetParent is above it
+    return scrollParent.offsetTop * -1;
+  }
+
+  return element.offsetTop + getTotalOffsetTop(element.offsetParent as HTMLElement, scrollParent);
+};
+
+const getScrollMargins = (element: HTMLElement) => {
+  const computedStyles = getComputedStyle(element);
+  const scrollMarginTop =
+    getIntValueOfComputedStyle(computedStyles.scrollMarginTop) ??
+    getIntValueOfComputedStyle(computedStyles.scrollMarginBlockStart);
+  const scrollMarginBottom =
+    getIntValueOfComputedStyle(computedStyles.scrollMarginBottom) ??
+    getIntValueOfComputedStyle(computedStyles.scrollMarginBlockEnd);
+  return {
+    scrollMarginTop,
+    scrollMarginBottom,
+  };
+};
+
+const getIntValueOfComputedStyle = (computedStyle: string) => {
+  return computedStyle ? parseInt(computedStyle, 10) : 0;
 };
