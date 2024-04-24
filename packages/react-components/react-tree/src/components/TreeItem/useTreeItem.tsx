8b4337c108d7367763b3356fb15b3bbed0ddd571
Esteban Munoz <estebanmu@microsoft.com>
2024-04-24 10:24:38 -0700

    simplify line

--- packages/react-components/react-tree/src/components/TreeItem/useTreeItem.tsx
+++ packages/react-components/react-tree/src/components/TreeItem/useTreeItem.tsx
@@ -243,7 +243,7 @@ export function useTreeItem_unstable(props: TreeItemProps, ref: React.Ref<HTMLDi
         'aria-checked': selectionMode === 'multiselect' ? checked : undefined,
         // Casting: when selectionMode is 'single', checked is a boolean
         'aria-selected': ariaSelected !== undefined ? ariaSelected : selectionMode === 'single' ? !!checked : undefined,
-        'aria-expanded': ariaExpanded !== undefined ? ariaExpanded : itemType === 'branch' ? open : undefined,
+        'aria-expanded': ariaExpanded ?? (itemType === 'branch' ? open : undefined),
         onClick: handleClick,
         onKeyDown: handleKeyDown,
         onChange: handleChange,
