54bca2a68f3f90a28a7accb8800de8337d0db3ad
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-24 15:29:08 -0700

    update comment

--- packages/react-components/react-nav-preview/src/components/NavDrawer/NavDrawer.test.tsx
+++ packages/react-components/react-nav-preview/src/components/NavDrawer/NavDrawer.test.tsx
@@ -1,9 +1,12 @@
 import { isConformant } from '../../testing/isConformant';
 import { NavDrawer } from './NavDrawer';
 
-/// Why are these tests disabled?
-/// Because the Drawer component uses the Dialog under the hood and Dialog does not handle ref,
-// The underlying Drawer component tests also disable these.
+/**
+ * Why these tests are disabled:
+ * component-handles-ref|component-has-root-ref: Drawer (NavDrawer's base) uses the Dialog under the hood and Dialog do not handle ref, as it is a renderless component
+ * component-handles-classname|component-has-static-classnames-object|make-styles-overrides-win: Drawer uses the DialogSurface component to render the classname, so the main component do not handle classname.
+ * consistent-callback-args: Disabled that as the Drawer callback function uses the same signature as the Dialog, and Dialog has those tests disabled.
+ */
 
 describe('NavDrawer', () => {
   isConformant({
