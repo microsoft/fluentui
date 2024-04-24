eb8212a598d09a1c605f7b78cb15a8255cd34784
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-24 15:41:03 -0700

    render nav changes

--- packages/react-components/react-nav-preview/src/components/NavDrawer/renderNavDrawer.tsx
+++ packages/react-components/react-nav-preview/src/components/NavDrawer/renderNavDrawer.tsx
@@ -1,8 +1,6 @@
-/** @jsxRuntime automatic */
 /** @jsxImportSource @fluentui/react-jsx-runtime */
 
-import { assertSlots } from '@fluentui/react-utilities';
-import { DrawerSlots } from '@fluentui/react-drawer';
+import { DrawerState, renderDrawer_unstable } from '@fluentui/react-drawer';
 import { NavContextValues } from '../NavContext.types';
 import { NavProvider } from '../NavContext';
 import type { NavDrawerState } from './NavDrawer.types';
@@ -11,11 +9,5 @@ import type { NavDrawerState } from './NavDrawer.types';
  * Render the final JSX of NavDrawer
  */
 export const renderNavDrawer_unstable = (state: NavDrawerState, contextValues: NavContextValues) => {
-  assertSlots<DrawerSlots>(state);
-
-  return (
-    <NavProvider value={contextValues.nav}>
-      <state.root />
-    </NavProvider>
-  );
+  return <NavProvider value={contextValues.nav}>{renderDrawer_unstable(state as DrawerState)}</NavProvider>;
 };
