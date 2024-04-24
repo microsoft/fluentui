1c85f05df8e35cc3f56e9d947e65c394c46940e7
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-24 16:03:29 -0700

    remove comments

--- packages/react-components/react-nav-preview/src/components/NavDrawer/renderNavDrawer.tsx
+++ packages/react-components/react-nav-preview/src/components/NavDrawer/renderNavDrawer.tsx
@@ -6,9 +6,6 @@ import { NavContextValues } from '../NavContext.types';
 import { NavProvider } from '../NavContext';
 import type { NavDrawerState } from './NavDrawer.types';
 
-/**
- * Render the final JSX of NavDrawer
- */
 export const renderNavDrawer_unstable = (state: NavDrawerState, contextValues: NavContextValues) => {
   return <NavProvider value={contextValues.nav}>{renderDrawer_unstable(state as DrawerState)}</NavProvider>;
 };
