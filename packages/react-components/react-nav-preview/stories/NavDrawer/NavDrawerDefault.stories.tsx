72e358553d8ad3793fa9e79e179471c21c90bde4
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-04-30 15:23:06 -0700

    first styling pass

--- packages/react-components/react-nav-preview/stories/NavDrawer/NavDrawerDefault.stories.tsx
+++ packages/react-components/react-nav-preview/stories/NavDrawer/NavDrawerDefault.stories.tsx
@@ -8,20 +8,36 @@ import {
   NavSubItem,
   NavSubItemGroup,
 } from '@fluentui/react-nav-preview';
-import { DrawerBody, DrawerProps } from '@fluentui/react-drawer';
-import { Button, Label, Radio, RadioGroup, makeStyles, shorthands, tokens, useId } from '@fluentui/react-components';
-import { Folder20Filled, Folder20Regular, bundleIcon } from '@fluentui/react-icons';
+import { DrawerBody, DrawerFooter, DrawerHeader, DrawerHeaderNavigation, DrawerProps } from '@fluentui/react-drawer';
+import {
+  Button,
+  Caption1Strong,
+  Label,
+  Radio,
+  RadioGroup,
+  makeStyles,
+  shorthands,
+  tokens,
+  useId,
+} from '@fluentui/react-components';
+import {
+  Folder20Filled,
+  Folder20Regular,
+  NavigationFilled,
+  PersonFilled,
+  PersonRegular,
+  bundleIcon,
+} from '@fluentui/react-icons';
+import { navItemTokens } from '../../src/components/sharedNavStyles.styles';
 
 const useStyles = makeStyles({
   root: {
     ...shorthands.border('2px', 'solid', '#ccc'),
     ...shorthands.overflow('hidden'),
-
     display: 'flex',
     height: '480px',
     backgroundColor: '#fff',
   },
-
   content: {
     ...shorthands.flex(1),
     ...shorthands.padding('16px'),
@@ -32,14 +48,43 @@ const useStyles = makeStyles({
     gridRowGap: tokens.spacingVerticalXXL,
     gridAutoRows: 'max-content',
   },
-
   field: {
     display: 'grid',
     gridRowGap: tokens.spacingVerticalS,
   },
+
+  headingContent: {
+    marginInlineStart: `10px`,
+  },
+  drawerFooterOverrides: {
+    display: 'unset',
+    ...shorthands.padding(0, tokens.spacingVerticalMNudge),
+  },
+  drawerHeaderOverrides: {
+    ...shorthands.margin('unset'),
+    paddingInlineStart: '8px',
+    paddingBlockStart: '0px',
+  },
+  drawerBodyOverrides: {
+    ...shorthands.padding(0, tokens.spacingVerticalMNudge),
+  },
+  buttonStyles: {
+    marginInlineStart: '-8px',
+    backgroundColor: navItemTokens.backgroundColor,
+    color: tokens.colorNeutralForeground2,
+    textDecorationLine: 'none',
+    ':hover': {
+      backgroundColor: navItemTokens.backgroundColorHover,
+    },
+    ':active': {
+      backgroundColor: navItemTokens.backgroundColorPressed,
+    },
+  },
 });
 
 const Folder = bundleIcon(Folder20Filled, Folder20Regular);
+
+const Person = bundleIcon(PersonFilled, PersonRegular);
 
 type DrawerType = Required<DrawerProps>['type'];
 
@@ -48,8 +93,8 @@ export const NavDrawerDefault = (props: Partial<NavDrawerProps>) => {
 
   const labelId = useId('type-label');
 
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [type, setType] = React.useState<DrawerType>('overlay');
+  const [isOpen, setIsOpen] = React.useState(true);
+  const [type, setType] = React.useState<DrawerType>('inline');
 
   const someClickHandler = () => {
     console.log('someClickHandler');
@@ -58,53 +103,75 @@ export const NavDrawerDefault = (props: Partial<NavDrawerProps>) => {
   return (
     <div className={styles.root}>
       <NavDrawer
-        defaultSelectedValue={'10'}
-        defaultSelectedCategoryValue={'8'}
+        defaultSelectedValue={'2'}
+        defaultSelectedCategoryValue={'1'}
         open={isOpen}
         type={type}
         onOpenChange={(_, { open }) => setIsOpen(open)}
         size="small"
       >
-        <DrawerBody>
-          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="1">
-            First
-          </NavItem>
-          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="2">
-            Second
-          </NavItem>
-          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="3">
-            Third
-          </NavItem>
-          <NavCategory value="4">
-            <NavCategoryItem icon={<Folder />}>NavCategoryItem 1</NavCategoryItem>
+        <DrawerBody className={styles.drawerBodyOverrides}>
+          <DrawerHeader className={styles.drawerHeaderOverrides}>
+            <DrawerHeaderNavigation>
+              <Button
+                appearance={'transparent'}
+                icon={<NavigationFilled />}
+                value={'10000'}
+                className={styles.buttonStyles}
+              />
+            </DrawerHeaderNavigation>
+          </DrawerHeader>
+          <NavCategory value="1">
+            <Caption1Strong className={styles.headingContent}>Groceries</Caption1Strong>
+            <NavCategoryItem icon={<Folder />}>Produce</NavCategoryItem>
             <NavSubItemGroup>
-              <NavSubItem target="_blank" onClick={someClickHandler} value="5">
-                Five
+              <NavSubItem target="_blank" onClick={someClickHandler} value="2">
+                Carrot
               </NavSubItem>
-              <NavSubItem target="_blank" onClick={someClickHandler} value="6">
-                Six
-              </NavSubItem>
-              <NavSubItem target="_blank" onClick={someClickHandler} value="7">
-                Seven
+              <NavSubItem target="_blank" onClick={someClickHandler} value="3">
+                Lettuce
               </NavSubItem>
             </NavSubItemGroup>
           </NavCategory>
-          <NavCategory value="8">
-            <NavCategoryItem icon={<Folder />}>NavCategoryItem2</NavCategoryItem>
+          <NavCategory value="4">
+            <NavCategoryItem icon={<Folder />}>Meat</NavCategoryItem>
             <NavSubItemGroup>
               <NavSubItem target="_blank" onClick={someClickHandler} value="9">
-                Nine
+                Beef
               </NavSubItem>
               <NavSubItem target="_blank" onClick={someClickHandler} value="10">
-                Ten
-              </NavSubItem>
-              <NavSubItem target="_blank" onClick={someClickHandler} value="11">
-                Eleven
+                Pork
               </NavSubItem>
             </NavSubItemGroup>
           </NavCategory>
+          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="5">
+            Bakery
+          </NavItem>
+          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="6">
+            Dairy
+          </NavItem>
+          <Caption1Strong className={styles.headingContent}>Home Improvement</Caption1Strong>
+          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="7">
+            Lumber
+          </NavItem>
+          <NavItem icon={<Folder />} target="_blank" onClick={someClickHandler} value="8">
+            Garden
+          </NavItem>
         </DrawerBody>
-      </NavDrawer>{' '}
+        <DrawerFooter className={styles.drawerFooterOverrides}>
+          <NavCategory value="9">
+            <NavCategoryItem icon={<Person />}>Profile</NavCategoryItem>
+            <NavSubItemGroup>
+              <NavSubItem target="_blank" onClick={someClickHandler} value="9">
+                Settings
+              </NavSubItem>
+              <NavSubItem target="_blank" onClick={someClickHandler} value="10">
+                Support
+              </NavSubItem>
+            </NavSubItemGroup>
+          </NavCategory>
+        </DrawerFooter>
+      </NavDrawer>
       <div className={styles.content}>
         <Button appearance="primary" onClick={() => setIsOpen(!isOpen)}>
           {type === 'inline' ? 'Toggle' : 'Open'}
