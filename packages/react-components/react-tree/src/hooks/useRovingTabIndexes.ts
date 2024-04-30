a7fa72ed8c03d2c811bf42a7aaedb9bbcd67190a
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2024-04-30 15:14:21 +0200

    chore(react-tree): updates roving tab indexes to ensure Tree is focusable

--- packages/react-components/react-tree/src/hooks/useRovingTabIndexes.ts
+++ packages/react-components/react-tree/src/hooks/useRovingTabIndexes.ts
@@ -1,13 +1,27 @@
 import * as React from 'react';
 import { HTMLElementWalker } from '../utils/createHTMLElementWalker';
+import { useListenFocusedElement } from '@fluentui/react-tabster';
+import { elementContains } from '@fluentui/react-utilities';
 
 /**
  * https://www.w3.org/WAI/ARIA/apg/practices/keyboard-interface/#kbd_roving_tabindex
  */
 export function useRovingTabIndex() {
   const currentElementRef = React.useRef<HTMLElement>();
+  const walkerRef = React.useRef<HTMLElementWalker | null>(null);
+  useListenFocusedElement(element => {
+    if (
+      walkerRef.current &&
+      element &&
+      elementContains(walkerRef.current.root, element) &&
+      element.getAttribute('role') === 'treeitem'
+    ) {
+      rove(element);
+    }
+  });
 
   const initialize = React.useCallback((walker: HTMLElementWalker) => {
+    walkerRef.current = walker;
     walker.currentElement = walker.root;
     let tabbableChild = walker.firstChild(element =>
       element.tabIndex === 0 ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP,
