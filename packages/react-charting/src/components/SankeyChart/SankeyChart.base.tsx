33a2727ed12c947d3deec9e7bade02647d29ef49
Kumar Kshitij <kumarkshitij@microsoft.com>
2024-05-08 23:59:55 +0530

    disable tab key navigation

--- packages/react-charting/src/components/SankeyChart/SankeyChart.base.tsx
+++ packages/react-charting/src/components/SankeyChart/SankeyChart.base.tsx
@@ -792,7 +792,7 @@ export class SankeyChartBase extends React.Component<ISankeyChartProps, ISankeyC
           ref={(rootElem: HTMLDivElement) => (this.chartContainer = rootElem)}
           onMouseLeave={this._onCloseCallout}
         >
-          <FocusZone direction={FocusZoneDirection.bidirectional} handleTabKey={FocusZoneTabbableElements.all}>
+          <FocusZone direction={FocusZoneDirection.bidirectional}>
             <svg width={width} height={height} id={this._chartId}>
               <g className={classNames.links} strokeOpacity={1}>
                 {linkData}
