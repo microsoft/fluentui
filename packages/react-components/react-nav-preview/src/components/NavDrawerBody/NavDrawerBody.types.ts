e5969289c89d88ffa628aab94451cf82aa717221
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-05-08 11:25:20 -0700

    first changes

--- packages/react-components/react-nav-preview/src/components/NavDrawerBody/NavDrawerBody.types.ts
+++ packages/react-components/react-nav-preview/src/components/NavDrawerBody/NavDrawerBody.types.ts
@@ -1,17 +1,14 @@
-import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
+import { DrawerBodySlots, DrawerBodyState } from '@fluentui/react-drawer';
+import type { ComponentProps, ComponentState } from '@fluentui/react-utilities';
 
-export type NavDrawerBodySlots = {
-  root: Slot<'div'>;
-};
+export type NavDrawerBodySlots = ComponentProps<DrawerBodySlots>;
 
 /**
  * NavDrawerBody Props
  */
-export type NavDrawerBodyProps = ComponentProps<NavDrawerBodySlots> & {};
+export type NavDrawerBodyProps = ComponentProps<DrawerBodySlots>;
 
 /**
  * State used in rendering NavDrawerBody
  */
-export type NavDrawerBodyState = ComponentState<NavDrawerBodySlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from NavDrawerBodyProps.
-// & Required<Pick<NavDrawerBodyProps, 'propName'>>
+export type NavDrawerBodyState = ComponentState<Partial<DrawerBodySlots>> & DrawerBodyState;
--- packages/react-components/react-nav-preview/src/components/NavDrawerBody/useNavDrawerBodyStyles.styles.ts
+++ packages/react-components/react-nav-preview/src/components/NavDrawerBody/useNavDrawerBodyStyles.styles.ts
@@ -1,19 +1,14 @@
-import { makeStyles, mergeClasses } from '@griffel/react';
-import type { SlotClassNames } from '@fluentui/react-utilities';
-import type { NavDrawerBodySlots, NavDrawerBodyState } from './NavDrawerBody.types';
-
-export const navDrawerBodyClassNames: SlotClassNames<NavDrawerBodySlots> = {
-  root: 'fui-NavDrawerBody',
-  // TODO: add class names for all slots on NavDrawerBodySlots.
-  // Should be of the form `<slotName>: 'fui-NavDrawerBody__<slotName>`
-};
+import { makeStyles, mergeClasses, shorthands } from '@griffel/react';
+import { tokens } from '@fluentui/react-theme';
+import type { NavDrawerBodyState } from './NavDrawerBody.types';
 
 /**
  * Styles for the root slot
  */
 const useStyles = makeStyles({
   root: {
-    // TODO Add default styles for the root element
+    ...shorthands.padding(0, tokens.spacingVerticalMNudge),
+    backgroundColor: 'red',
   },
 
   // TODO add additional classes for different states and/or slots
@@ -24,7 +19,7 @@ const useStyles = makeStyles({
  */
 export const useNavDrawerBodyStyles_unstable = (state: NavDrawerBodyState): NavDrawerBodyState => {
   const styles = useStyles();
-  state.root.className = mergeClasses(navDrawerBodyClassNames.root, styles.root, state.root.className);
+  state.root.className = mergeClasses(styles.root, state.root.className);
 
   // TODO Add class names to slots, for example:
   // state.mySlot.className = mergeClasses(styles.mySlot, state.mySlot.className);
