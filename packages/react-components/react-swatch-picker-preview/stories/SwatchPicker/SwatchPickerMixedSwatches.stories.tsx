28b1befd7e8e0cbdd317ff106970ed811f7bac60
Valentina <vkozlova@microsoft.com>
2024-05-09 15:06:20 +0200

    docs(react-swatch-picker): added description and best practices (#31280)

--- packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerMixedSwatches.stories.tsx
+++ packages/react-components/react-swatch-picker-preview/stories/SwatchPicker/SwatchPickerMixedSwatches.stories.tsx
@@ -3,9 +3,9 @@ import { makeStyles, shorthands } from '@fluentui/react-components';
 import {
   SwatchPicker,
   SwatchPickerOnSelectEventHandler,
-  ColorSwatch,
   renderSwatchPickerGrid,
 } from '@fluentui/react-swatch-picker-preview';
+import type { ColorSwatchProps, ImageSwatchProps, SwatchProps } from '@fluentui/react-swatch-picker-preview';
 
 const useStyles = makeStyles({
   example: {
@@ -19,36 +19,52 @@ const useStyles = makeStyles({
   },
 });
 
-const colors = [
+const colors: ColorSwatchProps[] = [
   { color: '#FF1921', value: 'FF1921', 'aria-label': 'red' },
   { color: '#FF7A00', value: 'FF7A00', 'aria-label': 'orange' },
   { color: '#90D057', value: '90D057', 'aria-label': 'light green' },
   { color: '#00B053', value: '00B053', 'aria-label': 'green' },
   { color: '#00AFED', value: '00AFED', 'aria-label': 'light blue' },
   { color: '#006EBD', value: '006EBD', 'aria-label': 'blue' },
-  { color: '#011F5E', value: '011F5E', 'aria-label': 'dark blue' },
-  { color: '#712F9E', value: '712F9E', 'aria-label': 'purple' },
-  { color: '#FF0099', value: 'FF0099', 'aria-label': 'pink' },
 ];
 
-export const SwatchPickerLayout = () => {
+const images: ImageSwatchProps[] = [
+  {
+    src: 'https://fabricweb.azureedge.net/fabric-website/assets/images/swatch-picker/sea-swatch.jpg',
+    value: 'sea',
+    'aria-label': 'sea',
+  },
+  {
+    src: 'https://fabricweb.azureedge.net/fabric-website/assets/images/swatch-picker/bridge-swatch.jpg',
+    value: 'bridge',
+    'aria-label': 'bridge',
+  },
+  {
+    src: 'https://fabricweb.azureedge.net/fabric-website/assets/images/swatch-picker/park-swatch.jpg',
+    value: 'park',
+    'aria-label': 'park',
+  },
+];
+
+const items: SwatchProps[] = [...colors, ...images];
+
+export const SwatchPickerMixedSwatches = () => {
   const [selectedValue, setSelectedValue] = React.useState('00B053');
   const [selectedSwatch, setSelectedSwatch] = React.useState('#00B053');
   const handleSelect: SwatchPickerOnSelectEventHandler = (_, data) => {
     setSelectedValue(data.selectedValue);
-    setSelectedSwatch(data.selectedSwatch);
+    const swatch = items.find(item => item.value === data.selectedValue) || items[0];
+    const src = (swatch as ImageSwatchProps).src;
+    if (swatch.color) {
+      setSelectedSwatch(swatch.color);
+    } else if (src) {
+      setSelectedSwatch(`url(${src}`);
+    }
   };
 
   const styles = useStyles();
   return (
     <>
-      <h3>Row</h3>
-      <SwatchPicker aria-label="SwatchPicker row layout" selectedValue={selectedValue} onSelectionChange={handleSelect}>
-        {colors.map(color => {
-          return <ColorSwatch key={color.value} {...color} />;
-        })}
-      </SwatchPicker>
-      <h3>Grid</h3>
       <SwatchPicker
         layout="grid"
         aria-label="SwatchPicker grid layout"
@@ -56,7 +72,7 @@ export const SwatchPickerLayout = () => {
         onSelectionChange={handleSelect}
       >
         {renderSwatchPickerGrid({
-          items: colors,
+          items,
           columnCount: 3,
         })}
       </SwatchPicker>
@@ -70,10 +86,10 @@ export const SwatchPickerLayout = () => {
   );
 };
 
-SwatchPickerLayout.parameters = {
+SwatchPickerMixedSwatches.parameters = {
   docs: {
     description: {
-      story: 'The `layout` prop places items in a `row` or a `grid`.',
+      story: "It's possible to use `ColorSwatch` and `ImageSwatch` in one SwatchPicker.",
     },
   },
 };
