03cc333493ac231fcbe5affc7eb72ceedd3770c4
brianchristopherbrady <brian.christopher.brady@gmail.com>
2024-05-15 12:07:36 -0700

    removes tabindex from thumb

--- packages/web-components/src/slider/slider.template.ts
+++ packages/web-components/src/slider/slider.template.ts
@@ -2,5 +2,5 @@ import type { ElementViewTemplate } from '@microsoft/fast-element';
 import { FASTSlider, sliderTemplate } from '@microsoft/fast-foundation/slider.js';
 
 export const template: ElementViewTemplate<FASTSlider> = sliderTemplate({
-  thumb: `<div class="thumb-cursor" tabindex="0"></div>`,
+  thumb: `<div class="thumb-cursor"></div>`,
 });
