26fd4be89a5c9512dfbc991da124350f021e1f07
Robert Penner <robert@robertpenner.com>
2024-05-20 16:53:09 +0000

    chore(Tree): update chevron rotation easing to match the collapse

--- packages/react-components/react-tree/src/components/TreeItemChevron.tsx
+++ packages/react-components/react-tree/src/components/TreeItemChevron.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { useFluent_unstable } from '@fluentui/react-shared-contexts';
 import { ChevronRight12Regular } from '@fluentui/react-icons';
 import { useTreeItemContext_unstable } from '../contexts/treeItemContext';
-import { durations } from '@fluentui/react-motions-preview';
+import { durations, curves } from '@fluentui/react-motions-preview';
 
 // TODO: keep this chevron rotation duration in sync with the tree's open/close duration
-const motionDuration = durations.durationNormal;
+const duration = durations.durationNormal;
+const easing = curves.curveEasyEaseMax;
 
 export const TreeItemChevron = React.memo(() => {
   const open = useTreeItemContext_unstable(ctx => ctx.open);
@@ -15,7 +16,10 @@ export const TreeItemChevron = React.memo(() => {
   const expandIconRotation = open ? 90 : dir !== 'rtl' ? 0 : 180;
   return (
     <ChevronRight12Regular
-      style={{ ...expandIconInlineStyles[expandIconRotation], transition: `transform ${motionDuration}ms ease-out` }}
+      style={{
+        ...expandIconInlineStyles[expandIconRotation],
+        transition: `transform ${duration}ms ${easing}`,
+      }}
     />
   );
 });
