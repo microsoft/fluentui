a7428049165fbaf190f9c68c3fc63c63cb6f9774
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2024-05-21 23:04:56 +0530

    Change variant example from class to function component

--- packages/react-examples/src/react-charting/HorizontalBarChart/HorizontalBarChart.Variant.Example.tsx
+++ packages/react-examples/src/react-charting/HorizontalBarChart/HorizontalBarChart.Variant.Example.tsx
@@ -9,127 +9,114 @@ import {
 import { ThemeContext } from '@fluentui/react';
 import { Checkbox } from '@fluentui/react/lib/Checkbox';
 
-interface IHBCVariantExampleState {
-  hideLabels: boolean;
-}
+export const HorizontalBarChartCustomCalloutExample: React.FunctionComponent<{}> = () => {
+  const [hideLabels, setHideLabels] = React.useState<boolean>(false);
+  const theme = React.useContext(ThemeContext);
 
-export class HorizontalBarChartVariantExample extends React.Component<{}, IHBCVariantExampleState> {
-  constructor(props = {}) {
-    super(props);
-
-    this.state = {
-      hideLabels: false,
-    };
-  }
-
-  public render() {
-    const theme = React.useContext(ThemeContext);
-
-    const getData = (isDarkMode: boolean): IChartProps[] => {
-      return [
-        {
-          chartTitle: 'one',
-          chartData: [
-            {
-              legend: 'one',
-              horizontalBarChartdata: { x: 1543, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color17, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'two',
-          chartData: [
-            {
-              legend: 'two',
-              horizontalBarChartdata: { x: 800, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color18, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'three',
-          chartData: [
-            {
-              legend: 'three',
-              horizontalBarChartdata: { x: 8888, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color19, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'four',
-          chartData: [
-            {
-              legend: 'four',
-              horizontalBarChartdata: { x: 15888, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color20, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'five',
-          chartData: [
-            {
-              legend: 'five',
-              horizontalBarChartdata: { x: 11444, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color21, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'six',
-          chartData: [
-            {
-              legend: 'six',
-              horizontalBarChartdata: { x: 14000, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color22, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'seven',
-          chartData: [
-            {
-              legend: 'seven',
-              horizontalBarChartdata: { x: 9855, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color23, isDarkMode),
-            },
-          ],
-        },
-        {
-          chartTitle: 'eight',
-          chartData: [
-            {
-              legend: 'eight',
-              horizontalBarChartdata: { x: 4250, y: 15000 },
-              color: getColorFromToken(DataVizPalette.color24, isDarkMode),
-            },
-          ],
-        },
-      ];
-    };
-
-    return (
-      <>
-        <Checkbox
-          label="Hide labels"
-          checked={this.state.hideLabels}
-          onChange={this._onCheckChange}
-          styles={{ root: { marginBottom: '20px' } }}
-        />
-        <div style={{ maxWidth: 600 }}>
-          <HorizontalBarChart
-            data={getData(theme?.isInverted ?? false)}
-            variant={HorizontalBarChartVariant.AbsoluteScale}
-            hideLabels={this.state.hideLabels}
-          />
-        </div>
-      </>
-    );
-  }
-
-  private _onCheckChange = (ev: React.MouseEvent<HTMLElement>, checked: boolean) => {
-    this.setState({ hideLabels: checked });
+  const getData = (isDarkMode: boolean): IChartProps[] => {
+    return [
+      {
+        chartTitle: 'one',
+        chartData: [
+          {
+            legend: 'one',
+            horizontalBarChartdata: { x: 1543, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color17, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'two',
+        chartData: [
+          {
+            legend: 'two',
+            horizontalBarChartdata: { x: 800, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color18, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'three',
+        chartData: [
+          {
+            legend: 'three',
+            horizontalBarChartdata: { x: 8888, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color19, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'four',
+        chartData: [
+          {
+            legend: 'four',
+            horizontalBarChartdata: { x: 15888, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color20, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'five',
+        chartData: [
+          {
+            legend: 'five',
+            horizontalBarChartdata: { x: 11444, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color21, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'six',
+        chartData: [
+          {
+            legend: 'six',
+            horizontalBarChartdata: { x: 14000, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color22, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'seven',
+        chartData: [
+          {
+            legend: 'seven',
+            horizontalBarChartdata: { x: 9855, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color23, isDarkMode),
+          },
+        ],
+      },
+      {
+        chartTitle: 'eight',
+        chartData: [
+          {
+            legend: 'eight',
+            horizontalBarChartdata: { x: 4250, y: 15000 },
+            color: getColorFromToken(DataVizPalette.color24, isDarkMode),
+          },
+        ],
+      },
+    ];
   };
-}
+
+  const onCheckChange = (ev: React.MouseEvent<HTMLElement>, checked: boolean) => {
+    setHideLabels(checked);
+  };
+
+  return (
+    <>
+      <Checkbox
+        label="Hide labels"
+        checked={hideLabels}
+        onChange={onCheckChange}
+        styles={{ root: { marginBottom: '20px' } }}
+      />
+      <div style={{ maxWidth: 600 }}>
+        <HorizontalBarChart
+          data={getData(theme?.isInverted ?? false)}
+          variant={HorizontalBarChartVariant.AbsoluteScale}
+          hideLabels={hideLabels}
+        />
+      </div>
+    </>
+  );
+};
