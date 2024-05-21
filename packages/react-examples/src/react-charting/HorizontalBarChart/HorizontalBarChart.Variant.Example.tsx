9844465890a10c34b3c18884672b9223b66bbf69
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2024-05-21 22:34:29 +0530

    Fix theme context usage

--- packages/react-examples/src/react-charting/HorizontalBarChart/HorizontalBarChart.Variant.Example.tsx
+++ packages/react-examples/src/react-charting/HorizontalBarChart/HorizontalBarChart.Variant.Example.tsx
@@ -23,90 +23,92 @@ export class HorizontalBarChartVariantExample extends React.Component<{}, IHBCVa
   }
 
   public render() {
-    const isDarkMode: boolean = React.useContext(ThemeContext)?.isInverted ?? false;
+    const theme = React.useContext(ThemeContext);
 
-    const data: IChartProps[] = [
-      {
-        chartTitle: 'one',
-        chartData: [
-          {
-            legend: 'one',
-            horizontalBarChartdata: { x: 1543, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color17, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'two',
-        chartData: [
-          {
-            legend: 'two',
-            horizontalBarChartdata: { x: 800, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color18, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'three',
-        chartData: [
-          {
-            legend: 'three',
-            horizontalBarChartdata: { x: 8888, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color19, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'four',
-        chartData: [
-          {
-            legend: 'four',
-            horizontalBarChartdata: { x: 15888, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color20, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'five',
-        chartData: [
-          {
-            legend: 'five',
-            horizontalBarChartdata: { x: 11444, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color21, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'six',
-        chartData: [
-          {
-            legend: 'six',
-            horizontalBarChartdata: { x: 14000, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color22, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'seven',
-        chartData: [
-          {
-            legend: 'seven',
-            horizontalBarChartdata: { x: 9855, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color23, isDarkMode),
-          },
-        ],
-      },
-      {
-        chartTitle: 'eight',
-        chartData: [
-          {
-            legend: 'eight',
-            horizontalBarChartdata: { x: 4250, y: 15000 },
-            color: getColorFromToken(DataVizPalette.color24, isDarkMode),
-          },
-        ],
-      },
-    ];
+    const getData = (isDarkMode: boolean): IChartProps[] => {
+      return [
+        {
+          chartTitle: 'one',
+          chartData: [
+            {
+              legend: 'one',
+              horizontalBarChartdata: { x: 1543, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color17, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'two',
+          chartData: [
+            {
+              legend: 'two',
+              horizontalBarChartdata: { x: 800, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color18, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'three',
+          chartData: [
+            {
+              legend: 'three',
+              horizontalBarChartdata: { x: 8888, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color19, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'four',
+          chartData: [
+            {
+              legend: 'four',
+              horizontalBarChartdata: { x: 15888, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color20, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'five',
+          chartData: [
+            {
+              legend: 'five',
+              horizontalBarChartdata: { x: 11444, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color21, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'six',
+          chartData: [
+            {
+              legend: 'six',
+              horizontalBarChartdata: { x: 14000, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color22, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'seven',
+          chartData: [
+            {
+              legend: 'seven',
+              horizontalBarChartdata: { x: 9855, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color23, isDarkMode),
+            },
+          ],
+        },
+        {
+          chartTitle: 'eight',
+          chartData: [
+            {
+              legend: 'eight',
+              horizontalBarChartdata: { x: 4250, y: 15000 },
+              color: getColorFromToken(DataVizPalette.color24, isDarkMode),
+            },
+          ],
+        },
+      ];
+    };
 
     return (
       <>
@@ -118,7 +120,7 @@ export class HorizontalBarChartVariantExample extends React.Component<{}, IHBCVa
         />
         <div style={{ maxWidth: 600 }}>
           <HorizontalBarChart
-            data={data}
+            data={getData(theme?.isInverted ?? false)}
             variant={HorizontalBarChartVariant.AbsoluteScale}
             hideLabels={this.state.hideLabels}
           />
