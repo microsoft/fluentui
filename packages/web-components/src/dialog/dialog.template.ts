43edeaa933cf26827e4fbf53dad0e7ea9cb18674
Dave Rupert <rupertdavid@microsoft.com>
2024-05-29 15:45:42 -0500

    rm unneeded template

--- packages/web-components/src/dialog/dialog.template.ts
+++ packages/web-components/src/dialog/dialog.template.ts
@@ -7,20 +7,18 @@ import { DialogType } from './dialog.options.js';
  * @public
  */
 export const template: ElementViewTemplate<Dialog> = html`
-  <template>
-    <dialog
-      role="${x => (x.type === DialogType.alert ? 'alertdialog' : 'dialog')}"
-      type="${x => x.type}"
-      class="dialog"
-      part="dialog"
-      aria-modal="${x => (x.type === DialogType.modal || x.type === DialogType.alert ? 'true' : void 0)}"
-      aria-describedby="${x => x.ariaDescribedby}"
-      aria-labelledby="${x => x.ariaLabelledby}"
-      aria-label="${x => x.ariaLabel}"
-      @cancel="${(x, c) => (x.type === DialogType.alert ? c.event.preventDefault() : true)}"
-      ${ref('dialog')}
-    >
-      <slot></slot>
-    </dialog>
-  </template>
+  <dialog
+    role="${x => (x.type === DialogType.alert ? 'alertdialog' : 'dialog')}"
+    type="${x => x.type}"
+    class="dialog"
+    part="dialog"
+    aria-modal="${x => (x.type === DialogType.modal || x.type === DialogType.alert ? 'true' : void 0)}"
+    aria-describedby="${x => x.ariaDescribedby}"
+    aria-labelledby="${x => x.ariaLabelledby}"
+    aria-label="${x => x.ariaLabel}"
+    @cancel="${(x, c) => (x.type === DialogType.alert ? c.event.preventDefault() : true)}"
+    ${ref('dialog')}
+  >
+    <slot></slot>
+  </dialog>
 `;
