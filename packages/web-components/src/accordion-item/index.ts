9b681402fa473147564aeb5f661c871c41f2355a
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-05-31 15:54:39 -0700

    (web-components) Resolve output warnings for storybook and api-extractor (#31467)

--- packages/web-components/src/accordion-item/index.ts
+++ packages/web-components/src/accordion-item/index.ts
@@ -1,4 +1,5 @@
-export { AccordionItem, AccordionItemOptions } from './accordion-item.js';
+export { AccordionItem } from './accordion-item.js';
+export type { AccordionItemOptions } from './accordion-item.js';
 export { AccordionItemSize, AccordionItemExpandIconPosition } from './accordion-item.options.js';
 export { styles as accordionItemStyles } from './accordion-item.styles.js';
 export { definition as accordionItemDefinition } from './accordion-item.definition.js';
