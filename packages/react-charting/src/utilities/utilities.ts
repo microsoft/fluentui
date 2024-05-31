b0f9af20f07e192dca5741d085780c094b797196
krkshitij <110246001+krkshitij@users.noreply.github.com>
2024-05-31 19:11:02 +0000

    resolve review comments

--- packages/react-charting/src/utilities/utilities.ts
+++ packages/react-charting/src/utilities/utilities.ts
@@ -1382,5 +1382,5 @@ export const isScalePaddingDefined = (prop: number | undefined, shorthandProp?: 
 
 export const formatDate = (date: Date, useUTC?: boolean) => {
   const timeFormat = useUTC ? d3UtcFormat : d3TimeFormat;
-  return timeFormat('%-e %b %Y, %H:%M')(date) + (useUTC ? ' UTC' : '');
+  return timeFormat('%-e %b %Y, %H:%M')(date) + (useUTC ? ' GMT' : '');
 };
