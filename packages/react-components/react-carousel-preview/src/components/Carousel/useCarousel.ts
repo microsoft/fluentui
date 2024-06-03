61d92694c85613d74820684c5f26019908ec8d26
Mitch-At-Work <mifraser@microsoft.com>
2024-06-03 10:56:30 -0700

    Add store to dep use effect

--- packages/react-components/react-carousel-preview/src/components/Carousel/useCarousel.ts
+++ packages/react-components/react-carousel-preview/src/components/Carousel/useCarousel.ts
@@ -55,8 +55,7 @@ export function useCarousel_unstable(props: CarouselProps, ref: React.Ref<HTMLDi
 
   useIsomorphicLayoutEffect(() => {
     store.setActiveValue(value);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+  }, [store, value]);
 
   React.useEffect(() => {
     const allItems = rootRef.current?.querySelectorAll(`[${CAROUSEL_ITEM}]`)!;
