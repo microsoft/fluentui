microsoft/fluentui

commit 78fdc477f4330c033fc4df6d42ea34adac4c41b1

doc(react-tree): simplifies InlineStylingTreeItemLevel story (#30962)

packages/react-components/react-tree/stories/Tree/TreeInlineStylingTreeItemLevel.stories.tsx
@@ -4,30 +4,24 @@ import {
   TreeItem,
   TreeItemLayout,
   treeItemLevelToken,
-  useTreeItemContext_unstable,
   useSubtreeContext_unstable,
 } from '@fluentui/react-components';
 
-const RecursiveTreeItem: React.FC = () => {
+export const InlineStylingTreeItemLevel = () => {
   const { level } = useSubtreeContext_unstable();
-  const open = useTreeItemContext_unstable(ctx => ctx.open || level === 1);
-  return (
+  return level === 0 ? (
+    <Tree aria-label="Inline Styling Tree Item Level">
+      <InlineStylingTreeItemLevel />
+    </Tree>
+  ) : (
     <TreeItem value={level} itemType="branch" style={{ [treeItemLevelToken]: level }}>
       <TreeItemLayout>{`level ${level}, item 1`}</TreeItemLayout>
-      {open && (
-        <Tree>
-          <RecursiveTreeItem />
-        </Tree>
-      )}
+      <Tree>
+        <InlineStylingTreeItemLevel />
+      </Tree>
     </TreeItem>
   );
 };
-
-export const InlineStylingTreeItemLevel = () => (
-  <Tree aria-label="Inline Styling Tree Item Level">
-    <RecursiveTreeItem />
-  </Tree>
-);
 
 InlineStylingTreeItemLevel.parameters = {
   docs: {

commit ab0acb8f96347eeb08e4f63ac4fd19a6016c5d8b

fix(Tree): Tree story locks browser when motion component used - One specific story was freezing: InlineStylingTreeItemLevel - Using a motion component like Collapse or Fade triggered the freeze - Commit 78fdc477 introduced the bug in the story code - The fix was to restore the previous `open` check during recursion

packages/react-components/react-tree/stories/Tree/TreeInlineStylingTreeItemLevel.stories.tsx
@@ -4,11 +4,13 @@ import {
   TreeItem,
   TreeItemLayout,
   treeItemLevelToken,
+  useTreeItemContext_unstable,
   useSubtreeContext_unstable,
 } from '@fluentui/react-components';
 
 export const InlineStylingTreeItemLevel = () => {
   const { level } = useSubtreeContext_unstable();
+  const open = useTreeItemContext_unstable(ctx => ctx.open || level === 1);
   return level === 0 ? (
     <Tree aria-label="Inline Styling Tree Item Level">
       <InlineStylingTreeItemLevel />
@@ -16,9 +18,11 @@ export const InlineStylingTreeItemLevel = () => {
   ) : (
     <TreeItem value={level} itemType="branch" style={{ [treeItemLevelToken]: level }}>
       <TreeItemLayout>{`level ${level}, item 1`}</TreeItemLayout>
-      <Tree>
-        <InlineStylingTreeItemLevel />
-      </Tree>
+      {open && (
+        <Tree>
+          <InlineStylingTreeItemLevel />
+        </Tree>
+      )}
     </TreeItem>
   );
 };