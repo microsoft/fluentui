6cf078f21232eded85aa71f2a65d4f88c659f8a6
Zacky Ma <machi@microsoft.com>
2024-06-11 22:28:50 -0700

    reduce duplicated code

--- packages/web-components/src/theme/set-theme.ts
+++ packages/web-components/src/theme/set-theme.ts
@@ -10,6 +10,8 @@ const tokenNames = Object.keys(tokens) as (keyof Theme)[];
  */
 export const setTheme = (theme: Theme) => {
   for (const t of tokenNames) {
+    let registered = false;
+
     if ('registerProperty' in CSS) {
       try {
         CSS.registerProperty({
@@ -17,11 +19,15 @@ export const setTheme = (theme: Theme) => {
           inherits: true,
           initialValue: theme[t] as string,
         });
-      } catch {
-        document.body.style.setProperty(`--${t}`, theme[t] as string);
-      }
-    } else {
-      document.body.style.setProperty(`--${t}`, theme[t] as string);
+        registered = true;
+      } catch {}
+    }
+
+    if (!registered) {
+      // TODO: Find a better way to update the values. Current approach adds
+      // lots of code to the `style` attribute on `<body>`. Maybe look into
+      // `document.adoptedStyleSheets`.
+      setThemeFor(document.body, theme);
     }
   }
 };
