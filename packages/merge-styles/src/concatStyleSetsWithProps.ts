3e3f54e07553611f300638ca2d2ea253b14fb324
Martin Hochel <martinhochel@microsoft.com>
2024-06-13 15:02:19 +0200

    fix(merge-styles): improve DeepPartial type infinite recursion triggerpoint

--- packages/merge-styles/src/concatStyleSetsWithProps.ts
+++ packages/merge-styles/src/concatStyleSetsWithProps.ts
@@ -1,7 +1,7 @@
 import { concatStyleSets } from './concatStyleSets';
 import { IStyleSetBase } from './IStyleSet';
 import { IStyleFunctionOrObject } from './IStyleFunction';
-import { DeepPartial } from './DeepPartial';
+import { DeepPartialV2 } from './DeepPartial';
 
 /**
  * Concatenates style sets into one, but resolves functional sets using the given props.
@@ -11,22 +11,22 @@ import { DeepPartial } from './DeepPartial';
 export function concatStyleSetsWithProps<TStyleProps, TStyleSet extends IStyleSetBase>(
   styleProps: TStyleProps,
   ...allStyles: (IStyleFunctionOrObject<TStyleProps, TStyleSet> | undefined)[]
-): DeepPartial<TStyleSet> {
-  const result: DeepPartial<TStyleSet>[] = [];
+): DeepPartialV2<TStyleSet> {
+  const result: DeepPartialV2<TStyleSet>[] = [];
   for (const styles of allStyles) {
     if (styles) {
       result.push(typeof styles === 'function' ? styles(styleProps) : styles);
     }
   }
   if (result.length === 1) {
-    return result[0] as DeepPartial<TStyleSet>;
+    return result[0] as DeepPartialV2<TStyleSet>;
   } else if (result.length) {
     // cliffkoh: I cannot figure out how to avoid the cast to any here.
     // It is something to do with the use of Omit in IStyleSet.
     // It might not be necessary once  Omit becomes part of lib.d.ts (when we remove our own Omit and rely on
     // the official version).
-    return concatStyleSets(...(result as any)) as any;
+    return concatStyleSets(...result) as any;
   }
 
-  return {};
+  return {} as any;
 }
