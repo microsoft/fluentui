3b89d2e39bcae45b06a49f6f5a22cd9f38c17e7f
Oleksandr Fediashov <olfedias@microsoft.com>
2024-06-14 16:15:44 +0200

    fix: fix react-18-tests-v9 to run tests (#31716)

--- apps/react-18-tests-v9/jest.config.js
+++ apps/react-18-tests-v9/jest.config.js
@@ -6,6 +6,12 @@
 module.exports = {
   displayName: 'react-18-tests-v9',
   preset: '../../jest.preset.js',
+  // Heads up!
+  // Forces React to be resolved from the root node_modules to ensure the same instance is used across all packages
+  moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
+    '^react-dom$': '<rootDir>/node_modules/react-dom',
+  },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
