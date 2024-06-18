c60fac5812617e9bf301f289ba604bebb9331a05
Zacky Ma <machi@microsoft.com>
2024-06-18 16:19:39 -0700

    add perf test

---
+++ packages/web-components/src/theme/set-theme.bench.ts
@@ -0,0 +1,47 @@
+// eslint-disable-next-line
+// @ts-nocheck
+import { measureJavascript, measureLayout } from '@tensile-perf/tools';
+import { teamsDarkTheme, teamsLightTheme, webDarkTheme, webLightTheme } from '@fluentui/tokens';
+
+import { setTheme } from './set-theme.js';
+
+const measurePerformance = () => {
+  let startTime: number;
+
+  const startMeasure = () => {
+    startTime = performance.now();
+  }
+
+  const endMeasure = () => {
+    measureJavascript(startTime, performance.now());
+    measureLayout();
+  };
+
+  return { startMeasure, endMeasure };
+};
+
+const tests = {
+  'mount': ({ onComplete }) => {
+    const { startMeasure, endMeasure} = measurePerformance();
+
+    startMeasure();
+
+    // Newly set themes
+    setTheme(webLightTheme);
+    setTheme(webDarkTheme);
+    setTheme(teamsDarkTheme);
+    setTheme(teamsLightTheme);
+
+    // Cached themes
+    setTheme(webLightTheme);
+    setTheme(webDarkTheme);
+    setTheme(teamsDarkTheme);
+    setTheme(teamsLightTheme);
+
+    endMeasure();
+
+    onComplete();
+  },
+};
+
+export { tests };
