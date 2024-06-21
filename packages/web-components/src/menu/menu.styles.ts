8cfadd0245da036b77c921a7e6a128969cb72462
Dave Rupert <rupertdavid@microsoft.com>
2024-06-21 16:49:54 -0500

    refactor(web-components): refactor menu to popover (#31674)

--- packages/web-components/src/menu/menu.styles.ts
+++ packages/web-components/src/menu/menu.styles.ts
@@ -1,18 +1,31 @@
 import { css } from '@microsoft/fast-element';
-import {} from '../theme/design-tokens.js';
+import { display } from '../utils/index.js';
 
 /** Menu styles
  * @public
  */
 export const styles = css`
-  :host {
-    position: relative;
-    z-index: var(--z-index-menu, 1);
+  ${display('inline-block')}
+
+  ::slotted([slot='trigger']) {
+    anchor-name: --menu-trigger;
   }
-  .positioning-container {
-    position: fixed;
-    top: 0;
-    left: 0;
-    transform: translate(0, 0);
+
+  ::slotted([popover]) {
+    inset-area: block-end span-inline-end;
+    margin: 0;
+    max-height: var(--menu-max-height, auto);
+    position-anchor: --menu-trigger;
+    position-try-options: flip-block;
+    position: absolute;
+    z-index: 1;
+  }
+
+  ::slotted([popover]:popover-open) {
+    inset: unset;
+  }
+
+  ::slotted([popover]:not(:popover-open)) {
+    display: none;
   }
 `;
