5bc67ade99cba68ff80113480aacf49c70e74a04
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-06-26 14:24:02 -0700

    scaffold divider and app node

---
+++ packages/react-components/react-nav-preview/library/src/components/AppNode/AppNode.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
+import { useAppNode_unstable } from './useAppNode';
+import { renderAppNode_unstable } from './renderAppNode';
+import { useAppNodeStyles_unstable } from './useAppNodeStyles.styles';
+import type { AppNodeProps } from './AppNode.types';
+
+/**
+ * AppNode component - TODO: add more docs
+ */
+export const AppNode: ForwardRefComponent<AppNodeProps> = React.forwardRef((props, ref) => {
+  const state = useAppNode_unstable(props, ref);
+
+  useAppNodeStyles_unstable(state);
+  // TODO update types in packages/react-components/react-shared-contexts/src/CustomStyleHooksContext/CustomStyleHooksContext.ts
+  // https://github.com/microsoft/fluentui/blob/master/rfcs/react-components/convergence/custom-styling.md
+  useCustomStyleHook_unstable('useAppNodeStyles_unstable')(state);
+  return renderAppNode_unstable(state);
+});
+
+AppNode.displayName = 'AppNode';
