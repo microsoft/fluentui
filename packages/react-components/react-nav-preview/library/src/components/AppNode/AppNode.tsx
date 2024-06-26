37b1f50a378d93159dc07e0ea72da0c1af95258b
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-06-26 15:20:17 -0700

    remove custom style hooks

--- packages/react-components/react-nav-preview/library/src/components/AppNode/AppNode.tsx
+++ packages/react-components/react-nav-preview/library/src/components/AppNode/AppNode.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
-import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
 import { useAppNode_unstable } from './useAppNode';
 import { renderAppNode_unstable } from './renderAppNode';
 import { useAppNodeStyles_unstable } from './useAppNodeStyles.styles';
@@ -15,7 +14,7 @@ export const AppNode: ForwardRefComponent<AppNodeProps> = React.forwardRef((prop
   useAppNodeStyles_unstable(state);
   // TODO update types in packages/react-components/react-shared-contexts/src/CustomStyleHooksContext/CustomStyleHooksContext.ts
   // https://github.com/microsoft/fluentui/blob/master/rfcs/react-components/convergence/custom-styling.md
-  useCustomStyleHook_unstable('useAppNodeStyles_unstable')(state);
+  // useCustomStyleHook_unstable('useAppNodeStyles_unstable')(state);
   return renderAppNode_unstable(state);
 });
 
--- packages/react-components/react-nav-preview/library/src/components/NavDivider/NavDivider.tsx
+++ packages/react-components/react-nav-preview/library/src/components/NavDivider/NavDivider.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
-import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
 import { useNavDivider_unstable } from './useNavDivider';
 import { renderNavDivider_unstable } from './renderNavDivider';
 import { useNavDividerStyles_unstable } from './useNavDividerStyles.styles';
@@ -15,7 +14,7 @@ export const NavDivider: ForwardRefComponent<NavDividerProps> = React.forwardRef
   useNavDividerStyles_unstable(state);
   // TODO update types in packages/react-components/react-shared-contexts/src/CustomStyleHooksContext/CustomStyleHooksContext.ts
   // https://github.com/microsoft/fluentui/blob/master/rfcs/react-components/convergence/custom-styling.md
-  useCustomStyleHook_unstable('useNavDividerStyles_unstable')(state);
+  // useCustomStyleHook_unstable('useNavDividerStyles_unstable')(state);
   return renderNavDivider_unstable(state);
 });
 
