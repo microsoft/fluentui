8feeeae6fd4c701c03f6765ce2f18e7f6394d473
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-06-27 12:10:26 -0700

    use ElementInternals in radio component

--- packages/web-components/src/radio/radio.ts
+++ packages/web-components/src/radio/radio.ts
@@ -1,122 +1,80 @@
-import { observable } from '@microsoft/fast-element';
-import { keySpace } from '@microsoft/fast-web-utilities';
-import type { RadioGroup } from '../radio-group/index.js';
-import type { StaticallyComposableHTML } from '../utils/template-helpers.js';
-import { FormAssociatedRadio } from './radio.form-associated.js';
-
-/**
- * @public
- */
-export type RadioControl = Pick<HTMLInputElement, 'checked' | 'disabled' | 'focus' | 'setAttribute' | 'getAttribute'>;
-
-/**
- * Radio configuration options
- * @public
- */
-export type RadioOptions = {
-  checkedIndicator?: StaticallyComposableHTML<Radio>;
-};
+import { BaseCheckbox } from '../checkbox/checkbox.js';
 
 /**
  * A Radio Custom HTML Element.
- * Implements the {@link https://www.w3.org/TR/wai-aria-1.1/#radio | ARIA radio }.
+ * Implements the {@link https://w3c.github.io/aria/#radio | ARIA `radio` role}.
  *
- * @slot checked-indicator - The checked indicator
- * @slot - The default slot for the label
- * @csspart control - The element representing the visual radio control
- * @csspart label - The label
+ * @slot checked-indicator - The checked indicator slot
  * @fires change - Emits a custom change event when the checked state changes
+ * @fires input - Emits a custom input event when the checked state changes
  *
  * @public
  */
-export class Radio extends FormAssociatedRadio implements RadioControl {
+export class Radio extends BaseCheckbox {
   /**
-   * The name of the radio. See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefname | name attribute} for more info.
-   */
-  @observable
-  public name!: string;
-
-  /**
-   * The element's value to be included in form submission when checked.
-   * Default to "on" to reach parity with input[type="radio"]
+   * Indicates the position of the radio in a radio group.
    *
-   * @internal
+   * @public
+   * @remarks
+   * Reflects the {@link https://developer.mozilla.org/docs/Web/API/ElementInternals/ariaPosInSet | `ElementInternals.ariaPosInSet`} property.
    */
-  public initialValue: string = 'on';
+  public get ariaPosInSet(): string | null {
+    return this.elementInternals.ariaPosInSet;
+  }
 
-  /**
-   * @internal
-   */
-  @observable
-  public defaultSlottedNodes!: Node[];
-
-  private get radioGroup() {
-    return (this as HTMLElement).closest('[role=radiogroup]') as RadioGroup | null;
+  public set ariaPosInSet(value: string | null) {
+    this.elementInternals.ariaPosInSet = value;
   }
 
   /**
-   * @internal
+   * Indicates the number of radio buttons in the associated radio group.
+   *
+   * @public
+   * @remarks
+   * Reflects the {@link https://developer.mozilla.org/docs/Web/API/ElementInternals/ariaSetSize | `ElementInternals.ariaSetSize`} property.
    */
-  public defaultCheckedChanged(): void {
-    if (this.$fastController.isConnected && !this.dirtyChecked) {
-      // Setting this.checked will cause us to enter a dirty state,
-      // but if we are clean when defaultChecked is changed, we want to stay
-      // in a clean state, so reset this.dirtyChecked
-      if (!this.isInsideRadioGroup()) {
-        this.checked = this.defaultChecked ?? false;
-        this.dirtyChecked = false;
-      }
-    }
+  public get ariaSetSize(): string | null {
+    return this.elementInternals.ariaSetSize;
+  }
+
+  public set ariaSetSize(value: string | null) {
+    this.elementInternals.ariaSetSize = value;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.tabIndex = this.disabled ? -1 : 0;
   }
 
   constructor() {
     super();
-    this.proxy.setAttribute('type', 'radio');
+    this.elementInternals.role = 'radio';
   }
 
-  /**
-   * @internal
-   */
-  public connectedCallback(): void {
-    super.connectedCallback();
-    this.validate();
-
-    if (this.parentElement?.getAttribute('role') !== 'radiogroup' && this.getAttribute('tabindex') === null) {
-      if (!this.disabled) {
-        this.setAttribute('tabindex', '0');
-      }
+  protected disabledChanged(prev: boolean | undefined, next: boolean | undefined): void {
+    super.disabledChanged(prev, next);
+    if (next) {
+      this.checked = false;
+      this.tabIndex = -1;
     }
 
-    if (this.checkedAttribute) {
-      if (!this.dirtyChecked) {
-        // Setting this.checked will cause us to enter a dirty state,
-        // but if we are clean when defaultChecked is changed, we want to stay
-        // in a clean state, so reset this.dirtyChecked
-        if (!this.isInsideRadioGroup()) {
-          this.checked = this.defaultChecked ?? false;
-          this.dirtyChecked = false;
-        }
-      }
-    }
+    this.$emit('disabled', next, { bubbles: true });
   }
 
-  private isInsideRadioGroup(): boolean {
-    return this.radioGroup !== null;
+  public requiredChanged(): void {
+    return;
   }
 
-  /**
-   * Handles key presses on the radio.
-   * @beta
-   */
-  public keypressHandler(e: KeyboardEvent): boolean | void {
-    switch (e.key) {
-      case keySpace:
-        if (!this.checked && !this.radioGroup?.readOnly) {
-          this.checked = true;
-        }
-        return;
-    }
+  public setFormValue(): void {
+    return;
+  }
 
-    return true;
+  public setValidity(): void {
+    this.elementInternals.setValidity({});
+  }
+
+  public toggleChecked(state: boolean = true): void {
+    super.toggleChecked(state);
   }
 }
