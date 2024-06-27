c4878f367d3fd2a7bffb186305aae4e5e50eebdf
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-06-27 12:12:20 -0700

    use ElementInternals in radio-group component

--- packages/web-components/src/radio-group/radio-group.template.ts
+++ packages/web-components/src/radio-group/radio-group.template.ts
@@ -1,34 +1,25 @@
-import { elements, html, slotted } from '@microsoft/fast-element';
 import type { ElementViewTemplate } from '@microsoft/fast-element';
+import { children, html } from '@microsoft/fast-element';
+import { Radio } from '../radio/radio.js';
 import type { RadioGroup } from './radio-group.js';
-import { RadioGroupOrientation } from './radio-group.options.js';
 
 export function radioGroupTemplate<T extends RadioGroup>(): ElementViewTemplate<T> {
   return html<T>`
     <template
-      role="radiogroup"
-      tabindex="${x => (x.disabled ? -1 : void 0)}"
-      aria-disabled="${x => x.disabled}"
-      aria-readonly="${x => x.readOnly}"
-      aria-orientation="${x => x.orientation}"
+      @disabled="${(x, c) => x.disabledRadioHandler(c.event as CustomEvent)}"
+      @change="${(x, c) => x.changeHandler(c.event as Event)}"
       @click="${(x, c) => x.clickHandler(c.event as MouseEvent)}"
-      @mousedown="${(x, c) => x.handleDisabledClick(c.event as MouseEvent)}"
+      @focusin="${(x, c) => x.focusinHandler(c.event as FocusEvent)}"
+      @focusout="${(x, c) => x.focusoutHandler(c.event as FocusEvent)}"
       @keydown="${(x, c) => x.keydownHandler(c.event as KeyboardEvent)}"
-      @focusout="${(x, c) => x.focusOutHandler(c.event as FocusEvent)}"
+      ${children({
+        property: 'radios',
+        filter: x => x instanceof Radio,
+        selector: '*',
+        subtree: true,
+      })}
     >
-      <slot name="label"></slot>
-      <div
-        class="positioning-region ${x =>
-          x.orientation === RadioGroupOrientation.horizontal ? 'horizontal' : 'vertical'}"
-        part="positioning-region"
-      >
-        <slot
-          ${slotted({
-            property: 'slottedRadioButtons',
-            filter: elements('[role=radio]'),
-          })}
-        ></slot>
-      </div>
+      <slot></slot>
     </template>
   `;
 }
