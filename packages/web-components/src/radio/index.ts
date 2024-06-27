0b69e040f1571e44619f929c736167737c50871f
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-06-27 12:10:26 -0700

    use ElementInternals in radio component

--- packages/web-components/src/radio/index.ts
+++ packages/web-components/src/radio/index.ts
@@ -1,5 +1,5 @@
 export { definition as RadioDefinition } from './radio.definition.js';
 export { Radio } from './radio.js';
-export type { RadioControl, RadioOptions } from './radio.js';
+export type { RadioControl, RadioOptions } from './radio.options.js';
 export { styles as RadioStyles } from './radio.styles.js';
 export { template as RadioTemplate } from './radio.template.js';
