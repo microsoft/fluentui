edc55fa4fd0744751010ec6b5646a2f424b17531
Mitch-At-Work <mifraser@microsoft.com>
2024-07-02 11:39:18 -0700

    Update test

--- packages/react-components/react-carousel-preview/library/src/components/CarouselSlider/CarouselSlider.test.tsx
+++ packages/react-components/react-carousel-preview/library/src/components/CarouselSlider/CarouselSlider.test.tsx
@@ -2,22 +2,12 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { CarouselSlider } from './CarouselSlider';
-import type { IsConformantOptions } from '@fluentui/react-conformance';
-
-export const getRootElement: IsConformantOptions['getTargetElement'] = (result, attr) => {
-  const sliderRoot = result.baseElement.querySelectorAll('.fui-CarouselSlider');
-  expect(sliderRoot[0]).not.toBeNull();
-  return sliderRoot[0]! as HTMLElement;
-};
 
 describe('CarouselSlider', () => {
   isConformant({
     Component: CarouselSlider,
     displayName: 'CarouselSlider',
-    // getTargetElement: getRootElement,
   });
-
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
 
   it('renders a default state', () => {
     const result = render(<CarouselSlider>Default CarouselSlider</CarouselSlider>);
