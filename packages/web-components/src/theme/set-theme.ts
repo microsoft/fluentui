e5d7891fc5e8f4dd4a2c42c928af8fe1627ead6d
Zacky Ma <machi@microsoft.com>
2024-07-08 16:33:48 -0700

    refactor setTheme() for setting theme on elements

--- packages/web-components/src/theme/set-theme.ts
+++ packages/web-components/src/theme/set-theme.ts
@@ -1,28 +1,46 @@
 import type { Theme } from '@fluentui/tokens';
+import { uniqueId } from '@microsoft/fast-web-utilities';
+
 import * as tokens from './design-tokens.js';
 
 const tokenNames = Object.keys(tokens) as (keyof Theme)[];
 
 const SUPPORTS_REGISTER_PROPERTY = 'registerProperty' in CSS;
 const SUPPORTS_ADOPTED_STYLE_SHEETS = 'adoptedStyleSheets' in document;
-const themeStyleSheet = new CSSStyleSheet();
+const SUPPORTS_CSS_SCOPE = 'CSSScopeRule' in window;
+
+const globalThemeStyleSheet = new CSSStyleSheet();
+
+// A map from a theme to Custom Property declarations for the theme as a string.
 const themeStyleTextMap = new Map<Theme, string>();
 
 /**
  * Sets the theme tokens on defaultNode.
- * @param theme - Flat object of theme token values.
+ * @param theme - Flat object of theme token values, set to `null` to unset
+ *     the theme.
+ * @param node - The node to set the theme on, defaults to `document` for
+ *     setting global theme.
  * @internal
  */
-export const setTheme = (theme: Theme) => {
+export const setTheme = (theme: Theme | null, node: Document | HTMLElement = document) => {
   // Fallback to setting token custom properties on `<html>` element’s `style`
   // attribute, only checking the support of  `document.adoptedStyleSheets`
   // here because it has broader support than `CSS.registerProperty()`, which
   // is checked later.
   if (!SUPPORTS_ADOPTED_STYLE_SHEETS) {
-    setThemeFor(document.documentElement, theme);
+    const target: HTMLElement = document ? document.documentElement : (node as HTMLElement);
+    setThemePropertiesOnElement(theme, target);
     return;
   }
 
+  if (node === document) {
+    setGlobalTheme(theme);
+  } else {
+    setElementTheme(theme, node as HTMLElement);
+  }
+};
+
+function getThemeStyleText(theme: Theme): string {
   if (!themeStyleTextMap.has(theme)) {
     const tokenDeclarations: string[] = [];
 
@@ -39,23 +57,59 @@ export const setTheme = (theme: Theme) => {
       tokenDeclarations.push(`--${t}: ${theme[t] as string};`);
     }
 
-    themeStyleTextMap.set(theme, `html{${tokenDeclarations.join('')}}`);
+    themeStyleTextMap.set(theme, tokenDeclarations.join(''));
+  }
+
+  return themeStyleTextMap.get(theme)!;
+}
+
+function setGlobalTheme(theme: Theme | null) {
+  if (theme === null) {
+    if (document.adoptedStyleSheets.includes(globalThemeStyleSheet)) {
+      globalThemeStyleSheet.replaceSync('');
+    }
+    return;
   }
 
   // Update the CSSStyleSheet with the new theme
-  themeStyleSheet.replaceSync(themeStyleTextMap.get(theme)!);
+  globalThemeStyleSheet.replaceSync(`html{${getThemeStyleText(theme)}}`);
 
   // Adopt the updated CSSStyleSheet if it hasn't been adopted yet
-  if (!document.adoptedStyleSheets.includes(themeStyleSheet)) {
-    document.adoptedStyleSheets.push(themeStyleSheet);
+  if (!document.adoptedStyleSheets.includes(globalThemeStyleSheet)) {
+    document.adoptedStyleSheets.push(globalThemeStyleSheet);
   }
-};
+}
+
+function setElementTheme(theme: Theme | null, element: HTMLElement) {
+  if (theme === null) {
+    if (element.shadowRoot) {
+      // TODO
+    } else {
+      delete element.dataset.fluentThemed;
+    }
+    return;
+  }
+
+  if (element.shadowRoot) {
+    // TODO: set theme for an element with shadow root on its adopted style sheets.
+  } else {
+    // TODO: set theme for an element with CSS scope.
+  }
+}
 
 /**
  * @internal
  */
-export const setThemeFor = (element: HTMLElement, theme: Theme) => {
+function setThemePropertiesOnElement(theme: Theme | null, element: HTMLElement) {
   for (const t of tokenNames) {
-    element.style.setProperty(`--${t}`, theme[t] as string);
+    element.style.setProperty(`--${t}`, theme !== null ? (theme[t] as string) : 'unset');
   }
+}
+
+/**
+ * @internal
+ * @deprecated Use `setTheme(theme, element)` instead.
+ */
+export const setThemeFor = (element: HTMLElement, theme: Theme | null) => {
+  setThemePropertiesOnElement(theme, element);
 };
