0ed153e7a324c6474b2ef5415da465562e75fb6a
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-07-09 16:46:12 -0700

    fix test again

--- packages/react-components/react-nav-preview/library/src/components/AppNode/AppNode.test.tsx
+++ packages/react-components/react-nav-preview/library/src/components/AppNode/AppNode.test.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { AppNode } from './AppNode';
 import { appNodeClassNames } from './useAppNodeStyles.styles';
