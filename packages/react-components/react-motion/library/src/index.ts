ccdcfa5819bfbf749e3a0625eaba2a157a6ec100
Alexander Katrukhin <olkatruk@microsoft.com>
2024-07-10 11:45:02 -0400

    feat(motion): add createPresenceComponentVariant() factory (#31943)

--- packages/react-components/react-motion/library/src/index.ts
+++ packages/react-components/react-motion/library/src/index.ts
@@ -1,7 +1,12 @@
 export { motionTokens, durations, curves } from './motions/motionTokens';
 
 export { createMotionComponent, type MotionComponentProps } from './factories/createMotionComponent';
-export { createPresenceComponent, type PresenceComponentProps } from './factories/createPresenceComponent';
+export {
+  createPresenceComponent,
+  type PresenceComponentProps,
+  type PresenceComponent,
+} from './factories/createPresenceComponent';
+export { createPresenceComponentVariant } from './factories/createPresenceComponentVariant';
 
 export { PresenceGroup } from './components/PresenceGroup';
 
