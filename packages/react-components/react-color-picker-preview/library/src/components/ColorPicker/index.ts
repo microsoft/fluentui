a9b717f718bd46410bb94ca4edeeb9edf6ccb461
Valentyna <vkozlova@microsoft.com>
2024-07-11 17:16:37 +0200

    Revert "updated tsconfig.base.all.json"
    This reverts commit fa0cc3068aca31d81f02cd63a8d7ab1455ce5c28.
    
    Revert "Generated empty ColorPicker component"
    
    This reverts commit 1cdd0a62b78851dee26bae1405892596c4520333.

--- packages/react-components/react-color-picker-preview/library/src/components/ColorPicker/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ColorPicker';
-export * from './ColorPicker.types';
-export * from './renderColorPicker';
-export * from './useColorPicker';
-export * from './useColorPickerStyles.styles';
--- packages/react-components/react-color-picker-preview/stories/src/ColorPicker/ColorPickerDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { ColorPicker, ColorPickerProps } from '@fluentui/react-color-picker-preview';
-
-export const Default = (props: Partial<ColorPickerProps>) => <ColorPicker {...props} />;
