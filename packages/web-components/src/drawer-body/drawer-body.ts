9f0f51868c2e63c397ba99c127752f87b1f6897e
Dave Rupert <rupertdavid@microsoft.com>
2024-07-12 15:03:25 -0700

    chore(web-components): cleanup and simplify drawer storybook

--- packages/web-components/src/drawer-body/drawer-body.ts
+++ packages/web-components/src/drawer-body/drawer-body.ts
@@ -1,3 +1,10 @@
 import { FASTElement } from '@microsoft/fast-element';
 
-export class DrawerBody extends FASTElement {}
+export class DrawerBody extends FASTElement {
+  /**
+   * @slot title - The title slot
+   * @slot close - The close button slot
+   * @slot - The default slot
+   * @slot footer - The footer slot
+   */
+}
