8f5164114742456885fa17157a52e4c2c455a372
mainframev <vgenaev@gmail.com>
2024-07-14 15:34:30 +0200

    chore(scripts): remove eslint camelcase errors

--- scripts/lint-staged/src/eslint-for-package.js
+++ scripts/lint-staged/src/eslint-for-package.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const { eslintConstants } = require('@fluentui/scripts-monorepo');
 const { ESLint } = require('eslint');
