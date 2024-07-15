b86019978bc13837fc83be9a9af8a6e68ab62022
Anush2303 <74965306+Anush2303@users.noreply.github.com>
2024-07-15 08:58:25 +0000

    change in Largedata example

--- packages/react-components/react-charts-preview/stories/LineChart/LineChartLargeData.stories.tsx
+++ packages/react-components/react-charts-preview/stories/LineChart/LineChartLargeData.stories.tsx
@@ -20,7 +20,7 @@ export const LCLargeData = (props: ILineChartProps) => {
     setAllowMultipleShapes(checked);
   };
 
-  const _getData = () => {
+  const _getdata = () => {
     const data: ILineChartDataPoint[] = [];
     const startdate = new Date('2020-03-01T00:00:00.000Z');
     for (let i = 0; i < 10000; i++) {
