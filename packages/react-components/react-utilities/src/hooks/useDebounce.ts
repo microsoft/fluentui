255a3fb4d3b36d3b7df7924b332815557053f250
Emma Jiang <jiangemma@microsoft.com>
2024-07-16 10:24:13 -0700

    move useDebounce from FAI to react-utilities

---
+++ packages/react-components/react-utilities/src/hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { useTimeout } from './useTimeout'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const useDebounce = (fn: (...args: any[]) => void, duration: number) => {
+  const [setTimeout, clearTimeout] = useTimeout();
+  return React.useCallback(
+    (...args: unknown[]) => {
+      clearTimeout();
+      setTimeout(() => {
+        fn(...args);
+      }, duration);
+    },
+    [clearTimeout, duration, fn, setTimeout],
+  );
+};
