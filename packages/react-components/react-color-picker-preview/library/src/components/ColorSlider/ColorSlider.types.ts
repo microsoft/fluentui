bec2fbee8c43e2038219963cb826d6da4876c978
Valentyna <vkozlova@microsoft.com>
2024-07-19 09:47:15 +0200

    Added color slider

--- packages/react-components/react-color-picker-preview/library/src/components/ColorSlider/ColorSlider.types.ts
+++ packages/react-components/react-color-picker-preview/library/src/components/ColorSlider/ColorSlider.types.ts
@@ -1,17 +1,54 @@
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
 
 export type ColorSliderSlots = {
-  root: Slot<'div'>;
+  root: NonNullable<Slot<'div'>>;
+  rail: NonNullable<Slot<'div'>>;
+  thumb: NonNullable<Slot<'div'>>;
+  input: NonNullable<Slot<'input'>> & {
+    /**
+     * Orient is a non standard attribute that allows for vertical orientation in Firefox. It is set internally
+     * when `vertical` is set to true.
+     * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range#non_standard_attributes
+     * Webkit/Chromium support for vertical inputs is provided via -webkit-appearance css property
+     */
+    orient?: 'horizontal' | 'vertical';
+  };
 };
 
 /**
  * ColorSlider Props
  */
-export type ColorSliderProps = ComponentProps<ColorSliderSlots> & {};
+export type ColorSliderProps = Omit<
+  ComponentProps<Partial<ColorSliderSlots>, 'input'>,
+  'defaultValue' | 'onChange' | 'size' | 'value'
+> & {
+  value?: number;
+  /**
+   * The max value of the Slider.
+   * @default 100
+   */
+  max?: number;
+
+  /**
+   * The min value of the Slider.
+   * @default 0
+   */
+  min?: number;
+
+  onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: SliderOnChangeData) => void;
+
+  /**
+   * Render the Slider in a vertical orientation, smallest value on the bottom.
+   * @default `false`
+   */
+  vertical?: boolean;
+};
+
+export type SliderOnChangeData = {
+  value: number;
+};
 
 /**
  * State used in rendering ColorSlider
  */
-export type ColorSliderState = ComponentState<ColorSliderSlots>;
-// TODO: Remove semicolon from previous line, uncomment next line, and provide union of props to pick from ColorSliderProps.
-// & Required<Pick<ColorSliderProps, 'propName'>>
+export type ColorSliderState = ComponentState<ColorSliderSlots> & Pick<ColorSliderProps, 'vertical'>;
---
+++ packages/react-components/react-color-picker-preview/library/src/components/ColorSlider/useColorSliderState.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { clamp, useControllableState, useEventCallback } from '@fluentui/react-utilities';
+import { useFluent_unstable as useFluent } from '@fluentui/react-shared-contexts';
+import { colorSliderCSSVars } from './useColorSliderStyles.styles';
+import type { ColorSliderState, ColorSliderProps } from './ColorSlider.types';
+
+const { sliderStepsPercentVar, sliderProgressVar, sliderDirectionVar, thumbColorVar } = colorSliderCSSVars;
+
+const MAX_COLOR_HUE = 360;
+
+const getPercent = (value: number, min: number, max: number) => {
+  return max === min ? 0 : ((value - min) / (max - min)) * 100;
+};
+
+export const useColorSliderState_unstable = (state: ColorSliderState, props: ColorSliderProps) => {
+  'use no memo';
+
+  const { min = 0, max = MAX_COLOR_HUE } = props;
+  const step = 1;
+  const { dir } = useFluent();
+  const [currentValue, setCurrentValue] = useControllableState({
+    state: props.value,
+    defaultState: props.value,
+    initialState: 0,
+  });
+  const clampedValue = clamp(currentValue, min, max);
+  const valuePercent = getPercent(clampedValue, min, max);
+
+  const inputOnChange = state.input.onChange;
+  const propsOnChange = props.onChange;
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = useEventCallback(ev => {
+    const newValue = Number(ev.target.value);
+    setCurrentValue(clamp(newValue, min, max));
+
+    if (inputOnChange && inputOnChange !== propsOnChange) {
+      inputOnChange(ev);
+    } else if (propsOnChange) {
+      propsOnChange(ev, { value: newValue });
+    }
+  });
+
+  const rootVariables = {
+    [sliderDirectionVar]: state.vertical ? '0deg' : dir === 'ltr' ? '90deg' : '270deg',
+    [sliderStepsPercentVar]: step && step > 0 ? `${(step * 100) / (max - min)}%` : '',
+    [sliderProgressVar]: `${valuePercent}%`,
+    [thumbColorVar]: `hsl(${clampedValue}, 100%, 50%)`,
+  };
+
+  // Root props
+  state.root.style = {
+    ...rootVariables,
+    ...state.root.style,
+  };
+
+  // Input Props
+  state.input.value = clampedValue;
+  state.input.onChange = onChange;
+
+  return state;
+};
