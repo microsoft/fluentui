c97bb0764808a927cbd75efbb04737aeae273c42
Patrick Wang <yuewang1@microsoft.com>
2024-07-19 17:26:10 +0800

    add: more exports and change file

--- packages/web-components/src/tree-item/index.ts
+++ packages/web-components/src/tree-item/index.ts
@@ -1,1 +1,4 @@
 export { TreeItem } from './tree-item.js';
+export { template as treeItemTemplate } from './tree-item.template.js';
+export { styles as treeItemStyles } from './tree-item.style.js';
+export { treeItemDefinition } from './tree-item.definition.js';
--- packages/web-components/src/tree-view/index.ts
+++ packages/web-components/src/tree-view/index.ts
@@ -1,1 +1,4 @@
 export { TreeView } from './tree-view.js';
+export { template as treeItemTemplate } from './tree-view.template.js';
+export { styles as treeItemStyles } from './tree-view.style.js';
+export { treeViewDefinition } from './tree-view.definition.js';
