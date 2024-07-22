1bfb9c97a65007e4a97936a3f189d0495864f2f9
Dave Rupert <rupertdavid@microsoft.com>
2024-07-22 12:35:09 -0500

    fix(web-components): move spinner animation off of mainthread (#32056)

--- packages/web-components/src/spinner/spinner.template.ts
+++ packages/web-components/src/spinner/spinner.template.ts
@@ -2,9 +2,17 @@ import { html } from '@microsoft/fast-element';
 import { Spinner } from './spinner.js';
 
 export const template = html<Spinner>`
-  <slot name="indicator"
-    ><svg class="progress" part="progress" viewBox="0 0 16 16">
-      <circle class="background" cx="8px" cy="8px" r="7px"></circle>
-      <circle class="indicator" cx="8px" cy="8px" r="7px"></circle></svg
-  ></slot>
+  <slot name="indicator">
+    <div class="background"></div>
+    <div class="progress">
+      <div class="spinner">
+        <div class="start">
+          <div class="indicator"></div>
+        </div>
+        <div class="end">
+          <div class="indicator"></div>
+        </div>
+      </div>
+    </div>
+  </slot>
 `;
