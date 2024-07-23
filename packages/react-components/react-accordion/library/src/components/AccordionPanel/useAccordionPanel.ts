08b6e61fdffada1813ed97c29ebb03a279af7e7d
Alexander Katrukhin <olkatruk@microsoft.com>
2024-07-23 17:57:43 -0400

    feat: add collapse motion for Accordion
    - Updated dependencies in `package.json`
    - Enhanced `useAccordionHeader.tsx` for collapse motion
    - Added `AccordionMotion.ts`
    - Updated types and rendering logic in Accordion components
    - Styled `useAccordionPanelStyles.styles.ts` for animations
    - Created stories custom motion Accordion

--- packages/react-components/react-accordion/library/src/components/AccordionPanel/useAccordionPanel.ts
+++ packages/react-components/react-accordion/library/src/components/AccordionPanel/useAccordionPanel.ts
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { getIntrinsicElementProps, slot } from '@fluentui/react-utilities';
 import { useTabsterAttributes } from '@fluentui/react-tabster';
+import { presenceMotionSlot, type PresenceMotionSlotProps } from '@fluentui/react-motion';
+import { Collapse } from '@fluentui/react-motion-components-preview';
 import { useAccordionContext_unstable } from '../../contexts/accordion';
 import type { AccordionPanelProps, AccordionPanelState } from './AccordionPanel.types';
 import { useAccordionItemContext_unstable } from '../../contexts/accordionItem';
@@ -22,17 +24,28 @@ export const useAccordionPanel_unstable = (
     open,
     components: {
       root: 'div',
+      // TODO: remove once React v18 slot API is modified
+      // This is a problem at the moment due to UnknownSlotProps assumption
+      // that `children` property is `ReactNode`, which in this case is not valid
+      // as PresenceComponentProps['children'] is `ReactElement`
+      collapseMotion: Collapse as React.FC<PresenceMotionSlotProps>,
     },
     root: slot.always(
       getIntrinsicElementProps('div', {
         // FIXME:
         // `ref` is wrongly assigned to be `HTMLElement` instead of `HTMLDivElement`
         // but since it would be a breaking change to fix it, we are casting ref to it's proper type
-        ref: ref as React.Ref<HTMLDivElement>,
         ...props,
         ...(navigation && focusableProps),
       }),
       { elementType: 'div' },
     ),
+    collapseMotion: presenceMotionSlot(props.collapseMotion, {
+      elementType: Collapse,
+      defaultProps: {
+        visible: open,
+        unmountOnExit: true,
+      },
+    }),
   };
 };
