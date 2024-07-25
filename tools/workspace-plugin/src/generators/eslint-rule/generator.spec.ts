543e279eea15f1a792396d770bda29b67966c301
mainframev <vgenaev@gmail.com>
2024-07-25 13:49:08 +0200

    fixup! chore(tools): adjust eslint rule generator

--- tools/workspace-plugin/src/generators/eslint-rule/generator.spec.ts
+++ tools/workspace-plugin/src/generators/eslint-rule/generator.spec.ts
@@ -70,9 +70,7 @@ describe('eslint-rule generator', () => {
     expect(tree.read(paths.spec, 'utf-8')).toMatchInlineSnapshot(`
       "import { RuleTester } from '@typescript-eslint/rule-tester';
       import { rule, RULE_NAME } from './uppercase';
-
       const ruleTester = new RuleTester();
-
       ruleTester.run(RULE_NAME, rule, {
         valid: [\`const example = true;\`],
         invalid: [],
