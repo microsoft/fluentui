41c36fecbee0423e3458bb8a8e7f20c0056d01de
Mason Tejera <17346018+mltejera@users.noreply.github.com>
2024-07-25 10:26:44 -0700

    remove snaps

--- packages/react-components/react-nav-preview/library/src/components/AppItem/AppItem.test.tsx
+++ packages/react-components/react-nav-preview/library/src/components/AppItem/AppItem.test.tsx
@@ -8,11 +8,4 @@ describe('AppItem', () => {
     Component: AppItem,
     displayName: 'AppItem',
   });
-
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
-
-  it('renders a default state', () => {
-    const result = render(<AppItem>Default AppItem</AppItem>);
-    expect(result.container).toMatchSnapshot();
-  });
 });
