aac32549e99a4cd86d6fd685a7952cd7db0e526a
Oleksandr Fediashov <olfedias@microsoft.com>
2024-07-29 12:08:28 +0200

    restore ref

--- packages/react-components/react-accordion/library/src/components/AccordionPanel/useAccordionPanel.ts
+++ packages/react-components/react-accordion/library/src/components/AccordionPanel/useAccordionPanel.ts
@@ -35,6 +35,7 @@ export const useAccordionPanel_unstable = (
         // FIXME:
         // `ref` is wrongly assigned to be `HTMLElement` instead of `HTMLDivElement`
         // but since it would be a breaking change to fix it, we are casting ref to it's proper type
+        ref: ref as React.Ref<HTMLDivElement>,
         ...props,
         ...(navigation && focusableProps),
       }),
