8c2703045489e9b07e154797671d9fec47496c6e
Zacky Ma <machi@microsoft.com>
2024-07-29 11:34:15 -0700

    improve docs

--- packages/web-components/src/theme/set-theme.stories.ts
+++ packages/web-components/src/theme/set-theme.stories.ts
@@ -2,15 +2,92 @@ import { html } from '@microsoft/fast-element';
 import { teamsDarkTheme, teamsLightTheme, webDarkTheme, webLightTheme } from '@fluentui/tokens';
 
 import { renderComponent } from '../helpers.stories.js';
-import { setTheme } from './set-theme.js';
+import { setTheme, type Theme } from './set-theme.js';
+
+const fluentTheme: Record<string, Theme> = {
+  teamsDarkTheme,
+  teamsLightTheme,
+  webDarkTheme,
+  webLightTheme,
+};
 
 export default {
   title: 'Theme/SetTheme',
+  argTypes: {
+    theme: {
+      type: 'string',
+      control: 'select',
+      options: Object.keys(fluentTheme),
+      description: [
+        'Flat object of theme tokens. Each object entry’s key is the',
+        'name of the token (usually in camel case) and used as the CSS Custom',
+        'Property’s name. Note that this argument is not limited to existing',
+        'theme objects (from `@fluentui/tokens`), you can pass in an arbitrary',
+        'theme object as long as each entry’s value is either a string or a number.',
+      ].join(' '),
+    },
+  },
+  args: {
+    theme: 'webLightTheme',
+  },
 };
 
+const ComponentCloudTemplate = html`
+  <p><fluent-button>A button</fluent-button></p>
+  <p><fluent-spinner></fluent-spinner></p>
+  <p>
+    <fluent-text-input>
+      <fluent-label>Text input</fluent-label>
+    </fluent-text-input>
+  </p>
+  <fluent-divider></fluent-divider>
+  <p>
+    <fluent-menu>
+      <fluent-menu-button appearance="primary" slot="trigger">Toggle Menu</fluent-menu-button>
+      <fluent-menu-list>
+        <fluent-menu-item>Menu item 1</fluent-menu-item>
+        <fluent-menu-item>Menu item 2</fluent-menu-item>
+        <fluent-menu-item>Menu item 3</fluent-menu-item>
+        <fluent-menu-item>Menu item 4</fluent-menu-item>
+      </fluent-menu-list>
+    </fluent-menu>
+  </p>
+  <fluent-divider></fluent-divider>
+  <p>
+    <fluent-field>
+      <label slot="label" for="radiogroup">Radio group</label>
+      <fluent-radio-group slot="input" id="radiogroup" orientation="vertical">
+        <fluent-field label-position="after">
+          <fluent-radio slot="input" id="radiogroup-radio-1"></fluent-radio>
+          <label slot="label" for="radiogroup-radio-1">Option 1</label>
+        </fluent-field>
+        <fluent-field label-position="after">
+          <fluent-radio slot="input" id="radiogroup-radio-2"></fluent-radio>
+          <label slot="label" for="radiogroup-radio-2">Option 2</label>
+        </fluent-field>
+        <fluent-field label-position="after">
+          <fluent-radio slot="input" id="radiogroup-radio-3"></fluent-radio>
+          <label slot="label" for="radiogroup-radio-3">Option 3</label>
+        </fluent-field>
+      </fluent-radio-group>
+    </fluent-field>
+  </p>
+  <fluent-divider></fluent-divider>
+  <p>
+    <fluent-field label-position="after">
+      <label slot="label" for="checkbox">I would like this option</label>
+      <fluent-checkbox slot="input" id="checkbox"></fluent-checkbox>
+    </fluent-field>
+  </p>
+  <fluent-divider></fluent-divider>
+  <p>
+    <fluent-slider step="10" min="0" max="100"></fluent-slider>
+  </p>
+`;
+
 export const SetTheme = renderComponent(html`
-  <fluent-button @click="${() => setTheme(webLightTheme)}">webLightTheme</fluent-button>
-  <fluent-button @click="${() => setTheme(webDarkTheme)}">webDarkTheme</fluent-button>
-  <fluent-button @click="${() => setTheme(teamsLightTheme)}">teamsLightTheme</fluent-button>
-  <fluent-button @click="${() => setTheme(teamsDarkTheme)}">teamsDarkTheme</fluent-button>
+  <script>
+    ${x => setTheme(fluentTheme[x.theme])};
+  </script>
+  ${ComponentCloudTemplate}
 `);
--- packages/web-components/src/theme/set-theme.ts
+++ packages/web-components/src/theme/set-theme.ts
@@ -11,8 +11,15 @@ const themeStyleSheet = new CSSStyleSheet();
 const themeStyleTextMap = new Map<Theme, string>();
 
 /**
- * Sets the theme tokens on defaultNode.
- * @param theme - Flat object of theme token values.
+ * Sets the theme tokens as CSS Custom Properties. The Custom Properties are
+ * set in a constructed stylesheet on `document.adoptedStyleSheets` if
+ * supported, and on `document.documentElement.style` as a fallback.
+ *
+ * @param theme - Flat object of theme tokens. Each object entry’s key is the
+ *     name of the token (usually in camel case) and used as the CSS Custom
+ *     Property’s name. Note that this argument is not limited to existing
+ *     theme objects (from `@fluentui/tokens`), you can pass in an arbitrary
+ *     theme object as long as each entry’s value is either a string or a number.
  * @internal
  */
 export const setTheme = (theme: Theme) => {
@@ -29,6 +36,12 @@ export const setTheme = (theme: Theme) => {
     const tokenDeclarations: string[] = [];
 
     for (const [tokenName, tokenValue] of Object.entries(theme)) {
+      if (typeof tokenValue !== 'string' && Number.isNaN(tokenValue)) {
+        throw new Error(
+          `The value of "${tokenName}" must be a string or a number, but got "${tokenValue.toString()}" instead.`,
+        );
+      }
+
       if (SUPPORTS_REGISTER_PROPERTY) {
         try {
           CSS.registerProperty({
