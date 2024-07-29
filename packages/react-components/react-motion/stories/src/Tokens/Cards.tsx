b2ef2368aeb047a0e67bf2b2c5415950083a1bfb
Oleksandr Fediashov <olfedias@microsoft.com>
2024-07-29 14:12:30 +0200

    fix lint issues

--- packages/react-components/react-motion/stories/src/Tokens/Cards.tsx
+++ packages/react-components/react-motion/stories/src/Tokens/Cards.tsx
@@ -1,133 +1,7 @@
+import { Divider, motionTokens, tokens, Switch } from '@fluentui/react-components';
 import * as React from 'react';
-import { Divider, makeStyles, motionTokens, tokens, Switch } from '@fluentui/react-components';
 
-const useClasses = makeStyles({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  controls: {
-    alignSelf: 'end',
-    backgroundColor: tokens.colorNeutralBackground1,
-    borderRadius: tokens.borderRadiusMedium,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-    padding: '6px 10px',
-    border: `2px solid ${tokens.colorNeutralStroke1}`,
-    borderBottom: 'none',
-    width: 'fit-content',
-  },
-  card: {
-    backgroundColor: tokens.colorNeutralBackground1,
-    borderRadius: tokens.borderRadiusMedium,
-    borderTopRightRadius: 0,
-    padding: '30px',
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
-    gap: '30px 50px',
-    placeItems: 'center',
-    border: `2px solid ${tokens.colorNeutralStroke1}`,
-  },
-});
-
-const useCardClasses = makeStyles({
-  container: {
-    display: 'grid',
-    gridTemplate: `
-        "graph point"
-        "title ." auto / min-content var(--point-size)
-    `,
-    gap: '10px',
-
-    '--container-size': '250px',
-    '--point-size': '20px',
-  },
-  point: {
-    gridArea: 'point',
-
-    width: 'var(--point-size)',
-    height: 'var(--point-size)',
-
-    backgroundColor: tokens.colorBrandBackground,
-    clipPath: 'polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%)',
-
-    animationName: {
-      '0%': { transform: 'translateY(calc(var(--container-size) - var(--point-size)))' },
-      '60%, 100%': { transform: 'translateY(0)' },
-    },
-    animationDuration: '2s',
-    animationIterationCount: 'infinite',
-    animationFillMode: 'forwards',
-  },
-
-  graph: {
-    gridArea: 'graph',
-    position: 'relative',
-  },
-  graphX: {
-    color: tokens.colorNeutralStroke1,
-    fontFamily: tokens.fontFamilyMonospace,
-    position: 'absolute',
-    left: '10px',
-    top: '10px',
-  },
-  graphT: {
-    color: tokens.colorNeutralStroke1,
-    fontFamily: tokens.fontFamilyMonospace,
-    position: 'absolute',
-    bottom: '10px',
-    right: '10px',
-  },
-
-  duration: {
-    placeSelf: 'center',
-
-    width: '128px',
-    height: '4px',
-    margin: '64px auto',
-    backgroundColor: tokens.colorNeutralForeground3,
-
-    animationName: {
-      from: { transform: 'rotate(0)' },
-      to: { transform: 'rotate(180deg)' },
-    },
-    animationIterationCount: 'infinite',
-  },
-  svg: {
-    borderLeft: `1px solid ${tokens.colorNeutralStroke1}`,
-    borderBottom: `1px solid ${tokens.colorNeutralStroke1}`,
-
-    overflow: 'visible',
-
-    height: 'var(--container-size)',
-    width: 'var(--container-size)',
-  },
-
-  title: {
-    gridArea: 'title',
-    justifySelf: 'center',
-    margin: '0 20px',
-
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '5px',
-
-    fontFamily: tokens.fontFamilyMonospace,
-    fontSize: tokens.fontSizeBase400,
-    padding: '5px 10px',
-    backgroundColor: tokens.colorNeutralBackground3,
-    borderRadius: tokens.borderRadiusMedium,
-  },
-  name: {
-    textAlign: 'center',
-  },
-  value: {
-    fontSize: tokens.fontSizeBase200,
-    fontStyle: 'italic',
-    whiteSpace: 'nowrap',
-    textAlign: 'center',
-  },
-});
+import { useCardClasses, useClasses } from './Cards.styles';
 
 function getPointsFromEasing(value: string) {
   if (value.startsWith('cubic-bezier')) {
@@ -192,7 +66,7 @@ const MotionDurationCard: React.FC<{ animationEnabled: boolean; tokenName: strin
             animationDuration: `${tokenValue}ms`,
             animationPlayState: animationEnabled ? 'running' : 'paused',
           }}
-        ></div>
+        />
       </div>
       <div className={classes.title}>
         <div className={classes.name}>{tokenName}</div>
@@ -206,6 +80,7 @@ const MotionDurationCard: React.FC<{ animationEnabled: boolean; tokenName: strin
 function useAnimationEnabled() {
   const [animationEnabled, setAnimationEnabled] = React.useState(() => {
     // Heads up/ Not the best way to detect reduced motion as it breaks SSR, but it's good enough for Storybook
+    // eslint-disable-next-line no-restricted-globals
     const isReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
     return !isReducedMotion;
@@ -218,17 +93,14 @@ export const MotionCurves = () => {
   const classes = useClasses();
   const [animationEnabled, setAnimationEnabled] = useAnimationEnabled();
 
+  const handleChange = React.useCallback(() => {
+    setAnimationEnabled(e => !e);
+  }, [setAnimationEnabled]);
+
   return (
     <div className={classes.container}>
       <div className={classes.controls}>
-        <Switch
-          label="Enable animations"
-          type="checkbox"
-          checked={animationEnabled}
-          onChange={() => {
-            setAnimationEnabled(e => !e);
-          }}
-        />
+        <Switch label="Enable animations" type="checkbox" checked={animationEnabled} onChange={handleChange} />
       </div>
       <div className={classes.card}>
         {(
@@ -250,17 +122,14 @@ export const MotionDuration = () => {
   const classes = useClasses();
   const [animationEnabled, setAnimationEnabled] = useAnimationEnabled();
 
+  const handleChange = React.useCallback(() => {
+    setAnimationEnabled(e => !e);
+  }, [setAnimationEnabled]);
+
   return (
     <div className={classes.container}>
       <div className={classes.controls}>
-        <Switch
-          label="Enable animations"
-          type="checkbox"
-          checked={animationEnabled}
-          onChange={() => {
-            setAnimationEnabled(e => !e);
-          }}
-        />
+        <Switch label="Enable animations" type="checkbox" checked={animationEnabled} onChange={handleChange} />
       </div>
       <div className={classes.card}>
         {(
