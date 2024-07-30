d4cea44aa0dbdd158cff8129925e1b5f52d59072
Martin Hochel <martinhochel@microsoft.com>
2024-07-30 10:33:18 +0200

    feat(scripts-executors): use enquirer for start.js and tweak project descriptions UI

--- scripts/executors/src/start.js
+++ scripts/executors/src/start.js
@@ -1,77 +1,97 @@
 const { spawnSync } = require('node:child_process');
 
 const { workspaceRoot, output } = require('@nx/devkit');
-const prompts = require('prompts');
+const { AutoComplete } = require('enquirer');
 
 const targetDescription = {
-  build: 'Build the project',
-  'bundle-size': 'Check the bundle size diff of the project',
-  bundle: 'Bundle the project with webpack',
-  storybook: 'Start storybook for the project',
-  'build-storybook': 'Build production version of storybook for project',
-  'generate-api': 're-generate projects api.md',
-  lint: 'Run eslint on the project',
-  'type-check': 'type-check the project',
-  test: 'Run tests for the project',
-  'test-integration': 'Run integration tests for the project',
-  'test-ssr': 'Run server-side rendering tests for the project',
-  'test-vr': 'Run visual regression tests for the project',
-  'test-perf': 'Run performance tests for the project',
-  e2e: 'Run e2e/component tests for the project (cypress)',
-  'verify-packaging': 'Verify npm packaging of the project',
-  start: 'Start the project',
+  build: 'transpile source TS to JS',
+  'bundle-size': 'check the bundle size diff',
+  bundle: 'bundle source with webpack',
+  storybook: 'start storybook in dev mode',
+  'build-storybook': 'build production version of storybook',
+  'generate-api': 're-generate api.md (using api-extractor)',
+  lint: 'validate code with eslint',
+  'type-check': 'validate type correctness via TypeScript',
+  test: 'run jest tests ',
+  'test-integration': 'run integration tests (using @fluentui/scripts-projects-test)',
+  'test-ssr': 'run server-side rendering tests (using @fluentui/scripts-test-ssr)',
+  'test-vr': 'run visual regression tests (using storywright)',
+  'test-perf': 'run performance tests',
+  e2e: 'run e2e/component tests for the project',
+  'verify-packaging': 'verify packaging assets that will be shipped to npm',
+  // non default targets
+  format: 'format code with prettier',
+  'code-style': 'format code with prettier and run eslint',
+  clean: 'remove build artifacts',
+  start: 'start the project',
+  help: 'show project help',
 };
 
 const omitTargets = ['nx-release-publish', 'just'];
 
-/**
- * @type {string[]}
- */
-const allProjects = JSON.parse(
-  spawnSync('nx', ['show', 'projects', '--json'], { cwd: workspaceRoot }).stdout.toString(),
-);
-const extraArgs = process.argv.slice(2) ?? [];
-
-/**
- *
- * @param {string} input
- * @param {import('prompts').Choice[]} choices
- */
-const suggest = (input, choices) => Promise.resolve(choices.filter(i => i.title.includes(input)));
-
 async function main() {
-  const projectPrompt = await prompts({
-    type: 'autocomplete',
-    name: 'project',
+  /**
+   * @type {string[]}
+   */
+  const allProjects = JSON.parse(
+    spawnSync('nx', ['show', 'projects', '--json'], { cwd: workspaceRoot }).stdout.toString(),
+  );
+  const extraArgs = process.argv.slice(2) ?? [];
 
-    message: 'Which project to start (select or type partial name)?',
-    suggest,
-    choices: [...allProjects.map(p => ({ title: p }))],
+  const projectPrompt = new AutoComplete({
+    name: 'project',
+    message: 'Select project to start',
+    choices: allProjects,
+    limit: 10,
+    footer() {
+      return output.dim('(Scroll up and down to reveal more choices)');
+    },
   });
 
+  /** @type {string} */
+  const selectedProject = await projectPrompt.run();
+
   const projectTargets = JSON.parse(
-    spawnSync('nx', ['show', 'project', projectPrompt.project, '--json'], { cwd: workspaceRoot }).stdout.toString(),
+    spawnSync('nx', ['show', 'project', selectedProject, '--json'], { cwd: workspaceRoot }).stdout.toString(),
   );
 
   const availableTargets = /** @type {Array<keyof typeof  targetDescription>} */ (
     Object.keys(projectTargets.targets).filter(targetName => {
       return omitTargets.includes(targetName) ? false : true;
     })
-  );
+  ).concat('help');
 
-  const targetPrompt = await prompts({
-    type: 'autocomplete',
+  const longestTargetName = availableTargets.reduce((acc, targetName) => {
+    return targetName.length > acc ? targetName.length : acc;
+  }, 0);
+
+  const targetChoices = availableTargets.map(targetName => ({
+    name: formatTargetOutput(targetName, createTaskDescription(targetName, projectTargets.targets)),
+    value: targetName,
+  }));
+
+  const targetPrompt = new AutoComplete({
     name: 'target',
-
-    message: 'Which target to start (select or type partial name)?',
-    suggest,
-    choices: [
-      ...availableTargets.map(targetName => ({
-        title: targetName,
-        description: createTaskDescription(targetName, projectTargets.targets),
-      })),
-    ],
+    message: 'Select target to start',
+    choices: targetChoices,
+    limit: 5,
+    footer() {
+      return output.dim('(Scroll up and down to reveal more choices)');
+    },
   });
+
+  /** @type {string} */
+  const selectedTarget = await targetPrompt.run();
+
+  /**
+   *
+   * @param {string} targetName
+   * @param {string} description
+   */
+  function formatTargetOutput(targetName, description) {
+    const padding = ' '.repeat(longestTargetName - targetName.length);
+    return `${targetName}${padding} - ${description}`;
+  }
 
   /**
    *
@@ -80,19 +100,54 @@ async function main() {
    * @returns
    */
   function createTaskDescription(targetName, targetsMetadata) {
+    const description = targetDescription[targetName];
+    const nxTargetDefinition = targetsMetadata[targetName];
+
+    if (!nxTargetDefinition) {
+      return description;
+    }
+
     if (targetName === 'start') {
-      const startTarget = targetsMetadata[targetName];
-      const scriptContent = startTarget.metadata.scriptContent;
+      const scriptContent = nxTargetDefinition.metadata.scriptContent;
       if (scriptContent.includes('storybook')) {
         return `Start the project (Alias of "storybook" target)`;
       }
-      return targetDescription[targetName];
+      return description;
     }
 
-    return targetDescription[targetName];
+    if (targetName === 'e2e') {
+      const scriptContent = nxTargetDefinition.metadata.scriptContent;
+      const runnerType = getRunnerType(scriptContent);
+      return description + ` (using ${runnerType})`;
+    }
+
+    return description ?? nxTargetDefinition.metadata.scriptContent;
+
+    /**
+     * @param {string} scriptContent
+     * @returns {'cypress' | 'playwright'}
+     */
+    function getRunnerType(scriptContent) {
+      if (scriptContent.includes('cypress')) {
+        return 'cypress';
+      }
+      if (scriptContent.includes('playwright')) {
+        return 'playwright';
+      }
+      throw new Error('invalid runner type');
+    }
   }
 
-  const cmd = `${projectPrompt.project}:${targetPrompt.target}`;
+  if (selectedTarget === 'help') {
+    spawnSync('nx', ['show', 'project', selectedProject], {
+      shell: true,
+      stdio: 'inherit',
+    });
+
+    process.exit(0);
+  }
+
+  const cmd = `${selectedProject}:${selectedTarget}`;
 
   output.logSingleLine(`Running nx run ${cmd} ${extraArgs.join(' ')}`);
 
