df01bf81b8193b3b632b4c6db5841c5609f19576
Valentyna <vkozlova@microsoft.com>
2024-08-01 16:36:00 +0200

    ...

--- packages/react-components/react-color-picker-preview/stories/src/ColorPicker/index.stories.tsx
+++ packages/react-components/react-color-picker-preview/stories/src/ColorPicker/index.stories.tsx
@@ -4,6 +4,7 @@ import descriptionMd from './ColorPickerDescription.md';
 import bestPracticesMd from './ColorPickerBestPractices.md';
 
 export { Default } from './ColorPickerDefault.stories';
+export { ColorPickerGeneral } from './ColorPickerGeneral.stories';
 
 export default {
   title: 'Preview Components/ColorPicker',
