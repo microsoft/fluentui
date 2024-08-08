b55775e28ceba5e21f74a370ffce38c89d9db057
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-08-07 23:59:39 -0700

    update tab-panel tests to be async

--- packages/web-components/src/tab-panel/tab-panel.spec.ts
+++ packages/web-components/src/tab-panel/tab-panel.spec.ts
@@ -1,42 +1,29 @@
-import type { Locator, Page } from '@playwright/test';
 import { expect, test } from '@playwright/test';
 import { fixtureURL } from '../helpers.tests.js';
 
 test.describe('TabPanel', () => {
-  let page: Page;
-  let element: Locator;
-  let root: Locator;
-
-  test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-
-    element = page.locator('fluent-tab-panel');
-
-    root = page.locator('#storybook-root');
-
+  test.beforeEach(async ({ page }) => {
     await page.goto(fixtureURL('components-tabs--tabs-default'));
+
+    await page.waitForFunction(() => customElements.whenDefined('fluent-tab-panel'));
   });
 
-  test.afterAll(async () => {
-    await page.close();
-  });
+  test('should have a role of `tabpanel`', async ({ page }) => {
+    const element = page.locator('fluent-tab-panel');
 
-  test('should have a role of `tabpanel`', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-                <fluent-tab-panel></fluent-tab-panel>
-            `;
-    });
+    await page.setContent(/* html */ `
+        <fluent-tab-panel></fluent-tab-panel>
+    `);
 
     await expect(element).toHaveAttribute('role', 'tabpanel');
   });
 
-  test('should have a slot attribute of `tabpanel`', async () => {
-    await root.evaluate(node => {
-      node.innerHTML = /* html */ `
-                <fluent-tab-panel></fluent-tab-panel>
-            `;
-    });
+  test('should have a slot attribute of `tabpanel`', async ({ page }) => {
+    const element = page.locator('fluent-tab-panel');
+
+    await page.setContent(/* html */ `
+        <fluent-tab-panel></fluent-tab-panel>
+    `);
 
     await expect(element).toHaveAttribute('slot', 'tabpanel');
   });
