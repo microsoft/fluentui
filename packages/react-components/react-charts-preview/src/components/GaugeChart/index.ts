ff42a4faa71b2f6a131727e9fe175b36e2aa11d4
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2024-08-09 07:00:33 +0000

    Remove charts not planned in this releae

--- packages/react-components/react-charts-preview/src/components/GaugeChart/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './GaugeChart';
-export * from './GaugeChart.types';
--- packages/react-components/react-charts-preview/src/components/StackedBarChart/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './StackedBarChart';
-export * from './MultiStackedBarChart';
-export * from './StackedBarChart.types';
-export * from './MultiStackedBarChart.types';
-export * from '../../types/index';
