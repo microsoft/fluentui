870b6313ccdec67ed0e75631c51b353b6485cf7f
Emma Jiang <jiangemma@microsoft.com>
2024-08-16 09:24:25 -0700

    update TagPicker to use disabled on TagGroup instead of individual tags

--- packages/react-components/react-tag-picker/stories/src/TagPicker/TagPickerDisabled.stories.tsx
+++ packages/react-components/react-tag-picker/stories/src/TagPicker/TagPickerDisabled.stories.tsx
@@ -35,7 +35,6 @@ export const Disabled = () => {
           <TagPickerGroup>
             {selectedOptions.map(option => (
               <Tag
-                disabled
                 key={option}
                 shape="rounded"
                 media={<Avatar aria-hidden name={option} color="colorful" />}
