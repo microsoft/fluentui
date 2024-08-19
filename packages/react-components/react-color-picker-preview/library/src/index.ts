020a197e16437ad15fca5be1d732f56f9c9a61bd
Valentyna <vkozlova@microsoft.com>
2024-08-19 08:51:41 +0200

    feat(react-color-picker): generated components

--- packages/react-components/react-color-picker-preview/library/src/index.ts
+++ packages/react-components/react-color-picker-preview/library/src/index.ts
@@ -1,1 +1,18 @@
-export {};
+export type { ColorSliderProps, ColorSliderSlots, ColorSliderState } from './ColorSlider';
+export {
+  ColorSlider,
+  colorSliderClassNames,
+  renderColorSlider_unstable,
+  useColorSliderStyles_unstable,
+  useColorSlider_unstable,
+} from './ColorSlider';
+export type { ColorPickerProps, ColorPickerSlots, ColorPickerState } from './ColorPicker';
+export {
+  ColorPicker,
+  colorPickerClassNames,
+  renderColorPicker_unstable,
+  useColorPickerStyles_unstable,
+  useColorPicker_unstable,
+} from './ColorPicker';
+export type { ColorAreaProps, ColorAreaSlots, ColorAreaState } from './ColorArea';
+export { ColorArea, colorAreaClassNames, renderColorArea_unstable, useColorAreaStyles_unstable, useColorArea_unstable } from './ColorArea';
