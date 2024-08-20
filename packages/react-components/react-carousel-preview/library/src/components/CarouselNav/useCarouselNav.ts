4c447b480efd2cd0b572d8f8e74f3cfb28ce7d7a
Mitch-At-Work <mifraser@microsoft.com>
2024-08-20 11:42:19 -0700

    Remove tab index comment

--- packages/react-components/react-carousel-preview/library/src/components/CarouselNav/useCarouselNav.ts
+++ packages/react-components/react-carousel-preview/library/src/components/CarouselNav/useCarouselNav.ts
@@ -42,7 +42,6 @@ export const useCarouselNav_unstable = (props: CarouselNavProps, ref: React.Ref<
       getIntrinsicElementProps('div', {
         ref,
         role: 'tablist',
-        // tabIndex: 0,
         ...props,
         ...focusableGroupAttr,
         children: null,
