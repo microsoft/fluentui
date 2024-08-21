513956f7d967dd6baa60a12f1b967135c934ea54
Dmytro Kirpa <kirpadv@gmail.com>
2024-08-21 13:23:32 +0200

    trigger build

--- apps/public-docsite-v9/src/Utilities/Theme/createCSSRuleFromTheme/index.stories.tsx
+++ apps/public-docsite-v9/src/Utilities/Theme/createCSSRuleFromTheme/index.stories.tsx
@@ -6,7 +6,7 @@ import bestPracticesMd from './createCSSRuleFromThemeBestPractices.md';
 export { Default } from './createCSSRuleFromThemeDefault.stories';
 export { Switching } from './createCSSRuleFromThemeSwitching.stories';
 
-export default {
+const meta: Meta = {
   title: 'Utilities/Theme/createCSSRuleFromTheme',
   parameters: {
     docs: {
@@ -15,4 +15,6 @@ export default {
       },
     },
   },
-} as Meta;
+};
+
+export default meta;
