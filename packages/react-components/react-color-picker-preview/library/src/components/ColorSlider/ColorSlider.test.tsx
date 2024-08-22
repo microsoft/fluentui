aacbb429c6bf89dae83efcd03ce9cf237e6e13be
Valentyna <vkozlova@microsoft.com>
2024-08-22 05:10:39 -0700

    feat(react-color-picker): added ColorSlider (#32346)

--- packages/react-components/react-color-picker-preview/library/src/components/ColorSlider/ColorSlider.test.tsx
+++ packages/react-components/react-color-picker-preview/library/src/components/ColorSlider/ColorSlider.test.tsx
@@ -7,12 +7,31 @@ describe('ColorSlider', () => {
   isConformant({
     Component: ColorSlider,
     displayName: 'ColorSlider',
+    primarySlot: 'input',
   });
 
-  // TODO add more tests here, and create visual regression tests in /apps/vr-tests
-
   it('renders a default state', () => {
-    const result = render(<ColorSlider>Default ColorSlider</ColorSlider>);
-    expect(result.container).toMatchSnapshot();
+    const result = render(<ColorSlider />);
+    expect(result.container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="fui-ColorSlider"
+          style="--fui-Slider--direction: -90deg; --fui-Slider--progress: 0%; --fui-Slider__thumb--color: hsl(0, 100%, 50%);"
+        >
+          <input
+            class="fui-ColorSlider__input"
+            id="slider-9"
+            type="range"
+            value="0"
+          />
+          <div
+            class="fui-ColorSlider__rail"
+          />
+          <div
+            class="fui-ColorSlider__thumb"
+          />
+        </div>
+      </div>
+    `);
   });
 });
