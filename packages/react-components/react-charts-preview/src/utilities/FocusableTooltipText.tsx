a7a134f3468864c248d042d5d9566c82ae955489
srmukher <120183316+srmukher@users.noreply.github.com>
2024-08-23 10:59:18 +0000

    Adding comments

--- packages/react-components/react-charts-preview/src/utilities/FocusableTooltipText.tsx
+++ packages/react-components/react-charts-preview/src/utilities/FocusableTooltipText.tsx
@@ -13,9 +13,9 @@ interface IFocusableTooltipTextProps {
   accessibilityData?: IAccessibilityProps;
 }
 
-export const FocusableTooltipText:  React.FunctionComponent<IFocusableTooltipTextProps> = React.forwardRef<
-HTMLDivElement,
-IFocusableTooltipTextProps
+export const FocusableTooltipText: React.FunctionComponent<IFocusableTooltipTextProps> = React.forwardRef<
+  HTMLDivElement,
+  IFocusableTooltipTextProps
 >((props, forwardedRef) => {
   const [textOverflow, setTextOverflow] = useState(false);
   const tooltipChild = useRef<HTMLSpanElement>(null);
@@ -42,12 +42,13 @@ IFocusableTooltipTextProps
   }, [checkTextOverflow]);
 
   useEffect(() => {
+    // setup part executed only when the component mounts/updates
     const overflowElement = getTargetElement();
     if (window.ResizeObserver && overflowElement) {
       resizeObserver.current = new window.ResizeObserver(async.debounce(checkTextOverflow, 500));
       resizeObserver.current.observe(overflowElement);
     }
-
+    // cleanup part executed only when the component unmounts
     return () => {
       resizeObserver.current?.disconnect();
       async.dispose();
@@ -55,7 +56,7 @@ IFocusableTooltipTextProps
   }, [async, checkTextOverflow, getTargetElement]);
 
   return (
-    <div className={props.className}>
+    <div className={props.className} tabIndex={0}>
       <Tooltip content={props.content} relationship="label">
         <span {...getAccessibleDataObject(props.accessibilityData)} ref={tooltipChild} data-is-focusable={textOverflow}>
           {props.content}
