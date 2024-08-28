b733c238d23784e1f65ab35f18f57d8d56a66ec8
Martin Hochel <martinhochel@microsoft.com>
2024-08-28 18:33:08 +0200

    feat(workspace-plugin): implement type-check executor (#32370)

--- tools/workspace-plugin/src/executors/type-check/schema.d.ts
+++ tools/workspace-plugin/src/executors/type-check/schema.d.ts
@@ -1,1 +1,6 @@
-export interface TypeCheckExecutorSchema {} // eslint-disable-line
+export interface TypeCheckExecutorSchema {
+  /**
+   * Exclude TsConfigs from type checking (e2e,spec). By default all TsConfigs specified in project root tsconfig#references are checked.
+   */
+  excludeProject?: { spec: boolean; e2e: boolean };
+}
