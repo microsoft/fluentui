66dcb2f375fd3bc65a0f231a875b5543d53d2f55
Dmytro Kirpa <kirpadv@gmail.com>
2024-08-30 14:35:51 +0200

    feat(react-slider): adjust slider thumb position

--- packages/react-components/react-slider/library/src/components/Slider/useSliderState.tsx
+++ packages/react-components/react-slider/library/src/components/Slider/useSliderState.tsx
@@ -4,10 +4,38 @@ import { useFluent_unstable as useFluent } from '@fluentui/react-shared-contexts
 import { sliderCSSVars } from './useSliderStyles.styles';
 import type { SliderState, SliderProps } from './Slider.types';
 
-const { sliderStepsPercentVar, sliderProgressVar, sliderDirectionVar } = sliderCSSVars;
+const {
+  sliderStepsPercentVar,
+  sliderProgressVar,
+  sliderProgressValueVar,
+  sliderDirectionVar,
+  sliderStepsColorVar,
+  sliderStepsGradientVar,
+} = sliderCSSVars;
 
 const getPercent = (value: number, min: number, max: number) => {
   return max === min ? 0 : ((value - min) / (max - min)) * 100;
+};
+
+/**
+ * Generates a linear gradient CSS value representing the steps of a slider.
+ *
+ * @param stepSize - The step size for each gradient stop.
+ * @returns The linear gradient CSS value.
+ */
+const getStepsGradient = (stepSize: number) => {
+  const steps = [];
+
+  for (let step = stepSize; step < 100; step += stepSize) {
+    steps.push(
+      `#0000 calc(${step}% - 1px)`,
+      `var(${sliderStepsColorVar}) calc(${step}% - 1px)`,
+      `var(${sliderStepsColorVar}) ${step}%`,
+      `#0000 ${step}%`,
+    );
+  }
+
+  return `linear-gradient(var(${sliderDirectionVar}), #0000 0, ${steps.join(', ')}, #0000 100%)`;
 };
 
 export const useSliderState_unstable = (state: SliderState, props: SliderProps) => {
@@ -37,10 +65,14 @@ export const useSliderState_unstable = (state: SliderState, props: SliderProps) 
     }
   });
 
+  const stepsPercent = step && step > 0 ? (step * 100) / (max - min) : 0;
+
   const rootVariables = {
     [sliderDirectionVar]: state.vertical ? '0deg' : dir === 'ltr' ? '90deg' : '270deg',
-    [sliderStepsPercentVar]: step && step > 0 ? `${(step * 100) / (max - min)}%` : '',
+    [sliderStepsPercentVar]: stepsPercent ? `${stepsPercent}%` : '',
     [sliderProgressVar]: `${valuePercent}%`,
+    [sliderProgressValueVar]: `${valuePercent / 100}`,
+    [sliderStepsGradientVar]: getStepsGradient(stepsPercent),
   };
 
   // Root props
