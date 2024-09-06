8fa03593565acc81320f1252458d3f0e566f9561
Martin Hochel <martinhochel@microsoft.com>
2024-09-06 14:48:32 +0200

    test: update jest-config in order to resolve faulty @swc/types package

--- tools/workspace-plugin/jest.config.ts
+++ tools/workspace-plugin/jest.config.ts
@@ -4,14 +4,14 @@ export default {
   displayName: 'workspace-plugin',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', {}],
+    '^.+\\.[tj]s$': ['@swc/jest', { exclude: '/node_modules/' }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  /**
+   * NOTE: because @swc/types ships index.ts and has not properly set up the package.json "types" field, if we set 'ts' first, it will try to use the index.ts file and fail.
+   */
+  moduleFileExtensions: ['js', 'ts', 'html'],
   transformIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
   watchPathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
-  moduleNameMapper: {
-    '@swc/types': require.resolve('@swc/types'),
-  },
   coverageDirectory: '../../coverage/tools/workspace-plugin',
   setupFiles: ['<rootDir>/jest-setup.js'],
 } as import('@jest/types').Config.InitialOptions;
