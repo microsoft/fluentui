75d9c498c3bdb9e4997326efb8f44d0b6492ac3a
Marcos Moura <marcosvmmoura@gmail.com>
2024-09-11 13:03:38 +0200

    fix: use storybook styled implementation

--- packages/react-components/react-storybook-addon/src/components/DirectionSwitch.tsx
+++ packages/react-components/react-storybook-addon/src/components/DirectionSwitch.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { IconButton } from '@storybook/components';
+import { styled } from '@storybook/theming';
 
 import { DIR_ID } from '../constants';
 import { useGlobals } from '../hooks';
 
-const monospaceStyle = {
+const Monospace = styled.span({
   fontFamily: "'Cascadia Code', Menlo, 'Courier New', Courier, monospace",
   letterSpacing: '-0.05em',
-};
+});
 
 export const DirectionSwitch = () => {
   const [globals, updateGlobals] = useGlobals();
@@ -26,7 +27,7 @@ export const DirectionSwitch = () => {
   return (
     <IconButton key={DIR_ID} title="Change Direction" onClick={toggleDirection}>
       <div>
-        Direction: <span style={monospaceStyle}>{direction.toUpperCase()}</span>
+        Direction: <Monospace>{direction.toUpperCase()}</Monospace>
       </div>
     </IconButton>
   );
