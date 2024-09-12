a33aebddf12d9d36bfcab405377367718d198187
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-09-12 09:14:50 -0700

    add isAccordionItem function and baseName export

--- packages/web-components/src/accordion-item/accordion-item.definition.ts
+++ packages/web-components/src/accordion-item/accordion-item.definition.ts
@@ -1,16 +1,36 @@
-import { FluentDesignSystem } from '../fluent-design-system.js';
-import { AccordionItem } from './accordion-item.js';
+import { generateElementName } from '../fluent-design-system.js';
+import { AccordionItem, type BaseAccordionItem } from './accordion-item.js';
 import { styles } from './accordion-item.styles.js';
 import { template } from './accordion-item.template.js';
 
 /**
+ * The base name of the {@link BaseAccordionItem | AccordionItem} element.
+ *
+ * @public
+ */
+export const baseName = 'accordion-item';
+
+/**
+ * The {@link BaseAccordionItem | AccordionItem} custom element definition.
  *
  * @public
  * @remarks
- * HTML Element: \<fluent-accordion-item\>
+ * HTML Element: `<fluent-accordion-item>`
  */
 export const definition = AccordionItem.compose({
-  name: `${FluentDesignSystem.prefix}-accordion-item`,
+  name: generateElementName(baseName),
   template,
   styles,
 });
+
+/**
+ * Checks if an element is an {@link BaseAccordionItem | AccordionItem}.
+ *
+ * @param element - The element to check
+ * @returns - The element is an AccordionItem
+ *
+ * @public
+ */
+export function isAccordionItem(element: Element): element is BaseAccordionItem {
+  return element.tagName.toLowerCase().endsWith(baseName);
+}
