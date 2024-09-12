fcb9f8ce6f55696f5c5d954f02e32ac075e805bf
srmukher <120183316+srmukher@users.noreply.github.com>
2024-09-12 12:33:33 +0000

    Adding boilerplate code for line chart styles

---
+++ packages/react-components/react-charts-preview/library/src/components/LineChart/useLineChartStyles.styles.ts
@@ -0,0 +1,44 @@
+import { makeStyles, mergeClasses, shorthands } from '@griffel/react';
+import { tokens } from '@fluentui/react-theme';
+import { ILineChartProps, ILineChartStyles } from './LineChart.types';
+import { SlotClassNames } from '@fluentui/react-utilities/src/index';
+
+/**
+ * @internal
+ */
+export const linechartClassNames: SlotClassNames<ILineChartStyles> = {
+  tooltip: 'fui-line__tooltip',
+  root: 'fui-line__root',
+  xAxis: 'fui-line__xAxis',
+  yAxis: 'fui-line__yAxis',
+  legendContainer: 'fui-line__legendContainer',
+  hover: 'fui-line__hover',
+  calloutContentRoot: 'fui-line__calloutContentRoot',
+  calloutContentX: 'fui-line__calloutContentX',
+  calloutContentY: 'fui-line__calloutContentY',
+  descriptionMessage: 'fui-line__descriptionMessage',
+  calloutDateTimeContainer: 'fui-line__calloutDateTimeContainer',
+  calloutInfoContainer: 'fui-line__calloutInfoContainer',
+  calloutBlockContainer: 'fui-line__calloutBlockContainer',
+  calloutlegendText: 'fui-line__calloutLegendText',
+  axisTitle: 'fui-line__axisTitle',
+  chartTitle: 'fui-line__chartTitle',
+  opacityChangeOnHover: 'fui-line__opacityChangeOnHover',
+  shapeStyles: 'fui-line__shapeStyles',
+  chartWrapper: 'fui-line__chartWrapper',
+  calloutBlockContainertoDrawShapefalse: '', // Not used so marked as empty. ToDo - How to configure inheritence properly
+  calloutBlockContainertoDrawShapetrue: '', // Not used so marked as empty. ToDo - How to configure inheritence properly
+};
+
+/**
+ * Base Styles
+ */
+const useStyles = makeStyles({});
+
+/**
+ * Apply styling to the Carousel slots based on the state
+ */
+export const useLineChartStyles_unstable = (props: ILineChartProps): ILineChartStyles => {
+  const baseStyles = useStyles();
+  return {};
+};
