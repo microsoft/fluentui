e2c60f6a60e93e51a0940662f035729fe19c0ba1
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-09-12 09:18:49 -0700

    add isAccordion function and baseName export

--- packages/web-components/src/accordion/accordion.definition.ts
+++ packages/web-components/src/accordion/accordion.definition.ts
@@ -1,15 +1,36 @@
-import { FluentDesignSystem } from '../fluent-design-system.js';
+import { generateElementName } from '../fluent-design-system.js';
 import { Accordion } from './accordion.js';
 import { styles } from './accordion.styles.js';
 import { template } from './accordion.template.js';
 
 /**
+ * The base name of the {@link Accordion}.
+ *
+ * @public
+ */
+export const baseName = 'accordion';
+
+/**
+ * The {@link Accordion} custom element definition.
+ *
  * @public
  * @remarks
- * HTML Element: \<fluent-accordion\>
+ * HTML Element: `<fluent-accordion>`
  */
 export const definition = Accordion.compose({
-  name: `${FluentDesignSystem.prefix}-accordion`,
+  name: generateElementName(baseName),
   template,
   styles,
 });
+
+/**
+ * Checks if an element is an {@link Accordion}.
+ *
+ * @param element - The element to check
+ * @returns - The element is an Accordion
+ *
+ * @public
+ */
+export function isAccordion(element: Element): element is Accordion {
+  return element.tagName.toLowerCase().endsWith(baseName);
+}
