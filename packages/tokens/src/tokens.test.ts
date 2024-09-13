b24ba5b9c34e24370937399f900b6798a8671461
Marcos Moura <marcosvmmoura@gmail.com>
2024-09-13 10:02:31 +0200

    fix: use regex to simplify logic

--- packages/tokens/src/tokens.test.ts
+++ packages/tokens/src/tokens.test.ts
@@ -4,18 +4,10 @@ import { tokens } from './tokens';
 describe('tokens', () => {
   it('CSS variables match expected format', () => {
     (Object.keys(tokens) as (keyof Theme)[]).forEach(token => {
-      const value = tokens[token];
-      const hasFallback = value.includes('var(--') && value.includes(',');
+      const tokenValue = tokens[token];
+      const tokenRegex = new RegExp(`var\\(--${token}(, .+)?\\)`);
 
-      if (hasFallback) {
-        const [, fallback] = value.replace(/ |\)/g, '').split(',');
-
-        // In case of a fallback, we expect the value to be in the format `var(--token, fallback)`.
-        expect(value).toBe(`var(--${token}, ${fallback})`);
-      } else {
-        // In case of no fallback, we expect the value to be in the format `var(--token)`.
-        expect(value).toBe(`var(--${token})`);
-      }
+      expect(tokenValue).toMatch(tokenRegex);
     });
   });
 });
