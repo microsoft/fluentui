247ab94ad2f1fa6d70f9494c94e2633566ff8d34
Mitch-At-Work <mifraser@microsoft.com>
2024-09-16 16:17:15 -0700

    Update test

--- packages/react-components/react-carousel-preview/library/src/components/CarouselAnnouncer/CarouselAnnouncer.test.tsx
+++ packages/react-components/react-carousel-preview/library/src/components/CarouselAnnouncer/CarouselAnnouncer.test.tsx
@@ -3,23 +3,21 @@ import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { CarouselAnnouncer } from './CarouselAnnouncer';
 
-const renderFunc = (index, totalSlides, groupList) => `Slide ${index} of ${totalSlides}`;
+const renderFunc = (index: number, totalSlides: number, groupList: number[][]) => `Slide ${index} of ${totalSlides}`;
 
 describe('CarouselAnnouncer', () => {
   isConformant({
     Component: CarouselAnnouncer,
     displayName: 'CarouselAnnouncer',
     requiredProps: {
-      children: (index, totalSlides, groupList) => `Slide ${index} of ${totalSlides}`,
+      children: renderFunc,
     },
   });
 
   // TODO add more tests here, and create visual regression tests in /apps/vr-tests
 
   it('renders a default state', () => {
-    const result = render(
-      <CarouselAnnouncer>{(index, totalSlides, groupList) => `Slide ${index} of ${totalSlides}`}</CarouselAnnouncer>,
-    );
+    const result = render(<CarouselAnnouncer>{renderFunc}</CarouselAnnouncer>);
     expect(result.container).toMatchSnapshot();
   });
 });
