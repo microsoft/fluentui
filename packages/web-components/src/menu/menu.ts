8238075fec80afd584ea8210609fdb5a8a006bd3
viktorgenaev <viktorgenaev@microsoft.com>
2024-09-18 11:57:15 +0200

    chore(web-components): remove redundant ts suppressions

--- packages/web-components/src/menu/menu.ts
+++ packages/web-components/src/menu/menu.ts
@@ -161,7 +161,6 @@ export class Menu extends FASTElement {
    * @public
    */
   public toggleMenu = () => {
-    // @ts-expect-error - Baseline 2024
     this._menuList?.togglePopover(!this._open);
   };
 
@@ -178,7 +177,6 @@ export class Menu extends FASTElement {
     ) {
       return;
     }
-    // @ts-expect-error - Baseline 2024
     this._menuList?.togglePopover(false);
 
     if (this.closeOnScroll) {
@@ -191,7 +189,6 @@ export class Menu extends FASTElement {
    * @public
    */
   public openMenu = (e?: Event) => {
-    // @ts-expect-error - Baseline 2024
     this._menuList?.togglePopover(true);
 
     if (e && this.openOnContext) {
