3aa17828e40e22aad82329df4e4aca8839fd1ce0
Mitch-At-Work <mifraser@microsoft.com>
2024-09-23 12:57:34 -0700

    tidy up

--- packages/react-components/react-carousel-preview/library/src/components/CarouselAnnouncer/useCarouselAnnouncer.ts
+++ packages/react-components/react-carousel-preview/library/src/components/CarouselAnnouncer/useCarouselAnnouncer.ts
@@ -17,6 +17,7 @@ export const useCarouselAnnouncer_unstable = (
   props: CarouselAnnouncerProps,
   ref: React.Ref<HTMLDivElement>,
 ): CarouselAnnouncerState => {
+  const { children: renderAnnouncerText } = props;
   const subscribeForValues = useCarouselContext(ctx => ctx.subscribeForValues);
   const textRef: React.Ref<HTMLElement> = React.useRef(null);
 
@@ -24,17 +25,20 @@ export const useCarouselAnnouncer_unstable = (
   const navLengthRef = React.useRef<number>(0);
   const navGroupRef = React.useRef<number[][]>([]);
 
-  const updateInnerText = useCallback((index: number, totalSlides: number, groupIndexList: number[][]) => {
-    if (totalSlides <= 0) {
-      // Ignore announcements until slides discovered
-      return;
-    }
-    const announcementText = props.children(index, totalSlides, groupIndexList);
+  const updateInnerText = useCallback(
+    (index: number, totalSlides: number, groupIndexList: number[][]) => {
+      if (totalSlides <= 0) {
+        // Ignore announcements until slides discovered
+        return;
+      }
+      const announcementText = renderAnnouncerText(index, totalSlides, groupIndexList);
 
-    if (textRef.current) {
-      textRef.current.innerText = announcementText;
-    }
-  }, []);
+      if (textRef.current) {
+        textRef.current.innerText = announcementText;
+      }
+    },
+    [renderAnnouncerText],
+  );
 
   useIsomorphicLayoutEffect(() => {
     return subscribeForValues(data => {
@@ -49,14 +53,10 @@ export const useCarouselAnnouncer_unstable = (
   }, [currentIndex, updateInnerText]);
 
   return {
-    renderAnnouncerText: props.children,
-    // TODO add appropriate props/defaults
+    renderAnnouncerText,
     components: {
-      // TODO add each slot's element type or component
       root: 'div',
     },
-    // TODO add appropriate slots, for example:
-    // mySlot: resolveShorthand(props.mySlot),
     root: slot.always(
       getIntrinsicElementProps('div', {
         ref: useMergedRefs(ref, textRef),
