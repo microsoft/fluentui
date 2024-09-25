3a44a20a664e203298598d638b2a068657ac8ae9
Robert Penner <robertpenner@microsoft.com>
2024-09-25 20:28:38 +0000

    chore(Toast): re-enable ToastContainerMotion unit test

--- packages/react-components/react-toast/library/src/components/ToastContainer/ToastContainer.test.tsx
+++ packages/react-components/react-toast/library/src/components/ToastContainer/ToastContainer.test.tsx
@@ -5,7 +5,7 @@ import { isConformant } from '../../testing/isConformant';
 import { ToastContainerProps } from './ToastContainer.types';
 import { toastContainerClassNames } from './useToastContainerStyles.styles';
 import { resetIdsForTests } from '@fluentui/react-utilities';
-// import { type PresenceComponentProps } from '@fluentui/react-motion';
+import { type PresenceComponentProps } from '@fluentui/react-motion';
 
 const defaultToastContainerProps: ToastContainerProps = {
   announce: () => null,
@@ -35,7 +35,6 @@ const pausedTimerSelector = '[data-timer-status="paused"]';
 
 const FAKE_MOTION_DURATION = 500;
 
-/*
 jest.mock('./ToastContainerMotion', () => ({
   // eslint-disable-next-line @typescript-eslint/naming-convention
   ToastContainerMotion: (props: PresenceComponentProps) => {
@@ -54,7 +53,6 @@ jest.mock('./ToastContainerMotion', () => ({
     return <>{children}</>;
   },
 }));
-*/
 
 describe('ToastContainer', () => {
   beforeEach(() => {
@@ -232,7 +230,7 @@ describe('ToastContainer', () => {
     expect(container.querySelector(runningTimerSelector)).not.toBeNull();
   });
 
-  xit('should render different timer on update', () => {
+  it('should render different timer on update', () => {
     const toastProps: ToastContainerProps = { ...defaultToastContainerProps, timeout: 1 };
     const { container, rerender } = render(<ToastContainer {...toastProps}>ToastContainer</ToastContainer>);
 
