e26f9e3c940ca60563770416fc79ad103751d3fa
Mason Tejera <matejera@microsoft.com>
2024-09-25 14:09:41 -0700

    update conformance tests

--- packages/react-components/react-nav-preview/library/src/components/SplitNavItem/SplitNavItem.test.tsx
+++ packages/react-components/react-nav-preview/library/src/components/SplitNavItem/SplitNavItem.test.tsx
@@ -2,10 +2,31 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { isConformant } from '../../testing/isConformant';
 import { SplitNavItem } from './SplitNavItem';
+import { splitNavItemClassNames } from './useSplitNavItemStyles.styles';
 
 describe('SplitNavItem', () => {
   isConformant({
     Component: SplitNavItem,
     displayName: 'SplitNavItem',
+    testOptions: {
+      'has-static-classnames': [
+        {
+          props: {
+            icon: 'Test Icon',
+            primaryNavItem: 'Some Content',
+            secondaryActionButton: 'Some Content',
+            secondaryToggleButton: 'Some Content',
+            menuButton: 'Some Content',
+          },
+          expectedClassNames: {
+            root: splitNavItemClassNames.root,
+            primaryNavItem: splitNavItemClassNames.primaryNavItem,
+            secondaryActionButton: splitNavItemClassNames.secondaryActionButton,
+            secondaryToggleButton: splitNavItemClassNames.secondaryToggleButton,
+            menuButton: splitNavItemClassNames.menuButton,
+          },
+        },
+      ],
+    },
   });
 });
