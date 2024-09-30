71faacdef6a857e749d4b39027d0df5f7d4387dc
Carlos Perez <caperez@microsoft.com>
2024-09-30 12:29:04 -0700

    post-refactoring cleanup of an unused type

--- packages/react-file-type-icons/src/FileIconType.test.ts
+++ packages/react-file-type-icons/src/FileIconType.test.ts
@@ -1,9 +1,8 @@
 import { FileIconType } from './FileIconType';
-import type { FileIconTypeInput } from './FileIconType';
 
 let allFileTypeIconValues: FileIconType | undefined;
 
-function validateFileIconTypeValues(allowedFileTypeIconValues: FileIconTypeInput | undefined): void {
+function validateFileIconTypeValues(allowedFileTypeIconValues: FileIconType | undefined): void {
   // The purpose of this function is to verify that the below call compiles,
   // which may only occur if every enum value matches its key.
 }
