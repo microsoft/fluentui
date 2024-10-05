e39b52e93bd54609afba2781cd76c1190e78b549
Patrick Wang <yuewang1@microsoft.com>
2024-10-05 15:57:22 +0800

    remove header annotation

--- packages/web-components/src/tree-item/tree-item.style.ts
+++ packages/web-components/src/tree-item/tree-item.style.ts
@@ -1,7 +1,3 @@
-// Copyright (C) Microsoft Corporation. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import { css } from '@microsoft/fast-element';
 import {
   borderRadiusMedium,
