cc61203b2d51c4853826d3b46c926ea25be723dd
Robert Penner <robertpenner@microsoft.com>
2024-10-07 21:56:07 +0000

    feat(Collapse): add `orientation` param & horizontal implementation

--- packages/react-components/react-motion-components-preview/stories/src/Collapse/index.stories.ts
+++ packages/react-components/react-motion-components-preview/stories/src/Collapse/index.stories.ts
@@ -2,6 +2,7 @@ import { Collapse } from '@fluentui/react-motion-components-preview';
 import CollapseDescription from './CollapseDescription.md';
 
 export { Default } from './CollapseDefault.stories';
+export { Horizontal } from './CollapseHorizontal.stories';
 export { Snappy } from './CollapseSnappy.stories';
 export { Exaggerated } from './CollapseExaggerated.stories';
 export { Customization } from './CollapseCustomization.stories';
