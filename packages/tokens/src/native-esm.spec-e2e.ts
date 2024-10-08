1f57b39096c1f41017be997be1b205f1b5288b56
Martin Hochel <martinhochel@microsoft.com>
2024-10-08 14:38:37 +0200

    e2e(tokens): add playwright in order to e2e test native ESM support in the browser

---
+++ packages/tokens/src/native-esm.spec-e2e.ts
@@ -0,0 +1,16 @@
+import { expect } from '@playwright/test';
+import { test } from '@playwright/test';
+
+import { webLightTheme } from './index';
+
+test.describe('Native ESM', () => {
+  test('should render UI divs including all webLightTheme colors', async ({ page }) => {
+    await page.goto('test-esm.html');
+
+    await expect(page).toHaveTitle('native ESM for @fluentui/tokens');
+    await expect(page.locator('.root')).not.toBeEmpty();
+    await expect(page.locator('.color-ramp')).toHaveCount(
+      Object.keys(webLightTheme).filter(value => value.startsWith('color')).length,
+    );
+  });
+});
