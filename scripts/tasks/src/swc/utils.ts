8fb0859a47e77cb76d6ec558b304e7a3127dc7b7
Martin Hochel <martinhochel@microsoft.com>
2024-10-09 09:32:47 +0200

    docs: add TODO and notes to utils

--- scripts/tasks/src/swc/utils.ts
+++ scripts/tasks/src/swc/utils.ts
@@ -1,9 +1,15 @@
 import { type Options } from './types';
 
 const importPaths = {
+  // TODO - cover dynamic imports eg `import("file.js")`
   es6: /from\s+["']([^"']+)["']/g,
   commonjs: /require\(["']([^"']+)["']\)/g,
 };
+
+/**
+ *
+ * NOTE: this is not used ATM as swc implemented changes we needed. keeping for future reference if we might need to do some extra transforms on our side
+ */
 export function addJsExtensionToImports(code: string, type: Options['module']['type'] = 'es6') {
   if (!(type === 'es6' || type === 'commonjs')) {
     return code;
