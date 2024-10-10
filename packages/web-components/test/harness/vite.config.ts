2152288aee794500f3386df2d4075b059d953cca
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-10-10 10:27:05 -0700

    add vite test harness

---
+++ packages/web-components/test/harness/vite.config.ts
@@ -0,0 +1,22 @@
+import type { UserConfig } from 'vite';
+
+export default {
+  clearScreen: false,
+  publicDir: '../../public',
+  esbuild: {
+    target: 'ES2019',
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    debug: true,
+  },
+  build: {
+    outDir: './dist',
+    minify: false,
+  },
+  preview: {
+    port: 4173,
+    open: true,
+  },
+} as UserConfig;
