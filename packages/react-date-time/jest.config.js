08c7ed46ba819d85a77a7408c7f8a03442e2ffc4
Martin Hochel <martinhochel@microsoft.com>
2024-10-15 13:31:32 +0200

    test(react-date-time): fix invalid jest config exposed by @nx/jest plugin

--- packages/react-date-time/jest.config.js
+++ packages/react-date-time/jest.config.js
@@ -1,7 +1,6 @@
 const { createV8Config: createConfig } = require('@fluentui/scripts-jest');
 
 const config = createConfig({
-  setupFiles: ['./config/tests.js'],
   snapshotSerializers: ['@fluentui/jest-serializer-merge-styles'],
 });
 
