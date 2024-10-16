ed80ac6a0457a397d2a87b976d9f6ddb077c2fcd
srmukher <120183316+srmukher@users.noreply.github.com>
2024-10-16 18:09:00 +0530

    Fixing styles for custom callout stories (#33054)

--- packages/react-components/react-charts-preview/stories/src/HorizontalBarChart/HorizontalBarChartCustomCallout.stories.tsx
+++ packages/react-components/react-charts-preview/stories/src/HorizontalBarChart/HorizontalBarChartCustomCallout.stories.tsx
@@ -8,7 +8,7 @@ import {
   IPopoverComponentProps,
 } from '@fluentui/react-charts-preview';
 // import * as d3 from 'd3-format';
-import { Switch } from '@fluentui/react-components';
+import { Switch, tokens } from '@fluentui/react-components';
 
 export const HorizontalBarCustomCallout = () => {
   const hideRatio: boolean[] = [true, false];
@@ -133,10 +133,34 @@ export const HorizontalBarCustomCallout = () => {
     const xValue = props ? props.xAxisCalloutData! : 'X Value';
     const legend = props ? props.legend : 'Legend';
     return useCustomPopover ? (
-      <div style={{ border: '1.5px dotted black' }}>
-        <div style={{ color: getColorFromToken(DataVizPalette.color5), border: '1px black' }}>{xValue}</div>
-        <div style={{ color: getColorFromToken(DataVizPalette.color6), border: '1px black' }}>{legend}</div>
-        <div style={{ color: getColorFromToken(DataVizPalette.color7) }}>{yValue}</div>
+      <div style={{ border: `1.5px dotted ${getColorFromToken(DataVizPalette.color10)}`, padding: '10px' }}>
+        <div
+          style={{
+            color: getColorFromToken(DataVizPalette.warning),
+            fontSize: tokens.fontSizeBase400,
+            fontWeight: tokens.fontWeightBold,
+          }}
+        >
+          {xValue}
+        </div>
+        <div
+          style={{
+            color: getColorFromToken(DataVizPalette.color3),
+            fontSize: tokens.fontSizeBase400,
+            fontWeight: tokens.fontWeightBold,
+          }}
+        >
+          {legend}
+        </div>
+        <div
+          style={{
+            color: getColorFromToken(DataVizPalette.color2),
+            fontSize: tokens.fontSizeBase400,
+            fontWeight: tokens.fontWeightBold,
+          }}
+        >
+          {yValue}
+        </div>
       </div>
     ) : undefined;
   };
