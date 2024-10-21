5503364e320a75ab266df26018bf82d352083b6e
Martin Hochel <martinhochel@microsoft.com>
2024-10-21 18:31:37 +0200

    fix(scripts-babel): build griffel path aliases based on caller origin to properly transpile griffel styles

--- scripts/babel/src/preset-v9.spec.ts
+++ scripts/babel/src/preset-v9.spec.ts
@@ -1,19 +1,45 @@
+import { workspaceRoot } from '@nx/devkit';
 import preset from './preset-v9';
 
+function getAliasDeclaration(presetConfig: ReturnType<typeof preset>) {
+  const griffelPreset = presetConfig.presets.find(presetItem => presetItem[0] === '@griffel')!;
+  const griffelPresetConfig = griffelPreset[1];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const alias = (griffelPresetConfig as any).babelOptions.plugins[0][1].alias;
+  const aliasesEntries: Array<[string, string]> = Object.entries(alias);
+
+  /**
+   * we pick 5th entry from the big list of path aliases entries. there is nothing special about it and we can pick any existing entry index.
+   * I chose to not pick first nor last. In general this is an integration test that checks if we would introduce some violation within tsconfig.base.json path aliases config
+   */
+  const aliasDeclaration = aliasesEntries[5];
+  const [aliasKey, aliasPath] = aliasDeclaration;
+
+  return {
+    griffelPresetConfig,
+    aliasKey,
+    aliasPath,
+  };
+}
+
 describe(`babel preset v9`, () => {
-  const babelMockedApi = {
-    assertVersion: jest.fn(),
-    caller: jest.fn(),
-    env: jest.fn(),
-    cache: {},
-    version: '0',
-  } as unknown as import('@babel/core').ConfigAPI;
-  it(`should generate module-resolve alias mappings for @griffel preset`, () => {
+  it(`should generate module-resolve alias mappings for @griffel preset when calling from just-scripts`, () => {
+    const babelMockedApi = {
+      assertVersion: jest.fn(),
+      caller: jest.fn(caller => {
+        return caller({ name: 'just-scripts' });
+      }),
+      env: jest.fn(),
+      cache: {},
+      version: '0',
+    } as unknown as import('@babel/core').ConfigAPI;
+
     const presetConfig = preset(babelMockedApi, { tsBaseConfigPath: './tsconfig.base.json' });
 
-    const griffelPreset = presetConfig.presets.find(presetItem => presetItem[0] === '@griffel')!;
-    const griffelPresetConfig = griffelPreset[1];
+    const { aliasKey, aliasPath, griffelPresetConfig } = getAliasDeclaration(presetConfig);
 
+    expect(babelMockedApi.caller).toHaveBeenCalled();
     expect(griffelPresetConfig).toEqual({
       babelOptions: {
         plugins: [
@@ -32,18 +58,28 @@ describe(`babel preset v9`, () => {
       ],
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const alias = (griffelPresetConfig as any).babelOptions.plugins[0][1].alias;
-    const aliasesEntries = Object.entries(alias);
-
-    /**
-     * we pick 5th entry from the big list of path aliases entries. there is nothing special about it and we can pick any existing entry index.
-     * I chose to not pick first nor last. In general this is an integration test that checks if we would introduce some violation within tsconfig.base.json path aliases config
-     */
-    const aliasDeclaration = aliasesEntries[5];
-    const [aliasKey, aliasPath] = aliasDeclaration;
-
     expect(aliasKey).toEqual(expect.stringContaining('^@fluentui/'));
     expect(aliasPath).toEqual(expect.stringMatching(/[../]+[a-z-/]+\/lib\/index\.js/));
+  });
+
+  it(`should generate module-resolve alias mappings for @griffel preset when calling from nx executor from workspaceRoot`, () => {
+    const babelMockedApi = {
+      assertVersion: jest.fn(),
+      caller: jest.fn(caller => {
+        return caller({ name: '@fluentui/workspace-plugin:build' });
+      }),
+      env: jest.fn(),
+      cache: {},
+      version: '0',
+    } as unknown as import('@babel/core').ConfigAPI;
+
+    const presetConfig = preset(babelMockedApi, { tsBaseConfigPath: './tsconfig.base.json' });
+
+    const { aliasKey, aliasPath } = getAliasDeclaration(presetConfig);
+
+    expect(aliasKey).toEqual(expect.stringContaining('^@fluentui/'));
+
+    expect(aliasPath.startsWith(workspaceRoot));
+    expect(aliasPath).toEqual(expect.stringMatching(/[a-z-/]+\/lib\/index\.js/));
   });
 });
