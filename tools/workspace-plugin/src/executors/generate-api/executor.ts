25c6570ae11f563098df6ec9caaf65a5af02a3e9
Martin Hochel <martinhochel@microsoft.com>
2024-10-23 14:51:17 +0200

    ci: troubleshoot ws plugin issues with api-extractor v2

--- tools/workspace-plugin/src/executors/generate-api/executor.ts
+++ tools/workspace-plugin/src/executors/generate-api/executor.ts
@@ -70,8 +70,8 @@ function normalizeOptions(schema: GenerateApiExecutorSchema, context: ExecutorCo
   };
 
   function isCI() {
-    console.log({ ci: process.env.CI, gh: process.env.GITHUB_ACTIONS });
-    console.log({ env: JSON.stringify(process.env, null, 2) });
+    console.log({ ci: process.env.CI, tfBuild: process.env.TF_BUILD, gh: process.env.GITHUB_ACTIONS });
+    // console.log({ env: JSON.stringify(process.env, null, 2) });
     return (
       (process.env.CI && process.env.CI !== 'false') ||
       process.env.TF_BUILD === 'true' ||
