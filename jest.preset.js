836a14e9c71124340c39dee9cd0ef656606a1c9a
Martin Hochel <martinhochel@microsoft.com>
2024-10-24 14:50:41 +0200

    chore: remove env logs from jest preset

--- jest.preset.js
+++ jest.preset.js
@@ -58,7 +58,7 @@ module.exports = {
 
 function isCI() {
   console.log({ ci: process.env.CI, gh: process.env.GITHUB_ACTIONS });
-  console.log({ env: JSON.stringify(process.env, null, 2) });
+
   return (
     (process.env.CI && process.env.CI !== 'false') ||
     (process.env.TF_BUILD && process.env.TF_BUILD.toLowerCase() === 'true') ||
