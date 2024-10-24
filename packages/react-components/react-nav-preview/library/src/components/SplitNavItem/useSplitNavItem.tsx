bc87596a3d5c7ad75de034975fb176026bb8b6a7
Mason Tejera <matejera@microsoft.com>
2024-10-24 11:58:25 -0700

    remove random change

--- packages/react-components/react-nav-preview/library/src/components/SplitNavItem/useSplitNavItem.tsx
+++ packages/react-components/react-nav-preview/library/src/components/SplitNavItem/useSplitNavItem.tsx
@@ -9,8 +9,6 @@ import { NavItem } from '../NavItem/index';
 import { NavSubItem } from '../NavSubItem/NavSubItem';
 import { useNavCategoryContext_unstable } from '../NavCategoryContext';
 
-// a random change to trigger a PR.
-
 /**
  * Create the state required to render SplitNavItem.
  *
