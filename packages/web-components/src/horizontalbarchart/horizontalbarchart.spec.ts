b45180f0383a10da183131acad339ba03854fa17
v-baambati <132879294+v-baambati@users.noreply.github.com>
2024-10-25 14:36:41 +0530

    added basic tests to Horizontalbar chart

--- packages/web-components/src/horizontalbarchart/horizontalbarchart.spec.ts
+++ packages/web-components/src/horizontalbarchart/horizontalbarchart.spec.ts
@@ -24,14 +24,23 @@ test.describe('horizontalbarchart - Basic', () => {
     const element = page.locator('fluent-horizontalbarchart');
     const bars = element.locator('.bar')
     await expect(bars).toHaveCount(12);
+    const firstBar = bars.first();
+    await expect(firstBar).toHaveCSS('fill', 'rgb(0, 153, 188)');
+    await expect(firstBar).toHaveCSS('opacity', '1');
+    await expect(firstBar).toHaveAttribute(`height`, '12');
     const barLabels = element.locator('.barLabel')
     await expect(barLabels).toHaveCount(12);
+    const firstBarLabel = barLabels.first();
+    await expect(firstBarLabel).toHaveText('272');
+    await expect(firstBarLabel).toHaveAttribute(`aria-label`, 'Total: 272');
   });
 
   test('Should render legends data properly', async ({ page }) => {
     const element = page.locator('fluent-horizontalbarchart');
     const legends = element.locator('.legend')
     await expect(legends).toHaveCount(6);
+    const firstLegend = legends.first();
+    await expect(firstLegend.locator('div').first()).toHaveCSS('background-color', 'rgb(0, 153, 188)');
+    await expect(firstLegend).toHaveText('Debit card numbers (EU and USA)');
   });
-
 });
