a58bfacee1ad861e2e3d6a84c98789a1918c9f65
v-baambati <132879294+v-baambati@users.noreply.github.com>
2024-10-28 16:20:17 +0530

    added mouse event test cases to horizontalbarchart

--- packages/web-components/src/horizontalbarchart/horizontalbarchart.spec.ts
+++ packages/web-components/src/horizontalbarchart/horizontalbarchart.spec.ts
@@ -8,7 +8,7 @@ test.describe('horizontalbarchart - Basic', () => {
     await page.waitForFunction(() => customElements.whenDefined('fluent-horizontalbarchart'));
   });
 
-  test('Should render chart properly', async ({ page }) => {
+  test('Should render horizontalbarchart properly', async ({ page }) => {
     const element = page.locator('fluent-horizontalbarchart');
     await expect(element.getByRole('button', { name: 'Debit card numbers (EU and USA)' })).toBeVisible();
     await expect(element.getByRole('button', { name: 'Passport numbers (USA)' })).toBeVisible();
@@ -42,5 +42,89 @@ test.describe('horizontalbarchart - Basic', () => {
     const firstLegend = legends.first();
     await expect(firstLegend.locator('div').first()).toHaveCSS('background-color', 'rgb(0, 153, 188)');
     await expect(firstLegend).toHaveText('Debit card numbers (EU and USA)');
+  });
+
+  test('Should update bar css/opaity when mouse hover on legend', async ({ page }) => {
+    const element = page.locator('fluent-horizontalbarchart');
+    const legends = element.locator('.legend')
+    await expect(legends).toHaveCount(6);
+    const firstLegend = legends.first();
+    //mouse events
+    await legends.nth(0).dispatchEvent('mouseover');
+    const bars = element.locator('.bar')
+    await expect(bars).toHaveCount(12);
+    await expect(bars.nth(0)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(1)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(2)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(3)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(4)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(5)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(6)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(7)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(8)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(9)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(10)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(11)).toHaveCSS('opacity', '0.1');
+  });
+
+  test('Should update bar css/opaity when mouse moved from one legend to another legend', async ({ page }) => {
+    const element = page.locator('fluent-horizontalbarchart');
+    const legends = element.locator('.legend')
+    await expect(legends).toHaveCount(6);
+    await legends.nth(0).dispatchEvent('mouseover');
+    const bars = element.locator('.bar')
+    await expect(bars.nth(0)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(1)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(2)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(3)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(4)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(5)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(6)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(7)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(8)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(9)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(10)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(11)).toHaveCSS('opacity', '0.1');
+    await legends.nth(0).dispatchEvent('mouseout');
+    await legends.nth(1).dispatchEvent('mouseover');
+    await expect(bars.nth(0)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(1)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(2)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(3)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(4)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(5)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(6)).toHaveCSS('opacity', '1');
+    await expect(bars.nth(7)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(8)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(9)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(10)).toHaveCSS('opacity', '0.1');
+    await expect(bars.nth(11)).toHaveCSS('opacity', '0.1');
+  });
+
+  test('Should show callout when mouse hover on bar', async ({ page }) => {
+    const element = page.locator('fluent-horizontalbarchart');
+    const bars = element.locator('.bar')
+    const tooltip = element.locator('.tooltip')
+    await expect(tooltip).toHaveCount(0);
+    await bars.nth(0).dispatchEvent('mouseover');
+    await expect(tooltip).toHaveCount(1);
+    await expect(tooltip.nth(0)).toHaveCSS('opacity', '1');
+    await expect(tooltip.nth(0).locator('div').first()).toHaveText('Debit card numbers (EU and USA) 40');
+  });
+
+  test('Should update callout data when mouse moved from one bar to another bar', async ({ page }) => {
+    const element = page.locator('fluent-horizontalbarchart');
+    const bars = element.locator('.bar')
+    const tooltip = element.locator('.tooltip')
+    await expect(tooltip).toHaveCount(0);
+    await bars.nth(0).dispatchEvent('mouseover');
+    await expect(tooltip).toHaveCount(1);
+    await expect(tooltip.nth(0)).toHaveCSS('opacity', '1');
+    await expect(tooltip.nth(0).locator('div').first()).toHaveText('Debit card numbers (EU and USA) 40');
+    await bars.nth(0).dispatchEvent('mouseout');
+    await bars.nth(1).dispatchEvent('mouseover');
+    await expect(tooltip).toHaveCount(2);
+    await expect(tooltip.nth(1)).toHaveCSS('opacity', '1');
+    await expect(tooltip.nth(1).locator('div').first()).toHaveText('Passport numbers (USA) 23');
   });
 });
