cc455564efc9692fa02359abe11a33fb814c4695
Oleksandr Fediashov <olfedias@microsoft.com>
2024-10-30 15:30:37 +0100

    chore(react-portal): refactor portals creation

--- packages/react-components/react-portal/library/src/components/Portal/Portal.types.ts
+++ packages/react-components/react-portal/library/src/components/Portal/Portal.types.ts
@@ -1,4 +1,9 @@
+import type { ComponentState, Slot } from '@fluentui/react-utilities';
 import * as React from 'react';
+
+export type PortalInternalSlots = {
+  root?: Slot<'div'>;
+};
 
 export type PortalProps = {
   /**
@@ -14,11 +19,12 @@ export type PortalProps = {
   mountNode?: HTMLElement | null | { element?: HTMLElement | null; className?: string };
 };
 
-export type PortalState = Pick<PortalProps, 'children'> & {
-  mountNode: HTMLElement | null | undefined;
+export type PortalState = ComponentState<PortalInternalSlots> &
+  Pick<PortalProps, 'children'> & {
+    mountNode: HTMLElement | null | undefined;
 
-  /**
-   * Ref to the root span element as virtual parent
-   */
-  virtualParentRootRef: React.MutableRefObject<HTMLSpanElement | null>;
-};
+    /**
+     * Ref to the root span element as virtual parent
+     */
+    virtualParentRootRef: React.MutableRefObject<HTMLSpanElement | null>;
+  };
