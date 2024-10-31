dffe6d663ea6bd8b3ea1183942fc35750990b088
Mitch-At-Work <mifraser@microsoft.com>
2024-10-30 17:03:24 -0700

    remove Viewport story folder

--- packages/react-components/react-carousel/stories/src/CarouselViewport/CarouselViewportDefault.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { CarouselViewport, CarouselViewportProps } from '@fluentui/react-carousel';
-
-export const Default = (props: Partial<CarouselViewportProps>) => <CarouselViewport {...props} />;
