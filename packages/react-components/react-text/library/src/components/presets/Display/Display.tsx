ab081d02c9adbeece5a647e8b3753d2ce6081ca5
mainframev <vgenaev@gmail.com>
2024-11-12 01:08:15 +0100

    chore(react-text): use consistent react imports

--- packages/react-components/react-text/library/src/components/presets/Display/Display.tsx
+++ packages/react-components/react-text/library/src/components/presets/Display/Display.tsx
@@ -1,4 +1,4 @@
-import type { FunctionComponent } from 'react';
+import * as React from 'react';
 import { createPreset } from '../createPreset';
 import type { TextPresetProps } from '../../Text/Text.types';
 import { displayClassNames, useDisplayStyles } from './useDisplayStyles.styles';
@@ -6,7 +6,7 @@ import { displayClassNames, useDisplayStyles } from './useDisplayStyles.styles';
 /**
  * Text preset component for the Display typography variant
  */
-export const Display: FunctionComponent<TextPresetProps> = createPreset({
+export const Display: React.FunctionComponent<TextPresetProps> = createPreset({
   useStyles: useDisplayStyles,
   className: displayClassNames.root,
   displayName: 'Display',
