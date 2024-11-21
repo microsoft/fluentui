1ac0c6b5a27761050d478a714faca682351008d7
krkshitij <110246001+krkshitij@users.noreply.github.com>
2024-11-21 12:31:34 +0000

    update donut chart plotly example

--- packages/react-examples/src/react-charting/DonutChart/DonutChart.Plotly.Example.tsx
+++ packages/react-examples/src/react-charting/DonutChart/DonutChart.Plotly.Example.tsx
@@ -6,14 +6,42 @@ const plotlyJson = {
     {
       hole: 0.6,
       type: 'pie',
+      frame: null,
+      marker: {
+        line: {
+          color: 'transparent',
+        },
+        color: 'rgba(31,119,180,1)',
+        fillcolor: 'rgba(31,119,180,1)',
+      },
+      labelssrc: 'koolio:2:346057',
       labels: ['Rural', 'Suburban', 'Urban'],
+      valuessrc: 'koolio:2:95d156',
       values: [125, 625, 1625],
     },
   ],
   layout: {
     title: 'Donut w Ply',
-    showlegend: false,
+    xaxis: {
+      showgrid: false,
+      zeroline: false,
+      showticklabels: false,
+    },
+    yaxis: {
+      showgrid: false,
+      zeroline: false,
+      showticklabels: false,
+    },
+    margin: {
+      b: 40,
+      l: 60,
+      r: 10,
+      t: 25,
+    },
+    hovermode: 'closest',
+    showlegend: true,
   },
+  frames: [],
 };
 
 export class DonutChartPlotlyExample extends React.Component<{}, {}> {
