8902f850e3c3a569aba816de1c4e5e1c1a628125
Atishay Jain (atisjai) <atishay.jain@microsoft.com>
2024-11-24 20:01:49 +0530

    Update data processing

--- packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
+++ packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
@@ -3,36 +3,38 @@ import { IChartProps, IVerticalStackedChartProps } from '../../types/IDataPoint'
 import { getNextColor } from '../../utilities/colors';
 import { IVerticalStackedBarChartProps } from '../VerticalStackedBarChart/index';
 
-export const transformPlotlyJsonToDonutProps = (obj: any): IDonutChartProps => {
+export const transformPlotlyJsonToDonutProps = (jsonObj: any): IDonutChartProps => {
+    const { data, layout } = jsonObj;
     const donutData: IChartProps = {
-      chartTitle: obj.layout.title,
-      chartData: obj.data[0].labels.map((label: string, index: number) => {
+      chartTitle: layout.title,
+      chartData: data[0].labels.map((label: string, index: number) => {
         return {
           legend: label,
-          data: obj.data[0].values[index],
+          data: data[0].values[index],
           color: getNextColor(index),
         };
       }),
     };
   
-    const width: number = obj.layout.width || 440;
-    const height: number = obj.layout.height || 220;
-    const innerRadius: number = (Math.min(width, height - 40) * (obj.data[0].hole || 0.5)) / 2;
+    const width: number = layout.width || 440;
+    const height: number = layout.height || 220;
+    const innerRadius: number = (Math.min(width, height - 40) * (data[0].hole || 0.5)) / 2;
   
     return {
       data: donutData,
-      hideLegend: !obj.layout.showlegend,
+      hideLegend: !layout.showlegend,
       width,
       height,
       innerRadius,
     };
   };
   
-export const transformPlotlyJsonToColumnProps = (obj: any): IVerticalStackedBarChartProps => {
+export const transformPlotlyJsonToColumnProps = (jsonObj: any): IVerticalStackedBarChartProps => {
+    const { data, layout } = jsonObj;
     const mapXToDataPoints: { [key: string]: IVerticalStackedChartProps } = {};
     let yMaxValue = 0;
 
-    obj.data.forEach((series: any, index1: number) => {
+    data.forEach((series: any, index1: number) => {
       series.x.forEach((x: string | number, index2: number) => {
         if (!mapXToDataPoints[x]) {
           mapXToDataPoints[x] = { xAxisPoint: x, chartData: [], lineData: [] };
@@ -56,9 +58,9 @@ export const transformPlotlyJsonToColumnProps = (obj: any): IVerticalStackedBarC
   
     return {
       data: Object.values(mapXToDataPoints),
-      chartTitle: obj.layout.title,
-      // width: obj.layout.width,
-      // height: obj.layout.height,
+      chartTitle: layout.title,
+      // width: layout.width,
+      // height: layout.height,
       barWidth: 'auto',
       yMaxValue,
     };
