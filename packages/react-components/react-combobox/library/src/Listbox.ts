1a46a5a461123b1c862aa948ef0e14eba7b43ed0
Oleksandr Fediashov <olfedias@microsoft.com>
2024-12-02 15:19:57 +0100

    chore: remove usage of "export *" [part 2]

--- packages/react-components/react-combobox/library/src/Listbox.ts
+++ packages/react-components/react-combobox/library/src/Listbox.ts
@@ -1,1 +1,8 @@
-export * from './components/Listbox/index';
+export type { ListboxContextValues, ListboxProps, ListboxSlots, ListboxState } from './components/Listbox/index';
+export {
+  Listbox,
+  listboxClassNames,
+  renderListbox_unstable,
+  useListboxStyles_unstable,
+  useListbox_unstable,
+} from './components/Listbox/index';
