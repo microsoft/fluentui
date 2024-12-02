99e5e6a9d394d6be64a58ac4ffb38b11023bcb72
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2024-12-02 17:06:25 +0530

    feat(charts): Declarative chart component to render chart using plotly json schema (#33348)
    Co-authored-by: krkshitij <110246001+krkshitij@users.noreply.github.com>
    Co-authored-by: srmukher <120183316+srmukher@users.noreply.github.com>
    Co-authored-by: Anush Gupta <74965306+Anush2303@users.noreply.github.com>

---
+++ packages/react-examples/src/react-charting/DeclarativeChart/index.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { DeclarativeChartBasicExample } from './DeclarativeChart.Basic.Example';
+
+export const Basic = () => <DeclarativeChartBasicExample />;
+
+export default {
+  title: 'Components/DeclarativeChart',
+};
