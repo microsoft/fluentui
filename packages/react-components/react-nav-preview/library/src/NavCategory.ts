e1a735b95e04b60ce0b4a0d03d55ac9eeaa6c0f5
Oleksandr Fediashov <olfedias@microsoft.com>
2024-12-02 12:33:17 +0100

    chore: remove usage of "export *"

--- packages/react-components/react-nav-preview/library/src/NavCategory.ts
+++ packages/react-components/react-nav-preview/library/src/NavCategory.ts
@@ -1,1 +1,2 @@
-export * from './components/NavCategory/index';
+export type { NavCategoryProps, NavCategoryState } from './components/NavCategory/index';
+export { NavCategory, renderNavCategory_unstable, useNavCategory_unstable } from './components/NavCategory/index';
