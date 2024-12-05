2b9ef3dd040f6df19d6332f5660bc62b21e07ec2
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-12-04 23:41:12 -0800

    update dependencies and add CSS anchor positioning polyfill

--- packages/web-components/test/harness/src/main.ts
+++ packages/web-components/test/harness/src/main.ts
@@ -1,3 +1,11 @@
+if (!CSS.supports('anchor-name: --foo')) {
+  import('@oddbird/css-anchor-positioning/fn').then(({ default: applyPolyfill }) => {
+    Object.defineProperty(window, 'CSS_ANCHOR_POLYFILL', {
+      value: applyPolyfill,
+    });
+  });
+}
+
 import { webLightTheme } from '@fluentui/tokens';
 import { setTheme } from '../../../src/theme/set-theme.js';
 import '../../../src/index-rollup.js';
