0ab8d636f70ccfd16e505b557e40b418aff2bdd7
Vijay Soni <vs4vijay@gmail.com>
2024-12-06 20:19:49 +0530

    Update MessageBarTitle to support different HTML tags
    Fixes #33373

--- packages/react-components/react-message-bar/library/src/components/MessageBarTitle/MessageBarTitle.types.ts
+++ packages/react-components/react-message-bar/library/src/components/MessageBarTitle/MessageBarTitle.types.ts
@@ -1,13 +1,15 @@
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
 
 export type MessageBarTitleSlots = {
-  root: Slot<'span'>;
+  root: Slot<'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p'>;
 };
 
 /**
  * MessageBarTitle Props
  */
-export type MessageBarTitleProps = ComponentProps<MessageBarTitleSlots>;
+export type MessageBarTitleProps = ComponentProps<MessageBarTitleSlots> & {
+  as?: 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+};
 
 /**
  * State used in rendering MessageBarTitle
