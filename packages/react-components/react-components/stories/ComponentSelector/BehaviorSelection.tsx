aa544c277865b40102b1aab01a1a28bc6881b5e9
Adam Samec <asamec@microsoft.com>
2024-12-12 14:35:27 +0100

    Move behaviorOptions to JSON file

--- packages/react-components/react-components/stories/ComponentSelector/BehaviorSelection.tsx
+++ packages/react-components/react-components/stories/ComponentSelector/BehaviorSelection.tsx
@@ -1,51 +1,12 @@
 import * as React from 'react';
 import { ToggleButton, makeStyles } from '@fluentui/react-components';
 
+import behaviorOptions from './selection-logic/BehaviorOptions.json';
+
 const useStyles = makeStyles({
   heading: { margin: '30px 0 10px 0' },
   behaviors: { display: 'flex', gap: '10px' },
 });
-
-const behaviorOptions = [
-  {
-    title: 'Choose Behavior',
-    behaviors: [
-      {
-        id: 'interactive',
-        name: 'Interactive',
-      },
-      {
-        id: 'static',
-        name: 'Static',
-      },
-      {
-        id: 'toggle',
-        name: 'Toggle',
-      },
-      {
-        id: 'moreActions',
-        name: 'Multiple actions',
-      },
-    ],
-  },
-  {
-    title: 'Choose Keyboard and Screen Reader Experience',
-    behaviors: [
-      {
-        id: 'navigationByTabKey',
-        name: 'Navigation by Tab key',
-      },
-      {
-        id: 'navigationByArrowKeys',
-        name: 'Navigation by arrow keys',
-      },
-      {
-        id: 'narratesPosition',
-        name: 'Screen reader narrates position',
-      },
-    ],
-  },
-];
 
 const getDefaultBehaviors = () => {
   const behaviors = {};
