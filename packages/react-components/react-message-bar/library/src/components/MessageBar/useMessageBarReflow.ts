81850f846bb61166e542e4d7b5a8b3ac51d5ac2c
Lingfan Gao <lingfangao@hotmail.com>
2024-12-16 17:06:21 +0000

    change reflowmechanism

--- packages/react-components/react-message-bar/library/src/components/MessageBar/useMessageBarReflow.ts
+++ packages/react-components/react-message-bar/library/src/components/MessageBar/useMessageBarReflow.ts
@@ -1,89 +1,78 @@
 import * as React from 'react';
 import { useFluent_unstable as useFluent } from '@fluentui/react-shared-contexts';
-import { isHTMLElement } from '@fluentui/react-utilities';
+import { useIsomorphicLayoutEffect } from '@fluentui/react-utilities';
 
 export function useMessageBarReflow(enabled: boolean = false) {
   const { targetDocument } = useFluent();
-  const forceUpdate = React.useReducer(() => ({}), {})[1];
-  const reflowingRef = React.useRef(false);
   const prevInlineSizeRef = React.useRef(-1);
+  const messageBarRef = React.useRef<HTMLElement | null>(null);
 
-  const handleResize: ResizeObserverCallback = React.useCallback(
-    entries => {
-      // Resize observer is only owned by this component - one resize observer entry expected
-      // No need to support multiple fragments - one border box entry expected
-      if (process.env.NODE_ENV !== 'production' && entries.length > 1) {
-        // eslint-disable-next-line no-console
-        console.error(
-          [
-            'useMessageBarReflow: Resize observer should only have one entry. ',
-            'If multiple entries are observed, the first entry will be used.',
-            'This is a bug, please report it to the Fluent UI team.',
-          ].join(' '),
-        );
+  const [reflowing, setReflowing] = React.useState(false);
+
+  // This layout effect 'sanity checks' what observers have done
+  // since DOM has not been flushed when observers run
+  useIsomorphicLayoutEffect(() => {
+    if (!messageBarRef.current) {
+      return;
+    }
+
+    setReflowing(prevReflowing => {
+      if (
+        !prevReflowing &&
+        messageBarRef.current &&
+        (messageBarRef.current.scrollWidth > messageBarRef.current.offsetWidth ||
+          !isFullyInViewport(messageBarRef.current))
+      ) {
+        return true;
       }
 
-      const entry = entries[0];
-      // `borderBoxSize` is not supported before Chrome 84, Firefox 92, nor Safari 15.4
-      const inlineSize = entry?.borderBoxSize?.[0]?.inlineSize ?? entry?.target.getBoundingClientRect().width;
+      return prevReflowing;
+    });
+  }, [reflowing]);
 
-      if (inlineSize === undefined || !entry) {
-        return;
-      }
+  const handleResize: ResizeObserverCallback = React.useCallback(() => {
+    if (!messageBarRef.current) {
+      return;
+    }
 
-      const { target } = entry;
+    const inlineSize = messageBarRef.current.getBoundingClientRect().width;
+    const scrollWidth = messageBarRef.current.scrollWidth;
 
-      if (!isHTMLElement(target)) {
-        return;
-      }
+    const expanding = prevInlineSizeRef.current < inlineSize;
+    const overflowing = inlineSize < scrollWidth;
 
-      let nextReflowing: boolean | undefined;
+    setReflowing(!expanding || overflowing);
+  }, []);
 
-      // No easy way to really determine when the single line layout will fit
-      // Just keep try to set single line layout as long as the size is growing
-      // Will cause flickering when size is being adjusted gradually (i.e. drag) - but this should not be a common case
-      if (reflowingRef.current) {
-        if (prevInlineSizeRef.current < inlineSize) {
-          nextReflowing = false;
-        }
-      } else {
-        const scrollWidth = target.scrollWidth;
-        if (inlineSize < scrollWidth) {
-          nextReflowing = true;
-        }
-
-        if (!isFullyInViewport(target)) {
-          nextReflowing = true;
-        }
-      }
-
-      prevInlineSizeRef.current = inlineSize;
-      if (typeof nextReflowing !== 'undefined' && reflowingRef.current !== nextReflowing) {
-        reflowingRef.current = nextReflowing;
-        forceUpdate();
-      }
-    },
-    [forceUpdate],
-  );
+  const handleIntersection: IntersectionObserverCallback = React.useCallback(entries => {
+    if (entries[0].intersectionRatio < 1) {
+      setReflowing(true);
+    }
+  }, []);
 
   const ref = React.useMemo(() => {
     let resizeObserver: ResizeObserver | null = null;
+    let intersectionObserer: IntersectionObserver | null = null;
+
     return (el: HTMLElement | null) => {
       if (!enabled || !el || !targetDocument?.defaultView) {
         resizeObserver?.disconnect();
+        intersectionObserer?.disconnect();
         return;
       }
 
-      resizeObserver?.disconnect();
+      messageBarRef.current = el;
 
       const win = targetDocument.defaultView;
       resizeObserver = new win.ResizeObserver(handleResize);
-      resizeObserver.observe(el, { box: 'border-box' });
-      resizeObserver.observe(el.ownerDocument.body);
-    };
-  }, [targetDocument, handleResize, enabled]);
+      intersectionObserer = new win.IntersectionObserver(handleIntersection);
 
-  return { ref, reflowing: reflowingRef.current };
+      intersectionObserer.observe(el);
+      resizeObserver.observe(el, { box: 'border-box' });
+    };
+  }, [handleResize, handleIntersection, enabled, targetDocument]);
+
+  return { ref, reflowing };
 }
 
 const isFullyInViewport = (el: HTMLElement) => {
