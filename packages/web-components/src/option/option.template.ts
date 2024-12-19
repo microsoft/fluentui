d776ba0285b09ebd17e2835538dec8d452e6d444
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-12-19 15:09:46 -0800

    remove unnecessary xmlns attributes from option SVGs

--- packages/web-components/src/option/option.template.ts
+++ packages/web-components/src/option/option.template.ts
@@ -6,12 +6,12 @@ import type { Option } from './option.js';
 import type { OptionOptions } from './option.options.js';
 
 const checkedIndicator = html.partial(/* html */ `
-  <svg aria-hidden="true" class="checkmark-16-filled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+  <svg aria-hidden="true" class="checkmark-16-filled" viewBox="0 0 16 16">
     <path
       d="M14.046 3.486a.75.75 0 0 1-.032 1.06l-7.93 7.474a.85.85 0 0 1-1.188-.022l-2.68-2.72a.75.75 0 1 1 1.068-1.053l2.234 2.267l7.468-7.038a.75.75 0 0 1 1.06.032"
     />
   </svg>
-  <svg aria-hidden="true" class="checkmark-12-regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
+  <svg aria-hidden="true" class="checkmark-12-regular" viewBox="0 0 12 12">
     <path
       d="M9.854 3.146a.5.5 0 0 1 0 .708l-4.5 4.5a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L5 7.293l4.146-4.147a.5.5 0 0 1 .708 0"
     />
