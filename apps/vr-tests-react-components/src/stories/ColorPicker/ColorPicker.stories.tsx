1dcade941241ca8a23cf511e67d730687f526d20
Valentyna <vkozlova@microsoft.com>
2024-12-19 16:15:18 +0100

    PR fix

--- apps/vr-tests-react-components/src/stories/ColorPicker/ColorPicker.stories.tsx
+++ apps/vr-tests-react-components/src/stories/ColorPicker/ColorPicker.stories.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import type { Meta } from '@storybook/react';
 import { ColorPicker } from '@fluentui/react-color-picker-preview';
-import { SampleColorPicker, steps } from './utils';
+import { SampleColorPicker } from './utils';
+import { Steps } from 'storywright';
 
 import { DARK_MODE, getStoryVariant, HIGH_CONTRAST, RTL, withStoryWrightSteps } from '../../utilities';
 
 export default {
   title: 'ColorPicker Converged',
-  decorators: [story => withStoryWrightSteps({ story, steps })],
+  decorators: [
+    story => withStoryWrightSteps({ story, steps: new Steps().snapshot('default', { cropTo: '.testWrapper' }).end() }),
+  ],
 } satisfies Meta<typeof ColorPicker>;
 
 export const Default = () => <SampleColorPicker color={{ h: 109, s: 1, v: 0.91 }} />;
-Default.storyName = 'default';
 
 export const DefaultDarkMode = getStoryVariant(Default, DARK_MODE);
 
--- apps/vr-tests-react-components/src/stories/ColorPicker/utils.tsx
+++ apps/vr-tests-react-components/src/stories/ColorPicker/utils.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Steps } from 'storywright';
 import {
   ColorPicker,
   ColorArea,
@@ -7,16 +6,6 @@ import {
   ColorSlider,
   type ColorPickerProps,
 } from '@fluentui/react-color-picker-preview';
-
-export const steps = new Steps()
-  .snapshot('default', { cropTo: '.testWrapper' })
-  .hover('.breadcrumb-sample')
-  .snapshot('hover', { cropTo: '.testWrapper' })
-  .mouseDown('.breadcrumb-sample')
-  .snapshot('pressed', { cropTo: '.testWrapper' })
-  .focus('.breadcrumb-sample')
-  .snapshot('focused', { cropTo: '.testWrapper' })
-  .end();
 
 export const SampleColorPicker = (props: ColorPickerProps) => (
   <ColorPicker {...props}>
