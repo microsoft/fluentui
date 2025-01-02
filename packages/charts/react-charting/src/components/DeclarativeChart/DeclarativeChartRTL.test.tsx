ff91b63ad598b559b17113afdf6ee6e6bdeea17f
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-01-02 11:36:39 +0530

    added component tests to Declarative chart

---
+++ packages/charts/react-charting/src/components/DeclarativeChart/DeclarativeChartRTL.test.tsx
@@ -0,0 +1,74 @@
+import { render } from '@testing-library/react';
+import { DeclarativeChart } from './index';
+import * as React from 'react';
+import { resetIds } from '../../Utilities';
+
+describe('DeclarativeChart', () => {
+  const mockGetComputedTextLength = jest.fn().mockReturnValue(100);
+  // Replace the original method with the mock implementation
+  Object.defineProperty(
+    Object.getPrototypeOf(document.createElementNS('http://www.w3.org/2000/svg', 'tspan')),
+    'getComputedTextLength',
+    {
+      value: mockGetComputedTextLength,
+    },
+  );
+  beforeEach(() => {
+    resetIds();
+  });
+  test('Should render areachart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_area.json');
+    const { container } = render(<DeclarativeChart key={'areachart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render donutchart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_donut.json');
+    const { container } = render(<DeclarativeChart key={'donutchart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render gaugechart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_gauge.json');
+    const { container } = render(<DeclarativeChart key={'gaugechart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render heatmapchart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_heatmap.json');
+    const { container } = render(<DeclarativeChart key={'heatmapchart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render linechart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_line.json');
+    const { container } = render(<DeclarativeChart key={'linechart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render piechart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_pie.json');
+    const { container } = render(<DeclarativeChart key={'piechart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render sankeychart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_sankey.json');
+    const { container } = render(<DeclarativeChart key={'sankeychart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Should render verticalbarchart in DeclarativeChart', () => {
+    // Arrange
+    const plotlySchema = require('./schema/fluent_verticalbar.json');
+    const { container } = render(<DeclarativeChart key={'verticalbarchart'} chartSchema={{ plotlySchema }} />);
+    expect(container).toMatchSnapshot();
+  });
+});
