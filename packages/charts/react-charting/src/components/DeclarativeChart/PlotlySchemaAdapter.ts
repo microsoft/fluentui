dd1876c600c5a7a19a6e233d861612d72b66581c
krkshitij <110246001+krkshitij@users.noreply.github.com>
2025-01-21 05:29:07 +0000

    fix overlapping bars in histogram

--- packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
+++ packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
@@ -328,7 +328,6 @@ export const transformPlotlyJsonToVBCProps = (
     data: vbcData,
     // width: layout?.width,
     // height: layout?.height,
-    barWidth: 24,
     supportNegativeData: true,
     chartTitle,
     xAxisTitle,
