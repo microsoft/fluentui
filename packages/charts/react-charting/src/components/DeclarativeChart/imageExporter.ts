57165f4aa96f1417347275e52262f6de755225d5
krkshitij <110246001+krkshitij@users.noreply.github.com>
2025-01-24 12:50:59 +0530

    fix(react-charting): ensure text elements use the correct font-family in SVG export (#33719)

--- packages/charts/react-charting/src/components/DeclarativeChart/imageExporter.ts
+++ packages/charts/react-charting/src/components/DeclarativeChart/imageExporter.ts
@@ -53,7 +53,9 @@ function toSVG(chartContainer: HTMLElement, background: string) {
     .attr('width', null)
     .attr('height', null)
     .attr('viewBox', null);
+  const { fontFamily } = getComputedStyle(chartContainer);
 
+  clonedSvg.selectAll('text').style('font-family', fontFamily);
   if (legendGroup.node) {
     clonedSvg.append(() => legendGroup.node);
   }
