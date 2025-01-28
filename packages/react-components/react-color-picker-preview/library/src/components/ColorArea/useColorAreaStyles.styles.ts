b4314b82b3a97b2a8a36bee9b50b00a78791797a
Valentyna <vkozlova@microsoft.com>
2025-01-28 17:25:23 +0100

    fix(react-color-picker): made thumb design according to partners ask

--- packages/react-components/react-color-picker-preview/library/src/components/ColorArea/useColorAreaStyles.styles.ts
+++ packages/react-components/react-color-picker-preview/library/src/components/ColorArea/useColorAreaStyles.styles.ts
@@ -54,7 +54,8 @@ const useThumbStyles = makeStyles({
     outlineStyle: 'none',
     forcedColorAdjust: 'none',
     borderRadius: tokens.borderRadiusCircular,
-    boxShadow: `0 0 0 calc(var(${thumbSizeVar}) * .18) ${tokens.colorNeutralBackground1} inset`,
+    border: `1px solid ${tokens.colorNeutralForeground4}`,
+    boxShadow: tokens.shadow4,
     backgroundColor: `var(${colorAreaCSSVars.thumbColorVar})`,
     transform: 'translate(-50%, 50%)',
     [`${thumbPositionXVar}`]: `clamp(var(${innerThumbRadiusVar}), var(${colorAreaCSSVars.areaXProgressVar}), calc(100% - var(${innerThumbRadiusVar})))`,
@@ -67,7 +68,7 @@ const useThumbStyles = makeStyles({
       borderRadius: tokens.borderRadiusCircular,
       boxSizing: 'border-box',
       content: "''",
-      border: `calc(var(${thumbSizeVar}) * .05) solid ${tokens.colorNeutralStroke1Pressed}`,
+      border: `2px solid ${tokens.colorNeutralBackground1}`,
     },
   },
   focusIndicator: createFocusOutlineStyle({
