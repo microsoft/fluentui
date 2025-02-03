65d40baf0b8ac0e06188afe429e670532faa3fb2
Lingfan Gao <lingfangao@hotmail.com>
2025-02-03 08:44:36 +0000

    test storywright steps

--- packages/react-components/react-button/library/src/components/Button/useButtonStyles.styles.ts
+++ packages/react-components/react-button/library/src/components/Button/useButtonStyles.styles.ts
@@ -550,6 +550,8 @@ export const useButtonStyles_unstable = (state: ButtonState): ButtonState => {
 
   const { appearance, disabled, disabledFocusable, icon, iconOnly, iconPosition, shape, size } = state;
 
+  state.root.style = { background: 'red' };
+
   state.root.className = mergeClasses(
     buttonClassNames.root,
     rootBaseClassName,
