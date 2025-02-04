144b64d4ab583f8a68aedcc443a446f14baba114
srmukher <120183316+srmukher@users.noreply.github.com>
2025-02-04 12:46:30 +0000

    Fixed tooltip position and background on Gauge and Pie charts

--- packages/charts/react-charting/src/components/GaugeChart/GaugeChart.base.tsx
+++ packages/charts/react-charting/src/components/GaugeChart/GaugeChart.base.tsx
@@ -317,6 +317,7 @@ export class GaugeChartBase extends React.Component<IGaugeChartProps, IGaugeChar
                   }}
                   maxWidth={this._innerRadius * 2 - 24}
                   wrapContent={this._wrapContent}
+                  theme={this.props.theme}
                 />
               </g>
               {this.props.sublabel && (
