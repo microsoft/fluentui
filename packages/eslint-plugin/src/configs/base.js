076242652e880ae0aebc7804bb57a1af51d39b42
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-02-10 19:46:10 +0100

    apply plugin only for v9 packages

--- packages/eslint-plugin/src/configs/base.js
+++ packages/eslint-plugin/src/configs/base.js
@@ -2,11 +2,16 @@
 
 const path = require('path');
 
-const { getNamingConventionRule } = require('../utils/configHelpers');
+const { getNamingConventionRule, testFiles, storyFiles } = require('../utils/configHelpers');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [path.join(__dirname, 'core')],
+  extends: [
+    path.join(__dirname, 'core'),
+    // Browser compatibility linter rules
+    'plugin:compat/recommended',
+  ],
+  plugins: ['compat'],
   rules: {
     /**
      * `@typescript-eslint`plugin eslint rules
@@ -24,5 +29,28 @@ module.exports = {
         '@rnx-kit/no-export-all': ['error', { expand: 'all' }],
       },
     },
+    {
+      files: [...testFiles, ...storyFiles],
+      rules: {
+        'compat/compat': 'off',
+      },
+    },
   ],
+  settings: {
+    // Browser matrix support - https://react.fluentui.dev/?path=/docs/concepts-developer-browser-support-matrix--docs#partial-browser-support-matrix
+    targets: [
+      // Desktop browsers
+      'edge >= 79',
+      'firefox >= 69',
+      'chrome >= 79',
+      'safari >= 13.1',
+      'opera >= 64',
+      'not ie <= 11',
+      // Mobile browsers
+      'ios_saf >= 13.4',
+      'android >= 79',
+      'samsung >= 14',
+      'not op_mini all',
+    ],
+  },
 };
