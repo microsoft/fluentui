403112117048296c87471783e1a86bacfbc48bdc
Martin Hochel <martinhochel@microsoft.com>
2025-02-10 13:36:40 +0100

    feat(public-docsite-v9): normalize import to avoid suite imports for future component extraction

--- apps/public-docsite-v9/src/DocsComponents/FluentDocsContainer.stories.tsx
+++ apps/public-docsite-v9/src/DocsComponents/FluentDocsContainer.stories.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { DocsContainer, DocsContextProps } from '@storybook/addon-docs';
-import { FluentStoryContext } from '@fluentui/react-storybook-addon';
+import { DocsContainer, type DocsContextProps } from '@storybook/addon-docs';
+import { type FluentStoryContext } from '@fluentui/react-storybook-addon';
 import { webLightTheme, FluentProvider } from '@fluentui/react-components';
+
 
 interface FluentDocsContainerProps {
   context: FluentStoryContext & DocsContextProps;
