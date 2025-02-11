691e39c919a9307a794cfe5587e1259897ceceb8
krkshitij <110246001+krkshitij@users.noreply.github.com>
2025-02-11 14:56:37 +0000

    add tests

--- packages/charts/react-charting/src/utilities/UtilityUnitTests.test.ts
+++ packages/charts/react-charting/src/utilities/UtilityUnitTests.test.ts
@@ -158,9 +158,8 @@ const convertXAxisResultToJson = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   result: { xScale: any; tickValues: string[] },
   isStringAxis: boolean = false,
-  tickCount: number = 6,
 ): [number, string][] => {
-  const tickValues = isStringAxis ? result.tickValues : result.xScale.ticks(tickCount);
+  const tickValues = isStringAxis ? result.tickValues : result.xScale.ticks(result.tickValues.length);
   return tickValues.map((item: number | Date | string, i: number) => [result.xScale(item), result.tickValues[i]]);
 };
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -195,13 +194,19 @@ describe('createNumericXAxis', () => {
   it('should create the x-axis labels correctly for a specific number of ticks', () => {
     const xAxisParams = createXAxisParams({ xAxisCount: 3 });
     const result = utils.createNumericXAxis(xAxisParams, {}, utils.ChartTypes.VerticalBarChart);
-    matchResult(convertXAxisResultToJson(result, false, xAxisParams.xAxisCount));
+    matchResult(convertXAxisResultToJson(result, false));
   });
 
   it('should render the x-axis labels correctly for horizontal bar chart with axis', () => {
     const xAxisParams = createXAxisParams();
     utils.createNumericXAxis(xAxisParams, {}, utils.ChartTypes.HorizontalBarChartWithAxis);
     expect(xAxisParams.xAxisElement).toMatchSnapshot();
+  });
+
+  it('should render fewer x-axis labels when hideTickOverlap is true and container width is small', () => {
+    const xAxisParams = createXAxisParams({ domainNRangeValues: { rEndValue: 50 }, hideTickOverlap: true });
+    const result = utils.createNumericXAxis(xAxisParams, {}, utils.ChartTypes.VerticalBarChart);
+    matchResult(convertXAxisResultToJson(result));
   });
 });
 
@@ -226,7 +231,7 @@ conditionalDescribe(isTimezoneSet(Timezone.UTC) && env === 'TEST')('createDateXA
   it('should create the x-axis labels correctly for a specific number of ticks', () => {
     const xAxisParams = createXAxisParams({ domainNRangeValues, xAxisCount: 12 });
     const result = utils.createDateXAxis(xAxisParams, {});
-    matchResult(convertXAxisResultToJson(result, false, xAxisParams.xAxisCount));
+    matchResult(convertXAxisResultToJson(result, false));
   });
 
   it('should create the x-axis labels correctly when customDateTimeFormatter is provided', () => {
@@ -265,6 +270,15 @@ conditionalDescribe(isTimezoneSet(Timezone.UTC) && env === 'TEST')('createDateXA
     });
     expect(xAxisParams.xAxisElement).toMatchSnapshot();
   });
+
+  it('should render fewer x-axis labels when hideTickOverlap is true and container width is small', () => {
+    const xAxisParams = createXAxisParams({
+      domainNRangeValues: { ...domainNRangeValues, rEndValue: 50 },
+      hideTickOverlap: true,
+    });
+    const result = utils.createDateXAxis(xAxisParams, {});
+    matchResult(convertXAxisResultToJson(result));
+  });
 });
 
 describe('createStringXAxis', () => {
@@ -294,6 +308,12 @@ describe('createStringXAxis', () => {
   // before the first band (bar) and after the last band (bar).
   it('should create the x-axis labels correctly for specific inner and outer padding values', () => {
     const xAxisParams = createXAxisParams({ xAxisInnerPadding: 0.5, xAxisOuterPadding: 1 / 3 });
+    const result = utils.createStringXAxis(xAxisParams, {}, dataset);
+    matchResult(convertXAxisResultToJson(result, true));
+  });
+
+  it('should render fewer x-axis labels when hideTickOverlap is true and container width is small', () => {
+    const xAxisParams = createXAxisParams({ containerWidth: 50, hideTickOverlap: true });
     const result = utils.createStringXAxis(xAxisParams, {}, dataset);
     matchResult(convertXAxisResultToJson(result, true));
   });
