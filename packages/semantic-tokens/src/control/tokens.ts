13424bcbd59af7cb52560e8edbf44fea249f0ebb
Mitch-At-Work <mifraser@microsoft.com>
2025-02-12 13:33:56 -0800

    Add initial Link tokens as example

--- packages/semantic-tokens/src/control/tokens.ts
+++ packages/semantic-tokens/src/control/tokens.ts
@@ -1,3 +1,20 @@
-import { tokens } from '@fluentui/react-theme';
+/*
+ * Control level tokens are generic tokens that can be used across multiple components.
+ *
+ * Components with a different fluent 2 fallback than the control should define a component specific token instead.
+ */
 
-export const backgroundCtrlBrandRest = `var(--background-ctrl-brand-rest,${tokens.colorBrandBackground}))`;
+import { tokens } from '@fluentui/tokens';
+
+// Text
+export const textStyleDefaultRegularFontfamily = `var(--text-style-default-regular-fontfamily, ${tokens.fontFamilyBase})`;
+export const textGlobalBody3Fontsize = `var(--text-global-body3-fontsize, ${tokens.fontSizeBase300})`;
+export const textStyleDefaultRegularWeight = `var(--text-style-default-regular-weight, ${tokens.fontWeightRegular})`;
+
+// Stroke
+export const strokewidthDefault = `var(--strokewidth-default, ${tokens.strokeWidthThin})`;
+export const ctrlFocusOuterStroke = `var(--ctrl-focus-outer-stroke, ${tokens.colorStrokeFocus2})`;
+
+// Disabled
+export const foregroundCtrlOntransparentDisabled = `var(--foreground-ctrl-ontransparent-disabled, ${tokens.colorNeutralForegroundDisabled})`;
+export const foregroundCtrlNeutralPrimaryDisabled = `var(--foreground-ctrl-neutral-primary-disabled, ${tokens.colorNeutralForegroundDisabled})`;
