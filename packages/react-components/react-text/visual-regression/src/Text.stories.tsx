9aa178ca51f2872306f6e44c817fc52a09cf2f02
Martin Hochel <martinhochel@microsoft.com>
2025-02-24 16:41:44 +0100

    feat(react-text-vr): add proper deps and sb config for VR

--- packages/react-components/react-text/visual-regression/src/Text.stories.tsx
+++ packages/react-components/react-text/visual-regression/src/Text.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { Meta } from '@storybook/react';
+import { type Meta } from '@storybook/react';
 import { Steps, type StoryParameters } from 'storywright';
 import {
   Body1,
