1115b9326cc915802278305d826b345d318c16f2
krkshitij <110246001+krkshitij@users.noreply.github.com>
2025-02-25 00:53:45 +0530

    feat(react-charting): make charts responsive (#33723)

--- packages/react-examples/src/react-charting/DonutChart/DonutChart.doc.tsx
+++ packages/react-examples/src/react-charting/DonutChart/DonutChart.doc.tsx
@@ -6,6 +6,7 @@ import { DonutChartBasicExample } from './DonutChart.Basic.Example';
 import { DonutChartDynamicExample } from './DonutChart.Dynamic.Example';
 import { DonutChartCustomCalloutExample } from './DonutChart.CustomCallout.Example';
 import { DonutChartCustomAccessibilityExample } from './DonutChart.CustomAccessibility.Example';
+import { DonutChartResponsiveExample } from './DonutChart.Responsive.Example';
 
 const DonutChartBasicExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/DonutChart/DonutChart.Basic.Example.tsx') as string;
@@ -15,6 +16,8 @@ const DonutChartCustomCalloutExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/DonutChart/DonutChart.CustomCallout.Example.tsx') as string;
 const DonutChartCustomAccessibilityExampleCode =
   require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/DonutChart/DonutChart.CustomAccessibility.Example.tsx') as string;
+const DonutChartResponsiveExampleCode =
+  require('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/DonutChart/DonutChart.Responsive.Example.tsx') as string;
 
 export const DonutChartPageProps: IDocPageProps = {
   title: 'DonutChart',
@@ -41,6 +44,11 @@ export const DonutChartPageProps: IDocPageProps = {
       title: 'DonutChart Custom Accessibility',
       code: DonutChartCustomAccessibilityExampleCode,
       view: <DonutChartCustomAccessibilityExample />,
+    },
+    {
+      title: 'DonutChart Responsive',
+      code: DonutChartResponsiveExampleCode,
+      view: <DonutChartResponsiveExample />,
     },
   ],
   overview: require<string>('!raw-loader?esModule=false!@fluentui/react-examples/src/react-charting/DonutChart/docs/DonutChartOverview.md'),
