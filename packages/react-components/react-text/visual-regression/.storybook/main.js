2655f1f36d846a117d96d41dfaa575d49bb8a860
Martin Hochel <martinhochel@microsoft.com>
2025-02-24 16:41:44 +0100

    feat(react-text-vr): add proper deps and sb config for VR

--- packages/react-components/react-text/visual-regression/.storybook/main.js
+++ packages/react-components/react-text/visual-regression/.storybook/main.js
@@ -1,14 +1,54 @@
-const rootMain = require('../../../../../.storybook/main');
+const path = require('path');
 
-module.exports = /** @type {Omit<import('../../../../../.storybook/main'), 'typescript'|'babel'>} */ ({
-  ...rootMain,
-  stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/index.stories.@(ts|tsx)'],
-  addons: [...rootMain.addons],
-  webpackFinal: (config, options) => {
-    const localConfig = { ...rootMain.webpackFinal(config, options) };
+const { registerTsPaths, registerRules, rules, loadWorkspaceAddon } = require('@fluentui/scripts-storybook');
+const tsConfigPath = path.resolve(__dirname, '../../../../../tsconfig.base.json');
 
-    // add your own webpack tweaks if needed
+module.exports = /** @type {import('@storybook/react-webpack5').StorybookConfig} */ ({
+  addons: [loadWorkspaceAddon('@fluentui/react-storybook-addon', { tsConfigPath })],
+  stories: ['../src/**/*.stories.tsx'],
+  core: {
+    disableTelemetry: true,
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: {
+        useSWC: true,
+        lazyCompilation: false,
+      },
+    },
+  },
+  typescript: {
+    // disable react-docgen-typescript (totally not needed here, slows things down a lot)
+    reactDocgen: false,
+  },
+  webpackFinal: config => {
+    registerTsPaths({ config, configFile: tsConfigPath });
+    registerRules({ config, rules: [rules.griffelRule] });
 
-    return localConfig;
+    return config;
+  },
+  swc() {
+    return {
+      jsc: {
+        target: 'es2019',
+        parser: {
+          syntax: 'typescript',
+          tsx: true,
+          decorators: true,
+          dynamicImport: true,
+        },
+        transform: {
+          decoratorMetadata: true,
+          legacyDecorator: true,
+        },
+        keepClassNames: true,
+        externalHelpers: true,
+        loose: true,
+        minify: {
+          mangle: false,
+        },
+      },
+    };
   },
 });
--- packages/react-components/react-text/visual-regression/.storybook/preview.js
+++ packages/react-components/react-text/visual-regression/.storybook/preview.js
@@ -1,7 +1,8 @@
-import * as rootPreview from '../../../../../.storybook/preview';
-
-/** @type {typeof rootPreview.decorators} */
-export const decorators = [...rootPreview.decorators];
-
-/** @type {typeof rootPreview.parameters} */
-export const parameters = { ...rootPreview.parameters };
+/** @type {import("@fluentui/react-storybook-addon").FluentParameters} */
+export const parameters = {
+  layout: 'none',
+  mode: 'vr-test',
+  reactStorybookAddon: {
+    disabledDecorators: ['AriaLive'],
+  },
+};
