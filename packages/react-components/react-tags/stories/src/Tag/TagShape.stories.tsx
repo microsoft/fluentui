14f67a7c86768db765119246732d08622e9fe7d4
Sarah Higley <smhigley@users.noreply.github.com>
2025-02-26 10:46:42 -0800

    docs: update Tag examples to reflow (#33905)

--- packages/react-components/react-tags/stories/src/Tag/TagShape.stories.tsx
+++ packages/react-components/react-tags/stories/src/Tag/TagShape.stories.tsx
@@ -3,40 +3,49 @@ import { Tag, Avatar, makeStyles } from '@fluentui/react-components';
 import { CalendarMonthRegular } from '@fluentui/react-icons';
 
 const useContainerStyles = makeStyles({
-  root: {
-    display: 'grid',
-    rowGap: '10px',
+  innerWrapper: {
+    alignItems: 'start',
     columnGap: '10px',
-    gridTemplateColumns: 'auto 1fr',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  outerWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: '10px',
   },
 });
 
 export const Shape = () => {
   const containerStyles = useContainerStyles();
   return (
-    <div className={containerStyles.root}>
-      <Tag media={<Avatar name="Katri Athokas" badge={{ status: 'busy' }} />}>Rounded</Tag>
-      <Tag shape="circular" media={<Avatar name="Katri Athokas" badge={{ status: 'busy' }} />}>
-        Circular
-      </Tag>
+    <div className={containerStyles.outerWrapper}>
+      <div className={containerStyles.innerWrapper}>
+        <Tag media={<Avatar name="Katri Athokas" badge={{ status: 'busy' }} />}>Rounded</Tag>
+        <Tag shape="circular" media={<Avatar name="Katri Athokas" badge={{ status: 'busy' }} />}>
+          Circular
+        </Tag>
+      </div>
 
-      <Tag
-        dismissible
-        dismissIcon={{ 'aria-label': 'remove' }}
-        icon={<CalendarMonthRegular />}
-        secondaryText="Secondary text"
-      >
-        Rounded
-      </Tag>
-      <Tag
-        shape="circular"
-        dismissible
-        dismissIcon={{ 'aria-label': 'remove' }}
-        icon={<CalendarMonthRegular />}
-        secondaryText="Secondary text"
-      >
-        Circular
-      </Tag>
+      <div className={containerStyles.innerWrapper}>
+        <Tag
+          dismissible
+          dismissIcon={{ 'aria-label': 'remove' }}
+          icon={<CalendarMonthRegular />}
+          secondaryText="Secondary text"
+        >
+          Rounded
+        </Tag>
+        <Tag
+          shape="circular"
+          dismissible
+          dismissIcon={{ 'aria-label': 'remove' }}
+          icon={<CalendarMonthRegular />}
+          secondaryText="Secondary text"
+        >
+          Circular
+        </Tag>
+      </div>
     </div>
   );
 };
