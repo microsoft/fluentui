fe81a61b4a269c534d5f154a05f094f6a6d89687
terynkum <terynkum@microsoft.com>
2025-02-25 16:57:45 -0800

    added custom style hooks for Card components

--- packages/react-components/react-card/library/src/components/CardPreview/CardPreview.tsx
+++ packages/react-components/react-card/library/src/components/CardPreview/CardPreview.tsx
@@ -4,6 +4,7 @@ import { renderCardPreview_unstable } from './renderCardPreview';
 import { useCardPreviewStyles_unstable } from './useCardPreviewStyles.styles';
 import type { CardPreviewProps } from './CardPreview.types';
 import type { ForwardRefComponent } from '@fluentui/react-utilities';
+import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * Component to render image previews of documents or articles in a Card component.
@@ -12,6 +13,9 @@ export const CardPreview: ForwardRefComponent<CardPreviewProps> = React.forwardR
   const state = useCardPreview_unstable(props, ref);
 
   useCardPreviewStyles_unstable(state);
+
+  useCustomStyleHook_unstable('useCardPreviewStyles_unstable')(state);
+
   return renderCardPreview_unstable(state);
 });
 
