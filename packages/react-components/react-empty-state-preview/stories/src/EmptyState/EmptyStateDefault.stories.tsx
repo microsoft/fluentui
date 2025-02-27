ee0958ca55298651e15c5f8e1683e4757808be7c
Julien San Diego <aisandie@microsoft.com>
2025-02-27 09:40:52 -0800

    wip - empty state component

---
+++ packages/react-components/react-empty-state-preview/stories/src/EmptyState/EmptyStateDefault.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { EmptyState } from '../../../library/src'; // @fluentui/react-empty-state-preview?
+
+export const EmptyStateDefault = () => <EmptyState title="No data" />;
