647047879c39e3ad761bb0cf3a97dfa98d80ca02
Julien San Diego <aisandie@microsoft.com>
2025-02-27 09:40:52 -0800

    wip - empty state component

---
+++ packages/react-components/react-empty-state-preview/library/src/components/EmptyState/EmptyState.types.ts
@@ -0,0 +1,18 @@
+import type { Slot } from '@fluentui/react-utilities';
+import { ComponentProps, ComponentState } from '@fluentui/react-utilities/src/index';
+
+export type EmptyStateSlots = {
+  /**
+   * Root element of the component.
+   */
+  root: Slot<'div'>;
+
+  /**
+   * Element used to render the main header title.
+   */
+  title: Slot<'span'>;
+};
+
+export type EmptyStateProps = ComponentProps<EmptyStateSlots> & {};
+
+export type EmptyStateState = ComponentState<EmptyStateSlots> & {};
---
+++ packages/react-components/react-empty-state-preview/library/src/index.ts
@@ -0,0 +1,2 @@
+export { EmptyState, renderEmptyState, useEmptyState } from './EmptyState';
+export type { EmptyStateProps, EmptyStateSlots, EmptyStateState } from './EmptyState';
