ef0cad7f290ac8d313b043416351f30d122050c0
mainframev <vgenaev@gmail.com>
2025-02-28 14:57:52 +0100

    fix(react-combobox): use correct tokens for hover, active and focus states
    fix(react-dropdown): use correct tokens for hover, active and focus states

--- packages/react-components/react-combobox/library/src/components/Dropdown/useDropdownStyles.styles.ts
+++ packages/react-components/react-combobox/library/src/components/Dropdown/useDropdownStyles.styles.ts
@@ -149,12 +149,12 @@ const useStyles = makeStyles({
   outlineInteractive: {
     '&:hover': {
       ...shorthands.borderColor(tokens.colorNeutralStroke1Hover),
-      borderBottomColor: tokens.colorNeutralStrokeAccessible,
+      borderBottomColor: tokens.colorNeutralStrokeAccessibleHover,
     },
 
-    '&:active': {
+    '&:active, &:focus-within': {
       ...shorthands.borderColor(tokens.colorNeutralStroke1Pressed),
-      borderBottomColor: tokens.colorNeutralStrokeAccessible,
+      borderBottomColor: tokens.colorNeutralStrokeAccessiblePressed,
     },
   },
   underline: {
