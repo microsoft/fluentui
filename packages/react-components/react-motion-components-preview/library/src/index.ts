ecb0d6fac21b8eb1827c60ca3205f74c1d1f73b9
Robert Penner <robertpenner@microsoft.com>
2025-03-04 04:24:24 +0000

    feat(motion): add Rotate presence component

--- packages/react-components/react-motion-components-preview/library/src/index.ts
+++ packages/react-components/react-motion-components-preview/library/src/index.ts
@@ -10,3 +10,4 @@ export type { CollapseRuntimeParams } from './components/Collapse';
 export { Fade, FadeSnappy, FadeRelaxed } from './components/Fade';
 export { Scale, ScaleSnappy, ScaleRelaxed } from './components/Scale';
 export { Slide, SlideSnappy, SlideRelaxed, createSlidePresence } from './components/Slide';
+export { Rotate } from './components/Rotate';
