bbc41bf31172db3390cdc44bb437b1f8c3a2860a
Robert Penner <robertpenner@microsoft.com>
2025-03-06 05:09:35 +0000

    chore(motion): delete old code for Series

--- packages/react-components/react-motion-components-preview/stories/src/Experiments/ExperimentsSeries.stories.tsx
+++ packages/react-components/react-motion-components-preview/stories/src/Experiments/ExperimentsSeries.stories.tsx
@@ -77,28 +77,7 @@ const LoremIpsum = () => (
   </>
 );
 
-type MotionComponentDef = {
-  fn: Function;
-  props: Record<string, any>;
-};
-
 // A Series is a component that accepts an array of motion components and plays them in sequence
-const Series: React.FC<{ components: MotionComponentDef[] }> = ({ components }) => {
-  // const [currentComponent, setCurrentComponent] = React.useState(components[0]);
-  const [index, setIndex] = React.useState(1);
-
-  const currentComponent = components[index];
-  const onMotionFinish = () => {
-    if (index < components.length - 1) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
-  };
-
-  return <currentComponent.fn {...currentComponent.props} onMotionFinish={onMotionFinish} />;
-};
-
 const SeriesB: React.FC<{ components: React.ReactElement[] }> = ({ components }) => {
   const [index, setIndex] = React.useState(0);
 
@@ -153,118 +132,6 @@ const componentsB = ({ animateOpacity = true }): React.ReactElement[] => [
   </Slide.Out>,
 ];
 
-const componentsA = ({ animateOpacity = true }): MotionComponentDef[] => [
-  {
-    fn: FadeRelaxed.In,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'red', width: '100px', height: '100px', borderRadius: '50%' }} />,
-    },
-  },
-  {
-    fn: FadeRelaxed.Out,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'red', width: '100px', height: '100px', borderRadius: '50%' }} />,
-    },
-  },
-
-  {
-    fn: ScaleRelaxed.In,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }} />,
-    },
-  },
-  {
-    fn: ScaleRelaxed.Out,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }} />,
-    },
-  },
-
-  {
-    fn: Collapse.In,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'purple', width: '100px', height: '100px', borderRadius: '50%' }} />,
-    },
-  },
-  {
-    fn: Collapse.Out,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'purple', width: '100px', height: '100px', borderRadius: '50%' }} />,
-    },
-  },
-
-  {
-    fn: Blur.In,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'orange', width: '100px', height: '100px' }} />,
-    },
-  },
-  {
-    fn: Blur.Out,
-    props: {
-      duration: 5000,
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'orange', width: '100px', height: '100px' }} />,
-    },
-  },
-
-  {
-    fn: Slide.In,
-    props: {
-      duration: 5000,
-      distance: '100%',
-      orientation: 'vertical',
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'darkgreen', width: '100px', height: '100px' }} />,
-    },
-  },
-  {
-    fn: Slide.Out,
-    props: {
-      duration: 5000,
-      distance: '100%',
-      orientation: 'vertical',
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'darkgreen', width: '100px', height: '100px' }} />,
-    },
-  },
-
-  {
-    fn: Slide.In,
-    props: {
-      duration: 5000,
-      distance: '100%',
-      orientation: 'horizontal',
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'darkRed', width: '100px', height: '100px' }} />,
-    },
-  },
-  {
-    fn: Slide.Out,
-    props: {
-      duration: 5000,
-      distance: '100%',
-      orientation: 'horizontal',
-      animateOpacity,
-      children: <div style={{ backgroundColor: 'darkRed', width: '100px', height: '100px' }} />,
-    },
-  },
-];
-
 export const ExperimentsSeries = () => {
   const classes = useClasses();
   const [visible, setVisible] = React.useState<boolean>(false);
@@ -299,7 +166,6 @@ export const ExperimentsSeries = () => {
         </Field>
       </div>
 
-      {/* <div className={classes.card}>{seriesA}</div> */}
       <div className={classes.card}>{seriesB}</div>
     </div>
   );
