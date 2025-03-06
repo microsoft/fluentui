1dbb6b6c82bfe182cb4285b2556ad50101874890
Robert Penner <robertpenner@microsoft.com>
2025-03-06 06:58:22 +0000

    refactor(motion): move Series to own file & use with Stagger

--- packages/react-components/react-motion-components-preview/stories/src/Experiments/ExperimentsSeries.stories.tsx
+++ packages/react-components/react-motion-components-preview/stories/src/Experiments/ExperimentsSeries.stories.tsx
@@ -16,6 +16,7 @@ import { slideAtom } from '../../../library/src/atoms/slide-atom';
 import { PresenceMotionFnCreator } from '../../../library/src/types';
 import { MotionComponentProps, PresenceComponent } from '@fluentui/react-motion/src/index';
 import { Blur, Collapse, Fade, FadeRelaxed, ScaleRelaxed, Slide } from '@fluentui/react-motion-components-preview';
+import { Series } from './Series';
 
 // import description from './ExperimentsWipe.stories.md';
 
@@ -76,38 +77,6 @@ const LoremIpsum = () => (
     )}
   </>
 );
-
-const isFragment = (child: React.ReactNode): child is React.ReactElement => {
-  return React.isValidElement(child) && child.type === React.Fragment;
-};
-
-// Convert children that is either React.Fragment or regular React.Children to an array of React elements
-const childrenOrFragmentToArray = (children: React.ReactNode): React.ReactElement[] => {
-  if (isFragment(children)) {
-    return React.Children.toArray(children.props.children) as React.ReactElement[];
-  }
-  return React.Children.toArray(children) as React.ReactElement[];
-};
-
-// A Series is a component that accepts an array of motion components and plays them in sequence
-// TODO: add a prop to control the autoplay
-// TODO: add onMotionFinish callback to support nesting of Series
-// TODO: add a Pause component to pause the series for a duration
-const Series: React.FC = ({ children }) => {
-  const [index, setIndex] = React.useState(0);
-
-  const components = childrenOrFragmentToArray(children);
-  const currentComponent = components[index];
-  const onMotionFinish = () => {
-    if (index < components.length - 1) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
-  };
-
-  return React.cloneElement(currentComponent, { onMotionFinish });
-};
 
 export const ExperimentsSeries = () => {
   const classes = useClasses();
