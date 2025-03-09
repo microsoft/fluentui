91a3e6b32b4bf7496dd3a10662e0da2a2d0ac3b6
The web walker <contact@ipoh.dev>
2025-03-10 00:52:14 +0800

    fix: export AnchorButtonStyles in its index.ts
    Exporting missing AnchorButtonStyles to make it consistent with other components.

--- packages/web-components/src/anchor-button/index.ts
+++ packages/web-components/src/anchor-button/index.ts
@@ -1,5 +1,6 @@
 export { definition as AnchorButtonDefinition } from './anchor-button.definition.js';
-export { BaseAnchor, AnchorButton } from './anchor-button.js';
+export { AnchorButton, BaseAnchor } from './anchor-button.js';
 export { AnchorButtonAppearance, AnchorButtonShape, AnchorButtonSize, AnchorTarget } from './anchor-button.options.js';
 export type { AnchorButtonOptions } from './anchor-button.options.js';
+export { styles as AnchorButtonStyles } from './anchor-button.styles.js';
 export { template as AnchorButtonTemplate } from './anchor-button.template.js';
