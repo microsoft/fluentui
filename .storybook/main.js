f31ab9cf98096c7da8147115d2dcea65cd675a3f
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-03-13 12:02:01 +0100

    fix type-check issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 const {
-  loadWorkspaceAddon,
+  // loadWorkspaceAddon,
   registerTsPaths,
   processBabelLoaderOptions,
   getImportMappingsForExportToSandboxAddon,
