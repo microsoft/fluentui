5bedb27b9fb335d2df58f7159259f3eadef635b0
Robert Penner <robertpenner@microsoft.com>
2025-03-18 17:55:46 +0000

    chore(motion): remove old code: createFadePresence, createScalePresence

--- packages/react-components/react-motion-components-preview/library/src/index.ts
+++ packages/react-components/react-motion-components-preview/library/src/index.ts
@@ -7,8 +7,8 @@ export {
   createCollapseDelayedPresence,
 } from './components/Collapse';
 export type { CollapseRuntimeParams } from './components/Collapse';
-export { Fade, FadeSnappy, FadeRelaxed, createFadePresence } from './components/Fade';
-export { Scale, ScaleSnappy, ScaleRelaxed, createScalePresence } from './components/Scale';
+export { Fade, FadeSnappy, FadeRelaxed } from './components/Fade';
+export { Scale, ScaleSnappy, ScaleRelaxed } from './components/Scale';
 export { Slide, SlideSnappy, SlideRelaxed, createSlidePresence } from './components/Slide';
 export { Rotate } from './components/Rotate';
 export { Blur } from './components/Blur';
