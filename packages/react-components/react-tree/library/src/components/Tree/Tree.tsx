4b46277d231e41dd56b7d97c0ad607a3f9e979d8
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-03-19 17:18:43 +0100

    fix import

--- packages/react-components/react-tree/library/src/components/Tree/Tree.tsx
+++ packages/react-components/react-tree/library/src/components/Tree/Tree.tsx
@@ -5,7 +5,7 @@ import type { TreeProps } from './Tree.types';
 import { useTreeContextValues_unstable } from './useTreeContextValues';
 import { useTreeStyles_unstable } from './useTreeStyles.styles';
 import { renderTree_unstable } from './renderTree';
-import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts/src/index';
+import { useCustomStyleHook_unstable } from '@fluentui/react-shared-contexts';
 
 /**
  * The `Tree` component renders nested items in a hierarchical structure.
