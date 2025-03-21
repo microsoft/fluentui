8f03d3c1b35c12ce8a96d4ac8caa67de662eb322
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-03-21 13:38:21 +0530

    removed  BUG 9821460 related chnages from this PR

--- packages/charts/react-charting/src/components/AreaChart/AreaChart.base.tsx
+++ packages/charts/react-charting/src/components/AreaChart/AreaChart.base.tsx
@@ -1022,7 +1022,7 @@ export class AreaChartBase extends React.Component<IAreaChartProps, IAreaChartSt
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private _getFilteredLegendValues = (values: any) => {
-    return !this._noLegendHighlighted()
+    !this._noLegendHighlighted()
       ? values.filter((value: { legend: string }) => this._legendHighlighted(value.legend))
       : values;
   };
