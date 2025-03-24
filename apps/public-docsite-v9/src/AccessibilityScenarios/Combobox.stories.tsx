7b5c94aa2a36543abdf41e9b969626e1c1146127
Juraj Kapsiar <jurokapsiar@gmail.com>
2025-03-24 20:32:53 +0000

    examples

---
+++ apps/public-docsite-v9/src/AccessibilityScenarios/Combobox.stories.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import { Combobox, Option, makeStyles, useId } from '@fluentui/react-components';
+import { Scenario } from './utils';
+
+const useStyles = makeStyles({
+  root: {
+    // Stack the label above the field with a gap
+    display: 'grid',
+    gridTemplateRows: 'repeat(1fr)',
+    justifyItems: 'start',
+    gap: '2px',
+    maxWidth: '400px',
+  },
+});
+
+const options = [
+  'Apple',
+  'Apricot',
+  'Avocado',
+  'Banana',
+  'Blackberry',
+  'Blueberry',
+  'Boysenberry',
+  'Cantaloupe',
+  'Coconut',
+  'Currant',
+  'Date',
+  'Dragon fruit',
+  'Fig',
+  'Grape',
+  'Grapefruit',
+  'Honeydew',
+  'Kiwi',
+  'Lemon',
+  'Lime',
+  'Lychee',
+  'Mango',
+  'Mandarin orange',
+  'Melon',
+  'Nectarine',
+  'Olive',
+  'Orange',
+  'Papaya',
+  'Passion Fruit',
+  'Peach',
+  'Pear',
+  'Persimmon',
+  'Pineapple',
+  'Plum',
+  'Pomegranate',
+  'Raspberry',
+  'Ramnutan',
+  'Strawberry',
+  'Tangerine',
+  'Watermelon',
+];
+
+export const FilteringCombobox: React.FunctionComponent = () => {
+  const comboId = useId();
+  const styles = useStyles();
+
+  return (
+    <Scenario pageTitle="Filtering combobox">
+      <div>Dual state</div>
+      <div className={styles.root}>
+        <label id={comboId}>Favourite fruit to have with ice-cream</label>
+        <Combobox aria-labelledby={comboId} placeholder="Pick a fruit" positioning={{ autoSize: true }}>
+          {options.map(option => (
+            <Option key={option} disabled={option === 'Ferret'}>
+              {option}
+            </Option>
+          ))}
+        </Combobox>
+      </div>
+    </Scenario>
+  );
+};
--- apps/public-docsite-v9/src/AccessibilityScenarios/Menu.stories.tsx
+++ apps/public-docsite-v9/src/AccessibilityScenarios/Menu.stories.tsx
@@ -1,99 +1,71 @@
 import * as React from 'react';
 
-import {
-  Menu,
-  MenuButton,
-  MenuGroup,
-  MenuGroupHeader,
-  MenuItem,
-  MenuItemCheckbox,
-  MenuItemRadio,
-  MenuList,
-  MenuPopover,
-  MenuTrigger,
-} from '@fluentui/react-components';
+import { Menu, MenuButton, MenuItem, MenuList, MenuPopover, MenuTrigger } from '@fluentui/react-components';
 
 import { Scenario } from './utils';
 
-interface StatusSubmenuProps {
-  checkedValues: Record<string, string[]>;
-  onChange: OnCheckedValueChangeCallback;
-}
-
-const StatusSubmenu: React.FunctionComponent<StatusSubmenuProps> = props => {
-  const { checkedValues, onChange } = props;
-
-  return (
-    <Menu checkedValues={checkedValues} onCheckedValueChange={onChange}>
-      <MenuTrigger disableButtonEnhancement>
-        <MenuItem>Status</MenuItem>
-      </MenuTrigger>
-
-      <MenuPopover>
-        <MenuList>
-          <MenuItemRadio name="status" value="online">
-            Online
-          </MenuItemRadio>
-          <MenuItemRadio name="status" value="away">
-            Away
-          </MenuItemRadio>
-          <MenuItemRadio name="status" value="offline" disabled>
-            Offline
-          </MenuItemRadio>
-        </MenuList>
-      </MenuPopover>
-    </Menu>
-  );
-};
-
-type OnCheckedValueChangeDataType = {
-  name: string;
-  checkedItems: string[];
-};
-
-type OnCheckedValueChangeCallback = (
-  event: React.MouseEvent | React.KeyboardEvent,
-  data: OnCheckedValueChangeDataType,
-) => void;
-
-type ProfileMenuStatus = { status: Array<'online' | 'away' | 'offline'> };
-
 export const ProfileMenu: React.FunctionComponent = () => {
-  const [statusCheckedValues, setStatusCheckedValues] = React.useState<ProfileMenuStatus>({ status: ['online'] });
-  const onStatusChange = (
-    event: React.MouseEvent | React.KeyboardEvent,
-    { name, checkedItems }: OnCheckedValueChangeDataType,
-  ) => {
-    setStatusCheckedValues(state => ({ ...state, [name]: checkedItems }));
-  };
-
   return (
     <Scenario pageTitle="Profile menu">
-      <Menu>
+      <div>Dual state</div>
+      <Menu positioning={{ autoSize: true }}>
         <MenuTrigger disableButtonEnhancement>
-          <MenuButton>Profile - dual state</MenuButton>
+          <MenuButton>Pick a fruit</MenuButton>
         </MenuTrigger>
 
         <MenuPopover>
           <MenuList>
-            <MenuGroup>
-              <MenuGroupHeader>Information</MenuGroupHeader>
-              <MenuItem>Help</MenuItem>
-            </MenuGroup>
-            <MenuGroup>
-              <MenuGroupHeader>Settings</MenuGroupHeader>
-              <MenuItemCheckbox name="settings" value="runAtStartup">
-                Run at startup
-              </MenuItemCheckbox>
-              <MenuItemCheckbox name="settings" value="showNotifications">
-                Show notifications
-              </MenuItemCheckbox>
-            </MenuGroup>
-            <MenuGroup>
-              <MenuGroupHeader>Account</MenuGroupHeader>
-              <StatusSubmenu checkedValues={statusCheckedValues} onChange={onStatusChange} />
-              <MenuItem>Logout</MenuItem>
-            </MenuGroup>
+            <MenuItem>Apple</MenuItem>
+            <MenuItem>Apricot</MenuItem>
+            <MenuItem>Avocado</MenuItem>
+            <MenuItem>Banana</MenuItem>
+
+            <Menu>
+              <MenuTrigger disableButtonEnhancement>
+                <MenuItem>Berries</MenuItem>
+              </MenuTrigger>
+
+              <MenuPopover>
+                <MenuList>
+                  <MenuItem>Blackberry</MenuItem>
+                  <MenuItem>Boysenberry</MenuItem>
+                  <MenuItem>Blueberry</MenuItem>
+                  <MenuItem>Strawberry</MenuItem>
+                  <MenuItem>Rapsberry</MenuItem>
+                  <MenuItem>Barry White</MenuItem>
+                </MenuList>
+              </MenuPopover>
+            </Menu>
+
+            <MenuItem>Cantaloupe</MenuItem>
+            <MenuItem>Coconut</MenuItem>
+            <MenuItem>Currant</MenuItem>
+            <MenuItem>Dragon Fruit</MenuItem>
+            <MenuItem>Grape</MenuItem>
+            <MenuItem>Grapefruit</MenuItem>
+            <MenuItem>Honeydew</MenuItem>
+            <MenuItem>Kiwi</MenuItem>
+            <MenuItem>Lemon</MenuItem>
+            <MenuItem>Lime</MenuItem>
+            <MenuItem>Lychee</MenuItem>
+            <MenuItem>Mango</MenuItem>
+            <MenuItem>Mandarin Orange</MenuItem>
+            <MenuItem>Melon</MenuItem>
+            <MenuItem>Nectarine</MenuItem>
+            <MenuItem>Olive</MenuItem>
+            <MenuItem>Orange</MenuItem>
+            <MenuItem>Papaya</MenuItem>
+            <MenuItem>Passion Fruit</MenuItem>
+            <MenuItem>Peach</MenuItem>
+            <MenuItem>Pear</MenuItem>
+            <MenuItem>Persimmon</MenuItem>
+            <MenuItem>Pineapple</MenuItem>
+            <MenuItem>Plum</MenuItem>
+            <MenuItem>Pomegranate</MenuItem>
+            <MenuItem>Raspberry</MenuItem>
+            <MenuItem>Rambutan</MenuItem>
+            <MenuItem>Strawberry</MenuItem>
+            <MenuItem>Watermelon</MenuItem>
           </MenuList>
         </MenuPopover>
       </Menu>
