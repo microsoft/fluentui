8e2b097fed29a830d1e960cd8d889f84b9c22db5
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-03-24 03:15:59 +0000

    Update package name

--- packages/charts/react-charts/stories/src/HorizontalBarChart/HorizontalBarChartStacked.stories.tsx
+++ packages/charts/react-charts/stories/src/HorizontalBarChart/HorizontalBarChartStacked.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HorizontalBarChart, getColorFromToken, DataVizPalette } from '@fluentui/react-charts-preview';
+import { HorizontalBarChart, getColorFromToken, DataVizPalette } from '@fluentui/react-charts';
 
 export const HorizontalBarStacked = () => {
   const data = [
