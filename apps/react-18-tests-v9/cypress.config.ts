98b32d43038a899523bd26064042bb16268c2d23
Marcos Moura <marcosvmmoura@gmail.com>
2025-03-25 21:43:23 +0100

    fix: separate array to better document file

--- apps/react-18-tests-v9/cypress.config.ts
+++ apps/react-18-tests-v9/cypress.config.ts
@@ -1,16 +1,20 @@
 import * as path from 'path';
 import { baseConfig } from '@fluentui/scripts-cypress';
 
-const specs = [
-  path.resolve('../../apps/react-18-tests-v9/src/**/*.cy.{tsx,ts}'),
-  path.resolve('../../packages/react-components/**/library/src/**/*.cy.{tsx,ts}'),
-
-  // Exclude files that are not compatible with React 18 yet
+// Exclude files that are not compatible with React 18 yet
+const excludedSpecs = [
   '!' + path.resolve('../../packages/react-components/react-overflow/library/src/**/*.cy.{tsx,ts}'),
   '!' + path.resolve('../../packages/react-components/react-tag-picker/library/src/**/*.cy.{tsx,ts}'),
   '!' + path.resolve('../../packages/react-components/react-toolbar/library/src/**/*.cy.{tsx,ts}'),
 ];
 
+// Include all tests from this app and the components package
+const includedSpecs = [
+  path.resolve('../../apps/react-18-tests-v9/src/**/*.cy.{tsx,ts}'),
+  path.resolve('../../packages/react-components/**/library/src/**/*.cy.{tsx,ts}'),
+];
+
+const specs = [...includedSpecs, ...excludedSpecs];
 const config = { ...baseConfig };
 
 config.component.specPattern = specs;
