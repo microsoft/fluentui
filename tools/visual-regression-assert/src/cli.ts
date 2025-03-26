8158f6a795f833913d0dabdaced5be8d0e2c4b94
Martin Hochel <martinhochel@microsoft.com>
2025-03-26 12:42:48 +0100

    feat(vra): add update-baseline cmd for updating local baseline from CI report (#34076)
    Co-authored-by: Victor Genaev <vgenaev@gmail.com>

--- tools/visual-regression-assert/src/cli.ts
+++ tools/visual-regression-assert/src/cli.ts
@@ -2,6 +2,7 @@ import yargs from 'yargs';
 import { runSnapshotTests } from './assert';
 import { prepareReport } from './report';
 import { reporterFileNames } from './shared';
+import { updateBaseline } from './update-baseline';
 
 main().catch(reason => {
   console.error(reason);
@@ -17,16 +18,42 @@ async function main() {
       'report',
       'prepare diff report for CI',
       y => {
-        return y.option('outputPath', {
-          type: 'string',
-          demandOption: true,
-          description: 'relative path all projects report output should be created',
-        });
+        return y
+          .option('outputPath', {
+            type: 'string',
+            demandOption: true,
+            description: 'relative path all projects report output should be created',
+          })
+          .strict(true);
       },
       async argv => {
         const outputPath = argv.outputPath;
 
         return prepareReport(reportFilesGlob, outputPath);
+      },
+    )
+    .command(
+      'update-baseline',
+      'update baseline to actual from assert report',
+      y => {
+        return y
+          .option('reportPath', {
+            type: 'string',
+            demandOption: true,
+            description: 'relative/absolute path to report file generated on CI',
+          })
+          .strict(true);
+      },
+      async argv => {
+        const reportPath = argv.reportPath;
+
+        const result = await updateBaseline(reportPath);
+        if (result.success === false) {
+          console.error('============================');
+          console.error('🚨 Baseline Update failed! 🚨');
+          console.error('============================');
+          process.exit(1);
+        }
       },
     )
     .command(
@@ -44,7 +71,14 @@ async function main() {
             demandOption: true,
             description: 'relative path where report output should be created',
           })
-          .option('updateSnapshots', { type: 'boolean', default: false, alias: 'u' });
+          .option('updateSnapshots', {
+            type: 'boolean',
+            default: false,
+            alias: 'u',
+            description:
+              'NOTE: updating snapshots during test execution is suggested only if you can guarantee that all users and CI are using same OS environment',
+          })
+          .strict(true);
       },
       async argv => {
         const { baselineDir, outputPath, updateSnapshots } = argv;
@@ -55,7 +89,7 @@ async function main() {
           reportFileName,
           updateSnapshots,
         });
-        if (!result.passed) {
+        if (!result.success) {
           console.error('===============================================');
           console.error('🚨 Snapshots changed! Please Review VR Report 🚨');
           console.error('===============================================');
