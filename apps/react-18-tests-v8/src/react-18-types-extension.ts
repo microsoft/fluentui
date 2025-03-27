2738aede281cd3b92b7b65f67bf4ebe95d9ffa11
Martin Hochel <martinhochel@microsoft.com>
2025-03-27 18:10:21 +0100

    revert: react 18 type overrides

--- apps/react-18-tests-v8/src/react-18-types-extension.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/naming-convention */
-import * as React from 'react';
-
-declare module 'react' {
-  interface FunctionComponent<P = {}> {
-    (props: React.PropsWithChildren<P>, context?: any): React.ReactElement<any, any> | null;
-  }
-}
