6a8bc8748cb124dd27e5c54d1f677f75d60a4656
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-04-02 02:59:28 +0000

    Revert unnecessary changes

--- packages/charts/react-charts/stories/src/DeclarativeChart/index.stories.tsx
+++ packages/charts/react-charts/stories/src/DeclarativeChart/index.stories.tsx
@@ -1,5 +1,5 @@
 import { DeclarativeChart } from '@fluentui/react-charts';
-import descriptionMd from './DeclarativeChartOverview.md';
+import descriptionMd from './docs/DeclarativeChartOverview.md';
 
 export { DeclarativeChartBasicExample } from './DeclarativeChartDefault.stories';
 
