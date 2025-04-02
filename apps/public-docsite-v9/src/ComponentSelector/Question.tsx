e6526b7d2a33c0d791797209dd1f62f89b0f1a50
Adam Samec <asamec@microsoft.com>
2025-04-02 15:42:41 +0200

    Extract styles to separate files from component files

--- apps/public-docsite-v9/src/ComponentSelector/Question.tsx
+++ apps/public-docsite-v9/src/ComponentSelector/Question.tsx
@@ -1,68 +1,8 @@
 import * as React from 'react';
-import { Field, Radio, RadioGroup, tokens, makeStyles } from '@fluentui/react-components';
+import { Field, Radio, RadioGroup } from '@fluentui/react-components';
 
+import { useStyles } from './Question.styles';
 import { GroupQuestion } from './ComponentSelector';
-
-const useStyles = makeStyles({
-  secondLevel: { 'margin-left': '30px' },
-  thirdLevel: { 'margin-left': '60px' },
-  forthLevel: { 'margin-left': '90px' },
-  foundMessage: { 'margin-bottom': '10px' },
-  heading: { margin: '30px 0 10px 0' },
-  root: {
-    // Stack the label above the field with a gap
-    display: 'flex',
-    flexWrap: 'wrap',
-    gap: '2px',
-    maxWidth: '1600px',
-  },
-  tagsList: {
-    listStyleType: 'none',
-    marginBottom: tokens.spacingVerticalXXS,
-    marginTop: 0,
-    paddingLeft: 0,
-    display: 'flex',
-    gridGap: tokens.spacingHorizontalXXS,
-  },
-  tooltip: { maxWidth: '500px important!', backgroundColor: 'red' },
-  componentWrapper: {
-    margin: '10px',
-    padding: '10px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    display: 'flex',
-    alignItems: 'center',
-    gap: '10px',
-  },
-  questionsWrapper: {
-    padding: '20px',
-    margin: '20px 0',
-    backgroundColor: 'white',
-    borderRadius: '16px',
-    border: '1px solid var(--colorNeutralStroke1, #e1dfdd)',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-  },
-  questionsLabel: {
-    color: '#ff00ff',
-    fontWeight: tokens.fontWeightBold,
-    marginRight: '8px',
-  },
-  questionsText: {
-    fontWeight: tokens.fontWeightBold,
-    fontSize: tokens.fontSizeBase400,
-  },
-  questionContainer: {
-    display: 'flex',
-  },
-  questionRightSide: {
-    borderLeft: '1px solid #ff00ff',
-    padding: '0 10px',
-  },
-  radioItem: {
-    display: 'flex',
-  },
-  behaviors: { display: 'flex', gap: '10px' },
-});
 
 interface QuestionProps {
   question: GroupQuestion;
--- apps/public-docsite-v9/src/ComponentSelector/SelectionCard.tsx
+++ apps/public-docsite-v9/src/ComponentSelector/SelectionCard.tsx
@@ -1,49 +1,8 @@
 import * as React from 'react';
-import { Caption1, Card, CardHeader, CardPreview, Checkbox, makeStyles, tokens } from '@fluentui/react-components';
+import { Caption1, Card, CardHeader, CardPreview, Checkbox } from '@fluentui/react-components';
 
+import { useStyles } from './SelectionCard.styles';
 import { ComponentsImages } from './ComponentSelector';
-
-const useStyles = makeStyles({
-  main: {
-    gap: '16px',
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '5px 10px 10px 5px',
-  },
-
-  card: {
-    width: '250px',
-    maxWidth: '100%',
-    height: 'fit-content',
-    minHeight: '196px',
-  },
-
-  caption: {
-    color: tokens.colorNeutralForeground3,
-  },
-
-  cardImage: {
-    borderRadius: tokens.borderRadiusXLarge,
-    border: '1px solid var(--colorNeutralStroke1, #e1dfdd)',
-    filter: 'grayscale(100%) brightness(95%)',
-    opacity: 0.5,
-
-    '&:hover': {
-      filter: 'grayscale(0%) brightness(100%)',
-      opacity: 1,
-    },
-  },
-  cardImageSelected: {
-    borderRadius: tokens.borderRadiusXLarge,
-    border: '1px solid var(--colorNeutralStroke1, #e1dfdd)',
-    filter: 'grayscale(0%) brightness(100%)',
-    opacity: 1,
-  },
-
-  grayBackground: {
-    padding: '0 15px 15px',
-  },
-});
 
 interface SelectionCardProps {
   componentsImages: ComponentsImages;
