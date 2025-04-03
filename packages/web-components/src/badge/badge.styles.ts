b9d88f936b437f9ac9fabc69d14338d468ee9ff5
Chris Holt <13071055+chrisdholt@users.noreply.github.com>
2025-04-02 20:04:33 -0700

    update badge styles

--- packages/web-components/src/badge/badge.styles.ts
+++ packages/web-components/src/badge/badge.styles.ts
@@ -9,23 +9,22 @@ import {
   badgeTintStyles,
 } from '../styles/index.js';
 import { borderRadiusMedium, borderRadiusNone, borderRadiusSmall } from '../theme/design-tokens.js';
-import { extraSmallState, roundedState, smallState, squareState, tinyState } from '../styles/states/index.js';
 // why is the border not showing up?
 /** Badge styles
  * @public
  */
 export const styles = css`
-  :host(${squareState}) {
+  :host([shape='square']) {
     border-radius: ${borderRadiusNone};
   }
 
-  :host(${roundedState}) {
+  :host([shape='rounded']) {
     border-radius: ${borderRadiusMedium};
   }
 
-  :host(${roundedState}${tinyState}),
-  :host(${roundedState}${extraSmallState}),
-  :host(${roundedState}${smallState}) {
+  :host([shape='rounded'][size='tiny']),
+  :host([shape='rounded'][size='extra-small']),
+  :host([shape='rounded'][size='small']) {
     border-radius: ${borderRadiusSmall};
   }
 
