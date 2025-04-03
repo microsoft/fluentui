2edb84198d13320479780070ec47e599a5599bf9
Chris Holt <13071055+chrisdholt@users.noreply.github.com>
2025-04-03 13:16:28 -0700

    remove change handlers from checkbox

--- packages/web-components/src/checkbox/checkbox.ts
+++ packages/web-components/src/checkbox/checkbox.ts
@@ -46,17 +46,6 @@ export class Checkbox extends BaseCheckbox {
   public shape?: CheckboxShape;
 
   /**
-   * Applies shape states when the `shape` property changes.
-   *
-   * @param prev - the previous shape value
-   * @param next - the next shape value
-   * @internal
-   */
-  protected shapeChanged(prev: CheckboxShape | undefined, next: CheckboxShape | undefined) {
-    swapStates(this.elementInternals, prev, next, CheckboxShape);
-  }
-
-  /**
    * Indicates the size of the control.
    *
    * @public
@@ -65,17 +54,6 @@ export class Checkbox extends BaseCheckbox {
    */
   @attr
   public size?: CheckboxSize;
-
-  /**
-   * Applies size states when the `size` property changes.
-   *
-   * @param prev - the previous state
-   * @param next - the next state
-   * @internal
-   */
-  protected sizeChanged(prev: CheckboxSize | undefined, next: CheckboxSize | undefined) {
-    swapStates(this.elementInternals, prev, next, CheckboxSize);
-  }
 
   constructor() {
     super();
