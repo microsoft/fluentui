7af6eb40ba3b5109af8816b5613491207578b20f
Chris Holt <13071055+chrisdholt@users.noreply.github.com>
2025-04-03 13:27:45 -0700

    remove change handlers from progress bar

--- packages/web-components/src/progress-bar/progress-bar.ts
+++ packages/web-components/src/progress-bar/progress-bar.ts
@@ -1,5 +1,4 @@
 import { attr } from '@microsoft/fast-element';
-import { swapStates } from '../utils/element-internals.js';
 import { BaseProgressBar } from './progress-bar.base.js';
 import { ProgressBarShape, ProgressBarThickness } from './progress-bar.options.js';
 
@@ -20,28 +19,10 @@ export class ProgressBar extends BaseProgressBar {
   public thickness?: ProgressBarThickness;
 
   /**
-   * Handles changes to thickness attribute custom states
-   * @param prev - the previous state
-   * @param next - the next state
-   */
-  public thicknessChanged(prev: ProgressBarThickness | undefined, next: ProgressBarThickness | undefined) {
-    swapStates(this.elementInternals, prev, next, ProgressBarThickness);
-  }
-
-  /**
    * The shape of the progress bar
    * @public
    * HTML Attribute: `shape`
    */
   @attr
   public shape?: ProgressBarShape;
-
-  /**
-   * Handles changes to shape attribute custom states
-   * @param prev - the previous state
-   * @param next - the next state
-   */
-  public shapeChanged(prev: ProgressBarShape | undefined, next: ProgressBarShape | undefined) {
-    swapStates(this.elementInternals, prev, next, ProgressBarShape);
-  }
 }
