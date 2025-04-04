bde7a93af6572f4afde168a7b5429e125ab1eb2b
Anush Gupta <74965306+Anush2303@users.noreply.github.com>
2025-04-04 21:19:12 +0530

    feat(react-charts): Migrate Area Chart to v9 (#34148)

--- packages/charts/react-charts/library/src/index.ts
+++ packages/charts/react-charts/library/src/index.ts
@@ -14,5 +14,6 @@ export * from './utilities/colors';
 export * from './Popover';
 export * from './ResponsiveContainer';
 export * from './DeclarativeChart';
+export * from './AreaChart';
 export * from './HorizontalBarChartWithAxis';
 export * from './HeatMapChart';
