6cce82671bec21c986608b0763f4fd9ed7891c7e
Chris Holt <13071055+chrisdholt@users.noreply.github.com>
2025-04-08 09:48:40 -0700

    deprecate tab panel

--- packages/web-components/src/tab-panel/tab-panel.template.ts
+++ packages/web-components/src/tab-panel/tab-panel.template.ts
@@ -1,6 +1,9 @@
 import { type ElementViewTemplate, html } from '@microsoft/fast-element';
 import type { TabPanel } from './tab-panel.js';
 
+/**
+ * @deprecated - Use tablist instead
+ */
 export function tabPanelTemplate<T extends TabPanel>(): ElementViewTemplate<T> {
   return html<T>`
     <template slot="tabpanel" role="tabpanel">
