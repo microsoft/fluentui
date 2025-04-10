b3cf20ee2936d69a05e6eeacfd558019fb623a3d
Marcos Moura <marcosvmmoura@gmail.com>
2025-04-10 17:00:09 +0200

    fix: wrong merge

--- apps/react-18-tests-v9/cypress-react-18.config.ts
+++ apps/react-18-tests-v9/cypress-react-18.config.ts
@@ -9,7 +9,11 @@ const specs = [
 const config = { ...baseConfig };
 
 config.component.specPattern = specs;
+config.component.devServer.webpackConfig.resolve ??= {};
 config.component.devServer.webpackConfig.resolve.alias = {
+  ...config.component.devServer.webpackConfig.resolve.alias,
+  '@cypress/react': path.resolve(__dirname, './node_modules/@cypress/react'),
+  '@types/react': path.resolve(__dirname, './node_modules/@types/react'),
   '@types/react-dom': path.resolve(__dirname, './node_modules/@types/react-dom'),
   react: path.resolve(__dirname, './node_modules/react'),
   'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
