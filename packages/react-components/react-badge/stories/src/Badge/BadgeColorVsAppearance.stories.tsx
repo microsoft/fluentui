fe534d81ccc7156eb34121d35335c95d9c3de731
Valentyna <vkozlova@microsoft.com>
2025-04-22 18:08:27 +0200

    refactor

--- packages/react-components/react-badge/stories/src/Badge/BadgeColorVsAppearance.stories.tsx
+++ packages/react-components/react-badge/stories/src/Badge/BadgeColorVsAppearance.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Badge, makeStyles, tokens } from '@fluentui/react-components';
+import { Badge, makeStyles, tokens, BadgeProps } from '@fluentui/react-components';
 import { ClipboardPasteRegular as PasteIcon } from '@fluentui/react-icons';
 
 const useStyles = makeStyles({
@@ -21,123 +21,54 @@ const useStyles = makeStyles({
   },
 });
 
+const Badges = (props: BadgeProps) => {
+  const styles = useStyles();
+  const { appearance } = props;
+
+  const colors: BadgeProps['color'][] = [
+    'brand',
+    'danger',
+    'important',
+    'informative',
+    'severe',
+    'subtle',
+    'success',
+    'warning',
+  ];
+
+  return (
+    <div className={styles.badge}>
+      {colors.map(color => {
+        const BadgeWrapper =
+          color === 'subtle' && (appearance === 'ghost' || appearance === 'outline')
+            ? ({ children }: { children: React.ReactNode }) => <div className={styles.brand}>{children}</div>
+            : React.Fragment;
+
+        return (
+          <BadgeWrapper>
+            <Badge key={`${appearance}-${color}`} appearance={appearance} color={color} icon={<PasteIcon />}>
+              999+
+            </Badge>
+          </BadgeWrapper>
+        );
+      })}
+    </div>
+  );
+};
+
 export const ColorAndAppearance = () => {
   const styles = useStyles();
 
   return (
     <div className={styles.example}>
       <h3>Filled</h3>
-      <div className={styles.badge}>
-        <Badge appearance="filled" color="brand" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="danger" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="important" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="informative" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="severe" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="subtle" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="success" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="filled" color="warning" icon={<PasteIcon />}>
-          999+
-        </Badge>
-      </div>
+      <Badges appearance="filled" />
       <h3>Ghost</h3>
-      <div className={styles.badge}>
-        <Badge appearance="ghost" color="brand" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="ghost" color="danger" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="ghost" color="important" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="ghost" color="informative" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="ghost" color="severe" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <div className={styles.brand}>
-          <Badge appearance="ghost" color="subtle" icon={<PasteIcon />}>
-            999+
-          </Badge>
-        </div>
-        <Badge appearance="ghost" color="success" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="ghost" color="warning" icon={<PasteIcon />}>
-          999+
-        </Badge>
-      </div>
+      <Badges appearance="ghost" />
       <h3>Outline</h3>
-      <div className={styles.badge}>
-        <Badge appearance="outline" color="brand" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="outline" color="danger" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="outline" color="important" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="outline" color="informative" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="outline" color="severe" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <div className={styles.brand}>
-          <Badge appearance="outline" color="subtle" icon={<PasteIcon />}>
-            999+
-          </Badge>
-        </div>
-        <Badge appearance="outline" color="success" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="outline" color="warning" icon={<PasteIcon />}>
-          999+
-        </Badge>
-      </div>
+      <Badges appearance="outline" />
       <h3>Tint</h3>
-      <div className={styles.badge}>
-        <Badge appearance="tint" color="brand" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="danger" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="important" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="informative" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="severe" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="subtle" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="success" icon={<PasteIcon />}>
-          999+
-        </Badge>
-        <Badge appearance="tint" color="warning" icon={<PasteIcon />}>
-          999+
-        </Badge>
-      </div>
+      <Badges appearance="tint" />
     </div>
   );
 };
