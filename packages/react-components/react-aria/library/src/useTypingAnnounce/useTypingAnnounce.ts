80957d29b4fe23b9704d1c672dc29b9de525608e
Sarah Higley <sarah.higley@microsoft.com>
2025-05-02 15:19:03 -0700

    change file, fix lint issues

--- packages/react-components/react-aria/library/src/useTypingAnnounce/useTypingAnnounce.ts
+++ packages/react-components/react-aria/library/src/useTypingAnnounce/useTypingAnnounce.ts
@@ -18,42 +18,48 @@ const valueMutationOptions = {
 
 export const useTypingAnnounce = (inputEl: React.RefObject<HTMLElement>): { typingAnnounce: AriaLiveAnnounceFn } => {
   const { targetDocument } = useFluent();
-  const win = targetDocument?.defaultView;
   const { announce } = useAnnounce();
 
   const observer = React.useRef<MutationObserver>();
   const [setTypingTimeout, clearTypingTimeout] = useTimeout();
   const messageQueue = React.useRef<Message[]>([]);
 
-  const callback: MutationCallback = React.useCallback((mutationList, observer) => {
-    clearTypingTimeout();
-    setTypingTimeout(() => {
-      messageQueue.current.forEach(({ message, options }) => {
-        announce(message, options);
-      });
-      messageQueue.current.length = 0;
-      observer.disconnect();
-    }, 500);
-  }, []);
+  const callback: MutationCallback = React.useCallback(
+    (mutationList, mutationObserver) => {
+      clearTypingTimeout();
+      setTypingTimeout(() => {
+        messageQueue.current.forEach(({ message, options }) => {
+          announce(message, options);
+        });
+        messageQueue.current.length = 0;
+        mutationObserver.disconnect();
+      }, 500);
+    },
+    [announce, clearTypingTimeout, setTypingTimeout],
+  );
 
-  const typingAnnounce: AriaLiveAnnounceFn = React.useCallback((message: string, options: AnnounceOptions = {}) => {
-    if (!win || !inputEl?.current) {
-      return;
-    }
+  const typingAnnounce: AriaLiveAnnounceFn = React.useCallback(
+    (message: string, options: AnnounceOptions = {}) => {
+      const win = targetDocument?.defaultView;
+      if (!win || !inputEl?.current) {
+        return;
+      }
 
-    messageQueue.current.push({ message, options });
+      messageQueue.current.push({ message, options });
 
-    if (!observer.current) {
-      // create mutation observer if it doesn't exist
-      observer.current = new win.MutationObserver(callback);
-    }
+      if (!observer.current) {
+        // create mutation observer if it doesn't exist
+        observer.current = new win.MutationObserver(callback);
+      }
 
-    observer.current.observe(inputEl.current, valueMutationOptions);
+      observer.current.observe(inputEl.current, valueMutationOptions);
 
-    setTypingTimeout(() => {
-      observer.current && callback([], observer.current);
-    }, 500);
-  }, []);
+      setTypingTimeout(() => {
+        observer.current && callback([], observer.current);
+      }, 500);
+    },
+    [callback, inputEl, setTypingTimeout, targetDocument],
+  );
 
   return { typingAnnounce };
 };
