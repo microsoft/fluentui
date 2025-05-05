d676aaaa0efd19f90b19203c63a45444258ff9d5
Sarah Higley <sarah.higley@microsoft.com>
2025-05-05 13:22:05 -0700

    add extra filtering story, add and update docs

--- packages/react-components/react-aria/stories/src/useTypingAnnounce/index.stories.tsx
+++ packages/react-components/react-aria/stories/src/useTypingAnnounce/index.stories.tsx
@@ -1,16 +1,17 @@
 import type { Meta } from '@storybook/react';
 
-// import descriptionMd from './AriaLiveAnnouncerDescription.md';
+import descriptionMd from './useTypingAnnounceDescription.md';
 
 export { Default } from './useTypingAnnounceDefault.stories';
 export { ContentEditable } from './useTypingAnnounceContentEditable.stories';
+export { Filtering } from './useTypingAnnounceFiltering.stories';
 
 export default {
   title: 'Utilities/ARIA live/useTypingAnnounce',
   parameters: {
     docs: {
       description: {
-        // component: [descriptionMd].join('\n'),
+        component: [descriptionMd].join('\n'),
       },
     },
   },
