439fbc31d77c8d9ff29bd23478c320e8f14c8e8b
srmukher <120183316+srmukher@users.noreply.github.com>
2025-05-06 06:45:50 +0000

    Adding change file

--- packages/charts/chart-utilities/src/PlotlySchemaConverter.ts
+++ packages/charts/chart-utilities/src/PlotlySchemaConverter.ts
@@ -294,7 +294,10 @@ export const mapFluentChart = (input: any): OutputChartType => {
           if (isXDate || isXNumber) {
             return { isValid: true, type: isAreaChart ? 'area' : 'line', validTracesInfo: validTraces };
           } else if (isAreaChart) {
-            throw new Error('Fallback to VerticalStackedBarChart is not allowed for Area Charts.');
+            return {
+              isValid: false,
+              errorMessage: 'Fallback to VerticalStackedBarChart is not allowed for Area Charts.',
+            };
           }
           return { isValid: true, type: 'fallback', validTracesInfo: validTraces };
         }
