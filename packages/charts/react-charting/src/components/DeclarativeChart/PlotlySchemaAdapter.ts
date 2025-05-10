14e63a718e92ff54eb479949074aff36a06646c7
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-05-10 19:38:03 +0530

    fix(react-charting): Improve formatting of numeric labels (#34410)
    Co-authored-by: Atishay Jain <atisjai@microsoft.com>

--- packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
+++ packages/charts/react-charting/src/components/DeclarativeChart/PlotlySchemaAdapter.ts
@@ -509,6 +509,7 @@ export const transformPlotlyJsonToScatterChartProps = (
       height: input.layout?.height ?? 350,
       hideTickOverlap: true,
       hideLegend,
+      useUTC: false,
     } as IAreaChartProps;
   } else {
     return {
@@ -525,6 +526,7 @@ export const transformPlotlyJsonToScatterChartProps = (
       hideTickOverlap: true,
       enableReflow: false,
       hideLegend,
+      useUTC: false,
     } as ILineChartProps;
   }
 };
