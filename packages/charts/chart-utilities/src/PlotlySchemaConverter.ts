162838e742ccd6c2e21d8a33c6760cf2d4cb1a91
srmukher <120183316+srmukher@users.noreply.github.com>
2025-05-15 11:08:49 +0000

    Add chart type to error logs

--- packages/charts/chart-utilities/src/PlotlySchemaConverter.ts
+++ packages/charts/chart-utilities/src/PlotlySchemaConverter.ts
@@ -200,7 +200,7 @@ const getValidTraces = (dataArr: Data[]) => {
           try {
             validator(data);
           } catch (error) {
-            errorMessages.push(`data[${index}]: ${error}`);
+            errorMessages.push(`type: ${data.type}: data[${index}]: ${error}`);
             return [-1, DEFAULT_CHART_TYPE];
           }
         }
