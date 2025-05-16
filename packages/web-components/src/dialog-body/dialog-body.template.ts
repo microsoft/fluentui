9f6c46428ca870a98d976738c2c3593afc785337
jeff smith <jes@microsoft.com>
2025-05-16 11:46:13 -0700

    move click handler to close slot

--- packages/web-components/src/dialog-body/dialog-body.template.ts
+++ packages/web-components/src/dialog-body/dialog-body.template.ts
@@ -5,11 +5,11 @@ import { type ElementViewTemplate, html, ref } from '@microsoft/fast-element';
  * @public
  */
 export const template: ElementViewTemplate = html`
-<template @click="${(x, c) => x.clickHandler(c.event as PointerEvent)}">
+<template>
   <div class="title" part="title">
     <slot name="title"></slot>
     <slot name="title-action"></slot>
-    <slot name="close"></slot>
+    <slot name="close" @click="${(x, c) => x.clickHandler(c.event as PointerEvent)}"></slot>
   </div>
   <div class="content" part="content"><slot></slot></div>
   <div class="actions" part="actions"><slot name="action"></slot></div>
