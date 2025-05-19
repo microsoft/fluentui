9ee143910c6e6df9eababa68609e6633c77fdbc7
jeff smith <jes@microsoft.com>
2025-05-19 10:22:36 -0700

    fix formatting

--- packages/web-components/src/dialog-body/dialog-body.template.ts
+++ packages/web-components/src/dialog-body/dialog-body.template.ts
@@ -5,13 +5,13 @@ import { type ElementViewTemplate, html, ref } from '@microsoft/fast-element';
  * @public
  */
 export const template: ElementViewTemplate = html`
-<template>
-  <div class="title" part="title">
-    <slot name="title"></slot>
-    <slot name="title-action"></slot>
-    <slot name="close" @click="${(x, c) => x.clickHandler(c.event as PointerEvent)}"></slot>
-  </div>
-  <div class="content" part="content"><slot></slot></div>
-  <div class="actions" part="actions"><slot name="action"></slot></div>
-</template>
+  <template>
+    <div class="title" part="title">
+      <slot name="title"></slot>
+      <slot name="title-action"></slot>
+      <slot name="close" @click="${(x, c) => x.clickHandler(c.event as PointerEvent)}"></slot>
+    </div>
+    <div class="content" part="content"><slot></slot></div>
+    <div class="actions" part="actions"><slot name="action"></slot></div>
+  </template>
 `;
