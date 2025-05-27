ad2bd9c261b4df741ab0dbb019b90b2a92734600
v-baambati <132879294+v-baambati@users.noreply.github.com>
2025-05-27 12:11:49 +0530

    removed unwanted code

--- packages/charts/react-charting/src/components/StackedBarChart/StackedBarChart.test.tsx
+++ packages/charts/react-charting/src/components/StackedBarChart/StackedBarChart.test.tsx
@@ -1,7 +1,6 @@
 // jest.mock('react-dom');
 import * as React from 'react';
 import { resetIds } from '../../Utilities';
-import * as renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import { DefaultPalette } from '@fluentui/react/lib/Styling';
 import { IChartProps, IChartDataPoint, StackedBarChart } from '../../index';
