5c028e858b0de2cab710a10a2d7d727198f803ad
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-05-29 19:07:54 +0200

    fix lint error

--- packages/react-components/react-toast/library/src/components/ToastContainer/useToastContainer.ts
+++ packages/react-components/react-toast/library/src/components/ToastContainer/useToastContainer.ts
@@ -50,7 +50,7 @@ export const useToastContainer_unstable = (
     pauseOnWindowBlur,
     imperativeRef,
     tryRestoreFocus,
-    content: _, // `content` is a slot and it's type clashes with the HTMLElement `content` attribute
+    content: _content, // `content` is a slot and it's type clashes with the HTMLElement `content` attribute
     ...rest
   } = props;
   const titleId = useId('toast-title');
