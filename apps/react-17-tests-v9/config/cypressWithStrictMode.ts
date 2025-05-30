30c57eb8d378f73a494a367198e9b4e3d7797747
Martin Hochel <martinhochel@microsoft.com>
2025-05-30 15:39:39 +0200

    chore(r17-v9): fix cypress import path within global config override

--- apps/react-17-tests-v9/config/cypressWithStrictMode.ts
+++ apps/react-17-tests-v9/config/cypressWithStrictMode.ts
@@ -1,8 +1,9 @@
-import { mount as cypressMount } from '../node_modules/@cypress/react';
+// TODO: these relative paths won;t work once repo is migrated to react18, will need to adjust to local node_modules
+import { mount as cypressMount } from '../../../node_modules/@cypress/react';
 
 type MountWithStrictMode = typeof cypressMount;
 
-export * from '../node_modules/@cypress/react';
+export * from '../../../node_modules/@cypress/react';
 
 export const mount: MountWithStrictMode = (jsx, options) => {
   const { strict = true, ...rest } = options || {};
