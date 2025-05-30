b9a0acc5904fb03dd7d34bc3167a015f4129fc28
nathansokol <nathansokol@microsoft.com>
2025-05-30 15:10:08 -0500

    add span to root slot option of skeleton and skeleton item

--- packages/react-components/react-skeleton/stories/src/Skeleton/SkeletonDefault.stories.tsx
+++ packages/react-components/react-skeleton/stories/src/Skeleton/SkeletonDefault.stories.tsx
@@ -3,7 +3,7 @@ import { Skeleton, SkeletonItem } from '@fluentui/react-components';
 import type { SkeletonProps } from '@fluentui/react-components';
 
 export const Default = (props: Partial<SkeletonProps>) => (
-  <Skeleton {...props} aria-label="Loading Content">
-    <SkeletonItem />
+  <Skeleton {...props} aria-label="Loading Content" as="span">
+    <SkeletonItem as="span" />
   </Skeleton>
 );
