241b47e368468adf16087e1846315cdcdb717ebc
Martin Hochel <martinhochel@microsoft.com>
2025-06-12 18:20:50 +0200

    chore(react-tests): update integration testing setup to resolve against proper react deps

--- apps/react-18-tests-v9/cypress.config.ts
+++ apps/react-18-tests-v9/cypress.config.ts
@@ -1,16 +1,20 @@
 import * as path from 'path';
 import { baseConfig } from '@fluentui/scripts-cypress';
 
+import { getNodeModulesPath } from './config/utils';
+
+const usedNodeModulesPath = getNodeModulesPath();
+
 const config = { ...baseConfig };
 
 config.component.devServer.webpackConfig.resolve ??= {};
 config.component.devServer.webpackConfig.resolve.alias = {
   ...config.component.devServer.webpackConfig.resolve.alias,
-  '@cypress/react': path.resolve(__dirname, './node_modules/@cypress/react'),
-  '@types/react': path.resolve(__dirname, './node_modules/@types/react'),
-  '@types/react-dom': path.resolve(__dirname, './node_modules/@types/react-dom'),
-  react: path.resolve(__dirname, './node_modules/react'),
-  'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+  '@cypress/react': path.resolve(usedNodeModulesPath, './@cypress/react'),
+  '@types/react': path.resolve(usedNodeModulesPath, './@types/react'),
+  '@types/react-dom': path.resolve(usedNodeModulesPath, './@types/react-dom'),
+  react: path.resolve(usedNodeModulesPath, './react'),
+  'react-dom': path.resolve(usedNodeModulesPath, './react-dom'),
 };
 
 export default config;
