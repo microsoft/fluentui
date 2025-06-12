62af9910f4d86cf7b8949305cd9d1a83d2400393
Martin Hochel <martinhochel@microsoft.com>
2025-06-12 17:52:37 +0200

    chore(react-tests): remove no longer needed cypress setup

--- apps/react-18-tests-v8/cypress-react-18.config.ts
+++ apps/react-18-tests-v8/cypress-react-18.config.ts
@@ -20,14 +20,4 @@ config.component.devServer.webpackConfig.resolve.alias = {
   'cypress-real-events': path.resolve(__dirname, './node_modules/cypress-real-events'),
 };
 
-/**
- * Resolve the support file and index.html file paths
- * This is a workaround for the issue where Cypress does not resolve the paths correctly, as it
- * internally prepend the __dirname, making them invalid.
- *
- * TODO: Remove this workaround once we upgrade the whole repo to Cypress 14
- */
-config.component.supportFile = path.normalize('../../scripts/cypress/src/support/component.js');
-config.component.indexHtmlFile = path.normalize('../../scripts/cypress/src/support/component-index.html');
-
 export default config;
