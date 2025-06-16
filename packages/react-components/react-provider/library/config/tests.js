867ebe319824af394dce1266f015ea5c087a6ab0
Martin Hochel <martinhochel@microsoft.com>
2025-06-16 17:15:32 +0200

    test(react-provider): set TextEncoder to global to mitigate jest failures

--- packages/react-components/react-provider/library/config/tests.js
+++ packages/react-components/react-provider/library/config/tests.js
@@ -1,3 +1,6 @@
 /** Jest test setup file. */
 
+const { TextEncoder, TextDecoder } = require('node:util');
 require('@testing-library/jest-dom');
+
+Object.assign(global, { TextDecoder, TextEncoder });
