4b362ce38c46b1270a130b1daf938f4ec2af447f
Anush <anushgupta@microsoft.com>
2025-06-17 19:41:53 +0530

    add support for negative vsbc

--- packages/charts/react-charts/stories/src/VerticalStackedBarChart/VerticalStackedBarChartNegative.stories.tsx
+++ packages/charts/react-charts/stories/src/VerticalStackedBarChart/VerticalStackedBarChartNegative.stories.tsx
@@ -9,11 +9,11 @@ import {
 } from '@fluentui/react-charts';
 import { Checkbox, CheckboxOnChangeData, Switch } from '@fluentui/react-components';
 
-export const VerticalStackedBarDefault = () => {
+export const VerticalStackedBarNegative = () => {
   const [width, setWidth] = React.useState(650);
   const [height, setHeight] = React.useState(350);
-  const [showLine, setShowLine] = React.useState(true);
   const [barGapMax, setBarGapMax] = React.useState(2);
+  const [showLine, setShowLine] = React.useState(true);
   const [hideLabels, setHideLabels] = React.useState(false);
   const [showAxisTitles, setShowAxisTitles] = React.useState(true);
   const [margins, setMargins] = React.useState({
@@ -24,6 +24,27 @@ export const VerticalStackedBarDefault = () => {
   });
   const [roundCorners, setRoundCorners] = React.useState(false);
   const [legendMultiSelect, setLegendMultiSelect] = React.useState(false);
+
+  React.useEffect(() => {
+    const style = document.createElement('style');
+    const focusStylingCSS = `
+      .containerDiv [contentEditable=true]:focus,
+      .containerDiv [tabindex]:focus,
+      .containerDiv area[href]:focus,
+      .containerDiv button:focus,
+      .containerDiv iframe:focus,
+      .containerDiv input:focus,
+      .containerDiv select:focus,
+      .containerDiv textarea:focus {
+        outline: -webkit-focus-ring-color auto 5px;
+      }
+    `;
+    style.appendChild(document.createTextNode(focusStylingCSS));
+    document.head.appendChild(style);
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
 
   const _onWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWidth(parseInt(e.target.value, 10));
@@ -69,94 +90,94 @@ export const VerticalStackedBarDefault = () => {
     {
       legend: 'Metadata1',
       data: 40,
-      color: getColorFromToken(DataVizPalette.color11),
+      color: getColorFromToken(DataVizPalette.color1),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '40%',
+      yAxisCalloutData: '68%',
     },
     {
       legend: 'Metadata2',
       data: 5,
-      color: 'darkblue',
+      color: getColorFromToken(DataVizPalette.color2),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '5%',
+      yAxisCalloutData: '8.5%',
     },
     {
       legend: 'Metadata3',
-      data: 20,
-      color: getColorFromToken(DataVizPalette.color6),
+      data: -20,
+      color: getColorFromToken(DataVizPalette.color3),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '20%',
+      yAxisCalloutData: '34%',
     },
     {
       legend: 'Metadata4',
       data: 10,
       color: getColorFromToken(DataVizPalette.color4),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '10%',
+      yAxisCalloutData: '17%',
     },
     {
       legend: 'Metadata5',
       data: 23,
       color: getColorFromToken(DataVizPalette.color5),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '23%',
+      yAxisCalloutData: '39%',
     },
     {
       legend: 'Metadata6',
       data: 0.4,
       color: getColorFromToken(DataVizPalette.color6),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '0.4%',
+      yAxisCalloutData: '0.7%',
     },
     {
       legend: 'Metadata7',
-      data: 0.5,
+      data: -0.5,
       color: getColorFromToken(DataVizPalette.color7),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '0.5%',
+      yAxisCalloutData: '0.85%',
     },
     {
       legend: 'Metadata8',
-      data: 0.3,
+      data: -0.3,
       color: getColorFromToken(DataVizPalette.color8),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '0.3%',
+      yAxisCalloutData: '0.5%',
     },
     {
       legend: 'Metadata9',
       data: 0.7,
       color: getColorFromToken(DataVizPalette.color9),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '0.7%',
+      yAxisCalloutData: '1.2%',
     },
     {
       legend: 'Metadata10',
       data: 0.1,
       color: getColorFromToken(DataVizPalette.color10),
       xAxisCalloutData: '2020/04/30',
-      yAxisCalloutData: '0.1%',
+      yAxisCalloutData: '0.2%',
     },
   ];
 
   const secondChartPoints: VSChartDataPoint[] = [
     {
       legend: 'Metadata1',
-      data: 30,
-      color: getColorFromToken(DataVizPalette.color11),
+      data: -30,
+      color: getColorFromToken(DataVizPalette.color1),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '33%',
     },
     {
       legend: 'Metadata2',
-      data: 20,
-      color: 'darkblue',
+      data: -20,
+      color: getColorFromToken(DataVizPalette.color2),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '22%',
     },
     {
       legend: 'Metadata3',
-      data: 40,
-      color: getColorFromToken(DataVizPalette.color6),
+      data: -40,
+      color: getColorFromToken(DataVizPalette.color3),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '45%',
     },
@@ -166,21 +187,21 @@ export const VerticalStackedBarDefault = () => {
     {
       legend: 'Metadata1',
       data: 44,
-      color: getColorFromToken(DataVizPalette.color11),
+      color: getColorFromToken(DataVizPalette.color1),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '43%',
     },
     {
       legend: 'Metadata2',
       data: 28,
-      color: 'darkblue',
+      color: getColorFromToken(DataVizPalette.color2),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '27%',
     },
     {
       legend: 'Metadata3',
       data: 30,
-      color: getColorFromToken(DataVizPalette.color6),
+      color: getColorFromToken(DataVizPalette.color3),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '30%',
     },
@@ -190,21 +211,21 @@ export const VerticalStackedBarDefault = () => {
     {
       legend: 'Metadata1',
       data: 88,
-      color: getColorFromToken(DataVizPalette.color11),
+      color: getColorFromToken(DataVizPalette.color1),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '63%',
     },
     {
       legend: 'Metadata2',
       data: 22,
-      color: 'darkblue',
+      color: getColorFromToken(DataVizPalette.color2),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '16%',
     },
     {
       legend: 'Metadata3',
       data: 30,
-      color: getColorFromToken(DataVizPalette.color6),
+      color: getColorFromToken(DataVizPalette.color3),
       xAxisCalloutData: '2020/04/30',
       yAxisCalloutData: '21%',
     },
@@ -217,8 +238,8 @@ export const VerticalStackedBarDefault = () => {
 
       ...(showLine && {
         lineData: [
-          { y: 42, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color2) },
-          { y: 10, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color17) },
+          { y: 42, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color5) },
+          { y: 10, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color9) },
         ],
       }),
     },
@@ -226,7 +247,7 @@ export const VerticalStackedBarDefault = () => {
       chartData: secondChartPoints,
       xAxisPoint: 20,
       ...(showLine && {
-        lineData: [{ y: 33, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color2) }],
+        lineData: [{ y: 33, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color5) }],
       }),
     },
     {
@@ -234,8 +255,8 @@ export const VerticalStackedBarDefault = () => {
       xAxisPoint: 40,
       ...(showLine && {
         lineData: [
-          { y: 60, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color2) },
-          { y: 20, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color17) },
+          { y: 60, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color5) },
+          { y: 20, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color9) },
         ],
       }),
     },
@@ -244,8 +265,8 @@ export const VerticalStackedBarDefault = () => {
       xAxisPoint: 60,
       ...(showLine && {
         lineData: [
-          { y: 41, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color2) },
-          { y: 10, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color17) },
+          { y: 41, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color5) },
+          { y: 10, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color9) },
         ],
       }),
     },
@@ -254,8 +275,8 @@ export const VerticalStackedBarDefault = () => {
       xAxisPoint: 80,
       ...(showLine && {
         lineData: [
-          { y: 100, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color2) },
-          { y: 70, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color17) },
+          { y: 100, legend: 'Supported Builds', color: getColorFromToken(DataVizPalette.color5) },
+          { y: 70, legend: 'Recommended Builds', color: getColorFromToken(DataVizPalette.color9) },
         ],
       }),
     },
@@ -270,39 +291,37 @@ export const VerticalStackedBarDefault = () => {
   const rootStyle = { width: `${width}px`, height: `${height}px` };
 
   return (
-    <>
-      <div style={{ display: 'flex' }}>
-        <label htmlFor="changeWidth_Basic">Change Width:</label>
-        <input
-          type="range"
-          value={width}
-          min={200}
-          max={1000}
-          id="changeWidth_Basic"
-          onChange={_onWidthChange}
-          aria-valuetext={`ChangeWidthSlider${width}`}
-        />
-        <label htmlFor="changeHeight_Basic">Change Height:</label>
-        <input
-          type="range"
-          value={height}
-          min={200}
-          max={1000}
-          id="changeHeight_Basic"
-          onChange={_onHeightChange}
-          aria-valuetext={`ChangeHeightslider${height}`}
-        />
-        <label htmlFor="changeBarGapMax_Basic">BarGapMax:</label>
-        <input
-          type="range"
-          value={barGapMax}
-          min={0}
-          max={10}
-          id="changeBarGapMax_Basic"
-          onChange={e => setBarGapMax(+e.target.value)}
-          aria-valuetext={`ChangebarGapMaxSlider${barGapMax}`}
-        />
-      </div>
+    <div className="containerDiv">
+      <label htmlFor="changeWidth_Basic">Change Width:</label>
+      <input
+        type="range"
+        value={width}
+        min={200}
+        max={1000}
+        id="changeWidth_Basic"
+        onChange={_onWidthChange}
+        aria-valuetext={`ChangeWidthSlider${width}`}
+      />
+      <label htmlFor="changeHeight_Basic">Change Height:</label>
+      <input
+        type="range"
+        value={height}
+        min={200}
+        max={1000}
+        id="changeHeight_Basic"
+        onChange={_onHeightChange}
+        aria-valuetext={`ChangeHeightslider${height}`}
+      />
+      <label htmlFor="changeBarGapMax_Basic">BarGapMax:</label>
+      <input
+        type="range"
+        value={barGapMax}
+        min={0}
+        max={10}
+        id="changeBarGapMax_Basic"
+        onChange={e => setBarGapMax(+e.target.value)}
+        aria-valuetext={`ChangebarGapMaxSlider${barGapMax}`}
+      />
       <div style={{ marginTop: '10px' }}>
         <Checkbox label="show the lines (hide or show the lines)" checked={showLine} onChange={_onShowLineChange} />
       </div>
@@ -339,10 +358,12 @@ export const VerticalStackedBarDefault = () => {
               canSelectMultipleLegends: legendMultiSelect,
             }}
             hideLabels={hideLabels}
+            enableReflow={true}
             yAxisTitle={showAxisTitles ? 'Variation of number of sales' : undefined}
             xAxisTitle={showAxisTitles ? 'Number of days' : undefined}
             roundCorners={roundCorners}
             roundedTicks={true}
+            supportNegativeData={true}
           />
         </div>
       )}
@@ -362,15 +383,17 @@ export const VerticalStackedBarDefault = () => {
               canSelectMultipleLegends: legendMultiSelect,
             }}
             hideLabels={hideLabels}
+            enableReflow={true}
             roundCorners={roundCorners}
             roundedTicks={true}
+            supportNegativeData={true}
           />
         </div>
       )}
-    </>
+    </div>
   );
 };
-VerticalStackedBarDefault.parameters = {
+VerticalStackedBarNegative.parameters = {
   docs: {
     description: {},
   },
