9f3a652c277ca000ef318801ab6db728bae9feba
mainframev <vgenaev@gmail.com>
2025-06-23 08:42:29 +0200

    feat(eslint-plugin): add support of flat configs

--- packages/eslint-plugin/src/configs/node.js
+++ packages/eslint-plugin/src/configs/node.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 
-/** @type {import("eslint").Linter.Config} */
+/** @type {import("eslint").Linter.LegacyConfig} */
 module.exports = {
   extends: [path.join(__dirname, 'base')],
   rules: {
---
+++ packages/eslint-plugin/src/flat-configs/node/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+const baseConfig = require('../base');
+const tseslint = require('typescript-eslint');
+
+/** @type {import('typescript-eslint').ConfigArray} */
+module.exports = tseslint.config([
+  {
+    files: ['**/*.{ts,js}'],
+    extends: [baseConfig],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+]);
