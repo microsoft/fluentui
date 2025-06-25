bf2901be27814544f5c67d0a0ae0efceb001cf4a
Adam <adam.samec@gmail.com>
2025-06-25 12:42:24 +0200

    Merge agents and bots array with people array for grid variant

--- apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsGridList.stories.tsx
+++ apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsGridList.stories.tsx
@@ -5,13 +5,15 @@ import { Prototype } from './utils';
 import { ParticipantsMenuWrapper } from './ParticipantsMenuWrapper';
 import { participantsList } from './participantsList';
 
+const participantsListMerged = participantsList.people.concat(participantsList.agentsAndBots);
+
 export const ParticipantsGridList = () => {
   return (
     <Prototype pageTitle="Grid">
       <h1>Grid</h1>
       <ParticipantsMenuWrapper>
         <List navigationMode="composite">
-          {participantsList.people.map((name, index) => (
+          {participantsListMerged.map((name, index) => (
             <ListItem key={index} aria-label={name}>
               <div role="gridcell">{name}</div>
               <div role="gridcell">
