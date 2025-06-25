66c4a4e7cadc040a42a177c0a331ee4196132fd1
Mitch-At-Work <99835933+Mitch-At-Work@users.noreply.github.com>
2025-06-25 10:51:03 -0700

    Extended Tokens: React-Spinner (#34645)

--- packages/semantic-tokens/src/components/spinner/tokens.ts
+++ packages/semantic-tokens/src/components/spinner/tokens.ts
@@ -1,6 +1,7 @@
 // THIS FILE IS GENERATED AS PART OF THE BUILD PROCESS. DO NOT MANUALLY MODIFY THIS FILE
+import { strokeWidthThicker } from '../../legacy/tokens';
 import { ctrlProgressHeightFilledRaw } from '../progress/variables';
 import { ctrlSpinnerStrokeWidthRaw, ctrlSpinnerShowEmptyTrackRaw } from './variables';
 
-export const ctrlSpinnerStrokeWidth = `var(${ctrlSpinnerStrokeWidthRaw}, var(${ctrlProgressHeightFilledRaw}))`;
+export const ctrlSpinnerStrokeWidth = `var(${ctrlSpinnerStrokeWidthRaw}, var(${ctrlProgressHeightFilledRaw}, ${strokeWidthThicker}))`;
 export const ctrlSpinnerShowEmptyTrack = `var(${ctrlSpinnerShowEmptyTrackRaw})`;
