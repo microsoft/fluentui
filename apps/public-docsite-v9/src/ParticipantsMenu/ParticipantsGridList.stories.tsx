6397b9bb21c1d43cbb85428aa79710085a961956
Adam <adam.samec@gmail.com>
2025-06-25 12:16:19 +0200

    Introduce Menu wrapper component and use it for the prototypes

--- apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsGridList.stories.tsx
+++ apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsGridList.stories.tsx
@@ -2,26 +2,28 @@ import * as React from 'react';
 import { Button, List, ListItem } from '@fluentui/react-components';
 
 import { Prototype } from './utils';
-
+import { ParticipantsMenuWrapper } from './ParticipantsMenuWrapper';
 import { participantsList } from './participantsList';
 
 export const ParticipantsGridList = () => {
   return (
     <Prototype pageTitle="Grid">
       <h1>Grid</h1>
-      <List navigationMode="composite">
-        {participantsList.people.map((name, index) => (
-          <ListItem key={index} aria-label={name}>
-            <div role="gridcell">{name}</div>
-            <div role="gridcell">
-              <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
-            </div>
-            <div role="gridcell">
-              <Button>Remove {name}</Button>
-            </div>
-          </ListItem>
-        ))}
-      </List>
+      <ParticipantsMenuWrapper>
+        <List navigationMode="composite">
+          {participantsList.people.map((name, index) => (
+            <ListItem key={index} aria-label={name}>
+              <div role="gridcell">{name}</div>
+              <div role="gridcell">
+                <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
+              </div>
+              <div role="gridcell">
+                <Button>Remove {name}</Button>
+              </div>
+            </ListItem>
+          ))}
+        </List>
+      </ParticipantsMenuWrapper>
     </Prototype>
   );
 };
--- apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsRowGroupGridListTwoColumns.stories.tsx
+++ apps/public-docsite-v9/src/ParticipantsMenu/ParticipantsRowGroupGridListTwoColumns.stories.tsx
@@ -1,47 +1,42 @@
 import * as React from 'react';
-import { Button, List, ListItem, Menu, MenuTrigger, MenuPopover } from '@fluentui/react-components';
+import { Button, List, ListItem } from '@fluentui/react-components';
 
 import { Prototype } from './utils';
-
+import { ParticipantsMenuWrapper } from './ParticipantsMenuWrapper';
 import { participantsList } from './participantsList';
 
 export const ParticipantsRowGroupGridListTwoColumns = () => {
   return (
     <Prototype pageTitle="Row group grid with just two columns">
       <h1>Row group grid with just two columns</h1>
-      <Menu>
-        <MenuTrigger>
-          <Button>Add people, agents, and bots</Button>
-        </MenuTrigger>
-        <MenuPopover>
-          <List navigationMode="composite">
-            <div aria-hidden="true">People</div>
-            <div role="rowgroup" aria-label="People">
-              {participantsList.people.map((name, index) => (
-                <ListItem key={index} role="row" aria-label={name}>
-                  <div role="gridcell">{name}</div>
-                  <div role="gridcell">
-                    <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
-                    <Button>Remove {name}</Button>
-                  </div>
-                </ListItem>
-              ))}
-            </div>
-            <div aria-hidden="true">Agents and bots</div>
-            <div role="rowgroup" aria-label="Agents and bots">
-              {participantsList.agentsAndBots.map((name, index) => (
-                <ListItem key={index} role="row" aria-label={name}>
-                  <div role="gridcell">{name}</div>
-                  <div role="gridcell">
-                    <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
-                    <Button>Remove {name}</Button>
-                  </div>
-                </ListItem>
-              ))}
-            </div>
-          </List>
-        </MenuPopover>
-      </Menu>
+      <ParticipantsMenuWrapper>
+        <List navigationMode="composite">
+          <div aria-hidden="true">People</div>
+          <div role="rowgroup" aria-label="People">
+            {participantsList.people.map((name, index) => (
+              <ListItem key={index} role="row" aria-label={name}>
+                <div role="gridcell">{name}</div>
+                <div role="gridcell">
+                  <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
+                  <Button>Remove {name}</Button>
+                </div>
+              </ListItem>
+            ))}
+          </div>
+          <div aria-hidden="true">Agents and bots</div>
+          <div role="rowgroup" aria-label="Agents and bots">
+            {participantsList.agentsAndBots.map((name, index) => (
+              <ListItem key={index} role="row" aria-label={name}>
+                <div role="gridcell">{name}</div>
+                <div role="gridcell">
+                  <Button aria-description={`Show profile card for ${name}`}>Avatar for {name}</Button>
+                  <Button>Remove {name}</Button>
+                </div>
+              </ListItem>
+            ))}
+          </div>
+        </List>
+      </ParticipantsMenuWrapper>
     </Prototype>
   );
 };
