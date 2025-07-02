d34548439a4a90da4744f0ff90edcacb7d1997ae
Robert Penner <robertpenner@microsoft.com>
2025-07-02 20:16:26 +0000

    docs(Slide): clean up UI and naming

--- packages/react-components/react-motion-components-preview/stories/src/Slide/index.stories.ts
+++ packages/react-components/react-motion-components-preview/stories/src/Slide/index.stories.ts
@@ -4,7 +4,7 @@ import SlideDescription from './SlideDescription.md';
 export { Default } from './SlideDefault.stories';
 export { Snappy } from './SlideSnappy.stories';
 export { Relaxed } from './SlideRelaxed.stories';
-export { Customization } from './SlideCustomization.stories';
+export { Directions } from './SlideDirections.stories';
 export { CardsDemo } from './SlideCardsDemo.stories';
 
 export default {
