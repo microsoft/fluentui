a1dd6fe7d55b994c476d4805b8b2fc04012ad8ac
Robert Penner <robertpenner@microsoft.com>
2025-07-08 12:19:17 +0000

    refactor(Slide): use shorter `translate` CSS property instead of `transform...`

--- packages/react-components/react-motion-components-preview/library/src/atoms/slide-atom.ts
+++ packages/react-components/react-motion-components-preview/library/src/atoms/slide-atom.ts
@@ -24,7 +24,7 @@ export const slideAtom = ({
   fromX = '0px',
   fromY = '0px',
 }: SlideAtomParams): AtomMotion => {
-  const keyframes = [{ transform: `translate3d(${fromX}, ${fromY}, 0)` }, { transform: 'translate3d(0, 0, 0)' }];
+  const keyframes = [{ translate: `${fromX} ${fromY}` }, { translate: '0px 0px' }];
   if (direction === 'exit') {
     keyframes.reverse();
   }
