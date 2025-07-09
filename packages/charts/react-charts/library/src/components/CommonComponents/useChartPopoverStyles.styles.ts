dbc7e8467057375d58f2d16175d4ea71bf69ffa3
Anush <anushgupta@microsoft.com>
2025-07-09 15:26:30 +0530

    fix sankey chart hover issue

--- packages/charts/react-charts/library/src/components/CommonComponents/useChartPopoverStyles.styles.ts
+++ packages/charts/react-charts/library/src/components/CommonComponents/useChartPopoverStyles.styles.ts
@@ -50,7 +50,6 @@ const useStyles = makeStyles({
   },
   calloutBlockContainerCartesian: {
     ...typographyStyles.caption1,
-    marginTop: '13px',
     forcedColorAdjust: 'none',
   },
   calloutBlockContainerNonCartesian: {
