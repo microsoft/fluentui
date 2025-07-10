a2d403b8edcc3de3925374d44c7b6e1f3a565c35
mainframev <vgenaev@gmail.com>
2025-07-10 19:44:36 +0200

    chore: suppress deprecation errors

--- packages/react/src/Icons.ts
+++ packages/react/src/Icons.ts
@@ -1,3 +1,4 @@
 export { initializeIcons } from '@fluentui/font-icons-mdl2';
 
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 export type { IconNames } from '@fluentui/font-icons-mdl2';
