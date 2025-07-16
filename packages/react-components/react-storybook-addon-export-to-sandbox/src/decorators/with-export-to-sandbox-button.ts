2df893649e3507fbe1a4b4cf34daa4c5caa047f2
mainframev <vgenaev@gmail.com>
2025-07-16 19:16:56 +0200

    docs(storybook): fix export-to-sandbox-button

--- packages/react-components/react-storybook-addon-export-to-sandbox/src/decorators/with-export-to-sandbox-button.ts
+++ packages/react-components/react-storybook-addon-export-to-sandbox/src/decorators/with-export-to-sandbox-button.ts
@@ -1,14 +1,21 @@
 import { addDemoActionButton } from '../sandbox-factory';
-
 import { StoryContext } from '../types';
 
 export const withExportToSandboxButton = (
-  storyFn: (context: StoryContext) => // eslint-disable-next-line @typescript-eslint/no-deprecated
-  JSX.Element,
+  // eslint-disable-line @typescript-eslint/no-deprecated
+  storyFn: (context: StoryContext) => JSX.Element,
   context: StoryContext,
 ) => {
-  if (context.viewMode === 'docs') {
-    addDemoActionButton(context);
+  if (context.viewMode === 'docs' && typeof window !== 'undefined') {
+    const observer = new MutationObserver(() => {
+      const toolbarContainer = document.querySelector('.docblock-controls');
+      if (toolbarContainer) {
+        addDemoActionButton(context);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
   }
 
   return storyFn(context);
