348ff8c282cd91a093035f06d59e225937470b66
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-07-17 11:13:21 +0200

    add support for Storybook composition

---
+++ apps/public-docsite-v9/llms.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+const storybookConfig = require('./.storybook/main');
+
+/**
+ * Config for the LLMs docs generator script.
+ * @see {@link file://./../../scripts/storybook/src/scripts/generate-llms-docs.ts}
+ *
+ * @type {import('../../scripts/storybook/src/scripts/generate-llms-docs').Args}
+ */
+module.exports = {
+  distPath: './dist/storybook',
+  baseUrl: 'https://react.fluentui.dev',
+  summaryTitle: 'Fluent UI React v9',
+  summaryDescription:
+    "Fluent UI React is a library of React components that implement Microsoft's [Fluent Design System](https://fluent2.microsoft.design).",
+  refs: Object.values(storybookConfig.refs ?? {}),
+};
