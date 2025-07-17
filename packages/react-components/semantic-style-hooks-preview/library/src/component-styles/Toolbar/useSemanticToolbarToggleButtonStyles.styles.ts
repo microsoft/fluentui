1440974db7ca8d4a77a5ba08f1903743adb60670
Rachel Yoo <57475719+rachethecreator@users.noreply.github.com>
2025-07-17 15:41:41 -0400

    add toolbar to semantic style hooks (#34840)

---
+++ packages/react-components/semantic-style-hooks-preview/library/src/component-styles/Toolbar/useSemanticToolbarToggleButtonStyles.styles.ts
@@ -0,0 +1,45 @@
+import { tokens } from '@fluentui/react-theme';
+import { makeStyles, mergeClasses } from '@griffel/react';
+import { useSemanticToggleButtonStyles } from '../Button/useSemanticToggleButtonStyles.styles';
+import { type ToolbarToggleButtonState } from '@fluentui/react-toolbar';
+import { getSlotClassNameProp_unstable } from '@fluentui/react-utilities';
+
+const useBaseStyles = makeStyles({
+  /* use subtle ToggleButton selected styles for Toolbar Radio selected state */
+  selected: {
+    backgroundColor: tokens.colorSubtleBackgroundSelected,
+    color: tokens.colorNeutralForeground2Selected,
+  },
+
+  iconSelected: {
+    color: tokens.colorNeutralForeground2BrandSelected,
+  },
+});
+
+/**
+ * Apply styling to the ToolbarToggleButton slots based on the state
+ */
+export const useSemanticToolbarToggleButtonStyles = (_state: unknown): ToolbarToggleButtonState => {
+  'use no memo';
+
+  const state = _state as ToolbarToggleButtonState;
+
+  useSemanticToggleButtonStyles(state);
+  const toggleButtonStyles = useBaseStyles();
+
+  state.root.className = mergeClasses(
+    state.root.className,
+    state.checked && toggleButtonStyles.selected,
+    getSlotClassNameProp_unstable(state.root),
+  );
+
+  if (state.icon) {
+    state.icon.className = mergeClasses(
+      state.icon.className,
+      state.checked && toggleButtonStyles.iconSelected,
+      getSlotClassNameProp_unstable(state.icon),
+    );
+  }
+
+  return state;
+};
