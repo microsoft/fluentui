13d2accf80ecb45b814691f9d1b2071661052a3a
Oleksandr Fediashov <olfedias@microsoft.com>
2025-07-18 12:04:33 +0200

    improve stories

--- packages/react-components/react-portal/stories/src/Portal/PortalDefault.stories.tsx
+++ packages/react-components/react-portal/stories/src/Portal/PortalDefault.stories.tsx
@@ -1,32 +1,72 @@
 import * as React from 'react';
-import { makeStyles, tokens, Portal } from '@fluentui/react-components';
+import { ToggleButton, makeStyles, tokens, Portal, typographyStyles } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
-  content: {
-    backgroundColor: tokens.colorPaletteYellowBackground3,
-    border: '3px dashed',
-    padding: '5px',
-  },
   container: {
-    border: '3px dashed',
-    padding: '5px',
+    display: 'flex',
+    gap: '8px',
+  },
+  controls: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    minWidth: '200px',
+  },
+  state: {
+    fontFamily: tokens.fontFamilyMonospace,
+    border: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
+    padding: '4px',
+    textAlign: 'center',
+  },
+  clippingContainer: {
+    ...typographyStyles.subtitle2,
+
+    backgroundColor: tokens.colorNeutralBackground1,
+    color: tokens.colorNeutralForeground1,
+    border: `${tokens.strokeWidthThick} solid ${tokens.colorNeutralStroke1}`,
+    padding: '24px 8px',
+
+    overflow: 'hidden',
+  },
+  portalContent: {
+    ...typographyStyles.subtitle1,
+
+    backgroundColor: tokens.colorBrandBackground2,
+    color: tokens.colorBrandForeground2,
+    border: `${tokens.strokeWidthThick} dashed ${tokens.colorBrandStroke2}`,
+    padding: '12px 6px',
   },
 });
 
 export const Default = () => {
   const styles = useStyles();
-  const [rootElement, setRootElement] = React.useState<HTMLElement | null>(null);
+
+  const [mountNode, setMountNode] = React.useState<HTMLElement | null>(null);
+  const [open, setOpen] = React.useState(false);
 
   return (
-    <>
-      <div className={styles.container} style={{ overflow: 'hidden' }}>
-        Clipping parent container
-        <Portal mountNode={rootElement}>
-          <div className={styles.content}>Portal content</div>
-        </Portal>
+    <React.StrictMode>
+      <div className={styles.container}>
+        <div className={styles.clippingContainer}>
+          <span>Clipping parent container</span>
+
+          {open && mountNode && (
+            <Portal mountNode={mountNode}>
+              <div className={styles.portalContent}>Portal content</div>
+            </Portal>
+          )}
+        </div>
+
+        <div className={styles.controls}>
+          <ToggleButton checked={open} onClick={() => setOpen(!open)}>
+            Toggle portal
+          </ToggleButton>
+
+          <code className={styles.state}>{JSON.stringify({ open }, null, 2)}</code>
+        </div>
       </div>
 
-      <div ref={setRootElement} />
-    </>
+      <div ref={setMountNode} />
+    </React.StrictMode>
   );
 };
