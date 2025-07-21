86eafaf557c95bcf4003d6e4d6d533e1dfa17155
Adam <adam.samec@gmail.com>
2025-07-21 19:17:46 +0200

    Fix Menu button opening on arrow down

--- packages/react-components/react-menu/stories/src/MenuGrid/MenuGridWithSubmenu.stories.tsx
+++ packages/react-components/react-menu/stories/src/MenuGrid/MenuGridWithSubmenu.stories.tsx
@@ -1,8 +1,35 @@
 import * as React from 'react';
 import { Button, Menu, MenuTrigger, MenuList, MenuItem, MenuPopover } from '@fluentui/react-components';
+import type { MenuProps } from '@fluentui/react-components';
 import { MenuGrid, MenuGridCell, MenuGridRow } from '@fluentui/react-menu';
 
 const items = ['Olivia Carter', 'Liam Thompson', 'Sophia Martinez', 'Noah Patel', 'Emma Robinson'];
+
+const Submenu = () => {
+  const [open, setOpen] = React.useState(false);
+  const onOpenChange: MenuProps['onOpenChange'] = (e, data) => {
+    if (e.type === 'keydown' && (e as KeyboardEvent).key === 'ArrowDown') {
+      return;
+    }
+    setOpen(data.open);
+  };
+
+  return (
+    <Menu open={open} onOpenChange={onOpenChange} positioning={{ autoSize: true }}>
+      <MenuTrigger disableButtonEnhancement>
+        <Button>More actions</Button>
+      </MenuTrigger>
+      <MenuPopover>
+        <MenuList>
+          <MenuItem>Show profile </MenuItem>
+          <MenuItem>Audio call</MenuItem>
+          <MenuItem>Video call</MenuItem>
+          <MenuItem>Remove</MenuItem>
+        </MenuList>
+      </MenuPopover>
+    </Menu>
+  );
+};
 
 export const WithSubmenu = () => {
   return (
@@ -11,19 +38,7 @@ export const WithSubmenu = () => {
         <MenuGridRow key={index} aria-label={name}>
           <MenuGridCell>{name}</MenuGridCell>
           <MenuGridCell>
-            <Menu positioning={{ autoSize: true }}>
-              <MenuTrigger disableButtonEnhancement>
-                <Button>More actions</Button>
-              </MenuTrigger>
-              <MenuPopover>
-                <MenuList>
-                  <MenuItem>Show profile </MenuItem>
-                  <MenuItem>Audio call</MenuItem>
-                  <MenuItem>Video call</MenuItem>
-                  <MenuItem>Remove</MenuItem>
-                </MenuList>
-              </MenuPopover>
-            </Menu>
+            <Submenu />
           </MenuGridCell>
         </MenuGridRow>
       ))}
