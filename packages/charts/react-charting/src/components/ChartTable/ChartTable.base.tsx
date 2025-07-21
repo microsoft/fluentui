8adf7bb6acf7261be4f901b39722453941fbe250
Anush <anushgupta@microsoft.com>
2025-07-21 12:12:17 +0530

    make logic change

--- packages/charts/react-charting/src/components/ChartTable/ChartTable.base.tsx
+++ packages/charts/react-charting/src/components/ChartTable/ChartTable.base.tsx
@@ -17,20 +17,20 @@ function invertHexColor(hex: string, theme: ITheme): string {
   return d3.rgb(255 - rgb.r, 255 - rgb.g, 255 - rgb.b).formatHex();
 }
 
-function getSafeTextColor(theme: ITheme, foreground?: string, background?: string): string {
-  const fallbackColor = theme.semanticColors.bodyText!;
+function getSafeBackgroundColor(theme: ITheme, foreground?: string, background?: string): string {
+  const fallbackFg = theme.semanticColors.bodyText!;
   const fallbackBg = theme.semanticColors.bodyBackground!;
 
-  const fg = d3.color(foreground || fallbackColor)!;
+  const fg = d3.color(foreground || fallbackFg)!;
   const bg = d3.color(background || fallbackBg)!;
   const contrast = getColorContrast(fg.formatHex(), bg.formatHex());
   if (contrast >= 3) {
-    return fg.formatHex();
+    return bg.formatHex();
   }
 
-  const inverted = invertHexColor(fg.formatHex(), theme);
-  const invertedContrast = getColorContrast(inverted, bg.formatHex());
-  return invertedContrast >= 3 ? inverted : fallbackColor;
+  const invertedBg = invertHexColor(bg.formatHex(), theme);
+  const invertedContrast = getColorContrast(fg.formatHex(), invertedBg);
+  return invertedContrast >= 3 ? invertedBg : fallbackBg;
 }
 
 const getClassNames = classNamesFunction<IChartTableStyleProps, IChartTableStyles>();
@@ -88,7 +88,7 @@ export class ChartTableBase extends React.Component<IChartTableProps> {
                       const fg = style.color;
                       const bg = style.backgroundColor;
                       if (fg || bg) {
-                        style.color = getSafeTextColor(theme!, fg, bg);
+                        style.backgroundColor = getSafeBackgroundColor(theme!, fg, bg);
                       }
                       return (
                         <th key={idx} className={classNames.headerCell} style={style}>
@@ -107,7 +107,7 @@ export class ChartTableBase extends React.Component<IChartTableProps> {
                           const fg = style.color;
                           const bg = style.backgroundColor;
                           if (fg || bg) {
-                            style.color = getSafeTextColor(theme!, fg, bg);
+                            style.backgroundColor = getSafeBackgroundColor(theme!, fg, bg);
                           }
                           return (
                             <td key={colIdx} className={classNames.bodyCell} style={style}>
