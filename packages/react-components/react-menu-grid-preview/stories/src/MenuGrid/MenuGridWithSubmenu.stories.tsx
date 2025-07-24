6c09b520d21fab9b99de9e2a602e767283cbdd87
Adam <adam.samec@gmail.com>
2025-07-24 17:44:14 +0200

    Feature(react-menu-grid-preview): Add Default and WithSubmenu stories to MenuGrid storybook

---
+++ packages/react-components/react-menu-grid-preview/stories/src/MenuGrid/MenuGridWithSubmenu.stories.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Button, Menu, MenuTrigger, MenuList, MenuItem, MenuPopover } from '@fluentui/react-components';
+import type { MenuProps } from '@fluentui/react-components';
+import { MenuGrid, MenuGridCell, MenuGridRow } from '@fluentui/react-menu-grid-preview';
+
+const items = ['Olivia Carter', 'Liam Thompson', 'Sophia Martinez', 'Noah Patel', 'Emma Robinson'];
+
+const Submenu = () => {
+  const [open, setOpen] = React.useState(false);
+  const onOpenChange: MenuProps['onOpenChange'] = (e, data) => {
+    if (data.type === 'menuTriggerKeyDown' && (e as KeyboardEvent).key === 'ArrowDown') {
+      return;
+    }
+    setOpen(data.open);
+  };
+
+  return (
+    <Menu open={open} onOpenChange={onOpenChange} positioning={{ autoSize: true }}>
+      <MenuTrigger disableButtonEnhancement>
+        <Button>More actions</Button>
+      </MenuTrigger>
+      <MenuPopover>
+        <MenuList>
+          <MenuItem>Show profile </MenuItem>
+          <MenuItem>Audio call</MenuItem>
+          <MenuItem>Video call</MenuItem>
+          <MenuItem>Remove</MenuItem>
+        </MenuList>
+      </MenuPopover>
+    </Menu>
+  );
+};
+
+export const WithSubmenu = () => {
+  return (
+    <Menu>
+      <MenuTrigger disableButtonEnhancement>
+        <Button>Chat participants</Button>
+      </MenuTrigger>
+      <MenuPopover>
+        <MenuGrid>
+          {items.map((name, index) => (
+            <MenuGridRow key={index} aria-label={name}>
+              <MenuGridCell>{name}</MenuGridCell>
+              <MenuGridCell>
+                <Submenu />
+              </MenuGridCell>
+            </MenuGridRow>
+          ))}
+        </MenuGrid>
+      </MenuPopover>
+    </Menu>
+  );
+};
+
+WithSubmenu.parameters = {
+  docs: {
+    description: {
+      story: [
+        'If you need to provide a submenu for a `MenuGrid` item, use a menu button, e.g. "More actions", placed into its own `MenuGridCell`',
+      ].join('\n'),
+    },
+  },
+};
---
+++ packages/react-components/react-menu-grid-preview/stories/src/MenuGrid/index.stories.tsx
@@ -0,0 +1,29 @@
+import {
+  MenuGrid,
+  MenuGridCell,
+  MenuGridRow,
+  MenuGridRowGroup,
+  MenuGridRowGroupHeader,
+} from '@fluentui/react-menu-grid-preview';
+import descriptionMd from './MenuGridDescription.md';
+
+export { Default } from './MenuGridDefault.stories';
+export { WithSubmenu } from './MenuGridWithSubmenu.stories';
+
+export default {
+  title: 'Components/Menu/MenuGrid',
+  component: MenuGrid,
+  subcomponents: {
+    MenuGridCell,
+    MenuGridRow,
+    MenuGridRowGroup,
+    MenuGridRowGroupHeader,
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: [descriptionMd].join('\n'),
+      },
+    },
+  },
+};
