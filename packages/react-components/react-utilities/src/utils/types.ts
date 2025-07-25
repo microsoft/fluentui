4a1d378d4694d6fa890ef9c653495fe14edb613d
Martin Hochel <martinhochel@microsoft.com>
2025-07-25 14:37:59 +0200

    feat(react-utilities): add custom `JSX.*` types that are compatible with various react major versions (#34733)

--- packages/react-components/react-utilities/src/utils/types.ts
+++ packages/react-components/react-utilities/src/utils/types.ts
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import type { JSXIntrinsicElementKeysCompat, JSXIntrinsicElementKeysLatest } from './generated-types';
 
 /**
  * Helper type that works similar to Omit,
@@ -134,3 +135,45 @@ export type PropsWithoutChildren<P> = 'children' extends keyof P ? DistributiveO
  * **THIS TYPE IS INTERNAL AND SHOULD NEVER BE EXPOSED**
  */
 export type ReactVersionDependent<Modern, Legacy> = {} extends React.ReactNode ? Legacy : Modern;
+
+/**
+ * Our own alias for `JSX.Element` type that is compatible with both React 17 and React 18+.
+ * Use this type when annotating JSX markup in all our code in order to avoid issues between different React versions.
+ *
+ * Example usage:
+ *
+ * BAD:
+ * ```tsx
+ * const renderFoo = (state: FooState) = <div {...props}>Hello World</div>;
+ * // infers
+ * // R17:  declare const renderFoo: (state: FooState) => JSX.Element;
+ * // R18+: declare const renderFoo: (state: FooState) => React.JSX.Element;
+ * ```
+ *
+ * GOOD:
+ * ```tsx
+ * import type { JSXElement } from '@fluentui/react-utilities';
+ * const renderFoo = (state: FooState): JSXElement = <div {...props}>Hello World</div>;
+ * ```
+ */
+export type JSXElement = React.ReactElement<
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  any,
+  any
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+>;
+
+/**
+ * Union of all intrinsic element keys in React.
+ * Based on the React version, it will either be the latest set of intrinsic elements or the legacy set.
+ */
+export type JSXIntrinsicElementKeys = ReactVersionDependent<
+  JSXIntrinsicElementKeysLatest,
+  JSXIntrinsicElementKeysCompat
+>;
+
+/**
+ * Our own alias for `JSX.IntrinsicElements` type that is compatible with both React 17 and React 18+.
+ * Use this type to get the intrinsic elements from React types in order to avoid issues between different React versions.
+ */
+export type JSXIntrinsicElement<Element extends JSXIntrinsicElementKeys> = React.ComponentProps<Element>;
