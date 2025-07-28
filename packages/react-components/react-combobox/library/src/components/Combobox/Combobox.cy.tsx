309b9dab3598e584fbf61a91c27f71c504e120a6
Martin Hochel <martinhochel@microsoft.com>
2025-07-28 14:04:49 +0200

    chore: migrate to R19 compatible JSX.* namespace types in cy and story files

--- packages/react-components/react-combobox/library/src/components/Combobox/Combobox.cy.tsx
+++ packages/react-components/react-combobox/library/src/components/Combobox/Combobox.cy.tsx
@@ -8,11 +8,9 @@ import { Combobox, Option } from '@fluentui/react-combobox';
 import type { ComboboxProps } from '@fluentui/react-combobox';
 
 import { triggerSelector, listboxSelector, triggerId } from '../../testing/selectors';
+import type { JSXElement } from '@fluentui/react-utilities';
 
-const mount = (
-  element: // eslint-disable-next-line @typescript-eslint/no-deprecated
-  JSX.Element,
-) => {
+const mount = (element: JSXElement) => {
   mountBase(<FluentProvider theme={teamsLightTheme}>{element}</FluentProvider>);
 };
 
--- packages/react-components/react-table/stories/src/Table/ResizableColumnsDisableAutoFit.stories.tsx
+++ packages/react-components/react-table/stories/src/Table/ResizableColumnsDisableAutoFit.stories.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+// eslint-disable-next-line @fluentui/no-restricted-imports
+import type { JSXElement } from '@fluentui/react-utilities';
 import {
   Table,
   TableBody,
@@ -50,7 +52,7 @@ const columnsDef: TableColumnDefinition<Item>[] = [
 
 type FileCell = {
   label: string;
-  icon: JSX.Element; // eslint-disable-line @typescript-eslint/no-deprecated
+  icon: JSXElement;
 };
 
 type LastUpdatedCell = {
@@ -60,7 +62,7 @@ type LastUpdatedCell = {
 
 type LastUpdateCell = {
   label: string;
-  icon: JSX.Element; // eslint-disable-line @typescript-eslint/no-deprecated
+  icon: JSXElement;
 };
 
 type AuthorCell = {
