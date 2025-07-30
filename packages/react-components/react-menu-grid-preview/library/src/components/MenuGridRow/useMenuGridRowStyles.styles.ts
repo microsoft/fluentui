799878c7751e6b96c396de7a48eb38fc0552ea92
Juraj Kapsiar <jukapsia@microsoft.com>
2025-07-30 14:04:42 +0200

    chore(MenuGridRow): base styles

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRowStyles.styles.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRowStyles.styles.ts
@@ -1,15 +1,72 @@
 import type { SlotClassNames } from '@fluentui/react-utilities';
-import { mergeClasses } from '@griffel/react';
+import { mergeClasses, makeResetStyles } from '@griffel/react';
+import { iconFilledClassName, iconRegularClassName } from '@fluentui/react-icons';
 import type { MenuGridRowSlots, MenuGridRowState } from './MenuGridRow.types';
+import { tokens } from '@fluentui/react-theme';
+import { createFocusOutlineStyle } from '@fluentui/react-tabster';
 
 export const menuGridRowClassNames: SlotClassNames<MenuGridRowSlots> = {
   root: 'fui-MenuGridRow',
 };
 
+// Base styles similar to regular menu item
+// Right now ecludes icon color, hover active style for secondary text
+const useRootBaseStyles = makeResetStyles({
+  borderRadius: tokens.borderRadiusMedium,
+  position: 'relative',
+  color: tokens.colorNeutralForeground2,
+  backgroundColor: tokens.colorNeutralBackground1,
+  paddingRight: tokens.spacingVerticalSNudge, // 6px
+  paddingLeft: tokens.spacingVerticalSNudge,
+  paddingTop: tokens.spacingVerticalSNudge,
+  paddingBottom: tokens.spacingVerticalSNudge,
+  boxSizing: 'border-box',
+  maxWidth: '290px',
+  minHeight: '32px',
+  flexShrink: 0,
+  display: 'flex',
+  alignItems: 'start',
+  fontSize: tokens.fontSizeBase300,
+  cursor: 'pointer',
+  gap: '4px',
+
+  ':hover': {
+    backgroundColor: tokens.colorNeutralBackground1Hover,
+    color: tokens.colorNeutralForeground2Hover,
+
+    [`& .${iconFilledClassName}`]: {
+      display: 'inline',
+    },
+    [`& .${iconRegularClassName}`]: {
+      display: 'none',
+    },
+  },
+
+  ':hover:active': {
+    backgroundColor: tokens.colorNeutralBackground1Pressed,
+    color: tokens.colorNeutralForeground2Pressed,
+  },
+
+  // High contrast styles
+  '@media (forced-colors: active)': {
+    ':hover': {
+      backgroundColor: 'Canvas',
+      borderColor: 'Highlight',
+      color: 'Highlight',
+    },
+    ...createFocusOutlineStyle({ style: { outlineColor: 'Highlight' } }),
+  },
+
+  userSelect: 'none',
+  ...createFocusOutlineStyle(),
+});
+
+
 export const useMenuGridRowStyles_unstable = (state: MenuGridRowState): MenuGridRowState => {
   'use no memo';
 
-  state.root.className = mergeClasses(menuGridRowClassNames.root, state.root.className);
+  const rootBaseStyles = useRootBaseStyles();
+  state.root.className = mergeClasses(menuGridRowClassNames.root, rootBaseStyles, state.root.className);
 
   return state;
 };
--- packages/react-components/react-menu-grid-preview/stories/src/MenuGrid/MenuGridDefault.stories.tsx
+++ packages/react-components/react-menu-grid-preview/stories/src/MenuGrid/MenuGridDefault.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Button, Menu, MenuPopover, MenuTrigger } from '@fluentui/react-components';
 import { MenuGrid, MenuGridCell, MenuGridRow } from '@fluentui/react-menu-grid-preview';
+import { DeleteRegular, GlobePersonRegular } from '@fluentui/react-icons';
 
 const items = ['Olivia Carter', 'Liam Thompson', 'Sophia Martinez', 'Noah Patel', 'Emma Robinson'];
 
@@ -16,11 +17,11 @@ export const Default = () => {
             <MenuGridRow key={index} aria-label={name}>
               <MenuGridCell>{name}</MenuGridCell>
               <MenuGridCell>
-                <Button aria-label={`Profile card for ${name}`}>Avatar icon</Button>
+                <Button size="small" appearance="transparent" icon={<GlobePersonRegular />} aria-label={`Profile card for ${name}`}></Button>
               </MenuGridCell>
               <MenuGridCell>
-                <Button>Remove {name}</Button>
-              </MenuGridCell>
+                <Button size="small" appearance='transparent' icon={<DeleteRegular />} aria-label={`Remove ${name}`}></Button>
+               </MenuGridCell>
             </MenuGridRow>
           ))}
         </MenuGrid>
