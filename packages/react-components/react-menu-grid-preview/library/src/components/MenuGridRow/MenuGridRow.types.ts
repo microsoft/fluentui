379d34ee555f33e6c1078e933902d680f2239fda
Adam <adam.samec@gmail.com>
2025-08-05 17:36:41 +0200

    Feature(react-menu-grid-preview): Add row slots

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/MenuGridRow.types.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/MenuGridRow.types.ts
@@ -1,9 +1,37 @@
 import type { ComponentProps, ComponentState, Slot } from '@fluentui/react-utilities';
+import { MenuGridCellProps } from './../MenuGridCell/MenuGridCell.types';
 
 export type MenuGridRowSlots = {
   root: Slot<'div'>;
+
+  /**
+   * Icon slot rendered as cell before content cell
+   */
+  iconCell?: Slot<MenuGridCellProps>;
+
+  /**
+   * Component children are placed in this slot
+   * Avoid using the `children` property in this slot in favour of Component children whenever possible
+   */
+  contentCell?: Slot<MenuGridCellProps>;
+
+  /**
+   * Additional descriptor to main content that creates a multiline layout
+   */
+  subText?: Slot<'span'>;
+
+  /**
+   * Second action slot rendered as cell after content cell
+   */
+  secondActionCell?: Slot<MenuGridCellProps>;
+
+  /**
+   * Third action slot rendered as cell after second action cell
+   */
+  thirdActionCell?: Slot<MenuGridCellProps>;
 };
 
-export type MenuGridRowProps = ComponentProps<MenuGridRowSlots>;
+export type MenuGridRowProps = Omit<ComponentProps<Partial<MenuGridRowSlots>>, 'contentCell'> &
+  Pick<Partial<MenuGridRowSlots>, 'contentCell'>;
 
 export type MenuGridRowState = ComponentState<MenuGridRowSlots>;
--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRow.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRow.ts
@@ -3,16 +3,26 @@ import { getIntrinsicElementProps, slot } from '@fluentui/react-utilities';
 
 import { useMenuGridContext_unstable } from '../../contexts/menuGridContext';
 import { MenuGridRowProps, MenuGridRowState } from './MenuGridRow.types';
+import { MenuGridCell } from './../MenuGridCell/MenuGridCell';
 
 /**
  * Given user props, returns state and render function for a MenuGridRow.
  */
 export function useMenuGridRow_unstable(props: MenuGridRowProps, ref: React.Ref<HTMLDivElement>): MenuGridRowState {
+  // const {
+  // content: _content, // `content` is a slot and it's type clashes with the HTMLElement `content` attribute
+  // ...rest
+  // } = props;
   const { tableRowTabsterAttribute } = useMenuGridContext_unstable();
 
   return {
     components: {
       root: 'div',
+      iconCell: MenuGridCell,
+      contentCell: MenuGridCell,
+      subText: 'span',
+      secondActionCell: MenuGridCell,
+      thirdActionCell: MenuGridCell,
     },
     root: slot.always(
       getIntrinsicElementProps('div', {
@@ -24,5 +34,14 @@ export function useMenuGridRow_unstable(props: MenuGridRowProps, ref: React.Ref<
       }),
       { elementType: 'div' },
     ),
+    iconCell: slot.optional(props.iconCell, { elementType: MenuGridCell }),
+    contentCell: slot.optional(props.content, {
+      renderByDefault: !!props.children,
+      defaultProps: { children: props.children },
+      elementType: MenuGridCell,
+    }),
+    subText: slot.optional(props.subText, { elementType: 'span' }),
+    secondActionCell: slot.optional(props.secondActionCell, { elementType: MenuGridCell }),
+    thirdActionCell: slot.optional(props.thirdActionCell, { elementType: MenuGridCell }),
   };
 }
