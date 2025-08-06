64d4b18e0b06d85399eb7220f54b96a9ca98d2e0
Patrycja Fogelman <patrycja@fogelman.pl>
2025-08-06 15:45:10 +0200

    Fixed no onNavigate event not firing when clicked on a leaf (#34964)
    Co-authored-by: Patrycja Fogelman <patrycja.fogelman@microsoft.com>

--- packages/react-components/react-tree/library/src/components/TreeItem/useTreeItem.tsx
+++ packages/react-components/react-tree/library/src/components/TreeItem/useTreeItem.tsx
@@ -122,7 +122,7 @@ export function useTreeItem_unstable(props: TreeItemProps, ref: React.Ref<HTMLDi
     } else if (!isEventFromExpandIcon) {
       onClick?.(event);
     }
-    if (event.isDefaultPrevented() || itemType === 'leaf') {
+    if (event.isDefaultPrevented()) {
       return;
     }
 
@@ -134,12 +134,14 @@ export function useTreeItem_unstable(props: TreeItemProps, ref: React.Ref<HTMLDi
         target: event.currentTarget,
         type: isEventFromExpandIcon ? treeDataTypes.ExpandIconClick : treeDataTypes.Click,
       } as const;
-      props.onOpenChange?.(event, data);
-      requestTreeResponse({
-        ...data,
-        itemType,
-        requestType: 'open',
-      });
+      if (itemType !== 'leaf') {
+        props.onOpenChange?.(event, data);
+        requestTreeResponse({
+          ...data,
+          itemType,
+          requestType: 'open',
+        });
+      }
       requestTreeResponse({
         ...data,
         itemType,
