df2029a855f21b8c87dc4b0490276b8f6f5746a2
Juraj Kapsiar <jukapsia@microsoft.com>
2025-08-11 07:46:25 +0200

    border-box

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridCell/useMenuGridCellStyles.styles.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridCell/useMenuGridCellStyles.styles.ts
@@ -1,15 +1,23 @@
 import type { SlotClassNames } from '@fluentui/react-utilities';
-import { mergeClasses } from '@griffel/react';
+import { makeStyles, mergeClasses } from '@griffel/react';
 import type { MenuGridCellSlots, MenuGridCellState } from './MenuGridCell.types';
 
 export const menuGridCellClassNames: SlotClassNames<MenuGridCellSlots> = {
   root: 'fui-MenuGridCell',
 };
 
+const useStyles = makeStyles({
+  root: {
+    boxSizing: 'border-box',
+  },
+});
+
 export const useMenuGridCellStyles_unstable = (state: MenuGridCellState): MenuGridCellState => {
   'use no memo';
 
-  state.root.className = mergeClasses(menuGridCellClassNames.root, state.root.className);
+  const classes = useStyles();
+
+  state.root.className = mergeClasses(menuGridCellClassNames.root, classes.root, state.root.className);
 
   return state;
 };
