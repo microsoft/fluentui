e81e4694b47d90f3375db3873ccce8ca93ef0db5
Zacky Ma <machi@microsoft.com>
2025-08-12 10:44:53 -0700

    feat: support SSR in dialog, drawer, field, slider, textarea (#34996)

--- packages/web-components/src/dialog/dialog.template.ts
+++ packages/web-components/src/dialog/dialog.template.ts
@@ -8,11 +8,8 @@ import { DialogType } from './dialog.options.js';
  */
 export const template: ElementViewTemplate<Dialog> = html`
   <dialog
-    role="${x => (x.type === DialogType.alert ? 'alertdialog' : 'dialog')}"
-    type="${x => x.type}"
     class="dialog"
     part="dialog"
-    aria-modal="${x => (x.type === DialogType.modal || x.type === DialogType.alert ? 'true' : void 0)}"
     aria-describedby="${x => x.ariaDescribedby}"
     aria-labelledby="${x => x.ariaLabelledby}"
     aria-label="${x => x.ariaLabel}"
