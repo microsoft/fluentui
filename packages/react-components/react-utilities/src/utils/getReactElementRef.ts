be2a04aa08e21d27f57d97550945ceaa7bcb6a97
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-08-18 10:35:01 +0200

    update React version check to support versions higher than 19

--- packages/react-components/react-utilities/src/utils/getReactElementRef.ts
+++ packages/react-components/react-utilities/src/utils/getReactElementRef.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const IS_REACT_19 = React.version.startsWith('19.');
+const IS_REACT_19_OR_HIGHER = parseInt(React.version, 10) > 19;
 
 /**
  * Returns a ref for the React element in a backwards-compatible way.
@@ -13,7 +13,7 @@ export function getReactElementRef<T>(element: React.ReactElement | null | undef
     return undefined;
   }
 
-  if (IS_REACT_19) {
+  if (IS_REACT_19_OR_HIGHER) {
     return element.props.ref;
   }
 
