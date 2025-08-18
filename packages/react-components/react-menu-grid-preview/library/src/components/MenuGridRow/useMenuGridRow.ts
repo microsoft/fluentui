287d0029a3cd6d5068dd173407c8ecc084fe2996
Adam <adam.samec@gmail.com>
2025-08-18 17:55:26 +0200

    Feat(react-menu-grid-preview): Introduce MenuGridItem and change slot names (#35027)

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRow.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useMenuGridRow.ts
@@ -3,26 +3,16 @@ import { getIntrinsicElementProps, slot } from '@fluentui/react-utilities';
 
 import { useMenuGridContext_unstable } from '../../contexts/menuGridContext';
 import { MenuGridRowProps, MenuGridRowState } from './MenuGridRow.types';
-import { MenuGridCell } from './../MenuGridCell/MenuGridCell';
 
 /**
  * Given user props, returns state and render function for a MenuGridRow.
  */
 export function useMenuGridRow_unstable(props: MenuGridRowProps, ref: React.Ref<HTMLDivElement>): MenuGridRowState {
-  // const {
-  // content: _content, // `content` is a slot and it's type clashes with the HTMLElement `content` attribute
-  // ...rest
-  // } = props;
   const { tableRowTabsterAttribute } = useMenuGridContext_unstable();
 
   return {
     components: {
       root: 'div',
-      iconCell: MenuGridCell,
-      contentCell: MenuGridCell,
-      subText: 'span',
-      secondActionCell: MenuGridCell,
-      thirdActionCell: MenuGridCell,
     },
     root: slot.always(
       getIntrinsicElementProps('div', {
@@ -34,14 +24,5 @@ export function useMenuGridRow_unstable(props: MenuGridRowProps, ref: React.Ref<
       }),
       { elementType: 'div' },
     ),
-    iconCell: slot.optional(props.iconCell, { elementType: MenuGridCell }),
-    contentCell: slot.optional(props.contentCell, {
-      renderByDefault: !!props.children,
-      defaultProps: { children: props.children },
-      elementType: MenuGridCell,
-    }),
-    subText: slot.optional(props.subText, { elementType: 'span' }),
-    secondActionCell: slot.optional(props.secondActionCell, { elementType: MenuGridCell }),
-    thirdActionCell: slot.optional(props.thirdActionCell, { elementType: MenuGridCell }),
   };
 }
