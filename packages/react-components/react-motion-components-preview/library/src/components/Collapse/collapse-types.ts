d7be95074faf6c1f294706ebd783232021a2c804
Robert Penner <robertpenner@microsoft.com>
2025-08-26 17:34:47 +0000

    refactor(motion): simplify Collapse types

--- packages/react-components/react-motion-components-preview/library/src/components/Collapse/collapse-types.ts
+++ packages/react-components/react-motion-components-preview/library/src/components/Collapse/collapse-types.ts
@@ -1,26 +1,23 @@
-import type { BasePresenceParams, PresenceEasing, PresenceDelay, AnimateOpacity } from '../../types';
+import type { BasePresenceParams, AnimateOpacity } from '../../types';
 
 export type CollapseOrientation = 'horizontal' | 'vertical';
-
-/** Common properties shared by all collapse components */
-type CollapseBaseParams = PresenceEasing &
-  PresenceDelay &
-  AnimateOpacity & {
-    /** The orientation of the size animation. Defaults to `'vertical'` to expand/collapse the height. */
-    orientation?: CollapseOrientation;
-    /** The starting size for the expand animation. Defaults to `'0px'`. */
-    fromSize?: string;
-  };
 
 export type CollapseParams = BasePresenceParams &
   AnimateOpacity & {
     /** The orientation of the size animation. Defaults to `'vertical'` to expand/collapse the height. */
     orientation?: CollapseOrientation;
+
     /** The starting size for the expand animation. Defaults to `'0px'`. */
     fromSize?: string;
   };
 
-export type CollapseDelayedParams = CollapseBaseParams & {
+/**
+ * Collapse parameters with granular duration control.
+ *
+ * Omits `duration` and `exitDuration` from CollapseParams to allow independent
+ * control of size and opacity animation timing for staggered effects.
+ */
+export type CollapseDelayedParams = Omit<CollapseParams, 'duration' | 'exitDuration'> & {
   /** Time (ms) for the size expand. Defaults to the `durationNormal` value (200 ms). */
   sizeDuration?: number;
 
@@ -32,10 +29,4 @@ export type CollapseDelayedParams = CollapseBaseParams & {
 
   /** Time (ms) for the fade-out. Defaults to the `exitSizeDuration` param, to sync the fade-out with the collapse. */
   exitOpacityDuration?: number;
-
-  /** Time (ms) between the size expand start and the fade-in start. Defaults to `0`. */
-  delay?: number;
-
-  /** Time (ms) between the fade-out start and the size collapse start. Defaults to `0`. */
-  exitDelay?: number;
 };
