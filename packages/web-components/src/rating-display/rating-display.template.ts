e53420441448ba37d6ba7b6a93bcc034b36bcf67
Zacky Ma <machi@microsoft.com>
2025-08-29 15:43:31 -0700

    reduce more diffs

--- packages/web-components/src/rating-display/rating-display.template.ts
+++ packages/web-components/src/rating-display/rating-display.template.ts
@@ -19,11 +19,11 @@ export const defaultIconOutlined = `
  * @public
  */
 export function ratingDisplayTemplate<T extends RatingDisplay>(): ElementViewTemplate<T> {
-    return html<T>`
-      <div ${ref("display")} class="display" aria-hidden="true"></div>
-      <slot name="icon" ${ref("iconSlot")} @slotchange="${x => x.handleSlotChange()}"></slot>
-      <slot name="value"><span class="value-label" aria-hidden="true">${x => x.value}</span></slot>
-      <slot name="count"><span class="count-label" aria-hidden="true">${x => x.formattedCount}</span></slot>
+  return html<T>`
+    <div ${ref("display")} class="display" aria-hidden="true"></div>
+    <slot name="icon" ${ref("iconSlot")} @slotchange="${x => x.handleSlotChange()}"></slot>
+    <slot name="value"><span class="value-label" aria-hidden="true">${x => x.value}</span></slot>
+    <slot name="count"><span class="count-label" aria-hidden="true">${x => x.formattedCount}</span></slot>
   `;
 }
 
