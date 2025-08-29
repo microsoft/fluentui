2dc134f2e7364b2f3d2d2b61a084cebfd2b23855
mainframev <vgenaev@gmail.com>
2025-08-29 16:22:31 +0200

    fixup! docs(react-image): add migration guide from v8

--- apps/public-docsite-v9/src/Concepts/Migration/FromV8/Components/Image/FitDifferences.tsx
+++ apps/public-docsite-v9/src/Concepts/Migration/FromV8/Components/Image/FitDifferences.tsx
@@ -1,62 +1,7 @@
 import * as React from 'react';
 import { Image as ImageV9 } from '@fluentui/react-components';
-import { makeStyles, tokens, mergeClasses } from '@fluentui/react-components';
-
-const useStyles = makeStyles({
-  flexColumn: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  gridWrapper: {
-    display: 'grid',
-    margin: '60px 0',
-    gridTemplateColumns: 'repeat(2, 150px)',
-    justifyContent: 'center',
-    gap: '20px',
-  },
-  box: {
-    height: '150px',
-    width: '150px',
-    boxSizing: 'border-box',
-    border: '1px solid green',
-  },
-  imageBox: {
-    border: '1px dashed red',
-  },
-  imageWrapperV9: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  imageWrapperV8: {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'relative',
-    overflow: 'hidden',
-  },
-  imageV8: {
-    display: 'block',
-    position: 'absolute',
-    borderRadius: `${tokens.borderRadiusCircular}`,
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-  flexCenter: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  auto: {
-    height: 'auto',
-    width: 'auto',
-  },
-  caption: {
-    fontSize: '12px !important',
-    lineHeight: tokens.lineHeightBase200,
-    marginTop: '8px !important',
-    textAlign: 'center',
-  },
-});
+import { mergeClasses } from '@fluentui/react-components';
+import { useStyles } from './FitDifferences.styles';
 
 export const FitDifferences = () => {
   const styles = useStyles();
