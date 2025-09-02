71646c6b7e7c7411f64cd33a5c27e9ef933bddde
Martin Hochel <martinhochel@microsoft.com>
2025-09-02 13:43:19 +0200

    fixup! feat(scripts-cypress): expose custom mount which uses React Strict mode by default

--- scripts/cypress/src/index.ts
+++ scripts/cypress/src/index.ts
@@ -1,2 +1,7 @@
 export { baseConfig, baseWebpackConfig } from './base.config';
-export { mount } from './mount';
+
+// =========== BROWSER APIs ==================
+
+// TODO: Browser related APIs should be exposed via export maps or moved to separate package
+// Expose Browser specific API under same barrel
+export declare const mount: typeof import('./browser').mount;
