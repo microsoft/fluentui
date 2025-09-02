f73877cfe468eb98c0c6b8bea93bef0ba15868b5
Martin Hochel <martinhochel@microsoft.com>
2025-09-02 13:07:46 +0200

    e2e: use our mount within cypress component test to enforce test execution within React Strict mode

--- packages/react-components/react-utilities/src/hooks/useOnScrollOutside.cy.tsx
+++ packages/react-components/react-utilities/src/hooks/useOnScrollOutside.cy.tsx
@@ -1,4 +1,4 @@
-import { mount } from '@cypress/react';
+import { mount } from '@fluentui/scripts-cypress';
 import * as React from 'react';
 
 import { useOnScrollOutside } from './useOnScrollOutside';
