d6b9203e28a09baa467e63869d585ba0ee891802
Adam <adam.samec@gmail.com>
2025-09-02 15:06:43 +0200

    Fix(react-menu-grid-preview): Fix lint errors and format

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useCheckMenuGridRowNesting.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridRow/useCheckMenuGridRowNesting.ts
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 import { useCheckNesting } from '../../utils/useCheckNesting';
 
-export const useCheckMenuGridRowNesting = (ref: React.RefObject<HTMLElement>) => {
+export const useCheckMenuGridRowNesting = (ref: React.RefObject<HTMLElement>): void => {
   useCheckNesting(ref, 'MenuGridRow');
 };
