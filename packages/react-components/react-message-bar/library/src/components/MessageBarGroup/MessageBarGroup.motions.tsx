c959162abe2ce69ce85d1ba2bf64e32816b97510
Robert Penner <robertpenner@microsoft.com>
2025-09-09 18:44:44 +0000

    refactor(MessageBar): update motion atoms to latest implementation

--- packages/react-components/react-message-bar/library/src/components/MessageBarGroup/MessageBarGroup.motions.tsx
+++ packages/react-components/react-message-bar/library/src/components/MessageBarGroup/MessageBarGroup.motions.tsx
@@ -3,28 +3,45 @@ import { MessageBarGroupProps } from './MessageBarGroup.types';
 
 // TODO: import these atoms from react-motion-components-preview once they're available there
 
-interface FadeAtomParams {
+type BaseAtomParams = {
+  /** The functional direction of the motion: 'enter' or 'exit'. */
   direction: PresenceDirection;
+
+  /** The duration of the motion in milliseconds. */
   duration: number;
-  easing?: string;
-  fromValue?: number;
+
+  /** The easing curve for the motion. */
+  easing?: EffectTiming['easing'];
+
+  /** Time (ms) to delay the animation. */
+  delay?: EffectTiming['delay'];
+};
+
+interface FadeAtomParams extends BaseAtomParams {
+  /** Defines how values are applied before and after execution. Defaults to 'both'. */
+  fill?: FillMode;
+
+  /** The starting opacity value. Defaults to 0. */
+  fromOpacity?: number;
 }
 
 /**
- * Generates a motion atom object for a fade in or fade out.
+ * Generates a motion atom object for a fade-in or fade-out.
  * @param direction - The functional direction of the motion: 'enter' or 'exit'.
  * @param duration - The duration of the motion in milliseconds.
  * @param easing - The easing curve for the motion. Defaults to `motionTokens.curveLinear`.
- * @param fromValue - The starting opacity value. Defaults to 0.
+ * @param delay - The delay before the motion starts. Defaults to 0.
+ * @param fromOpacity - The starting opacity value. Defaults to 0.
  * @returns A motion atom object with opacity keyframes and the supplied duration and easing.
  */
-const fadeAtom = ({
+export const fadeAtom = ({
   direction,
   duration,
   easing = motionTokens.curveLinear,
-  fromValue = 0,
+  delay = 0,
+  fromOpacity = 0,
 }: FadeAtomParams): AtomMotion => {
-  const keyframes = [{ opacity: fromValue }, { opacity: 1 }];
+  const keyframes = [{ opacity: fromOpacity }, { opacity: 1 }];
   if (direction === 'exit') {
     keyframes.reverse();
   }
@@ -32,31 +49,36 @@ const fadeAtom = ({
     keyframes,
     duration,
     easing,
+    delay,
+    // Applying opacity backwards and forwards in time is important
+    // to avoid a bug where a delayed animation is not hidden when it should be.
+    fill: 'both',
   };
 };
 
+interface SlideAtomParams extends BaseAtomParams {
+  fromX?: string;
+  fromY?: string;
+}
+
 /**
- * Generates a motion atom object for an X or Y translation, from a specified distance to zero.
+ * Generates a motion atom object for a slide-in or slide-out.
  * @param direction - The functional direction of the motion: 'enter' or 'exit'.
- * @param axis - The axis of the translation: 'X' or 'Y'.
- * @param fromValue - The starting position of the slide; it can be a percentage or pixels.
  * @param duration - The duration of the motion in milliseconds.
- * @param easing - The easing curve for the motion. Defaults to `motionTokens.curveDecelerateMid`.
+ * @param easing - The easing curve for the motion. Defaults to `motionTokens.curveLinear`.
+ * @param fromX - The starting X translate value with units (e.g., '0px', '100%'). Defaults to '0px'.
+ * @param fromY - The starting Y translate value with units (e.g., '-20px', '100%'). Defaults to '0px'.
+ * @returns A motion atom object with translate keyframes and the supplied duration and easing.
  */
-const slideAtom = ({
+export const slideAtom = ({
   direction,
-  axis,
-  fromValue,
   duration,
-  easing = motionTokens.curveDecelerateMid,
-}: {
-  direction: PresenceDirection;
-  axis: 'X' | 'Y';
-  fromValue: string;
-  duration: number;
-  easing?: string;
-}): AtomMotion => {
-  const keyframes = [{ transform: `translate${axis}(${fromValue})` }, { transform: `translate${axis}(0)` }];
+  easing = motionTokens.curveLinear,
+  delay = 0,
+  fromX = '0px',
+  fromY = '20px',
+}: SlideAtomParams): AtomMotion => {
+  const keyframes = [{ translate: `${fromX} ${fromY}` }, { translate: '0px 0px' }];
   if (direction === 'exit') {
     keyframes.reverse();
   }
@@ -64,6 +86,7 @@ const slideAtom = ({
     keyframes,
     duration,
     easing,
+    delay,
   };
 };
 
@@ -81,10 +104,7 @@ export const MessageBarMotion = createPresenceComponent<{ animate?: MessageBarGr
       enter:
         animate === 'both'
           ? // enter with slide and fade
-            [
-              fadeAtom({ direction: 'enter', duration }),
-              slideAtom({ direction: 'enter', axis: 'Y', fromValue: '-100%', duration }),
-            ]
+            [fadeAtom({ direction: 'enter', duration }), slideAtom({ direction: 'enter', fromY: '-100%', duration })]
           : [], // no enter motion
 
       // Always exit with a fade
