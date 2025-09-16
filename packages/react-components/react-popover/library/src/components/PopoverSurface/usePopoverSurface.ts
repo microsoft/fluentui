5a918d4567c12acff3abf0ae78eef4537e5d6ede
Sean Monahan <seanmonahan@microsoft.com>
2025-09-16 23:03:45 +0000

    first pass on Popover and anchor positioning

--- packages/react-components/react-popover/library/src/components/PopoverSurface/usePopoverSurface.ts
+++ packages/react-components/react-popover/library/src/components/PopoverSurface/usePopoverSurface.ts
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { getIntrinsicElementProps, useMergedRefs, slot } from '@fluentui/react-utilities';
+import { getIntrinsicElementProps, useMergedRefs, slot, useFirstMount } from '@fluentui/react-utilities';
 import { useModalAttributes } from '@fluentui/react-tabster';
 import { usePopoverContext_unstable } from '../../popoverContext';
 import type { PopoverSurfaceProps, PopoverSurfaceState } from './PopoverSurface.types';
+import { resolvePositioningShorthand } from '../../../../../react-components/src/index';
 
 /**
  * Create the state required to render PopoverSurface.
@@ -17,8 +18,11 @@ export const usePopoverSurface_unstable = (
   props: PopoverSurfaceProps,
   ref: React.Ref<HTMLDivElement>,
 ): PopoverSurfaceState => {
+  const popoverId = usePopoverContext_unstable(context => context.popoverId);
+  const positioningCtx = usePopoverContext_unstable(context => context.positioning);
   const contentRef = usePopoverContext_unstable(context => context.contentRef);
   const openOnHover = usePopoverContext_unstable(context => context.openOnHover);
+  const open = usePopoverContext_unstable(context => context.open);
   const setOpen = usePopoverContext_unstable(context => context.setOpen);
   const mountNode = usePopoverContext_unstable(context => context.mountNode);
   const arrowRef = usePopoverContext_unstable(context => context.arrowRef);
@@ -27,15 +31,16 @@ export const usePopoverSurface_unstable = (
   const appearance = usePopoverContext_unstable(context => context.appearance);
   const trapFocus = usePopoverContext_unstable(context => context.trapFocus);
   const inertTrapFocus = usePopoverContext_unstable(context => context.inertTrapFocus);
-  const inline = usePopoverContext_unstable(context => context.inline);
   const { modalAttributes } = useModalAttributes({
     trapFocus,
     legacyTrapFocus: !inertTrapFocus,
     alwaysFocusable: !trapFocus,
   });
 
+  const surfaceRef = React.useRef();
+  const positioning = resolvePositioningShorthand(positioningCtx);
+
   const state: PopoverSurfaceState = {
-    inline,
     appearance,
     withArrow,
     size,
@@ -49,11 +54,20 @@ export const usePopoverSurface_unstable = (
         // FIXME:
         // `contentRef` is wrongly assigned to be `HTMLElement` instead of `HTMLDivElement`
         // but since it would be a breaking change to fix it, we are casting ref to it's proper type
-        ref: useMergedRefs(ref, contentRef) as React.Ref<HTMLDivElement>,
+        ref: useMergedRefs(ref, contentRef, surfaceRef) as React.Ref<HTMLDivElement>,
         role: trapFocus ? 'dialog' : 'group',
         'aria-modal': trapFocus ? true : undefined,
+        id: popoverId,
+        popover: openOnHover ? 'hint' : 'auto',
         ...modalAttributes,
         ...props,
+        style: {
+          positionAnchor: `--${popoverId}`,
+          positionArea: toPositionArea(positioning?.align, positioning?.position),
+          positionTryFallbacks: 'flip-block, flip-inline',
+          margin: 0,
+          ...props.style,
+        },
       }),
       { elementType: 'div' },
     ),
@@ -62,7 +76,8 @@ export const usePopoverSurface_unstable = (
   const {
     onMouseEnter: onMouseEnterOriginal,
     onMouseLeave: onMouseLeaveOriginal,
-    onKeyDown: onKeyDownOriginal,
+    // onKeyDown: onKeyDownOriginal,
+    onToggle: onToggleOriginal,
   } = state.root;
   state.root.onMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
     if (openOnHover) {
@@ -80,16 +95,70 @@ export const usePopoverSurface_unstable = (
     onMouseLeaveOriginal?.(e);
   };
 
-  state.root.onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    // only close if the event happened inside the current popover
-    // If using a stack of inline popovers, the user should call `stopPropagation` to avoid dismissing the entire stack
-    if (e.key === 'Escape' && contentRef.current?.contains(e.target as HTMLDivElement)) {
-      e.preventDefault();
-      setOpen(e, false);
-    }
+  // React 18 doesn't properly support `popover`
+  // so manually wire up the events :/
+  // See: https://github.com/facebook/react/issues/27479
+  // React.useEffect(() => {
+  //   const onToggle = (e: any) => {
+  //     if (e.oldState === 'open' && e.newState === 'closed' && e.target === state.root.ref?.current) {
+  //       setOpen(e, false);
+  //     }
 
-    onKeyDownOriginal?.(e);
-  };
+  //     onToggleOriginal?.(e);
+  //   };
+  //   surfaceRef.current?.addEventListener('toggle', onToggle);
+
+  //   return () => surfaceRef.current?.removeEventListener('toggle', onToggle);
+  // }, [onToggleOriginal]);
+
+  const isFirst = useFirstMount();
+  // Handle the default open case
+  React.useEffect(() => {
+    if (open && isFirst) {
+      surfaceRef.current?.showPopover();
+    }
+  }, [open, isFirst]);
 
   return state;
+};
+
+const getPositionMap = (rtl?: boolean): Record<Position, PlacementPosition> => ({
+  above: 'top',
+  below: 'bottom',
+  before: rtl ? 'right' : 'left',
+  after: rtl ? 'left' : 'right',
+});
+
+// Floating UI automatically flips alignment
+// https://github.com/floating-ui/floating-ui/issues/1563
+const getAlignmentMap = (): Record<Alignment, PlacementAlign | undefined> => ({
+  start: 'start',
+  end: 'end',
+  top: 'start',
+  bottom: 'end',
+  center: undefined,
+});
+
+const shouldAlignToCenter = (p?: Position, a?: Alignment): boolean => {
+  const positionedVertically = p === 'above' || p === 'below';
+  const alignedVertically = a === 'top' || a === 'bottom';
+
+  return (positionedVertically && alignedVertically) || (!positionedVertically && !alignedVertically);
+};
+
+const toPositionArea = (align?: Alignment, position?: Position, rtl?: boolean): string => {
+  const alignment = shouldAlignToCenter(position, align) ? 'center' : align;
+
+  const computedPosition = position && getPositionMap(rtl)[position];
+  const computedAlignment = alignment && getAlignmentMap()[alignment];
+
+  if (computedPosition && computedAlignment) {
+    return `${computedPosition} ${computedAlignment}` as Placement;
+  }
+
+  if (!computedPosition) {
+    return 'top span-all';
+  }
+
+  return computedPosition;
 };
