1920123aea1229ced8c77af3bb838224454fa246
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-10-05 10:21:15 +0200

    fix swc loader options for vr-tests

--- apps/vr-tests/.storybook/main.js
+++ apps/vr-tests/.storybook/main.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const { registerRules, registerTsPaths, rules } = require('@fluentui/scripts-storybook');
+const { option } = require('yargs');
 
 const tsConfigPath = path.resolve(__dirname, '../../../tsconfig.base.v8.json');
 module.exports = /** @type {import('@storybook/react-webpack5').StorybookConfig} */ ({
@@ -22,8 +23,39 @@ module.exports = /** @type {import('@storybook/react-webpack5').StorybookConfig}
     reactDocgen: false,
   },
   webpackFinal: config => {
+    const swcRule = {
+      test: /\.(([cm])?([jt])sx?)$/,
+      exclude: ['/node_modules/'],
+      use: [
+        {
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              target: 'es2019',
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+                decorators: true,
+                dynamicImport: true,
+              },
+              transform: {
+                decoratorMetadata: true,
+                legacyDecorator: true,
+              },
+              keepClassNames: true,
+              externalHelpers: true,
+              loose: true,
+              minify: {
+                mangle: false,
+              },
+            },
+          },
+        },
+      ],
+    };
+
     registerTsPaths({ config, configFile: tsConfigPath });
-    registerRules({ config, rules: [rules.swcRule, rules.scssRule] });
+    registerRules({ config, rules: [swcRule, rules.scssRule] });
 
     return config;
   },
