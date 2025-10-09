7ef9687c111bd056072d0acac0f2b9eb142e5245
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-10-10 00:01:41 +0200

    fix

--- packages/foundation-legacy/src/hooks/controlled.test.tsx
+++ packages/foundation-legacy/src/hooks/controlled.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useControlledState, getControlledDerivedProps } from './controlled';
 
 interface ITestProps {
