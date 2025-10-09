6e3a6f73c03fbd922e1aae2f4cfc4167f0422a5d
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-10-09 22:24:22 +0200

    fix

--- packages/react-components/react-storybook-addon/src/preset/manager.ts
+++ packages/react-components/react-storybook-addon/src/preset/manager.ts
@@ -8,20 +8,19 @@ import { DirectionSwitch } from '../components/DirectionSwitch';
 addons.register(ADDON_ID, () => {
   addons.add(THEME_ID, {
     title: 'Fluent Theme Picker',
-     
+
     type: types.TOOL,
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: ThemePicker,
   });
   addons.add(DIR_ID, {
     title: 'Direction Switch',
-     
+
     type: types.TOOL,
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: DirectionSwitch,
   });
   addons.add(STRICT_MODE_ID, {
-     
     type: types.TOOL,
     title: 'React Strict Mode',
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
