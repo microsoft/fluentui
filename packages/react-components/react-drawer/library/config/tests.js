920571cfdac6124ea4a3d810898673c5c08fdf02
Marcos Moura <marcosvmmoura@gmail.com>
2025-10-15 14:37:33 +0200

    fix: remove library for mock

--- packages/react-components/react-drawer/library/config/tests.js
+++ packages/react-components/react-drawer/library/config/tests.js
@@ -1,3 +1,24 @@
-const ResizeObserver = require('resize-observer-polyfill');
+/** Jest test setup file. */
 
-global.ResizeObserver = ResizeObserver;
+/**
+ * Mock ResizeObserver for test environment
+ *
+ * Required because DrawerBody component depends on ResizeObserver, but jsdom doesn't provide this browser API
+ */
+global.ResizeObserver = class ResizeObserver {
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  observe() {
+    // Do nothing in tests - we're only testing component behavior, not resize functionality
+  }
+
+  unobserve() {
+    // Do nothing in tests
+  }
+
+  disconnect() {
+    // Do nothing in tests
+  }
+};
