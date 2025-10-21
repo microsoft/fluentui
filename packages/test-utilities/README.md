b7d43051d434184a7cfd3b761b3442e9b29c9840
Martin Hochel <martinhochel@microsoft.com>
2025-10-21 16:08:27 +0200

    chore: revert runtime assertions

--- packages/test-utilities/README.md
+++ packages/test-utilities/README.md
@@ -2,15 +2,18 @@
 
 Set of common test utilities for testing code within the Fluent UI React repo.
 
-**React support:** 16 and 17.
+**React support:**
+
+- 16 and 17
+- `>17` limited
 
 > [!WARNING]
-> ⚠️ Most of this package public API is deprecated and won't work with new React major versions. Pleas migrate to `@testing-library/react` instead
+> ⚠️ Most of this package public API is deprecated and may not work with new React major versions. Pleas migrate to `@testing-library/react` instead
 >
-> If you gonna run incompatible APIs with new React major versions those will throw Runtime Error with appropriate message.
+> To learn more see https://react.dev/warnings/react-test-renderer
 >
 > **Affected APIs:**
 >
-> - `create` / wont work with React >=19
-> - `safeCreate` / wont work with React >=19
+> - `create` / may not work with React >=19
+> - `safeCreate` / may not work with React >=19
 > - `safeMount` / wont work with React >=18
--- packages/test-utilities/src/deprecated/safeCreate.ts
+++ packages/test-utilities/src/deprecated/safeCreate.ts
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import { assertReactVersion } from './assertReactVersion';
 
 /**
  * Calls renderer.create from react-test-renderer, then calls the callback,
  * then unmounts. This prevents mounted components from sitting around doing
  * unfathomable things in the background while other tests start executing.
  *
- * @deprecated Use `@testing-library/react` directly instead. This function uses `react-test-renderer` which doesn't work with react >= v19
+ * @deprecated Use `@testing-library/react` directly instead. This function uses `react-test-renderer` which may not work with react >= v19
  *
  * @param content - JSX content to test.
  * @param callback - Function callback which receives the component to use.
@@ -17,7 +16,6 @@ export function safeCreate<TProps>(
   callback: (wrapper: renderer.ReactTestRenderer) => void,
   options?: renderer.TestRendererOptions,
 ): void {
-  assertReactVersion(19);
   let wrapper: renderer.ReactTestRenderer;
   /* eslint-disable @typescript-eslint/no-deprecated */
   renderer.act(() => {
