6470a95c1b0bb37c9ee11f400ff67ffd3eedb7c3
Mitch-At-Work <99835933+Mitch-At-Work@users.noreply.github.com>
2025-10-24 09:29:10 -0700

    Format

--- packages/semantic-tokens/src/controls/compoundbutton/tokens.ts
+++ packages/semantic-tokens/src/controls/compoundbutton/tokens.ts
@@ -1,20 +1,30 @@
 export const ctrlCompoundbuttonIconSize = 'var(--smtc-ctrl-compoundbutton-icon-size, 40px)';
 export const ctrlCompoundbuttonGap = 'var(--smtc-ctrl-compoundbutton-gap, var(--spacingHorizontalM))';
-export const ctrlCompoundbuttonSmallPaddingHorizontal = 'var(--smtc-ctrl-compoundbutton-small-padding-horizontal, var(--spacingHorizontalS))';
-export const ctrlCompoundbuttonSmallPaddingTop = 'var(--smtc-ctrl-compoundbutton-small-padding-top, var(--spacingHorizontalS))';
-export const ctrlCompoundbuttonSmallPaddingBottom = 'var(--smtc-ctrl-compoundbutton-small-padding-bottom, var(--spacingHorizontalMNudge))';
+export const ctrlCompoundbuttonSmallPaddingHorizontal =
+  'var(--smtc-ctrl-compoundbutton-small-padding-horizontal, var(--spacingHorizontalS))';
+export const ctrlCompoundbuttonSmallPaddingTop =
+  'var(--smtc-ctrl-compoundbutton-small-padding-top, var(--spacingHorizontalS))';
+export const ctrlCompoundbuttonSmallPaddingBottom =
+  'var(--smtc-ctrl-compoundbutton-small-padding-bottom, var(--spacingHorizontalMNudge))';
 export const ctrlCompoundbuttonSmallMinwidth = 'var(--smtc-ctrl-compoundbutton-small-minwidth)';
 export const ctrlCompoundbuttonSmallMinheight = 'var(--smtc-ctrl-compoundbutton-small-minheight)';
-export const ctrlCompoundbuttonMediumPaddingHorizontal = 'var(--smtc-ctrl-compoundbutton-medium-padding-horizontal, var(--spacingHorizontalM))';
+export const ctrlCompoundbuttonMediumPaddingHorizontal =
+  'var(--smtc-ctrl-compoundbutton-medium-padding-horizontal, var(--spacingHorizontalM))';
 export const ctrlCompoundbuttonMediumPaddingTop = 'var(--smtc-ctrl-compoundbutton-medium-padding-top, 14px)';
-export const ctrlCompoundbuttonMediumPaddingBottom = 'var(--smtc-ctrl-compoundbutton-medium-padding-bottom, var(--spacingHorizontalL))';
+export const ctrlCompoundbuttonMediumPaddingBottom =
+  'var(--smtc-ctrl-compoundbutton-medium-padding-bottom, var(--spacingHorizontalL))';
 export const ctrlCompoundbuttonMediumMinwidth = 'var(--smtc-ctrl-compoundbutton-medium-minwidth)';
 export const ctrlCompoundbuttonMediumMinheight = 'var(--smtc-ctrl-compoundbutton-medium-minheight)';
-export const ctrlCompoundbuttonLargePaddingHorizontal = 'var(--smtc-ctrl-compoundbutton-large-padding-horizontal, var(--spacingHorizontalL))';
+export const ctrlCompoundbuttonLargePaddingHorizontal =
+  'var(--smtc-ctrl-compoundbutton-large-padding-horizontal, var(--spacingHorizontalL))';
 export const ctrlCompoundbuttonLargePaddingTop = 'var(--smtc-ctrl-compoundbutton-large-padding-top, 18px)';
-export const ctrlCompoundbuttonLargePaddingBottom = 'var(--smtc-ctrl-compoundbutton-large-padding-bottom, var(--spacingHorizontalXL))';
+export const ctrlCompoundbuttonLargePaddingBottom =
+  'var(--smtc-ctrl-compoundbutton-large-padding-bottom, var(--spacingHorizontalXL))';
 export const ctrlCompoundbuttonLargeMinwidth = 'var(--smtc-ctrl-compoundbutton-large-minwidth)';
 export const ctrlCompoundbuttonLargeMinheight = 'var(--smtc-ctrl-compoundbutton-large-minheight)';
-export const ctrlCompoundbuttonSmallIcononlyPadding = 'var(--smtc-ctrl-compoundbutton-small-icononly-padding, var(--spacingHorizontalXS))';
-export const ctrlCompoundbuttonMediumIcononlyPadding = 'var(--smtc-ctrl-compoundbutton-medium-icononly-padding, var(--spacingHorizontalSNudge))';
-export const ctrlCompoundbuttonLargeIcononlyPadding = 'var(--smtc-ctrl-compoundbutton-large-icononly-padding, var(--spacingHorizontalS))';
+export const ctrlCompoundbuttonSmallIcononlyPadding =
+  'var(--smtc-ctrl-compoundbutton-small-icononly-padding, var(--spacingHorizontalXS))';
+export const ctrlCompoundbuttonMediumIcononlyPadding =
+  'var(--smtc-ctrl-compoundbutton-medium-icononly-padding, var(--spacingHorizontalSNudge))';
+export const ctrlCompoundbuttonLargeIcononlyPadding =
+  'var(--smtc-ctrl-compoundbutton-large-icononly-padding, var(--spacingHorizontalS))';
