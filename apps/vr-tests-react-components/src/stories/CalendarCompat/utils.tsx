5af70f9847908238e7b29f798f2d01f159c24a22
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-10-27 16:23:33 +0100

    revert calendar story change

--- apps/vr-tests-react-components/src/stories/CalendarCompat/utils.tsx
+++ apps/vr-tests-react-components/src/stories/CalendarCompat/utils.tsx
@@ -2,13 +2,5 @@ import * as React from 'react';
 import { Calendar } from '@fluentui/react-calendar-compat';
 
 export const SampleCalendarCompatMultiDayView = ({ daysToSelectInDayView }: { daysToSelectInDayView: number }) => {
-  return (
-    <Calendar
-      // Hardcoded date to avoid VR test flakes
-      today={new Date('10-22-2025')}
-      highlightSelectedMonth
-      showGoToToday
-      calendarDayProps={{ daysToSelectInDayView }}
-    />
-  );
+  return <Calendar highlightSelectedMonth showGoToToday calendarDayProps={{ daysToSelectInDayView }} />;
 };
