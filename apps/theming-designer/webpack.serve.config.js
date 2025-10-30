2bd7670bfdf2ab8661e7f991da727f05ffaca1e1
Martin Hochel <martinhochel@microsoft.com>
2025-10-30 15:05:30 +0100

    chore: migrate apps,examples,devloop to react-dom/client

--- apps/theming-designer/webpack.serve.config.js
+++ apps/theming-designer/webpack.serve.config.js
@@ -1,3 +1,4 @@
+const path = require('node:path');
 const { getResolveAlias, resources } = require('@fluentui/scripts-webpack');
 
 module.exports = resources.createServeConfig(
@@ -8,7 +9,12 @@ module.exports = resources.createServeConfig(
     },
 
     resolve: {
-      alias: getResolveAlias(),
+      alias: {
+        ...getResolveAlias(),
+        // react-monaco-editor dynamically loads @types/react via proprietary webpack require.ensure,
+        // this doesn't work starting @types/react@17.0.48 as the types package introduced Export Maps
+        '@types/react/index.d.ts': path.resolve(__dirname, '../../node_modules/@types/react/index.d.ts'),
+      },
     },
   },
   'dist',
