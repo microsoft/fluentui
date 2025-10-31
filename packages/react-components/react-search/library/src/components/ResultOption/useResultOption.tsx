9d43500212fce30361d7e5e4dd975ed2b414f513
Hannah K <7254163+mindlessroman@users.noreply.github.com>
2025-10-31 17:26:10 -0400

    Starting a spec sheet for the typeaheard search

--- packages/react-components/react-search/library/src/components/ResultOption/useResultOption.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-import * as React from 'react';
-import { getIntrinsicElementProps, useId, useMergedRefs, slot } from '@fluentui/react-utilities';
-import { useActiveDescendantContext } from '@fluentui/react-aria';
-import type { ResultOptionProps, ResultOptionState } from './ResultOption.types';
