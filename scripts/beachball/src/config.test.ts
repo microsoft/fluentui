42fe0dce050a4c37588c2442726a164f8c5ce5e9
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-11-03 13:14:46 +0100

    fix tests

--- scripts/beachball/src/config.test.ts
+++ scripts/beachball/src/config.test.ts
@@ -103,7 +103,7 @@ describe(`beachball configs`, () => {
     ]);
 
     // Ensure that vNext config does not include "tools" packages
-    expect(vNextConfig.scope).not.toEqual(expect.arrayContaining(toolsConfig.scope));
+    expect(vNextConfig.scope.some(scope => toolsConfig.scope.includes(scope))).toBe(false);
   });
 
   it(`should generate web-components release config`, () => {
