5d49ae1def1908801ddace60902e43061872e240
Martin Hochel <martinhochel@microsoft.com>
2025-11-04 10:24:25 +0100

    fix(ssr-tests): make the ssr test sync to match behaviour if renderToString, increase timeout, and fix webpack bundling errors caused by react-monaco

--- apps/public-docsite/webpack.serve.config.js
+++ apps/public-docsite/webpack.serve.config.js
@@ -61,7 +61,8 @@ module.exports = [
 
         resolve: {
           alias: {
-            ...getResolveAlias(), // react-monaco-editor dynamically loads @types/react via proprietary webpack require.ensure,
+            ...getResolveAlias(),
+            // react-monaco-editor dynamically loads @types/react via proprietary webpack require.ensure,
             // this doesn't work starting @types/react@17.0.48 as the types package introduced Export Maps
             '@types/react/index.d.ts': path.resolve(__dirname, '../../node_modules/@types/react/index.d.ts'),
           },
