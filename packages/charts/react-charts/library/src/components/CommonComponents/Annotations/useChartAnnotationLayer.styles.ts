8d39b1a1e870d0b00ad16400484a988703a05bc3
Anush <anushgupta@microsoft.com>
2025-11-05 14:47:03 +0530

    fix lint error

--- packages/charts/react-charts/library/src/components/CommonComponents/Annotations/useChartAnnotationLayer.styles.ts
+++ packages/charts/react-charts/library/src/components/CommonComponents/Annotations/useChartAnnotationLayer.styles.ts
@@ -1,3 +1,4 @@
+'use-client';
 import { makeStyles, mergeClasses } from '@griffel/react';
 import { SlotClassNames } from '@fluentui/react-utilities';
 import { tokens, typographyStyles } from '@fluentui/react-theme';
