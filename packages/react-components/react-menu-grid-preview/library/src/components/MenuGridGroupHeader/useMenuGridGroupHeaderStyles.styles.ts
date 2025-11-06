c8451720a026f5aaefddb68841846190d8e7d251
YuanboXue-Amber <yuanboxue@microsoft.com>
2025-11-06 11:44:34 +0100

    styles

--- packages/react-components/react-menu-grid-preview/library/src/components/MenuGridGroupHeader/useMenuGridGroupHeaderStyles.styles.ts
+++ packages/react-components/react-menu-grid-preview/library/src/components/MenuGridGroupHeader/useMenuGridGroupHeaderStyles.styles.ts
@@ -1,15 +1,30 @@
 import type { SlotClassNames } from '@fluentui/react-utilities';
-import { mergeClasses } from '@griffel/react';
+import { makeStyles, mergeClasses } from '@griffel/react';
 import type { MenuGridGroupHeaderSlots, MenuGridGroupHeaderState } from './MenuGridGroupHeader.types';
+import { tokens } from '@fluentui/react-theme';
 
 export const MenuGridGroupHeaderClassNames: SlotClassNames<MenuGridGroupHeaderSlots> = {
   root: 'fui-MenuGridGroupHeader',
 };
 
+const useStyles = makeStyles({
+  root: {
+    fontSize: tokens.fontSizeBase200,
+    color: tokens.colorNeutralForeground3,
+    paddingLeft: '8px',
+    paddingRight: '8px',
+    fontWeight: tokens.fontWeightSemibold,
+    height: '32px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
 export const useMenuGridGroupHeaderStyles_unstable = (state: MenuGridGroupHeaderState): MenuGridGroupHeaderState => {
   'use no memo';
 
-  state.root.className = mergeClasses(MenuGridGroupHeaderClassNames.root, state.root.className);
+  const styles = useStyles();
+  state.root.className = mergeClasses(MenuGridGroupHeaderClassNames.root, styles.root, state.root.className);
 
   return state;
 };
