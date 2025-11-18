f3d1acde577866c04ba1725264a2de8c696090c8
mainframev <vgenaev@gmail.com>
2025-11-18 18:37:56 +0100

    chore(apps): migrate apps to flat configs

---
+++ apps/rit-tests-v9/eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+const fluentPlugin = require('@fluentui/eslint-plugin');
+
+/** @type {import("eslint").Linter.Config[]} */
+module.exports = [
+  {
+    ignores: ['src/react-18/**/*', 'src/react-17/**/*'],
+  },
+  ...fluentPlugin.configs['flat/react'],
+  {
+    rules: {
+      '@nx/workspace-enforce-use-client': 'off',
+      '@nx/workspace-no-restricted-globals': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-restricted-types': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-deprecated': 'warn',
+    },
+  },
+];
