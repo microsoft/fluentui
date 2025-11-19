5d63ed336879dd291c5e89e5ac98312bdfd26349
Eddie Liu <edwl@microsoft.com>
2025-11-19 13:55:03 -0800

    Adding new story with increase/decrease badge count buttons

---
+++ packages/react-components/react-badge/stories/src/CounterBadge/CounterBadgeAnimated.stories.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { JSXElement } from '@fluentui/react-components';
+
+import { CounterBadge } from '@fluentui/react-components';
+import type { CounterBadgeProps } from '@fluentui/react-components';
+
+export const AnimatedBadge = (args: CounterBadgeProps): JSXElement => {
+  const [count, setCount] = React.useState(5);
+
+  return (
+    <div>
+      <CounterBadge count={count} />
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+      </div>
+    </div>
+  );
+};
