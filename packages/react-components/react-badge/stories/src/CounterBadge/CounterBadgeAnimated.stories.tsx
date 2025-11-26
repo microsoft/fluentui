05fbd774d40e26ad259ac59da3df7be898e1a463
Eddie Liu <edwl@microsoft.com>
2025-11-26 09:37:55 -0800

    Update

--- packages/react-components/react-badge/stories/src/CounterBadge/CounterBadgeAnimated.stories.tsx
+++ packages/react-components/react-badge/stories/src/CounterBadge/CounterBadgeAnimated.stories.tsx
@@ -6,6 +6,20 @@ import type { CounterBadgeProps } from '@fluentui/react-components';
 
 export const AnimatedBadge = (args: CounterBadgeProps): JSXElement => {
   const [count, setCount] = React.useState(5);
+  const [inputValue, setInputValue] = React.useState('5');
+
+  const handleUpdate = () => {
+    const newCount = Number(inputValue);
+    if (!isNaN(newCount)) {
+      setCount(newCount);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleUpdate();
+    }
+  };
 
   return (
     <div>
@@ -13,6 +27,16 @@ export const AnimatedBadge = (args: CounterBadgeProps): JSXElement => {
       <div>
         <button onClick={() => setCount(count + 1)}>Increase</button>
         <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          style={{ marginLeft: '10px', padding: '5px' }}
+        />
+        <button onClick={handleUpdate} style={{ marginLeft: '5px' }}>
+          Update
+        </button>
       </div>
     </div>
   );
