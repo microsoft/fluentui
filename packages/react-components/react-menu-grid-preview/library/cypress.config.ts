a64035a822e1a9af9faf6d3edde7c4e1c4c8d677
Adam <adam.samec@gmail.com>
2025-12-09 19:43:32 +0100

    Feat: Add cypress e2e tests for MenuGrid

---
+++ packages/react-components/react-menu-grid-preview/library/cypress.config.ts
@@ -0,0 +1,11 @@
+import { baseConfig } from "@fluentui/scripts-cypress";
+
+export default {
+  ...baseConfig,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
