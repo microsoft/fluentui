1623e226c1685013c147cb929599257ca94b7445
mainframev <vgenaev@gmail.com>
2026-01-05 14:20:11 +0100

    fixup! chore: migrate to flat eslint flat configs

--- packages/web-components/scripts/verify-packaging.js
+++ packages/web-components/scripts/verify-packaging.js
@@ -20,6 +20,7 @@ function main() {
   const rootConfigFiles = [
     'just.config.[jt]s',
     'jest.config.[jt]s',
+    'eslint.config.js',
     'project.json',
     '.babelrc.json',
     '.swcrc',
