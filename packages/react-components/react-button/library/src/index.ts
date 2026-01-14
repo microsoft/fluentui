c41dd7ecc08bc4fe18344d1c00c77a14a3f0db35
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-01-14 16:47:23 +0100

    update exports

--- packages/react-components/react-button/library/src/index.ts
+++ packages/react-components/react-button/library/src/index.ts
@@ -4,76 +4,54 @@ export {
   renderButton_unstable,
   useButtonStyles_unstable,
   useButton_unstable,
-  // useButtonBase_unstable,
 } from './Button';
-export type {
-  // ButtonBaseProps,
-  ButtonProps,
-  ButtonSlots,
-  //  ButtonBaseState,
-  ButtonState,
-} from './Button';
+export type { ButtonProps, ButtonSlots, ButtonState } from './Button';
 export {
   CompoundButton,
   compoundButtonClassNames,
   renderCompoundButton_unstable,
   useCompoundButtonStyles_unstable,
   useCompoundButton_unstable,
-  // useCompoundButtonBase_unstable,
 } from './CompoundButton';
-export type {
-  // CompoundButtonBaseProps,
-  CompoundButtonProps,
-  CompoundButtonSlots,
-  // CompoundButtonBaseState,
-  CompoundButtonState,
-} from './CompoundButton';
+export type { CompoundButtonProps, CompoundButtonSlots, CompoundButtonState } from './CompoundButton';
 export {
   MenuButton,
   menuButtonClassNames,
   renderMenuButton_unstable,
   useMenuButtonStyles_unstable,
   useMenuButton_unstable,
-  // useMenuButtonBase_unstable,
 } from './MenuButton';
-export type {
-  // MenuButtonBaseProps,
-  MenuButtonProps,
-  MenuButtonSlots,
-  // MenuButtonBaseState,
-  MenuButtonState,
-} from './MenuButton';
+export type { MenuButtonProps, MenuButtonSlots, MenuButtonState } from './MenuButton';
 export {
   SplitButton,
   renderSplitButton_unstable,
   splitButtonClassNames,
   useSplitButtonStyles_unstable,
   useSplitButton_unstable,
-  // useSplitButtonBase_unstable,
 } from './SplitButton';
-export type {
-  // SplitButtonBaseProps,
-  SplitButtonProps,
-  SplitButtonSlots,
-  // SplitButtonBaseState,
-  SplitButtonState,
-} from './SplitButton';
+export type { SplitButtonProps, SplitButtonSlots, SplitButtonState } from './SplitButton';
 export {
   ToggleButton,
   renderToggleButton_unstable,
   toggleButtonClassNames,
   useToggleButtonStyles_unstable,
   useToggleButton_unstable,
-  // useToggleButtonBase_unstable,
 } from './ToggleButton';
-export type {
-  // ToggleButtonBaseProps,
-  ToggleButtonProps,
-  // ToggleButtonBaseState,
-  ToggleButtonState,
-} from './ToggleButton';
+export type { ToggleButtonProps, ToggleButtonState } from './ToggleButton';
 
 export { useToggleState } from './utils/index';
 
 export { ButtonContextProvider, useButtonContext } from './contexts/index';
 export type { ButtonContextValue } from './contexts/index';
+
+// Experimental APIs - will be uncommented in experimental release
+// export { useButtonBase_unstable } from './Button';
+// export type { ButtonBaseProps, ButtonBaseState } from './Button';
+// export { useCompoundButtonBase_unstable } from './CompoundButton';
+// export type { CompoundButtonBaseProps, CompoundButtonBaseState } from './CompoundButton';
+// export { useMenuButtonBase_unstable } from './MenuButton';
+// export type { MenuButtonBaseProps, MenuButtonBaseState } from './MenuButton';
+// export { useSplitButtonBase_unstable } from './SplitButton';
+// export type { SplitButtonBaseProps, SplitButtonBaseState } from './SplitButton';
+// export { useToggleButtonBase_unstable } from './ToggleButton';
+// export type { ToggleButtonBaseProps, ToggleButtonBaseState } from './ToggleButton';
