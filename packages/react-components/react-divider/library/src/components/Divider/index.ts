3383bfe463a6e80c5b813b2a8e49ff1d7c88acd6
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-01-14 19:09:21 +0100

    feat(react-divider): add base hooks (#35634)

--- packages/react-components/react-divider/library/src/components/Divider/index.ts
+++ packages/react-components/react-divider/library/src/components/Divider/index.ts
@@ -1,5 +1,6 @@
 export { Divider } from './Divider';
-export type { DividerProps, DividerSlots, DividerState } from './Divider.types';
+export type { DividerBaseProps, DividerProps, DividerSlots, DividerBaseState, DividerState } from './Divider.types';
 export { renderDivider_unstable } from './renderDivider';
 export { useDivider_unstable } from './useDivider';
+export { useDividerBase_unstable } from './useDividerBase';
 export { dividerClassNames, useDividerStyles_unstable } from './useDividerStyles.styles';
