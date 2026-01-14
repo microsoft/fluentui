216dffcaa8e10195a8f7b271b87321704cb4a398
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-01-14 18:57:23 +0100

    fix comment

--- packages/react-components/react-divider/library/src/index.ts
+++ packages/react-components/react-divider/library/src/index.ts
@@ -7,6 +7,6 @@ export {
 } from './Divider';
 export type { DividerProps, DividerSlots, DividerState } from './Divider';
 
-// Experimental APIs - will
+// Experimental APIs - will be uncommented in the experimental release branch
 // export { useDividerBase_unstable } from './Divider';
 // export type { DividerBaseProps, DividerBaseState } from './Divider';
