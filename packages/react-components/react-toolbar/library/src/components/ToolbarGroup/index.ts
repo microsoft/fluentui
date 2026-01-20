1ee2498e4360ba474450a19e21109a945fb7c322
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-01-20 13:47:00 +0100

    feat(react-toolbar): add base hooks for Toolbar components (#35658)

--- packages/react-components/react-toolbar/library/src/components/ToolbarGroup/index.ts
+++ packages/react-components/react-toolbar/library/src/components/ToolbarGroup/index.ts
@@ -1,5 +1,5 @@
 export { ToolbarGroup } from './ToolbarGroup';
 export type { ToolbarGroupProps, ToolbarGroupSlots, ToolbarGroupState } from './ToolbarGroup.types';
-export { useToolbarGroup_unstable } from './useToolbarGroup';
+export { useToolbarGroup_unstable, useToolbarGroupBase_unstable } from './useToolbarGroup';
 export { toolbarGroupClassNames, useToolbarGroupStyles_unstable } from './useToolbarGroupStyles.styles';
 export { renderToolbarGroup_unstable } from './renderToolbarGroup';
