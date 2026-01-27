53b59cab8e537c24dbf7a75c5d17805a17b7b6c1
mainframev <vgenaev@gmail.com>
2026-01-27 14:24:19 +0100

    fixup! feat(react-dialog): extend backdrop slot to support appearance prop

--- packages/react-components/react-dialog/stories/src/Dialog/DialogBackdropAppearance.stories.tsx
+++ packages/react-components/react-dialog/stories/src/Dialog/DialogBackdropAppearance.stories.tsx
@@ -13,12 +13,33 @@ import {
   DrawerHeader,
   DrawerHeaderTitle,
   Button,
+  Label,
+  RadioGroup,
+  Radio,
+  useId,
+  tokens,
+  makeStyles,
 } from '@fluentui/react-components';
 import { Dismiss24Regular } from '@fluentui/react-icons';
 import story from './DialogBackdropAppearance.md';
 
+const useStyles = makeStyles({
+  field: {
+    display: 'grid',
+    gridRowGap: tokens.spacingVerticalS,
+    marginBottom: tokens.spacingVerticalL,
+  },
+});
+
+type BackdropAppearanceOption = 'dimmed' | 'transparent';
+
 export const BackdropAppearance = (): JSXElement => {
+  const styles = useStyles();
+  const labelId = useId('backdrop-appearance-label');
+
   const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [backdropAppearance, setBackdropAppearance] = React.useState<BackdropAppearanceOption>();
+  const backdropProp = backdropAppearance ? { appearance: backdropAppearance } : undefined;
 
   return (
     <>
@@ -41,26 +62,32 @@ export const BackdropAppearance = (): JSXElement => {
             Drawer
           </DrawerHeaderTitle>
         </DrawerHeader>
+
         <DrawerBody>
-          <p>When you open a Dialog from here, it will show a dimmed backdrop.</p>
-          <p>
-            Without{' '}
-            <code>
-              backdrop={'{{'} appearance: &quot;dimmed&quot; {'}}'}
-            </code>
-            , the backdrop would be transparent since this Dialog is nested inside the Drawer (which internally uses
-            Dialog).
-          </p>
+          <div className={styles.field}>
+            <Label id={labelId}>Backdrop appearance</Label>
+            <RadioGroup
+              value={backdropAppearance}
+              onChange={(_, data) => setBackdropAppearance(data.value as BackdropAppearanceOption)}
+              aria-labelledby={labelId}
+            >
+              <Radio value="default" label="Default (transparent for nested dialogs based on context)" />
+              <Radio value="dimmed" label="Dimmed" />
+              <Radio value="transparent" label="Transparent" />
+            </RadioGroup>
+          </div>
+
           <Dialog>
             <DialogTrigger disableButtonEnhancement>
               <Button>Open Dialog</Button>
             </DialogTrigger>
-            <DialogSurface backdrop={{ appearance: 'dimmed' }}>
+            <DialogSurface backdrop={backdropProp}>
               <DialogBody>
-                <DialogTitle>Dialog with dimmed backdrop</DialogTitle>
+                <DialogTitle>Dialog</DialogTitle>
                 <DialogContent>
-                  This dialog uses backdrop appearance on DialogSurface to force a dimmed backdrop, even though it's
-                  rendered inside the OverlayDrawer component.
+                  This Dialog is rendered inside an OverlayDrawer, which internally uses Dialog. By default, nested
+                  dialogs have a transparent backdrop to avoid stacking multiple dimmed layers. Use the{' '}
+                  <code>backdrop</code> prop to override this behavior.
                 </DialogContent>
                 <DialogActions>
                   <DialogTrigger disableButtonEnhancement>
