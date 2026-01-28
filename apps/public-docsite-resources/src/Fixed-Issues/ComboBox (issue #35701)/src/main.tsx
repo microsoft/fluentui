8c655ff6dc3306ca1b58270dfd44fc29b7047b07
AdyaTech <adygr6@gmail.com>
2026-01-28 21:02:08 +0530

    Fixed the issue 35701 regarding the Combo Box Free Form Popup.

---
+++ apps/public-docsite-resources/src/Fixed-Issues/ComboBox (issue #35701)/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./ComboBox";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
