8a8304bcb3e96b7e364939da5b2819f3f8711053
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-02-04 13:12:44 +0100

    refactor(react-button): streamline button component base hook and remove unused base hooks (#35689)
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

--- packages/react-components/react-button/library/src/components/MenuButton/index.ts
+++ packages/react-components/react-button/library/src/components/MenuButton/index.ts
@@ -1,12 +1,5 @@
-export type {
-  MenuButtonBaseProps,
-  MenuButtonProps,
-  MenuButtonSlots,
-  MenuButtonBaseState,
-  MenuButtonState,
-} from './MenuButton.types';
+export type { MenuButtonProps, MenuButtonSlots, MenuButtonState } from './MenuButton.types';
 export { MenuButton } from './MenuButton';
 export { renderMenuButton_unstable } from './renderMenuButton';
 export { useMenuButton_unstable } from './useMenuButton';
-export { useMenuButtonBase_unstable } from './useMenuButtonBase';
 export { menuButtonClassNames, useMenuButtonStyles_unstable } from './useMenuButtonStyles.styles';
