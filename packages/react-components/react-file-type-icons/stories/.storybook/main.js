38bf864fe1e73f813b5b4494ca7110ba7be68818
Carlos Perez (M365) <caperez@microsoft.com>
2026-02-25 21:12:03 -0800

    feat: add Storybook support for react-file-type-icons and mock set-version module

--- packages/react-components/react-file-type-icons/stories/.storybook/main.js
+++ packages/react-components/react-file-type-icons/stories/.storybook/main.js
@@ -1,13 +1,19 @@
 const rootMain = require('../../../../../.storybook/main');
+const path = require('path');
 
 module.exports = /** @type {Omit<import('../../../../../.storybook/main'), 'typescript'|'babel'>} */ ({
   ...rootMain,
   stories: [...rootMain.stories, '../src/**/*.mdx', '../src/**/index.stories.@(ts|tsx)'],
   addons: [...rootMain.addons],
-  webpackFinal: (config, options) => {
-    const localConfig = { ...rootMain.webpackFinal(config, options) };
+  webpackFinal: async (config, options) => {
+    const localConfig = await rootMain.webpackFinal(config, options);
 
-    // add your own webpack tweaks if needed
+    localConfig.resolve = localConfig.resolve || {};
+    localConfig.resolve.alias = {
+      ...(localConfig.resolve.alias || {}),
+      '@fluentui/set-version$': path.resolve(__dirname, './mocks/set-version.js'),
+      '@fluentui/set-version': path.resolve(__dirname, './mocks/set-version.js'),
+    };
 
     return localConfig;
   },
