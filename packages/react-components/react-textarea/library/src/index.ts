cd29f13fec3bd7fe638323d6a821b4aaf931a29b
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-03-20 12:28:35 +0100

    feat(react-textarea): expose base hook and types for Textarea (#35886)

--- packages/react-components/react-textarea/library/src/index.ts
+++ packages/react-components/react-textarea/library/src/index.ts
@@ -4,9 +4,13 @@ export {
   textareaClassNames,
   useTextareaStyles_unstable,
   useTextarea_unstable,
+  useTextareaBase_unstable,
 } from './Textarea';
-export type { TextareaOnChangeData, TextareaProps, TextareaSlots, TextareaState } from './Textarea';
-
-// Experimental APIs - will be uncommented in the experimental release branch
-// export { useTextareaBase_unstable } from './Textarea';
-// export type { TextareaBaseProps, TextareaBaseState } from './Textarea';
+export type {
+  TextareaOnChangeData,
+  TextareaProps,
+  TextareaSlots,
+  TextareaState,
+  TextareaBaseProps,
+  TextareaBaseState,
+} from './Textarea';
