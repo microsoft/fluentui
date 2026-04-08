0a54e905e70ed8634c2ea9c6db6deb2fcc126db9
Zacky Ma <machi@microsoft.com>
2026-04-08 11:36:54 -0700

    add menu-list

---
+++ packages/web-components/src/menu-list/menu-list.ts
@@ -0,0 +1,23 @@
+import { Updates } from '@microsoft/fast-element';
+import { polyfill as focusgroupPolyfill } from '@microsoft/focusgroup-polyfill';
+import { BaseMenuList } from './menu-list.base';
+
+/**
+ * A MenuList Custom HTML Element.
+ * Implements the {@link https://www.w3.org/TR/wai-aria-1.1/#menu | ARIA menu }.
+ *
+ * @tag fluent-menu-list
+ *
+ * @slot - The default slot for the menu items
+ *
+ * @public
+ */
+export class MenuList extends BaseMenuList {
+  public connectedCallback(): void {
+    super.connectedCallback();
+
+    Updates.enqueue(() => {
+      focusgroupPolyfill(this);
+    });
+  }
+}
