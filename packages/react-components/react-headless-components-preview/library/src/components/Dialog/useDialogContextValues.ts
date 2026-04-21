43441f901fb0e78d8111b15d5b91812cfb846c0a
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-04-21 17:16:18 +0200

    fixup

--- packages/react-components/react-headless-components-preview/library/src/components/Dialog/useDialogContextValues.ts
+++ packages/react-components/react-headless-components-preview/library/src/components/Dialog/useDialogContextValues.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 import type { DialogContextValue, DialogSurfaceContextValue } from './dialogContext';
 import type { DialogContextValues, DialogState } from './Dialog.types';
