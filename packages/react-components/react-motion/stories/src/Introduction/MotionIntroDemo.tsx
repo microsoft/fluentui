b03689c341375717065f7a2c45d2e1aa98d845a1
Robert Penner <robertpenner@microsoft.com>
2026-04-29 17:26:21 +0000

    fix(react-motion): add 'use client' directive to Introduction story demos

--- packages/react-components/react-motion/stories/src/Introduction/MotionIntroDemo.tsx
+++ packages/react-components/react-motion/stories/src/Introduction/MotionIntroDemo.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 import { Button } from '@fluentui/react-components';
 import { useFluent_unstable as useFluent } from '@fluentui/react-shared-contexts';
--- packages/react-components/react-motion/stories/src/Introduction/MotionVsPresenceDemo.tsx
+++ packages/react-components/react-motion/stories/src/Introduction/MotionVsPresenceDemo.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 import { Button } from '@fluentui/react-components';
 import { createMotionComponent, createPresenceComponent, motionTokens } from '@fluentui/react-motion';
