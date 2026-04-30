8d0bd41261f9db762a5c597e7c42522a8afe8502
Sarah Higley <sarah.higley@microsoft.com>
2026-04-29 22:26:03 -0700

    fix(Option): when disabled, hover styles should not show

--- packages/react-components/react-combobox/library/src/components/Option/useOptionStyles.styles.ts
+++ packages/react-components/react-combobox/library/src/components/Option/useOptionStyles.styles.ts
@@ -27,7 +27,9 @@ const useStyles = makeStyles({
     lineHeight: tokens.lineHeightBase300,
     padding: `${tokens.spacingVerticalSNudge} ${tokens.spacingHorizontalS}`,
     position: 'relative',
+  },
 
+  interactive: {
     ':hover': {
       backgroundColor: tokens.colorNeutralBackground1Hover,
       color: tokens.colorNeutralForeground1Hover,
@@ -60,18 +62,7 @@ const useStyles = makeStyles({
 
   disabled: {
     color: tokens.colorNeutralForegroundDisabled,
-
-    ':hover': {
-      backgroundColor: tokens.colorTransparentBackground,
-      color: tokens.colorNeutralForegroundDisabled,
-      [`& .${optionClassNames.checkIcon}`]: shorthands.borderColor(tokens.colorNeutralForegroundDisabled),
-    },
-
-    ':active': {
-      backgroundColor: tokens.colorTransparentBackground,
-      color: tokens.colorNeutralForegroundDisabled,
-      [`& .${optionClassNames.checkIcon}`]: shorthands.borderColor(tokens.colorNeutralForegroundDisabled),
-    },
+    cursor: 'default',
 
     '@media (forced-colors: active)': {
       color: 'GrayText',
@@ -141,6 +132,7 @@ export const useOptionStyles_unstable = (state: OptionState): OptionState => {
   state.root.className = mergeClasses(
     optionClassNames.root,
     styles.root,
+    !disabled && styles.interactive,
     styles.active,
     disabled && styles.disabled,
     selected && styles.selected,
