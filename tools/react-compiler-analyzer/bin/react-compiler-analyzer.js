7f265b8740ee03be615f8b2e4e907e47e92f527d
Martin Hochel <martinhochel@microsoft.com>
2026-04-30 18:55:10 +0200

    chore(react-compiler-analyzer): prepare for experimental release

--- tools/react-compiler-analyzer/bin/react-compiler-analyzer.js
+++ tools/react-compiler-analyzer/bin/react-compiler-analyzer.js
@@ -2,13 +2,7 @@
 
 // @ts-check
 
-if (process.env.NODE_ENV !== 'production') {
-  const { joinPathFragments } = require('@nx/devkit');
-  const { registerTsProject } = require('@nx/js/src/internal');
-  registerTsProject(joinPathFragments(__dirname, '..', 'tsconfig.lib.json'));
-}
-
-const { cli } = process.env.NODE_ENV !== 'production' ? require('../src/cli') : require('../dist/src/cli');
+const { cli } = require('../dist/src/cli');
 
 cli().catch(err => {
   console.error(err);
