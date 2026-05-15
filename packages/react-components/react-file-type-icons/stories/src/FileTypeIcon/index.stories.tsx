279217fbc6f2eb7c9d54ad328f3efa7047820474
Carlos Perez (M365) <caperez@microsoft.com>
2026-05-14 21:09:51 -0700

    feat(react-file-type-icons): refactor stories to use new file structure and restore deleted stories

--- packages/react-components/react-file-type-icons/stories/src/FileTypeIcon/index.stories.tsx
+++ packages/react-components/react-file-type-icons/stories/src/FileTypeIcon/index.stories.tsx
@@ -2,11 +2,11 @@ import { FileTypeIcon } from '@fluentui/react-file-type-icons';
 
 import descriptionMd from './FileTypeIconDescription.md';
 
-export { Default } from './FileTypeIconDefault.stories';
-export { ByFileIconType } from './FileTypeIconByFileIconType.stories';
-export { SizeAndFormats } from './FileTypeIconSizeAndFormats.stories';
-export { CustomBaseUrl } from './FileTypeIconCustomBaseUrl.stories';
-export { V8UtilityInterop } from './FileTypeIconV8UtilityInterop.stories';
+export { Default } from './FileTypeIconDefaultStory';
+export { ByFileIconType } from './FileTypeIconByFileIconTypeStory';
+export { SizeAndFormats } from './FileTypeIconSizeAndFormatsStory';
+export { CustomBaseUrl } from './FileTypeIconCustomBaseUrlStory';
+export { V8UtilityInterop } from './FileTypeIconV8UtilityInteropStory';
 
 export default {
   title: 'Components/FileTypeIcon',
