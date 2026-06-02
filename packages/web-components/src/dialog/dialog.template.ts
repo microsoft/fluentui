bfb111ae3beaf5a1224939801a6879a8c58fccc1
Zacky Ma <machi@microsoft.com>
2026-06-02 15:48:54 -0700

    add context pointer comment for the solution

--- packages/web-components/src/dialog/dialog.template.ts
+++ packages/web-components/src/dialog/dialog.template.ts
@@ -3,6 +3,11 @@ import type { Dialog } from './dialog.js';
 
 /**
  * Template for the Dialog component
+ *
+ * Note: The empty `<div tabindex="-1">` element above the `<slot>` element is
+ * for working around a dialog focus issue, learn more at
+ * https://github.com/microsoft/fluentui/pull/36278
+ *
  * @public
  */
 export const template: ElementViewTemplate<Dialog> = html`
