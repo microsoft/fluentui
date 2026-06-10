14ed04ac21f591f252b4503f6f2c8cc712f2f1a3
Zacky Ma <machi@microsoft.com>
2026-06-10 15:03:23 -0700

    no need to check disability

--- packages/web-components/src/utils/autofocus.ts
+++ packages/web-components/src/utils/autofocus.ts
@@ -1,9 +1,5 @@
 import { Updates } from '@microsoft/fast-element';
 
-interface FluentElement extends HTMLElement {
-  disabled?: boolean;
-}
-
 /**
  * Artificial sets the focus to the given element, if no other element in the
  * document is currently focused and the given element meets the following
@@ -15,12 +11,11 @@ interface FluentElement extends HTMLElement {
  *
  * For more details of this issue, see https://codepen.io/editor/marchbox/pen/019e9ab9-cd81-7c21-a3ae-1b7fe2d3458a
  */
-export function maybeSetAutoFocus(element: FluentElement) {
+export function maybeSetAutoFocus(element: HTMLElement) {
   const doc = element.ownerDocument;
   if (
     element?.isConnected &&
     element?.hasAttribute('autofocus') &&
-    !element?.disabled &&
     // Note: opacity=0 is considered visible based on the native `autofocus` implementation
     element?.checkVisibility?.({ contentVisibilityAuto: true, visibilityProperty: true }) &&
     [null, element, doc.body, doc.documentElement].includes(doc.activeElement as HTMLElement | null)
