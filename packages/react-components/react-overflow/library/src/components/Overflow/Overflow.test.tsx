7530c0c22d690b02ec26159b43437f3493d8ed90
Victor Genaev <vgenaev@gmail.com>
2026-06-15 17:25:30 +0200

    feat(react-overflow): split and expose hooks for headless reuse (#36304)

--- packages/react-components/react-overflow/library/src/components/Overflow/Overflow.test.tsx
+++ packages/react-components/react-overflow/library/src/components/Overflow/Overflow.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Overflow } from './Overflow';
-import { OverflowItem } from './OverflowItem';
+import { OverflowItem } from '../OverflowItem';
 
 describe('Overflow', () => {
   beforeAll(() => {
