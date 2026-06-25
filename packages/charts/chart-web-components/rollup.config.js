8286926666be2e462707a39c887e65c44c0c7a47
John Kreitlow <863023+radium-v@users.noreply.github.com>
2026-06-25 13:59:43 -0700

    refactor(chart-web-components): replace rollup-plugin-transform-tagged-template with rollup-plugin-fast-tagged-templates and remove unused transform-fragments script

--- packages/charts/chart-web-components/rollup.config.js
+++ packages/charts/chart-web-components/rollup.config.js
@@ -6,12 +6,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonJS from 'rollup-plugin-commonjs';
 import esbuild, { minify } from 'rollup-plugin-esbuild';
-import transformTaggedTemplate from 'rollup-plugin-transform-tagged-template';
-import { transformCSSFragment, transformHTMLFragment } from './scripts/transform-fragments';
-
-const parserOptions = {
-  sourceType: 'module',
-};
+import fastTaggedTemplates from 'rollup-plugin-fast-tagged-templates';
 
 export default [
   {
@@ -27,22 +22,14 @@ export default [
         plugins: [minify()],
       },
     ],
+    context: 'window',
     plugins: [
       nodeResolve({ browser: true }),
       commonJS(),
       esbuild({
         tsconfig: './tsconfig.lib.json',
       }),
-      transformTaggedTemplate({
-        tagsToProcess: ['css'],
-        transformer: transformCSSFragment,
-        parserOptions,
-      }),
-      transformTaggedTemplate({
-        tagsToProcess: ['html'],
-        transformer: transformHTMLFragment,
-        parserOptions,
-      }),
+      fastTaggedTemplates(),
     ],
   },
 ];
