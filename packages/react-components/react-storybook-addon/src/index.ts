4d58813f2ab238971892be182b14d51b0d35b7c3
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-06-30 10:13:16 +0200

    feat(react-storybook-addon-themes): allow theme picker options configuration

--- packages/react-components/react-storybook-addon/src/index.ts
+++ packages/react-components/react-storybook-addon/src/index.ts
@@ -1,7 +1,7 @@
 export type { FluentGlobals, FluentParameters, FluentStoryContext } from './hooks';
-export type { ThemeIds } from './theme';
+export type { ThemeIds, Theme } from './theme';
 export { themes } from './theme';
-export { DIR_ID, THEME_ID } from './constants';
+export { DIR_ID, THEME_ID, THEMES } from './constants';
 export { parameters } from './hooks';
 export { FluentCanvas, FluentDocsPage, FluentStory } from './docs';
 export type { FluentDocsPageProps } from './docs';
