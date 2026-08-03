726f185a1d2ed4a7cb1e0d0594b9a0506302cf1d
PaulGMardling <paulmardling@microsoft.com>
2026-08-03 10:41:18 +0200

    Update apps/vr-tests-react-components/src/stories/CalendarCompat/utils.tsx
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

--- apps/vr-tests-react-components/src/stories/CalendarCompat/utils.tsx
+++ apps/vr-tests-react-components/src/stories/CalendarCompat/utils.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Calendar } from '@fluentui/react-calendar-compat';
 
-const referenceDate = new Date(2023, 2, 15);
+const referenceDate = new Date('2023-03-15T12:00:00.000Z');
 
 export const SampleCalendarCompatMultiDayView = ({ daysToSelectInDayView }: { daysToSelectInDayView: number }) => {
   return (
