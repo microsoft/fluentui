636ccaeb44f01c6560ac1bb32cb016441e1e364a
mainframev <vgenaev@gmail.com>
2026-08-06 16:32:47 +0200

    fix(react-headless-components-preview): align CompoundButton release artifacts

--- packages/react-components/react-headless-components-preview/library/src/components/CompoundButton/renderCompoundButton.ts
+++ packages/react-components/react-headless-components-preview/library/src/components/CompoundButton/renderCompoundButton.ts
@@ -1,6 +1,1 @@
-import { renderCompoundButton_unstable } from '@fluentui/react-button';
-
-/**
- * Renders a CompoundButton component.
- */
-export const renderCompoundButton = renderCompoundButton_unstable;
+export { renderCompoundButton_unstable as renderCompoundButton } from '@fluentui/react-button';
