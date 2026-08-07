64f390b6e863f1b935c2c53bde416b49c06a3b89
mainframev <vgenaev@gmail.com>
2026-08-07 14:25:31 +0200

    chore(react-button): remove CompoundButton base fixture

--- packages/react-components/react-button/library/bundle-size/useCompoundButtonBase.fixture.js
+++
@@ -1,7 +0,0 @@
-import { renderCompoundButton_unstable, useCompoundButtonBase_unstable } from '@fluentui/react-button';
-
-console.log(renderCompoundButton_unstable, useCompoundButtonBase_unstable);
-
-export default {
-  name: 'useCompoundButtonBase',
-};
