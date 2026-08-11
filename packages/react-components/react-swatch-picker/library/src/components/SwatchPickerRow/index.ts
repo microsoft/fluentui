c4e007e21787500386c08ecca1369bea9af2447b
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-08-11 09:58:58 +0200

    feat(react-swatch-picker): expose headless base APIs (#36534)

--- packages/react-components/react-swatch-picker/library/src/components/SwatchPickerRow/index.ts
+++ packages/react-components/react-swatch-picker/library/src/components/SwatchPickerRow/index.ts
@@ -1,5 +1,11 @@
 export { SwatchPickerRow } from './SwatchPickerRow';
-export type { SwatchPickerRowProps, SwatchPickerRowSlots, SwatchPickerRowState } from './SwatchPickerRow.types';
+export type {
+  SwatchPickerRowBaseProps,
+  SwatchPickerRowBaseState,
+  SwatchPickerRowProps,
+  SwatchPickerRowSlots,
+  SwatchPickerRowState,
+} from './SwatchPickerRow.types';
 export { renderSwatchPickerRow_unstable } from './renderSwatchPickerRow';
-export { useSwatchPickerRow_unstable } from './useSwatchPickerRow';
+export { useSwatchPickerRow_unstable, useSwatchPickerRowBase_unstable } from './useSwatchPickerRow';
 export { swatchPickerRowClassNames, useSwatchPickerRowStyles_unstable } from './useSwatchPickerRowStyles.styles';
