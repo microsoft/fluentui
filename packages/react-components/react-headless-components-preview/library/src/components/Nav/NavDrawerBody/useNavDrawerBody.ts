4aadb8354f4fa2fb5cf3b6c1ba8070a7e0c32a0b
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2026-08-22 13:50:57 +0200

    fix(react-headless-components-preview): restore navigation landmark role on Nav and NavDrawerBody

--- packages/react-components/react-headless-components-preview/library/src/components/Nav/NavDrawerBody/useNavDrawerBody.ts
+++ packages/react-components/react-headless-components-preview/library/src/components/Nav/NavDrawerBody/useNavDrawerBody.ts
@@ -1,1 +1,17 @@
-export { useDrawerBody as useNavDrawerBody } from '../../Drawer/DrawerBody';
+'use client';
+
+import type * as React from 'react';
+import type { NavDrawerBodyProps, NavDrawerBodyState } from './NavDrawerBody.types';
+import { useDrawerBody } from '../../Drawer/DrawerBody';
+
+/**
+ * Create the state required to render NavDrawerBody.
+ */
+export const useNavDrawerBody = (props: NavDrawerBodyProps, ref: React.Ref<HTMLElement>): NavDrawerBodyState => {
+  const state: NavDrawerBodyState = useDrawerBody(props, ref);
+
+  // eslint-disable-next-line react-hooks/immutability
+  state.root.role = 'navigation';
+
+  return state;
+};
