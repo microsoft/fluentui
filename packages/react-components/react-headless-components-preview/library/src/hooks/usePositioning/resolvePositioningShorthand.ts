4ce56675abcf715b7d05741580b0bb8971837a16
Victor Genaev <vgenaev@gmail.com>
2026-08-25 11:15:31 +0200

    Update packages/react-components/react-headless-components-preview/library/src/hooks/usePositioning/resolvePositioningShorthand.ts
    Co-authored-by: Dmytro Kirpa <kirpadv@gmail.com>

--- packages/react-components/react-headless-components-preview/library/src/hooks/usePositioning/resolvePositioningShorthand.ts
+++ packages/react-components/react-headless-components-preview/library/src/hooks/usePositioning/resolvePositioningShorthand.ts
@@ -1,8 +1,6 @@
 import { resolvePositioningShorthand as resolveCanonicalPositioningShorthand } from '@fluentui/react-positioning';
 import type { PositioningProps, PositioningShorthand } from './types';
 
-export function resolvePositioningShorthand(
-  shorthand: PositioningShorthand | undefined | null,
-): Readonly<PositioningProps> {
-  return resolveCanonicalPositioningShorthand(shorthand) as Readonly<PositioningProps>;
-}
+type ResolvePositioningShorthand = (shorthand: PositioningShorthand | undefined | null) => Readonly<PositioningProps>
+
+export const resolvePositioningShorthand = resolveCanonicalPositioningShorthand as ResolvePositioningShorthand;
