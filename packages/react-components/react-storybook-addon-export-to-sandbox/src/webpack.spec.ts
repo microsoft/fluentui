8711cce1de84fc6babb9117de89c9eec3dfab1f6
Atishay Jain <atisjai@microsoft.com>
2026-09-15 21:37:15 +0530

    Revert "fix(storybook): preserve addon options on Windows"
    This reverts commit ce2b5ce93741847b764d63908595b280d697e0fa.

--- packages/react-components/react-storybook-addon-export-to-sandbox/src/webpack.spec.ts
+++ packages/react-components/react-storybook-addon-export-to-sandbox/src/webpack.spec.ts
@@ -23,7 +23,7 @@ describe(`webpack`, () => {
             plugins: [
               [
                 expect.stringContaining('babel-preset-storybook-full-source'),
-                { importMappings: {}, cssModules: false },
+                { importMappings: undefined, cssModules: false },
               ],
             ],
           },
@@ -76,33 +76,6 @@ describe(`webpack`, () => {
     ]);
   });
 
-  it(`should register user provided options from a Windows preset path`, () => {
-    const actual = webpack({ module: { rules: [] } }, {
-      presetsList: [
-        {
-          name: 'node_modules\\@fluentui\\react-storybook-addon-export-to-sandbox\\lib\\preset.js',
-          preset: {},
-          options: {
-            importMappings: { '@proj/foo': { replace: '@proj/moo' } },
-          } as PresetConfig,
-        },
-      ],
-    } as WebpackFinalOptions);
-
-    expect(actual.module?.rules?.[0]).toMatchObject({
-      use: {
-        options: {
-          plugins: [
-            [
-              expect.stringContaining('babel-preset-storybook-full-source'),
-              { importMappings: { '@proj/foo': { replace: '@proj/moo' } }, cssModules: false },
-            ],
-          ],
-        },
-      },
-    });
-  });
-
   it.each([
     ['boolean true', true as const],
     ['object with tokensFilePath', { tokensFilePath: '/path/to/tokens.css' }],
@@ -126,7 +99,10 @@ describe(`webpack`, () => {
           options: {
             parserOpts: { plugins: ['typescript', 'jsx'] },
             plugins: [
-              [expect.stringContaining('babel-preset-storybook-full-source'), { importMappings: {}, cssModules }],
+              [
+                expect.stringContaining('babel-preset-storybook-full-source'),
+                { importMappings: undefined, cssModules },
+              ],
             ],
           },
         },
